f8a41f345e53bda9aee4542b6850ae9f00cd4a0f
Wannte <T.wannte@gmail.com>
2021-08-25 03:19:26 +0900

    feat: HttpRequest parsing

--- app/src/main/java/nextstep/jwp/RequestHandler.java
+++ app/src/main/java/nextstep/jwp/RequestHandler.java
@@ -1,19 +1,27 @@
 package nextstep.jwp;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import nextstep.jwp.model.HttpRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RequestHandler implements Runnable {
 
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private final Socket connection;
+    private String httpMethod;
+    private String httpUri;
+    private Map<String, List<String>> httpRequestHeaders;
 
     public RequestHandler(Socket connection) {
         this.connection = Objects.requireNonNull(connection);
@@ -21,10 +29,17 @@ public class RequestHandler implements Runnable {
 
     @Override
     public void run() {
-        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
+        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
+                connection.getPort());
 
         try (final InputStream inputStream = connection.getInputStream();
-             final OutputStream outputStream = connection.getOutputStream()) {
+                final OutputStream outputStream = connection.getOutputStream()) {
+
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            List<String> lines = reader.lines().collect(Collectors.toList());
+
+            HttpRequest httpRequest = HttpRequest.of(lines);
 
             final String responseBody = "Hello world!";
 
---
+++ app/src/main/java/nextstep/jwp/model/HttpRequest.java
@@ -0,0 +1,47 @@
+package nextstep.jwp.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class HttpRequest {
+    private final String method;
+    private final String uri;
+    private final Map<String, List<String>> headers;
+
+    private HttpRequest(String method, String uri,
+                       Map<String, List<String>> headers) {
+        this.method = method;
+        this.uri = uri;
+        this.headers = headers;
+    }
+
+    public static HttpRequest of(List<String> lines) {
+        String[] firstTokens = lines.get(0).split(" ");
+        String method = firstTokens[0];
+        String uri = firstTokens[1];
+
+        List<String> headersLines = lines.subList(1, lines.size());
+        Map<String, List<String>> headers = extractHeaders(headersLines);
+
+        return new HttpRequest(method, uri, headers);
+    }
+
+    private static Map<String, List<String>> extractHeaders(List<String> lines) {
+        Map<String, List<String>> headers = new HashMap<>();
+
+        for (String line: lines) {
+            if ("".equals(line)) {
+                break;
+            }
+            String[] splits = line.split(": ", 2);
+            String key = splits[0];
+            List<String> values = Stream.of(splits[1].split(",")).map(String::trim).collect(Collectors.toList());
+            headers.put(key, values);
+        }
+        return headers;
+    }
+}

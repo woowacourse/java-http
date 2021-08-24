e0fe9989e8016f4dff7a8a289b0b69f97507d12d
sihyung92 <fjzjqhdl@gmail.com>
2021-08-24 13:11:12 +0900

    feat: FileTest 학습테스트 구현

--- app/src/test/java/nextstep/learning/http/FileTest.java
+++ app/src/test/java/nextstep/learning/http/FileTest.java
@@ -1,5 +1,9 @@
 package nextstep.learning.http;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,8 +29,9 @@ class FileTest {
     void resource_디렉터리에_있는_파일의_경로를_찾는다() {
         final String fileName = "nextstep.txt";
 
-        // todo
-        final String actual = "";
+        URL resource = getClass().getClassLoader().getResource(fileName);
+        File file = new File(resource.getPath());
+        final String actual = file.getName();
 
         assertThat(actual).endsWith(fileName);
     }
@@ -36,14 +41,12 @@ class FileTest {
      * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws IOException {
         final String fileName = "nextstep.txt";
 
-        // todo
-        final Path path = null;
-
-        // todo
-        final List<String> actual = Lists.emptyList();
+        final URL resource = getClass().getClassLoader().getResource(fileName);
+        final Path path = new File(resource.getPath()).toPath();
+        final List<String> actual = Files.readAllLines(path);
 
         assertThat(actual).containsOnly("nextstep");
     }

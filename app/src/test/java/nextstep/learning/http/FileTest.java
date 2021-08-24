99a4afde6b8eed5d894a1dabd588b32ed01a4c10
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-08-24 21:48:53 +0900

    test(FileTest): 파일 클래스 학습 테스트 작성

--- app/src/test/java/nextstep/learning/http/FileTest.java
+++ app/src/test/java/nextstep/learning/http/FileTest.java
@@ -1,11 +1,16 @@
 package nextstep.learning.http;
 
-import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,7 +31,10 @@ class FileTest {
         final String fileName = "nextstep.txt";
 
         // todo
-        final String actual = "";
+        final ClassLoader classLoader = getClass().getClassLoader();
+        final File file = new File(Objects.requireNonNull(classLoader.getResource(fileName)).getFile());
+
+        final String actual = file.getName();
 
         assertThat(actual).endsWith(fileName);
     }
@@ -36,15 +44,16 @@ class FileTest {
      * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws IOException {
         final String fileName = "nextstep.txt";
 
         // todo
-        final Path path = null;
+        final ClassLoader classLoader = getClass().getClassLoader();
+        final File file = new File(Objects.requireNonNull(classLoader.getResource(fileName)).getFile());
+        final String path = URLDecoder.decode(file.getAbsolutePath(), StandardCharsets.UTF_8);
+        final List<String> actual = Files.readAllLines(Path.of(path));
 
         // todo
-        final List<String> actual = Lists.emptyList();
-
         assertThat(actual).containsOnly("nextstep");
     }
 }

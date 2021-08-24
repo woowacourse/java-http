a21253da850c89ecf126c98d78c922d0306d1d0d
jaeseongDev <qkrwotjd1445@naver.com>
2021-08-24 22:02:55 +0900

    test: GET login.html 요청에 대한 테스트 코드 추가

--- app/src/test/java/nextstep/jwp/RequestHandlerTest.java
+++ app/src/test/java/nextstep/jwp/RequestHandlerTest.java
@@ -38,4 +38,31 @@ class RequestHandlerTest {
                 new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
         assertThat(socket.output()).isEqualTo(expected);
     }
+
+    @Test
+    @DisplayName("GET /login.html로 요청을 할 경우, resources/static/login.html을 response로 응답한다.")
+    void login() throws IOException {
+        // given
+        final String httpRequest= String.join("\r\n",
+            "GET /login.html HTTP/1.1 ",
+            "Host: localhost:8080 ",
+            "Connection: keep-alive ",
+            "",
+            "");
+
+        final MockSocket socket = new MockSocket(httpRequest);
+        final RequestHandler requestHandler = new RequestHandler(socket);
+
+        // when
+        requestHandler.run();
+
+        // then
+        final URL resource = getClass().getClassLoader().getResource("static/login.html");
+        String expected = "HTTP/1.1 200 OK \r\n" +
+            "Content-Type: text/html;charset=utf-8 \r\n" +
+            "Content-Length: 3796 \r\n" +
+            "\r\n"+
+            new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+        assertThat(socket.output()).isEqualTo(expected);
+    }
 }

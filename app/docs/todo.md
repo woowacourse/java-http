4d81d92e520533c5effe90f6db9af637780c755b
seunghyun <ggyool@naver.com>
2021-08-26 16:51:13 +0900

    feat: HttpStatusCode enum 생성

--- app/docs/todo.md
+++ app/docs/todo.md
@@ -1,7 +1,8 @@
 ## 🚀 1단계 - HTTP 서버 구현하기
 ### ✏️ GET /index.html 응답하기
-- [ ] http request 를 파싱하기
+- [x] http request 를 파싱하기
   - [x] StringUtils 만들기
+- [ ] http response 만들기
 - [ ] 추출한 uri 정보에 매핑된 resource 가 있는지 확인한다.
     - [ ] 매핑이 있으면 적절한 응답을 보낸다. 
     - [ ] 매핑이 없으면 404를 응답한다. 
---
+++ app/src/main/java/nextstep/jwp/http/HttpStatusCode.java
@@ -0,0 +1,44 @@
+package nextstep.jwp.http;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public enum HttpStatusCode {
+    OK(200, "OK"),
+    CREATED(201, "Created"),
+    NO_CONTENT(204, "No Content"),
+    MOVED_PERMANENTLY(301, "Moved Permanently"),
+    FOUND(302, "Found"),
+    BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    CONFLICT(409, "Conflict"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+
+    private final int code;
+    private final String description;
+
+    HttpStatusCode(int code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public static HttpStatusCode from(int code) {
+        return Arrays.stream(values())
+                .filter(httpStatusCode -> httpStatusCode.code == code)
+                .findAny()
+                .orElseThrow(() -> new NoSuchElementException(
+                        String.format("서버에서 지원하는 HttpStatusCode가 아닙니다.(%s)", code))
+                );
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}

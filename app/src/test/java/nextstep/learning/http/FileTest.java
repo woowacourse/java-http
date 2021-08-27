356a24475e525d60d500abc75951b3b25fa1c457
dwl5 <ssop6403@gmail.com>
2021-08-27 10:57:00 +0900

    test: File 학습테스트 진행

--- app/src/test/java/nextstep/learning/http/FileTest.java
+++ app/src/test/java/nextstep/learning/http/FileTest.java
@@ -3,7 +3,12 @@ package nextstep.learning.http;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.util.io.IOUtil;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
@@ -26,7 +31,8 @@ class FileTest {
         final String fileName = "nextstep.txt";
 
         // todo
-        final String actual = "";
+        final URL resource = getClass().getClassLoader().getResource(fileName);
+        final String actual = resource.getFile();
 
         assertThat(actual).endsWith(fileName);
     }
@@ -36,14 +42,16 @@ class FileTest {
      * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws IOException {
         final String fileName = "nextstep.txt";
 
         // todo
-        final Path path = null;
+        final URL resource = getClass().getClassLoader().getResource(fileName);
+        final Path path = new File(resource.getPath()).toPath();
+
 
         // todo
-        final List<String> actual = Lists.emptyList();
+        final List<String> actual = Files.readAllLines(path);
 
         assertThat(actual).containsOnly("nextstep");
     }

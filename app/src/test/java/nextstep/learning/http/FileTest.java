16bbc12eab958178e22b8dd7cc4d390f9a3f850d
rok93 <goodboy302@naver.com>
2021-08-27 16:56:42 +0900

    refactor: java.io.File은 이제 레거시로 간주됨에 따라 직접적으로 File 생성자를 호출하지 않도록 변경
    참고자료: https://www.baeldung.com/java-path-vs-file

--- app/src/test/java/nextstep/learning/http/FileTest.java
+++ app/src/test/java/nextstep/learning/http/FileTest.java
@@ -1,10 +1,8 @@
 package nextstep.learning.http;
 
-import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -28,11 +26,10 @@ class FileTest {
      * resource 디렉터리의 경로는 어떻게 알아낼 수 있을까?
      */
     @Test
-    void resource_디렉터리에_있는_파일의_경로를_찾는다() throws URISyntaxException {
+    void resource_디렉터리에_있는_파일의_경로를_찾는다() {
         final String fileName = "nextstep.txt";
         URL resource = getClass().getClassLoader().getResource(fileName);
-        File file = Paths.get(resource.toURI()).toFile();
-        String actual = file.getAbsolutePath();
+        String actual = resource.getFile();
 
         assertThat(actual).endsWith(fileName);
     }
@@ -42,11 +39,11 @@ class FileTest {
      * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() throws IOException {
+    void 파일의_내용을_읽는다() throws IOException, URISyntaxException {
         final String fileName = "nextstep.txt";
 
         URL resource = getClass().getClassLoader().getResource(fileName);
-        final Path path = new File(resource.getPath()).toPath();
+        final Path path = Paths.get(resource.toURI());
         final List<String> actual = Files.readAllLines(path);
 
         assertThat(actual).containsOnly("nextstep");

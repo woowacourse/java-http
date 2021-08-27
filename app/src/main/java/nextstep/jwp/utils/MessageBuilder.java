655e3e594fd39afa8bcd5c23009558175a9f3766
sjpark-dev <sjpark.dev@gmail.com>
2021-08-27 14:00:06 +0900

    feat: Query String 파싱 & HTTP Status Code 302 구현

--- app/src/main/java/nextstep/jwp/utils/MessageBuilder.java
+++ app/src/main/java/nextstep/jwp/utils/MessageBuilder.java
@@ -5,15 +5,22 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Optional;
+import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.model.Request;
+import nextstep.jwp.model.User;
 
 public class MessageBuilder {
+
+    public static final String FILE_EXTENSION = ".html";
 
     private MessageBuilder() {
     }
 
     public static String build(Request request) throws IOException {
-        if ("/".equals(request.getPath())) {
+        String requestPath = request.getPath();
+
+        if ("/".equals(requestPath)) {
             final String responseBody = "Hello world!";
 
             return String.join("\r\n",
@@ -24,7 +31,46 @@ public class MessageBuilder {
                     responseBody);
         }
 
-        final URL resource = MessageBuilder.class.getClassLoader().getResource("static" + request.getPath());
+        if (!requestPath.contains(FILE_EXTENSION)) {
+            String uri = requestPath;
+            int index = uri.indexOf("?");
+            if (index == -1) {
+                requestPath += FILE_EXTENSION;
+
+                final URL resource = MessageBuilder.class.getClassLoader().getResource("static" + requestPath);
+                final Path path = new File(resource.getPath()).toPath();
+                final String responseBody = new String(Files.readAllBytes(path));
+
+                return String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody);
+            }
+
+            String queryString = uri.substring(index + 1);
+            String[] queries = queryString.split("&");
+            String account = queries[0].split("=")[1];
+            String password = queries[1].split("=")[1];
+            Optional<User> dbUser = InMemoryUserRepository.findByAccount(account);
+
+            if (dbUser.isPresent() && dbUser.get().checkPassword(password)) {
+                return String.join("\r\n",
+                        "HTTP/1.1 302 FOUND ",
+                        "Location: /index.html ",
+                        "",
+                        "");
+            }
+
+            return String.join("\r\n",
+                    "HTTP/1.1 302 FOUND ",
+                    "Location: /401.html ",
+                    "",
+                    "");
+        }
+
+        final URL resource = MessageBuilder.class.getClassLoader().getResource("static" + requestPath);
         final Path path = new File(resource.getPath()).toPath();
         final String responseBody = new String(Files.readAllBytes(path));
 
--- app/src/test/java/nextstep/jwp/RequestHandlerTest.java
+++ app/src/test/java/nextstep/jwp/RequestHandlerTest.java
@@ -55,4 +55,83 @@ class RequestHandlerTest {
                 responseBody;
         assertThat(socket.output()).isEqualTo(expected);
     }
+
+    @Test
+    void login() throws IOException {
+        // given
+        final String httpRequest = String.join("\r\n",
+                "GET /login HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+        final MockSocket socket = new MockSocket(httpRequest);
+        final RequestHandler requestHandler = new RequestHandler(socket);
+
+        // when
+        requestHandler.run();
+
+        // then
+        final URL resource = getClass().getClassLoader().getResource("static/login.html");
+        String responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+        String expected = "HTTP/1.1 200 OK \r\n" +
+                "Content-Type: text/html;charset=utf-8 \r\n" +
+                "Content-Length: " + responseBody.getBytes().length + " \r\n" +
+                "\r\n" +
+                responseBody;
+        assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @Test
+    void loginWithCorrectQueryString() throws IOException {
+        // given
+        final String httpRequest = String.join("\r\n",
+                "GET /login?account=gugu&password=password HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+        final MockSocket socket = new MockSocket(httpRequest);
+        final RequestHandler requestHandler = new RequestHandler(socket);
+
+        // when
+        requestHandler.run();
+
+        // then
+        final URL resource = getClass().getClassLoader().getResource("static/login.html");
+        String responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+        String expected = "HTTP/1.1 302 FOUND \r\n" +
+                "Location: /index.html \r\n" +
+                "\r\n" +
+                "";
+        assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @Test
+    void loginWithIncorrectQueryString() throws IOException {
+        // given
+        final String httpRequest = String.join("\r\n",
+                "GET /login?account=gugu&password=notpassword HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+        final MockSocket socket = new MockSocket(httpRequest);
+        final RequestHandler requestHandler = new RequestHandler(socket);
+
+        // when
+        requestHandler.run();
+
+        // then
+        final URL resource = getClass().getClassLoader().getResource("static/login.html");
+        String responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+        String expected = "HTTP/1.1 302 FOUND \r\n" +
+                "Location: /401.html \r\n" +
+                "\r\n" +
+                "";
+        assertThat(socket.output()).isEqualTo(expected);
+    }
 }

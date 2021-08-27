e85ffbcab57c1a5e30771fdfa825fa06131c8a7d
Hyeon9mak <jinha3507@gmail.com>
2021-08-27 18:26:53 +0900

    refactor: Request-Line 파싱 보완

--- app/src/main/java/nextstep/jwp/http/request/requestline/RequestLine.java
+++ app/src/main/java/nextstep/jwp/http/request/requestline/RequestLine.java
@@ -1,10 +1,13 @@
 package nextstep.jwp.http.request.requestline;
 
+import nextstep.jwp.exception.InvalidRequestLineException;
+
 public class RequestLine {
 
     private static final int METHOD_INDEX = 0;
     private static final int URI_INDEX = 1;
     private static final int VERSION_INDEX = 2;
+    private static final int EXPECT_REQUEST_LINE_LENGTH = 3;
 
     private final Method method;
     private final RequestURI requestURI;
@@ -17,12 +20,22 @@ public class RequestLine {
     }
 
     public static RequestLine parse(String requestLine) {
-        String[] splitedRequestLine = requestLine.split(" ");
+        String[] splitedLine = splitRequestLine(requestLine);
 
-        Method method = Method.matchOf(splitedRequestLine[METHOD_INDEX]);
-        RequestURI requestURI = new RequestURI(splitedRequestLine[URI_INDEX]);
-        HttpVersion httpVersion = HttpVersion.matchOf(splitedRequestLine[VERSION_INDEX]);
+        Method method = Method.matchOf(splitedLine[METHOD_INDEX]);
+        RequestURI requestURI = new RequestURI(splitedLine[URI_INDEX]);
+        HttpVersion httpVersion = HttpVersion.matchOf(splitedLine[VERSION_INDEX]);
 
         return new RequestLine(method, requestURI, httpVersion);
+    }
+
+    private static String[] splitRequestLine(String requestLine) {
+        String[] splitedLine = requestLine.split(" ");
+
+        if (splitedLine.length != EXPECT_REQUEST_LINE_LENGTH) {
+            throw new InvalidRequestLineException();
+        }
+
+        return splitedLine;
     }
 }
--- app/src/main/java/nextstep/jwp/http/request/requestline/RequestURI.java
+++ app/src/main/java/nextstep/jwp/http/request/requestline/RequestURI.java
@@ -1,5 +1,8 @@
 package nextstep.jwp.http.request.requestline;
 
+import java.util.Objects;
+import nextstep.jwp.exception.InvalidRequestUriException;
+
 public class RequestURI {
 
     private final String value;
@@ -7,5 +10,12 @@ public class RequestURI {
     // TODO: '*'이 들어올 경우 모든 경로를 처리한다.
     public RequestURI(String value) {
         this.value = value;
+        validateNull(this.value);
+    }
+
+    private void validateNull(String value) {
+        if (Objects.isNull(value)) {
+            throw new InvalidRequestUriException();
+        }
     }
 }

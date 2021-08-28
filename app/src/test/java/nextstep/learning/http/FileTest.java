29156dce061be6f6264cca5a5c57c85dd8299295
pika96 <yjwdbswldn@naver.com>
2021-08-28 13:05:53 +0900

    test: 파일 학습 테스트 완료

--- app/src/test/java/nextstep/learning/http/FileTest.java
+++ app/src/test/java/nextstep/learning/http/FileTest.java
@@ -1,5 +1,12 @@
 package nextstep.learning.http;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,40 +17,40 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * 웹서버는 사용자가 요청한 html 파일을 제공 할 수 있어야 한다.<br>
- * File 클래스를 사용해서 파일을 읽어오고, 사용자에게 전달한다.
+ * 웹서버는 사용자가 요청한 html 파일을 제공 할 수 있어야 한다.<br> File 클래스를 사용해서 파일을 읽어오고, 사용자에게 전달한다.
  */
 @DisplayName("File 클래스 학습 테스트")
 class FileTest {
 
     /**
-     * File 객체를 생성하려면 파일의 경로를 알아야 한다.<br>
-     * 자바 애플리케이션은 resource 디렉터리에 정적 파일을 저장한다.<br>
-     * resource 디렉터리의 경로는 어떻게 알아낼 수 있을까?
+     * File 객체를 생성하려면 파일의 경로를 알아야 한다.<br> 자바 애플리케이션은 resource 디렉터리에 정적 파일을 저장한다.<br> resource 디렉터리의
+     * 경로는 어떻게 알아낼 수 있을까?
      */
     @Test
     void resource_디렉터리에_있는_파일의_경로를_찾는다() {
         final String fileName = "nextstep.txt";
 
         // todo
-        final String actual = "";
+        String file = getClass().getClassLoader().getResource("nextstep.txt").getFile();
+
+        final String actual = file;
 
         assertThat(actual).endsWith(fileName);
     }
 
     /**
-     * 읽어온 파일의 내용을 I/O Stream을 사용해서 사용자에게 전달 해야 한다.
-     * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
+     * 읽어온 파일의 내용을 I/O Stream을 사용해서 사용자에게 전달 해야 한다. File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws IOException {
         final String fileName = "nextstep.txt";
 
         // todo
-        final Path path = null;
+        String fileUrl = getClass().getClassLoader().getResource(fileName).getFile();
+        final Path path = Path.of(fileUrl);
 
         // todo
-        final List<String> actual = Lists.emptyList();
+        final List<String> actual = Files.readAllLines(path);
 
         assertThat(actual).containsOnly("nextstep");
     }

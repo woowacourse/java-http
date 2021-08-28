28f51d22637ab999ad9b5cb3c6700e451823d2e5
Hyeon9mak <jinha3507@gmail.com>
2021-08-28 12:42:20 +0900

    refactor: 인메모리 DB 인스턴스화

--- app/src/main/java/nextstep/jwp/controller/Controllers.java
+++ app/src/main/java/nextstep/jwp/controller/Controllers.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.http.request.HttpRequest;
 import nextstep.jwp.http.response.HttpResponse;
+import nextstep.jwp.service.LoginService;
+import nextstep.jwp.service.RegisterService;
 import nextstep.jwp.service.StaticResourceService;
 
 public class Controllers {
@@ -19,12 +22,15 @@ public class Controllers {
     }
 
     public static Controllers loadContext() {
+        InMemoryUserRepository userRepository = InMemoryUserRepository.initialize();
+        LoginService loginService = new LoginService(userRepository);
+        RegisterService registerService = new RegisterService(userRepository);
         StaticResourceService staticResourceService = new StaticResourceService();
 
         Controller staticResourceController = new StaticResourceController(staticResourceService);
 
-        Controller loginController = new LoginController(staticResourceService);
-        Controller registerController = new RegisterController(staticResourceService);
+        Controller loginController = new LoginController(loginService, staticResourceService);
+        Controller registerController = new RegisterController(registerService, staticResourceService);
         List<Controller> controllers = Arrays.asList(loginController, registerController);
 
         return new Controllers(controllers, staticResourceController);

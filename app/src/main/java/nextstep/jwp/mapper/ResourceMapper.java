7c669e70162aa0a1dfb34fd78527cfb227f8c713
ecsimsw <ecsimsw@gmail.com>
2021-08-29 04:25:26 +0900

    feat : 자원 존재 확인 구현

--- app/src/main/java/nextstep/jwp/mapper/ResourceMapper.java
+++ app/src/main/java/nextstep/jwp/mapper/ResourceMapper.java
@@ -1,24 +1,29 @@
 package nextstep.jwp.mapper;
 
-import nextstep.jwp.http.request.RequestLine;
-import nextstep.jwp.http.request.RequestUriPath;
-import nextstep.jwp.http.response.HttpStatus;
+import java.io.File;
+import java.net.URL;
 import nextstep.jwp.handler.Model;
 import nextstep.jwp.handler.ModelAndView;
+import nextstep.jwp.http.request.RequestLine;
+import nextstep.jwp.http.response.HttpStatus;
 
 public class ResourceMapper implements HandlerMapper {
+
+    private static final String RESOURCE_BASE_PATH = "/static";
 
     @Override
     public ModelAndView mapping(RequestLine requestLine) {
         String method = requestLine.getMethod();
-        RequestUriPath uriPath = requestLine.getUriPath();
+        String filePath = requestLine.getUriPath().getPath();
 
-        if (method.equalsIgnoreCase("GET")) {
-            // TODO :: 자원 존재 확인
-            String path = uriPath.getPath();
-            String viewPath = path.substring(1);
-            return new ModelAndView(Model.of(HttpStatus.OK), viewPath);
+        if (method.equalsIgnoreCase("GET") && isExistResource(filePath)) {
+            return new ModelAndView(Model.of(HttpStatus.OK), filePath);
         }
         return null;
+    }
+
+    private boolean isExistResource(String fileName) {
+        final URL resourceUrl = getClass().getResource(RESOURCE_BASE_PATH + fileName);
+        return new File(resourceUrl.getFile()).exists();
     }
 }

475c81680838ae6777f52ecbaef22d5681dc4680
seunghyun <ggyool@naver.com>
2021-08-29 07:10:12 +0900

    refactor: 로그인 Post로 변경

--- app/src/main/java/nextstep/jwp/controller/LoginController.java
+++ app/src/main/java/nextstep/jwp/controller/LoginController.java
@@ -2,38 +2,46 @@ package nextstep.jwp.controller;
 
 import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.exception.UnauthorizedException;
-import nextstep.jwp.http.HttpPath;
-import nextstep.jwp.http.HttpStatusCode;
+import nextstep.jwp.http.message.MessageBody;
 import nextstep.jwp.http.message.request.HttpRequestMessage;
 import nextstep.jwp.http.message.response.HttpResponseMessage;
 import nextstep.jwp.model.User;
+import nextstep.jwp.utils.HttpParseUtils;
 
 import java.util.Map;
+import java.util.Objects;
 
 public class LoginController extends AbstractController {
 
     @Override
     protected void doGet(HttpRequestMessage httpRequestMessage, HttpResponseMessage httpResponseMessage) {
-        HttpPath httpPath = httpRequestMessage.requestPath();
-        if (!httpPath.hasQueryString()) {
-            httpRequestMessage.changeRequestUri("/login.html");
-            return;
-        }
+        httpRequestMessage.changeRequestUri("/login.html");
+    }
 
-        Map<String, String> queryParams = httpPath.extractQueryParams();
-        String account = queryParams.get("account");
-        String password = queryParams.get("password");
+    @Override
+    protected void doPost(HttpRequestMessage httpRequestMessage, HttpResponseMessage httpResponseMessage) {
+        MessageBody messageBody = httpRequestMessage.getBody();
+        Map<String, String> formData = HttpParseUtils.extractFormData(messageBody);
+        String account = formData.get("account");
+        String password = formData.get("password");
         login(account, password);
 
         httpRequestMessage.changeRequestUri("redirect:/index.html");
     }
 
     private User login(String account, String password) {
+        validateRequiredValue(account, password);
         User user = InMemoryUserRepository.findByAccount(account)
                 .orElseThrow(() -> new UnauthorizedException(String.format("해당 아이디가 없습니다.(%s)", account)));
         if (!user.checkPassword(password)) {
             throw new UnauthorizedException("로그인에 싪패했습니다.");
         }
         return user;
+    }
+
+    void validateRequiredValue(String account, String password) {
+        if (Objects.isNull(account) || Objects.isNull(password)) {
+            throw new UnauthorizedException("아이디 또는 비밀번호를 입력하지 않았습니다.");
+        }
     }
 }

8f8685165c3ca842c984eacfa584f9277145cd53
xrabcde <xrabcde@gmail.com>
2021-08-28 17:40:22 +0900

    feat: Query String 파싱

--- app/src/main/java/nextstep/jwp/RequestHandler.java
+++ app/src/main/java/nextstep/jwp/RequestHandler.java
@@ -1,5 +1,7 @@
 package nextstep.jwp;
 
+import nextstep.jwp.db.InMemoryUserRepository;
+import nextstep.jwp.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,7 +37,26 @@ public class RequestHandler implements Runnable {
                 return;
             }
             final String[] request = line.split(" ");
-            final String fileName = request[1];
+            final String uri = request[1];
+            String fileName = uri;
+            String queryString = null;
+
+            if (uri.startsWith("/login")) {
+                int index = uri.indexOf("?");
+                if (index == -1) {
+                    fileName = "/login.html";
+                } else {
+                    fileName = uri.substring(0, index) + ".html";
+                    queryString = uri.substring(index + 1);
+
+                    int andIndex = queryString.indexOf("&");
+                    String account = getDataFromQueryString(queryString.substring(0, andIndex));
+                    String password = getDataFromQueryString(queryString.substring(andIndex));
+
+                    User user = InMemoryUserRepository.findByAccount(account).orElseThrow();
+                    user.checkPassword(password);
+                }
+            }
 
             while (!"".equals(line)) {
                 line = bufferedReader.readLine();
@@ -60,6 +81,9 @@ public class RequestHandler implements Runnable {
         } finally {
             close();
         }
+    }
+    private String getDataFromQueryString(String data) {
+        return data.substring(data.indexOf("=") + 1);
     }
 
     private File getResources(String fileName) {

f88dffad152cc3a2595a7dd96b0f8c908b5b08bc
김주원 <kjw11077@naver.com>
2021-08-29 13:55:07 +0900

    feat: Router 수정

--- app/src/main/java/nextstep/jwp/webserver/Router.java
+++ app/src/main/java/nextstep/jwp/webserver/Router.java
@@ -3,33 +3,29 @@ package nextstep.jwp.webserver;
 import nextstep.jwp.application.controller.HelloWorldController;
 import nextstep.jwp.application.controller.LoginController;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Router {
 
-    private static final Map<String, Class<? extends Controller>> router = new HashMap<>();
+    private static final Map<String, Controller> router = new HashMap<>();
 
     static {
-        add("/", HelloWorldController.class);
-        add("/login", LoginController.class);
+        add("/", new HelloWorldController());
+        add("/login", new LoginController());
     }
 
     private Router() {
     }
 
-    public static void add(String path, Class<? extends Controller> controller) {
+    public static void add(String path, Controller controller) {
         router.put(path, controller);
     }
 
     public static Controller get(String path) {
-        try {
-            return router.get(path).getConstructor().newInstance();
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-            e.printStackTrace();
+        if (!router.containsKey(path)) {
+            return null;
         }
-        // todo 라우팅 실패
-        return null;
+        return router.get(path);
     }
 }

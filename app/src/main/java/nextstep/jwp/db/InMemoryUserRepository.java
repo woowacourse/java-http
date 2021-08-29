447759eca4785c7b698781e5f364517c640aea2b
Byung Wook Park <bbwwpark@naver.com>
2021-08-29 23:21:44 +0900

    refactor: 유저 저장시 id를 자동으로 넣어주도록 수정

--- app/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
+++ app/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
@@ -10,13 +10,16 @@ import java.util.concurrent.ConcurrentHashMap;
 public class InMemoryUserRepository {
 
     private static final Map<String, User> database = new ConcurrentHashMap<>();
+    private static long id = 0;
 
     static {
-        final User user = new User(1, "gugu", "password", "hkkang@woowahan.com");
+        final User user = new User("gugu", "password", "hkkang@woowahan.com");
+        user.setId(++id);
         database.put(user.getAccount(), user);
     }
 
     public static void save(User user) {
+        user.setId(++id);
         database.put(user.getAccount(), user);
     }
 

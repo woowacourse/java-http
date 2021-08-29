75531607d37b5b33ca9b49f29fd9cc0e7ec767ac
Seed <hsik0225@gmail.com>
2021-08-30 02:13:45 +0900

    feat: 기존 HttpHeader 와 HttpHeaders 를 통합

--- app/src/main/java/nextstep/jwp/framework/http/HttpHeaders.java
+++ app/src/main/java/nextstep/jwp/framework/http/HttpHeaders.java
@@ -1,43 +1,68 @@
 package nextstep.jwp.framework.http;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class HttpHeaders {
-    private final ConcurrentLinkedQueue<HttpHeader> headers;
+
+    // General Header
+    public static final String CONNECTION = "Connection";
+    public static final String DATE = "Date";
+
+    // Request Header
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String COOKIE = "Cookie";
+    public static final String USER_AGENT = "User-Agent";
+    public static final String HOST = "Host";
+    public static final String REFERER = "Referer";
+    public static final String ORIGIN = "Origin";
+
+    // Entity Header,
+    public static final String CONTENT_LENGTH = "Content-Length";
+    public static final String CONTENT_TYPE = "Content-Type";
+
+    // Response Header,
+    public static final String LOCATION = "Location";
+    public static final String SERVER = "Server";
+    public static final String SET_COOKIE = "Set-Cookie";
+
+    private final Map<String, String> headers;
 
     public HttpHeaders(HttpHeaders httpHeaders) {
         this(httpHeaders.headers);
     }
 
     public HttpHeaders() {
-        this(new ConcurrentLinkedQueue<>());
+        this(new LinkedHashMap<>());
     }
 
-    public HttpHeaders(ConcurrentLinkedQueue<HttpHeader> headers) {
-        this.headers = new ConcurrentLinkedQueue<>(headers);
+    public HttpHeaders(Map<String, String> httpHeaders) {
+        this.headers = httpHeaders;
     }
 
-    public HttpHeaders addHeader(String name, String... values) {
-        return addHeader(name, Arrays.asList(values));
+    public Map<String, String> getHeaders() {
+        return headers;
     }
 
-    public HttpHeaders addHeader(String name, List<String> values) {
-        headers.add(new HttpHeader(name, values));
+    public String get(String name) {
+        return headers.get(name);
+    }
+
+    public HttpHeaders addHeader(String name, String value) {
+        headers.put(name, value);
         return this;
     }
 
-    public HttpHeaders addHeader(HttpHeader httpHeader) {
-        headers.add(httpHeader);
+    public HttpHeaders addAll(HttpHeaders headers) {
+        this.headers.putAll(headers.headers);
         return this;
-    }
-
-    public HttpHeader poll() {
-        return headers.poll();
     }
 
     public boolean isEmpty() {
         return headers.isEmpty();
+    }
+
+    public boolean contains(String name) {
+        return headers.containsKey(name);
     }
 }

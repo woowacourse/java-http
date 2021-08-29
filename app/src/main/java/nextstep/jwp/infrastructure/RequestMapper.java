17e4f0cf3413e0de6ea0f842f504ad498a0619ab
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-08-29 15:59:19 +0900

    feat: Processor및 Mapper 추가

---
+++ app/src/main/java/nextstep/jwp/infrastructure/RequestMapper.java
@@ -0,0 +1,12 @@
+package nextstep.jwp.infrastructure;
+
+import nextstep.jwp.infrastructure.processor.RequestProcessor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestMapper {
+
+    private final static Map<String, RequestProcessor> MAPPER = new HashMap<>();
+
+}
---
+++ app/src/main/java/nextstep/jwp/infrastructure/processor/LoginRequestProcessor.java
@@ -0,0 +1,4 @@
+package nextstep.jwp.infrastructure.processor;
+
+public class LoginRequestProcessor {
+}

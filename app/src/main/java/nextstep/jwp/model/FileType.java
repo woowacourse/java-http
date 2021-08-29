8f3405848a01f31305b25a96829aa74a4bbb2c05
sjpark-dev <sjpark.dev@gmail.com>
2021-08-29 18:10:09 +0900

    feat: favicon 추가

--- app/src/main/java/nextstep/jwp/model/FileType.java
+++ app/src/main/java/nextstep/jwp/model/FileType.java
@@ -4,7 +4,8 @@ public enum FileType {
 
     HTML("text/html", ".html"),
     CSS("text/css", ".css"),
-    JS("application/js", ".js");
+    JS("application/js", ".js"),
+    ICO("image/x-icon", ".ico");
 
     private String type;
     private String extension;

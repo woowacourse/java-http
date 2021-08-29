6bef25ed3a16aebc7a1b1bf3d47c243a977542aa
sihyung92 <fjzjqhdl@gmail.com>
2021-08-30 02:32:48 +0900

    refactor: requestMapping 객체 추가

---
+++ app/src/main/java/nextstep/jwp/web/handler/RequestMapping.java
@@ -0,0 +1,47 @@
+package nextstep.jwp.web.handler;
+
+import static nextstep.jwp.web.http.request.HttpMethod.*;
+
+import java.util.Map;
+import nextstep.jwp.application.controller.LoginController;
+import nextstep.jwp.application.controller.RegisterController;
+import nextstep.jwp.application.controller.WelcomeController;
+import nextstep.jwp.web.http.request.MethodUrl;
+import nextstep.jwp.web.mvc.controller.Controller;
+import nextstep.jwp.web.mvc.controller.ExceptionController;
+import nextstep.jwp.web.mvc.controller.NotFoundController;
+
+public class RequestMapping {
+
+    private static final Map<MethodUrl, Controller> myMap;
+    private final Map<MethodUrl, Controller> controllerMap;
+    private final Controller NOT_FOUND_CACHE = new NotFoundController();
+    private final ExceptionController ERROR_CACHE = new ExceptionController();
+
+    static {
+        myMap = Map.of(
+            new MethodUrl(GET, "/index"), new WelcomeController(),
+            new MethodUrl(GET, "/login"), new LoginController(),
+            new MethodUrl(POST, "/login"), new LoginController(),
+            new MethodUrl(GET, "/register"), new LoginController(),
+            new MethodUrl(POST, "/register"), new RegisterController()
+        );
+    }
+
+    public RequestMapping() {
+        this(myMap);
+    }
+
+    public RequestMapping(
+        Map<MethodUrl, Controller> myMap) {
+        this.controllerMap = myMap;
+    }
+
+    public Controller getController(MethodUrl methodUrl) {
+        return myMap.getOrDefault(methodUrl, NOT_FOUND_CACHE);
+    }
+
+    public ExceptionController errorController() {
+        return ERROR_CACHE;
+    }
+}

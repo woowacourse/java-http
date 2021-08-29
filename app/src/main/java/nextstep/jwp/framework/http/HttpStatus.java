9f4ccacb2c9a0f7b7d743c0ab088f9318840204a
Seed <hsik0225@gmail.com>
2021-08-29 09:05:49 +0900

    feat: 사용할 것 같은 Status Code 추가

--- app/src/main/java/nextstep/jwp/framework/http/HttpStatus.java
+++ app/src/main/java/nextstep/jwp/framework/http/HttpStatus.java
@@ -2,8 +2,12 @@ package nextstep.jwp.framework.http;
 
 public enum HttpStatus {
     OK(200, "OK"),
+    CREATED(201, "Created"),
     FOUND(302, "FOUND"),
-    NOT_FOUND(404, "NOT FOUND");
+    BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    NOT_FOUND(404, "NOT FOUND"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
 
     private final int code;
     private final String reasonPhrase;

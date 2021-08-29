6c1895fe678723d07fd683812bc3a27688d7fc10
sihyung92 <fjzjqhdl@gmail.com>
2021-08-30 02:33:29 +0900

    refactor: 상태에 따라 분기하는 WebHandler 객체 추가

---
+++ app/src/main/java/nextstep/jwp/web/handler/WebStatusHandler.java
@@ -0,0 +1,63 @@
+package nextstep.jwp.web.handler;
+
+import static nextstep.jwp.web.http.response.HttpStatus.FOUND;
+import static nextstep.jwp.web.http.response.HttpStatus.INTERNAL_ERROR;
+import static nextstep.jwp.web.http.response.HttpStatus.NOT_FOUND;
+import static nextstep.jwp.web.http.response.HttpStatus.OK;
+
+import java.util.List;
+import nextstep.jwp.web.exception.NotFoundException;
+import nextstep.jwp.web.http.MimeType;
+import nextstep.jwp.web.http.request.HttpRequest;
+import nextstep.jwp.web.http.response.HttpResponse;
+import nextstep.jwp.web.http.response.body.HttpResponseBody;
+import nextstep.jwp.web.resolver.DataResolver;
+import nextstep.jwp.web.resolver.HtmlResolver;
+import nextstep.jwp.web.resolver.StaticResourceResolver;
+
+public class WebStatusHandler implements WebHandler {
+
+    private static final String NOT_FOUND_PAGE_PATH = "/404";
+    private static final String INTERNAL_ERROR_PAGE_PATH = "/500";
+
+    private final List<DataResolver> dataResolvers;
+
+    public WebStatusHandler() {
+        this(List.of(new HtmlResolver(), new StaticResourceResolver()));
+    }
+
+    public WebStatusHandler(List<DataResolver> dataResolvers) {
+        this.dataResolvers = dataResolvers;
+    }
+
+    @Override
+    public void doHandle(HttpRequest request, HttpResponse response) {
+        if (response.status() == OK) {
+            response.replaceResponseBody(
+                resolveData(request.methodUrl().url(), request.mimeType())
+            );
+        } else if (response.status() == FOUND) {
+        } else if (response.status() == NOT_FOUND) {
+            response.replaceResponseBody(
+                resolveData(NOT_FOUND_PAGE_PATH, MimeType.TEXT_HTML)
+            );
+        } else if (response.status() == INTERNAL_ERROR) {
+            response.replaceResponseBody(
+                resolveData(INTERNAL_ERROR_PAGE_PATH, MimeType.TEXT_HTML)
+            );
+        }
+    }
+
+    private HttpResponseBody resolveData(String filePath, MimeType acceptType) {
+        for (DataResolver dataResolver : dataResolvers) {
+            if (dataResolver.isSuitable(acceptType) &&
+                dataResolver.isResourceExist(filePath)
+            ) {
+                return dataResolver.resolve(filePath);
+            }
+        }
+        throw new NotFoundException(
+            String.format("해당 타입의 파일이 없습니다. 요청받은 accept -> %s, filepath -> %s", acceptType,
+                filePath));
+    }
+}

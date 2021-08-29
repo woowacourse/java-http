a0be2b68144884e15ff43060c5b642109c3fe09d
RinSabbit <celena1004@naver.com>
2021-08-30 02:23:54 +0900

    feat: RequestLine 구현

---
+++ app/src/main/java/nextstep/jwp/http/request/HttpVersion.java
@@ -0,0 +1,20 @@
+package nextstep.jwp.http.request;
+
+import java.util.Arrays;
+
+public enum HttpVersion {
+    HTTP_1_1("HTTP/1.1");
+
+    private final String value;
+
+    HttpVersion(String value) {
+        this.value = value;
+    }
+
+    public static HttpVersion matchOf(String requestVersion) {
+        return Arrays.stream(values())
+            .filter(version -> requestVersion.equals(version.value))
+            .findAny()
+            .orElseThrow(RuntimeException::new);
+    }
+}
---
+++ app/src/main/java/nextstep/jwp/http/request/RequestLine.java
@@ -0,0 +1,29 @@
+package nextstep.jwp.http.request;
+
+public class RequestLine {
+
+    private static final int METHOD_INDEX = 0;
+    private static final int URI_INDEX = 1;
+    private static final int VERSION_INDEX = 2;
+
+    private final HttpMethod method;
+    private final RequestURI requestURI;
+    private final HttpVersion httpVersion;
+
+    private RequestLine(HttpMethod method, RequestURI requestURI,
+        HttpVersion httpVersion) {
+        this.method = method;
+        this.requestURI = requestURI;
+        this.httpVersion = httpVersion;
+    }
+
+    public static RequestLine parse(String requestLine) {
+        String[] slicedLine = requestLine.split(" ");
+
+        HttpMethod method = HttpMethod.matchOf(slicedLine[METHOD_INDEX]);
+        RequestURI requestURI = new RequestURI(slicedLine[URI_INDEX]);
+        HttpVersion httpVersion = HttpVersion.matchOf(slicedLine[VERSION_INDEX]);
+
+        return new RequestLine(method, requestURI, httpVersion);
+    }
+}

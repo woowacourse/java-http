a6420d298d434967c67229f725ed7c861f84ff83
Daeun Lee <leede0418@likelion.org>
2021-08-30 00:48:11 +0900

    test: GET 요청 테스트 작성

--- app/src/test/java/nextstep/jwp/FrontControllerTest.java
+++ app/src/test/java/nextstep/jwp/FrontControllerTest.java
@@ -6,16 +6,13 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
-import nextstep.jwp.FrontController;
-import nextstep.jwp.MockSocket;
-import nextstep.jwp.db.InMemoryUserRepository;
-import nextstep.jwp.exception.user.UserNotFoundException;
-import nextstep.jwp.model.User;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class FrontControllerTest {
 
+    @DisplayName("애플리케이션을 실행한다.")
     @Test
     void run() throws IOException {
         // given
@@ -36,11 +33,12 @@ class FrontControllerTest {
         assertThat(socket.output()).isEqualTo(expected);
     }
 
+    @DisplayName("home 페이지를 조회한다.")
     @Test
-    void index() throws IOException {
+    void home() throws IOException {
         // given
         final String httpRequest = String.join("\r\n",
-            "GET /index.html HTTP/1.1 ",
+            "GET / HTTP/1.1 ",
             "Host: localhost:8080 ",
             "Connection: keep-alive ",
             "",
@@ -61,5 +59,188 @@ class FrontControllerTest {
             new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
 
         assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @DisplayName("index 페이지를 조회한다.")
+    @Nested
+    class GetIndex {
+
+        @DisplayName("확장자가 없는 경우")
+        @Test
+        void withoutExtension() throws IOException {
+            // given
+            final String httpRequest = String.join("\r\n",
+                "GET /index HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+            final MockSocket socket = new MockSocket(httpRequest);
+            final FrontController frontController = new FrontController(socket);
+
+            // when
+            frontController.run();
+
+            // then
+            final URL resource = getClass().getClassLoader().getResource("static/index.html");
+            String expected = "HTTP/1.1 200 OK \r\n" +
+                "Content-Length: 5564 \r\n" +
+                "Content-Type: text/html;charset=utf-8 \r\n" +
+                "\r\n" +
+                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+
+            assertThat(socket.output()).isEqualTo(expected);
+        }
+
+        @DisplayName("확장자가 있는 경우")
+        @Test
+        void withExtension() throws IOException {
+            // given
+            final String httpRequest = String.join("\r\n",
+                "GET /index.html HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+            final MockSocket socket = new MockSocket(httpRequest);
+            final FrontController frontController = new FrontController(socket);
+
+            // when
+            frontController.run();
+
+            // then
+            final URL resource = getClass().getClassLoader().getResource("static/index.html");
+            String expected = "HTTP/1.1 200 OK \r\n" +
+                "Content-Length: 5564 \r\n" +
+                "Content-Type: text/html;charset=utf-8 \r\n" +
+                "\r\n" +
+                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+
+            assertThat(socket.output()).isEqualTo(expected);
+        }
+    }
+
+    @DisplayName("login 페이지를 조회한다.")
+    @Nested
+    class GetLogin {
+
+        @DisplayName("확장자가 없는 경우")
+        @Test
+        void withoutExtension() throws IOException {
+            // given
+            final String httpRequest = String.join("\r\n",
+                "GET /login HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+            final MockSocket socket = new MockSocket(httpRequest);
+            final FrontController frontController = new FrontController(socket);
+
+            // when
+            frontController.run();
+
+            // then
+            final URL resource = getClass().getClassLoader().getResource("static/login.html");
+            String expected = "HTTP/1.1 200 OK \r\n" +
+                "Content-Length: 3797 \r\n" +
+                "Content-Type: text/html;charset=utf-8 \r\n" +
+                "\r\n" +
+                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+
+            assertThat(socket.output()).isEqualTo(expected);
+        }
+
+        @DisplayName("확장자가 있는 경우")
+        @Test
+        void withExtension() throws IOException {
+            // given
+            final String httpRequest = String.join("\r\n",
+                "GET /login.html HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+            final MockSocket socket = new MockSocket(httpRequest);
+            final FrontController frontController = new FrontController(socket);
+
+            // when
+            frontController.run();
+
+            // then
+            final URL resource = getClass().getClassLoader().getResource("static/login.html");
+            String expected = "HTTP/1.1 200 OK \r\n" +
+                "Content-Length: 3797 \r\n" +
+                "Content-Type: text/html;charset=utf-8 \r\n" +
+                "\r\n" +
+                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+
+            assertThat(socket.output()).isEqualTo(expected);
+        }
+    }
+
+    @DisplayName("register 페이지를 조회한다.")
+    @Nested
+    class GetRegister {
+
+        @DisplayName("확장자가 없는 경우")
+        @Test
+        void withoutExtension() throws IOException {
+            // given
+            final String httpRequest = String.join("\r\n",
+                "GET /register HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+            final MockSocket socket = new MockSocket(httpRequest);
+            final FrontController frontController = new FrontController(socket);
+
+            // when
+            frontController.run();
+
+            // then
+            final URL resource = getClass().getClassLoader().getResource("static/register.html");
+            String expected = "HTTP/1.1 200 OK \r\n" +
+                "Content-Length: 4319 \r\n" +
+                "Content-Type: text/html;charset=utf-8 \r\n" +
+                "\r\n" +
+                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+
+            assertThat(socket.output()).isEqualTo(expected);
+        }
+
+        @DisplayName("확장자가 있는 경우")
+        @Test
+        void withExtension() throws IOException {
+            // given
+            final String httpRequest = String.join("\r\n",
+                "GET /register.html HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+            final MockSocket socket = new MockSocket(httpRequest);
+            final FrontController frontController = new FrontController(socket);
+
+            // when
+            frontController.run();
+
+            // then
+            final URL resource = getClass().getClassLoader().getResource("static/register.html");
+            String expected = "HTTP/1.1 200 OK \r\n" +
+                "Content-Length: 4319 \r\n" +
+                "Content-Type: text/html;charset=utf-8 \r\n" +
+                "\r\n" +
+                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+
+            assertThat(socket.output()).isEqualTo(expected);
+        }
     }
 }

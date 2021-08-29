ed58f19cd45848f95d18fab605e761ae13271011
TaewanKimmmm <ktww88@gmail.com>
2021-08-29 20:58:50 +0900

    feat: POST 방식으로 회원가입

--- app/src/main/java/nextstep/jwp/RequestHandler.java
+++ app/src/main/java/nextstep/jwp/RequestHandler.java
@@ -34,56 +34,99 @@ public class RequestHandler implements Runnable {
              final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));) {
 
             String requestLine = bufferedReader.readLine();
+
+            String requestHttpMethod = requestLine.split(" ")[0];
             String requestUri = requestLine.split(" ")[1];
 
-            while (bufferedReader.ready()) {
-                String line = bufferedReader.readLine();
+            final StringBuilder stringBuilder = new StringBuilder();
+
+
+            String line = null;
+            do {
+                line = bufferedReader.readLine();
                 if (line == null) {
                     return;
                 }
+                stringBuilder.append(line).append("\r\n");
                 log.debug(line);
+            } while (!"".equals(line));
+
+
+            if ("POST".equals(requestHttpMethod)) {
+                if ("/register".equals(requestUri)) {
+                    HttpHeaders httpHeaders = new HttpHeaders(stringBuilder.toString());
+                    int contentLength = Integer.parseInt(httpHeaders.get("Content-Length"));
+                    char[] buffer = new char[contentLength];
+                    bufferedReader.read(buffer, 0, contentLength);
+                    String requestBody = new String(buffer);
+
+                    String[] splitBody = requestBody.split("&");
+                    String account = splitBody[0].split("=")[1];
+                    String password = splitBody[1].split("=")[1];
+                    String email = splitBody[2].split("=")[1];
+
+                    User user = new User(2, account, password, email);
+                    InMemoryUserRepository.save(user);
+
+                    String response = createViewResponse("/index.html", "302 Found");
+                    outputStream.write(response.getBytes());
+                    outputStream.flush();
+                }
             }
 
-            if ("/login".equals(requestUri)) {
-                requestUri += ".html";
-                String response = createResponse(requestUri, "200 OK");
+            if ("GET".equals(requestHttpMethod)) {
+
+                if ("/register".equals(requestUri)) {
+                    String response = createViewResponse("/register.html", "302 Found");
+                    outputStream.write(response.getBytes());
+                    outputStream.flush();
+                    return;
+                }
+
+                if ("/login".equals(requestUri)) {
+                    requestUri += ".html";
+                    String response = createViewResponse(requestUri, "200 OK");
+                    outputStream.write(response.getBytes());
+                    outputStream.flush();
+                    return;
+                }
+
+                if (requestUri.contains("/login?")) {
+                    int index = requestUri.indexOf("?");
+                    String path = requestUri.substring(0, index);
+                    String queryString = requestUri.substring(index + 1);
+
+                    int index2 = queryString.indexOf("&");
+                    String account = queryString.substring(0, index2).split("=")[1];
+                    String password = queryString.substring(index2 + 1).split("=")[1];
+
+                    Optional<User> user = InMemoryUserRepository.findByAccount(account);
+                    log.debug(user.toString());
+
+                    if (user.isEmpty()) {
+                        String failureResponse = createViewResponse("/401.html", "302 Found");
+                        outputStream.write(failureResponse.getBytes());
+                        outputStream.flush();
+                        return;
+                    }
+
+                    if (!user.get().checkPassword(password)) {
+                        String failureResponse = createViewResponse("/401.html", "302 Found");
+                        outputStream.write(failureResponse.getBytes());
+                        outputStream.flush();
+                        return;
+                    }
+
+                    String successResponse = createViewResponse("/index.html", "302 Found");
+                    outputStream.write(successResponse.getBytes());
+                    outputStream.flush();
+                    return;
+                }
+
+                String response = createViewResponse("/index.html", "200 OK");
                 outputStream.write(response.getBytes());
                 outputStream.flush();
-                return;
             }
-
-            if (requestUri.contains("/login?")) {
-                int index = requestUri.indexOf("?");
-                String path = requestUri.substring(0, index);
-                String queryString = requestUri.substring(index + 1);
-
-                int index2 = queryString.indexOf("&");
-                String account = queryString.substring(0, index2).split("=")[1];
-                String password = queryString.substring(index2 + 1).split("=")[1];
-
-                Optional<User> user = InMemoryUserRepository.findByAccount(account);
-                log.debug(user.toString());
-
-                if (user.isEmpty()) {
-                    String failureResponse = createResponse("/401.html", "302 Found");
-                    outputStream.write(failureResponse.getBytes());
-                    outputStream.flush();
-                    return;
-                }
-
-                if (!user.get().checkPassword(password)) {
-                    String failureResponse = createResponse("/401.html", "302 Found");
-                    outputStream.write(failureResponse.getBytes());
-                    outputStream.flush();
-                    return;
-                }
-
-                String successResponse = createResponse("/index.html", "302 Found");
-                outputStream.write(successResponse.getBytes());
-                outputStream.flush();
-                return;
-            }
-
         } catch (IOException exception) {
             log.error("Exception stream", exception);
         } finally {
@@ -91,15 +134,14 @@ public class RequestHandler implements Runnable {
         }
     }
 
-    private String createResponse(String requestUri, String statusCode) throws IOException {
-        final URL resource = getClass().getClassLoader().getResource("static" + requestUri);
+    private String createViewResponse(String viewName, String statusCode) throws IOException {
+        final URL resource = getClass().getClassLoader().getResource("static" + viewName);
         final Path path = new File(resource.getPath()).toPath();
         final List<String> lines = Files.readAllLines(path);
 
         String result = lines.stream()
                              .map(String::valueOf)
                              .collect(Collectors.joining());
-
 
         final String response = String.join("\r\n",
                 "HTTP/1.1 " + statusCode + " ",

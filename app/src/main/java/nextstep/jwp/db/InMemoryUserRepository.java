3747cae68051d0b9d7733045cbc68a79a868fb48
Jinho <jh8579@gmail.com>
2021-08-30 03:31:04 +0900

    feat: repository id 증가하도록 변경

--- app/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
+++ app/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
@@ -1,18 +1,18 @@
 package nextstep.jwp.db;
 
-
-import nextstep.jwp.model.User;
-
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
+import nextstep.jwp.model.User;
+
 public class InMemoryUserRepository {
 
     private static final Map<String, User> database = new ConcurrentHashMap<>();
+    private static int currentId = 1;
 
     static {
-        final User user = new User(1, "gugu", "password", "hkkang@woowahan.com");
+        final User user = new User(currentId++, "gugu", "password", "hkkang@woowahan.com");
         database.put(user.getAccount(), user);
     }
 
@@ -22,5 +22,9 @@ public class InMemoryUserRepository {
 
     public static Optional<User> findByAccount(String account) {
         return Optional.ofNullable(database.get(account));
+    }
+
+    public static int getCurrentId() {
+        return currentId;
     }
 }

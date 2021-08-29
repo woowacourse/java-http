01a6c4f28c420dfd0a8780269aaee6673d410173
Seed <hsik0225@gmail.com>
2021-08-30 02:21:45 +0900

    refactor: HeaderFormatter 리팩토링

---
+++ app/src/test/java/nextstep/jwp/framework/http/formatter/HeaderFormatterTest.java
@@ -0,0 +1,46 @@
+package nextstep.jwp.framework.http.formatter;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import nextstep.jwp.framework.http.HttpHeaders;
+import nextstep.jwp.framework.http.HttpResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HeaderFormatterTest {
+
+    @DisplayName("HttpHeader 를 HTTP 형식의 String 으로 변환 테스트")
+    @ParameterizedTest
+    @MethodSource
+    void transformTest(HttpHeaders httpHeaders, String expected) {
+
+        // given
+        final HttpResponse httpResponse = new HttpResponse.Builder().httpHeaders(httpHeaders).build();
+        final HeaderFormatter headerFormatter = new HeaderFormatter(httpResponse);
+
+        // when
+        final String httpStatusLine = headerFormatter.transform();
+
+        // then
+        assertThat(httpStatusLine).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> transformTest() {
+        return Stream.of(
+                Arguments.of(
+                        new HttpHeaders().addHeader(HttpHeaders.HOST, "localhost:8080"), "Host: localhost:8080 \r\n\r\n"
+                ),
+                Arguments.of(
+                        new HttpHeaders().addHeader(HttpHeaders.HOST, "localhost:8080")
+                                         .addHeader(HttpHeaders.CONNECTION, "keep-alive"),
+
+                        "Host: localhost:8080 \r\nConnection: keep-alive \r\n\r\n"
+                )
+        );
+    }
+}

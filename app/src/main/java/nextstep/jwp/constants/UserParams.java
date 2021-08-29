31611d0ac317472b25963260c9a45ec34c078dcf
naeunkim <knae11@naver.com>
2021-08-30 02:44:09 +0900

    refactor: UserService 관련 상수 처리

---
+++ app/src/main/java/nextstep/jwp/constants/UserParams.java
@@ -0,0 +1,7 @@
+package nextstep.jwp.constants;
+
+public class UserParams {
+    public static final String ACCOUNT = "account";
+    public static final String PASSWORD = "password";
+    public static final String EMAIL = "email";
+}
--- app/src/main/java/nextstep/jwp/service/HttpService.java
+++ app/src/main/java/nextstep/jwp/service/HttpService.java
@@ -1,6 +1,7 @@
 package nextstep.jwp.service;
 
 import java.util.Map;
+import nextstep.jwp.constants.UserParams;
 import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.exception.AuthorizationException;
 import nextstep.jwp.model.User;
@@ -8,14 +9,14 @@ import nextstep.jwp.model.User;
 public class HttpService {
 
     public Boolean isAuthorized(Map<String, String> params) {
-        User user = InMemoryUserRepository.findByAccount(params.get("account"))
+        User user = InMemoryUserRepository.findByAccount(params.get(UserParams.ACCOUNT))
                 .orElseThrow(() -> new AuthorizationException("해당하는 유저가 없어요"));
-        return user.checkPassword(params.get("password"));
+        return user.checkPassword(params.get(UserParams.PASSWORD));
     }
 
     public void register(Map<String, String> params) {
-        final User user = new User(InMemoryUserRepository.size() + 1, params.get("account"), params.get("password"),
-                params.get("email"));
+        final User user = new User(InMemoryUserRepository.size() + 1, params.get(UserParams.ACCOUNT), params.get(UserParams.PASSWORD),
+                params.get(UserParams.EMAIL));
         InMemoryUserRepository.save(user);
     }
 }

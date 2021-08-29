8e4cb845795b458a7e42dbaa90f5e17460d66c60
Kimun Kim <kkm97351@gmail.com>
2021-08-30 03:06:49 +0900

    학습테스트 FileTest 풀이

--- app/src/test/java/nextstep/learning/http/FileTest.java
+++ app/src/test/java/nextstep/learning/http/FileTest.java
@@ -4,10 +4,16 @@ import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 
 /**
  * 웹서버는 사용자가 요청한 html 파일을 제공 할 수 있어야 한다.<br>
@@ -23,11 +29,13 @@ class FileTest {
      */
     @Test
     void resource_디렉터리에_있는_파일의_경로를_찾는다() {
+        // given
         final String fileName = "nextstep.txt";
 
-        // todo
-        final String actual = "";
+        // when
+        final String actual = getClass().getClassLoader().getResource(fileName).getFile();
 
+        // then
         assertThat(actual).endsWith(fileName);
     }
 
@@ -36,15 +44,25 @@ class FileTest {
      * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws IOException {
+        // given
         final String fileName = "nextstep.txt";
 
-        // todo
-        final Path path = null;
+        // when
+        String filePath = getClass().getClassLoader().getResource(fileName).getPath();
+        final Path path = Path.of(filePath);
 
-        // todo
-        final List<String> actual = Lists.emptyList();
+        InputStream inputStream = Files.newInputStream(path);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 
+        final List<String> actual = new ArrayList<>();
+
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            actual.add(line);
+        }
+
+        // then
         assertThat(actual).containsOnly("nextstep");
     }
 }

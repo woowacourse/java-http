76394e117cab77f13075419dc9a5a5fa4d965191
Seed <hsik0225@gmail.com>
2021-08-30 02:12:51 +0900

    refactor: 엔드 라인 파싱 기능 리팩토링

--- app/src/main/java/nextstep/jwp/framework/http/parser/EndLineParser.java
+++ app/src/main/java/nextstep/jwp/framework/http/parser/EndLineParser.java
@@ -1,20 +1,28 @@
 package nextstep.jwp.framework.http.parser;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 import nextstep.jwp.framework.http.HttpRequest;
 
-public class EndLineParser extends AbstractLineParser {
+public class EndLineParser extends AbstractHttpParser {
 
-    public EndLineParser(HttpRequest.Builder builder) {
-        super(builder);
+    public EndLineParser(BufferedReader reader, HttpRequest.Builder builder) {
+        super(reader, builder);
     }
 
     @Override
-    public boolean canParse() {
+    public boolean isParsing() {
         return false;
     }
 
     @Override
-    public LineParser parseLine(String line) {
+    public String readParsingContent() throws IOException {
+        throw new UnsupportedOperationException("파싱이 종료된 리퀘스트입니다.");
+    }
+
+    @Override
+    public HttpParser parse() throws IOException {
         throw new UnsupportedOperationException("파싱이 종료된 리퀘스트입니다.");
     }
 }

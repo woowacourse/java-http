412c0b3271efd45f086cf8169d4da1f437858d9e
sihyung92 <fjzjqhdl@gmail.com>
2021-08-30 18:04:29 +0900

    test: 정적자원 찾아오는 통합테스트 추가

--- app/src/test/java/nextstep/jwp/RequestHandlerTest.java
+++ app/src/test/java/nextstep/jwp/RequestHandlerTest.java
@@ -7,10 +7,12 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import nextstep.jwp.web.handler.RequestHandler;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RequestHandlerTest {
 
+    @DisplayName("인덱스 페이지를 확인")
     @Test
     void index() throws IOException {
         // given
@@ -32,6 +34,33 @@ class RequestHandlerTest {
         String expected = "HTTP/1.1 200 OK \r\n" +
             "Content-Type: text/html;charset=utf-8 \r\n" +
             "Content-Length: 5549 \r\n" +
+            "\r\n" +
+            new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+        assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @DisplayName("css자원을 확인")
+    @Test
+    void cssTest() throws IOException {
+        // given
+        final String httpRequest = String.join("\r\n",
+            "GET /css/styles.css HTTP/1.1 ",
+            "Host: localhost:8080 ",
+            "Connection: keep-alive ",
+            "",
+            "");
+
+        final MockSocket socket = new MockSocket(httpRequest);
+        final RequestHandler requestHandler = new RequestHandler(socket);
+
+        // when
+        requestHandler.run();
+
+        // then
+        final URL resource = getClass().getClassLoader().getResource("static/css/styles.css");
+        String expected = "HTTP/1.1 200 OK \r\n" +
+            "Content-Type: text/css \r\n" +
+            "Content-Length: 211991 \r\n" +
             "\r\n" +
             new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
         assertThat(socket.output()).isEqualTo(expected);

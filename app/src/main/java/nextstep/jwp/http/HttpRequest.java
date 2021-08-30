9f50af507f401a1ecdb37ec47caabd20de26141e
jaeseongDev <qkrwotjd1445@naver.com>
2021-08-30 18:26:28 +0900

    refactor: 하드 코딩된 부분 수정

--- app/src/main/java/nextstep/jwp/http/HttpRequest.java
+++ app/src/main/java/nextstep/jwp/http/HttpRequest.java
@@ -7,13 +7,13 @@ import java.util.Objects;
 
 public class HttpRequest {
 
-    private static final String BLANK_DELIMITER = " ";
     private static final int FIRST_WORD_INDEX = 0;
     private static final int SECOND_WORD_INDEX = 1;
-    private static final String PATH_AND_QUERY_STRING_DELIMITER = "?";
     private static final int START_INDEX = 0;
-    private static final String QUERY_STRING_DELIMITER = "&";
-    private static final String KEY_AND_VALUE_DELIMITER = "=";
+    private static final int FORM_DATA_KEY_INDEX = 0;
+    private static final int FORM_DATA_VALUE_INDEX = 1;
+    private static final int QUERY_PARAMS_KEY_INDEX = 0;
+    private static final int QUERY_PARAMS_VALUE_INDEX = 1;
 
     private final String statusLine;
     private final String bodyLine;
@@ -30,21 +30,21 @@ public class HttpRequest {
     }
 
     public String extractURI() {
-        return statusLine.split(BLANK_DELIMITER)[SECOND_WORD_INDEX];
+        return statusLine.split(" ")[SECOND_WORD_INDEX];
     }
 
     public Map<String, String> extractURIQueryParams() {
         String uri = extractURI();
         Map<String, String> queryParams = new HashMap<>();
-        int index = uri.indexOf(PATH_AND_QUERY_STRING_DELIMITER);
+        int index = uri.indexOf("?");
         if (index != -1) {
             String queryString = uri.substring(index + 1);
-            String[] splitQueryStrings = queryString.split(QUERY_STRING_DELIMITER);
+            String[] splitQueryStrings = queryString.split("&");
 
             for (String splitQueryString : splitQueryStrings) {
-                String[] splitParam = splitQueryString.split(KEY_AND_VALUE_DELIMITER);
-                String key = splitParam[0];
-                String value = splitParam[1];
+                String[] splitParam = splitQueryString.split("=");
+                String key = splitParam[QUERY_PARAMS_KEY_INDEX];
+                String value = splitParam[QUERY_PARAMS_VALUE_INDEX];
                 queryParams.put(key, value);
             }
         }
@@ -53,7 +53,7 @@ public class HttpRequest {
 
     public String extractURIPath() {
         String uri = extractURI();
-        int queryStringDelimiterIndex = uri.indexOf(PATH_AND_QUERY_STRING_DELIMITER);
+        int queryStringDelimiterIndex = uri.indexOf("?");
         if (queryStringDelimiterIndex != -1) {
             return uri.substring(START_INDEX, queryStringDelimiterIndex);
         }
@@ -61,16 +61,16 @@ public class HttpRequest {
     }
 
     public HttpMethod extractHttpMethod() {
-        String httpMethodName = statusLine.split(BLANK_DELIMITER)[FIRST_WORD_INDEX];
+        String httpMethodName = statusLine.split(" ")[FIRST_WORD_INDEX];
         return HttpMethod.findHttpMethod(httpMethodName);
     }
 
     public Map<String, String> extractFormData() {
-        String[] splitFormData = bodyLine.split(QUERY_STRING_DELIMITER);
+        String[] splitFormData = bodyLine.split("&");
         Map<String, String> result = new HashMap<>();
         for (String s : splitFormData) {
-            String[] split = s.split(KEY_AND_VALUE_DELIMITER);
-            result.put(split[0], split[1]);
+            String[] split = s.split("=");
+            result.put(split[FORM_DATA_KEY_INDEX], split[FORM_DATA_VALUE_INDEX]);
         }
         return result;
     }

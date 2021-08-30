c476422ed1a899372f903c6e9b36145abd9dcdc5
youngbin <thisisyoungbin@gmail.com>
2021-08-31 02:03:39 +0900

    refactor: login POST 구현

--- app/src/main/java/nextstep/jwp/controller/LoginController.java
+++ app/src/main/java/nextstep/jwp/controller/LoginController.java
@@ -1,14 +1,9 @@
 package nextstep.jwp.controller;
 
 import nextstep.jwp.db.InMemoryUserRepository;
-import nextstep.jwp.model.HttpRequest;
-import nextstep.jwp.model.HttpResponse;
-import nextstep.jwp.model.HttpStatus;
-import nextstep.jwp.model.User;
+import nextstep.jwp.model.*;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 
 public class LoginController extends AbstractController {
@@ -16,23 +11,27 @@ public class LoginController extends AbstractController {
     @Override
     protected HttpResponse doGet(HttpRequest request) throws IOException {
         HttpResponse response = new HttpResponse();
-        Map<String, String> queryMap = request.getQueryMap();
-        if (Objects.isNull(queryMap)) {
-            return response.respond(request.getUri() + ".html");
-        }
-        String account = queryMap.get("account");
-        Optional<User> user = InMemoryUserRepository.findByAccount(account);
-        if (user.isPresent()) {
-            return response.redirect("/index.html", HttpStatus.FOUND);
-        }
-        if (user.isEmpty()) {
-            return response.redirect("404.html", HttpStatus.NOT_FOUND);
-        }
         return response.respond(request.getUri() + ".html");
     }
 
     @Override
     protected HttpResponse doPost(HttpRequest request) throws IOException {
-        return null;
+        RequestBody requestBody = request.getRequestBody();
+        HttpResponse response = new HttpResponse();
+        String account = requestBody.getParam("account");
+        String password = requestBody.getParam("password");
+
+        return postRespond(response, account, password);
+    }
+
+    private HttpResponse postRespond(HttpResponse response, String account, String password) {
+        Optional<User> user = InMemoryUserRepository.findByAccount(account);
+        if (user.isEmpty()) {
+            return response.redirect("/400.html", HttpStatus.BAD_REQUEST);
+        }
+        if (!user.get().checkPassword(password)) {
+            return response.redirect("/401.html", HttpStatus.UNAUTHORIZED);
+        }
+        return response.redirect("/index.html", HttpStatus.FOUND);
     }
 }

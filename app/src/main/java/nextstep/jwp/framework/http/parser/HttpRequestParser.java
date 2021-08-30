fbe0c48454dc9ef0b1c014b9bf7ad7bbdc4b011d
Seed <hsik0225@gmail.com>
2021-08-30 21:51:34 +0900

    feat: RequestHeader 파싱 시 인코딩 설정

--- app/src/main/java/nextstep/jwp/framework/http/parser/HttpRequestParser.java
+++ app/src/main/java/nextstep/jwp/framework/http/parser/HttpRequestParser.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 import nextstep.jwp.framework.http.HttpRequest;
 
@@ -12,7 +13,7 @@ public class HttpRequestParser {
     private final BufferedReader reader;
 
     public HttpRequestParser(InputStream inputStream) {
-        this.reader = new BufferedReader(new InputStreamReader(inputStream));
+        this.reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
     }
 
     public HttpRequest parseRequest() throws IOException {

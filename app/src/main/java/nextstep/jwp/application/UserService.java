3a5d91613645d68359015536223fc465b742365a
sor <bosl95@naver.com>
2021-08-30 12:23:03 +0900

    refactor: optiona를 log 찍지 않도록 수정

--- app/src/main/java/nextstep/jwp/application/UserService.java
+++ app/src/main/java/nextstep/jwp/application/UserService.java
@@ -6,6 +6,7 @@ import nextstep.jwp.model.httpmessage.request.HttpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.Optional;
 
 public class UserService {
@@ -21,13 +22,14 @@ public class UserService {
                 request.getParameter(PASSWORD));
     }
 
-    public void saveUser(HttpRequest request) {
+    public void saveUser(HttpRequest request) throws UserPrincipalNotFoundException {
         User newUser = new User(request.getParameter(ACCOUNT),
                 request.getParameter(PASSWORD),
                 request.getParameter(EMAIL));
         InMemoryUserRepository.save(newUser);
 
-        Optional<User> savedUser = findByAccount(request);
+        User savedUser = findByAccount(request)
+                .orElseThrow(() -> new UserPrincipalNotFoundException("해당 유저가 존재하지 않습니다. (account : " + request.getParameter(ACCOUNT) + ")"));
         LOG.debug("Saved user : {}", savedUser);
     }
 

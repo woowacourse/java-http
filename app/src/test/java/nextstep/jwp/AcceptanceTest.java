0ae04ff8ebc16305fa419cbcfdfebcbc3887347b
YeongBeom Heo <hybeom@gmail.com>
2021-08-30 09:41:57 +0900

    test: AcceptanceTest 보강

--- app/src/test/java/nextstep/jwp/AcceptanceTest.java
+++ app/src/test/java/nextstep/jwp/AcceptanceTest.java
@@ -2,10 +2,12 @@ package nextstep.jwp;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class AcceptanceTest {
 
+    @DisplayName("미션 첫 번째 요구사항")
     @Test
     void getIndex() {
         //given
@@ -28,6 +30,59 @@ public class AcceptanceTest {
         assertThat(getCode(entire)).isEqualTo("200");
     }
 
+    @DisplayName("미션 2,3 번째 요구사항 - 1")
+    @Test
+    void loginSuccess() {
+        //given
+        final String httpRequest= String.join("\r\n",
+            "POST /login HTTP/1.1 ",
+            "Host: localhost:8080 ",
+            "Connection: keep-alive ",
+            "Content-Length: 80",
+            "Content-Type: application/x-www-form-urlencoded",
+            "Accept: */*",
+            "",
+            "account=gugu&password=password&email=hybeom@gmail.com");
+
+        final MockSocket socket = new MockSocket(httpRequest);
+        final RequestHandler requestHandler = new RequestHandler(socket);
+
+        // when
+        requestHandler.run();
+
+        // then
+        String entire = socket.output();
+        assertThat(socket.output()).isNotEmpty();
+        assertThat(redirectCode(entire)).isEqualTo("302");
+    }
+
+    @DisplayName("미션 2,3 번째 요구사항 - 2")
+    @Test
+    void loginFailed() {
+        //given
+        final String httpRequest= String.join("\r\n",
+            "POST /login HTTP/1.1 ",
+            "Host: localhost:8080 ",
+            "Connection: keep-alive ",
+            "Content-Length: 80",
+            "Content-Type: application/x-www-form-urlencoded",
+            "Accept: */*",
+            "",
+            "account=younge&password=password&email=hybeom@gmail.com");
+
+        final MockSocket socket = new MockSocket(httpRequest);
+        final RequestHandler requestHandler = new RequestHandler(socket);
+
+        // when
+        requestHandler.run();
+
+        // then
+        String entire = socket.output();
+        assertThat(socket.output()).isNotEmpty();
+        assertThat(redirectCode(entire)).isEqualTo("401");
+    }
+
+    @DisplayName("미션 4 번째 요구사항 - 1")
     @Test
     void signUpSuccess() {
         //given
@@ -53,6 +108,7 @@ public class AcceptanceTest {
         assertThat(redirectCode(entire)).isEqualTo("302");
     }
 
+    @DisplayName("미션 4 번째 요구사항 - 2")
     @Test
     void signUpFail() {
         //given
@@ -78,6 +134,7 @@ public class AcceptanceTest {
         assertThat(redirectCode(entire)).isEqualTo("401");
     }
 
+    @DisplayName("미션 5 번째 요구사항")
     @Test
     void staticSuccess() {
         //given

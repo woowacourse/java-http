61460fad9b3fa3eee90a4b994c923c59d6a84d56
naeunkim <knae11@naver.com>
2021-08-31 21:23:10 +0900

    test: RequestBody 테스트 추가

--- app/src/main/java/nextstep/jwp/http/RequestBody.java
+++ app/src/main/java/nextstep/jwp/http/RequestBody.java
@@ -16,7 +16,7 @@ public class RequestBody {
         String[] queries = body.split(Query.SEPARATOR);
         for (String query : queries) {
             String[] split = query.split(Query.EQUAL);
-            params.put(split[Query.KEY], split[Query.VALUE]);
+            params.put(split[Query.KEY].trim(), split[Query.VALUE].trim());
         }
         return params;
     }

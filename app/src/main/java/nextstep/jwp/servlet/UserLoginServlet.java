8905e2a2051c54eaffdb8945305c55aefcb2e4d6
Byung Wook Park <bbwwpark@naver.com>
2021-08-31 21:16:59 +0900

    refactor: 로그인 서블릿 리팩터링

--- app/src/main/java/nextstep/jwp/servlet/UserLoginServlet.java
+++ app/src/main/java/nextstep/jwp/servlet/UserLoginServlet.java
@@ -1,9 +1,11 @@
 package nextstep.jwp.servlet;
 
+import java.io.IOException;
 import java.util.Optional;
 import nextstep.jwp.db.InMemoryUserRepository;
-import nextstep.jwp.http.HttpRequest;
-import nextstep.jwp.http.HttpResponse;
+import nextstep.jwp.http.reponse.HttpResponse;
+import nextstep.jwp.http.reponse.HttpStatus;
+import nextstep.jwp.http.request.HttpRequest;
 import nextstep.jwp.model.User;
 import nextstep.jwp.tomcat.Servlet;
 
@@ -14,19 +16,28 @@ public class UserLoginServlet extends Servlet {
     }
 
     @Override
-    public void doGet(HttpRequest httpRequest, HttpResponse httpResponse) {
-        Optional<User> user = InMemoryUserRepository.findByAccount(httpRequest.getParameterValues("account"));
-        httpResponse.addStartLine("HTTP/1.1", "302", "Found");
-        httpResponse.addContentType("text/html;charset=utf-8");
+    public void doGet(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
+        httpResponse.setStatus(HttpStatus.OK);
+        httpResponse.addFile("/login.html");
+    }
 
-        if (user.isEmpty() || !user.get().checkPassword(httpRequest.getParameterValues("password"))) {
-            httpResponse.addLocation("401.html");
+    @Override
+    public void doPost(HttpRequest httpRequest, HttpResponse httpResponse) {
+        Optional<User> user = InMemoryUserRepository.findByAccount(httpRequest.getParameter("account"));
+        httpResponse.setStatus(HttpStatus.FOUND);
+        httpResponse.setHeader("Content-Type", "text/html;charset=utf-8");
+
+        if (checkUser(httpRequest, user)) {
+            httpResponse.setHeader("Location", "401.html");
             return;
         }
 
-        httpResponse.addLocation("index.html");
-        httpResponse.addBody(user.toString());
         log.debug("로그인 완료! 유저 정보 : {}", user);
+        httpResponse.setHeader("Location", "index.html");
+    }
+
+    private boolean checkUser(HttpRequest httpRequest, Optional<User> user) {
+        return user.isEmpty() || !user.get().checkPassword(httpRequest.getParameter("password"));
     }
 
 }
--- app/src/test/java/nextstep/jwp/RequestHandlerTest.java
+++ app/src/test/java/nextstep/jwp/RequestHandlerTest.java
@@ -59,4 +59,59 @@ class RequestHandlerTest {
         assertThat(socket.output()).isEqualTo(expected);
     }
 
+
+    @DisplayName("\"GET /login\" 요청을 보내면, login.html 파일을 응답한다.")
+    @Test
+    void login_html() throws IOException {
+        // given
+        final String httpRequest = String.join("\r\n",
+            "GET /login HTTP/1.1 ",
+            "Host: localhost:8080 ",
+            "Connection: keep-alive ",
+            "",
+            "");
+
+        final MockSocket socket = new MockSocket(httpRequest);
+        final RequestHandler requestHandler = new RequestHandler(socket);
+
+        // when
+        requestHandler.run();
+
+        // then
+        final URL resource = getClass().getClassLoader().getResource("static/login.html");
+        String expected = "HTTP/1.1 200 OK \r\n" +
+            "Content-Length: 3797 \r\n" +
+            "Content-Type: text/html;charset=utf-8 \r\n" +
+            "\r\n" +
+            new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+
+        assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @DisplayName("\"POST /login\" 요청을 보내서 로그인이 됐다면, index.html로 리다이렉트한다.")
+    @Test
+    void login() throws IOException {
+        // given
+        final String httpRequest = String.join("\r\n",
+            "POST /login HTTP/1.1 ",
+            "Host: localhost:8080 ",
+            "Connection: keep-alive ",
+            "Content-Length: 30",
+            "",
+            "account=gugu&password=password");
+
+        final MockSocket socket = new MockSocket(httpRequest);
+        final RequestHandler requestHandler = new RequestHandler(socket);
+
+        // when
+        requestHandler.run();
+
+        // then
+        String expected = "HTTP/1.1 302 Found \r\n" +
+            "Location: index.html \r\n" +
+            "Content-Type: text/html;charset=utf-8 ";
+
+        assertThat(socket.output()).isEqualTo(expected);
+    }
+
 }

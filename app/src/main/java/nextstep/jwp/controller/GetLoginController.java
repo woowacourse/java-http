44e7bfc57eb9bf960a306fa5025cf93168a3547b
ROOT <junroot0909@gmail.com>
2021-08-31 10:25:34 +0900

    refactor: 불필요한 상수 제거

--- app/src/main/java/nextstep/jwp/controller/GetLoginController.java
+++ app/src/main/java/nextstep/jwp/controller/GetLoginController.java
@@ -1,7 +1,5 @@
 package nextstep.jwp.controller;
 
-import java.util.Arrays;
-import java.util.List;
 import nextstep.jwp.infrastructure.http.request.HttpMethod;
 import nextstep.jwp.infrastructure.http.request.HttpRequest;
 import nextstep.jwp.infrastructure.http.request.HttpRequestLine;
@@ -9,10 +7,6 @@ import nextstep.jwp.infrastructure.http.view.ResourceView;
 import nextstep.jwp.infrastructure.http.view.View;
 
 public class GetLoginController implements Controller {
-
-    private static final String ACCOUNT = "account";
-    private static final String PASSWORD = "password";
-    private static final List<String> REQUIRED_PARAMETERS = Arrays.asList(ACCOUNT, PASSWORD);
 
     @Override
     public HttpRequestLine requestLine() {

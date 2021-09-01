6f8f30306cc285e663a623217eaabb0beeed7e18
TaewanKimmmm <ktww88@gmail.com>
2021-09-01 22:06:19 +0900

    feat: HttpRequest 클래스 추가

---
+++ app/src/main/java/nextstep/jwp/request/HttpRequest.java
@@ -0,0 +1,30 @@
+package nextstep.jwp.request;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class HttpRequest {
+    private final RequestLine requestLine;
+    private final RequestHeaders requestHeaders;
+
+    public HttpRequest(BufferedReader bufferedReader) throws IOException {
+        this.requestLine = new RequestLine(bufferedReader.readLine());
+        this.requestHeaders = new RequestHeaders(bufferedReader);
+    }
+
+    public boolean isGet() {
+        return requestLine.isGet();
+    }
+
+    public String getPath() {
+        return requestLine.getPath();
+    }
+
+    public boolean isPost() {
+        return requestLine.isPost();
+    }
+
+    public String get(String key) {
+        return requestHeaders.get(key);
+    }
+}
---
+++ app/src/test/java/nextstep/jwp/HttpRequestTest.java
@@ -0,0 +1,54 @@
+package nextstep.jwp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import nextstep.jwp.request.HttpRequest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HttpRequestTest {
+    @Test
+    @DisplayName("HttpRequest 생성 - GET")
+    void createGetHttpRequest() throws IOException {
+        final String request = String.join("\r\n",
+                "GET /index.html HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "Content-Length: 30",
+                "",
+                "");
+
+        final MockSocket socket = new MockSocket(request);
+        final RequestHandler requestHandler = new RequestHandler(socket);
+
+        requestHandler.run();
+
+        HttpRequest httpRequest = new HttpRequest(new BufferedReader(new InputStreamReader(socket.getInputStream())));
+        assertTrue(httpRequest.isGet());
+    }
+
+//    @Test
+//    @DisplayName("HttpRequest 생성 - POST")
+//    void createPostHttpRequest() throws IOException {
+//        final String request = String.join("\r\n",
+//                "POST /login HTTP/1.1 ",
+//                "Host: localhost:8080 ",
+//                "Connection: keep-alive ",
+//                "Content-Length: 30",
+//                "",
+//                "");
+//
+//        final MockSocket socket = new MockSocket(request);
+//        final RequestHandler requestHandler = new RequestHandler(socket);
+//
+//        requestHandler.run();
+//
+//        HttpRequest httpRequest = new HttpRequest(new BufferedReader(new InputStreamReader(socket.getInputStream())));
+//        assertTrue(httpRequest.isPost());
+//    }
+}

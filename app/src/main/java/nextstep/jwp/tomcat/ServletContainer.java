106f2ae2ca39e9493719f3eae53cda1d06eb747a
Byung Wook Park <bbwwpark@naver.com>
2021-09-01 23:09:32 +0900

    refactor: uri에 매핑되는 서블릿이 존재하지 않을 경우 500 페이지 응답하도록 리팩터링

--- app/src/main/java/nextstep/jwp/tomcat/ServletContainer.java
+++ app/src/main/java/nextstep/jwp/tomcat/ServletContainer.java
@@ -7,14 +7,18 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import nextstep.jwp.RequestHandler;
 import nextstep.jwp.http.reponse.HttpResponse;
 import nextstep.jwp.http.request.HttpRequest;
 import nextstep.jwp.servlet.StaticResourceServlet;
 import nextstep.jwp.servlet.UserLoginServlet;
 import nextstep.jwp.servlet.UserRegisterServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ServletContainer {
 
+    private static final Logger log = LoggerFactory.getLogger(ServletContainer.class);
     private static final List<Servlet> SERVLETS = new ArrayList<>();
     private static final StaticResourceServlet STATIC_RESOURCE_SERVLET = new StaticResourceServlet();
 
@@ -33,7 +37,8 @@ public class ServletContainer {
             Servlet servlet = findServletByRequestURI(httpRequest.getRequestURI());
             servlet.service(httpRequest, httpResponse);
         } catch (Exception e){
-            System.out.println(e.getMessage());
+            log.error("uri에 매핑되는 서블릿이 존재하지 않습니다. 에러 메세지");
+           httpResponse.sendRedirect("/500.html");
         }
     }
 

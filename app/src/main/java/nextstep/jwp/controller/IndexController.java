e253b386b2be7e24401d5590cb8da371093c91d1
youngbin <thisisyoungbin@gmail.com>
2021-09-01 12:43:45 +0900

    refactor: IndexController 분기처리 수정

--- app/src/main/java/nextstep/jwp/controller/IndexController.java
+++ app/src/main/java/nextstep/jwp/controller/IndexController.java
@@ -5,8 +5,19 @@ import nextstep.jwp.domain.response.HttpResponse;
 import nextstep.jwp.domain.response.HttpStatus;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class IndexController implements Controller {
+
+    private final Map<String, HttpStatus> invalidResponseMap = new HashMap<>();
+
+    public IndexController() {
+        invalidResponseMap.put("/400.html", HttpStatus.BAD_REQUEST);
+        invalidResponseMap.put("/401.html", HttpStatus.UNAUTHORIZED);
+        invalidResponseMap.put("/404.html", HttpStatus.NOT_FOUND);
+    }
 
     @Override
     public HttpResponse service(HttpRequest request) {
@@ -20,15 +31,13 @@ public class IndexController implements Controller {
     }
 
     private HttpResponse findHttpResponse(HttpRequest request, HttpResponse response, String uri) throws IOException {
-        if (uri.equals("/400.html")) {
-            return response.respond(uri, HttpStatus.BAD_REQUEST);
-        }
-        if (uri.equals("/401.html")) {
-            return response.respond(uri, HttpStatus.UNAUTHORIZED);
-        }
-        if (uri.equals("/404.html")) {
-            return response.respond(uri, HttpStatus.NOT_FOUND);
+        if (isInvalidUri(uri)) {
+            return response.respond(uri, invalidResponseMap.get(uri));
         }
         return response.respond(request.getUri());
+    }
+
+    private boolean isInvalidUri(String uri) {
+        return Objects.nonNull(invalidResponseMap.get(uri));
     }
 }

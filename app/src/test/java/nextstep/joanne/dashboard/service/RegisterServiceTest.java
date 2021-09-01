7f2770a3d41ab662fff4c31734ba2dcab411b441
seovalue <seominjeong.dev@gmail.com>
2021-09-01 23:34:40 +0900

    test: 테스트 추가

---
+++ app/src/test/java/nextstep/joanne/dashboard/service/RegisterServiceTest.java
@@ -0,0 +1,24 @@
+package nextstep.joanne.dashboard.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegisterServiceTest {
+
+    @DisplayName("회원 가입을 한다.")
+    @Test
+    void join() {
+        // given
+        String account = "joanne";
+        String email = "joanne@woowahan.com";
+        String password = "1234";
+
+        // when
+        RegisterService registerService = new RegisterService();
+        assertThatCode(() -> registerService.join(account, email, password))
+                .doesNotThrowAnyException();
+    }
+}
---
+++ app/src/test/java/nextstep/joanne/server/handler/HandlerMappingTest.java
@@ -0,0 +1,77 @@
+package nextstep.joanne.server.handler;
+
+import nextstep.joanne.dashboard.controller.LoginController;
+import nextstep.joanne.dashboard.controller.RegisterController;
+import nextstep.joanne.dashboard.service.RegisterService;
+import nextstep.joanne.server.handler.controller.Controller;
+import nextstep.joanne.server.handler.controller.ControllerFactory;
+import nextstep.joanne.server.handler.controller.ResourceController;
+import nextstep.joanne.server.http.request.HttpRequest;
+import nextstep.joanne.server.http.request.RequestLine;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HandlerMappingTest {
+
+    private HandlerMapping handlerMapping;
+
+    @BeforeEach
+    void setUp() {
+        handlerMapping = new HandlerMapping(ControllerFactory.addControllers());
+    }
+
+    @DisplayName("HandlerMapping에서 url로 Controller를 찾는다. - index.html")
+    @Test
+    void getWhenIndex() {
+        // given
+        HttpRequest httpRequest = new HttpRequest(
+                new RequestLine(null, "/index", null),
+                null,
+                null
+        );
+
+        // when
+        Controller controller = handlerMapping.get(httpRequest);
+
+        // then
+        assertThat(controller).isInstanceOf(ResourceController.class);
+    }
+
+    @DisplayName("HandlerMapping에서 url로 Controller를 찾는다. - login.html")
+    @Test
+    void getWhenLogin() {
+        // given
+        HttpRequest httpRequest = new HttpRequest(
+                new RequestLine(null, "/login", null),
+                null,
+                null
+        );
+
+        // when
+        Controller controller = handlerMapping.get(httpRequest);
+
+        // then
+        assertThat(controller).isInstanceOf(LoginController.class);
+    }
+
+    @DisplayName("HandlerMapping에서 url로 Controller를 찾는다. - register.html")
+    @Test
+    void getWhenRegister() {
+        // given
+        HttpRequest httpRequest = new HttpRequest(
+                new RequestLine(null, "/register", null),
+                null,
+                null
+        );
+
+        // when
+        Controller controller = handlerMapping.get(httpRequest);
+
+        // then
+        assertThat(controller).isInstanceOf(RegisterController.class);
+    }
+}

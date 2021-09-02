7f1a9acd545d519ca7076124d5f7c1e468d798e6
seunghyun <ggyool@naver.com>
2021-09-03 04:07:46 +0900

    feat: HttpVersion 생성

--- app/src/main/java/nextstep/jwp/http/common/HttpVersion.java
+++ app/src/main/java/nextstep/jwp/http/common/HttpVersion.java
@@ -1,12 +1,24 @@
 package nextstep.jwp.http.common;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public enum HttpVersion {
     HTTP_1_1("HTTP/1.1");
 
-    private String value;
+    private final String value;
 
     HttpVersion(String value) {
         this.value = value;
+    }
+
+    public static HttpVersion from(String version) {
+        return Arrays.stream(values())
+                .filter(httpVersion -> httpVersion.value.equals(version))
+                .findAny()
+                .orElseThrow(() -> new NoSuchElementException(
+                        String.format("존재하지 않는 HTTP 버전입니다.(%s)", version))
+                );
     }
 
     public String getValue() {

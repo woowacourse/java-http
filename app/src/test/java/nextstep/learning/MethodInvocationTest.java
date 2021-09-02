6047bfc62dab0108518679109ece553f58092845
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-09-02 21:35:02 +0900

    test: 리플렉션 Method Invoke에 대한 테스트 작성

--- app/src/test/java/nextstep/learning/MethodInvocationTest.java
+++ app/src/test/java/nextstep/learning/MethodInvocationTest.java
@@ -22,7 +22,7 @@ public class MethodInvocationTest {
         assertThat((String) result).isEqualTo("String&&1000");
     }
 
-    String textNumberCombine(String text, Integer number) {
+    public String textNumberCombine(String text, Integer number) {
         return text + "&&" + number;
     }
 }

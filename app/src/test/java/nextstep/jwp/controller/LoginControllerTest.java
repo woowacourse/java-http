0ac38a0511b127d7d35a16589098917909ded89b
dwl5 <ssop6403@gmail.com>
2021-09-03 23:55:42 +0900

    refactor: LoginController 접근지정자 빠진 부분

--- app/src/test/java/nextstep/jwp/controller/LoginControllerTest.java
+++ app/src/test/java/nextstep/jwp/controller/LoginControllerTest.java
@@ -18,7 +18,7 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.*;
 
 class LoginControllerTest {
-    LoginController loginController;
+    private LoginController loginController;
 
     @BeforeEach
     private void setUp() {

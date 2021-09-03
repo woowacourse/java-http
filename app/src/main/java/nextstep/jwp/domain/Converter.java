b2bf3e3f46b3a2e47ef288edc3fa8ef7ab297b8c
youngbin <thisisyoungbin@gmail.com>
2021-09-03 11:28:37 +0900

    refactor: Converter 클래스 내 매직넘버 상수화

--- app/src/main/java/nextstep/jwp/domain/Converter.java
+++ app/src/main/java/nextstep/jwp/domain/Converter.java
@@ -13,6 +13,17 @@ import java.util.Map;
 public class Converter {
 
     private static final String NEW_LINE = "\r\n";
+    private static final String HEADER_DELIMITER = ": ";
+    private static final int REQUEST_LINE_INDEX = 0;
+    private static final int METHOD = 0;
+    private static final int URI = 1;
+    private static final int PROTOCOL_VERSION = 2;
+    private static final String CONTENT_LENGTH = "Content-Length";
+    private static final String BLANK = " ";
+    private static final String REQUEST_BODY_DELIMITER = "&";
+    private static final String REQUEST_BODY_KEY_VALUE_DELIMITER = "=";
+    private static final int KEY = 0;
+    private static final int VALUE = 1;
 
     private Converter() {
     }
@@ -34,9 +45,9 @@ public class Converter {
 
     private static HttpRequest createHttpRequest(StringBuilder requestHeaders, BufferedReader reader) throws IOException {
         String[] splitRequestHeaders = requestHeaders.toString().split(NEW_LINE);
-        String[] requestLineArr = splitRequestHeaders[0].split(" ");
+        String[] requestLineArr = splitRequestHeaders[REQUEST_LINE_INDEX].split(BLANK);
 
-        RequestLine requestLine = new RequestLine(requestLineArr[0], Uri.of(requestLineArr[1]), requestLineArr[2]);
+        RequestLine requestLine = new RequestLine(requestLineArr[METHOD], Uri.of(requestLineArr[URI]), requestLineArr[PROTOCOL_VERSION]);
         Map<String, String> httpHeaders = createRequestHeaders(splitRequestHeaders);
         RequestBody requestBody = createRequestBody(httpHeaders, reader);
 
@@ -46,16 +57,16 @@ public class Converter {
     private static Map<String, String> createRequestHeaders(String[] splitRequestHeaders) {
         Map<String, String> headers = new LinkedHashMap<>();
         for (int i = 1; i < splitRequestHeaders.length; i++) {
-            String[] split = splitRequestHeaders[i].split(": ");
-            headers.put(split[0].trim(), split[1].trim());
+            String[] split = splitRequestHeaders[i].split(HEADER_DELIMITER);
+            headers.put(split[KEY].trim(), split[VALUE].trim());
 
         }
         return headers;
     }
 
     private static RequestBody createRequestBody(Map<String, String> httpHeaders, BufferedReader reader) throws IOException {
-        if (httpHeaders.get("Content-Length") != null) {
-            int contentLength = Integer.parseInt(httpHeaders.get("Content-Length"));
+        if (httpHeaders.get(CONTENT_LENGTH) != null) {
+            int contentLength = Integer.parseInt(httpHeaders.get(CONTENT_LENGTH));
             char[] buffer = new char[contentLength];
             reader.read(buffer, 0, contentLength);
             Map<String, String> requestBodyMap = collectRequestBody(new String(buffer));
@@ -69,10 +80,10 @@ public class Converter {
             return null;
         }
         Map<String, String> requestBodyMap = new HashMap<>();
-        String[] splitRequestBody = requestBody.split("&");
+        String[] splitRequestBody = requestBody.split(REQUEST_BODY_DELIMITER);
         for (String requestParam : splitRequestBody) {
-            String[] splitRequestParam = requestParam.split("=");
-            requestBodyMap.put(splitRequestParam[0], splitRequestParam[1]);
+            String[] splitRequestParam = requestParam.split(REQUEST_BODY_KEY_VALUE_DELIMITER);
+            requestBodyMap.put(splitRequestParam[KEY], splitRequestParam[VALUE]);
         }
         return requestBodyMap;
     }

492a8ca5adf4377a849af71466a33b8abffcd94b
Solji Choi <bssj9307@gmail.com>
2021-09-13 15:47:47 +0900

    refactor: 세션 분기 처리 수정

--- app/src/main/java/nextstep/jwp/http/HttpSessions.java
+++ app/src/main/java/nextstep/jwp/http/HttpSessions.java
@@ -8,13 +8,16 @@ public class HttpSessions {
     private static final Map<String, HttpSession> SESSIONS = new HashMap<>();
 
     public static HttpSession getSession(String id) {
-        HttpSession httpSession = SESSIONS.get(id);
-
-        if (httpSession == null) {
-            httpSession = new HttpSession(id);
-            SESSIONS.put(id, httpSession);
+        if (hasSession(id)) {
+            return SESSIONS.get(id);
         }
+        HttpSession httpSession = new HttpSession(id);
+        SESSIONS.put(id, httpSession);
         return httpSession;
+    }
+
+    public static boolean hasSession(String id) {
+        return SESSIONS.get(id) != null;
     }
 
     static void remove(String id) {

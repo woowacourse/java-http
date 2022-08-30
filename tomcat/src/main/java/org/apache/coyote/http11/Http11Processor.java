02529d9c7469bbacdb01311d54b4e701314f875c
EastHShin <xldk78@gmail.com>
2022-08-30 17:47:01 +0900

    feat: html 응답 및 css 지원 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,17 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,19 +33,85 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            final BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            final String line = bufferedReader.readLine();
+
+            if (line == null) {
+                return;
+            }
+            final String response = getResponse(line);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String getResponse(String line) throws IOException {
+        final String[] split = line.split(" ");
+        final String resource = split[1];
+
+        if (resource.contains("css")) {
+            return extractCssResponse(getResponseBody(resource));
+        }
+        if (resource.contains("js")) {
+            return extractJsResponse(getResponseBody(resource));
+        }
+
+        return extractHtmlResponse(getHtmlResponseBody(resource));
+
+    }
+
+    private String getHtmlResponseBody(String resource) {
+        final StringBuilder stringBuilder = new StringBuilder(resource);
+        if (!resource.contains(".html")) {
+            stringBuilder.append(".html");
+        }
+
+        return getResponseBody(stringBuilder.toString());
+    }
+
+    private String getResponseBody(String resource) {
+        URL url = this.getClass().getClassLoader().getResource("static/" + resource);
+        final File file = new File(url.getPath());
+        final Path path = file.toPath();
+
+        try {
+            final String responseBody = Files.readString(path);
+            return responseBody;
+        } catch (IOException exception) {
+            log.error(exception.getMessage());
+            return null;
+        }
+    }
+
+    private String extractHtmlResponse(String responseBody) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+    }
+
+    private String extractCssResponse(String responseBody) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/css;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+    }
+
+    private String extractJsResponse(String responseBody) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/javascript;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
     }
 }
--- tomcat/src/test/java/study/FileTest.java
+++ tomcat/src/test/java/study/FileTest.java
@@ -1,5 +1,9 @@
 package study;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -26,7 +30,8 @@ class FileTest {
         final String fileName = "nextstep.txt";
 
         // todo
-        final String actual = "";
+        File file = new File(this.getClass().getClassLoader().getResource(fileName).getPath());
+        final String actual = file.getPath();
 
         assertThat(actual).endsWith(fileName);
     }
@@ -36,14 +41,16 @@ class FileTest {
      * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws IOException, URISyntaxException {
         final String fileName = "nextstep.txt";
 
         // todo
-        final Path path = null;
+
+        File file = new File(this.getClass().getClassLoader().getResource(fileName).getPath());
+        final Path path = file.toPath();
 
         // todo
-        final List<String> actual = Collections.emptyList();
+        final List<String> actual = Files.readAllLines(path);
 
         assertThat(actual).containsOnly("nextstep");
     }

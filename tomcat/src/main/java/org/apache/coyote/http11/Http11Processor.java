36663bf6fe6aadb409879cae3e86c7957a1e1bcf
DOES <ldk980130@gmail.com>
2022-08-31 20:28:27 +0900

    feat: GET /index.html 응답하기 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,46 +1,90 @@
 package org.apache.coyote.http11;
 
-import nextstep.jwp.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
+import nextstep.jwp.exception.UncheckedServletException;
 
 public class Http11Processor implements Runnable, Processor {
 
-    private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+	private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
 
-    private final Socket connection;
+	private final Socket connection;
 
-    public Http11Processor(final Socket connection) {
-        this.connection = connection;
-    }
+	public Http11Processor(final Socket connection) {
+		this.connection = connection;
+	}
 
-    @Override
-    public void run() {
-        process(connection);
-    }
+	@Override
+	public void run() {
+		process(connection);
+	}
 
-    @Override
-    public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+	@Override
+	public void process(final Socket connection) {
+		try (final var inputStream = connection.getInputStream();
+			 final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+			final var request = parseHttpRequest(inputStream);
+			final var response = makeHttpResponse(request);
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+			outputStream.write(response.getBytes());
+			outputStream.flush();
+		} catch (IOException | UncheckedServletException e) {
+			log.error(e.getMessage(), e);
+		}
+	}
 
-            outputStream.write(response.getBytes());
-            outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
-            log.error(e.getMessage(), e);
-        }
-    }
+	private String parseHttpRequest(InputStream inputStream) throws IOException {
+		BufferedReader requestReader = new BufferedReader(new InputStreamReader(inputStream));
+		return requestReader.readLine();
+	}
+
+	private String makeHttpResponse(String request) throws IOException {
+		String requestUri = extractRequestUri(request);
+		if (requestUri.equals("/")) {
+			return constructHttpMessage("Hello world!", 200);
+		}
+		if (requestUri.equals("/index.html")) {
+			return constructHttpMessage(getHtml("static/index.html"), 200);
+		}
+		return constructHttpMessage(getHtml("static/404.html"), 404);
+	}
+
+	private String extractRequestUri(String request) {
+		return request
+			.split("\r\n")[0]
+			.split(" ")[1];
+	}
+
+	private String getHtml(String path) throws IOException {
+		final URL resource = getClass().getClassLoader().getResource(path);
+		return new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+	}
+
+	private String constructHttpMessage(String responseBody, int statusCode) {
+		String status = "";
+		if (statusCode == 200) {
+			status = " OK ";
+		}
+		if (statusCode == 404) {
+			status = " Not Found ";
+		}
+		return String.join("\r\n",
+			"HTTP/1.1 " + statusCode + status,
+			"Content-Type: text/html;charset=utf-8 ",
+			"Content-Length: " + responseBody.getBytes().length + " ",
+			"",
+			responseBody);
+	}
 }
--- tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -2,6 +2,7 @@ package nextstep.org.apache.coyote.http11;
 
 import support.StubSocket;
 import org.apache.coyote.http11.Http11Processor;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -13,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class Http11ProcessorTest {
 
+    @DisplayName("/ 요청이 들어오면 Hello world!를 출력한다.")
     @Test
     void process() {
         // given
@@ -33,6 +35,7 @@ class Http11ProcessorTest {
         assertThat(socket.output()).isEqualTo(expected);
     }
 
+    @DisplayName("/index.html 요청이 들어오면 index.html을 출력한다.")
     @Test
     void index() throws IOException {
         // given
@@ -51,11 +54,41 @@ class Http11ProcessorTest {
 
         // then
         final URL resource = getClass().getClassLoader().getResource("static/index.html");
+        String responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
         var expected = "HTTP/1.1 200 OK \r\n" +
                 "Content-Type: text/html;charset=utf-8 \r\n" +
-                "Content-Length: 5564 \r\n" +
+                "Content-Length: " + responseBody.getBytes().length + " \r\n" +
                 "\r\n"+
-                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+            responseBody;
+
+        assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @DisplayName("없는 페이지 요청이 들어오면 404.html을 출력한다.")
+    @Test
+    void notFound() throws IOException {
+        // given
+        final String httpRequest= String.join("\r\n",
+            "GET /notFound.html HTTP/1.1 ",
+            "Host: localhost:8080 ",
+            "Connection: keep-alive ",
+            "",
+            "");
+
+        final var socket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        final URL resource = getClass().getClassLoader().getResource("static/404.html");
+        String responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+        var expected = "HTTP/1.1 404 Not Found \r\n" +
+            "Content-Type: text/html;charset=utf-8 \r\n" +
+            "Content-Length: " + responseBody.getBytes().length + " \r\n" +
+            "\r\n"+
+            responseBody;
 
         assertThat(socket.output()).isEqualTo(expected);
     }

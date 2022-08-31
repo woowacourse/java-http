bff9bcf70fb7e9fc2b1e32563d5dae1f556f52bb
YJGwon <yj970125@gmail.com>
2022-08-31 19:41:51 +0900

    feat(Http11Processor): 정적 자원 제공 구현
    - 경로에 따라 static resource 제공
    - accept header에 따라 content type 응답

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,19 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -26,21 +33,60 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
+             final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final String requestUrl = parseUrl(reader);
+            log.info("request url : {}", requestUrl);
+            if (reader.readLine() == null) {
+                return;
+            }
+            final Map<String, String> header = parseHeader(reader);
+            final String responseBody = getResourceFromUrl(requestUrl);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Type: " + parseContentType(header) + ";charset=utf-8 ",
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
                     responseBody);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String parseUrl(final BufferedReader reader) throws IOException {
+        return reader.readLine().split(" ")[1];
+    }
+
+    private Map<String, String> parseHeader(final BufferedReader reader) throws IOException {
+        final Map<String, String> header = new HashMap<>();
+        String line = reader.readLine();
+        while (!"".equals(line)) {
+            final String[] splitLine = line.split(": ", 2);
+            header.put(splitLine[0], splitLine[1]);
+            line = reader.readLine();
+        }
+        return header;
+    }
+
+    private String parseContentType(final Map<String, String> header) {
+        return header
+                .getOrDefault("Accept", "text/html")
+                .split(",")[0];
+    }
+
+    private String getResourceFromUrl(final String requestUrl) throws URISyntaxException, IOException {
+        if (!requestUrl.equals("/")) {
+            final URI uri = getClass()
+                    .getClassLoader()
+                    .getResource("static" + requestUrl)
+                    .toURI();
+            return new String(Files.readAllBytes(Path.of(uri)));
+        }
+        return "Hello world!";
     }
 }

e6061ad8b282e3b9ef3a13b69665fda490e6dccc
cjlee38 <cjlee38@github.com>
2022-08-31 17:29:33 +0900

    feat: index.html 및 정적 파일에 응답할 수 있도록 수정

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,18 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -25,10 +31,11 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+        try (final InputStream inputStream = connection.getInputStream();
+             final OutputStream outputStream = connection.getOutputStream()) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
-            final var responseBody = "Hello world!";
+            String responseBody = findResponseBody(reader);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -42,5 +49,40 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String findResponseBody(BufferedReader reader) {
+        String path = parsePath(reader);
+        if (isRootRequest(path)) {
+            return "Hello world!";
+        }
+        return ConvertToString(findStaticFile(path));
+    }
+
+    private String ConvertToString(File file) {
+        try {
+            byte[] bytes = Files.readAllBytes(file.toPath());
+            return new String(bytes);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private File findStaticFile(String path) {
+        final URL resource = getClass().getClassLoader().getResource("static/" + path);
+        return new File(resource.getFile());
+    }
+
+    private String parsePath(BufferedReader reader) {
+        try {
+            String startLine = reader.readLine();
+            return startLine.split(" ")[1];
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static boolean isRootRequest(String path) {
+        return path.equals("/");
     }
 }

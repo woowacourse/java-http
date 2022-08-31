29b43b0b0a537fd2c86a0d3946e20487d3832e3e
Donggyu <a29661498@gmail.com>
2022-08-31 13:20:17 +0900

    feat: Http11Processor 파일 읽도록 수정

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,18 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -26,9 +32,17 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+             final BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
 
-            final var responseBody = "Hello world!";
+            String responseBody = "Hello world!";
+
+            final String requestUrl = readRequestUrl(bufferedReader);
+
+            if (requestUrl.contains(".html")) {
+                responseBody = new String(readAllFile(requestUrl), StandardCharsets.UTF_8);
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -42,5 +56,16 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private static String readRequestUrl(final BufferedReader bufferedReader) throws IOException {
+        final String httpStartLine = bufferedReader.readLine();
+        return httpStartLine.split(" ")[1];
+    }
+
+    private static byte[] readAllFile(final String requestUrl) throws IOException {
+        final URL resourceUrl = ClassLoader.getSystemResource("static" + requestUrl);
+        final Path path = new File(resourceUrl.getPath()).toPath();
+        return Files.readAllBytes(path);
     }
 }

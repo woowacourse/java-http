c5b921df115582ded236bee92a2c86eeb6531edd
ulimy <ulim130@gmail.com>
2022-08-31 14:06:48 +0900

    feat : GET /index.html 응답하기

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,20 @@
 package org.apache.coyote.http11;
 
-import nextstep.jwp.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
+import nextstep.jwp.exception.UncheckedServletException;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,19 +36,48 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            BufferedReader request = new BufferedReader(new InputStreamReader(inputStream));
+
+            String inputUri = request.readLine().split(" ")[1];
+
+            var responseBody = "Hello world!";
+
+            if (!inputUri.equals("/")) {
+                responseBody = readFile(inputUri);
+            }
 
             final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        }
+    }
+
+    private String readFile(String inputUri) {
+        try {
+            final Path path = Paths.get(getClass()
+                .getClassLoader()
+                .getResource("static" + inputUri)
+                .toURI());
+
+            final List<String> contents = Files.readAllLines(path);
+
+            StringBuilder result = new StringBuilder();
+            for (String content : contents) {
+                result.append(content + "\n");
+            }
+
+            return result.toString();
+        } catch (IOException | URISyntaxException e) {
+            log.error(e.getMessage(), e);
+            return "";
         }
     }
 }

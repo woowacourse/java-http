7f3649b6810fe1fa376bc4f19a9e2ad77c1f2165
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-08-31 18:38:24 +0900

    refactor: 미사용 메소드 제거

--- tomcat/src/main/java/nextstep/jwp/exception/UncheckedServletException.java
+++ tomcat/src/main/java/nextstep/jwp/exception/UncheckedServletException.java
@@ -2,10 +2,6 @@ package nextstep.jwp.exception;
 
 public class UncheckedServletException extends RuntimeException {
 
-    public UncheckedServletException(Exception e) {
-        super(e);
-    }
-
     public UncheckedServletException(final String message) {
         super(message);
     }

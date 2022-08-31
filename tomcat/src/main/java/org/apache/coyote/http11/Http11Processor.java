2b04c5b8690d707233e46f8666549a0b2472aa28
devHudi <devhudi@gmail.com>
2022-08-31 23:14:26 +0900

    feat: 정적 파일 서빙 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,15 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,7 +31,14 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            String httpMessageStartLine = bufferedReader.readLine();
+            String requestUri = httpMessageStartLine.split(" ")[1];
+
+            Path path = Path.of(getClass().getClassLoader().getResource("static" + requestUri).getPath());
+            final var responseBody = Files.readString(path);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",

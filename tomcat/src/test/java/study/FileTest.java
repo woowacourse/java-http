03c9b1dcb2986c714413406e3109afa109cd1068
jayjaehunchoi <wogns0108@naver.com>
2022-08-31 10:57:00 +0900

    test: resource 디렐터리 파일 경로 찾기 테스트

--- tomcat/src/test/java/study/FileTest.java
+++ tomcat/src/test/java/study/FileTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import java.io.File;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -24,9 +25,7 @@ class FileTest {
     @Test
     void resource_디렉터리에_있는_파일의_경로를_찾는다() {
         final String fileName = "nextstep.txt";
-
-        // todo
-        final String actual = "";
+        final String actual = new File(fileName).getPath();
 
         assertThat(actual).endsWith(fileName);
     }

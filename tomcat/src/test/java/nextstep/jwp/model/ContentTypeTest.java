8a38e0579adea3d25e50c7e0ce865b4989a7113d
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-08-31 11:52:56 +0900

    feat: contenttype enum 추가 및 변환 테스트

---
+++ tomcat/src/test/java/nextstep/jwp/model/ContentTypeTest.java
@@ -0,0 +1,24 @@
+package nextstep.jwp.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import nextstep.jwp.exception.NotFoundContentTypeException;
+import org.junit.jupiter.api.Test;
+
+class ContentTypeTest {
+
+    @Test
+    void 없는_extension을_입력하는_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> ContentType.fromExtension(".exe"))
+                .isInstanceOf(NotFoundContentTypeException.class);
+    }
+
+    @Test
+    void extension에_일치하는_contenttype으로_변환한다() {
+        ContentType expected = ContentType.TEXT_HTML;
+        ContentType actual = ContentType.fromExtension(".html");
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}

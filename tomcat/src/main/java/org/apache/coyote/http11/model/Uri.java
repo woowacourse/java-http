d95b2b6bc2c0adfa0c12300d284eea536cab9e08
yeon-06 <rnjstldus2@gmail.com>
2022-09-01 16:52:32 +0900

    refactor: Uri 내부의 하드코딩 제거

--- tomcat/src/main/java/org/apache/coyote/http11/model/Uri.java
+++ tomcat/src/main/java/org/apache/coyote/http11/model/Uri.java
@@ -6,6 +6,14 @@ import java.util.Map;
 
 public class Uri {
 
+    private static final char QUERY_STRING_STANDARD = '?';
+    private static final String QUERY_STRINGS_BOUNDARY = "&";
+    private static final String KEY_VALUE_BOUNDARY = "=";
+    private static final String PATH_REGEX = "\\.";
+    private static final int INDEX_NOT_FOUND = -1;
+    private static final int KEY_INDEX = 0;
+    private static final int VALUE_INDEX = 1;
+
     private final String url;
     private final Map<String, String> queryString;
 
@@ -15,33 +23,33 @@ public class Uri {
     }
 
     private String extractURL(final String uri) {
-        int index = uri.lastIndexOf('?');
-        if (index == -1) {
+        int index = uri.lastIndexOf(QUERY_STRING_STANDARD);
+        if (index == INDEX_NOT_FOUND) {
             return uri;
         }
         return uri.substring(0, index);
     }
 
     private Map<String, String> extractQueryString(final String uri) {
-        int index = uri.lastIndexOf('?');
-        if (index == -1) {
+        int index = uri.lastIndexOf(QUERY_STRING_STANDARD);
+        if (index == INDEX_NOT_FOUND) {
             return new HashMap<>();
         }
 
         final Map<String, String> result = new HashMap<>();
         final String[] queryStrings = uri.substring(index + 1)
-                .split("&");
+                .split(QUERY_STRINGS_BOUNDARY);
 
         for (String queryString : queryStrings) {
-            String[] keyValuePair = queryString.split("=");
-            result.put(keyValuePair[0], keyValuePair[1]);
+            String[] keyValuePair = queryString.split(KEY_VALUE_BOUNDARY);
+            result.put(keyValuePair[KEY_INDEX], keyValuePair[VALUE_INDEX]);
         }
 
         return result;
     }
 
     public ContentType findContentType() {
-        final String[] pathInfos = this.url.split("\\.");
+        final String[] pathInfos = this.url.split(PATH_REGEX);
         final String fileExtension = pathInfos[pathInfos.length - 1];
         return ContentType.of(fileExtension);
     }

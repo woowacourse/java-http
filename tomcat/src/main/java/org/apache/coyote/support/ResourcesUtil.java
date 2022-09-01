10dd3a6eabf8636cf5d2abfa9344f08c06bc3de0
Ohzzi <clover7even@naver.com>
2022-09-01 21:05:33 +0900

    feat: resource가 존재하는지 검증하는 로직 추가

--- tomcat/src/main/java/org/apache/coyote/support/ResourcesUtil.java
+++ tomcat/src/main/java/org/apache/coyote/support/ResourcesUtil.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Objects;
+import org.apache.coyote.exception.ResourceNotFoundException;
 
 public class ResourcesUtil {
 
@@ -13,7 +15,11 @@ public class ResourcesUtil {
 
     public static String readResource(final String resourcePath) throws IOException {
         URL resource = Thread.currentThread().getContextClassLoader().getResource(resourcePath);
-        assert resource != null;
+        try {
+            Objects.requireNonNull(resource);
+        } catch (NullPointerException e) {
+            throw new ResourceNotFoundException(resourcePath);
+        }
         Path path = new File(resource.getPath()).toPath();
         return new String(Files.readAllBytes(path));
     }
--- tomcat/src/test/java/org/apache/coyote/support/ResourcesUtilTest.java
+++ tomcat/src/test/java/org/apache/coyote/support/ResourcesUtilTest.java
@@ -1,11 +1,13 @@
 package org.apache.coyote.support;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import org.apache.coyote.exception.ResourceNotFoundException;
 import org.junit.jupiter.api.Test;
 
 class ResourcesUtilTest {
@@ -24,5 +26,15 @@ class ResourcesUtilTest {
         String expected = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
 
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void 없는_resource를_읽으려_하면_예외를_반환한다() throws IOException {
+        // given
+        String fileName = "invalid resource";
+
+        // when, then
+        assertThatThrownBy(() -> ResourcesUtil.readResource(fileName))
+                .isExactlyInstanceOf(ResourceNotFoundException.class);
     }
 }

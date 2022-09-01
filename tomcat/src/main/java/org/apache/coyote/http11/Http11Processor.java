f50d208251dd24e69b26ec431056797c22efc68c
dwl21 <nggus5@naver.com>
2022-09-01 23:45:47 +0900

    feat: 로그인 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,16 +1,17 @@
 package org.apache.coyote.http11;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import nextstep.jwp.exception.UncheckedServletException;
+import nextstep.jwp.handler.LoginHandler;
 import nextstep.jwp.http.ContentType;
+import nextstep.jwp.http.HttpRequest;
+import nextstep.jwp.utils.FileUtils;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,8 +20,6 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
-
-    private static final String FILE_NAME_DELIMITER = ".";
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
 
@@ -37,11 +36,13 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
+        final int REQUEST_LINE_INDEX = 0;
         try (final var inputStream = connection.getInputStream();
             final var outputStream = connection.getOutputStream()) {
             final List<String> request = extractRequest(inputStream);
-            String responseBody = handler(extractUri(request));
-            outputStream.write(writeResponseOk(ContentType.of(extractFileExtension(request)).getMediaType(), responseBody));
+            HttpRequest httpRequest = HttpRequest.from(request.get(REQUEST_LINE_INDEX));
+            String responseBody = handler(httpRequest);
+            outputStream.write(writeResponseOk(ContentType.from(httpRequest.getFileExtension()).getMediaType(), responseBody));
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
@@ -58,31 +59,16 @@ public class Http11Processor implements Runnable, Processor {
         }
         return request;
     }
-
-    private String extractUri(List<String> request) {
-        return request.get(0).split(" ")[1];
-    }
-
-    private String extractFileExtension(List<String> request) {
-        final int NO_EXISTING = -1;
-        String uri = extractUri(request);
-        int fileNameDelimiterIndex = uri.lastIndexOf(FILE_NAME_DELIMITER);
-        if (fileNameDelimiterIndex == NO_EXISTING) {
-            return ContentType.TEXT_HTML.getFileExtension();
-        }
-        return uri.substring(fileNameDelimiterIndex + 1);
-    }
-
-    private String handler(String uri) throws IOException {
-        if ("/".equals(uri)) {
+    private String handler(HttpRequest httpRequest) throws IOException {
+        String path = httpRequest.getPath();
+        if ("/".equals(path)) {
             return "Hello world!";
         }
-        return readFile(getResource(uri));
-    }
-
-    private String readFile(URL resource) throws IOException {
-        return new String(Files.readAllBytes(new File(resource.getFile())
-            .toPath()));
+        if ("/login".equals(path)) {
+            LoginHandler.login(httpRequest.getQueryParams());
+            return FileUtils.readFile(getResource("/login.html"));
+        }
+        return FileUtils.readFile(getResource(path));
     }
 
     private URL getResource(String uri) {

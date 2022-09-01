1e46c60477a8560dab0659d414e3c7ffb838ae14
BeomsooKim <kbsat@naver.com>
2022-09-01 14:51:58 +0900

    feat: `GET /index.html 응답하기` 구현

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ## 기능 요구사항
 
 ### 1. HTTP 서버 구현하기
-- [ ] GET /index.html 응답하기
+- [x] GET /index.html 응답하기
 - [ ] CSS 지원하기
 - [ ] Query String 파싱
--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,19 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,19 +35,48 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            // HTTP 요청 읽기
+            final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            final List<String> httpRequest = new ArrayList<>();
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            String line = bufferedReader.readLine();
+            httpRequest.add(line);
+            while (!"".equals(line)) {
+                if (line == null) {
+                    return;
+                }
+                line = bufferedReader.readLine();
+                httpRequest.add(line);
+            }
 
-            outputStream.write(response.getBytes());
+            final String s = httpRequest.get(0);
+            final String[] firstLine = s.split(" ");
+            final String httpMethod = firstLine[0];
+            final String targetUri = firstLine[1];
+
+            // 화면 렌더링
+            if (targetUri.equals("/index.html") && httpMethod.equals("GET")) {
+                render(outputStream, "index.html");
+            } else {
+                render(outputStream, "404.html");
+            }
+
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (final IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private void render(final OutputStream outputStream, final String viewName) throws IOException {
+        final URL resource = getClass().getClassLoader().getResource("static/" + viewName);
+        final byte[] indexHtml = Files.readAllBytes(new File(resource.getFile()).toPath());
+
+        final var response = String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + indexHtml.length + " ",
+                "",
+                new String(indexHtml));
+        outputStream.write(response.getBytes());
     }
 }

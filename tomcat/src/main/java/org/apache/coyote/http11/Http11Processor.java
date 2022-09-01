e6ae6d491af68996896993b5114e41977476294a
kth990303 <kth990303@naver.com>
2022-09-01 14:01:23 +0900

    feat: html 파일을 받아오도록 Http11Processor 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,16 +1,28 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.regex.Pattern;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
-
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final int REQUEST_URL_INDEX = 1;
+    private static final Pattern FILE_REGEX = Pattern.compile(".+\\.(html|css|js|ico)");
+
 
     private final Socket connection;
 
@@ -26,21 +38,44 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
-
-            final var responseBody = "Hello world!";
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
-
-            outputStream.write(response.getBytes());
+             final var outputStream = connection.getOutputStream();
+             final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))
+        ) {
+            final String requestUrl =
+                    Objects.requireNonNull(bufferedReader.readLine()).split(" ")[REQUEST_URL_INDEX];
+            if (requestUrl.equals("/")) {
+                outputStream.write(createHelloResponse().getBytes(StandardCharsets.UTF_8));
+            }
+            if (FILE_REGEX.matcher(requestUrl).matches()) {
+                outputStream.write(createFileResponse(requestUrl, "text/html").getBytes());
+            }
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String createFileResponse(final String requestUrl, final String contentType)
+            throws URISyntaxException, IOException {
+        final URL resource =
+                this.getClass().getClassLoader().getResource("static" + requestUrl);
+        final Path path = Paths.get(resource.toURI());
+        final var responseBody = new String(Files.readAllBytes(path));
+
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: " + contentType + ";charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+    }
+
+    private String createHelloResponse() {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: 12 ",
+                "",
+                "Hello world!");
     }
 }

4f3943f5bcb85e949d6ee8a2a45a651e4d29201c
beomWhale <cndqjacndqja@gmail.com>
2022-09-02 02:54:53 +0900

    test: Http11ProcessorTest 학습 테스트 추가

--- tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -51,11 +51,13 @@ class Http11ProcessorTest {
 
         // then
         final URL resource = getClass().getClassLoader().getResource("static/index.html");
+        String responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+        System.out.println("테스트"+responseBody.getBytes().length);
         var expected = "HTTP/1.1 200 OK \r\n" +
                 "Content-Type: text/html;charset=utf-8 \r\n" +
-                "Content-Length: 5564 \r\n" +
+                "Content-Length: "+ responseBody.getBytes().length +" \r\n" +
                 "\r\n"+
-                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+                responseBody;
 
         assertThat(socket.output()).isEqualTo(expected);
     }

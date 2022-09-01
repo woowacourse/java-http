396d4f0aa192b9cba805146406aa3e3464f16e32
nailseong <nnis9857@gmail.com>
2022-09-01 17:10:38 +0900

    feat: POST 방식으로 회원가입

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -26,6 +26,7 @@ public class Http11Processor implements Runnable, Processor {
     private static final String TEXT_HTML = "text/html";
     private static final String TEXT_CSS = "text/css";
     private static final String APPLICATION_JAVASCRIPT = "application/javascript";
+    private static final String POST = "POST";
 
     private final Socket connection;
 
@@ -45,21 +46,39 @@ public class Http11Processor implements Runnable, Processor {
              final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
              final BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
 
-            final String requestUri = getRequestUri(bufferedReader);
+            final String requestStartLine = bufferedReader.readLine();
+            log.info("request - {}", requestStartLine);
 
-            final Map<String, String> queryParam = getQueryParam(requestUri);
-            if (requestUri.contains("login") && queryParam.containsKey("account") && queryParam.containsKey(
-                    "password")) {
-                final Optional<User> possibleUser = InMemoryUserRepository.findByAccount(queryParam.get("account"));
-                if (possibleUser.isPresent()) {
+            final String requestUri = getRequestUri(requestStartLine);
+            final String requestMethod = getRequestMethod(requestStartLine);
+            final Map<String, String> headers = getHeaders(bufferedReader);
+
+            if (requestMethod.equals(POST)) {
+                final int contentLength = Integer.parseInt(headers.get("Content-Length"));
+                final Map<String, String> requestBody = getRequestBody(bufferedReader, contentLength);
+
+                if (requestUri.contains("register")) {
+                    final User user = new User(requestBody.get("account"), requestBody.get("password"),
+                            requestBody.get("email"));
+                    InMemoryUserRepository.save(user);
                     final String response = getFoundResponse();
                     outputStream.write(response.getBytes());
                     outputStream.flush();
                     return;
                 }
+
+                if (requestUri.contains("login")) {
+                    final Optional<User> possibleUser = InMemoryUserRepository.findByAccount(requestBody.get("account"));
+                    if (possibleUser.isPresent()) {
+                        final String response = getFoundResponse();
+                        outputStream.write(response.getBytes());
+                        outputStream.flush();
+                        return;
+                    }
+                }
             }
 
-            final Map<String, String> headers = getHeaders(bufferedReader);
+            final Map<String, String> queryParam = getQueryParam(requestUri);
 
             final String responseBody = getResponseBody(requestUri);
             final String contentType = getContentType(requestUri);
@@ -72,10 +91,12 @@ public class Http11Processor implements Runnable, Processor {
         }
     }
 
-    private String getRequestUri(final BufferedReader bufferedReader) throws IOException {
-        final String requestStartLine = bufferedReader.readLine();
-        final String[] splitRequestStartLine = requestStartLine.split(" ");
-        return splitRequestStartLine[1];
+    private String getRequestUri(final String requestStartLine) {
+        return requestStartLine.split(" ")[1];
+    }
+
+    private String getRequestMethod(final String requestStartLine) {
+        return requestStartLine.split(" ")[0];
     }
 
     private String getRequestPath(final String requestUri) {
@@ -110,6 +131,18 @@ public class Http11Processor implements Runnable, Processor {
 
         return headers;
     }
+
+    private Map<String, String> getRequestBody(final BufferedReader bufferedReader, final int contentLength)
+            throws IOException {
+        final char[] buffer = new char[contentLength];
+        bufferedReader.read(buffer, 0, contentLength);
+        final String requestBody = new String(buffer);
+
+        return Arrays.stream(requestBody.split("&"))
+                .map(it -> it.split("="))
+                .collect(Collectors.toMap(it -> it[0], it -> it[1], (a, b) -> b));
+    }
+
 
     private String getContentType(final String requestUri) {
         if (requestUri.contains(".css")) {

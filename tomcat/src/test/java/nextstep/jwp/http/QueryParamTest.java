e736148b17fe2e4ebf96fb1645177c8664f492b5
dwl21 <nggus5@naver.com>
2022-09-01 23:32:53 +0900

    feat: QueryParams 일급 컬렉션 구현

---
+++ tomcat/src/test/java/nextstep/jwp/http/QueryParamTest.java
@@ -0,0 +1,20 @@
+package nextstep.jwp.http;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class QueryParamTest {
+
+    @Test
+    @DisplayName("key=value 형식의 query parameter 를 구문 분석한다.")
+    void from_success() {
+        String queryParam = "key=value";
+
+        QueryParam actual = QueryParam.from(queryParam);
+
+        QueryParam expected = new QueryParam("key", "value");
+        assertThat(actual).isEqualTo(expected);
+    }
+}

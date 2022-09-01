26f09d19715f947dc2a834e1c16ac0e8838bc9ca
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-09-01 14:01:10 +0900

    feat: GET /index.html 응답 기능 구현 및 테스트 풀이

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,8 +5,17 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -24,9 +33,27 @@ public class Http11Processor implements Runnable, Processor {
     }
 
     @Override
-    public void process(final Socket connection) {
+    public void process(final Socket connection){
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final var inputStreamReader = new InputStreamReader(inputStream);
+             final var bufferedReader = new BufferedReader(inputStreamReader)) {
+
+
+            String[] startLine = bufferedReader.readLine().split(" ");
+            if (startLine[0].equals("GET") && startLine[1].equals("/index.html")) {
+                URL resource = getClass().getClassLoader().getResource("static/index.html");
+
+                final var response = "HTTP/1.1 200 OK \r\n" +
+                        "Content-Type: text/html;charset=utf-8 \r\n" +
+                        "Content-Length: 5564 \r\n" +
+                        "\r\n"+
+                        new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+                return;
+            }
 
             final var responseBody = "Hello world!";
 

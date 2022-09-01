1c14611b5c0601213d6570a4c4ae8339a93f02d8
jojogreen91 <iawbg13@gmail.com>
2022-09-01 15:57:44 +0900

    feat: 3. Query String 파싱 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,14 +1,20 @@
 package org.apache.coyote.http11;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.exception.UncheckedServletException;
-import org.apache.HttpRequestMessage;
-import org.apache.HttpResponseMessage;
+import nextstep.jwp.model.User;
+import org.apache.HttpRequest;
+import org.apache.HttpResponse;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,54 +38,65 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
+            HttpRequest httpRequest = new HttpRequest(inputStream);
 
-            HttpRequestMessage httpRequestMessage = new HttpRequestMessage(inputStream);
+            log.info("[ REQUEST START LINE : {} ]", httpRequest.getStartLine());
 
-//            if (httpRequestMessage.isStaticResourceRequest()) {
-//                String body = Files.readString(
-//                        Paths.get("./tomcat/src/main/resources/static" + httpRequestMessage.getUri())
-//                );
-//                Map<String, String> headerMap = new HashMap<>();
-//                headerMap.put("Content-Type", "text/" + httpRequestMessage.getStaticResourceType() + ";charset=utf-8");
-//                headerMap.put("Content-Length", String.valueOf(body.getBytes().length));
-//                HttpResponseMessage httpResponseMessage = new HttpResponseMessage(
-//                        "HTTP/1.1", "200 OK", headerMap, body
-//                );
-//                outputStream.write(httpResponseMessage.generateMessage().getBytes());
-//                outputStream.flush();
-//
-//                log.info("1");
-//            } else if (httpRequestMessage.getUri().equals("/")) {
-//                String body = "Hello world!";
-//                Map<String, String> headerMap = new HashMap<>();
-//                headerMap.put("Content-Type", "text");
-//                headerMap.put("Content-Length", String.valueOf(body.getBytes().length));
-//                HttpResponseMessage httpResponseMessage = new HttpResponseMessage(
-//                        "HTTP/1.1", "200 OK", headerMap, body
-//                );
-//                outputStream.write(httpResponseMessage.generateMessage().getBytes());
-//                outputStream.flush();
-//
-//                log.info("2");
-//            }
-
-            String body = "Hello world!";
-            Map<String, String> headerMap = new HashMap<>();
-            headerMap.put("Content-Type", "text/html;charset=utf-8");
-            headerMap.put("Content-Length", String.valueOf(body.getBytes().length));
-            HttpResponseMessage httpResponseMessage = new HttpResponseMessage(
-                    "HTTP/1.1", "200 OK", headerMap, body
-            );
-
-            System.out.println("###");
-            System.out.println(httpResponseMessage.generateMessage());
-
-            outputStream.write(httpResponseMessage.generateMessage().getBytes());
-            outputStream.flush();
-
-            log.info("3");
+            if (httpRequest.getUri().equals("/")) {
+                returnIndexPage(outputStream);
+                return;
+            }
+            if (httpRequest.isStaticResourceRequest()) {
+                returnStaticResource(outputStream, httpRequest);
+                return;
+            }
+            if (httpRequest.isQueryString()) {
+                Map<String, String> queryString = httpRequest.parseQueryString();
+                Optional<User> user = InMemoryUserRepository.findByAccount(queryString.get("account"));
+                if (!user.isEmpty() && user.get().checkPassword(queryString.get("password"))) {
+                    log.info("[ {} 계정이 존재합니다. : {} ]", queryString.get("account"), user.get());
+                }
+                returnStaticResource(outputStream, HttpRequest.of("/login.html"));
+                return;
+            }
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private void returnIndexPage(final OutputStream outputStream) throws IOException {
+        String body = "Hello world!";
+        Map<String, String> headerMap = new HashMap<>();
+        headerMap.put("Content-Type", "text/html;charset=utf-8");
+        headerMap.put("Content-Length", String.valueOf(body.getBytes().length));
+        HttpResponse httpResponse = new HttpResponse(
+                "HTTP/1.1", "200 OK", headerMap, body
+        );
+
+        sendResponse(outputStream, httpResponse);
+    }
+
+    private void returnStaticResource(
+            final OutputStream outputStream, final HttpRequest httpRequest
+    ) throws IOException {
+        URL resource = getClass().getClassLoader().getResource("static" + httpRequest.getUri());
+        Path resourcePath = Paths.get(resource.getPath());
+        String body = Files.readString(resourcePath);
+        String httpStatusCode = "200 OK";
+        Map<String, String> headerMap = new HashMap<>();
+        headerMap.put("Content-Type", "text/" + httpRequest.getStaticResourceType() + ";charset=utf-8");
+        headerMap.put("Content-Length", String.valueOf(body.getBytes().length));
+        HttpResponse httpResponse = new HttpResponse(
+                "HTTP/1.1", httpStatusCode, headerMap, body
+        );
+
+        sendResponse(outputStream, httpResponse);
+    }
+
+    private void sendResponse(
+            final OutputStream outputStream, final HttpResponse httpResponse
+    ) throws IOException {
+        outputStream.write(httpResponse.createFullMessage().getBytes());
+        outputStream.flush();
     }
 }

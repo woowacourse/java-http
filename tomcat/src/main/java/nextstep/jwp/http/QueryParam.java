dd6e869f4f11389cc9b8c8943f58fe693367e408
dwl21 <nggus5@naver.com>
2022-09-01 23:32:47 +0900

    feat: QueryParam 값 객체 구현

---
+++ tomcat/src/main/java/nextstep/jwp/http/QueryParam.java
@@ -0,0 +1,34 @@
+package nextstep.jwp.http;
+
+public class QueryParam {
+
+    private static final String QUERY_PARAM_SEPARATOR = "=";
+    private static final int QUERY_PARAM_BEGUN_INDEX = 0;
+    private static final int EXCLUDING_HERE = 1;
+    private final String key;
+    private final String value;
+
+    public QueryParam(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public static QueryParam from(String queryParam) {
+        int separatorIndex = queryParam.lastIndexOf(QUERY_PARAM_SEPARATOR);
+        String key = queryParam.substring(QUERY_PARAM_BEGUN_INDEX, separatorIndex);
+        String value = queryParam.substring(EXCLUDING_HERE + separatorIndex);
+        return new QueryParam(key, value);
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public boolean same(String key) {
+        return this.key.equals(key);
+    }
+}

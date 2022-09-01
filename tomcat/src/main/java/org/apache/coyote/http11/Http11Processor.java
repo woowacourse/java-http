659770853c3d0a196b6d854c00363c2820f7acf0
hyeonic <dev.hyeonic@gmail.com>
2022-09-01 16:03:17 +0900

    feat: 존재하는 html file인 경우 응답 하도록 수정

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,5 +1,15 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.LinkedHashMap;
+import java.util.Objects;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
@@ -25,22 +35,56 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+        try (InputStream inputStream = connection.getInputStream();
+             InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+             OutputStream outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            HttpRequestMessage httpRequestMessage = generateHttpRequestMessage(bufferedReader);
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            String requestTarget = httpRequestMessage.getRequestTarget();
+            String fileName = requestTarget.substring(1);
+
+            HttpResponseMessage httpResponseMessage = generateResponseMessage(fileName);
+            String response = httpResponseMessage.parseResponse();
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private HttpRequestMessage generateHttpRequestMessage(final BufferedReader bufferedReader) throws IOException {
+        HttpRequestMessage requestMessage = new HttpRequestMessage(Objects.requireNonNull(bufferedReader.readLine()));
+
+        String line;
+        while (!(line = Objects.requireNonNull(bufferedReader.readLine())).equals("")) {
+            String[] splitHeader = line.split(": ");
+            requestMessage.addHeader(splitHeader[0], splitHeader[1]);
+        }
+
+        return requestMessage;
+    }
+
+    private HttpResponseMessage generateResponseMessage(final String fileName) throws IOException {
+        String statusLine = "HTTP/1.1 200 OK";
+
+        LinkedHashMap<String, String> headers = new LinkedHashMap<>();
+        headers.put("Content-Type", "text/html;charset=utf-8");
+
+        String responseBody = "Hello world!";
+
+        if (fileName.isBlank()) {
+            headers.put("Content-Length", String.valueOf(responseBody.getBytes().length));
+            return new HttpResponseMessage(statusLine, headers, responseBody);
+        }
+
+        URL resourceUrl = getClass().getClassLoader().getResource("static/" + fileName);
+        responseBody = new String(Files.readAllBytes(new File(resourceUrl.getFile()).toPath()));
+
+        headers.put("Content-Length", String.valueOf(responseBody.getBytes().length));
+
+        return new HttpResponseMessage(statusLine, headers, responseBody);
     }
 }

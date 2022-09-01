03a7332765d629d19a9b9982e267fa5d44f1b630
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-01 11:52:02 +0900

    feat(Http11Processor): respond with static file matching the requested URI on GET

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,7 +1,17 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
+import org.apache.coyote.support.HttpRequestMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,10 +36,17 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
+             final var streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+             final var reader = new BufferedReader(streamReader);
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final var request = toRequestMessage(reader);
 
+            String responseBody = "Hello world!";
+            if (request.isGetMethod() && !request.getUri().equals("/")) {
+                final URL resource = getClass().getClassLoader().getResource("static" + request.getUri());
+                responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+            }
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",
@@ -42,5 +59,14 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private HttpRequestMessage toRequestMessage(BufferedReader reader) throws IOException {
+        List<String> request = new ArrayList<>();
+        String line;
+        while ((line = reader.readLine()).length() > 0) {
+            request.add(line);
+        }
+        return HttpRequestMessage.of(request);
     }
 }

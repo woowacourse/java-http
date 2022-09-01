164fd2c196db27a891b040b4f38b6ea18ee906fc
hyeonic <dev.hyeonic@gmail.com>
2022-09-01 17:46:14 +0900

    feat: Content-Type 변경을 위한 ContentType 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/ContentType.java
@@ -0,0 +1,35 @@
+package org.apache.coyote.http11;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public enum ContentType {
+
+    HTML("html", "text/html;charset=utf-8"),
+    CSS("css", "text/css"),
+    JAVASCRIPT("js", "text/javascript");
+
+    private final String extension;
+    private final String value;
+
+
+    ContentType(final String extension, final String value) {
+        this.extension = extension;
+        this.value = value;
+    }
+
+    public static ContentType parse(final String extension) {
+        return Arrays.stream(values())
+                .filter(contentType -> contentType.extension.equals(extension))
+                .findFirst()
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}

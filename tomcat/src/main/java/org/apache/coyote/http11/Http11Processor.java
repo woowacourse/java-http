e3fe200f26108dede24fce4c2890e780f6c610a1
DOES <ldk980130@gmail.com>
2022-09-01 13:35:55 +0900

    refactor: private 메서드 적절한 클래스로 분리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,22 +1,11 @@
 package org.apache.coyote.http11;
 
-import static java.util.stream.Collectors.*;
-
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.Socket;
-import java.net.URL;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import org.apache.coyote.Processor;
+import org.apache.coyote.http11.util.StaticResourceUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,8 +15,8 @@ import nextstep.jwp.model.User;
 
 public class Http11Processor implements Runnable, Processor {
 
-	private static final String UNAUTHORIZED_HTML = "static/401.html";
-	private static final String NOT_FOUND_HTML = "static/404.html";
+	private static final String UNAUTHORIZED_HTML = "401.html";
+	private static final String NOT_FOUND_HTML = "404.html";
 
 	private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
 
@@ -47,7 +36,7 @@ public class Http11Processor implements Runnable, Processor {
 		try (final var inputStream = connection.getInputStream();
 			 final var outputStream = connection.getOutputStream()) {
 
-			final var request = parseHttpRequest(inputStream);
+			final var request = new HttpRequest(inputStream);
 			final var response = makeHttpResponse(request);
 
 			outputStream.write(response.getBytes());
@@ -57,77 +46,39 @@ public class Http11Processor implements Runnable, Processor {
 		}
 	}
 
-	private String parseHttpRequest(InputStream inputStream) throws IOException {
-		BufferedReader requestReader = new BufferedReader(new InputStreamReader(inputStream));
-		List<String> requestLines = new ArrayList<>();
-		while (requestReader.ready()) {
-			requestLines.add(requestReader.readLine());
-		}
-		log.info("\r\n" + String.join("\r\n", requestLines));
-		return requestLines.get(0);
-	}
-
-	private HttpResponse makeHttpResponse(String request) throws IOException {
-		String requestUri = extractRequestUri(request);
-		if (requestUri.equals("/")) {
+	private HttpResponse makeHttpResponse(HttpRequest httpRequest) throws IOException {
+		String requestUrl = httpRequest.getUrl();
+		if (requestUrl.equals("/")) {
 			return new HttpResponse(StatusCode.OK, "Hello world!", ContentType.HTML);
 		}
 
-		String extension = extractExtension(requestUri);
-		if (!extension.equals("")) {
-			return handleStaticRequest(requestUri, extension);
+		if (httpRequest.isStaticResourceRequest()) {
+			return handleStaticRequest(httpRequest);
 		}
-
-		return handleApiRequest(requestUri);
+		return handleApiRequest(httpRequest);
 	}
 
-	private String extractRequestUri(String request) {
-		return request
-			.split("\r\n")[0]
-			.split(" ")[1];
-	}
-
-	private String extractExtension(String requestUri) {
-		if (requestUri.contains(".")) {
-			return requestUri.split("\\.")[1];
-		}
-		return "";
-	}
-
-	private HttpResponse handleStaticRequest(String requestUri, String extension) throws IOException {
-		ContentType contentType = ContentType.from(extension);
+	private HttpResponse handleStaticRequest(HttpRequest httpRequest) throws IOException {
 		try {
-			return makeHttpResponseWithHtml("static/" + requestUri, 200, contentType);
-		} catch (NullPointerException e) {
-			return makeHttpResponseWithHtml(NOT_FOUND_HTML, 404, ContentType.HTML);
-		}
-	}
-
-	private HttpResponse makeHttpResponseWithHtml(
-		String htmlPath, int statusCode, ContentType contentType
-	) throws IOException {
-		final URL resource = getClass().getClassLoader().getResource(htmlPath);
-		String responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
-		return new HttpResponse(StatusCode.from(statusCode), responseBody, contentType);
-	}
-
-	private HttpResponse handleApiRequest(String requestUri) throws IOException {
-		if (requestUri.startsWith("/login")) {
-			Map<String, String> queryStrings = parseQueryString(requestUri);
-			return checkValidLogin(queryStrings.get("account"), queryStrings.get("password"));
-		}
-		return makeHttpResponseWithHtml(NOT_FOUND_HTML, 404, ContentType.HTML);
-	}
-
-	private Map<String, String> parseQueryString(String requestUri) {
-		return Arrays.stream(
-				requestUri.split("\\?")[1]
-					.split("&")
-			)
-			.collect(toMap(
-				value -> value.split("=")[0],
-				value -> value.split("=")[1])
+			return new HttpResponse(
+				StatusCode.OK,
+				StaticResourceUtil.getContent(httpRequest.getUrl()),
+				httpRequest.getContentType()
 			);
+		} catch (NullPointerException e) {
+			return responseErrorPage(NOT_FOUND_HTML, 404);
+		}
+	}
+
+	private HttpResponse handleApiRequest(HttpRequest httpRequest) throws IOException {
+		String requestUrl = httpRequest.getUrl();
+		if (requestUrl.startsWith("/login")) {
+			return checkValidLogin(
+				httpRequest.getQueryString("account"),
+				httpRequest.getQueryString("password")
+			);
+		}
+		return responseErrorPage(NOT_FOUND_HTML, 404);
 	}
 
 	private HttpResponse checkValidLogin(String account, String password) throws IOException {
@@ -135,6 +86,11 @@ public class Http11Processor implements Runnable, Processor {
 		if (findUser.isPresent() && findUser.get().checkPassword(password)) {
 			return new HttpResponse(StatusCode.OK, findUser.get().toString(), ContentType.HTML);
 		}
-		return makeHttpResponseWithHtml(UNAUTHORIZED_HTML, 401, ContentType.HTML);
+		return responseErrorPage(UNAUTHORIZED_HTML, 401);
+	}
+
+	private HttpResponse responseErrorPage(String htmlPath, int statusCode) throws IOException {
+		String responseBody = StaticResourceUtil.getContent(htmlPath);
+		return new HttpResponse(StatusCode.from(statusCode), responseBody, ContentType.HTML);
 	}
 }

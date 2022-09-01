8f50671e00498c634df73c43f2f2ae7894612be9
Seongwon97 <osw970325@naver.com>
2022-09-01 17:30:00 +0900

    feat: Index.html 응답하기 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,19 @@
 package org.apache.coyote.http11;
 
-import nextstep.jwp.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
+import nextstep.jwp.exception.UncheckedServletException;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -26,21 +33,57 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
 
-            final var responseBody = "Hello world!";
+            String request = bufferedReader.readLine();
+            validateEmptyHeader(request);
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            final var response = createResponse(request);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (IOException | UncheckedServletException | NoSuchElementException | IllegalArgumentException e) {
             log.error(e.getMessage(), e);
+        }
+    }
+
+    private void validateEmptyHeader(String request) {
+        if (request == null) {
+            throw new IllegalArgumentException("Http request header가 비어있습니다.");
+        }
+    }
+
+    private String createResponse(String request) throws IOException {
+        String[] requestUrlInfo = request.split(" ");
+        if (requestUrlInfo[1].equals("/")) {
+            return createOkResponse("Hello world!");
+        }
+        return createOkResponseWithContent(requestUrlInfo);
+    }
+
+    private String createOkResponse(String responseBody) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+    }
+
+    private String createOkResponseWithContent(String[] requestUrlInfo) throws IOException {
+        String fileName = requestUrlInfo[1];
+        Path filePath = findFilePath(fileName);
+        String content = new String(Files.readAllBytes(filePath));
+        return createOkResponse(content);
+    }
+
+    private Path findFilePath(String fileName) {
+        try {
+            return Path.of(Objects.requireNonNull(
+                    this.getClass().getClassLoader().getResource("static/" + fileName)).getPath());
+        } catch (NullPointerException e) {
+            throw new NoSuchElementException(fileName + " 파일이 존재하지 않습니다.");
         }
     }
 }

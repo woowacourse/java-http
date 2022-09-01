6b862f4b65b07495a9c8cfdc320d4b0aacd4e960
summerlunaa <summerlunaa9@gmail.com>
2022-09-01 15:51:58 +0900

    feat: request uri 읽어와서 응답하는 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,19 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -26,9 +33,26 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
+             final var inputStreamReader = new InputStreamReader(inputStream);
+             final var bufferedReader = new BufferedReader(inputStreamReader);
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            List<String> request = new ArrayList<>();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                request.add(line);
+            }
+
+            var requestURI = request.get(0).split(" ")[1];
+            var responseBody = "Hello world!";
+            if (!requestURI.equals("/")) {
+                final Path path =
+                        new File(Objects.requireNonNull(getClass().getClassLoader().getResource("static" + requestURI))
+                                .getFile())
+                                .toPath();
+
+                responseBody = new String(Files.readAllBytes(path));
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",

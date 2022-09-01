d19054d4b401e6acf045d6bbb816acce44868c5f
awesomeo184 <awesomeo184@gmail.com>
2022-09-01 17:26:43 +0900

    feat: 파일명을 입력받아 파일을 찾아오는 기능 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/ResourceLocator.java
@@ -0,0 +1,45 @@
+package org.apache.coyote.http11;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ResourceLocator {
+
+    public File findResource(String fileName) {
+        if (Objects.isNull(fileName) || fileName.isBlank()) {
+            throw new IllegalArgumentException("파일명은 null 혹은 빈칸일 수 없습니다.");
+        }
+
+        URL rootUrl = getClass().getClassLoader().getResource("static");
+        if (Objects.isNull(rootUrl)) {
+            throw new IllegalArgumentException("static 디렉토리가 존재하지 않습니다.");
+        }
+
+        return new File(getAllFiles(new ArrayList<>(), Path.of(rootUrl.getPath())).stream()
+                .filter(file -> file.contains(fileName))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("해당 파일이 존재하지 않습니다. fileName = " + fileName)));
+    }
+
+    private List<String> getAllFiles(List<String> fileNames, Path dir) {
+        try(DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+            for (Path path : stream) {
+                if(path.toFile().isDirectory()) {
+                    getAllFiles(fileNames, path);
+                } else {
+                    fileNames.add(path.toAbsolutePath().toString());
+                }
+            }
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+        return fileNames;
+    }
+}
---
+++ tomcat/src/test/java/org/apache/coyote/http11/ResourceLocatorTest.java
@@ -0,0 +1,38 @@
+package org.apache.coyote.http11;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ResourceLocatorTest {
+
+    @Test
+    @DisplayName("입력된 파일명과 일치하는 파일을 찾아온다")
+    void getResource() {
+        ResourceLocator resourceLocator = new ResourceLocator();
+        String requestURI = "styles.css";
+
+        File resource = resourceLocator.findResource(requestURI);
+        assertThat(resource).isNotNull();
+    }
+
+    @Test
+    @DisplayName("null을 입력받는 경우 예외를 발생시킨다")
+    void throwExceptionWhenInputIsNull() {
+        ResourceLocator resourceLocator = new ResourceLocator();
+        assertThatThrownBy(() -> resourceLocator.findResource(null))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("공백을 입력받는 경우 예외를 발생시킨다")
+    void throwExceptionWhenInputIsBlank() {
+        ResourceLocator resourceLocator = new ResourceLocator();
+        assertThatThrownBy(() -> resourceLocator.findResource(" "))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

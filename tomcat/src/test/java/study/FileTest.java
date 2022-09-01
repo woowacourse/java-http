acdf1710c8817a75418ba5fa7cf92c40b2b8848c
beomWhale <cndqjacndqja@gmail.com>
2022-09-01 18:44:24 +0900

    test: FileTest 학습 테스트 추가

--- tomcat/src/test/java/study/FileTest.java
+++ tomcat/src/test/java/study/FileTest.java
@@ -1,13 +1,13 @@
 package study;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.nio.file.Path;
-import java.util.Collections;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * 웹서버는 사용자가 요청한 html 파일을 제공 할 수 있어야 한다.
@@ -26,7 +26,7 @@ class FileTest {
         final String fileName = "nextstep.txt";
 
         // todo
-        final String actual = "";
+        final String actual = getClass().getClassLoader().getResource(fileName).getFile();
 
         assertThat(actual).endsWith(fileName);
     }
@@ -36,15 +36,14 @@ class FileTest {
      * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws IOException {
         final String fileName = "nextstep.txt";
 
         // todo
-        final Path path = null;
+        final Path path = Path.of(getClass().getClassLoader().getResource(fileName).getPath());
 
         // todo
-        final List<String> actual = Collections.emptyList();
-
+        final List<String> actual = Files.readAllLines(path);
         assertThat(actual).containsOnly("nextstep");
     }
 }

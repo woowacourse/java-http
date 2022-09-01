10c9441fab10979d9363086db0167dccae3d3686
meatsby <meatsby@gmail.com>
2022-09-01 22:05:32 +0900

    feat: 1단계 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,24 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+import javassist.NotFoundException;
+import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.exception.UncheckedServletException;
+import nextstep.jwp.model.User;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -25,22 +37,70 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+        try (InputStream inputStream = connection.getInputStream();
+             BufferedReader bufferedReader = new BufferedReader(
+                     new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            OutputStream outputStream = connection.getOutputStream();
 
-            final var responseBody = "Hello world!";
+            String requestMessage = parseRequest(bufferedReader);
+            HttpRequest httpRequest = HttpRequest.from(requestMessage);
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            inquireUser(httpRequest);
 
-            outputStream.write(response.getBytes());
+            String responseMessage = createResponseMessage(httpRequest);
+            outputStream.write(responseMessage.getBytes());
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (IOException | UncheckedServletException | NotFoundException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String parseRequest(final BufferedReader bufferedReader) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        while (bufferedReader.ready()) {
+            stringBuilder.append(bufferedReader.readLine())
+                    .append("\r\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    private void inquireUser(final HttpRequest httpRequest) throws NotFoundException {
+        if (httpRequest.getUri().equals("/login.html")) {
+            String account = httpRequest.getQueryParameters().get("account");
+            String password = httpRequest.getQueryParameters().get("password");
+            User user = InMemoryUserRepository.findByAccount(account)
+                    .orElseThrow(() -> new NotFoundException("User not found."));
+            if (user.checkPassword(password)) {
+                log.info("User : {}", user);
+            }
+        }
+    }
+
+    private String createResponseMessage(final HttpRequest httpRequest) throws IOException {
+        String requestTarget = httpRequest.getUri();
+        if (requestTarget.equals("/")) {
+            return response("text/html", "Hello world!");
+        }
+        String responseBody = createResponseBody(requestTarget);
+        return response(httpRequest.getContentType(), responseBody);
+    }
+
+    private String createResponseBody(final String requestTarget) throws IOException {
+        URL resource = getClass()
+                .getClassLoader()
+                .getResource("static" + requestTarget);
+        String filePath = Objects.requireNonNull(resource)
+                .getFile();
+        Path path = new File(filePath).toPath();
+        return new String(Files.readAllBytes(path));
+    }
+
+    private String response(final String contentType, final String responseBody) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: " + contentType + ";charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
     }
 }
---
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpRequest.java
@@ -0,0 +1,81 @@
+package org.apache.coyote.http11;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+public class HttpRequest {
+
+    private final String httpMethod;
+    private final String uri;
+    private final Map<String, String> queryParameters;
+    private final String version;
+    private final Map<String, String> headers;
+
+    private HttpRequest(final String httpMethod, final String uri, final Map<String, String> queryParameters,
+                        final String version,
+                        final Map<String, String> headers) {
+        this.httpMethod = httpMethod;
+        this.uri = uri;
+        this.queryParameters = queryParameters;
+        this.version = version;
+        this.headers = headers;
+    }
+
+    public static HttpRequest from(final String requestMessage) {
+        List<String> requestMessageLines = Arrays.asList(requestMessage.split("\r\n"));
+
+        String requestLine = requestMessageLines.get(0);
+        List<String> requestLines = Arrays.asList(requestLine.split(" "));
+        String method = requestLines.get(0);
+        String requestTarget = requestLines.get(1);
+        String uri = requestTarget.split("\\?")[0];
+        if (!(uri.equals("/") || uri.contains("."))) {
+            uri += ".html";
+        }
+        Map<String, String> queryString = parseQueryString(requestTarget);
+        String httpVersion = requestLines.get(2);
+        Map<String, String> headers = toMap(requestMessageLines.subList(1, requestMessageLines.size()), ": ");
+        return new HttpRequest(method, uri, queryString, httpVersion, headers);
+    }
+
+    private static Map<String, String> parseQueryString(final String requestTarget) {
+        if (requestTarget.contains("?")) {
+            String queryString = requestTarget.split("\\?")[1];
+            return toMap(Arrays.asList(queryString.split("&")), "=");
+        }
+        return null;
+    }
+
+    private static Map<String, String> toMap(final List<String> toBeParsed, final String delimiter) {
+        return toBeParsed.stream()
+                .map(line -> line.split(delimiter))
+                .collect(Collectors.toMap(line -> line[0], line -> line[1]));
+    }
+
+    public String getContentType() {
+        return "text/" + uri.split("\\.")[1];
+    }
+
+    public String getHttpMethod() {
+        return httpMethod;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public Map<String, String> getQueryParameters() {
+        return queryParameters;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+}

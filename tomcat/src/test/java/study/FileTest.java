2001a41498d2d05e8edd03501a63d447cdd0229f
Wishoon <heonga2@gmail.com>
2022-09-01 13:06:38 +0900

    feat: FileTest 작성

--- tomcat/src/test/java/study/FileTest.java
+++ tomcat/src/test/java/study/FileTest.java
@@ -1,10 +1,13 @@
 package study;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
-import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,9 +28,9 @@ class FileTest {
     void resource_디렉터리에_있는_파일의_경로를_찾는다() {
         final String fileName = "nextstep.txt";
 
-        // todo
-        final String actual = "";
+        final URL uri = ClassLoader.getSystemResource(fileName);
 
+        final String actual = uri.getFile();
         assertThat(actual).endsWith(fileName);
     }
 
@@ -36,15 +39,13 @@ class FileTest {
      * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws IOException {
         final String fileName = "nextstep.txt";
 
-        // todo
-        final Path path = null;
+        final URL uri = ClassLoader.getSystemResource(fileName);
+        final Path path = new File(uri.getFile()).toPath();
 
-        // todo
-        final List<String> actual = Collections.emptyList();
-
+        final List<String> actual = Files.readAllLines(path);
         assertThat(actual).containsOnly("nextstep");
     }
 }

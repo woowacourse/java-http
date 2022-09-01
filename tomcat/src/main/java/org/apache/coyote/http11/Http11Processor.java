f4720e1d823fce29e702b72d743c2b1223218004
Wishoon <heonga2@gmail.com>
2022-09-01 13:16:27 +0900

    feat: GET /index.html 응답 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,5 +1,12 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.Objects;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
@@ -28,19 +35,40 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            final String extractUri = extractUri(bufferedReader.readLine());
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            String responseBody;
+            final String contentType = "text/html";
 
-            outputStream.write(response.getBytes());
-            outputStream.flush();
+            if (extractUri.equals("/")) {
+               responseBody = "Hello world!";
+            } else {
+                final URL resource = getClass().getClassLoader().getResource("static" + extractUri);
+                String file = Objects.requireNonNull(resource).getFile();
+                responseBody = new String(Files.readAllBytes(new File(file).toPath()));
+            }
+
+            final String response = String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: " + contentType + ";charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+
+            writeResponse(outputStream, response);
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private void writeResponse(final OutputStream outputStream, final String response) throws IOException {
+        outputStream.write(response.getBytes());
+        outputStream.flush();
+    }
+
+    private String extractUri(final String line) {
+        final String[] uri = line.split(" ");
+        return uri[1];
     }
 }

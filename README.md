3f2f1e0a95014f0602caf799e7b41f339d07496c
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-01 16:20:56 +0900

    feat(HttpRequestHandler): parse query string and find user from in-memory DB on GET /login.html

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # 톰캣 구현하기
 
 - [x] GET 요청시 URI에 대응되는 정적 자원 응답하기
-  - [x] `GET /` 요청에 대해서도 index.html 응답하기
-  - [x] 존재하지 않는 자원을 요청한 경우, 404 예외 응답하기
-  - [x] 요청한 자원의 확장자에 따라 Content-Type 설정하여 응답하기 (text/html, text/css, text/javascript, etc)
+  - [x] `GET /` 요청에 대해 index.html 응답
+  - [x] 존재하지 않는 자원을 요청한 경우, 404.html 응답
+  - [x] 요청한 자원의 확장자에 따라 Content-Type 설정하여 응답 (text/html, text/css, text/javascript, etc)
+
+- [x] uri에 query string이 포함되면 파싱하여 처리한다.
+  - [x] `GET /login.html` 요청시 query string에 대응되는 사용자 데이터가 인메모리 DB에 존재하면 로그에 출력한다.
--- tomcat/src/main/java/org/apache/coyote/support/HttpRequestHandler.java
+++ tomcat/src/main/java/org/apache/coyote/support/HttpRequestHandler.java
@@ -1,32 +1,64 @@
 package org.apache.coyote.support;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import nextstep.jwp.db.InMemoryUserRepository;
+import nextstep.jwp.model.User;
 import org.apache.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HttpRequestHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(HttpRequestHandler.class);
+
     private final Method method;
     private final String uri;
+    private final Map<String, String> parameters;
 
-    private HttpRequestHandler(Method method, String uri) {
+    private HttpRequestHandler(Method method, String uri, Map<String, String> parameters) {
         this.method = method;
         this.uri = uri;
+        this.parameters = parameters;
+    }
+
+    private HttpRequestHandler(Method method, String uri) {
+        this(method, uri, new HashMap<>());
     }
 
     public static HttpRequestHandler of(List<String> request) {
         final var startLine = request.get(0).split(" ");
         final var method = Method.valueOf(startLine[0]);
-        var uri = startLine[1];
-        if (Method.GET.equals(method) && uri.contains("?")) {
-            final var uriAndParams = uri.split("\\?");
-            uri = uriAndParams[0];
+        final var uri = startLine[1];
+        if (uri.contains("?")) {
+            final var delimiterIndex = uri.indexOf("?");
+            final var path = uri.substring(0, delimiterIndex);
+            final var queryString = uri.substring(delimiterIndex + 1);
+            return new HttpRequestHandler(method, path, parseQueryString(queryString));
         }
         return new HttpRequestHandler(method, uri);
     }
 
+    private static Map<String, String> parseQueryString(String queryString) {
+        Map<String, String> parameters = new HashMap<>();
+        for (String param : queryString.split("&")) {
+            final var delimiterIndex = param.indexOf("=");
+            if (delimiterIndex == -1) {
+                continue;
+            }
+            final var key = param.substring(0, delimiterIndex);
+            final var value = param.substring(delimiterIndex + 1);
+            parameters.put(key, value);
+        }
+        return parameters;
+    }
+
     public String handle() throws IOException {
         if (method.equals(Method.GET)) {
+            checkLoginAccount();
             return get();
         }
         throw new UnsupportedOperationException("Not implemented");
@@ -37,6 +69,16 @@ public class HttpRequestHandler {
             return new ResourceResponse(uri).toHttpResponseMessage();
         } catch (NotFoundException e) {
             return ResourceResponse.ofNotFound().toHttpResponseMessage();
+        }
+    }
+
+    private void checkLoginAccount() {
+        if (uri.equals("/login.html")) {
+            Optional<User> account = InMemoryUserRepository.findByAccount(parameters.get("account"));
+            if (account.isEmpty()) {
+                return;
+            }
+            log.info(account.get().toString());
         }
     }
 }

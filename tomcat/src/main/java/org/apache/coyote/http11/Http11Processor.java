399f5439f71fa4c0da0661a422de91ce31d5b91d
yeon-06 <rnjstldus2@gmail.com>
2022-09-01 13:44:21 +0900

    feat: GET /index.html 응답하기

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,16 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Objects;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -25,10 +29,16 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+        try (final var inputStream = new InputStreamReader(connection.getInputStream());
+             final var outputStream = connection.getOutputStream();
+             final var bufferedReader = new BufferedReader(inputStream)) {
 
-            final var responseBody = "Hello world!";
+            String[] inputs = bufferedReader.readLine()
+                    .split(" ");
+            String method = inputs[0];
+            String path = inputs[1];
+
+            final var responseBody = generateResponseBody(method, path);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -42,5 +52,22 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String generateResponseBody(final String method, final String path) throws IOException {
+        if ("GET".equals(method) && "/".equals(path)) {
+            return "Hello world!";
+        }
+
+        if ("GET".equals(method) && "/index.html".equals(path)) {
+            final File file = new File(Objects.requireNonNull(getClass()
+                            .getClassLoader()
+                            .getResource("static" + path))
+                    .getFile());
+
+            return new String(Files.readAllBytes(file.toPath()));
+        }
+
+        return "";
     }
 }

ac2c7ebab6919b6e6a6a663abf254526bd832edc
hyewoncc <hyewoncc@gmail.com>
2022-09-01 11:05:56 +0900

    feat: 기본 문자열 및 index.html 응답 테스트 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,14 @@
 package org.apache.coyote.http11;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,7 +30,15 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            String[] lines = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8).split("\r\n");
+            String[] get = lines[0].split(" ");
+
+            var responseBody = "Hello world!";
+
+            if (!get[1].equals("/")) {
+                responseBody = Files.readString(
+                                Path.of(this.getClass().getResource("/static" + get[1]).getPath()));
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",

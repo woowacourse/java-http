5c49eacb732506ce8ef5ed6212c6a43b088b1fdd
awesomeo184 <awesomeo184@gmail.com>
2022-09-01 11:19:22 +0900

    test: 학습테스트 작성

--- tomcat/src/test/java/study/FileTest.java
+++ tomcat/src/test/java/study/FileTest.java
@@ -1,5 +1,10 @@
 package study;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -26,7 +31,8 @@ class FileTest {
         final String fileName = "nextstep.txt";
 
         // todo
-        final String actual = "";
+        URL url = getClass().getClassLoader().getResource("nextstep.txt");
+        final String actual = new File(url.getFile()).getName();
 
         assertThat(actual).endsWith(fileName);
     }
@@ -36,14 +42,14 @@ class FileTest {
      * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws URISyntaxException, IOException {
         final String fileName = "nextstep.txt";
 
         // todo
-        final Path path = null;
+        final Path path = Path.of(getClass().getClassLoader().getResource("nextstep.txt").toURI());
 
         // todo
-        final List<String> actual = Collections.emptyList();
+        final List<String> actual = Files.readAllLines(path);
 
         assertThat(actual).containsOnly("nextstep");
     }

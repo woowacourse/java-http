c44ffb86f6d1f743c3a9f5c36659edb2b75b52d7
dwl21 <nggus5@naver.com>
2022-09-01 23:45:28 +0900

    feat: Http Request Line 정보를 가지는 객체 구현

---
+++ tomcat/src/main/java/nextstep/jwp/http/HttpRequest.java
@@ -0,0 +1,73 @@
+package nextstep.jwp.http;
+
+import static java.util.List.of;
+
+import java.util.List;
+import nextstep.jwp.utils.FileUtils;
+
+public class HttpRequest {
+
+    private static final String REQUEST_LINE_SEPARATOR = " ";
+    private static final String URI_PATH_SEPARATOR = "\\?";
+    private static final int ONLY_PATH_SIZE = 1;
+
+    private final String httpMethod;
+    private final String uri;
+    private final String path;
+    private final QueryParams queryParams;
+
+    public HttpRequest(String httpMethod, String uri, String path, QueryParams queryParams) {
+        this.httpMethod = httpMethod;
+        this.uri = uri;
+        this.path = path;
+        this.queryParams = queryParams;
+    }
+
+    public static HttpRequest from(String requestLine) {
+        final int HTTP_METHOD_INDEX = 0;
+        final int URI_INDEX = 1;
+
+        List<String> request = of(requestLine.split(REQUEST_LINE_SEPARATOR));
+        String httpMethod = request.get(HTTP_METHOD_INDEX);
+        String uri = request.get(URI_INDEX);
+        List<String> separatedUri = of(uri.split(URI_PATH_SEPARATOR));
+        return handleHavingQueryParams(httpMethod, uri, separatedUri);
+    }
+
+    private static HttpRequest handleHavingQueryParams(String httpMethod, String uri,
+                                                       List<String> separatedUri) {
+        final int PATH_INDEX = 0;
+        final int QUERY_PARAM_INDEX = 1;
+
+        if (hasQueryParam(separatedUri)) {
+            return new HttpRequest(httpMethod, uri, separatedUri.get(PATH_INDEX),
+                QueryParams.from(separatedUri.get(QUERY_PARAM_INDEX)));
+        }
+        return new HttpRequest(httpMethod, uri, uri, QueryParams.empty());
+    }
+
+    private static boolean hasQueryParam(List<String> separatedUri) {
+        return separatedUri.size() != ONLY_PATH_SIZE;
+    }
+
+    public String getFileExtension() {
+        return FileUtils.extractFileExtension(path);
+    }
+
+
+    public String getHttpMethod() {
+        return httpMethod;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public QueryParams getQueryParams() {
+        return queryParams;
+    }
+}

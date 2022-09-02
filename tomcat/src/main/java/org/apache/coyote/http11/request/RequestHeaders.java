deef3d2592dc155131cd5600bce8fe54d8be5e40
RIANAEH <ririhan217@gmail.com>
2022-09-02 16:45:20 +0900

    refactor: RequestHeaders 필드 이름 변경

--- tomcat/src/main/java/org/apache/coyote/http11/request/RequestHeaders.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/RequestHeaders.java
@@ -6,27 +6,27 @@ import org.apache.coyote.http11.common.HttpParser;
 
 public class RequestHeaders {
 
-    private final Map<String, String> value;
+    private final Map<String, String> values;
 
-    public RequestHeaders(final Map<String, String> value) {
-        this.value = value;
+    public RequestHeaders(final Map<String, String> values) {
+        this.values = values;
     }
 
     public static RequestHeaders from(final String lines) {
         return new RequestHeaders(HttpParser.parseHeaders(lines));
     }
 
-    public Map<String, String> getValue() {
-        return value;
+    public Map<String, String> getValues() {
+        return values;
     }
 
     public String get(final String key) {
-        return value.get(key);
+        return values.get(key);
     }
 
     @Override
     public String toString() {
-        return value.entrySet()
+        return values.entrySet()
                 .stream()
                 .map(entry -> entry.getKey() + ": " + entry.getValue() + " ")
                 .collect(Collectors.joining("\r\n"));

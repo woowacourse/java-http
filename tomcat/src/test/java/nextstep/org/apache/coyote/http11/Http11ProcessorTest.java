78ef33f2590b39f0a3cee0eee7cd24be1b9200fb
yangdongjue5510 <djwhy5510@gmail.com>
2022-09-02 13:51:18 +0900

    test: 로그인 성공할 경우 로깅 되는 지 테스트

--- tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -2,22 +2,42 @@ package nextstep.org.apache.coyote.http11;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import nextstep.jwp.db.InMemoryUserRepository;
+import nextstep.jwp.model.User;
 import org.apache.coyote.http11.Http11Processor;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+import support.MemoryAppender;
 import support.StubSocket;
 
 class Http11ProcessorTest {
 
     private StubSocket stubSocket;
+    private MemoryAppender memoryAppender;
+
+    @BeforeEach
+    void setUp() {
+        Logger logger = (Logger) LoggerFactory.getLogger(Http11Processor.class);
+        memoryAppender = new MemoryAppender();
+        memoryAppender.setContext((LoggerContext) org.slf4j.LoggerFactory.getILoggerFactory());
+        logger.addAppender(memoryAppender);
+        logger.setLevel(Level.DEBUG);
+        memoryAppender.start();
+    }
 
     @AfterEach
     void afterEach() throws IOException {
         stubSocket.close();
+        memoryAppender.reset();
     }
 
     @Test
@@ -66,5 +86,48 @@ class Http11ProcessorTest {
                 new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
 
         assertThat(stubSocket.output()).isEqualTo(expected);
+    }
+
+    @Test
+    void logUser() {
+        // given
+        final String httpRequest = String.join("\r\n",
+                "GET /login?account=gugu&password=password HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+        stubSocket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(stubSocket);
+        final User expected = InMemoryUserRepository.findByAccount("gugu").orElseThrow();
+
+        // when
+        processor.process(stubSocket);
+
+        // then
+        assertThat(memoryAppender.contains(expected.toString())).isTrue();
+    }
+
+    @Test
+    void logUserFail() {
+
+        // given
+        final String httpRequest = String.join("\r\n",
+                "GET /login?account=gugu&password=uncorrect HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+        stubSocket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(stubSocket);
+        final User expected = InMemoryUserRepository.findByAccount("gugu").orElseThrow();
+
+        // when
+        processor.process(stubSocket);
+
+        // then
+        assertThat(memoryAppender.contains(expected.toString())).isFalse();
     }
 }

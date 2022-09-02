c223d6a929336c151fca36f5050bfdffd5261e3e
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-02 15:19:34 +0900

    refactor(HttpRequest): replace parameters field with first class collection Parameters
    - constants for all magic number and strings

--- tomcat/src/main/java/org/apache/coyote/request/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/request/HttpRequest.java
@@ -2,7 +2,6 @@ package org.apache.coyote.request;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.model.User;
@@ -10,45 +9,37 @@ import org.apache.coyote.support.HttpMethod;
 
 public class HttpRequest {
 
+    private static final int START_LINE_INDEX = 0;
+    private static final String START_LINE_DELIMITER = " ";
+    private static final int START_LINE_METHOD_INDEX = 0;
+    private static final int START_LINE_URI_INDEX = 1;
+    private static final String QUERY_STRING_BEGIN_SIGN = "?";
+
     private final HttpMethod method;
     private final String uri;
-    private final Map<String, String> parameters;
+    private final Parameters parameters;
 
-    private HttpRequest(HttpMethod method, String uri, Map<String, String> parameters) {
+    private HttpRequest(HttpMethod method, String uri, Parameters parameters) {
         this.method = method;
         this.uri = uri;
         this.parameters = parameters;
     }
 
     private HttpRequest(HttpMethod method, String uri) {
-        this(method, uri, new HashMap<>());
+        this(method, uri, new Parameters(new HashMap<>()));
     }
 
     public static HttpRequest of(List<String> request) {
-        final var startLine = request.get(0).split(" ");
-        final var method = HttpMethod.valueOf(startLine[0]);
-        final var uri = startLine[1];
-        if (uri.contains("?")) {
-            final var delimiterIndex = uri.indexOf("?");
+        final var startLine = request.get(START_LINE_INDEX).split(START_LINE_DELIMITER);
+        final var method = HttpMethod.valueOf(startLine[START_LINE_METHOD_INDEX]);
+        final var uri = startLine[START_LINE_URI_INDEX];
+        if (uri.contains(QUERY_STRING_BEGIN_SIGN)) {
+            final var delimiterIndex = uri.indexOf(QUERY_STRING_BEGIN_SIGN);
             final var path = uri.substring(0, delimiterIndex);
             final var queryString = uri.substring(delimiterIndex + 1);
-            return new HttpRequest(method, path, parseQueryString(queryString));
+            return new HttpRequest(method, path, Parameters.ofQueryString(queryString));
         }
         return new HttpRequest(method, uri);
-    }
-
-    private static Map<String, String> parseQueryString(String queryString) {
-        Map<String, String> parameters = new HashMap<>();
-        for (String param : queryString.split("&")) {
-            final var delimiterIndex = param.indexOf("=");
-            if (delimiterIndex == -1) {
-                continue;
-            }
-            final var key = param.substring(0, delimiterIndex);
-            final var value = param.substring(delimiterIndex + 1);
-            parameters.put(key, value);
-        }
-        return parameters;
     }
 
     public boolean isGet() {
@@ -56,7 +47,7 @@ public class HttpRequest {
     }
 
     public Optional<User> checkLoginAccount() {
-        if (uri.startsWith("/login") && parameters.containsKey("account")) {
+        if (uri.startsWith("/login") && parameters.contains("account")) {
             return InMemoryUserRepository.findByAccount(parameters.get("account"));
         }
         return Optional.empty();

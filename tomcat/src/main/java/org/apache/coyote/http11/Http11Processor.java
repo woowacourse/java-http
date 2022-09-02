4a4b3d9e01c22a40b88b18aa2143da934f2f20d3
brorae <jhr3239@gmail.com>
2022-09-02 14:09:55 +0900

    feat: index.html 응답 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,16 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -26,9 +30,14 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final var bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
 
-            final var responseBody = "Hello world!";
+            final String requestURI = bufferedReader.readLine();
+            final String resource = requestURI.split(" ")[1]
+                    .substring(1);
+            final String requestHeader = getRequestHeader(bufferedReader);
+            final String responseBody = getResponseBody(resource);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -42,5 +51,27 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String getResponseBody(String resource) throws IOException {
+        if (resource.isEmpty()) {
+            return "Hello world!";
+        }
+        final Path path = Paths.get(this.getClass().getClassLoader().getResource("static/" + resource).getFile());
+        return new String(Files.readAllBytes(path));
+    }
+
+
+    private String getRequestHeader(BufferedReader bufferedReader) throws IOException {
+        final StringBuilder sb = new StringBuilder();
+        while (true) {
+            final String line = bufferedReader.readLine();
+            if (line == null) {
+                break;
+            }
+            sb.append(line)
+                    .append("\r\n");
+        }
+        return sb.toString();
     }
 }

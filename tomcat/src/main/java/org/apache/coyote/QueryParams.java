e42fdd2504d9583b65bbabd0ec5959ac7af09f8a
Ohzzi <clover7even@naver.com>
2022-09-02 09:59:32 +0900

    feat: QueryParams 생성 제약 조건 추가

--- tomcat/src/main/java/org/apache/coyote/QueryParams.java
+++ tomcat/src/main/java/org/apache/coyote/QueryParams.java
@@ -2,6 +2,7 @@ package org.apache.coyote;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.coyote.exception.QueryStringFormatException;
 
 public class QueryParams {
 
@@ -9,6 +10,7 @@ public class QueryParams {
     public static final String KEY_AND_VALUE_DELIMITER = "=";
     public static final int KEY_INDEX = 0;
     public static final int VALUE_INDEX = 1;
+    public static final int QUERY_PARAMS_FORMAT_SIZE = 2;
     private final Map<String, String> params;
 
     private QueryParams(final Map<String, String> params) {
@@ -16,16 +18,30 @@ public class QueryParams {
     }
 
     public static QueryParams parseQueryParams(final String queryString) {
+        if (queryString.isBlank()) {
+            return new QueryParams(new HashMap<>());
+        }
         String[] queries = queryString.split(QUERY_PARAMS_DELIMITER);
         Map<String, String> params = new HashMap<>();
         for (String query : queries) {
             String[] keyAndValue = query.split(KEY_AND_VALUE_DELIMITER);
+            validateFormat(keyAndValue);
             params.put(keyAndValue[KEY_INDEX], keyAndValue[VALUE_INDEX]);
         }
         return new QueryParams(params);
     }
 
+    private static void validateFormat(final String[] keyAndValue) {
+        if (keyAndValue.length != QUERY_PARAMS_FORMAT_SIZE) {
+            throw new QueryStringFormatException();
+        }
+    }
+
     public String get(final String key) {
         return params.get(key);
+    }
+
+    public boolean exists() {
+        return params.size() > 0;
     }
 }
--- tomcat/src/test/java/org/apache/coyote/QueryParamsTest.java
+++ tomcat/src/test/java/org/apache/coyote/QueryParamsTest.java
@@ -1,8 +1,10 @@
 package org.apache.coyote;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import org.apache.coyote.exception.QueryStringFormatException;
 import org.junit.jupiter.api.Test;
 
 class QueryParamsTest {
@@ -20,5 +22,27 @@ class QueryParamsTest {
                 () -> assertThat(queryParams.get("account")).isEqualTo("gugu"),
                 () -> assertThat(queryParams.get("password")).isEqualTo("password")
         );
+    }
+
+    @Test
+    void 빈_query_string을_받으면_query_params가_없다() {
+        // given
+        String queryString = "";
+
+        // when
+        QueryParams actual = QueryParams.parseQueryParams(queryString);
+
+        // then
+        assertThat(actual.exists()).isFalse();
+    }
+
+    @Test
+    void 형식에_맞지_않으면_예외를_반환한다() {
+        // given
+        String queryString = "a%b$c^d";
+
+        // when, then
+        assertThatThrownBy(() -> QueryParams.parseQueryParams(queryString))
+                .isExactlyInstanceOf(QueryStringFormatException.class);
     }
 }

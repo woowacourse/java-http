94e01e4c2f50781442a04e6571ab2d5fe39fc66f
Hongdonggeon <ghd700@naver.com>
2022-09-02 15:12:07 +0900

    feat: html파일 응답하기

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,19 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,8 +35,13 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final String requestValue = readHttpRequest(inputStream);
 
+            if (requestValue == null) {
+                return;
+            }
+
+            final String responseBody = makeResponseBody(requestValue);
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",
@@ -41,6 +53,25 @@ public class Http11Processor implements Runnable, Processor {
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        } catch (URISyntaxException e) {
+            log.error(e.getMessage(), e);
         }
+    }
+
+    public String readHttpRequest(final InputStream inputStream) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        final String requestValue = br.readLine();
+        return requestValue;
+    }
+
+    public String makeResponseBody(final String requestPath) throws IOException, URISyntaxException {
+        String resource = requestPath.split(" ")[1];
+
+        if ("/".equals(resource)) {
+            return "Hello world!";
+        }
+        final URI uri = Objects.requireNonNull(getClass().getClassLoader().getResource("static" + resource))
+                .toURI();
+        return new String(Files.readAllBytes(Paths.get(uri)));
     }
 }

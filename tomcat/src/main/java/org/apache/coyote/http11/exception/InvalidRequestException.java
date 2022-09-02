71373933ebe91020c797f07a54f8622e374297c2
devHudi <devhudi@gmail.com>
2022-09-02 15:37:34 +0900

    refactor: 사용하지 않는 예외 클래스 제거

--- tomcat/src/main/java/org/apache/coyote/http11/exception/InvalidRequestException.java
+++
@@ -1,8 +0,0 @@
-package org.apache.coyote.http11.exception;
-
-public class InvalidRequestException extends RuntimeException {
-
-    public InvalidRequestException() {
-        super("올바르지 않은 요청입니다.");
-    }
-}

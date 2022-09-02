c01da3851d76bb76a4830a4367ad2788bb14ab52
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-09-02 16:28:00 +0900

    feat: Query String 파싱 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,26 +1,44 @@
 package org.apache.coyote.http11;
 
+import static java.util.stream.Collectors.*;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.exception.UncheckedServletException;
+import nextstep.jwp.model.User;
 
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+
+    private static final String REQUEST_URI = "REQUEST URI";
+    private static final String HTTP_METHOD = "HTTP METHOD";
+    private static final String HTTP_VERSION = "HTTP VERSION";
 
     private final Socket connection;
 
@@ -40,20 +58,33 @@ public class Http11Processor implements Runnable, Processor {
 
             Map<String, String> requestHeaders = getRequestHeaders(inputStream);
 
-            final var responseBody = getResponseBody(requestHeaders.get("REQUEST URI"));
+            URI requestUri = new URI("http://" + requestHeaders.get("Host") + requestHeaders.get(REQUEST_URI));
+            URL requestUrl = getRequestUrl(requestUri);
+            Map<String, List<String>> queryParams = getQueryParams(requestUri);
+
+            if (!queryParams.isEmpty() && queryParams.containsKey("account") && queryParams.containsKey("password")) {
+                User user = InMemoryUserRepository.findByAccount(queryParams.get("account").get(0))
+                    .orElseThrow(() -> new IllegalArgumentException("Account Not Found"));
+                if (!user.checkPassword(queryParams.get("password").get(0))) {
+                    throw new IllegalArgumentException("Password Not Matched");
+                }
+                log.info("user : {}", user);
+            }
+
+            final var responseBody = getResponseBody(requestUrl);
 
             final var response = getResponse(requestHeaders, responseBody);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
         }
     }
 
     private String getResponse(Map<String, String> requestHeaders, String responseBody) {
         String contentType = "Content-Type: text/html;charset=utf-8 ";
-        if (requestHeaders.get("Accept").contains("text/css")) {
+        if (requestHeaders.containsKey("Accept") && requestHeaders.get("Accept").contains("text/css")) {
             contentType = "Content-Type: text/css;";
         }
 
@@ -89,24 +120,82 @@ public class Http11Processor implements Runnable, Processor {
 
     private void putHeader(Map<String, String> requestHeaders, String requestLine) {
         if (!requestHeaders.isEmpty()) {
-            System.out.println(requestLine);
-            String[] HeaderAndValue = requestLine.split(": ");
-            requestHeaders.put(HeaderAndValue[0], HeaderAndValue[1]);
+            List<String> headerAndValue = parseRequestLine(requestLine, ":");
+            requestHeaders.put(headerAndValue.get(0), headerAndValue.get(1));
             return;
         }
-        String[] startLine = requestLine.split(" ");
-        requestHeaders.put("HTTP METHOD", startLine[0]);
-        requestHeaders.put("REQUEST URI", startLine[1]);
-        requestHeaders.put("HTTP VERSION", startLine[2]);
+        List<String> startLine = parseRequestLine(requestLine, " ");
+        requestHeaders.put(HTTP_METHOD, startLine.get(0));
+        requestHeaders.put(REQUEST_URI, startLine.get(1));
+        requestHeaders.put(HTTP_VERSION, startLine.get(2));
     }
 
-    private String getResponseBody(String requestURI) throws IOException {
-        if (requestURI.equals("/")) {
+    private List<String> parseRequestLine(String requestLine, String delimiter) {
+        return Arrays.stream(requestLine.split(delimiter))
+            .map(String::trim)
+            .collect(toList());
+    }
+
+    private String getResponseBody(URL requestUrl) throws IOException {
+        if (requestUrl.getPath().equals("/")) {
             return "Hello world!";
         }
 
-        final URL resource = getClass().getClassLoader().getResource("static/" + requestURI);
+        return new String(Files.readAllBytes(new File(Objects.requireNonNull(requestUrl).getFile()).toPath()));
+    }
 
-        return new String(Files.readAllBytes(new File(Objects.requireNonNull(resource).getFile()).toPath()));
+    private Map<String, List<String>> getQueryParams(URI resource) {
+        if (Objects.isNull(resource)) {
+            return Collections.emptyMap();
+        }
+
+        String query = resource.getQuery();
+        if (Objects.isNull(query) || query.isEmpty()) {
+            return Collections.emptyMap();
+        }
+
+        return Arrays.stream(query.split("&"))
+            .map(this::splitQueryParameters)
+            .collect(Collectors.groupingBy(
+                AbstractMap.SimpleImmutableEntry::getKey, HashMap::new, mapping(Map.Entry::getValue, toList())));
+    }
+
+    private AbstractMap.SimpleImmutableEntry<String, String> splitQueryParameters(String query) {
+        int index = query.indexOf("=");
+        String key = query;
+        String value = "";
+        if (index > 0) {
+            key = key.substring(0, index);
+        }
+
+        if (index > 0 && query.length() > index + 1) {
+            value = query.substring(index + 1);
+        }
+
+        return new AbstractMap.SimpleImmutableEntry<>(
+            URLDecoder.decode(key, StandardCharsets.UTF_8),
+            URLDecoder.decode(value, StandardCharsets.UTF_8)
+        );
+    }
+
+    private URL getRequestUrl(URI requestURI) throws MalformedURLException, URISyntaxException {
+        if (requestURI.getPath().equals("/")) {
+            return requestURI.toURL();
+        }
+        if (!Objects.isNull(requestURI.getQuery())) {
+            return getRequestUrl(new URI(
+                requestURI.getScheme() + "://" + requestURI.getHost() + ":" + requestURI.getPort()
+                    + requestURI.getPath()));
+        }
+
+        if (!requestURI.getPath().contains(".")) {
+            requestURI = new URI(requestURI + ".html");
+        }
+
+        URL resource = getClass().getClassLoader().getResource("static" + requestURI.getPath());
+        if (Objects.isNull(resource)) {
+            return requestURI.toURL();
+        }
+        return resource;
     }
 }

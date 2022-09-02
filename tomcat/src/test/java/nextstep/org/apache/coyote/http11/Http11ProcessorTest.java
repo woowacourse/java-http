3ba3af572b087e781d539d9dd3ecd69d93ce2a7d
BETTERFUTURE4 <rerub0831@gmail.com>
2022-09-03 04:32:02 +0900

    test: 1단계 테스트코드 작성

--- tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -1,16 +1,14 @@
 package nextstep.org.apache.coyote.http11;
 
-import support.StubSocket;
-import org.apache.coyote.http11.Http11Processor;
-import org.junit.jupiter.api.Test;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
-
 import static org.apache.coyote.Constants.CRLF;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Files;
+import org.apache.coyote.http11.Http11Processor;
+import org.junit.jupiter.api.Test;
+import support.StubSocket;
 
 class Http11ProcessorTest {
 
@@ -35,9 +33,9 @@ class Http11ProcessorTest {
     }
 
     @Test
-    void index() throws IOException {
+    void index() {
         // given
-        final String httpRequest= String.join(CRLF,
+        final String httpRequest = String.join(CRLF,
                 "GET /index.html HTTP/1.1 ",
                 "Host: localhost:8080 ",
                 "Connection: keep-alive ",
@@ -55,9 +53,75 @@ class Http11ProcessorTest {
         var expected = "HTTP/1.1 200 OK " + CRLF +
                 "Content-Type: text/html;charset=utf-8 " + CRLF +
                 "Content-Length: 5564 " + CRLF +
-                CRLF+
-                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+                CRLF +
+                getBody(resource);
 
         assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @Test
+    void css() {
+        // given
+        final String httpRequest = String.join(CRLF,
+                "GET /css/styles.css HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+        final var socket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        final URL resource = getClass().getClassLoader().getResource("static/css/styles.css");
+        var expected = "HTTP/1.1 200 OK " + CRLF +
+                "Content-Type: text/css;charset=utf-8 " + CRLF +
+                "Content-Length: 211991 " + CRLF +
+                CRLF +
+                getBody(resource);
+
+        assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @Test
+    void login() {
+        // given
+        final String httpRequest = String.join(CRLF,
+                "GET /login?account=gugu&password=password HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+        final var socket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        final URL resource = getClass().getClassLoader().getResource("static/login.html");
+        var expected = "HTTP/1.1 200 OK " + CRLF +
+                "Content-Type: text/html;charset=utf-8 " + CRLF +
+                "Content-Length: 3796 " + CRLF +
+                CRLF +
+                getBody(resource);
+
+        assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    private String getBody(URL resource) {
+        try {
+            if (resource == null) {
+                throw new RuntimeException();
+            }
+            return new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
     }
 }

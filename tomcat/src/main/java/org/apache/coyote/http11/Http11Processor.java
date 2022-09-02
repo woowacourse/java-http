859b5130d210f41784e47d617fd61e5142e665c7
syoun602 <syoun602@gmail.com>
2022-09-02 15:42:45 +0900

    refactor: 출력문 제거

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -100,7 +100,6 @@ public class Http11Processor implements Runnable, Processor {
     }
 
     private HttpResponse createHttpResponse(final HttpRequest httpRequest, final String uri) throws IOException {
-        System.err.println(httpRequest.toString());
         return new HttpResponse(HttpStatus.OK, httpRequest.getAcceptType(), ResourceLoader.getContent(uri));
     }
 }

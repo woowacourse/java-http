83a131ebd68b0d5d45e9a55577649c6eaacbb2b4
corinne <yoo77hyeon@gmail.com>
2022-09-02 15:50:23 +0900

    test: controller 테스트 추가

---
+++ tomcat/src/test/java/nextstep/jwp/controller/GreetingControllerTest.java
@@ -0,0 +1,22 @@
+package nextstep.jwp.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GreetingControllerTest {
+
+    private final Controller controller = new GreetingController();
+
+    @Test
+    void 성공코드를_반환한다() throws Exception {
+        // given
+        final RequestEntity requestEntity = new RequestEntity("text/html", "/", null);
+
+        // when
+        final ResponseEntity actual = controller.execute(requestEntity);
+
+        // then
+        assertThat(actual.getHttpStatus()).isEqualTo(HttpStatus.OK);
+    }
+}
---
+++ tomcat/src/test/java/nextstep/jwp/controller/LoginControllerTest.java
@@ -0,0 +1,57 @@
+package nextstep.jwp.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LoginControllerTest {
+
+    private final Controller controller = new LoginController();
+
+    @Test
+    void account값과_password값이_일치하면_OK를_반환한다() throws Exception {
+        // given
+        final RequestEntity requestEntity = new RequestEntity("text/html", "/login", "account=gugu&password=password");
+
+        // when
+        final ResponseEntity actual = controller.execute(requestEntity);
+
+        // then
+        assertThat(actual.getHttpStatus()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    void account값이_일치하지_않으면_BAD_REQUEST를_반환한다() throws Exception {
+        // given
+        final RequestEntity requestEntity = new RequestEntity("text/html", "/login", "account=gonggong&password=password");
+
+        // when
+        final ResponseEntity actual = controller.execute(requestEntity);
+
+        // then
+        assertThat(actual.getHttpStatus()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    void paasword값이_일치하지_않으면_BAD_REQUEST를_반환한다() throws Exception {
+        // given
+        final RequestEntity requestEntity = new RequestEntity("text/html", "/login", "account=gugu&password=password1");
+
+        // when
+        final ResponseEntity actual = controller.execute(requestEntity);
+
+        // then
+        assertThat(actual.getHttpStatus()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+    
+    @Test
+    void queryString에_값이_존재하지_않으면_예외를_발생한다() throws Exception {
+        // given
+        final RequestEntity requestEntity = new RequestEntity("text/html", "/login", "account=&password=");
+
+        // when
+        assertThatThrownBy(() -> controller.execute(requestEntity))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

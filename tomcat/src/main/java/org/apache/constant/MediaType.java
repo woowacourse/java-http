79da09e2afd294e812c7ef67bb7a912d5bb9d661
progress0407 <progress0407@gmail.com>
2022-09-02 17:45:27 +0900

    refactor(MediaType): 상수 분리

---
+++ tomcat/src/main/java/org/apache/constant/MediaType.java
@@ -0,0 +1,10 @@
+package org.apache.constant;
+
+public class MediaType {
+
+    public static final String PLAIN = "text/plain";
+    public static final String HTML = "text/html";
+    public static final String CSS = "text/css";
+    public static final String JAVASCRIPT = "text/javascript";
+    public static final String JSON = "application/json";
+}
--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,5 +1,6 @@
 package org.apache.coyote.http11;
 
+import java.awt.image.MemoryImageSource;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -17,6 +18,7 @@ import java.util.stream.Collectors;
 import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.exception.UncheckedServletException;
 import nextstep.jwp.model.User;
+import org.apache.constant.MediaType;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -44,16 +46,20 @@ public class Http11Processor implements Runnable, Processor {
 
             final String uri = StreamUtils.readAllLines(inputStream).split(" ")[1];
 
-            if (uri.contains(".")) {
-                routeForStaticFile(outputStream, uri);
-            } else if ("/".equals(uri)) {
-                routeForRoot(outputStream);
-            } else if (uri.contains("/login")) {
-                routeForLogin(outputStream, uri);
-            }
+            route(outputStream, uri);
             return;
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        }
+    }
+
+    private void route(final OutputStream outputStream, final String uri) throws IOException {
+        if (uri.contains(".")) {
+            routeForStaticFile(outputStream, uri);
+        } else if ("/".equals(uri)) {
+            routeForRoot(outputStream);
+        } else if (uri.contains("/login")) {
+            routeForLogin(outputStream, uri);
         }
     }
 
@@ -65,14 +71,14 @@ public class Http11Processor implements Runnable, Processor {
         final User findUser = InMemoryUserRepository.findByAccount(account)
                 .stream()
                 .findAny()
-                .orElseThrow(() -> new IllegalArgumentException(">>>>>>> not found user >>>>>>>"));
+                .orElseThrow(() -> new IllegalArgumentException("User Not Found !"));
 
         if (findUser.checkPassword(password)) {
-            log.info("Correct ! {}", findUser);
+            log.info("Login Success ! {}", findUser);
             return;
         }
 
-        log.info("user not found ! user : {}", findUser);
+        log.info("Login Fail ! user : {}", findUser);
     }
 
     /**
@@ -102,26 +108,21 @@ public class Http11Processor implements Runnable, Processor {
         final String[] fileNameSeperated = uri.split("\\.");
         final String extension = fileNameSeperated[fileNameSeperated.length - 1];
         final String responseBody = getResponseBody(uri);
-        String fileType = getFileType(extension);
+        String fileType = getContentType(extension);
 
-        final var response = String.join("\r\n",
-                "HTTP/1.1 200 OK ",
-                "Content-Type: " + fileType + ";charset=utf-8 ",
-                "Content-Length: " + responseBody.getBytes().length + " ",
-                "",
-                responseBody);
+        final var response = getResponse(responseBody, fileType);
         outputStream.write(response.getBytes());
         outputStream.flush();
     }
 
+    /**
+     * Welcome Page
+     * <p/>
+     * "/" 경로에 대한 라우팅
+     */
     private void routeForRoot(final OutputStream outputStream) {
         final String welcomeMessage = "Hello world!";
-        final var response = String.join("\r\n",
-                "HTTP/1.1 200 OK ",
-                "Content-Type: text/plain;charset=utf-8 ",
-                "Content-Length: " + welcomeMessage.getBytes().length + " ",
-                "",
-                welcomeMessage);
+        final var response = getResponse(MediaType.PLAIN, welcomeMessage);
         try {
             outputStream.write(response.getBytes());
             outputStream.flush();
@@ -130,14 +131,25 @@ public class Http11Processor implements Runnable, Processor {
         }
     }
 
-    private String getFileType(final String extension) {
-        switch (extension) {
+    private String getResponse(final String responseBody, final String fileType) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: " + fileType + ";charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+    }
+
+    private String getContentType(final String fileExtension) {
+        switch (fileExtension) {
             case "html":
-                return "text/html";
+                return MediaType.HTML;
             case "css":
-                return "text/css";
+                return MediaType.CSS;
+            case "js":
+                return MediaType.JAVASCRIPT;
             default:
-                return "text/plain";
+                return MediaType.PLAIN;
         }
     }
 

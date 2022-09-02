e8aa5c3d9fac92591d876b785b809fc009f535f8
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-02 15:42:46 +0900

    feat(StartLine): add validations on version elements

--- tomcat/src/main/java/org/apache/coyote/request/StartLine.java
+++ tomcat/src/main/java/org/apache/coyote/request/StartLine.java
@@ -8,6 +8,7 @@ import org.apache.coyote.support.HttpVersion;
 public class StartLine {
 
     private static final String DELIMITER = " ";
+    private static final int START_LINE_ELEMENT_COUNT = 3;
     private static final int METHOD_INDEX = 0;
     private static final int URI_INDEX = 1;
     private static final int VERSION_INDEX = 2;
@@ -24,12 +25,35 @@ public class StartLine {
 
     public static StartLine of(String startLine) {
         final var elements = startLine.split(DELIMITER);
-        if (elements.length != 3) {
+        if (elements.length != START_LINE_ELEMENT_COUNT) {
             throw new HttpException(HttpStatus.BAD_REQUEST);
         }
-        final var method = HttpMethod.valueOf(elements[METHOD_INDEX]);
-        final var uri = elements[URI_INDEX];
-        final var version = HttpVersion.valueOf(elements[VERSION_INDEX]);
+        final var method = toValidMethod(elements[METHOD_INDEX]);
+        final var uri = toUri(elements[URI_INDEX]);
+        final var version = toValidVersion(elements[VERSION_INDEX]);
         return new StartLine(method, uri, version);
+    }
+
+    private static HttpMethod toValidMethod(String method) {
+        try {
+            return HttpMethod.valueOf(method);
+        } catch (IllegalArgumentException e) {
+            throw new HttpException(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private static String toUri(String uri) {
+        if (!uri.startsWith("/")) {
+            throw new HttpException(HttpStatus.BAD_REQUEST);
+        }
+        return uri;
+    }
+
+    private static HttpVersion toValidVersion(String version) {
+        try {
+            return HttpVersion.valueOf(version);
+        } catch (IllegalArgumentException e) {
+            throw new HttpException(HttpStatus.BAD_REQUEST);
+        }
     }
 }

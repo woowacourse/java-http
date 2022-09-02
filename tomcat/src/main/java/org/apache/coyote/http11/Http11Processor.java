46278c963672f6d92dcc0f9b829111ae7a6b02a2
RIANAEH <ririhan217@gmail.com>
2022-09-02 16:39:04 +0900

    feat: 처리 못한 예외가 발생한 경우 500.html로 리다이렉트하는 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -2,9 +2,11 @@ package org.apache.coyote.http11;
 
 import java.io.IOException;
 import java.net.Socket;
+import nextstep.jwp.controller.Controller;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.response.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,12 +32,20 @@ public class Http11Processor implements Runnable, Processor {
 
             final var httpRequest = HttpRequest.from(inputStream);
             final var mappedController = RequestMapper.mapToController(httpRequest);
-            final var httpResponse = mappedController.doService(httpRequest);
+            final var httpResponse = doService(httpRequest, mappedController);
 
             outputStream.write(httpResponse.toString().getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        }
+    }
+
+    private HttpResponse doService(final HttpRequest httpRequest, final Controller mappedController) {
+        try {
+            return mappedController.doService(httpRequest);
+        } catch (Exception e) {
+            return HttpResponse.found("/500.html");
         }
     }
 }

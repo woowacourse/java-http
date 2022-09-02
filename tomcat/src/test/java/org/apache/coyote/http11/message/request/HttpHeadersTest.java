9882df4f70338995b1ba9c478f47b184306b0584
devHudi <devhudi@gmail.com>
2022-09-02 13:22:25 +0900

    test: 테스트 코드 수정

--- tomcat/src/test/java/org/apache/coyote/http11/message/request/HttpHeadersTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/message/request/HttpHeadersTest.java
@@ -32,11 +32,11 @@ class HttpHeadersTest {
     void getHeader() {
         // given
         String requestHeaders =
-                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\n"
-                        + "Accept-Encoding: gzip, deflate, br\n"
-                        + "Accept-Language: ko,en-US;q=0.9,en;q=0.8,zh-CN;q=0.7,zh;q=0.6,fr;q=0.5\n"
-                        + "Connection: keep-alive\n"
-                        + "Host: localhost:8080\n"
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n"
+                        + "Accept-Encoding: gzip, deflate, br\r\n"
+                        + "Accept-Language: ko,en-US;q=0.9,en;q=0.8,zh-CN;q=0.7,zh;q=0.6,fr;q=0.5\r\n"
+                        + "Connection: keep-alive\r\n"
+                        + "Host: localhost:8080\r\n"
                         + "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36";
 
         HttpHeaders httpHttpHeaders = new HttpHeaders(requestHeaders);

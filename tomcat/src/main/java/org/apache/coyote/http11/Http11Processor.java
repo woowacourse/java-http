5d0d00f13b83340278fb622f09ee9cfcfb0a9bba
SeungCheol <ssc6839@gmail.com>
2022-09-02 14:13:06 +0900

    feat: GET /index.html 응답하기 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,16 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -26,9 +30,20 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final var bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
 
-            final var responseBody = "Hello world!";
+            final var request = bufferedReader.readLine();
+            final var uri = request.split(" ")[1];
+            final String responseBody;
+
+            if (uri.equals("/")) {
+                responseBody = "Hello world!";
+            } else {
+                final URL resource = getClass().getClassLoader().getResource("static/" + uri);
+                final var content = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+                responseBody = content;
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",

b5f6043e500f4601d8bb73c69a3b75f6f740344b
shindong96 <2006peter@naver.com>
2022-09-02 15:16:33 +0900

    feat: index.html 응답 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,19 @@
 package org.apache.coyote.http11;
 
-import nextstep.jwp.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
+import nextstep.jwp.exception.UncheckedServletException;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -26,21 +33,50 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final BufferedReader headerReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"))) {
 
-            final var responseBody = "Hello world!";
+            final String resourceNameLine = headerReader.readLine();
+
+            if (resourceNameLine == null) {
+                return;
+            }
+
+            final URL resourceUrl = getResourceUrl(resourceNameLine);
+
+            final String responseBody = readContext(resourceUrl);
 
             final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private URL getResourceUrl(final String resourceNameLine) {
+        final String[] strings = resourceNameLine.split(" ");
+        final String resourceUri = strings[1];
+
+        if (resourceUri.equals("/")) {
+            return null;
+        }
+        return getClass().getClassLoader().getResource("static" + resourceUri);
+    }
+
+    private String readContext(final URL resourceUrl) throws IOException {
+        if (resourceUrl == null) {
+            return "Hello world!";
+        }
+        final File resourceFile = new File(resourceUrl.getFile());
+        final Path resourcePath = resourceFile.toPath();
+        final byte[] resourceContents = Files.readAllBytes(resourcePath);
+        return new String(resourceContents);
     }
 }

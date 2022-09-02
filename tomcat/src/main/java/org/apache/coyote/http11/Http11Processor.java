1ea1d46bb3cd5c02b25704eb75969f8725a9f7dc
pup-paw <wldusdl0310@gmail.com>
2022-09-02 17:27:27 +0900

    feat: Query String 파싱해 존재하는 유저인지 확인하는 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -8,7 +8,12 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.exception.UncheckedServletException;
+import nextstep.jwp.model.User;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,14 +41,14 @@ public class Http11Processor implements Runnable, Processor {
             final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
             final BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
             final String request = bufferedReader.readLine().trim();
-            final String path = request.split(" ")[1];
-            if (path == null) {
+            String uri = request.split(" ")[1];
+            String responseBody = "Hello world!";
+            String contentType = "html";
+            if (uri == null) {
                 throw new IllegalArgumentException("잘못된 형식의 요청입니다.");
             }
 
-            if ("/".equals(path)) {
-                var responseBody = "Hello world!";
-                var contentType = "html";
+            if ("/".equals(uri)) {
                 final var response = String.join("\r\n",
                         "HTTP/1.1 200 OK ",
                         "Content-Type: text/" + contentType + ";charset=utf-8 ",
@@ -56,12 +61,31 @@ public class Http11Processor implements Runnable, Processor {
                 return;
             }
 
-            final URL resource = getClass().getClassLoader().getResource("static" + path);
-            if (resource == null) {
-                throw new FileNotFoundException(path + "not found");
+            String resourcePath = uri;
+            if ("/login".equals(uri.substring(0, 6))) {
+                resourcePath = "/login.html";
+
+                int index = uri.indexOf("?");
+                Map<String, String> queryStrings = new HashMap<>();
+                for (String querystring : uri.substring(index + 1).split("&")) {
+                    String name = querystring.split("=")[0];
+                    String value = querystring.split("=")[1];
+                    queryStrings.put(name, value);
+                }
+
+                Optional<User> user = InMemoryUserRepository.findByAccount(queryStrings.get("account"));
+
+                if (user.isPresent() && user.get().checkPassword(queryStrings.get("password"))) {
+                    log.info(queryStrings.get("account") + "는 존재하는 유저입니다.");
+                }
             }
-            var responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
-            var contentType = path.split("\\.")[1];
+
+            final URL resource = getClass().getClassLoader().getResource("static" + resourcePath);
+            if (resource == null) {
+                throw new FileNotFoundException(resourcePath + " not found");
+            }
+            responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+            contentType = resourcePath.split("\\.")[1];
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/" + contentType + ";charset=utf-8 ",

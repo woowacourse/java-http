8e635067d86f22ec6532288b1138ca906d1741cd
devHudi <devhudi@gmail.com>
2022-09-02 12:57:27 +0900

    feat: ContentType을 나타내는 enum 클래스 작성

---
+++ tomcat/src/main/java/org/apache/coyote/http11/message/common/ContentType.java
@@ -0,0 +1,27 @@
+package org.apache.coyote.http11.message.common;
+
+import java.util.Arrays;
+import lombok.Getter;
+
+@Getter
+public enum ContentType {
+    HTML("html", "text/html"),
+    CSS("css", "text/css"),
+    JAVASCRIPT("javascript", "text/javascript"),
+    FAVICON("ico", "image/x-icon");
+
+    private final String extension;
+    private final String value;
+
+    ContentType(final String extension, final String value) {
+        this.extension = extension;
+        this.value = value;
+    }
+
+    public static ContentType from(final String extension) {
+        return Arrays.stream(values())
+                .filter(it -> it.getExtension().equals(extension))
+                .findFirst()
+                .orElse(HTML);
+    }
+}

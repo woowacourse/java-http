25d49d8c55d1343a7a1061bdc6bb20dd29188458
이한울 <dkfghg3@naver.com>
2022-09-02 14:59:35 +0900

    feat: GET /index.html 응답하기 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,16 +1,25 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
-
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final String URL_START_REGEX = " ";
+    private static final int URL_INDEX = 1;
+    private static final String DEFAULT_URL = "/";
+    private static final String DEFAULT_RESPONSE_BODY = "Hello world!";
 
     private final Socket connection;
 
@@ -26,21 +35,43 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
 
-            final var responseBody = "Hello world!";
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
-
+            final String responseBody = getResponseBody(parseUrl(bufferedReader));
+            final String response = createResponse(responseBody);
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String parseUrl(final BufferedReader bufferedReader) throws IOException {
+        return bufferedReader.readLine()
+                .split(URL_START_REGEX)[URL_INDEX];
+    }
+
+    private String getResponseBody(final String url) throws IOException {
+        if (url.equals(DEFAULT_URL)) {
+            return DEFAULT_RESPONSE_BODY;
+        }
+        return readFile(url);
+    }
+
+    private String readFile(String url) throws IOException {
+        final String filePath = "static" + url;
+        final URL resource = this.getClass().getClassLoader().getResource(filePath);
+        final String path = Objects.requireNonNull(resource).getPath();
+        return Files.readString(Path.of(path));
+    }
+
+    private String createResponse(final String responseBody) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
     }
 }

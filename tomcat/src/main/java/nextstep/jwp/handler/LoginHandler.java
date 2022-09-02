f67d3a78e2224c2c5dcddefa03e57a3bd5ac2b63
Seongwon97 <osw970325@naver.com>
2022-09-02 17:14:51 +0900

    refactor: 매직넘버 상수로 분리

--- tomcat/src/main/java/nextstep/jwp/handler/LoginHandler.java
+++ tomcat/src/main/java/nextstep/jwp/handler/LoginHandler.java
@@ -9,10 +9,13 @@ import nextstep.jwp.model.User;
 
 public class LoginHandler {
 
+    private static final String ACCOUNT = "account";
+    private static final String PASSWORD = "password";
+
     public User login(Map<String, String> request) {
-        Optional<User> findUser = InMemoryUserRepository.findByAccount(request.get("account"));
+        Optional<User> findUser = InMemoryUserRepository.findByAccount(request.get(ACCOUNT));
         User user = findUser.orElseThrow(MemberNotFoundException::new);
-        user.checkPassword(request.get("password"));
+        user.checkPassword(request.get(PASSWORD));
         System.out.println(user);
         return user;
     }

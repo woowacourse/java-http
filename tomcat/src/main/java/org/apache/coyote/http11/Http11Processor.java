ec5567d6822ff156b951a8d4120595c4112abb2f
Gyuchool <balance0591@naver.com>
2022-09-02 12:59:35 +0900

    feat: response index file

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,17 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -26,9 +31,12 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+             BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
 
-            final var responseBody = "Hello world!";
+            String file = getFileName(bufferedReader);
+            String responseBody = getResponseBody(file);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -39,8 +47,25 @@ public class Http11Processor implements Runnable, Processor {
 
             outputStream.write(response.getBytes());
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String getFileName(final BufferedReader bufferedReader) throws IOException {
+        return bufferedReader.readLine()
+                .split(" ")[1]
+                .substring(1);
+    }
+
+    private String getResponseBody(final String fileName) throws IOException, URISyntaxException {
+        if (fileName.isEmpty()) {
+            return "Hello world!";
+        }
+        URL resource = this.getClass()
+                .getClassLoader()
+                .getResource("static/" + fileName);
+
+        return new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
     }
 }

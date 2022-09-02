5a2f2d485909d8e8b6a1d2beccb102ff5278fbba
injoon2019 <ison@sfu.ca>
2022-09-02 11:58:17 +0900

    feat: FileTest 학습테스트

--- tomcat/src/test/java/study/FileTest.java
+++ tomcat/src/test/java/study/FileTest.java
@@ -1,5 +1,10 @@
 package study;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,11 +27,14 @@ class FileTest {
      * resource 디렉터리의 경로는 어떻게 알아낼 수 있을까?
      */
     @Test
-    void resource_디렉터리에_있는_파일의_경로를_찾는다() {
+    void resource_디렉터리에_있는_파일의_경로를_찾는다() throws URISyntaxException {
         final String fileName = "nextstep.txt";
 
         // todo
-        final String actual = "";
+        URL resource = getClass().getClassLoader().getResource(fileName);
+        Path path = Paths.get(resource.toURI());
+
+        final String actual = path.toString();
 
         assertThat(actual).endsWith(fileName);
     }
@@ -36,14 +44,15 @@ class FileTest {
      * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws URISyntaxException, IOException {
         final String fileName = "nextstep.txt";
 
         // todo
-        final Path path = null;
+        URL resource = getClass().getClassLoader().getResource(fileName);
+        final Path path = Paths.get(resource.toURI());
 
         // todo
-        final List<String> actual = Collections.emptyList();
+        final List<String> actual = Files.readAllLines(path);
 
         assertThat(actual).containsOnly("nextstep");
     }

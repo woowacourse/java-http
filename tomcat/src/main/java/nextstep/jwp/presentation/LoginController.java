5c3577be3e725416521633495fcafcc3bd862adf
yhh1056 <yhh1056@naver.com>
2022-09-04 02:44:24 +0900

    refactor: 쿼리스트링 생성방식 수정

--- tomcat/src/main/java/nextstep/jwp/presentation/LoginController.java
+++ tomcat/src/main/java/nextstep/jwp/presentation/LoginController.java
@@ -18,7 +18,7 @@ public class LoginController {
     }
 
     public String login() {
-        QueryStrings queryStrings = new QueryStrings(httpRequest.getUri().getValue());
+        QueryStrings queryStrings = QueryStrings.of(httpRequest.getUri().getValue());
         String account = queryStrings.find("account");
         String password = queryStrings.find("password");
 
--- tomcat/src/main/java/org/apache/coyote/http11/QueryStrings.java
+++ tomcat/src/main/java/org/apache/coyote/http11/QueryStrings.java
@@ -1,23 +1,32 @@
 package org.apache.coyote.http11;
 
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class QueryStrings {
 
     private static final String QUERY_PARAMETER = "?";
     private static final String VALUE_DELIMITER = "&";
     private static final String KEY_VALUE = "=";
+    public static final int KEY = 0;
+    public static final int VALUE = 1;
 
-    private final Map<String, String> values = new HashMap<>();
+    private final Map<String, String> values;
 
-    public QueryStrings(final String uri) {
+    private QueryStrings(final Map<String, String> values) {
+        this.values = Map.copyOf(values);
+    }
+
+    public static QueryStrings of(final String uri) {
         String queryString = uri.substring(uri.indexOf(QUERY_PARAMETER) + 1);
-        String[] queries = queryString.split(VALUE_DELIMITER);
-        for (String query : queries) {
-            String[] split = query.split(KEY_VALUE);
-            values.put(split[0], split[1]);
-        }
+        return new QueryStrings(toMap(queryString.split(VALUE_DELIMITER)));
+    }
+
+    private static Map<String, String> toMap(final String[] queries) {
+        return Arrays.stream(queries)
+                .map(query -> query.split(KEY_VALUE))
+                .collect(Collectors.toMap(split -> split[KEY], split -> split[VALUE], (a, b) -> b));
     }
 
     public String find(final String key) {

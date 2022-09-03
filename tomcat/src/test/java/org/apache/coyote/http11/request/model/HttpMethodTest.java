0335953f749a251312d74d874d81127a7e9f6292
yhh1056 <yhh1056@naver.com>
2022-09-04 00:12:09 +0900

    refactor: 개행 수정

--- tomcat/src/test/java/org/apache/coyote/http11/request/model/HttpMethodTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/request/model/HttpMethodTest.java
@@ -2,7 +2,6 @@ package org.apache.coyote.http11.request.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.apache.coyote.http11.request.model.HttpMethod;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -23,5 +22,4 @@ class HttpMethodTest {
 
         assertThat(httpMethod.isGet()).isTrue();
     }
-
 }

67a4e14447fa687c00c0a954bb538d5580952981
dwl21 <nggus5@naver.com>
2022-09-03 13:10:26 +0900

    test: URI 대신 URL 객체 사용

--- tomcat/src/test/java/study/FileTest.java
+++ tomcat/src/test/java/study/FileTest.java
@@ -3,8 +3,8 @@ package study;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -36,10 +36,10 @@ class FileTest {
      * 읽어온 파일의 내용을 I/O Stream을 사용해서 사용자에게 전달 해야 한다. File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() throws URISyntaxException, IOException {
+    void 파일의_내용을_읽는다() throws IOException {
         final String fileName = "nextstep.txt";
-        URI uri = Objects.requireNonNull(getClass().getClassLoader().getResource(fileName)).toURI();
-        Path path = Path.of(uri);
+        URL url = Objects.requireNonNull(getClass().getClassLoader().getResource(fileName));
+        Path path = Path.of(url.getPath());
 
         final List<String> actual = Files.readAllLines(path);
 

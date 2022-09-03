ab410654a8ad694847eefbed1e4834ac06490e3f
pup-paw <wldusdl0310@gmail.com>
2022-09-04 00:32:09 +0900

    feat: resourcePath를 이용해 응답을 생성하는 책임을 가진 Http11Response 생성
    200 ok 응답을 문자열로 만드는 기능 구현

---
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ResponseTest.java
@@ -0,0 +1,17 @@
+package org.apache.coyote.http11;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import nextstep.jwp.exception.ResourcePathNotFoundException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class Http11ResponseTest {
+
+    @DisplayName("존재하지 않는 경로를 요청하면 예외를 반환한다.")
+    @Test
+    void getResponse_ResourcePathNotFoundException() {
+        assertThatThrownBy(() -> Http11Response.from("야호"))
+                .isInstanceOf(ResourcePathNotFoundException.class);
+    }
+}

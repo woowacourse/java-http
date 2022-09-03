c8813d77e60daca3dfe148c3862f3a354a74a832
dwl21 <nggus5@naver.com>
2022-09-03 13:41:54 +0900

    refactor: 리스트 인덱스 상수처리

--- tomcat/src/main/java/nextstep/jwp/http/HttpRequest.java
+++ tomcat/src/main/java/nextstep/jwp/http/HttpRequest.java
@@ -8,7 +8,11 @@ import nextstep.jwp.utils.FileUtils;
 public class HttpRequest {
 
     private static final String REQUEST_LINE_SEPARATOR = " ";
+    private static final int HTTP_METHOD_INDEX = 0;
+    private static final int URI_INDEX = 1;
     private static final String URI_PATH_SEPARATOR = "\\?";
+    private static final int PATH_INDEX = 0;
+    private static final int QUERY_PARAM_INDEX = 1;
     private static final int ONLY_PATH_SIZE = 1;
 
     private final String httpMethod;
@@ -24,9 +28,6 @@ public class HttpRequest {
     }
 
     public static HttpRequest from(String requestLine) {
-        final int HTTP_METHOD_INDEX = 0;
-        final int URI_INDEX = 1;
-
         List<String> request = of(requestLine.split(REQUEST_LINE_SEPARATOR));
         String httpMethod = request.get(HTTP_METHOD_INDEX);
         String uri = request.get(URI_INDEX);
@@ -36,9 +37,6 @@ public class HttpRequest {
 
     private static HttpRequest handleHavingQueryParams(String httpMethod, String uri,
                                                        List<String> separatedUri) {
-        final int PATH_INDEX = 0;
-        final int QUERY_PARAM_INDEX = 1;
-
         if (hasQueryParam(separatedUri)) {
             return new HttpRequest(httpMethod, uri, separatedUri.get(PATH_INDEX),
                 QueryParams.from(separatedUri.get(QUERY_PARAM_INDEX)));

6e70e3cce63d963f14e86d4100d7c9dd6657ef72
Wishoon <heonga2@gmail.com>
2022-09-04 05:10:15 +0900

    feat: 사용하지 않는 로직 제거

--- tomcat/src/main/java/nextstep/jwp/http/response/ResponseBody.java
+++ tomcat/src/main/java/nextstep/jwp/http/response/ResponseBody.java
@@ -2,16 +2,10 @@ package nextstep.jwp.http.response;
 
 public class ResponseBody {
 
-    private static final ResponseBody EMPTY_BODY = new ResponseBody(null);
-
     private final String value;
 
     public ResponseBody(final String value) {
         this.value = value;
-    }
-
-    public static ResponseBody empty() {
-        return EMPTY_BODY;
     }
 
     public String getValue() {

1d1941ad7edfba2742ae2b31dd33de0c7d410227
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-03 16:48:48 +0900

    feat(HttpStatus): add 302, 401 and capitalize phases

--- tomcat/src/main/java/org/apache/coyote/support/HttpStatus.java
+++ tomcat/src/main/java/org/apache/coyote/support/HttpStatus.java
@@ -3,8 +3,10 @@ package org.apache.coyote.support;
 public enum HttpStatus {
 
     OK(200, "OK"),
-    BAD_REQUEST(400, "BAD REQUEST"),
-    NOT_FOUND(404, "NOT FOUND"),
+    FOUND(302, "Found"),
+    BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    NOT_FOUND(404, "Not Found"),
     INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
     ;
 

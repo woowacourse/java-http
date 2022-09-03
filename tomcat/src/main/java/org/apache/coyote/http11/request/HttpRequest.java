5a704692341df04be8e55e2687cd091bcafe1d47
yukong <kyb062666@gmail.com>
2022-09-03 19:14:05 +0900

    refactor: Http request header 패키지 생성

--- tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequest.java
@@ -3,6 +3,7 @@ package org.apache.coyote.http11.request;
 import java.util.List;
 
 import org.apache.coyote.http11.HttpHeaders;
+import org.apache.coyote.http11.request.header.RequestLine;
 
 public class HttpRequest {
 

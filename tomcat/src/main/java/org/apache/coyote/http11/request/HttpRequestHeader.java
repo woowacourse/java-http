c0011146a08523b082820970c080ab5430f015da
youngwooyoo <fbduddn97@naver.com>
2022-09-03 19:09:10 +0900

    refactor: RequestLine class 분리
    RequestHeaders의 적절하지 않은 책임 분리

--- tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequestHeader.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequestHeader.java
@@ -1,21 +1,15 @@
 package org.apache.coyote.http11.request;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class HttpRequestHeader {
 
-    private static final String REQUEST_LINE_DELIMITER = " ";
-    private static final int REQUEST_URI_INDEX = 1;
-
-    private final String requestLine;
+    private final RequestLine requestLine;
     private final RequestHeaders requestHeaders;
 
     private HttpRequestHeader(String requestLine, Map<String, String> headers) {
-        this.requestLine = requestLine;
-        this.requestHeaders = new RequestHeaders(headers, getRequestUri());
+        this.requestLine = new RequestLine(requestLine);
+        this.requestHeaders = new RequestHeaders(headers);
     }
 
     public static HttpRequestHeader of(String requestLine, Map<String, String> headers) {
@@ -23,26 +17,23 @@ public class HttpRequestHeader {
     }
 
     public String getRequestUri() {
-        List<String> httpRequestMethodInformation = Arrays.stream(requestLine
-                .split(REQUEST_LINE_DELIMITER))
-                .collect(Collectors.toList());
-        return httpRequestMethodInformation.get(REQUEST_URI_INDEX);
+        return requestLine.getRequestUri();
     }
 
     public String getAcceptHeaderValue() {
         return requestHeaders.getAcceptHeaderValue();
     }
 
-    private boolean isExistAccept() {
-        return requestHeaders.isAcceptValueCss();
-    }
-
     public boolean isAcceptValueCss() {
         return isExistAccept() && (getAcceptHeaderValue().contains("text/css"));
     }
 
+    private boolean isExistAccept() {
+        return requestHeaders.isAcceptValueCss();
+    }
+
     public String getRequestLine() {
-        return requestLine;
+        return requestLine.getRequestLine();
     }
 
     public RequestHeaders getRequestHeaders() {
--- tomcat/src/main/java/org/apache/coyote/http11/request/RequestHeaders.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/RequestHeaders.java
@@ -1,69 +1,19 @@
 package org.apache.coyote.http11.request;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.stream.Collectors;
-import nextstep.jwp.db.InMemoryUserRepository;
-import nextstep.jwp.model.User;
-import org.apache.coyote.http11.Http11Processor;
-import org.apache.coyote.http11.LoginFailureException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class RequestHeaders {
 
-    private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
-
     private static final String ACCEPT = "Accept";
-    private static final String QUERY_STRING_BEGIN_DELIMITER = "?";
-    private static final String QUERY_STRING_KEY_VALUE_DELIMITER = "=";
-    private static final String QUERY_STRING_UNIT_DELIMITER = "&";
-
 
     private final Map<String, String> requestHeaders;
 
-    public RequestHeaders(Map<String, String> requestHeaders, String requestUri) {
+    public RequestHeaders(Map<String, String> requestHeaders) {
         this.requestHeaders = requestHeaders;
-
-        if (isQueryStringExist(requestUri)) {
-            validateAccount(requestUri);
-        }
-    }
-
-    private boolean isQueryStringExist(String requestUri) {
-        return requestUri.contains(QUERY_STRING_BEGIN_DELIMITER);
-    }
-
-    private void validateAccount(String requestUri) {
-        Map<String, String> queryString = getQueryString(requestUri);
-        if (queryString.containsKey("account")) {
-            User existedUser = findUser(queryString.get("account"), queryString.get("password"));
-            log.info(existedUser.toString());
-        }
-    }
-
-    private User findUser(String account, String password) {
-        User existedUser = InMemoryUserRepository.findByAccount(account)
-                .orElseThrow(LoginFailureException::new);
-        if (!existedUser.checkPassword(password)) {
-            throw new LoginFailureException();
-        }
-        return existedUser;
-    }
-
-    private Map<String, String> getQueryString(String requestUri) {
-        int index = requestUri.indexOf(QUERY_STRING_BEGIN_DELIMITER);
-        String queryString = requestUri.substring(index + 1);
-        String[] dividedQueryString = queryString.split(QUERY_STRING_UNIT_DELIMITER);
-        Arrays.stream(dividedQueryString).forEach(System.out::println);
-
-        return Arrays.stream(dividedQueryString)
-                .map(query -> query.split(QUERY_STRING_KEY_VALUE_DELIMITER))
-                .collect(Collectors.toMap(key -> key[0].trim(), value -> value[1].trim()));
     }
 
     public String getAcceptHeaderValue() {
-        if(isExistAccept()) {
+        if (isExistAccept()) {
             return requestHeaders.get(ACCEPT);
         }
         throw new IllegalArgumentException("header에 Accept 필드가 없습니다.");

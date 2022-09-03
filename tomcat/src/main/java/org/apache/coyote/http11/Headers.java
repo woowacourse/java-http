6388a23cf38fdfc203246e4a74d5372386234a94
hyeonic <dev.hyeonic@gmail.com>
2022-09-03 20:42:02 +0900

    feat: request 및 response의 헤더 표현을 위한 Headers 일급 컬렉션 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/Headers.java
@@ -0,0 +1,25 @@
+package org.apache.coyote.http11;
+
+import java.util.LinkedHashMap;
+import org.apache.coyote.http11.exception.NoSuchHeaderException;
+
+public class Headers {
+
+    private final LinkedHashMap<String, String> values;
+
+    public Headers(final LinkedHashMap<String, String> values) {
+        this.values = values;
+    }
+
+    public void addHeader(final String key, final String value) {
+        values.put(key, value);
+    }
+
+    public String findByHeaderKey(final String key) {
+        if (!values.containsKey(key)) {
+            throw new NoSuchHeaderException();
+        }
+
+        return values.get(key);
+    }
+}

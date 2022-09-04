08194ceea7b2ce455a1f8913ca6bee00f7627678
Hongdonggeon <ghd700@naver.com>
2022-09-04 20:56:43 +0900

    refactor: queryString map 타입 변경

--- tomcat/src/main/java/org/apache/coyote/http11/QueryStringParser.java
+++ tomcat/src/main/java/org/apache/coyote/http11/QueryStringParser.java
@@ -10,17 +10,16 @@ public class QueryStringParser {
     private static final String VALUE_DELIMITER = "=";
 
 
-    public static Map<Integer, String> parsing(final String requestUrl) {
+    public static Map<String, String> parsing(final String requestUrl) {
         final int index = requestUrl.indexOf(URL_DELIMITER);
         final String queryString = requestUrl.substring(index + 1);
 
         final String[] requestParams = queryString.split(REQUEST_PARAM_DELIMITER);
-        final Map<Integer, String> map = new HashMap<>();
+        final Map<String, String> map = new HashMap<>();
 
-        int order = 0;
         for (String requestParam : requestParams) {
             final String[] values = requestParam.split(VALUE_DELIMITER);
-            map.put(order++, values[1]);
+            map.put(values[0], values[1]);
         }
         return map;
     }

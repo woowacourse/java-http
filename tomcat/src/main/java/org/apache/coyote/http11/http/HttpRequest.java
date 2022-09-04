363d2d74f5c6c7f5f2c8b27d04f56b6e0b0ec719
meatsby <meatsby@gmail.com>
2022-09-04 22:47:56 +0900

    refactor: HttpRequest 및 테스트 추가

--- tomcat/src/main/java/org/apache/coyote/http11/http/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/http11/http/HttpRequest.java
@@ -1,53 +1,40 @@
 package org.apache.coyote.http11.http;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public class HttpRequest {
 
-    private final HttpMethod httpMethod;
-    private final RequestTarget requestTarget;
-    private final HttpVersion httpVersion;
-    private final Map<String, String> headers;
+    private final RequestLine requestLine;
+    private final Headers headers;
+    private final MessageBody messageBody;
 
-    private HttpRequest(final HttpMethod httpMethod, final RequestTarget requestTarget, final HttpVersion httpVersion,
-                        final Map<String, String> headers) {
-        this.httpMethod = httpMethod;
-        this.requestTarget = requestTarget;
-        this.httpVersion = httpVersion;
+    private HttpRequest(final RequestLine requestLine, final Headers headers, final MessageBody messageBody) {
+        this.requestLine = requestLine;
         this.headers = headers;
+        this.messageBody = messageBody;
     }
 
-    public static HttpRequest from(final String requestMessage) {
-        List<String> requestMessageLines = Arrays.asList(requestMessage.split("\r\n"));
-        String requestLine = requestMessageLines.get(0);
-        Map<String, String> headers = requestMessageLines.subList(1, requestMessageLines.size()).stream()
-                .map(line -> line.split(": "))
-                .collect(Collectors.toMap(line -> line[0], line -> line[1]));
-
-        List<String> requestLines = Arrays.asList(requestLine.split(" "));
-        return new HttpRequest(
-                HttpMethod.valueOf(requestLines.get(0)),
-                RequestTarget.from(requestLines.get(1)),
-                HttpVersion.from(requestLines.get(2)),
-                headers);
+    public static HttpRequest from(final BufferedReader bufferedReader) {
+        try {
+            return new HttpRequest(
+                    RequestLine.from(bufferedReader),
+                    Headers.from(bufferedReader),
+                    MessageBody.from(bufferedReader));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public HttpMethod getHttpMethod() {
-        return httpMethod;
+    public RequestLine getRequestLine() {
+        return requestLine;
     }
 
-    public RequestTarget getRequestTarget() {
-        return requestTarget;
-    }
-
-    public HttpVersion getHttpVersion() {
-        return httpVersion;
-    }
-
-    public Map<String, String> getHeaders() {
+    public Headers getHeaders() {
         return headers;
+    }
+
+    public MessageBody getMessageBody() {
+        return messageBody;
     }
 }
---
+++ tomcat/src/test/java/org/apache/coyote/http11/http/HttpRequestTest.java
@@ -0,0 +1,34 @@
+package org.apache.coyote.http11.http;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.io.BufferedReader;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import support.BufferedReaderFactory;
+
+class HttpRequestTest {
+
+    @Test
+    void from() {
+        String httpRequest = String.join("\r\n",
+                "GET /index.html HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+        BufferedReader bufferedReader = BufferedReaderFactory.getBufferedReader(httpRequest);
+        HttpRequest actual = HttpRequest.from(bufferedReader);
+
+        assertAll(
+                () -> assertThat(actual.getRequestLine().getHttpMethod()).isEqualTo(HttpMethod.GET),
+                () -> assertThat(actual.getRequestLine().getRequestTarget().getUri()).isEqualTo("/index.html"),
+                () -> assertThat(actual.getRequestLine().getHttpVersion()).isEqualTo(HttpVersion.HTTP_1_1),
+                () -> assertThat(actual.getHeaders().getValue()).containsAllEntriesOf(Map.of(
+                        "Host", "localhost:8080",
+                        "Connection", "keep-alive")),
+                () -> assertThat(actual.getMessageBody().getValue()).isEqualTo("")
+        );
+    }
+}

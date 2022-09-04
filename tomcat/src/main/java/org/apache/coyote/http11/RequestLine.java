04552d244603718a42d46eba584019749f7db4de
summerlunaa <summerlunaa9@gmail.com>
2022-09-04 15:07:07 +0900

    feat: RequestLine 구현

---
+++ tomcat/src/main/java/org/apache/coyote/http11/RequestLine.java
@@ -0,0 +1,24 @@
+package org.apache.coyote.http11;
+
+public class RequestLine {
+
+    private final HttpMethod httpMethod;
+    private final RequestUri requestUri;
+    private final String httpVersion;
+
+    private RequestLine(final HttpMethod httpMethod, final RequestUri requestUri, final String httpVersion) {
+        this.httpMethod = httpMethod;
+        this.requestUri = requestUri;
+        this.httpVersion = httpVersion;
+    }
+
+    public static RequestLine of(final String requestLine) {
+        String[] splitRequestLine = requestLine.split(" ");
+
+        return new RequestLine(
+                HttpMethod.valueOf(splitRequestLine[0]),
+                RequestUri.of(splitRequestLine[1]),
+                splitRequestLine[2]
+        );
+    }
+}

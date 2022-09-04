0c4245c232105746584a6a0d67923de6b85449c0
meatsby <meatsby@gmail.com>
2022-09-04 17:40:02 +0900

    feat: StatusCode 구현

---
+++ tomcat/src/test/java/org/apache/coyote/http11/http/StatusCodeTest.java
@@ -0,0 +1,15 @@
+package org.apache.coyote.http11.http;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class StatusCodeTest {
+
+    @Test
+    void getStatusCode() {
+        String statusCode = StatusCode.OK.getStatusCode();
+
+        assertThat(statusCode).isEqualTo("200 OK");
+    }
+}

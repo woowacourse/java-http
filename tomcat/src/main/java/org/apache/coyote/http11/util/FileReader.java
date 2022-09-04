d7b700b4e2fe03efd721bd3fe83c9b863d5e0b8d
meatsby <meatsby@gmail.com>
2022-09-04 17:48:58 +0900

    refactor: FileReader 테스트 추가

--- tomcat/src/main/java/org/apache/coyote/http11/util/FileReader.java
+++ tomcat/src/main/java/org/apache/coyote/http11/util/FileReader.java
@@ -11,15 +11,15 @@ import nextstep.jwp.exception.NotFoundException;
 public class FileReader {
 
     public static String read(final String resourcePath) {
-        URL resource = FileReader.class
-                .getClassLoader()
-                .getResource("static" + resourcePath);
-        String filePath = Objects.requireNonNull(resource)
-                .getFile();
-        Path path = new File(filePath).toPath();
         try {
+            URL resource = FileReader.class
+                    .getClassLoader()
+                    .getResource("static" + resourcePath);
+            String filePath = Objects.requireNonNull(resource)
+                    .getFile();
+            Path path = new File(filePath).toPath();
             return new String(Files.readAllBytes(path));
-        } catch (IOException e) {
+        } catch (IOException | NullPointerException e) {
             throw new NotFoundException("File not found.");
         }
     }

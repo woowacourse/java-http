3adc6eb9ca188d82e2bfa3fba0e3e13f370b5abd
김찬 <gmelddl514@gmail.com>
2022-09-05 01:21:27 +0900

    refactor: Http11Processor 로직 변경

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -2,7 +2,6 @@ package org.apache.coyote.http11;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -12,6 +11,7 @@ import org.apache.coyote.Processor;
 import org.apache.coyote.controller.Controller;
 import org.apache.coyote.request.HttpRequest;
 import org.apache.coyote.response.HttpResponse;
+import org.apache.util.ResourceUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,13 +33,16 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final var bufferedReader = new BufferedReader(new InputStreamReader(inputStream))
+        ) {
 
-            HttpRequest httpRequest = toHttpRequest(inputStream);
+            HttpRequest httpRequest = toHttpRequest(bufferedReader);
+
             RequestMapping requestMapping = RequestMapping.init();
             Controller controller = requestMapping.find(httpRequest.getRequestUri());
+            HttpResponse httpResponse = toHttpResponse(httpRequest, controller);
 
-            HttpResponse httpResponse = controller.service(httpRequest);
             outputStream.write(httpResponse.getResponse());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
@@ -47,16 +50,34 @@ public class Http11Processor implements Runnable, Processor {
         }
     }
 
-    private HttpRequest toHttpRequest(final InputStream inputStream) throws IOException {
+    private HttpResponse toHttpResponse(final HttpRequest httpRequest, final Controller controller) throws IOException {
+        String response = controller.service(httpRequest);
+        if (controller.isRest()) {
+            return HttpResponse.of(httpRequest, response);
+        }
+        return HttpResponse.of(httpRequest, ResourceUtil.getResource(response));
+    }
+
+    private HttpRequest toHttpRequest(final BufferedReader bufferedReader) throws IOException {
         List<String> requests = new ArrayList<>();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
         String line;
         while ((line = bufferedReader.readLine()) != null) {
             if ("".equals(line)) {
                 break;
             }
+            System.out.println(line);
             requests.add(line);
         }
         return HttpRequest.from(requests);
+    }
+
+    private void addResponseBody(final HttpRequest httpRequest, final HttpResponse httpResponse,
+                                 final Controller controller)
+            throws IOException {
+        if (controller.isRest()) {
+            httpResponse.setResponseBody(controller.service(httpRequest));
+            return;
+        }
+        httpResponse.setResponseBody(ResourceUtil.getResource(controller.service(httpRequest)));
     }
 }

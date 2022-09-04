3acc35b3935383046ff69ff0015f381b2a976507
Soomin Sohn <soomin.sohn.dev@gmail.com>
2022-09-04 15:09:30 +0900

    refactor: request, response의 Headers 필드 원시값 포장

--- tomcat/src/main/java/org/apache/coyote/http11/httpmessage/Response.java
+++ tomcat/src/main/java/org/apache/coyote/http11/httpmessage/Response.java
@@ -3,16 +3,16 @@ package org.apache.coyote.http11.httpmessage;
 public class Response {
 
     String startLine;
-    String headers;
+    Headers headers;
     String body;
 
-    public Response(String startLine, String headers, String body) {
+    public Response(String startLine, Headers headers, String body) {
         this.startLine = startLine;
         this.headers = headers;
         this.body = body;
     }
 
-    public Response(String startLine, String headers) {
+    public Response(String startLine, Headers headers) {
         this.startLine = startLine;
         this.headers = headers;
     }
@@ -20,26 +20,30 @@ public class Response {
     public Response() {
     }
 
-    public static Response newInstanceWithResponseBody(HttpStatus httpStatus, ContentType contentType, String responseBody) {
-        String startLine = "HTTP/1.1 " + httpStatus.getStatusCode() + " " + httpStatus.getStatus();
-        String headers = "Content-Type: " + contentType.getContentType() + ";charset=utf-8 " + "\r\n" +
-                "Content-Length: " + responseBody.getBytes().length + " " + "\r\n";
+    public static Response newInstanceWithResponseBody(final HttpStatus httpStatus, final ContentType contentType,
+                                                       final String responseBody) {
+        final String startLine = "HTTP/1.1 " + httpStatus.getStatusCode() + " " + httpStatus.getStatus();
+
+        final Headers headers = new Headers()
+                .add("Content-Type", contentType.getContentType() + ";charset=utf-8")
+                .add("Content-Length", String.valueOf(responseBody.getBytes().length));
 
         return new Response(startLine, headers, responseBody);
     }
 
-    public static Response of(HttpStatus httpStatus, ContentType contentType, String location) {
-        String startLine = "HTTP/1.1 " + httpStatus.getStatusCode() + " " + httpStatus.getStatus();
-        String headers = "Location: " + location + " " + "\r\n" +
-                "Content-Type: " + contentType.getContentType() + ";charset=utf-8 ";
+    public static Response of(final HttpStatus httpStatus, final ContentType contentType, final String location) {
+        final String startLine = "HTTP/1.1 " + httpStatus.getStatusCode() + " " + httpStatus.getStatus();
+        final Headers headers = new Headers()
+                .add("Location", location)
+                .add("Content-Type", contentType.getContentType() + ";charset=utf-8");
 
         return new Response(startLine, headers);
     }
 
     public byte[] getBytes() {
         if (this.body == null) {
-            return String.join("\r\n", startLine, headers).getBytes();
+            return String.join("\r\n", startLine, headers.parseToString()).getBytes();
         }
-        return String.join("\r\n", startLine, headers, body).getBytes();
+        return String.join("\r\n", startLine, headers.parseToString() + "\r\n", body).getBytes();
     }
 }

7162349bd4a37fa13a97457d6ce1ff30bbef7dac
hyewoncc <hyewoncc@gmail.com>
2022-09-05 16:02:42 +0900

    refactor: UserService가 받는 메소드 파라미터 변경

--- tomcat/src/main/java/nextstep/jwp/Controller.java
+++ tomcat/src/main/java/nextstep/jwp/Controller.java
@@ -1,5 +1,6 @@
 package nextstep.jwp;
 
+import java.util.NoSuchElementException;
 import org.apache.coyote.http11.model.request.Request;
 import org.apache.coyote.http11.model.response.Response;
 import org.apache.coyote.http11.model.response.Status;
@@ -12,19 +13,23 @@ public class Controller {
 
     public static Response process(final Request request) {
         if (isLoginRequest(request)) {
-            try {
-                userService.login(request);
-                Response response = Response.of(Status.FOUND);
-                response.addHeader("Location", "/index.html");
-                return response;
-            } catch (IllegalArgumentException e) {
-                return Response.of(Status.UNAUTHORIZED);
-            }
+            return login(request);
         }
         return Response.of(Status.OK);
     }
 
     private static boolean isLoginRequest(final Request request) {
         return request.getUrl().startsWith(URL_LOGIN) && !request.getQueryParams().isEmpty();
+    }
+
+    private static Response login(final Request request) {
+        try {
+            userService.login(request.getQueryParams());
+            Response response = Response.of(Status.FOUND);
+            response.addHeader("Location", "/index.html");
+            return response;
+        } catch (IllegalArgumentException | NoSuchElementException e) {
+            return Response.of(Status.UNAUTHORIZED);
+        }
     }
 }
--- tomcat/src/main/java/nextstep/jwp/UserService.java
+++ tomcat/src/main/java/nextstep/jwp/UserService.java
@@ -4,7 +4,6 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.model.User;
-import org.apache.coyote.http11.model.request.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,13 +14,11 @@ public class UserService {
     public static final String KEY_ACCOUNT = "account";
     public static final String KEY_PASSWORD = "password";
 
-    public void login(final Request request) {
-        Map<String, String> queryParams = request.getQueryParams();
-
-        User user = InMemoryUserRepository.findByAccount(queryParams.get(KEY_ACCOUNT))
+    public void login(final Map<String, String> params) {
+        User user = InMemoryUserRepository.findByAccount(params.get(KEY_ACCOUNT))
                 .orElseThrow(NoSuchElementException::new);
 
-        if (user.checkPassword(queryParams.get(KEY_PASSWORD))) {
+        if (user.checkPassword(params.get(KEY_PASSWORD))) {
             log.info(user.toString());
             return;
         }

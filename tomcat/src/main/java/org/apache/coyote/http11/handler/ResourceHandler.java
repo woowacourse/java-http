fa47c3bcca6e49b5d046359a9c5da6ab5d9d323d
meatsby <meatsby@gmail.com>
2022-09-05 14:48:00 +0900

    refactor: ResourceHandler 및 테스트 추가

--- tomcat/src/main/java/org/apache/coyote/http11/handler/ResourceHandler.java
+++ tomcat/src/main/java/org/apache/coyote/http11/handler/ResourceHandler.java
@@ -2,14 +2,18 @@ package org.apache.coyote.http11.handler;
 
 import org.apache.coyote.http11.http.ContentType;
 import org.apache.coyote.http11.http.HttpRequest;
+import org.apache.coyote.http11.http.HttpResponse;
+import org.apache.coyote.http11.http.MessageBody;
 import org.apache.coyote.http11.util.FileReader;
 
 public class ResourceHandler implements Handler {
 
     @Override
-    public String handle(final HttpRequest httpRequest) {
-        String uri = httpRequest.getRequestTarget().getUri();
+    public HttpResponse handle(final HttpRequest httpRequest) {
+        String uri = httpRequest.getRequestLine()
+                .getRequestTarget()
+                .getUri();
         String responseBody = FileReader.read(uri);
-        return createResponseMessage(ContentType.from(uri), responseBody);
+        return HttpResponse.ok(ContentType.from(uri), new MessageBody(responseBody));
     }
 }

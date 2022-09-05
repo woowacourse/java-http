b3b34f02eabcdc9147655d71c2cf979c5773ff0e
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-09-06 08:18:50 +0900

    feat: 로그인 페이지 post 받도록 수정

--- tomcat/src/main/java/nextstep/jwp/handler/LoginRequestHandler.java
+++ tomcat/src/main/java/nextstep/jwp/handler/LoginRequestHandler.java
@@ -1,13 +1,15 @@
 package nextstep.jwp.handler;
 
-import java.util.Map;
 import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.exception.NotFoundUserException;
+import nextstep.jwp.http.HttpMethod;
 import nextstep.jwp.http.HttpRequest;
+import nextstep.jwp.http.HttpRequestBody;
 import nextstep.jwp.http.HttpResponse;
 import nextstep.jwp.http.HttpVersion;
 import nextstep.jwp.http.Location;
 import nextstep.jwp.model.User;
+import nextstep.jwp.util.ResourcesUtil;
 
 public class LoginRequestHandler implements HttpRequestHandler {
 
@@ -19,24 +21,31 @@ public class LoginRequestHandler implements HttpRequestHandler {
 
     @Override
     public HttpResponse handleHttpRequest(final HttpRequest httpRequest) {
-        Map<String, String> queryParams = httpRequest.getQueryParams();
-        String account = queryParams.get("account");
-        String password = queryParams.get("password");
+        if (httpRequest.getHttpMethod().equals(HttpMethod.GET)) {
+            return handleHttpGetRequest(httpRequest);
+        }
+        if (httpRequest.getHttpMethod().equals(HttpMethod.POST)) {
+            return handleHttpPostRequest(httpRequest);
+        }
+        return null;
+    }
+
+    @Override
+    public HttpResponse handleHttpGetRequest(final HttpRequest httpRequest) {
+        String responseBody = ResourcesUtil.readResource(httpRequest.getFilePath(), this.getClass());
+        return HttpResponse.ok(httpVersion, responseBody);
+    }
+
+    @Override
+    public HttpResponse handleHttpPostRequest(final HttpRequest httpRequest) {
+        HttpRequestBody httpRequestBody = httpRequest.getHttpRequestBody();
+        String account = httpRequestBody.getValue("account");
+        String password = httpRequestBody.getValue("password");
         User user = InMemoryUserRepository.findByAccount(account)
                 .orElseThrow(NotFoundUserException::new);
         if (user.checkPassword(password)) {
             return HttpResponse.found(httpVersion, new Location("/index.html"));
         }
         return HttpResponse.found(httpVersion, new Location("/401.html"));
-    }
-
-    @Override
-    public HttpResponse handleHttpGetRequest(final HttpRequest httpRequest) {
-        return null;
-    }
-
-    @Override
-    public HttpResponse handleHttpPostRequest(final HttpRequest httpRequest) {
-        return null;
     }
 }

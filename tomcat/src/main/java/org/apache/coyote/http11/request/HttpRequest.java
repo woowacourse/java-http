b3bc818a1bc4ec034c5ac52741be4dd02fc3f591
sure-why-not <pkse1213@gmail.com>
2022-09-05 18:30:06 +0900

    refactor: 상수 분리

--- tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequest.java
@@ -4,24 +4,30 @@ import java.util.Optional;
 
 public class HttpRequest {
 
+    private static final String REQUEST_LINE_SPLITTER = " ";
+    private static final String FILE_EXTENSION_SIGN = ".";
+    private static final int REQUEST_METHOD_INDEX = 0;
+    private static final int REQUEST_URI_INDEX = 1;
+    private static final int REQUEST_PROTOCOL_INDEX = 2;
+
     private final String requestMethod;
     private final URI uri;
     private final String protocol;
 
     public HttpRequest(String requestLine) {
-        String[] str = requestLine.split(" ");
-        this.requestMethod = str[0];
-        this.uri = new URI(str[1]);
-        this.protocol = str[2];
+        String[] str = requestLine.split(REQUEST_LINE_SPLITTER);
+        this.requestMethod = str[REQUEST_METHOD_INDEX];
+        this.uri = new URI(str[REQUEST_URI_INDEX]);
+        this.protocol = str[REQUEST_PROTOCOL_INDEX];
     }
 
     public boolean isStaticFileRequest() {
-        return uri.getPath().contains(".");
+        return uri.getPath().contains(FILE_EXTENSION_SIGN);
     }
 
     public Optional<String> getExtension() {
         String path = uri.getPath();
-        int index = path.indexOf(".");
+        int index = path.indexOf(FILE_EXTENSION_SIGN);
         return Optional.of(path.substring(index + 1));
     }
 
--- tomcat/src/main/java/org/apache/coyote/http11/request/URI.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/URI.java
@@ -5,6 +5,12 @@ import java.util.Map;
 
 public class URI {
 
+    private static final String PATH_SPLITTER = "?";
+    private static final String QUERY_STRING_KEY_VALUE_SEPARATOR = "=";
+    private static final String QUERY_STRING_SEPARATOR = "&";
+    private static final int QUERY_STRING_KEY_INDEX = 0;
+    private static final int QUERY_STRING_VALUE_INDEX = 1;
+
     private final String path;
     private final Map<String, String> queryParams = new HashMap<>();
 
@@ -14,21 +20,21 @@ public class URI {
     }
 
     private String getPath(String uri) {
-        if (uri.contains("?")) {
-            int index = uri.indexOf("?");
+        if (uri.contains(PATH_SPLITTER)) {
+            int index = uri.indexOf(PATH_SPLITTER);
             return uri.substring(0, index);
         }
         return uri;
     }
 
     private void parseToQuery(String uri) {
-        if (uri.contains("?")) {
-            int index = uri.indexOf("?");
+        if (uri.contains(PATH_SPLITTER)) {
+            int index = uri.indexOf(PATH_SPLITTER);
             String queryString = uri.substring(index + 1);
-            String[] queries = queryString.split("&");
+            String[] queries = queryString.split(QUERY_STRING_SEPARATOR);
             for (String query : queries) {
-                String[] s = query.split("=");
-                queryParams.put(s[0], s[1]);
+                String[] s = query.split(QUERY_STRING_KEY_VALUE_SEPARATOR);
+                queryParams.put(s[QUERY_STRING_KEY_INDEX], s[QUERY_STRING_VALUE_INDEX]);
             }
         }
     }

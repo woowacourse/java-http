d31b708318161b7fad075d889f51305e3c07a348
pup-paw <wldusdl0310@gmail.com>
2022-09-06 00:33:35 +0900

    refactor: index를 상수로 분리해 보다 명시적으로 표현
    validateRequestMethod()의 static 제거

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Request.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Request.java
@@ -10,6 +10,8 @@ import nextstep.jwp.exception.MethodNotAllowedException;
 public class Http11Request {
 
     private static final List<String> ALLOWED_METHODS = List.of("GET");
+    private static final int REQUEST_METHOD_INDEX = 0;
+    private static final int REQUEST_URL_INDEX = 1;
 
     private final String requestMethod;
     private final String requestUrl;
@@ -20,7 +22,7 @@ public class Http11Request {
         this.requestUrl = requestUrl;
     }
 
-    private static void validateRequestMethod(String requestMethod) {
+    private void validateRequestMethod(String requestMethod) {
         if (!ALLOWED_METHODS.contains(requestMethod)) {
             throw new MethodNotAllowedException(requestMethod + "는 사용할 수 없는 메서드입니다.");
         }
@@ -30,7 +32,7 @@ public class Http11Request {
         final String startLine = getStartLine(inputStream);
         final String[] startLineParts = startLine.split(" ");
         validateUrl(startLineParts);
-        return new Http11Request(startLineParts[0], startLineParts[1]);
+        return new Http11Request(startLineParts[REQUEST_METHOD_INDEX], startLineParts[REQUEST_URL_INDEX]);
     }
 
     private static String getStartLine(InputStream inputStream) throws IOException {
@@ -40,7 +42,7 @@ public class Http11Request {
     }
 
     private static void validateUrl(String[] startLineParts) {
-        if (startLineParts[1] == null) {
+        if (startLineParts[REQUEST_URL_INDEX] == null) {
             throw new IllegalArgumentException("잘못된 형식의 요청입니다.");
         }
     }

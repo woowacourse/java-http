ccf6ae4a3682b1bd2d3145fbbb0bbec63c7f70a6
SeungCheol Shin <47477359+sc0116@users.noreply.github.com>
2022-09-05 14:37:18 +0900

    [톰캣 구현하기 - 1단계] 짱구(신승철) 미션 제출합니다. (#171)
    * test: File 클래스 학습 테스트 완료
    
    * test: Java I/O Stream 클래스 학습 테스트 완료
    
    * feat: GET /index.html 응답하기 구현
    
    * feat: CSS 지원하기 구현
    
    * feat: Query String 파싱 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,18 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.exception.UncheckedServletException;
+import nextstep.jwp.model.User;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -26,21 +32,73 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final var bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
 
-            final var responseBody = "Hello world!";
+            final var uri = parseUri(bufferedReader);
+            int index = uri.indexOf("?");
+            final var path = getPath(uri);
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            if (path.equals("/login")) {
+                final var queryString = uri.substring(index + 1).split("&");
+                final var account = queryString[0].substring(queryString[0].lastIndexOf("=") + 1);
+                final var password = queryString[1].substring(queryString[1].lastIndexOf("=") + 1);
+
+                final User user = InMemoryUserRepository.findByAccount(account)
+                        .orElseThrow();
+
+                if (user.checkPassword(password)) {
+                    log.info("{}", user);
+                }
+            }
+
+            final var contentType = parseContentType(path);
+            final var responseBody = createResponseBody(path);
+
+            final var response = createResponse(contentType, responseBody);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String getPath(final String uri) {
+        final int index = uri.indexOf("?");
+
+        if (index != -1) {
+            return uri.substring(0, index);
+        }
+        return uri;
+    }
+
+    private String parseUri(final BufferedReader bufferedReader) throws IOException {
+        return bufferedReader.readLine().split(" ")[1];
+    }
+
+    private ContentType parseContentType(final String uri) {
+        final var extension = uri.substring(uri.lastIndexOf(".") + 1);
+        return ContentType.of(extension);
+    }
+
+    private String createResponseBody(String uri) throws IOException {
+        if (uri.equals("/")) {
+            return "Hello world!";
+        } else if (!uri.contains(".")) {
+            uri = uri + ".html";
+        }
+
+        URL resource = getClass().getClassLoader().getResource("static/" + uri);
+        return new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+    }
+
+    private String createResponse(final ContentType contentType, final String responseBody) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: " + contentType.getValue() + ";charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
     }
 }
--- tomcat/src/test/java/study/FileTest.java
+++ tomcat/src/test/java/study/FileTest.java
@@ -1,13 +1,12 @@
 package study;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.nio.file.Path;
-import java.util.Collections;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * 웹서버는 사용자가 요청한 html 파일을 제공 할 수 있어야 한다.
@@ -23,10 +22,12 @@ class FileTest {
      */
     @Test
     void resource_디렉터리에_있는_파일의_경로를_찾는다() {
-        final String fileName = "nextstep.txt";
+        final var fileName = "nextstep.txt";
 
         // todo
-        final String actual = "";
+        final var path = Paths.get("src/test/resources/" + fileName);
+        final var file = path.toFile();
+        final var actual = file.getName();
 
         assertThat(actual).endsWith(fileName);
     }
@@ -36,14 +37,14 @@ class FileTest {
      * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
-        final String fileName = "nextstep.txt";
+    void 파일의_내용을_읽는다() throws IOException {
+        final var fileName = "nextstep.txt";
 
         // todo
-        final Path path = null;
+        final var path = Paths.get("src/test/resources/" + fileName);
 
         // todo
-        final List<String> actual = Collections.emptyList();
+        final var actual = Files.readAllLines(path);
 
         assertThat(actual).containsOnly("nextstep");
     }

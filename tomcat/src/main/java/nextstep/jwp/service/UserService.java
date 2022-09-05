7eba508a651c4e130021f23269bb3399a8ca718b
YJGwon <yj970125@gmail.com>
2022-09-05 22:04:54 +0900

    feat(UserService): 회원 가입 기능 추가

--- tomcat/src/main/java/nextstep/jwp/service/UserService.java
+++ tomcat/src/main/java/nextstep/jwp/service/UserService.java
@@ -15,4 +15,23 @@ public class UserService {
             throw new UnauthorizedException("잘못된 비밀번호입니다.");
         }
     }
+
+    public static void register(final String account, final String password, final String email) {
+        checkUniqueness(account);
+        checkEmail(email);
+        final User user = new User(account, password, email);
+        InMemoryUserRepository.save(user);
+    }
+
+    private static void checkUniqueness(final String account) {
+        if (InMemoryUserRepository.findByAccount(account).isPresent()) {
+            throw new IllegalArgumentException("이미 존재하는 아이디입니다.");
+        }
+    }
+
+    private static void checkEmail(final String email) {
+        if (!email.contains("@")) {
+            throw new IllegalArgumentException("이메일 형식이 올바르지 않습니다.");
+        }
+    }
 }
--- tomcat/src/test/java/nextstep/jwp/service/UserServiceTest.java
+++ tomcat/src/test/java/nextstep/jwp/service/UserServiceTest.java
@@ -30,4 +30,27 @@ class UserServiceTest {
         assertThatExceptionOfType(UnauthorizedException.class)
                 .isThrownBy(() -> UserService.login("gugu", "wrong"));
     }
+
+    @DisplayName("계정과 비밀번호, 이메일을 입력해 회원가입한다.")
+    @Test
+    void register() {
+        assertThatNoException()
+                .isThrownBy(() -> UserService.register("newAccount", "password", "forky@foo.com"));
+    }
+
+    @DisplayName("중복된 아이디로 회원가입하면 예외가 발생한다.")
+    @Test
+    void register_ifAccountNotUnique() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> UserService.register("gugu", "password", "email@foo.com"))
+                .withMessage("이미 존재하는 아이디입니다.");
+    }
+
+    @DisplayName("잘못된 형식의 이메일로 회원가입하면 예외가 발생한다.")
+    @Test
+    void register_ifIllegalEmail() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> UserService.register("ggyu", "password", "email.foo.com"))
+                .withMessage("이메일 형식이 올바르지 않습니다.");
+    }
 }

0878c0ae50262007153a9ea6da4efc132a1b1391
her0807 <her0807@naver.com>
2022-09-05 10:47:19 +0900

    refactor: log 포맷 수정

---
+++ tomcat/src/main/java/org/apache/coyote/http11/UserInfoLogger.java
@@ -0,0 +1,37 @@
+package org.apache.coyote.http11;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nextstep.jwp.db.InMemoryUserRepository;
+import nextstep.jwp.model.User;
+
+public class UserInfoLogger {
+
+	private static final Logger log = LoggerFactory.getLogger(UserInfoLogger.class);
+	private static final String ID = "account";
+	private static final String PASSWORD = "password";
+
+	private UserInfoLogger() {
+	}
+
+	public static void info(Map<String, String> params) {
+		if (params.isEmpty()) {
+			return;
+		}
+		final Optional<User> optionalUser = InMemoryUserRepository.findByAccount(params.get(ID));
+		if (optionalUser.isPresent()) {
+			User user = optionalUser.get();
+			loggingInfoUser(params, user);
+		}
+	}
+
+	private static void loggingInfoUser(Map<String, String> params, User user) {
+		if (user.checkPassword(params.get(PASSWORD))) {
+			log.info("user : " + user.toString());
+		}
+	}
+}

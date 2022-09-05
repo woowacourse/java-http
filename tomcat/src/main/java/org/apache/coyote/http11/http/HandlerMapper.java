d26c81ad4192d3910591fe8ef170d78ef24ddde7
dongho <dongho108@naver.com>
2022-09-05 17:11:19 +0900

    refactor: Handler URL Regex 분리

--- tomcat/src/main/java/org/apache/coyote/http11/http/HandlerMapper.java
+++ tomcat/src/main/java/org/apache/coyote/http11/http/HandlerMapper.java
@@ -9,9 +9,9 @@ import org.apache.coyote.http11.handler.LoginHandler;
 import org.apache.coyote.http11.handler.ResourceHandler;
 
 public enum HandlerMapper {
-    HOME(Pattern.compile("^/$"), new HomeHandler()),
-    RESOURCE(Pattern.compile("^(/[a-z|A-Z|가-힣|ㄱ-ㅎ|_|\\-]*)+(\\.[a-z]*)$"), new ResourceHandler()),
-    LOGIN(Pattern.compile("^(/login)(\\?([^#\\s]*))?"), new LoginHandler());
+    HOME(Constants.HOME_URL_REGEX, new HomeHandler()),
+    RESOURCE(Constants.RESOURCE_URL_REGEX, new ResourceHandler()),
+    LOGIN(Constants.LOGIN_URL_REGEX, new LoginHandler());
 
     private final Pattern urlRegex;
     private final Handler handler;
@@ -27,5 +27,11 @@ public enum HandlerMapper {
                 .findFirst()
                 .orElseThrow(() -> new NoSuchElementException("해당하는 handler가 없습니다. " + url))
                 .handler;
+    }
+
+    private static class Constants {
+        private static final Pattern HOME_URL_REGEX = Pattern.compile("^/$");
+        private static final Pattern RESOURCE_URL_REGEX = Pattern.compile("^(/[a-z|A-Z|가-힣|ㄱ-ㅎ|_|\\-]*)+(\\.[a-z]*)$");
+        private static final Pattern LOGIN_URL_REGEX = Pattern.compile("^(/login)(\\?([^#\\s]*))?");
     }
 }

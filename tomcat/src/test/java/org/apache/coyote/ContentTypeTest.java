7e204ff2fc22a8132e77c30e1776e1836cc4e426
Ohzzi <clover7even@naver.com>
2022-09-05 16:06:53 +0900

    test: 확장자로 ContentType을 반환하는 테스트 추가

---
+++ tomcat/src/test/java/org/apache/coyote/ContentTypeTest.java
@@ -0,0 +1,20 @@
+package org.apache.coyote;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class ContentTypeTest {
+
+    @Test
+    void 일치하지_않는_확장자가_들어오면_TEXT_HTML을_반환한다() {
+        // given
+        String extension = "not_match";
+
+        // when
+        ContentType contentType = ContentType.fromExtension(extension);
+
+        // then
+        assertThat(contentType).isEqualTo(ContentType.TEXT_HTML);
+    }
+}

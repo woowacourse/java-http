8cd29e6192d867c5914d9e066efab9e4fb49fed3
pup-paw <wldusdl0310@gmail.com>
2022-09-06 00:32:27 +0900

    refactor: queryParam의 값이 없는 경우 IndexOutOfRange 예외가 발생할 수 있는 부분 처리
    index를 상수로 분리해 보다 명시적으로 표현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11QueryParams.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11QueryParams.java
@@ -2,9 +2,14 @@ package org.apache.coyote.http11;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Http11QueryParams {
+
+    private static final int KEY_INDEX = 0;
+    private static final int VALUE_INDEX = 1;
+    private static final Pattern QUERY_PARAM_PATTERN = Pattern.compile(".+=.+");
 
     private final Map<String, String> queryParams;
 
@@ -15,10 +20,14 @@ public class Http11QueryParams {
     public static Http11QueryParams from(String url) {
         final int index = url.indexOf("?");
         final String[] keyValues = url.substring(index + 1).split("&");
-        final Map<String, String> queryParams = Arrays.stream(keyValues)
+        return new Http11QueryParams(extractQueryParams(keyValues));
+    }
+
+    private static Map<String, String> extractQueryParams(String[] keyValues) {
+        return Arrays.stream(keyValues)
+                .filter(keyValue -> QUERY_PARAM_PATTERN.matcher(keyValue).find())
                 .map(keyValue -> keyValue.split("="))
-                .collect(Collectors.toMap(keyValue -> keyValue[0], keyValue -> keyValue[1]));
-        return new Http11QueryParams(queryParams);
+                .collect(Collectors.toMap(keyValue -> keyValue[KEY_INDEX], keyValue -> keyValue[VALUE_INDEX]));
     }
 
     public String getValueFrom(String key) {

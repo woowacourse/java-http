3020efc2bb99591a686932cdde23c388ee8d34dd
Hongdonggeon <ghd700@naver.com>
2022-09-05 15:43:17 +0900

    refactor: 변수명 변경

--- tomcat/src/main/java/org/apache/coyote/http11/QueryStringParser.java
+++ tomcat/src/main/java/org/apache/coyote/http11/QueryStringParser.java
@@ -13,14 +13,14 @@ public class QueryStringParser {
         final int index = requestUrl.indexOf(URL_DELIMITER);
         final String queryString = requestUrl.substring(index + 1);
 
-        final String[] requestParams = queryString.split(REQUEST_PARAM_DELIMITER);
-        final Map<String, String> map = new HashMap<>();
+        final String[] queryPairs = queryString.split(REQUEST_PARAM_DELIMITER);
+        final Map<String, String> queryParams = new HashMap<>();
 
-        for (String requestParam : requestParams) {
-            final String[] values = requestParam.split(VALUE_DELIMITER);
-            map.put(values[0], values[1]);
+        for (String queryPair : queryPairs) {
+            final String[] values = queryPair.split(VALUE_DELIMITER);
+            queryParams.put(values[0], values[1]);
         }
 
-        return map;
+        return queryParams;
     }
 }

2317f3c26c341e2bf35cb54284e1d538d5d92a63
kth990303 <kth990303@naver.com>
2022-09-06 20:15:15 +0900

    refactor: 불필요한 메서드 제거

--- tomcat/src/main/java/org/apache/coyote/http11/utils/QueryParamsParser.java
+++ tomcat/src/main/java/org/apache/coyote/http11/utils/QueryParamsParser.java
@@ -4,20 +4,8 @@ import java.util.HashMap;
 
 public class QueryParamsParser {
 
-    private static final int REQUEST_URL_QUERY_PARAM_START_INDEX = 1;
     private static final int PARAM_INFO_INDEX = 0;
     private static final int PARAM_VALUE_INDEX = 1;
-
-    public static HashMap<String, String> parseByUrl(final String requestUrl) {
-        final HashMap<String, String> data = new HashMap<>();
-        if (!requestUrl.contains("?") || !requestUrl.contains("=")) {
-            return data;
-        }
-        final String queryParams = requestUrl.split("\\?")[REQUEST_URL_QUERY_PARAM_START_INDEX];
-        final String[] params = queryParams.split("&");
-        initData(data, params);
-        return data;
-    }
 
     public static HashMap<String, String> parseByBody(final String requestBody) {
         final HashMap<String, String> data = new HashMap<>();

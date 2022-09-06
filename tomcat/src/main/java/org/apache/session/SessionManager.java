a5c5f787467e981fc771187bbcab7d39ef62fd6c
dwl21 <nggus5@naver.com>
2022-09-07 00:34:52 +0900

    feat: SessionManager 클래스 구현

---
+++ tomcat/src/main/java/org/apache/session/SessionManager.java
@@ -0,0 +1,31 @@
+package org.apache.session;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+public class SessionManager {
+
+    private static final Map<String, Session> SESSIONS = new HashMap<>();
+
+    public void add(Session session) {
+        SESSIONS.put(session.getId(), session);
+    }
+
+    public Optional<Session> find(String id) {
+        if (SESSIONS.containsKey(id)) {
+            return Optional.of(SESSIONS.get(id));
+        }
+        return Optional.empty();
+    }
+
+    public void remove(String id) {
+        SESSIONS.remove(id);
+    }
+
+    public Session generateNewSession() {
+        UUID uuid = UUID.randomUUID();
+        return new Session(uuid.toString());
+    }
+}

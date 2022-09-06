21562a53dacd0eba8c44e5cbbe172f7d68bb2388
hyeonic <dev.hyeonic@gmail.com>
2022-09-07 01:04:03 +0900

    feat: Session 표현 및 관리를 위한 Session, SessionManager 추가

---
+++ tomcat/src/main/java/org/apache/catalina/Session.java
@@ -0,0 +1,31 @@
+package org.apache.catalina;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Session {
+
+    private final String id;
+    private final Map<String, Object> values;
+
+    public Session(final String id) {
+        this.id = id;
+        this.values = new HashMap<>();
+    }
+
+    public String getId() {
+        return null;
+    }
+
+    public Object getAttribute(final String name) {
+        return values.get(name);
+    }
+
+    public void setAttribute(final String name, final Object value) {
+        values.put(name, value);
+    }
+
+    public void removeAttribute(final String name) {
+        values.remove(name);
+    }
+}
---
+++ tomcat/src/main/java/org/apache/catalina/SessionManager.java
@@ -0,0 +1,24 @@
+package org.apache.catalina;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SessionManager {
+
+    private static final Map<String, Session> SESSIONS = new HashMap<>();
+
+    private SessionManager() {
+    }
+
+    public static void add(final Session session) {
+        SESSIONS.put(session.getId(), session);
+    }
+
+    public static Session findSession(final String id) {
+        return SESSIONS.get(id);
+    }
+
+    public static void remove(final Session session) {
+        SESSIONS.remove(session.getId());
+    }
+}

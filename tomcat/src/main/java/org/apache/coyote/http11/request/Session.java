2205da137878fce8c3bcdd6f61289456024c0625
leokim <kunga077@gmail.com>
2022-09-07 02:00:55 +0900

    feat: Session 과 Session Manager 객체 구현

---
+++ tomcat/src/main/java/org/apache/coyote/http11/request/Session.java
@@ -0,0 +1,26 @@
+package org.apache.coyote.http11.request;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Session {
+
+    private final String id;
+    private final Map<String, Object> values = new HashMap<>();
+
+    public Session(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Object getAttribute(final String name) {
+        return values.get(name);
+    }
+
+    public void setAttribute(final String name, final Object value) {
+        values.put(name, value);
+    }
+}
---
+++ tomcat/src/main/java/org/apache/coyote/http11/request/SessionManager.java
@@ -0,0 +1,28 @@
+package org.apache.coyote.http11.request;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.catalina.Manager;
+
+public class SessionManager implements Manager {
+
+    private static final Map<String, Session> SESSIONS = new HashMap<>();
+
+    @Override
+    public void add(Session session) {
+    }
+
+    public static void addSession(Session session) {
+        SESSIONS.put(session.getId(), session);
+    }
+
+    @Override
+    public Session findSession(String id) {
+        return SESSIONS.get(id);
+    }
+
+    @Override
+    public void remove(Session session) {
+
+    }
+}

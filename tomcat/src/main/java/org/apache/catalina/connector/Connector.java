04d1dd8201c7fe7464ed84560e006a5bd87ef03a
DOES <ldk980130@gmail.com>
2022-09-06 22:29:01 +0900

    feat: 스레드 풀 적용

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.apache.coyote.http11.Http11Processor;
 import org.slf4j.Logger;
@@ -15,17 +17,20 @@ public class Connector implements Runnable {
 
 	private static final int DEFAULT_PORT = 8080;
 	private static final int DEFAULT_ACCEPT_COUNT = 100;
+	private static final int DEFAULT_MAX_THREAD = 200;
 
+	private final ExecutorService executorService;
 	private final ServerSocket serverSocket;
 	private boolean stopped;
 
 	public Connector() {
-		this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT);
+		this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT, DEFAULT_MAX_THREAD);
 	}
 
-	public Connector(final int port, final int acceptCount) {
+	public Connector(final int port, final int acceptCount, final int maxThreads) {
 		this.serverSocket = createServerSocket(port, acceptCount);
 		this.stopped = false;
+		executorService = Executors.newFixedThreadPool(maxThreads);
 	}
 
 	private ServerSocket createServerSocket(final int port, final int acceptCount) {
@@ -66,7 +71,7 @@ public class Connector implements Runnable {
 		}
 		log.info("connect host: {}, port: {}", connection.getInetAddress(), connection.getPort());
 		var processor = new Http11Processor(connection);
-		new Thread(processor).start();
+		executorService.submit(processor);
 	}
 
 	public void stop() {

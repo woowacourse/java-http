87ad304d5325e6e5af0b4542e30d4a8cb298302e
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-09-06 11:30:45 +0900

    feat: Session 추가

---
+++ tomcat/src/main/java/nextstep/jwp/http/Session.java
@@ -0,0 +1,34 @@
+package nextstep.jwp.http;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Session {
+
+    private final String id;
+    private final Map<String, Object> values = new ConcurrentHashMap<>();
+
+    public Session(final String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Object getAttribute(final String name) {
+        return values.get(name);
+    }
+
+    public void setAttribute(final String name, final Object value) {
+        values.put(name, value);
+    }
+
+    public void removeAttribute(final String name) {
+        values.remove(name);
+    }
+
+    public void invalidate() {
+        SessionManager.remove(id);
+    }
+}
---
+++ tomcat/src/main/java/nextstep/jwp/http/SessionManager.java
@@ -0,0 +1,21 @@
+package nextstep.jwp.http;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SessionManager {
+
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
+
+    public static void add(final Session session) {
+        SESSIONS.put(session.getId(), session);
+    }
+
+    public static Session findSession(final String id) {
+        return SESSIONS.get(id);
+    }
+
+    public static void remove(final String id) {
+        SESSIONS.remove(id);
+    }
+}

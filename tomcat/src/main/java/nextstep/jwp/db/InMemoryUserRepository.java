0b41f3ad6ad81d575d10ac705d09bd972027ba4b
syoun602 <syoun602@gmail.com>
2022-09-06 17:15:16 +0900

    feat: 회원 id counter 추가 및 테스트 격리 메서드 추가

--- tomcat/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
+++ tomcat/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
@@ -1,16 +1,19 @@
 package nextstep.jwp.db;
 
 import nextstep.jwp.model.User;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 public class InMemoryUserRepository {
 
     private static final Map<String, User> database = new ConcurrentHashMap<>();
+    private static long idCounter = 1L;
 
     static {
-        final User user = new User(1L, "gugu", "password", "hkkang@woowahan.com");
+        final User user = new User(idCounter++, "gugu", "password", "hkkang@woowahan.com");
         database.put(user.getAccount(), user);
     }
 
@@ -18,10 +21,20 @@ public class InMemoryUserRepository {
     }
 
     public static void save(final User user) {
+        user.setId(idCounter++);
         database.put(user.getAccount(), user);
     }
 
     public static Optional<User> findByAccount(final String account) {
         return Optional.ofNullable(database.get(account));
+    }
+
+    public static void cleanUp() {
+        final List<String> accounts = database.keySet()
+                .stream()
+                .filter(key -> !key.equals("gugu"))
+                .collect(Collectors.toList());
+
+        accounts.forEach(database::remove);
     }
 }

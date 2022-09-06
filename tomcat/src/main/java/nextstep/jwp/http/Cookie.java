36718a9ae303ca4875b887ab5c4311f0fb57a46f
dwl21 <nggus5@naver.com>
2022-09-07 01:55:09 +0900

    refactor: Cookie 클래스 상수 처리

--- tomcat/src/main/java/nextstep/jwp/http/Cookie.java
+++ tomcat/src/main/java/nextstep/jwp/http/Cookie.java
@@ -4,11 +4,15 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.UUID;
 
 public class Cookie {
 
     private static final String JSESSIONID_KEY = "JSESSIONID";
+    private static final String COOKIES_DELIMITER_REGEX = "; ";
+    private static final String COOKIE_DELIMITER_REGEX = "=";
+    private static final int COOKIE_DIVIDED_LIMIT = 2;
+    private static final int COOKIE_KEY_INDEX = 0;
+    private static final int COOKIE_VALUE_INDEX = 1;
     private final Map<String, String> cookies;
 
     public Cookie(Map<String, String> cookies) {
@@ -17,10 +21,11 @@ public class Cookie {
 
     public static Cookie parse(String cookieLine) {
         Map<String, String> cookies = new LinkedHashMap<>();
-        List<String> dividedCookies = List.of(cookieLine.split("; "));
+        List<String> dividedCookies = List.of(cookieLine.split(COOKIES_DELIMITER_REGEX));
         for (String cookie : dividedCookies) {
-            List<String> dividedCookie = List.of(cookie.split("=", 2));
-            cookies.put(dividedCookie.get(0), dividedCookie.get(1));
+            List<String> dividedCookie = List.of(
+                cookie.split(COOKIE_DELIMITER_REGEX, COOKIE_DIVIDED_LIMIT));
+            cookies.put(dividedCookie.get(COOKIE_KEY_INDEX), dividedCookie.get(COOKIE_VALUE_INDEX));
         }
         return new Cookie(cookies);
     }

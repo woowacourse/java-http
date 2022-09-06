9ff83589e3f3b3056f48097f19fa8ec04d458bfa
awesomeo184 <awesomeo184@gmail.com>
2022-09-06 16:47:08 +0900

    feat: 회원가입 기능 추가

--- tomcat/src/main/java/nextstep/jwp/LoginService.java
+++ tomcat/src/main/java/nextstep/jwp/LoginService.java
@@ -3,16 +3,28 @@ package nextstep.jwp;
 import java.util.Optional;
 import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LoginService {
+
+    private static final Logger log = LoggerFactory.getLogger(LoginService.class);
+
+    public void register(String account, String password, String email) {
+        User user = new User(account, password, email);
+        InMemoryUserRepository.save(user);
+        log.info("회원가입 성공, User = {}", user);
+    }
 
     public boolean login(String account, String password) {
         Optional<User> optionalUser = InMemoryUserRepository.findByAccount(account);
         if (optionalUser.isEmpty()) {
+            log.info("로그인 실패 = 존재하지 않는 사용자, account = {}", account);
             return false;
         }
         User user = optionalUser.get();
         if (!user.checkPassword(password)) {
+            log.info("로그인 실패 = 잘못된 패스워드");
             return false;
         }
         return true;
--- tomcat/src/main/java/nextstep/jwp/RegisterController.java
+++ tomcat/src/main/java/nextstep/jwp/RegisterController.java
@@ -1,9 +1,12 @@
 package nextstep.jwp;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.coyote.http11.AbstractController;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.request.ResourceLocator;
 import org.apache.coyote.http11.response.HttpResponse;
+import org.apache.coyote.http11.response.spec.HttpStatus;
 
 public class RegisterController extends AbstractController {
 
@@ -20,5 +23,34 @@ public class RegisterController extends AbstractController {
     protected void doGet(HttpRequest request, HttpResponse response) {
         String path = request.getPathString() + ".html";
         doHtmlResponse(response, path);
+    }
+
+    @Override
+    protected void doPost(HttpRequest request, HttpResponse response) {
+        Map<String, String> headers = request.getHeaders();
+        String contentType = headers.get("Content-Type");
+        if (!contentType.equals("application/x-www-form-urlencoded")) {
+            response.setStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+            return;
+        }
+        String body = request.getBody();
+        Map<String, String> data = new HashMap<>();
+        String[] components = body.split("&");
+        for (String component : components) {
+            String[] keyVal = component.split("=");
+            String key = keyVal[0];
+            String value = keyVal[1];
+            data.put(key, value);
+        }
+
+        String account = data.get("account");
+        String password = data.get("password");
+        String email = data.get("email");
+
+        LoginService loginService = new LoginService();
+        loginService.register(account, password, email);
+
+        response.setStatus(HttpStatus.FOUND);
+        response.addHeader("Location", "/index.html");
     }
 }

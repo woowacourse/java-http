c7a726570ef20b627d4b5d8ad0851d452d88b284
Richard JEON <ztzy1907@gmail.com>
2022-09-06 17:09:53 +0900

    refactor: 동적으로 서블릿 인터페이스 구현체들을 초기화하도록 개선

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -3,56 +3,38 @@ package org.apache.coyote.http11;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
-import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
-import org.apache.coyote.RegisterAsServlet;
 import org.apache.coyote.Servlet;
 import org.apache.http.BasicHttpRequest;
+import org.apache.http.HttpRequest;
+import org.reflections.Reflections;
+import org.richard.utils.CustomReflectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
-    private static final String BASE_PACKAGE_FOR_SERVLETS = "spring.servlet";
+    private static final String DEFAULT_SPRING_SERVLET_PACKAGE = "org.springframework.servlet";
+    private static final List<Servlet> servlets;
 
-    private final Socket connection;
-    private final List<Servlet> servlets;
-
-    public Http11Processor(final Socket connection) {
-        this.connection = connection;
-        this.servlets = findAllClassesUsingClassLoader(BASE_PACKAGE_FOR_SERVLETS)
+    static {
+        servlets = new Reflections(DEFAULT_SPRING_SERVLET_PACKAGE)
+                .getSubTypesOf(Servlet.class)
                 .stream()
-                .filter(classInBasePackage -> classInBasePackage.isAnnotationPresent(RegisterAsServlet.class))
-                .map(servletClasses -> (Object) servletClasses)
-                .map(servletObjects -> (Servlet) servletObjects)
+                .map(CustomReflectionUtils::newInstance)
                 .collect(Collectors.toList());
     }
 
-    private static Set<Class> findAllClassesUsingClassLoader(String packageName) {
-        InputStream stream = ClassLoader.getSystemClassLoader()
-                .getResourceAsStream(packageName.replaceAll("[.]", "/"));
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-        return reader.lines()
-                .filter(line -> line.endsWith(".class"))
-                .map(line -> getClass(line, packageName))
-                .collect(Collectors.toSet());
-    }
+    private final Socket connection;
 
-    private static Class getClass(String className, String packageName) {
-        try {
-            return Class.forName(packageName + "."
-                    + className.substring(0, className.lastIndexOf('.')));
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+    public Http11Processor(final Socket connection) {
+        this.connection = connection;
     }
 
     @Override
@@ -68,16 +50,24 @@ public class Http11Processor implements Runnable, Processor {
                 final var bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
                 final var bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
         ) {
-            final var httpMessageRequest = parseRequest(bufferedReader);
-            final var httpRequest = BasicHttpRequest.from(httpMessageRequest);
-//            final var httpResponse = mainController.doService(httpRequest);
-//            final var httpMessageResponse = httpResponse.getResponseHttpMessage();
+            final var requestHttpMessage = parseRequest(bufferedReader);
+            final var httpRequest = BasicHttpRequest.from(requestHttpMessage);
+            final var supportServlet = findSupportServlet(httpRequest);
+            final var httpResponse = supportServlet.doService(httpRequest);
+            final var responseHttpMessage = httpResponse.getResponseHttpMessage();
 
-            bufferedWriter.write("");
+            bufferedWriter.write(responseHttpMessage);
             bufferedWriter.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (Exception e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private Servlet findSupportServlet(final HttpRequest httpRequest) {
+        return servlets.stream()
+                .filter(servlet -> servlet.support(httpRequest))
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
     }
 
     private String parseRequest(final BufferedReader bufferedReader) throws IOException {

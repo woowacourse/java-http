322740a758a4b20aef348f6e15b1bef2c8f23d18
dwl21 <nggus5@naver.com>
2022-09-07 00:06:49 +0900

    feat: Request에 Cookie가 없으면 Response Header Set-Cookie를 추가하는 기능 구현

--- tomcat/src/main/java/nextstep/jwp/http/Cookie.java
+++ tomcat/src/main/java/nextstep/jwp/http/Cookie.java
@@ -4,6 +4,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 public class Cookie {
 
--- tomcat/src/main/java/nextstep/jwp/http/HttpResponse.java
+++ tomcat/src/main/java/nextstep/jwp/http/HttpResponse.java
@@ -1,28 +1,55 @@
 package nextstep.jwp.http;
 
+import java.util.UUID;
+
 public class HttpResponse {
+
+    private static final String SET_COOKIE_HEADER = "Set-Cookie: JSESSIONID=";
 
     private final StatusCode statusCode;
     private final ContentType contentType;
     private final String responseBody;
+    private final Cookie cookie;
 
-    private HttpResponse(StatusCode statusCode, ContentType contentType, String responseBody) {
+    private HttpResponse(StatusCode statusCode, ContentType contentType, String responseBody,
+                         Cookie cookie) {
         this.statusCode = statusCode;
         this.contentType = contentType;
         this.responseBody = responseBody;
+        this.cookie = cookie;
     }
 
     public static HttpResponse of(StatusCode statusCode, ContentType contentType,
                                   String responseBody) {
-        return new HttpResponse(statusCode, contentType, responseBody);
+        return new HttpResponse(statusCode, contentType, responseBody, null);
+    }
+
+    public static HttpResponse of(StatusCode statusCode, ContentType contentType,
+                                            String responseBody, Cookie cookie) {
+        return new HttpResponse(statusCode, contentType, responseBody, cookie);
     }
 
     public byte[] writeResponse() {
-        return String.join("\r\n",
-            "HTTP/1.1 " + statusCode.getStatus() + " ",
+        final String responseLine = String.format("HTTP/1.1 %s ", statusCode.getStatus());
+        if (cookie != null) {
+            final String header = String.join("\r\n",
+                 writeSetCookieOfHeader(),
+                "Content-Type: " + contentType.getMediaType() + ";charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ");
+            return write(responseLine, header);
+        }
+        final String header = String.join("\r\n",
             "Content-Type: " + contentType.getMediaType() + ";charset=utf-8 ",
-            "Content-Length: " + responseBody.getBytes().length + " ",
-            "",
-            responseBody).getBytes();
+            "Content-Length: " + responseBody.getBytes().length + " ");
+        return write(responseLine, header);
+    }
+
+    private String writeSetCookieOfHeader() {
+        UUID uuid = UUID.randomUUID();
+        return SET_COOKIE_HEADER + uuid;
+    }
+
+    private byte[] write(String responseLine, String header) {
+        return String.join("\r\n", responseLine, header, "", responseBody).getBytes();
     }
 }

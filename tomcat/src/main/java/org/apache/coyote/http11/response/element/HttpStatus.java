68653ae5511bacc7ed1224ccb3f93df157b66eef
BETTERFUTURE4 <rerub0831@gmail.com>
2022-09-06 17:33:51 +0900

    feat: 302 상태코드 추가

--- tomcat/src/main/java/org/apache/coyote/http11/response/element/HttpStatus.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/element/HttpStatus.java
@@ -2,9 +2,11 @@ package org.apache.coyote.http11.response.element;
 
 public enum HttpStatus {
     OK("200 OK"),
-    NOT_FOUND("404 NOT FOUND"),
-    UNAUTHORIZED("401 UNAUTHORIZED"),
-    INTERNAL_SERVER_ERROR("500 INTERNAL SERVER ERROR");
+    NOT_FOUND("404 Not Found"),
+    UNAUTHORIZED("401 Unauthorized"),
+    INTERNAL_SERVER_ERROR("500 Internal Server Error"),
+    FOUND("302 Found"),
+    ;
 
     private final String value;
 

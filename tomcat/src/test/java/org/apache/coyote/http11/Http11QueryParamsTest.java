8af4f9287d7a365833f7612aa1c7c0afb8d58336
pup-paw <wldusdl0310@gmail.com>
2022-09-08 05:00:28 +0900

    refactor: 사용되지 않는 로직의 테스트 삭제

--- tomcat/src/test/java/org/apache/coyote/http11/Http11QueryParamsTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11QueryParamsTest.java
@@ -1,7 +1,6 @@
 package org.apache.coyote.http11;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,14 +15,5 @@ class Http11QueryParamsTest {
         final String account = queryParams.getValueFrom("account");
 
         assertThat(account).isEqualTo("gugu");
-    }
-
-    @DisplayName("key가 존재하지 않으면 예외를 반환한다.")
-    @Test
-    void getValueFromKey_keyNotFound() {
-        final Http11QueryParams queryParams = Http11QueryParams.from("/login?account=gugu&password=password");
-
-        assertThatThrownBy(() -> queryParams.getValueFrom("email"))
-                .isInstanceOf(IllegalArgumentException.class);
     }
 }

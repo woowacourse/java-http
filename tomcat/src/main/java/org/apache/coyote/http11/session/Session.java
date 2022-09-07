5c56bf1697812dfb6926e0b7a3ea8c00cfacb6bc
Hyunta <mohai2618@gmail.com>
2022-09-08 03:30:56 +0900

    refactor: 동시성 컬렉션 사용

--- tomcat/src/main/java/org/apache/coyote/http11/session/Session.java
+++ tomcat/src/main/java/org/apache/coyote/http11/session/Session.java
@@ -1,12 +1,12 @@
 package org.apache.coyote.http11.session;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Session {
 
     private final String id;
-    private final Map<String, Object> values = new HashMap<>();
+    private final Map<String, Object> values = new ConcurrentHashMap<>();
 
     public Session(String id) {
         this.id = id;

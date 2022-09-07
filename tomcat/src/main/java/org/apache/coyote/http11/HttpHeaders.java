228422b5bce4796b83becee5b2ff9c1a95f566a2
Ohzzi <clover7even@naver.com>
2022-09-07 21:51:18 +0900

    feat: HTTP 헤더를 담는 객체 구현

---
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpHeaders.java
@@ -0,0 +1,39 @@
+package org.apache.coyote.http11;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HttpHeaders {
+
+    private static final int HEADER_END_NOT_FOUND = -1;
+
+    private final Map<String, HttpHeader> headers;
+
+    private HttpHeaders(final Map<String, HttpHeader> headers) {
+        this.headers = headers;
+    }
+
+    public static HttpHeaders parse(final List<String> headerLines) {
+        Map<String, HttpHeader> headers = new HashMap<>();
+        int headerEndIndex = findHeaderEnd(headerLines);
+        for (String headerLine : headerLines.subList(0, headerEndIndex)) {
+            HttpHeader header = HttpHeader.parse(headerLine);
+            headers.put(header.getName(), header);
+        }
+        return new HttpHeaders(headers);
+    }
+
+    private static int findHeaderEnd(final List<String> headerLines) {
+        int index = headerLines.indexOf("");
+        if (index == HEADER_END_NOT_FOUND) {
+            return headerLines.size();
+        }
+        return index;
+    }
+
+    public List<HttpHeader> getAll() {
+        return new ArrayList<>(headers.values());
+    }
+}

4a81f4917e9db324cf6e2d6e11b261bfe172ccbd
Nam Minjeong <nmj5012@naver.com>
2022-09-07 17:33:53 +0900

    fix: js 파일 읽지 못하는 에러 수정

--- tomcat/src/main/java/org/apache/coyote/http11/util/FileReader.java
+++ tomcat/src/main/java/org/apache/coyote/http11/util/FileReader.java
@@ -2,7 +2,6 @@ package org.apache.coyote.http11.util;
 
 import static org.apache.coyote.http11.response.StatusCode.*;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,10 +9,6 @@ import java.nio.file.Paths;
 import org.apache.coyote.http11.response.HttpResponse;
 
 public class FileReader {
-
-    private static final String CHARSET_UTF_8 = ";charset=utf-8";
-    private static final String TEXT_HTML = "text/html";
-
     public FileReader() {
     }
 
@@ -25,7 +20,7 @@ public class FileReader {
                 .toURI());
 
             String responseBody = new String(Files.readAllBytes(path));
-            String contentType = readContentType(path);
+            String contentType = Files.probeContentType(path);
             String contentLength = Integer.toString(responseBody.getBytes().length);
 
             return HttpResponse.of(httpVersion, OK, responseBody)
@@ -34,14 +29,5 @@ public class FileReader {
         } catch (Exception e) {
             throw new IllegalArgumentException("파일을 읽는 과정에서 오류가 발생하였습니다.");
         }
-    }
-
-    private String readContentType(Path path) throws IOException {
-        String contentType = Files.probeContentType(path);
-
-        if (contentType.equals(TEXT_HTML)) {
-            return contentType + CHARSET_UTF_8;
-        }
-        return contentType;
     }
 }
--- tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -52,7 +52,7 @@ class Http11ProcessorTest {
         // then
         final URL resource = getClass().getClassLoader().getResource("static/index.html");
         var expected = "HTTP/1.1 200 OK \r\n" +
-                "Content-Type: text/html;charset=utf-8 \r\n" +
+                "Content-Type: text/html \r\n" +
                 "Content-Length: 5670 \r\n" +
                 "\r\n"+
                 new String(Files.readAllBytes(new File(resource.getFile()).toPath()));

859bf7c606e4fad7734a86263576bdad20405e2d
EastHShin <xldk78@gmail.com>
2022-09-07 16:14:22 +0900

    feat: 회원가입 기능 구현

--- tomcat/src/main/java/nextstep/jwp/controller/RegisterController.java
+++ tomcat/src/main/java/nextstep/jwp/controller/RegisterController.java
@@ -1,6 +1,9 @@
 package nextstep.jwp.controller;
 
+import java.util.Map;
+import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.exception.UncheckedServletException;
+import nextstep.jwp.model.User;
 import org.apache.coyote.http11.request.Extension;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.request.Path;
@@ -20,6 +23,21 @@ public class RegisterController implements Controller {
                     .status(HttpStatus.OK)
                     .contentType(path.getContentType())
                     .responseBody(responseBody)
+                    .build();
+        }
+
+        if (request.isPostMethod()) {
+            final Map<String, String> params = request.getBody();
+            final String account = params.get("account");
+            final String password = params.get("password");
+            final String email = params.get("email");
+            final User user = new User(account, password, email);
+
+            InMemoryUserRepository.save(user);
+
+            return new HttpResponse.Builder()
+                    .status(HttpStatus.FOUND)
+                    .location("/index.html")
                     .build();
         }
 
--- tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -1,11 +1,15 @@
 package nextstep.org.apache.coyote.http11;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.Optional;
+import nextstep.jwp.db.InMemoryUserRepository;
+import nextstep.jwp.model.User;
 import org.apache.coyote.http11.Http11Processor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -112,5 +116,38 @@ class Http11ProcessorTest {
         final String expected = "HTTP/1.1 302 Found \r\n" +
                 "Location: /401.html \r\n";
         assertThat(socket.output()).contains(expected);
+    }
+
+    @DisplayName("회원가입 성공시 회원 정보를 저장하고 302 응답과 함께 /index.html 로 리다이렉팅 한다.")
+    @Test
+    void register() {
+        //given
+        final String account = "east";
+        final String password = "password";
+        final String email = "xldk78@gmail.com";
+        final String httpRequest = String.join("\r\n",
+                "POST /register HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "Content-Length: 58",
+                "Content-Type: application/x-www-form-urlencoded",
+                "Accept: */*",
+                "",
+                "account=" + account + "&password=" + password + "&email=" + email);
+        final StubSocket socket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(socket);
+
+        //when
+        processor.process(socket);
+
+        //then
+        final String expected = "HTTP/1.1 302 Found \r\n" +
+                "Location: /index.html \r\n";
+        final Optional<User> user = InMemoryUserRepository.findByAccount(account);
+
+        assertAll(
+                () -> assertThat(socket.output()).contains(expected),
+                () -> assertThat(user.get()).isNotNull()
+        );
     }
 }

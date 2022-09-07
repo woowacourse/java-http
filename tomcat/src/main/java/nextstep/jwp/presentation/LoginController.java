997c805027865bcd93b045df7fc8b15e22d02a12
BeomsooKim <kbsat@naver.com>
2022-09-08 04:37:12 +0900

    refactor: UserService 추가 및 LoginController, RegisterController 개선

--- tomcat/src/main/java/nextstep/jwp/presentation/LoginController.java
+++ tomcat/src/main/java/nextstep/jwp/presentation/LoginController.java
@@ -4,11 +4,9 @@ import static nextstep.jwp.presentation.StaticResource.INDEX_PAGE;
 import static nextstep.jwp.presentation.StaticResource.UNAUTHORIZED_PAGE;
 import static org.apache.catalina.Session.JSESSIONID;
 
-import java.util.Objects;
-import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.exception.UnauthorizedException;
-import nextstep.jwp.exception.UserNotFoundException;
 import nextstep.jwp.model.User;
+import nextstep.jwp.service.UserService;
 import org.apache.catalina.Session;
 import org.apache.coyote.HttpRequest;
 import org.apache.coyote.HttpResponse;
@@ -18,14 +16,14 @@ import org.slf4j.LoggerFactory;
 
 public class LoginController extends AbstractController {
 
-    private static final LoginController INSTANCE = new LoginController();
+    private static final LoginController INSTANCE = new LoginController(UserService.getInstance());
 
     private static final Logger LOG = LoggerFactory.getLogger(LoginController.class);
 
-    private static final String ACCOUNT_PARAM = "account";
-    private static final String PASSWORD_PARAM = "password";
+    private final UserService userService;
 
-    private LoginController() {
+    private LoginController(final UserService userService) {
+        this.userService = userService;
     }
 
     public static LoginController getInstance() {
@@ -46,9 +44,7 @@ public class LoginController extends AbstractController {
     @Override
     protected void doPost(final HttpRequest request, final HttpResponse response) throws Exception {
         try {
-            final User user = login(request);
-
-            logSuccessMessage(user);
+            final User user = userService.login(request);
             setLoginSession(request, response, user);
             redirectIndex(response);
         } catch (final UnauthorizedException unauthorizedException) {
@@ -65,26 +61,6 @@ public class LoginController extends AbstractController {
             return null;
         }
         return (User) session.getAttribute("user");
-    }
-
-    private User login(final HttpRequest request) {
-        final String account = request.getBodyParam(ACCOUNT_PARAM);
-        final String password = request.getBodyParam(PASSWORD_PARAM);
-
-        Objects.requireNonNull(account, "account는 null이면 안됩니다.");
-        Objects.requireNonNull(password, "password는 null이면 안됩니다.");
-
-        final User user = InMemoryUserRepository.findByAccount(account)
-                .orElseThrow(UserNotFoundException::new);
-        if (!user.checkPassword(password)) {
-            throw new UnauthorizedException();
-        }
-        return user;
-    }
-
-    private void logSuccessMessage(final User user) {
-        final String msg = user.toString();
-        LOG.info(msg);
     }
 
     private void setLoginSession(final HttpRequest request, final HttpResponse response, final User user) {
--- tomcat/src/main/java/nextstep/jwp/presentation/RegisterController.java
+++ tomcat/src/main/java/nextstep/jwp/presentation/RegisterController.java
@@ -3,10 +3,8 @@ package nextstep.jwp.presentation;
 import static nextstep.jwp.presentation.StaticResource.INDEX_PAGE;
 import static org.apache.catalina.Session.JSESSIONID;
 
-import java.util.Objects;
-import java.util.Optional;
-import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.model.User;
+import nextstep.jwp.service.UserService;
 import org.apache.catalina.Session;
 import org.apache.coyote.HttpRequest;
 import org.apache.coyote.HttpResponse;
@@ -16,15 +14,14 @@ import org.slf4j.LoggerFactory;
 
 public class RegisterController extends AbstractController {
 
-    private static final RegisterController INSTANCE = new RegisterController();
+    private static final RegisterController INSTANCE = new RegisterController(UserService.getInstance());
 
     private static final Logger LOG = LoggerFactory.getLogger(RegisterController.class);
 
-    private static final String ACCOUNT_PARAM = "account";
-    private static final String EMAIL_PARAM = "email";
-    private static final String PASSWORD_PARAM = "password";
+    private final UserService userService;
 
-    private RegisterController() {
+    private RegisterController(final UserService userService) {
+        this.userService = userService;
     }
 
     public static RegisterController getInstance() {
@@ -39,9 +36,7 @@ public class RegisterController extends AbstractController {
     @Override
     protected void doPost(final HttpRequest request, final HttpResponse response) throws Exception {
         try {
-            final User user = register(request);
-
-            logSuccessMessage(user);
+            final User user = userService.register(request);
             setLoginSession(request, response, user);
             redirectIndex(response);
         } catch (final RuntimeException runtimeException) {
@@ -49,37 +44,6 @@ public class RegisterController extends AbstractController {
         }
 
         response.setBody(StaticResource.ofRequest(request));
-    }
-
-    private User register(final HttpRequest request) {
-        final String account = request.getBodyParam(ACCOUNT_PARAM);
-        final String email = request.getBodyParam(EMAIL_PARAM);
-        final String password = request.getBodyParam(PASSWORD_PARAM);
-
-        checkNull(account, email, password);
-        checkAccountDuplication(account);
-
-        final User user = new User(account, password, email);
-        InMemoryUserRepository.save(user);
-        return user;
-    }
-
-    private void checkNull(final String account, final String email, final String password) {
-        Objects.requireNonNull(account, "account는 null이면 안됩니다.");
-        Objects.requireNonNull(email, "email은 null이면 안됩니다.");
-        Objects.requireNonNull(password, "password는 null이면 안됩니다.");
-    }
-
-    private void checkAccountDuplication(final String account) {
-        final Optional<User> savedAccount = InMemoryUserRepository.findByAccount(account);
-        if (savedAccount.isPresent()) {
-            throw new IllegalStateException("중복된 아이디입니다.");
-        }
-    }
-
-    private void logSuccessMessage(final User user) {
-        final String msg = user.toString();
-        LOG.info(msg);
     }
 
     private void setLoginSession(final HttpRequest request, final HttpResponse response, final User user) {
@@ -92,5 +56,4 @@ public class RegisterController extends AbstractController {
         response.setStatus(HttpStatus.FOUND);
         response.setLocation(INDEX_PAGE);
     }
-
 }

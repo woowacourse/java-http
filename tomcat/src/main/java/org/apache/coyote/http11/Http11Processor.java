de8cf206f08754732a2ef63ca8fd3048639e9a6c
Chris <bjuuuu98@gmail.com>
2022-09-07 20:56:49 +0900

    refactor: 불필요한 코드 삭제

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,15 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import nextstep.jwp.exception.UncheckedServletException;
-import org.apache.catalina.SessionManager;
 import org.apache.coyote.Processor;
 import org.apache.coyote.http11.controller.Controller;
-import org.apache.coyote.http11.exception.ResourceNotFoundException;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.response.HttpResponse;
-import org.apache.coyote.http11.response.generator.NotFoundResponseGenerator;
-import org.apache.coyote.http11.response.generator.ResponseGenerator;
-import org.apache.coyote.http11.response.generator.ResponseGeneratorFinder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -49,63 +44,4 @@ public class Http11Processor implements Runnable, Processor {
             log.error(e.getMessage(), e);
         }
     }
-
-
-    private String getResponse(HttpRequest httpRequest) throws IOException {
-        ResponseGeneratorFinder responseGeneratorFinder =
-                ResponseGeneratorFinder.withSessionManager(SessionManager.getInstance());
-        ResponseGenerator responseGenerator = responseGeneratorFinder.find(httpRequest);
-        try {
-            HttpResponse httpResponse = responseGenerator.generate(httpRequest);
-            return httpResponse.getResponse();
-        } catch (ResourceNotFoundException e) {
-            responseGenerator = NotFoundResponseGenerator.getInstance();
-            HttpResponse httpResponse = responseGenerator.generate(httpRequest);
-            return httpResponse.getResponse();
-        }
-    }
-
-    //private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
-    //
-    //private final Socket connection;
-    //
-    //public Http11Processor(final Socket connection) {
-    //    this.connection = connection;
-    //}
-    //
-    //@Override
-    //public void run() {
-    //    process(connection);
-    //}
-    //
-    //@Override
-    //public void process(final Socket connection) {
-    //    try (var inputStream = connection.getInputStream();
-    //         var outputStream = connection.getOutputStream();
-    //         var bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
-    //
-    //        HttpRequest httpRequest = HttpRequest.from(bufferedReader);
-    //        String response = getResponse(httpRequest);
-    //
-    //        outputStream.write(response.getBytes());
-    //        outputStream.flush();
-    //    } catch (IOException | UncheckedServletException e) {
-    //        log.error(e.getMessage(), e);
-    //    }
-    //}
-    //
-    //
-    //private String getResponse(HttpRequest httpRequest) throws IOException {
-    //    ResponseGeneratorFinder responseGeneratorFinder =
-    //            ResponseGeneratorFinder.withSessionManager(SessionManager.getInstance());
-    //    ResponseGenerator responseGenerator = responseGeneratorFinder.find(httpRequest);
-    //    try {
-    //        HttpResponse httpResponse = responseGenerator.generate(httpRequest);
-    //        return httpResponse.getResponse();
-    //    } catch (ResourceNotFoundException e) {
-    //        responseGenerator = NotFoundResponseGenerator.getInstance();
-    //        HttpResponse httpResponse = responseGenerator.generate(httpRequest);
-    //        return httpResponse.getResponse();
-    //    }
-    //}
 }

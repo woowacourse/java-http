77a7512d668a8bdce01bcbc788e1cfa7466e539f
pup-paw <wldusdl0310@gmail.com>
2022-09-08 00:24:16 +0900

    feat: Session 클래스와 SessionManager 클래스 생성

---
+++ tomcat/src/main/java/org/apache/coyote/http11/Session.java
@@ -0,0 +1,27 @@
+package org.apache.coyote.http11;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class Session {
+
+    private final String id;
+    private final Map<String, Object> values = new HashMap<>();
+
+    public Session(UUID uuid) {
+        this(uuid.toString());
+    }
+
+    public Session(String id) {
+        this.id = id;
+    }
+
+    public void setAttribute(final String name, final Object value) {
+        values.put(name, value);
+    }
+
+    public String getId() {
+        return id;
+    }
+}

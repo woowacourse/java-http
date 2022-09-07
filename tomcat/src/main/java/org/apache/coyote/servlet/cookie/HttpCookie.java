c8b976a4d5dedcf7a6f6783c46175905de6d51bd
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-07 23:32:08 +0900

    refactor(HttpCookie): unspecified max age value of -1 instead of null
    - int instead of Integer

--- tomcat/src/main/java/org/apache/coyote/servlet/cookie/HttpCookie.java
+++ tomcat/src/main/java/org/apache/coyote/servlet/cookie/HttpCookie.java
@@ -5,22 +5,23 @@ import org.apache.coyote.support.HttpException;
 
 public class HttpCookie {
 
+    private static final int MAX_AGE_UNSPECIFIED = -1;
     private static final String KEY_VALUE_DELIMITER = "=";
     private static final int COOKIE_FORMAT_ELEMENT_COUNT = 2;
     private static final String SET_MAX_AGE_FORMAT = "; Max-Age=%d";
 
     private final String name;
     private final String value;
-    private final Integer maxAge;
+    private final int maxAge;
 
-    private HttpCookie(String name, String value, Integer maxAge) {
+    private HttpCookie(String name, String value, int maxAge) {
         this.name = name;
         this.value = value;
         this.maxAge = maxAge;
     }
 
     private HttpCookie(String name, String value) {
-        this(name, value, null);
+        this(name, value, MAX_AGE_UNSPECIFIED);
     }
 
     public static HttpCookie of(String cookie) {
@@ -37,7 +38,7 @@ public class HttpCookie {
 
     public String toHeaderFormat() {
         String cookie = name + KEY_VALUE_DELIMITER + value;
-        if (maxAge != null) {
+        if (maxAge != MAX_AGE_UNSPECIFIED) {
             cookie = cookie + String.format(SET_MAX_AGE_FORMAT, maxAge);
         }
         return cookie;

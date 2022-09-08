cdef3e0602d7b6e76b3bf6ec682eba1ecdccf30d
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-08 17:08:10 +0900

    fix(test): fix broken tests

--- tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -6,9 +6,12 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
-import org.apache.catalina.servlet.CustomServlet;
-import org.apache.coyote.http11.Http11Processor;
+import nextstep.jwp.controller.HomeController;
+import nextstep.jwp.controller.ResourceController;
+import nextstep.jwp.exception.ExceptionListener;
+import org.apache.catalina.servlet.RequestMapping;
 import org.apache.catalina.session.SessionManager;
+import org.apache.coyote.http11.Http11Processor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import support.StubSocket;
@@ -20,20 +23,18 @@ class Http11ProcessorTest {
     void process() throws IOException {
         // given
         final var socket = new StubSocket();
-        final var processor = new Http11Processor(socket, new CustomServlet(), new SessionManager());
+        final var processor = new Http11Processor(socket, toRequestMapping(), new SessionManager());
 
         // when
         processor.process(socket);
 
         // then
+        final var actual = socket.output();
         final URL resource = getClass().getClassLoader().getResource("static/index.html");
-        var expected = "HTTP/1.1 200 OK \r\n" +
-                "Content-Type: text/html;charset=utf-8 \r\n" +
-                "Content-Length: 5564 \r\n" +
-                "\r\n" +
-                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
-
-        assertThat(socket.output()).isEqualTo(expected);
+        assertThat(actual).startsWith("HTTP/1.1 200 OK \r\n");
+        assertThat(actual).contains("Content-Type: text/html;charset=utf-8 \r\n");
+        assertThat(actual).contains("Content-Length: 5564 \r\n");
+        assertThat(actual).endsWith(new String(Files.readAllBytes(new File(resource.getFile()).toPath())));
     }
 
     @DisplayName("GET /index.html 요청은 index.html 파일을 응답한다")
@@ -48,19 +49,23 @@ class Http11ProcessorTest {
                 "");
 
         final var socket = new StubSocket(httpRequest);
-        final Http11Processor processor = new Http11Processor(socket, new CustomServlet(), new SessionManager());
+        final Http11Processor processor = new Http11Processor(socket, toRequestMapping(), new SessionManager());
 
         // when
         processor.process(socket);
 
         // then
+        final var actual = socket.output();
         final URL resource = getClass().getClassLoader().getResource("static/index.html");
-        var expected = "HTTP/1.1 200 OK \r\n" +
-                "Content-Type: text/html;charset=utf-8 \r\n" +
-                "Content-Length: 5564 \r\n" +
-                "\r\n" +
-                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+        assertThat(actual).startsWith("HTTP/1.1 200 OK \r\n");
+        assertThat(actual).contains("Content-Type: text/html;charset=utf-8 \r\n");
+        assertThat(actual).contains("Content-Length: 5564 \r\n");
+        assertThat(actual).endsWith(new String(Files.readAllBytes(new File(resource.getFile()).toPath())));
+    }
 
-        assertThat(socket.output()).isEqualTo(expected);
+    private RequestMapping toRequestMapping() {
+        final var exceptionHandler = new ExceptionListener();
+        return RequestMapping.of(new HomeController(exceptionHandler),
+                new ResourceController(exceptionHandler));
     }
 }

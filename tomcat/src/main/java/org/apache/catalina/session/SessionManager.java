872fe242de9195978ed5035d9d98c2ac19f8277e
dongho <dongho108@naver.com>
2022-09-08 17:38:41 +0900

    refactor: 동시성 컬렉션 적용

--- tomcat/src/main/java/org/apache/catalina/session/SessionManager.java
+++ tomcat/src/main/java/org/apache/catalina/session/SessionManager.java
@@ -1,13 +1,13 @@
 package org.apache.catalina.session;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 import org.apache.coyote.http11.header.HttpCookie;
 
 public class SessionManager {
 
-    private static final Map<String, Session> SESSIONS = new HashMap<>();
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
 
     public static void add(final Session session) {
         SESSIONS.put(session.getId(), session);

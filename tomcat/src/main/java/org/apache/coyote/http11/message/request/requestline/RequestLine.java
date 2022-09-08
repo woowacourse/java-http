f702df79883e46aceec52bf592488428f79ca3d6
YJGwon <yj970125@gmail.com>
2022-09-08 11:08:57 +0900

    refactor(RequestLine): index값 상수로 선언

--- tomcat/src/main/java/org/apache/coyote/http11/message/request/requestline/RequestLine.java
+++ tomcat/src/main/java/org/apache/coyote/http11/message/request/requestline/RequestLine.java
@@ -5,6 +5,9 @@ import org.apache.coyote.http11.message.request.QueryParams;
 
 public class RequestLine {
 
+    private static final int INDEX_METHOD = 0;
+    private static final int INDEX_URI = 1;
+
     private final Method method;
     private final RequestUri uri;
 
@@ -15,8 +18,8 @@ public class RequestLine {
 
     public static RequestLine from(final String requestLine) {
         final String[] splitLine = requestLine.split(Regex.BLANK.getValue());
-        final Method method = Method.valueOf(splitLine[0]);
-        final String uri = splitLine[1];
+        final Method method = Method.valueOf(splitLine[INDEX_METHOD]);
+        final String uri = splitLine[INDEX_URI];
         return new RequestLine(method, RequestUri.from(uri));
     }
 

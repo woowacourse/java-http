1d58d34586412e54caecd372c6a4562a82d6daaa
dongho <dongho108@naver.com>
2022-09-08 17:56:19 +0900

    test: 동시성 컬렉션 테스트 추가

--- tomcat/src/test/java/nextstep/app/ApplicationTest.java
+++ tomcat/src/test/java/nextstep/app/ApplicationTest.java
@@ -1,5 +1,6 @@
 package nextstep.app;
 
+import nextstep.jwp.dto.UserRegisterRequest;
 import org.junit.jupiter.api.Test;
 
 class ApplicationTest {
@@ -11,6 +12,28 @@ class ApplicationTest {
 
         for (int i = 0; i < NUMBER_OF_THREAD; i++) {
             threads[i] = new Thread(() -> TestHttpUtils.send("/index.html"));
+        }
+
+        for (final var thread : threads) {
+            thread.start();
+            Thread.sleep(10);
+        }
+
+        for (final var thread : threads) {
+            thread.join();
+        }
+    }
+
+    @Test
+    void postLogin() throws Exception {
+        final var NUMBER_OF_THREAD = 10;
+        var threads = new Thread[NUMBER_OF_THREAD];
+        final UserRegisterRequest user = new UserRegisterRequest("dongho108",
+                "1234",
+                "dongho108@naver.com");
+
+        for (int i = 0; i < NUMBER_OF_THREAD; i++) {
+            threads[i] = new Thread(() -> TestHttpUtils.postRegister(user));
         }
 
         for (final var thread : threads) {
--- tomcat/src/test/java/nextstep/app/TestHttpUtils.java
+++ tomcat/src/test/java/nextstep/app/TestHttpUtils.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import nextstep.jwp.dto.UserRegisterRequest;
 
 public class TestHttpUtils {
 
@@ -17,7 +19,23 @@ public class TestHttpUtils {
     public static HttpResponse<String> send(final String path) {
         final var request = HttpRequest.newBuilder()
                 .uri(URI.create("http://localhost:8080" + path))
-                .timeout(Duration.ofSeconds(1))
+                .build();
+
+        try {
+            return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static HttpResponse<String> postRegister(final UserRegisterRequest userRegisterRequest) {
+        final String body = String.join("&",
+                "account=" + userRegisterRequest.getAccount(),
+                "email=" + userRegisterRequest.getEmail(),
+                "password=" + userRegisterRequest.getPassword());
+        final var request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080" + "/register"))
+                .POST(BodyPublishers.ofString(body))
                 .build();
 
         try {

25dd875d03604791c3b02af021c78d1f46882dce
shindong96 <2006peter@naver.com>
2022-09-08 15:07:24 +0900

    feat: ThreadPool 이용해서 동시성 문제 해결

--- tomcat/src/main/java/org/apache/coyote/http11/authorization/SessionManager.java
+++ tomcat/src/main/java/org/apache/coyote/http11/authorization/SessionManager.java
@@ -1,13 +1,17 @@
 package org.apache.coyote.http11.authorization;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class SessionManager {
 
     public static final SessionManager SESSION_MANAGER = new SessionManager();
-    private static final Map<String, Session> SESSIONS = new HashMap<>();
+
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
+
+    private SessionManager() {
+    }
 
     public void add(final Session session) {
         SESSIONS.put(session.getId(), session);
@@ -19,8 +23,5 @@ public class SessionManager {
 
     public void remove(final String id) {
         SESSIONS.remove(id);
-    }
-
-    private SessionManager() {
     }
 }

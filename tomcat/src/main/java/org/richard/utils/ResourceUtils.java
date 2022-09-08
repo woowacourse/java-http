8c6e320e3ae5c3d0e37ec4c27327cda3ebc62409
Richard JEON <ztzy1907@gmail.com>
2022-09-08 13:31:13 +0900

    feat: ResourceUtils 구현

---
+++ tomcat/src/main/java/org/richard/utils/ResourceUtils.java
@@ -0,0 +1,35 @@
+package org.richard.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.Objects;
+
+public class ResourceUtils {
+
+    private ResourceUtils() {
+    }
+
+    public static String createResourceAsString(final String resourcePath) {
+        final var resource = findResource(resourcePath);
+        final var path = new File(resource.getFile()).toPath();
+
+        try {
+            return new String(Files.readAllBytes(path));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static URL findResource(final String resourcePath) {
+        final var classLoader = ResourceUtils.class.getClassLoader();
+        final var resource = classLoader.getResource(resourcePath);
+
+        if (Objects.nonNull(resource)) {
+            return resource;
+        }
+
+        throw new IllegalArgumentException("resource does not exists");
+    }
+}

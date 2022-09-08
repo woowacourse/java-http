33cd931b4c71e3b567a744287fd1eb040092b0dc
injoon2019 <ison@sfu.ca>
2022-09-08 15:53:01 +0900

    feat: 4단계 구현

--- tomcat/src/main/java/org/apache/coyote/http11/session/SessionManager.java
+++ tomcat/src/main/java/org/apache/coyote/http11/session/SessionManager.java
@@ -1,12 +1,12 @@
 package org.apache.coyote.http11.session;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import org.apache.coyote.http11.request.HttpRequest;
 
 public class SessionManager implements Manager {
 
-    private static final Map<String, Session> SESSIONS = new HashMap<>();
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
 
     public SessionManager() {
     }

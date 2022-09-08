c3f57f630eb12c743e8e5995db24d89c3190784b
dwl21 <nggus5@naver.com>
2022-09-08 18:22:54 +0900

    feat: 스레드 관리 executorService 사용

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -71,7 +71,7 @@ public class Connector implements Runnable {
         }
         log.info("connect host: {}, port: {}", connection.getInetAddress(), connection.getPort());
         var processor = new Http11Processor(connection);
-        new Thread(processor).start();
+        executorService.submit(processor);
     }
 
     public void stop() {

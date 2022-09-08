598495e9cee895ce3897ff18c6f38c92a7a5a66c
asebn1 <asebn121@gmail.com>
2022-09-08 09:09:13 +0900

    refactor: 동시성 컬렉션 사용하도록 변경

--- tomcat/src/main/java/org/apache/coyote/model/session/SessionManager.java
+++ tomcat/src/main/java/org/apache/coyote/model/session/SessionManager.java
@@ -3,10 +3,11 @@ package org.apache.coyote.model.session;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class SessionManager {
 
-    private static final Map<String, Session> sessions = new HashMap<>();
+    private static final Map<String, Session> sessions = new ConcurrentHashMap<>();
 
     public static Optional<Session> findSession(final String key) {
         return Optional.ofNullable(sessions.getOrDefault(key, null));

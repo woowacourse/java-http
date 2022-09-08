d999212c9b68300ab4834269ec312190ed5f72ab
dwl21 <nggus5@naver.com>
2022-09-08 17:46:03 +0900

    feat: SessionManager를 static 하게 변경

--- tomcat/src/main/java/org/apache/session/SessionManager.java
+++ tomcat/src/main/java/org/apache/session/SessionManager.java
@@ -9,27 +9,30 @@ public class SessionManager {
 
     private static final Map<String, Session> SESSIONS = new HashMap<>();
 
-    public void add(Session session) {
+    private SessionManager() {
+    }
+
+    public static void add(Session session) {
         SESSIONS.put(session.getId(), session);
     }
 
-    public Optional<Session> find(String id) {
+    public static Optional<Session> find(String id) {
         if (SESSIONS.containsKey(id)) {
             return Optional.of(SESSIONS.get(id));
         }
         return Optional.empty();
     }
 
-    public void remove(String id) {
+    public static void remove(String id) {
         SESSIONS.remove(id);
     }
 
-    public Session generateNewSession() {
+    public static Session generateNewSession() {
         UUID uuid = UUID.randomUUID();
         return new Session(uuid.toString());
     }
 
-    public boolean contains(String id) {
+    public static boolean contains(String id) {
         return SESSIONS.containsKey(id);
     }
 }

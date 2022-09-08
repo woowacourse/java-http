5a6d4beace45ef4c2d2bc35c5eebcc693ae97d9a
skullKim <kyk990328@gmail.com>
2022-09-08 18:04:33 +0900

    chore: 코딩 컨벤션에 맞추어 코딩 스타일 수정

--- tomcat/src/main/java/org/apache/coyote/common/session/Session.java
+++ tomcat/src/main/java/org/apache/coyote/common/session/Session.java
@@ -5,7 +5,6 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class Session {
 
--- tomcat/src/main/java/org/apache/coyote/common/session/SessionManager.java
+++ tomcat/src/main/java/org/apache/coyote/common/session/SessionManager.java
@@ -1,6 +1,5 @@
 package org.apache.coyote.common.session;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;

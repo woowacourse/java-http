81632769de342f193112c5688b235246659f9de6
beomWhale <cndqjacndqja@gmail.com>
2022-09-08 16:42:24 +0900

    refactor: AbstractController service 추상 메서드로 수정

--- tomcat/src/main/java/nextstep/jwp/controller/AbstractController.java
+++ tomcat/src/main/java/nextstep/jwp/controller/AbstractController.java
@@ -1,18 +1,12 @@
 package nextstep.jwp.controller;
 
-import nextstep.jwp.controller.support.FileReader;
 import org.apache.coyote.http11.model.request.HttpRequest;
-import org.apache.coyote.http11.model.response.ContentType;
 import org.apache.coyote.http11.model.response.HttpResponse;
-import org.apache.coyote.http11.model.response.ResponseStatusCode;
 
 public abstract class AbstractController implements Controller{
 
     @Override
-    public HttpResponse service(final HttpRequest httpRequest) {
-        String body = FileReader.getFile("/404.html", getClass());
-        return HttpResponse.of(ResponseStatusCode.NOT_FOUND, httpRequest.getVersion(), ContentType.HTML, body);
-    }
+    public abstract HttpResponse service(final HttpRequest httpRequest);
 
     protected abstract HttpResponse doGet(final HttpRequest httpRequest);
 

53dece28fc3dce8473d7d1718c4700fb790b4c96
Wishoon <heonga2@gmail.com>
2022-09-08 16:29:33 +0900

    feat: Tomcat의 기본 값으로 변경

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -17,7 +17,7 @@ public class Connector implements Runnable {
 
     private static final int DEFAULT_PORT = 8080;
     private static final int DEFAULT_ACCEPT_COUNT = 100;
-    private static final int MAX_THREADS = 100;
+    private static final int MAX_THREADS = 200;
 
     private final ServerSocket serverSocket;
     private boolean stopped;

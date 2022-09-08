d3873976b82bf66cef71b9d096d2a59ee040df98
pup-paw <wldusdl0310@gmail.com>
2022-09-08 17:35:22 +0900

    style: 클래스 블럭 시작 전 빈칸 추가

--- tomcat/src/main/java/nextstep/jwp/exception/NotFoundUserException.java
+++ tomcat/src/main/java/nextstep/jwp/exception/NotFoundUserException.java
@@ -1,6 +1,6 @@
 package nextstep.jwp.exception;
 
-public class NotFoundUserException extends NotFoundException{
+public class NotFoundUserException extends NotFoundException {
 
     public NotFoundUserException() {
         super("존재하지 않는 유저입니다.");

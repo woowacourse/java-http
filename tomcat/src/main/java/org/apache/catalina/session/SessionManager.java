ecd80489b8f695e2066d4dcf4e4bb660e59ddd62
hyunrrr <whgusrms96@gmail.com>
2022-09-08 13:20:47 +0900

    refactor: manager 인터페이스 삭제 및 sessionManager 불필요 메서드 제거

--- tomcat/src/main/java/org/apache/catalina/session/SessionManager.java
+++ tomcat/src/main/java/org/apache/catalina/session/SessionManager.java
@@ -3,10 +3,8 @@ package org.apache.catalina.session;
 import java.util.HashMap;
 import java.util.Map;
 import nextstep.jwp.model.user.User;
-import org.apache.catalina.Manager;
-import org.apache.catalina.session.exception.InvalidSessionIdException;
 
-public class SessionManager implements Manager {
+public class SessionManager {
 
     private static final Map<String, Session> SESSIONS = new HashMap<>();
     private static final SessionManager sessionManager = new SessionManager();
@@ -18,34 +16,19 @@ public class SessionManager implements Manager {
         return sessionManager;
     }
 
-    @Override
-    public void add(Session session) {
-        SESSIONS.put(session.getId(), session);
-    }
-
-    @Override
-    public Session findSession(String id) throws InvalidSessionIdException {
-        return SESSIONS.get(id);
-    }
-
-    @Override
-    public void remove(Session session) {
-        SESSIONS.remove(session.getId());
-    }
-
     public String createSession() {
         Session session = new Session();
-        add(session);
+        SESSIONS.put(session.getId(), session);
         return session.getId();
     }
 
     public void addUserInSession(String sessionId, User user) {
-        Session session = findSession(sessionId);
+        Session session = SESSIONS.get(sessionId);
         session.setAttribute(USER_KEY, user);
     }
 
     public boolean checkLogin(String sessionId) {
-        Session session = findSession(sessionId);
+        Session session = SESSIONS.get(sessionId);
         return session.hasAttribute(USER_KEY);
     }
 }

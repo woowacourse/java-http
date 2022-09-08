f67b856156f5d63f37b2f9670745e1599ec95a7d
Richard JEON <ztzy1907@gmail.com>
2022-09-08 16:35:15 +0900

    refactor: NextstepController 에 루트 경로만 남겨두고 이동

--- tomcat/src/main/java/nextstep/jwp/view/NextstepController.java
+++ tomcat/src/main/java/nextstep/jwp/view/NextstepController.java
@@ -1,10 +1,5 @@
 package nextstep.jwp.view;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Objects;
-import nextstep.jwp.db.InMemoryUserRepository;
 import org.apache.http.BasicHttpResponse;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
@@ -12,15 +7,12 @@ import org.apache.http.info.ContentType;
 import org.apache.http.info.HttpMethod;
 import org.apache.http.info.HttpVersion;
 import org.apache.http.info.StatusCode;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.annotation.Controller;
 import org.springframework.annotation.RequestMapping;
 
 @Controller
 public class NextstepController {
 
-    private static final Logger log = LoggerFactory.getLogger(NextstepController.class);
     private static final String WELCOME_MESSAGE = "Hello world!";
 
     @RequestMapping(method = HttpMethod.GET, uri = "/")
@@ -31,36 +23,5 @@ public class NextstepController {
                 .contentType(ContentType.TEXT_HTML.getName())
                 .body(WELCOME_MESSAGE)
                 .build();
-    }
-
-    @RequestMapping(method = HttpMethod.GET, uri = "/login")
-    public HttpResponse login(final HttpRequest httpRequest) {
-        final var loginHtml = String.format("%s.html", httpRequest.getRequestURIWithoutQueryParams());
-        final var resource = getClass().getClassLoader().getResource(String.format("static%s", loginHtml));
-        final var path = new File(resource.getFile()).toPath();
-
-        logAccountInfo(httpRequest);
-
-        try {
-            return BasicHttpResponse.builder()
-                    .httpVersion(HttpVersion.HTTP_1_1)
-                    .statusCode(StatusCode.OK_200)
-                    .contentType(ContentType.TEXT_HTML.getName())
-                    .body(new String(Files.readAllBytes(path)))
-                    .build();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void logAccountInfo(final HttpRequest httpRequest) {
-        var account = (String) httpRequest.getParameter("account");
-        var password = (String) httpRequest.getParameter("password");
-        if (Objects.isNull(account)) {
-            account = "";
-        }
-
-        InMemoryUserRepository.findByAccountAndPassword(account, password)
-                .ifPresent(member -> log.info("found account info : {}", member));
     }
 }

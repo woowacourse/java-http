af80fe45287d5fa38bc7b239a819fcb633c5837b
dwl21 <nggus5@naver.com>
2022-09-08 17:57:24 +0900

    feat: RequestMapping 컨트롤러 등록 및 정적 파일 핸들 기능 구현

--- tomcat/src/main/java/nextstep/jwp/controller/RequestMapping.java
+++ tomcat/src/main/java/nextstep/jwp/controller/RequestMapping.java
@@ -7,14 +7,23 @@ import org.apache.controller.Controller;
 
 public class RequestMapping {
 
+    public static final String STATIC_FILE_CONTROLLER_KEY = "staticFileController";
+
     private static final Map<String, Controller> controllers = new HashMap<>();
 
     static {
         controllers.put("/", new HelloWorldController());
+        controllers.put("/register", new RegisterController());
+        controllers.put("/login", new LoginController());
+        controllers.put(STATIC_FILE_CONTROLLER_KEY, new StaticFileController());
     }
 
     public static Controller get(HttpRequest httpRequest) {
         String uri = httpRequest.getUri();
-        return controllers.get(uri);
+        final Controller controller = controllers.get(uri);
+        if (controller == null) {
+            return controllers.get(STATIC_FILE_CONTROLLER_KEY);
+        }
+        return controller;
     }
 }

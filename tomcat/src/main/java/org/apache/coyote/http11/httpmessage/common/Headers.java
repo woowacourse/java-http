dcce40da36575f0c3b814321a370eabfa4337c1d
Soomin Sohn <soomin.sohn.dev@gmail.com>
2022-09-08 14:49:05 +0900

    refactor: Headers 클래스 매직넘버 삭제

--- tomcat/src/main/java/org/apache/coyote/http11/httpmessage/common/Headers.java
+++ tomcat/src/main/java/org/apache/coyote/http11/httpmessage/common/Headers.java
@@ -9,38 +9,46 @@ import java.util.stream.Collectors;
 
 public class Headers {
 
+    private final static int HEADER_NAME = 0;
+    private final static int HEADER_VALUE = 0;
+
+    private final static String HEADER_SPLITTER = ": ";
+    private final static String BLANK = " ";
+    private final static String DEFAULT_CONTENT_LENGTH = "0";
+
+
     final Map<String, String> headers;
 
     public Headers() {
         this.headers = new LinkedHashMap<>();
     }
 
-    public Headers(Map<String, String> headers) {
+    public Headers(final Map<String, String> headers) {
         this.headers = headers;
     }
 
-    public Headers(BufferedReader bufferedReader) throws IOException {
+    public Headers(final BufferedReader bufferedReader) throws IOException {
         headers = new HashMap<>();
         while (true) {
-            String buffer = bufferedReader.readLine();
-            if (buffer.equals("\r\n") || "".equals(buffer)) {
+            String headerLine = bufferedReader.readLine();
+            if (headerLine.equals("\r\n") || "".equals(headerLine)) {
                 break;
             }
-            String key = buffer.split(": ")[0];
-            String value = buffer.split(": ")[1];
+            String key = headerLine.split(HEADER_SPLITTER)[HEADER_NAME];
+            String value = headerLine.split(HEADER_SPLITTER)[HEADER_VALUE];
             headers.put(key, value);
         }
     }
 
     public Headers add(final String fieldName, final String fieldValue) {
-        Map<String, String> newHeaders = new LinkedHashMap<>(headers);
+        final Map<String, String> newHeaders = new LinkedHashMap<>(headers);
         newHeaders.put(fieldName, fieldValue);
 
         return new Headers(newHeaders);
     }
 
     public int getContentLength() {
-        return Integer.parseInt(headers.getOrDefault("Content-Length", "0"));
+        return Integer.parseInt(headers.getOrDefault("Content-Length", DEFAULT_CONTENT_LENGTH));
     }
 
     public boolean exist(final String headerName) {
@@ -54,7 +62,7 @@ public class Headers {
     public String parseToString() {
         return headers.entrySet()
                 .stream()
-                .map(it -> it.getKey() + ": " + it.getValue() + " ")
+                .map(it -> it.getKey() + HEADER_SPLITTER + it.getValue() + BLANK)
                 .collect(Collectors.joining("\r\n"));
     }
 }

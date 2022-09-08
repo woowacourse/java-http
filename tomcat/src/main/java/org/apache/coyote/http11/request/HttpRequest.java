8313d940ad95ad1cb18230e34e59e947e4434601
이한울 <dkfghg3@naver.com>
2022-09-09 03:16:20 +0900

    refactor: request 필드값 변경

--- tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequest.java
@@ -1,12 +1,42 @@
 package org.apache.coyote.http11.request;
 
+import org.apache.coyote.http11.ContentType;
+
 public class HttpRequest {
 
-    private final HttpRequestLine httpRequestLine;
+    private final HttpMethod httpMethod;
+    private final HttpPath httpPath;
+    private final HttpVersion httpVersion;
     private final HttpHeaders httpHeaders;
 
     public HttpRequest(final HttpRequestLine httpRequestLine, final HttpHeaders httpHeaders) {
-        this.httpRequestLine = httpRequestLine;
+        this.httpMethod = httpRequestLine.getMethod();
+        this.httpPath = httpRequestLine.getPath();
+        this.httpVersion = httpRequestLine.getVersion();
         this.httpHeaders = httpHeaders;
+    }
+
+    public boolean isLoginRequest() {
+        return httpPath.isLoginRequest();
+    }
+
+    public boolean isDefaultRequest() {
+        return httpPath.isDefaultRequest();
+    }
+
+    public String getParam(final String name) {
+        return httpPath.getParam(name);
+    }
+
+    public ContentType getContentType() {
+        return ContentType.from(httpPath.getPath());
+    }
+
+    public HttpPath getHttpPath() {
+        return httpPath;
+    }
+
+    public HttpVersion getHttpVersion() {
+        return httpVersion;
     }
 }

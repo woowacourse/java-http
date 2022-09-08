6d0c58360df43ae89da1716f239df98402adc75c
jayjaehunchoi <wogns0108@naver.com>
2022-09-08 21:50:03 +0900

    refactor: submit -> execute로 수정

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -70,7 +70,7 @@ public class Connector implements Runnable {
         log.info("connect host: {}, port: {}", connection.getInetAddress(), connection.getPort());
 
         var processor = new Http11Processor(connection);
-        executorService.submit(processor);
+        executorService.execute(processor);
     }
 
     public void stop() {

4037d838df1f063ecb9165a907b1ed1666ee284f
leokim <kunga077@gmail.com>
2022-09-08 16:30:33 +0900

    refactor: 서버 구동 시 Request Mapping 초기화

--- tomcat/src/main/java/nextstep/Application.java
+++ tomcat/src/main/java/nextstep/Application.java
@@ -1,5 +1,13 @@
 package nextstep;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import nextstep.jwp.RequestMapping;
+import nextstep.jwp.ui.Controller;
+import nextstep.jwp.ui.HomeController;
+import nextstep.jwp.ui.LoginController;
+import nextstep.jwp.ui.RegisterController;
 import org.apache.catalina.startup.Tomcat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +18,16 @@ public class Application {
 
     public static void main(String[] args) {
         log.info("web server start.");
+        registerRequestMapper();
         final var tomcat = new Tomcat();
         tomcat.start();
+    }
+
+    private static void registerRequestMapper() {
+        Map<String, Controller> requestMapper = new HashMap<>();
+        requestMapper.put("/", new HomeController());
+        requestMapper.put("/login", new LoginController());
+        requestMapper.put("/register", new RegisterController());
+        RequestMapping.registerController(requestMapper);
     }
 }

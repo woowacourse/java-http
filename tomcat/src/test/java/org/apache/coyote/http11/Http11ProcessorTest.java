834b5ab64b3771edc924d9091124e9db52cbf9f5
Hyunta <mohai2618@gmail.com>
2022-09-08 19:16:05 +0900

    refactor: 불필요한 메서드 삭제

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -1,4 +1,4 @@
-package nextstep.org.apache.coyote.http11;
+package org.apache.coyote.http11;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
-import org.apache.coyote.http11.Http11Processor;
 import org.junit.jupiter.api.Test;
 import support.StubSocket;
 

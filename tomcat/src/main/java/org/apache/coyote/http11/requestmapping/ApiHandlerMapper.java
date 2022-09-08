8fae96ee86d9b7452bdc0bd4fa525dd650b9390f
seong-wooo <seongwoo0513@uos.ac.kr>
2022-09-08 12:35:44 +0900

    feat: /login으로 접속 시 login.html을 반환하는 기능 추가

--- tomcat/src/main/java/org/apache/coyote/http11/requestmapping/ApiHandlerMapper.java
+++ tomcat/src/main/java/org/apache/coyote/http11/requestmapping/ApiHandlerMapper.java
@@ -2,6 +2,7 @@ package org.apache.coyote.http11.requestmapping;
 
 import java.util.List;
 import org.apache.coyote.http11.controller.apicontroller.LoginController;
+import org.apache.coyote.http11.controller.apicontroller.LoginPageApiController;
 import org.apache.coyote.http11.controller.apicontroller.RegisterApiController;
 import org.apache.coyote.http11.controller.apicontroller.RegisterPageApiController;
 import org.apache.coyote.http11.controller.apicontroller.RootApiHandler;
@@ -11,7 +12,7 @@ import org.apache.coyote.http11.httpmessage.request.HttpRequest;
 public class ApiHandlerMapper implements RequestMapper {
 
     private static final List<Controller> API_HANDLERS = List.of(
-            new RootApiHandler(), new LoginController(),
+            new RootApiHandler(), new LoginController(), new LoginPageApiController(),
             new RegisterPageApiController(), new RegisterApiController()
     );
 

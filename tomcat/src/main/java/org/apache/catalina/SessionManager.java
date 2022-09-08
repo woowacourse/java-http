5fedd42303a5e68e88876bebfc6db68a8b954e70
Chris <bjuuuu98@gmail.com>
2022-09-08 11:27:29 +0900

    feat: 2. 동시성 컬렉션 사용하기 구현

--- tomcat/src/main/java/org/apache/catalina/SessionManager.java
+++ tomcat/src/main/java/org/apache/catalina/SessionManager.java
@@ -1,11 +1,11 @@
 package org.apache.catalina;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class SessionManager implements Manager {
 
-    private final Map<String, Session> sessions = new HashMap<>();
+    private final Map<String, Session> sessions = new ConcurrentHashMap<>();
     private static final SessionManager INSTANCE = new SessionManager();
 
     public static SessionManager getInstance() {

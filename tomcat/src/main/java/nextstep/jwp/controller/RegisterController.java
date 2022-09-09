373a0f7b8db2a395f81dbc9e9f27bb34c57ebe21
bomi <bonita9931@gmail.com>
2022-09-10 00:36:15 +0900

    refactor: 회원가입 기능 RegisterController 구현

--- tomcat/src/main/java/nextstep/jwp/controller/RegisterController.java
+++ tomcat/src/main/java/nextstep/jwp/controller/RegisterController.java
@@ -1,5 +1,30 @@
 package nextstep.jwp.controller;
 
+import nextstep.jwp.handler.RegisterHandler;
+import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.response.Headers;
+import org.apache.coyote.http11.response.HttpResponse;
+import org.apache.coyote.http11.response.HttpResponse.ResponseBuilder;
+import org.apache.coyote.http11.response.Status;
+
 public class RegisterController extends AbstractController {
-    
+    @Override
+    protected HttpResponse doGet(final HttpRequest request, final HttpResponse response) throws Exception {
+        String url = request.getUrl() + ".html";
+        final String body = readResourceBody(url);
+        final Headers headers = readResourceHeader(url, body);
+
+        return new ResponseBuilder().status(Status.OK)
+                .headers(headers)
+                .body(body)
+                .build();
+    }
+
+    @Override
+    protected HttpResponse doPost(final HttpRequest request, final HttpResponse response) throws Exception {
+        if (RegisterHandler.handle(request)) {
+            return response.redirect("/index.html");
+        }
+        return response.redirect("/401.html");
+    }
 }
--- tomcat/src/main/java/nextstep/jwp/handler/RegisterHandler.java
+++ tomcat/src/main/java/nextstep/jwp/handler/RegisterHandler.java
@@ -1,9 +1,12 @@
 package nextstep.jwp.handler;
 
+import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
 import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.model.User;
 import org.apache.coyote.http11.Http11Processor;
+import org.apache.coyote.http11.request.HttpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +14,8 @@ public class RegisterHandler {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
 
-    public static boolean handle(Map<String, String> body) {
+    public static boolean handle(HttpRequest request) {
+        Map<String, String> body = parse(request.getRequestBody());
         String account = body.get("account");
         if (InMemoryUserRepository.findByAccount(account).isPresent()) {
             return false;
@@ -20,5 +24,11 @@ public class RegisterHandler {
         InMemoryUserRepository.save(user);
 
         return true;
+    }
+
+    private static Map<String, String> parse(final String requestBody) {
+        return Arrays.stream(requestBody.split("&"))
+                .map(data -> data.split("="))
+                .collect(Collectors.toMap(data -> data[0], data -> data[1]));
     }
 }

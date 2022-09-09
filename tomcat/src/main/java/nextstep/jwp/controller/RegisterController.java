887d0a913ff798b288cd07ac5672151b22a93dcd
yeon-06 <rnjstldus2@gmail.com>
2022-09-09 22:51:36 +0900

    feat: 회원가입 시 계정 중복 검사 로직 추가

--- tomcat/src/main/java/nextstep/jwp/controller/RegisterController.java
+++ tomcat/src/main/java/nextstep/jwp/controller/RegisterController.java
@@ -1,6 +1,7 @@
 package nextstep.jwp.controller;
 
 import nextstep.jwp.db.InMemoryUserRepository;
+import nextstep.jwp.exception.DuplicatedUserException;
 import nextstep.jwp.model.User;
 import org.apache.coyote.http11.enums.HttpStatusCode;
 import org.apache.coyote.http11.request.HttpRequest;
@@ -27,9 +28,17 @@ public class RegisterController extends AbstractController {
     protected HttpResponse doPost(final HttpRequest httpRequest) {
         final HttpRequestBody requestBody = httpRequest.getBody();
         final User user = createUser(requestBody);
+
+        validateDuplicateUser(user);
         InMemoryUserRepository.save(user);
 
         return generateSuccessResponse();
+    }
+
+    private void validateDuplicateUser(final User user) {
+        final String account = user.getAccount();
+        InMemoryUserRepository.findByAccount(account)
+                .orElseThrow(() -> new DuplicatedUserException(account));
     }
 
     private User createUser(final HttpRequestBody requestBody) {

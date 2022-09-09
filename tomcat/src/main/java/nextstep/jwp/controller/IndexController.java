89295a27c1dda1979e255b760d6838072b0f2af8
bomi <bonita9931@gmail.com>
2022-09-10 00:49:34 +0900

    refactor: indexController 구현

--- tomcat/src/main/java/nextstep/jwp/controller/IndexController.java
+++ tomcat/src/main/java/nextstep/jwp/controller/IndexController.java
@@ -1,5 +1,23 @@
 package nextstep.jwp.controller;
 
+import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.response.Headers;
+import org.apache.coyote.http11.response.HttpResponse;
+import org.apache.coyote.http11.response.HttpResponse.ResponseBuilder;
+import org.apache.coyote.http11.response.Status;
+
 public class IndexController extends AbstractController {
-    
+
+    private static final String INDEX_HTML = "/index.html";
+
+    @Override
+    protected HttpResponse doGet(final HttpRequest request, final HttpResponse response) throws Exception {
+        final String body = readResourceBody(INDEX_HTML);
+        final Headers headers = readResourceHeader(INDEX_HTML, body);
+
+        return new ResponseBuilder().status(Status.OK)
+                .headers(headers)
+                .body(body)
+                .build();
+    }
 }

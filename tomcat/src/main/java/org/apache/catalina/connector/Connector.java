c92e9fa4e8ccdaef6a956d59c8585c2eabc1286b
yxxnghwan <younghwan960@gmail.com>
2022-09-11 03:40:49 +0900

    feat: 스레드 풀 구현 및 설정값을 받는 메서드 추가

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import org.apache.coyote.http11.Http11Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,18 +17,33 @@ public class Connector implements Runnable {
     private static final Logger log = LoggerFactory.getLogger(Connector.class);
 
     private static final int DEFAULT_PORT = 8080;
-    private static final int DEFAULT_ACCEPT_COUNT = 100;
+    private static final int DEFAULT_BACKLOG_COUNT = 100;
+    private static final int DEFAULT_POOL_SIZE = 250;
+    private static final int DEFAULT_CORE_SIZE = 50;
+    private static final long DEFAULT_KEEP_ALIVE_TIME = 120L;
 
     private final ServerSocket serverSocket;
     private boolean stopped;
+    private final ExecutorService executorService;
 
     public Connector() {
-        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT);
+        this(DEFAULT_PORT, DEFAULT_BACKLOG_COUNT, DEFAULT_POOL_SIZE, DEFAULT_CORE_SIZE, DEFAULT_KEEP_ALIVE_TIME);
     }
 
-    public Connector(final int port, final int acceptCount) {
-        this.serverSocket = createServerSocket(port, acceptCount);
+    public Connector(final int port,
+                     final int backlogCount,
+                     final int maxThreadSize,
+                     final int coreThreadSize,
+                     final long keepAliveTimeSecond) {
+        this.serverSocket = createServerSocket(port, backlogCount);
         this.stopped = false;
+        this.executorService = new ThreadPoolExecutor(
+                maxThreadSize,
+                coreThreadSize,
+                keepAliveTimeSecond,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>()
+        );
     }
 
     private ServerSocket createServerSocket(final int port, final int acceptCount) {
@@ -65,7 +84,7 @@ public class Connector implements Runnable {
         }
         log.info("connect host: {}, port: {}", connection.getInetAddress(), connection.getPort());
         var processor = new Http11Processor(connection);
-        new Thread(processor).start();
+        executorService.submit(processor);
     }
 
     public void stop() {
@@ -88,6 +107,6 @@ public class Connector implements Runnable {
     }
 
     private int checkAcceptCount(final int acceptCount) {
-        return Math.max(acceptCount, DEFAULT_ACCEPT_COUNT);
+        return Math.max(acceptCount, DEFAULT_BACKLOG_COUNT);
     }
 }
--- tomcat/src/main/java/org/apache/catalina/startup/Tomcat.java
+++ tomcat/src/main/java/org/apache/catalina/startup/Tomcat.java
@@ -23,4 +23,23 @@ public class Tomcat {
             connector.stop();
         }
     }
+
+    public void start(final int port,
+                      final int backlogCount,
+                      final int maxThreadSize,
+                      final int coreThreadSize,
+                      final long keepAliveTimeSecond) {
+        var connector = new Connector(port, backlogCount, maxThreadSize, coreThreadSize, keepAliveTimeSecond);
+        connector.start();
+
+        try {
+            // make the application wait until we press any key.
+            System.in.read();
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            log.info("web server stop.");
+            connector.stop();
+        }
+    }
 }

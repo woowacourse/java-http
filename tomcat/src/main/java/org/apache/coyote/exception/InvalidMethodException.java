9e5eea4d919c0e6031bd1944b4d262ca70e5c617
sure-why-not <pkse1213@gmail.com>
2022-09-10 14:07:49 +0900

    refactor: Exception 메세지 추가

--- tomcat/src/main/java/org/apache/coyote/exception/InvalidMethodException.java
+++ tomcat/src/main/java/org/apache/coyote/exception/InvalidMethodException.java
@@ -2,7 +2,9 @@ package org.apache.coyote.exception;
 
 public class InvalidMethodException extends RuntimeException {
 
+    private static final String MESSAGE = "유효한 Http Method 가 아닙니다.";
+
     public InvalidMethodException() {
-        super();
+        super(MESSAGE);
     }
 }

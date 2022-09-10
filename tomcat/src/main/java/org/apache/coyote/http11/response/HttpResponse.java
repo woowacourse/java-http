6d51e96af6ccb061a9506e25d9eb95036937bd29
이한울 <dkfghg3@naver.com>
2022-09-10 14:34:38 +0900

    refactor: 불필요한 response 공백 제거

--- tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponse.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponse.java
@@ -30,7 +30,6 @@ public class HttpResponse {
                 getContentType(),
                 getContentLength(),
                 getLocation(),
-                "",
                 responseBody);
     }
 

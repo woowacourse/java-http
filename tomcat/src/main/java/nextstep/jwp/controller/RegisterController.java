9c7970879242853c3ff518ac7ef2eaa375d48af6
koo <gudonghee2000@gmail.com>
2022-09-10 21:19:38 +0900

    test: 실패하는 테스트 코드 추가 수정

--- tomcat/src/main/java/nextstep/jwp/controller/RegisterController.java
+++ tomcat/src/main/java/nextstep/jwp/controller/RegisterController.java
@@ -8,15 +8,13 @@ import nextstep.jwp.exception.InvalidUserException;
 import nextstep.jwp.http.reqeust.HttpRequest;
 import nextstep.jwp.http.reqeust.QueryParams;
 import nextstep.jwp.http.response.HttpResponse;
-import nextstep.jwp.http.response.StatusCode;
 import nextstep.jwp.model.User;
 
 public class RegisterController extends AbstractController {
 
     @Override
-    protected void doPost(final HttpRequest request, final HttpResponse response) throws Exception {
-        super.doPost(request, response);
-        response.setStatusCode(StatusCode.CREATE);
+    protected void doPost(final HttpRequest request, final HttpResponse response) {
+        response.sendRedirect(INDEX_PAGE_URL);
 
         registerUser(request);
     }
--- tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -218,7 +218,7 @@ class Http11ProcessorTest {
         final URL resource = getClass().getClassLoader().getResource("static/register.html");
         final String responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
 
-        var expected = "HTTP/1.1 201 CREATE \r\n" +
+        var expected = "HTTP/1.1 302 FOUND \r\n" +
                 "Content-Type: text/html;charset=utf-8 \r\n" +
                 "Location: ./index.html \r\n" +
                 "\r\n";

3ce860004b2d48b39ed13bac44aef6552b65be19
Juhyung990122 <harukawa99@naver.com>
2022-09-11 05:17:55 +0900

    refactor: 세션 삭제 스레드 세팅 위치 변경

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -23,7 +23,6 @@ public class Connector implements Runnable {
     private static final int DEFAULT_PORT = 8080;
     private static final int DEFAULT_ACCEPT_COUNT = 350;
     private static final int DEFAULT_THREAD_POOL_SIZE = 250;
-    private static final int SESSION_EXPIRED_TIME = 30;
 
     private final ServerSocket serverSocket;
     private boolean stopped;
@@ -48,17 +47,6 @@ public class Connector implements Runnable {
                 timeUnit,
                 new LinkedBlockingQueue<>()
         );
-        settingRemoveSessionThread();
-    }
-
-    private void settingRemoveSessionThread() {
-        ScheduledExecutorService sessionRemover = Executors.newScheduledThreadPool(1);
-        Runnable runnable = () -> {
-            for (final Session session : SessionManager.findExpiredSession()) {
-                SessionManager.remove(session);
-            }
-        };
-        sessionRemover.scheduleAtFixedRate(runnable, 1, SESSION_EXPIRED_TIME, TimeUnit.MINUTES);
     }
 
     private ServerSocket createServerSocket(final int port, final int acceptCount) {
--- tomcat/src/main/java/org/apache/coyote/http/SessionManager.java
+++ tomcat/src/main/java/org/apache/coyote/http/SessionManager.java
@@ -7,10 +7,28 @@ import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class SessionManager {
 
+    private static final int SESSION_EXPIRED_TIME = 30;
     private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
+
+    public SessionManager(){
+        settingRemoveSessionThread();
+    }
+
+    private void settingRemoveSessionThread() {
+        ScheduledExecutorService sessionRemover = Executors.newScheduledThreadPool(1);
+        Runnable runnable = () -> {
+            for (final Session session : findExpiredSession()) {
+                remove(session);
+            }
+        };
+        sessionRemover.scheduleAtFixedRate(runnable, 1, SESSION_EXPIRED_TIME, TimeUnit.MINUTES);
+    }
 
     public static Optional<Session> add() {
         final Session session = new Session(UUID.randomUUID().toString());
@@ -34,8 +52,5 @@ public class SessionManager {
 
     public static void remove(Session session) {
         SESSIONS.remove(session);
-    }
-
-    public SessionManager() {
     }
 }

8cf76ce60aa3c0af306f10f2d2a997df1614bc49
yhh1056 <yhh1056@naver.com>
2022-09-11 23:43:06 +0900

    fix: 쓰레드 실행하도록 수정

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -70,14 +70,14 @@ public class Connector implements Runnable {
         }
         log.info("connect host: {}, port: {}", connection.getInetAddress(), connection.getPort());
         var processor = new Http11Processor(connection);
-        new Thread(processor).start();
+        executorService.submit(processor);
     }
 
     public void stop() {
         stopped = true;
         try {
-            serverSocket.close();
             executorService.shutdown();
+            serverSocket.close();
         } catch (IOException e) {
             log.error(e.getMessage(), e);
         }

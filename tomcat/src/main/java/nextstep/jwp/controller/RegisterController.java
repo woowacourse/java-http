be4519913e1ec9376afa88313226ad70e890181a
sinb57 <sinb57@naver.com>
2022-09-12 01:21:47 +0900

    feat: RegisterController 세션이 존재하면 index로 리다이렉트

--- tomcat/src/main/java/nextstep/jwp/controller/RegisterController.java
+++ tomcat/src/main/java/nextstep/jwp/controller/RegisterController.java
@@ -1,5 +1,7 @@
 package nextstep.jwp.controller;
 
+import java.util.function.Function;
+
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.request.Params;
 import org.apache.coyote.http11.response.HttpResponse;
@@ -18,23 +20,33 @@ public class RegisterController extends AbstractController {
 
     @Override
     protected HttpResponse doPost(final HttpRequest request) {
-        final Params params = request.getParamsFromBody();
+        return ifSessionNotExist(request, params -> {
+            try {
+                final String account = params.find("account");
+                final String password = params.find("password");
+                final String email = params.find("email");
 
-        try {
-            final String account = params.find("account");
-            final String password = params.find("password");
-            final String email = params.find("email");
+                authService.register(account, password, email);
+                return redirectToIndex();
 
-            authService.register(account, password, email);
-            return redirectToIndex();
-
-        } catch (final AccountDuplicatedException e) {
-            return redirect(HttpStatus.FOUND, Page.BAD_REQUEST.getPath());
-        }
+            } catch (final AccountDuplicatedException e) {
+                return redirect(HttpStatus.FOUND, Page.BAD_REQUEST.getPath());
+            }
+        });
     }
 
     @Override
     protected HttpResponse doGet(final HttpRequest request) {
-        return success(HttpStatus.OK, Page.REGISTER);
+        return ifSessionNotExist(request, params ->
+                success(HttpStatus.OK, Page.REGISTER)
+        );
+    }
+
+    private HttpResponse ifSessionNotExist(final HttpRequest request,
+                                           final Function<Params, HttpResponse> function) {
+        if (request.existSession()) {
+            return redirectToIndex();
+        }
+        return function.apply(request.getParamsFromBody());
     }
 }

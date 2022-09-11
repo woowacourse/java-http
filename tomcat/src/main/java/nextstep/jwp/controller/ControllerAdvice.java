53017fd0f1092c829dc5ee0b5e60b2a860020fcb
youngwooyoo <fbduddn97@naver.com>
2022-09-11 19:12:46 +0900

    fix(ControllerAdvice): 리다이렉트 uri 수정(401, 404 바뀜)

--- tomcat/src/main/java/nextstep/jwp/controller/ControllerAdvice.java
+++ tomcat/src/main/java/nextstep/jwp/controller/ControllerAdvice.java
@@ -1,25 +1,15 @@
 package nextstep.jwp.controller;
 
-import static org.apache.coyote.http11.HeaderField.LOCATION;
-
-import org.apache.coyote.http11.HttpStatus;
 import org.apache.coyote.http11.response.HttpResponse;
-import org.apache.coyote.http11.response.ResponseBody;
-import org.apache.coyote.http11.response.ResponseHeaders;
+import org.apache.coyote.http11.response.HttpResponseBuilder;
 
 public class ControllerAdvice {
 
     public static HttpResponse handleUnauthorized() {
-        ResponseBody body = new ResponseBody();
-        final ResponseHeaders headers = ResponseHeaders.create()
-                .addHeader(LOCATION, "/404");
-        return HttpResponse.create(HttpStatus.FOUND, headers, body);
+        return HttpResponseBuilder.found("/401");
     }
 
     public static HttpResponse handleNotFound() {
-        ResponseBody body = new ResponseBody();
-        final ResponseHeaders headers = ResponseHeaders.create()
-                .addHeader(LOCATION, "/401");
-        return HttpResponse.create(HttpStatus.FOUND, headers, body);
+        return HttpResponseBuilder.found("/404");
     }
 }

9b4b8f83ae725869689839acfeba31b3db89b4e9
sinb57 <sinb57@naver.com>
2022-09-12 13:24:29 +0900

    refactor: 사용하지 않는 메서드 및 import 제거

--- tomcat/src/main/java/org/apache/catalina/session/SessionManager.java
+++ tomcat/src/main/java/org/apache/catalina/session/SessionManager.java
@@ -1,7 +1,6 @@
 package org.apache.catalina.session;
 
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class SessionManager {
@@ -17,13 +16,6 @@ public class SessionManager {
 
         sessions.put(session.getId(), session);
         return session;
-    }
-
-    public static Session findSession(final String id) {
-        if (!sessions.containsKey(id)) {
-            throw new NoSuchElementException("id에 해당하는 세션을 찾지 못했습니다 : " + id);
-        }
-        return sessions.get(id);
     }
 
     public static boolean exist(final String id) {

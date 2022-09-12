a2aa7c9bb23fc27df87ff4d2b17bd77d0c13ecf3
meatsby <meatsby@gmail.com>
2022-09-12 22:07:06 +0900

    feat: 존재하지 않는 경로로 접근 시 404

--- tomcat/src/main/java/org/apache/coyote/http11/controller/ResourceController.java
+++ tomcat/src/main/java/org/apache/coyote/http11/controller/ResourceController.java
@@ -1,5 +1,6 @@
 package org.apache.coyote.http11.controller;
 
+import nextstep.jwp.exception.NotFoundException;
 import org.apache.coyote.http11.http.HttpRequest;
 import org.apache.coyote.http11.http.HttpResponse;
 import org.apache.coyote.http11.http.domain.ContentType;
@@ -9,11 +10,17 @@ public class ResourceController extends AbstractController {
 
     @Override
     protected void doGet(final HttpRequest httpRequest, final HttpResponse httpResponse) {
-        String uri = httpRequest.getUri();
-        String responseBody = FileReader.read(uri);
-        httpResponse.ok()
-                .contentType(ContentType.from(uri))
-                .body(responseBody)
-                .flushBuffer();
+        try {
+            String uri = httpRequest.getUri();
+            String responseBody = FileReader.read(uri);
+            httpResponse.ok()
+                    .contentType(ContentType.from(uri))
+                    .body(responseBody)
+                    .flushBuffer();
+        } catch (NotFoundException e) {
+            httpResponse.found()
+                    .location("/404.html")
+                    .flushBuffer();
+        }
     }
 }

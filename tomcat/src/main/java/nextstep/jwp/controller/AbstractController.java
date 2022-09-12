ce4653405905024cf1effbf22b0f9ca32533e7cb
corinne <yoo77hyeon@gmail.com>
2022-09-12 16:17:15 +0900

    refactor: AbstractController service 리팩터링

--- tomcat/src/main/java/nextstep/jwp/controller/AbstractController.java
+++ tomcat/src/main/java/nextstep/jwp/controller/AbstractController.java
@@ -1,22 +1,21 @@
 package nextstep.jwp.controller;
 
 import nextstep.jwp.exception.MethodNotAllowedException;
+import org.apache.coyote.HttpMethod;
 import org.apache.coyote.support.Request;
 import org.apache.coyote.support.RequestInfo;
 import org.apache.coyote.support.Response;
-import org.apache.coyote.HttpMethod;
 
 public abstract class AbstractController implements Controller {
 
     @Override
     public void service(final Request request, final Response response) {
         final RequestInfo requestInfo = request.getRequestInfo();
-        if (requestInfo.sameHttpMethod(HttpMethod.GET)) {
-            doGet(request, response);
-        }
         if (requestInfo.sameHttpMethod(HttpMethod.POST)) {
             doPost(request, response);
+            return;
         }
+        doGet(request, response);
     }
 
     protected void doGet(final Request request, final Response response) {

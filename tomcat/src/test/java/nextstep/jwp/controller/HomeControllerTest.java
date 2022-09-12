72633403d4440df0c88f0ede16686236d28d1411
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-12 14:10:35 +0900

    test(HomeController): add test cases on GET and POST on / & /index request

---
+++ tomcat/src/test/java/nextstep/jwp/controller/HomeControllerTest.java
@@ -0,0 +1,63 @@
+package nextstep.jwp.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import org.apache.catalina.servlet.Controller;
+import org.apache.coyote.response.HttpResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import support.HttpMessageUtils;
+
+@DisplayName("/ & /index 경로에 대한 요청에 대해")
+class HomeControllerTest {
+
+    private final Controller homeController = new HomeController();
+
+    @DisplayName("GET 메서드의 요청은 상태코드 200과 index.html 파일을 응답한다")
+    @Test
+    void get() throws IOException {
+        final String httpRequest = String.join("\r\n",
+                "GET /index HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "",
+                "");
+        HttpResponse response = new HttpResponse();
+
+        homeController.service(HttpMessageUtils.toRequest(httpRequest), response);
+
+        final var actual =response.toMessage();
+        final var resource = getClass().getClassLoader().getResource("static/index.html");
+        assertAll(
+                () -> assertThat(actual).startsWith("HTTP/1.1 200 OK \r\n"),
+                () -> assertThat(actual).contains("Content-Type: text/html;charset=utf-8 \r\n"),
+                () -> assertThat(actual).contains("Content-Length: 5564 \r\n"),
+                () -> assertThat(actual).endsWith(new String(Files.readAllBytes(new File(resource.getFile()).toPath())))
+        );
+    }
+
+    @DisplayName("POST 메서드의 요청은 상태코드 404과 404.html 파일을 응답한다")
+    @Test
+    void post() throws IOException {
+        final String httpRequest = String.join("\r\n",
+                "POST /index HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "",
+                "");
+        HttpResponse response = new HttpResponse();
+
+        homeController.service(HttpMessageUtils.toRequest(httpRequest), response);
+
+        final var actual = response.toMessage();
+        final var resource = getClass().getClassLoader().getResource("static/404.html");
+        assertAll(
+                () -> assertThat(actual).startsWith("HTTP/1.1 404 Not Found \r\n"),
+                () -> assertThat(actual).contains("Content-Type: text/html;charset=utf-8 \r\n"),
+                () -> assertThat(actual).contains("Content-Length: 2426 \r\n"),
+                () -> assertThat(actual).endsWith(new String(Files.readAllBytes(new File(resource.getFile()).toPath())))
+        );
+    }
+}
--- tomcat/src/test/java/support/HttpMessageUtils.java
+++ tomcat/src/test/java/support/HttpMessageUtils.java
@@ -1,9 +1,24 @@
 package support;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import org.apache.coyote.request.HttpRequest;
+
 public class HttpMessageUtils {
 
     public static String extractSetCookieSessionId(String socketOutput) {
         final var right = socketOutput.split("Set-Cookie: JSESSIONID=")[1];
         return right.split("; Max-Age=600")[0];
+    }
+
+    public static HttpRequest toRequest(String httpRequest) throws IOException {
+        try (final var inputStream = new ByteArrayInputStream(httpRequest.getBytes());
+             final var reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+             final var bufferedReader = new BufferedReader(reader)) {
+            return HttpRequest.of(bufferedReader);
+        }
     }
 }

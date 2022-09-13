06ee4c203e807a83f1c7b3b477f52b5089937834
bcc0830 <bcc0830@bithub.com>
2022-09-13 23:38:24 +0900

    refactor : 헤더 파싱 시 방어 로직 추가

--- tomcat/src/main/java/nextstep/jwp/vo/RequestHeaders.java
+++ tomcat/src/main/java/nextstep/jwp/vo/RequestHeaders.java
@@ -8,6 +8,7 @@ public class RequestHeaders {
     private static final String HEADER_DELIMITER = ": ";
     private static final int HEADER_KEY_INDEX = 0;
     private static final int HEADER_VALUE_INDEX = 1;
+    private static final int HEADER_SPLIT_SIZE = 2;
 
     private final Map<String, String> headers;
 
@@ -16,12 +17,22 @@ public class RequestHeaders {
     }
 
     public static RequestHeaders from(List<String> rawRequest) {
+        validateRawRequest(rawRequest);
         Map<String, String> headers = new HashMap<>();
         rawRequest.subList(1, rawRequest.size())
                 .stream()
                 .map(header -> header.split(HEADER_DELIMITER))
                 .forEach(parsedHeader -> headers.put(parsedHeader[HEADER_KEY_INDEX], parsedHeader[HEADER_VALUE_INDEX]));
         return new RequestHeaders(headers);
+    }
+
+    private static void validateRawRequest(List<String> rawRequest) {
+        boolean headerCondition = rawRequest.subList(1, rawRequest.size())
+                .stream()
+                .anyMatch(each -> each.split(HEADER_DELIMITER).length != HEADER_SPLIT_SIZE);
+        if (!headerCondition) {
+            throw new IllegalArgumentException("Request의 두번째 줄부터 마지막 줄 중 헤더의 형식이 아닌 줄이 있습니다.");
+        }
     }
 
     public HttpCookie generateCookie() {

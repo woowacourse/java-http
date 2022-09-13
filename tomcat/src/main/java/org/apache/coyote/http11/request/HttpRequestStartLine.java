d6543676b3ff5598b9450adf3efcab0f60b72c65
shindong96 <2006peter@naver.com>
2022-09-13 16:09:48 +0900

    refactor: queryParam이 사용되는 API가 없기떄문에 삭제

--- tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequestStartLine.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequestStartLine.java
@@ -1,28 +1,22 @@
 package org.apache.coyote.http11.request;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class HttpRequestStartLine {
 
     private static final String QUERY_STRING_PREFIX = "?";
 
     private final HttpMethod method;
     private final String uri;
-    private final Map<String, String> queryParams;
 
-    public HttpRequestStartLine(final HttpMethod method, final String uri, final Map<String, String> queryParams) {
+    public HttpRequestStartLine(final HttpMethod method, final String uri) {
         this.method = method;
         this.uri = uri;
-        this.queryParams = queryParams;
     }
 
     public static HttpRequestStartLine from(final String startLine) {
         final String[] startLineContents = startLine.split(" ");
 
         return new HttpRequestStartLine(HttpMethod.valueOf(startLineContents[0]),
-                takeUri(startLineContents[1]),
-                takeQueryParams(startLineContents[1]));
+                takeUri(startLineContents[1]));
     }
 
     private static String takeUri(final String uriAndQueryParams) {
@@ -33,29 +27,7 @@ public class HttpRequestStartLine {
 
         return uriAndQueryParams.substring(0, indexOfQueryStringPrefix);
     }
-
-    private static Map<String, String> takeQueryParams(final String uriAndQueryParams) {
-        final Map<String, String> queryParams = new HashMap<>();
-
-        if (!uriAndQueryParams.contains(QUERY_STRING_PREFIX)) {
-            return queryParams;
-        }
-
-        final int indexOfQueryStringPrefix = uriAndQueryParams.indexOf(QUERY_STRING_PREFIX);
-
-        return parseQueryParams(uriAndQueryParams.substring(indexOfQueryStringPrefix + 1));
-    }
-
-    private static Map<String, String> parseQueryParams(final String queryParams) {
-        final Map<String, String> result = new HashMap<>();
-        final String[] keyAndValues = queryParams.split("&");
-
-        for (final String keyValue : keyAndValues) {
-            final String[] keyAndValue = keyValue.split("=");
-            result.put(keyAndValue[0], keyAndValue[1]);
-        }
-        return result;
-    }
+    
 
     public HttpMethod getMethod() {
         return method;

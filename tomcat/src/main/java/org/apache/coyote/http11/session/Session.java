ce13fd610539584ec63b3cac854b247cfa158348
sure-why-not <pkse1213@gmail.com>
2022-09-13 21:20:41 +0900

    refactor: Session ConcurrentHashMap 으로 변경

--- tomcat/src/main/java/org/apache/coyote/http11/session/Session.java
+++ tomcat/src/main/java/org/apache/coyote/http11/session/Session.java
@@ -1,13 +1,13 @@
 package org.apache.coyote.http11.session;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Session {
 
     private final String id;
-    private final Map<String, Object> values = new HashMap<>();
+    private final Map<String, Object> values = new ConcurrentHashMap<>();
 
     public Session() {
         this.id = createId();

341cd1f78751bba8c6d1c001fd8fe5f63857def5
SuyeonChoi <tndus970120@gmail.com>
2022-09-13 13:06:31 +0900

    feat(QueryParser): 문자열을 파싱하는 클래스 생성

--- tomcat/src/main/java/org/apache/coyote/http11/request/RequestBody.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/RequestBody.java
@@ -1,11 +1,8 @@
 package org.apache.coyote.http11.request;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class RequestBody {
-
-    private static final int DEFAULT_QUERY_PARAMETER_PAIR_SIZE = 2;
 
     private final String contents;
 
@@ -14,25 +11,6 @@ public class RequestBody {
     }
 
     public Map<String, String> parseApplicationFormData() {
-        final Map<String, String> params = new HashMap<>();
-        final String[] paramPairs = contents.split("&");
-
-        for (final String param : paramPairs) {
-            addParamPair(params, param);
-        }
-
-        return params;
-    }
-
-    private void addParamPair(final Map<String, String> params, final String param) {
-        if (param == null || param.isBlank()) {
-            return;
-        }
-
-        final String[] pair = param.split("=");
-        if (pair.length != DEFAULT_QUERY_PARAMETER_PAIR_SIZE) {
-            throw new IllegalArgumentException("올바른 application/x-www-form-urlencoded 형식이 아닙니다.");
-        }
-        params.put(pair[0], pair[1]);
+        return QueryParser.parse(contents, "올바른 application/x-www-form-urlencoded 형식이 아닙니다.");
     }
 }

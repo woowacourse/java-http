74dbd623a10317d1370c8afd3434a55b8b307d3c
Richard JEON <ztzy1907@gmail.com>
2022-09-15 10:49:49 +0900

    feat: YamlUtils 요청한 파일이 없을 경우에 대한 검증 추가

--- tomcat/src/main/java/org/richard/utils/YamlUtils.java
+++ tomcat/src/main/java/org/richard/utils/YamlUtils.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Objects;
 
 public class YamlUtils {
 
@@ -12,12 +14,19 @@ public class YamlUtils {
 
     public static <T> T readPropertyAsObject(final String propertyFileName, Class<T> clazz) {
         final var resource = YamlUtils.class.getClassLoader().getResource(propertyFileName);
+        validateExistence(resource, propertyFileName);
 
         try {
             final var uri = resource.toURI();
             return objectMapper.readValue(new File(uri), clazz);
         } catch (IOException | URISyntaxException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static void validateExistence(final URL resource, final String fileName) {
+        if (Objects.isNull(resource)) {
+            throw new IllegalArgumentException(String.format("Requested file does not exist : %s", fileName));
         }
     }
 

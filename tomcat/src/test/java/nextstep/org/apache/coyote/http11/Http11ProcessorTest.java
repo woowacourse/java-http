813eb99cd8991040032168c0eae582b48f097ed8
Richard JEON <ztzy1907@gmail.com>
2022-09-15 14:18:59 +0900

    test: Http11ProcessorTest css, js 파일 응답 테스트 추가

--- tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -8,12 +8,13 @@ import java.net.URL;
 import java.nio.file.Files;
 import org.apache.coyote.http11.Http11Processor;
 import org.junit.jupiter.api.Test;
+import org.richard.utils.ResourceUtils;
 import support.StubSocket;
 
 class Http11ProcessorTest {
 
     @Test
-    void process() {
+    void 루트경로_요청시_Hello_world를_응답한다() {
         // given
         final var socket = new StubSocket();
         final var processor = new Http11Processor(socket);
@@ -33,7 +34,7 @@ class Http11ProcessorTest {
     }
 
     @Test
-    void index() throws IOException {
+    void index페이지_요청시_해당_html파일이_응답된다() throws IOException {
         // given
         final String httpRequest = String.join("\r\n",
                 "GET /index.html HTTP/1.1 ",
@@ -55,6 +56,64 @@ class Http11ProcessorTest {
                 "Content-Length: 5564 \r\n" +
                 "\r\n" +
                 new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+
+        assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @Test
+    void css파일_요청시_정상응답된다() {
+        // given
+        final String httpRequest = String.join("\r\n",
+                "GET /css/styles.css HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Accept: text/css,*/*;q=0.1 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+        final var socket = new StubSocket(httpRequest);
+        final var processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        final var expected = String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/css ",
+                "Content-Length: 211991 ",
+                "",
+                ResourceUtils.createResourceAsString("static/css/styles.css")
+        );
+
+        assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @Test
+    void js파일_요청시_정상응답된다() {
+        // given
+        final String httpRequest = String.join("\r\n",
+                "GET /js/scripts.js HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Accept: text/javascript ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+        final var socket = new StubSocket(httpRequest);
+        final var processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        final var expected = String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/javascript  ",
+                "Content-Length: 976 ",
+                "",
+                ResourceUtils.createResourceAsString("static/js/scripts.js")
+        );
 
         assertThat(socket.output()).isEqualTo(expected);
     }

98bafeefd19db195ebb2e0fcd9abe3a69bd0424d
seokjin8678 <seokjin8678@gmail.com>
2023-09-02 02:17:30 +0900

    test: FileTest 작성

--- study/src/test/java/study/FileTest.java
+++ study/src/test/java/study/FileTest.java
@@ -1,13 +1,15 @@
 package study;
 
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.nio.file.Path;
-import java.util.Collections;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * 웹서버는 사용자가 요청한 html 파일을 제공 할 수 있어야 한다.
@@ -28,7 +30,7 @@ class FileTest {
         final String fileName = "nextstep.txt";
 
         // todo
-        final String actual = "";
+        final String actual = getClass().getResource("/"+ fileName).getFile();
 
         assertThat(actual).endsWith(fileName);
     }
@@ -40,14 +42,16 @@ class FileTest {
      * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws IOException {
         final String fileName = "nextstep.txt";
 
         // todo
-        final Path path = null;
+        URL resource = getClass().getResource("/" + fileName);
+        File file = new File(resource.getPath());
 
         // todo
-        final List<String> actual = Collections.emptyList();
+        final List<String> actual = Files.lines(file.toPath())
+            .collect(toList());
 
         assertThat(actual).containsOnly("nextstep");
     }

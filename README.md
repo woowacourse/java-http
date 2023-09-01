f6a3c297ac646188406c099ab8f5448d05193c2e
sh111-coder <ohk9134@naver.com>
2023-09-01 22:44:20 +0900

    feat: GET /index.html 응답 기능 구현

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## 🎯 1단계 - HTTP 서버 구현하기
 
-- [ ] GET /index.html 요청이 들어오면 index.html 파일을 응답한다.
+- [x] GET /index.html 요청이 들어오면 index.html 파일을 응답한다.
 
 - [ ] CSS를 지원한다.
 
--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,16 +1,21 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
-
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final int PATH_MESSAGE_INDEX = 1;
 
     private final Socket connection;
 
@@ -29,14 +34,33 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            String startLine = br.readLine();
+            String[] startLineElements = startLine.split(" ");
+            String path = startLineElements[PATH_MESSAGE_INDEX];
+
+            String response = "";
+            if (path.equals("/")) {
+                final String responseBody = "Hello world!";
+
+                response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody);
+            } else if (path.endsWith(".html")) {
+                final URL resource = getClass().getClassLoader().getResource("static" + path);
+                String responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+
+                response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody);
+            }
 
             outputStream.write(response.getBytes());
             outputStream.flush();

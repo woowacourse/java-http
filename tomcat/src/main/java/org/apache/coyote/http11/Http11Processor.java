5074caa43c03191298fb76c29f255ba992219e6b
Rosie <dev.kayoung@gmail.com>
2023-09-01 18:09:44 +0900

    feat: query string 파싱

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -8,8 +8,11 @@ import java.net.Socket;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import javax.annotation.Nullable;
+import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
@@ -37,22 +40,64 @@ public class Http11Processor implements Runnable, Processor {
              final var outputStream = connection.getOutputStream()) {
             final var bufferedReader = new BufferedReader(
                     new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+
             final var uri = bufferedReader.readLine().split(" ")[1];
-            final File file = getFile(uri);
-            final String responseBody = buildResponseBody(file);
-            final String contentType = getContentType(file);
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    contentType,
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            final String response = buildResponse(uri);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private File resolveFile(String uri) {
+        if (uri.contains("/login")) {
+            final var params = queryParams(uri);
+            validateUser(params);
+            return getFile("/login.html");
+        }
+        return getFile(uri);
+    }
+
+    private void validateUser(Map<String, String> params) {
+        InMemoryUserRepository.findByAccount(params.get("account"))
+                .filter(user -> user.checkPassword(params.get("password")))
+                .ifPresent(user -> log.info("user: {}", user));
+    }
+
+    private String buildResponse(final String uri) throws IOException {
+        final File file = resolveFile(uri);
+        final String response = buildResponse(file);
+        return response;
+    }
+
+    private String buildResponse(final File file) throws IOException {
+        final String contentType = getContentType(file);
+        final String responseBody = buildResponseBody(file);
+
+        final var response = String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                contentType,
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+        return response;
+    }
+
+    private Map<String, String> queryParams(String uri) {
+        final var params = new HashMap<String, String>();
+        int index = uri.indexOf("?");
+        String queryString = uri.substring(index + 1);
+        while (queryString.contains("&")) {
+            String[] split = queryString.split("&");
+            for (String s : split) {
+                String[] keyValue = s.split("=");
+                params.put(keyValue[0], keyValue[1]);
+            }
+            queryString = split[split.length - 1];
+        }
+        return params;
     }
 
     private String getContentType(final File file) throws IOException {

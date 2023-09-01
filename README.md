4ba44f6f93b7e7c29e319efe1f719539f859aa88
parkmuhyeun <pjhg410@gmail.com>
2023-09-01 17:51:04 +0900

    feat: /index 응답되도록 기능 추가

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 
 ## 1단계 - HTTP 구현하기
 
-- [ ] GET /index.html 응답하기
+- [x] GET /index.html 응답하기
 - [ ] CSS 지원하기
 - [ ] Query String 파싱
--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,12 +5,20 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
 
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final String STATIC_DIRECTORY = "static";
+    private static final String SPACE = " ";
+    private static final String LINE_FEED = "\r\n";
+    private static final int PATH_INDEX = 1;
+    private static final int PROTOCOL_INDEX = 2;
 
     private final Socket connection;
 
@@ -29,14 +37,11 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            byte[] bytes = new byte[2048];
+            inputStream.read(bytes);
+            final String request = new String(bytes);
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            final String response = createResponse(request);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
@@ -44,4 +49,37 @@ public class Http11Processor implements Runnable, Processor {
             log.error(e.getMessage(), e);
         }
     }
+
+    private String createResponse(String request) throws IOException {
+        String path = getPath(request);
+        String protocol = getRequestElement(request, PROTOCOL_INDEX);
+        String status = "200 OK";
+        String contentType = "Content-Type: text/html;charset=utf-8";
+
+        String content = getContent(path);
+        String contentLength = "Content-Length: " + content.getBytes().length;
+
+        return protocol + SPACE + status + SPACE + LINE_FEED +
+                contentType + SPACE + LINE_FEED +
+                contentLength + SPACE + LINE_FEED +
+                LINE_FEED +
+                content;
+    }
+
+    private String getContent(String path) throws IOException {
+        if (path.equals("/")) {
+            return "Hello world!";
+        }
+        URL resource = getClass().getClassLoader().getResource(STATIC_DIRECTORY + path);
+        return new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+    }
+
+    private String getPath(String request) {
+        return getRequestElement(request, PATH_INDEX);
+    }
+
+    private String getRequestElement(String request, int index) {
+        return request.split(SPACE + "|" + LINE_FEED)[index];
+    }
+
 }

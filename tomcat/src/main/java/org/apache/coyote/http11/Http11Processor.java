51adb4e068376f2ad15bb97293636d035ed1159a
sosow0212 <sosow0212@naver.com>
2023-09-01 18:54:24 +0900

    feat: scripts, assets js 파일 띄우기

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,14 +5,18 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Http11Processor implements Runnable, Processor {
@@ -35,15 +39,17 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final InputStream inputStream = connection.getInputStream();
-             final OutputStream outputStream = connection.getOutputStream()) {
+             final OutputStream outputStream = connection.getOutputStream();
+             final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))
+        ) {
+            List<String> headers = getHeader(reader);
 
-            String[] headers = getHeader(inputStream).split(HEADER_DELIMETER);
             for (String header : headers) {
                 System.out.println(header);
             }
             System.out.println("-----------------");
 
-            if (headers[0].contains("/index.html")) {
+            if (headers.get(0).contains("/index.html")) {
                 URL fileUrl = this.getClass()
                         .getClassLoader()
                         .getResource("static" + "/index.html");
@@ -60,7 +66,7 @@ public class Http11Processor implements Runnable, Processor {
 
                 outputStream.write(response.getBytes());
 
-            } else if (headers[0].contains("css")) {
+            } else if (headers.get(0).contains("css")) {
                 URL fileUrl = this.getClass()
                         .getClassLoader()
                         .getResource("static/css" + "/styles.css");
@@ -80,23 +86,48 @@ public class Http11Processor implements Runnable, Processor {
                         responseBody);
 
                 outputStream.write(response.getBytes());
+            } else if (headers.get(0).contains("js")) {
+                URL fileUrl;
+                if (headers.get(0).contains("scripts")) {
+                    fileUrl = this.getClass()
+                            .getClassLoader()
+                            .getResource("static/js" + "/scripts.js");
+                } else {
+                    String fileDir = headers.get(0).split(" ")[1];
+                    String[] fileDirSplits = fileDir.split("assets/");
+                    fileUrl = this.getClass()
+                            .getClassLoader()
+                            .getResource("static/assets/" + fileDirSplits[1]);
+                }
+
+                Path path = new File(Objects.requireNonNull(fileUrl).getFile()).toPath();
+                String responseBody = new String(Files.readAllBytes(path));
+
+                String response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody);
+
+                outputStream.write(response.getBytes());
+
             }
+
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
     }
 
-    private String getHeader(final InputStream inputStream) throws IOException {
-        StringBuilder stringBuilder = new StringBuilder();
+    private List<String> getHeader(final BufferedReader reader) throws IOException {
+        List<String> lines = new ArrayList<>();
 
-        int c;
-        while ((c = inputStream.read()) != -1) {
-            stringBuilder.append((char) c);
-            if (stringBuilder.toString().endsWith("\r\n\r\n")) {
-                break;
-            }
+        String line = Objects.requireNonNull(reader.readLine());
+        while (!line.isEmpty()) {
+            lines.add(line);
+            line = reader.readLine();
         }
 
-        return stringBuilder.toString();
+        return lines;
     }
 }
---
+++ tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequest.java
@@ -0,0 +1,6 @@
+package org.apache.coyote.http11.request;
+
+public class HttpRequest {
+
+//    HttpMethod
+}

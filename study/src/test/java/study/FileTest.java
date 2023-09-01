2ee69b7597449d32668601c7a07ad495290d9cb9
waterricecake <fruturum@nate.com>
2023-09-01 19:49:32 +0900

    feat: fileTest 학습

--- study/src/test/java/study/FileTest.java
+++ study/src/test/java/study/FileTest.java
@@ -1,11 +1,19 @@
 package study;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,7 +36,9 @@ class FileTest {
         final String fileName = "nextstep.txt";
 
         // todo
-        final String actual = "";
+        URL url = getClass().getClassLoader().getResource(fileName);
+        final String actual = url.getFile();
+
 
         assertThat(actual).endsWith(fileName);
     }
@@ -40,14 +50,15 @@ class FileTest {
      * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws IOException, URISyntaxException {
         final String fileName = "nextstep.txt";
 
         // todo
-        final Path path = null;
+        URL url = getClass().getClassLoader().getResource(fileName);
+        final Path path = Path.of(url.toURI());
 
         // todo
-        final List<String> actual = Collections.emptyList();
+        final List<String> actual = Files.readAllLines(path);
 
         assertThat(actual).containsOnly("nextstep");
     }

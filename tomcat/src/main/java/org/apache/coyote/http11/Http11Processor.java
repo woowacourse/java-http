d352cbf07e1ca398d18b153477dfa83720e80159
Rosie <dev.kayoung@gmail.com>
2023-09-01 16:43:27 +0900

    feat: index.html 조회

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,17 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,8 +33,10 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
-
-            final var responseBody = "Hello world!";
+            final var bufferedReader = new BufferedReader(
+                    new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            final var uri = bufferedReader.readLine().split(" ")[1];
+            final String responseBody = buildResponseBody(uri);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -44,4 +51,13 @@ public class Http11Processor implements Runnable, Processor {
             log.error(e.getMessage(), e);
         }
     }
+
+    private String buildResponseBody(final String uri) throws IOException {
+        if (uri.equals("/")) {
+            return "Hello world!";
+        }
+        final URL resource = getClass().getClassLoader().getResource("static" + uri);
+        return new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+    }
+
 }

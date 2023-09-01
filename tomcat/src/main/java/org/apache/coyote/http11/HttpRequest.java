37779e12404b0446d026db6f4d6f4e1f203a52ca
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-09-02 00:24:32 +0900

    feat: Http 요청 객체 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpRequest.java
@@ -0,0 +1,21 @@
+package org.apache.coyote.http11;
+
+import java.util.Map;
+
+public class HttpRequest {
+
+    private final HttpStartLine httpStartLine;
+    private final HttpRequestHeaders httpRequestHeaders;
+
+    public HttpRequest(final HttpStartLine httpStartLine, final HttpRequestHeaders httpRequestHeaders) {
+        this.httpStartLine = httpStartLine;
+        this.httpRequestHeaders = httpRequestHeaders;
+    }
+
+    public static HttpRequest of(final String startLine, final Map<String, String> headers) {
+        return new HttpRequest(
+                HttpStartLine.from(startLine),
+                HttpRequestHeaders.from(headers)
+        );
+    }
+}

29024cd4d9376add5c9bde2ee26879598ff424f3
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-09-02 00:24:07 +0900

    feat: Http 요청 헤더 객체 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpRequestHeaders.java
@@ -0,0 +1,20 @@
+package org.apache.coyote.http11;
+
+import java.util.Map;
+
+public class HttpRequestHeaders {
+
+    private final Map<String, String> headers;
+
+    private HttpRequestHeaders(final Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public static HttpRequestHeaders from(final Map<String, String> headers) {
+        return new HttpRequestHeaders(headers);
+    }
+
+    public String get(final String header) {
+        return headers.get(header);
+    }
+}

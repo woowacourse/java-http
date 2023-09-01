baacb35deb0aef0b8d56aab5fc8296b4e9ee8cee
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-09-02 00:27:45 +0900

    feat: Http 요청 메세지 시작줄 생성 검증 테스트 추가

--- tomcat/src/test/java/org/apache/coyote/http11/HttpStartLineTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/HttpStartLineTest.java
@@ -1,11 +1,14 @@
 package org.apache.coyote.http11;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @SuppressWarnings("NonAsciiCharacters")
+@DisplayName("HttpStartLine 테스트")
 class HttpStartLineTest {
 
     @Test
@@ -22,5 +25,16 @@ class HttpStartLineTest {
             assertThat(httpStartLine.getRequestURI()).isEqualTo("/index.html");
             assertThat(httpStartLine.getHttpVersion()).isEqualTo("HTTP/1.1");
         });
+    }
+
+    @Test
+    void 잘못된_HTTP_요청_메세지_시작라인인_경우_예외_발생() {
+        // given
+        final String startLine = "GETHTTP/1.1";
+
+        // when & then
+        assertThatThrownBy(() -> HttpStartLine.from(startLine))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("시작 라인의 토큰은 3개여야 합니다.");
     }
 }

2b14c5dacef4f68409144efc44057db004e9936f
songusika <sws981202@gmail.com>
2023-09-01 21:17:15 +0900

    feat: MultiValueMap 구현

---
+++ tomcat/src/main/java/org/apache/coyote/domain/MultiValueMap.java
@@ -0,0 +1,62 @@
+package org.apache.coyote.domain;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MultiValueMap<K, V> {
+
+    private final Map<K, List<V>> multiValueMap;
+
+    public MultiValueMap() {
+        this(0);
+    }
+
+    public MultiValueMap(int initialCapacity) {
+        this.multiValueMap = new HashMap<>(initialCapacity);
+    }
+
+    public void put(final K key, final V value) {
+        if (isNotContain(key)) {
+            List<V> values = new ArrayList<>();
+            values.add(value);
+            multiValueMap.put(key, values);
+            return;
+        }
+
+        multiValueMap.get(key).add(value);
+    }
+
+    public void putAll(final K key, final List<V> values) {
+        if (isNotContain(key)) {
+            multiValueMap.put(key, new ArrayList<>(values));
+            return;
+        }
+
+        multiValueMap.get(key).addAll(values);
+    }
+
+    public V getRecentValue(final K key) {
+        validateKey(key);
+
+        List<? extends V> values = multiValueMap.get(key);
+        return values.get(values.size()-1);
+    }
+
+    public List<V> getValues(final K key) {
+        validateKey(key);
+
+        return multiValueMap.get(key);
+    }
+
+    private boolean isNotContain(final K key) {
+        return !multiValueMap.containsKey(key);
+    }
+
+    private void validateKey(final K key) {
+        if(isNotContain(key)) {
+            throw new IllegalArgumentException("해당 키가 존재하지 않습니다.");
+        }
+    }
+}
---
+++ tomcat/src/test/java/org/apache/coyote/domain/MultiValueMapTest.java
@@ -0,0 +1,79 @@
+package org.apache.coyote.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(ReplaceUnderscores.class)
+class MultiValueMapTest {
+
+    @Test
+    void multiValueMap_은_키에_대한_제일_마지막에_들어간_값을_가져올_수_있다() {
+        // given
+        final String key = "Key";
+        final String value1 = "Value1";
+        final String value2 = "Value2";
+        final String value3 = "Value3";
+        final MultiValueMap<String, String> multiValueMap = new MultiValueMap<>();
+
+        // when
+        multiValueMap.put(key, value1);
+        multiValueMap.put(key, value2);
+        multiValueMap.put(key, value3);
+
+        // then
+        assertThat(multiValueMap.getRecentValue(key)).isEqualTo(value3);
+    }
+
+    @Test
+    void multiValueMap_키에_대한_모든_값을_가져올_수_있다() {
+        // given
+        final String key = "Key";
+        final String value1 = "Value1";
+        final String value2 = "Value2";
+        final String value3 = "Value3";
+        final MultiValueMap<String, String> multiValueMap = new MultiValueMap<>();
+
+        // when
+        multiValueMap.put(key, value1);
+        multiValueMap.put(key, value2);
+        multiValueMap.put(key, value3);
+
+        // then
+        assertThat(multiValueMap.getValues(key)).isEqualTo(
+                List.of(value1, value2, value3)
+        );
+    }
+
+    @Test
+    void multiValueMap_없는_키에_대한_값을_가져올_시_예외가_발생한다() {
+        assertThatThrownBy(() -> new MultiValueMap<String, String>().getValues("Key"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 키가 존재하지 않습니다.");
+    }
+
+    @Test
+    void multiValueMap_은_값을_넣은_순서대로_리스트에_저장한다() {
+        // given
+        final String key = "Key";
+        final List<String> values1 = List.of("Value1", "Value2", "Value3");
+        final String value = "Value4";
+        final List<String> values2 = List.of("Value5", "Value6", "Value7");
+        final MultiValueMap<String, String> multiValueMap = new MultiValueMap<>();
+
+        // when
+        multiValueMap.putAll(key, values1);
+        multiValueMap.put(key, value);
+        multiValueMap.putAll(key, values2);
+
+        // then
+        assertThat(multiValueMap.getValues(key)).isEqualTo(
+                List.of("Value1", "Value2", "Value3", "Value4", "Value5", "Value6", "Value7")
+        );
+    }
+}

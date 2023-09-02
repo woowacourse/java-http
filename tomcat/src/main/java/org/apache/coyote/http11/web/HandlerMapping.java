4346b28644b51e5260042fdc4f3ad4db9dff9b37
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-09-03 00:19:49 +0900

    refactor: HandlerMapping 에서 mapping되는 Controller 변경

--- tomcat/src/main/java/org/apache/coyote/http11/web/HandlerMapping.java
+++ tomcat/src/main/java/org/apache/coyote/http11/web/HandlerMapping.java
@@ -2,7 +2,6 @@ package org.apache.coyote.http11.web;
 
 import java.util.Map;
 import java.util.Optional;
-import nextstep.web.HelloController;
 import nextstep.web.IndexController;
 import nextstep.web.LoginController;
 import nextstep.web.RegisterController;
@@ -12,7 +11,7 @@ public class HandlerMapping {
     private static final String HTML_EXTENSION = ".html";
 
     private final Map<String, Controller> controllerMap = Map.of(
-            "/", new HelloController(),
+            "/", new IndexController(),
             "/index", new IndexController(),
             "/login", new LoginController(),
             "/register", new RegisterController()

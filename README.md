854849b1380b227d0616fc165ca6b5582857c9db
chaewon121 <cw121@naver.com>
2023-09-02 15:48:37 +0900

    feat: index.html 페이지 접근 기능 구현

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ## HTTP 서버 구현 요구사항
 
-- [ ] `http://localhost:8080/index.html` 페이지에 접근 가능하다.
+- [x] `http://localhost:8080/index.html` 페이지에 접근 가능하다.
 - [ ] 접근한 페이지의 `js`, `css` 파일을 불러올 수 있다.
 - [ ] `uri`의 `QueryString`을 파싱하는 기능이 있다.
--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,8 +5,13 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,14 +34,36 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            StringBuilder requestBuilder = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null && !line.isEmpty()) {
+                requestBuilder.append(line).append("\r\n");
+            }
+
+            String request = requestBuilder.toString();
+
+            String[] requestLines = request.split("\\s+");
+            if (requestLines.length < 2) {
+                throw new UncheckedServletException(new Exception("예외"));
+            }
+            String resourcePath = requestLines[1];
+
+            byte[] responseBodyBytes;
+            if (resourcePath.equals("/index.html")) {
+                String projectRootPath = System.getProperty("user.dir");
+                String htmlFilePath = projectRootPath + "/src/main/resources/static/index.html";
+                responseBodyBytes = Files.readAllBytes(Paths.get(htmlFilePath));
+            } else {
+                responseBodyBytes = "Resource not found".getBytes(StandardCharsets.UTF_8);
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "Content-Length: " + responseBodyBytes.length + " ",
                     "",
-                    responseBody);
+                    new String(responseBodyBytes, StandardCharsets.UTF_8));
 
             outputStream.write(response.getBytes());
             outputStream.flush();
@@ -44,4 +71,5 @@ public class Http11Processor implements Runnable, Processor {
             log.error(e.getMessage(), e);
         }
     }
+
 }

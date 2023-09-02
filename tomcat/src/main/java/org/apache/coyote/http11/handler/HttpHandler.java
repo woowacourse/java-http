5eacbeb93e5342179132d77dfa5ae3abf4f66a56
hong-sile <gurwns9325@naver.com>
2023-09-02 20:11:57 +0900

    refactor: handleTo에서 IOException이 발생할 수 있도록 리팩터링

--- tomcat/src/main/java/org/apache/coyote/http11/handler/HttpHandler.java
+++ tomcat/src/main/java/org/apache/coyote/http11/handler/HttpHandler.java
@@ -1,5 +1,7 @@
 package org.apache.coyote.http11.handler;
 
+import java.io.IOException;
+
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.response.HttpResponse;
 
@@ -7,5 +9,5 @@ public interface HttpHandler {
 
 	boolean isSupported(final HttpRequest request);
 
-	HttpResponse handleTo(final HttpRequest request);
+	HttpResponse handleTo(final HttpRequest request) throws IOException;
 }

fbdd91ef73688a8929d2ceb627763cc3ca203185
greeng00se <thegreengoose7@gmail.com>
2023-09-02 19:33:14 +0900

    feat: 쿠키값을 가지고 있는 httpCookie 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/common/HttpCookie.java
@@ -0,0 +1,46 @@
+package org.apache.coyote.http11.common;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toMap;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpCookie {
+
+    private static final String SEPARATOR = "; ";
+    private static final String DELIMITER = "=";
+    private static final int KEY_INDEX = 0;
+    private static final int VALUE_INDEX = 1;
+
+    private final Map<String, String> items = new HashMap<>();
+
+    public HttpCookie() {
+    }
+
+    private HttpCookie(final Map<String, String> items) {
+        this.items.putAll(items);
+    }
+
+    public static HttpCookie from(final String headers) {
+        return Arrays.stream(headers.split(SEPARATOR))
+                .map(header -> header.split(DELIMITER))
+                .collect(collectingAndThen(
+                        toMap(header -> header[KEY_INDEX], header -> header[VALUE_INDEX]),
+                        HttpCookie::new
+                ));
+    }
+
+    public void put(final String key, final String value) {
+        items.put(key, value);
+    }
+
+    public String get(final String key) {
+        return items.get(key);
+    }
+
+    public Map<String, String> getItems() {
+        return items;
+    }
+}

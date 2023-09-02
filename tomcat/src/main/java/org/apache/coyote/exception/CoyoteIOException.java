ca075d93508fbb3c2bc7a28c5298550a154c6ec1
hyena <goatsfish@gmail.com>
2023-09-03 00:09:21 +0900

    feat: Coyote 커스텀 예외 구현

---
+++ tomcat/src/main/java/org/apache/coyote/exception/CoyoteIOException.java
@@ -0,0 +1,8 @@
+package org.apache.coyote.exception;
+
+public class CoyoteIOException extends CoyoteException {
+
+    public CoyoteIOException(final String message) {
+        super(message);
+    }
+}

621d880eca8022f972385a78908cb6176c7fc285
hong-sile <gurwns9325@naver.com>
2023-09-02 17:41:58 +0900

    feat: not found handler 정의

---
+++ tomcat/src/main/java/org/apache/coyote/http11/handler/NotFoundHandler.java
@@ -0,0 +1,21 @@
+package org.apache.coyote.http11.handler;
+
+import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.response.HttpResponse;
+import org.apache.coyote.http11.response.HttpStatusCode;
+
+public class NotFoundHandler implements HttpHandler{
+
+	@Override
+	public boolean isSupported(final HttpRequest request) {
+		return false;
+	}
+
+	@Override
+	public HttpResponse handleTo(final HttpRequest request) {
+		return new HttpResponse(
+			HttpStatusCode.NOT_FOUND_404,
+			"요청하신 리소스를 찾을 수 없습니다."
+		);
+	}
+}
--- tomcat/src/main/java/org/apache/coyote/http11/response/HttpStatusCode.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/HttpStatusCode.java
@@ -3,7 +3,8 @@ package org.apache.coyote.http11.response;
 public enum HttpStatusCode {
 
 	OK_200(200, "OK"),
-	INTERNAL_SERVER_ERROR_500(500, "Internal server error");
+	INTERNAL_SERVER_ERROR_500(500, "Internal Server Error"),
+	NOT_FOUND_404(404, "Not Found");
 
 	private final int code;
 	private final String description;

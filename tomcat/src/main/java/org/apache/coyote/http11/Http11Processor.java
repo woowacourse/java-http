cc4330d388fa893d6b56fb2758026dd2a5e7748c
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2023-09-02 15:46:23 +0900

    feat: index.html 응답하도록 수정

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,11 @@
 package org.apache.coyote.http11;
 
+import java.io.IOException;
+import java.net.Socket;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -27,21 +26,31 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+            final var outputStream = connection.getOutputStream()
+        ) {
 
-            final var responseBody = "Hello world!";
+            final var responseBody = readIndexFile();
 
             final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        }
+    }
+
+    private String readIndexFile() {
+        try (final var inputStream = getClass().getClassLoader().getResourceAsStream("static/index.html")) {
+            final byte[] indexContentBytes = inputStream.readAllBytes();
+            return new String(indexContentBytes);
+        } catch (final IOException e) {
+            throw new UncheckedServletException(e);
         }
     }
 }

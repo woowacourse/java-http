e230ecf7aa288f3ca490c434e8a522514b5beabc
woosung1223 <dntjd991223@naver.com>
2023-09-02 20:56:36 +0900

    feat: 회원가입 기능 추가

--- tomcat/src/main/java/org/apache/coyote/http11/handler/RegisterHandler.java
+++ tomcat/src/main/java/org/apache/coyote/http11/handler/RegisterHandler.java
@@ -1,7 +1,10 @@
 package org.apache.coyote.http11.handler;
 
 import java.io.IOException;
+import nextstep.jwp.db.InMemoryUserRepository;
+import nextstep.jwp.model.User;
 import org.apache.coyote.http11.ContentTypeParser;
+import org.apache.coyote.http11.httpmessage.Body;
 import org.apache.coyote.http11.httpmessage.Request;
 import org.apache.coyote.http11.httpmessage.Response;
 
@@ -9,6 +12,17 @@ public class RegisterHandler extends Handler {
 
     @Override
     public Response handle(Request request) throws IOException {
+        if (request.getMethod().equals("GET")) {
+            return responseWhenHttpMethodIsGet(request);
+        }
+        if (request.getMethod().equals("POST")) {
+            return responseWhenHttpMethodIsPost(request);
+        }
+
+        throw new IllegalArgumentException();
+    }
+
+    private Response responseWhenHttpMethodIsGet(Request request) throws IOException {
         String target = "register.html";
 
         String resource = findResourceWithPath(target);
@@ -17,5 +31,27 @@ public class RegisterHandler extends Handler {
 
         return Response.from(request.getHttpVersion(), "200 OK",
                 contentType, contentLength, resource);
+    }
+
+    private Response responseWhenHttpMethodIsPost(Request request) throws IOException {
+        saveUser(request);
+
+        String absolutePath = "index.html";
+        String resource = findResourceWithPath(absolutePath);
+        String contentType = ContentTypeParser.parse(absolutePath);
+        int contentLength = resource.getBytes().length;
+
+        return Response.from(request.getHttpVersion(), "302 FOUND",
+                contentType, contentLength, resource);
+    }
+
+    private static void saveUser(Request request) {
+        Body body = request.getBody();
+        String account = body.get("account");
+        String password = body.get("password");
+        String email = body.get("email");
+        User user = new User(account, password, email);
+
+        InMemoryUserRepository.save(user);
     }
 }

8ba90146f31501715ce520c056d6aa180f0ee15a
jjongwa <troas96@naver.com>
2023-09-02 16:10:43 +0900

    refactor: index.html 에 접근할 수 있도록 process 로직 변경

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,8 +5,14 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,8 +34,10 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
+            final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            final BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-            final var responseBody = "Hello world!";
+            final var responseBody = getResponseBody(bufferedReader);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -43,5 +51,29 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String getResponseBody(final BufferedReader bufferedReader) throws IOException {
+        final StringBuilder httpRequest = new StringBuilder();
+
+        String line = bufferedReader.readLine();
+        while (!"".equals(line)) {
+            if (line == null) {
+                break;
+            }
+            httpRequest.append(line);
+            line = bufferedReader.readLine();
+        }
+
+        final String requestUri = new String(httpRequest).split(" ")[1];
+
+        if (requestUri.equals("/")) {
+            return "Hello world!";
+        }
+
+        final var fileName = "static" + requestUri;
+        final URL resource = getClass().getClassLoader().getResource(fileName);
+        final Path path = new File(resource.getFile()).toPath();
+        return new String(Files.readAllBytes(path));
     }
 }

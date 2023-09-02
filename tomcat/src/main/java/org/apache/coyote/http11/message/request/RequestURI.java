4a1ff62dd118ec3468c17dbb69cd1f3f3d43058c
woosung1223 <dntjd991223@naver.com>
2023-09-03 02:36:10 +0900

    refactor: 사용하지 않는 메소드 삭제

--- tomcat/src/main/java/org/apache/coyote/http11/message/request/RequestURI.java
+++ tomcat/src/main/java/org/apache/coyote/http11/message/request/RequestURI.java
@@ -1,10 +1,5 @@
 package org.apache.coyote.http11.message.request;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
 public class RequestURI {
 
     private final String value;
@@ -22,18 +17,6 @@ public class RequestURI {
             return value.substring(0, value.indexOf("?"));
         }
         return value;
-    }
-
-    public Map<String, String> queryParameters() {
-        String[] split = value.substring(value.indexOf("?") + 1)
-                .split("&");
-
-        Map<String, String> queryParameters = new HashMap<>();
-        Arrays.stream(split)
-                .map(each -> each.split("="))
-                .forEach(each -> queryParameters.put(each[0], each[1]));
-
-        return Collections.unmodifiableMap(queryParameters);
     }
 
     public boolean hasQueryParameters() {

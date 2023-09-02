dbbddcdc4d962a72291c36827b29c352a1ef4704
hyena <goatsfish@gmail.com>
2023-09-02 23:18:44 +0900

    refactor: HttpHeader 내 매핑 필드 삭제

--- tomcat/src/main/java/org/apache/coyote/common/HttpHeader.java
+++ tomcat/src/main/java/org/apache/coyote/common/HttpHeader.java
@@ -1,11 +1,5 @@
 package org.apache.coyote.common;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
 public enum HttpHeader {
 
     ACCEPT("Accept"),
@@ -14,21 +8,10 @@ public enum HttpHeader {
     HOST("Host"),
     CONNECTION("Connection");
 
-    private static final Map<String, HttpHeader> mapping = new HashMap<>(collectAllHeaders());
-
-    private static Map<String, HttpHeader> collectAllHeaders() {
-        return Arrays.stream(HttpHeader.values())
-                .collect(Collectors.toMap(HttpHeader::source, Function.identity()));
-    }
-
     private final String source;
 
     HttpHeader(final String source) {
         this.source = source;
-    }
-
-    public static HttpHeader from(final String name) {
-        return mapping.get(name);
     }
 
     public String source() {

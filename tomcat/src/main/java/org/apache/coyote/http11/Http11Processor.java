648b4f7f10375827ad5ffc65b3a974db9d19eb46
kiarakim <kiarakim413@gmail.com>
2023-09-02 12:13:55 +0900

    feat: '/index.html' 요청 응답 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,12 +5,18 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final String LINE_BREAK = "\n";
 
     private final Socket connection;
 
@@ -29,7 +35,11 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            var responseBody = "Hello world!";
+
+            String[] headers = getHeader(inputStream).toString().split(LINE_BREAK);
+
+            responseBody = getResponseBody(headers, responseBody);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -43,5 +53,29 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String getResponseBody(String[] headers, String responseBody) throws IOException {
+        if (headers[0].contains("/index.html")) {
+            URL resourceURL = getClass()
+                    .getClassLoader()
+                    .getResource("static" + "/index.html");
+            Path path = new File(resourceURL.getFile()).toPath();
+            responseBody = new String(Files.readAllBytes(path));
+        }
+        return responseBody;
+    }
+
+    private static StringBuilder getHeader(InputStream inputStream) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int c;
+        while ((c = inputStream.read()) != -1) {
+            stringBuilder.append((char) c);
+            if (stringBuilder.toString().endsWith("\r\n\r\n")) {
+                break;
+            }
+        }
+        return stringBuilder;
     }
 }

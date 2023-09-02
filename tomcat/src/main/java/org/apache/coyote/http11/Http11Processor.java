e0606ffe65c620221af9beb62541a4098e55af0e
Youngho <cyh6099@gmail.com>
2023-09-02 17:37:28 +0900

    feat: html 파일 응답

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,8 +5,16 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,13 +36,37 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            String httpRequest = br.readLine();
+            String[] requestInfos = httpRequest.split(" ");
 
-            final var responseBody = "Hello world!";
+            String requestFile = requestInfos[1];
+
+            StringBuilder responseBody = new StringBuilder();
+            if (requestFile.equals("/")) {
+                responseBody.append("Hello world!");
+            } else {
+                ClassLoader classLoader = getClass().getClassLoader();
+                URL resource = classLoader.getResource("static" + requestFile);
+
+                File file = new File(resource.getFile());
+
+                Path path = Paths.get(file.getAbsolutePath());
+
+                InputStream fileInputStream = Files.newInputStream(path);
+                br = new BufferedReader(new InputStreamReader(fileInputStream));
+
+
+                String line;
+                while ((line = br.readLine()) != null) {
+                    responseBody.append(line).append('\n');
+                }
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "Content-Length: " + responseBody.toString().getBytes().length + " ",
                     "",
                     responseBody);
 

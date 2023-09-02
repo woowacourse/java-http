9e1615a9ed77320c0b9d2bb2a92061fd35c65120
hyena <goatsfish@gmail.com>
2023-09-02 23:31:03 +0900

    feat: 헤더 객체 구현

---
+++ tomcat/src/main/java/org/apache/coyote/common/Headers.java
@@ -0,0 +1,37 @@
+package org.apache.coyote.common;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Headers {
+
+    private static final String HEADER_DELIMITER = ":";
+    private static final int HEADER_NAME_INDEX = 0;
+    private static final int HEADER_VALUE_INDEX = 1;
+
+    private final Map<String, String> mapping = new HashMap<>();
+
+    public Headers(final List<String> headersWithValue) {
+        headersWithValue.stream()
+                .map(headerWithValue -> headerWithValue.split(HEADER_DELIMITER))
+                .forEach(header -> mapping.put(header[HEADER_NAME_INDEX].trim(), header[HEADER_VALUE_INDEX].trim()));
+    }
+
+    public Headers(final Map<String, String> headers) {
+        mapping.putAll(headers);
+    }
+
+    public String getHeaderValue(final String name) {
+        return mapping.getOrDefault(name, null);
+    }
+
+    public List<String> headerNames() {
+        return mapping.keySet()
+                .stream()
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.toList());
+    }
+}

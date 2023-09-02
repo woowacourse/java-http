0f6aa13feb8a59f376f164e0eb36a106946847fb
greeng00se <thegreengoose7@gmail.com>
2023-09-02 16:32:17 +0900

    feat: RequestBody 클래스 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/RequestBody.java
@@ -0,0 +1,49 @@
+package org.apache.coyote.http11;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toMap;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestBody {
+
+    private static final String SEPARATOR = "&";
+    private static final String DELIMITER = "=";
+    private static final int KEY_INDEX = 0;
+    private static final int VALUE_INDEX = 1;
+
+    private final Map<String, String> items = new HashMap<>();
+
+    private RequestBody() {
+    }
+
+    private RequestBody(final Map<String, String> items) {
+        this.items.putAll(items);
+    }
+
+    public static RequestBody empty() {
+        return new RequestBody();
+    }
+
+    public static RequestBody from(final String body) {
+        if (body.isEmpty()) {
+            return new RequestBody();
+        }
+        return Arrays.stream(body.split(SEPARATOR))
+                .map(field -> field.split(DELIMITER))
+                .collect(collectingAndThen(
+                        toMap(field -> field[KEY_INDEX], field -> field[VALUE_INDEX]),
+                        RequestBody::new
+                ));
+    }
+
+    public String get(final String key) {
+        return items.get(key);
+    }
+
+    public Map<String, String> getItems() {
+        return items;
+    }
+}

4cb9a8c6acadae4aaf0cb9fc36323fc2ad88fb71
junpakPark <junpak.park@gmail.com>
2023-09-02 19:22:55 +0900

    test: FileTest path에서 getResource로 방법 변경

--- study/src/test/java/study/FileTest.java
+++ study/src/test/java/study/FileTest.java
@@ -2,11 +2,15 @@ package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Objects;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -28,37 +32,31 @@ class FileTest {
     void resource_디렉터리에_있는_파일의_경로를_찾는다() {
         final String fileName = "nextstep.txt";
 
-        Path path = Paths.get("src/test/resources")
-                .resolve(fileName);
         // todo
-        final String actual = path.toString();
+        URL resource = getClass().getClassLoader().getResource(fileName);
+
+        File file = new File(Objects.requireNonNull(resource).getPath());
+
+        String actual = file.getName();
 
         assertThat(actual).endsWith(fileName);
     }
 
     /**
      * 파일 내용 읽기
-     *
-     * 읽어온 파일의 내용을 I/O Stream을 사용해서 사용자에게 전달 해야 한다.
-     * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
+     * <p>
+     * 읽어온 파일의 내용을 I/O Stream을 사용해서 사용자에게 전달 해야 한다. File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws URISyntaxException, IOException {
         final String fileName = "nextstep.txt";
 
         // todo
-        final Path path = Paths.get("src/test/resources")
-                .resolve(fileName);
-
-        String read;
-        try {
-            read = Files.readString(path);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        URL resource = getClass().getClassLoader().getResource(fileName);
+        final Path path = Paths.get(Objects.requireNonNull(resource).toURI());
 
         // todo
-        final List<String> actual = List.of(read);
+        final List<String> actual = Files.readAllLines(path);
 
         assertThat(actual).containsOnly("nextstep");
     }

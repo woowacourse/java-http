c7d2a48923a9c0af43cce4e8b1b80b88ff9a30a4
hong-sile <gurwns9325@naver.com>
2023-09-02 17:45:45 +0900

    feat: index.html 반환하는 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,47 +1,66 @@
 package org.apache.coyote.http11;
 
-import nextstep.jwp.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.List;
+
 import org.apache.coyote.Processor;
+import org.apache.coyote.http11.handler.HttpHandler;
+import org.apache.coyote.http11.handler.IndexHtmlHandler;
+import org.apache.coyote.http11.handler.NotFoundHandler;
+import org.apache.coyote.http11.handler.RootHandler;
+import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.response.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
+import nextstep.jwp.exception.UncheckedServletException;
 
 public class Http11Processor implements Runnable, Processor {
 
-    private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+	private static final List<HttpHandler> handlers = List.of(
+		new IndexHtmlHandler()
+	);
+	private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
 
-    private final Socket connection;
+	private final Socket connection;
 
-    public Http11Processor(final Socket connection) {
-        this.connection = connection;
-    }
+	public Http11Processor(final Socket connection) {
+		this.connection = connection;
+	}
 
-    @Override
-    public void run() {
-        log.info("connect host: {}, port: {}", connection.getInetAddress(), connection.getPort());
-        process(connection);
-    }
+	@Override
+	public void run() {
+		log.info("connect host: {}, port: {}", connection.getInetAddress(), connection.getPort());
+		process(connection);
+	}
 
-    @Override
-    public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+	@Override
+	public void process(final Socket connection) {
+		try (final var inputStream = connection.getInputStream();
+			 final var outputStream = connection.getOutputStream()) {
+			final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+			final StringBuilder builder = new StringBuilder();
+			while (reader.ready()) {
+				builder.append(reader.readLine());
+				builder.append(System.lineSeparator());
+			}
 
-            final var responseBody = "Hello world!";
+			final HttpRequest request = HttpRequest.from(builder.toString());
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+			final HttpHandler findHandler = handlers.stream()
+				.filter(handler -> handler.isSupported(request))
+				.findAny()
+				.orElseGet(NotFoundHandler::new);
+			final HttpResponse response = findHandler
+				.handleTo(request);
 
-            outputStream.write(response.getBytes());
-            outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
-            log.error(e.getMessage(), e);
-        }
-    }
+			outputStream.write(response.buildResponse().getBytes());
+			outputStream.flush();
+		} catch (IOException | UncheckedServletException e) {
+			log.error(e.getMessage(), e);
+		}
+	}
 }

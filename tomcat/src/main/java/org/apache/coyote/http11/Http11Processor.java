eaf05ed2bd82c5fdcb14a15f527895427ddb4405
aiaiaiai1 <cvc100326@naver.com>
2023-09-02 20:34:33 +0900

    feat : GET /index.html 응답하기 +  CSS 지원하기

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,16 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,15 +32,35 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String requestLine = bufferedReader.readLine();
+            String[] requests = requestLine.split(" ");
 
-            final var responseBody = "Hello world!";
+            String httpMethod = requests[0];
+            String httpUrl = requests[1];
+            String fileName = httpUrl.substring(httpUrl.lastIndexOf('/') + 1);
+            String response = null;
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            if (httpMethod.equals("GET") && httpUrl.equals("/")) {
+                response = createResponse("text/plain;", "Hello world!");
+            }
+
+            if (httpMethod.equals("GET") && fileName.equals("index.html")) {
+                response = createResponse("text/html", readFile("static", fileName));
+            }
+
+            if (httpMethod.equals("GET") && fileName.equals("styles.css")) {
+                response = createResponse("text/css", readFile("static/css", fileName));
+            }
+
+            if (httpMethod.equals("GET") && fileName.endsWith(".js") && !fileName.equals("scripts.js")) {
+                response = createResponse("text/javascript", readFile("static/assets", fileName));
+            }
+
+            if (httpMethod.equals("GET") && fileName.equals("scripts.js")) {
+                response = createResponse("text/javascript", readFile("static/js", fileName));
+            }
 
             outputStream.write(response.getBytes());
             outputStream.flush();
@@ -44,4 +68,20 @@ public class Http11Processor implements Runnable, Processor {
             log.error(e.getMessage(), e);
         }
     }
+
+    private String createResponse(String contentType, String responseBody) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: " + contentType + ";charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+    }
+
+    private String readFile(String directory, String fileName) throws IOException {
+        URL resource = getClass().getClassLoader().getResource(directory + "/" + fileName);
+        Path path = Path.of(resource.getPath());
+        return Files.readString(path);
+    }
+
 }

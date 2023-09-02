21b0885c2b42fc70251bf92435af0704d453d31c
yoondgu <doyou2ing@naver.com>
2023-09-03 05:10:12 +0900

    feat: 정적 파일에 대한 응답 구현

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -1,13 +1,12 @@
 package org.apache.catalina.connector;
 
-import org.apache.coyote.http11.Http11Processor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import org.apache.coyote.http11.Http11Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Connector implements Runnable {
 
@@ -50,6 +49,7 @@ public class Connector implements Runnable {
     public void run() {
         // 클라이언트가 연결될때까지 대기한다.
         while (!stopped) {
+            log.info("wait for connect client");
             connect();
         }
     }
@@ -57,6 +57,7 @@ public class Connector implements Runnable {
     private void connect() {
         try {
             process(serverSocket.accept());
+            log.info("socket accepted");
         } catch (IOException e) {
             log.error(e.getMessage(), e);
         }
@@ -68,6 +69,7 @@ public class Connector implements Runnable {
         }
         var processor = new Http11Processor(connection);
         new Thread(processor).start();
+        log.info("thread start");
     }
 
     public void stop() {
--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,20 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,7 +37,14 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            List<String> requestLines = extractRequestLines(inputStream);
+            String requestURI = extractResourceName(requestLines);
+            URL resource = getClass().getClassLoader().getResource("static/" + requestURI);
+
+            assert resource != null;
+            final Path path = Paths.get(resource.getPath());
+            List<String> contents = Files.readAllLines(path);
+            final var responseBody = String.join("", contents);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -43,5 +58,26 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private List<String> extractRequestLines(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        List<String> requestLines = new ArrayList<>();
+
+        String line = bufferedReader.readLine();
+        if (line == null) {
+            return requestLines;
+        }
+        while (!"".equals(line)) {
+            requestLines.add(line);
+            log.info("request line {}", line);
+            line = bufferedReader.readLine();
+        }
+        return requestLines;
+    }
+
+    private String extractResourceName(List<String> requestLines) {
+        String requestHead = requestLines.get(0);
+        return requestHead.split(" ")[1];
     }
 }

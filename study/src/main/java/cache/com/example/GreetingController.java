02d4da1d8d4ea2eada58f5e235d13f58850966bd
eunbii0213 <eunbii0213@naver.com>
2023-09-02 20:41:43 +0900

    feat: GET /index.html 응답 기능 구현

--- study/src/main/java/cache/com/example/GreetingController.java
+++ study/src/main/java/cache/com/example/GreetingController.java
@@ -1,11 +1,18 @@
 package cache.com.example;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 @Controller
 public class GreetingController {
@@ -13,6 +20,24 @@ public class GreetingController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/index.html")
+    public String accessIndexHtml() {
+        return "index";
+    }
+
+    @GetMapping("/css/styles.css")
+    @ResponseBody
+    public String getCss() throws IOException {
+        System.out.println("call");
+        Resource resource = new ClassPathResource("static/css/styles.css");
+        InputStream inputStream = resource.getInputStream();
+
+        try (Scanner scanner = new Scanner(inputStream, StandardCharsets.UTF_8.name())) {
+            scanner.useDelimiter("\\A"); // 전체 내용을 읽기 위해 구분자 설정
+            return scanner.hasNext() ? scanner.next() : "";
+        }
     }
 
     /**
--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,7 +5,10 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
@@ -29,7 +32,11 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            // 파싱된 HTTP 요청에서 경로 추출
+            final String path = parseHttpRequest(inputStream);
+
+            // 경로를 기반으로 정적 파일을 읽고 응답 생성
+            final String responseBody = readStaticFile(path);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -42,6 +49,46 @@ public class Http11Processor implements Runnable, Processor {
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        }
+    }
+
+    private String parseHttpRequest(InputStream inputStream) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String requestLine = reader.readLine(); // HTTP 요청 라인을 읽음 (예: "GET /index.html HTTP/1.1")
+
+        // 요청 라인을 공백으로 분리하여 경로를 추출
+        String[] requestParts = requestLine.split(" ");
+
+        if (requestParts.length >= 2) {
+            return requestParts[1]; // 두 번째 요소가 경로 (예: "/index.html")
+        } else {
+            throw new IOException("Invalid HTTP request"); // 유효하지 않은 요청 처리
+        }
+    }
+
+    private String readStaticFile(String path) throws IOException {
+        // 경로를 기반으로 정적 파일을 읽고 그 내용을 반환하는 로직을 작성해야 합니다.
+        // 이 예제에서는 간단하게 파일을 읽어오는 방법을 보여줍니다.
+
+        if(path.equals("/")) {
+            return "Hello world!";
+        }
+
+        // 클래스 패스에서 정적 파일을 읽을 수 있도록 리소스 로더를 사용
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("static" + path);
+
+        if (inputStream != null) {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+                StringBuilder content = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    content.append(line).append("\n");
+                }
+                return content.toString();
+            }
+        } else {
+            throw new IOException("Static file not found: " + path); // 파일이 없는 경우 처리
         }
     }
 }

6663ca75b4908afe880b877b9bd564827a3ff918
dooboocookie <bkh3030@gmail.com>
2023-09-02 20:58:54 +0900

    feat: CSS 지원하기 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -11,8 +11,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -37,28 +36,42 @@ public class Http11Processor implements Runnable, Processor {
              final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
              final BufferedReader bufferedReader = new BufferedReader(inputStreamReader);) {
 
-            final List<String> requestHeaders = new ArrayList<>();
+            final List<String> requestHeaderLines = new ArrayList<>();
 
             String nextLine;
             while (!"".equals(nextLine = bufferedReader.readLine())) {
                 if (nextLine == null) {
                     return;
                 }
-                requestHeaders.add(nextLine);
+                requestHeaderLines.add(nextLine);
             }
 
-            final String requestFirstLine = requestHeaders.get(0);
+            Map<String, String> requestHeaders = new HashMap<>();
+            for (int i = 1; i < requestHeaderLines.size(); i++) {
+                String requestHeader = requestHeaderLines.get(i);
+                String[] requestHeaderNameAndValue = requestHeader.split(":");
+
+                String requestHeaderName = requestHeaderNameAndValue[0].trim().toLowerCase();
+                String requestHeaderValue = requestHeaderNameAndValue[1].trim().toLowerCase();
+                requestHeaders.put(requestHeaderName, requestHeaderValue);
+            }
+
+            final String requestFirstLine = requestHeaderLines.get(0);
 
             final String[] requestFirstLineElements = requestFirstLine.split(" ");
             final String requestMethod = requestFirstLineElements[0];
             final String requestUri = requestFirstLineElements[1];
             final String requestProtocol = requestFirstLineElements[2];
 
+            log.info("method={}\r\nuri={}\r\nprotocol={}", requestMethod, requestUri, requestProtocol);
+
+            String responseContentType = getResponseContentType(requestHeaders, requestUri);
+
             final String responseBody = getResponseBody(requestUri);
 
             final String response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Type: " + responseContentType + " ",
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
                     responseBody);
@@ -68,6 +81,18 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String getResponseContentType(final Map<String, String> requestHeaders, final String requestUri) {
+        String requestAcceptHeader = requestHeaders.getOrDefault("accept", "");
+        String responseFileExtension = requestUri.substring(requestUri.indexOf(".") + 1);
+        if ("text/css".equals(requestAcceptHeader) || "css".equals(responseFileExtension)) {
+            return  "text/css,*/*;q=0.1";
+        }
+        if ("application/javascript".equals(requestAcceptHeader) || "js".equals(responseFileExtension)) {
+            return "application/javascript;charset=utf-8";
+        }
+        return "text/html;charset=utf-8";
     }
 
     private String getResponseBody(final String requestUri) throws URISyntaxException, IOException {

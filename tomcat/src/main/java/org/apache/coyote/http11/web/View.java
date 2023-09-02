aac19b1f0322c64be877cff5e24dac951f8d923a
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-09-02 16:14:40 +0900

    feat: 정적 리소스 반환 로직 추가

--- tomcat/src/main/java/org/apache/coyote/http11/web/View.java
+++ tomcat/src/main/java/org/apache/coyote/http11/web/View.java
@@ -1,10 +1,38 @@
 package org.apache.coyote.http11.web;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
 public class View {
+
+    private static final String CONTENT_TYPE = "text/html;charset=utf-8";
+    private static final String RESOURCE_DIRECTORY = "static/";
 
     private final String viewName;
 
     public View(String viewName) {
         this.viewName = viewName;
+    }
+
+    public String renderView() throws IOException {
+        final URL resource = findStaticResource();
+        if (resource == null) {
+            throw new IllegalArgumentException("해당하는 뷰가 존재하지 않습니다.");
+        }
+        final Path path = Path.of(Objects.requireNonNull(resource).getFile());
+
+        return String.join("\r\n", Files.readAllLines(path, StandardCharsets.UTF_8));
+    }
+
+    private URL findStaticResource() {
+        return getClass().getClassLoader().getResource(RESOURCE_DIRECTORY + viewName);
+    }
+
+    public String getContentType() {
+        return CONTENT_TYPE;
     }
 }
---
+++ tomcat/src/test/java/org/apache/coyote/http11/web/ViewTest.java
@@ -0,0 +1,36 @@
+package org.apache.coyote.http11.web;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayName("View 테스트")
+class ViewTest {
+
+    @Test
+    void 지정한_리소스를_읽는다() throws IOException {
+        // given
+        final View view = new View("index.html");
+
+        // when
+        final String resource = view.renderView();
+
+        // then
+        assertThat(resource).contains("<title>대시보드</title>");
+    }
+
+    @Test
+    void 지정한_리소스가_존재하지_않으면_예외발생() {
+        // given
+        final View view = new View("notfoundasdfas.html");
+
+        // when & then
+        Assertions.assertThatThrownBy(view::renderView)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당하는 뷰가 존재하지 않습니다.");
+    }
+}

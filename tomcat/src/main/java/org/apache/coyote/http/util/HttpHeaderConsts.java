a0c651095ab262313fdd0bf8e25cac765d7224e7
apptie <appti@naver.com>
2023-09-04 00:18:55 +0900

    feat: HTTP 헤더 관련 상수 클래스 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http/util/HttpHeaderConsts.java
@@ -0,0 +1,11 @@
+package org.apache.coyote.http.util;
+
+public final class HttpHeaderConsts {
+
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String CONTENT_LENGTH = "Content-Length";
+    public static final String ACCEPT = "Accept";
+
+    private HttpHeaderConsts() {
+    }
+}

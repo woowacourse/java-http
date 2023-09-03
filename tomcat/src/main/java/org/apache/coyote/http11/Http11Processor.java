3da9aba69bd7eb4d868ca3b2ab8818e834571932
HyeonjaeKwon <kwonhyeonjae01@gmail.com>
2023-09-03 16:24:07 +0900

    feat: 리소스 파일 반환 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,47 +1,71 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
-
 public class Http11Processor implements Runnable, Processor {
 
-    private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+  private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+  private static final String PREFIX_STATIC_PATH = "tomcat/src/main/resources/static";
+  private static final String NEW_LINE = "\r\n";
 
-    private final Socket connection;
+  private final Socket connection;
 
-    public Http11Processor(final Socket connection) {
-        this.connection = connection;
+  public Http11Processor(final Socket connection) {
+    this.connection = connection;
+  }
+
+  @Override
+  public void run() {
+    log.info("connect host: {}, port: {}", this.connection.getInetAddress(),
+        this.connection.getPort());
+    process(this.connection);
+  }
+
+  @Override
+  public void process(final Socket connection) {
+    try (final var inputStream = bufferingInputStream(connection.getInputStream());
+        final var outputStream = connection.getOutputStream()
+    ) {
+      final String request = inputStream.readLine();
+      final String url = request.split(" ")[1];
+
+      final String responseBody = readContentsFromFile(url);
+
+      final var response = String.join(NEW_LINE,
+          "HTTP/1.1 200 OK ",
+          "Content-Type: text/html;charset=utf-8 ",
+          "Content-Length: " + responseBody.getBytes().length + " ",
+          "",
+          responseBody);
+
+      outputStream.write(response.getBytes());
+      outputStream.flush();
+    } catch (final IOException | UncheckedServletException e) {
+      log.error(e.getMessage(), e);
     }
+  }
 
-    @Override
-    public void run() {
-        log.info("connect host: {}, port: {}", connection.getInetAddress(), connection.getPort());
-        process(connection);
-    }
+  private BufferedReader bufferingInputStream(final InputStream inputStream) {
+    final InputStreamReader reader = new InputStreamReader(inputStream);
+    return new BufferedReader(reader);
+  }
 
-    @Override
-    public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+  private String readContentsFromFile(final String url) throws IOException {
+    final Path path = Paths.get(PREFIX_STATIC_PATH + url);
+    final List<String> contents = Files.readAllLines(path);
 
-            final var responseBody = "Hello world!";
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
-
-            outputStream.write(response.getBytes());
-            outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
-            log.error(e.getMessage(), e);
-        }
-    }
+    return String.join(NEW_LINE, contents);
+  }
 }

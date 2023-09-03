579107493c4c2e89d00a548497bb20ff1e7bf223
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-09-03 17:44:44 +0900

    feat: 요청의 RequestLine 읽고 생성한다

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,7 +1,15 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
+import org.apache.coyote.http11.request.headers.RequestHeaders;
+import org.apache.coyote.http11.request.line.RequestLine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,7 +18,10 @@ import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
-    private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final Logger LOG = LoggerFactory.getLogger(Http11Processor.class);
+    private static final String INDEX_PAGE = "/index.html";
+    private static final String REGISTER_PAGE = "/register.html";
+    private static final String LOGIN_PAGE = "/login.html";
 
     private final Socket connection;
 
@@ -20,28 +31,46 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void run() {
-        log.info("connect host: {}, port: {}", connection.getInetAddress(), connection.getPort());
+        LOG.info("connect host: {}, port: {}", connection.getInetAddress(), connection.getPort());
         process(connection);
     }
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+        try (final InputStream inputStream = connection.getInputStream();
+             final OutputStream outputStream = connection.getOutputStream();
+             final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+            final String firstLine = bufferedReader.readLine();
+            if (firstLine == null) {
+                return;
+            }
+            final RequestLine requestLine = RequestLine.from(firstLine);
+//            final RequestHeaders requestHeader = getHeaders(bufferedReader);
+//            final RequestBody requestBody = readBody(bufferedReader, requestHeader);
+//
+//            final ResponseEntity responseEntity = handleRequest(requestLine, requestHeader, requestBody);
 
-            final var responseBody = "Hello world!";
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
-
-            outputStream.write(response.getBytes());
+//            final String response = httpResponseGenerator.generate(responseEntity);
+//            outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
-            log.error(e.getMessage(), e);
+            LOG.error(e.getMessage(), e);
         }
     }
+
+    private RequestHeaders getHeaders(final BufferedReader bufferedReader) throws IOException {
+        final List<String> requestHeaders = readRequestHeaders(bufferedReader);
+        return RequestHeaders.from(requestHeaders);
+    }
+
+    private List<String> readRequestHeaders(final BufferedReader bufferedReader) throws IOException {
+        final List<String> requestHeaders = new ArrayList<>();
+        String line;
+
+        while ((line = bufferedReader.readLine()) != null && !line.isEmpty()) {
+            requestHeaders.add(line);
+        }
+        return requestHeaders;
+    }
+
 }
--- tomcat/src/main/java/org/apache/coyote/http11/request/line/RequestLine.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/line/RequestLine.java
@@ -1,5 +1,8 @@
 package org.apache.coyote.http11.request.line;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class RequestLine {
 
     private final HttpMethod method;
@@ -12,7 +15,11 @@ public class RequestLine {
         this.protocol = protocol;
     }
 
-    public static RequestLine of(HttpMethod method, Path path, Protocol protocol) {
+    public static RequestLine from(String requestLine) {
+        List<String> requestLineElements = Arrays.asList(requestLine.split(" "));
+         HttpMethod method = HttpMethod.from(requestLineElements.get(0));
+         Path path = Path.from(requestLineElements.get(1));
+         Protocol protocol = Protocol.from(requestLineElements.get(2));
         return new RequestLine(method, path, protocol);
     }
 

01777dcaea1065d9d2eb51127b9d03bee489fa5b
BGuga <tmdwns9913@gmail.com>
2023-09-03 15:51:19 +0900

    feat: Method 구현

---
+++ tomcat/src/main/java/org/apache/coyote/http11/Method.java
@@ -0,0 +1,26 @@
+package org.apache.coyote.http11;
+
+import java.util.Arrays;
+
+public enum Method {
+    GET("GET"),
+    POST("POST"),
+    PUT("PUT"),
+    DELETE("DELETE");
+
+    private final String methodName;
+
+    Method(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public static Method of(String targetMethod) {
+        if (targetMethod == null) {
+            throw new IllegalArgumentException("null 에 대한 메서드는 존재하지 않습니다.");
+        }
+        return Arrays.stream(Method.values())
+            .filter(method -> targetMethod.toUpperCase().equals(method.methodName))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 메서드 입니다."));
+    }
+}

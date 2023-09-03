4b1065242976d171ce1b6912a37c82c8ef109fa3
amaran-th <songsy405@gmail.com>
2023-09-03 19:41:38 +0900

    feat: 1단계 1, 2번 요구사항 구현
    - index.html 페이지 요청에 대한 처리 구현

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # 톰캣 구현하기
+
+## 1단계
+
+- [x] GET /index.html에 대해 응답하도록 한다.
+    - [x] 요청의 첫 라인을 읽어오도록 파싱한다.
+        - [x] request uri를 추출한다.
+        - [x] 요청 url에 해당되는 파일을 resource 디렉토리에서 읽게 한다.
+    - [x] 요청의 나머지 헤더를 읽어오도록 파싱한다.
+    - [x] line이 null인 경우 예외처리를 한다.
+    - [x] 존재하지 않는 페이지를 요청할 경우 404.html을 반환한다.
+- [x] 사용자가 페이지를 열었을 때 CSS 파일도 호출하도록 한다.
+    - [x] 요청 파일의 확장자가 css, 또는 요청 헤더의 Accept가 text/css인 경우 응답 헤더의 Content-Type을 text/css로 전송한다.
+- [ ] Query String을 파싱하게 한다.
+    - [ ] request의 첫 라인에서 추출한 uri에서 쿼리 데이터를 추출한다.
+    - [ ] InMemoryUserRepository를 사용해 미리 가입되어 있는 회원을 조회하고 로그로 출력한다.
+
+## 2단계
+
+- [ ] 로그인 여부에 따라 다른 페이지로 이동시킨다.
+    - [ ] /login 페이지에서 로그인에 성공하면 http status code를 302로 반환하고 /index.html로 리다이렉트한다.
+    - [ ] /login 페이지에서 로그인에 실패하면 401.html로 리다이렉트한다.
+- [ ] POST 방식으로 회원가입
+    - [ ] `http://localhost:8080/register` 으로 접속하면 회원가입 페이지(register.html)를 보여준다.
+    - [ ] POST 요청에 대해 별도의 처리를 하도록 로직을 분리한다.
+    - [ ] 회원가입이 완료되면 index.html로 리다이렉트한다.
+    - [ ] 로그인 페이지에서 버튼을 눌렀을 때 POST 방식으로 전송하도록 변경한다.
+- [ ] Cookie에 JSESSIONID 값을 저장한다.
+    - [ ] 세션 아이디를 전달하는 이름으로 `JSESSIONID`를 사용한다.
+    - [ ] 서버에서 HTTP 응답을 전달할 때 응답 헤더에 Set-Cookie를 추가하여 값을 전달한다.
+    - [ ] Cookie 클래스 구현
+    - [ ] 요청 헤더의 Cookie에 JSESSIONID가 없으면 응답 헤더에 Set-Cookie를 반환하게 한다.
+- [ ] Session 구현
+    - [ ] 쿠키에서 전달받은 JSESIONID의 값으로 로그인 여부를 체크한다.
+    - [ ] 로그인에 성공하면 Session 객체의 값으로 User 객체를 저장한다.
+    - [ ] 로그인된 상태에서 /login 페이지에 GET method로 접근하면 index.html 페이지로 리다이렉트한다.
+
+## 3단계
+
+## 4단계
--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,47 +1,109 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
-
 public class Http11Processor implements Runnable, Processor {
 
-    private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+  private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
 
-    private final Socket connection;
+  private final Socket connection;
 
-    public Http11Processor(final Socket connection) {
-        this.connection = connection;
+  public Http11Processor(final Socket connection) {
+    this.connection = connection;
+  }
+
+  @Override
+  public void run() {
+    log.info("connect host: {}, port: {}", connection.getInetAddress(), connection.getPort());
+    process(connection);
+  }
+
+  @Override
+  public void process(final Socket connection) {
+    try (final var inputStream = connection.getInputStream();
+        final var outputStream = connection.getOutputStream()) {
+      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+      final String startLine = extractStartLine(bufferedReader);
+      if (startLine == null) {
+        return;
+      }
+      final Map<String, String> requestHeaders = extractHeader(bufferedReader);
+
+      final String response = handleRequest(startLine, requestHeaders);
+
+      outputStream.write(response.getBytes());
+      outputStream.flush();
+    } catch (IOException | UncheckedServletException | URISyntaxException e) {
+      log.error(e.getMessage(), e);
+    }
+  }
+
+  private String extractStartLine(final BufferedReader bufferedReader) throws IOException {
+    return bufferedReader.readLine();
+  }
+
+  private Map<String, String> extractHeader(final BufferedReader bufferedReader)
+      throws IOException {
+    Map<String, String> headers = new HashMap<>();
+    String line = bufferedReader.readLine();
+    while (!"".equals(line)) {
+      String[] tokens = line.split(": ");
+      headers.put(tokens[0], tokens[1]);
+      line = bufferedReader.readLine();
+    }
+    return headers;
+  }
+
+  private String handleRequest(final String startLine, final Map<String, String> headers)
+      throws URISyntaxException, IOException {
+    String responseBody = "";
+    String contentType = "text/html";
+    final List<String> startLineTokens = List.of(startLine.split(" "));
+    if (startLineTokens.get(0).equals("GET")) {
+      responseBody = handleGetRequest(startLineTokens);
+      if (isRequestCssFile(headers, startLineTokens.get(1).split("."))) {
+        contentType = "text/css";
+      }
     }
 
-    @Override
-    public void run() {
-        log.info("connect host: {}, port: {}", connection.getInetAddress(), connection.getPort());
-        process(connection);
-    }
+    return String.join("\r\n",
+        "HTTP/1.1 200 OK ",
+        "Content-Type: " + contentType + ";charset=utf-8 ",
+        "Content-Length: " + responseBody.getBytes().length + " ",
+        "",
+        responseBody);
+  }
 
-    @Override
-    public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+  private boolean isRequestCssFile(final Map<String, String> headers, final String[] tokens) {
+    return (tokens.length >= 1 && tokens[tokens.length - 1].equals("css")) || headers.get("Accept")
+        .contains("text/css");
+  }
 
-            final var responseBody = "Hello world!";
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
-
-            outputStream.write(response.getBytes());
-            outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
-            log.error(e.getMessage(), e);
-        }
-    }
+  private String handleGetRequest(final List<String> tokens)
+      throws URISyntaxException, IOException {
+    final URL filePath = Optional.ofNullable(getClass().getResource("/static" + tokens.get(1)))
+        .orElse(getClass().getResource("/static/404.html"));
+    final Path path = Paths.get(Objects.requireNonNull(filePath).toURI());
+    Charset charset = StandardCharsets.UTF_8;
+    return String.join(System.lineSeparator(), Files.readAllLines(path, charset));
+  }
 }

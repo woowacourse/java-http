d38ab6a1efafd3808e7afd40626344ceb52f0e14
hum02 <ehdgma02@gmail.com>
2023-09-03 18:30:55 +0900

    feat:정적 리소스 응답 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,13 +5,20 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.Objects;
 
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
-
+    private static final String RESOURCE_PATH = "static";
+    private static final int URL_INDEX = 1;
     private final Socket connection;
 
     public Http11Processor(final Socket connection) {
@@ -27,21 +34,42 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+             final BufferedReader br = new BufferedReader(inputStreamReader)) {
 
-            final var responseBody = "Hello world!";
+            String requestLine = br.readLine();
+            if (Objects.isNull(requestLine)) {
+                return;
+            }
+            String requestUrl = RESOURCE_PATH + requestLine.split(" ")[URL_INDEX];
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
-
+            URL resource = getClass().getClassLoader().getResource(requestUrl);
+            String response = generateResponse(resource);
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private static String generateResponse(URL resource) throws IOException {
+        File location = new File(resource.getFile());
+        String responseBody = "";
+
+        if (location.isDirectory()) {
+            responseBody = "Hello world!";
+        }
+
+        if (location.isFile()) {
+            responseBody = new String(Files.readAllBytes(location.toPath()));
+        }
+
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
     }
 }

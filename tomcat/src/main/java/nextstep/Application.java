4e9dd7acab0b18ba188a4fa6ccab2d26fc59e26c
gitchan <eunkeeeee@gmail.com>
2023-09-03 22:07:59 +0900

    feat: Application 실행 로그 추가

--- tomcat/src/main/java/nextstep/Application.java
+++ tomcat/src/main/java/nextstep/Application.java
@@ -1,10 +1,15 @@
 package nextstep;
 
 import org.apache.catalina.startup.Tomcat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Application {
 
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
+        log.info("START WEB SERVER");
         final var tomcat = new Tomcat();
         tomcat.start();
     }

9bb7106716fe61ba11a5c4dc33074f6d102fd5ce
Youngho <cyh6099@gmail.com>
2023-09-03 14:30:35 +0900

    feat: reqeust객체 구현

---
+++ tomcat/src/main/java/org/apache/coyote/http11/http/HttpRequest.java
@@ -0,0 +1,16 @@
+package org.apache.coyote.http11.http;
+
+public class HttpRequest {
+
+    private final RequestFirstLine requestFirstLine;
+    private final RequestHeader requestHeader;
+
+    public HttpRequest(RequestFirstLine requestFirstLine, RequestHeader requestHeader) {
+        this.requestFirstLine = requestFirstLine;
+        this.requestHeader = requestHeader;
+    }
+
+    public String getRequestUri() {
+        return requestFirstLine.getRequestUri();
+    }
+}

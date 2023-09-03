5d37c15052921c01ebfa46b2596d34dc0bf31f9c
zillionme <beatus96@naver.com>
2023-09-04 06:22:00 +0900

    feat: 로그인 세션 유지 기능 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -4,6 +4,9 @@ import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.exception.UncheckedServletException;
 import nextstep.jwp.model.User;
 import org.apache.coyote.Processor;
+import org.apache.coyote.http11.common.Cookie;
+import org.apache.coyote.http11.common.Session;
+import org.apache.coyote.http11.common.SessionManager;
 import org.apache.coyote.http11.request.RequestBody;
 import org.apache.coyote.http11.request.RequestHeader;
 import org.apache.coyote.http11.request.RequestLine;
@@ -33,22 +36,6 @@ public class Http11Processor implements Runnable, Processor {
         process(connection);
     }
 
-    //    @Override
-//    public void process(final Socket connection) {
-//        try (final BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//             final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()))) {
-//
-//            RequestLine requestLine = readRequestLine(reader);
-//            RequestHeader requestHeader = readHeader(reader);
-//            String requestBody = readRequestBody(reader, requestHeader);
-//
-//            String response = ResponseGenerator.generate(requestLine, requestHeader, requestBody);
-//            writer.write(response);
-//            writer.flush();
-//        } catch (IOException | UncheckedServletException e) {
-//            log.error(e.getMessage(), e);
-//        }
-//    }
     private static final String DEFAULT_PAGE = "/index.html";
     private static final String DEFAULT = "html";
 
@@ -72,7 +59,7 @@ public class Http11Processor implements Runnable, Processor {
         }
     }
 
-    private static Response getResponse(final RequestLine requestLine, final RequestHeader requestHeader, RequestBody requestBody) {
+    private Response getResponse(final RequestLine requestLine, final RequestHeader requestHeader, RequestBody requestBody) {
         String requestUrl = requestLine.getRequestUrl();
         // "/"인 경우
         if (requestUrl.equals("/") && requestLine.getRequestMethod().equals("GET")) {
@@ -98,22 +85,22 @@ public class Http11Processor implements Runnable, Processor {
         return responseStaticFile(requestUrl, requestHeader);
     }
 
-    private static Response login(final RequestHeader requestHeader, final RequestBody requestBody) {
+    private Response login(final RequestHeader requestHeader, final RequestBody requestBody) {
         try {
-            if (requestBody == null) {
-                return Response.redirection("401.html");
+            Session savedSession = findSession(requestHeader);
+            if(savedSession!=null) {
+                return Response.redirection("index.html");
             }
             String account = requestBody.getContentValue("account");
             String password = requestBody.getContentValue("password");
-            User user = InMemoryUserRepository.findByAccount(account).orElseThrow(() -> new IllegalArgumentException()
-            );//todo : 해당하는 계정이 존재하지 않는다
+            User user = InMemoryUserRepository.findByAccount(account)
+                    .orElseThrow(() -> new IllegalArgumentException());//todo : 해당하는 계정이 존재하지 않는다
             if (!user.checkPassword(password)) {
                 throw new RuntimeException(); //todo :비밀번호가 일치하지 않는다.
             }
             Response response = Response.redirection("/index.html");
-            if(!requestHeader.getHeaderValue("Cookie").contains("JSESSIONID")) {
-                response.addHeader("Set-Cookie", "JSESSIONID=656cef62-e3c4-40bc-a8df-94732920ed46");
-            }
+            Session session = createSession(user);
+            response.addHeader("Set-Cookie", "JSESSIONID="+session.getId());
             return response;
         } catch (RuntimeException e) {
             log.error(e.getMessage(), e);
@@ -121,11 +108,30 @@ public class Http11Processor implements Runnable, Processor {
         return Response.redirection("/401.html");
     }
 
+    private Session createSession(final User user) {
+        Session session = new Session();
+        session.setAttribute("user", user);
+        SessionManager.addSession(session);
+        return session;
+    }
+
+    private Session findSession(final RequestHeader requestHeader) {
+        String[] cookies = requestHeader.getHeaderValue("Cookie").split("; ");
+        for(String cookie : cookies) {
+            if (cookie.contains("JSESSIONID")) {
+                Cookie httpCookie = Cookie.from(cookie);
+                String sessionId = httpCookie.getValue();
+                return SessionManager.findSession(sessionId);
+            }
+        }
+        return null;
+    }
+
     //회원가입을 버튼을 누르면 HTTP method를 GET이 아닌 POST를 사용한다.
     //회원가입을 완료하면 index.html로 리다이렉트한다.
     //로그인 페이지도 버튼을 눌렀을 때 GET 방식에서 POST 방식으로 전송하도록 변경하자.
 
-    private static Response register(final RequestBody requestBody) {
+    private Response register(final RequestBody requestBody) {
         if (requestBody == null) {
             return Response.redirection("401.html");
         }
@@ -143,7 +149,7 @@ public class Http11Processor implements Runnable, Processor {
     - 파일 있으면 : 파일 읽어서 responseBody + http 요청 생성
     - 파일 없으면 : Location 헤더만 돌려줌
      */
-    private static Response responseStaticFile(String requestUri, RequestHeader requestHeader) {
+    private Response responseStaticFile(String requestUri, RequestHeader requestHeader) {
         try {
             String requestedFile = ClassLoader.getSystemClassLoader().getResource("static" + requestUri).getFile();
             StringBuilder sb = new StringBuilder();

19f80a8a47ceb4b94608c3873b2667d562b4248d
shin-mallang <huipulci1@naver.com>
2023-09-03 18:47:15 +0900

    Http11Processor에서 LoginRequestHandler 사용하도록 수정

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -2,26 +2,19 @@ package org.apache.coyote.http11;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
-import java.net.URL;
-import java.nio.file.Files;
-import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.exception.UncheckedServletException;
+import nextstep.jwp.handler.LoginRequestHandler;
 import nextstep.jwp.handler.RootPageRequestHandler;
 import nextstep.jwp.handler.StaticResourceRequestHandler;
-import nextstep.jwp.model.User;
 import org.apache.coyote.Processor;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.request.StartLine;
 import org.apache.coyote.http11.request.URI;
 import org.apache.coyote.http11.response.HttpResponse;
-import org.apache.coyote.http11.response.HttpStatus;
-import org.apache.coyote.http11.response.ResponseHeaders;
-import org.apache.coyote.http11.response.StatusLine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -53,34 +46,18 @@ public class Http11Processor implements Runnable, Processor {
             if (startLine.isEmpty()) {
                 return;
             }
-            String responseBody = "";
             URI uri = startLine.uri();
+            HttpResponse response;
             if (uri.path().equals("/")) {
                 RootPageRequestHandler rootPageRequestHandler = new RootPageRequestHandler();
-                HttpResponse response = rootPageRequestHandler.handle(request);
-                bufferedWriter.write(response.toString());
-                bufferedWriter.flush();
-                return;
+                response = rootPageRequestHandler.handle(request);
             } else if (uri.path().equals("/login")) {
-                final URL resource = classLoader.getResource("static" + uri.path() + ".html");
-                final File file = new File(resource.getFile());
-                responseBody = new String(Files.readAllBytes(file.toPath()));
-                User user = InMemoryUserRepository.findByAccount(uri.queryStrings().get("account"))
-                        .orElse(null);
-                log.info("User={}", user);
+                LoginRequestHandler loginRequestHandler = new LoginRequestHandler();
+                response = loginRequestHandler.handle(request);
             } else {
                 StaticResourceRequestHandler staticResourceRequestHandler = new StaticResourceRequestHandler();
-                HttpResponse response = staticResourceRequestHandler.handle(request);
-                bufferedWriter.write(response.toString());
-                bufferedWriter.flush();
-                return;
+                response = staticResourceRequestHandler.handle(request);
             }
-
-            StatusLine statusLine = new StatusLine(HttpStatus.OK);
-            ResponseHeaders responseHeaders = new ResponseHeaders();
-            responseHeaders.put("Content-Type", contentType(uri) + "charset=utf-8");
-            HttpResponse response = new HttpResponse(statusLine, responseHeaders, responseBody);
-
             bufferedWriter.write(response.toString());
             bufferedWriter.flush();
         } catch (IOException | UncheckedServletException e) {

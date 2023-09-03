623162787a5ec0b9df10da6398c7685f0025564f
gitchan <eunkeeeee@gmail.com>
2023-09-03 23:19:59 +0900

    refactor: response generate 메서드 분리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,10 +1,13 @@
 package org.apache.coyote.http11;
 
+import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.exception.UncheckedServletException;
+import nextstep.jwp.model.User;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.security.sasl.AuthenticationException;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -12,8 +15,7 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Http11Processor implements Runnable, Processor {
@@ -60,12 +62,26 @@ public class Http11Processor implements Runnable, Processor {
 
         if (requestURI.equals("/")) {
             final var responseBody = "Hello world!";
-            return String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            return generateResponse(requestURI, responseBody);
+        }
+
+        if (requestURI.equals("/login")) {
+            Map<String, String> queryStrings = parseQueryString(requestURI);
+            User account = findAccount(queryStrings);
+
+            String password = queryStrings.get("password");
+            boolean isCorrectPassword = account.checkPassword(password);
+            if (!isCorrectPassword) {
+                log.info("accout {} 비밀번호 불일치로 로그인 실패", account.getAccount());
+                throw new AuthenticationException();
+            }
+
+            URL resource = getClass()
+                    .getClassLoader()
+                    .getResource("static" + requestURI + ".html");
+            File file = new File(resource.getFile());
+            String responseBody = new String(Files.readAllBytes(file.toPath()));
+            return generateResponse(requestURI, responseBody);
         }
 
         URL resource = getClass()
@@ -73,6 +89,10 @@ public class Http11Processor implements Runnable, Processor {
                 .getResource("static" + requestURI);
         File file = new File(resource.getFile());
         String responseBody = new String(Files.readAllBytes(file.toPath()));
+        return generateResponse(requestURI, responseBody);
+    }
+
+    private String generateResponse(String requestURI, String responseBody) {
         return String.join("\r\n",
                 "HTTP/1.1 200 OK ",
                 "Content-Type: " + contentType(requestURI) + ";charset=utf-8 ",
@@ -82,10 +102,33 @@ public class Http11Processor implements Runnable, Processor {
         );
     }
 
-    private String contentType(String requestURI) {
-        if (requestURI.endsWith(".html")) {
-            return "text/html";
+    private User findAccount(Map<String, String> queryStrings) {
+        String account = queryStrings.get("account");
+        Optional<User> findAccount = InMemoryUserRepository.findByAccount(account);
+        if (findAccount.isEmpty()) {
+            log.info("해당하는 Account를 찾을 수 없음! account={}", account);
+            throw new NoSuchElementException();
         }
-        return "text/css";
+
+        return findAccount.get();
+    }
+
+    private Map<String, String> parseQueryString(String requestURI) {
+        int questionIndex = requestURI.indexOf("?");
+        if (questionIndex == -1) {
+            return Map.of();
+        }
+
+        String nonParsedQueryString = requestURI.substring(questionIndex + 1);
+        return Arrays.stream(nonParsedQueryString.split("&"))
+                .map(it -> Arrays.stream(it.split("=")).collect(Collectors.toList()))
+                .collect(Collectors.toMap(it -> it.get(0), it -> it.get(1)));
+    }
+
+    private String contentType(String requestURI) {
+        if (requestURI.endsWith(".css")) {
+            return "text/css";
+        }
+        return "text/html";
     }
 }

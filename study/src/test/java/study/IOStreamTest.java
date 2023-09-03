1afb1d182440ca9fe8ddaa94ed494f15f2806856
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-09-04 00:15:44 +0900

    test: IOStream, FileStream 학습 테스트 진행

--- study/src/test/java/study/IOStreamTest.java
+++ study/src/test/java/study/IOStreamTest.java
@@ -1,3 +1,4 @@
+
 package study;
 
 import org.junit.jupiter.api.DisplayName;
@@ -54,8 +55,9 @@ class IOStreamTest {
              * OutputStream 객체의 write 메서드를 사용해서 테스트를 통과시킨다
              */
 
-            final String actual = outputStream.toString();
+            outputStream.write(bytes);
 
+            final String actual = outputStream.toString();
             assertThat(actual).isEqualTo("nextstep");
             outputStream.close();
         }
@@ -79,6 +81,7 @@ class IOStreamTest {
              * ByteArrayOutputStream과 어떤 차이가 있을까?
              */
 
+            outputStream.flush();
             verify(outputStream, atLeastOnce()).flush();
             outputStream.close();
         }
@@ -96,7 +99,8 @@ class IOStreamTest {
              * try-with-resources를 사용한다.
              * java 9 이상에서는 변수를 try-with-resources로 처리할 수 있다.
              */
-
+            try (OutputStream referenceOutputStream = outputStream) {
+            }
             verify(outputStream, atLeastOnce()).close();
         }
     }
@@ -128,7 +132,9 @@ class IOStreamTest {
              * todo
              * inputStream에서 바이트로 반환한 값을 문자열로 어떻게 바꿀까?
              */
-            final String actual = "";
+
+            byte[] readAllBytes = inputStream.readAllBytes();
+            final String actual = new String(readAllBytes);
 
             assertThat(actual).isEqualTo("🤩");
             assertThat(inputStream.read()).isEqualTo(-1);
@@ -148,7 +154,8 @@ class IOStreamTest {
              * try-with-resources를 사용한다.
              * java 9 이상에서는 변수를 try-with-resources로 처리할 수 있다.
              */
-
+            try (InputStream referenceInputStream = inputStream) {
+            }
             verify(inputStream, atLeastOnce()).close();
         }
     }
@@ -169,12 +176,12 @@ class IOStreamTest {
          * 버퍼 크기를 지정하지 않으면 버퍼의 기본 사이즈는 얼마일까?
          */
         @Test
-        void 필터인_BufferedInputStream를_사용해보자() {
+        void 필터인_BufferedInputStream를_사용해보자() throws IOException {
             final String text = "필터에 연결해보자.";
             final InputStream inputStream = new ByteArrayInputStream(text.getBytes());
-            final InputStream bufferedInputStream = null;
+            final InputStream bufferedInputStream = new BufferedInputStream(inputStream);
 
-            final byte[] actual = new byte[0];
+            final byte[] actual = bufferedInputStream.readAllBytes();
 
             assertThat(bufferedInputStream).isInstanceOf(FilterInputStream.class);
             assertThat(actual).isEqualTo("필터에 연결해보자.".getBytes());
@@ -197,15 +204,26 @@ class IOStreamTest {
          * 필터인 BufferedReader를 사용하면 readLine 메서드를 사용해서 문자열(String)을 한 줄 씩 읽어올 수 있다.
          */
         @Test
-        void BufferedReader를_사용하여_문자열을_읽어온다() {
+        void BufferedReader를_사용하여_문자열을_읽어온다() throws IOException {
             final String emoji = String.join("\r\n",
                     "😀😃😄😁😆😅😂🤣🥲☺️😊",
                     "😇🙂🙃😉😌😍🥰😘😗😙😚",
                     "😋😛😝😜🤪🤨🧐🤓😎🥸🤩",
                     "");
             final InputStream inputStream = new ByteArrayInputStream(emoji.getBytes());
+            final BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+            final InputStreamReader inputStreamReader = new InputStreamReader(bufferedInputStream);
+            final BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
             final StringBuilder actual = new StringBuilder();
+            while (true) {
+                String data = bufferedReader.readLine();
+                if (data == null) {
+                    break;
+                }
+                actual.append(data);
+                actual.append("\r\n");
+            }
 
             assertThat(actual).hasToString(emoji);
         }

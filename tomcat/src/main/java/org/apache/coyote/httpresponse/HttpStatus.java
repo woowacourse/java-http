388e7a25ef7541b033e45b475194ad6f6489c372
jeonghoon <shb03323@naver.com>
2023-09-03 21:58:34 +0900

    feat: Http Status 추가

---
+++ tomcat/src/main/java/org/apache/coyote/httpresponse/HttpStatus.java
@@ -0,0 +1,13 @@
+package org.apache.coyote.httpresponse;
+
+public enum HttpStatus {
+    OK(200, "OK");
+
+    private final int value;
+    private final String reasonPhrase;
+
+    HttpStatus(int value, String reasonPhrase) {
+        this.value = value;
+        this.reasonPhrase = reasonPhrase;
+    }
+}

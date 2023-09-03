ce1acc13fee3833093ae9e67cdeb2ddea9555c0d
MoonJeWoong <munjin0201@naver.com>
2023-09-04 01:27:56 +0900

    feat: HttpCookie 클래스 구현

---
+++ tomcat/src/main/java/nextstep/org/apache/coyote/http11/HttpCookie.java
@@ -0,0 +1,38 @@
+package nextstep.org.apache.coyote.http11;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class HttpCookie {
+
+    public static final String SET_COOKIE_HEADER = "Set-Cookie: %s \r\n";
+    private final Map<String, String> cookie = new HashMap<>();
+    public static final String COOKIE_DELIMITER = "; ";
+    public static final String KEY_VALUE_DELIMITER = "=";
+    public static final int KEY_INDEX = 0;
+    public static final int VALUE_INDEX = 1;
+
+    public void parseCookieHeaders(String cookieHeaders) {
+        Arrays.asList(cookieHeaders.split(COOKIE_DELIMITER)).forEach(header -> {
+            String[] splited = header.split(KEY_VALUE_DELIMITER);
+            cookie.put(splited[KEY_INDEX], splited[VALUE_INDEX]);
+        });
+    }
+
+    public void set(String key, String value) {
+        cookie.put(key, value);
+    }
+
+    public boolean isEmpty() {
+        return cookie.isEmpty();
+    }
+
+    public String createSetCookieHeader() {
+        String cookies = cookie.entrySet().stream()
+                .map(entry -> entry.getKey() + KEY_VALUE_DELIMITER + entry.getValue())
+                .collect(Collectors.joining(COOKIE_DELIMITER));
+        return String.format(SET_COOKIE_HEADER, cookies);
+    }
+}

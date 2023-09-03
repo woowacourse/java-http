bb982703a4f9fe1261a8c87d30a6c979aa3df283
hong-sile <gurwns9325@naver.com>
2023-09-03 15:27:45 +0900

    refactor: 상태코드 응답 수정

--- tomcat/src/main/java/org/apache/coyote/http11/response/HttpStatusCode.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/HttpStatusCode.java
@@ -5,7 +5,7 @@ public enum HttpStatusCode {
 	OK_200(200, "OK"),
 	INTERNAL_SERVER_ERROR_500(500, "Internal Server Error"),
 	NOT_FOUND_404(404, "Not Found"),
-	TEMPORARILY_MOVED_302(302, "Temporarily Moved");
+	TEMPORARILY_MOVED_302(302, "Found");
 
 	private final int code;
 	private final String description;

ac149fbe2ab9c1a5d57e1024b37c8ab1022c4310
hong-sile <gurwns9325@naver.com>
2023-09-03 14:06:04 +0900

    feat: account로 접근 시 유저 조회 후 로깅하는 기능 추가

--- tomcat/src/main/java/org/apache/coyote/http11/handler/LoginHandler.java
+++ tomcat/src/main/java/org/apache/coyote/http11/handler/LoginHandler.java
@@ -7,15 +7,22 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.Set;
 
 import org.apache.coyote.http11.headers.HttpHeaders;
 import org.apache.coyote.http11.headers.MimeType;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.response.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nextstep.jwp.db.InMemoryUserRepository;
 
 public class LoginHandler implements HttpHandler {
 
 	private static final String END_POINT = "/login";
+	private static final Logger log = LoggerFactory.getLogger(LoginHandler.class);
+	private static final String ACCOUNT_PARAM_KEY = "account";
 
 	@Override
 	public boolean isSupported(final HttpRequest request) {
@@ -24,11 +31,16 @@ public class LoginHandler implements HttpHandler {
 
 	@Override
 	public HttpResponse handleTo(final HttpRequest request) throws IOException {
+		final Set<String> accounts = request.getQueryParam().get(ACCOUNT_PARAM_KEY);
+		for (final String account : accounts) {
+			InMemoryUserRepository.findByAccount(account)
+				.ifPresent(user -> log.info(user.toString()));
+		}
 		final String body = resolveBody();
 		return new HttpResponse(
 			OK_200,
 			body,
-			resolveHeader(request, body)
+			resolveHeader(body)
 		);
 	}
 
@@ -38,7 +50,7 @@ public class LoginHandler implements HttpHandler {
 		return new String(Files.readAllBytes(new File(url.getFile()).toPath()));
 	}
 
-	private HttpHeaders resolveHeader(HttpRequest request, String body) {
+	private HttpHeaders resolveHeader(final String body) {
 		final HttpHeaders headers = new HttpHeaders();
 		headers.put(CONTENT_TYPE.getValue(), MimeType.HTML.getValue());
 		headers.put(CONTENT_LENGTH.getValue(), String.valueOf(body.getBytes().length));

a25e33cca730f6d420de6fa46cd2dd9a2885dc8a
apptie <appti@naver.com>
2023-09-04 01:17:08 +0900

    feat: Url 클래스 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http/request/Url.java
@@ -0,0 +1,77 @@
+package org.apache.coyote.http.request;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.apache.coyote.http.util.HttpConsts;
+
+public class Url {
+
+    private static final String STATIC_RESOURCE_EXTENSION_SEPARATOR = ".";
+    private static final String QUERY_PARAMETER_DELIMITER = "?";
+    private static final int INVALID_QUERY_PARAMETER_INDEX = -1;
+    private static final int VALID_PATH_TOKEN_LENGTH = 2;
+
+    private final String path;
+    private final List<String> hierarchy;
+    private final String resourceName;
+
+    private Url(
+            final String path,
+            final List<String> hierarchy,
+            final String resourceName
+    ) {
+        this.path = path;
+        this.hierarchy = hierarchy;
+        this.resourceName = resourceName;
+    }
+
+    public static Url from(final String url) {
+        final int queryParameterDelimiterIndex = url.indexOf(QUERY_PARAMETER_DELIMITER);
+        final String path = findPath(url, queryParameterDelimiterIndex);
+        final String[] urlTokens = url.split(HttpConsts.SLASH);
+
+        if (urlTokens.length == 0) {
+            return new Url(url, Collections.emptyList(), HttpConsts.BLANK);
+        }
+
+        final String resourceName = urlTokens[urlTokens.length - 1];
+        final List<String> hierarchy = Arrays.asList(Arrays.copyOf(urlTokens, urlTokens.length - 1));
+
+        return new Url(path, hierarchy, resourceName);
+    }
+
+    private static String findPath(final String url, final int queryParameterDelimiterIndex) {
+        if (queryParameterDelimiterIndex == INVALID_QUERY_PARAMETER_INDEX) {
+            return url;
+        }
+
+        return url.substring(0, queryParameterDelimiterIndex);
+    }
+
+    public boolean matchesByPath(final String targetPath, final String rootContextPath) {
+        if (HttpConsts.SLASH.equals(rootContextPath)) {
+            return path.equals(targetPath);
+        }
+
+        final String[] pathTokens = path.split(rootContextPath);
+
+        if (pathTokens.length < VALID_PATH_TOKEN_LENGTH) {
+            return false;
+        }
+
+        return targetPath.equals(pathTokens[pathTokens.length - 1]);
+    }
+
+    public boolean isWelcomePageUrl(final String rootContextPath) {
+        return path.equals(rootContextPath);
+    }
+
+    public boolean isStaticResource() {
+        if (resourceName.isEmpty() || resourceName.isBlank()) {
+            return false;
+        }
+
+        return resourceName.contains(STATIC_RESOURCE_EXTENSION_SEPARATOR);
+    }
+}
---
+++ tomcat/src/test/java/org/apache/coyote/http/request/UrlTest.java
@@ -0,0 +1,85 @@
+package org.apache.coyote.http.request;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class UrlTest {
+
+    @Test
+    void from_메서드는_url을_전달하면_Url을_초기화한다() {
+        final String url = "/login";
+
+        assertThatCode(() -> Url.from(url)).doesNotThrowAnyException();
+    }
+
+    @Test
+    void matchesByPath_메서드는_path와_rootContextPath를_전달할때_path가_일치하면_true를_반환한다() {
+        final String path = "/hello/login";
+        final String equalTargetPath = "/login";
+        final String rootContextPath = "/hello";
+        final Url url = Url.from(path);
+
+        final boolean actual = url.matchesByPath(equalTargetPath, rootContextPath);
+
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void matchesByPath_메서드는_path와_rootContextPath를_전달할때_path가_일치하지_않으면_false를_반환한다() {
+        final String path = "/hello/login";
+        final String notEqualTargetPath = "/loginabc";
+        final String rootContextPath = "/hello";
+        final Url url = Url.from(path);
+
+        final boolean actual = url.matchesByPath(notEqualTargetPath, rootContextPath);
+
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    void isWelcomePageUrl_메서드는_url이_welcome_page인_경우_true를_반환한다() {
+        final String path = "/";
+        final String rootContextPath = "/";
+        final Url url = Url.from(path);
+
+        final boolean actual = url.isWelcomePageUrl(rootContextPath);
+
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void isWelcomePageUrl_메서드는_url이_welcome_page가_아닌_경우_false를_반환한다() {
+        final String path = "/login";
+        final String rootContextPath = "/";
+        final Url url = Url.from(path);
+
+        final boolean actual = url.isWelcomePageUrl(rootContextPath);
+
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    void isStaticResource_메서드는_url이_정적_리소스_요청인_경우_true를_반환한다() {
+        final String path = "/index.html";
+        final Url url = Url.from(path);
+
+        final boolean actual = url.isStaticResource();
+
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void isStaticResource_메서드는_url이_정적_리소스_요청이_아닌_경우_false를_반환한다() {
+        final String path = "/login";
+        final Url url = Url.from(path);
+
+        final boolean actual = url.isStaticResource();
+
+        assertThat(actual).isFalse();
+    }
+}

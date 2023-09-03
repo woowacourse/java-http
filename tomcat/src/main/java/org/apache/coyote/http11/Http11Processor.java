00337ad076c0da7a2412f974ce956ee53669bc44
odo27 <odo27@naver.com>
2023-09-03 11:06:03 +0900

    refactor: 로그인 부분 분리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.util.HashMap;
@@ -45,58 +46,15 @@ public class Http11Processor implements Runnable, Processor {
             String[] split = request.split(" ");
             String requestUri = split[1];
 
-            int index = requestUri.indexOf("?");
-            String path = requestUri;
-            if (index != -1) {
-                path = requestUri.substring(0, index);
-                String queryString = requestUri.substring(index + 1);
-                String[] keyValues = queryString.split("&");
-                Map<String, String> queryParams = new HashMap<>();
-                for (String keyValue : keyValues) {
-                    String[] keyAndValue = keyValue.split("=");
-                    String key = keyAndValue[0];
-                    String value = keyAndValue[1];
-                    queryParams.put(key, value);
-                }
-                Optional<User> user = InMemoryUserRepository.findByAccount(queryParams.get("account"));
-                if (user.isPresent()) {
-                    if (user.get().checkPassword(queryParams.get("password"))) {
-                        log.info(user.get().toString());
-                        String response = String.join("\r\n",
-                                "HTTP/1.1 302 FOUND ",
-                                "Location: /index.html ",
-                                "",
-                                "");
-                        try {
-                            outputStream.write(response.getBytes());
-                            outputStream.flush();
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                        return;
-                    }
-                }
-                URL resource = getClass().getClassLoader().getResource("static/401.html");
-                responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
-                String response = String.join("\r\n",
-                        "HTTP/1.1 401 UNAUTHORIZED ",
-                        "Content-Type: text/html;charset=utf-8 ",
-                        "Content-Length: " + responseBody.getBytes().length + " ",
-                        "",
-                        responseBody);
-                try {
-                    outputStream.write(response.getBytes());
-                    outputStream.flush();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+            if (requestUri.startsWith("/login?")) {
+                login(requestUri, outputStream);
                 return;
             }
 
-            if (!path.equals("/")) {
-                URL resource = getClass().getClassLoader().getResource("static" + path);
-                if (path.equals("/login")) {
-                    resource = getClass().getClassLoader().getResource("static" + path + ".html");
+            if (!requestUri.equals("/")) {
+                URL resource = getClass().getClassLoader().getResource("static" + requestUri);
+                if (requestUri.equals("/login")) {
+                    resource = getClass().getClassLoader().getResource("static" + requestUri + ".html");
                 }
                 responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
             }
@@ -122,6 +80,47 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private void login(String requestUri, OutputStream outputStream) throws IOException {
+        int index = requestUri.indexOf("?");
+        if (index == -1) {
+            throw new IllegalArgumentException("로그인 정보가 없습니다");
+        }
+        String path = requestUri.substring(0, index);
+        String queryString = requestUri.substring(index + 1);
+        String[] keyValues = queryString.split("&");
+        Map<String, String> queryParams = new HashMap<>();
+        for (String keyValue : keyValues) {
+            String[] keyAndValue = keyValue.split("=");
+            String key = keyAndValue[0];
+            String value = keyAndValue[1];
+            queryParams.put(key, value);
+        }
+        Optional<User> user = InMemoryUserRepository.findByAccount(queryParams.get("account"));
+        if (user.isPresent()) {
+            if (user.get().checkPassword(queryParams.get("password"))) {
+                log.info(user.get().toString());
+                String response = String.join("\r\n",
+                        "HTTP/1.1 302 FOUND ",
+                        "Location: /index.html ",
+                        "",
+                        "");
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+                return;
+            }
+        }
+        URL resource = getClass().getClassLoader().getResource("static/401.html");
+        String responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+        String response = String.join("\r\n",
+                "HTTP/1.1 401 UNAUTHORIZED ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+        outputStream.write(response.getBytes());
+        outputStream.flush();
     }
 
     private String getRequest(InputStream inputStream) throws IOException {

f602ebd9f63c5cc160ea04f3094f6045cf966ea9
Youngho <cyh6099@gmail.com>
2023-09-03 14:32:20 +0900

    feat: 요청에 맞는 핸들러 호출하여 response응답 로직 작성

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -2,19 +2,18 @@ package org.apache.coyote.http11;
 
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
+import org.apache.coyote.http11.handler.Handlers;
+import org.apache.coyote.http11.http.HttpRequest;
+import org.apache.coyote.http11.http.RequestFirstLine;
+import org.apache.coyote.http11.http.RequestHeader;
+import org.apache.coyote.http11.http.ResponseEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -36,39 +35,16 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
+
             BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-            String httpRequest = br.readLine();
-            String[] requestInfos = httpRequest.split(" ");
+            String httpRequestText = br.readLine();
+            RequestFirstLine requestLine = RequestFirstLine.from(httpRequestText);
+            log.info(requestLine.getRequestUri());
+            RequestHeader requestHeader = RequestHeader.from(br);
 
-            String requestFile = requestInfos[1];
-
-            StringBuilder responseBody = new StringBuilder();
-            if (requestFile.equals("/")) {
-                responseBody.append("Hello world!");
-            } else {
-                ClassLoader classLoader = getClass().getClassLoader();
-                URL resource = classLoader.getResource("static" + requestFile);
-
-                File file = new File(resource.getFile());
-
-                Path path = Paths.get(file.getAbsolutePath());
-
-                InputStream fileInputStream = Files.newInputStream(path);
-                br = new BufferedReader(new InputStreamReader(fileInputStream));
-
-
-                String line;
-                while ((line = br.readLine()) != null) {
-                    responseBody.append(line).append('\n');
-                }
-            }
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.toString().getBytes().length + " ",
-                    "",
-                    responseBody);
+            HttpRequest httpRequest = new HttpRequest(requestLine, requestHeader);
+            ResponseEntity responseEntity = Handlers.handle(httpRequest);
+            String response = responseEntity.generateResponseMessage();
 
             outputStream.write(response.getBytes());
             outputStream.flush();

fd9e81b1e9d864c4d15e7303be3d2658f34610a7
jeomxon <jeomxon@gmail.com>
2023-09-04 01:57:55 +0900

    feat: CSS 지원하기

--- tomcat/src/main/java/org/apache/coyote/http11/HttpResponse.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpResponse.java
@@ -10,19 +10,28 @@ public class HttpResponse {
     private static final String BLANK = " ";
 
     public static String getResponse(final String path, final String protocolVersion) throws IOException {
+        String contentType = "text/html;charset=utf-8";
         if (path.equals("/")) {
-            return makeResponse(protocolVersion, HttpStatus.OK, "Hello world!");
+            return makeResponse(protocolVersion, contentType, HttpStatus.OK, "Hello world!");
+        }
+        if (path.endsWith(".css")) {
+            contentType = "text/css;charset=utf-8";
         }
 
         final URL resource = HttpResponse.class.getClassLoader().getResource("static" + path);
         final String responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
-        return makeResponse(protocolVersion, HttpStatus.OK, responseBody);
+        return makeResponse(protocolVersion, contentType, HttpStatus.OK, responseBody);
     }
 
-    private static String makeResponse(final String protocolVersion, final HttpStatus status, final String body) {
+    private static String makeResponse(
+            final String protocolVersion,
+            final String contentType,
+            final HttpStatus status,
+            final String body
+    ) {
         return String.join("\r\n",
                 protocolVersion + BLANK + status.getCode() + BLANK + status.name() + BLANK,
-                "Content-Type: " + "text/html;charset=utf-8" + BLANK,
+                "Content-Type: " + contentType + BLANK,
                 "Content-Length: " + body.getBytes().length + BLANK,
                 "",
                 body

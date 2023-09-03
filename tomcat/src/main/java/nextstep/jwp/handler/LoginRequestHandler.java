cbcb49c91714e1e90425a255c67eceb889d8bebe
shin-mallang <huipulci1@naver.com>
2023-09-03 20:37:39 +0900

    LoginRequestHandler에서 AuthService 사용

--- tomcat/src/main/java/nextstep/jwp/handler/LoginRequestHandler.java
+++ tomcat/src/main/java/nextstep/jwp/handler/LoginRequestHandler.java
@@ -5,8 +5,9 @@ import static org.apache.coyote.http11.response.HttpStatus.FOUND;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import nextstep.jwp.db.InMemoryUserRepository;
+import nextstep.jwp.exception.UnAuthenticatedException;
 import nextstep.jwp.model.User;
+import nextstep.jwp.service.AuthService;
 import org.apache.coyote.http11.Http11Processor;
 import org.apache.coyote.http11.handler.RequestHandler;
 import org.apache.coyote.http11.request.BadQueryStringException;
@@ -20,6 +21,7 @@ import org.slf4j.LoggerFactory;
 public class LoginRequestHandler implements RequestHandler {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private final AuthService authService = new AuthService();
 
     @Override
     public boolean canHandle(HttpRequest request) {
@@ -30,28 +32,16 @@ public class LoginRequestHandler implements RequestHandler {
     @Override
     public HttpResponse handle(HttpRequest request) {
         Map<String, String> formData = parseFormData(request);
-        String account = formData.get("account");
-        User user;
-        if (account == null) {
-            user = null;
-        } else {
-            user = InMemoryUserRepository.findByAccount(account).orElse(null);
-            if (user != null && !user.checkPassword(formData.get("password"))) {
-                user = null;
-            }
-        }
-        log.info("User={}", user);
         StatusLine statusLine = new StatusLine(FOUND);
         ResponseHeaders responseHeaders = new ResponseHeaders();
-        responseHeaders.put("Location", location(user));
-        return new HttpResponse(statusLine, responseHeaders, null);
-    }
-
-    private String location(User user) {
-        if (user == null) {
-            return "/401.html";
+        try {
+            User user = authService.login(formData.get("account"), formData.get("password"));
+            log.info("User={}", user);
+            responseHeaders.put("Location", "/index.html");
+        } catch (UnAuthenticatedException e) {
+            responseHeaders.put("Location", "/401.html");
         }
-        return "/index.html";
+        return new HttpResponse(statusLine, responseHeaders, null);
     }
 
     private Map<String, String> parseFormData(HttpRequest request) {

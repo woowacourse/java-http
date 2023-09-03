06b2e320ca4ff50665b91056d6dcc9deab2ffeae
Go-Jaecheol <gojaech@naver.com>
2023-09-03 13:35:02 +0900

    feat: 정적 페이지 요청에 대해 응답하는 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,17 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.Objects;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,8 +33,11 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
+            final var inputStreamReader = new InputStreamReader(inputStream);
+            final var bufferedReader = new BufferedReader(inputStreamReader);
 
-            final var responseBody = "Hello world!";
+            final String uri = readRequestHeader(bufferedReader);
+            final String responseBody = findResponseBody(Objects.requireNonNull(uri));
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -43,5 +51,27 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String findResponseBody(final String uri) throws IOException {
+        if (uri.equals("/")) {
+            return "Hello world!";
+        }
+        final URL fileUrl = getClass().getClassLoader().getResource("./static" + uri);
+        final String filePath = Objects.requireNonNull(fileUrl).getPath();
+        return Files.readString(new File(filePath).toPath());
+    }
+
+    private String readRequestHeader(final BufferedReader bufferedReader) throws IOException {
+        String line = bufferedReader.readLine();
+        if (line == null) {
+            return null;
+        }
+        final String uri = line.split(" ")[1];
+
+        while (!"".equals(line)) {
+            line = bufferedReader.readLine();
+        }
+        return uri;
     }
 }

8fbce356592bd3b0ddeccba5dff33f265151d29f
Ohjintaek <jt950930@gmail.com>
2023-09-03 20:08:18 +0900

    feat: /index.html 응답 및 CSS 지원 기능

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,19 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,18 +36,49 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final InputStreamReader reader = new InputStreamReader(inputStream);
+            final BufferedReader bufferedReader = new BufferedReader(reader);
+            final List<String> request = new ArrayList<>();
+            String line;
+            do {
+                line = bufferedReader.readLine();
+                if (line == null) {
+                    return;
+                }
+                request.add(line);
+            } while (!"".equals(line));
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            String responseBody = "Hello world!";
+            final String requestFileName = request.get(0).split(" ")[1];
+            if (requestFileName.length() > 1) {
+                final URL url = getClass().getClassLoader().getResource("static/" + requestFileName);
+                if (url != null) {
+                    final var path = Paths.get(url.toURI());
+                    responseBody = Files.readString(path);
+                }
+            }
+
+            final String response;
+            if (request.get(0).split(" ")[1].endsWith(".css")) {
+                response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/css",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody);
+            }
+            else {
+                response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody);
+            }
 
             outputStream.write(response.getBytes());
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
         }
     }

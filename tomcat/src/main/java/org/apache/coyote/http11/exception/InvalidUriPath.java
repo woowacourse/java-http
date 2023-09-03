c289fbe91aea7335fa43a1394b88124319c7cf87
dev_kong <ahdjd5@gmail.com>
2023-09-03 23:28:30 +0900

    feat: registeredUri 구현

---
+++ tomcat/src/main/java/org/apache/coyote/http11/exception/InvalidUriPath.java
@@ -0,0 +1,7 @@
+package org.apache.coyote.http11.exception;
+
+public class InvalidUriPath extends RuntimeException {
+    public InvalidUriPath(final String message) {
+        super(message);
+    }
+}

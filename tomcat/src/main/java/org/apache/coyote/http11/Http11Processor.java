274655d2fcd09bb79cbf545187d856a7e5e7a1d4
gitchan <eunkeeeee@gmail.com>
2023-09-03 22:08:05 +0900

    feat: 정적 파일 제공 기능 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,8 +5,16 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,15 +36,37 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String request = bufferedReader.readLine();
 
-            final var responseBody = "Hello world!";
+            List<String> requests = Arrays.stream(request.split(" ")).collect(Collectors.toList());
+            String requestURI = requests.get(1);
 
-            final var response = String.join("\r\n",
+            if (requestURI.equals("/")) {
+                final var responseBody = "Hello world!";
+                final var response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody);
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+                return;
+            }
+
+            URL resource = getClass()
+                    .getClassLoader()
+                    .getResource("static" + requestURI);
+            File file = new File(resource.getFile());
+            String responseBody = new String(Files.readAllBytes(file.toPath()));
+            String response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "Content-Length: 5564 ",
                     "",
-                    responseBody);
+                    responseBody
+            );
 
             outputStream.write(response.getBytes());
             outputStream.flush();

7fea21e62dbdd47c239c48ed8d351326c515c8ac
BackFoxx <backfoxxx@gmail.com>
2023-09-03 15:05:08 +0900

    feat: /index.html로 요청을 보내면 index.html 응답하기

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,7 +1,14 @@
 package org.apache.coyote.http11;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import java.util.StringTokenizer;
@@ -9,9 +16,6 @@ import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -21,6 +25,23 @@ public class Http11Processor implements Runnable, Processor {
 
     public Http11Processor(final Socket connection) {
         this.connection = connection;
+    }
+
+    private static void withNoURI(final OutputStream outputStream, final String httpRequestMethod,
+                                  final String httpRequestUri) throws IOException {
+        if (Objects.equals(httpRequestMethod, "GET") && Objects.equals(httpRequestUri, "/")) {
+            final var responseBody = "Hello world!";
+
+            final var response = String.join("\r\n",
+                    "HTTP/1.1 200 OK ",
+                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "",
+                    responseBody);
+
+            outputStream.write(response.getBytes());
+            outputStream.flush();
+        }
     }
 
     @Override
@@ -39,37 +60,50 @@ public class Http11Processor implements Runnable, Processor {
             final String httpRequestMethod = stringTokenizer.nextToken();
             final String httpRequestUri = stringTokenizer.nextToken();
 
-            if (Objects.equals(httpRequestMethod, "GET") && Objects.equals(httpRequestUri, "/")) {
-                final var responseBody = "Hello world!";
+            withNoURI(outputStream, httpRequestMethod, httpRequestUri);
 
-                final var response = String.join("\r\n",
-                        "HTTP/1.1 200 OK ",
-                        "Content-Type: text/html;charset=utf-8 ",
-                        "Content-Length: " + responseBody.getBytes().length + " ",
-                        "",
-                        responseBody);
-
-                outputStream.write(response.getBytes());
-                outputStream.flush();
-            }
-
-            if (Objects.equals(httpRequestMethod, "GET") && Objects.equals(httpRequestUri, "/index.html")) {
-                final var responseBody = "Hello world!";
-
-                final var response = String.join("\r\n",
-                        "HTTP/1.1 200 OK ",
-                        "Content-Type: text/html;charset=utf-8 ",
-                        "Content-Length: " + responseBody.getBytes().length + " ",
-                        "",
-                        responseBody);
-
-                outputStream.write(response.getBytes());
-                outputStream.flush();
-            }
+            withIndexPage(outputStream, httpRequestMethod, httpRequestUri);
 
 
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        }
+    }
+
+    private void withIndexPage(final OutputStream outputStream, final String httpRequestMethod,
+                               final String httpRequestUri)
+            throws IOException {
+        if (Objects.equals(httpRequestMethod, "GET") && Objects.equals(httpRequestUri, "/index.html")) {
+            final var responseBody = new StringBuilder();
+
+            final URL indexPageURL = this.getClass().getClassLoader().getResource("static/index.html");
+            final File indexFile;
+            try {
+                indexFile = new File(indexPageURL.toURI());
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+            try (
+                    final FileInputStream fileInputStream = new FileInputStream(indexFile);
+                    final BufferedReader bufferedReader = new BufferedReader(
+                            new InputStreamReader(fileInputStream, StandardCharsets.UTF_8))
+            ) {
+                while (bufferedReader.ready()) {
+                    responseBody
+                            .append(bufferedReader.readLine())
+                            .append(System.lineSeparator());
+                }
+            }
+
+            final var response = String.join("\r\n",
+                    "HTTP/1.1 200 OK ",
+                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Length: " + responseBody.toString().getBytes().length + " ",
+                    "",
+                    responseBody);
+
+            outputStream.write(response.getBytes());
+            outputStream.flush();
         }
     }
 }

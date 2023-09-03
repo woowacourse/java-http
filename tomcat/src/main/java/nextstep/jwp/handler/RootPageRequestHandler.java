e0b940164399cc40267717914fd3bc0a6403c872
shin-mallang <huipulci1@naver.com>
2023-09-03 18:37:22 +0900

    루트 접근을 처리하는 RootPageRequestHandler 구현

---
+++ tomcat/src/main/java/nextstep/jwp/handler/RootPageRequestHandler.java
@@ -0,0 +1,20 @@
+package nextstep.jwp.handler;
+
+import org.apache.coyote.http11.handler.RequestHandler;
+import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.response.HttpResponse;
+import org.apache.coyote.http11.response.HttpStatus;
+import org.apache.coyote.http11.response.ResponseHeaders;
+import org.apache.coyote.http11.response.StatusLine;
+
+public class RootPageRequestHandler implements RequestHandler {
+
+    @Override
+    public HttpResponse handle(HttpRequest request) {
+        String responseBody = "Hello world!";
+        StatusLine statusLine = new StatusLine(HttpStatus.OK);
+        ResponseHeaders responseHeaders = new ResponseHeaders();
+        responseHeaders.put("Content-Type", "text/html;charset=utf-8");
+        return new HttpResponse(statusLine, responseHeaders, responseBody);
+    }
+}
---
+++ tomcat/src/test/java/nextstep/jwp/handler/RootPageRequestHandlerTest.java
@@ -0,0 +1,41 @@
+package nextstep.jwp.handler;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.request.StartLine;
+import org.apache.coyote.http11.response.HttpResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("RootPageRequestHandler 은(는)")
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(ReplaceUnderscores.class)
+class RootPageRequestHandlerTest {
+
+    private final RootPageRequestHandler handler = new RootPageRequestHandler();
+
+    @DisplayName("Hello world! 를 반환한다")
+    @Test
+    void test() {
+        // given
+        HttpRequest request = HttpRequest.builder()
+                .startLine(StartLine.from("GET / HTTP/1.1 "))
+                .build();
+
+        // when
+        HttpResponse response = handler.handle(request);
+
+        // then
+        var expected = String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: 12 ",
+                "",
+                "Hello world!");
+
+        assertThat(response.toString()).isEqualTo(expected);
+    }
+}

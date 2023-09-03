59cf38e9b912a0b64048a3bd167a478d99bdd633
JunDonghyuk <ehdgur4814@naver.com>
2023-09-04 07:23:34 +0900

    feat: HttpProcessor 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -2,11 +2,20 @@ package org.apache.coyote.http11;
 
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
+import org.apache.coyote.handler.Handler;
+import org.apache.coyote.handlermapping.HandlerMapping;
+import org.apache.coyote.handlermapping.HandlerMatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -27,18 +36,30 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final var bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String startLine = bufferedReader.readLine();
 
-            final var responseBody = "Hello world!";
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
-
-            outputStream.write(response.getBytes());
+            HttpRequest request = new HttpRequest(startLine);
+            HandlerMatcher handlerMatcher = new HandlerMatcher(HttpMethod.valueOf(request.method()), request.uri());
+            HandlerMapping.init();
+            if (!HandlerMapping.canHandle(handlerMatcher)) {
+                Path path = Paths.get(getClass().getClassLoader().getResource("static" + "/" + "404.html").getPath());
+                List<String> contents = Files.readAllLines(path);
+                String responseBody = String.join("\n", contents) + "\n";
+                String response = String.join("\r\n",
+                        "HTTP/1.1 404 NotFound ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody);
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+                return;
+            }
+            Handler handler = HandlerMapping.getHandler(handlerMatcher);
+            HttpResponse response = handler.handle(request);
+            outputStream.write(response.getResponse().getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);

29d5233c3fb599b06fe0bcce3e14285a73dba76d
BGuga <tmdwns9913@gmail.com>
2023-09-04 16:04:01 +0900

    refactor: RequestHeaders 가 Cookies 를 가지고 있도록 변경

--- tomcat/src/main/java/org/apache/coyote/http11/request/RequestHeaders.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/RequestHeaders.java
@@ -10,10 +10,22 @@ public class RequestHeaders {
     private static final String HEADER_SEPARATOR = System.lineSeparator();
     private static final String HEADER_KEY_VALUE_SPLIT = ":";
     private static final String COOKIE = "Cookie";
+
     private final Map<String, String> headers;
+    private final Optional<Cookies> cookies;
 
     public RequestHeaders(String headers) {
-        this.headers = extractHeader(headers);
+        Map<String, String> headerValues = extractHeader(headers);
+        this.headers = headerValues;
+        this.cookies = extractCookie(headerValues);
+    }
+
+    private Optional<Cookies> extractCookie(Map<String, String> headerValues) {
+        String cookieHeader = headerValues.get(COOKIE);
+        if (cookieHeader == null) {
+            return Optional.empty();
+        }
+        return Optional.of(new Cookies(cookieHeader));
     }
 
     private Map<String, String> extractHeader(String headers) {
@@ -39,10 +51,7 @@ public class RequestHeaders {
     }
 
     public Optional<Cookies> getCookie() {
-        if (!headers.containsKey(COOKIE)) {
-            return Optional.empty();
-        }
-        return Optional.of(new Cookies(headers.get(COOKIE)));
+        return cookies;
     }
 
     public Map<String, String> getHeaders() {

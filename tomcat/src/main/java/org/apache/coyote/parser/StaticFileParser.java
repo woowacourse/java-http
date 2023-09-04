dad21c9d18d513161cf3804bb688286474fa7eeb
java-saeng <gusdn85554@naver.com>
2023-09-04 15:52:46 +0900

    refactor : 유틸리티 클래스에 private 생성자 추가

--- tomcat/src/main/java/org/apache/coyote/parser/StaticFileParser.java
+++ tomcat/src/main/java/org/apache/coyote/parser/StaticFileParser.java
@@ -16,6 +16,9 @@ public class StaticFileParser {
   private static final Pattern PATTERN = Pattern.compile(REGEX);
   private static final String EMPTY_EXTENSION = "";
 
+  private StaticFileParser() {
+  }
+
   public static boolean isStaticFile(final String fileName) {
     return EXTENDER.containsKey(extractExtension(fileName));
   }

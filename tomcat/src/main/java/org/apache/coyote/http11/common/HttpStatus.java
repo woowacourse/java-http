fae9599ce509329bf0fc1766c383521688170c97
zillionme <beatus96@naver.com>
2023-09-04 14:35:56 +0900

    fix: http응답 수정

--- tomcat/src/main/java/org/apache/coyote/http11/common/HttpStatus.java
+++ tomcat/src/main/java/org/apache/coyote/http11/common/HttpStatus.java
@@ -1,9 +1,9 @@
 package org.apache.coyote.http11.common;
 
 public enum HttpStatus {
-    OK(200, "OK"),
-    NOT_FOUND(404, "Not Found"),
-    REDIRECTION(302, "Moved Temporarily");
+    OK(200, "OK "),
+    NOT_FOUND(404, "Not Found "),
+    REDIRECTION(302, "Moved Temporarily ");
 
     private final int code;
     private final String message;

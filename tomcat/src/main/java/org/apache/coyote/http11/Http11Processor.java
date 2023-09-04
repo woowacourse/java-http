2a7c45cb739724947ecf2ddd4903c0a3a0253a63
Ethan <kim76744dev@gmail.com>
2023-09-04 14:58:46 +0900

    feat: 정적파일 파싱 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,8 +5,16 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,20 +36,102 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
+            final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            final BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            final String firstLine = bufferedReader.readLine();
 
+            if (firstLine == null) {
+                return;
+            }
+
+            final Map<String, String> headers = new HashMap<>();
+            String header = bufferedReader.readLine();
+            while (!"".equals(header)) {
+                final String[] parsedHeader = header.split(": ");
+                headers.put(parsedHeader[0], parsedHeader[1]);
+                header = bufferedReader.readLine();
+            }
+
+            String response = null;
+            final String[] parsedFirstLine = firstLine.split(" ");
+            response = parseStaticFiles(parsedFirstLine, response);
+
+            outputStream.write(response.getBytes());
+            outputStream.flush();
+        } catch (IOException |
+                 UncheckedServletException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    private String parseStaticFiles(final String[] parsedFirstLine, String response) throws IOException {
+        if ("/".equals(parsedFirstLine[1])) {
             final var responseBody = "Hello world!";
 
-            final var response = String.join("\r\n",
+            return String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
                     responseBody);
-
-            outputStream.write(response.getBytes());
-            outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
-            log.error(e.getMessage(), e);
         }
+
+        if ("/index.html".equals(parsedFirstLine[1])) {
+            final String filePath = "static" + parsedFirstLine[1];
+            final URL fileUrl = getClass().getClassLoader().getResource(filePath);
+            final Path path = new File(fileUrl.getPath()).toPath();
+            final String responseBody = new String(Files.readAllBytes(path));
+
+            return String.join("\r\n",
+                    "HTTP/1.1 200 OK ",
+                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "",
+                    responseBody);
+        }
+
+        if (parsedFirstLine[1].endsWith(".js")) {
+            final String filePath = "static" + parsedFirstLine[1];
+            final URL fileUrl = getClass().getClassLoader().getResource(filePath);
+            final Path path = new File(fileUrl.getPath()).toPath();
+            final String responseBody = new String(Files.readAllBytes(path));
+
+            return String.join("\r\n",
+                    "HTTP/1.1 200 OK ",
+                    "Content-Type: text/javascript ",
+                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "",
+                    responseBody);
+        }
+
+        if (parsedFirstLine[1].endsWith(".css")) {
+            final String filePath = "static" + parsedFirstLine[1];
+            final URL fileUrl = getClass().getClassLoader().getResource(filePath);
+            final Path path = new File(fileUrl.getPath()).toPath();
+            final String responseBody = new String(Files.readAllBytes(path));
+
+            return String.join("\r\n",
+                    "HTTP/1.1 200 OK ",
+                    "Content-Type: text/css;charset=utf-8 ",
+                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "",
+                    responseBody);
+        }
+
+        if (parsedFirstLine[1].endsWith(".ico")) {
+            final String filePath = "static" + parsedFirstLine[1];
+            final URL fileUrl = getClass().getClassLoader().getResource(filePath);
+            final Path path = new File(fileUrl.getPath()).toPath();
+            final String responseBody = new String(Files.readAllBytes(path));
+
+            return String.join("\r\n",
+                    "HTTP/1.1 200 OK ",
+                    "Content-Type: image/x-icon ",
+                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "",
+                    responseBody);
+        }
+
+        return null;
     }
 }

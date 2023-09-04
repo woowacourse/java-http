76ccbe50c39c07f23ebcf8873ba3f60be1a4908f
BGuga <tmdwns9913@gmail.com>
2023-09-04 14:23:45 +0900

    refactor: HttpRequest 생성을 HttpRequest 에게 위임

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -13,9 +13,6 @@ import org.apache.coyote.Processor;
 import org.apache.coyote.http11.controller.Controller;
 import org.apache.coyote.http11.controller.HandlerMapper;
 import org.apache.coyote.http11.request.HttpRequest;
-import org.apache.coyote.http11.request.RequestHeaders;
-import org.apache.coyote.http11.request.RequestLine;
-import org.apache.coyote.http11.request.ResponseBody;
 import org.apache.coyote.http11.response.HttpResponse;
 import org.apache.coyote.http11.response.HttpStatusCode;
 import org.slf4j.Logger;
@@ -45,59 +42,12 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             final var outputStream = connection.getOutputStream()) {
-            HttpRequest httpRequest = readRequest(inputReader);
+            HttpRequest httpRequest = HttpRequest.makeRequest(inputReader);
             String response = getResponse(httpRequest);
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
-        }
-    }
-
-    private HttpRequest readRequest(BufferedReader inputReader) {
-        RequestLine requestLine = new RequestLine(readRequestLine(inputReader));
-        RequestHeaders requestHeaders = new RequestHeaders(readHeaders(inputReader));
-        ResponseBody responseBody = new ResponseBody(readBody(inputReader, requestHeaders));
-        return new HttpRequest(requestLine,
-            requestHeaders,
-            responseBody);
-    }
-
-    private String readRequestLine(BufferedReader inputReader) {
-        try {
-            return inputReader.readLine();
-        } catch (IOException e) {
-            return null;
-        }
-    }
-
-    private String readHeaders(BufferedReader inputReader) {
-        try {
-            StringBuilder stringBuilder = new StringBuilder();
-            String line = inputReader.readLine();
-            while (!"".equals(line)) {
-                stringBuilder.append(line);
-                stringBuilder.append(System.lineSeparator());
-                line = inputReader.readLine();
-            }
-            return stringBuilder.toString();
-        } catch (IOException e) {
-            throw new IllegalArgumentException("RequestLine 을 읽을 수 없습니다.");
-        }
-    }
-
-    private String readBody(BufferedReader inputReader, RequestHeaders requestHeaders) {
-        try {
-            String s = requestHeaders.getHeaders().get("Content-Length");
-            if (s == null) {
-                return null;
-            }
-            int contentLength = Integer.parseInt(s);
-            char[] buffer = new char[contentLength];
-            inputReader.read(buffer, 0, contentLength);
-            return new String(buffer);
-        } catch (IOException e) {
-            throw new IllegalArgumentException("RequestBody 를 읽을 수 없습니다.");
         }
     }
 
@@ -129,15 +79,19 @@ public class Http11Processor implements Runnable, Processor {
     private String controllerResponse(HttpRequest httpRequest) {
         Controller handler = handlerMapper.getHandler(httpRequest);
         HttpResponse<Object> httpResponse = (HttpResponse<Object>) handler.handle(httpRequest);
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(requestLine(httpResponse));
+        return parse(httpResponse);
+    }
+
+    private String parse(HttpResponse<Object> httpResponse) {
+        StringBuilder response = new StringBuilder();
+        response.append(requestLine(httpResponse));
         Optional<String> body = bodyOf(httpResponse);
         if (body.isPresent()) {
-            return stringBuilder.append(responseWithBody(httpResponse, body.get())).toString();
+            return response.append(responseWithBody(httpResponse, body.get())).toString();
         }
         String str = responseNoBody(httpResponse);
-        stringBuilder.append("\r\n");
-        return stringBuilder.append(str).toString();
+        response.append("\r\n");
+        return response.append(str).toString();
     }
 
     private String responseNoBody(HttpResponse<Object> httpResponse) {
--- tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequest.java
@@ -1,5 +1,8 @@
 package org.apache.coyote.http11.request;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class HttpRequest {
 
     private final RequestLine requestLine;
@@ -11,6 +14,54 @@ public class HttpRequest {
         this.requestHeaders = requestHeaders;
         this.responseBody = responseBody;
     }
+
+    public static HttpRequest makeRequest(BufferedReader inputReader) {
+        RequestLine requestLine = new RequestLine(readRequestLine(inputReader));
+        RequestHeaders requestHeaders = new RequestHeaders(readHeaders(inputReader));
+        ResponseBody responseBody = new ResponseBody(readBody(inputReader, requestHeaders));
+        return new HttpRequest(requestLine,
+            requestHeaders,
+            responseBody);
+    }
+
+    private static String readRequestLine(BufferedReader inputReader) {
+        try {
+            return inputReader.readLine();
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    private static String readHeaders(BufferedReader inputReader) {
+        try {
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = inputReader.readLine();
+            while (!"".equals(line)) {
+                stringBuilder.append(line);
+                stringBuilder.append(System.lineSeparator());
+                line = inputReader.readLine();
+            }
+            return stringBuilder.toString();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("RequestLine 을 읽을 수 없습니다.");
+        }
+    }
+
+    private static String readBody(BufferedReader inputReader, RequestHeaders requestHeaders) {
+        try {
+            String s = requestHeaders.getHeaders().get("Content-Length");
+            if (s == null) {
+                return null;
+            }
+            int contentLength = Integer.parseInt(s);
+            char[] buffer = new char[contentLength];
+            inputReader.read(buffer, 0, contentLength);
+            return new String(buffer);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("RequestBody 를 읽을 수 없습니다.");
+        }
+    }
+
 
     public RequestLine getRequestLine() {
         return requestLine;

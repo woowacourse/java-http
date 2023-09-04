f70394861885c5f746f2e1ab7b996e3679837a3c
echo724 <echo.backend@gmail.com>
2023-09-04 11:46:04 +0900

    refactor: 정적 파일 요청 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -29,7 +29,15 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final List<String> request = new BufferedReader(new InputStreamReader(inputStream))
+                    .lines()
+                    .takeWhile(line -> !line.isEmpty())
+                    .collect(Collectors.toList());
+
+            final String requestLine = request.get(0);
+            final String[] requestLineTokens = requestLine.split(" ");
+            final String method = requestLineTokens[0];
+            final String requestUri = requestLineTokens[1];
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -37,9 +45,16 @@ public class Http11Processor implements Runnable, Processor {
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
                     responseBody);
+            if (method.equals("GET") && requestUri.contains(".")) {
+                generateResourceResponse(requestUri, outputStream);
+                return;
+            }
 
-            outputStream.write(response.getBytes());
-            outputStream.flush();
+
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+                return;
+            }
             generateDefaultResponse(outputStream);
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
@@ -51,6 +66,23 @@ public class Http11Processor implements Runnable, Processor {
 
         final var response = new HttpResponse(HttpStatus.OK)
                 .addContentType(ContentType.HTML)
+                .addContentLength(responseBody.length())
+                .build(responseBody);
+
+        log.info("response: {}", response);
+        outputStream.write(response.getBytes());
+        outputStream.flush();
+    }
+
+    private void generateResourceResponse(final String requestUri, final OutputStream outputStream) throws IOException {
+        final String extension = requestUri.substring(requestUri.lastIndexOf(".") + 1);
+        final ContentType contentType = ContentType.of(extension);
+        final var resource = getClass().getClassLoader().getResource("static/" + requestUri);
+
+        final String responseBody = Files.readString(Path.of(Objects.requireNonNull(resource).getPath()));
+
+        final var response = new HttpResponse(HttpStatus.OK)
+                .addContentType(contentType)
                 .addContentLength(responseBody.length())
                 .build(responseBody);
 

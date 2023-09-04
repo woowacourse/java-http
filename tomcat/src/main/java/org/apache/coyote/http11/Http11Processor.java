31d111b19cfc4f4893ddda369434bcad8b8bbf08
hum02 <ehdgma02@gmail.com>
2023-09-04 09:40:02 +0900

    feat:로그인 Query String 파싱 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,8 @@
 package org.apache.coyote.http11;
 
+import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.exception.UncheckedServletException;
+import nextstep.jwp.model.User;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,8 +21,9 @@ public class Http11Processor implements Runnable, Processor {
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
     private static final String RESOURCE_PATH = "static";
     private static final int URL_INDEX = 1;
+    public static final String ILLEGAL_REQUEST = "부적절한 요청입니다.";
     private final Socket connection;
-    
+
     public Http11Processor(final Socket connection) {
         this.connection = connection;
     }
@@ -40,17 +43,58 @@ public class Http11Processor implements Runnable, Processor {
 
             String requestLine = br.readLine();
             if (Objects.isNull(requestLine)) {
-                return;
+                throw new IllegalArgumentException(ILLEGAL_REQUEST);
             }
-            String requestUrl = RESOURCE_PATH + requestLine.split(" ")[URL_INDEX];
-            
-            URL resource = getClass().getClassLoader().getResource(requestUrl);
+
+            URL resource = getResource(requestLine);
+            if (Objects.isNull(resource)) {
+                throw new IllegalArgumentException(ILLEGAL_REQUEST);
+            }
+
             String response = buildResponse(resource);
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private URL getResource(String requestLine) {
+        String requestUri = requestLine.split(" ")[URL_INDEX];
+
+        if (requestUri.contains("/login")) {
+            int index = requestUri.indexOf("?");
+            if (index == -1) {
+                String resourcePath = RESOURCE_PATH + "/login.html";
+                return getClass().getClassLoader().getResource(resourcePath);
+            }
+
+            String path = requestUri.substring(0, index);
+            String queryString = requestUri.substring(index + 1);
+
+            String[] queryParams = queryString.split("&");
+            String account = getParamValueWithKey(queryParams, "account");
+            User user = InMemoryUserRepository.findByAccount(account).get();
+            if (user.checkPassword(getParamValueWithKey(queryParams, "password"))) {
+                log.info("User: " + user.toString());
+            }
+
+            String resourcePath = RESOURCE_PATH + path + ".html";
+            return getClass().getClassLoader().getResource(resourcePath);
+        }
+
+        String resourcePath = RESOURCE_PATH + requestUri;
+        return getClass().getClassLoader().getResource(resourcePath);
+    }
+
+    private String getParamValueWithKey(String[] queryParams, String key) {
+        for (String param : queryParams) {
+            String[] paramKeyValue = param.split("=");
+            if (paramKeyValue[0].equals(key)) {
+                return paramKeyValue[1];
+            }
+        }
+        return "";
     }
 
     private String buildResponse(URL resource) throws IOException {
@@ -67,7 +111,7 @@ public class Http11Processor implements Runnable, Processor {
 
         return String.join("\r\n",
                 "HTTP/1.1 200 OK ",
-                "Content-Type: " + contentType(resource.getPath())+ ";charset=utf-8 ",
+                "Content-Type: " + contentType(resource.getPath()) + ";charset=utf-8 ",
                 "Content-Length: " + responseBody.getBytes().length + " ",
                 "",
                 responseBody);

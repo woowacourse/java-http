2434a640f4906b9fb37517ed8426293beaff1d6f
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-09-04 14:52:43 +0900

    feat: Resource 객체의 ResourceType을 여러개 가질 수 있도록 변경

--- tomcat/src/main/java/org/apache/coyote/response/Resource.java
+++ tomcat/src/main/java/org/apache/coyote/response/Resource.java
@@ -1,34 +1,38 @@
 package org.apache.coyote.response;
 
 import java.net.URL;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Resource {
 
     private static final String NOT_FOUND_RESOURCE_PATH = "/static/404.html";
 
     private final URL url;
-    private final ResourceType resourceType;
+    private final List<ResourceType> resourceTypes;
     private final boolean isExists;
 
-    public Resource of(URL url, ResourceType resourceType) {
+    public static Resource of(URL url, List<ResourceType> resourceType) {
         if (url == null) {
-            return new Resource(getClass().getResource(NOT_FOUND_RESOURCE_PATH), resourceType, false);
+            return new Resource(Resource.class.getResource(NOT_FOUND_RESOURCE_PATH), resourceType, false);
         }
         return new Resource(url, resourceType, true);
     }
 
-    private Resource(URL url, ResourceType resourceType, boolean isExists) {
+    private Resource(URL url, List<ResourceType> resourceTypes, boolean isExists) {
         this.url = url;
-        this.resourceType = resourceType;
+        this.resourceTypes = resourceTypes;
         this.isExists = isExists;
+    }
+
+    public String getResourceTypes() {
+        return resourceTypes.stream()
+                .map(ResourceType::getResourceType)
+                .collect(Collectors.joining(","));
     }
 
     public URL getUrl() {
         return url;
-    }
-
-    public ResourceType getResourceType() {
-        return resourceType;
     }
 
     public boolean isExists() {
--- tomcat/src/main/java/org/apache/coyote/response/ResourceType.java
+++ tomcat/src/main/java/org/apache/coyote/response/ResourceType.java
@@ -1,6 +1,26 @@
 package org.apache.coyote.response;
 
+import java.util.Arrays;
+
 public enum ResourceType {
-    HTML,
-    CSS
+    HTML("text/html"),
+    CSS("text/css"),
+    ALL("*/*");
+
+    private final String resourceType;
+
+    ResourceType(String resourceType) {
+        this.resourceType = resourceType;
+    }
+
+    public static ResourceType findResourceType(String resourceType) {
+        return Arrays.stream(values())
+                .filter(resource -> resource.getResourceType().equals(resourceType))
+                .findFirst()
+                .orElse(HTML);
+    }
+
+    public String getResourceType() {
+        return resourceType;
+    }
 }

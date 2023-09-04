8c2a0b435003cc4f822b623c8fb93b7371e2de7f
Rosie <dev.kayoung@gmail.com>
2023-09-04 13:59:29 +0900

    test: 회원가입 페이지 조회

--- tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -87,4 +87,29 @@ class Http11ProcessorTest {
         //then
         assertThat(socket.output()).isEqualTo(expected);
     }
+
+    @Test
+    void register() throws IOException {
+        //given
+        final String httpRequest = String.join("\r\n",
+                "GET /register HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+        final var socket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(socket);
+
+        final URL resource = getClass().getClassLoader().getResource("static/register.html");
+        var expected = "HTTP/1.1 200 OK \r\n" +
+                "Content-Type: text/html;charset=utf-8 \r\n" +
+                "Content-Length: 4319 \r\n" +
+                "\r\n"+
+                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+        //when
+        processor.process(socket);
+
+        //then
+        assertThat(socket.output()).isEqualTo(expected);
+    }
 }

cf6099a8aebb216df66022755e6e7290d4b7a679
thdwoqor <celebarte01@gmail.com>
2023-09-04 23:51:52 +0900

    error: Http11Processor 클래스에 개행이 제대로 되지않는 문제 해결

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,1 +1,49 @@
-package org.apache.coyote.http11;import java.io.IOException;import java.net.Socket;import java.net.URISyntaxException;import nextstep.jwp.exception.UncheckedServletException;import org.apache.coyote.Processor;import org.apache.coyote.http11.request.HttpRequest;import org.apache.coyote.http11.response.HttpResponse;import org.slf4j.Logger;import org.slf4j.LoggerFactory;public class Http11Processor implements Runnable, Processor {    private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);    private final Socket connection;    public Http11Processor(final Socket connection) {        this.connection = connection;    }    @Override    public void run() {        log.info("connect host: {}, port: {}", connection.getInetAddress(), connection.getPort());        process(connection);    }    @Override    public void process(final Socket connection) {        try (final var inputStream = connection.getInputStream();             final var outputStream = connection.getOutputStream()) {            HttpRequest httpRequest = HttpRequest.of(inputStream);            HttpResponse httpResponse = new HttpResponse();            HttpServletMapper httpServletMapper = new HttpServletMapper();            HttpServlet httpServlet = httpServletMapper.get(httpRequest.getPath());            httpServlet.service(httpRequest, httpResponse);            outputStream.write(httpResponse.generateResponse());            outputStream.flush();        } catch (IOException | UncheckedServletException e) {            log.error(e.getMessage(), e);        } catch (URISyntaxException e) {            throw new RuntimeException(e);        }    }}
+package org.apache.coyote.http11;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import nextstep.jwp.exception.UncheckedServletException;
+import org.apache.coyote.Processor;
+import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.response.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Http11Processor implements Runnable, Processor {
+
+    private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+
+    private final Socket connection;
+
+    public Http11Processor(final Socket connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public void run() {
+        log.info("connect host: {}, port: {}", connection.getInetAddress(), connection.getPort());
+        process(connection);
+    }
+
+    @Override
+    public void process(final Socket connection) {
+        try (final var inputStream = connection.getInputStream();
+             final var outputStream = connection.getOutputStream()) {
+
+            HttpRequest httpRequest = HttpRequest.of(inputStream);
+            HttpResponse httpResponse = new HttpResponse();
+
+            HttpServletMapper httpServletMapper = new HttpServletMapper();
+            HttpServlet httpServlet = httpServletMapper.get(httpRequest.getPath());
+            httpServlet.service(httpRequest, httpResponse);
+
+            outputStream.write(httpResponse.generateResponse());
+            outputStream.flush();
+        } catch (IOException | UncheckedServletException e) {
+            log.error(e.getMessage(), e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

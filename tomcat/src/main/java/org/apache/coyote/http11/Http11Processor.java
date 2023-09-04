6da0d8526d02423b2015153a2f6006979ec02f0c
yenawee <yaena0319@naver.com>
2023-09-04 14:05:15 +0900

    feat: QueryString 파싱

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,8 @@
 package org.apache.coyote.http11;
 
+import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.exception.UncheckedServletException;
+import nextstep.jwp.model.User;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +14,10 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -47,22 +52,52 @@ public class Http11Processor implements Runnable, Processor {
                 request.append(line).append("\r\n");
             }
 
-            String fileName = extractResourcePathFromRequest(request);
+            String uri = extractRequestUriFromRequest(request);
+
+            int index = uri.indexOf("?");
+            String path;
+            String queryString = "";
+
+            if (index != -1) {
+                path = uri.substring(0, index);
+                queryString = uri.substring(index + 1);
+            } else {
+                path = uri;
+            }
+
+            if (path.equals("login") && !queryString.equals("")) {
+                Map<String, String> queryParams = parseQueryParams(queryString);
+                String account = queryParams.get("account");
+                String password = queryParams.get("password");
+
+                Optional<User> user = InMemoryUserRepository.findByAccount(account);
+                if (user.isPresent()) {
+                    log.info(user.toString());
+                } else {
+                    log.warn("미가입회원입니다");
+                }
+            }
+
 
             String contentType = "html";
-            if (fileName.length() != 0) {
-                String[] splitedFileName = fileName.split("\\.");
-                contentType = splitedFileName[1];
+            if (path.length() != 0) {
+                String[] splitedFileName = path.split("\\.");
+                if (splitedFileName.length != 1) {
+                    contentType = splitedFileName[1];
+                }
+                if (splitedFileName.length == 1) {
+                    path += ".html";
+                }
             }
 
             var responseBody = "";
 
-            if (fileName.length() == 0) {
+            if (uri.length() == 0) {
                 responseBody = "Hello world!";
             } else {
-                final Path path = Path.of(Objects.requireNonNull(getClass().getClassLoader().getResource("static/" + fileName)).getPath());
+                final Path filePath = Path.of(Objects.requireNonNull(getClass().getClassLoader().getResource("static/" + path)).getPath());
 
-                responseBody = new String(Files.readAllBytes(path));
+                responseBody = new String(Files.readAllBytes(filePath));
             }
 
             final var response = String.join("\r\n",
@@ -80,7 +115,7 @@ public class Http11Processor implements Runnable, Processor {
         }
     }
 
-    private String extractResourcePathFromRequest(StringBuilder request) {
+    private String extractRequestUriFromRequest(StringBuilder request) {
         int startIndex = request.indexOf("GET ") + 4;
         int endIndex = request.indexOf(" HTTP/1.1");
 
@@ -89,5 +124,21 @@ public class Http11Processor implements Runnable, Processor {
         }
 
         return "";
+    }
+
+    private Map<String, String> parseQueryParams(String input) {
+        Map<String, String> params = new HashMap<>();
+
+        String[] keyValuePairs = input.split("&");
+        for (String pair : keyValuePairs) {
+            String[] keyValue = pair.split("=");
+            if (keyValue.length == 2) {
+                String key = keyValue[0];
+                String value = keyValue[1];
+                params.put(key, value);
+            }
+        }
+
+        return params;
     }
 }

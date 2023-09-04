5189da083e0f90df9697623a7b846fca363750cc
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2023-09-04 14:53:34 +0900

    refactor: 불필요한 출력문 삭제

--- tomcat/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
+++ tomcat/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
@@ -21,7 +21,6 @@ public class InMemoryUserRepository {
     public static void save(User user) {
         final User userForSave = new User(++DEFAULT_ID, user.getAccount(), user.getPassword(), user.getEmail());
         database.put(userForSave.getAccount(), userForSave);
-        System.out.println(userForSave);
     }
 
     public static Optional<User> findByAccount(String account) {

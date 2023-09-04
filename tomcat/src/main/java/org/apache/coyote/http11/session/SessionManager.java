35451a0780a15dfd4c7931bb3d37966c025dbafc
gitchan <eunkeeeee@gmail.com>
2023-09-04 16:37:09 +0900

    feat: SessionManager 메서드 구현

--- tomcat/src/main/java/org/apache/coyote/http11/session/SessionManager.java
+++ tomcat/src/main/java/org/apache/coyote/http11/session/SessionManager.java
@@ -1,25 +1,21 @@
 package org.apache.coyote.http11.session;
 
-import jakarta.servlet.http.HttpSession;
-import org.apache.catalina.Manager;
+import java.util.HashMap;
+import java.util.Map;
 
-import java.io.IOException;
+public class SessionManager {
 
-public class SessionManager implements Manager {
+    private static final Map<String, Session> SESSIONS = new HashMap<>();
 
-    @Override
-    public void add(HttpSession session) {
+    public void add(Session session) {
+        SESSIONS.put(session.getId(), session);
     }
 
-    @Override
-    public HttpSession findSession(String id) throws IOException {
-        return null;
+    public Session findSession(String id) {
+        return SESSIONS.get(id);
     }
 
-    @Override
-    public void remove(HttpSession session) {
-    }
-
-    private SessionManager() {
+    public void remove(Session session) {
+        SESSIONS.remove(session);
     }
 }

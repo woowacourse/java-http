f48ed1d0d1dddccb30d2e5c037a097b2b6bc7197
dev_kong <ahdjd5@gmail.com>
2023-09-04 14:07:28 +0900

    feat: RequestBody 구현

--- tomcat/src/main/java/org/apache/coyote/http11/request/Request.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/Request.java
@@ -3,22 +3,38 @@ package org.apache.coyote.http11.request;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Map;
+import java.util.Objects;
 
 import org.apache.coyote.http11.common.HttpVersion;
 
 public class Request {
+    private static final String CONTENT_LENGTH = "Content-Length";
     private final RequestLine line;
     private final RequestHeader header;
+    private final RequestBody body;
 
-    private Request(final RequestLine line, final RequestHeader header) {
+    private Request(final RequestLine line, final RequestHeader header, final RequestBody body) {
         this.line = line;
         this.header = header;
+        this.body = body;
     }
 
     public static Request convert(BufferedReader bufferedReader) throws IOException {
         RequestLine requestLine = RequestLine.convert(bufferedReader.readLine());
         RequestHeader requestHeader = RequestHeader.convert(bufferedReader);
-        return new Request(requestLine, requestHeader);
+        final String contentLength = requestHeader.getHeader().get(CONTENT_LENGTH);
+        if (Objects.isNull(contentLength)) {
+            return new Request(requestLine, requestHeader, RequestBody.EMPTY_REQUEST_BODY);
+        }
+        return new Request(requestLine, requestHeader, RequestBody.convert(bufferedReader));
+    }
+
+    public boolean hasQueryString() {
+        return line.hasQueryString();
+    }
+
+    public Map<String, String> getQueryString() {
+        return line.getQueryString();
     }
 
     public RequestLine getLine() {
@@ -37,11 +53,7 @@ public class Request {
         return line.getHttpVersion();
     }
 
-    public boolean hasQueryString() {
-        return line.hasQueryString();
-    }
-
-    public Map<String, String> getQueryString() {
-        return line.getQueryString();
+    public RequestBody getBody() {
+        return body;
     }
 }

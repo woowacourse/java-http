196993443d6decbae4ae5dec04782e4c530d1592
ingpyo <juninpyo726@naver.com>
2023-09-04 17:50:48 +0900

    refactor: 타입 변경

--- tomcat/src/main/java/nextstep/jwp/db/SessionRepository.java
+++ tomcat/src/main/java/nextstep/jwp/db/SessionRepository.java
@@ -20,11 +20,11 @@ public class SessionRepository {
     public static boolean islogin(final String id) {
         return sessions.values().stream().anyMatch(session -> session.getId().equals(id));
     }
-    public Session findSession(final String id) {
+    public Session findSession(final Long id) {
         return sessions.get(id);
     }
 
-    public void remove(final String id) {
+    public void remove(final Long id) {
         sessions.remove(id);
     }
 

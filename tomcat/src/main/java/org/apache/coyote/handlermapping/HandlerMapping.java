7cd5a6c0f373c4367e80dbef0cf4f9f8b73a4c7b
JunDonghyuk <ehdgur4814@naver.com>
2023-09-04 14:54:26 +0900

    refactor: 생성자 변경

--- tomcat/src/main/java/org/apache/coyote/handlermapping/HandlerMapping.java
+++ tomcat/src/main/java/org/apache/coyote/handlermapping/HandlerMapping.java
@@ -7,26 +7,26 @@ import org.apache.coyote.handler.statichandler.*;
 import org.apache.coyote.http11.HttpMethod;
 import org.apache.coyote.http11.HttpStatus;
 
+import java.util.AbstractMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class HandlerMapping {
 
-    private static Map<HandlerMatcher, Handler> handlerMap = new ConcurrentHashMap<>() {
-        {
-            put(new HandlerMatcher(HttpMethod.GET, "/"), new HelloHandler());
-            put(new HandlerMatcher(HttpMethod.GET, "/index.html"), new HtmlHandler());
-            put(new HandlerMatcher(HttpMethod.GET, "/assets/chart-area.js"), new JsHandler());
-            put(new HandlerMatcher(HttpMethod.GET, "/assets/chart-bar.js"), new JsHandler());
-            put(new HandlerMatcher(HttpMethod.GET, "/assets/chart-pie.js"), new JsHandler());
-            put(new HandlerMatcher(HttpMethod.GET, "/js/scripts.js"), new JsHandler());
-            put(new HandlerMatcher(HttpMethod.GET, "/css/styles.css"), new CssHandler());
-            put(new HandlerMatcher(HttpMethod.GET, "/login"), new LoginHandler());
-            put(new HandlerMatcher(HttpMethod.POST, "/login"), new LoginHandler());
-            put(new HandlerMatcher(HttpMethod.GET, "/register"), new RegisterHandler());
-            put(new HandlerMatcher(HttpMethod.POST, "/register"), new RegisterHandler());
-        }
-    };
+    private static Map<HandlerMatcher, Handler> handlerMap = new ConcurrentHashMap<>(
+            Map.ofEntries(
+                    new AbstractMap.SimpleEntry<HandlerMatcher, Handler>(new HandlerMatcher(HttpMethod.GET, "/"), new HelloHandler()),
+                    new AbstractMap.SimpleEntry<HandlerMatcher, Handler>(new HandlerMatcher(HttpMethod.GET, "/index.html"), new HtmlHandler()),
+                    new AbstractMap.SimpleEntry<HandlerMatcher, Handler>(new HandlerMatcher(HttpMethod.GET, "/assets/chart-area.js"), new JsHandler()),
+                    new AbstractMap.SimpleEntry<HandlerMatcher, Handler>(new HandlerMatcher(HttpMethod.GET, "/assets/chart-bar.js"), new JsHandler()),
+                    new AbstractMap.SimpleEntry<HandlerMatcher, Handler>(new HandlerMatcher(HttpMethod.GET, "/assets/chart-pie.js"), new JsHandler()),
+                    new AbstractMap.SimpleEntry<HandlerMatcher, Handler>(new HandlerMatcher(HttpMethod.GET, "/js/scripts.js"), new JsHandler()),
+                    new AbstractMap.SimpleEntry<HandlerMatcher, Handler>(new HandlerMatcher(HttpMethod.GET, "/css/styles.css"), new CssHandler()),
+                    new AbstractMap.SimpleEntry<HandlerMatcher, Handler>(new HandlerMatcher(HttpMethod.GET, "/login"), new LoginHandler()),
+                    new AbstractMap.SimpleEntry<HandlerMatcher, Handler>(new HandlerMatcher(HttpMethod.POST, "/login"), new LoginHandler()),
+                    new AbstractMap.SimpleEntry<HandlerMatcher, Handler>(new HandlerMatcher(HttpMethod.GET, "/register"), new RegisterHandler()),
+                    new AbstractMap.SimpleEntry<HandlerMatcher, Handler>(new HandlerMatcher(HttpMethod.POST, "/register"), new RegisterHandler())
+            ));
 
     public static boolean canHandle(HandlerMatcher handlerMatcher) {
         if (handlerMap.containsKey(handlerMatcher)) {

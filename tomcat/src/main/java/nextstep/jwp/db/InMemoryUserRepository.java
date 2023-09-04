49856108411a31b74f292e4aba8a34b8a00bb2a4
ReO <yt930310@gmail.com>
2023-09-04 14:17:03 +0900

    refactor: AtomicLong을 사용하여 동시성 문제 해결하도록 user save로직 변경

--- tomcat/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
+++ tomcat/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
@@ -2,17 +2,18 @@ package nextstep.jwp.db;
 
 import nextstep.jwp.model.User;
 
-import java.util.Comparator;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class InMemoryUserRepository {
 
     private static final Map<String, User> database = new ConcurrentHashMap<>();
+    private static final AtomicLong id = new AtomicLong(1L);
 
     static {
-        final User user = new User(1L, "gugu", "password", "hkkang@woowahan.com");
+        final User user = new User(id.getAndIncrement(), "gugu", "password", "hkkang@woowahan.com");
         database.put(user.getAccount(), user);
     }
 
@@ -20,12 +21,14 @@ public class InMemoryUserRepository {
     }
 
     public static void save(User user) {
-        database.values().stream()
-                .max(Comparator.comparingLong(User::getId))
-                .ifPresent(maxIdUser -> {
-                    long nextId = maxIdUser.getId() + 1;
-                    user.setId(nextId);
-                });
+        if (user.getId() != null) {
+            if (user.equals(database.get(user.getAccount()))) {
+                throw new IllegalArgumentException("이미 가입된 유저입니다.");
+            }
+            throw new IllegalArgumentException("잘못된 유저 정보입니다.");
+        }
+
+        user.setId(id.getAndIncrement());
         database.put(user.getAccount(), user);
     }
 

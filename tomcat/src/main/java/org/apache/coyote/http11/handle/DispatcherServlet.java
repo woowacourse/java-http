925cd07a124589a3226baef25bcc0151efaeb73e
ingpyo <juninpyo726@naver.com>
2023-09-04 17:39:53 +0900

    refactor: 메서드 분리

--- tomcat/src/main/java/org/apache/coyote/http11/handle/DispatcherServlet.java
+++ tomcat/src/main/java/org/apache/coyote/http11/handle/DispatcherServlet.java
@@ -10,13 +10,13 @@ import org.apache.coyote.http11.handle.view.ViewHandler;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.request.start.HttpExtension;
 import org.apache.coyote.http11.response.HttpResponse;
-import org.apache.coyote.http11.response.HttpStatus;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.apache.coyote.http11.request.start.HttpExtension.*;
+import static org.apache.coyote.http11.response.HttpStatus.*;
 import static org.apache.coyote.http11.response.HttpStatus.FOUND;
 import static org.apache.coyote.http11.response.HttpStatus.OK;
 
@@ -33,17 +33,27 @@ public class DispatcherServlet {
     }
 
     public static HttpResponse from(HttpRequest request) throws IOException {
-        if (request.getHttpStartLine().getExtension().equals(DEFAULT)) {
-            final HandleResponse handleResponse = logicHandler.handle(request);
-            if (handleResponse.getHttpStatus().equals(OK)||handleResponse.getHttpStatus().equals(FOUND)) {
-                return HttpResponse.of(request.getHttpStartLine().getHttpVersion(), FOUND.getStatusName(),handleResponse.getBody(), "/index.html");
-            }
-            if (handleResponse.getHttpStatus().equals(HttpStatus.NOT)) {
-                return HttpResponse.of(request.getHttpStartLine().getHttpVersion(), FOUND.getStatusName(), "/login.html");
-            }
+        HttpExtension extension = request.getHttpStartLine().getExtension();
+        if (extension.equals(DEFAULT)) {
+            return handleLogicRequest(request);
+        }
+        return handleViewRequest(request, extension);
+    }
+
+    private static HttpResponse handleViewRequest(final HttpRequest request, final HttpExtension extension) throws IOException {
+        final ViewHandler viewHandler = viewHandlers.get(extension);
+        return viewHandler.handle(request);
+    }
+
+    private static HttpResponse handleLogicRequest(final HttpRequest request) {
+        final HandleResponse handleResponse = logicHandler.handle(request);
+        if (handleResponse.getHttpStatus().equals(OK)
+                || handleResponse.getHttpStatus().equals(FOUND)) {
+            return HttpResponse.of(request.getHttpStartLine().getHttpVersion(), FOUND.getStatusName(), handleResponse.getBody(), "/index.html");
+        }
+        if (handleResponse.getHttpStatus().equals(NOT)) {
             return HttpResponse.of(request.getHttpStartLine().getHttpVersion(), FOUND.getStatusName(), "/login.html");
         }
-        final ViewHandler viewHandler = viewHandlers.get(request.getHttpStartLine().getExtension());
-        return viewHandler.handle(request);
+        return HttpResponse.of(request.getHttpStartLine().getHttpVersion(), FOUND.getStatusName(), "/login.html");
     }
 }

84a16313a83fc15b771b375ee3428c2645e36c1a
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-09-04 14:34:23 +0900

    feat: 전달받은 request header의 content-type을 추출하는 기능 구현

--- tomcat/src/main/java/org/apache/coyote/request/UrlParser.java
+++ tomcat/src/main/java/org/apache/coyote/request/UrlParser.java
@@ -4,15 +4,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import org.apache.coyote.response.Resource;
+import org.apache.coyote.response.ResourceType;
 
 public class UrlParser {
 
+    private static final String HOME_URL = "/";
     private static final String DEFAULT_RESOURCE_PATH = "/static";
     private static final String INDEX_HTML_FILE_NAME = "/index.html";
-    private static final String SPLIT_DELIMITER = " ";
+    private static final String ACCEPT_HEADER = "Accept: ";
+    private static final String CONTENT_TYPE = "Content-Type: ";
+    private static final String SPLIT_HEADER_DELIMITER = " ";
+    private static final String FINISH_SPLIT_DELIMITER = ";";
+    private static final String SPLIT_VALUE_DELIMITER = ",";
+
     private static final int RESOURCE_INDEX = 1;
+    private static final int FINISH_RESOURCE_INDEX = 0;
 
     private final BufferedReader bufferedReader;
 
@@ -21,11 +32,32 @@ public class UrlParser {
         this.bufferedReader = new BufferedReader(inputStreamReader);
     }
 
-    public URL getResourceUrl() throws IOException {
-        String resourceUrl = bufferedReader.readLine().split(SPLIT_DELIMITER)[RESOURCE_INDEX];
-        if (Objects.equals(resourceUrl, "/")) {
-            return getClass().getResource(DEFAULT_RESOURCE_PATH + INDEX_HTML_FILE_NAME);
+    public Resource getResource() throws IOException {
+        String resourceUrl = getLines(bufferedReader.readLine());
+        List<ResourceType> resourceType = getResourceType();
+
+        if (Objects.equals(resourceUrl, HOME_URL)) {
+            return Resource.of(getClass().getResource(DEFAULT_RESOURCE_PATH + INDEX_HTML_FILE_NAME), resourceType);
         }
-        return getClass().getResource(DEFAULT_RESOURCE_PATH + resourceUrl);
+        return Resource.of(getClass().getResource(DEFAULT_RESOURCE_PATH + resourceUrl), resourceType);
+    }
+
+    private String getLines(String message) {
+        return message.split(SPLIT_HEADER_DELIMITER)[RESOURCE_INDEX]
+                .split(FINISH_SPLIT_DELIMITER)[FINISH_RESOURCE_INDEX];
+    }
+
+    private List<ResourceType> getResourceType() throws IOException {
+        String header;
+        while ((header = bufferedReader.readLine()) != null) {
+            if (header.startsWith(ACCEPT_HEADER) || header.startsWith(CONTENT_TYPE)) {
+                String resourceType = getLines(header);
+                return Arrays.stream(resourceType.split(SPLIT_VALUE_DELIMITER))
+                        .map(ResourceType::findResourceType)
+                        .collect(Collectors.toList());
+
+            }
+        }
+        return List.of(ResourceType.HTML);
     }
 }
--- tomcat/src/test/java/org/apache/coyote/request/UrlParserTest.java
+++ tomcat/src/test/java/org/apache/coyote/request/UrlParserTest.java
@@ -1,47 +1,62 @@
 package org.apache.coyote.request;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
+import org.apache.coyote.response.Resource;
+import org.apache.coyote.response.ResourceType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class UrlParserTest {
 
     @Test
-    @DisplayName("주어진 InputStream의 url을 파싱하여 url을 생성한다.")
-    void getResourceUrl_normal() throws IOException {
+    @DisplayName("주어진 InputStream의 정보를 파싱하여 resource 객체를 생성한다.")
+    void getResource_normal() throws IOException {
         InputStream inputStream = new ByteArrayInputStream("GET /index.html HTTP/1.1".getBytes());
         UrlParser urlParser = new UrlParser(inputStream);
 
-        URL resourceUrl = urlParser.getResourceUrl();
+        Resource resource = urlParser.getResource();
 
-        assertThat(resourceUrl.getFile()).contains("/index.html");
+        assertAll(
+                () -> assertThat(resource.getUrl().getFile()).contains("/index.html"),
+                () -> assertThat(resource.getResourceTypes()).contains(ResourceType.HTML.getResourceType()),
+                () -> assertThat(resource.isExists()).isTrue()
+        );
     }
 
     @Test
-    @DisplayName("주어진 url이 /라면 index.html에 대한 url을 생성한다.")
-    void getResourceUrl_home() throws IOException {
+    @DisplayName("주어진 url이 /라면 index.html에 대한 resource를 생성한다.")
+    void getResource_home() throws IOException {
         InputStream inputStream = new ByteArrayInputStream("GET / HTTP/1.1".getBytes());
         UrlParser urlParser = new UrlParser(inputStream);
 
-        URL resourceUrl = urlParser.getResourceUrl();
+        Resource resource = urlParser.getResource();
 
-        assertThat(resourceUrl.getFile()).contains("/index.html");
+        assertAll(
+                () -> assertThat(resource.getUrl().getFile()).contains("/index.html"),
+                () -> assertThat(resource.getResourceTypes()).contains(ResourceType.HTML.getResourceType()),
+                () -> assertThat(resource.isExists()).isTrue()
+        );
     }
 
 
     @Test
-    @DisplayName("주어진 url의 파일이 없다면 null을 반환한다.")
+    @DisplayName("주어진 url의 파일이 없다면 url이 null이고 isExists가 false인 resource 객체가 반환한다.")
     void getResourceUrl_noFile() throws IOException {
-        InputStream inputStream = new ByteArrayInputStream("GET /trestset/qweqsdae/asdawdqd/qwdqweqw HTTP/1.1".getBytes());
+        InputStream inputStream = new ByteArrayInputStream(
+                "GET /trestset/qweqsdae/asdawdqd/qwdqweqw HTTP/1.1".getBytes());
         UrlParser urlParser = new UrlParser(inputStream);
 
-        URL resourceUrl = urlParser.getResourceUrl();
+        Resource resource = urlParser.getResource();
 
-        assertThat(resourceUrl).isNull();
+        assertAll(
+                () -> assertThat(resource.getUrl().getFile()).contains("/404.html"),
+                () -> assertThat(resource.getResourceTypes()).contains(ResourceType.HTML.getResourceType()),
+                () -> assertThat(resource.isExists()).isFalse()
+        );
     }
 }

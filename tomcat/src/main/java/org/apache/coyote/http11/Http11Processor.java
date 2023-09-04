33bab93a565f30304f24dd8c0d89f5b1014df95d
Combi153 <jcm04@naver.com>
2023-09-04 14:13:36 +0900

    [feat] Post 방식 회원가입

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,7 +5,6 @@ import static java.util.stream.Collectors.toList;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.URL;
@@ -51,12 +50,13 @@ public class Http11Processor implements Runnable, Processor {
                 final var outputStream = connection.getOutputStream()
         ) {
             // read input stream
-            String request = readAsUTF8(inputStream);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            String request = readAsUTF8(reader);
             // parse headers
             Map<String, String> headers = parseHeaders(request);
 
             // Query String
-            String response = makeResponse(headers);
+            String response = makeResponse(headers, reader);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
@@ -65,32 +65,30 @@ public class Http11Processor implements Runnable, Processor {
         }
     }
 
-    private String makeResponse(Map<String, String> headers) throws IOException {
-        String url = headers.get("URL");
+    private String makeResponse(Map<String, String> headers, BufferedReader reader) throws IOException {
+        if (Objects.equals(headers.get("HTTP Method"), "POST")) {
+            String url = headers.get("URL");
 
-        if (Objects.equals(url, "/")) {
-            return makeRootResponse();
-        }
+            if (url.contains("/login")) {
+                String code = "302 Found";
+                String location = "index.html";
 
-        if (url.contains("/login?")) {
-            String code = "302 Found";
-            String location = "index.html";
+                int contentLength = Integer.parseInt(headers.get("Content-Length"));
+                char[] buffer = new char[contentLength];
+                reader.read(buffer, 0, contentLength);
+                String requestBody = new String(buffer);
 
-            int queryIndex = url.indexOf("?");
-            if (queryIndex != -1) {
-                String query = url.substring(queryIndex + 1);
-                url = url.substring(0, queryIndex);
-
-                Map<String, String> parameters = new HashMap<>();
-                List<String> queryParameters = Arrays.stream(query.split("&")).collect(toList());
-                for (String parameter : queryParameters) {
+                List<String> parameters = Arrays.stream(requestBody.split("&")).collect(toList());
+                Map<String, String> parametersMap = new HashMap<>();
+                for (String parameter : parameters) {
                     int delimiterIndex = parameter.indexOf("=");
                     String field = parameter.substring(0, delimiterIndex);
                     String value = parameter.substring(delimiterIndex + 1);
-                    parameters.put(field, value);
+                    parametersMap.put(field, value);
                 }
-                String account = parameters.get("account");
-                String password = parameters.get("password");
+
+                String account = parametersMap.get("account");
+                String password = parametersMap.get("password");
 
                 try {
                     checkUserCredential(account, password);
@@ -98,7 +96,47 @@ public class Http11Processor implements Runnable, Processor {
                     code = "401 Unauthorized ";
                     location = "401.html";
                 }
+
+                File file = mapToResourceFile(location);
+
+                // file type
+                String fileName = file.getName();
+                String fileExtension = extractFileExtension(fileName);
+                String type = mapMimeType(fileExtension);
+
+                // resource
+                String responseBody = new String(Files.readAllBytes(file.toPath()));
+
+                return String.join(
+                        System.lineSeparator(),
+                        "HTTP/1.1 " + code,
+                        "Location:" + location,
+                        "Content-Type: " + type + ";charset=utf-8 ",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody
+                );
             }
+
+            int contentLength = Integer.parseInt(headers.get("Content-Length"));
+            char[] buffer = new char[contentLength];
+            reader.read(buffer, 0, contentLength);
+            String requestBody = new String(buffer);
+
+            List<String> parameters = Arrays.stream(requestBody.split("&")).collect(toList());
+            Map<String, String> parametersMap = new HashMap<>();
+            for (String parameter : parameters) {
+                int delimiterIndex = parameter.indexOf("=");
+                String field = parameter.substring(0, delimiterIndex);
+                String value = parameter.substring(delimiterIndex + 1);
+                parametersMap.put(field, value);
+            }
+            User user = new User(parametersMap.get("account"), parametersMap.get("password"),
+                    parametersMap.get("email"));
+            InMemoryUserRepository.save(user);
+
+            String location = "index.html";
+            String code = "200 OK ";
 
             File file = mapToResourceFile(location);
 
@@ -119,6 +157,11 @@ public class Http11Processor implements Runnable, Processor {
                     "",
                     responseBody
             );
+        }
+        String url = headers.get("URL");
+
+        if (Objects.equals(url, "/")) {
+            return makeRootResponse();
         }
 
         File file = mapToResourceFile(url);
@@ -188,9 +231,7 @@ public class Http11Processor implements Runnable, Processor {
         return fileName.substring(lastDotIndex + 1);
     }
 
-    private String readAsUTF8(InputStream inputStream) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-
+    private String readAsUTF8(BufferedReader reader) throws IOException {
         String line;
         StringBuilder builder = new StringBuilder();
         while ((line = reader.readLine()) != null & !Objects.equals(END_OF_LINE, line)) {

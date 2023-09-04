e57fcf7aa566c2dbd95564fdae264e45cc526b7b
woosung1223 <dntjd991223@naver.com>
2023-09-04 23:22:26 +0900

    chore: 리소스 패키지 상수처리

--- tomcat/src/main/java/org/apache/coyote/http11/handler/Handler.java
+++ tomcat/src/main/java/org/apache/coyote/http11/handler/Handler.java
@@ -9,10 +9,12 @@ import org.apache.coyote.http11.message.response.Response;
 
 public abstract class Handler {
 
+    private static final String RESOURCE_BASE_PATH = "static/";
+
     abstract Response handle(Request request) throws IOException;
 
     protected String findResourceWithPath(String absolutePath) throws IOException {
-        URL resourceUrl = Handler.class.getClassLoader().getResource("static/" + absolutePath);
+        URL resourceUrl = Handler.class.getClassLoader().getResource(RESOURCE_BASE_PATH + absolutePath);
         return new String(Files.readAllBytes(new File(resourceUrl.getFile()).toPath()));
     }
 }

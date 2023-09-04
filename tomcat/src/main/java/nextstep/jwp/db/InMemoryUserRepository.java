836ccc8fa0ab33f00ef9df4cca897ae1f46355a0
ingpyo <juninpyo726@naver.com>
2023-09-04 17:20:17 +0900

    feat: 자동으로 id를 증가시키는 counter정의

--- tomcat/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
+++ tomcat/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
@@ -5,22 +5,29 @@ import nextstep.jwp.model.User;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class InMemoryUserRepository {
 
     private static final Map<String, User> database = new ConcurrentHashMap<>();
+    private static AtomicLong counter = new AtomicLong();
 
     static {
         final User user = new User(1L, "gugu", "password", "hkkang@woowahan.com");
         database.put(user.getAccount(), user);
     }
 
-    public static void save(User user) {
-        database.put(user.getAccount(), user);
+    public static long save(User user) {
+        final long andIncrement = counter.getAndIncrement();
+        database.put(user.getAccount(), user.setId(andIncrement));
+        return andIncrement;
     }
 
     public static Optional<User> findByAccount(String account) {
-        return Optional.ofNullable(database.get(account));
+        if (database.keySet().stream().anyMatch(key -> key.equals(account))) {
+            return Optional.of(database.get(account));
+        }
+        return Optional.empty();
     }
 
     private InMemoryUserRepository() {}

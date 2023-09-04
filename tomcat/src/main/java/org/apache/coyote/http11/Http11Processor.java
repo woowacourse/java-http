b310ed7506c8ae40a65dd9cb0660e89dbf664e0e
echo724 <echo.backend@gmail.com>
2023-09-04 12:14:03 +0900

    feat: 로그인 및 리다이렉션 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,22 @@
 package org.apache.coyote.http11;
 
+import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.exception.UncheckedServletException;
+import nextstep.jwp.model.User;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -34,31 +44,39 @@ public class Http11Processor implements Runnable, Processor {
                     .takeWhile(line -> !line.isEmpty())
                     .collect(Collectors.toList());
 
-            final String requestLine = request.get(0);
-            final String[] requestLineTokens = requestLine.split(" ");
-            final String method = requestLineTokens[0];
-            final String requestUri = requestLineTokens[1];
+            final HttpRequest httpRequest = new HttpRequest(request);
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
-            if (method.equals("GET") && requestUri.contains(".")) {
-                generateResourceResponse(requestUri, outputStream);
+            if (httpRequest.getMethod() == HttpMethod.GET && httpRequest.getRequestUri().contains(".")) {
+                generateResourceResponse(httpRequest, outputStream);
                 return;
             }
 
-
-                outputStream.write(response.getBytes());
-                outputStream.flush();
+            final String path = httpRequest.getPath();
+            if (httpRequest.getMethod() == HttpMethod.GET && path.equals("/login")) {
+                generateLoginResponse(httpRequest, outputStream);
                 return;
             }
             generateDefaultResponse(outputStream);
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private static void generateLoginResponse(final HttpRequest httpRequest, final OutputStream outputStream) throws IOException {
+        final QueryString queryString = httpRequest.getQueryString();
+        final String account = queryString.getValue("account");
+        final String password = queryString.getValue("password");
+        final User loginUser = InMemoryUserRepository.findByAccount(account)
+                .filter(user -> user.checkPassword(password))
+                .orElseThrow();
+        log.info("login user: {}", loginUser);
+
+        final String response = new HttpResponse(HttpStatus.FOUND)
+                .addLocation("/index.html")
+                .build();
+
+        outputStream.write(response.getBytes());
+        outputStream.flush();
     }
 
     private static void generateDefaultResponse(final OutputStream outputStream) throws IOException {
@@ -74,7 +92,8 @@ public class Http11Processor implements Runnable, Processor {
         outputStream.flush();
     }
 
-    private void generateResourceResponse(final String requestUri, final OutputStream outputStream) throws IOException {
+    private void generateResourceResponse(final HttpRequest request, final OutputStream outputStream) throws IOException {
+        final String requestUri = request.getRequestUri();
         final String extension = requestUri.substring(requestUri.lastIndexOf(".") + 1);
         final ContentType contentType = ContentType.of(extension);
         final var resource = getClass().getClassLoader().getResource("static/" + requestUri);

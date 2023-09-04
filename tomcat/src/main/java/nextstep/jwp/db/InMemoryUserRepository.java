57d1cd9a53f61b7d4423d2c93efced5cfc830bc2
xxeol2 <jhj01177@naver.com>
2023-09-04 13:11:52 +0900

    refactor: InMemoryUserRepository에서 id 저장 로직 추가

--- tomcat/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
+++ tomcat/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
@@ -3,15 +3,17 @@ package nextstep.jwp.db;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 import nextstep.jwp.model.User;
 
 public class InMemoryUserRepository {
 
 	private static final Map<String, User> database = new ConcurrentHashMap<>();
+	private static final AtomicLong idCounter = new AtomicLong();
 
 	static {
-		final User user = new User(1L, "gugu", "password", "hkkang@woowahan.com");
+		final User user = new User(idCounter.incrementAndGet(), "gugu", "password", "hkkang@woowahan.com");
 		database.put(user.getAccount(), user);
 	}
 
@@ -19,7 +21,7 @@ public class InMemoryUserRepository {
 	}
 
 	public static void save(User user) {
-		database.put(user.getAccount(), user);
+		database.put(user.getAccount(), user.addId(idCounter.incrementAndGet()));
 	}
 
 	public static Optional<User> findByAccount(String account) {

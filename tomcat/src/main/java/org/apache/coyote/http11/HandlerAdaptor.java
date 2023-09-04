5b9f66bb8cda2756f0af9a82672cc793c8cd86ff
yoondgu <doyou2ing@naver.com>
2023-09-04 14:39:21 +0900

    refactor: 핸들러 메서드 함수형 인터페이스 정의

--- tomcat/src/main/java/org/apache/coyote/http11/HandlerAdaptor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HandlerAdaptor.java
@@ -5,15 +5,15 @@ import static org.apache.coyote.http11.common.Method.POST;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Function;
 import nextstep.jwp.controller.UserController;
 import org.apache.coyote.http11.common.HandlerMapping;
+import org.apache.coyote.http11.common.HandlerMethod;
 import org.apache.coyote.http11.request.Request;
 import org.apache.coyote.http11.response.Response;
 
 public class HandlerAdaptor {
 
-    private static final Map<HandlerMapping, Function<Request, Response>> handlerMethods = new HashMap<>(
+    private static final Map<HandlerMapping, HandlerMethod> handlerMethods = new HashMap<>(
             Map.of(
                     new HandlerMapping(POST, "/login"), UserController::login,
                     new HandlerMapping(POST, "/register"), UserController::register
@@ -23,12 +23,12 @@ public class HandlerAdaptor {
     public Response getMapping(Request request) {
         /// TODO: 2023/09/04 매핑되는 메서드가 없을 때 예외처리
         return handlerMethods.get(new HandlerMapping(GET, request.getPath()))
-                .apply(request);
+                .handle(request);
     }
 
     public Response postMapping(Request request) {
         /// TODO: 2023/09/04 매핑되는 메서드가 없을 때 예외처리
         return handlerMethods.get(new HandlerMapping(POST, request.getPath()))
-                .apply(request);
+                .handle(request);
     }
 }

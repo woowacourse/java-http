a2c4a3fc8446b0825b548eaed7d8cdf34f46b02f
kdkdhoho <hkim4410@naver.com>
2023-09-04 09:34:18 +0900

    feat: HttpMethod enum 클래스 생성

---
+++ tomcat/src/test/java/org/apache/coyote/HttpMethodTest.java
@@ -0,0 +1,31 @@
+package org.apache.coyote;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+class HttpMethodTest {
+
+    @Test
+    void 문자열로_HTTP_Method를_찾는다() {
+        // given
+        String requestMethod = "GET";
+
+        // when
+        HttpMethod result = HttpMethod.from(requestMethod);
+
+        // then
+        HttpMethod expect = HttpMethod.GET;
+        assertThat(expect).isEqualTo(result);
+    }
+
+    @Test
+    void 존재하지_않는_Method이면_예외가_발생한다() {
+        // given
+        String requestMethod = "GETPOST";
+
+        // expect
+        assertThrows(RuntimeException.class, () -> HttpMethod.from(requestMethod));
+    }
+}

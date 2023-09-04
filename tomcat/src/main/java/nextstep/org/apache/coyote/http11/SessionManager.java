34ead5afe91b4c3891d1d76243ee78c8d4e8f484
MoonJeWoong <munjin0201@naver.com>
2023-09-04 14:29:39 +0900

    feat: session, sessionManager 기능 구현

---
+++ tomcat/src/main/java/nextstep/org/apache/coyote/http11/SessionManager.java
@@ -0,0 +1,28 @@
+package nextstep.org.apache.coyote.http11;
+
+import java.util.HashMap;
+import java.util.Map;
+import nextstep.org.apache.catalina.Manager;
+
+public class SessionManager implements Manager {
+
+    private static final Map<String, Session> SESSIONS = new HashMap<>();
+
+    public SessionManager() {}
+
+    @Override
+    public void add(Session session) {
+        SESSIONS.put(session.getId(), session);
+    }
+
+    @Override
+    public Session findSession(String id) {
+        return SESSIONS.get(id);
+    }
+
+    @Override
+    public void remove(Session session) {
+        SESSIONS.remove(session.getId());
+    }
+}
+

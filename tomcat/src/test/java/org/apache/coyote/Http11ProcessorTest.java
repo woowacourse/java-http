595a26e2c02d66ac51967390af70e9c7a8a3105d
apptie <appti@naver.com>
2023-09-04 14:42:22 +0900

    feat: Http11Processor에 Context 연동

--- tomcat/src/test/java/org/apache/coyote/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/Http11ProcessorTest.java
@@ -1,41 +1,55 @@
-package nextstep.org.apache.coyote.http11;
+package org.apache.coyote;
 
-import support.StubSocket;
-import org.apache.coyote.http11.Http11Processor;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.apache.coyote.http11.Http11Processor;
+import org.junit.jupiter.api.Test;
+import support.StubSocket;
 
 class Http11ProcessorTest {
 
     @Test
     void process() {
-        // given
         final var socket = new StubSocket();
         final var processor = new Http11Processor(socket);
 
-        // when
         processor.process(socket);
 
-        // then
         var expected = String.join("\r\n",
                 "HTTP/1.1 200 OK ",
                 "Content-Type: text/html;charset=utf-8 ",
                 "Content-Length: 12 ",
                 "",
-                "Hello world!");
+                "Hello World!");
 
         assertThat(socket.output()).isEqualTo(expected);
     }
 
     @Test
+    void 존재하지_않는_contextPath로_요청하면_404가_발생한다() {
+        final String httpRequest= String.join("\r\n",
+                "GET /hello/index.html HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+        final var socket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(socket);
+
+        processor.process(socket);
+
+        final String actual = socket.output();
+
+        assertThat(actual).contains("404 Not Found");
+    }
+
+    @Test
     void index() throws IOException {
-        // given
         final String httpRequest= String.join("\r\n",
                 "GET /index.html HTTP/1.1 ",
                 "Host: localhost:8080 ",
@@ -46,10 +60,8 @@ class Http11ProcessorTest {
         final var socket = new StubSocket(httpRequest);
         final Http11Processor processor = new Http11Processor(socket);
 
-        // when
         processor.process(socket);
 
-        // then
         final URL resource = getClass().getClassLoader().getResource("static/index.html");
         var expected = "HTTP/1.1 200 OK \r\n" +
                 "Content-Type: text/html;charset=utf-8 \r\n" +

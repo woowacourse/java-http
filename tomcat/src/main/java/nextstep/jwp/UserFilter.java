1ab0ad79ee79546060e9b2a899caab1501caa63b
ingpyo <juninpyo726@naver.com>
2023-09-04 17:21:29 +0900

    refactor: login.html 수정 및 세션을 검사하는 UserFilter적용

---
+++ tomcat/src/main/java/nextstep/jwp/UserFilter.java
@@ -0,0 +1,21 @@
+package nextstep.jwp;
+
+import nextstep.jwp.controller.dto.Response;
+import nextstep.jwp.db.SessionRepository;
+import org.apache.coyote.http11.response.HttpStatus;
+
+import java.util.Map;
+
+public class UserFilter {
+    public static Response verifyLogin(final Map<String, String> httpStartLine) {
+        if (httpStartLine.keySet().stream().anyMatch(key -> key.equals("Cookie"))) {
+            if (httpStartLine.get("Cookie").contains("JSESSIONID")) {
+                final String id = httpStartLine.get("Cookie").split("=")[1];
+                if (SessionRepository.islogin(id)) {
+                    return new Response(HttpStatus.FOUND);
+                }
+            }
+        }
+        return new Response(HttpStatus.NOT);
+    }
+}

2909da4480f5245e664a09c7630ce0e9852e2975
BGuga <tmdwns9913@gmail.com>
2023-09-05 21:37:24 +0900

    refactor: Enum 의 name method 를 통해 HttpMethod 를 반환하도록 변경

--- tomcat/src/main/java/org/apache/coyote/http11/request/HttpMethod.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/HttpMethod.java
@@ -3,23 +3,17 @@ package org.apache.coyote.http11.request;
 import java.util.Arrays;
 
 public enum HttpMethod {
-    GET("GET"),
-    POST("POST"),
-    PUT("PUT"),
-    DELETE("DELETE");
-
-    private final String methodName;
-
-    HttpMethod(String methodName) {
-        this.methodName = methodName;
-    }
+    GET,
+    POST,
+    PUT,
+    DELETE;
 
     public static HttpMethod of(String targetMethod) {
         if (targetMethod == null) {
             throw new IllegalArgumentException("null 에 대한 메서드는 존재하지 않습니다.");
         }
         return Arrays.stream(HttpMethod.values())
-            .filter(httpMethod -> targetMethod.toUpperCase().equals(httpMethod.methodName))
+            .filter(httpMethod -> targetMethod.toUpperCase().equals(httpMethod.name()))
             .findFirst()
             .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 메서드 입니다."));
     }

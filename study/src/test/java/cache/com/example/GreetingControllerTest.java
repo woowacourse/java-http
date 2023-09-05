7e92de78790fb126194f3a07fe836842f3fddbcd
jaeyeon kim <jakind@naver.com>
2023-09-05 16:26:20 +0900

    test : cache 미션 1, 2 단계 완료

--- study/src/test/java/cache/com/example/GreetingControllerTest.java
+++ study/src/test/java/cache/com/example/GreetingControllerTest.java
@@ -47,7 +47,7 @@ class GreetingControllerTest {
                 .expectStatus().isOk()
 
                 // gzip으로 요청 보내도 어떤 방식으로 압축할지 서버에서 결정한다.
-                // 웹브라우저에서 localhost:8080으로 접근하면 응답 헤더에 "Content-Encoding: gzip"이 있다.
+                // 웹브라우저에서 localhost8080으로 접근하면 응답 헤더에 "Content-Encoding: gzip"이 있다.
                 .expectHeader().valueEquals(HttpHeaders.TRANSFER_ENCODING, "chunked")
                 .expectBody(String.class).returnResult();
 

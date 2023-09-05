08b0e2fc3b282198581bd13dbce032e3ead53254
junpakPark <junpak.park@gmail.com>
2023-09-05 20:53:31 +0900

    refactor: 네이밍 변경

--- tomcat/src/main/java/org/apache/coyote/http11/common/MimeType.java
+++ tomcat/src/main/java/org/apache/coyote/http11/common/MimeType.java
@@ -1,6 +1,6 @@
-package org.apache.coyote.http11;
+package org.apache.coyote.http11.common;
 
-public enum FileExtension {
+public enum MimeType {
 
     NONE("text/plain"),
     HTML("text/html"),
@@ -10,15 +10,15 @@ public enum FileExtension {
 
     private final String contentType;
 
-    FileExtension(String contentType) {
+    MimeType(String contentType) {
         this.contentType = contentType;
     }
 
-    public static FileExtension from(String fileExtension) {
+    public static MimeType from(String fileExtension) {
         if (fileExtension == null) {
             return NONE;
         }
-        return FileExtension.valueOf(fileExtension.toUpperCase());
+        return MimeType.valueOf(fileExtension.toUpperCase());
     }
 
     public String getContentType() {

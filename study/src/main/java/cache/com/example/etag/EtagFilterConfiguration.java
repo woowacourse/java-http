aeab281e66bafae3d98b309dbefadcdaac58e568
hong-sile <gurwns9325@naver.com>
2023-09-05 17:52:04 +0900

    feat: 캐시 버스팅 추가

--- study/src/main/java/cache/com/example/etag/EtagFilterConfiguration.java
+++ study/src/main/java/cache/com/example/etag/EtagFilterConfiguration.java
@@ -8,12 +8,15 @@ import org.springframework.web.filter.ShallowEtagHeaderFilter;
 @Configuration
 public class EtagFilterConfiguration {
 
+	public static final String PREFIX_STATIC_RESOURCES = "/resources";
+
 	@Bean
 	public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
 		final ShallowEtagHeaderFilter shallowEtagHeaderFilter = new ShallowEtagHeaderFilter();
 		final FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean
 			= new FilterRegistrationBean<>(shallowEtagHeaderFilter);
 		filterRegistrationBean.addUrlPatterns("/etag");
+		filterRegistrationBean.addUrlPatterns(PREFIX_STATIC_RESOURCES + "/*");
 		return filterRegistrationBean;
 	}
 }

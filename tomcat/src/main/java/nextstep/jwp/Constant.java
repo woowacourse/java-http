347055c54b39f4f13fb5f1fe6573ebbb15f9c652
hyunseo <ohseo8813@gmail.com>
2023-09-05 19:11:26 +0900

    refactor: 개행문자 상수화

---
+++ tomcat/src/main/java/nextstep/jwp/Constant.java
@@ -0,0 +1,9 @@
+package nextstep.jwp;
+
+public class Constant {
+
+    private Constant() {
+    }
+
+    public static final String CRLF = "\r\n";
+}
--- tomcat/src/main/java/nextstep/jwp/handler/HttpResponseParser.java
+++ tomcat/src/main/java/nextstep/jwp/handler/HttpResponseParser.java
@@ -1,5 +1,7 @@
 package nextstep.jwp.handler;
 
+import static nextstep.jwp.Constant.CRLF;
+
 import java.util.stream.Collectors;
 import org.apache.coyote.http11.HttpCookie;
 import org.apache.coyote.http11.HttpResponse;
@@ -12,24 +14,29 @@ public class HttpResponseParser {
 
     public static byte[] parse(HttpResponse response) {
         HttpStatus status = response.getHttpStatus();
-        String result = String.join("\r\n",
-            "HTTP/1.1 " + status.code + " " + status.name() + " ",
+        String result = String.join(CRLF,
+            parseStartLine(status),
             parseHeaders(response) + parseCookie(response.getCookie()),
             "",
             response.getBody());
         return result.getBytes();
     }
 
+    private static String parseStartLine(HttpStatus status) {
+        return String.format("HTTP/1.1 %s %s ", status.code, status.name());
+    }
+
     private static String parseHeaders(HttpResponse response) {
         return response.getHeaders().entrySet().stream()
             .map(it -> String.join(": ", it.getKey(), it.getValue() + " "))
-            .collect(Collectors.joining("\r\n"));
+            .collect(Collectors.joining(CRLF));
     }
 
     private static String parseCookie(HttpCookie cookie) {
         if (cookie.getCookies().size() == 0) {
             return "";
         }
+
         return "\nSet-Cookie: " + cookie.getCookies().entrySet().stream()
             .map(it -> String.join("=", it.getKey(), it.getValue() + " "))
             .collect(Collectors.joining());

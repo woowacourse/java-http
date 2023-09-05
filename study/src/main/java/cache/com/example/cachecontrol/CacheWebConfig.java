fbbd644623b98e568ca20cd11455b432ee33bec5
mcodnjs <codnjs8989@gmail.com>
2023-09-06 01:15:00 +0900

    test: Http 활용하기 테스트 구현

--- study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
+++ study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
@@ -1,13 +1,21 @@
 package cache.com.example.cachecontrol;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
 @Configuration
 public class CacheWebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        final WebContentInterceptor interceptor = new WebContentInterceptor();
+        final CacheControl cacheControl = CacheControl
+                .noCache()
+                .cachePrivate();
+        interceptor.addCacheMapping(cacheControl, "/**");
+        registry.addInterceptor(interceptor);
     }
 }

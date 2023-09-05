e6a808a5cb66ddad100023ca9f92df3b75a24e01
jjongwa <troas96@naver.com>
2023-09-06 02:52:02 +0900

    feat: Session 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,7 @@
 package org.apache.coyote.http11;
 
 import nextstep.jwp.db.InMemoryUserRepository;
+import nextstep.jwp.exception.LoginException;
 import nextstep.jwp.exception.UncheckedServletException;
 import nextstep.jwp.model.User;
 import org.apache.coyote.Processor;
@@ -23,6 +24,7 @@ public class Http11Processor implements Runnable, Processor {
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
 
     private final Socket connection;
+    private final SessionManager sessionManager = new SessionManager();
 
     public Http11Processor(final Socket connection) {
         this.connection = connection;
@@ -54,23 +56,11 @@ public class Http11Processor implements Runnable, Processor {
     private HttpResponse handleRequest(final HttpRequest httpRequest) throws IOException {
         final RequestPath requestPath = httpRequest.getRequestPath();
 
+        // 로그인
         if (requestPath.getResource().equals("/login")) {
-            if (httpRequest.getHttpMethod().equals(HttpMethod.GET)) {
-                if (requestPath.isParamEmpty()) {
-                    final URL url = getClass().getClassLoader()
-                            .getResource("static" + requestPath.getResource() + HttpExtensionType.HTML.getExtension());
-                    final Path path = new File(url.getPath()).toPath();
-                    final String content = new String(Files.readAllBytes(path));
-                    return HttpResponse.of(HttpStatusCode.OK, ResponseBody.of(HttpExtensionType.HTML.getExtension(), content));
-                }
-            }
-            if (httpRequest.getHttpMethod().equals(HttpMethod.POST)) {
-                if (executeLogin(httpRequest.getRequestBody())) {
-                    HttpCookie cookie = HttpCookie.from(httpRequest.getRequestHeaders().geHeaderValue("Cookie"));
-                    if (!cookie.contains("JSESSIONID")) {
-                        cookie.setCookie("JSESSIONID", UUID.randomUUID().toString());
-                    }
-
+            if (httpRequest.getHttpMethod().equals(HttpMethod.GET) && requestPath.isParamEmpty()) {
+                final HttpCookie cookie = HttpCookie.from(httpRequest.getRequestHeaders().geHeaderValue("Cookie"));
+                if (cookie.contains("JSESSIONID")) {
                     final URL url = getClass().getClassLoader()
                             .getResource("static" + "/index" + HttpExtensionType.HTML.getExtension());
                     final Path path = new File(url.getPath()).toPath();
@@ -78,15 +68,51 @@ public class Http11Processor implements Runnable, Processor {
                     final ResponseBody responseBody = ResponseBody.of(HttpExtensionType.HTML.getExtension(), content);
                     return HttpResponse.withCookie(HttpStatusCode.FOUND, responseBody, cookie);
                 }
+
                 final URL url = getClass().getClassLoader()
-                        .getResource("static" + "/401" + HttpExtensionType.HTML.getExtension());
+                        .getResource("static" + requestPath.getResource() + HttpExtensionType.HTML.getExtension());
                 final Path path = new File(url.getPath()).toPath();
                 final String content = new String(Files.readAllBytes(path));
-                final ResponseBody responseBody = ResponseBody.of(HttpExtensionType.HTML.getExtension(), content);
-                return HttpResponse.of(HttpStatusCode.UNAUTHORIZED, responseBody);
+                return HttpResponse.of(HttpStatusCode.OK, ResponseBody.of(HttpExtensionType.HTML.getExtension(), content));
+            }
+            if (httpRequest.getHttpMethod().equals(HttpMethod.POST)) {
+                try {
+                    final String userName = httpRequest.getRequestBody().get("account");
+                    final String password = httpRequest.getRequestBody().get("password");
+
+                    final User user = InMemoryUserRepository.findByAccount(userName)
+                            .orElseThrow(LoginException::new);
+                    if (user.checkPassword(password)) {
+                        log.info(user.toString());
+                        final HttpCookie cookie = HttpCookie.from(httpRequest.getRequestHeaders().geHeaderValue("Cookie"));
+                        if (!cookie.contains("JSESSIONID")) {
+                            cookie.setCookie("JSESSIONID", UUID.randomUUID().toString());
+                        }
+
+                        final Session session = new Session(cookie.getCookie("JSESSIONID"));
+                        session.setAttribute("user", user);
+                        sessionManager.add(session);
+
+                        final URL url = getClass().getClassLoader()
+                                .getResource("static" + "/index" + HttpExtensionType.HTML.getExtension());
+                        final Path path = new File(url.getPath()).toPath();
+                        final String content = new String(Files.readAllBytes(path));
+                        final ResponseBody responseBody = ResponseBody.of(HttpExtensionType.HTML.getExtension(), content);
+                        return HttpResponse.withCookie(HttpStatusCode.FOUND, responseBody, cookie);
+                    }
+                    throw new LoginException();
+                } catch (LoginException exception) {
+                    final URL url = getClass().getClassLoader()
+                            .getResource("static" + "/401" + HttpExtensionType.HTML.getExtension());
+                    final Path path = new File(url.getPath()).toPath();
+                    final String content = new String(Files.readAllBytes(path));
+                    final ResponseBody responseBody = ResponseBody.of(HttpExtensionType.HTML.getExtension(), content);
+                    return HttpResponse.of(HttpStatusCode.UNAUTHORIZED, responseBody);
+                }
             }
         }
 
+        // 회원가입 조회
         if (requestPath.getResource().equals("/register") && httpRequest.getHttpMethod().equals(HttpMethod.GET)) {
             final URL url = getClass().getClassLoader()
                     .getResource("static" + "/register" + HttpExtensionType.HTML.getExtension());
@@ -96,6 +122,7 @@ public class Http11Processor implements Runnable, Processor {
             return HttpResponse.of(HttpStatusCode.OK, responseBody);
         }
 
+        // 회원가입
         if (requestPath.getResource().equals("/register") && httpRequest.getHttpMethod().equals(HttpMethod.POST)) {
             final Map<String, String> requestBody = httpRequest.getRequestBody();
 
@@ -124,6 +151,7 @@ public class Http11Processor implements Runnable, Processor {
             return HttpResponse.of(HttpStatusCode.OK, responseBody);
         }
 
+        // 기본
         if (requestPath.getResource().equals("/")) {
             final ResponseBody responseBody = ResponseBody.of(HttpExtensionType.HTML.getExtension(), "Hello world!");
             return HttpResponse.of(HttpStatusCode.OK, responseBody);
@@ -139,24 +167,6 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException exception) {
             log.error(exception.getMessage());
             throw new UncheckedServletException("유효하지 않은 리소스에 대한 접근입니다.");
-        }
-    }
-
-    private boolean executeLogin(Map<String, String> params) {
-        try {
-            final String userName = params.get("account");
-            final String password = params.get("password");
-
-            final User user = InMemoryUserRepository.findByAccount(userName)
-                    .orElseThrow(() -> new UncheckedServletException("존재하지 않는 userName입니다."));
-
-            if (!user.checkPassword(password)) {
-                throw new UncheckedServletException("비밀번호가 일치하지 않습니다.");
-            }
-            log.info(user.toString());
-            return true;
-        } catch (UncheckedServletException exception) {
-            return false;
         }
     }
 }

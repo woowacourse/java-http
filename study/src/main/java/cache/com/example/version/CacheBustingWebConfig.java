f713a8d0f07a223a56d28453356f9ed26f074d71
iamjooon2 <iamjooon2@gmail.com>
2023-09-05 16:38:39 +0900

    study: 학습 테스트

--- study/src/main/java/cache/com/example/version/CacheBustingWebConfig.java
+++ study/src/main/java/cache/com/example/version/CacheBustingWebConfig.java
@@ -1,7 +1,11 @@
 package cache.com.example.version;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.reactive.resource.VersionResourceResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,6 +24,8 @@ public class CacheBustingWebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         registry.addResourceHandler(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/**")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS).cachePublic())
+                .setUseLastModified(true)
                 .addResourceLocations("classpath:/static/");
     }
 }

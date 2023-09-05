2867cc2faae1d25809c28e8a77c78729656b762a
yujamint <woawoa0513@gmail.com>
2023-09-05 14:55:24 +0900

    test: HTTP 캐시 학습 테스트 작성

--- study/src/main/java/cache/com/example/etag/EtagFilterConfiguration.java
+++ study/src/main/java/cache/com/example/etag/EtagFilterConfiguration.java
@@ -1,12 +1,25 @@
 package cache.com.example.etag;
 
+import cache.com.example.version.ResourceVersion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @Configuration
 public class EtagFilterConfiguration {
 
-//    @Bean
-//    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
-//        return null;
-//    }
+    @Autowired
+    private ResourceVersion resourceVersion;
+
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        ShallowEtagHeaderFilter shallowEtagHeaderFilter = new ShallowEtagHeaderFilter();
+        FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean
+                = new FilterRegistrationBean<>(shallowEtagHeaderFilter);
+        filterRegistrationBean.addUrlPatterns("/etag");
+        filterRegistrationBean.addUrlPatterns("/resources/*");
+        return filterRegistrationBean;
+    }
 }

92b35c93b3aead24463b419b4029baf6ef4ec7ce
yoondgu <doyou2ing@naver.com>
2023-09-05 16:30:06 +0900

    test: Cache 학습 테스트 작성

--- study/src/test/java/cache/com/example/GreetingControllerTest.java
+++ study/src/test/java/cache/com/example/GreetingControllerTest.java
@@ -1,6 +1,9 @@
 package cache.com.example;
 
+import static cache.com.example.version.CacheBustingWebConfig.PREFIX_STATIC_RESOURCES;
+
 import cache.com.example.version.ResourceVersion;
+import java.time.Duration;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,10 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.reactive.server.WebTestClient;
-
-import java.time.Duration;
-
-import static cache.com.example.version.CacheBustingWebConfig.PREFIX_STATIC_RESOURCES;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GreetingControllerTest {
@@ -35,6 +34,7 @@ class GreetingControllerTest {
                 .expectHeader().cacheControl(CacheControl.noCache().cachePrivate())
                 .expectBody(String.class).returnResult();
 
+        log.info("{}", response);
         log.info("response body\n{}", response.getResponseBody());
     }
 
@@ -51,6 +51,7 @@ class GreetingControllerTest {
                 .expectHeader().valueEquals(HttpHeaders.TRANSFER_ENCODING, "chunked")
                 .expectBody(String.class).returnResult();
 
+        log.info("{}", response);
         log.info("response body\n{}", response.getResponseBody());
     }
 
@@ -64,14 +65,13 @@ class GreetingControllerTest {
                 .expectHeader().exists(HttpHeaders.ETAG)
                 .expectBody(String.class).returnResult();
 
+        log.info("{}", response);
         log.info("response body\n{}", response.getResponseBody());
     }
 
     /**
-     * http://localhost:8080/resource-versioning
-     * 위 url의 html 파일에서 사용하는 js, css와 같은 정적 파일에 캐싱을 적용한다.
-     * 보통 정적 파일을 캐싱 무효화하기 위해 캐싱과 함께 버전을 적용시킨다.
-     * 정적 파일에 변경 사항이 생기면 배포할 때 버전을 바꿔주면 적용된 캐싱을 무효화(Caching Busting)할 수 있다.
+     * http://localhost:8080/resource-versioning 위 url의 html 파일에서 사용하는 js, css와 같은 정적 파일에 캐싱을 적용한다. 보통 정적 파일을 캐싱 무효화하기
+     * 위해 캐싱과 함께 버전을 적용시킨다. 정적 파일에 변경 사항이 생기면 배포할 때 버전을 바꿔주면 적용된 캐싱을 무효화(Caching Busting)할 수 있다.
      */
     @Test
     void testCacheBustingOfStaticResources() {

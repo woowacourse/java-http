1b3376438192d63355ca77f2d54cd84d83584363
wugawuga <dnr3655@naver.com>
2023-09-05 17:28:32 +0900

    test: cache 테스트 추가

--- study/src/main/java/cache/com/example/etag/EtagFilterConfiguration.java
+++ study/src/main/java/cache/com/example/etag/EtagFilterConfiguration.java
@@ -1,12 +1,23 @@
 package cache.com.example.etag;
 
+import static cache.com.example.version.CacheBustingWebConfig.PREFIX_STATIC_RESOURCES;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @Configuration
 public class EtagFilterConfiguration {
 
-//    @Bean
-//    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
-//        return null;
-//    }
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        final ShallowEtagHeaderFilter shallowEtagHeaderFilter = new ShallowEtagHeaderFilter();
+
+        final FilterRegistrationBean<ShallowEtagHeaderFilter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(shallowEtagHeaderFilter);
+        filterFilterRegistrationBean.addUrlPatterns("/etag");
+        filterFilterRegistrationBean.addUrlPatterns(PREFIX_STATIC_RESOURCES + "/*");
+        return filterFilterRegistrationBean;
+    }
 }

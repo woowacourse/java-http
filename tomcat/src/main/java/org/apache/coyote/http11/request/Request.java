ca69602e7899252ab62972c32e8f58219b647cfd
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-09-06 17:40:42 +0900

    feat: request 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/request/Request.java
@@ -0,0 +1,30 @@
+package org.apache.coyote.http11.request;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Request {
+
+    private final StartLine startLine;
+    private final Headers headers;
+
+    public Request(final StartLine startLine, final Headers headers) {
+        this.startLine = startLine;
+        this.headers = headers;
+    }
+
+    public static Request from(final BufferedReader bufferedReader) throws IOException {
+        final StartLine requestStartLine = StartLine.from(bufferedReader.readLine());
+        final Headers requestHeaders = Headers.from(bufferedReader.lines());
+
+        return new Request(requestStartLine, requestHeaders);
+    }
+
+    public StartLine getStartLine() {
+        return startLine;
+    }
+
+    public Headers getHeaders() {
+        return headers;
+    }
+}
---
+++ tomcat/src/test/java/org/apache/coyote/http11/request/RequestTest.java
@@ -0,0 +1,38 @@
+package org.apache.coyote.http11.request;
+
+import org.assertj.core.api.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+class RequestTest {
+
+    @Test
+    void from() throws IOException {
+        // given
+        final String request = "GET /login HTTP/1.1\n"
+                + "Host: localhost:8080\n"
+                + "Connection: keep-alive";
+
+        final BufferedReader bufferedReader = new BufferedReader(new StringReader(request));
+
+        // when
+        final Request actual = Request.from(bufferedReader);
+
+        // then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            final StartLine startLine = actual.getStartLine();
+            final Headers headers = actual.getHeaders();
+
+            softAssertions.assertThat(startLine.getMethod()).isEqualTo(HttpMethod.GET);
+            softAssertions.assertThat(startLine.getUri()).isEqualTo("/login");
+            softAssertions.assertThat(startLine.getVersion()).isEqualTo(HttpVersion.HTTP_1_1);
+            softAssertions.assertThat(headers.getHeaders().get(0).getName()).isEqualTo("Host");
+            softAssertions.assertThat(headers.getHeaders().get(0).getValue()).isEqualTo("localhost:8080");
+            softAssertions.assertThat(headers.getHeaders().get(1).getName()).isEqualTo("Connection");
+            softAssertions.assertThat(headers.getHeaders().get(1).getValue()).isEqualTo("keep-alive");
+        });
+    }
+}

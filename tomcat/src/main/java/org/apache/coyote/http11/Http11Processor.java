da124f1ad200476113063030a03f2d4ca348b01a
kwonyj1022 <kwonyj1022@naver.com>
2023-09-07 04:51:13 +0900

    feat: 서버에서 에러 발생 시 500.html을 보여주는 기능 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -2,9 +2,11 @@ package org.apache.coyote.http11;
 
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
+import org.apache.coyote.http11.common.HttpVersion;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.response.HttpResponse;
 import org.apache.coyote.http11.response.ResponseEntity;
+import org.apache.coyote.http11.response.statusLine.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,16 +42,23 @@ public class Http11Processor implements Runnable, Processor {
              final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
              final BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream))
         ) {
-
-            final HttpRequest httpRequest = HttpRequest.from(bufferedReader);
-            final ResponseEntity responseEntity = ControllerFinder.find(httpRequest);
-
-            final HttpResponse response = HttpResponse.of(httpRequest.getHttpVersion(), responseEntity);
+            final HttpResponse response = getHttpResponse(bufferedReader);
 
             bufferedWriter.write(response.convertToString());
             bufferedWriter.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        }
+    }
+
+    private static HttpResponse getHttpResponse(final BufferedReader bufferedReader) throws IOException {
+        try {
+            final HttpRequest httpRequest = HttpRequest.from(bufferedReader);
+            final ResponseEntity responseEntity = ControllerFinder.find(httpRequest);
+            return HttpResponse.of(httpRequest.getHttpVersion(), responseEntity);
+        } catch (RuntimeException e) {
+            final ResponseEntity responseEntity = ResponseEntity.of(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+            return HttpResponse.of(HttpVersion.HTTP_1_1, responseEntity);
         }
     }
 }

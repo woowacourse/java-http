8317d970560082609c619014333022f9c711fd50
apptie <appti@naver.com>
2023-09-06 14:03:34 +0900

    feat: DB에 이미 해당 아이디가 있는지 여부를 판단하는 기능 추가

--- tomcat/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
+++ tomcat/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
@@ -1,26 +1,32 @@
 package nextstep.jwp.db;
 
-import nextstep.jwp.model.User;
-
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import nextstep.jwp.model.User;
 
 public class InMemoryUserRepository {
 
     private static final Map<String, User> database = new ConcurrentHashMap<>();
+    private static final AtomicLong sequence = new AtomicLong(1);
+    private static final long DEFAULT_INCREASE_SIZE = 1L;
 
     static {
-        final User user = new User(1L, "gugu", "password", "hkkang@woowahan.com");
+        final User user = new User(sequence.getAndAdd(DEFAULT_INCREASE_SIZE), "gugu", "password", "hkkang@woowahan.com");
         database.put(user.getAccount(), user);
     }
 
-    public static void save(User user) {
+    public static void save(final User user) {
         database.put(user.getAccount(), user);
     }
 
-    public static Optional<User> findByAccount(String account) {
+    public static Optional<User> findByAccount(final String account) {
         return Optional.ofNullable(database.get(account));
+    }
+
+    public static boolean existsByAccount(final String account) {
+        return database.containsKey(account);
     }
 
     private InMemoryUserRepository() {}
---
+++ tomcat/src/test/java/nextstep/jwp/db/InMemoryUserRepositoryTest.java
@@ -0,0 +1,57 @@
+package nextstep.jwp.db;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.Optional;
+import nextstep.jwp.model.User;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class InMemoryUserRepositoryTest {
+
+    @Test
+    void save_메서드는_전달한_User를_저장한다() {
+        final User user = new User(5L, "asdf", "asdf", "asdf@asdf.com");
+
+        assertDoesNotThrow(() -> InMemoryUserRepository.save(user));
+    }
+
+    @Test
+    void findByAccount_메서드는_존재하는_account를_전달하면_해당_User를_Optional로_감싸_반환한다() {
+        final String existsAccount = "gugu";
+
+        final Optional<User> actual = InMemoryUserRepository.findByAccount(existsAccount);
+
+        assertThat(actual).isPresent();
+    }
+
+    @Test
+    void findByAccount_메서드는_존재하지_않는_account를_전달하면_빈_Optional을_반환한다() {
+        final String notExistsAccount = "notExistsAccount";
+
+        final Optional<User> actual = InMemoryUserRepository.findByAccount(notExistsAccount);
+
+        assertThat(actual).isEmpty();
+    }
+
+    @Test
+    void existsByAccount_메서드는_존재하는_account를_전달하면_true를_반환한다() {
+        final String existsAccount = "gugu";
+
+        final boolean actual = InMemoryUserRepository.existsByAccount(existsAccount);
+
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void existsByAccount_메서드는_존재하지_않는_account를_전달하면_false를_반환한다() {
+        final String notExistsAccount = "asdf";
+
+        final boolean actual = InMemoryUserRepository.existsByAccount(notExistsAccount);
+
+        assertThat(actual).isTrue();
+    }
+}

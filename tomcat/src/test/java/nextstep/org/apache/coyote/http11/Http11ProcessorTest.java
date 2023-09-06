b35980a07d02398608d5da781373b190603c8e74
splitCoding <voicesplit@daum.net>
2023-09-06 10:31:50 +0900

    test: 세션 존재시 로그인 처리 테스트 추가

--- tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -145,6 +145,39 @@ class Http11ProcessorTest {
     }
 
     @Test
+    void login_session_exist() throws IOException {
+        final User existUser = InMemoryUserRepository.findByAccount("gugu").get();
+        final Session userSession = InMemorySessionRepository.findByUser(existUser).get();
+
+        final String httpRequest = String.join(System.lineSeparator(),
+            "GET /login HTTP/1.1 ",
+            "Host: localhost:8080 ",
+            "Connection: keep-alive ",
+            "cookie: JSESSIONID=" + userSession.getUuid() + " ",
+            "",
+            ""
+        );
+
+        final var socket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        final URL resource = getClass().getClassLoader().getResource("static/index.html");
+        final byte[] responseBody = Files.readAllBytes(new File(resource.getFile()).toPath());
+
+        var expected = String.join(System.lineSeparator(),
+            "HTTP/1.1 200 OK ",
+            "Content-Type: text/html;charset=utf-8 ",
+            "Content-Length: " + responseBody.length + " ",
+            "",
+            new String(responseBody));
+
+        assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @Test
     void register_post() {
         // given
         final String formData = "account=split&email=split@daum.net&password=1234";

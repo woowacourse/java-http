223d2477f70a5f0ef28e187e21e30db329044cef
aak2075 <lks971003@gmail.com>
2023-09-07 08:52:05 +0900

    feat: thread 미션

--- study/src/test/java/thread/stage1/UserServlet.java
+++ study/src/test/java/thread/stage1/UserServlet.java
@@ -1,27 +1,32 @@
 package thread.stage1;
 
+import com.google.common.util.concurrent.Atomics;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class UserServlet {
 
-    private final List<User> users = new ArrayList<>();
+    private final AtomicReference<List<User>> users = new AtomicReference<>(new ArrayList<>());
 
     public void service(final User user) {
         join(user);
     }
 
     private void join(final User user) {
-        if (!users.contains(user)) {
-            users.add(user);
+        List<User> expected = users.get();
+        List<User> newValue = new ArrayList<>(expected);
+        newValue.add(new User("gugu2"));
+        if (!expected.contains(user)) {
+            users.compareAndSet(expected, newValue);
         }
     }
 
     public int size() {
-        return users.size();
+        return users.get().size();
     }
 
     public List<User> getUsers() {
-        return users;
+        return users.get();
     }
 }

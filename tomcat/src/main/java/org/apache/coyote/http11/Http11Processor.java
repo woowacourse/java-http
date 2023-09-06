ab8c3626f8a649e81a07ed386212079060d90299
Miseong Kim <miseongk@naver.com>
2023-09-06 21:09:49 +0900

    refactor: 가독성 개선

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -10,17 +10,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.Map;
-import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Http11Processor implements Runnable, Processor {
 
+    private static final String EXTENSION_DELIMITER = ".";
+
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final HttpRequestParser httpRequestParser = new HttpRequestParser();
+    private static final RequestHandler requestHandler = new RequestHandlerMapping();
+    private static final ViewResolver viewResolver = new ViewResolver();
 
     private final Socket connection;
-    private final RequestHandler requestHandler = new RequestHandlerMapping();
 
     public Http11Processor(final Socket connection) {
         this.connection = connection;
@@ -34,35 +37,40 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        try (final var bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
              final var outputStream = connection.getOutputStream()) {
-            final HttpRequestParser httpRequestParser = new HttpRequestParser();
-            final HttpRequest httpRequest = httpRequestParser.parse(inputStream);
-
-            final String requestUri = httpRequest.getRequestUri();
-
-            final String view;
+            final HttpRequest httpRequest = httpRequestParser.parse(bufferedReader);
             final HttpResponse httpResponse = new HttpResponse();
-            if (requestUri.contains(".")) {
-                view = requestUri;
-                httpResponse.setStatusCode(HttpStatusCode.OK);
-            } else {
-                final Controller controller = requestHandler.getHandler(requestUri);
-                view = controller.run(httpRequest, httpResponse);
-            }
 
-            final ViewResolver viewResolver = new ViewResolver();
-            final String responseBody = viewResolver.read(view);
+            final String viewName = getViewName(httpRequest, httpResponse);
+            final String responseBody = viewResolver.read(viewName);
+
             httpResponse.setHeader("Content-Type", httpRequest.getContentType());
             httpResponse.setHeader("Content-Length", String.valueOf(responseBody.getBytes().length));
             httpResponse.setResponseBody(responseBody);
+            final String response = makeResponseString(httpResponse);
 
-            final var responseString = makeResponseString(httpResponse);
-            outputStream.write(responseString.getBytes());
+            outputStream.write(response.getBytes());
             outputStream.flush();
-        } catch (final IOException | UncheckedServletException e) {
+        } catch (final IOException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String getViewName(final HttpRequest httpRequest, final HttpResponse httpResponse) {
+        final String requestUri = httpRequest.getRequestUri();
+
+        if (isFileRequest(requestUri)) {
+            httpResponse.setStatusCode(HttpStatusCode.OK);
+            return requestUri;
+        }
+        final Controller controller = requestHandler.getHandler(requestUri);
+
+        return controller.run(httpRequest, httpResponse);
+    }
+
+    private boolean isFileRequest(final String requestUri) {
+        return requestUri.contains(EXTENSION_DELIMITER);
     }
 
     private String makeResponseString(final HttpResponse httpResponse) {

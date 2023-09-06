68bd7d0fc3183313001f49a1abee63de4c3000e1
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-09-06 13:56:57 +0900

    refactor: 핸들러 어뎁터 쿼리 스트링 메서드 분리

--- tomcat/src/main/java/org/apache/handler/HandlerAdapter.java
+++ tomcat/src/main/java/org/apache/handler/HandlerAdapter.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class HandlerAdapter {
 
     private static final Map<String, RequestHandler> handlers = new HashMap<>();
+    private static final String QUERY_STRING = "?";
 
     static {
         handlers.put("/", new DefaultHandler());
@@ -17,11 +18,20 @@ public class HandlerAdapter {
     }
 
     public static RequestHandler findRequestHandler(String path) {
-        if (path.contains("?")) {
-            int index = path.indexOf("?");
-            path = path.substring(0, index);
+        if (isQueryString(path)) {
+            path = extractPathWithoutQuery(path);
         }
 
         return handlers.getOrDefault(path, new FileHandler());
+    }
+
+    private static boolean isQueryString(String path) {
+        return path.contains(QUERY_STRING);
+    }
+
+    private static String extractPathWithoutQuery(String path) {
+        int index = path.indexOf(QUERY_STRING);
+        path = path.substring(0, index);
+        return path;
     }
 }

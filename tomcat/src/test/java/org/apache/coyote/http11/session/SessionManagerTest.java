adfbc6611aa1bf8bd862af0c69a8a44e5f347268
jeonghoon <shb03323@naver.com>
2023-09-06 23:16:57 +0900

    test: 세션 저장소 조회 테스트

---
+++ tomcat/src/test/java/org/apache/coyote/http11/session/SessionManagerTest.java
@@ -0,0 +1,37 @@
+package org.apache.coyote.http11.session;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SuppressWarnings("NonAsciiCharacters")
+class SessionManagerTest {
+
+    @Test
+    void 세션이_저장소에_존재하면_조회에_성공한다() {
+        // given
+        final SessionManager sessionManager = SessionManager.getInstance();
+        final String id = "bebe";
+        final Session expected = new Session(id);
+        sessionManager.add(expected);
+
+        // when
+        final Session actual = sessionManager.findSession(id);
+
+        // then
+        assertThat(expected).isEqualTo(actual);
+    }
+
+    @Test
+    void 세션이_저장소에_존재하지_않으면_null을_반환한다() {
+        // given
+        final SessionManager sessionManager = SessionManager.getInstance();
+        final String notExistId = "ditoo";
+
+        // when
+        final Session actual = sessionManager.findSession(notExistId);
+
+        // then
+        assertThat(actual).isNull();
+    }
+}

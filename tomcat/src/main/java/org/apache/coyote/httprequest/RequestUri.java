3cae32084967aee8e5c0b2e36a01d0a132667d8f
jeonghoon <shb03323@naver.com>
2023-09-07 01:52:15 +0900

    refactor: Request uri 상수화

--- tomcat/src/main/java/org/apache/coyote/httprequest/RequestUri.java
+++ tomcat/src/main/java/org/apache/coyote/httprequest/RequestUri.java
@@ -2,6 +2,11 @@ package org.apache.coyote.httprequest;
 
 public class RequestUri {
 
+    private static final String QUERY_STRING_SEPARATOR = "?";
+    private static final int INVALID_INDEX = -1;
+    private static final int START_INDEX = 0;
+    private static final int NEXT_INDEX = 1;
+
     private final String path;
     private final QueryString queryString;
 
@@ -11,12 +16,12 @@ public class RequestUri {
     }
 
     public static RequestUri from(final String requestUri) {
-        final int index = requestUri.indexOf("?");
-        if (index == -1) {
+        final int queryStringSeparatorIndex = requestUri.indexOf(QUERY_STRING_SEPARATOR);
+        if (queryStringSeparatorIndex == INVALID_INDEX) {
             return new RequestUri(requestUri, QueryString.empty());
         }
-        final String path = requestUri.substring(0, index);
-        return new RequestUri(path, QueryString.from(requestUri.substring(index + 1)));
+        final String path = requestUri.substring(START_INDEX, queryStringSeparatorIndex);
+        return new RequestUri(path, QueryString.from(requestUri.substring(queryStringSeparatorIndex + NEXT_INDEX)));
     }
 
     public String getPath() {

da41b0dd7e001eb2b7dac9c9fa7f135c30531879
java-saeng <gusdn85554@naver.com>
2023-09-06 15:23:07 +0900

    refactor : path -> uri 로 변수 변경

--- tomcat/src/main/java/org/apache/coyote/handler/StaticFileHandler.java
+++ tomcat/src/main/java/org/apache/coyote/handler/StaticFileHandler.java
@@ -9,18 +9,18 @@ public class StaticFileHandler implements StaticHandler {
 
   @Override
   public boolean canHandle(final HttpRequest httpRequest) {
-    return StaticFileParser.isStaticFile(httpRequest.getPath());
+    return StaticFileParser.isStaticFile(httpRequest.getUri());
   }
 
   @Override
   public String handle(final HttpRequest httpRequest) throws IOException {
-    final String path = httpRequest.getPath();
+    final String uri = httpRequest.getUri();
 
-    final String responseBody = FileDetector.detect("static" + path);
+    final String responseBody = FileDetector.detect("static" + uri);
 
     return String.join("\r\n",
         "HTTP/1.1 200 OK ",
-        "Content-Type: " + StaticFileParser.parsingFileType(path) + ";charset=utf-8 ",
+        "Content-Type: " + StaticFileParser.parsingFileType(uri) + ";charset=utf-8 ",
         "Content-Length: " + responseBody.getBytes().length + " ",
         "",
         responseBody);

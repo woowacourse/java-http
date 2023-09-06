edcbb39bdb6f5906c5cbd3cdda58f82ddd2a5dfd
apptie <appti@naver.com>
2023-09-06 23:24:04 +0900

    fix: LoginHandler와 LoginPageHandler를 LoginController로 통합

---
+++ tomcat/src/main/java/mvc/controller/LoginController.java
@@ -0,0 +1,65 @@
+package mvc.controller;
+
+import nextstep.handler.exception.InvalidQueryParameterException;
+import nextstep.jwp.application.UserService;
+import nextstep.jwp.model.User;
+import org.apache.coyote.http.HttpCookie;
+import org.apache.coyote.http.HttpSession;
+import org.apache.coyote.http.request.HttpRequest;
+import servlet.response.HttpResponse;
+
+public class LoginController extends AbstractPathController {
+
+    public static final String ACCOUNT_KEY = "account";
+    private static final String PASSWORD_KEY = "password";
+
+    private final String path;
+    private final UserService userService;
+
+    public LoginController(final String path, final UserService userService) {
+        this.path = path;
+        this.userService = userService;
+    }
+
+    @Override
+    public boolean supports(final HttpRequest request) {
+        return request.matchesByPath(path);
+    }
+
+    @Override
+    protected void doPost(final HttpRequest request, final HttpResponse response) throws Exception {
+        final String account = request.getParameter(ACCOUNT_KEY);
+        final String password = request.getParameter(PASSWORD_KEY);
+
+        if (isInvalidQueryParameter(account) || isInvalidQueryParameter(password)) {
+            throw new InvalidQueryParameterException();
+        }
+
+        final User loginUser = userService.login(account, password);
+
+        final HttpSession session = request.getSession(true);
+        session.setAttribute(ACCOUNT_KEY, loginUser);
+        final HttpCookie cookie = HttpCookie.fromSessionId(session.getId());
+
+        response.addCookie(cookie);
+    }
+
+    private boolean isInvalidQueryParameter(final String targetParameter) {
+        return targetParameter == null || targetParameter.isEmpty() || targetParameter.isBlank();
+    }
+
+    @Override
+    protected void doGet(final HttpRequest request, final HttpResponse response) throws Exception {
+        final HttpSession session = request.getSession(false);
+
+        if (session != null) {
+            final Object user = session.getAttribute(LoginController.ACCOUNT_KEY);
+
+            if (user != null) {
+                response.sendRedirect("/index.html");
+                return;
+            }
+        }
+        response.sendRedirect("/login.html");
+    }
+}
--- tomcat/src/main/java/nextstep/Application.java
+++ tomcat/src/main/java/nextstep/Application.java
@@ -1,13 +1,11 @@
 package nextstep;
 
-import nextstep.jwp.application.UserService;
-import org.apache.catalina.startup.Tomcat;
-import org.apache.coyote.Container;
-import nextstep.handler.LoginHandler;
-import nextstep.handler.LoginPageHandler;
 import nextstep.handler.RegisterHandler;
 import nextstep.handler.RegisterPageHandler;
 import nextstep.handler.WelcomeHandler;
+import nextstep.jwp.application.UserService;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.coyote.Container;
 
 public class Application {
 
@@ -16,8 +14,6 @@ public class Application {
         final Container container = tomcat.addContainer("/");
 
         container.addHandler(new WelcomeHandler());
-        container.addHandler(new LoginHandler("/login", userService()));
-        container.addHandler(new LoginPageHandler("/login", "login.html"));
         container.addHandler(new RegisterPageHandler("/register", "register.html"));
         container.addHandler(new RegisterHandler("/register", userService()));
 

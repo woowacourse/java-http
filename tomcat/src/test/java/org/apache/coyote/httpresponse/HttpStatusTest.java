9af0a6b938dab129ed49b541d8fa7299942860e0
jeonghoon <shb03323@naver.com>
2023-09-07 22:15:29 +0900

    test: http status 응답 테스트

---
+++ tomcat/src/test/java/org/apache/coyote/httpresponse/HttpStatusTest.java
@@ -0,0 +1,22 @@
+package org.apache.coyote.httpresponse;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SuppressWarnings("NonAsciiCharacters")
+class HttpStatusTest {
+
+    @Test
+    void 올바른_형식으로_http_status_를_응답한다() {
+        // given
+        final HttpStatus httpStatus = HttpStatus.CREATED;
+
+        // when
+        final String actual = httpStatus.getHttpStatus();
+        final String expected = "201 Created";
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+}

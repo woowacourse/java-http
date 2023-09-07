7360a8d70cf2c108700ee8aa331595b0fccfc79b
JunDonghyuk <ehdgur4814@naver.com>
2023-09-07 17:05:20 +0900

    refactor: 테스트 코드 컨텐트 타입 수정

--- tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -25,7 +25,7 @@ class Http11ProcessorTest {
         // then
         var expected = String.join(System.lineSeparator(),
                 "HTTP/1.1 200 OK ",
-                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Type: text/html; charset=utf-8 ",
                 "Content-Length: 12 ",
                 "",
                 "Hello world!");

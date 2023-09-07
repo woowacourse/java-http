c0875e68aa0c2d0529638399a5015b94e0ae1c90
nuyh <hjaehyun25@gmail.com>
2023-09-07 16:46:59 +0900

    test: 캐싱 학습 테스트

--- study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
+++ study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
@@ -1,13 +1,26 @@
 package cache.com.example.cachecontrol;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
 @Configuration
 public class CacheWebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(cacheInterceptor()).addPathPatterns("/**");
+    }
+
+    @Bean
+    public HandlerInterceptor cacheInterceptor() {
+        final WebContentInterceptor webContentInterceptor = new WebContentInterceptor();
+        webContentInterceptor.setCacheControl(CacheControl.noCache().cachePrivate());
+
+        return webContentInterceptor;
     }
 }

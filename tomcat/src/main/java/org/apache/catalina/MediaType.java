32a7681eba93fcb0a05c8726c285c665e41c816e
mcodnjs <codnjs8989@gmail.com>
2023-09-08 00:18:35 +0900

    refactor: MediaType value 수정

--- tomcat/src/main/java/org/apache/catalina/MediaType.java
+++ tomcat/src/main/java/org/apache/catalina/MediaType.java
@@ -5,21 +5,10 @@ import java.util.Arrays;
 public enum MediaType {
 
     ALL("*", "*"),
-    APPLICATION_ATOM_XML("application", "atom+xml"),
-    APPLICATION_CBOR("application", "cbor"),
     APPLICATION_FORM_URLENCODED("application", "x-www-form-urlencoded"),
-    APPLICATION_GRAPHQL("application", "graphql+json"),
     APPLICATION_JSON("application", "json"),
-    APPLICATION_JSON_UTF8("application", "json"), // TODO: utf-8
-    APPLICATION_NDJSON("application", "x-ndjson"),
     APPLICATION_OCTET_STREAM("application", "octet-stream"),
     APPLICATION_PDF("application", "pdf"),
-    APPLICATION_PROBLEM_JSON("application", "problem+json"),
-    APPLICATION_PROBLEM_JSON_UTF8("application", "problem+json"), // TODO: utf-8
-    APPLICATION_PROBLEM_XML("application", "problem+xml"),
-    APPLICATION_RSS_XML("application", "rss+xml"),
-    APPLICATION_STREAM_JSON("application", "stream+json"),
-    APPLICATION_XHTML_XML("application", "xhtml+xml"),
     APPLICATION_XML("application", "xml"),
     IMAGE_GIF("image", "gif"),
     IMAGE_JPEG("image", "jpeg"),
@@ -27,7 +16,6 @@ public enum MediaType {
     MULTIPART_FORM_DATA("multipart", "form-data"),
     MULTIPART_MIXED("multipart", "mixed"),
     MULTIPART_RELATED("multipart", "related"),
-    TEXT_EVENT_STREAM("text", "event-stream"),
     TEXT_HTML("text", "html"),
     TEXT_MARKDOWN("text", "markdown"),
     TEXT_PLAIN("text", "plain"),

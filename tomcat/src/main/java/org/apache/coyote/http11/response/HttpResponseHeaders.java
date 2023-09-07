5fe8f46f6ae4df82c914a51dceb563fab12fd57a
gitchan <eunkeeeee@gmail.com>
2023-09-07 15:44:01 +0900

    refactor: HttpResponseHeaders 메서드 변경

--- tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponseHeaders.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponseHeaders.java
@@ -2,12 +2,8 @@ package org.apache.coyote.http11.response;
 
 import lombok.Getter;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-
-import static java.util.stream.Collectors.collectingAndThen;
-import static java.util.stream.Collectors.toMap;
 
 @Getter
 public class HttpResponseHeaders {
@@ -18,16 +14,11 @@ public class HttpResponseHeaders {
         this.headers = new HashMap<>(headers);
     }
 
-    private HttpResponseHeaders() {
+    public HttpResponseHeaders() {
         this(new HashMap<>());
     }
 
-    public static HttpResponseHeaders from(String headers) {
-        return Arrays.stream(headers.split("\r\n"))
-                .map(header -> header.split(": "))
-                .collect(collectingAndThen(
-                        toMap(header -> header[0], header -> header[1]),
-                        HttpResponseHeaders::new
-                ));
+    public void setHeader(String key, String value) {
+        headers.put(key, value);
     }
 }

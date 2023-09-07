91dceb5c3769c6e5b76e5f6be041ebb9da48d6e2
java-saeng <gusdn85554@naver.com>
2023-09-07 10:39:23 +0900

    refactor : handle 반환값 void 로 변경

--- tomcat/src/main/java/org/apache/coyote/handler/Handler.java
+++ tomcat/src/main/java/org/apache/coyote/handler/Handler.java
@@ -2,16 +2,17 @@ package org.apache.coyote.handler;
 
 import java.io.IOException;
 import org.apache.coyote.request.HttpRequest;
+import org.apache.coyote.response.HttpResponse;
 
 public interface Handler {
 
   boolean canHandle(final HttpRequest httpRequest);
 
-  String handle(final HttpRequest httpRequest) throws IOException;
+  void handle(final HttpRequest httpRequest, final HttpResponse httpResponse) throws IOException;
 
-  default String safeHandle(final HttpRequest httpRequest) {
+  default void safeHandle(final HttpRequest httpRequest, final HttpResponse httpResponse) {
     try {
-      return handle(httpRequest);
+      handle(httpRequest, httpResponse);
     } catch (IOException e) {
       throw new IllegalArgumentException("I/O 작업 관련 에러가 발생했습니다.");
     }

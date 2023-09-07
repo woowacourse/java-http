5479d54cccaaf449b12f38dc6cda93944415e695
junpakPark <junpak.park@gmail.com>
2023-09-07 16:33:32 +0900

    feat: User ID AutoIncrement 기능 구현

--- tomcat/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
+++ tomcat/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
@@ -1,22 +1,23 @@
 package nextstep.jwp.db;
 
-import nextstep.jwp.model.User;
-
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import nextstep.jwp.model.User;
 
 public class InMemoryUserRepository {
 
     private static final Map<String, User> database = new ConcurrentHashMap<>();
+    private static final AtomicLong idCounter = new AtomicLong();
 
     static {
-        final User user = new User(1L, "gugu", "password", "hkkang@woowahan.com");
+        final User user = new User("gugu", "password", "hkkang@woowahan.com").persist(idCounter.incrementAndGet());
         database.put(user.getAccount(), user);
     }
 
     public static void save(User user) {
-        database.put(user.getAccount(), user);
+        database.put(user.getAccount(), user.persist(idCounter.incrementAndGet()));
     }
 
     public static Optional<User> findByAccount(String account) {

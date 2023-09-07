df6a5781e0cab5a9a3a2b64c36216674c17f9a6f
amaran-th <songsy405@naver.com>
2023-09-07 16:11:43 +0900

    refactor: Http Cookie 생성자를 정적 팩토리 메서드로 변경

--- tomcat/src/main/java/org/apache/coyote/http11/HttpCookie.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpCookie.java
@@ -7,16 +7,20 @@ public class HttpCookie {
 
     private final Map<String, String> requestCookies;
 
-    public HttpCookie(String cookie) {
-        this.requestCookies = new HashMap<>();
-        if (cookie == null) {
-            return;
+    public static HttpCookie from(String cookie) {
+        Map<String, String> requestCookies = new HashMap<>();
+        if (cookie != null) {
+            String[] cookies = cookie.split("; ");
+            for (int i = 0; i < cookies.length; i++) {
+                String[] cookieTokens = cookies[i].split("=");
+                requestCookies.put(cookieTokens[0], cookieTokens[1]);
+            }
         }
-        String[] cookies = cookie.split("; ");
-        for (int i = 0; i < cookies.length; i++) {
-            String[] cookieTokens = cookies[i].split("=");
-            requestCookies.put(cookieTokens[0], cookieTokens[1]);
-        }
+        return new HttpCookie(requestCookies);
+    }
+
+    private HttpCookie(Map<String, String> requestCookies) {
+        this.requestCookies = requestCookies;
     }
 
     public boolean isExist(String key) {

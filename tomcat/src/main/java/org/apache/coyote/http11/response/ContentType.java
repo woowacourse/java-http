9da36873164bd4d3235c3526d91d94a9afba3f51
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-09-07 13:26:19 +0900

    fix: enum 이름이 아닌 값과 비교하는 문제 해결

--- tomcat/src/main/java/org/apache/coyote/http11/response/ContentType.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/ContentType.java
@@ -4,21 +4,23 @@ import java.util.Arrays;
 
 public enum ContentType {
 
-    HTML("text/html;charset=utf-8");
+    HTML("text/html"),
+    CSS("text/css"),
+    JS("application/x-javascript");
 
     public static final String SEPARATOR = "\\.";
     public static final int EXTENSION_INDEX = 1;
     private final String value;
 
     ContentType(final String value) {
-        this.value = value;
+        this.value = value + ";charset=utf-8";
     }
 
     public static ContentType findBy(final String content) {
         final String extension = content.split(SEPARATOR)[EXTENSION_INDEX].toUpperCase();
 
         return Arrays.stream(values())
-                     .filter(type -> type.value.equals(extension))
+                     .filter(type -> type.name().equals(extension))
                      .findFirst()
                      .orElse(HTML);
     }

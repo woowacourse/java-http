5e70de28fd05ab55863872c3fbd56739c9f52f47
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-09-07 22:36:45 +0900

    feat: HTTP 요청 메서드를 지원하지 않으면 405 페이지를 보여주도록 추가

--- tomcat/src/main/java/org/apache/handler/AbstractController.java
+++ tomcat/src/main/java/org/apache/handler/AbstractController.java
@@ -1,5 +1,6 @@
 package org.apache.handler;
 
+import java.io.IOException;
 import org.apache.common.FileReader;
 import org.apache.request.HttpRequest;
 import org.apache.response.ContentType;
@@ -22,13 +23,21 @@ public abstract class AbstractController implements Controller {
             return;
         }
 
+        resolveMethodNotAllowed(httpResponse);
+    }
+
+    protected void doPost(HttpRequest request, HttpResponse httpResponse) throws Exception {
+        resolveMethodNotAllowed(httpResponse);
+    }
+
+    protected void doGet(HttpRequest request, HttpResponse httpResponse) throws Exception {
+        resolveMethodNotAllowed(httpResponse);
+    }
+
+    private void resolveMethodNotAllowed(HttpResponse httpResponse) throws IOException {
         String content = FileReader.read(METHOD_NOT_ALLOWED_PAGE);
         httpResponse.setHttpStatus(HttpStatus.METHOD_NOT_ALLOWED);
         httpResponse.setContentType(ContentType.TEXT_HTML);
         httpResponse.setBody(content);
     }
-
-    protected void doPost(HttpRequest request, HttpResponse response) throws Exception {}
-
-    protected void doGet(HttpRequest request, HttpResponse response) throws Exception {}
 }

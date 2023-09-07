2a9dc62fd1fe7c97b5e74d42cc6f8ae047f9d651
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-09-07 22:55:35 +0900

    feat: USER ID를 sequence 하게 부여하도록 변경

--- tomcat/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
+++ tomcat/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
@@ -1,26 +1,28 @@
 package nextstep.jwp.db;
 
-import nextstep.jwp.model.User;
-
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import nextstep.jwp.model.User;
 
 public class InMemoryUserRepository {
 
-    private static final Map<String, User> database = new ConcurrentHashMap<>();
+    private static final Map<String, User> DATABASE = new ConcurrentHashMap<>();
+    private static final AtomicLong ID_GENERATOR = new AtomicLong(1);
 
     static {
-        final User user = new User(1L, "gugu", "password", "hkkang@woowahan.com");
-        database.put(user.getAccount(), user);
+        final User user = new User(ID_GENERATOR.getAndIncrement(), "gugu", "password", "hkkang@woowahan.com");
+        DATABASE.put(user.getAccount(), user);
     }
 
     public static void save(User user) {
-        database.put(user.getAccount(), user);
+        User saveUser = new User(ID_GENERATOR.getAndIncrement(), user.getAccount(), user.getPassword(), user.getEmail());
+        DATABASE.put(user.getAccount(), saveUser);
     }
 
     public static Optional<User> findByAccount(String account) {
-        return Optional.ofNullable(database.get(account));
+        return Optional.ofNullable(DATABASE.get(account));
     }
 
     private InMemoryUserRepository() {}

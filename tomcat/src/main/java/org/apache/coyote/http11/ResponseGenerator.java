633988d099b3eb13db5479f91e6b79410e0c7bac
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-09-07 13:24:16 +0900

    feat: 파일 요청에 대한 응답 추가

--- tomcat/src/main/java/org/apache/coyote/http11/ResponseGenerator.java
+++ tomcat/src/main/java/org/apache/coyote/http11/ResponseGenerator.java
@@ -6,18 +6,58 @@ import org.apache.coyote.http11.response.Response;
 import org.apache.coyote.http11.response.StartLine;
 import org.apache.coyote.http11.response.StatusCode;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Objects;
+
 public class ResponseGenerator {
+
+    public static final String ROOT_URI = "/";
+    private static final String STATIC_PATH = "static";
 
     private ResponseGenerator() {
     }
 
     public static String generate(final Request request) {
+        try {
+            if (ROOT_URI.equals(request.getUri())) {
+                final Response response = getDefaultResponse();
+                return response.toMessage();
+            }
+
+            final Response response = getFileResponse(request);
+            return response.toMessage();
+        } catch (final Exception e) {
+            return null;
+        }
+    }
+
+    private static Response getDefaultResponse() {
         final StartLine startLine = new StartLine(HttpVersion.HTTP_1_1, StatusCode.OK);
         final ContentType contentType = ContentType.HTML;
         final String responseBody = "Hello world!";
 
-        final Response response = Response.of(startLine, contentType, responseBody);
+        return Response.of(startLine, contentType, responseBody);
+    }
 
-        return response.toMessage();
+    private static Response getFileResponse(final Request request) throws IOException {
+        final StartLine startLine = new StartLine(HttpVersion.HTTP_1_1, StatusCode.OK);
+        final ContentType contentType = ContentType.findBy(request.getUri());
+        final String responseBody = getFileToResponseBody(request.getUri());
+
+        return Response.of(startLine, contentType, responseBody);
+    }
+
+    private static String getFileToResponseBody(final String fileName) throws IOException {
+        final String path = STATIC_PATH + fileName;
+        final URL resource = ClassLoader.getSystemClassLoader().getResource(path);
+        final String filePath = Objects.requireNonNull(resource).getPath();
+        final File file = new File(URLDecoder.decode(filePath, StandardCharsets.UTF_8));
+
+        return new String(Files.readAllBytes(file.toPath()));
     }
 }

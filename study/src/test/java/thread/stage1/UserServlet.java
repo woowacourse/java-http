d5180cf3bc00edf95a5a4ab2b88cd1959517d967
sh111-coder <ohk9134@naver.com>
2023-09-07 14:55:59 +0900

    test: 스레드 실습 진행

--- study/src/test/java/thread/stage1/UserServlet.java
+++ study/src/test/java/thread/stage1/UserServlet.java
@@ -2,13 +2,18 @@ package thread.stage1;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UserServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(UserServlet.class);
 
     private final List<User> users = new ArrayList<>();
 
     public void service(final User user) {
         join(user);
+        log.info("Join User : {}", user);
     }
 
     private void join(final User user) {

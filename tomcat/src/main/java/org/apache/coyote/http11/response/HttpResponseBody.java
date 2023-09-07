ad982e8fe4c58a2246a90b7899c2bf746d6f5337
gitchan <eunkeeeee@gmail.com>
2023-09-07 16:38:46 +0900

    refactor: HttpResponseBody 필드 변경

--- tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponseBody.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponseBody.java
@@ -1,40 +1,15 @@
 package org.apache.coyote.http11.response;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-import static java.util.stream.Collectors.collectingAndThen;
-import static java.util.stream.Collectors.toMap;
-
+@Getter
+@RequiredArgsConstructor
 public class HttpResponseBody {
 
-    private Map<String, String> body = new HashMap<>();
-
-    public HttpResponseBody(Map<String, String> body) {
-        this.body = new HashMap<>(body);
-    }
-
-    private HttpResponseBody() {
-    }
+    private final String body;
 
     public static HttpResponseBody from(String body) {
-        if (body.isEmpty()) {
-            return HttpResponseBody.none();
-        }
-        return Arrays.stream(body.split("&"))
-                .map(field -> field.split("="))
-                .collect(collectingAndThen(
-                        toMap(field -> field[0], field -> field[1]),
-                        HttpResponseBody::new
-                ));
-    }
-
-    private static HttpResponseBody none() {
-        return new HttpResponseBody();
-    }
-
-    public String find(String key) {
-        return body.get(key);
+        return new HttpResponseBody(body);
     }
 }

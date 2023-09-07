df1cafd0fcaba42fdb69d9db547be421508fe945
jeonghoon <shb03323@naver.com>
2023-09-07 22:12:39 +0900

    test: 쿠키 응답 헤더에 session id 삽입 테스트

---
+++ tomcat/src/test/java/org/apache/coyote/httpresponse/CookieResponseHeaderTest.java
@@ -0,0 +1,23 @@
+package org.apache.coyote.httpresponse;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SuppressWarnings("NonAsciiCharacters")
+class CookieResponseHeaderTest {
+
+    @Test
+    void jSessionId로_쿠키_값을_세팅한다() {
+        // given
+        final String jSessionId = "bebe";
+
+        // when
+        final CookieResponseHeader cookieResponseHeader = CookieResponseHeader.createByJSessionId(jSessionId);
+        final String actual = cookieResponseHeader.getFormattedValue();
+        final String expected = "Set-Cookie: JSESSIONID=bebe";
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+}

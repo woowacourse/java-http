1c0c0ae6eb091c1de3e00d33a7ff32050262ed5e
jeonghoon <shb03323@naver.com>
2023-09-07 21:50:27 +0900

    test: Location 헤더 응답 테스트

---
+++ tomcat/src/test/java/org/apache/coyote/httpresponse/header/LocationHeaderTest.java
@@ -0,0 +1,23 @@
+package org.apache.coyote.httpresponse.header;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SuppressWarnings("NonAsciiCharacters")
+class LocationHeaderTest {
+
+    @Test
+    void 올바른_형식의_헤더로_응답한다() {
+        // given
+        final String path = "/index.html";
+        final LocationHeader locationHeader = new LocationHeader(path);
+
+        // when
+        final String actual = locationHeader.getKeyAndValue(ResponseHeaderType.LOCATION);
+        final String expected = String.format("Location: %s", path);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+}

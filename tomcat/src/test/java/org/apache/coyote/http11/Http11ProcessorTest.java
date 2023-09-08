a68693e21e99cabc534b17eb117d609f7f61654b
Rosie <dev.kayoung@gmail.com>
2023-09-08 14:44:03 +0900

    test: RegisterController 테스트

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -1,15 +1,9 @@
 package org.apache.coyote.http11;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import support.StubSocket;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 class Http11ProcessorTest {
 
@@ -32,59 +26,5 @@ class Http11ProcessorTest {
 
         assertThat(socket.output()).isEqualTo(expected);
     }
-
-    @Nested
-    class Register {
-
-        @Test
-        void get() throws IOException {
-            //given
-            final String httpRequest = String.join("\r\n",
-                    "GET /register HTTP/1.1 ",
-                    "Host: localhost:8080 ",
-                    "Connection: keep-alive ",
-                    "",
-                    "");
-            final var socket = new StubSocket(httpRequest);
-            final Http11Processor processor = new Http11Processor(socket);
-
-            final URL resource = getClass().getClassLoader().getResource("static/register.html");
-            var expected = "HTTP/1.1 200 OK \r\n" +
-                    "Content-Type: text/html;charset=utf-8 \r\n" +
-                    "Content-Length: 4319 \r\n" +
-                    "\r\n" +
-                    new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
-            //when
-            processor.process(socket);
-
-            //then
-            assertThat(socket.output()).isEqualTo(expected);
-        }
-
-        @Test
-        void post() {
-            //given
-            final String httpRequest = String.join("\r\n",
-                    "POST /register HTTP/1.1 ",
-                    "Host: localhost:8080 ",
-                    "Connection: keep-alive ",
-                    "Content-Length: 30",
-                    "Content-Type: application/x-www-form-urlencoded ",
-                    "",
-                    "account=rosie&email=rosie@zipgo.pet&password=password");
-            final var socket = new StubSocket(httpRequest);
-            final Http11Processor processor = new Http11Processor(socket);
-
-            //when
-            processor.process(socket);
-
-            //then
-            final var expected = "HTTP/1.1 302 Found \r\n" +
-                    "Location: /index.html \r\n";
-            assertThat(socket.output()).isEqualTo(expected);
-        }
-
-    }
-
 
 }

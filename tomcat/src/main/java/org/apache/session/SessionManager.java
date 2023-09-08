cbf6eaf416f357b3cc282840f3982ba7cb7f4840
SproutMj <mjk54565@gmail.com>
2023-09-08 13:45:53 +0900

    feat: Executors로 Thread Pool 적용, session에 동시성 컬렉션 사용

--- tomcat/src/main/java/org/apache/session/SessionManager.java
+++ tomcat/src/main/java/org/apache/session/SessionManager.java
@@ -1,11 +1,11 @@
 package org.apache.session;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class SessionManager {
 
-    private static final Map<String, Session> SESSIONS = new HashMap<>();
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
 
     public void add(final Session session) {
         SESSIONS.put(session.getId(), session);

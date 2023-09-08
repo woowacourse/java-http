8b54c3d5e736e1d1f2eed3c3839ff2f2d8da51ff
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-09-08 17:32:30 +0900

    refactor: doPost, doGet 추상 메서드로 변경, 405응답 추가

--- tomcat/src/main/java/nextstep/jwp/controller/HomeController.java
+++ tomcat/src/main/java/nextstep/jwp/controller/HomeController.java
@@ -1,5 +1,8 @@
 package nextstep.jwp.controller;
 
+import java.util.List;
+import org.apache.common.FileReader;
+import org.apache.common.HttpMethod;
 import org.apache.handler.AbstractController;
 import org.apache.request.HttpRequest;
 import org.apache.response.ContentType;
@@ -9,11 +12,22 @@ import org.apache.response.HttpStatus;
 public class HomeController extends AbstractController {
 
     private static final String DEFAULT_RESPONSE = "Hello world!";
+    private static final String METHOD_NOT_ALLOWED_PAGE = "/405.html";
+    private static final List<HttpMethod> ALLOW_METHOD = List.of(HttpMethod.GET);
 
     @Override
     protected void doGet(HttpRequest httpRequest, HttpResponse httpResponse) {
         httpResponse.setHttpStatus(HttpStatus.OK);
         httpResponse.setContentType(ContentType.TEXT_HTML);
         httpResponse.setBody(DEFAULT_RESPONSE);
+    }
+
+    @Override
+    protected void doPost(HttpRequest request, HttpResponse httpResponse) throws Exception {
+        String content = FileReader.read(METHOD_NOT_ALLOWED_PAGE);
+        httpResponse.setHttpStatus(HttpStatus.METHOD_NOT_ALLOWED);
+        httpResponse.setAllow(ALLOW_METHOD);
+        httpResponse.setContentType(ContentType.TEXT_HTML);
+        httpResponse.setBody(content);
     }
 }
--- tomcat/src/main/java/org/apache/handler/AbstractController.java
+++ tomcat/src/main/java/org/apache/handler/AbstractController.java
@@ -1,7 +1,9 @@
 package org.apache.handler;
 
 import java.io.IOException;
+import java.util.List;
 import org.apache.common.FileReader;
+import org.apache.common.HttpMethod;
 import org.apache.request.HttpRequest;
 import org.apache.response.ContentType;
 import org.apache.response.HttpResponse;
@@ -10,6 +12,7 @@ import org.apache.response.HttpStatus;
 public abstract class AbstractController implements Controller {
 
     private static final String METHOD_NOT_ALLOWED_PAGE = "/405.html";
+    private static final List<HttpMethod> ALLOW_METHOD = List.of(HttpMethod.GET, HttpMethod.POST);
 
     @Override
     public void service(HttpRequest httpRequest, HttpResponse httpResponse) throws Exception {
@@ -26,17 +29,14 @@ public abstract class AbstractController implements Controller {
         resolveMethodNotAllowed(httpResponse);
     }
 
-    protected void doPost(HttpRequest request, HttpResponse httpResponse) throws Exception {
-        resolveMethodNotAllowed(httpResponse);
-    }
+    protected abstract void doPost(HttpRequest request, HttpResponse httpResponse) throws Exception;
 
-    protected void doGet(HttpRequest request, HttpResponse httpResponse) throws Exception {
-        resolveMethodNotAllowed(httpResponse);
-    }
+    protected abstract void doGet(HttpRequest request, HttpResponse httpResponse) throws Exception;
 
     private void resolveMethodNotAllowed(HttpResponse httpResponse) throws IOException {
         String content = FileReader.read(METHOD_NOT_ALLOWED_PAGE);
         httpResponse.setHttpStatus(HttpStatus.METHOD_NOT_ALLOWED);
+        httpResponse.setAllow(ALLOW_METHOD);
         httpResponse.setContentType(ContentType.TEXT_HTML);
         httpResponse.setBody(content);
     }

99d31f1379e6d8d681b8e26d488d8efac4839b7a
HubCreator <chocomilk8604@gmail.com>
2023-09-08 16:04:36 +0900

    test: UnAuthorizedControllerTest 테스트 케이스 작성

---
+++ tomcat/src/test/java/org/apache/coyote/http11/controller/UnAuthorizedControllerTest.java
@@ -0,0 +1,76 @@
+package org.apache.coyote.http11.controller;
+
+import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.response.HttpResponse;
+import org.apache.coyote.http11.response.StaticResource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class UnAuthorizedControllerTest {
+
+    private final Controller unAuthorizedController = new UnAuthorizedController();
+    private HttpRequest request;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        // given
+        final String rawRequest = String.join("\r\n",
+                "GET /401.html HTTP/1.1 ",
+                "Content-Type: text/html",
+                "",
+                "");
+        final StringReader stringReader = new StringReader(rawRequest);
+        request = HttpRequest.from(new BufferedReader(stringReader));
+    }
+
+    @Test
+    @DisplayName("Reqeust를 해당 컨트롤러가 처리할 수 있다.")
+    void handleTest() {
+
+        //when, then
+        assertThat(unAuthorizedController.canHandle(request)).isTrue();
+    }
+
+    @Test
+    @DisplayName("지원하지 않는 HttpMethod는 해당 컨트롤러가 처리할 수 없다.")
+    void handleExceptionTest() throws IOException {
+        // given
+        final String rawRequest = String.join("\r\n",
+                "POST /401.html HTTP/1.1 ",
+                "Content-Type: text/html",
+                "",
+                "");
+        final StringReader stringReader = new StringReader(rawRequest);
+        request = HttpRequest.from(new BufferedReader(stringReader));
+
+        //when, then
+        assertThatThrownBy(() -> unAuthorizedController.service(request))
+                .isInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    @DisplayName("401.html을 응답할 수 있다.")
+    void requestTest() throws Exception {
+        //when
+        final HttpResponse response = unAuthorizedController.service(request);
+
+        //then
+        final StaticResource staticResource = StaticResource.from("/401.html");
+        final String content = staticResource.getContent();
+        String expected = String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + content.getBytes().length + " ",
+                "",
+                staticResource.getContent());
+        assertThat(expected).isEqualTo(response.toString());
+    }
+}

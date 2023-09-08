d5f60bcc224fe1053de8f6b387082659dd216f05
HyeonjaeKwon <kwonhyeonjae01@gmail.com>
2023-09-08 14:08:36 +0900

    feat: 핸들러에 걸리지 않을 경우 "Hello world!" 반환

--- tomcat/src/main/java/org/apache/coyote/http11/handler/RequestHandler.java
+++ tomcat/src/main/java/org/apache/coyote/http11/handler/RequestHandler.java
@@ -6,8 +6,11 @@ import org.apache.coyote.http11.handler.mapper.HandlerMapper;
 import org.apache.coyote.http11.handler.mapper.LoginHandlerMapper;
 import org.apache.coyote.http11.handler.mapper.RegisterHandlerMapper;
 import org.apache.coyote.http11.handler.mapper.StaticFileHandlerMapper;
+import org.apache.coyote.http11.header.HttpHeader;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.response.HttpResponse;
+import org.apache.coyote.http11.responseline.HttpStatus;
+import org.apache.coyote.http11.responseline.ResponseLine;
 
 public class RequestHandler {
 
@@ -19,12 +22,20 @@ public class RequestHandler {
     mappers.add(new StaticFileHandlerMapper());
   }
 
+  private static HttpResponse responseHelloWorld() {
+    final ResponseLine responseLine = new ResponseLine(HttpStatus.OK);
+    final HttpHeader header = new HttpHeader();
+    header.setHeader("Content-Length", "12");
+    header.setHeader("Content-Type", "text/html;charset=utf-8");
+    return new HttpResponse(responseLine, header, "Hello world!");
+  }
+
   public HttpResponse handle(final HttpRequest request) throws Exception {
     for (final HandlerMapper mapper : mappers) {
       if (mapper.isSupport(request)) {
         return mapper.handle(request);
       }
     }
-    return null;
+    return responseHelloWorld();
   }
 }

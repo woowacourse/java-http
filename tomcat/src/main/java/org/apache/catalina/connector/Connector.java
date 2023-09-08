95f43f26513ded82e5d8dd169d6b3e936fe4de07
sosow0212 <sosow0212@naver.com>
2023-09-08 15:17:38 +0900

    refactor: 최대 쓰레드 풀 변경

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -17,7 +17,7 @@ public class Connector implements Runnable {
 
     private static final int DEFAULT_PORT = 8080;
     private static final int DEFAULT_ACCEPT_COUNT = 100;
-    private static final int MAX_THREADS = 200;
+    private static final int MAX_THREADS = 250;
 
     private final ServerSocket serverSocket;
     private final ExecutorService executorService;

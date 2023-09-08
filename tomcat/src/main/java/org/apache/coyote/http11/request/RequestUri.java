e81290534fc814ab112a6f0de7c736e8a854b4a9
kiarakim <kiarakim413@gmail.com>
2023-09-09 03:05:07 +0900

    fix: body가 hello world인 응답을 주도록 조건 부여

--- tomcat/src/main/java/org/apache/coyote/http11/request/RequestUri.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/RequestUri.java
@@ -7,18 +7,23 @@ public class RequestUri {
 
     private static final String PERIOD = ".";
     private static final String HTML_EXTENSION = ".html";
+    private static final String MAIN_PAGE = "/";
+    private static final String AMPERSAND = "&";
+    private static final String EQUAL_SIGN = "=";
+    private static final String QUERY_SEPARATOR = "\\?";
+    private static final int QUERY_EXISTENCE_SIZE = 1;
 
     private final String uri;
     private final HashMap<String, String> queries = new HashMap<>();
 
     public RequestUri(String requestUri) {
-        String[] uriSplit = requestUri.split("\\?");
+        String[] uriSplit = requestUri.split(QUERY_SEPARATOR);
         uri = resolveExtension(uriSplit[0]);
 
-        if (uriSplit.length > 1) {
-            String[] queriesSplit = uriSplit[1].split("&");
+        if (uriSplit.length > QUERY_EXISTENCE_SIZE) {
+            String[] queriesSplit = uriSplit[1].split(AMPERSAND);
             for (String query : queriesSplit) {
-                String[] keyValue = query.split("=");
+                String[] keyValue = query.split(EQUAL_SIGN);
                 queries.put(keyValue[0], keyValue[1]);
             }
         }
@@ -33,9 +38,10 @@ public class RequestUri {
     }
 
     private String resolveExtension(String uri) {
-        if (uri.contains(PERIOD)) {
+        if (uri.contains(PERIOD) || uri.equals(MAIN_PAGE)) {
             return uri;
         }
+
         return uri + HTML_EXTENSION;
     }
 }

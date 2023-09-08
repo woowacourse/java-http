c09714c125425467eefce939d3b8fbbb6427855f
junpakPark <junpak.park@gmail.com>
2023-09-08 19:39:03 +0900

    refactor: statusCode 타입 변경

--- tomcat/src/main/java/org/apache/coyote/http11/common/HttpStatus.java
+++ tomcat/src/main/java/org/apache/coyote/http11/common/HttpStatus.java
@@ -2,20 +2,20 @@ package org.apache.coyote.http11.common;
 
 public enum HttpStatus {
 
-    OK(200),
-    FOUND(302),
-    UNAUTHORIZED(401),
-    NOT_FOUND(404),
-    INTERNAL_SERVER_ERROR(500);
+    OK("200"),
+    FOUND("302"),
+    UNAUTHORIZED("401"),
+    NOT_FOUND("404"),
+    INTERNAL_SERVER_ERROR("500");
 
-    private final int statusCode;
+    private final String statusCode;
 
-    HttpStatus(int statusCode) {
+    HttpStatus(String statusCode) {
         this.statusCode = statusCode;
     }
 
     public String getHttpStatus() {
-        return String.format("%d %s", statusCode, name());
+        return String.join(" ", statusCode, name());
     }
 
 }

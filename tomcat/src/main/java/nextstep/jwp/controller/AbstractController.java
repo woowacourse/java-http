1ecb2f680151c0a8da7411a54c0f203512e47aee
70825 <hello70825@gmail.com>
2023-09-08 13:42:36 +0900

    feat: AbstractController 추가

---
+++ tomcat/src/main/java/nextstep/jwp/controller/AbstractController.java
@@ -0,0 +1,28 @@
+package nextstep.jwp.controller;
+
+import java.io.IOException;
+import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.response.HttpResponse;
+
+public abstract class AbstractController implements Controller {
+
+    private static final String POST = "POST";
+
+    @Override
+    public HttpResponse process(final HttpRequest request) throws IOException {
+        final String requestMethod = request.getMethod();
+
+        if (POST.equals(requestMethod)) {
+            doPost(request);
+        }
+        return doGet(request);
+    }
+
+    protected HttpResponse doPost(final HttpRequest request) {
+        throw new IllegalArgumentException("지원하지 않는 메서드입니다.");
+    }
+
+    protected HttpResponse doGet(final HttpRequest request) {
+        throw new IllegalArgumentException("지원하지 않는 메서드입니다.");
+    }
+}

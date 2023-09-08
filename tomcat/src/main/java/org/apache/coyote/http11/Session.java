d4326ef931ae6bae0ffd8d2315d0552a8587d522
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2023-09-08 21:46:01 +0900

    refactor: 동시성을 위해 ConcurrentHashMap 으로 변경

--- tomcat/src/main/java/org/apache/coyote/http11/Session.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Session.java
@@ -3,11 +3,12 @@ package org.apache.coyote.http11;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Session {
 
     private final String id;
-    private final Map<String, Object> values = new HashMap<>();
+    private final Map<String, Object> values = new ConcurrentHashMap<>();
 
     public Session() {
         this.id = UUID.randomUUID().toString();

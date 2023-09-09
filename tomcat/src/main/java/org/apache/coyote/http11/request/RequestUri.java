77de4f188df7d668edfe97d5c6583cfb94b945d9
kiarakim <kiarakim413@gmail.com>
2023-09-09 16:07:57 +0900

    refactor: 요청line 파싱 코드 제거

--- tomcat/src/main/java/org/apache/coyote/http11/request/RequestUri.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/RequestUri.java
@@ -1,40 +1,19 @@
 package org.apache.coyote.http11.request;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class RequestUri {
 
     private static final String PERIOD = ".";
     private static final String HTML_EXTENSION = ".html";
     private static final String MAIN_PAGE = "/";
-    private static final String AMPERSAND = "&";
-    private static final String EQUAL_SIGN = "=";
-    private static final String QUERY_SEPARATOR = "\\?";
-    private static final int QUERY_EXISTENCE_SIZE = 1;
 
     private final String uri;
-    private final HashMap<String, String> queries = new HashMap<>();
 
     public RequestUri(String requestUri) {
-        String[] uriSplit = requestUri.split(QUERY_SEPARATOR);
-        uri = resolveExtension(uriSplit[0]);
-
-        if (uriSplit.length > QUERY_EXISTENCE_SIZE) {
-            String[] queriesSplit = uriSplit[1].split(AMPERSAND);
-            for (String query : queriesSplit) {
-                String[] keyValue = query.split(EQUAL_SIGN);
-                queries.put(keyValue[0], keyValue[1]);
-            }
-        }
+        uri = resolveExtension(requestUri);
     }
 
     public String getUri() {
         return uri;
-    }
-
-    public Map<String, String> getQueries() {
-        return new HashMap<>(queries);
     }
 
     private String resolveExtension(String uri) {
--- tomcat/src/test/java/org/apache/coyote/http11/request/RequestUriTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/request/RequestUriTest.java
@@ -4,10 +4,7 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
-import java.util.Map;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
@@ -35,33 +32,5 @@ class RequestUriTest {
 
         // then
         assertThat("/").isEqualTo(requestUri.getUri());
-    }
-
-    @Test
-    void 쿼리_추출_테스트(){
-        // given
-        String uriString = "/index.html?userId=1&password=1234";
-
-        // when
-        RequestUri requestUri = new RequestUri(uriString);
-        Map<String, String> queries = requestUri.getQueries();
-
-        // then
-        assertSoftly(softly -> {
-                softly.assertThat(queries.get("userId")).isEqualTo("1");
-                softly.assertThat(queries.get("password")).isEqualTo("1234");
-        });
-    }
-
-    @Test
-    void 쿼리_존재시_Uri_추출_테스트(){
-        // given
-        String uriString = "/index.html?userId=1&password=1234";
-
-        // when
-        RequestUri requestUri = new RequestUri(uriString);
-
-        // then
-        assertThat("/index.html").isEqualTo(requestUri.getUri());
     }
 }

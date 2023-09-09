49c7bc1b048e78d50d8e08f8fb8622586dec81a8
yujamint <woawoa0513@gmail.com>
2023-09-09 21:01:30 +0900

    refactor: HeaderValue 클래스 구현
    ";" 뒤에 옵션이 붙는 것을 고려하여 이를 처리하는 역할을 담당한다.

--- tomcat/src/main/java/org/apache/coyote/http11/request/HttpHeaders.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/HttpHeaders.java
@@ -1,42 +1,40 @@
 package org.apache.coyote.http11.request;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class HttpHeaders {
 
     private static final char KEY_VALUE_DELIMITER = ':';
-    private static final String MULTIPLE_VALUE_DELIMITER = "[,;]";
 
-    private final Map<String, List<String>> headers;
+    private final Map<String, HeaderValue> headers;
 
-    private HttpHeaders(Map<String, List<String>> headers) {
+    private HttpHeaders(Map<String, HeaderValue> headers) {
         this.headers = headers;
     }
 
     public static HttpHeaders from(List<String> headers) {
-        Map<String, List<String>> headerValueByKey = new HashMap<>();
+        Map<String, HeaderValue> headerValueByKey = new HashMap<>();
         for (String header : headers) {
             int keyValueDelimiterIndex = header.indexOf(KEY_VALUE_DELIMITER);
             String key = header.substring(0, keyValueDelimiterIndex);
             String value = header.substring(keyValueDelimiterIndex + 1).trim();
-            headerValueByKey.put(key, separateValuesAndTrim(value));
-        }
 
+            headerValueByKey.put(key, HeaderValue.from(value));
+        }
         return new HttpHeaders(headerValueByKey);
     }
 
-    private static List<String> separateValuesAndTrim(String value) {
-        List<String> values = Arrays.asList(value.split(MULTIPLE_VALUE_DELIMITER));
-        return values.stream()
-                     .map(String::trim)
-                     .collect(Collectors.toList());
+    public static HttpHeaders empty() {
+        return new HttpHeaders(new HashMap<>());
+    }
+
+    public void add(String key, String value) {
+        headers.put(key, HeaderValue.from(value));
     }
 
     public List<String> get(String key) {
-        return headers.get(key);
+        return headers.getOrDefault(key, HeaderValue.empty()).getValues();
     }
 }

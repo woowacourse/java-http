53bc8ba8e36c29f97d6c363bb3e9d8998a569cfe
dooboocookie <bkh3030@gmail.com>
2023-09-09 16:13:21 +0900

    feat: RequestUri 구현

---
+++ tomcat/src/main/java/org/apache/coyote/http11/request/RequestUri.java
@@ -0,0 +1,40 @@
+package org.apache.coyote.http11.request;
+
+public class RequestUri {
+
+    public static final String SEPARATOR = "?";
+
+    private final String path;
+
+    private final String queryString;
+
+    private RequestUri(final String path,
+                      final String queryString) {
+        this.path = path;
+        this.queryString = queryString;
+    }
+
+    public static RequestUri from(final String requestUri) {
+        if (requestUri.contains(SEPARATOR)) {
+            String[] split = requestUri.split("\\" + SEPARATOR);
+            return new RequestUri(split[0], split[1]);
+        }
+        return new RequestUri(requestUri, "");
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getQueryString() {
+        return queryString;
+    }
+
+    @Override
+    public String toString() {
+        return "RequestUri{" +
+                "path='" + path + '\'' +
+                ", queryString='" + queryString + '\'' +
+                '}';
+    }
+}
---
+++ tomcat/src/test/java/org/apache/coyote/http11/request/RequestUriTest.java
@@ -0,0 +1,26 @@
+package org.apache.coyote.http11.request;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RequestUriTest {
+
+    @Test
+    void String_RequestURI_를_입력받아_객체를_생성하고_그_안에는_Path와_QueryString으로_나눠진다() {
+        //given
+        final String requestUriValue = "/path?a=1&b=2";
+        //when
+        RequestUri requestUri = RequestUri.from(requestUriValue);
+
+        //then
+        assertSoftly(softAssertions -> {
+            softAssertions.assertThat(requestUri.getPath()).isEqualTo("/path");
+            softAssertions.assertThat(requestUri.getQueryString()).isEqualTo("a=1&b=2");
+        });
+    }
+
+}

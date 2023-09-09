e6e0a9ebd8b8054dfcc7ac8b6f42bb6c1f3efca9
greeng00se <thegreengoose7@gmail.com>
2023-09-09 13:45:26 +0900

    feat: 알 수 없는 예외가 발생하는 경우 Internal Server Error 반환하는 기능 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -8,6 +8,8 @@ import java.io.OutputStream;
 import java.net.Socket;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
+import org.apache.coyote.http11.common.HttpStatus;
+import org.apache.coyote.http11.common.HttpVersion;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.request.HttpRequestParser;
 import org.apache.coyote.http11.response.HttpResponse;
@@ -40,14 +42,34 @@ public class Http11Processor implements Runnable, Processor {
         try (final InputStream inputStream = connection.getInputStream();
              final OutputStream outputStream = connection.getOutputStream();
              final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+            execute(bufferedReader, outputStream);
+        } catch (final IOException | UncheckedServletException e) {
+            LOG.error(e.getMessage(), e);
+        }
+    }
+
+    private void execute(final BufferedReader bufferedReader, final OutputStream outputStream) throws IOException {
+        try {
             final HttpRequest httpRequest = httpRequestParser.parse(bufferedReader);
             final HttpResponse httpResponse = new HttpResponse(httpRequest.getRequestLine().getHttpVersion());
+
             mapper.service(httpRequest, httpResponse);
+
             final String generate = httpResponseGenerator.generate(httpResponse);
             outputStream.write(generate.getBytes());
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (final Exception e) {
             LOG.error(e.getMessage(), e);
+            internalServerError(outputStream);
         }
+    }
+
+    private void internalServerError(final OutputStream outputStream) throws IOException {
+        final HttpResponse httpResponse = new HttpResponse(HttpVersion.HTTP_1_1)
+                .setHttpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+                .sendRedirect("/500.html");
+        final String response = httpResponseGenerator.generate(httpResponse);
+        outputStream.write(response.getBytes());
+        outputStream.flush();
     }
 }
--- tomcat/src/main/java/org/apache/coyote/http11/common/HttpStatus.java
+++ tomcat/src/main/java/org/apache/coyote/http11/common/HttpStatus.java
@@ -5,7 +5,8 @@ public enum HttpStatus {
     FOUND("302"),
     UNAUTHORIZED("401"),
     NOT_FOUND("404"),
-    CONFLICT("409");
+    CONFLICT("409"),
+    INTERNAL_SERVER_ERROR("500");
 
     private final String code;
 

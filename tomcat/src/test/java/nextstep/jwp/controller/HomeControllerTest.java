fca1ca3525eceda6d568c69beb37b5d695215b68
Combi153 <jcm04@naver.com>
2023-09-09 17:29:43 +0900

    [test] HomeController 테스트코드 변경

--- tomcat/src/test/java/nextstep/jwp/controller/HomeControllerTest.java
+++ tomcat/src/test/java/nextstep/jwp/controller/HomeControllerTest.java
@@ -1,55 +1,61 @@
 package nextstep.jwp.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.apache.coyote.http11.request.line.HttpMethod.GET;
+import static org.apache.coyote.http11.response.line.ResponseStatus.OK;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.response.HttpResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class HomeControllerTest {
 
-    private static final String EXPECTED_RESPONSE_MESSAGE = String.join(
-            System.lineSeparator(),
-            "HTTP/1.1 200 OK ",
-            "Content-Type: text/html;charset=utf-8 ",
-            "Content-Length: 12 ",
-            "",
-            "Hello world!"
-    );
+    private HttpRequest mockHttpRequest;
+    private HttpResponse mockHttpResponse;
+    private HomeController homeController;
+
+    @BeforeEach
+    void setUp() {
+        mockHttpRequest = Mockito.mock(HttpRequest.class);
+        mockHttpResponse = Mockito.mock(HttpResponse.class);
+        homeController = new HomeController();
+    }
 
     @Test
     void uri가_일치하면_GET_요청을_처리한다() {
         // given
-        HomeController homeController = new HomeController();
-
-        String uri = "/";
-        HttpRequest httpRequest = HttpRequest.of("GET " + uri + " HTTP/1.1 ", "Host: localhost:8080 ");
-        HttpResponse httpResponse = new HttpResponse(httpRequest.httpVersion());
+        when(mockHttpRequest.consistsOf(GET, "/"))
+                .thenReturn(true);
 
         // when
-        homeController.service(httpRequest, httpResponse);
+        homeController.service(mockHttpRequest, mockHttpResponse);
 
         // then
-        assertThat(httpResponse.responseMessage()).isEqualTo(EXPECTED_RESPONSE_MESSAGE);
+        verify(mockHttpRequest, times(1)).consistsOf(GET, "/");
+        verify(mockHttpResponse, times(1)).setResponseMessage(OK, "Hello world!");
     }
 
     @Test
     void uri가_일치하지_않으면_요청을_처리하지_않는다() {
         // given
-        HomeController homeController = new HomeController();
-
-        String wrongUri = "/wrong";
-        HttpRequest httpRequest = HttpRequest.of("GET " + wrongUri + " HTTP/1.1 ", "Host: localhost:8080 ");
-        HttpResponse httpResponse = new HttpResponse(httpRequest.httpVersion());
+        when(mockHttpRequest.consistsOf(any(), any()))
+                .thenReturn(false);
 
         // when
-        homeController.service(httpRequest, httpResponse);
+        homeController.service(mockHttpRequest, mockHttpResponse);
 
         // then
-        assertThat(httpResponse.responseMessage()).isNotEqualTo(EXPECTED_RESPONSE_MESSAGE);
+        verify(mockHttpRequest, times(1)).consistsOf(any(), any());
+        verify(mockHttpResponse, never()).setResponseMessage(OK, "Hello world!");
     }
 }

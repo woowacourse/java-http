b0f0ae3a52d74fd0a87fa67e0bf6fd55d4ada773
dooboocookie <bkh3030@gmail.com>
2023-09-09 19:39:37 +0900

    test: Headers 테스트 추가

---
+++ tomcat/src/test/java/org/apache/coyote/http11/header/HeadersTest.java
@@ -0,0 +1,38 @@
+package org.apache.coyote.http11.header;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeadersTest {
+
+    @Test
+    void 헤더_정보를_String으로_변환한다() {
+        //given
+        final Headers headers = new Headers();
+        headers.addHeader(EntityHeader.CONTENT_TYPE, "text/html");
+        headers.addHeader(EntityHeader.CONTENT_LENGTH, "100");
+
+        //when
+        final String actual = headers.parseResponse();
+
+        //then
+        final String expected = "Content-Type: text/html \r\n" +
+                "Content-Length: 100 ";
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void 헤더_정보를_추가한다() {
+        //given
+        final Headers headers = new Headers();
+
+        //when
+        headers.addHeader(EntityHeader.CONTENT_TYPE, "text/html");
+
+        //then
+        assertThat(headers.getValue(EntityHeader.CONTENT_TYPE)).isEqualTo("text/html");
+    }
+}

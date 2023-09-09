15c2edcaa21cf3469c4228fa8da4abfdb576b05d
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-09-09 17:17:10 +0900

    refactor: Resource 찾는 역할 분리

---
+++ tomcat/src/main/java/org/apache/coyote/http11/mvc/view/StaticResourceResolver.java
@@ -0,0 +1,31 @@
+package org.apache.coyote.http11.mvc.view;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.Optional;
+
+public class StaticResourceResolver {
+
+    private static final String RESOURCE_DIRECTORY = "static/";
+
+    public static String resolveResource(String viewName) {
+        final URL viewPath = findResourceByViewName(viewName);
+        return readResource(viewPath);
+    }
+
+    private static URL findResourceByViewName(final String viewName) {
+        final ClassLoader classLoader = StaticResourceResolver.class.getClassLoader();
+        return Optional.ofNullable(classLoader.getResource(RESOURCE_DIRECTORY + viewName))
+                .orElseThrow(() -> new IllegalArgumentException("Resource not found: " + viewName));
+    }
+
+    private static String readResource(final URL viewPath) {
+        try {
+            return new String(Files.readAllBytes(new File(viewPath.getFile()).toPath()));
+        } catch (final IOException e) {
+            throw new IllegalArgumentException("Resource read fail: " + viewPath.getFile(), e);
+        }
+    }
+}
--- tomcat/src/main/java/org/apache/coyote/http11/mvc/view/StaticResourceView.java
+++ tomcat/src/main/java/org/apache/coyote/http11/mvc/view/StaticResourceView.java
@@ -1,15 +1,8 @@
 package org.apache.coyote.http11.mvc.view;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.util.Optional;
 import org.apache.coyote.http11.common.ResourceContentType;
 
 public class StaticResourceView implements View {
-
-    private static final String RESOURCE_DIRECTORY = "static/";
 
     private final String viewName;
     private final String contentType;
@@ -25,14 +18,7 @@ public class StaticResourceView implements View {
 
     @Override
     public String renderView() {
-        try {
-            final ClassLoader classLoader = StaticResourceView.class.getClassLoader();
-            final URL viewPath = Optional.ofNullable(classLoader.getResource(RESOURCE_DIRECTORY + viewName))
-                    .orElseThrow(() -> new IllegalArgumentException("Resource not found: " + viewName));
-            return new String(Files.readAllBytes(new File(viewPath.getFile()).toPath()));
-        } catch (final IOException e) {
-            throw new IllegalArgumentException(e);
-        }
+        return StaticResourceResolver.resolveResource(viewName);
     }
 
     @Override

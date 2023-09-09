14cea648f0e03df3beb77afecc5f1156b7af89b7
jjongwa <troas96@naver.com>
2023-09-10 02:35:59 +0900

    refactor: user 저장 시 id 값 추가

--- tomcat/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
+++ tomcat/src/main/java/nextstep/jwp/db/InMemoryUserRepository.java
@@ -3,19 +3,23 @@ package nextstep.jwp.db;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 import nextstep.jwp.model.User;
 
 public class InMemoryUserRepository {
 
+    private static final AtomicLong id = new AtomicLong(1L);
+
     private static final Map<String, User> database = new ConcurrentHashMap<>();
 
     static {
-        final User user = new User(1L, "gugu", "password", "hkkang@woowahan.com");
+        final User user = new User(id.getAndIncrement(), "gugu", "password", "hkkang@woowahan.com");
         database.put(user.getAccount(), user);
     }
 
     public static void save(User user) {
-        database.put(user.getAccount(), user);
+        final User userWithId = new User(id.getAndIncrement(), user.getAccount(), user.getPassword(), user.getEmail());
+        database.put(user.getAccount(), userWithId);
     }
 
     public static Optional<User> findByAccount(String account) {

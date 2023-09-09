5eccdbe06ca270c03647bb6249f79747a77a3963
hyena <goatsfish@gmail.com>
2023-09-09 14:37:55 +0900

    refactor: Http Request 내부 생성 메서드 리팩터링

--- tomcat/src/main/java/org/apache/coyote/request/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/request/HttpRequest.java
@@ -2,21 +2,12 @@ package org.apache.coyote.request;
 
 import org.apache.coyote.common.Headers;
 import org.apache.coyote.common.MediaType;
-import org.apache.coyote.exception.CoyoteIOException;
-import org.apache.coyote.session.Cookies;
+import org.apache.coyote.common.MessageBody;
 import org.apache.coyote.session.Session;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
-import static org.apache.coyote.common.HeaderType.CONTENT_LENGTH;
-
 public class HttpRequest {
-
-    private static final String HEADER_END_CONDITION = "";
 
     private final RequestLine requestLine;
     private final RequestHeaders requestHeaders;
@@ -34,48 +25,20 @@ public class HttpRequest {
         this.requestBody = requestBody;
     }
 
-    public static HttpRequest from(final BufferedReader br) {
-        try {
-            final RequestLine requestLine = RequestLine.from(br.readLine());
-            final RequestHeaders requestHeaders = parseToHttpHeaders(br);
-            final MediaType mediaType = MediaType.from(requestLine.requestPath().value());
-            final RequestBody requestBody = parseToResponseBody(br, requestHeaders);
+    public static HttpRequest of(final RequestLine requestLine, final Headers headers, final MessageBody messageBody) {
+        final RequestHeaders requestHeaders = RequestHeaders.from(headers);
+        final MediaType mediaType = MediaType.from(requestLine.requestPath().value());
+        final RequestBody requestBody = RequestBody.from(messageBody);
 
-            return new HttpRequest(requestLine, requestHeaders, mediaType, requestBody);
-        } catch (IOException e) {
-            throw new CoyoteIOException("HTTP 요청 정보를 읽던 도중에 예외가 발생하였습니다.");
-        }
-    }
-
-    private static RequestHeaders parseToHttpHeaders(final BufferedReader br) throws IOException {
-        final List<String> headersWithValue = new ArrayList<>();
-        String header = br.readLine();
-        while (!header.equals(HEADER_END_CONDITION)) {
-            headersWithValue.add(header);
-            header = br.readLine();
-        }
-
-        return RequestHeaders.from(headersWithValue);
-    }
-
-    private static RequestBody parseToResponseBody(final BufferedReader br, final RequestHeaders requestHeaders) throws IOException {
-        final String contentLengthHeader = requestHeaders.getHeaderValue(CONTENT_LENGTH.value());
-        if (Objects.isNull(contentLengthHeader)) {
-            return RequestBody.empty();
-        }
-
-        final int contentLength = Integer.parseInt(contentLengthHeader);
-        final char[] buffer = new char[contentLength];
-        br.read(buffer, 0, contentLength);
-        return RequestBody.from(new String(buffer));
-    }
-
-    public RequestLine requestLine() {
-        return requestLine;
+        return new HttpRequest(requestLine, requestHeaders, mediaType, requestBody);
     }
 
     public String getCookieValue(final String cookieName) {
         return requestHeaders.getCookieValue(cookieName);
+    }
+
+    public RequestLine requestLine() {
+        return requestLine;
     }
 
     public HttpMethod httpMethod() {
@@ -90,14 +53,6 @@ public class HttpRequest {
         return requestLine.queryParams();
     }
 
-    public Headers headers() {
-        return requestHeaders.headers();
-    }
-
-    public Cookies cookies() {
-        return requestHeaders.cookies();
-    }
-
     public Session session() {
         return requestHeaders.session();
     }
@@ -108,6 +63,19 @@ public class HttpRequest {
 
     public RequestBody requestBody() {
         return requestBody;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final HttpRequest that = (HttpRequest) o;
+        return Objects.equals(requestLine, that.requestLine) && Objects.equals(requestHeaders, that.requestHeaders) && mediaType == that.mediaType && Objects.equals(requestBody, that.requestBody);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(requestLine, requestHeaders, mediaType, requestBody);
     }
 
     @Override

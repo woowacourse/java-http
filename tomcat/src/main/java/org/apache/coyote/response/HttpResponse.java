d727cb43d7eeb910f83b152792ce69f1750f3b22
yenawee <yaena0319@naver.com>
2023-09-10 23:36:31 +0900

    feat: HttpResponse 정적팩터리메소드 추가

--- tomcat/src/main/java/org/apache/coyote/response/HttpResponse.java
+++ tomcat/src/main/java/org/apache/coyote/response/HttpResponse.java
@@ -8,9 +8,27 @@ import java.util.Map;
 
 public class HttpResponse {
 
+    private static final String DEFAULT_VERSION = "HTTP/1.1";
+
     private final StatusLine statusLine;
     private final HttpResponseHeader headers;
-    private final String body;
+
+    private String body;
+
+    private HttpResponse(StatusLine statusLine, HttpResponseHeader headers, String body) {
+        this.statusLine = statusLine;
+        this.headers = headers;
+        this.body = body;
+    }
+
+    public static HttpResponse createDefaultResponse() {
+        String version = DEFAULT_VERSION;
+        HttpStatus httpStatus = HttpStatus.OK;
+        Map<String, String> headers = new LinkedHashMap<>();
+        String body = "";
+
+        return new HttpResponse(new StatusLine(version, httpStatus), new HttpResponseHeader(headers), body);
+    }
 
     private HttpResponse(Builder builder) {
         this.statusLine = new StatusLine(builder.version, builder.httpStatus);
@@ -18,8 +36,17 @@ public class HttpResponse {
         this.body = builder.body;
     }
 
+
     public String getResponse() {
         return statusLine.getStatusLine() + "\r\n" + headers.getResponseHeader() + "\r\n" + body;
+    }
+
+    public void setContentType(ContentType contentType) {
+        this.headers.add("Content-Type", contentType.getType() + ";charset=utf-8");
+    }
+
+    public void setBody(String body) {
+        this.body = body;
     }
 
     public static class Builder {
@@ -29,7 +56,7 @@ public class HttpResponse {
         private String body;
 
         public Builder() {
-            this.version = "HTTP/1.1";
+            this.version = DEFAULT_VERSION;
             this.httpStatus = HttpStatus.OK;
             this.headers = new LinkedHashMap<>();
             this.body = "";

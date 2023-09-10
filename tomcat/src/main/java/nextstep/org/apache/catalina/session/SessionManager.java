7d922b632acd181c046786a0c6003e07418d2a97
MoonJeWoong <munjin0201@naver.com>
2023-09-11 00:40:35 +0900

    feat: 로그인 session 동시성 관리를 위한 ConcurrentHashMap 사용

--- tomcat/src/main/java/nextstep/org/apache/catalina/session/SessionManager.java
+++ tomcat/src/main/java/nextstep/org/apache/catalina/session/SessionManager.java
@@ -1,12 +1,12 @@
 package nextstep.org.apache.catalina.session;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import nextstep.org.apache.catalina.Manager;
 
 public class SessionManager implements Manager {
 
-    private static final Map<String, Session> SESSIONS = new HashMap<>();
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
 
     @Override
     public void add(Session session) {

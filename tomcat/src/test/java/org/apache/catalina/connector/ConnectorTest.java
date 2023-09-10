23c6c8c784972f90ca4447f091ae7de17362050e
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2023-09-11 00:14:18 +0900

    test: 임계값 변경

--- tomcat/src/test/java/org/apache/catalina/connector/ConnectorTest.java
+++ tomcat/src/test/java/org/apache/catalina/connector/ConnectorTest.java
@@ -8,6 +8,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 import nextstep.servlet.Servlet;
@@ -21,43 +22,57 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ConnectorTest {
 
-    @DisplayName("스레드 크기가 5일 때, 요청이 5개가 들어오면 정상적으로 실행된다.")
+    @DisplayName("스레드 크기가 4일 때, 요청이 4개가 들어오면 정상적으로 실행된다.")
     @Order(1)
     @Test
     void processAll() throws InterruptedException {
         // given
         int port = makeRandomPort();
-        final AtomicInteger threads = new AtomicInteger(0);
+        final AtomicInteger waitThreads = new AtomicInteger(0);
         final Connector connector = new Connector(new StubServletContainer(), port, 250, 4);
         connector.start();
 
         // when
+        final ArrayList<Thread> threads = new ArrayList<>();
         for (int i = 0; i < 4; i++) {
-            final Thread thread = new Thread(() -> new TestHttpUtils(port).send("/index.html", threads));
+            final Thread thread = new Thread(() -> new TestHttpUtils(port).send("/index.html", waitThreads));
             thread.start();
+            threads.add(thread);
         }
 
-        Thread.sleep(1000);
-        assertThat(threads.get()).isEqualTo(0);
+        Thread.sleep(300);
+        removeThreads(threads);
+        assertThat(waitThreads.get()).isEqualTo(0);
     }
 
-    @DisplayName("스레드 크기가 5일 때, 요청이 10개가 들어오면 5개가 대기한다.")
+    @DisplayName("스레드 크기가 1일 때, 요청이 4개가 들어오면 3개가 대기한다.")
     @Order(2)
     @Test
     void waitFiveThreads() throws InterruptedException {
         // given
         int port = makeRandomPort();
-        final AtomicInteger threads = new AtomicInteger(0);
+        final AtomicInteger waitingThreads = new AtomicInteger(0);
         final Connector connector = new Connector(new StubServletContainer(), port, 250, 1);
         connector.start();
 
+
+        final ArrayList<Thread> threads = new ArrayList<>();
         // when
         for (int i = 0; i < 4; i++) {
-            new Thread(() -> new TestHttpUtils(port).send("/index.html", threads)).start();
+            final Thread thread = new Thread(() -> new TestHttpUtils(port).send("/index.html", waitingThreads));
+            thread.start();
+            threads.add(thread);
         }
 
         Thread.sleep(1000);
-        assertThat(threads.get()).isEqualTo(3);
+        removeThreads(threads);
+        assertThat(waitingThreads.get()).isEqualTo(3);
+    }
+
+    private void removeThreads(final ArrayList<Thread> threads) throws InterruptedException {
+        for (Thread thread : threads) {
+            thread.join();
+        }
     }
 
     private int makeRandomPort() {
@@ -77,7 +92,7 @@ class ConnectorTest {
             public void service(final org.apache.coyote.http11.HttpRequest httpRequest,
                 final org.apache.coyote.http11.HttpResponse httpResponse) throws IOException {
                 try {
-                    Thread.sleep(100);
+                    Thread.sleep(200);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
@@ -115,7 +130,9 @@ class ConnectorTest {
 
             final long afterSend = System.currentTimeMillis();
 
-            if (afterSend - beforeSend > 200) {
+            System.out.println("total = " + (afterSend - beforeSend));
+
+            if (afterSend - beforeSend > 350) {
                 waitingThreads.incrementAndGet();
             }
         }

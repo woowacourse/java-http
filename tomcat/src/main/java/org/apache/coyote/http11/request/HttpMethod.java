7def6c6576b6c572126565935916ecea0823bcbd
BackFoxx <backfoxxx@gmail.com>
2023-09-10 22:06:10 +0900

    refactor: HttpMethod를 가져오는 방법으로 stream 대신 enum을 채택

--- tomcat/src/main/java/org/apache/coyote/http11/request/HttpMethod.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/HttpMethod.java
@@ -1,13 +1,25 @@
 package org.apache.coyote.http11.request;
 
-import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
 
 public enum HttpMethod {
     GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS, TRACE, CONNECT;
 
+    public static final Map<String, HttpMethod> methods = Map.of(
+            "GET", GET,
+            "POST", POST,
+            "PUT", PUT,
+            "PATCH", PATCH,
+            "DELETE", DELETE,
+            "HEAD", HEAD,
+            "OPTIONS", OPTIONS,
+            "TRACE", TRACE,
+            "CONNECT", CONNECT
+    );
+
     public static HttpMethod from(String name) {
-        return Arrays.stream(values()).filter(method -> method.name().equalsIgnoreCase(name))
-                .findAny()
+        return Optional.ofNullable(methods.get(name))
                 .orElseThrow(() -> new IllegalArgumentException("Not existing Http method."));
     }
 }

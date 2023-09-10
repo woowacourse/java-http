48f7d3957e3eeace20fffb4fb0909fc3c2c78215
yoondgu <doyoungwork@gmail.com>
2023-09-10 23:20:33 +0900

    fix: 500 에러 발생 시 500.html 페이지로 리다이렉트하도록 수정

--- tomcat/src/main/java/org/apache/catalina/core/Container.java
+++ tomcat/src/main/java/org/apache/catalina/core/Container.java
@@ -1,7 +1,6 @@
 package org.apache.catalina.core;
 
 import java.util.Set;
-import javassist.NotFoundException;
 import org.apache.catalina.core.servlet.DefaultServlet;
 import org.apache.catalina.core.servlet.HttpServlet;
 import org.apache.catalina.core.servlet.HttpServletRequest;
@@ -9,9 +8,12 @@ import org.apache.catalina.core.servlet.HttpServletResponse;
 import org.apache.catalina.core.session.SessionManager;
 import org.apache.coyote.http11.request.Request;
 import org.apache.coyote.http11.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Container {
 
+    private static final Logger log = LoggerFactory.getLogger(Container.class);
     private final RequestHandlerAdaptor requestHandlerAdaptor;
     private final SessionManager sessionManager;
 
@@ -26,12 +28,10 @@ public class Container {
 
         try {
             requestHandlerAdaptor.service(httpServletRequest, httpServletResponse);
-
             return httpServletResponse.build();
-        } catch (final NotFoundException exception) {
-            return HttpServletResponse.notFound().build();
         } catch (final Exception exception) {
-            return HttpServletResponse.internalSeverError().build();
+            log.error(exception.getMessage(), exception);
+            return HttpServletResponse.redirect("/500.html").build();
         }
     }
 

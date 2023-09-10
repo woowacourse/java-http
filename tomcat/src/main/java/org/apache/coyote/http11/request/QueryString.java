a1a1fd501877f1c498f1cbf8c9528c3febac4fc3
jeomxon <jeomxon@gmail.com>
2023-09-10 22:39:37 +0900

    refactor: 상수화

--- tomcat/src/main/java/org/apache/coyote/http11/request/QueryString.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/QueryString.java
@@ -7,24 +7,23 @@ import java.util.stream.Collectors;
 
 public class QueryString {
 
+    private static final String QUERY_STRING_DELIMITER = "&";
+    private static final String QUERY_STRING_SEPARATOR = "=";
+
     private final Map<String, String> params;
 
     public static QueryString from(final String queryString) {
         if (queryString.isEmpty()) {
             return new QueryString(new HashMap<>());
         }
-        final Map<String, String> params = Arrays.stream(queryString.split("&"))
-                .map(string -> Arrays.asList(string.split("=")))
+        final Map<String, String> params = Arrays.stream(queryString.split(QUERY_STRING_DELIMITER))
+                .map(string -> Arrays.asList(string.split(QUERY_STRING_SEPARATOR)))
                 .collect(Collectors.toMap(data -> data.get(0), data -> data.get(1), (a, b) -> b));
         return new QueryString(params);
     }
 
     private QueryString(final Map<String, String> params) {
         this.params = params;
-    }
-
-    public boolean hasNoQueryString() {
-        return params.isEmpty();
     }
 
     public String get(final String key) {
--- tomcat/src/main/java/org/apache/coyote/http11/request/Uri.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/Uri.java
@@ -2,17 +2,20 @@ package org.apache.coyote.http11.request;
 
 public class Uri {
 
+    private static final String QUERY_STRING_START_LETTER = "?";
+    private static final String EMPTY_STRING = "";
+
     private final String path;
     private final QueryString queryString;
 
     public static Uri from(final String uri) {
-        if (uri.contains("?")) {
-            final int index = uri.indexOf("?");
+        if (uri.contains(QUERY_STRING_START_LETTER)) {
+            final int index = uri.indexOf(QUERY_STRING_START_LETTER);
             final String path = uri.substring(0, index);
             final String queryString = uri.substring(index + 1);
             return new Uri(path, queryString);
         }
-        return new Uri(uri, "");
+        return new Uri(uri, EMPTY_STRING);
     }
 
     private Uri(final String path, final String queryString) {
@@ -22,9 +25,5 @@ public class Uri {
 
     public String getPath() {
         return path;
-    }
-
-    public QueryString getQueryString() {
-        return queryString;
     }
 }

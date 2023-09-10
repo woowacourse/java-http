930fb1039e3c45fe8259f6dba1d9678b19c6be16
Go-Jaecheol <gojaech@naver.com>
2023-09-10 16:51:17 +0900

    test: HttpResponse toString 테스트 추가

--- tomcat/src/test/java/org/apache/coyote/http11/response/HttpResponseTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/response/HttpResponseTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 @SuppressWarnings("NonAsciiCharacters")
@@ -43,7 +44,7 @@ class HttpResponseTest {
         @Test
         void POST_메서드에_대한_HttpResponse_객체를_생성한다() {
             // given
-            final var statusLine = StatusLine.of("HTTP/1.1", HttpStatus.OK);
+            final var statusLine = StatusLine.of("HTTP/1.1", HttpStatus.FOUND);
             final var responseHeader = ResponseHeader.createEmpty();
             responseHeader.addHeader("Location", "/index.html");
 
@@ -59,6 +60,53 @@ class HttpResponseTest {
                         .usingRecursiveComparison()
                         .isEqualTo(responseHeader);
             });
+        }
+    }
+
+    @Nested
+    class toString_테스트 {
+
+        @Test
+        void ResponseBody가_없는_경우의_HttpResponse_메세지를_출력한다() {
+            // given
+            final var statusLine = StatusLine.of("HTTP/1.1", HttpStatus.FOUND);
+            final var responseHeader = ResponseHeader.createEmpty();
+            responseHeader.addHeader("Location", "/index.html");
+            final var httpResponse = HttpResponse.createPostResponse(statusLine, responseHeader);
+
+            final var expected = String.join("\r\n",
+                    "HTTP/1.1 302 Found",
+                    "Location: /index.html");
+
+            // when
+            final var actual = httpResponse.toString();
+
+            // then
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        void ResponseBody가_있는_경우의_HttpResponse_메세지를_출력한다() {
+            // given
+            final var statusLine = StatusLine.of("HTTP/1.1", HttpStatus.OK);
+            final var responseHeader = ResponseHeader.createEmpty();
+            responseHeader.addHeader("Content-Type", "text/html");
+            responseHeader.addHeader("Content-Length", "12");
+            final var responseBody = ResponseBody.fromText("Hello World!");
+            final var httpResponse = HttpResponse.createGetResponse(statusLine, responseHeader, responseBody);
+
+            final var expected = String.join("\r\n",
+                    "HTTP/1.1 200 OK",
+                    "Content-Type: text/html",
+                    "Content-Length: 12",
+                    "",
+                    "Hello World!");
+
+            // when
+            final var actual = httpResponse.toString();
+
+            // then
+            assertThat(actual).isEqualTo(expected);
         }
     }
 }

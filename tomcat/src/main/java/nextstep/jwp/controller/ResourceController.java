2cc44492237181eca80ae9d86a11fed75ee0abf0
Go-Jaecheol <gojaech@naver.com>
2023-09-10 19:04:02 +0900

    feat: ResourceController 구현

---
+++ tomcat/src/main/java/nextstep/jwp/controller/ResourceController.java
@@ -0,0 +1,36 @@
+package nextstep.jwp.controller;
+
+import nextstep.jwp.exception.HttpRequestException;
+import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.response.HttpResponse;
+import org.apache.coyote.http11.response.HttpStatus;
+import org.apache.coyote.http11.response.ResponseBody;
+import org.apache.coyote.http11.response.StatusLine;
+
+import java.io.IOException;
+
+public class ResourceController extends AbstractController {
+
+    @Override
+    protected void doPost(final HttpRequest request, final HttpResponse response) {
+        throw new HttpRequestException(HTTP_METHOD_EXCEPTION_MESSAGE);
+    }
+
+    @Override
+    protected void doGet(final HttpRequest request, final HttpResponse response) throws IOException {
+        final var statusLine = StatusLine.of(request.getRequestLine().getProtocol(), HttpStatus.OK);
+        final String uri = request.getRequestLine().getPath().split("\\?")[0];
+        final var responseBody = ResponseBody.fromUri(uri);
+        response.setStatusLine(statusLine);
+        response.addResponseHeader("Content-Type", getContentType(uri));
+        response.addResponseHeader("Content-Length", String.valueOf(responseBody.getBody().getBytes().length));
+        response.setResponseBody(responseBody);
+    }
+
+    private String getContentType(final String uri) {
+        if (uri.endsWith(".css")) {
+            return TEXT_CSS;
+        }
+        return TEXT_HTML;
+    }
+}

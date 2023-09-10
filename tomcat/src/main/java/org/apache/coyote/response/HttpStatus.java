82e3e5ca6674149e188d9bfc6f36c7663360e6f4
yenawee <yaena0319@naver.com>
2023-09-10 22:01:34 +0900

    refactor: HttpStatus 추가

--- tomcat/src/main/java/org/apache/coyote/response/HttpStatus.java
+++ tomcat/src/main/java/org/apache/coyote/response/HttpStatus.java
@@ -4,7 +4,9 @@ public enum HttpStatus {
 
     OK(200),
     FOUND(302),
-    UNAUTHORIZED(401);
+    UNAUTHORIZED(401),
+    NOT_FOUND(404),
+    INTERNAL_SERVER_ERROR(500);
 
     private final int code;
 

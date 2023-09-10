58a60ff94ba494ae3469ec953e1d7d70ce545a8c
BGuga <tmdwns9913@gmail.com>
2023-09-10 13:57:46 +0900

    feat: StatusLine 객체 생성

---
+++ tomcat/src/main/java/org/apache/coyote/http11/response/StatusLine.java
@@ -0,0 +1,16 @@
+package org.apache.coyote.http11.response;
+
+public class StatusLine {
+
+    private static final String HTTP_VERSION = "HTTP/1.1";
+    private final HttpStatusCode httpStatusCode;
+
+    public StatusLine(HttpStatusCode httpStatusCode) {
+        this.httpStatusCode = httpStatusCode;
+    }
+
+    @Override
+    public String toString() {
+        return HTTP_VERSION + " " + httpStatusCode.getStatusCode() + " " + httpStatusCode.name();
+    }
+}

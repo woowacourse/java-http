d792f64c630fb13c9ee2ded776cd1c4d6f69bf8b
java-saeng <gusdn85554@naver.com>
2023-09-11 13:48:21 +0900

    refactor : 동시성 보장을 위한 ConcurrentHashMap 사용

--- tomcat/src/main/java/org/apache/coyote/request/SessionManager.java
+++ tomcat/src/main/java/org/apache/coyote/request/SessionManager.java
@@ -1,13 +1,13 @@
 package org.apache.coyote.request;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import org.apache.catalina.Manager;
 
 public class SessionManager implements Manager {
 
-  private static final Map<String, Session> SESSIONS = new HashMap<>();
+  private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
 
   @Override
   public void add(final Session session) {

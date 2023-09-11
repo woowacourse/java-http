75cf8a9416f1a03bed5d6f1d23f9542656c7f1ad
kokodak <kokodakadokok@gmail.com>
2023-09-11 11:12:11 +0900

    feat: ResourceController 구현

---
+++ tomcat/src/main/java/kokodak/controller/ResourceController.java
@@ -0,0 +1,24 @@
+package kokodak.controller;
+
+import java.net.URL;
+import kokodak.http.HttpRequest;
+import kokodak.http.HttpResponse;
+
+public class ResourceController extends AbstractController {
+
+    @Override
+    protected void doPost(final HttpRequest httpRequest, final HttpResponse httpResponse) throws Exception {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void doGet(final HttpRequest httpRequest, final HttpResponse httpResponse) throws Exception {
+        final String path = httpRequest.getRequestTarget().getPath();
+        final String fileName = "static" + path;
+        final URL resourceUrl = getClass().getClassLoader().getResource(fileName);
+        if (resourceUrl == null) {
+            httpResponse.notFound(httpRequest);
+        }
+        httpResponse.setBody(fileName, httpRequest.header("Accept"));
+    }
+}

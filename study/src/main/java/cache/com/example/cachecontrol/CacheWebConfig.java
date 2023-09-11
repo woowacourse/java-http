f665ef695e402b1b61a008bb9eaea352c7798a63
amaran-th <songsy405@naver.com>
2023-09-11 16:06:44 +0900

    chore: 캐시 무효화 학습테스트 해결
    - Config 파일 수정

--- study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
+++ study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
@@ -1,18 +1,27 @@
 package cache.com.example.cachecontrol;
 
+import cache.com.example.version.ResourceVersion;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
+import static cache.com.example.version.CacheBustingWebConfig.PREFIX_STATIC_RESOURCES;
+
 @Configuration
 public class CacheWebConfig implements WebMvcConfigurer {
+    private final ResourceVersion version;
+
+    public CacheWebConfig(final ResourceVersion version) {
+        this.version = version;
+    }
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
         WebContentInterceptor interceptor = new WebContentInterceptor();
         interceptor.addCacheMapping(CacheControl.noCache().cachePrivate(), "/*");
-        registry.addInterceptor(interceptor);
+        registry.addInterceptor(interceptor)
+                .excludePathPatterns(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/**");
     }
 }

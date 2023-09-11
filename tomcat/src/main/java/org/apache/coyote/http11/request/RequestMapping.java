f06fc4cd8739d1ac8c1a9da027dd2699178e0196
최준호 <junho5336@gmail.com>
2023-09-11 13:51:37 +0900

    refactor: Controller 매핑 Map으로 변경

--- tomcat/src/main/java/org/apache/coyote/http11/request/RequestMapping.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/RequestMapping.java
@@ -1,29 +1,25 @@
 package org.apache.coyote.http11.request;
 
-import org.apache.coyote.http11.controller.Controller;
-import org.apache.coyote.http11.controller.DefaultController;
-import org.apache.coyote.http11.controller.LoginController;
-import org.apache.coyote.http11.controller.RegisterController;
+import org.apache.coyote.http11.controller.*;
+
+import java.util.Map;
+
+import static org.apache.coyote.http11.controller.URIPath.LOGIN_URI;
+import static org.apache.coyote.http11.controller.URIPath.REGISTER_URI;
 
 public class RequestMapping {
 
-    private static final String REGISTER_URI = "/register";
-    private static final String LOGIN_URI = "/login";
-
-    private static final Controller LOGIN_CONTROLLER = new LoginController();
-    private static final Controller REGISTER_CONTROLLER = new RegisterController();
-    private static final Controller DEFAULT_CONTROLLER = new DefaultController();
+    private static final Map<String, Controller> CONTROLLER_MAPPER = Map.of(
+            REGISTER_URI, new RegisterController(),
+            LOGIN_URI, new LoginController()
+    );
 
     public Controller getController(HttpRequest request) {
         String path = request.getPath();
-        if (path.equals(REGISTER_URI)) {
-            return REGISTER_CONTROLLER;
+        Controller controller = CONTROLLER_MAPPER.get(path);
+        if (controller == null) {
+            return new DefaultController();
         }
-
-        if (path.equals(LOGIN_URI)) {
-            return LOGIN_CONTROLLER;
-        }
-
-        return DEFAULT_CONTROLLER;
+        return controller;
     }
 }

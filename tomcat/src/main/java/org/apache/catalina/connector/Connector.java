6cb1b9ad6edcd03e3f4e74641e61648ada9eaab5
the9kim <the9rla@gmail.com>
2023-09-11 15:56:21 +0900

    feat: 동시성 구현

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -8,6 +8,9 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Connector implements Runnable {
 
@@ -15,17 +18,22 @@ public class Connector implements Runnable {
 
     private static final int DEFAULT_PORT = 8080;
     private static final int DEFAULT_ACCEPT_COUNT = 100;
+    private static final int DEFAULT_MAX_THREADS = 250;
+
+    private static final int TERMINATION_LIMIT_SECONDS = 60;
 
     private final ServerSocket serverSocket;
+    private final ExecutorService executorService;
     private boolean stopped;
 
     public Connector() {
-        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT);
+        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT, DEFAULT_MAX_THREADS);
     }
 
-    public Connector(final int port, final int acceptCount) {
+    public Connector(final int port, final int acceptCount, final int maxThreads) {
         this.serverSocket = createServerSocket(port, acceptCount);
         this.stopped = false;
+        this.executorService = Executors.newFixedThreadPool(maxThreads);
     }
 
     private ServerSocket createServerSocket(final int port, final int acceptCount) {
@@ -67,15 +75,28 @@ public class Connector implements Runnable {
             return;
         }
         var processor = new Http11Processor(connection);
+        executorService.execute(processor);
         new Thread(processor).start();
     }
 
     public void stop() {
         stopped = true;
         try {
+            executorService.shutdown();
+            terminateWhenTimeLimitExceed();
             serverSocket.close();
         } catch (IOException e) {
             log.error(e.getMessage(), e);
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
+            log.error(e.getMessage(), e);
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void terminateWhenTimeLimitExceed() throws InterruptedException {
+        if (!executorService.awaitTermination(TERMINATION_LIMIT_SECONDS, TimeUnit.SECONDS)) {
+            executorService.shutdownNow();
         }
     }
 

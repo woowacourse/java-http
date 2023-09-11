c54f0c7d37ea2a6096c6c476f48f71a97c14db8a
Combi153 <jcm04@naver.com>
2023-09-11 22:26:28 +0900

    [style] 불필요한 메소드 제거

--- tomcat/src/main/java/nextstep/jwp/exception/UncheckedServletException.java
+++ tomcat/src/main/java/nextstep/jwp/exception/UncheckedServletException.java
@@ -5,16 +5,7 @@ public class UncheckedServletException extends RuntimeException {
     public UncheckedServletException() {
     }
 
-    public UncheckedServletException(Exception e) {
-        super(e);
-    }
-
     public UncheckedServletException(String message) {
         super(message);
-    }
-
-    @Override
-    public String getMessage() {
-        return super.getMessage();
     }
 }

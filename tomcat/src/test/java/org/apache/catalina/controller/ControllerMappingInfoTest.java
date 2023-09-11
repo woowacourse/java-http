43617b79eab88a43aac2bad9205544c043f1f459
hyena <goatsfish@gmail.com>
2023-09-11 15:10:53 +0900

    test: 컨트롤러 매핑 정보 테스트 수정

--- tomcat/src/test/java/org/apache/catalina/controller/ControllerMappingInfoTest.java
+++ tomcat/src/test/java/org/apache/catalina/controller/ControllerMappingInfoTest.java
@@ -18,20 +18,30 @@ class ControllerMappingInfoTest {
     @ParameterizedTest
     @MethodSource("controllerMappingInfoDummy")
     void HttpMethod와_파라미터_유무와_요청_path로_생성에_성공한다(
-            HttpMethod httpMethod,
-            boolean isParameterized,
-            String requestPath
+            Expected expected
     ) {
-        assertThatCode(() -> ControllerMappingInfo.of(httpMethod, isParameterized, requestPath))
+        assertThatCode(() -> ControllerMappingInfo.of(expected.httpMethod, expected.isParameterized, expected.requestUri))
                 .doesNotThrowAnyException();
     }
 
-    static Stream<Arguments> controllerMappingInfoDummy() {
+    private static class Expected {
+        private final HttpMethod httpMethod;
+        private final boolean isParameterized;
+        private final String requestUri;
+
+        public Expected(final HttpMethod httpMethod, final boolean isParameterized, final String requestUri) {
+            this.httpMethod = httpMethod;
+            this.isParameterized = isParameterized;
+            this.requestUri = requestUri;
+        }
+    }
+
+    private static Stream<Arguments> controllerMappingInfoDummy() {
         return Stream.of(
-                Arguments.arguments(HttpMethod.GET, false, "/index.html"),
-                Arguments.arguments(HttpMethod.GET, true, "/index.html?name=hyena"),
-                Arguments.arguments(HttpMethod.POST, false, "/index.html"),
-                Arguments.arguments(HttpMethod.POST, true, "/index.html?name=hyena")
+                Arguments.arguments(new Expected(HttpMethod.GET, false, "/index.html")),
+                Arguments.arguments(new Expected(HttpMethod.GET, true, "/index.html?name=hyena")),
+                Arguments.arguments(new Expected(HttpMethod.POST, false, "/index.html")),
+                Arguments.arguments(new Expected(HttpMethod.POST, true, "/index.html?name=hyena"))
         );
     }
 }

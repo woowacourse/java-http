8011ced3de1cc25c1057b5958d5e4ef377ae0e92
jyeost <pig9619@naver.com>
2023-09-12 16:08:15 +0900

    refactor: url에 맞지 않는 method로 요청이 들어오면 해당하는 예외를 던지도록 한다

--- tomcat/src/main/java/nextstep/jwp/controller/HelloWorldController.java
+++ tomcat/src/main/java/nextstep/jwp/controller/HelloWorldController.java
@@ -2,9 +2,15 @@ package nextstep.jwp.controller;
 
 import org.apache.coyote.request.HttpRequest;
 import org.apache.coyote.response.HttpResponse;
+import org.apache.exception.MethodMappingFailException;
 import org.apache.front.AbstractController;
 
 public class HelloWorldController extends AbstractController {
+
+    @Override
+    protected void doPost(final HttpRequest request, final HttpResponse response) {
+        throw new MethodMappingFailException();
+    }
 
     @Override
     protected void doGet(final HttpRequest request, final HttpResponse response) {

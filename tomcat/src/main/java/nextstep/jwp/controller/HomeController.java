b05c414713a484a5e95b8da824182e23a44d8580
dooboocookie <bkh3030@gmail.com>
2023-09-12 22:42:39 +0900

    fix: POST / 요청 405 응답으로 수정

--- tomcat/src/main/java/nextstep/jwp/controller/HomeController.java
+++ tomcat/src/main/java/nextstep/jwp/controller/HomeController.java
@@ -4,11 +4,13 @@ import org.apache.coyote.controller.AbstractController;
 import org.apache.coyote.http11.request.Request;
 import org.apache.coyote.http11.response.Response;
 
+import static org.apache.coyote.http11.response.StatusCode.METHOD_NOT_ALLOWED;
+
 public class HomeController extends AbstractController {
 
     @Override
     protected void doPost(final Request request, final Response response) {
-        response.responseNotFound();
+        response.setStatusCode(METHOD_NOT_ALLOWED);
     }
 
     @Override

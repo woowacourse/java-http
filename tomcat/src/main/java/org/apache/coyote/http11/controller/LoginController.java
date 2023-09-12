d3519539e6f4b3529745ad5066798b57c2fbdace
This2sho <zx00018@naver.com>
2023-09-12 14:03:11 +0900

    refactor: 비밀번호 오류 시 401 페이지 반환하도록 수정

--- tomcat/src/main/java/org/apache/coyote/http11/controller/LoginController.java
+++ tomcat/src/main/java/org/apache/coyote/http11/controller/LoginController.java
@@ -1,6 +1,7 @@
 package org.apache.coyote.http11.controller;
 
 import java.util.Objects;
+import java.util.Optional;
 import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.model.User;
 import org.apache.catalina.Session;
@@ -10,8 +11,6 @@ import org.apache.coyote.http11.response.HttpResponse;
 import org.apache.coyote.http11.StatusCode;
 import org.apache.coyote.http11.ViewLoader;
 import org.apache.coyote.http11.request.RequestBody;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class LoginController extends AbstractController {
 
@@ -21,39 +20,48 @@ public class LoginController extends AbstractController {
     protected void doPost(final HttpRequest request, final HttpResponse response) throws Exception {
         final RequestBody requestBody = request.initRequestBody();
         if (Objects.isNull(requestBody) || requestBody.size() != LOGIN_PARAMETER_SIZE) {
-            response
-                .statusCode(StatusCode.UNAUTHORIZED)
-                .contentType(ContentType.TEXT_HTML)
-                .responseBody(ViewLoader.toUnauthorized());
+            redirectByUnauthorized(response);
             return;
         }
         final String account = requestBody.get("account");
         final String password = requestBody.get("password");
-        final User user = login(account, password);
 
-        final Session session = request.getSession(true);
-        session.setAttribute("user", user);
+        if (isValidUser(account, password)) {
+            final Session session = request.getSession(true);
+            session.setAttribute("user", getUser(account));
+            redirectByFound(response, session);
+        }
+        redirectByUnauthorized(response);
+    }
 
+    private void redirectByUnauthorized(final HttpResponse response) {
+        response
+                .statusCode(StatusCode.UNAUTHORIZED)
+                .contentType(ContentType.TEXT_HTML)
+                .responseBody(ViewLoader.toUnauthorized());
+    }
+
+    private boolean isValidUser(final String account, final String password) {
+        final Optional<User> user = InMemoryUserRepository.findByAccount(account);
+        if (user.isPresent() && user.get().checkPassword(password)) {
+            return true;
+        }
+        return false;
+    }
+
+    private User getUser(final String account) {
+        final User user = InMemoryUserRepository.findByAccount(account)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 계정입니다."));
+        return user;
+    }
+
+    private void redirectByFound(final HttpResponse response, final Session session) {
         response
             .statusCode(StatusCode.FOUND)
             .contentType(ContentType.TEXT_HTML)
             .responseBody(ViewLoader.toIndex())
             .addCookie(session.getId())
             .redirect("/index.html");
-    }
-
-    private User login(final String account, final String password) {
-        final User user = InMemoryUserRepository.findByAccount(account)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 계정입니다."));
-        validatePassword(user, password);
-        return user;
-    }
-
-    private void validatePassword(final User user, final String password) {
-        if (user.checkPassword(password)) {
-            return;
-        }
-        throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
     }
 
     @Override

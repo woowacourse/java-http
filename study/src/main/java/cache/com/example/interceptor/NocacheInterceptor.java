ec51e20e79bed9467d4041631533f665e4c70333
Ohjintaek <jt950930@gmail.com>
2023-09-13 14:15:32 +0900

    teat:  캐시 학습 테스트

---
+++ study/src/main/java/cache/com/example/interceptor/NocacheInterceptor.java
@@ -0,0 +1,15 @@
+package cache.com.example.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class NocacheInterceptor implements HandlerInterceptor {
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+        response.addHeader("Cache-Control", "no-cache, private");
+    }
+}

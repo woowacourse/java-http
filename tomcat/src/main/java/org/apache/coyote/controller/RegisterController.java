147d760a4845d3843adfed00986050427e6d5f6c
aiaiaiai1 <cvc100326@naver.com>
2023-09-14 18:24:36 +0900

    refactor: 방어 로직 추가

--- tomcat/src/main/java/org/apache/coyote/controller/RegisterController.java
+++ tomcat/src/main/java/org/apache/coyote/controller/RegisterController.java
@@ -4,6 +4,7 @@ import static org.apache.coyote.http11.HttpStatus.FOUND;
 import static org.apache.coyote.http11.HttpStatus.OK;
 
 import java.util.Map;
+import java.util.Objects;
 import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.model.User;
 import org.apache.coyote.http11.HttpRequest;
@@ -14,15 +15,28 @@ import org.slf4j.LoggerFactory;
 
 public class RegisterController extends AbstractController {
     private static final Logger log = LoggerFactory.getLogger(RegisterController.class);
+    public static final int LOGIN_QUERY_PARAMS = 3;
 
     @Override
-    protected void doPost(HttpRequest request, HttpResponse response) throws Exception {
+    protected void doPost(HttpRequest request, HttpResponse response) {
         Map<String, String> queryParms = Utils.parseToQueryParms(request.getBody());
+
+        validateQueryParms(queryParms);
         User user = new User(queryParms.get("account"), queryParms.get("password"),
                 queryParms.get("email"));
+
         InMemoryUserRepository.save(user);
         response.setStatus(FOUND);
         response.setRedirectUrl("/index.html");
+    }
+
+    private void validateQueryParms(Map<String, String> queryParms) {
+        long count = queryParms.entrySet().stream()
+                .filter(entry -> Objects.nonNull(entry.getValue()))
+                .count();
+        if (count != LOGIN_QUERY_PARAMS) {
+            throw new IllegalArgumentException();
+        }
     }
 
     @Override

b8d2ea6dca9f53372bf3d196345fb71ac3957a4b
Donghoon Lee <aru0504@naver.com>
2024-09-03 15:04:24 +0900

    fix: replace javax with jakarta on imports (#500)

--- study/src/main/java/cache/com/example/GreetingController.java
+++ study/src/main/java/cache/com/example/GreetingController.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class GreetingController {
 

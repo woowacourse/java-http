35ffa1357d6aefbfb61e80f99f725734df06f75e
Arachneee <jhg2819@naver.com>
2024-09-04 02:14:06 +0900

    feat: http 정적파일 응답 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,16 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -26,17 +30,10 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+        try (InputStream inputStream = connection.getInputStream();
+             OutputStream outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            String response = createResponse(inputStream);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
@@ -44,4 +41,39 @@ public class Http11Processor implements Runnable, Processor {
             log.error(e.getMessage(), e);
         }
     }
+
+    private String createResponse(final InputStream inputStream) throws IOException {
+        Http11Request http11Request = Http11Request.from(inputStream);
+        log.info("http request : {}", http11Request);
+
+        String responseBody = getResponseBody(http11Request);
+        String contentType = getContentType(http11Request);
+
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: " + contentType + " ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+    }
+
+    private String getResponseBody(Http11Request http11Request) throws IOException {
+        if (http11Request.isStaticResourceRequest()) {
+            URL resource = getClass().getClassLoader().getResource("static" + http11Request.getEndpoint());
+            if (resource == null) {
+                throw new IllegalArgumentException("존재하지 않는 자원입니다.");
+            }
+            return new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+        }
+
+        return "Hello world!";
+    }
+
+    private String getContentType(Http11Request http11Request) {
+        String accept = http11Request.getHeader("Accept")
+                .orElse("text/html");
+
+        return accept + ";charset=utf-8";
+    }
+
 }

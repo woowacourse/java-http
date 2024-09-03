15630b9c3562c3ab31a136b95adcfdc23a75f3c3
haiseong <wjdgotjd9908@gmail.com>
2024-09-03 21:45:07 +0900

    feat: RequestLine 구현

---
+++ tomcat/src/main/java/org/apache/coyote/http11/request/RequestLine.java
@@ -0,0 +1,39 @@
+package org.apache.coyote.http11.request;
+
+import java.util.Arrays;
+import java.util.List;
+import org.apache.coyote.http11.HttpMethod;
+
+public class RequestLine {
+
+    HttpMethod method;
+    String requestURI;
+    String HttpVersion;
+
+    public RequestLine(String line) {
+        List<String> tokens = Arrays.stream(line.split(" ")).toList();
+        validate(tokens);
+
+        this.method = HttpMethod.ofName(tokens.get(0));
+        this.requestURI = tokens.get(1);
+        this.HttpVersion = tokens.get(2);
+    }
+
+    private void validate(List<String> tokens) {
+        if (tokens.size() != 3) {
+            throw new IllegalArgumentException("Invalid Request Line Length: " + tokens.size());
+        }
+    }
+
+    public HttpMethod getMethod() {
+        return method;
+    }
+
+    public String getRequestURI() {
+        return requestURI;
+    }
+
+    public String getHttpVersion() {
+        return HttpVersion;
+    }
+}

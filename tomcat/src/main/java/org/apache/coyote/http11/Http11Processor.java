568b9d309c552ffc89f483c5c4e1ed10824204b3
jinwoo22 <jenny010925@naver.com>
2024-09-03 20:55:50 +0900

    feat: /index.html 읽어오는 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,8 +5,14 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,19 +35,41 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader br = new BufferedReader(inputStreamReader);
 
+            String line = br.readLine();
+            Path path = parseResource(line);
+
+            if (path == null) {
+                return;
+            }
+
+            while (!"".equals(line)) {
+                line = br.readLine();
+            }
+
+            final var responseBody = Files.readAllBytes(path);
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "Content-Length: " + responseBody.length + " ",
                     "",
-                    responseBody);
+                    new String(responseBody));
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private Path parseResource(String requestLine) {
+        if (requestLine == null) return null;
+
+        String[] lines = requestLine.split(" ");
+        URL resource = getClass().getClassLoader().getResource("static" + lines[1]);
+        File file = new File(resource.getFile());
+        return file.toPath();
     }
 }

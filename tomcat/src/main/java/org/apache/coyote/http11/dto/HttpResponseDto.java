8ce1de98c558b167c9ced62d2551c99a040397d5
haiseong <wjdgotjd9908@gmail.com>
2024-09-04 01:11:16 +0900

    feat: HttpResponseDto 생성

---
+++ tomcat/src/main/java/org/apache/coyote/http11/dto/HttpResponseDto.java
@@ -0,0 +1,20 @@
+package org.apache.coyote.http11.dto;
+
+import java.util.Map;
+import org.apache.coyote.http11.response.HttpResponse;
+
+public record HttpResponseDto(
+        int httpStatusCode,
+        String reasonPhrase,
+        Map<String, String> responseHeaders,
+        String messageBody
+) {
+    public static HttpResponseDto from(HttpResponse httpResponse) {
+        return new HttpResponseDto(
+                httpResponse.getHttpStatus().getCode(),
+                httpResponse.getHttpStatus().getReasonPhrase(),
+                httpResponse.getResponseHeaders(),
+                httpResponse.getMessageBody()
+        );
+    }
+}

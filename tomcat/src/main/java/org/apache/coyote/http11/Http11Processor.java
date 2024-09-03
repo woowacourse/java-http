ddc31a75f52d1a0dac03a4505c9aede2c9d50d9e
kunsanglee <gosmdochee2@gmail.com>
2024-09-03 21:31:35 +0900

    feat: 인덱스 페이지에 접근 가능하도록 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,18 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -26,10 +32,19 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
+        try (final var bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            String connectionInput = bufferedReader.readLine();
+            String[] httpRequest = connectionInput.split(" ");
+            String httpMethod = httpRequest[0];
+            String resourcePath = httpRequest[1];
+            String httpProtocolVersion = httpRequest[2];
+
+            if (!httpMethod.equals("GET")) {
+                throw new IOException("HTTP Method is not GET");
+            }
+            String responseBody = getResponseBody(resourcePath);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -44,4 +59,18 @@ public class Http11Processor implements Runnable, Processor {
             log.error(e.getMessage(), e);
         }
     }
+
+    private String getResponseBody(String resourcePath) throws IOException {
+        if (resourcePath.equals("/")) {
+            return "Hello world!";
+        }
+
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("static" + resourcePath)) {
+            if (inputStream == null) {
+                throw new IOException("Resource not found: " + resourcePath);
+            }
+            return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+        }
+    }
+
 }

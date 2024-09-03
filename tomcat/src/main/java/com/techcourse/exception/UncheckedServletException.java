3be2f0cc11735df7342a4d22f95e4e2e17520d75
libienz <leekh10195@gmail.com>
2024-09-03 20:10:29 +0900

    feat: UncheckedServletExceptiona 생성 로직 추가

--- tomcat/src/main/java/com/techcourse/exception/UncheckedServletException.java
+++ tomcat/src/main/java/com/techcourse/exception/UncheckedServletException.java
@@ -5,4 +5,12 @@ public class UncheckedServletException extends RuntimeException {
     public UncheckedServletException(Exception e) {
         super(e);
     }
+
+    public UncheckedServletException(String message) {
+        super(message);
+    }
+
+    public UncheckedServletException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }

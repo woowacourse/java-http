a0161f2ae8e95df1bff0c82e9a51619515d51ca3
재우 <Akaps6768@gmail.com>
2024-09-03 21:44:42 +0900

    feat: GET `index.html` 응답하기

---
+++ docs/requirements.md
@@ -0,0 +1,5 @@
+# 기능 요구 사항
+
+- [x] 1\. GET `index.html` 응답하기
+- [ ] 2\. CSS 지원하기
+- [ ] 3\. Query String 파싱
--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,17 @@
 package org.apache.coyote.http11;
 
-import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
+import com.techcourse.exception.UncheckedServletException;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,8 +34,33 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            // 1. Request
+            final var inputStreamReader = new InputStreamReader(inputStream);
+            final var bufferedReader = new BufferedReader(inputStreamReader);
 
+            // 1.1. Request Line
+            final var requestLine = bufferedReader.readLine();
+            final var requestLineSplit = requestLine.split(" ");
+            final var _method = requestLineSplit[0];
+            final var path = requestLineSplit[1];
+            final var _protocolVersion = requestLineSplit[2];
+
+            final var filename = path.replace("/", "");
+
+            // 1.1.1. Find Static Resource
+            String responseBody;
+            if (!filename.isEmpty()) {
+                final var classLoader = getClass().getClassLoader();
+                final var url = classLoader.getResource("static/" + filename);
+                final var resourcePath = Path.of(url.getPath());
+
+                responseBody = Files.readString(resourcePath);
+
+            } else {
+                responseBody = "Hello world!";
+            }
+
+            // 2. Response
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",

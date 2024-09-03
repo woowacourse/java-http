44661a3ff115669f6e180485ecb9c13d2387c55c
le2sky <leehaneul990623@gmail.com>
2024-09-04 07:58:38 +0900

    feat: index.html 응답 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,8 +5,13 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,20 +33,39 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
-
-            final var responseBody = "Hello world!";
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            final var clientReader = new BufferedReader(new InputStreamReader(inputStream));
+            final var httpRequestHeader = clientReader.readLine();
+            final var response = createHttpResponse(loadStaticFile(httpRequestHeader));
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private byte[] loadStaticFile(String httpRequestHeader) throws IOException {
+        final var startLine = httpRequestHeader.split(" ");
+        if ("/".equals(startLine[1])) {
+            return "Hello world!".getBytes();
+        }
+
+        final var requestResource = startLine[1].split("/");
+        final var resourceName = requestResource[requestResource.length - 1];
+        final var resourcePath = getClass().getClassLoader().getResource("static/" + resourceName).getPath();
+        final var bufferedInputStream = new BufferedInputStream(new FileInputStream(resourcePath));
+        final var responseBody = bufferedInputStream.readAllBytes();
+        bufferedInputStream.close();
+
+        return responseBody;
+    }
+
+    private String createHttpResponse(byte[] responseBody) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.length + " ",
+                "",
+                new String(responseBody, StandardCharsets.UTF_8));
     }
 }

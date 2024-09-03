ffb752f1e367990d456348441492c00a66df918e
haiseong <wjdgotjd9908@gmail.com>
2024-09-03 22:05:21 +0900

    feat: HttpRequest 구현

---
+++ tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequest.java
@@ -0,0 +1,48 @@
+package org.apache.coyote.http11.request;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.coyote.http11.HttpMethod;
+
+public class HttpRequest {
+
+    private final RequestLine requestLine;
+
+    public HttpRequest(InputStream inputStream) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+            requestLine = readRequestLine(bufferedReader);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private RequestLine readRequestLine(BufferedReader reader) throws IOException {
+        String line = reader.readLine();
+        validateLineEmpty(line);
+
+        return new RequestLine(line);
+    }
+
+    private void validateLineEmpty(String line) {
+        if (StringUtils.isEmpty(line)) {
+            throw new IllegalArgumentException("Line is Empty");
+        }
+    }
+
+    public HttpMethod getMethod() {
+        return requestLine.getMethod();
+    }
+
+    public String getRequestURI() {
+        return requestLine.getRequestURI();
+    }
+
+    public String getHttpVersion() {
+        return requestLine.getHttpVersion();
+    }
+}
+
---
+++ tomcat/src/test/java/org/apache/coyote/http11/request/HttpRequestTest.java
@@ -0,0 +1,39 @@
+package org.apache.coyote.http11.request;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.io.ByteArrayInputStream;
+import org.apache.coyote.http11.HttpMethod;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class HttpRequestTest {
+
+    @Test
+    @DisplayName("HttpRequest 을 생성한다.")
+    void createHttpRequest() {
+        String line = "GET /index.html HTTP/1.1";
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(line.getBytes());
+
+        HttpRequest httpRequest = new HttpRequest(byteArrayInputStream);
+
+        assertAll(
+                () -> assertThat(httpRequest.getMethod()).isEqualTo(HttpMethod.GET),
+                () -> assertThat(httpRequest.getRequestURI()).isEqualTo("/index.html"),
+                () -> assertThat(httpRequest.getHttpVersion()).isEqualTo("HTTP/1.1")
+        );
+    }
+
+    @Test
+    @DisplayName("HttpRequest 을 생성할 때 첫줄에 빈 라인이 들어오면 예외를 던진다.")
+    void createHttpRequestWithEmptyLine() {
+        String line = "";
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(line.getBytes());
+
+        assertThatThrownBy(() -> new HttpRequest(byteArrayInputStream))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Line is Empty");
+    }
+}

22a8a57d21a6499c219c542be46ad734bf6aee3b
kunsanglee <gosmdochee2@gmail.com>
2024-09-03 19:12:25 +0900

    feat: 학습 테스트 작성

--- study/src/test/java/study/FileTest.java
+++ study/src/test/java/study/FileTest.java
@@ -1,13 +1,16 @@
 package study;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.nio.file.Path;
-import java.util.Collections;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * 웹서버는 사용자가 요청한 html 파일을 제공 할 수 있어야 한다.
@@ -28,9 +31,19 @@ class FileTest {
         final String fileName = "nextstep.txt";
 
         // todo
-        final String actual = "";
+        final String actual = getClass().getClassLoader().getResource(fileName).getPath();
 
         assertThat(actual).endsWith(fileName);
+    }
+
+    @Test
+    void resource_디렉터리에_있는_파일의_경로를_찾는다2() throws IOException {
+        final String fileName = "nextstep.txt";
+
+        Path path = Paths.get("src", "test", "resources", fileName).toAbsolutePath();
+        List<String> actual = Files.readAllLines(path);
+
+        assertThat(actual).containsOnly("nextstep");
     }
 
     /**
@@ -40,14 +53,14 @@ class FileTest {
      * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws IOException {
         final String fileName = "nextstep.txt";
 
         // todo
-        final Path path = null;
+        final Path path = Path.of(getClass().getClassLoader().getResource(fileName).getPath());
 
         // todo
-        final List<String> actual = Collections.emptyList();
+        final List<String> actual = Files.readAllLines(path);
 
         assertThat(actual).containsOnly("nextstep");
     }

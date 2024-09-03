695ffa3b6df754ceda6abc0059d99ead25397ac9
김민수 MINSOO KIM <alstn113@gmail.com>
2024-09-03 15:23:35 +0900

    chore: 안쓰는 import 제거

--- study/src/test/java/study/FileTest.java
+++ study/src/test/java/study/FileTest.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;

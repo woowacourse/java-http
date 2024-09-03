01a069f87cb6c72dbda64c760eec91bf80f88a8e
Gyeongho Yang <chchch1213@naver.com>
2024-09-03 19:07:28 +0900

    feat: add client request class

---
+++ tomcat/src/main/java/org/apache/coyote/http11/ClientServletRequest.java
@@ -0,0 +1,34 @@
+package org.apache.coyote.http11;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class ClientServletRequest {
+
+    private final String method;
+    private final String path;
+    private final String protocolVersion;
+
+    public ClientServletRequest(InputStream inputStream) throws IOException {
+        List<String> lines = InputStreamConvertor.convertToLines(inputStream, StandardCharsets.UTF_8);
+
+        String[] startLineParts = lines.getFirst().split(" ");
+        method = startLineParts[0];
+        path = startLineParts[1];
+        protocolVersion = startLineParts[2];
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getProtocolVersion() {
+        return protocolVersion;
+    }
+}

b7b0e30c5dc1c2ab6b3e16800433f2aff47cc28e
yoonjuho <dbswn990@naver.com>
2024-09-04 18:50:24 +0900

    feat: CSS 지원하기

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -8,8 +8,10 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.StringTokenizer;
 
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
@@ -41,12 +43,16 @@ public class Http11Processor implements Runnable, Processor {
             Map<RequestLineElement, String> requestLineElements = extractRequestLine(bufferedReader);
             final URL resource = getClass().getClassLoader().getResource(
                     "static" + requestLineElements.get(RequestLineElement.REQUEST_URI));
+
+            Map<String, String> headers = parseHeaders(bufferedReader);
+            String contentType = convertContentTypeByAccept(headers.get("Accept"));
+
             if (isResourceNotExists(resource)) {
-                write(requestLineElements.get(RequestLineElement.REQUEST_URI), outputStream);
+                write(requestLineElements.get(RequestLineElement.REQUEST_URI), contentType, outputStream);
                 return;
             }
             final String content = new String(Files.readAllBytes(new File((resource).getFile()).toPath()));
-            write(content, outputStream);
+            write(content, contentType, outputStream);
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
@@ -56,10 +62,10 @@ public class Http11Processor implements Runnable, Processor {
         return Objects.isNull(resource);
     }
 
-    private static void write(String content, OutputStream outputStream) throws IOException {
+    private static void write(String content, String contentType, OutputStream outputStream) throws IOException {
         final var response = String.join("\r\n",
                 "HTTP/1.1 200 OK ",
-                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Type: " + contentType + ";charset=utf-8 ",
                 "Content-Length: " + content.getBytes().length + " ",
                 "",
                 content);
@@ -81,5 +87,25 @@ public class Http11Processor implements Runnable, Processor {
             return "/index.html";
         }
         return uri;
+    }
+
+    private static Map<String, String> parseHeaders(BufferedReader bufferedReader) throws IOException {
+        Map<String, String> headerMap = new HashMap<>();
+        String line = bufferedReader.readLine();
+        while (!line.isEmpty()) {
+            StringTokenizer tokenizer = new StringTokenizer(line, ":");
+            String key = tokenizer.nextToken().trim();
+            String value = tokenizer.nextToken("").trim();
+            headerMap.put(key, value);
+            line = bufferedReader.readLine();
+        }
+        return headerMap;
+    }
+
+    private static String convertContentTypeByAccept(String accept) {
+        if (!Objects.isNull(accept) && accept.contains("text/css")) {
+            return "text/css";
+        }
+        return "text/html";
     }
 }

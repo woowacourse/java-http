b1d4b701355f5281dd229cd622fd3377b3e17747
hjk0761 <hjk0761@naver.com>
2024-09-04 13:32:56 +0900

    feat: index.html 응답 구현

--- tomcat/README.md
+++ tomcat/README.md
@@ -1,6 +1,6 @@
 # 1단계 - HTTP 서버 구현하기
 
-- [ ] GET /index.html 응답하기
-  - [ ] Http11Processor 의 InputStream 사용하여 요청 처리
+- [x] GET /index.html 응답하기
+  - [x] Http11Processor 의 InputStream 사용하여 요청 처리
 - [ ] CSS 파일 요청 처리
 - [ ] 로그인 요청에 대해 Query String 파싱하여 login.html 응답
--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,8 +5,12 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,19 +33,47 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            final var response = responseBuilder(bufferedReader);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String responseBuilder(final BufferedReader bufferedReader) throws IOException {
+        final String BASIC_RESPONSE_BODY = "Hello world!";
+        final String BASIC_RESPONSE = String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + BASIC_RESPONSE_BODY.getBytes().length + " ",
+                "",
+                BASIC_RESPONSE_BODY);
+
+        List<String> request = new ArrayList<>();
+
+        while (bufferedReader.ready()) {
+            String line = bufferedReader.readLine();
+            request.add(line.concat("\r\n"));
+        }
+
+        List<String> requestUri = List.of(request.getFirst().split(" "));
+
+        if (requestUri.get(1).equals("/index.html")) {
+            final URL resource = getClass().getClassLoader().getResource("static/index.html");
+            final String responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+
+            return String.join("\r\n",
+                    "HTTP/1.1 200 OK ",
+                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "", responseBody
+                    );
+        }
+
+        return BASIC_RESPONSE;
     }
 }

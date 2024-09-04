7b81c9c2da19782b25740c438c5f4b2f3d24569f
tkdgur0906 <tkdgur0906@naver.com>
2024-09-04 10:54:31 +0900

    feat: /index.html 응답하는 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -4,9 +4,14 @@ import com.techcourse.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -27,9 +32,30 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final var bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
 
-            final var responseBody = "Hello world!";
+            String firstLine = bufferedReader.readLine();
+
+            if (firstLine == null) {
+                return;
+            }
+
+            String page = getPage(firstLine);
+            String responseBody = "";
+
+            if (page.equals("/")) {
+                responseBody = "Hello world!";
+            } else {
+                URL url = getClass().getClassLoader().getResource("static" + page);
+
+                if (url == null) {
+                    return;
+                }
+
+                Path path = Path.of(url.toURI());
+                responseBody = new String(Files.readAllBytes(path));
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -42,6 +68,12 @@ public class Http11Processor implements Runnable, Processor {
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    private String getPage(String firstLine) {
+        return firstLine.split(" ")[1];
     }
 }

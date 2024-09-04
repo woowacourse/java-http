b0b08ff38a0881fbeed76cfa21c5c7cad4cd096b
JUHA <84626225+khabh@users.noreply.github.com>
2024-09-04 22:19:09 +0900

    refactor: HttpRequest 객체에 헤더와 본문 필드 추가

--- tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequest.java
@@ -1,28 +1,67 @@
 package org.apache.coyote.http11.request;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class HttpRequest {
 
     private final RequestLine requestLine;
+    private final HttpHeaders headers;
+    private final String body;
 
-    public HttpRequest(RequestLine requestLine) {
+    public HttpRequest(RequestLine requestLine, HttpHeaders headers, String body) {
         this.requestLine = requestLine;
+        this.headers = headers;
+        this.body = body;
     }
 
-    public static HttpRequest of(List<String> requestLines) {
-        validateRequest(requestLines);
-        String firstLine = requestLines.getFirst();
-        RequestLine requestLine = RequestLine.of(firstLine);
+    public static HttpRequest of(BufferedReader requestReader) throws IOException {
+        List<String> requestHead = readRequestHead(requestReader);
+        validateRequestHead(requestHead);
+        RequestLine requestLine = getRequestLine(requestHead);
+        HttpHeaders headers = getHeaders(requestHead);
 
-        return new HttpRequest(requestLine);
+        int contentLength = headers.getAsInt("Content-Length").orElse(0);
+        String body = readBody(contentLength, requestReader);
+
+        return new HttpRequest(requestLine, headers, body);
     }
 
-    private static void validateRequest(List<String> requestLines) {
+    private static List<String> readRequestHead(BufferedReader requestReader) throws IOException {
+        List<String> requestHead = new ArrayList<>();
+        String line;
+        while ((line = requestReader.readLine()) != null && !line.isEmpty()) {
+            requestHead.add(line);
+        }
+        return requestHead;
+    }
+
+    private static void validateRequestHead(List<String> requestLines) {
         System.out.println("requestLines = " + requestLines);
         if (requestLines.isEmpty()) {
             throw new IllegalArgumentException("올바르지 않은 HTTP 요청 형식입니다.");
         }
+    }
+
+    private static RequestLine getRequestLine(List<String> requestHead) {
+        String firstLine = requestHead.getFirst();
+        return RequestLine.of(firstLine);
+    }
+
+    private static HttpHeaders getHeaders(List<String> requestHead) {
+        List<String> headers = new ArrayList<>(requestHead.subList(1, requestHead.size()));
+        return HttpHeaders.of(headers);
+    }
+
+    private static String readBody(int contentLength, BufferedReader requestReader) throws IOException {
+        if (contentLength <= 0) {
+            return "";
+        }
+        char[] body = new char[contentLength];
+        requestReader.read(body, 0, contentLength);
+        return new String(body);
     }
 
     public String getPath() {
@@ -35,5 +74,9 @@ public class HttpRequest {
 
     public boolean isQueriesEmpty() {
         return getQueries().isEmpty();
+    }
+
+    public HttpMethod getMethod() {
+        return requestLine.getMethod();
     }
 }

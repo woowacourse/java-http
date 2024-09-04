fe4981a6ec5c11664798e7b8665f8b8db5a928de
김민겸 <mingyum119@naver.com>
2024-09-04 22:12:25 +0900

    feat: `/index.html`에 접근했을 때 static/index.html 파일이 보여지도록 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,8 +5,15 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,6 +36,13 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String urlPath = reader.readLine().split(" ")[1];
+            if(urlPath.equals("/index.html")) {
+                printIndexHtmlFile(outputStream);
+                return;
+            }
+
             final var responseBody = "Hello world!";
 
             final var response = String.join("\r\n",
@@ -37,6 +51,28 @@ public class Http11Processor implements Runnable, Processor {
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
                     responseBody);
+
+            outputStream.write(response.getBytes());
+            outputStream.flush();
+        } catch (IOException | UncheckedServletException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    private void printIndexHtmlFile(OutputStream outputStream) {
+        final String fileName = "static/index.html";
+        final URL url = getClass().getClassLoader().getResource(fileName);
+        final File file = new File(url.getPath());
+        final Path path = file.toPath();
+
+        try {
+            final var responseBody = String.join("\n", Files.readAllLines(path));
+            final var response = String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.length() + " ",
+                "",
+                responseBody);
 
             outputStream.write(response.getBytes());
             outputStream.flush();

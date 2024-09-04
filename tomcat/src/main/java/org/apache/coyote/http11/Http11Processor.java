670827ab1a94b0801994582cca3e56f3773c2193
mzeong <mjeong@sookmyung.ac.kr>
2024-09-04 13:32:54 +0900

    feat: 인덱스 페이지 응답

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,19 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,19 +36,70 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            // inputstream read
+            byte[] bytes = new byte[18000];
+            int readByteCount = inputStream.read(bytes);
+            String data = new String(bytes, 0, readByteCount, StandardCharsets.UTF_8);
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            String referer = extractReferer(data);
 
-            outputStream.write(response.getBytes());
-            outputStream.flush();
+            // default page
+            if (referer.equals("/")) {
+                final var responseBody = "Hello world!";
+
+                final var response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody);
+
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+                return;
+            }
+
+            // index.html file read
+            String fileName = referer.substring(1);
+
+            System.out.println(fileName);
+            URL resource = getClass().getResource("/static/" + fileName);
+
+            Path path = null;
+            try {
+                path = Path.of(resource.toURI());
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+
+            try (BufferedReader bufferedReader = Files.newBufferedReader(path)) {
+                List<String> actual = bufferedReader.lines().toList();
+
+                String collect = actual.stream()
+                        .collect(Collectors.joining("\n")) + "\n";
+
+                final var response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + collect.getBytes().length + " ",
+                        "",
+                        collect);
+
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+            } catch (Exception e) {
+            }
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private static String extractReferer(String httpRequest) {
+        String firstLine = httpRequest.split("\n")[0];
+        String[] split = firstLine.split(" ");
+        if (split[0].equals("GET")) {
+            return split[1];
+        }
+        throw new IllegalArgumentException("GET 요청만 처리 가능..");
     }
 }

a3b0989a6e6e48ab656bc360b52de297bb1d9bc8
unifolio0 <boho82ha@gmail.com>
2024-09-04 17:24:43 +0900

    feat: 1단계 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,18 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,20 +34,89 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
+            var bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = bufferedReader.readLine();
+            System.out.println(line);
+            if (line == null || line.isEmpty()) {
+                return;
+            }
 
-            final var responseBody = "Hello world!";
+            String[] requestLine = line.split(" ");
+            if (requestLine.length < 3) {
+                return;
+            }
+            String uri = requestLine[1];
+            String path = uri;
+            String queryString = "";
+            if (uri.contains("?")) {
+                int index = uri.indexOf("?");
+                path = uri.substring(0, index);
+                queryString = uri.substring(index + 1);
+            }
+
+            var responseBody = "";
+            var contentType = "text/html;charset=utf-8 \r\n";
+            if (!path.equals("/") && !path.contains(".")) {
+                path += ".html";
+            }
+
+            if (path.equals("/")) {
+                responseBody = "Hello world!";
+            } else {
+                String fileName = "static" + path;
+                var resourceUrl = getClass().getClassLoader().getResource(fileName);
+                if (resourceUrl == null) {
+                    final var response = String.join("\r\n",
+                            "HTTP/1.1 404 Not Found",
+                            "Content-Type: text/html;charset=utf-8 \r\n",
+                            "",
+                            "<h1>404 Not Found</h1>");
+                    outputStream.write(response.getBytes());
+                    outputStream.flush();
+                    return;
+                }
+                Path filePath = Path.of(resourceUrl.toURI());
+                responseBody = new String(Files.readAllBytes(filePath));
+            }
+
+            if (path.endsWith(".css")) {
+                contentType = "text/css;charset=utf-8 \r\n";
+            } else if (path.endsWith(".js")) {
+                contentType = "application/javascript;charset=utf-8 \r\n";
+            } else if (path.endsWith(".html")) {
+                contentType = "text/html;charset=utf-8 \r\n";
+            } else if (path.endsWith(".png")) {
+                contentType = "image/png \r\n";
+            } else if (path.endsWith(".jpg") || path.endsWith(".jpeg")) {
+                contentType = "image/jpeg \r\n";
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Type: " + contentType +
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
                     responseBody);
+            if (!queryString.isEmpty()) {
+                String[] query = queryString.split("&");
+                String account = query[0].split("=")[1];
+                String password = query[1].split("=")[1];
+                System.out.println(account + " " + password);
+                User user = InMemoryUserRepository.findByAccount(account)
+                        .orElseThrow();
+                if (user.checkPassword(password)) {
+                    log.info(user.toString());
+                } else {
+                    log.error("비밀번호 불일치");
+                }
+            }
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
         }
     }
 }

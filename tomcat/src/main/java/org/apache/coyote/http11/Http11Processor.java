c6a454dc6834f1d73f4a4342b9eb366b8b3920e5
cutehumanS2 <oddpinkjadeite@gmail.com>
2024-09-04 13:55:51 +0900

    feat: GET /index.html 응답하기

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,17 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,8 +34,18 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String input = bufferedReader.readLine();
+            if (input == null) {
+                return;
+            }
+            String requestUri = input.split(" ")[1];
+            String resourceName = "static" + requestUri;
+            URL resource = getClass().getClassLoader().getResource(resourceName);
+            Path path = new File(resource.getPath()).toPath();
+            byte[] bytes = Files.readAllBytes(path);
 
+            final var responseBody = new String(bytes);
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",

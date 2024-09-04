9d5dc56ac3ef3ba1fe38a0a494013c7c2730b011
zangsu <zangsu_@naver.com>
2024-09-04 15:32:43 +0900

    feat: Method 클래스 생성

---
+++ tomcat/src/main/java/org/apache/coyote/http11/request/Method.java
@@ -0,0 +1,14 @@
+package org.apache.coyote.http11.request;
+
+import java.util.Arrays;
+
+public enum Method {
+    GET, POST, PUT, PATCH, DELETE;
+
+    public Method findByName(String name) {
+        return Arrays.stream(values())
+                .filter(method -> method.name().equals(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 Http 메서드 입니다."));
+    }
+}

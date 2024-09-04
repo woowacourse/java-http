88ff1345d5c832dd2161c75a374fa02ddd56f321
KIMGYUTAE <kiimwf0819@gmail.com>
2024-09-04 16:52:56 +0900

    refactor: 요청에 따라 자원을 반환하도록 변경

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,16 +1,22 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
-
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final String LINE_SEPARATOR = System.lineSeparator();
 
     private final Socket connection;
 
@@ -27,12 +33,44 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final StringBuilder requestHeader = new StringBuilder();
+            while (true) {
+                String readLine = bufferedReader.readLine();
+                if (readLine == null || readLine.isEmpty()) {
+                    break;
+                }
+                log.info("request line : {}", readLine);
+                requestHeader.append(readLine).append(LINE_SEPARATOR);
+            }
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
+            String[] requestLines = requestHeader.toString().split(LINE_SEPARATOR);
+            String[] requestLineParts = requestLines[0].split(" ");
+            String requestUri = requestLineParts[1];
+            log.info("request URI: {}", requestUri);
+
+            String statusLine = "HTTP/1.1 200 OK ";
+            String responseBody;
+
+            if (requestUri.equals("/")) {
+                responseBody = "Hello world!";
+
+            } else {
+                URL resource = getClass().getClassLoader().getResource("static" + requestUri);
+
+                if (resource != null) {
+                    log.info("response resource : {}", resource.toURI());
+                    responseBody = new String(Files.readAllBytes(Path.of(resource.toURI())));
+                } else {
+                    statusLine = "HTTP/1.1 404 Not Found ";
+                    responseBody = new String(Files.readAllBytes(Path.of("./static/404.html")));
+                }
+            }
+
+            final var response = String.join(LINE_SEPARATOR,
+                    statusLine,
                     "Content-Type: text/html;charset=utf-8 ",
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
@@ -40,7 +78,7 @@ public class Http11Processor implements Runnable, Processor {
 
             outputStream.write(response.getBytes());
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
         }
     }

5af60cec40c54300da678d524b34a5304b2013ab
eunjungL <dldmswjd322@gmail.com>
2024-09-04 17:10:11 +0900

    feat: 로그인 성공 실패 여부에 따른 응답 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -2,6 +2,7 @@ package org.apache.coyote.http11;
 
 import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,6 +17,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
@@ -48,11 +50,12 @@ public class Http11Processor implements Runnable, Processor {
                     .split(" ")[1];
             Map<String, String> queryParams = getQueryParams(requestUrl);
 
+            String response = getResponse(requestUrl, "200 OK");
             if (requestUrl.startsWith("/login") && !queryParams.isEmpty()) {
-                checkLoginUser(queryParams);
+                response = login(queryParams);
             }
 
-            outputStream.write(getResponse(requestUrl).getBytes());
+            outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
@@ -82,23 +85,24 @@ public class Http11Processor implements Runnable, Processor {
         return queryParams;
     }
 
-    private void checkLoginUser(Map<String, String> queryParams) {
+    private String login(Map<String, String> queryParams) throws IOException {
         String account = queryParams.get("account");
         String password = queryParams.get("password");
 
-        InMemoryUserRepository.findByAccount(account)
-                .ifPresent(user -> {
-                    if (user.checkPassword(password)) {
-                        log.info("user : {}", user);
-                    }
-                });
+        Optional<User> user = InMemoryUserRepository.findByAccount(account);
+
+        if (user.isEmpty() || !user.get().checkPassword(password)) {
+            return getResponse("/401.html", "401 Unauthorized");
+        }
+
+        return getResponse("/index.html", "302 Redirected");
     }
 
-    private String getResponse(String requestUrl) throws IOException {
+    private String getResponse(String requestUrl, String responseCode) throws IOException {
         String responseBody = getResponseBody(requestUrl);
 
         return String.join("\r\n",
-                "HTTP/1.1 200 OK ",
+                "HTTP/1.1 " + responseCode + " ",
                 "Content-Type: " + ContentType.findContentType(requestUrl) + ";charset=utf-8 ",
                 "Content-Length: " + responseBody.getBytes().length + " ",
                 "",

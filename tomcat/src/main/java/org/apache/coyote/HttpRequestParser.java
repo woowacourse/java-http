fe4966a8c8c249e42dbdbc9764b0f5c0788b8633
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-09-04 21:41:37 +0900

    feat(RequestHandler): login 엔드포인트 접속 시 로직 처리

--- tomcat/src/main/java/org/apache/coyote/HttpRequestParser.java
+++ tomcat/src/main/java/org/apache/coyote/HttpRequestParser.java
@@ -10,18 +10,19 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
 
 public class HttpRequestParser {
-    public static HttpRequest parseRequest(InputStream inputStream) throws IOException {
+    public static HttpRequest parseRequest(final InputStream inputStream) throws IOException {
+        // TODO: 다른 메서드일 때 처리
         return parseGetRequest(inputStream);
     }
 
-    public static HttpRequest parseGetRequest(InputStream inputStream) throws IOException {
+    public static HttpRequest parseGetRequest(final InputStream inputStream) throws IOException {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
         final String request = bufferedReader.readLine();
 
         final var requestStartLine = request.split(" ");
         return HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8080" + requestStartLine[0]))
-                .method(requestStartLine[1], BodyPublishers.noBody())
+                .method(requestStartLine[0], BodyPublishers.noBody())
+                .uri(URI.create("http://localhost:8080" + requestStartLine[1]))
                 .version(getHttpVersion(requestStartLine[2]))
                 .build();
     }
--- tomcat/src/main/java/org/apache/coyote/RequestHandler.java
+++ tomcat/src/main/java/org/apache/coyote/RequestHandler.java
@@ -5,30 +5,44 @@ import java.net.URL;
 import java.net.http.HttpRequest;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Optional;
 import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.techcourse.db.InMemoryUserRepository;
+import com.techcourse.model.User;
+
 public class RequestHandler {
-    private static final Set<String> STATIC_RESOURCE_EXTENSIONS = Set.of("css", "js", "ico");
+    private static final Set<String> STATIC_RESOURCE_EXTENSIONS = Set.of("css", "js");
     private static final String STATIC_RESOURCE_ROOT_PATH = "static/";
     private static final String PATH_DELIMITER = "/";
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
+    // mapping handler method
     public String handle(final HttpRequest httpRequest) throws IOException {
         final String endPoint = httpRequest.uri().getPath();
         final String[] paths = endPoint.split(PATH_DELIMITER);
 
         if (paths.length == 0) {
-            return processRootResponse();
+            return handleRoot();
         }
 
         final String resourceName = paths[paths.length - 1];
         if (resourceName.contains(".")) {
-            return processStaticResponse(resourceName);
+            return handleSimpleResource(resourceName);
         }
 
-        return processRootResponse();
+        return handleURL(httpRequest);
     }
 
-    private String processStaticResponse(final String resourceName) throws IOException {
+    private String handleRoot() {
+        final String responseBody = "Hello world!";
+        return getResponse("text/html", responseBody);
+    }
+
+    private String handleSimpleResource(final String resourceName) throws IOException {
         final URL resourceURL = getClass().getClassLoader().getResource(findResourcePath(resourceName));
         final Path resourcePath = Path.of(resourceURL.getPath());
         final String responseBody = Files.readString(resourcePath);
@@ -37,15 +51,40 @@ public class RequestHandler {
         return getResponse(mimeType, responseBody);
     }
 
-    private String processRootResponse() {
-        final String responseBody = "Hello world!";
-        return getResponse("text/html", responseBody);
+    // TOOD: change naming
+    private String handleURL(final HttpRequest httpRequest) throws IOException {
+        String uri = httpRequest.uri().getPath();
+        if (uri.contains("login")) {
+            return handleLogin(httpRequest);
+        }
+
+        throw new IllegalCallerException("유효하지 않은 기능입니다.");
+    }
+
+    private String handleLogin(final HttpRequest httpRequest) throws IOException {
+        String query = httpRequest.uri().getQuery();
+        String[] params = query.split("&");
+
+        String account = params[0].split("=")[1];
+        String password = params[1].split("=")[1];
+
+        Optional<User> userOptional = InMemoryUserRepository.findByAccount(account);
+        if (userOptional.isEmpty()) {
+            return handleSimpleResource("login.html");
+        }
+
+        User user = userOptional.get();
+        if (user.checkPassword(password)) {
+            log.info(user.toString());
+        }
+
+        return handleSimpleResource("401.html");
     }
 
     private String getResponse(String mimeType, String responseBody) {
         return String.join("\r\n",
                 "HTTP/1.1 200 OK ",
-                "Content-Type: " + mimeType +";charset=utf-8 ",
+                "Content-Type: " + mimeType + ";charset=utf-8 ",
                 "Content-Length: " + responseBody.getBytes().length + " ",
                 "",
                 responseBody);

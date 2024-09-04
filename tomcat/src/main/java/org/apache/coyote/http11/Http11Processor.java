c65471790a1bc368158784d5246551d715c3846f
hangillee <skfcb10@naver.com>
2024-09-04 15:19:53 +0900

    feat: 1단계 html 및 css 응답 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,15 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,19 +32,65 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+            final var requestLine = br.readLine();
+            log.info("request line: {}", requestLine);
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            if (requestLine == null) {
+                return;
+            }
+
+            final var requestLineTokens = requestLine.split(" ");
+
+            var response = buildHttpResponse("text/html", "Hello world!");
+            if (requestLineTokens[1].equals("/index.html")) {
+                response = buildHtmlResponse(requestLineTokens[1]);
+            }
+            if (requestLineTokens[1].equals("/css/styles.css")) {
+                response = buildStyleSheetResponse(requestLineTokens[1]);
+            }
+            if (requestLineTokens[1].equals("/js/scripts.js")) {
+                response = buildScriptResponse(requestLineTokens[1]);
+            }
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String buildHttpResponse(final String fileType, final String responseBody) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: " + fileType + ";charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+    }
+
+    private String buildResponseBodyFromStaticFile(String fileName) throws IOException {
+        final var resourceName = "static" + fileName;
+        final var path = Path.of(this.getClass().getClassLoader().getResource(resourceName).getPath());
+
+        return String.join("\n", Files.readAllLines(path)) + "\n";
+    }
+
+    private String buildHtmlResponse(final String fileName) throws IOException {
+        final var responseBody = buildResponseBodyFromStaticFile(fileName);
+
+        return buildHttpResponse("text/html", responseBody);
+    }
+
+    private String buildStyleSheetResponse(final String fileName) throws IOException {
+        final var responseBody = buildResponseBodyFromStaticFile(fileName);
+
+        return buildHttpResponse("text/css", responseBody);
+    }
+
+    private String buildScriptResponse(final String fileName) throws IOException {
+        final var responseBody = buildResponseBodyFromStaticFile(fileName);
+
+        return buildHttpResponse("text/javascript", responseBody);
     }
 }

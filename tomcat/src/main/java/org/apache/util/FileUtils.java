8517c2d6af583438cf620838244833745d02c415
libienz <leekh10195@gmail.com>
2024-09-04 16:40:26 +0900

    feat: 파일 확장자 얻어오는 util 기능 구현

---
+++ tomcat/src/main/java/org/apache/util/FileUtils.java
@@ -0,0 +1,16 @@
+package org.apache.util;
+
+public class FileUtils {
+
+    private FileUtils() {
+    }
+
+    public static String getFileExtension(String fileName) {
+        int lastDotIndex = fileName.lastIndexOf('.');
+        if (lastDotIndex == -1 || lastDotIndex == fileName.length() - 1) {
+            return "";
+        }
+
+        return fileName.substring(lastDotIndex + 1);
+    }
+}

5ed20907b036c5dba7e7a829084b5c2d4848fbc2
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-09-04 18:38:36 +0900

    fix: 운영체제 줄바꿈 형식 문제로 테스트 수정

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -50,11 +50,12 @@ class Http11ProcessorTest {
 
         // then
         final URL resource = getClass().getClassLoader().getResource("static/index.html");
+        byte[] bytes = Files.readAllBytes(new File(resource.getFile()).toPath());
         var expected = "HTTP/1.1 200 OK \r\n" +
                 "Content-Type: text/html;charset=utf-8 \r\n" +
-                "Content-Length: 5564 \r\n" +
+                "Content-Length: "+ bytes.length +" \r\n" +
                 "\r\n"+
-                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+                new String(bytes);
 
         assertThat(socket.output()).isEqualTo(expected);
     }

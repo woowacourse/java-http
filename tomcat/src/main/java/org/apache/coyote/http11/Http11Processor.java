faf19715b380a217ae6723a11fdafb5bfb6f37e2
3juhwan <13selfesteem91@naver.com>
2024-09-04 19:54:53 +0900

    feat: 요청과 응답 객체를 프로세서에 적용

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,15 +33,12 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream();
              final var requestReader = new BufferedReader(new InputStreamReader(inputStream))) {
-            String startLine = requestReader.readLine();
-            final var responseBody = parseStartLine(startLine);
 
-            final var response = String.join("\r\n",
-                                             "HTTP/1.1 200 OK ",
-                                             "Content-Type: text/html;charset=utf-8 ",
-                                             "Content-Length: " + responseBody.getBytes().length + " ",
-                                             "",
-                                             responseBody);
+            final var request = parseRequest(requestReader);
+            log.info("request: {}", request);
+            final var responseBody = getStaticResource(request);
+            final var response = makeResponse(responseBody);
+            log.info("response: {}", response);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
@@ -49,12 +47,26 @@ public class Http11Processor implements Runnable, Processor {
         }
     }
 
-    private String parseStartLine(String startLine) throws IOException {
-        String resourse = startLine.split(" ")[1];
-        if (resourse.equals("/")) {
+    private Request parseRequest(BufferedReader reader) throws IOException {
+        String startLine = reader.readLine();
+        String[] token = startLine.split(" ");
+        String[] headers = reader.lines().takeWhile(line -> !line.isEmpty()).toArray(String[]::new);
+        return new Request(token[0], token[1], token[2], headers, null);
+    }
+
+    private String getStaticResource(Request request) throws IOException {
+        String uri = request.getUri();
+        if (uri.equals("/")) {
             return "Hello world!";
         }
-        File file = new File(getClass().getClassLoader().getResource("static" + resourse).getFile());
+        File file = new File(getClass().getClassLoader().getResource("static" + uri).getFile());
         return new String(Files.readAllBytes(file.toPath()));
+    }
+
+    private Response makeResponse(String responseBody) {
+        return new Response("200 OK",
+                            Map.of("Content-Type", "text/html;charset=utf-8 ",
+                                   "Content-Length", responseBody.getBytes().length + " "),
+                            responseBody);
     }
 }

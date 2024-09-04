8e8435d6e027c8657f97229f76982c56e388a5bc
ksk0605 <sungkyum1@naver.com>
2024-09-05 01:46:52 +0900

    feat: index.html 반환 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,8 +5,15 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.StringTokenizer;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,14 +36,44 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = reader.readLine();
+
+            // TODO : 한 줄이 아닌 전체로 읽도록 수정
+//            while ((line = reader.readLine()) != null) {
+//                System.out.println(line);  // 라인 단위로 출력
+//            }
+
+            if (reader.readLine() == null) {
+                throw new IOException("http request is empty");
+            }
+
+            StringTokenizer tokenizer = new StringTokenizer(line);
+            String method = tokenizer.nextToken();
+            String pattern = tokenizer.nextToken();
+            String httpVersion = tokenizer.nextToken();
+
+            if (!httpVersion.equals("HTTP/1.1")) {
+                throw new IOException("not http1.1 request");
+            }
+
+            var responseBody = "Hello world!";
+
+            if (method.equals("GET") && !pattern.equals("/")) {
+                final URL resource = getClass().getClassLoader().getResource("static" + pattern);
+                responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()), StandardCharsets.UTF_8);
+            }
+
+            String fileContent = responseBody.replaceAll("\r\n", "\n");
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "Content-Length: " + fileContent.getBytes(StandardCharsets.UTF_8).length + " ",
                     "",
                     responseBody);
+
+
 
             outputStream.write(response.getBytes());
             outputStream.flush();

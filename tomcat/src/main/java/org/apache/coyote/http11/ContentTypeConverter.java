111a0350ca09fc96c3ba5d05ea03a0c04eafb2d2
nayonsoso <panda0329@naver.com>
2024-09-04 21:21:21 +0900

    feat: 파일확장자->contentType 변환 클래스 생성

---
+++ tomcat/src/main/java/org/apache/coyote/http11/ContentTypeConverter.java
@@ -0,0 +1,20 @@
+package org.apache.coyote.http11;
+
+import java.util.Map;
+
+class ContentTypeConverter {
+
+    private static final Map<String, String> MAP = Map.of(
+            "html", "text/html",
+            "css", "text/css",
+            "js", "application/javascript"
+    );
+
+    String mapToContentType(String fileExtension) {
+        if (MAP.get(fileExtension) == null) {
+            return "text/html";
+        }
+
+        return MAP.get(fileExtension);
+    }
+}
--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -18,8 +18,6 @@ public class Http11Processor implements Runnable, Processor {
     private static final String GET_METHOD = "GET";
     private static final String DEFAULT_ROUTE = "/";
     private static final String DEFAULT_RESPONSE_BODY = "Hello world!";
-    private static final String HOME_PAGE_ROUTE = "/index.html";
-    private static final String HOME_PAGE_RESPONSE_BODY = "static/index.html";
 
     private final Socket connection;
 
@@ -40,9 +38,11 @@ public class Http11Processor implements Runnable, Processor {
             final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
             final BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-            String requestFirstLine = bufferedReader.readLine();
-            final var response = bindResponse(requestFirstLine);
-            outputStream.write(response.getBytes());
+            final String requestFirstLine = bufferedReader.readLine();
+            if (GET_METHOD.equals(requestFirstLine.split(" ")[0])) {
+                final var response = bindResponse(requestFirstLine);
+                outputStream.write(response.getBytes());
+            }
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
@@ -50,24 +50,20 @@ public class Http11Processor implements Runnable, Processor {
     }
 
     private String bindResponse(String requestFirstLine) throws IOException {
-        String httpMethod = requestFirstLine.split(" ")[0];
         String route = requestFirstLine.split(" ")[1];
-
-        if (DEFAULT_ROUTE.equals(route) && GET_METHOD.equals(httpMethod)) {
+        if (DEFAULT_ROUTE.equals(route)) {
             return buildSuccessfulResponse(DEFAULT_RESPONSE_BODY);
         }
 
-        if (HOME_PAGE_ROUTE.equals(route) && GET_METHOD.equals(httpMethod)) {
-            URL resource = getClass().getClassLoader().getResource(HOME_PAGE_RESPONSE_BODY);
-            if (resource == null) {
-                throw new InternalError("기본으로 보여줄 파일을 찾을 수 없습니다.");
-            }
-            String body = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
-            return buildSuccessfulResponse(body);
+        URL resource = getClass().getClassLoader().getResource("static" + route);
+        if (resource == null) {
+            URL badRequestURL = getClass().getClassLoader().getResource("static/404.html");
+            return buildFailedResponse(404, "NotFound",
+                    new String(Files.readAllBytes(new File(badRequestURL.getFile()).toPath())));
         }
 
-        return buildFailedResponse(404, "NotFound",
-                "There is no static resource matched with rout and method");
+        String body = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+        return buildSuccessfulResponse(body);
     }
 
     private String buildSuccessfulResponse(String responseBody) {

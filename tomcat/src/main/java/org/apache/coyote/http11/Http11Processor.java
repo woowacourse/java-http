0753312a5ab63b1d5355d8bb216ee18ff744dbb0
jinwoo22 <jenny010925@naver.com>
2024-09-04 16:18:25 +0900

    feat: 로그인 성공, 실패에 따른 페이지 반환

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -16,6 +16,7 @@ import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Optional;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -44,6 +45,8 @@ public class Http11Processor implements Runnable, Processor {
             String line = br.readLine();
             String mimeType = "";
             String responseBody = "";
+            String response = "";
+
             if (line == null) {
                 return;
             }
@@ -52,6 +55,7 @@ public class Http11Processor implements Runnable, Processor {
             if (lines[1].equals("/")) {
                 mimeType = "text/html";
                 responseBody = "Hello world!";
+                response = getOKResponse(mimeType, responseBody);
             } else if (lines[1].startsWith("/css")) {
                 mimeType = "text/css";
 
@@ -59,46 +63,64 @@ public class Http11Processor implements Runnable, Processor {
                 File file = new File(resource.getFile());
                 Path path = file.toPath();
                 responseBody = new String(Files.readAllBytes(path));
+                response = getOKResponse(mimeType, responseBody);
             } else if (lines[1].startsWith("/login?")) {
                 int index = lines[1].indexOf("?");
-                String loginPath = lines[1].substring(0, index);
                 String queryString = lines[1].substring(index + 1);
                 String account = queryString.split("&")[0].split("=")[1];
                 String password = queryString.split("&")[1].split("=")[1];
 
-                User user = InMemoryUserRepository.findByAccount(account).orElseThrow();
-                if (user.checkPassword(password)) {
-                    log.info(user.toString());
+                Optional<User> optionalUser = InMemoryUserRepository.findByAccount(account);
+                if (optionalUser.isPresent()) {
+                    User user = optionalUser.get();
+                    if (user.checkPassword(password)) {
+                        log.info(user.toString());
+                        response = getRedirectResponse(responseBody, "/index.html");
+                    } else {
+                        response = getRedirectResponse(responseBody, "/401.html");
+                    }
+                } else {
+                    response = getRedirectResponse(responseBody, "/401.html");
                 }
-
-                URL resource = getClass().getClassLoader().getResource("static" + loginPath + ".html");
-                File file = new File(resource.getFile());
-                Path path = file.toPath();
-                responseBody = new String(Files.readAllBytes(path));
             } else {
                 mimeType = "text/html";
+
+                if (lines[1].equals("/login")) {
+                    lines[1] = "/login.html";
+                }
 
                 URL resource = getClass().getClassLoader().getResource("static" + lines[1]);
                 File file = new File(resource.getFile());
                 Path path = file.toPath();
                 responseBody = new String(Files.readAllBytes(path));
+                response = getOKResponse(mimeType, responseBody);
             }
 
             while (!"".equals(line)) {
                 line = br.readLine();
             }
 
-            String response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: " + mimeType + ";charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
-
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String getOKResponse(String mimeType, String responseBody) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: " + mimeType + ";charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+    }
+
+    private String getRedirectResponse(String responseBody, String location) {
+        return String.join("\r\n",
+                "HTTP/1.1 302 Found ",
+                "Location: " + location + " ",
+                "",
+                responseBody);
     }
 }

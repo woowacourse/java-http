49a8814e011dbb84c210b3e95a3a162302006e06
HyungUk Ryu <hw0603@naver.com>
2024-09-04 16:47:37 +0900

    feat(Http11Processor): 정적 리소스 응답 기능 구현

--- tomcat/src/main/java/org/apache/coyote/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/HttpRequest.java
@@ -1,11 +1,10 @@
 package org.apache.coyote;
 
-import jakarta.servlet.HttpMethodConstraintElement;
 import java.util.Map;
 
 public class HttpRequest {
 
-    private final HttpMethodConstraintElement httpMethod;
+    private final String httpMethod;
     private final String path;
     private final String version;
     private final Map<String, String> headers;
@@ -13,12 +12,16 @@ public class HttpRequest {
     public HttpRequest(String request) {
         String[] lines = request.split("\r\n");
         String[] requestLine = lines[0].split(" ");
-        this.httpMethod = new HttpMethodConstraintElement(requestLine[0]);
+        this.httpMethod = requestLine[0];
         this.path = requestLine[1];
         this.version = requestLine[2];
         this.headers = Map.of(
                 "Host", lines[1].split(": ")[1],
                 "Connection", lines[2].split(": ")[1]
         );
+    }
+
+    public String getPath() {
+        return path;
     }
 }
--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,22 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+import org.apache.coyote.HttpRequest;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,19 +39,42 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            HttpRequest httpRequest = readRequest(inputStream);
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            URL resource = getClass().getClassLoader().getResource("static" + httpRequest.getPath());
+            Path path = Optional.ofNullable(resource)
+                    .map(URL::getPath)
+                    .map(Path::of)
+                    .orElseThrow(() -> new RuntimeException(httpRequest.getPath() + " not found"));
+            List<String> strings = Files.readAllLines(path);
 
-            outputStream.write(response.getBytes());
-            outputStream.flush();
+            String responseBody = httpRequest.getPath().endsWith(".html") ? String.join("\r\n", strings) : "Hello world!";
+            HttpResponse response = new HttpResponse(1.1, 200, "OK")
+                    .addHeader("Content-Type", "text/html;charset=utf-8")
+                    .addHeader("Content-Length", String.valueOf(responseBody.getBytes().length))
+                    .setBody(responseBody);
+
+            writeResponse(outputStream, response);
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private HttpRequest readRequest(InputStream inputStream) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder sb = new StringBuilder();
+
+        String line = reader.readLine();
+        while (line != null && !line.isEmpty()) {
+            sb.append(line).append("\r\n");
+            line = reader.readLine();
+        }
+
+        return new HttpRequest(sb.toString());
+    }
+
+    private void writeResponse(OutputStream outputStream, HttpResponse response) throws IOException {
+        outputStream.write(response.getAsBytes());
+        outputStream.flush();
     }
 }

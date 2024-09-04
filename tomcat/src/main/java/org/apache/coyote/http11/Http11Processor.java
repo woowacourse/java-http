11864d5a31a680ed42bceda90417525e274942e0
juha <khabh@naver.com>
2024-09-04 16:16:57 +0900

    feat: 정적 파일 반환 및 login 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,7 +1,21 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import org.apache.coyote.Processor;
+import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.request.Queries;
+import org.apache.coyote.http11.response.HttpResponse;
+import org.apache.coyote.http11.response.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,10 +25,15 @@ import java.net.Socket;
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final Set<String> endpoints = Set.of(
+            "login",
+            "register",
+            "index"
+    );
 
     private final Socket connection;
 
-    public Http11Processor(final Socket connection) {
+    public Http11Processor(Socket connection) {
         this.connection = connection;
     }
 
@@ -26,22 +45,88 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+        try (InputStream inputStream = connection.getInputStream();
+             OutputStream outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            List<String> lines = new ArrayList<>();
+            String line;
+            while ((line = bufferedReader.readLine()) != null && !line.isEmpty()) {
+                lines.add(line);
+            }
+            log.info("HTTP Request: " + lines);
+            HttpRequest request = HttpRequest.of(lines);
+            HttpResponse response = getResponse(request);
+            String formattedResponse = response.toResponse();
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
-
-            outputStream.write(response.getBytes());
+            outputStream.write(formattedResponse.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        }
+    }
+
+    private HttpResponse getResponse(HttpRequest request) throws IOException {
+        String requestPath = request.getPath();
+
+        if (requestPath.equals("/login")) {
+            return login(request);
+        }
+
+        try {
+            URL resource = getClass().getClassLoader().getResource("static/" + requestPath);
+            if (resource == null) {
+                throw new IllegalArgumentException("존재하지 않는 자원입니다.");
+            }
+            ResponseFile responseFile = ResponseFile.of(resource);
+            return HttpResponse.createFileResponse(responseFile);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return HttpResponse.createRedirectResponse(
+                    HttpStatus.FOUND,
+                    "/404.html"
+            );
+        }
+    }
+
+    private HttpResponse login(HttpRequest request) throws IOException {
+        if (request.isQueriesEmpty()) {
+            URL resource = getClass().getClassLoader().getResource("static/login.html");
+            ResponseFile responseFile = ResponseFile.of(resource);
+
+            return HttpResponse.createFileResponse(responseFile);
+        }
+        Queries queries = request.getQueries();
+        String account = queries.get("account");
+        String password = queries.get("password");
+        if (isValidUser(account, password)) {
+            return HttpResponse.createRedirectResponse(
+                    HttpStatus.FOUND,
+                    "/index.html"
+            );
+        }
+        return HttpResponse.createRedirectResponse(
+                HttpStatus.FOUND,
+                "/401.html"
+        );
+    }
+
+    private boolean isValidUser(String account, String password) {
+        if (account == null || password == null) {
+            return false;
+        }
+        return InMemoryUserRepository.findByAccount(account)
+                .map(user -> authenticateUser(user, password))
+                .orElse(false);
+    }
+
+    private boolean authenticateUser(User user, String password) {
+        if (user.checkPassword(password)) {
+            log.info("User authenticated: " + user);
+            return true;
+        } else {
+            log.warn("Authentication failed for user: " + user.getAccount());
+            return false;
         }
     }
 }
--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -13,26 +13,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 class Http11ProcessorTest {
 
     @Test
-    void process() {
-        // given
-        final var socket = new StubSocket();
-        final var processor = new Http11Processor(socket);
-
-        // when
-        processor.process(socket);
-
-        // then
-        var expected = String.join("\r\n",
-                "HTTP/1.1 200 OK ",
-                "Content-Type: text/html;charset=utf-8 ",
-                "Content-Length: 12 ",
-                "",
-                "Hello world!");
-
-        assertThat(socket.output()).isEqualTo(expected);
-    }
-
-    @Test
     void index() throws IOException {
         // given
         final String httpRequest= String.join("\r\n",
@@ -50,12 +30,12 @@ class Http11ProcessorTest {
 
         // then
         final URL resource = getClass().getClassLoader().getResource("static/index.html");
-        var expected = "HTTP/1.1 200 OK \r\n" +
-                "Content-Type: text/html;charset=utf-8 \r\n" +
-                "Content-Length: 5564 \r\n" +
-                "\r\n"+
-                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
 
-        assertThat(socket.output()).isEqualTo(expected);
+        assertThat(socket.output()).contains(
+                "HTTP/1.1 200 OK \r\n",
+                "Content-Type: text/html;charset=utf-8 \r\n",
+                "Content-Length: 5564 \r\n",
+                new String(Files.readAllBytes(new File(resource.getFile()).toPath()))
+        );
     }
 }

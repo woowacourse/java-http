55f0b5ba488e09a4d0640ef9bfb28d0fb5255000
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-09-04 16:17:00 +0900

    feat: 1단계 3번 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,8 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +16,12 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -38,6 +46,9 @@ public class Http11Processor implements Runnable, Processor {
             String path = readPath(inputStream);
             if (path.equals("/")) {
                 processHome(outputStream);
+            }
+            if (path.startsWith("/login")) {
+                processLogin(outputStream, path);
             }
             processFiles(outputStream, path);
         } catch (IOException | UncheckedServletException e) {
@@ -96,6 +107,52 @@ public class Http11Processor implements Runnable, Processor {
 
         outputStream.write(response.getBytes());
         outputStream.flush();
+    }
 
+    private void processLogin(OutputStream outputStream, String path) throws IOException {
+        final URL resource = getClass().getClassLoader().getResource("static/login.html");
+        final var responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+
+        final var response = String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+
+        Map<String, List<String>> queryParams = parsingQueryString(path);
+
+        if (queryParams.containsKey("account")) {
+            User user = InMemoryUserRepository.findByAccount(queryParams.get("account").getFirst()).get();
+            log.info("user : {}", user);
+
+        }
+
+        outputStream.write(response.getBytes());
+        outputStream.flush();
+    }
+
+    private Map<String, List<String>> parsingQueryString(String path) {
+        Map<String, List<String>> queryParams = new HashMap<>();
+
+        int questionMarkIndex = path.indexOf('?');
+        if (questionMarkIndex == -1 || questionMarkIndex == path.length() - 1) {
+            return Collections.emptyMap();
+        }
+
+        String queryString = path.substring(questionMarkIndex + 1);
+        String[] pairs = queryString.split("&");
+
+        for (String pair : pairs) {
+            String[] keyValue = pair.split("=", 2);
+            if (keyValue.length != 2) {
+                continue;
+            }
+            String key = keyValue[0];
+            String value = keyValue[1];
+            queryParams.computeIfAbsent(key, k -> new ArrayList<>()).add(value);
+        }
+
+        return queryParams;
     }
 }

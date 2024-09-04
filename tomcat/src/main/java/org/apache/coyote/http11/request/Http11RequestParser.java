c99c4ad4fad1326fbdab3ea627a71b62ebced1e1
임수빈 <robin980108@naver.com>
2024-09-04 18:03:43 +0900

    fix: 간헐적으로 css, js 요청이 실패하는 오류 수정

--- tomcat/src/main/java/org/apache/coyote/http11/request/Http11RequestParser.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/Http11RequestParser.java
@@ -35,8 +35,11 @@ class Http11RequestParser {
     String readAsString(InputStream inputStream) {
         try {
             // 학습 InputStream 의 readAllBytes 는 요청 전체를 읽는다. 이때, EOF를 만날때 까지 읽으므로 EOF가 없다면 Integer.MAX_VALUE 크기만큼 읽는다.
-            int readByte = inputStream.available();
-            return new String(inputStream.readNBytes(readByte));
+            // readNBytes는 일정 크기만큼 읽는다. 그런데, 정확히 읽어야 하는 길이를 알아내기 어렵다.
+            // read(byte[])는 읽을 수 있는 만큼 읽어서 배열에 넣는다. 반환값은 읽은 크기다. 따라서 이를 사용해야 제대로 읽어올 수 있다.
+            byte[] data = new byte[100000];
+            int read = inputStream.read(data);
+            return new String(data, 0, read);
         } catch (IOException e) {
             throw new IllegalArgumentException("잘못된 연결에서 읽기 요청이 발생했습니다.", e);
         }

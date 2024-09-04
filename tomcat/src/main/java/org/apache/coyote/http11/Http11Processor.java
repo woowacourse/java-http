277aad0dd193c0e2e8ad8d12f7908d9379f16351
hyeonjilee <skylar12200@gmail.com>
2024-09-04 17:05:59 +0900

    feat: /index.html 응답 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,10 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,13 +32,26 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String firstLine = bufferedReader.readLine();
+            String requestURI = firstLine.split(" ")[1];
 
-            final var responseBody = "Hello world!";
+            final Path path = Path.of(getClass().getResource("/static" + requestURI).getPath());
+
+            StringBuilder responseBody = new StringBuilder();
+            if (requestURI.equals("/")) {
+                responseBody.append("Hello world!");
+            }
+            Files.readAllLines(path)
+                    .stream()
+                    .forEach(line -> responseBody.append(line).append("\n"));
+
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "Content-Length: " + responseBody.toString().getBytes().length + " ",
                     "",
                     responseBody);
 

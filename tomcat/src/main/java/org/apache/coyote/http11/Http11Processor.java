6301136785f97adbf69799cb18e0d46cd459ad4f
hyxrxn <gpfls981220@gmail.com>
2024-09-04 16:47:18 +0900

    feat: 인덱스 페이지 응답

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,13 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,20 +35,58 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
+            InputStreamReader reader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(reader);
 
-            final var responseBody = "Hello world!";
+            String requestURL = getRequestURL(bufferedReader);
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
-
-            outputStream.write(response.getBytes());
-            outputStream.flush();
+            if ("/".equals(requestURL)) {
+                processRootRequest(outputStream);
+                return;
+            }
+            processRequest(requestURL, outputStream);
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    private String getRequestURL(BufferedReader bufferedReader) throws IOException {
+        String firstLine = bufferedReader.readLine();
+        String[] requests = firstLine.split(" ");
+        return requests[1];
+    }
+
+    private void processRequest(String requestURL, OutputStream outputStream) throws URISyntaxException, IOException {
+        URI resource = getClass().getClassLoader().getResource("static" + requestURL).toURI();
+        final Path path = Path.of(resource);
+        byte[] fileBytes = Files.readAllBytes(path);
+
+        final var response = String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + fileBytes.length + " ",
+                "",
+                "");
+
+        outputStream.write(response.getBytes());
+        outputStream.write(fileBytes);
+
+        outputStream.flush();
+    }
+
+    private void processRootRequest(OutputStream outputStream) throws IOException {
+        final var responseBody = "Hello world!";
+
+        final var response = String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+
+        outputStream.write(response.getBytes());
+        outputStream.flush();
     }
 }

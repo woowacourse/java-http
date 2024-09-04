12d4ad1ab95d6dfcb1d5bfd33b99d98e039b1822
Nam Gi Beom <rlqja0523@naver.com>
2024-09-05 02:27:47 +0900

    feat: index.html 응답 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,14 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,9 +35,30 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
 
-            final var responseBody = "Hello world!";
+            String request = bufferedReader.readLine();
+            Map<String, String> headers = new HashMap<>();
+            String line = bufferedReader.readLine();
+            while (line != null && !line.isEmpty()) {
+                String[] header = line.split(": ");
+                headers.put(header[0], header[1]);
+                line = bufferedReader.readLine();
+            }
+            String[] requestLines = request.split(" ");
+            String httpMethod = requestLines[0];
+            String uri = requestLines[1];
+            String httpVersion = requestLines[2];
+
+            String file = this.getClass().getClassLoader().getResource("static/index.html").getFile();
+            File html = new File(file);
+
+
+            var responseBody = Files.readString(html.toPath());
+            if (!uri.equals("/index.html")) {
+                responseBody = "Hello world!";
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",

2f07450bd2bd353dc6635b46055d6844ac369505
unifolio0 <boho82ha@gmail.com>
2024-09-04 20:44:05 +0900

    feat: login 성공 시 쿠키 설정하는 기능

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -10,6 +10,7 @@ import java.net.Socket;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.UUID;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,19 +36,37 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
             var bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            String line = bufferedReader.readLine();
-            System.out.println(line);
-            if (line == null || line.isEmpty()) {
-                return;
-            }
 
-            String[] requestLine = line.split(" ");
+            var requestBuilder = new StringBuilder();
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null && !line.isEmpty()) {
+                requestBuilder.append(line + System.lineSeparator());
+            }
+//            System.out.println(requestBuilder);
+            line = requestBuilder.toString();
+
+            String[] requestLine = line.split(System.lineSeparator())[0].split(" ");
             if (requestLine.length < 3) {
                 return;
             }
+            String method = requestLine[0];
             String uri = requestLine[1];
             String path = uri;
             String queryString = "";
+            if (method.equals("POST") && path.equals("/register")) {
+                String contentLengthS = line.split(System.lineSeparator())[3].split(": ")[1];
+                int contentLength = Integer.parseInt(contentLengthS);
+                char[] buffer = new char[contentLength];
+                bufferedReader.read(buffer, 0, contentLength);
+                String requestBody = new String(buffer);
+                String[] token = requestBody.split("&");
+                String account = token[0].split("=")[1];
+                String email = token[1].split("=")[1];
+                String password = token[2].split("=")[1];
+                User user = new User(account, password, email);
+                InMemoryUserRepository.save(user);
+            }
             if (uri.contains("?")) {
                 int index = uri.indexOf("?");
                 path = uri.substring(0, index);
@@ -56,13 +75,13 @@ public class Http11Processor implements Runnable, Processor {
 
             var responseBody = "";
             var contentType = "text/html;charset=utf-8 \r\n";
-            if (!path.equals("/") && !path.contains(".")) {
+            if (!path.equals("/") && !path.contains(".") && method.equals("GET")) {
                 path += ".html";
             }
 
             if (path.equals("/")) {
                 responseBody = "Hello world!";
-            } else {
+            } else if (method.equals("GET")) {
                 String fileName = "static" + path;
                 var resourceUrl = getClass().getClassLoader().getResource(fileName);
                 if (resourceUrl == null) {
@@ -101,12 +120,13 @@ public class Http11Processor implements Runnable, Processor {
                 String[] query = queryString.split("&");
                 String account = query[0].split("=")[1];
                 String password = query[1].split("=")[1];
-                System.out.println(account + " " + password);
                 User user = InMemoryUserRepository.findByAccount(account)
                         .orElseThrow();
+                UUID uuid = UUID.randomUUID();
                 if (user.checkPassword(password)) {
                     response = String.join("\r\n",
                             "HTTP/1.1 302 Found ",
+                            "Set-Cookie: JSESSIONID=" + uuid,
                             "Location: " + "/index.html");
                     log.info(user.toString());
                 } else {
@@ -115,6 +135,11 @@ public class Http11Processor implements Runnable, Processor {
                             "Location: " + "/401.html");
                     log.error("비밀번호 불일치");
                 }
+            }
+            if (method.equals("POST") && path.equals("/register")) {
+                response = String.join("\r\n",
+                        "HTTP/1.1 302 Found ",
+                        "Location: " + "/index.html");
             }
 
             outputStream.write(response.getBytes());

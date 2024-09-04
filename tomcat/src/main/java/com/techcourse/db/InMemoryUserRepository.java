a15fde9d490ce3c8ef98767f496f148d4cd12969
reddevilmidzy <midzy0228@gmail.com>
2024-09-04 16:49:32 +0900

    style: final 누락 추가 및 더미데이터 수정

--- tomcat/src/main/java/com/techcourse/db/InMemoryUserRepository.java
+++ tomcat/src/main/java/com/techcourse/db/InMemoryUserRepository.java
@@ -11,15 +11,15 @@ public class InMemoryUserRepository {
     private static final Map<String, User> database = new ConcurrentHashMap<>();
 
     static {
-        final User user = new User(1L, "gugu", "password", "hkkang@woowahan.com");
+        final User user = new User(1L, "redddy", "password", "redddy@redddy.com");
         database.put(user.getAccount(), user);
     }
 
-    public static void save(User user) {
+    public static void save(final User user) {
         database.put(user.getAccount(), user);
     }
 
-    public static Optional<User> findByAccount(String account) {
+    public static Optional<User> findByAccount(final String account) {
         return Optional.ofNullable(database.get(account));
     }
 

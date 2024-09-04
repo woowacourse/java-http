94de84e21aed876be9d4fdc405fcf94844208bd8
hangillee <skfcb10@naver.com>
2024-09-04 19:03:02 +0900

    feat: Query String 파싱 및 로그인 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,21 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -41,32 +50,60 @@ public class Http11Processor implements Runnable, Processor {
             }
 
             final var requestLineTokens = requestLine.split(" ");
+            final var url = requestLineTokens[1];
 
-            var response = buildHttpResponse("text/html", "Hello world!");
-            if (requestLineTokens[1].equals("/index.html")) {
-                response = buildHtmlResponse(requestLineTokens[1]);
+            if ("/".equals(url)) {
+                build200Response(outputStream, "text/html", "Hello world!");
             }
-            if (requestLineTokens[1].equals("/css/styles.css")) {
-                response = buildStyleSheetResponse(requestLineTokens[1]);
+            if ("/index.html".equals(url)) {
+                buildHtmlResponse(outputStream, url);
             }
-            if (requestLineTokens[1].equals("/js/scripts.js")) {
-                response = buildScriptResponse(requestLineTokens[1]);
+            if ("/css/styles.css".equals(url)) {
+                buildStyleSheetResponse(outputStream, url);
+            }
+            if ("/js/scripts.js".equals(url)) {
+                buildScriptResponse(outputStream, url);
+            }
+            if (url.matches("/assets/.*\\.js")) {
+                buildScriptResponse(outputStream, url);
+            }
+            if (url.equals("/login")) {
+                buildHtmlResponse(outputStream, url + ".html");
+            }
+            if (url.matches("/login\\?account=.*&password=.*")) {
+                var index = url.indexOf('?');
+                var queryString = url.substring(index + 1);
+
+                login(queryString, outputStream);
             }
 
-            outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
     }
 
-    private String buildHttpResponse(final String fileType, final String responseBody) {
-        return String.join("\r\n",
-                "HTTP/1.1 200 OK ",
-                "Content-Type: " + fileType + ";charset=utf-8 ",
-                "Content-Length: " + responseBody.getBytes().length + " ",
-                "",
-                responseBody);
+    private void login(final String queryString, final OutputStream outputStream) throws IOException {
+        Map<String, String> params = parseQueryString(queryString);
+
+        if (InMemoryUserRepository.findByAccount(params.get("account")).isEmpty()) {
+            buildHtmlResponse(outputStream, "/401.html");
+            return;
+        }
+
+        final User user = InMemoryUserRepository.findByAccount(params.get("account")).get();
+        if (user.checkPassword(params.get("password"))) {
+            build302Response(outputStream, "/index.html");
+            log.info("user: {}", user);
+        }
+    }
+
+    private Map<String, String> parseQueryString(final String queryString) {
+        final var params = queryString.split("&");
+
+        return Arrays.stream(params)
+                .map(param -> param.split("="))
+                .collect(Collectors.toMap(token -> token[0], token -> token[1]));
     }
 
     private String buildResponseBodyFromStaticFile(String fileName) throws IOException {
@@ -76,21 +113,46 @@ public class Http11Processor implements Runnable, Processor {
         return String.join("\n", Files.readAllLines(path)) + "\n";
     }
 
-    private String buildHtmlResponse(final String fileName) throws IOException {
+    private void buildHtmlResponse(final OutputStream outputStream, final String fileName) throws IOException {
         final var responseBody = buildResponseBodyFromStaticFile(fileName);
 
-        return buildHttpResponse("text/html", responseBody);
+        build200Response(outputStream, "text/html", responseBody);
     }
 
-    private String buildStyleSheetResponse(final String fileName) throws IOException {
+    private void buildStyleSheetResponse(final OutputStream outputStream, final String fileName) throws IOException {
         final var responseBody = buildResponseBodyFromStaticFile(fileName);
 
-        return buildHttpResponse("text/css", responseBody);
+        build200Response(outputStream, "text/css", responseBody);
     }
 
-    private String buildScriptResponse(final String fileName) throws IOException {
+    private void buildScriptResponse(final OutputStream outputStream, final String fileName) throws IOException {
         final var responseBody = buildResponseBodyFromStaticFile(fileName);
 
-        return buildHttpResponse("text/javascript", responseBody);
+        build200Response(outputStream, "text/javascript", responseBody);
+    }
+
+    private void build200Response(final OutputStream outputStream, final String fileType, final String responseBody) {
+        try (final BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+             final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(bufferedOutputStream))) {
+            writer.write("HTTP/1.1 200 OK \r\n");
+            writer.write("Content-Type: " + fileType + ";charset=utf-8 \r\n");
+            writer.write("Content-Length: " + responseBody.getBytes().length + " \r\n");
+            writer.write("\r\n");
+            writer.write(responseBody);
+            writer.write("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    private void build302Response(final OutputStream outputStream, final String location) {
+        try (final BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+             final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(bufferedOutputStream))) {
+            writer.write("HTTP/1.1 302 Found \r\n");
+            writer.write("Location: " + location + " \r\n");
+            writer.write("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
     }
 }

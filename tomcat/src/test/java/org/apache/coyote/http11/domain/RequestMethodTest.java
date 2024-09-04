ce81eecab0c7fca76e30f7b491b2a64141310faa
Eugene Jang <kyunellroll@gmail.com>
2024-09-05 02:44:56 +0900

    feat(RequestMethod): 요청 메서드 도메인 생성
    - RequestMethod 테스트 작성

---
+++ tomcat/src/test/java/org/apache/coyote/http11/domain/RequestMethodTest.java
@@ -0,0 +1,27 @@
+package org.apache.coyote.http11.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class RequestMethodTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"FIND", "SEARCH", "PAAATCH"})
+    @DisplayName("일치하는 메서드가 없는 경우 에러를 발생한다.")
+    void findMethod_WhenNoCorrespondMethod(String inputRequestMethod) {
+        assertThrows(IllegalArgumentException.class, () -> RequestMethod.findMethod(inputRequestMethod));
+    }
+
+    @Test
+    @DisplayName("일치하는 메서드를 찾아서 반환한다.")
+    void findMethod_WhenCorrespondMethod() {
+        String requestMethod = "GET";
+
+        assertEquals(RequestMethod.findMethod(requestMethod), RequestMethod.GET);
+    }
+}

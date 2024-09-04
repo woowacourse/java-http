74c67d752c8ffef237767c63ef8b6360f34c88e9
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-09-04 12:53:41 +0900

    feat: 응답 코드 생성

---
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpStateCode.java
@@ -0,0 +1,43 @@
+package org.apache.coyote.http11;
+
+public enum HttpStateCode {
+    OK(200, "OK"),
+    CREATED(201, "Created"),
+    ACCEPTED(202, "Accepted"),
+    NO_CONTENT(204, "No Content"),
+
+    MOVED_PERMANENTLY(301, "Moved Permanently"),
+    FOUND(302, "Found"),
+    NOT_MODIFIED(304, "Not Modified"),
+
+    BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    NOT_IMPLEMENTED(501, "Not Implemented"),
+    BAD_GATEWAY(502, "Bad Gateway"),
+    SERVICE_UNAVAILABLE(503, "Service Unavailable");
+
+    private final int code;
+    private final String message;
+
+    HttpStateCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String toStatus() {
+        return code + " " + message;
+    }
+}
---
+++ tomcat/src/test/java/org/apache/coyote/http11/HttpStateCodeTest.java
@@ -0,0 +1,21 @@
+package org.apache.coyote.http11;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("HTTP 상태 코드")
+class HttpStateCodeTest {
+
+    @DisplayName("HTTP 상태 코드 응답에 맞게 반환한다.")
+    @Test
+    void toStatus() {
+        // given
+        HttpStateCode stateCode = HttpStateCode.OK;
+        String expected = stateCode.getCode() + " " + stateCode.getMessage();
+
+        // when & then
+        assertThat(stateCode.toStatus()).isEqualTo(expected);
+    }
+}

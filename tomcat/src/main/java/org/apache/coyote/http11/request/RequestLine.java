7f082ec274c19a002f7802a4eb259e472e817162
juha <khabh@naver.com>
2024-09-04 14:01:36 +0900

    feat: HTTP 요청 라인 추출 기능 구현

---
+++ tomcat/src/main/java/org/apache/coyote/http11/request/RequestLine.java
@@ -0,0 +1,61 @@
+package org.apache.coyote.http11.request;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RequestLine {
+
+    private static final int SPILT_REQUEST_LINE_COUNT = 3;
+    private static final int METHOD_INDEX = 0;
+    private static final int URI_INDEX = 1;
+    private static final int PROTOCOL_INDEX = 2;
+
+    private final HttpMethod method;
+    private final RequestUri requestUri;
+    private final String protocol;
+
+    public RequestLine(HttpMethod method, RequestUri requestUri, String protocol) {
+        this.method = method;
+        this.requestUri = requestUri;
+        this.protocol = protocol;
+    }
+
+    public static RequestLine of(String requestLine) {
+        List<String> splitRequestLine = Arrays.stream(requestLine.split(" ")).toList();
+        if (splitRequestLine.size() != SPILT_REQUEST_LINE_COUNT) {
+            throw new IllegalArgumentException("올바르지 않은 request line 형식입니다.");
+        }
+
+        return new RequestLine(
+                getMethod(splitRequestLine),
+                getUri(splitRequestLine),
+                splitRequestLine.get(PROTOCOL_INDEX)
+        );
+    }
+
+    private static HttpMethod getMethod(List<String> splitRequestLine) {
+        String methodName = splitRequestLine.get(METHOD_INDEX);
+        return HttpMethod.getByName(methodName);
+    }
+
+    private static RequestUri getUri(List<String> splitRequestLine) {
+        String uri = splitRequestLine.get(URI_INDEX);
+        return RequestUri.of(uri);
+    }
+
+    public HttpMethod getMethod() {
+        return method;
+    }
+
+    public String getPath() {
+        return requestUri.getPath();
+    }
+
+    public Queries getQueries() {
+        return requestUri.getQueries();
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+}

4bee65e0d291e26c1857685899909e9952b713b9
jimi567 <28584160+jimi567@users.noreply.github.com>
2024-09-04 19:40:07 +0900

    feat: index.html 랜더링

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,16 +1,25 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.LinkedList;
+import java.util.List;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
-
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final String STATIC_RESOURCE_BASE_URL = "static";
 
     private final Socket connection;
 
@@ -27,9 +36,27 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+             final BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
 
-            final var responseBody = "Hello world!";
+            final List<String> lines = new LinkedList<>();
+            String line;
+            while ((line = bufferedReader.readLine()) != null && !line.isEmpty()) {
+                lines.add(line);
+            }
+            final Http11Request request = new Http11Request(lines);
+            final String fileName = request.parsePath();
+            //response body 추출
+            String responseBody = "Hello world!";
+            if (fileName.endsWith("html")) {
+                URL resource = getClass().getClassLoader().getResource(STATIC_RESOURCE_BASE_URL + fileName);
+                final File file = new File(resource.getFile());
+                final Path path = file.toPath();
+                responseBody = Files.readString(path, StandardCharsets.UTF_8);
+            }
+
+            //final var responseBody = "Hello world!";
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -43,5 +70,6 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+
     }
 }

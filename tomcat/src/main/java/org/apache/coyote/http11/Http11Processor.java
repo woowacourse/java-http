0159622f0d7fbc6ea984d60de59bc27e2b1d562d
PgmJun <84304802+PgmJun@users.noreply.github.com>
2024-09-04 19:09:00 +0900

    feat: 2. CSS 지원하기 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,9 +5,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.http.HttpHeaders;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +21,7 @@ import java.net.Socket;
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final String HEADER_ACCEPT = "Accept";
 
     private final Socket connection;
 
@@ -35,26 +39,60 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
-            BufferedReader inputBr = new BufferedReader(new InputStreamReader(inputStream));
-            String[] httpRequest = inputBr.readLine().split(" ");
-            String resourcePath = "static" + httpRequest[1];
+            BufferedReader input = new BufferedReader(new InputStreamReader(inputStream));
 
-            URL resource = getClass().getClassLoader().getResource(resourcePath);
-            final Path path = new File(resource.getPath()).toPath();
+            String[] httpRequest = input.readLine().split(" ");
 
-            final String responseBody = Files.readString(path);
+            Map<String, String> httpHeaders = getHttpHeaders(input);
+            String accept = httpHeaders.get(HEADER_ACCEPT);
+            if(accept.contains("text/css")) {
+                final String responseBody = getFileValue(httpRequest);
+                final var response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/css;charset=utf-8 ",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody);
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+            } else {
+                final String responseBody = getFileValue(httpRequest);
+                final var response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody);
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
-
-            outputStream.write(response.getBytes());
-            outputStream.flush();
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+            }
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
     }
+
+    private Map<String, String> getHttpHeaders(BufferedReader input) throws IOException {
+        Map<String, String> httpHeader = new HashMap<>();
+
+        String value = input.readLine();
+        while(!value.equals("")) {
+            String[] header = value.split(": ");
+            httpHeader.put(header[0], header[1]);
+
+            value = input.readLine();
+        }
+
+        return httpHeader;
+    }
+
+    private String getFileValue(String[] httpRequest) throws IOException {
+        String resourcePath = "static" + httpRequest[1];
+        URL resource = getClass().getClassLoader().getResource(resourcePath);
+        final Path path = new File(resource.getPath()).toPath();
+
+        final String responseBody = Files.readString(path);
+        return responseBody;
+    }
+
 }

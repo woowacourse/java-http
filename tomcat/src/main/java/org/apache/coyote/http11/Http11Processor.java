877bdb5e1c1560d9c4b755cd8a832b3662494061
yoonjuho <dbswn990@naver.com>
2024-09-04 18:48:53 +0900

    refactor: RequestLine 추출 메서드 분리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,14 @@
 package org.apache.coyote.http11;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.Map;
 import java.util.Objects;
 
 import org.apache.coyote.Processor;
@@ -34,21 +36,27 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
-            String uri = extractURIByRequest(inputStream);
-            final URL resource = getClass().getClassLoader().getResource(uri);
-            if (resource == null) {
-                writeAndFlush(uri.substring("static".length()), outputStream);
+             final var outputStream = connection.getOutputStream();
+             final var bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+            Map<RequestLineElement, String> requestLineElements = extractRequestLine(bufferedReader);
+            final URL resource = getClass().getClassLoader().getResource(
+                    "static" + requestLineElements.get(RequestLineElement.REQUEST_URI));
+            if (isResourceNotExists(resource)) {
+                write(requestLineElements.get(RequestLineElement.REQUEST_URI), outputStream);
                 return;
             }
-            final String content = new String(Files.readAllBytes(new File(Objects.requireNonNull(resource).getFile()).toPath()));
-            writeAndFlush(content, outputStream);
+            final String content = new String(Files.readAllBytes(new File((resource).getFile()).toPath()));
+            write(content, outputStream);
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
     }
 
-    private static void writeAndFlush(String content, OutputStream outputStream) throws IOException {
+    private static boolean isResourceNotExists(URL resource) {
+        return Objects.isNull(resource);
+    }
+
+    private static void write(String content, OutputStream outputStream) throws IOException {
         final var response = String.join("\r\n",
                 "HTTP/1.1 200 OK ",
                 "Content-Type: text/html;charset=utf-8 ",
@@ -59,26 +67,19 @@ public class Http11Processor implements Runnable, Processor {
         outputStream.flush();
     }
 
-    public static String extractURIByRequest(InputStream inputStream) throws IOException {
-        consumeHeader(inputStream);
-        String uri = getUri(inputStream);
+    private static Map<RequestLineElement, String> extractRequestLine(BufferedReader bufferedReader) throws IOException {
+        String line = bufferedReader.readLine();
+        String[] requestLineElements = line.split(" ");
+        String requestURI = convertToDefaultURI(requestLineElements[1]);
+        return Map.of(RequestLineElement.HTTP_METHOD, requestLineElements[0],
+                RequestLineElement.REQUEST_URI, requestURI,
+                RequestLineElement.HTTP_VERSION, requestLineElements[2]);
+    }
+
+    private static String convertToDefaultURI(String uri) {
         if (uri.equals("/")) {
-            return "static/index.html";
+            return "/index.html";
         }
-        return "static" + uri;
-    }
-
-    private static void consumeHeader(InputStream inputStream) throws IOException {
-        while ((inputStream.read()) != ' ') {
-        }
-    }
-
-    private static String getUri(InputStream inputStream) throws IOException {
-        int ch;
-        StringBuilder uriBuilder = new StringBuilder();
-        while ((ch = inputStream.read()) != ' ') {
-            uriBuilder.append((char) ch);
-        }
-        return uriBuilder.toString();
+        return uri;
     }
 }
---
+++ tomcat/src/main/java/org/apache/coyote/http11/RequestLineElement.java
@@ -0,0 +1,7 @@
+package org.apache.coyote.http11;
+
+public enum RequestLineElement {
+    HTTP_METHOD,
+    REQUEST_URI,
+    HTTP_VERSION
+}

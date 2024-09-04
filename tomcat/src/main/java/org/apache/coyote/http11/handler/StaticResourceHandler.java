319482d0a1ea1153aa21ae03478e7b19da15e574
libienz <leekh10195@gmail.com>
2024-09-04 17:24:26 +0900

    refactor: 정적 파일을 한 줄씩 읽지 않고 한번에 읽도록 개선

--- tomcat/src/main/java/org/apache/coyote/http11/handler/StaticResourceHandler.java
+++ tomcat/src/main/java/org/apache/coyote/http11/handler/StaticResourceHandler.java
@@ -1,10 +1,9 @@
 package org.apache.coyote.http11.handler;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
-import java.util.stream.Collectors;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.response.HttpResponse;
 import org.apache.util.FileUtils;
@@ -32,10 +31,7 @@ public class StaticResourceHandler implements HttpRequestHandler {
     }
 
     private String readFile(final String fileName) throws IOException {
-        Path path = Path.of(getClass().getClassLoader().getResource(STATIC_RESOURCE_PATH + fileName).getFile());
-        List<String> fileContents = Files.readAllLines(path);
-
-        return fileContents.stream()
-                .collect(Collectors.joining(System.lineSeparator()));
+        URL resource = getClass().getClassLoader().getResource(STATIC_RESOURCE_PATH + fileName);
+        return new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
     }
 }

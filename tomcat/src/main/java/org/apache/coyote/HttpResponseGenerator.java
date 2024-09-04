7611c00621440c2f405fbfb80620bfe3d46399c0
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-09-04 22:14:51 +0900

    refactor(HttpResponseGenerator): response 생성 객체 분리

---
+++ tomcat/src/main/java/org/apache/coyote/HttpResponseGenerator.java
@@ -0,0 +1,18 @@
+package org.apache.coyote;
+
+public class HttpResponseGenerator {
+    public static String getOkResponse(String mimeType, String responseBody) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: " + mimeType + ";charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+    }
+
+    public static String getFoundResponse(String url) {
+        return String.join("\r\n",
+                "HTTP/1.1 302 FOUND ",
+                "Location:  " + url);
+    }
+}

0bf9b904d04780758d95503608a834cf93d26591
jongmee <ra123221@gmail.com>
2024-09-04 13:00:15 +0900

    feat: GET /index.html 응답

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,17 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Path;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -27,9 +32,36 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            String responseBody = "Hello world!";
+            String uri = "/";
+            String requestLine = bufferedReader.readLine();
+            if (requestLine != null) {
+                String[] requestParts = requestLine.split(" ");
+                if (requestParts.length >= 2) {
+                    uri = requestParts[1].trim();
+                }
+            }
+
+            if (uri.equals("/index.html")) {
+                final String fileName = "static/index.html";
+                final URL url = getClass().getClassLoader().getResource(fileName);
+                if (url != null) {
+                    final File file = new File(url.getFile());
+                    final Path path = file.toPath();
+
+                    final StringBuilder htmlContent = new StringBuilder();
+                    try (BufferedReader htmlBufferedReader = new BufferedReader(new FileReader(path.toString()))) {
+                        String line;
+                        while ((line = htmlBufferedReader.readLine()) != null) {
+                            htmlContent.append(line).append("\n");
+                        }
+                    }
+                    responseBody = htmlContent.toString();
+                }
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",

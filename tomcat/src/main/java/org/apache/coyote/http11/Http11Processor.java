e0766832c927214b7ba5a7fb14c45719973e9623
hyeonjilee <skylar12200@gmail.com>
2024-09-05 22:37:41 +0900

    feat: 로그인 여부에 따른 페이지 이동 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -9,6 +9,9 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,27 +43,49 @@ public class Http11Processor implements Runnable, Processor {
             Path path = Path.of("");
             StringBuilder responseBody = new StringBuilder();
             String contentType = "";
-            while (bufferedReader.ready()) {  // TODO: 404 추가하기
-                String line = bufferedReader.readLine();
-                if (line.startsWith("GET")) {
-                    String uri = line.split(" ")[1];
-                    if (uri.equals("/")) {
-                        contentType = "text/html; charset=utf-8 ";
-                        path = Path.of(getClass().getResource(STATIC_PATH + "/index.html").getPath());
-                    }
-                    if (uri.startsWith("/login")) {
-                        contentType = "text/html; charset=utf-8 ";
-                        path = Path.of(getClass().getResource(STATIC_PATH + "/login.html").getPath());
+            String statusCode = "";
+            String location = "";
 
-                        int index = uri.indexOf("?");
+            String rawLine;
+            Map<String, String> header = new HashMap<>();
+            header.put("requestLine", bufferedReader.readLine());
+            while ((rawLine = bufferedReader.readLine()) != null && !rawLine.isEmpty()) {
+                if (!rawLine.isEmpty()) {
+                    String[] line = rawLine.split(": ", 2);
+                    header.put(line[0], line[1]);
+                }
+            }
+
+            if (header.get("requestLine").startsWith("GET")) {  // TODO: 404 추가하기
+                String uri = header.get("requestLine").split(" ")[1];
+                if (uri.equals("/")) {
+                    contentType = "text/html; charset=utf-8 ";
+                    statusCode = "200 OK";
+                    path = Path.of(getClass().getResource(STATIC_PATH + "/index.html").getPath());
+                }
+                if (uri.startsWith("/login")) {
+                    contentType = "text/html; charset=utf-8 ";
+
+                    int index = uri.indexOf("?");
+                    if (index == -1) { // 쿼리 파라미터가 없는 경우
+                        statusCode = "200 OK";
+                        path = Path.of(getClass().getResource(STATIC_PATH + "/login.html").getPath());
+                    }
+                    if (index != -1) { // 쿼리 파라미터가 있는 경우
                         String queryString = uri.substring(index + 1);
                         String[] userInfo = queryString.split("&");
                         String account = userInfo[0].split("=")[1];
                         String password = userInfo[1].split("=")[1];
-                        User user = InMemoryUserRepository.findByAccount(account)
-                                .orElseThrow();
-                        if (user.checkPassword(password)) {
-                            log.info(user.toString());
+                        Optional<User> user = InMemoryUserRepository.findByAccount(account);
+                        if (!user.isPresent()
+                            || (user.isPresent() && !user.get().checkPassword(password))) {
+                            statusCode = "401 UNAUTHORIZED ";
+                            path = Path.of(getClass().getResource(STATIC_PATH + "/401.html").getPath());
+                        }
+                        if (user.isPresent() && user.get().checkPassword(password)) {
+                            log.info(user.get().toString());
+                            statusCode = "302 FOUND ";
+                            location = "/index.html";
                         }
                     }
                     if (uri.endsWith(".html")) {
@@ -79,16 +104,25 @@ public class Http11Processor implements Runnable, Processor {
                     }
                 }
             }
-            Files.readAllLines(path)
-                    .stream()
-                    .forEach(line -> responseBody.append(line).append("\n"));
 
-            var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: " + contentType,
-                    "Content-Length: " + responseBody.toString().getBytes().length + " ",
-                    "",
-                    responseBody);
+            var response = "";
+            if (statusCode.startsWith("200") || statusCode.startsWith("401")) {
+                Files.readAllLines(path)
+                        .stream()
+                        .forEach(line -> responseBody.append(line).append("\n"));
+
+                response = String.join("\r\n",
+                        "HTTP/1.1 " + statusCode,
+                        "Content-Type: " + contentType,
+                        "Content-Length: " + responseBody.toString().getBytes().length + " ",
+                        "",
+                        responseBody);
+            }
+            if (statusCode.startsWith("302")) {
+                response = String.join("\r\n",
+                        "HTTP/1.1 " + statusCode,
+                        "Location: " + location);
+            }
 
             outputStream.write(response.getBytes());
             outputStream.flush();
@@ -97,4 +131,3 @@ public class Http11Processor implements Runnable, Processor {
         }
     }
 }
-

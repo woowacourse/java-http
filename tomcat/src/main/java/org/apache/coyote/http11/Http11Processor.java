8e5fe4de71ec8b09fab4bf6cc0ee766c973f6f84
devhoya97 <diajbh123@gmail.com>
2024-09-05 13:10:13 +0900

    refactor: Http 요청 URL에 따라 분기 처리하도록 메서드 분리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -11,8 +11,11 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class Http11Processor implements Runnable, Processor {
@@ -36,54 +39,23 @@ public class Http11Processor implements Runnable, Processor {
         try (var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
             String uri = getUri(inputStream);
-            String path = uri;
 
-            if ("/favicon.ico".equals(path)) {
-                final var response = String.join("\r\n",
-                        "HTTP/1.1 204 No Content",
-                        "Content-Length: 0",
-                        "",
-                        "");
-                outputStream.write(response.getBytes());
-                outputStream.flush();
+            if ("/favicon.ico".equals(uri)) {
+                handleFaviconRequest(outputStream);
                 return;
             }
-            // TODO: 메서드 분리, /login 요청이 아닐 때 쿼리파라미터 처리 고민
-            int questionMarkIndex = uri.indexOf("?");
-            if (questionMarkIndex != -1) {
-                path = uri.substring(0, questionMarkIndex) + ".html";
-
-                String queryString = uri.substring(questionMarkIndex + 1);
-                int ampersandIndex = queryString.indexOf("&");
-
-                String accountKeyValue = queryString.substring(0, ampersandIndex);
-                int accountEqualsIndex = accountKeyValue.indexOf("=");
-                String accountKey = accountKeyValue.substring(0, accountEqualsIndex);
-                String accountValue = accountKeyValue.substring(accountEqualsIndex + 1);
-                if ("account".equals(accountKey)) {
-                    InMemoryUserRepository.findByAccount(accountValue).ifPresent(user -> {
-                        String passwordKeyValue = queryString.substring(ampersandIndex + 1);
-                        int passwordEqualsIndex = passwordKeyValue.indexOf("=");
-                        String passwordKey = passwordKeyValue.substring(0, passwordEqualsIndex);
-                        String passwordValue = passwordKeyValue.substring(passwordEqualsIndex + 1);
-                        if ("password".equals(passwordKey) && user.checkPassword(passwordValue)) {
-                            log.info("user : {}", user);
-                        }
-                    });
-                }
+            
+            if (uri.startsWith("/login")) {
+                uri = getLoginFileUri(uri);
             }
-
-            var responseBody = getStaticFileContent(path);
-
+            var responseBody = getStaticFileContent(uri);
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/" + getFileExtension(path) + ";charset=utf-8 ",
+                    "Content-Type: text/" + getFileExtension(uri) + ";charset=utf-8 ",
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
                     responseBody);
-
-            outputStream.write(response.getBytes());
-            outputStream.flush();
+            writeResponse(outputStream, response);
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
@@ -94,6 +66,56 @@ public class Http11Processor implements Runnable, Processor {
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
         return bufferedReader.readLine().split(" ")[1];
+    }
+
+    private void handleFaviconRequest(OutputStream outputStream) throws IOException {
+        final var response = String.join("\r\n",
+                "HTTP/1.1 204 No Content",
+                "Content-Length: 0",
+                "",
+                "");
+        writeResponse(outputStream, response);
+    }
+
+    private String getLoginFileUri(String uri) {
+        int questionMarkIndex = uri.indexOf("?");
+        validateContainsQueryString(questionMarkIndex);
+        String fileName = uri.substring(0, questionMarkIndex) + ".html";
+        checkLoginQueryString(uri.substring(questionMarkIndex + 1));
+
+        return fileName;
+    }
+
+    private void validateContainsQueryString(int questionMarkIndex) {
+        if (questionMarkIndex == -1) {
+            throw new IllegalArgumentException("로그인 요청에 쿼리파라미터를 포함시켜주세요.");
+        }
+    }
+
+    private void checkLoginQueryString(String queryString) {
+        Map<String, String> queryStringPairs = getQueryStringPairs(queryString);
+
+        String account = queryStringPairs.get("account");
+        String password = queryStringPairs.get("password");
+        if (account != null & password != null) {
+            InMemoryUserRepository.findByAccount(account).ifPresent(user -> {
+                if ("password".equals(password)) {
+                    log.info("user : {}", user);
+                }
+            });
+        }
+    }
+
+    private Map<String, String> getQueryStringPairs(String queryString) {
+        Map<String, String> queryStringPairs = new HashMap<>();
+        for (String pairs : queryString.split("&")) {
+            String[] pair = pairs.split("=");
+            if (pair.length == 2) {
+                queryStringPairs.put(pair[0], pair[1]);
+            }
+        }
+
+        return queryStringPairs;
     }
 
     private String getStaticFileContent(String path) throws IOException {
@@ -112,5 +134,10 @@ public class Http11Processor implements Runnable, Processor {
         }
         String[] splitPath = path.split("\\.");
         return splitPath[splitPath.length - 1];
+    }
+
+    private void writeResponse(OutputStream outputStream, String response) throws IOException {
+        outputStream.write(response.getBytes());
+        outputStream.flush();
     }
 }

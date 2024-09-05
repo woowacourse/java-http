da0fc8051a119442b5313f79401e26b07d0cfb74
Nam Gi Beom <rlqja0523@naver.com>
2024-09-05 16:35:26 +0900

    feat: MimeType enum 추가

---
+++ tomcat/src/main/java/org/apache/coyote/response/MimeType.java
@@ -0,0 +1,28 @@
+package org.apache.coyote.response;
+
+import java.util.Arrays;
+
+public enum MimeType {
+
+    HTML("text/html"),
+    CSS("text/css"),
+    JS("application/javascript"),
+    SVG("image/svg+xml");
+
+    private final String type;
+
+    MimeType(String type) {
+        this.type = type;
+    }
+
+    public static MimeType from(String fileName) {
+        return Arrays.stream(values())
+                .filter(mimeType -> fileName.endsWith(mimeType.name().toLowerCase()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Unknown mime type for file: " + fileName));
+    }
+
+    public String getType() {
+        return this.type;
+    }
+}

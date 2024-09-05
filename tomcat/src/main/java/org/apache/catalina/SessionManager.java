38adcfcd2ec363f9dbd454deaf266e1e45a1e2ed
cutehumanS2 <oddpinkjadeite@gmail.com>
2024-09-05 09:49:12 +0900

    feat: Session 구현

---
+++ tomcat/src/main/java/org/apache/catalina/SessionManager.java
@@ -0,0 +1,33 @@
+package org.apache.catalina;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.coyote.http11.Session;
+
+public class SessionManager implements Manager {
+
+    private static final Map<String, Session> SESSIONS = new HashMap<>();
+    private static final SessionManager INSTANCE = new SessionManager();
+
+    private SessionManager() {}
+
+    public static SessionManager getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public void add(Session session) {
+        SESSIONS.put(session.getAttribute("account").toString(), session);
+    }
+
+    @Override
+    public Session findSession(String id) throws IOException {
+        return SESSIONS.get(id);
+    }
+
+    @Override
+    public void remove(Session session) {
+        SESSIONS.remove(session.getId());
+    }
+}

c1c133381f7fa13256240631514171e848fe4abe
PgmJun <84304802+PgmJun@users.noreply.github.com>
2024-09-05 16:18:46 +0900

    feat: 3. Query String 파싱 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,16 +1,18 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.http.HttpHeaders;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +24,9 @@ public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
     private static final String HEADER_ACCEPT = "Accept";
+    private static final String ACCOUNT = "account";
+    private static final String PASSWORD = "password";
+
 
     private final Socket connection;
 
@@ -42,11 +47,16 @@ public class Http11Processor implements Runnable, Processor {
             BufferedReader input = new BufferedReader(new InputStreamReader(inputStream));
 
             String[] httpRequest = input.readLine().split(" ");
-
+            Map<String, String> requestQueries = getQueries(httpRequest);
+            if (!requestQueries.isEmpty()) {
+                int queryIndex = httpRequest[1].indexOf("?");
+                httpRequest[1] = httpRequest[1].substring(0, queryIndex);
+            }
             Map<String, String> httpHeaders = getHttpHeaders(input);
+
             String accept = httpHeaders.get(HEADER_ACCEPT);
-            if(accept.contains("text/css")) {
-                final String responseBody = getFileValue(httpRequest);
+            if (accept.contains("text/css")) {
+                final String responseBody = getFileValue(httpRequest, accept);
                 final var response = String.join("\r\n",
                         "HTTP/1.1 200 OK ",
                         "Content-Type: text/css;charset=utf-8 ",
@@ -56,7 +66,7 @@ public class Http11Processor implements Runnable, Processor {
                 outputStream.write(response.getBytes());
                 outputStream.flush();
             } else {
-                final String responseBody = getFileValue(httpRequest);
+                final String responseBody = getFileValue(httpRequest, accept);
                 final var response = String.join("\r\n",
                         "HTTP/1.1 200 OK ",
                         "Content-Type: text/html;charset=utf-8 ",
@@ -66,27 +76,58 @@ public class Http11Processor implements Runnable, Processor {
 
                 outputStream.write(response.getBytes());
                 outputStream.flush();
+                if (!requestQueries.isEmpty()) {
+                    logUserExist(requestQueries);
+                }
             }
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
     }
 
+    private Map<String, String> getQueries(String[] httpRequest) {
+        Map<String, String> requestQueries = new HashMap<>();
+        if (httpRequest[1].contains("?")) {
+            int queryStartIndex = httpRequest[1].indexOf("?") + 1;
+            String requestQueryString = httpRequest[1].substring(queryStartIndex);
+            String[] queries = requestQueryString.split("&");
+
+            for (String query : queries) {
+                String[] q = query.split("=");
+                String key = q[0];
+                String value = q[1];
+
+                requestQueries.put(key, value);
+            }
+        }
+
+        return requestQueries;
+    }
+
     private Map<String, String> getHttpHeaders(BufferedReader input) throws IOException {
         Map<String, String> httpHeader = new HashMap<>();
 
-        String value = input.readLine();
-        while(!value.equals("")) {
-            String[] header = value.split(": ");
+        String line = input.readLine();
+        while (!line.equals("")) {
+            String[] header = line.split(": ");
             httpHeader.put(header[0], header[1]);
 
-            value = input.readLine();
+            line = input.readLine();
         }
 
         return httpHeader;
     }
 
-    private String getFileValue(String[] httpRequest) throws IOException {
+    private String getFileValue(String[] httpRequest, String accept) throws IOException {
+        if (!httpRequest[1].contains(".")) {
+            if (accept.contains("text/html")) {
+                httpRequest[1] = httpRequest[1] + ".html";
+            } else if (accept.contains("text/css")) {
+                httpRequest[1] = httpRequest[1] + ".css";
+            } else {
+                log.warn("'{}' Accept 타입을 처리해주세요", accept);
+            }
+        }
         String resourcePath = "static" + httpRequest[1];
         URL resource = getClass().getClassLoader().getResource(resourcePath);
         final Path path = new File(resource.getPath()).toPath();
@@ -95,4 +136,23 @@ public class Http11Processor implements Runnable, Processor {
         return responseBody;
     }
 
+    private void logUserExist(Map<String, String> requestQueries) {
+        String account = "";
+        String password = "";
+        for (Entry<String, String> entry : requestQueries.entrySet()) {
+            if (entry.getKey().equals(ACCOUNT)) {
+                account = entry.getValue();
+            } else if (entry.getKey().equals(PASSWORD)) {
+                password = entry.getValue();
+            }
+        }
+
+        User user = InMemoryUserRepository.findByAccount(account)
+                .orElseThrow(() -> new IllegalArgumentException("찾으시는 유저가 존재하지 않습니다."));
+        if (!user.checkPassword(password)) {
+            throw new IllegalArgumentException("찾으시는 유저가 존재하지 않습니다.");
+        }
+
+        log.info(user.toString());
+    }
 }

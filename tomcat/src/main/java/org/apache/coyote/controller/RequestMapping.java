7de1c2769fb572e1c12bd8cc3e0c6d2b40b47c9f
nayonsoso <panda0329@naver.com>
2024-09-05 22:28:26 +0900

    feat: 요청에 매칭되는 컨트롤러 반환 로직 구현

---
+++ tomcat/src/main/java/org/apache/coyote/controller/RequestMapping.java
@@ -0,0 +1,31 @@
+package org.apache.coyote.controller;
+
+import java.util.Objects;
+
+public class RequestMapping {
+
+    private final String httpMethod;
+    private final String path;
+
+    public RequestMapping(String httpMethod, String path) {
+        this.httpMethod = httpMethod;
+        this.path = path;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        RequestMapping that = (RequestMapping) o;
+        return Objects.equals(httpMethod, that.httpMethod) && Objects.equals(path, that.path);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(httpMethod, path);
+    }
+}
--- tomcat/src/main/java/org/apache/coyote/handler/HandlerMapping.java
+++ tomcat/src/main/java/org/apache/coyote/handler/HandlerMapping.java
@@ -1,12 +1,19 @@
 package org.apache.coyote.handler;
 
-import java.util.List;
+import java.util.Map;
+import org.apache.coyote.controller.Controller;
+import org.apache.coyote.controller.LogInController;
+import org.apache.coyote.controller.RequestMapping;
+import org.apache.coyote.http11.HttpRequest;
 
 public class HandlerMapping {
 
-    private static final List<String> nonStaticPaths = List.of("/login");
+    private static final Map<RequestMapping, Controller> HANDLER_MAPPER = Map.of(
+            new RequestMapping("GET", "/login"), new LogInController()
+    );
 
-    public static boolean isNonStaticRequest(String path) {
-        return nonStaticPaths.contains(path);
+    public Controller getController(HttpRequest request) {
+        return HANDLER_MAPPER.get(
+                new RequestMapping(request.getHttpMethod(), request.getPath()));
     }
 }

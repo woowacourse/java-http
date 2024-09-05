0d40b5ceb95d25bc3892aa96f3c9f260c69d4de4
임수빈 <robin980108@naver.com>
2024-09-05 10:44:06 +0900

    test: Http11QueryParserTest 추가

---
+++ tomcat/src/test/java/org/apache/coyote/http11/request/Http11QueryParserTest.java
@@ -0,0 +1,51 @@
+package org.apache.coyote.http11.request;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+
+import java.util.LinkedHashMap;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class Http11QueryParserTest {
+
+    @Test
+    @DisplayName("한개의 쿼리 스트링이 포함된 경우 잘 파싱하는지 확인")
+    void parseSingle() {
+        Http11QueryParser queryStringParser = new Http11QueryParser();
+
+        LinkedHashMap<String, String> result = queryStringParser.parse("/login?account=1");
+
+        assertThat(result).containsExactly(entry("account", "1"));
+    }
+
+    @Test
+    @DisplayName("여러개의 쿼리 스트링이 포함된 경우 잘 파싱하는지 확인")
+    void parseMany() {
+        Http11QueryParser queryStringParser = new Http11QueryParser();
+
+        LinkedHashMap<String, String> result = queryStringParser.parse("/login?account=1&password=1234");
+
+        assertThat(result).containsExactly(entry("account", "1"), entry("password", "1234"));
+    }
+
+    @Test
+    @DisplayName("쿼리 스트링이 없는 경우 빈 Map을 반환하는지 확인")
+    void parseNone() {
+        Http11QueryParser queryStringParser = new Http11QueryParser();
+
+        LinkedHashMap<String, String> result = queryStringParser.parse("/login");
+
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    @DisplayName("쿼리 스트링이 잘못된 형식으로 있는 경우 가능한 부분만 파싱하는지 확인")
+    void parseWeired() {
+        Http11QueryParser queryStringParser = new Http11QueryParser();
+
+        LinkedHashMap<String, String> result = queryStringParser.parse("/login?account=&password=1");
+
+        assertThat(result).containsExactly(entry("password", "1"));
+    }
+}

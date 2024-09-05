d2f9c1c6987eb4530ccbfc7a013417bfafa71889
Gyeongho Yang <chchch1213@naver.com>
2024-09-05 17:34:12 +0900

    test: add reading page tests

---
+++ tomcat/src/test/java/com/techcourse/controller/HomeControllerTest.java
@@ -0,0 +1,36 @@
+package com.techcourse.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.apache.coyote.http11.HttpRequest;
+import org.apache.coyote.http11.HttpResponse;
+import org.apache.coyote.http11.HttpResponse.Builder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class HomeControllerTest {
+
+    @Test
+    @DisplayName("안녕세상 페이지를 조회한다.")
+    void viewHomePage() {
+        // given
+        RequestMapping requestMapping = new RequestMapping();
+        HttpRequest request = HttpRequest.parse(List.of(
+                "GET / HTTP/1.1"
+        ));
+        Builder responseBuilder = HttpResponse.builder();
+
+        // when
+        requestMapping.getController(request)
+                .service(request, responseBuilder);
+        HttpResponse response = responseBuilder.build();
+
+        // then
+        assertThat(response.toMessage())
+                .contains("HTTP/1.1 200 OK".getBytes())
+                .contains("Content-Type: text/html".getBytes())
+                .contains("Content-Length: 12".getBytes())
+                .contains("Hello world!".getBytes());
+    }
+}
---
+++ tomcat/src/test/java/com/techcourse/controller/LoginControllerTest.java
@@ -0,0 +1,35 @@
+package com.techcourse.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.apache.coyote.http11.HttpRequest;
+import org.apache.coyote.http11.HttpResponse;
+import org.apache.coyote.http11.HttpResponse.Builder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LoginControllerTest {
+
+    @Test
+    @DisplayName("로그인 페이지를 조회한다.")
+    void viewLoginPage() {
+        // given
+        RequestMapping requestMapping = new RequestMapping();
+        Builder responseBuilder = HttpResponse.builder();
+        HttpRequest request = HttpRequest.parse(List.of(
+                "GET /login HTTP/1.1"
+        ));
+
+        // when
+        requestMapping.getController(request)
+                .service(request, responseBuilder);
+        HttpResponse response = responseBuilder.build();
+
+        // then
+        assertThat(response.toMessage())
+                .contains("HTTP/1.1 200 OK".getBytes())
+                .contains("Content-Type: text/html".getBytes())
+                .contains("<title>로그인</title>".getBytes());
+    }
+}

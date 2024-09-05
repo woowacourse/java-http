b0c8d134b5b9b9070b1a54c685d32cb62ecf194e
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-09-05 20:03:16 +0900

    feat: Process 내 Session 받아오는 기능 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,13 +1,14 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
-import com.techcourse.executor.LoginExecutor;
-import com.techcourse.executor.PageExecutor;
+import com.techcourse.executor.*;
 import org.apache.coyote.Processor;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.request.HttpRequestParser;
 import org.apache.coyote.http11.response.HttpResponse;
 import org.apache.coyote.http11.response.HttpResponseWriter;
+import org.apache.coyote.http11.session.Session;
+import org.apache.coyote.http11.session.SessionManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,12 +22,15 @@ public class Http11Processor implements Runnable, Processor {
 
     private final Socket connection;
     private final List<Executor> executors;
-    private final Executor pageExecutor = new PageExecutor();
+    private final Executor pageExecutor = new ResourceExecutor();
+    private final SessionManager sessionManager = new SessionManager();
 
 
     public Http11Processor(final Socket connection) {
         this.connection = connection;
-        this.executors = List.of(new LoginExecutor());
+        this.executors = List.of(new LoginGetExecutor(), new RegisterGetExecutor(), new RegisterPostExecutor(),
+                new LoginPostExecutor()
+        );
     }
 
     @Override
@@ -41,17 +45,30 @@ public class Http11Processor implements Runnable, Processor {
              final var outputStream = connection.getOutputStream()) {
 
             final HttpRequest httpRequest = HttpRequestParser.parse(inputStream);
-
+            setSession(httpRequest);
+            log.info("요청 [HTTP 메소드: {}, 경로: {}", httpRequest.getMethod(), httpRequest.getPath());
             final HttpResponse response = executors.stream()
                     .filter(executor -> executor.isMatch(httpRequest))
                     .findFirst()
                     .map(executor -> executor.execute(httpRequest))
                     .orElseGet(() -> pageExecutor.execute(httpRequest));
 
+            log.info("응답 [HTTP 결과: {}, 헤더: {}", response.getStatusLine(), response.getHeaders());
             HttpResponseWriter.write(outputStream, response);
             outputStream.flush();
         } catch (final IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private void setSession(final HttpRequest httpRequest) {
+
+        sessionManager.findSession(httpRequest.getCookie("JSESSIONID"))
+                .ifPresentOrElse(httpRequest::setSession,
+                        () -> {
+                            final Session session = new Session();
+                            sessionManager.add(session);
+                            httpRequest.setSession(session);
+                        });
     }
 }

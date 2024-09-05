b85e6eab25c13eefce08762c3e1693d2e97cead7
coli-geonwoo <coli-geonwoo@gmail.com>
2024-09-05 10:18:31 +0900

    feat: requests 응답 분기 처리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,16 +1,30 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
-
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    public static final String DEFAULT_DIRECTORY_NAME = "static";
 
     private final Socket connection;
 
@@ -26,22 +40,105 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
-            // TODO : 요청 메시지를 읽고 처리한다
-            final var responseBody = "Hello world!";
+        try (InputStream inputStream = connection.getInputStream();
+             OutputStream outputStream = connection.getOutputStream()) {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String requestLine = bufferedReader.readLine();
 
-            final var response = String.join("\r\n",
+            if (requestLine == null) {
+                return;
+            }
+
+            if (isLogin(requestLine)) {
+                checkLogin(requestLine);
+            }
+
+            Map<String, String> requestHeaders = makeRequestHeaders(bufferedReader);
+            String[] s = requestLine.split(" ");
+            String body = makeResponseBody(s[1]);
+
+            String response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "Content-Type: " + findResponseContentType(s[1]) + ";charset=utf-8 ",
+                    "Content-Length: " + body.getBytes().length + " ",
                     "",
-                    responseBody);
+                    body);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
         }
+    }
+
+    private Map<String, String> makeRequestHeaders(BufferedReader bufferedReader) throws IOException {
+        Map<String, String> requestHeaders = new HashMap<>();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            if (line.isBlank()) {
+                break;
+            }
+            String[] split = line.split(":");
+            String key = split[0].trim();
+            String value = split[1].trim();
+            requestHeaders.put(key, value);
+        }
+        return requestHeaders;
+    }
+
+    private String makeResponseBody(String requestUrl) throws IOException {
+        final Path path = findPath(requestUrl);
+        return Files.readString(path);
+    }
+
+    private Path findPath(String url) {
+        try {
+            if (url.equals("/")) {
+                URL url1 = getClass().getClassLoader().getResource(DEFAULT_DIRECTORY_NAME + "/index.html");
+                return Path.of(Objects.requireNonNull(url1).toURI());
+            }
+
+            if (url.contains("?")) {
+                url = url.substring(0, url.indexOf("?")) + ".html";
+            }
+
+            URL foundUrl = getClass().getClassLoader().getResource(DEFAULT_DIRECTORY_NAME + url);
+            return Path.of(Objects.requireNonNull(foundUrl).toURI());
+        } catch (URISyntaxException e) {
+            throw new UncheckedServletException(e);
+        }
+    }
+
+    private String findResponseContentType(String url) {
+        Path path = findPath(url);
+        return "text/" + path.toString().split("\\.")[1];
+    }
+
+    private String findResponseContentType2(Map<String, String> requestHeaders) {
+        return requestHeaders.get("Accept").split(",")[0];
+    }
+
+    private boolean isLogin(String uri) {
+        return uri.contains("login");
+    }
+
+    private void checkLogin(String uri) {
+        int index = uri.indexOf("?");
+        String queryString = uri.substring(index + 1);
+
+        String[] elements = queryString.split("&");
+        Map<String, String> map = new HashMap<>();
+
+        for (String element : elements) {
+            String[] k = element.split("=");
+            map.put(k[0], k[1]);
+        }
+        String account = map.get("account");
+        User user = InMemoryUserRepository.findByAccount(account)
+                .orElseThrow(NoSuchElementException::new);
+
+        System.out.println("user.getAccount() = " + user.getAccount());
+        log.info("user : {}", user);
     }
 }

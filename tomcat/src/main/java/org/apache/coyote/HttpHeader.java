113c1f2814667c4e9a891e9d7ed3a0e5c879e973
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-09-05 21:51:29 +0900

    feat(HttpHeader): Header 객체 분리

---
+++ tomcat/src/main/java/org/apache/coyote/HttpHeader.java
@@ -0,0 +1,19 @@
+package org.apache.coyote;
+
+public class HttpHeader {
+    private final String key;
+    private final String value;
+
+    public HttpHeader(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}

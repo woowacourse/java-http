13f3d634bd01958a2a3037366790bcb94e4e46be
BurningFalls <sherlocky31415@gmail.com>
2024-09-06 01:23:42 +0900

    test: login 리다이렉트 성공 실패 테스트 구현

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -105,8 +105,8 @@ class Http11ProcessorTest {
         final URL resource = getClass().getClassLoader().getResource("static/login.html");
         assert resource != null;
         var expected = "HTTP/1.1 200 OK \r\n" +
-                "Content-Type: text/html;charset=utf-8 \r\n" +
                 "Content-Length: " + getFileSize(resource) + " \r\n" +
+                "Content-Type: text/html;charset=utf-8 \r\n" +
                 "\r\n" +
                 new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
 
@@ -114,7 +114,7 @@ class Http11ProcessorTest {
     }
 
     @Test
-    void loginWithQueryString() throws IOException {
+    void loginSuccessWithQueryString() throws IOException {
         // given
         final String httpRequest = String.join("\r\n",
                 "GET /login?account=gugu&password=password HTTP/1.1",
@@ -128,13 +128,31 @@ class Http11ProcessorTest {
         processor.process(socket);
 
         // then
-        final URL resource = getClass().getClassLoader().getResource("static/login.html");
-        assert resource != null;
-        var expected = "HTTP/1.1 200 OK \r\n" +
-                "Content-Type: text/html;charset=utf-8 \r\n" +
-                "Content-Length: " + getFileSize(resource) + " \r\n" +
-                "\r\n" +
-                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+        var expected = "HTTP/1.1 302 Found \r\n" +
+                "Location: /index.html \r\n" +
+                "\r\n";
+
+        assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @Test
+    void loginFailWithQueryString() {
+        // given
+        final String httpRequest = String.join("\r\n",
+                "GET /login?account=gugu&password=gugu HTTP/1.1",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ");
+
+        final var socket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        var expected = "HTTP/1.1 302 Found \r\n" +
+                "Location: /401.html \r\n" +
+                "\r\n";
 
         assertThat(socket.output()).isEqualTo(expected);
     }

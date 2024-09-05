e5cd64380f2e8b60d823506b83141ae6d1d10a70
Seokmyung Ham <gkatjraud1@gmail.com>
2024-09-05 16:13:16 +0900

    feat: 로그인 시 성공, 실패 여부에 따라 리다이렉트 처리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -11,6 +11,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.URL;
+import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
@@ -65,17 +66,10 @@ public class Http11Processor implements Runnable, Processor {
 
                 if (!"/".equals(uri)) {
                     String path = uri;
-                    String queryString = null;
-                    Map<String, String> params = new HashMap<>();
 
                     if (path.contains("?")) {
                         int index = uri.indexOf("?");
                         path = uri.substring(0, index);
-                        queryString = uri.substring(index + 1);
-
-                        for (String param : queryString.split("&")) {
-                            params.put(param.split("=")[0], param.split("=")[1]);
-                        }
                     }
 
                     String resourcePath = "static" + path;
@@ -89,8 +83,48 @@ public class Http11Processor implements Runnable, Processor {
                         responseBody = new String(Files.readAllBytes(new File(resource.get().getFile()).toPath()));
 
                         if ("/login".equals(path)) {
-                            Optional<User> user = InMemoryUserRepository.findByAccount(params.get("account"));
-                            user.ifPresent(u -> log.info(u.toString()));
+                            if (uri.contains("?")) {
+                                Map<String, String> params = new HashMap<>();
+                                String queryString = uri.substring(uri.indexOf("?") + 1);
+
+                                for (String param : queryString.split("&")) {
+                                    String[] keyValue = param.split("=");
+
+                                    String key = URLDecoder.decode(keyValue[0], StandardCharsets.UTF_8);
+                                    String value = URLDecoder.decode(keyValue[1], StandardCharsets.UTF_8);
+                                    params.put(key, value);
+                                }
+
+                                Optional<User> optionalUser = InMemoryUserRepository.findByAccount(
+                                        params.get("account"));
+
+                                if (optionalUser.isPresent()) {
+                                    User user = optionalUser.get();
+                                    if (user.checkPassword(params.get("password"))) {
+                                        log.info("user : {}", user);
+
+                                        String response = String.join("\r\n",
+                                                "HTTP/1.1 302 FOUND ",
+                                                "Location: /index.html ",
+                                                "Content-Length: 0 ",
+                                                "");
+
+                                        bufferedWriter.write(response);
+                                        bufferedWriter.flush();
+                                        return;
+                                    }
+                                }
+
+                                String response = String.join("\r\n",
+                                        "HTTP/1.1 302 FOUND ",
+                                        "Location: /401.html ",
+                                        "Content-Length: 0 ",
+                                        "");
+
+                                bufferedWriter.write(response);
+                                bufferedWriter.flush();
+                                return;
+                            }
                         }
                     }
                 }

f015d37706dd3ec0b4b169e327931a61a475a8f5
unifolio0 <boho82ha@gmail.com>
2024-09-05 17:14:17 +0900

    refactor: 메서드 분리

--- tomcat/src/main/java/org/apache/coyote/http11/HttpRequestConvertor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpRequestConvertor.java
@@ -13,6 +13,7 @@ public class HttpRequestConvertor {
             if (firstLine == null) {
                 throw new RuntimeException("요청이 비어 있습니다.");
             }
+
             String[] headerFirstLine = firstLine.split(" ");
             HttpMethod method = HttpMethod.getHttpMethod(headerFirstLine[0]);
             String path = headerFirstLine[1];
@@ -20,24 +21,34 @@ public class HttpRequestConvertor {
                 path += ".html";
             }
             String version = headerFirstLine[2];
-            String line;
-            Map<String, String> headers = new HashMap<>();
-            while ((line = bufferedReader.readLine()) != null && !line.isEmpty()) {
-                String[] requestLine = line.split(": ");
-                headers.put(requestLine[0], requestLine[1]);
-            }
+            Map<String, String> headers = getHeaders(bufferedReader);
+
             HttpRequestHeader httpRequestHeader = new HttpRequestHeader(method, path, version, headers);
+
             if (httpRequestHeader.containsKey("Content-Length")) {
                 int contentLength = Integer.parseInt(httpRequestHeader.getValue("Content-Length"));
                 char[] buffer = new char[contentLength];
                 bufferedReader.read(buffer, 0, contentLength);
                 String body = new String(buffer);
                 HttpRequestBody httpRequestBody = new HttpRequestBody(body);
+
                 return new HttpRequest(httpRequestHeader, httpRequestBody);
             }
+
             return new HttpRequest(httpRequestHeader);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static Map<String, String> getHeaders(BufferedReader bufferedReader) throws IOException {
+        String line;
+        Map<String, String> headers = new HashMap<>();
+        while ((line = bufferedReader.readLine()) != null && !line.isEmpty()) {
+            String[] requestLine = line.split(": ");
+            headers.put(requestLine[0], requestLine[1]);
+        }
+
+        return headers;
     }
 }

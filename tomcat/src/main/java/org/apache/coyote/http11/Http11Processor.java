9d8fc8561f321d3e6e67f73c388a1ab4402cdb00
hoyeonyy <highoption@naver.com>
2024-09-05 11:38:53 +0900

    feat: GET /index.html 응답하기 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,8 +5,19 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,19 +40,52 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            String firstLine = new BufferedReader(inputStreamReader).readLine();
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            StringTokenizer stringTokenizer = new StringTokenizer(firstLine);
 
-            outputStream.write(response.getBytes());
-            outputStream.flush();
+            String method = stringTokenizer.nextToken();
+            String requestUrl = stringTokenizer.nextToken();
+
+            if(method.equals("GET") && requestUrl.equals("/")){
+                responseRoot(outputStream);
+            }
+            if(method.equals("GET") && requestUrl.equals("/index.html")){
+                responseIndex(outputStream);
+            }
+
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private void responseIndex(OutputStream outputStream) throws IOException {
+        URL url = getClass().getClassLoader().getResource("static/index.html");
+        String responseBody = new String(Files.readAllBytes(new File(url.getFile()).toPath()));
+
+        final var response = String.join("\r\n",
+            "HTTP/1.1 200 OK ",
+            "Content-Type: text/html;charset=utf-8 ",
+            "Content-Length: " + responseBody.getBytes().length + " ",
+            "",
+            responseBody);
+
+        outputStream.write(response.getBytes());
+        outputStream.flush();
+    }
+
+    private void responseRoot(OutputStream outputStream) throws IOException {
+        final var responseBody = "Hello world!";
+
+        final var response = String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+
+        outputStream.write(response.getBytes());
+        outputStream.flush();
     }
 }

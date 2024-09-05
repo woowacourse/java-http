e6968ed227cef710b47a34bafca85992e25e4405
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-09-05 14:19:04 +0900

    feat: 정적 메소드로 변경

--- tomcat/src/test/java/org/apache/coyote/http11/ResourcesReaderTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/ResourcesReaderTest.java
@@ -1,5 +1,8 @@
 package org.apache.coyote.http11;
 
+import org.apache.coyote.file.ClassLoaderContext;
+import org.apache.coyote.file.Resource;
+import org.apache.coyote.file.ResourcesReader;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,8 +15,7 @@ class ResourcesReaderTest {
     @Test
     @DisplayName("static 디렉토리를 기본으로 파일을 찾는다.")
     void read_static_path_default() throws IOException {
-        final ResourcesReader reader = new ResourcesReader();
-        final Resource resource = reader.read("/main.html");
+        final Resource resource = ResourcesReader.read("/main.html");
 
 
         final byte[] content = ClassLoaderContext.getResourceAsStream("static/main.html")

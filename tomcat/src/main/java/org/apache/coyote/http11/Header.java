7eb2844e25b95410fa17785ea65345e0edb563c9
le2sky <leehaneul990623@gmail.com>
2024-09-06 12:36:27 +0900

    chore: import문 최적화

--- tomcat/src/main/java/org/apache/coyote/http11/Header.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Header.java
@@ -4,7 +4,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 
 public class Header {

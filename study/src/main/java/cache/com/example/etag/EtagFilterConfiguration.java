d52c5d1759f8afdbcdcefb8323ae98900d28b679
devhoya97 <diajbh123@gmail.com>
2024-09-06 23:57:50 +0900

    refactor: 학습테스트 코드에서 VersionHandlebarsHelper를 사용하도록 수정

--- study/src/main/java/cache/com/example/etag/EtagFilterConfiguration.java
+++ study/src/main/java/cache/com/example/etag/EtagFilterConfiguration.java
@@ -1,30 +1,28 @@
 package cache.com.example.etag;
 
-import static cache.com.example.version.CacheBustingWebConfig.PREFIX_STATIC_RESOURCES;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
-import cache.com.example.version.ResourceVersion;
+import cache.com.example.version.VersionHandlebarsHelper;
 
 @Configuration
 public class EtagFilterConfiguration {
 
-    private final ResourceVersion version;
+    private final VersionHandlebarsHelper versionHandlebarsHelper;
 
     @Autowired
-    public EtagFilterConfiguration(ResourceVersion version) {
-        this.version = version;
+    public EtagFilterConfiguration(VersionHandlebarsHelper versionHandlebarsHelper) {
+        this.versionHandlebarsHelper = versionHandlebarsHelper;
     }
 
     @Bean
     public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
         FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean = new FilterRegistrationBean<>(new ShallowEtagHeaderFilter());
         filterRegistrationBean.addUrlPatterns("/etag");
-        filterRegistrationBean.addUrlPatterns("/resources/" + version.getVersion() + "/js/index.js");
+        filterRegistrationBean.addUrlPatterns(versionHandlebarsHelper.staticUrls("/*", null));
 
         return filterRegistrationBean;
     }

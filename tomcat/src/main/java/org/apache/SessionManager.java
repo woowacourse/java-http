0802b5a626cbb76baf3c3c63a5b0a216969d48e0
Nam Gi Beom <rlqja0523@naver.com>
2024-09-06 15:21:40 +0900

    refactor: singleton으로 변경

--- tomcat/src/main/java/org/apache/SessionManager.java
+++ tomcat/src/main/java/org/apache/SessionManager.java
@@ -8,10 +8,21 @@ public class SessionManager implements Manager {
 
     private static final Map<String, Session> sessions = new HashMap<>();
 
+    private static SessionManager instance;
+
+    private SessionManager() {
+    }
+
+    public static SessionManager getInstance() {
+        if (instance == null) {
+            instance = new SessionManager();
+        }
+        return instance;
+    }
+
     @Override
     public void add(Session session) {
         sessions.put(session.getId(), session);
-
     }
 
     @Override

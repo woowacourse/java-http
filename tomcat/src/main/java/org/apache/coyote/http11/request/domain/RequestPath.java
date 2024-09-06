1d396f0857d80bf05e464feaea4bb21e66d867b7
Eugene Jang <kyunellroll@gmail.com>
2024-09-06 17:17:38 +0900

    feat(RequestPath): 경로를 담는 RequestPath 작성
    - 경로에서 확장자를 가져오는지 테스트
    - 경로에서 리소스 파일의 경로를 불러올 수 있는지 테스트

---
+++ tomcat/src/main/java/org/apache/coyote/http11/request/domain/RequestPath.java
@@ -0,0 +1,40 @@
+package org.apache.coyote.http11.request.domain;
+
+public class RequestPath {
+
+    private static final String DEFAULT_RESOURCE_PATH = "static";
+
+    private final String path;
+
+    public RequestPath(String path) {
+        if (path == null) {
+            throw new NullPointerException("경로가 존재하지 않습니다.");
+        }
+        this.path = path.trim();
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getQueryString() {
+        int index = path.indexOf("?");
+
+        return path.substring(index + 1);
+    }
+
+    public boolean isDefaultPath() {
+        return "/".equals(path);
+    }
+
+    public String findExtension() {
+        if (!path.contains(".")) {
+            throw new IllegalArgumentException("확장자를 찾을 수 없습니다.");
+        }
+        return path.split("\\.")[1];
+    }
+
+    public String toResourcePath() {
+        return DEFAULT_RESOURCE_PATH + path;
+    }
+}
--- tomcat/src/test/java/org/apache/coyote/http11/domain/RequestPathTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/domain/RequestPathTest.java
@@ -1,7 +1,10 @@
 package org.apache.coyote.http11.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.apache.coyote.http11.request.domain.RequestPath;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,5 +14,37 @@ class RequestPathTest {
     @DisplayName("경로가 Null인 경우 에러를 발생한다.")
     void getPath() {
         assertThrows(NullPointerException.class, () -> new RequestPath(null));
+    }
+
+    @Test
+    @DisplayName("경로에서 Query만 뽑아온다.")
+    void getQueryString() {
+        RequestPath requestPath = new RequestPath("/login?account=gugu&password=password");
+
+        assertEquals(requestPath.getQueryString(), "account=gugu&password=password");
+    }
+
+    @Test
+    @DisplayName("경로가 / 일 경우 true를 반환한다.")
+    void isDefaultPath() {
+        RequestPath requestPath = new RequestPath("/");
+
+        assertTrue(requestPath.isDefaultPath());
+    }
+
+    @Test
+    @DisplayName("파일 확장자를 가져온다.")
+    void findExtension() {
+        RequestPath requestPath = new RequestPath("/index.html");
+
+        assertEquals(requestPath.findExtension(), "html");
+    }
+
+    @Test
+    @DisplayName("정적 파일 리소스의 경로를 붙여 가져온다.")
+    void toResourcePath() {
+        RequestPath requestPath = new RequestPath("/index.html");
+
+        assertEquals(requestPath.toResourcePath(), "static/index.html");
     }
 }

67c38f4d02bbab8df056395d3fc6ca4a21ce1fb5
임수빈 <robin980108@naver.com>
2024-09-06 14:34:57 +0900

    학습 테스트 - 휴리스틱 캐싱 제거하기 구현

--- study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
+++ study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
@@ -2,7 +2,6 @@ package cache.com.example.cachecontrol;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.time.Duration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,15 +13,12 @@ public class CacheWebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
-        registry.addInterceptor(new CacheControlInterceptor())
-                .addPathPatterns("/**/*.js", "/**/*.css");
-    }
-
-    private static class CacheControlInterceptor implements HandlerInterceptor {
-        @Override
-        public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-                               ModelAndView modelAndView) throws Exception {
-            response.setHeader("Cache-Control", "max-age=" + Duration.ofDays(365).getSeconds() + ", public");
-        }
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                                   ModelAndView modelAndView) throws Exception {
+                response.setHeader("Cache-Control", "no-cache, private");
+            }
+        });
     }
 }

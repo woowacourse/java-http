ccffb6b3005a5335a68aae26867d172da049b0ab
tkdgur0906 <tkdgur0906@naver.com>
2024-09-06 23:39:53 +0900

    feat: 3단계 캐시 무효화 적용

--- study/src/main/java/cache/com/example/etag/EtagFilterConfiguration.java
+++ study/src/main/java/cache/com/example/etag/EtagFilterConfiguration.java
@@ -1,18 +1,28 @@
 package cache.com.example.etag;
 
+import cache.com.example.version.ResourceVersion;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
+import static cache.com.example.version.CacheBustingWebConfig.PREFIX_STATIC_RESOURCES;
+
 @Configuration
 public class EtagFilterConfiguration {
+
+    private final ResourceVersion resourceVersion;
+
+    public EtagFilterConfiguration(ResourceVersion resourceVersion) {
+        this.resourceVersion = resourceVersion;
+    }
 
     @Bean
     public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
         FilterRegistrationBean<ShallowEtagHeaderFilter> filterFilterRegistrationBean
                 = new FilterRegistrationBean<>(new ShallowEtagHeaderFilter());
         filterFilterRegistrationBean.addUrlPatterns("/etag");
+        filterFilterRegistrationBean.addUrlPatterns(PREFIX_STATIC_RESOURCES + "/" + resourceVersion.getVersion() + "/*");
         return filterFilterRegistrationBean;
     }
 }

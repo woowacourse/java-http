c9dd25153e779c2eddbbc295baa811a9f9dc9c0b
nhlee98 <e5slnh9876@gmail.com>
2024-09-06 16:35:01 +0900

    feat: 학습 테스트 2 구현

--- study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
+++ study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
@@ -1,13 +1,34 @@
 package cache.com.example.cachecontrol;
 
+import cache.com.example.version.ResourceVersion;
+import java.time.Duration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
 @Configuration
 public class CacheWebConfig implements WebMvcConfigurer {
 
+    private final ResourceVersion version;
+
+    @Autowired
+    public CacheWebConfig(ResourceVersion version) {
+        this.version = version;
+    }
+
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        WebContentInterceptor webContentInterceptor = new WebContentInterceptor();
+        webContentInterceptor.addCacheMapping(CacheControl.noCache().cachePrivate(), "/**");
+        webContentInterceptor.addCacheMapping(CacheControl.maxAge(Duration.ofDays(365L)), "/etag");
+//        webContentInterceptor.addCacheMapping(CacheControl.maxAge(Duration.ofDays(365L)).cachePublic(),
+//                "/resource-versioning");
+        webContentInterceptor.addCacheMapping(CacheControl.maxAge(Duration.ofDays(365L)).cachePublic(),
+                "/resources/" + version.getVersion() + "/**");
+
+        registry.addInterceptor(webContentInterceptor);
     }
 }
--- study/src/main/java/cache/com/example/etag/EtagFilterConfiguration.java
+++ study/src/main/java/cache/com/example/etag/EtagFilterConfiguration.java
@@ -1,12 +1,30 @@
 package cache.com.example.etag;
 
+import cache.com.example.version.ResourceVersion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @Configuration
 public class EtagFilterConfiguration {
 
-//    @Bean
-//    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
-//        return null;
-//    }
+    private final ResourceVersion version;
+
+    @Autowired
+    public EtagFilterConfiguration(ResourceVersion version) {
+        this.version = version;
+    }
+
+
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        FilterRegistrationBean<ShallowEtagHeaderFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new ShallowEtagHeaderFilter());
+        registrationBean.addUrlPatterns("/etag");
+        registrationBean.addUrlPatterns("/resource-versioning");
+        registrationBean.addUrlPatterns("/resources/" + version.getVersion() + "/*");
+        return registrationBean;
+    }
 }

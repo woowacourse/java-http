b546c128faa23dfae8d7f757365454a27f40ae66
pricelees <priceelees@gmail.com>
2024-09-06 16:19:05 +0900

    feat: Http11Processor 에 모든 작업 내용 반영

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,47 +1,50 @@
 package org.apache.coyote.http11;
 
-import com.techcourse.exception.UncheckedServletException;
+import java.io.IOException;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
+import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.web.HttpResponse;
+import com.techcourse.web.handler.RequestHandler;
+import com.techcourse.web.handler.RequestHandlerMapper;
 
 public class Http11Processor implements Runnable, Processor {
 
-    private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+	private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
 
-    private final Socket connection;
+	private final Socket connection;
+	private final RequestHandlerMapper handlerMapper;
 
-    public Http11Processor(final Socket connection) {
-        this.connection = connection;
-    }
+	public Http11Processor(final Socket connection) {
+		this.connection = connection;
+		this.handlerMapper = RequestHandlerMapper.getInstance();
+	}
 
-    @Override
-    public void run() {
-        log.info("connect host: {}, port: {}", connection.getInetAddress(), connection.getPort());
-        process(connection);
-    }
+	@Override
+	public void run() {
+		log.info("connect host: {}, port: {}", connection.getInetAddress(), connection.getPort());
+		process(connection);
+	}
 
-    @Override
-    public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+	@Override
+	public void process(final Socket connection) {
+		try (final var inputStream = connection.getInputStream();
+			 final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+			HttpRequest request = HttpRequestReader.read(inputStream);
+			RequestHandler handler = handlerMapper.findHandler(request.getRequestPath());
+			HttpResponse response = handler.handle(request);
+			String httpResponseMessage = response.createResponseMessage();
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
-
-            outputStream.write(response.getBytes());
-            outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
-            log.error(e.getMessage(), e);
-        }
-    }
+			outputStream.write(httpResponseMessage.getBytes(StandardCharsets.UTF_8));
+			outputStream.flush();
+		} catch (IOException | UncheckedServletException e) {
+			log.error(e.getMessage(), e);
+		}
+	}
 }

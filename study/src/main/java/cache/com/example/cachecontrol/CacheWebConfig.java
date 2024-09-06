21d2396a74604f13921780e526e1c50f4abf483f
devhoya97 <diajbh123@gmail.com>
2024-09-06 13:28:46 +0900

    test: 휴리스틱 캐시 제거하기 학습테스트

--- study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
+++ study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
@@ -1,6 +1,11 @@
 package cache.com.example.cachecontrol;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,5 +14,6 @@ public class CacheWebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(new CacheControlInterceptor()).addPathPatterns("/");
     }
 }

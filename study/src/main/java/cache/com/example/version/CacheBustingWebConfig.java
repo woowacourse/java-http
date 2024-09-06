3bf28e4c9c13fba5079091890e248a45a4f82a8d
김민수 MINSOO KIM <alstn113@gmail.com>
2024-09-06 20:15:59 +0900

    chore: 사용하지 않는 import 제거

--- study/src/main/java/cache/com/example/version/CacheBustingWebConfig.java
+++ study/src/main/java/cache/com/example/version/CacheBustingWebConfig.java
@@ -1,10 +1,7 @@
 package cache.com.example.version;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-import cache.com.example.etag.EtagFilterConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.Cache;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;

3d706a8f1ca6bc11e3c66835337381202be01c3c
Nam Gi Beom <rlqja0523@naver.com>
2024-09-06 15:23:45 +0900

    feat: 이미 로그인되었을 때 리다이렉트하는 기능 추가

--- tomcat/src/main/java/com/techcourse/presentation/LoginController.java
+++ tomcat/src/main/java/com/techcourse/presentation/LoginController.java
@@ -2,7 +2,10 @@ package com.techcourse.presentation;
 
 import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.model.User;
+import java.util.Optional;
 import java.util.UUID;
+import org.apache.Session;
+import org.apache.SessionManager;
 import org.apache.coyote.request.Request;
 import org.apache.coyote.response.Response;
 import org.apache.coyote.response.StatusCode;
@@ -15,7 +18,10 @@ public class LoginController {
 
     private static LoginController INSTANCE;
 
+    private final SessionManager sessionManager;
+
     private LoginController() {
+        this.sessionManager = SessionManager.getInstance();
     }
 
     public static LoginController getInstance() {
@@ -26,26 +32,38 @@ public class LoginController {
     }
 
     public void getLogin(Request request, Response response) {
+        if (isAlreadyLoggedIn(request)) {
+            response.configureViewAndStatus("/index", StatusCode.FOUND);
+            return;
+        }
         if (request.existQueryParams()) {
             String account = request.getQueryParamValue("account");
             String password = request.getQueryParamValue("password");
-            postLogin(account, password, response);
+            login(account, password, response);
+            return;
         }
         response.configureViewAndStatus("/login", StatusCode.OK);
+    }
+
+    private boolean isAlreadyLoggedIn(Request request) {
+        Optional<String> sessionId = request.findJSessionId();
+        Session session = sessionId.map(sessionManager::findSession).orElse(null);
+        return session != null && session.getAttribute("user") != null;
     }
 
     public void postLogin(Request request, Response response) {
         String account = request.getBodyValue("account");
         String password = request.getBodyValue("password");
-        postLogin(account, password, response);
+        login(account, password, response);
     }
 
-    private void postLogin(String account, String password, Response response) {
+    private void login(String account, String password, Response response) {
         try {
             User user = getUser(account, password);
-            log.info("로그인 성공! 아이디 : {}", user.getAccount());
-            response.setJsessionid(UUID.randomUUID().toString());
+            Session session = createSession(user);
+            response.setJsessionid(session.getId());
             response.configureViewAndStatus("/index", StatusCode.FOUND);
+            log.info("로그인 성공! 아이디 : {}", user.getAccount());
         } catch (IllegalArgumentException e) {
             response.configureViewAndStatus("/401", StatusCode.UNAUTHORIZED);
         }
@@ -58,5 +76,12 @@ public class LoginController {
             throw new IllegalArgumentException("계정 또는 비밀번호가 일치하지 않습니다.");
         }
         return user;
+    }
+
+    private Session createSession(User user) {
+        Session session = new Session(UUID.randomUUID().toString());
+        session.setAttribute("user", user);
+        sessionManager.add(session);
+        return session;
     }
 }

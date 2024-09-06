c3be4692df55cf1f0cd554537eef46b78f8b0c9b
le2sky <leehaneul990623@gmail.com>
2024-09-06 14:38:59 +0900

    feat: session 구현

---
+++ tomcat/src/main/java/org/apache/catalina/session/SimpleSession.java
@@ -0,0 +1,101 @@
+package org.apache.catalina.session;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSessionContext;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class SimpleSession implements HttpSession {
+
+    private final UUID sessionId = UUID.randomUUID();
+    private final Map<String, Object> values = new HashMap<>();
+
+    @Override
+    public long getCreationTime() {
+        return 0;
+    }
+
+    @Override
+    public String getId() {
+        return sessionId.toString();
+    }
+
+    @Override
+    public long getLastAccessedTime() {
+        return 0;
+    }
+
+    @Override
+    public ServletContext getServletContext() {
+        return null;
+    }
+
+    @Override
+    public void setMaxInactiveInterval(int interval) {
+
+    }
+
+    @Override
+    public int getMaxInactiveInterval() {
+        return 0;
+    }
+
+    @Override
+    public HttpSessionContext getSessionContext() {
+        return null;
+    }
+
+    @Override
+    public Object getAttribute(String name) {
+        return values.get(name);
+    }
+
+    @Override
+    public Object getValue(String name) {
+        return null;
+    }
+
+    @Override
+    public Enumeration<String> getAttributeNames() {
+        return null;
+    }
+
+    @Override
+    public String[] getValueNames() {
+        return new String[0];
+    }
+
+    @Override
+    public void setAttribute(String name, Object value) {
+        values.put(name, value);
+    }
+
+    @Override
+    public void putValue(String name, Object value) {
+
+    }
+
+    @Override
+    public void removeAttribute(String name) {
+
+    }
+
+    @Override
+    public void removeValue(String name) {
+
+    }
+
+    @Override
+    public void invalidate() {
+
+    }
+
+    @Override
+    public boolean isNew() {
+        return false;
+    }
+}
---
+++ tomcat/src/main/java/org/apache/catalina/session/SimpleSessionManager.java
@@ -0,0 +1,28 @@
+package org.apache.catalina.session;
+
+import jakarta.servlet.http.HttpSession;
+import org.apache.catalina.Manager;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SimpleSessionManager implements Manager {
+
+    private static final Map<String, HttpSession> SESSIONS = new ConcurrentHashMap<>();
+
+    @Override
+    public void add(HttpSession session) {
+        SESSIONS.put(session.getId(), session);
+    }
+
+    @Override
+    public HttpSession findSession(String id) throws IOException {
+        return SESSIONS.get(id);
+    }
+
+    @Override
+    public void remove(HttpSession session) {
+        SESSIONS.remove(session.getId());
+    }
+}

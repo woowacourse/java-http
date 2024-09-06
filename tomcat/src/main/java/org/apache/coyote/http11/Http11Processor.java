6e11994b00f73bca1412598cb2fe73362bb67f00
gyungchan Jo <namejgc@naver.com>
2024-09-06 16:32:15 +0900

    refactor: 응답 기능 메서드 분리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -34,30 +36,24 @@ public class Http11Processor implements Runnable, Processor {
 
             Controller controller = ControllerMapper.map(request.getPath());
             String viewName = controller.process(request.getUri());
-            String responseBody = FileReader.read("static" + viewName);
 
-            String response = "";
-            if (request.isCSS()) {
-                response = String.join("\r\n",
-                        "HTTP/1.1 200 OK ",
-                        "Content-Type: text/css;charset=utf-8 ",
-                        "Content-Length: " + responseBody.getBytes().length + " ",
-                        "",
-                        responseBody);
-            }
-            else {
-                response = String.join("\r\n",
-                        "HTTP/1.1 200 OK ",
-                        "Content-Type: text/html;charset=utf-8 ",
-                        "Content-Length: " + responseBody.getBytes().length + " ",
-                        "",
-                        responseBody);
-            }
+            String responseBody = FileReader.read("static" + viewName);
+            String contentType = Files.probeContentType(Path.of("static" + viewName));
+            String response = createResponse(contentType, responseBody);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String createResponse(String contentType, String responseBody) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                String.format("Content-Type: %s;charset=utf-8 ", contentType),
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
     }
 }

489c9de6d504380b01c0ad226f3a66db806ce821
hyeonjilee <skylar12200@gmail.com>
2024-09-06 17:30:27 +0900

    refactor: StatusCode 상수 적용

---
+++ tomcat/src/main/java/org/apache/catalina/StatusCode.java
@@ -0,0 +1,25 @@
+package org.apache.catalina;
+
+public enum StatusCode {
+    _200(200, "OK"),
+    _302(302, "FOUND"),
+    _401(401, "UNAUTHORIZED"),
+    _404(404, "NOT FOUND"),
+    ;
+
+    private final int code;
+    private final String message;
+
+    StatusCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

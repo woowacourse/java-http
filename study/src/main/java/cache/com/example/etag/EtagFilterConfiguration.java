42027b399869d732403f301918af41ba4b2f2b8c
ikjo39 <chi4321@naver.com>
2024-09-06 20:04:48 +0900

    feat: GreetingControllerTest 학습 추가

--- study/src/main/java/cache/com/example/etag/EtagFilterConfiguration.java
+++ study/src/main/java/cache/com/example/etag/EtagFilterConfiguration.java
@@ -1,12 +1,28 @@
 package cache.com.example.etag;
 
+import cache.com.example.version.ResourceVersion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @Configuration
 public class EtagFilterConfiguration {
 
-//    @Bean
-//    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
-//        return null;
-//    }
+    private final ResourceVersion resourceVersion;
+
+    @Autowired
+    public EtagFilterConfiguration(final ResourceVersion resourceVersion) {
+        this.resourceVersion = resourceVersion;
+    }
+
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        ShallowEtagHeaderFilter shallowEtagHeaderFilter = new ShallowEtagHeaderFilter();
+        FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean =
+                new FilterRegistrationBean<>(shallowEtagHeaderFilter);
+        filterRegistrationBean.addUrlPatterns("/etag", "/resources/" + resourceVersion.getVersion() + "/*");
+        return filterRegistrationBean;
+    }
 }

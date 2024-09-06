ba8f2d5f539da318d7080a3d7d64cb8c7e7e7976
Eugene Jang <kyunellroll@gmail.com>
2024-09-06 17:24:22 +0900

    feat(ResponseHeader): 응답 헤더를 담는 도메인 생성
    - 헤더의 정보를 조합하여 String으로 가져오는 메서드 테스트 작성

---
+++ tomcat/src/test/java/org/apache/coyote/http11/domain/header/ResponseHeaderTest.java
@@ -0,0 +1,20 @@
+package org.apache.coyote.http11.domain.header;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.apache.coyote.http11.domain.body.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ResponseHeaderTest {
+
+    @Test
+    @DisplayName("저장된 Header를 조합하여 가져온다.")
+    void toCombinedHeader() {
+        ResponseHeader responseHeader = new ResponseHeader(80, ContentType.HTML);
+
+        assertEquals(responseHeader.toCombinedHeader(), """
+                Content-Length: 80
+                Content-Type: text/html;charset=utf-8;""");
+    }
+}

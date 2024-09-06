030cac31e183ce7a52354ca6db1e77f00c7781bb
H <dmsgktn0208@gmail.com>
2024-09-06 17:37:57 +0900

    feat: Http 상태, 메서드 Enum 클래스 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpMethod.java
@@ -0,0 +1,19 @@
+package org.apache.coyote.http11;
+
+import java.util.Arrays;
+
+public enum HttpMethod {
+    GET,
+    POST,
+    DELETE,
+    PATCH,
+    PUT,
+    ;
+
+    public static HttpMethod from(String requestHttpMethod) {
+        return Arrays.stream(values())
+                .filter(httpMethod -> httpMethod.name().equals(requestHttpMethod))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 HTTP 메서드 요청입니다."));
+    }
+}

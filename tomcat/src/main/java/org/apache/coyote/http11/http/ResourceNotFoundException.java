3c7765984d1c806a2af83dba58246eca45a48c2a
재우 <Akaps6768@gmail.com>
2024-09-06 17:17:10 +0900

    refactor: 사용하지 않는 클래스 삭제

--- tomcat/src/main/java/org/apache/coyote/http11/http/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.apache.coyote.http11.http;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}

52f969f628aca0cec44011c61e5324bb6fdba121
pricelees <priceelees@gmail.com>
2024-09-06 15:59:07 +0900

    feat: HTTP 상태 코드 enum 구현

---
+++ tomcat/src/main/java/com/techcourse/web/HttpStatusCode.java
@@ -0,0 +1,30 @@
+package com.techcourse.web;
+
+public enum HttpStatusCode {
+
+	// success
+	OK(200, "OK"),
+
+	// client error
+	NOT_FOUND(404, "Not Found"),
+
+	// server error
+	INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+	;
+
+	private final int code;
+	private final String message;
+
+	HttpStatusCode(int code, String message) {
+		this.code = code;
+		this.message = message;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}

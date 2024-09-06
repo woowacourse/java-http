c13f3c4e9d3cd39162a294ea817d9eae0a096199
H <dmsgktn0208@gmail.com>
2024-09-06 17:46:54 +0900

    refactor: 뷰 반환 코드 리팩토링

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,23 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
 import org.apache.coyote.Processor;
+import org.apache.coyote.parsher.QueryParser;
+import org.apache.coyote.request.HttpRequest;
+import org.apache.coyote.response.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -26,22 +37,68 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+        try (InputStream inputStream = connection.getInputStream();
+             OutputStream outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            HttpRequest request = HttpRequest.from(inputStream);
+            log.info("http request : {}", request);
+            HttpResponse response = HttpResponse.create();
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            handle(request, response);
 
-            outputStream.write(response.getBytes());
+            outputStream.write(response.toString().getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private void handle(HttpRequest request, HttpResponse response) throws IOException {
+        try {
+            String endpoint = request.getEndpoint();
+            HttpMethod method = request.getMethod();
+            if (method == HttpMethod.GET && "/".equals(endpoint)) {
+                getView("/index.html", response);
+                return;
+            }
+            if (method == HttpMethod.GET && "/index.html".equals(endpoint)) {
+                getView("/index.html", response);
+                return;
+            }
+            if (method == HttpMethod.GET && "/login".equals(endpoint)) {
+                getView("/login.html", response);
+                return;
+            }
+            if (method == HttpMethod.POST && "/login".equals(endpoint)) {
+                login(request, response);
+                return;
+            }
+            getView("/401.html", response);
+        } catch (Exception exception) {
+            log.error(exception.getMessage());
+            getView("/404.html", response);
+        }
+    }
+
+    private void getView(String request, HttpResponse response) throws IOException {
+        URL resource = getClass().getClassLoader().getResource("static" + request);
+        if (resource == null) {
+            throw new IllegalArgumentException("유효하지 하지 않은 요청 입니다.");
+        }
+        Path path = Path.of(resource.getPath());
+        String contentType = Files.probeContentType(path);
+        response.addBody(new String(Files.readAllBytes(path)));
+        response.addContentType(contentType);
+    }
+
+    private void login(HttpRequest request, HttpResponse response) throws IOException {
+        String body = request.getBody();
+        Map<String, List<String>> queryStrings = QueryParser.parse(body);
+        String account = queryStrings.get("account").getFirst();
+
+        InMemoryUserRepository.findByAccount(account)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원 입니다."));
+
+        getView("/index.html", response);
     }
 }

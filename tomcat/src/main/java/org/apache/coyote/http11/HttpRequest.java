73c1a97a870c0e4793c1360c475439fe6acc269e
SCY <helenason@naver.com>
2024-09-06 13:59:49 +0900

    refactor: HttpRequest 필드 변경

--- tomcat/src/main/java/org/apache/coyote/http11/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpRequest.java
@@ -1,24 +1,25 @@
 package org.apache.coyote.http11;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class HttpRequest {
 
+    private final float version;
     private final String method;
     private final String path;
-    private final Map<String, String> queryParams;
     private final Map<String, String> headers;
     private final String body;
 
-    public HttpRequest(String method,
+    public HttpRequest(String version,
+                       String method,
                        String path,
-                       Map<String, String> queryParams,
                        Map<String, String> headers,
                        String body) {
+        this.version = Float.parseFloat(version); // TODO: 자리수 확인하기
         this.method = method;
         this.path = path;
-        this.queryParams = queryParams;
         this.headers = headers;
         this.body = body;
     }
@@ -36,8 +37,19 @@ public class HttpRequest {
         return path;
     }
 
-    public Map<String, String> getQueryParams() {
-        return Collections.unmodifiableMap(queryParams);
+    public Map<String, String> getQueryParams() { // TODO: test
+        Map<String, String> queryParams = new HashMap<>();
+        if (!path.contains("?")) {
+            return queryParams;
+        }
+        String queryString = path.split("\\?")[1];
+        String[] rawQueryParams = queryString.split("&");
+        for (String rawQueryParam : rawQueryParams) {
+            String key = rawQueryParam.split("=")[0];
+            String value = rawQueryParam.split("=")[1];
+            queryParams.put(key, value);
+        }
+        return queryParams;
     }
 
     public Map<String, String> getHeaders() {
--- tomcat/src/main/java/org/apache/coyote/http11/HttpRequestExtractor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpRequestExtractor.java
@@ -16,12 +16,12 @@ public class HttpRequestExtractor {
         String requestLine = br.readLine();
         String httpMethod = extractMethod(requestLine);
         String uri = extractUri(requestLine);
-        Map<String, String> queryParams = extractQueryParams(uri);
+        String version = extractVersion(requestLine);
         Map<String, String> headers = extractHeaders(br);
         String body = extractBody(br);
         // TODO: 메서드 호출 순서에 의존
 
-        return new HttpRequest(httpMethod, uri, queryParams, headers, body);
+        return new HttpRequest(version, httpMethod, uri, headers, body);
     }
 
     private static String extractMethod(String requestLine) {
@@ -32,19 +32,8 @@ public class HttpRequestExtractor {
         return requestLine.split(" ")[1];
     }
 
-    private static Map<String, String> extractQueryParams(String requestUri) {
-        Map<String, String> queryParams = new HashMap<>();
-        if (!requestUri.contains("?")) {
-            return queryParams;
-        }
-        String queryString = requestUri.split("\\?")[1];
-        String[] rawQueryParams = queryString.split("&");
-        for (String rawQueryParam : rawQueryParams) {
-            String key = rawQueryParam.split("=")[0];
-            String value = rawQueryParam.split("=")[1];
-            queryParams.put(key, value);
-        }
-        return queryParams;
+    private static String extractVersion(String requestLine) {
+        return requestLine.split(" ")[2].split("/")[1];
     }
 
     private static Map<String, String> extractHeaders(BufferedReader br) throws IOException {
@@ -65,7 +54,7 @@ public class HttpRequestExtractor {
         StringBuilder sb = new StringBuilder();
         while (br.ready()) {
             // 끝 개행이 없어 readLine 대신 read 사용
-            char[] one = Character.toChars(br.read());
+            char[] one = Character.toChars(br.read()); // TODO: refactoring
             sb.append(one);
         }
         return sb.toString();

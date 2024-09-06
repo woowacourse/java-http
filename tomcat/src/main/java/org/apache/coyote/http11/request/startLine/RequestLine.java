a8569c39f6b572ae78e1e2826d410b1f855ecdda
Seokmyung Ham <gkatjraud1@gmail.com>
2024-09-06 17:12:45 +0900

    refactor: RequestLine 정의

---
+++ tomcat/src/main/java/org/apache/coyote/http11/request/startLine/RequestLine.java
@@ -0,0 +1,52 @@
+package org.apache.coyote.http11.request.startLine;
+
+import org.apache.coyote.http11.HttpMethod;
+
+public class RequestLine {
+
+    private static final String REQUEST_LINE_DELIMITER = " ";
+    private static final int HTTP_METHOD_INDEX = 0;
+    private static final int URI_INDEX = 1;
+    private static final int HTTP_VERSION_INDEX = 2;
+    private static final int VALID_PARTS_LENGTH = 3;
+
+    private final HttpMethod httpMethod;
+    private final String uri;
+    private final HttpVersion httpVersion;
+
+    public RequestLine(String requestLine) {
+        validateRequestLine(requestLine);
+
+        String[] requestParts = splitRequestLine(requestLine);
+
+        this.httpMethod = HttpMethod.find(requestParts[HTTP_METHOD_INDEX]);
+        this.uri = requestParts[URI_INDEX];
+        this.httpVersion = new HttpVersion(requestParts[HTTP_VERSION_INDEX]);
+    }
+
+    private void validateRequestLine(String requestLine) {
+        if (requestLine == null || requestLine.isBlank()) {
+            throw new IllegalArgumentException("요청 라인은 비어있거나 공백일 수 없습니다.");
+        }
+    }
+
+    private String[] splitRequestLine(String requestLine) {
+        String[] requestParts = requestLine.split(REQUEST_LINE_DELIMITER);
+        if (requestParts.length != VALID_PARTS_LENGTH) {
+            throw new IllegalArgumentException("올바르지 않은 요청 라인 구조입니다.");
+        }
+        return requestParts;
+    }
+
+    public HttpMethod getHttpMethod() {
+        return httpMethod;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public HttpVersion getHttpVersion() {
+        return httpVersion;
+    }
+}
---
+++ tomcat/src/test/java/org/apache/coyote/http11/request/startLine/RequestLineTest.java
@@ -0,0 +1,48 @@
+package org.apache.coyote.http11.request.startLine;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.apache.coyote.http11.HttpMethod;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RequestLineTest {
+
+    @DisplayName("요청 라인이 null이거나 비어있으면 예외를 반환한다.")
+    @Test
+    void failureValidateRequestLineTest() {
+        assertAll(
+                () -> assertThatThrownBy(() -> new RequestLine(""))
+                        .isInstanceOf(IllegalArgumentException.class),
+
+                () -> assertThatThrownBy(() -> new RequestLine(null))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("요청 라인을 파싱하는데 성공한다.")
+    @Test
+    void successSplitRequestLineTest() {
+        String requestLine = "GET /static/jazz HTTP/1.1";
+
+        RequestLine expected = new RequestLine(requestLine);
+
+        assertAll(
+                () -> assertThat(expected.getHttpMethod().getName()).isEqualTo(HttpMethod.GET.getName()),
+                () -> assertThat(expected.getUri()).isEqualTo("/static/jazz"),
+                () -> assertThat(expected.getHttpVersion().getHttpVersion()).isEqualTo("HTTP/1.1")
+        );
+    }
+
+
+    @DisplayName("요청 라인의 구조가 올바르지 않으면 예외를 반환한다.")
+    @Test
+    void failureSplitRequestLineTest() {
+        String requestLine = "GET /static/jazz";
+
+        assertThatThrownBy(() -> new RequestLine(requestLine))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

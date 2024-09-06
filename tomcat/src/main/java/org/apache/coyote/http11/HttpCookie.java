75588b6c30d28662d748118b37928a1c1814b880
devhoya97 <diajbh123@gmail.com>
2024-09-06 09:50:24 +0900

    refactor: 동시성 보장을 위해 cookie를 저장할 map을 ConcurrentHashMap으로 변경

--- tomcat/src/main/java/org/apache/coyote/http11/HttpCookie.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpCookie.java
@@ -3,10 +3,11 @@ package org.apache.coyote.http11;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class HttpCookie {
 
-    private static final Map<String, String> cookies = new HashMap<>();
+    private static final Map<String, String> cookies = new ConcurrentHashMap<>();
 
     public static String getJsessionId(String account) {
         String uuid = UUID.randomUUID().toString();

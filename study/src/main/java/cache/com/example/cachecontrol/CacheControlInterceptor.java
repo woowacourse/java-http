36c22be49c15acb00880b79dccf7d72bcf0aab87
seunghye218 <seunghye@student.42seoul.kr>
2024-09-06 14:18:48 +0900

    test: 휴리스틱 캐싱 제거
    - CacheControlInterceptor 추가

---
+++ study/src/main/java/cache/com/example/cachecontrol/CacheControlInterceptor.java
@@ -0,0 +1,21 @@
+package cache.com.example.cachecontrol;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class CacheControlInterceptor implements HandlerInterceptor {
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+        final String cacheControl = CacheControl
+                .noCache()
+                .cachePrivate()
+                .getHeaderValue();
+        response.addHeader(HttpHeaders.CACHE_CONTROL, cacheControl);
+    }
+}

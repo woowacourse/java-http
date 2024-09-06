afbf5f8f952b1ed5d779cde736014171e1c2b5dd
Minjoo <m0522inju@gmail.com>
2024-09-06 14:20:31 +0900

    feat: http cache 학습 테스트

--- study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
+++ study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
@@ -1,13 +1,25 @@
 package cache.com.example.cachecontrol;
 
+import cache.com.example.version.ResourceVersion;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
 
 @Configuration
 public class CacheWebConfig implements WebMvcConfigurer {
 
+    private final CacheWebInterceptor cacheWebInterceptor;
+
+    public CacheWebConfig(final CacheWebInterceptor cacheWebInterceptor, final ResourceVersion version) {
+        this.cacheWebInterceptor = cacheWebInterceptor;
+    }
+
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(cacheWebInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/resources/static/**");
     }
 }

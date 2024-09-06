e3e1f407b1c4a5bd6d4b3b5c682eafc6d4152f1f
takoyakimchi <c4nyou@gmail.com>
2024-09-06 15:38:13 +0900

    refactor: interceptor 구현체를 지우고 WebContentIntercepor 사용

--- study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
+++ study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
@@ -1,18 +1,26 @@
 package cache.com.example.cachecontrol;
 
+import static java.util.concurrent.TimeUnit.DAYS;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
 @Configuration
 public class CacheWebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
-        registry.addInterceptor(new CacheResponseInterceptor())
-                .addPathPatterns("/**/*.js", "/**/*.css");
-
-        registry.addInterceptor(new NoCacheResponseInterceptor())
+        WebContentInterceptor noCacheIntercepter = new WebContentInterceptor();
+        noCacheIntercepter.addCacheMapping(CacheControl.noCache().cachePrivate(), "/**");
+        registry.addInterceptor(noCacheIntercepter)
                 .excludePathPatterns("/**/*.js", "/**/*.css");
+
+        WebContentInterceptor cacheIntercepter = new WebContentInterceptor();
+        cacheIntercepter.addCacheMapping(CacheControl.maxAge(365, DAYS).cachePublic(), "/**");
+        registry.addInterceptor(cacheIntercepter)
+                .addPathPatterns("/**/*.js", "/**/*.css");
     }
 }

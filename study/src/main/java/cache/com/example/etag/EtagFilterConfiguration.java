1ca0846e9336b26ba30b956977f5277bcaa8a694
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-09-06 14:59:02 +0900

    feat(EtagFilterConfiguration): ETag/If-None-Match 적용

--- study/src/main/java/cache/com/example/etag/EtagFilterConfiguration.java
+++ study/src/main/java/cache/com/example/etag/EtagFilterConfiguration.java
@@ -1,12 +1,17 @@
 package cache.com.example.etag;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @Configuration
 public class EtagFilterConfiguration {
 
-//    @Bean
-//    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
-//        return null;
-//    }
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        FilterRegistrationBean<ShallowEtagHeaderFilter> filter = new FilterRegistrationBean<>();
+        filter.setFilter(new ShallowEtagHeaderFilter());
+        return filter;
+    }
 }

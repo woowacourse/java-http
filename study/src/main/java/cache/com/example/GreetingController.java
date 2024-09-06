eb4bee02834e319cf196ca8ed9ebe74dd27c3591
Nam Gi Beom <rlqja0523@naver.com>
2024-09-06 16:21:53 +0900

    refactor: static으로 변경

--- study/src/main/java/cache/com/example/GreetingController.java
+++ study/src/main/java/cache/com/example/GreetingController.java
@@ -11,7 +11,7 @@ public class GreetingController {
 
     @GetMapping("/")
     public String index() {
-        return "index";
+        return "index.html";
     }
 
     /**
@@ -24,16 +24,16 @@ public class GreetingController {
                 .cachePrivate()
                 .getHeaderValue();
         response.addHeader(HttpHeaders.CACHE_CONTROL, cacheControl);
-        return "index";
+        return "index.html";
     }
 
     @GetMapping("/etag")
     public String etag() {
-        return "index";
+        return "index.html";
     }
 
     @GetMapping("/resource-versioning")
     public String resourceVersioning() {
-        return "resource-versioning";
+        return "resource-versioning.html";
     }
 }

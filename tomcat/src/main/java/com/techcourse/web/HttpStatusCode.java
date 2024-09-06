5821b9df8cb6cdad59b91c0a470e5cb59c802295
pricelees <priceelees@gmail.com>
2024-09-06 16:16:07 +0900

    feat: 400 상태 코드 상수 추가

--- tomcat/src/main/java/com/techcourse/web/HttpStatusCode.java
+++ tomcat/src/main/java/com/techcourse/web/HttpStatusCode.java
@@ -6,11 +6,11 @@ public enum HttpStatusCode {
 	OK(200, "OK"),
 
 	// client error
+	BAD_REQUEST(400, "Bad Request"),
 	NOT_FOUND(404, "Not Found"),
 
-	// server error
-	INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
-	;
+	// server error,
+	INTERNAL_SERVER_ERROR(500, "Internal Server Error");
 
 	private final int code;
 	private final String message;

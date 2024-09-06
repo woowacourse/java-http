9d16d2ad92e328ae675b9a00c5fa7518552a5648
H <dmsgktn0208@gmail.com>
2024-09-06 22:14:12 +0900

    refactor: 가독성 개선 및 HttpVersion 클래스 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpVersion.java
@@ -0,0 +1,23 @@
+package org.apache.coyote.http11;
+
+import java.util.Arrays;
+
+public enum HttpVersion {
+
+    HTTP1_1("HTTP/1.1"),
+    HTTP2("HTTP/2"),
+    ;
+
+    private final String value;
+
+    HttpVersion(String value) {
+        this.value = value;
+    }
+
+    public static HttpVersion from(String requestHttpVersion) {
+        return Arrays.stream(values())
+                .filter(httpVersion -> httpVersion.value.equals(requestHttpVersion))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 HTTP 버전입니다."));
+    }
+}
--- tomcat/src/main/java/org/apache/coyote/request/HttpRequestStartLine.java
+++ tomcat/src/main/java/org/apache/coyote/request/HttpRequestStartLine.java
@@ -1,63 +1,62 @@
 package org.apache.coyote.request;
 
 import org.apache.coyote.http11.HttpMethod;
+import org.apache.coyote.http11.HttpVersion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HttpRequestStartLine {
 
+    private static final Logger log = LoggerFactory.getLogger(HttpRequestStartLine.class);
+    private static final String START_LINE_DELIMITER = " ";
     private static final int HTTP_METHOD_INDEX = 0;
     private static final int REQUEST_TARGET_INDEX = 1;
     private static final int HTTP_VERSION_INDEX = 2;
-    private static final String HTTP_VERSION = "HTTP/1.1";
 
     private final HttpMethod method;
-    private final HttpRequestUri target;
+    private final HttpRequestTarget target;
+    private final HttpVersion httpVersion;
 
-    private HttpRequestStartLine(HttpMethod method, HttpRequestUri requestTarget) {
+    private HttpRequestStartLine(HttpMethod method, HttpRequestTarget requestTarget, HttpVersion httpVersion) {
         this.method = method;
         this.target = requestTarget;
+        this.httpVersion = httpVersion;
     }
 
-    public static HttpRequestStartLine from(String value) {
-        validateBlank(value);
-        String[] values = value.split(" ");
-
-        validateVersion(values[HTTP_VERSION_INDEX]);
-
+    public static HttpRequestStartLine from(String startLine) {
+        validateBlank(startLine);
+        String[] values = startLine.split(START_LINE_DELIMITER);
         HttpMethod method = HttpMethod.from(values[HTTP_METHOD_INDEX]);
-        HttpRequestUri requestTarget = HttpRequestUri.from(values[REQUEST_TARGET_INDEX]);
-
-        return new HttpRequestStartLine(method, requestTarget);
+        HttpRequestTarget requestTarget = HttpRequestTarget.from(values[REQUEST_TARGET_INDEX]);
+        HttpVersion requestHttpVersion = HttpVersion.from(values[HTTP_VERSION_INDEX]);
+        return new HttpRequestStartLine(method, requestTarget, requestHttpVersion);
     }
 
     private static void validateBlank(String value) {
         if (value == null || value.isBlank()) {
-            throw new IllegalArgumentException("startLine이 없습니다.");
+            log.debug("Start Line이 존재하지 않는 HTTP 요청");
+            throw new IllegalArgumentException("유효하지 않은 HTTP 요청입니다.");
         }
-    }
-
-    private static void validateVersion(String version) {
-        if (!HTTP_VERSION.equals(version)) {
-            throw new IllegalArgumentException("잘못된 HTTP 버전입니다.");
-        }
-    }
-
-    public String getEndPoint() {
-        return target.getEndPoint();
     }
 
     public HttpMethod getMethod() {
         return method;
     }
 
-    public HttpRequestUri getTarget() {
-        return target;
+    public String getTargetPath() {
+        return target.getPath();
+    }
+
+    public QueryParameters getQueryParameters() {
+        return target.getQueryParameters();
+    }
+
+    public HttpVersion getVersion() {
+        return httpVersion;
     }
 
     @Override
     public String toString() {
-        return "HttpRequestStartLine{" +
-                "method=" + method +
-                ", target=" + target +
-                '}';
+        return "method=" + method + " target=" + target + " httpVersion=" + httpVersion;
     }
 }

fe573200105dc1ca0813323b4e44e9ee4dd46ca9
Eugene Jang <kyunellroll@gmail.com>
2024-09-06 17:25:23 +0900

    test(ResourceFinderTest): 사용하지 않는 테스트 삭제

--- tomcat/src/test/java/org/apache/coyote/http11/finder/ResourceFinderTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/finder/ResourceFinderTest.java
@@ -1,32 +1,25 @@
 package org.apache.coyote.http11.finder;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.apache.coyote.http11.domain.ResourceFinder;
+import org.apache.coyote.http11.request.domain.RequestPath;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ResourceFinderTest {
 
     @Test
-    @DisplayName("존재하는 자원 경로가 없는 경우 Default를 응답한다.")
-    void find_WhenDefaultResourcePath() {
-        String body = ResourceFinder.find("/");
-
-        assertEquals(ResourceFinder.DEFAULT_BODY, body);
-    }
-
-    @Test
     @DisplayName("경로가 잘못되어 파일을 가져오지 못하는 경우 에러를 발생한다.")
     void find_WhenNoResourcePath() {
-        assertThrows(IllegalArgumentException.class, () -> ResourceFinder.find(""));
+        assertThrows(IllegalArgumentException.class, () -> ResourceFinder.find(new RequestPath("")));
     }
 
     @Test
     @DisplayName("파일이 존재하는 경우 읽어온다.")
     void find_WhenResourcePath() {
-        String body = ResourceFinder.find("/index.html");
+        String body = ResourceFinder.find(new RequestPath("/index.html"));
 
         assertTrue(body.contains("<!DOCTYPE html>"));
     }

dedc7139b99152e1a812363c1b5b20e0ec0d0c99
H <dmsgktn0208@gmail.com>
2024-09-07 05:34:40 +0900

    feat: 지원하는 파일 형식 검증 클래스 추가

---
+++ tomcat/src/main/java/org/apache/coyote/util/FileTypeChecker.java
@@ -0,0 +1,20 @@
+package org.apache.coyote.util;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class FileTypeChecker {
+
+    private static final Set<String> SUPPORTED_EXTENSIONS = new HashSet<>(List.of(".html", ".css", ".js"));
+
+    private FileTypeChecker() {
+    }
+
+    public static boolean isSupported(String target) {
+        if (SUPPORTED_EXTENSIONS.stream().noneMatch(target::endsWith)) {
+            throw new IllegalArgumentException(target + "은(는) 지원하지 않는 파일 형식입니다.");
+        }
+        return true;
+    }
+}

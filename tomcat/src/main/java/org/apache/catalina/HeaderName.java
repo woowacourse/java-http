d5085201d225baf644cf979af344366823ddb92f
hyeonjilee <skylar12200@gmail.com>
2024-09-07 14:36:52 +0900

    refactor: headerName에 set-cookie 추가

--- tomcat/src/main/java/org/apache/catalina/HeaderName.java
+++ tomcat/src/main/java/org/apache/catalina/HeaderName.java
@@ -1,25 +1,18 @@
 package org.apache.catalina;
 
-import java.util.Arrays;
-
 public enum HeaderName {
     CONTENT_LENGTH("Content-Length"),
     COOKIE("Cookie"),
     CONTENT_TYPE("Content-Type"),
     LOCATION("Location"),
+    SET_COOKIE("Set-Cookie"),
+
     ;
 
     private final String value;
 
     HeaderName(String value) {
         this.value = value;
-    }
-
-    public static HeaderName findByName(String name) {
-        return Arrays.stream(values())
-                .filter(value -> false)
-                .findAny()
-                .orElseThrow(); // TODO: 예외 추가
     }
 
     public String getValue() {

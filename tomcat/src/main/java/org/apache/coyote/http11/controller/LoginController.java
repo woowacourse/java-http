6d263ec3a41c3c15268433e0b2541e1a4c9118ae
coli-geonwoo <coli-geonwoo@gmail.com>
2024-09-07 20:28:29 +0900

    feat: loginService 분리

--- tomcat/src/main/java/org/apache/coyote/http11/controller/LoginController.java
+++ tomcat/src/main/java/org/apache/coyote/http11/controller/LoginController.java
@@ -1,18 +1,15 @@
 package org.apache.coyote.http11.controller;
 
-import com.techcourse.db.InMemoryUserRepository;
-import com.techcourse.model.User;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.request.RequestLine;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.coyote.http11.service.LoginService;
 
 
 public class LoginController implements Controller {
 
-    private static final Logger log = LoggerFactory.getLogger(LoginController.class);
+    private final LoginService loginService = new LoginService();
 
     @Override
     public boolean canHandle(String url) {
@@ -31,16 +28,7 @@ public class LoginController implements Controller {
 
     private Map<String, String> checkLogin(RequestLine requestLine) {
         Map<String, String> parameters = requestLine.getParameters();
-        String account = parameters.get("account");
-        String password = parameters.get("password");
-        User user = InMemoryUserRepository.findByAccount(account)
-                .orElseThrow(() -> new SecurityException("잘못된 유저 정보 입니다."));
-
-        if (!user.checkPassword(password)) {
-            throw new SecurityException("잘못된 유저 정보 입니다.");
-        }
-
-        log.info("user : {}", user);
+        loginService.checkLogin(parameters.get("account"), parameters.get("password"));
         return resolveResponse(302, "/index.html");
     }
 
---
+++ tomcat/src/main/java/org/apache/coyote/http11/service/LoginService.java
@@ -0,0 +1,21 @@
+package org.apache.coyote.http11.service;
+
+import com.techcourse.db.InMemoryUserRepository;
+import com.techcourse.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoginService {
+    private static final Logger log = LoggerFactory.getLogger(LoginService.class);
+
+    public void checkLogin(String account, String password) {
+        User user = InMemoryUserRepository.findByAccount(account)
+                .orElseThrow(() -> new SecurityException("잘못된 유저 정보 입니다."));
+
+        if (!user.checkPassword(password)) {
+            throw new SecurityException("잘못된 유저 정보 입니다.");
+        }
+
+        log.info("user : {}", user);
+    }
+}

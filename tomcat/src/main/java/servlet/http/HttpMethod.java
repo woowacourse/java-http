b8c9aa5b3ca2ae32f1f240510119522f092c19bb
Nam Gi Beom <rlqja0523@naver.com>
2024-09-08 00:50:21 +0900

    refactor: 대문자만 허용하도록 변경

--- tomcat/src/main/java/servlet/http/HttpMethod.java
+++ tomcat/src/main/java/servlet/http/HttpMethod.java
@@ -9,7 +9,7 @@ public enum HttpMethod {
 
     public static HttpMethod from(String method) {
         return Arrays.stream(values())
-                .filter(httpMethod -> httpMethod.name().equalsIgnoreCase(method))
+                .filter(httpMethod -> httpMethod.name().equals(method))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 HTTP Method 입니다. method: %s"
                         .formatted(method)));

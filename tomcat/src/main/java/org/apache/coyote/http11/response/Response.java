24c978702cf91e2767bd62064ddf526e9c06dfc8
zangsu <zangsu_@naver.com>
2024-09-08 19:19:45 +0900

    fix: content 가 null 일 경우 응답이 생성되지 않도록 변경

--- tomcat/src/main/java/org/apache/coyote/http11/response/Response.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/Response.java
@@ -4,6 +4,9 @@ import org.apache.coyote.http11.request.Request;
 
 public class Response {
     public static String writeResponse(Request request, String contentType, String content) {
+        if (content == null) {
+            return null;
+        }
         return String.join("\r\n",
                 String.format("%s 200 OK ", request.getHttpVersion()),
                 String.format("Content-Type: %s;charset=utf-8 ",contentType),

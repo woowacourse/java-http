fd9d01eab636b1ef89713fe8a2dc8e92c5b77519
PgmJun <84304802+PgmJun@users.noreply.github.com>
2024-09-08 09:28:10 +0900

    refactor: formatted 는 있지만 %s를 작성하지 않았던 로직 오류 수정

--- tomcat/src/main/java/org/apache/coyote/http11/HttpMethod.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpMethod.java
@@ -24,7 +24,7 @@ public enum HttpMethod {
         return Arrays.stream(values())
                 .filter(method -> method.value.equals(value))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 HttpMethod 값입니다.".formatted(value)));
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 HttpMethod 값입니다. (%s) ".formatted(value)));
     }
 
     public String getValue() {

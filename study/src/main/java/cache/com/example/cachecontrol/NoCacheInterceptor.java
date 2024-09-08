270e6cbf805c840fc59979634dcc53bd46861e94
HyungHoKim00 <hkim1109@naver.com>
2024-09-09 00:22:35 +0900

    refactor: postHandle을 사용하도록 수정

--- study/src/main/java/cache/com/example/cachecontrol/NoCacheInterceptor.java
+++ study/src/main/java/cache/com/example/cachecontrol/NoCacheInterceptor.java
@@ -5,15 +5,17 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 public class NoCacheInterceptor implements HandlerInterceptor {
 
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response,
+                           Object handler, ModelAndView modelAndView) {
         final String cacheControl = CacheControl
                 .noCache()
                 .cachePrivate()
                 .getHeaderValue();
         response.addHeader(HttpHeaders.CACHE_CONTROL, cacheControl);
-        return true;
     }
 }

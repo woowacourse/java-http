7cff4d7e1e71a8e52e4a48dd57c1131cd8d9b5c6
zangsu <zangsu_@naver.com>
2024-09-08 20:49:14 +0900

    refactor: 파일에 따라 MIME TYPE 을 결정하는 메서드 사용

--- tomcat/src/main/java/org/apache/coyote/http11/response/Content.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/Content.java
@@ -5,30 +5,21 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
 
 public class Content {
     private final String contentType;
     private final String content;
 
     public Content(String resourceName) throws IOException {
-        this.contentType = determineContentType(resourceName);
         String path = "static" + resourceName;
         URL resource = getClass().getClassLoader().getResource(path);
         if (resource == null) {
             throw new NoSuchFileException("파일 " + resourceName + "이 존재하지 않습니다.");
         }
-        this.content = new String(Files.readAllBytes(new File(resource.getPath()).toPath()));
-    }
-
-    private String determineContentType(String resourceName) {
-        String extension = resourceName.split("\\.")[1];
-        if (extension.equals("html")) {
-            return "text/html";
-        }
-        if (extension.equals("css")) {
-            return "text/css";
-        }
-        return "text/plain";
+        Path resourcePath = new File(resource.getPath()).toPath();
+        this.content = new String(Files.readAllBytes(resourcePath));
+        this.contentType = Files.probeContentType(resourcePath);
     }
 
     public String getContentType() {

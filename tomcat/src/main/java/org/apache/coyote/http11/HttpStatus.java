758f1f8b6c527378d1544ed13c7e3ee34ce38777
H <dmsgktn0208@gmail.com>
2024-09-09 03:33:44 +0900

    feat: 302, 401 응답 코드 추가

--- tomcat/src/main/java/org/apache/coyote/http11/HttpStatus.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpStatus.java
@@ -2,7 +2,9 @@ package org.apache.coyote.http11;
 
 public enum HttpStatus {
     OK(200, "OK"),
+    FOUND(302, "Found"),
     NOT_FOUND(400, "Not Found"),
+    UNAUTHORIZED(401, "Unauthorized"),
     BAD_REQUEST(404, "Bad Request"),
     INTERNAL_SERVER_ERROR(500, "Internal Server Error")
     ;

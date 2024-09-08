02480b2628a49daf872ef869d2801745a6a104e1
coli-geonwoo <coli-geonwoo@gmail.com>
2024-09-08 17:48:34 +0900

    refactor: account 탐색 메서드 추가

--- tomcat/src/main/java/org/apache/coyote/http11/service/LoginService.java
+++ tomcat/src/main/java/org/apache/coyote/http11/service/LoginService.java
@@ -9,13 +9,17 @@ public class LoginService {
     private static final Logger log = LoggerFactory.getLogger(LoginService.class);
 
     public void checkLogin(String account, String password) {
-        User user = InMemoryUserRepository.findByAccount(account)
-                .orElseThrow(() -> new SecurityException("잘못된 유저 정보 입니다."));
+        User user = findByAccount(account);
 
         if (!user.checkPassword(password)) {
             throw new SecurityException("잘못된 유저 정보 입니다.");
         }
 
         log.info("user : {}", user);
+    }
+
+    public User findByAccount(String account) {
+        return InMemoryUserRepository.findByAccount(account)
+                .orElseThrow(() -> new SecurityException("잘못된 유저 정보 입니다."));
     }
 }

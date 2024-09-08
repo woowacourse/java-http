cbb2428d00485675a403dabb0eec19a182fa19dd
H <dmsgktn0208@gmail.com>
2024-09-08 19:19:51 +0900

    test: 캐시 학습 테스트 추가

--- study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
+++ study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
@@ -1,19 +1,29 @@
 package cache.com.example.cachecontrol;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import static cache.com.example.version.CacheBustingWebConfig.PREFIX_STATIC_RESOURCES;
+
+import cache.com.example.version.ResourceVersion;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
 @Configuration
 public class CacheWebConfig implements WebMvcConfigurer {
 
-    @Autowired
-    private CacheControlInterceptor cacheControlInterceptor;
+    private final ResourceVersion version;
+
+    public CacheWebConfig(ResourceVersion version) {
+        this.version = version;
+    }
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
-        registry.addInterceptor(cacheControlInterceptor)
-                .addPathPatterns("/**");
+        WebContentInterceptor interceptor = new WebContentInterceptor();
+        interceptor.addCacheMapping(CacheControl.noCache().cachePrivate(), "/**");
+        registry.addInterceptor(interceptor);
+        registry.addInterceptor(interceptor)
+                .excludePathPatterns(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/**"); // ETAG 적용 경로 제외
     }
 }

fc3dbe04e5ffbc9a2918c7f98215f467b72c127c
정회성 <ghltjd3@gmail.com>
2024-09-09 15:42:03 +0900

    [톰캣 구현하기 1단계] 초롱 미션 제출합니다 (#535)
    * feat: GET /index.html 응답하기 구현
    
    * feat: 1차 학습 테스트
    
    * feat: 1단계 구현
    
    * fix: index.html 파일 못 읽는 오류 수정
    
    * feat: 휴리스틱 캐싱 제거하기 학습 테스트
    
    * feat: HTTP Compression 설정하기 학습 테스트
    
    * feat: ETag/If-None-Match 적용하기 학습 테스트
    
    * feat: 캐시 무효화 학습 테스트
    
    * refactor: 메서드 분리
    
    * refactor: 불필요한 변수 제거
    
    * fix: 특정 경로의 요청에만 ETag가 적용되도록 설정
    
    * refactor: 자원 반납을 해야 하는 객체들을 try-with-resource에 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,21 @@
 package org.apache.coyote.http11;
 
-import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
+import com.techcourse.db.InMemoryUserRepository;
+import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -27,13 +36,23 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+            var responseBody = "Hello world!";
+            String url = bufferedReader.readLine().split(" ")[1];
 
-            final var responseBody = "Hello world!";
+            String path = getPathFromUrl(url);
+            responseBody = updateResponseBody(path, responseBody);
+
+            String extension = path.split("\\.")[1];
+            String contentType = "text/html";
+            if(extension.equals("css")) {
+                contentType = "text/css";
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Type: " + contentType + ";charset=utf-8 ",
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
                     responseBody);
@@ -42,6 +61,44 @@ public class Http11Processor implements Runnable, Processor {
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        }
+    }
+
+    private String updateResponseBody(String path, String responseBody) throws IOException {
+        if(!path.equals("/")) {
+            URL resource = getClass().getClassLoader().getResource("static" + path);;
+            final Path filePath = new File(resource.getPath()).toPath();
+            responseBody = Files.readString(filePath);
+        }
+        return responseBody;
+    }
+
+    private static String getPathFromUrl(String path) {
+        String url = path;
+        if(path.contains("/login")) {
+            url = separateQueryString(path, url);
+        }
+        return url;
+    }
+
+    private static String separateQueryString(String path, String url) {
+        if(path.contains("?")) {
+            int index = path.indexOf("?");
+            String queryString = path.substring(index + 1);
+            url = path.substring(0, index) + ".html";
+            validateAccount(queryString);
+        }
+        return url;
+    }
+
+    private static void validateAccount(String queryString) {
+        String[] params = queryString.split("&");
+        String account = params[0].split("=")[1];
+        String password = params[1].split("=")[1];
+        User user = InMemoryUserRepository.findByAccount(account)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+        if(user.checkPassword(password)) {
+            log.info(user.toString());
         }
     }
 }

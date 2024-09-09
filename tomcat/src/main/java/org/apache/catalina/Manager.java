7ab4a568f258f6efbf2a3687d3e7fc533ed08bf6
ikjo39 <chi4321@naver.com>
2024-09-09 16:09:02 +0900

    feat: Session 및 싱글턴인 SessionManager 추가

--- tomcat/src/main/java/org/apache/catalina/Manager.java
+++ tomcat/src/main/java/org/apache/catalina/Manager.java
@@ -1,6 +1,5 @@
 package org.apache.catalina;
 
-import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -26,10 +25,11 @@ public interface Manager {
      *
      * @param session Session to be added
      */
-    void add(HttpSession session);
+    void add(Session session);
 
     /**
-     * Return the active Session, associated with this Manager, with the specified session id (if any); otherwise return
+     * Return the active Session, associated with this Manager, with the specified session id (if any); otherwise
+     * return
      * <code>null</code>.
      *
      * @param id The session id for the session to be returned
@@ -37,12 +37,12 @@ public interface Manager {
      * @throws IllegalStateException if a new session cannot be instantiated for any reason
      * @throws IOException           if an input/output error occurs while processing this request
      */
-    HttpSession findSession(String id) throws IOException;
+    Session findSession(String id) throws IOException;
 
     /**
      * Remove this Session from the active Sessions for this Manager.
      *
      * @param session Session to be removed
      */
-    void remove(HttpSession session);
+    void remove(Session session);
 }
---
+++ tomcat/src/main/java/org/apache/catalina/Session.java
@@ -0,0 +1,39 @@
+package org.apache.catalina;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class Session {
+
+    private final String id;
+    private final Map<String, Object> values = new HashMap<>();
+
+    public Session() {
+        this(UUID.randomUUID().toString());
+    }
+
+    private Session(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Object getAttribute(String name) {
+        return values.get(name);
+    }
+
+    public void setAttribute(String name, Object value) {
+        values.put(name, value);
+    }
+
+    public void removeAttribute(String name) {
+        values.remove(name);
+    }
+
+    public void invalidate() {
+        values.clear();
+    }
+}

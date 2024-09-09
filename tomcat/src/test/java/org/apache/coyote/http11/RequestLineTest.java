4a067106c6bb1dde7de00bdfe95451a84426e1f7
fromitive <fromitive@gmail.com>
2024-09-09 12:54:13 +0900

    test: RequestLine 예외 테스트 추가

--- tomcat/src/test/java/org/apache/coyote/http11/RequestLineTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/RequestLineTest.java
@@ -1,6 +1,7 @@
 package org.apache.coyote.http11;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
@@ -22,5 +23,16 @@ class RequestLineTest {
                 () -> assertThat(requestLine.getMethod()).isEqualTo("GET"),
                 () -> assertThat(requestLine.getUrl()).isEqualTo("/index.html")
         );
+    }
+
+    @DisplayName("RequestLine 양식이 잘못되면 예외를 발생시킨다.")
+    @Test
+    void exceptionParseRequestLine() {
+        // given
+        String rawRequestLine = "GET /index.html";
+
+        // then
+        assertThatThrownBy(() -> new RequestLine(rawRequestLine))
+                .isInstanceOf(RuntimeException.class);
     }
 }

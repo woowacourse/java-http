d0cb0855a1bbeedd67aa67f7a1d288952d78c1d8
Choo <choo000407@naver.com>
2024-09-09 17:37:59 +0900

    refactor: IllegalArgument 대신 NoSuchElement로 예외 처리

--- tomcat/src/main/java/com/techcourse/model/UserService.java
+++ tomcat/src/main/java/com/techcourse/model/UserService.java
@@ -1,6 +1,7 @@
 package com.techcourse.model;
 
 import com.techcourse.db.InMemoryUserRepository;
+import java.util.NoSuchElementException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,9 +11,9 @@ public class UserService {
 
     public void login(String account, String password) {
         User user = InMemoryUserRepository.findByAccount(account)
-                .orElseThrow(() -> new IllegalArgumentException("계정 정보가 틀렸습니다."));
+                .orElseThrow(() -> new NoSuchElementException("계정 정보가 틀렸습니다."));
         if (!user.checkPassword(password)) {
-            throw new IllegalArgumentException("계정 정보가 틀렸습니다.");
+            throw new NoSuchElementException("계정 정보가 틀렸습니다.");
         }
         log.info("user: {}", user);
     }

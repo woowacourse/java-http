b82c9bbb5af00974e35b3107758c223f5c6407b7
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-09-09 21:18:13 +0900

    feat: 컨트롤러 추가

--- tomcat/src/main/java/org/apache/coyote/http11/HttpStatusCode.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpStatusCode.java
@@ -4,7 +4,8 @@ public enum HttpStatusCode {
     OK(200, "OK"),
     FOUND(302, "Found"),
     UNAUTHORIZED(401, "Unauthorized"),
-    NOT_FOUND(404, "Not Found"),;
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),;
 
     private final int code;
     private final String reasonPhrase;

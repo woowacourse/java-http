9b9da79a8d36e531504b76e0ead8c2ba20f7a61f
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-09-09 20:20:03 +0900

    feat: 요청 스레드 250개, 대기 스레드 100개 까지만 가능하게 변경

--- tomcat/src/main/java/com/techcourse/Application.java
+++ tomcat/src/main/java/com/techcourse/Application.java
@@ -12,15 +12,19 @@ import org.apache.coyote.http11.session.SessionManager;
 
 import java.net.ServerSocket;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Application {
 
     public static void main(final String[] args) {
 
         final ServerSocket serverSocket = ServerSocketBuilder.createServerSocket();
-        final ExecutorService executorService = Executors.newFixedThreadPool(100);
+        final ExecutorService executorService = new ThreadPoolExecutor(
+                0, 250, 0L, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(100), new ThreadPoolExecutor.CallerRunsPolicy());
+
         final RequestExecutors requestExecutors = new RequestExecutors(
                 List.of(new LoginGetExecutor(), new RegisterGetExecutor(), new RegisterPostExecutor(), new LoginPostExecutor()
                 ));

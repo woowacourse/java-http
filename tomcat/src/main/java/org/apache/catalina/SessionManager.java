1f69b4f91037f125b5646440f41a7397c0e10590
HyungUk Ryu <hw0603@naver.com>
2024-09-09 15:07:59 +0900

    refactor(SessionManager): 세션 저장 시 Map 대신 ConcurrentHashMap을 사용하도록 변경

--- tomcat/src/main/java/org/apache/catalina/SessionManager.java
+++ tomcat/src/main/java/org/apache/catalina/SessionManager.java
@@ -1,13 +1,13 @@
 package org.apache.catalina;
 
 import jakarta.servlet.http.HttpSession;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class SessionManager implements Manager {
 
-    private static final Map<String, Session> SESSIONS = new HashMap<>();
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
 
     @Override
     public void add(HttpSession session) {

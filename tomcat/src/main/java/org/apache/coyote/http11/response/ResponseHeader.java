e3b06bd69335086a9de99d95f6c3bd74e8c590d0
Minjoo <m0522inju@gmail.com>
2024-09-09 15:25:55 +0900

    feat: 헤더 순서 유지하도록 LinkedHashMap 사용

--- tomcat/src/main/java/org/apache/coyote/http11/response/ResponseHeader.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/ResponseHeader.java
@@ -1,12 +1,12 @@
 package org.apache.coyote.http11.response;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ResponseHeader {
 
-    private final Map<String, String> headers = new HashMap<String, String>();
+    private final Map<String, String> headers = new LinkedHashMap<>();
 
     public void add(final String name, final String value) {
         headers.put(name, value);

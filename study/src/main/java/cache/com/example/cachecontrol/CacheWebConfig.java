4f772b383ef90a1595a85896be423f44be4858c5
Minjoo <m0522inju@gmail.com>
2024-09-09 14:04:38 +0900

    study: WebContentInterceptor 사용

--- study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
+++ study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
@@ -1,21 +1,20 @@
 package cache.com.example.cachecontrol;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
 @Configuration
 public class CacheWebConfig implements WebMvcConfigurer {
 
-    private final CacheWebInterceptor cacheWebInterceptor;
-
-    public CacheWebConfig(final CacheWebInterceptor cacheWebInterceptor) {
-        this.cacheWebInterceptor = cacheWebInterceptor;
-    }
-
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
-        registry.addInterceptor(cacheWebInterceptor)
+        final var interceptor = new WebContentInterceptor();
+        interceptor.setCacheControl(CacheControl.noCache().cachePrivate());
+
+        registry.addInterceptor(interceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/resources/static/**");
     }

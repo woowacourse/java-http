3e61da26a7ac9af28eee0e2c1566a42362e8de4f
le2sky <leehaneul990623@gmail.com>
2024-09-10 22:40:34 +0900

    feat: session 조회 메서드를 추가

--- tomcat/src/main/java/org/apache/coyote/http11/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpRequest.java
@@ -1,20 +1,93 @@
 package org.apache.coyote.http11;
 
+import jakarta.servlet.http.HttpSession;
+import org.apache.catalina.Manager;
+import org.apache.catalina.session.SimpleSession;
+
+import java.io.IOException;
 import java.net.URI;
 
-public record HttpRequest(
-        HttpMethod httpMethod,
-        URI uri,
-        HttpVersion httpVersion,
-        Header header,
-        HttpBody body
-) {
+public class HttpRequest {
 
-    public static HttpRequest createHttp11Message(String requestLine, Header header, HttpBody requestBody) {
+    private final HttpMethod httpMethod;
+    private final URI uri;
+    private final HttpVersion httpVersion;
+    private final Header header;
+    private final HttpBody body;
+    private final Manager sessionManager;
+    private String sessionId;
+
+    public HttpRequest(
+            HttpMethod httpMethod,
+            URI uri,
+            HttpVersion httpVersion,
+            Header header,
+            HttpBody body,
+            Manager sessionManager,
+            String sessionId
+    ) {
+        this.httpMethod = httpMethod;
+        this.uri = uri;
+        this.httpVersion = httpVersion;
+        this.header = header;
+        this.body = body;
+        this.sessionManager = sessionManager;
+        this.sessionId = sessionId;
+    }
+
+    public static HttpRequest createHttp11Message(String requestLine, Header header, HttpBody requestBody, Manager sessionManager) {
         String[] requestLines = requestLine.split(" ");
         HttpMethod httpMethod = HttpMethod.from(requestLines[0]);
         URI uri = URI.create(requestLines[1]);
 
-        return new HttpRequest(httpMethod, uri, HttpVersion.HTTP_1_1, header, requestBody);
+        return new HttpRequest(
+                httpMethod,
+                uri,
+                HttpVersion.HTTP_1_1,
+                header,
+                requestBody,
+                sessionManager,
+                header.getJSessionId().orElse("")
+        );
+    }
+
+    public HttpMethod getHttpMethod() {
+        return httpMethod;
+    }
+
+    public URI getUri() {
+        return uri;
+    }
+
+    public String getPath() {
+        return uri.getPath();
+    }
+
+    public QueryParameter getQueryParameter() {
+        return new QueryParameter(uri.getQuery());
+    }
+
+    public HttpVersion getHttpVersion() {
+        return httpVersion;
+    }
+
+    public Header getHeader() {
+        return header;
+    }
+
+    public HttpBody getBody() {
+        return body;
+    }
+
+    public HttpSession getSession(boolean sessionCreateIfAbsent) throws IOException {
+        HttpSession foundSession = sessionManager.findSession(sessionId);
+
+        if (foundSession == null && sessionCreateIfAbsent) {
+            HttpSession session = new SimpleSession();
+            sessionManager.add(session);
+            return session;
+        }
+
+        return foundSession;
     }
 }

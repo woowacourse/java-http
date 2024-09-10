f273f714b72a13123edadc035515334b528057b5
임수빈 <robin980108@naver.com>
2024-09-10 19:17:34 +0900

    feat: Controller 인터페이스 및 추상 클래스 추가

---
+++ tomcat/src/main/java/org/apache/catalina/AbstractController.java
@@ -0,0 +1,59 @@
+package org.apache.catalina;
+
+import org.apache.coyote.http11.request.Http11Method;
+import org.apache.coyote.http11.request.Http11Request;
+import org.apache.coyote.http11.response.Http11Response;
+
+public non-sealed abstract class AbstractController implements Controller {
+
+    @Override
+    public final void service(Http11Request request, Http11Response response) {
+        switch (getMethod(request)) {
+            case GET -> doGet(request, response);
+            case PUT -> doPut(request, response);
+            case HEAD -> doHead(request, response);
+            case POST -> doPost(request, response);
+            case TRACE -> doTrace(request, response);
+            case DELETE -> doDelete(request, response);
+            case CONNECT -> doConnect(request, response);
+            case OPTIONS -> doOptions(request, response);
+            case null, default -> throw new RuntimeException("지원하지 않는 메서드 입니다.");
+        }
+    }
+
+    private Http11Method getMethod(Http11Request request) {
+        return request.method();
+    }
+
+    protected void doGet(Http11Request request, Http11Response response) {
+
+    }
+
+    protected void doPut(Http11Request request, Http11Response response) {
+
+    }
+
+    protected void doHead(Http11Request request, Http11Response response) {
+
+    }
+
+    protected void doPost(Http11Request request, Http11Response response) {
+
+    }
+
+    protected void doTrace(Http11Request request, Http11Response response) {
+
+    }
+
+    protected void doDelete(Http11Request request, Http11Response response) {
+
+    }
+
+    protected void doConnect(Http11Request request, Http11Response response) {
+
+    }
+
+    protected void doOptions(Http11Request request, Http11Response response) {
+
+    }
+}
---
+++ tomcat/src/main/java/org/apache/catalina/Controller.java
@@ -0,0 +1,8 @@
+package org.apache.catalina;
+
+import org.apache.coyote.http11.request.Http11Request;
+import org.apache.coyote.http11.response.Http11Response;
+
+public sealed interface Controller permits AbstractController {
+    void service(Http11Request request, Http11Response response);
+}

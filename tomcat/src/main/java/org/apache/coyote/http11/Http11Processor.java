6ad41a6ba43ac474e5fa361d6a5031ac650388dd
zangsu <zangsu_@naver.com>
2024-09-10 15:45:12 +0900

    refactor: 핸들러 하나만을 사용하도록 변경

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -4,10 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.util.List;
 
 import org.apache.coyote.Processor;
-import org.apache.coyote.http11.exception.NoHandlerException;
 import org.apache.coyote.http11.handler.DefaultResourceHandler;
 import org.apache.coyote.http11.request.Request;
 import org.slf4j.Logger;
@@ -18,15 +16,12 @@ import com.techcourse.exception.UncheckedServletException;
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
-    private final List<RequestHandler> requestHandlers;
+    private final RequestHandler requestHandler;
     private final Socket connection;
 
     public Http11Processor(Socket connection) {
         this.connection = connection;
-        this.requestHandlers = List.of(
-                //new MethodHandler(),
-                new DefaultResourceHandler()
-        );
+        this.requestHandler = new DefaultResourceHandler();
     }
 
     @Override
@@ -54,12 +49,6 @@ public class Http11Processor implements Runnable, Processor {
     }
 
     private String getResponse(Request request) throws IOException {
-        for (RequestHandler requestHandler : requestHandlers) {
-            String response = requestHandler.handle(request);
-            if (response != null) {
-                return response;
-            }
-        }
-        throw new NoHandlerException("핸들러가 존재하지 않습니다. request : " + request);
+        return requestHandler.handle(request);
     }
 }

0bf0e9a8eb79c9c0da58b176c50b2598aef2fae6
Arachneee <jhg2819@naver.com>
2024-09-10 13:59:55 +0900

    refactor: 리플렉션 제거

--- tomcat/src/main/java/com/techcourse/Application.java
+++ tomcat/src/main/java/com/techcourse/Application.java
@@ -1,27 +1,15 @@
 package com.techcourse;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Set;
+import com.techcourse.controller.LoginController;
+import com.techcourse.controller.RegisterController;
 import org.apache.catalina.startup.Tomcat;
-import org.apache.controller.AbstractController;
 import org.apache.controller.HandlerContainer;
-import org.reflections.Reflections;
-import org.reflections.scanners.SubTypesScanner;
 
 public class Application {
 
-    public static void main(String[] args)
-            throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        Reflections reflections = new Reflections(Application.class.getPackage().getName(), new SubTypesScanner(false));
-
-        Set<Class<? extends AbstractController>> classes = reflections.getSubTypesOf(AbstractController.class);
-
-        for (Class<? extends AbstractController> clazz : classes) {
-            Constructor<? extends AbstractController> constructor = clazz.getConstructor();
-            AbstractController handler = constructor.newInstance();
-            HandlerContainer.add(handler);
-        }
+    public static void main(String[] args) {
+        HandlerContainer.add(new LoginController());
+        HandlerContainer.add(new RegisterController());
 
         final var tomcat = new Tomcat();
         tomcat.start();

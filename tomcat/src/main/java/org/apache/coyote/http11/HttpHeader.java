689c3e8d644fcc53e0ce4b5c890118400adb5ed4
eunjungL <dldmswjd322@gmail.com>
2024-09-10 17:34:36 +0900

    fix: HttpHeaderName은 상수화만 하게 수정

--- tomcat/src/main/java/org/apache/coyote/http11/HttpHeader.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpHeader.java
@@ -11,21 +11,21 @@ public class HttpHeader {
 
     public static final String HEADER_DELIMITER = ":";
 
-    private final Map<HttpHeaderName, String> headers;
+    private final Map<String, String> headers;
 
     public HttpHeader() {
         this.headers = new LinkedHashMap<>();
     }
 
-    public HttpHeader(Map<HttpHeaderName, String> headers) {
+    public HttpHeader(Map<String, String> headers) {
         this.headers = headers;
     }
 
     public static HttpHeader from(List<String> request) {
-        Map<HttpHeaderName, String> headers = request.stream()
+        Map<String, String> headers = request.stream()
                 .skip(1)
                 .collect(Collectors.toMap(
-                        r -> HttpHeaderName.findByHeaderName(r.split(HEADER_DELIMITER)[0]),
+                        r -> r.split(HEADER_DELIMITER)[0],
                         r -> r.split(HEADER_DELIMITER)[1].trim())
                 );
 
@@ -33,18 +33,18 @@ public class HttpHeader {
     }
 
     public void addHeader(HttpHeaderName headerName, String value) {
-        headers.put(headerName, value);
+        headers.put(headerName.getHeaderName(), value);
     }
 
     public String getHeader(HttpHeaderName headerName) {
-        return headers.get(headerName);
+        return headers.get(headerName.getHeaderName());
     }
 
     public String toHttpHeader() {
         List<String> httpHeader = new ArrayList<>();
 
-        for (Entry<HttpHeaderName, String> entry : headers.entrySet()) {
-            httpHeader.add(entry.getKey().getHeaderName() + HEADER_DELIMITER + " " + entry.getValue() + " ");
+        for (Entry<String, String> entry : headers.entrySet()) {
+            httpHeader.add(entry.getKey() + HEADER_DELIMITER + " " + entry.getValue() + " ");
         }
 
         return String.join("\r\n", httpHeader);

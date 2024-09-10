c51a5b01adf7fdcfa71647c25a811e2b6539cf32
Seokmyung Ham <gkatjraud1@gmail.com>
2024-09-11 00:26:54 +0900

    feat: ResourceReader 객체 정의

---
+++ tomcat/src/main/java/org/apache/coyote/http11/ResourceReader.java
@@ -0,0 +1,28 @@
+package org.apache.coyote.http11;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Optional;
+
+public class ResourceReader {
+
+    private static final String BASE_PATH = "static/";
+
+    private ResourceReader() {
+    }
+
+    public static String read(String resource) throws IOException {
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        Optional<URL> url = Optional.ofNullable(classLoader.getResource(BASE_PATH + resource));
+
+        if (url.isPresent()) {
+            File file = new File(url.get().getFile());
+            return Files.readString(file.toPath(), StandardCharsets.UTF_8);
+        }
+
+        throw new NullPointerException("파일이 존재하지 않습니다.");
+    }
+}
---
+++ tomcat/src/test/java/org/apache/coyote/http11/ResourceReaderTest.java
@@ -0,0 +1,30 @@
+package org.apache.coyote.http11;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.IOException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ResourceReaderTest {
+
+    @DisplayName("파일 데이터를 읽어온다.")
+    @Test
+    void successReadTest() throws IOException {
+        String resource = "index.html";
+
+        String content = ResourceReader.read(resource);
+
+        assertNotNull(content);
+    }
+
+    @DisplayName("존재하지 않는 파일을 읽어올 경우 예외를 반환한다.")
+    @Test
+    void failureReadTest() {
+        String resource = "jazz.html";
+
+        assertThatThrownBy(() -> ResourceReader.read(resource))
+                .isInstanceOf(NullPointerException.class);
+    }
+}

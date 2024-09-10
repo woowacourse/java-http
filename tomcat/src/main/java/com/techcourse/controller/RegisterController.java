203224f3cb9477a293af284b8d9f8a9e2c0a5f24
jhon3242 <rldotnr2@gmail.com>
2024-09-10 16:25:10 +0900

    feat: RegisterController AbstractController 적용

--- tomcat/src/main/java/com/techcourse/controller/RegisterController.java
+++ tomcat/src/main/java/com/techcourse/controller/RegisterController.java
@@ -2,30 +2,29 @@ package com.techcourse.controller;
 
 import com.techcourse.service.RegisterService;
 import org.apache.coyote.http11.HttpHeaders;
-import org.apache.coyote.http11.HttpMethod;
 import org.apache.coyote.http11.HttpRequest;
 import org.apache.coyote.http11.HttpResponse;
 import org.apache.coyote.http11.HttpStatus;
 import org.apache.coyote.view.ViewResolver;
 
-public class RegisterController implements Controller{
+public class RegisterController extends AbstractController{
 
     @Override
-    public void service(HttpRequest request, HttpResponse response) {
-        if (request.isSameMethod(HttpMethod.GET)) {
-            response.setView(ViewResolver.getView("register.html"));
-            response.setStatus(HttpStatus.OK);
-            response.setHeaders(HttpHeaders.create(request, response));
-        }
-        if (request.isSameMethod(HttpMethod.POST)) {
-            RegisterService.addUser(request.findFromBody("account"),
-                    request.findFromBody("password"),
-                    request.findFromBody("email"));
-            log.info("[REGISTER] {}", request.findFromBody("account"));
-            response.setView(ViewResolver.getView("register.html"));
-            response.setStatus(HttpStatus.FOUND);
-            response.setHeaders(HttpHeaders.create(request, response));
-            response.addHeader("Location", "/index.html");
-        }
+    protected void doGet(HttpRequest request, HttpResponse response) {
+        response.setView(ViewResolver.getView("register.html"));
+        response.setStatus(HttpStatus.OK);
+        response.setHeaders(HttpHeaders.create(request, response));
+    }
+
+    @Override
+    protected void doPost(HttpRequest request, HttpResponse response) {
+        RegisterService.addUser(request.findFromBody("account"),
+                request.findFromBody("password"),
+                request.findFromBody("email"));
+        log.info("[REGISTER] {}", request.findFromBody("account"));
+        response.setView(ViewResolver.getView("register.html"));
+        response.setStatus(HttpStatus.FOUND);
+        response.setHeaders(HttpHeaders.create(request, response));
+        response.addHeader("Location", "/index.html");
     }
 }

539d1162b0857af0192135547c7cd195f88bab38
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-09-10 17:31:00 +0900

    test: VersionOfProtocol 테스트 코드 작성

--- tomcat/src/main/java/org/apache/catalina/request/VersionOfProtocol.java
+++ tomcat/src/main/java/org/apache/catalina/request/VersionOfProtocol.java
@@ -7,7 +7,7 @@ public class VersionOfProtocol {
     public VersionOfProtocol(String value) {
         String[] values = value.split("/");
         if (values.length != 2) {
-            throw new IllegalArgumentException("HTTP 프로토콜 및 버전 형식이 잘못되었습니다.");
+            throw new IllegalArgumentException(value + ": HTTP 프로토콜 및 버전 형식이 잘못되었습니다.");
         }
         this.httpProtocol = HttpProtocol.of(values[0]);
         this.httpVersion = values[1];

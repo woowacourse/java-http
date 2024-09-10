671fffb7dc0134ccae28ac21989e2bd8f5acff5c
ashsty <qkralsdk991@naver.com>
2024-09-10 18:26:30 +0900

    feat: 회원가입 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -6,10 +6,7 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
 import java.net.URL;
 import java.net.URLDecoder;
@@ -48,48 +45,97 @@ public class Http11Processor implements Runnable, Processor {
              final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
              final BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
 
-            final String line = bufferedReader.readLine();
+            String firstHttpRequestHeaderLine = bufferedReader.readLine();
 
-            if (line == null) {
+            if (firstHttpRequestHeaderLine == null) {
                 return;
             }
 
-            final String[] httpRequestLine = line.split(" ");
+            Map<String, String> httpRequestHeaders = readHttpRequestHeaders(bufferedReader);
+
+            final String[] httpRequestLine = firstHttpRequestHeaderLine.split(" ");
             final String method = httpRequestLine[0];
             final String requestURL = httpRequestLine[1];
 
-            String contentType;
-            byte[] responseBody;
-
-            String[] searchResourcePath = determineResourcePath(requestURL);
-            String httpStatus = searchResourcePath[0];
-            String resourcePath = searchResourcePath[1];
-
-            contentType = determineContentType(resourcePath);
-
-            URL resource = getClass().getResource(resourcePath);
-
-            if (resource == null) {
-                resource = getClass().getResource("/static/404.html");
-                httpStatus = "404 NOT FOUND";
+            if (method.equals("GET")) {
+                doGet(requestURL, outputStream);
             }
 
-            responseBody = Files.readAllBytes(new File(resource.getFile()).toPath());
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 " + httpStatus + " ",
-                    "Content-Type: " + contentType + " ",
-                    "Content-Length: " + responseBody.length + " ",
-                    "",
-                    "");
-
-            outputStream.write(response.getBytes());
-            outputStream.write(responseBody);
-            outputStream.flush();
+            if (method.equals("POST")) {
+                doPost(httpRequestHeaders, bufferedReader, requestURL, outputStream);
+            }
 
         } catch (IOException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private Map<String, String> readHttpRequestHeaders(final BufferedReader bufferedReader) throws IOException {
+        Map<String, String> httpRequestHeaders = new HashMap<>();
+        String line;
+
+        while ((line = bufferedReader.readLine()) != null && !"".equals(line)) {
+            String[] headerLine = line.split(": ");
+
+            String key = URLDecoder.decode(headerLine[0]);
+            String value = URLDecoder.decode(headerLine[1]);
+
+            httpRequestHeaders.put(key, value);
+        }
+        return httpRequestHeaders;
+    }
+
+    private void doPost(final Map<String, String> httpRequestHeaders, final BufferedReader bufferedReader,
+                        final String requestURL, final OutputStream outputStream) throws IOException {
+        String requestBody = readRequestBody(httpRequestHeaders, bufferedReader);
+
+        if (requestURL.equals("/login")) {
+            String path = "/login" + "?" + requestBody;
+            doGet(path, outputStream);
+        }
+
+        if (requestURL.equals("/register")) {
+            register(requestBody);
+            String path = "/success";
+            doGet(path, outputStream);
+        }
+    }
+
+    private String readRequestBody(final Map<String, String> httpRequestHeaders, final BufferedReader bufferedReader) throws IOException {
+        int contentLength = Integer.parseInt(httpRequestHeaders.get("Content-Length"));
+        char[] buffer = new char[contentLength];
+        bufferedReader.read(buffer, 0, contentLength);
+        return new String(buffer);
+    }
+
+    private void register(String requestBody) {
+        Map<String, String> userInfo = parseUserInfo(requestBody);
+
+        String account = userInfo.get("account");
+        String password = userInfo.get("password");
+        String email = userInfo.get("email");
+
+        User newUser = new User(account, password, email);
+        InMemoryUserRepository.save(newUser);
+        log.info("user: {}의 회원가입이 완료되었습니다.", newUser.getAccount());
+    }
+
+    private void doGet(final String requestURL, final OutputStream outputStream) throws IOException {
+        String[] searchResourcePath = determineResourcePath(requestURL);
+        String httpStatus = searchResourcePath[0];
+        String resourcePath = searchResourcePath[1];
+
+        String contentType = determineContentType(resourcePath);
+        URL resource = getClass().getResource(resourcePath);
+
+        if (resource == null) {
+            resource = getClass().getResource("/static/404.html");
+            httpStatus = "404 NOT FOUND";
+        }
+
+        byte[] responseBody = Files.readAllBytes(new File(resource.getFile()).toPath());
+
+        createHttpResponse(outputStream, httpStatus, contentType, responseBody);
     }
 
     private String[] determineResourcePath(String requestURL) {
@@ -97,6 +143,12 @@ public class Http11Processor implements Runnable, Processor {
         result[0] = "200 OK";
 
         if (requestURL.equals("/") || requestURL.equals("/index.html")) {
+            result[1] = "/static/index.html";
+            return result;
+        }
+
+        if (requestURL.equals("/success")) {
+            result[0] = "201 CREATED";
             result[1] = "/static/index.html";
             return result;
         }
@@ -155,13 +207,16 @@ public class Http11Processor implements Runnable, Processor {
         return result;
     }
 
-    private Map<String, String> parseUserInfo(String queryString) {
+    private Map<String, String> parseUserInfo(String requestBody) {
         Map<String, String> userInfo = new HashMap<>();
 
-        String[] info = queryString.split("&");
+        String[] info = requestBody.split("&");
 
         for (String metadata : info) {
             String[] temp = metadata.split("=");
+            if (temp.length < 2 || temp[1].isEmpty()) {
+                throw new IllegalArgumentException("필수 입력값이 비어 있습니다.");
+            }
             String key = URLDecoder.decode(temp[0]);
             String value = URLDecoder.decode(temp[1]);
 
@@ -193,5 +248,19 @@ public class Http11Processor implements Runnable, Processor {
                     log.info(account + "의 비밀번호가 잘못 입력되었습니다.");
                     return false;
                 });
+    }
+
+    private void createHttpResponse(final OutputStream outputStream, final String httpStatus,
+                                    final String contentType, final byte[] responseBody) throws IOException {
+        final var response = String.join("\r\n",
+                "HTTP/1.1 " + httpStatus + " ",
+                "Content-Type: " + contentType + " ",
+                "Content-Length: " + responseBody.length + " ",
+                "",
+                "");
+
+        outputStream.write(response.getBytes());
+        outputStream.write(responseBody);
+        outputStream.flush();
     }
 }

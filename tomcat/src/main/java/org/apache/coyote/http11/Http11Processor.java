dbce3e537da8bd4bab7837dbbd46a26c8d8e8a6f
HoeSeong123 <ghltjd3@gmail.com>
2024-09-11 02:57:16 +0900

    refactor: 메서드 분리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -4,17 +4,17 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import org.apache.coyote.Processor;
-import org.apache.coyote.http11.domain.HttpStatusCode;
+import org.apache.coyote.http11.domain.HttpCookie;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,8 +27,6 @@ public class Http11Processor implements Runnable, Processor {
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
 
     private final Socket connection;
-    private HttpStatusCode httpStatusCode;
-    private String location = "";
 
     public Http11Processor(final Socket connection) {
         this.connection = connection;
@@ -44,122 +42,177 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream();
-             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
-            var responseBody = "Hello world!";
-            String contentType = "text/html";
-            String path = "/";
-
-            String[] firstLine = bufferedReader.readLine().split(" ");
+             final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String[] firstLine = reader.readLine().split(" ");
             String method = firstLine[0];
             String url = firstLine[1];
             String version = firstLine[2];
 
-            Map<String, String> requestHeaders = new HashMap<>();
-            String line;
-            while(!(line = bufferedReader.readLine()).isEmpty()) {
-                requestHeaders.put(line.split(": ")[0], line.split(": ")[1]);
+            Map<String, String> requestHeaders = readRequestHeaders(reader);
+
+            if (method.equals("GET")) {
+                doGet(url, requestHeaders, outputStream);
+            }
+            if (method.equals("POST")) {
+                doPost(url, requestHeaders, reader, outputStream);
             }
 
-            if(method.equals("GET")) {
-                path = getPathFromUrl(url);
-
-                if(!path.equals("/")) {
-                    String extension = path.split("\\.")[1];
-                    if (extension.equals("css")) {
-                        contentType = "text/css";
-                    }
-                }
-            }
-
-            if(method.equals("POST")) {
-                int contentLength = Integer.parseInt(requestHeaders.get("Content-Length"));
-                char[] buffer = new char[contentLength];
-                bufferedReader.read(buffer, 0, contentLength);
-                String[] requestBody = new String(buffer).split("&");
-                Map<String, String> userInfo = new HashMap<>();
-                for(String param : requestBody) {
-                    userInfo.put(param.split("=")[0], param.split("=")[1]);
-                }
-                User user = new User(userInfo.get("account"), userInfo.get("password"), userInfo.get("email"));
-                InMemoryUserRepository.save(user);
-                httpStatusCode = HttpStatusCode.CREATED;
-                path = "/index.html";
-            }
-
-            responseBody = updateResponseBody(path, responseBody);
-
-            String responseHeader = String.join("\r\n",
-                    "HTTP/1.1 " + httpStatusCode.getCode() + " " + httpStatusCode.getName() + " ",
-                    "Content-Type: " + contentType + ";charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ");
-
-            if(!location.isBlank()) {
-                responseHeader = String.join("\r\n",
-                        responseHeader,
-                        location);
-            }
-
-            final var response = String.join("\r\n",
-                    responseHeader,
-                    "",
-                    responseBody);
-
-            outputStream.write(response.getBytes());
-            outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
     }
 
-    private String updateResponseBody(String path, String responseBody) throws IOException {
-        if (!path.equals("/")) {
-            URL resource = getClass().getClassLoader().getResource("static" + path);
-            final Path filePath = new File(resource.getPath()).toPath();
-            responseBody = Files.readString(filePath);
+    private Map<String, String> readRequestHeaders(BufferedReader bufferedReader) throws IOException {
+        Map<String, String> requestHeaders = new HashMap<>();
+        String line;
+        while (!(line = bufferedReader.readLine()).isEmpty()) {
+            requestHeaders.put(line.split(": ")[0], line.split(": ")[1]);
         }
-        return responseBody;
+        return requestHeaders;
     }
 
-    private String getPathFromUrl(String url) {
-        String path = url;
-        httpStatusCode = HttpStatusCode.OK;
+    private void doGet(String url, Map<String, String> requestHeaders, OutputStream outputStream) throws IOException {
+        if (url.equals("/")) {
+            createHttpResponse(
+                    "Hello world!",
+                    createHttpResponseHeader("200 OK", "", "text/html", "Hello world!"),
+                    outputStream);
+            return;
+        }
+        String[] result = determineGetResponse(url, requestHeaders);
+        Path path = getPath(result);
+        var responseBody = Files.readString(path);
+        String contentType = Files.probeContentType(path);
+
+        String responseHeader = createHttpResponseHeader(result[0], result[2], contentType, responseBody);
+        createHttpResponse(responseBody, responseHeader, outputStream);
+    }
+
+    private String[] determineGetResponse(String url, Map<String, String> requestHeaders) {
+        String[] result = new String[3];
+        result[0] = "200 OK";
+        result[2] = "";
+        if (url.endsWith("html") || url.endsWith("js") || url.endsWith("css")) {
+            result[1] = "static" + url;
+            return result;
+        }
+        result[1] = "static" + url + ".html";
+        return result;
+    }
+
+    private Path getPath(String[] pathAndStatus) {
+        URL resource = getClass().getClassLoader().getResource(pathAndStatus[1]);
+        return new File(resource.getPath()).toPath();
+    }
+
+    private void doPost(String url, Map<String, String> requestHeaders, BufferedReader reader,
+            OutputStream outputStream
+    ) throws IOException {
+        String[] requestBody = readRequestBody(requestHeaders, reader);
+        String[] pathAndStatus = determinePostResponse(url, requestHeaders, requestBody);
+
+        final Path path = getPath(pathAndStatus);
+        var responseBody = Files.readString(path);
+        String contentType = Files.probeContentType(path);
+
+        String responseHeader = createHttpResponseHeader(pathAndStatus[0], pathAndStatus[2], contentType, responseBody);
+        createHttpResponse(responseBody, responseHeader, outputStream);
+    }
+
+    private String[] determinePostResponse(String url, Map<String, String> requestHeaders, String[] requestBody) {
+        String[] pathAndStatus = new String[3];
         if (url.startsWith("/login")) {
-            path = separateQueryString(url, path);
+            pathAndStatus = doLogin(requestHeaders, requestBody);
         }
-        if (url.startsWith("/register")) {
-            path = register(path);
+        if (url.equals("/register")) {
+            pathAndStatus = doRegister(requestBody);
         }
-        return path;
+        return pathAndStatus;
     }
 
-    private String separateQueryString(String url, String path) {
-        if (url.contains("?")) {
-            int index = url.indexOf("?");
-            String queryString = url.substring(index + 1);
-            path = validateAccount(queryString);
-        }
-        return path + ".html";
-    }
+    private String[] doLogin(Map<String, String> requestHeaders, String[] requestBody) {
+        String[] result = new String[3];
+        result[0] = "302 FOUND";
+        result[1] = "static/index.html";
 
-    private String validateAccount(String queryString) {
-        String[] params = queryString.split("&");
-        String account = params[0].split("=")[1];
-        String password = params[1].split("=")[1];
+        String account = requestBody[0].split("=")[1];
+        String password = requestBody[1].split("=")[1];
+
         User user = InMemoryUserRepository.findByAccount(account)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
         if (!user.checkPassword(password)) {
-            httpStatusCode = HttpStatusCode.UNAUTHORIZED;
-            location = "Location: /401.html ";
-            return "/401";
+            result[0] = "302 FOUND";
+            result[1] = "static/401.html";
+            result[2] = "Location: /401.html";
+            return result;
         }
-        httpStatusCode = HttpStatusCode.FOUND;
-        location = "Location: /index.html ";
+
         log.info(user.toString());
-        return "/index";
+        result[2] = checkCookie(requestHeaders);
+        return result;
     }
 
-    private String register(String path) {
+    private String checkCookie(Map<String, String> requestHeaders) {
+        String result = "Location: /index.html ";
+        HttpCookie httpCookie = new HttpCookie(requestHeaders.get("Cookie"));
+        if (!httpCookie.containsJSessionId()) {
+            result = String.join("\r\n",
+                    result,
+                    "Set-Cookie: JSESSIONID=" + UUID.randomUUID().toString() + " ");
+        }
 
-        return "/register.html";
+        return result;
+    }
+
+    private String[] readRequestBody(Map<String, String> requestHeaders, BufferedReader reader) throws IOException {
+        int contentLength = Integer.parseInt(requestHeaders.get("Content-Length"));
+        char[] buffer = new char[contentLength];
+        reader.read(buffer, 0, contentLength);
+        return new String(buffer).split("&");
+    }
+
+    private String[] doRegister(String[] requestBody) {
+        String[] result = new String[3];
+        result[0] = "302 FOUND";
+        result[1] = "static/index.html";
+        result[2] = "Location: /index.html ";
+        User user = createUser(requestBody);
+        InMemoryUserRepository.save(user);
+
+        return result;
+    }
+
+    private User createUser(String[] requestBody) {
+        Map<String, String> userInfo = new HashMap<>();
+        for (String param : requestBody) {
+            userInfo.put(param.split("=")[0], param.split("=")[1]);
+        }
+        return new User(userInfo.get("account"), userInfo.get("password"), userInfo.get("email"));
+    }
+
+    private static String createHttpResponseHeader(String status, String additionalHeader, String contentType,
+            String responseBody
+    ) {
+        String responseHeader = String.join("\r\n",
+                "HTTP/1.1 " + status + " ",
+                "Content-Type: " + contentType + ";charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ");
+        if (!additionalHeader.isBlank()) {
+            responseHeader = String.join("\r\n",
+                    responseHeader,
+                    additionalHeader);
+        }
+        return responseHeader;
+    }
+
+    private void createHttpResponse(String responseBody, String responseHeader, OutputStream outputStream)
+            throws IOException {
+        final var response = String.join("\r\n",
+                responseHeader,
+                "",
+                responseBody);
+
+        outputStream.write(response.getBytes());
+        outputStream.flush();
     }
 }

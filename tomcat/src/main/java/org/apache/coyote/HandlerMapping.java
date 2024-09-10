126135943ab8507762d99cdb6da8dca3e81ee9bb
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-09-10 17:07:35 +0900

    refactor(HandlerMapping): 에러에 대한 핸들링 처리

--- tomcat/src/main/java/org/apache/coyote/HandlerMapping.java
+++ tomcat/src/main/java/org/apache/coyote/HandlerMapping.java
@@ -5,12 +5,29 @@ import org.apache.coyote.handler.LoginHandler;
 import org.apache.coyote.handler.RegisterHandler;
 import org.apache.coyote.handler.RootEndPointHandler;
 import org.apache.coyote.handler.StaticResourceHandler;
+import org.apache.coyote.handler.exception.InternalServerErrorHandler;
+import org.apache.coyote.handler.exception.NotFoundHandler;
+import org.apache.coyote.handler.exception.UnAuthorizationHandler;
 import org.apache.http.request.HttpRequest;
 
+
 public class HandlerMapping {
+
+    private static final HandlerMapping INSTANCE = new HandlerMapping();
     private static final String PATH_DELIMITER = "/";
 
+    private HandlerMapping() {
+    }
+
+    public static HandlerMapping getInstance() {
+        return INSTANCE;
+    }
+
     public Handler getHandler(final HttpRequest httpRequest) {
+        return getHandlerByEndPoint(httpRequest);
+    }
+
+    private Handler getHandlerByEndPoint(final HttpRequest httpRequest) {
         final String path = httpRequest.getPath();
 
         if (path.equals(PATH_DELIMITER)) {
@@ -26,5 +43,17 @@ public class HandlerMapping {
         }
 
         return StaticResourceHandler.getInstance();
+    }
+
+    public Handler getHandlerByException(final Exception exception) {
+        if (exception instanceof NotFoundException) {
+            return NotFoundHandler.getInstance();
+        }
+
+        if (exception instanceof UnauthorizedException) {
+            return UnAuthorizationHandler.getInstance();
+        }
+
+        return InternalServerErrorHandler.getInstance();
     }
 }
--- tomcat/src/test/java/org/apache/coyote/HandlerMappingTest.java
+++ tomcat/src/test/java/org/apache/coyote/HandlerMappingTest.java
@@ -7,57 +7,73 @@ import org.apache.coyote.handler.LoginHandler;
 import org.apache.coyote.handler.RegisterHandler;
 import org.apache.coyote.handler.RootEndPointHandler;
 import org.apache.coyote.handler.StaticResourceHandler;
+import org.apache.coyote.handler.exception.InternalServerErrorHandler;
+import org.apache.coyote.handler.exception.NotFoundHandler;
+import org.apache.coyote.handler.exception.UnAuthorizationHandler;
 import org.apache.http.request.HttpRequest;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class HandlerMappingTest {
 
-    private HandlerMapping requestHandlerMapping;
+    @Nested
+    class getHandler {
+        @Test
+        @DisplayName("루트 경로를 포함하는 경로일 경우: RootEndPointHandler 반환")
+        void getHandler_RootPath_ReturnsRootEndPointHandler() {
+            HttpRequest request = new HttpRequest("GET", "/", "HTTP/1.1", null, null);
+            Handler handler = HandlerMapping.getInstance().getHandler(request);
+            assertThat(handler).isInstanceOf(RootEndPointHandler.class);
+        }
 
-    @BeforeEach
-    void setUp() {
-        requestHandlerMapping = new HandlerMapping();
+        @Test
+        @DisplayName("login을 포함하는 경로일 경우: LoginHandler 반환")
+        void getHandler_LoginPath_ReturnsLoginHandler() {
+            HttpRequest request = new HttpRequest("GET", "/login", "HTTP/1.1", null, null);
+            Handler handler = HandlerMapping.getInstance().getHandler(request);
+            assertThat(handler).isInstanceOf(LoginHandler.class);
+        }
+
+        @Test
+        @DisplayName("register 포함하는 경로일 경우: RegisterHandler를 반환")
+        void getHandler_RegisterPath_ReturnsRegisterHandler() {
+            HttpRequest request = new HttpRequest("GET", "/register", "HTTP/1.1", null, null);
+            Handler handler = HandlerMapping.getInstance().getHandler(request);
+            assertThat(handler).isInstanceOf(RegisterHandler.class);
+        }
+
+        @Test
+        @DisplayName("정적 리소스 경로: StaticResourceHandler 반환")
+        void getHandler_StaticResourcePath_ReturnsStaticResourceHandler() {
+            HttpRequest request = new HttpRequest("GET", "/index.html", "HTTP/1.1", null, null);
+            Handler handler = HandlerMapping.getInstance().getHandler(request);
+            assertThat(handler).isInstanceOf(StaticResourceHandler.class);
+        }
     }
 
-    @Test
-    @DisplayName("루트 경로를 포함하는 경로일 경우: RootEndPointHandler 반환")
-    void getHandler_RootPath_ReturnsRootEndPointHandler() {
-        HttpRequest request = new HttpRequest("GET", "/", "HTTP/1.1", null, null);
-        Handler handler = requestHandlerMapping.getHandler(request);
-        assertThat(handler).isInstanceOf(RootEndPointHandler.class);
-    }
+    @Nested
+    class getHandlerByException {
 
-    @Test
-    @DisplayName("login을 포함하는 경로일 경우: LoginHandler 반환")
-    void getHandler_LoginPath_ReturnsLoginHandler() {
-        HttpRequest request = new HttpRequest("GET", "/login", "HTTP/1.1", null, null);
-        Handler handler = requestHandlerMapping.getHandler(request);
-        assertThat(handler).isInstanceOf(LoginHandler.class);
-    }
+        @Test
+        @DisplayName("UnauthorizedException 처리 : UnAuthorizationHandler 반환")
+        void getHandlerByException_UnauthorizedException() {
+            Handler handler = HandlerMapping.getInstance().getHandlerByException(new UnauthorizedException("권한이 없습니다."));
+            assertThat(handler).isInstanceOf(UnAuthorizationHandler.class);
+        }
 
-    @Test
-    @DisplayName("register 포함하는 경로일 경우: RegisterHandler를 반환")
-    void getHandler_RegisterPath_ReturnsRegisterHandler() {
-        HttpRequest request = new HttpRequest("GET", "/register", "HTTP/1.1", null, null);
-        Handler handler = requestHandlerMapping.getHandler(request);
-        assertThat(handler).isInstanceOf(RegisterHandler.class);
-    }
+        @Test
+        @DisplayName("UnauthorizedException 처리 : UnAuthorizationHandler 반환")
+        void getHandlerByException_NotFoundException() {
+            Handler handler = HandlerMapping.getInstance().getHandlerByException(new NotFoundException("존재하지 않는 리소스입니다.."));
+            assertThat(handler).isInstanceOf(NotFoundHandler.class);
+        }
 
-    @Test
-    @DisplayName("정적 리소스 경로: StaticResourceHandler 반환")
-    void getHandler_StaticResourcePath_ReturnsStaticResourceHandler() {
-        HttpRequest request = new HttpRequest("GET", "/index.html", "HTTP/1.1", null, null);
-        Handler handler = requestHandlerMapping.getHandler(request);
-        assertThat(handler).isInstanceOf(StaticResourceHandler.class);
-    }
-
-    @Test
-    @DisplayName("알 수 없는 경로: StaticResourceHandler를 반환")
-    void getHandler_UnknownPath_ReturnsStaticResourceHandler() {
-        HttpRequest request = new HttpRequest("GET", "/unknown/path", "HTTP/1.1", null, null);
-        Handler handler = requestHandlerMapping.getHandler(request);
-        assertThat(handler).isInstanceOf(StaticResourceHandler.class);
+        @Test
+        @DisplayName("알 수 없는 에러: InternalServerErrornHandler 반환")
+        void getHandlerByException_UnknownPath_ReturnsStaticResourceHandler() {
+            Handler handler = HandlerMapping.getInstance().getHandlerByException(new Exception("에러"));
+            assertThat(handler).isInstanceOf(InternalServerErrorHandler.class);
+        }
     }
 }

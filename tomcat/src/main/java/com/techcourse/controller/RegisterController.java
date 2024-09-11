cd40c645833a19240cfb7808f4007129e1b1ed0f
KIMGYUTAE <kiimwf0819@gmail.com>
2024-09-11 16:01:47 +0900

    refactor: 변경 사항에 맞게 RegisterController 리팩토링

--- tomcat/src/main/java/com/techcourse/controller/RegisterController.java
+++ tomcat/src/main/java/com/techcourse/controller/RegisterController.java
@@ -1,70 +1,51 @@
 package com.techcourse.controller;
 
 import com.techcourse.db.InMemoryUserRepository;
+import com.techcourse.exception.MissingRequestBodyException;
 import com.techcourse.model.User;
-import java.io.IOException;
 import java.util.Map;
-import org.was.view.View;
-import org.was.view.ViewResolver;
+import org.apache.coyote.http11.HttpHeaders;
+import org.apache.coyote.http11.HttpStatusCode;
 import org.apache.coyote.http11.request.HttpRequest;
-import org.apache.coyote.http11.response.HttpResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.was.Controller.AbstractController;
 import org.was.Controller.ResponseResult;
 
 public class RegisterController extends AbstractController {
 
-    private static final Logger log = LoggerFactory.getLogger(RegisterController.class);
+    @Override
+    protected ResponseResult doGet(HttpRequest request) {
+        return ResponseResult
+                .status(HttpStatusCode.OK)
+                .path("/register.html");
+    }
 
     @Override
     protected ResponseResult doPost(HttpRequest request) {
-        return null;
-    }
-
-    @Override
-    protected ResponseResult doGet(HttpRequest request) {
-        return null;
-    }
-
-    @Override
-    protected void doPost(HttpRequest request, HttpResponse response) {
         try {
-            if (!request.hasBodyData()) {
-                throw new IllegalArgumentException("RequestBody is missing in the request");
-            }
-
-            Map<String, String> requestFormData = request.getFormData();
-            String account = requestFormData.get("account");
-            String password = requestFormData.get("password");
-            String email = requestFormData.get("email");
-
-            User user = new User(account, password, email);
+            User user = extractUserByFormData(request);
             InMemoryUserRepository.save(user);
-            responseRegisterSuccess(response);
+            return ResponseResult
+                    .status(HttpStatusCode.FOUND)
+                    .header(HttpHeaders.LOCATION.getName(), "/index.html")
+                    .build();
 
-        } catch (IllegalArgumentException e) {
-            response.setStatus400();
-            response.setResponseBody(e.getMessage());
-            log.info("Bad Request: {}", e.getMessage());
-
+        } catch (MissingRequestBodyException e) {
+            return ResponseResult
+                    .status(HttpStatusCode.BAD_REQUEST)
+                    .body(e.getMessage());
         }
     }
 
-    @Override
-    protected void doGet(HttpRequest request, HttpResponse response) throws IOException {
-        responseRegisterPage(request, response);
-    }
+    private User extractUserByFormData(HttpRequest request) {
+        if (!request.hasBodyData()) {
+            throw new MissingRequestBodyException();
+        }
 
-    private void responseRegisterSuccess(HttpResponse response) {
-        response.setStatus302();
-        response.setLocation("/index.html");
-    }
+        Map<String, String> requestFormData = request.getFormData();
+        String account = requestFormData.get("account");
+        String password = requestFormData.get("password");
+        String email = requestFormData.get("email");
 
-    private void responseRegisterPage(HttpRequest request, HttpResponse response) throws IOException {
-        View view = ViewResolver.getView("/register.html");
-        response.setStatus200();
-        response.setResponseBody(view.getContent());
-        response.setContentType(request.getContentType());
+        return new User(account, password, email);
     }
 }

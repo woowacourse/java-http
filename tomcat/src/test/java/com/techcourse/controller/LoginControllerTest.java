1551943ca533f872eb2655f5efa55189ce03679b
Arachneee <jhg2819@naver.com>
2024-09-11 21:17:54 +0900

    test: controller 테스트 추가

---
+++ tomcat/src/test/java/com/techcourse/controller/LoginControllerTest.java
@@ -0,0 +1,151 @@
+package com.techcourse.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import org.apache.coyote.HttpRequest;
+import org.apache.coyote.HttpResponse;
+import org.apache.coyote.http11.request.Http11Request;
+import org.apache.coyote.http11.response.Http11Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LoginControllerTest {
+
+    @DisplayName("로그인에 성공하면 /index.html 파일로 리다이렉트 된다.")
+    @Test
+    void doPostSuccess() throws IOException {
+        String httpRequestString = String.join(
+                "\r\n",
+                "POST /login HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "Content-Length: 30 ",
+                "",
+                "account=gugu&password=password"
+        );
+        InputStream inputStream = new ByteArrayInputStream(httpRequestString.getBytes());
+        HttpRequest httpRequest = Http11Request.of(inputStream, () -> "abcdefg");
+        HttpResponse httpResponse = Http11Response.create();
+
+        LoginController loginController = new LoginController();
+        loginController.doPost(httpRequest, httpResponse);
+
+        assertThat(httpResponse.toString()).isEqualTo(
+                String.join(
+                        "\r\n",
+                        "HTTP/1.1 302 Found ",
+                        "Set-Cookie: JSESSIONID=abcdefg ",
+                        "Location: /index.html ",
+                        ""
+                )
+        );
+    }
+
+    @DisplayName("로그인에 실패하면 /401.html 파일로 리다이렉트 된다.")
+    @Test
+    void doPostFail() throws IOException {
+        String httpRequestString = String.join(
+                "\r\n",
+                "POST /login HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "Content-Length: 29 ",
+                "",
+                "account=gugu&password=passwod"
+        );
+        InputStream inputStream = new ByteArrayInputStream(httpRequestString.getBytes());
+        HttpRequest httpRequest = Http11Request.of(inputStream, () -> "abcdefg");
+        HttpResponse httpResponse = Http11Response.create();
+
+        LoginController loginController = new LoginController();
+        loginController.doPost(httpRequest, httpResponse);
+
+        assertThat(httpResponse.toString()).isEqualTo(
+                String.join(
+                        "\r\n",
+                        "HTTP/1.1 302 Found ",
+                        "Location: /401.html ",
+                        ""
+                )
+        );
+    }
+
+    @DisplayName("이미 로그인 중이면 /index.html 파일로 리다이렉트 된다.")
+    @Test
+    void doGetAlreadyLogin() throws IOException {
+        LoginController loginController = new LoginController();
+        String login = String.join(
+                "\r\n",
+                "POST /login HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "Content-Length: 30 ",
+                "",
+                "account=gugu&password=password"
+        );
+        InputStream inputStream = new ByteArrayInputStream(login.getBytes());
+        HttpRequest httpRequest = Http11Request.of(inputStream, () -> "abcdefg");
+        HttpResponse httpResponse = Http11Response.create();
+
+        loginController.doPost(httpRequest, httpResponse);
+
+        String getLogin = String.join(
+                "\r\n",
+                "GET /login HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "Cookie: JSESSIONID=abcdefg ",
+                ""
+        );
+        InputStream inputStream1 = new ByteArrayInputStream(getLogin.getBytes());
+        HttpRequest httpRequest1 = Http11Request.of(inputStream1, () -> "abcdefg");
+        HttpResponse httpResponse1 = Http11Response.create();
+
+        loginController.doGet(httpRequest1, httpResponse1);
+
+        assertThat(httpResponse1.toString()).isEqualTo(
+                String.join(
+                        "\r\n",
+                        "HTTP/1.1 302 Found ",
+                        "Location: /index.html ",
+                        ""
+                )
+        );
+    }
+
+    @DisplayName("로그인 GET 요청이면 /login.html 을 응답한다.")
+    @Test
+    void doGet() throws IOException {
+        LoginController loginController = new LoginController();
+        String getLogin = String.join(
+                "\r\n",
+                "GET /login HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                ""
+        );
+        InputStream inputStream = new ByteArrayInputStream(getLogin.getBytes());
+        HttpRequest httpRequest = Http11Request.of(inputStream, () -> "abcdefg");
+        HttpResponse httpResponse = Http11Response.create();
+
+        loginController.doGet(httpRequest, httpResponse);
+
+        final URL resource = getClass().getClassLoader().getResource("static/login.html");
+        assertThat(httpResponse.toString()).isEqualTo(
+                String.join(
+                        "\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Length: 3797 ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "",
+                        new String(Files.readAllBytes(new File(resource.getFile()).toPath()))
+                )
+        );
+    }
+}
---
+++ tomcat/src/test/java/com/techcourse/controller/RegisterControllerTest.java
@@ -0,0 +1,79 @@
+package com.techcourse.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import org.apache.coyote.HttpRequest;
+import org.apache.coyote.HttpResponse;
+import org.apache.coyote.http11.request.Http11Request;
+import org.apache.coyote.http11.response.Http11Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RegisterControllerTest {
+
+    @DisplayName("회원가입 GET 요청이면 /register.html 을 응답한다.")
+    @Test
+    void doGet() throws IOException {
+        RegisterController registerController = new RegisterController();
+        String getLogin = String.join(
+                "\r\n",
+                "GET /register HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                ""
+        );
+        InputStream inputStream = new ByteArrayInputStream(getLogin.getBytes());
+        HttpRequest httpRequest = Http11Request.of(inputStream, () -> "abcdefg");
+        HttpResponse httpResponse = Http11Response.create();
+
+        registerController.doGet(httpRequest, httpResponse);
+
+        final URL resource = getClass().getClassLoader().getResource("static/register.html");
+        assertThat(httpResponse.toString()).isEqualTo(
+                String.join(
+                        "\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Length: 4319 ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "",
+                        new String(Files.readAllBytes(new File(resource.getFile()).toPath()))
+                )
+        );
+    }
+
+    @DisplayName("회원가입 성공후 /index.html 파일로 리다이렉트 된다.")
+    @Test
+    void doPost() throws IOException {
+        RegisterController registerController = new RegisterController();
+        String register = String.join(
+                "\r\n",
+                "POST /register HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "Content-Length: 50 ",
+                "",
+                "account=gugu&password=password&email=abc@naver.com"
+        );
+        InputStream inputStream = new ByteArrayInputStream(register.getBytes());
+        HttpRequest httpRequest = Http11Request.of(inputStream, () -> "abcdefg");
+        HttpResponse httpResponse = Http11Response.create();
+
+        registerController.doPost(httpRequest, httpResponse);
+
+        assertThat(httpResponse.toString()).isEqualTo(
+                String.join(
+                        "\r\n",
+                        "HTTP/1.1 302 Found ",
+                        "Location: /index.html ",
+                        ""
+                )
+        );
+    }
+}

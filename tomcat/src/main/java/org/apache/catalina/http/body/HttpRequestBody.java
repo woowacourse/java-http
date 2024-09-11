8f3eea0d49c75af264c34ce0079381faead5c556
HyungHoKim00 <hkim1109@naver.com>
2024-09-11 16:46:54 +0900

    refactor: stream을 활용하도록 로직 변경

--- tomcat/src/main/java/org/apache/catalina/http/body/HttpRequestBody.java
+++ tomcat/src/main/java/org/apache/catalina/http/body/HttpRequestBody.java
@@ -1,28 +1,33 @@
 package org.apache.catalina.http.body;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 import org.apache.catalina.exception.CatalinaException;
 
 public class HttpRequestBody {
 
-    private final Map<String, String> body;
+    private static final int KEY_INDEX = 0;
+    private static final int VALUE_INDEX = 1;
 
-    public HttpRequestBody(Map<String, String> body) {
-        this.body = body;
-    }
+    private final Map<String, String> body;
 
     public HttpRequestBody() {
         this(new HashMap<>());
     }
 
+    public HttpRequestBody(Map<String, String> body) {
+        this.body = body;
+    }
+
     public static HttpRequestBody parseUrlEncoded(String urlEncoded) {
-        Map<String, String> body = new HashMap<>();
-        String[] keyAndValues = urlEncoded.split("&");
-        for (String keyAndValue : keyAndValues) {
-            String[] keyValue = keyAndValue.split("=");
-            body.put(keyValue[0], keyValue[1]);
-        }
+        Map<String, String> body = Arrays.stream(urlEncoded.split("&"))
+                .map(parameter -> parameter.split("="))
+                .collect(Collectors.toMap(
+                        parameter -> parameter[KEY_INDEX],
+                        parameter -> parameter[VALUE_INDEX]
+                ));
         return new HttpRequestBody(body);
     }
 

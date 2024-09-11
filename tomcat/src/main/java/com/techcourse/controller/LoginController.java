8cd92edd21fba144f0ccf2e0076d65a64b9dff7e
HyungUk Ryu <hw0603@naver.com>
2024-09-12 00:48:53 +0900

    refactor(LoginController): Optional API를 활용하여 분기하도록 변경

--- tomcat/src/main/java/com/techcourse/controller/LoginController.java
+++ tomcat/src/main/java/com/techcourse/controller/LoginController.java
@@ -3,8 +3,6 @@ package com.techcourse.controller;
 import com.techcourse.controller.dto.LoginRequest;
 import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.model.User;
-import jakarta.servlet.http.HttpSession;
-import java.util.Optional;
 import java.util.UUID;
 import org.apache.catalina.Session;
 import org.apache.catalina.SessionManager;
@@ -21,6 +19,8 @@ public class LoginController extends AbstractController {
 
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
     private static final ResponseFile loginPage = StaticResourceManager.read("/login.html");
+    private static final String SESSION_KEY_USER = "user";
+
     private final SessionManager sessionManager;
 
     public LoginController() {
@@ -34,16 +34,12 @@ public class LoginController extends AbstractController {
 
     @Override
     public void doGet(HttpRequest request, HttpResponse response) {
-        Optional<HttpSession> session = sessionManager.getSession(request.getSessionId());
-        boolean isLogin = session.map(s -> s.getAttribute("user") != null)
-                .orElse(false);
-        if (isLogin) {
-            response.sendRedirect("index.html");
-            return;
-        }
-
-        response.setStatus(HttpStatusCode.OK)
-                .setBody(loginPage);
+        sessionManager.getSession(request.getSessionId())
+                .map(s -> s.getAttribute(SESSION_KEY_USER))
+                .ifPresentOrElse(
+                        user -> response.sendRedirect("index.html"),
+                        () -> response.setStatus(HttpStatusCode.OK).setBody(loginPage)
+                );
     }
 
     @Override
@@ -53,21 +49,25 @@ public class LoginController extends AbstractController {
         String userPassword = loginRequest.password();
         log.info("로그인 요청 -  id: {}, password: {}", userAccount, userPassword);
 
-        User user = InMemoryUserRepository.findByAccount(userAccount)
-                .orElse(null);
+        InMemoryUserRepository.findByAccount(userAccount)
+                .filter(user -> user.checkPassword(userPassword))
+                .ifPresentOrElse(
+                        user -> doLogin(request, response, user),
+                        () -> response.sendRedirect("/401.html")
+                );
+    }
 
-        if (user == null || !user.checkPassword(userPassword)) {
-            response.sendRedirect("/401.html");
-            return;
-        }
-
-        // 세션이 있으면 세션에 유저 정보를 넣어주고, 없다면 생성해서 넣어줌
-        HttpSession httpSession = sessionManager.getSession(request.getSessionId())
-                .orElse(new Session(UUID.randomUUID().toString()));
-        httpSession.setAttribute("user", user);
-        sessionManager.add(httpSession);
-
+    private void doLogin(HttpRequest request, HttpResponse response, User user) {
+        Session session = sessionManager.getSession(request.getSessionId())
+                .orElseGet(this::registerNewSession);
+        session.setAttribute(SESSION_KEY_USER, user);
         response.sendRedirect("/index.html")
-                .setCookie("JSESSIONID", httpSession.getId());
+                .setCookie("JSESSIONID", session.getId());
+    }
+
+    private Session registerNewSession() {
+        Session newSession = new Session(UUID.randomUUID().toString());
+        sessionManager.add(newSession);
+        return newSession;
     }
 }

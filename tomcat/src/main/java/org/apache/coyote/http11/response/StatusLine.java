b732ce5b21c9c5bdf41b61d43454c1d2ed70b303
Minjoo <m0522inju@gmail.com>
2024-09-11 14:54:16 +0900

    refactor: default http 버전을 사용할 수 있도록 개선

--- tomcat/src/main/java/org/apache/coyote/http11/response/StatusLine.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/StatusLine.java
@@ -4,12 +4,18 @@ import org.apache.coyote.http11.HttpStatus;
 
 public class StatusLine {
 
+    private static final String HTTP_VERSION = "HTTP/1.1";
+
     private final String protocolVersion;
     private final HttpStatus status;
 
     public StatusLine(String protocolVersion, HttpStatus status) {
         this.protocolVersion = protocolVersion;
         this.status = status;
+    }
+
+    public StatusLine(HttpStatus status) {
+        this(HTTP_VERSION, status);
     }
 
     public String formatStatusLine() {

931e2321e82c7d37542048b70917ea4490603ad8
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-09-11 23:35:34 +0900

    test: RequestHeader 관련 테스트 이동

--- tomcat/src/test/java/org/apache/catalina/request/RequestTest.java
+++ tomcat/src/test/java/org/apache/catalina/request/RequestTest.java
@@ -4,9 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Map;
 
-import org.apache.catalina.auth.HttpCookie;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class RequestTest {
@@ -20,62 +18,6 @@ class RequestTest {
         request.setBody(expected);
 
         Map<String, String> actual = request.getBody();
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Nested
-    @DisplayName("response 파일 타입 조회")
-    class getFileType {
-        @Test
-        @DisplayName("성공 : response 시 원하는 파일 타입 조회")
-        void getFileTypeSuccess() {
-            Request request = new Request("GET /index.html HTTP/1.1", Map.of("Accept", "text/css"));
-
-            String actual = request.getFileType();
-
-            assertThat(actual).isEqualTo("text/css");
-        }
-
-        @Test
-        @DisplayName("성공 : header에 해당 정보가 없으면 text/html을 기본 값으로 설정")
-        void getFileTypeSuccessByNotContainAccept() {
-            Request request = new Request("GET /index.html HTTP/1.1", Map.of());
-
-            String actual = request.getFileType();
-
-            assertThat(actual).isEqualTo("text/html");
-        }
-    }
-
-    @Test
-    @DisplayName("성공 : 콘텐트 길이 조회 가능")
-    void getContentLengthSuccess() {
-        int expected = 100;
-        Request request = new Request("GET /index.html HTTP/1.1", Map.of("Content-Length", String.valueOf(expected)));
-
-        int actual = request.getContentLength();
-
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    @DisplayName("성공 : header에 해당 정보가 없으면 0을 기본 값으로 설정")
-    void getContentLengthSuccessByNotContaionLength() {
-        Request request = new Request("GET /index.html HTTP/1.1", Map.of());
-
-        int actual = request.getContentLength();
-
-        assertThat(actual).isZero();
-    }
-
-    @Test
-    @DisplayName("성공 : 쿠키를 조회 가능")
-    void getCookie() {
-        Request request = new Request("GET /index.html HTTP/1.1", Map.of("Cookie", "Id=324;JSessionId=34567"));
-
-        HttpCookie actual = request.getCookie();
-
-        HttpCookie expected = new HttpCookie(Map.of("Id", "324", "JSessionId", "34567"));
         assertThat(actual).isEqualTo(expected);
     }
 }

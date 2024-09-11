92d4a9b75002560537620d7d973d3f36f2d1532e
takoyakimchi <c4nyou@gmail.com>
2024-09-11 14:06:29 +0900

    test: HttpCookieTest 보완

--- tomcat/src/test/java/org/apache/coyote/http11/HttpCookieTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/HttpCookieTest.java
@@ -1,6 +1,7 @@
 package org.apache.coyote.http11;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,9 +18,49 @@ class HttpCookieTest {
 
     @DisplayName("쿠키에 세션 추가")
     @Test
-    void addSession() {
+    void setSession() {
         HttpCookie httpCookie = new HttpCookie();
         httpCookie.setSession("value1");
         assertThat(httpCookie.buildMessage()).isEqualTo("JSESSIONID=value1");
+    }
+
+    @DisplayName("쿠키에 세션이 포함되어 있는지 여부 정상 반환")
+    @Test
+    void hasSession() {
+        // given
+        HttpCookie cookieWithSession = new HttpCookie("JSESSIONID=test-session; tasty_cookie=strawberry");
+        HttpCookie cookieWithoutSession = new HttpCookie("yummy_cookie=choco; tasty_cookie=strawberry");
+        HttpCookie blankCookie = new HttpCookie();
+
+        // when & then
+        assertAll(
+                () -> assertThat(cookieWithSession.hasSession()).isTrue(),
+                () -> assertThat(cookieWithoutSession.hasSession()).isFalse(),
+                () -> assertThat(blankCookie.hasSession()).isFalse()
+        );
+    }
+
+    @DisplayName("쿠키에서 세션 조회")
+    @Test
+    void getSession() {
+        // given
+        HttpCookie cookie = HttpCookie.ofSessionId("testsession");
+
+        // when & then
+        assertThat(cookie.getSession()).isEqualTo("testsession");
+    }
+
+    @DisplayName("쿠키를 String으로 변환")
+    @Test
+    void buildMessage() {
+        // given
+        HttpCookie cookie = new HttpCookie("yummy_cookie=choco");
+        cookie.setSession("testsession");
+
+        // when
+        assertThat(cookie.buildMessage()).containsAnyOf(
+                "yummy_cookie=choco; JSESSIONID=testsession",
+                "JSESSIONID=testsession; yummy_cookie=choco"
+        );
     }
 }

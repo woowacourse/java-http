ecda70437932a21d3eabb83f6435009e2ad596f2
ay-eonii <naayen@naver.com>
2024-09-11 13:51:49 +0900

    feat: 캐시 인터셉터에서 `no-cache`, `private` 디렉티브 지정

--- study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
+++ study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
@@ -1,5 +1,6 @@
 package cache.com.example.cachecontrol;
 
+import cache.com.example.interceptor.CacheInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
---
+++ study/src/main/java/cache/com/example/interceptor/CacheInterceptor.java
@@ -0,0 +1,15 @@
+package cache.com.example.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class CacheInterceptor implements HandlerInterceptor {
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        response.addHeader(HttpHeaders.CACHE_CONTROL, "no-cache, private");
+    }
+}

59833fc6e51f65d92c8921c4c259672d9c7ccf4b
Minjoo <m0522inju@gmail.com>
2024-09-11 14:57:40 +0900

    refactor: 200 응답과 정적 리소스 응답을 HttpResponse에서 할 수 있도록 개선

--- tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponse.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponse.java
@@ -1,17 +1,26 @@
 package org.apache.coyote.http11.response;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import org.apache.coyote.http11.ContentMimeType;
 import org.apache.coyote.http11.HttpHeaderKey;
 import org.apache.coyote.http11.HttpStatus;
 import org.apache.coyote.http11.cookie.Cookie;
 
 public class HttpResponse {
 
+    private static final String CHARSET_UTF8 = ";charset=utf-8";
+    private static final String LINE_SEPARATOR = "\r\n";
+    private static final String STATIC_FILES_BASE_PATH = "static";
+    private static final String FILE_EXTENSION_DELIMITER = ".";
+
     private final OutputStream outputStream;
-    private final StatusLine statusLine;
     private final ResponseHeaders headers;
-    private final ResponseBody body;
+    private StatusLine statusLine;
+    private ResponseBody body;
 
     private HttpResponse(
             OutputStream outputStream,
@@ -26,36 +35,58 @@ public class HttpResponse {
         this.body = body;
     }
 
-    public HttpResponse(OutputStream outputStream, HttpStatus status, String protocolVersion) {
-        this(outputStream, status, protocolVersion, new ResponseHeaders(), null);
-    }
-
-    public HttpResponse(OutputStream outputStream, HttpStatus status, String protocolVersion, ResponseBody body) {
-        this(outputStream, status, protocolVersion, new ResponseHeaders(), body);
-        headers.add(HttpHeaderKey.CONTENT_TYPE, body.getContentType() + ";charset=utf-8");
-        headers.add(HttpHeaderKey.CONTENT_LENGTH, body.getContentLength());
+    public HttpResponse(OutputStream outputStream) {
+        this(outputStream, null, null, new ResponseHeaders(), null);
     }
 
     public void setCookie(Cookie cookie) {
         headers.add(HttpHeaderKey.SET_COOKIE, cookie.getCookieString());
     }
 
+    public void setBody(ResponseBody body) {
+        this.body = body;
+        headers.add(HttpHeaderKey.CONTENT_TYPE, body.getContentType() + CHARSET_UTF8);
+        headers.add(HttpHeaderKey.CONTENT_LENGTH, body.getContentLength());
+    }
+
+    public void sendStaticResourceResponse(String urlWithExtension) throws IOException {
+        this.statusLine = new StatusLine(HttpStatus.OK);
+        int index = urlWithExtension.lastIndexOf(FILE_EXTENSION_DELIMITER);
+        String url = urlWithExtension.substring(0, index);
+        String extension = urlWithExtension.substring(index + 1);
+        String responseBody = readFileContent(url, extension);
+        setBody(new ResponseBody(ContentMimeType.getMimeByExtension(extension), responseBody));
+        outputStream.write(getResponse().getBytes());
+        outputStream.flush();
+    }
+
+    public void send200Response() throws IOException {
+        this.statusLine = new StatusLine(HttpStatus.OK);
+        outputStream.write(getResponse().getBytes());
+        outputStream.flush();
+    }
+
     public void sendRedirect(String location) throws IOException {
+        this.statusLine = new StatusLine(HttpStatus.FOUND);
         headers.add(HttpHeaderKey.LOCATION, location);
         outputStream.write(getResponse().getBytes());
         outputStream.flush();
     }
 
-    public void sendResponse() throws IOException {
-        outputStream.write(getResponse().getBytes());
-        outputStream.flush();
+    private String readFileContent(String url, String extension) throws IllegalArgumentException, IOException {
+        URL resource = getClass().getClassLoader()
+                .getResource(STATIC_FILES_BASE_PATH + url + FILE_EXTENSION_DELIMITER + extension);
+        if (resource == null) {
+            throw new IllegalArgumentException(STATIC_FILES_BASE_PATH + url + FILE_EXTENSION_DELIMITER + extension + "이 존재하지 않습니다.");
+        }
+        return new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
     }
 
     private String getResponse() {
         StringBuilder responseBuilder = new StringBuilder();
-        responseBuilder.append(statusLine.formatStatusLine()).append("\r\n");
-        responseBuilder.append(headers.getHeaderResponse()).append("\r\n");
-        responseBuilder.append("\r\n");
+        responseBuilder.append(statusLine.formatStatusLine()).append(LINE_SEPARATOR);
+        responseBuilder.append(headers.getHeaderResponse()).append(LINE_SEPARATOR);
+        responseBuilder.append(LINE_SEPARATOR);
 
         if (body != null) {
             responseBuilder.append(body.getValue());

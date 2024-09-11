2c7ebf85cf1c4f1fd267d334f5e4b6aa6ef52b22
hangillee <skfcb10@naver.com>
2024-09-11 18:22:04 +0900

    test: HTTP 프로세서 테스트 작성

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import support.StubSocket;
@@ -52,11 +53,13 @@ class Http11ProcessorTest {
 
         // then
         final URL resource = getClass().getClassLoader().getResource("static/index.html");
+        final var body = Files.readAllBytes(new File(resource.getFile()).toPath());
+
         var expected = "HTTP/1.1 200 OK \r\n" +
                 "Content-Type: text/html;charset=utf-8 \r\n" +
-                "Content-Length: 5564 \r\n" +
+                "Content-Length: " + body.length + " \r\n" +
                 "\r\n" +
-                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+                new String(body);
 
         assertThat(socket.output()).isEqualTo(expected);
     }
@@ -80,11 +83,13 @@ class Http11ProcessorTest {
 
         // then
         final URL resource = getClass().getClassLoader().getResource("static/css/styles.css");
+        final var body = Files.readAllBytes(new File(resource.getFile()).toPath());
+
         var expected = "HTTP/1.1 200 OK \r\n" +
                 "Content-Type: text/css;charset=utf-8 \r\n" +
-                "Content-Length: 211992 \r\n" +
+                "Content-Length: " + (body.length + 1) + " \r\n" +
                 "\r\n" +
-                new String(Files.readAllBytes(new File(resource.getFile()).toPath())) + "\n";
+                new String(body) + "\n";
 
         assertThat(socket.output()).isEqualTo(expected);
     }
@@ -108,12 +113,194 @@ class Http11ProcessorTest {
 
         // then
         final URL resource = getClass().getClassLoader().getResource("static/js/scripts.js");
+        final var body = Files.readAllBytes(new File(resource.getFile()).toPath());
+
         var expected = "HTTP/1.1 200 OK \r\n" +
                 "Content-Type: text/javascript;charset=utf-8 \r\n" +
-                "Content-Length: 976 \r\n" +
+                "Content-Length: " + body.length + " \r\n" +
                 "\r\n" +
-                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+                new String(body);
 
         assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("로그인 페이지로 이동할 수 있다.")
+    void loginPage() throws IOException {
+        // given
+        final String httpRequest = String.join("\r\n",
+                "GET /login.html HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+        final var socket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        final URL resource = getClass().getClassLoader().getResource("static/login.html");
+        final var body = Files.readAllBytes(new File(resource.getFile()).toPath());
+
+        var expected = "HTTP/1.1 200 OK \r\n" +
+                "Content-Type: text/html;charset=utf-8 \r\n" +
+                "Content-Length: " + body.length + " \r\n" +
+                "\r\n" +
+                new String(body);
+
+        assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("로그인 할 수 있다.")
+    void login() {
+        // given
+        final String httpRequest = String.join("\r\n",
+                "POST /login HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "Content-Length: 30",
+                "Content-Type: application/x-www-form-urlencoded ",
+                "Accept: */*",
+                "",
+                "account=gugu&password=password ");
+
+        final var socket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        List<String> expected = List.of(
+                "HTTP/1.1 302 Found \r\n",
+                "Set-Cookie: JSESSIONID=",
+                "Location: /index.html \r\n"
+        );
+
+        assertThat(socket.output()).contains(expected);
+    }
+
+    @Test
+    @DisplayName("로그인 실패 시, 401.html로 리다이렉트한다.")
+    void loginFailed() {
+        // given
+        final String httpRequest = String.join("\r\n",
+                "POST /login HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "Content-Length: 30",
+                "Content-Type: application/x-www-form-urlencoded ",
+                "Accept: */*",
+                "",
+                "account=gugu&password=wrong ");
+
+        final var socket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        String expected = String.join("\r\n",
+                "HTTP/1.1 302 Found ",
+                "Location: /401.html ",
+                ""
+        );
+
+        assertThat(socket.output()).contains(expected);
+    }
+
+    @Test
+    @DisplayName("회원가입 페이지로 이동할 수 있다.")
+    void registerPage() throws IOException {
+        // given
+        final String httpRequest = String.join("\r\n",
+                "GET /register.html HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+        final var socket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        final URL resource = getClass().getClassLoader().getResource("static/register.html");
+        final var body = Files.readAllBytes(new File(resource.getFile()).toPath());
+
+        var expected = "HTTP/1.1 200 OK \r\n" +
+                "Content-Type: text/html;charset=utf-8 \r\n" +
+                "Content-Length: " + body.length + " \r\n" +
+                "\r\n" +
+                new String(body);
+
+        assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("회원가입 할 수 있다.")
+    void register() {
+        // given
+        final String httpRequest = String.join("\r\n",
+                "POST /register HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "Content-Length: 30",
+                "Content-Type: application/x-www-form-urlencoded ",
+                "Accept: */*",
+                "",
+                "account=gugu2&password=password&email=hkkang%40woowahan.com ");
+
+        final var socket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        List<String> expected = List.of(
+                "HTTP/1.1 302 Found \r\n",
+                "Location: /index.html \r\n"
+        );
+
+        assertThat(socket.output()).contains(expected);
+    }
+
+    @Test
+    @DisplayName("회원 가입 실패 시, 401.html로 리다이렉트한다.")
+    void registerFailed() {
+        // given
+        final String formData = "account=gugu&password=password&email=hkkang%40woowahan.com";
+        final String httpRequest = String.join("\r\n",
+                "POST /register HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "Content-Length: " + formData.length(),
+                "Content-Type: application/x-www-form-urlencoded ",
+                "Accept: */*",
+                "",
+                formData);
+
+        final var socket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        String expected = String.join("\r\n",
+                "HTTP/1.1 302 Found ",
+                "Location: /401.html ",
+                ""
+        );
+
+        assertThat(socket.output()).contains(expected);
     }
 }

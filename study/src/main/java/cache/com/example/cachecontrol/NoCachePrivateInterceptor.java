aa5bb236c4c5ccda2f0f0ede8f6a93ca5d5cce2c
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-09-11 17:41:21 +0900

    study: Spring Framework에서 제공하는 값을 사용하도록 리팩토링 실시

--- study/src/main/java/cache/com/example/cachecontrol/NoCachePrivateInterceptor.java
+++ study/src/main/java/cache/com/example/cachecontrol/NoCachePrivateInterceptor.java
@@ -2,10 +2,15 @@ package cache.com.example.cachecontrol;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 public class NoCachePrivateInterceptor implements HandlerInterceptor {
+
+    public static final String NO_CACHE_PRIVATE = CacheControl.noCache().cachePrivate().getHeaderValue();
+
     @Override
     public void postHandle(
             HttpServletRequest request,
@@ -13,6 +18,6 @@ public class NoCachePrivateInterceptor implements HandlerInterceptor {
             Object handler,
             ModelAndView modelAndView
     ) {
-        response.setHeader("Cache-Control", "no-cache, private");
+        response.setHeader(HttpHeaders.CACHE_CONTROL, NO_CACHE_PRIVATE);
     }
 }

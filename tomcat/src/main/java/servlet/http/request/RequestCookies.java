1de054185b8650722715a1603123af4a62ca44b8
Nam Gi Beom <rlqja0523@naver.com>
2024-09-11 17:45:30 +0900

    feat: RequestCookies key, value 검증 추가

--- tomcat/src/main/java/servlet/http/request/RequestCookies.java
+++ tomcat/src/main/java/servlet/http/request/RequestCookies.java
@@ -10,6 +10,12 @@ import java.util.Map;
 public class RequestCookies {
 
     private static final RequestCookies EMPTY = new RequestCookies(Collections.emptyMap());
+    private static final String COOKIE_DELIMITER = "; ";
+    private static final String KEY_VALUE_DELIMITER = "=";
+    private static final int UNLIMITED_SPLIT = -1;
+    private static final int VALID_KEY_VALUE_LENGTH = 2;
+    private static final int KEY_INDEX = 0;
+    private static final int VALUE_INDEX = 1;
 
     private final Map<String, String> cookies;
 
@@ -18,12 +24,33 @@ public class RequestCookies {
     }
 
     protected static RequestCookies from(String cookies) {
-        if (cookies == null) {
+        if (isEmptyCookies(cookies)) {
             return EMPTY;
         }
-        return Arrays.stream(cookies.split("; "))
-                .map(cookie -> cookie.split("=", 2))
-                .collect(collectingAndThen(toMap(c -> c[0], c -> c[1]), RequestCookies::new));
+        return Arrays.stream(cookies.split(COOKIE_DELIMITER))
+                .map(RequestCookies::splitKeyValue)
+                .collect(collectingAndThen(
+                        toMap(c -> c[KEY_INDEX].strip(), c -> c[VALUE_INDEX].strip()), RequestCookies::new)
+                );
+    }
+
+    private static String[] splitKeyValue(String cookie) {
+        String[] keyValue = cookie.split(KEY_VALUE_DELIMITER, UNLIMITED_SPLIT);
+        if (keyValue.length != VALID_KEY_VALUE_LENGTH) {
+            throw new IllegalArgumentException("잘못된 Cookie입니다. request cookie: '%s'".formatted(cookie));
+        }
+        validateNotBlankKeyValue(keyValue[KEY_INDEX], keyValue[VALUE_INDEX]);
+        return keyValue;
+    }
+
+    private static void validateNotBlankKeyValue(String key, String value) {
+        if (key.isBlank() || value.isBlank()) {
+            throw new IllegalArgumentException("key 또는 value가 비어있습니다. key: '%s', value: '%s'".formatted(key, value));
+        }
+    }
+
+    private static boolean isEmptyCookies(String cookies) {
+        return cookies == null || cookies.isBlank();
     }
 
     protected String get(String key) {

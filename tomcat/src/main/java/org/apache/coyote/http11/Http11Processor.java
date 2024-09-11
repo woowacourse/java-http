f9f67310bf0084bac6981bb767802094a5286d3a
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-09-11 15:09:35 +0900

    refactor: requestMapping으로 변경

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -7,7 +7,7 @@ import java.io.OutputStream;
 import java.net.Socket;
 import org.apache.catalina.Manager;
 import org.apache.coyote.Processor;
-import org.apache.coyote.controller.FrontController;
+import org.apache.coyote.controller.RequestMapping;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.response.HttpResponse;
 import org.slf4j.Logger;
@@ -36,8 +36,8 @@ public class Http11Processor implements Runnable, Processor {
         try (InputStream inputStream = connection.getInputStream(); OutputStream outputStream = connection.getOutputStream()) {
 
             HttpRequest httpRequest = new HttpRequest(inputStream);
-            FrontController frontController = new FrontController();
-            HttpResponse httpResponse = frontController.dispatch(httpRequest, manager);
+            RequestMapping requestMapping = new RequestMapping();
+            HttpResponse httpResponse = requestMapping.dispatch(httpRequest);
 
             outputStream.write(httpResponse.toByte());
             outputStream.flush();

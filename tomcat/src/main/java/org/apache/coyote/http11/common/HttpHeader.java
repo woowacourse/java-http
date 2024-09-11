c7700a6dd34c5cc8b2c043f22e1189a7e42f281b
HyungUk Ryu <hw0603@naver.com>
2024-09-11 10:23:26 +0900

    refactor(HttpHeader): 해더 파싱 로직을 HttpHeader로 이동

--- tomcat/src/main/java/org/apache/coyote/http11/common/HttpHeader.java
+++ tomcat/src/main/java/org/apache/coyote/http11/common/HttpHeader.java
@@ -1,5 +1,7 @@
 package org.apache.coyote.http11.common;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.OptionalInt;
@@ -8,7 +10,16 @@ import java.util.StringJoiner;
 public record HttpHeader(Map<String, String> headers) {
 
     private static final String CONTENT_LENGTH = "Content-Length";
-    private static final String MULTIPLE_HEADER_DELIMITER = ",";
+    private static final String HEADER_DELIMITER = ": ";
+    private static final String MULTIPLE_HEADER_KEY_DELIMITER = ",";
+
+    public static HttpHeader from(String rawHeaders) {
+        HashMap<String, String> headers = Arrays.stream(rawHeaders.split(Constants.CRLF))
+                .map(line -> line.split(HEADER_DELIMITER, 2))
+                .filter(keyValue -> keyValue.length == 2)
+                .collect(HashMap::new, (map, entry) -> map.put(entry[0], entry[1]), HashMap::putAll);
+        return new HttpHeader(headers);
+    }
 
     public Optional<String> get(String key) {
         return Optional.ofNullable(headers.get(key));
@@ -22,13 +33,13 @@ public record HttpHeader(Map<String, String> headers) {
     }
 
     public void add(String key, String value) {
-        headers.computeIfPresent(key, (k, v) -> String.join(MULTIPLE_HEADER_DELIMITER, v, value));
+        headers.computeIfPresent(key, (k, v) -> String.join(MULTIPLE_HEADER_KEY_DELIMITER, v, value));
         headers.putIfAbsent(key, value);
     }
 
     public String toString() {
         StringJoiner joiner = new StringJoiner(Constants.CRLF);
-        headers.forEach((key, value) -> joiner.add(key + ": " + value));
+        headers.forEach((key, value) -> joiner.add(String.join("", key, HEADER_DELIMITER, value)));
         return joiner.toString();
     }
 }
--- tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequestMessageReader.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequestMessageReader.java
@@ -4,17 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import org.apache.coyote.http11.common.Constants;
 import org.apache.coyote.http11.common.HttpHeader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HttpRequestMessageReader {
-
-    private static final String HEADER_DELIMITER = ": ";
 
     private HttpRequestMessageReader() {
     }
@@ -25,7 +20,7 @@ public class HttpRequestMessageReader {
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
         RequestLine requestLine = RequestLine.from(reader.readLine());
-        HttpHeader header = parseHeader(readUntilHeaderEnds(reader));
+        HttpHeader header = HttpHeader.from(readUntilHeaderEnds(reader));
         HttpRequest request = new HttpRequest(requestLine, header, RequestBody.empty());
         header.getContentLength()
                 .ifPresent(byteLength -> request.setBody(readRequestBodyOfLength(reader, byteLength)));
@@ -54,22 +49,5 @@ public class HttpRequestMessageReader {
             throw new RuntimeException("요청 본문을 읽는 중 오류가 발생했습니다.");
         }
         return new String(buffer);
-    }
-
-    private static HttpHeader parseHeader(String rawHeaders) {
-        Map<String, String> headers = new HashMap<>();
-        Arrays.stream(rawHeaders.split(Constants.CRLF))
-                .map(line -> line.split(HEADER_DELIMITER))
-                .forEach(headerKeyValue -> addHeader(headerKeyValue, headers));
-        return new HttpHeader(headers);
-    }
-
-    private static void addHeader(String[] keyValue, Map<String, String> headers) {
-        if (keyValue.length != 2) {
-            String invalidHeaderLine = Arrays.toString(keyValue);
-            log.warn("올바른 헤더 형식이 아닙니다. {}", invalidHeaderLine);
-            return;
-        }
-        headers.put(keyValue[0].trim(), keyValue[1].trim());
     }
 }

fefe42ea26b2319547115ec950fe1a14b0a63d30
ay-eonii <naayen@naver.com>
2024-09-11 13:51:49 +0900

    feat: 캐시 인터셉터에서 `no-cache`, `private` 디렉티브 지정

--- study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
+++ study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
@@ -1,5 +1,6 @@
 package cache.com.example.cachecontrol;
 
+import cache.com.example.interceptor.CacheInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,7 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CacheWebConfig implements WebMvcConfigurer {
 
+    private final CacheInterceptor cacheInterceptor;
+
+    public CacheWebConfig() {
+        this.cacheInterceptor = new CacheInterceptor();
+    }
+
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(cacheInterceptor);
     }
 }

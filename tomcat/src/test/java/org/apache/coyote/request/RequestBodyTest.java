de53dfd8cf3b6bfedab2eb5206a19ad3884aa4e2
takoyakimchi <c4nyou@gmail.com>
2024-09-11 16:08:00 +0900

    test: RequestBodyTest 테스트 케이스 구체화

--- tomcat/src/test/java/org/apache/coyote/request/RequestBodyTest.java
+++ tomcat/src/test/java/org/apache/coyote/request/RequestBodyTest.java
@@ -1,34 +1,53 @@
 package org.apache.coyote.request;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.techcourse.exception.UncheckedServletException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class RequestBodyTest {
 
-    @DisplayName("생성 성공")
+    @DisplayName("생성 성공 - 빈 문자열")
     @Test
-    void construct_Success() {
-        RequestBody requestBody = new RequestBody("name=lee&age=20");
+    void construct_Success_EmptyString() {
+        assertThatCode(() -> new RequestBody(""))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("생성 성공 - 데이터 1개")
+    @Test
+    void construct_Success_SingleEntry() {
+        RequestBody requestBody = new RequestBody("name=lee");
+        assertThat(requestBody.get("name")).isEqualTo("lee");
+    }
+
+    @DisplayName("생성 성공 - 데이터 3개")
+    @Test
+    void construct_Success_TwoEntries() {
+        RequestBody requestBody = new RequestBody("name=lee&age=20&nickname=tre");
         assertAll(
                 () -> assertThat(requestBody.get("name")).isEqualTo("lee"),
-                () -> assertThat(requestBody.get("age")).isEqualTo("20")
+                () -> assertThat(requestBody.get("age")).isEqualTo("20"),
+                () -> assertThat(requestBody.get("nickname")).isEqualTo("tre")
         );
     }
 
     @DisplayName("생성 실패: 올바르지 않은 형식")
-    @Test
-    void construct_Fail() {
-        assertThatThrownBy(() -> new RequestBody("name=lee&age20"))
+    @ParameterizedTest
+    @ValueSource(strings = {"name=lee&age20", "name=lee&", "&name=lee", " "})
+    void construct_Fail(String rawRequestBody) {
+        assertThatThrownBy(() -> new RequestBody(rawRequestBody))
                 .isInstanceOf(UncheckedServletException.class)
                 .hasMessage("올바르지 않은 Request Body 형식입니다.");
     }
 
-    @DisplayName("Request Body가 key 값으로 특정 문자열들을 가지고 있는지 여부 정상 반환")
+    @DisplayName("요청 Body가 특정 문자열만을 포함하는지 반환")
     @Test
     void containsAll() {
         // given
@@ -43,7 +62,7 @@ class RequestBodyTest {
         );
     }
 
-    @DisplayName("Request Body에서 값 조회")
+    @DisplayName("요청 Body에서 값 조회")
     @Test
     void get() {
         // given

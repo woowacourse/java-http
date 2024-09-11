aa8696ace57567e6cbb41235b4efeee644aac621
pricelees <priceelees@gmail.com>
2024-09-11 12:52:55 +0900

    전체 코드 롤백

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -2,49 +2,47 @@ package org.apache.coyote.http11;
 
 import java.io.IOException;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.techcourse.exception.UncheckedServletException;
-import com.techcourse.web.HttpResponse;
-import com.techcourse.web.handler.RequestHandler;
-import com.techcourse.web.handler.RequestHandlerMapper;
 
 public class Http11Processor implements Runnable, Processor {
 
-	private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
 
-	private final Socket connection;
-	private final RequestHandlerMapper handlerMapper;
+    private final Socket connection;
 
-	public Http11Processor(final Socket connection) {
-		this.connection = connection;
-		this.handlerMapper = RequestHandlerMapper.getInstance();
-	}
+    public Http11Processor(final Socket connection) {
+        this.connection = connection;
+    }
 
-	@Override
-	public void run() {
-		log.info("connect host: {}, port: {}", connection.getInetAddress(), connection.getPort());
-		process(connection);
-	}
+    @Override
+    public void run() {
+        log.info("connect host: {}, port: {}", connection.getInetAddress(), connection.getPort());
+        process(connection);
+    }
 
-	@Override
-	public void process(final Socket connection) {
-		try (final var inputStream = connection.getInputStream();
-			 final var outputStream = connection.getOutputStream()) {
+    @Override
+    public void process(final Socket connection) {
+        try (final var inputStream = connection.getInputStream();
+             final var outputStream = connection.getOutputStream()) {
 
-			HttpRequest request = HttpRequestReader.read(inputStream);
-			RequestHandler handler = handlerMapper.findHandler(request.getRequestPath());
-			HttpResponse response = handler.handle(request);
-			String httpResponseMessage = response.createResponseMessage();
+            final var responseBody = "Hello world!";
 
-			outputStream.write(httpResponseMessage.getBytes(StandardCharsets.UTF_8));
-			outputStream.flush();
-		} catch (IOException | UncheckedServletException e) {
-			log.error(e.getMessage(), e);
-		}
-	}
+            final var response = String.join("\r\n",
+                    "HTTP/1.1 200 OK ",
+                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "",
+                    responseBody);
+
+            outputStream.write(response.getBytes());
+            outputStream.flush();
+        } catch (IOException | UncheckedServletException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
 }

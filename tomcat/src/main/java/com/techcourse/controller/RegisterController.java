da09da26d94f3b75f15ada4f4347fd3b2de38a13
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-09-11 21:31:53 +0900

    refactor: parse 로직 was로 변경

--- tomcat/src/main/java/com/techcourse/controller/RegisterController.java
+++ tomcat/src/main/java/com/techcourse/controller/RegisterController.java
@@ -2,16 +2,12 @@ package com.techcourse.controller;
 
 import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.model.User;
-import java.util.HashMap;
 import java.util.Map;
-import org.apache.catalina.Manager;
 import org.apache.coyote.ForwardResult;
 import org.apache.coyote.HttpStatusCode;
-import org.apache.coyote.MimeType;
 import org.apache.coyote.controller.AbstractController;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.response.HttpResponse;
-import org.apache.coyote.http11.response.ResponseHeader;
 
 public class RegisterController extends AbstractController {
 
@@ -21,29 +17,14 @@ public class RegisterController extends AbstractController {
 
     @Override
     public ForwardResult execute(HttpRequest request, HttpResponse response) {
-        String body = request.getBody();
-        Map<String, String> parsedBody = parseBody(body);
+        Map<String, String> body = request.getBody();
 
-        String account = parsedBody.get(ACCOUNT_KEY);
-        String email = parsedBody.get(EMAIL_KEY);
-        String password = parsedBody.get(PASSWORD_KEY);
+        String account = body.get(ACCOUNT_KEY);
+        String email = body.get(EMAIL_KEY);
+        String password = body.get(PASSWORD_KEY);
 
         InMemoryUserRepository.save(new User(account, password, email));
 
         return new ForwardResult(HttpStatusCode.FOUND, "index.html");
-    }
-
-    private Map<String, String> parseBody(String query) {
-        Map<String, String> result = new HashMap<>();
-        String[] pairs = query.split("&");
-
-        for (String pair : pairs) {
-            String[] keyValue = pair.split("=", 2);
-            String key = keyValue[0];
-            String value = keyValue[1];
-            result.put(key, value);
-        }
-
-        return result;
     }
 }
--- tomcat/src/main/java/org/apache/coyote/http11/request/RequestBody.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/RequestBody.java
@@ -1,27 +1,42 @@
 package org.apache.coyote.http11.request;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class RequestBody {
 
     private static final String EMPTY_BODY = "";
+    private static final String PARAMETER_DELIMITER = "&";
+    private static final String KEY_VALUE_DELIMITER = "=";
+    private static final int LIMIT_SPLIT = 2;
+    private static final int KEY_INDEX = 0;
+    private static final int VALUE_INDEX = 1;
 
-    private final String body;
+    private final Map<String, String> body;
 
     public RequestBody(String body) {
-        this.body = body;
+        this.body = parseBody(body);
     }
 
     public static RequestBody empty() {
         return new RequestBody(EMPTY_BODY);
     }
 
+    private Map<String, String> parseBody(String query) {
+        List<String> pairs = List.of(query.split(PARAMETER_DELIMITER));
+        return pairs.stream()
+                .map(pair -> pair.split(KEY_VALUE_DELIMITER, LIMIT_SPLIT))
+                .collect(Collectors.toMap(splitStrings -> splitStrings[KEY_INDEX],
+                        splitStrings -> splitStrings[VALUE_INDEX])
+                );
+    }
+
     public boolean isEmpty() {
         return body.isEmpty();
     }
 
-    public String getBodyValue() {
-        if (body.isEmpty()) {
-            throw new IllegalArgumentException("Body가 존재하지 않습니다.");
-        }
+    public Map<String, String> getBody() {
         return body;
     }
 }

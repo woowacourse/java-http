3af78218f7f2b2cb8c6f5e2377f75f15996872fd
takoyakimchi <c4nyou@gmail.com>
2024-09-11 19:07:39 +0900

    fix: 파일명이 아닌 endpoint 요청에 대해서 파일 리소스를 반환

--- tomcat/src/main/java/org/apache/catalina/controller/DefaultController.java
+++ tomcat/src/main/java/org/apache/catalina/controller/DefaultController.java
@@ -1,5 +1,6 @@
 package org.apache.catalina.controller;
 
+import com.techcourse.exception.UncheckedServletException;
 import org.apache.coyote.request.HttpRequest;
 import org.apache.coyote.response.HttpResponse;
 
@@ -8,6 +9,8 @@ public class DefaultController extends AbstractController {
     private static final String PATH_PREFIX = "/";
     private static final String EMPTY_PATH = "/";
     private static final String INDEX_PATH = "/index.html";
+    private static final String PERIOD = ".";
+    private static final String HTML_SUFFIX = ".html";
 
     @Override
     protected void doGet(HttpRequest request, HttpResponse response) {
@@ -18,8 +21,20 @@ public class DefaultController extends AbstractController {
             return;
         }
 
-        if (path.startsWith(PATH_PREFIX)) {
-            response.setStaticResource(path.substring(path.indexOf(PATH_PREFIX) + PATH_PREFIX.length()));
+        String fileName = parseFileName(path);
+        response.setStaticResource(fileName);
+    }
+
+    private String parseFileName(String path) {
+        if (!path.startsWith(PATH_PREFIX)) {
+            throw new UncheckedServletException("요청 URI는 %s로 시작해야 합니다.".formatted(PATH_PREFIX));
         }
+
+        String fileName = path.substring(path.indexOf(PATH_PREFIX) + PATH_PREFIX.length());
+        if (!fileName.contains(PERIOD)) {
+            fileName += HTML_SUFFIX;
+        }
+
+        return fileName;
     }
 }

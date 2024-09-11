ce699baf639e2285bc44d64f8f412fd99cdb98c7
linirini <2001yerin@naver.com>
2024-09-11 12:54:57 +0900

    refactor: 상수 접근 제어자 변경

--- tomcat/src/main/java/org/apache/coyote/http11/HttpHeaders.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpHeaders.java
@@ -5,11 +5,11 @@ import java.util.Map;
 import java.util.Objects;
 
 public class HttpHeaders {
-    public static final String COOKIE = "Cookie";
-    public static final String SET_COOKIE = "Set-Cookie";
-    public static final String LOCATION = "Location";
-    public static final String CONTENT_LENGTH = "Content-Length";
-    public static final String CONTENT_TYPE = "Content-Type";
+    private static final String COOKIE = "Cookie";
+    private static final String SET_COOKIE = "Set-Cookie";
+    private static final String LOCATION = "Location";
+    private static final String CONTENT_LENGTH = "Content-Length";
+    private static final String CONTENT_TYPE = "Content-Type";
 
     private final Map<String, String> headers;
 

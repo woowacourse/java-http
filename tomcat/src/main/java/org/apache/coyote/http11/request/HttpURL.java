8247cf9e0fd1a56a3df6039e31d7fdf119e7946f
HyungUk Ryu <hw0603@naver.com>
2024-09-11 10:03:38 +0900

    refactor(HttpURL): 매직넘버 상수화

--- tomcat/src/main/java/org/apache/coyote/http11/request/HttpURL.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/HttpURL.java
@@ -5,7 +5,9 @@ import java.util.Map;
 
 public record HttpURL(String fullUrl, String path, Map<String, String> queryParameters) {
 
-    private static final String QUERY_DELIMITER = "\\?";
+    private static final String PATH_QUERY_DELIMITER = "\\?";
+    private static final int PATH_INDEX = 0;
+    private static final int QUERY_INDEX = 1;
     private static final String QUERY_PAIR_DELIMITER = "&";
     private static final String QUERY_KEY_VALUE_DELIMITER = "=";
 
@@ -14,16 +16,17 @@ public record HttpURL(String fullUrl, String path, Map<String, String> queryPara
     }
 
     private static String parsePath(String url) {
-        return url.split(QUERY_DELIMITER)[0];
+        return url.split(PATH_QUERY_DELIMITER)[PATH_INDEX];
     }
+
     private static Map<String, String> parseQueryParameters(String url) {
         Map<String, String> queryParams = new HashMap<>();
-        String[] split = url.split(QUERY_DELIMITER);
-        if (split.length != 2) {
+        String[] pathAndQuery = url.split(PATH_QUERY_DELIMITER);
+        if (pathAndQuery.length != 2) {
             return queryParams;
         }
 
-        String queryPart = split[1];
+        String queryPart = pathAndQuery[QUERY_INDEX];
         String[] pairs = queryPart.split(QUERY_PAIR_DELIMITER);
         for (String pair : pairs) {
             String[] keyValue = pair.split(QUERY_KEY_VALUE_DELIMITER);

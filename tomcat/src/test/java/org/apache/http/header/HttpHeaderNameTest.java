e629c638cd54aac9cf1e3e4155881c18b1dc87f7
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-09-12 08:49:41 +0900

    refactor(HttpHeader): key 값을 HttpHeaderName enum으로 변경

--- tomcat/src/test/java/org/apache/http/header/HttpHeaderNameTest.java
+++ tomcat/src/test/java/org/apache/http/header/HttpHeaderNameTest.java
@@ -1,23 +1,39 @@
 package org.apache.http.header;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class HttpHeaderNameTest {
 
-    @Test
-    @DisplayName("문자열로부터 상수 파싱")
-    void from() {
-        assertAll(
-                () -> assertEquals(HttpHeaderName.COOKIE, HttpHeaderName.from("cookie")),
-                () -> assertEquals(HttpHeaderName.CONTENT_TYPE, HttpHeaderName.from("CONTENT-TYPE"))
-        );
+    @Nested
+    @DisplayName("문자열로부터 상수 추출")
+    class from {
+
+        @Test
+        @DisplayName("문자열로부터 상수 추출 성공")
+        void from() {
+            assertAll(
+                    () -> assertEquals(HttpHeaderName.COOKIE, HttpHeaderName.from("cookie")),
+                    () -> assertEquals(HttpHeaderName.CONTENT_TYPE, HttpHeaderName.from("CONTENT-TYPE"))
+            );
+        }
+
+        @Test
+        @DisplayName("문자열로부터 상수 추출 실패: 존재하지 않는 상수")
+        void from_WhenNotExistsName() {
+            assertThatThrownBy(() -> HttpHeaderName.from("INVALID"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("해당하는 HttpHeaderName이 없습니다.");
+        }
     }
 
     @Test

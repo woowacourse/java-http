0e024eb56af0d933eb67c6fe3f28c4de71d28358
ehtjsv2 <ehtjsv2@naver.com>
2024-09-11 14:39:32 +0900

    refactor: happy case 테스트 추가

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -40,4 +40,100 @@ class Http11ProcessorTest {
 
         assertThat(socket.output()).isEqualTo(expected);
     }
+
+    @DisplayName("/login Get요청시 login페이지를 반환한다.")
+    @Test
+    void response_login_html_When_request_get_login() throws IOException {
+        // given
+        String httpRequest = String.join("\r\n",
+                "GET /login HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+        StubSocket socket = new StubSocket(httpRequest);
+        Http11Processor processor = new Http11Processor(socket);
+        URL resource = getClass().getClassLoader().getResource("static/login.html");
+        String expectedBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+
+        // when
+        processor.process(socket);
+
+        // then
+        assertThat(socket.output()).contains(expectedBody);
+    }
+
+    @DisplayName("/login Post요청시, 성공 -> default페이지 리다이렉션")
+    @Test
+    void response_redirect_default_uri_When_request_post_login_success() throws IOException {
+        // given
+        String httpRequest = String.join("\r\n",
+                "POST /login HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "Content-Type: application/x-www-form-urlencoded",
+                "Content-Length: "+"account=gugu&password=password".length(),
+                "",
+                "account=gugu&password=password");
+        StubSocket socket = new StubSocket(httpRequest);
+        Http11Processor processor = new Http11Processor(socket);
+
+        String defaultLocation = "http://localhost:8080";
+
+        // when
+        processor.process(socket);
+
+        // then
+        assertThat(socket.output()).contains(defaultLocation);
+    }
+
+    @DisplayName("/login Post요청시, 실패 -> 401페이지 반환")
+    @Test
+    void response_401_html_When_request_post_login_fail() throws IOException {
+        // given
+        String httpRequest = String.join("\r\n",
+                "POST /login HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "Content-Type: application/x-www-form-urlencoded",
+                "Content-Length: "+"account=gugu&password=password".length(),
+                "",
+                "account=gugu&password=failPassword");
+        StubSocket socket = new StubSocket(httpRequest);
+        Http11Processor processor = new Http11Processor(socket);
+
+        URL resource = getClass().getClassLoader().getResource("static/401.html");
+        String expectedBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+
+        // when
+        processor.process(socket);
+
+        // then
+        assertThat(socket.output()).contains(expectedBody);
+    }
+
+
+    @DisplayName("/register Post요청시, 성공-> index페이지 반환")
+    @Test
+    void response_redirect_default_uri_When_request_post_register() throws IOException {
+        // given
+        String httpRequest = String.join("\r\n",
+                "POST /register HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "Content-Type: application/x-www-form-urlencoded",
+                "Content-Length: "+"account=gugu&password=password".length(),
+                "",
+                "account=dodo&email=dodo@a.com&password=password");
+        StubSocket socket = new StubSocket(httpRequest);
+        Http11Processor processor = new Http11Processor(socket);
+
+        String defaultLocation = "http://localhost:8080";
+
+        // when
+        processor.process(socket);
+
+        // then
+        assertThat(socket.output()).contains(defaultLocation);
+    }
 }

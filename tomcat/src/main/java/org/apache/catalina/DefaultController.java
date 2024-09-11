9b42e94b6844c3f1640e40e572833c06fe170eb0
takoyakimchi <c4nyou@gmail.com>
2024-09-11 18:41:52 +0900

    fix: Path가 / 인 요청은 /index.html 로 리다이렉트

--- tomcat/src/main/java/org/apache/catalina/DefaultController.java
+++ tomcat/src/main/java/org/apache/catalina/DefaultController.java
@@ -7,10 +7,17 @@ import org.apache.coyote.response.HttpResponse;
 public class DefaultController extends AbstractController {
 
     private static final String PATH_PREFIX = "/";
+    private static final String EMPTY_PATH = "/";
+    private static final String INDEX_PATH = "/index.html";
 
     @Override
     protected void doGet(HttpRequest request, HttpResponse response) {
         String path = request.getPath();
+
+        if (path.equals(EMPTY_PATH)) {
+            response.setRedirect(INDEX_PATH);
+            return;
+        }
 
         if (path.startsWith(PATH_PREFIX)) {
             response.setStaticResource(path.substring(path.indexOf(PATH_PREFIX) + PATH_PREFIX.length()));

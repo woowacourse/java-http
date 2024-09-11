e2c117bb68da90091548e8d6a1b960a222d2dc8d
Nam Gi Beom <rlqja0523@naver.com>
2024-09-11 17:26:25 +0900

    feat: QueryParams key, value 검증 추가

--- tomcat/src/main/java/servlet/http/request/uri/QueryParams.java
+++ tomcat/src/main/java/servlet/http/request/uri/QueryParams.java
@@ -12,7 +12,8 @@ public class QueryParams {
     private static final QueryParams EMPTY = new QueryParams(Collections.emptyMap());
     private static final String PARAMS_DELIMITER = "&";
     private static final String KEY_VALUE_DELIMITER = "=";
-    private static final int LIMIT = 2;
+    private static final int UNLIMITED_SPLIT = -1;
+    private static final int VALID_KEY_VALUE_LENGTH = 2;
     private static final int KEY_INDEX = 0;
     private static final int VALUE_INDEX = 1;
 
@@ -23,12 +24,33 @@ public class QueryParams {
     }
 
     protected static QueryParams from(String queryParams) {
-        if (queryParams == null || queryParams.isBlank()) {
+        if (isEmptyQueryParams(queryParams)) {
             return EMPTY;
         }
         return Arrays.stream(queryParams.split(PARAMS_DELIMITER))
-                .map(params -> params.split(KEY_VALUE_DELIMITER, LIMIT))
-                .collect(collectingAndThen(toMap(p -> p[KEY_INDEX], p -> p[VALUE_INDEX]), QueryParams::new));
+                .map(QueryParams::splitKeyValue)
+                .collect(collectingAndThen(
+                        toMap(p -> p[KEY_INDEX].strip(), p -> p[VALUE_INDEX].strip()), QueryParams::new)
+                );
+    }
+
+    private static boolean isEmptyQueryParams(String queryParams) {
+        return queryParams == null || queryParams.isBlank();
+    }
+
+    private static String[] splitKeyValue(String queryParam) {
+        String[] keyValue = queryParam.split(KEY_VALUE_DELIMITER, UNLIMITED_SPLIT);
+        if (keyValue.length != VALID_KEY_VALUE_LENGTH) {
+            throw new IllegalArgumentException("잘못된 Query parameter입니다. query parameter: '%s'".formatted(queryParam));
+        }
+        validateNotBlankKeyValue(keyValue[KEY_INDEX], keyValue[VALUE_INDEX]);
+        return keyValue;
+    }
+
+    private static void validateNotBlankKeyValue(String key, String value) {
+        if (key.isBlank() || value.isBlank()) {
+            throw new IllegalArgumentException("key 또는 value가 비어있습니다. key: '%s', value: '%s'".formatted(key, value));
+        }
     }
 
     protected String get(String key) {
--- tomcat/src/test/java/servlet/http/request/uri/QueryParamsTest.java
+++ tomcat/src/test/java/servlet/http/request/uri/QueryParamsTest.java
@@ -24,6 +24,72 @@ class QueryParamsTest {
     }
 
     @Test
+    void key_value_구분자가_없는_경우_예외가_발생한다() {
+        // given
+        String queryParams = "account=prin&password&team=ddangkong";
+
+        // when & then
+        assertThatThrownBy(() -> QueryParams.from(queryParams))
+            .isExactlyInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("잘못된 Query parameter입니다. query parameter: 'password'");
+    }
+
+    @Test
+    void key_value_구분자가_두_개_이상일_경우_예외가_발생한다() {
+        // given
+        String queryParams = "account=prin&password=1q2w3e4r!=1";
+
+        // when & then
+        assertThatThrownBy(() -> QueryParams.from(queryParams))
+            .isExactlyInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("잘못된 Query parameter입니다. query parameter: 'password=1q2w3e4r!=1'");
+    }
+
+    @Test
+    void query_param이_비어있을_경우_예외가_발생한다() {
+        // given
+        String queryParams = "account=prin&&password=1q2w3e4r!";
+
+        // when & then
+        assertThatThrownBy(() -> QueryParams.from(queryParams))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("잘못된 Query parameter입니다. query parameter: ''");
+    }
+
+    @Test
+    void key가_비어있을_경우_예외가_발생한다() {
+        // given
+        String queryParams = "account=prin&=1q2w3e4r!";
+
+        // when & then
+        assertThatThrownBy(() -> QueryParams.from(queryParams))
+            .isExactlyInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("key 또는 value가 비어있습니다. key: '', value: '1q2w3e4r!'");
+    }
+
+    @Test
+    void value가_비어있을_경우_예외가_발생한다() {
+        // given
+        String queryParams = "account=prin&password=";
+
+        // when & then
+        assertThatThrownBy(() -> QueryParams.from(queryParams))
+            .isExactlyInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("key 또는 value가 비어있습니다. key: 'password', value: ''");
+    }
+
+    @Test
+    void key_value가_비어있을_경우_예외가_발생한다() {
+        // given
+        String queryParams = "account=prin&=";
+
+        // when & then
+        assertThatThrownBy(() -> QueryParams.from(queryParams))
+            .isExactlyInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("key 또는 value가 비어있습니다. key: '', value: ''");
+    }
+
+    @Test
     void QueryParams가_비어있을_경우_key를_조회할_때_예외가_발생한다() {
         // given
         QueryParams queryParams = QueryParams.from(null);

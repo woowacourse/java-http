b25d2d69f14510c9b420fa078387e8c736e294cf
Eugene Jang <kyunellroll@gmail.com>
2024-09-12 22:47:05 +0900

    modify(UserService): logging 위치를 변경하도록 수정

--- tomcat/src/main/java/com/techcourse/service/UserService.java
+++ tomcat/src/main/java/com/techcourse/service/UserService.java
@@ -2,14 +2,19 @@ package com.techcourse.service;
 
 import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UserService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     public User findUser(String account, String password) {
         User user = InMemoryUserRepository.findByAccount(account)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
 
         if (user.checkPassword(password)) {
+            logger.info(user.toString());
             return user;
         }
         throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");

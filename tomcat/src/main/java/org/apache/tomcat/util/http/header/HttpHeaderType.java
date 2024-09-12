a913676d8254d981bd523ac7fa65bbeebb5bbc85
yoonjuho <dbswn990@naver.com>
2024-09-12 17:22:53 +0900

    feat: Http Header 클래스 분리

---
+++ tomcat/src/main/java/org/apache/tomcat/util/http/header/HttpHeaderType.java
@@ -0,0 +1,9 @@
+package org.apache.tomcat.util.http.header;
+
+public record HttpHeaderType(String header) {
+    public static final HttpHeaderType CONTENT_TYPE = new HttpHeaderType("Content-Type");
+    public static final HttpHeaderType CONTENT_LENGTH = new HttpHeaderType("Content-Length");
+    public static final HttpHeaderType COOKIE = new HttpHeaderType("Cookie");
+    public static final HttpHeaderType SET_COOKIE = new HttpHeaderType("Set-Cookie");
+    public static final HttpHeaderType LOCATION = new HttpHeaderType("Location");
+}
---
+++ tomcat/src/test/java/org/apache/tomcat/util/http/parser/HttpRequestHeaderParserTest.java
@@ -0,0 +1,33 @@
+package org.apache.tomcat.util.http.parser;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Map;
+
+import org.apache.tomcat.util.http.header.HttpHeaderType;
+import org.apache.tomcat.util.http.header.HttpHeaders;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class HttpRequestHeaderParserTest {
+
+    @DisplayName("HttpRequestHeader를 파싱할 수 있다.")
+    @Test
+    void parse() throws IOException {
+        String requestData = "Host: localhost\r\n" +
+                "Content-Type: text/html\r\n" +
+                "Content-Length: 0\r\n\r\n";
+
+        BufferedReader bufferedReader = new BufferedReader(new StringReader(requestData));
+
+        HttpHeaders actual = HttpRequestHeaderParser.parse(bufferedReader);
+        HttpHeaders expected = new HttpHeaders(
+                Map.of(new HttpHeaderType("Host"), "localhost",
+                        new HttpHeaderType("Content-Type"), "text/html",
+                        new HttpHeaderType("Content-Length"), "0"));
+        assertThat(actual).isEqualTo(expected);
+    }
+}

11c83f75a6b93c3a2933054d217d28a2011bb55e
KIMGYUTAE <kiimwf0819@gmail.com>
2024-09-12 17:03:05 +0900

    refactor: HttpContentType에 utf 설정 추가

--- tomcat/src/main/java/org/apache/coyote/http11/HttpContentType.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpContentType.java
@@ -2,7 +2,7 @@ package org.apache.coyote.http11;
 
 public enum HttpContentType {
 
-    TEXT_HTML("text/html"),
+    TEXT_HTML("text/html;charset=utf-8"),
     APPLICATION_JSON("application/json"),
     ;
 

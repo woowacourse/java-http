db6235473cb1bfc2bbefe3c37d02cff19431c89b
3juhwan <13selfesteem91@naver.com>
2024-09-13 01:10:46 +0900

    refactor: 리다이렉트 메서드 적용

--- tomcat/src/main/java/org/apache/catalina/connector/CatalinaAdapter.java
+++ tomcat/src/main/java/org/apache/catalina/connector/CatalinaAdapter.java
@@ -3,13 +3,14 @@ package org.apache.catalina.connector;
 import org.apache.catalina.controller.Controller;
 import org.apache.catalina.controller.RequestMapping;
 import org.apache.coyote.Adapter;
-import org.apache.coyote.http.Header;
 import org.apache.coyote.http.request.HttpRequest;
 import org.apache.coyote.http.response.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CatalinaAdapter implements Adapter {
+
+    private static final String SERVER_ERROR_PAGE = "/500.html";
 
     private static final Logger log = LoggerFactory.getLogger(CatalinaAdapter.class);
 
@@ -21,7 +22,7 @@ public class CatalinaAdapter implements Adapter {
             controller.service(request, response);
         } catch (Exception e) {
             log.error(e.getMessage(), e);
-            response.setHeader(Header.LOCATION.value(), "/500.html");
+            response.sendRedirect(SERVER_ERROR_PAGE);
         }
     }
 }

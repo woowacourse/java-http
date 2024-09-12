246de12679e74696deb8bbefd37c745235b14502
hyeonjilee <skylar12200@gmail.com>
2024-09-12 22:43:13 +0900

    refactor: 컨트롤러 매핑 책임 분리

---
+++ tomcat/src/main/java/org/apache/coyote/http11/Dispatcher.java
@@ -0,0 +1,39 @@
+package org.apache.coyote.http11;
+
+import java.util.Map;
+import org.apache.coyote.controller.Controller;
+import org.apache.coyote.controller.HomeController;
+import org.apache.coyote.controller.LoginController;
+import org.apache.coyote.controller.RegisterController;
+import org.apache.coyote.controller.StaticController;
+import org.apache.coyote.http.HeaderName;
+import org.apache.coyote.request.HttpRequest;
+import org.apache.coyote.response.HttpResponse;
+
+public class Dispatcher {
+
+    private final Map<String, Controller> controllers;
+    private final Controller staticController;
+
+    public Dispatcher() {
+        this.controllers = Map.of(
+                "/login", new LoginController(),
+                "/register", new RegisterController(),
+                "/", new HomeController()
+        );
+        this.staticController = new StaticController();
+    }
+
+    public void run(HttpRequest httpRequest, HttpResponse httpResponse) throws Exception {
+        httpResponse.addHeader(HeaderName.CONTENT_TYPE, httpRequest.getContentType());
+        httpResponse.addHeader(HeaderName.SET_COOKIE, httpRequest.getHttpCookie());
+
+        if (httpRequest.isStaticRequest()) {
+            staticController.service(httpRequest, httpResponse);
+        }
+        if (!httpRequest.isStaticRequest()) {
+            Controller controller = controllers.get(httpRequest.getPath());
+            controller.service(httpRequest, httpResponse);
+        }
+    }
+}

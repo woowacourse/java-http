e196042020214c36519e46b65f35da709c69f4c3
KIMGYUTAE <kiimwf0819@gmail.com>
2024-09-12 15:18:42 +0900

    refactor: RegisterController 메서드 구조 변경

--- tomcat/src/main/java/com/techcourse/controller/RegisterController.java
+++ tomcat/src/main/java/com/techcourse/controller/RegisterController.java
@@ -1,5 +1,8 @@
 package com.techcourse.controller;
 
+import static com.techcourse.controller.PagePath.INDEX_PAGE;
+import static com.techcourse.controller.PagePath.REGISTER_PAGE;
+
 import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.MissingRequestBodyException;
 import com.techcourse.model.User;
@@ -16,36 +19,37 @@ public class RegisterController extends AbstractController {
     protected ResponseResult doGet(HttpRequest request) {
         return ResponseResult
                 .status(HttpStatusCode.OK)
-                .path("/register.html");
+                .path(REGISTER_PAGE.getPath());
     }
 
     @Override
     protected ResponseResult doPost(HttpRequest request) {
-        try {
-            User user = extractUserByFormData(request);
-            InMemoryUserRepository.save(user);
-            return ResponseResult
-                    .status(HttpStatusCode.FOUND)
-                    .header(HttpHeaders.LOCATION.getName(), "/index.html")
-                    .build();
-
-        } catch (MissingRequestBodyException e) {
-            return ResponseResult
-                    .status(HttpStatusCode.BAD_REQUEST)
-                    .body(e.getMessage());
+        Map<String, String> requestFormData = request.getFormData();
+        if (requestFormData.isEmpty()) {
+            return responseMissingBody();
         }
+
+        registerUserByFormData(requestFormData);
+        return responseRedirectIndex();
     }
 
-    private User extractUserByFormData(HttpRequest request) {
-        if (!request.hasBodyData()) {
-            throw new MissingRequestBodyException();
-        }
-
-        Map<String, String> requestFormData = request.getFormData();
+    private void registerUserByFormData(Map<String, String> requestFormData) {
         String account = requestFormData.get("account");
         String password = requestFormData.get("password");
         String email = requestFormData.get("email");
+        InMemoryUserRepository.save(new User(account, password, email));
+    }
 
-        return new User(account, password, email);
+    private ResponseResult responseMissingBody() {
+        return ResponseResult
+                .status(HttpStatusCode.BAD_REQUEST)
+                .body(new MissingRequestBodyException().getMessage());
+    }
+
+    private static ResponseResult responseRedirectIndex() {
+        return ResponseResult
+                .status(HttpStatusCode.FOUND)
+                .header(HttpHeaders.LOCATION.getName(), INDEX_PAGE.getPath())
+                .build();
     }
 }

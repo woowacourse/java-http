6f9955f336def15c615fa397cee5bf854d5f29c8
BurningFalls <sherlocky31415@gmail.com>
2024-09-12 10:01:21 +0900

    feature: ResponseBody 객체화

---
+++ tomcat/src/main/java/org/apache/coyote/http11/response/component/ResponseBody.java
@@ -0,0 +1,17 @@
+package org.apache.coyote.http11.response.component;
+
+public class ResponseBody {
+    private String body;
+
+    public ResponseBody() {
+        this.body = "";
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}

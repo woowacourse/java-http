8bf0c0955db940d76f0386fd5960f8aa1b7f3f5f
devhoya97 <diajbh123@gmail.com>
2024-09-12 11:19:24 +0900

    refactor: requestLine 예외처리 메서드 분리

--- tomcat/src/main/java/org/apache/coyote/http11/request/RequestLine.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/RequestLine.java
@@ -3,19 +3,27 @@ package org.apache.coyote.http11.request;
 public class RequestLine {
 
     private static final int REQUEST_LINE_ELEMENTS_COUNT = 3;
+    private static final String SPACE = " ";
 
     private final String method;
     private final String path;
     private final String version;
 
     public RequestLine(String requestLine) {
-        String[] split = requestLine.split(" ");
-        if (split.length != REQUEST_LINE_ELEMENTS_COUNT) {
-            throw new IllegalArgumentException("requestLine의 형식이 올바르지 않습니다. requestLine = " + requestLine);
+        String[] requestLineElements = requestLine.split(SPACE);
+        validateRequestLineElementsCount(requestLineElements);
+        this.method = requestLineElements[0];
+        this.path = requestLineElements[1];
+        this.version = requestLineElements[2];
+    }
+
+    private void validateRequestLineElementsCount(String[] requestLineElements) {
+        if (requestLineElements.length != REQUEST_LINE_ELEMENTS_COUNT) {
+            throw new IllegalArgumentException(
+                    "request line은 공백 기준으로 쪼갰을 때, 3개의 요소로 이루어져야 합니다. " +
+                            "현재 요소 갯수 = " + requestLineElements.length
+            );
         }
-        this.method = split[0];
-        this.path = split[1];
-        this.version = split[2];
     }
 
     public String getMethod() {

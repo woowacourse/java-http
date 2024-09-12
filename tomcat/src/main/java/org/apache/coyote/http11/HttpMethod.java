e020241533bce27f50dbd5ba7545fc53658db47f
hangillee <skfcb10@naver.com>
2024-09-12 16:31:24 +0900

    refactor: 사용하지 않는 메소드 제거

--- tomcat/src/main/java/org/apache/coyote/http11/HttpMethod.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpMethod.java
@@ -1,9 +1,5 @@
 package org.apache.coyote.http11;
 
 public enum HttpMethod {
-    GET, POST;
-
-    public HttpMethod from(final String method) {
-        return HttpMethod.valueOf(method);
-    }
+    GET, POST
 }

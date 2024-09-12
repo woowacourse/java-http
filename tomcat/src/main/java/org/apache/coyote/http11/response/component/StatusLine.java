f08288f7fe0dfe81bf26ca8892e102815faa9068
BurningFalls <sherlocky31415@gmail.com>
2024-09-12 10:01:13 +0900

    feature: StatusLine 객체화

---
+++ tomcat/src/main/java/org/apache/coyote/http11/response/component/StatusLine.java
@@ -0,0 +1,35 @@
+package org.apache.coyote.http11.response.component;
+
+public class StatusLine {
+    private static final String VERSION = "HTTP/1.1";
+
+    private final String version;
+    private String statusCode;
+    private String statusMessage;
+
+    public StatusLine() {
+        this.version = VERSION;
+        this.statusCode = "";
+        this.statusMessage = "";
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public void setStatusMessage(String statusMessage) {
+        this.statusMessage = statusMessage;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public String getStatusMessage() {
+        return statusMessage;
+    }
+}

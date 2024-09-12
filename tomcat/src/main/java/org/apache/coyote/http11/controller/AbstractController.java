0afd161df6e7a25b715b53e3c9d874d0c196927f
BurningFalls <sherlocky31415@gmail.com>
2024-09-12 11:32:50 +0900

    refactor: getResponseBody를 RequestFactory 책임으로 넘김

--- tomcat/src/main/java/org/apache/coyote/http11/controller/AbstractController.java
+++ tomcat/src/main/java/org/apache/coyote/http11/controller/AbstractController.java
@@ -1,18 +1,12 @@
 package org.apache.coyote.http11.controller;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.util.Objects;
-
+import org.apache.coyote.http11.file.RequestFactory;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.response.HttpResponse;
 
 public class AbstractController implements Controller {
     private static final String POST_METHOD = "POST";
     private static final String GET_METHOD = "GET";
-    private static final String STATIC_PATH = "static";
     private static final String CHARSET_UTF8 = ";charset=UTF-8";
 
     @Override
@@ -37,24 +31,9 @@ public class AbstractController implements Controller {
     protected void doGet(HttpRequest request, HttpResponse response) throws Exception {
         response.setStatusLine("200", "OK");
 
-        String responseBody = getResponseBody(request);
+        String responseBody = RequestFactory.getResponseBody(request);
         response.setBody(responseBody);
         response.setFieldValue("Content-Length", String.valueOf(responseBody.length()));
         response.setFieldValue("Content-Type", request.getMimeType() + CHARSET_UTF8);
-    }
-
-    private String getResponseBody(HttpRequest request) {
-        try {
-            String requestUri = request.getRequestUri();
-            URL resourceUrl = getClass().getClassLoader().getResource(STATIC_PATH + requestUri);
-            if (Objects.isNull(resourceUrl)) {
-                return "";
-            }
-
-            File file = new File(resourceUrl.getFile());
-            return new String(Files.readAllBytes(file.toPath()));
-        } catch (IOException e) {
-            throw new RuntimeException("파일을 읽을 수 없습니다.");
-        }
     }
 }
--- tomcat/src/main/java/org/apache/coyote/http11/file/RequestFactory.java
+++ tomcat/src/main/java/org/apache/coyote/http11/file/RequestFactory.java
@@ -1,12 +1,18 @@
 package org.apache.coyote.http11.file;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
 import java.util.Objects;
+
+import org.apache.coyote.http11.request.HttpRequest;
 
 public class RequestFactory {
     private static final String ERROR_MESSAGE = "파일 읽기를 실패했습니다.";
     private static final String HTTP_LINE_SEPARATOR = "\r\n";
+    private static final String STATIC_PATH = "static";
 
     public static String readRequestLine(BufferedReader reader) throws IOException {
         return reader.readLine();
@@ -28,5 +34,17 @@ public class RequestFactory {
             throw new RuntimeException(ERROR_MESSAGE);
         }
         return new String(buffer);
+    }
+
+    public static String getResponseBody(HttpRequest request) throws IOException {
+        String requestUri = request.getRequestUri();
+        ClassLoader classLoader = RequestFactory.class.getClassLoader();
+        URL resourceUrl = classLoader.getResource(STATIC_PATH + requestUri);
+        if (Objects.isNull(resourceUrl)) {
+            return "";
+        }
+
+        File file = new File(resourceUrl.getFile());
+        return new String(Files.readAllBytes(file.toPath()));
     }
 }

da6d58ae43532eb39bc7a5a728ccc4e98c477434
BurningFalls <sherlocky31415@gmail.com>
2024-09-12 10:00:48 +0900

    feature: HttpRequest 클래스 생성

---
+++ tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequest.java
@@ -0,0 +1,69 @@
+package org.apache.coyote.http11.request;
+
+import java.util.Map;
+
+import org.apache.coyote.http11.MimeTypeMaker;
+import org.apache.coyote.http11.request.component.RequestBody;
+import org.apache.coyote.http11.request.component.RequestHeader;
+import org.apache.coyote.http11.request.component.RequestLine;
+
+import com.techcourse.model.User;
+
+public class HttpRequest {
+    private static final String ALL_MIME_TYPE = "*/*";
+
+    private final RequestLine requestLine;
+    private final RequestHeader headers;
+    private final RequestBody body;
+
+    public HttpRequest(String inputRequestLine, String inputHeaders) {
+        this.requestLine = new RequestLine(inputRequestLine);
+        this.headers = new RequestHeader(inputHeaders);
+        this.body = new RequestBody();
+    }
+
+    public int getContentLength() {
+        return headers.getContentLength();
+    }
+
+    public void setBody(String body) {
+        this.body.setBody(body);
+    }
+
+    public String getMethod() {
+        return requestLine.getMethod();
+    }
+
+    public String getSession() {
+        return headers.getSession();
+    }
+
+    public String getRequestUri() {
+        return requestLine.getRequestUri();
+    }
+
+    public String getRequestUrl() {
+        return requestLine.getRequestUrl();
+    }
+
+    public String getMimeType() {
+        String mimeType = headers.getMimeType();
+        if (mimeType.isBlank() || mimeType.equals(ALL_MIME_TYPE)) {
+            String extension = getExtension();
+            return MimeTypeMaker.getMimeTypeFromExtension(extension);
+        }
+        return mimeType;
+    }
+
+    private String getExtension() {
+        return requestLine.getExtension();
+    }
+
+    public User getUser() {
+        return body.getUser();
+    }
+
+    public Map<String, String> getUserInformation() {
+        return body.getUserInformation();
+    }
+}

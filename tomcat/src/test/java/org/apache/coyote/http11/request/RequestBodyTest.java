9d2008ca730f5fcd5bf2790b2d733690d75938d2
KIMGYUTAE <kiimwf0819@gmail.com>
2024-09-12 23:38:28 +0900

    test: request 패키지 테스트 작성

---
+++ tomcat/src/test/java/org/apache/coyote/http11/request/RequestBodyTest.java
@@ -0,0 +1,43 @@
+package org.apache.coyote.http11.request;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+class RequestBodyTest {
+
+    @Test
+    void 폼_데이터를_반환() {
+        // given
+        String account = "ted";
+        String password = "1234";
+        String email = "ted%40gmail.com";
+        String body = "account=" + account + "&password=" + password + "&email=" + email;
+        RequestBody requestBody = new RequestBody(body);
+
+        // when
+        Map<String, String> actual = requestBody.getFormData();
+
+        // then
+        assertAll(
+                () -> assertThat(actual.get("account")).isEqualTo(account),
+                () -> assertThat(actual.get("password")).isEqualTo(password),
+                () -> assertThat(actual.get("email")).isEqualTo(email)
+        );
+    }
+
+    @Test
+    void 폼_데이터가_없는_경우_빈_값을_반환() {
+        // given
+        RequestBody requestBody = new RequestBody(null);
+
+        // when
+        Map<String, String> actual = requestBody.getFormData();
+
+        // then
+        assertThat(actual).isEmpty();
+    }
+
+}

a0a58505315995a833ed702e28c7257ff1d2d685
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-09-12 19:33:05 +0900

    feat(FileContentParser): 파일 내용을 문자열로 읽어오는 객체 추출

--- tomcat/src/main/java/org/apache/coyote/handler/StaticResourceHandler.java
+++ tomcat/src/main/java/org/apache/coyote/handler/StaticResourceHandler.java
@@ -1,26 +1,14 @@
 package org.apache.coyote.handler;
 
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
 
-import org.apache.coyote.exception.NotFoundException;
+import org.apache.coyote.file.FileContentParser;
+import org.apache.coyote.file.FilePathParser;
 import org.apache.http.header.HttpHeaderName;
 import org.apache.http.request.HttpRequest;
 import org.apache.http.response.HttpResponse;
 
 public class StaticResourceHandler implements Controller {
-
-    private static final Map<String, String> STATIC_RESOURCE_EXTENSIONS = Map.of(
-            "html", "",
-            "css", "/css",
-            "js", "/assets"
-    );
-    private static final String STATIC_RESOURCE_ROOT_PATH = "static";
-    private static final String DEFAULT_EXTENSION = ".html";
-    private static final String PATH_DELIMITER = "/";
 
     private static final StaticResourceHandler INSTANCE = new StaticResourceHandler();
 
@@ -37,39 +25,16 @@ public class StaticResourceHandler implements Controller {
     }
 
     public HttpResponse handle(final HttpRequest httpRequest) {
-        final URL resourceURL = getClass().getClassLoader().getResource(findResourcePath(httpRequest.getPath()));
-        try {
-            final Path resourcePath = Path.of(resourceURL.getPath());
-            final String responseBody = Files.readString(resourcePath);
-            final String mimeType = Files.probeContentType(resourcePath);
-            return HttpResponse.builder()
-                    .body(responseBody)
-                    .addHeader(HttpHeaderName.CONTENT_TYPE, mimeType)
-                    .build();
-        } catch (NullPointerException e) {
-            throw new NotFoundException(e.getMessage());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+        final String requestPath = httpRequest.getPath();
+        final String resourcePath = FilePathParser.findResourcePath(requestPath);
+        final Path physicalPath = Path.of(getClass().getClassLoader().getResource(resourcePath).getPath());
 
-    // TODO: 분리
-    private String findResourcePath(final String path) {
-        final String[] resourceNames = findResourceName(path).split("\\.");
-        final String extension = resourceNames[resourceNames.length - 1];
+        final String fileContent = FileContentParser.parseContent(physicalPath);
+        final String mimeType = FileContentParser.parseContentType(physicalPath);
 
-        if (STATIC_RESOURCE_EXTENSIONS.containsKey(extension)) {
-            return STATIC_RESOURCE_ROOT_PATH
-                    .concat(path);
-        }
-
-        return STATIC_RESOURCE_ROOT_PATH
-                .concat(path)
-                .concat(DEFAULT_EXTENSION);
-    }
-
-    private String findResourceName(final String path) {
-        final String[] paths = path.split(PATH_DELIMITER);
-        return paths[paths.length - 1];
+        return HttpResponse.builder()
+                .body(fileContent)
+                .addHeader(HttpHeaderName.CONTENT_TYPE, mimeType)
+                .build();
     }
 }

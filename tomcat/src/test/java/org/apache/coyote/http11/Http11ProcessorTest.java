afa95869d1a87a6f662734318917caa6206ff2b9
unifolio0 <boho82ha@gmail.com>
2024-09-12 15:01:26 +0900

    test: DisplayName 추가

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -6,14 +6,15 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import support.StubSocket;
 
 class Http11ProcessorTest {
 
+    @DisplayName("정적 파일을 요청하면 해당 파일을 반환한다")
     @Test
-    void index() throws IOException {
-        // given
+    void staticResource() throws IOException {
         final String httpRequest= String.join("\r\n",
                 "GET /index.html HTTP/1.1 ",
                 "Host: localhost:8080 ",
@@ -24,10 +25,8 @@ class Http11ProcessorTest {
         final var socket = new StubSocket(httpRequest);
         final Http11Processor processor = new Http11Processor(socket);
 
-        // when
         processor.process(socket);
 
-        // then
         final URL resource = getClass().getClassLoader().getResource("static/index.html");
         String body = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
 

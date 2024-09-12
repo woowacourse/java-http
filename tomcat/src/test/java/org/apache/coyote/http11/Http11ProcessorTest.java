c001199c8860c1f3f57dc80d89e89270b0c5e0ad
yoonjuho <dbswn990@naver.com>
2024-09-12 17:35:09 +0900

    refactor: 패키지 별 역할 분리

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -1,19 +1,18 @@
 package org.apache.coyote.http11;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import support.StubSocket;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+import support.StubSocket;
 
 class Http11ProcessorTest {
 
-    @Disabled
     @Test
     void process() {
         // given
@@ -26,18 +25,17 @@ class Http11ProcessorTest {
         // then
         var expected = String.join("\r\n",
                 "HTTP/1.1 200 OK",
+                "Content-Length: 5564",
                 "Content-Type: text/html;charset=utf-8",
-                "Content-Length: 12",
-                "",
-                "Hello world!");
+                "");
 
-        assertThat(socket.output()).isEqualTo(expected);
+        assertThat(socket.output()).contains(expected);
     }
 
     @Test
     void index() throws IOException {
         // given
-        final String httpRequest= String.join("\r\n",
+        final String httpRequest = String.join("\r\n",
                 "GET /index.html HTTP/1.1",
                 "Host: localhost:8080",
                 "Connection: keep-alive",
@@ -55,7 +53,7 @@ class Http11ProcessorTest {
         var expected = "HTTP/1.1 200 OK\r\n" +
                 "Content-Length: 5564\r\n" +
                 "Content-Type: text/html;charset=utf-8\r\n" +
-                "\r\n"+
+                "\r\n" +
                 new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
 
         assertThat(socket.output()).isEqualTo(expected);
@@ -64,7 +62,7 @@ class Http11ProcessorTest {
     @Test
     void cssSupport() throws IOException {
         // given
-        final String httpRequest= String.join("\r\n",
+        final String httpRequest = String.join("\r\n",
                 "GET /css/styles.css HTTP/1.1",
                 "Host: localhost:8080",
                 "Accept: text/css,*/*;q=0.1",
@@ -83,37 +81,7 @@ class Http11ProcessorTest {
         var expected = "HTTP/1.1 200 OK\r\n" +
                 "Content-Length: 211991\r\n" +
                 "Content-Type: text/css\r\n" +
-                "\r\n"+
-                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
-
-        assertThat(socket.output()).isEqualTo(expected);
-    }
-
-    @Disabled
-    @Test
-    void queryStringParsing() throws IOException {
-        // given
-        final String httpRequest= String.join("\r\n",
-                "GET /login?account=gugu&password=password HTTP/1.1",
-                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-                "Host: localhost:8080",
-                "Connection: keep-alive",
-                "",
-                "");
-
-        final var socket = new StubSocket(httpRequest);
-        final Http11Processor processor = new Http11Processor(socket);
-
-        // when
-        processor.process(socket);
-
-        // then
-        final URL resource = getClass().getClassLoader().getResource("static/index.html");
-        var expected = "HTTP/1.1 302 FOUND\r\n" +
-                "Content-Length: 5564\r\n" +
-                "Location: /index.html\r\n" +
-                "Content-Type: text/html;charset=utf-8\r\n" +
-                "\r\n"+
+                "\r\n" +
                 new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
 
         assertThat(socket.output()).isEqualTo(expected);

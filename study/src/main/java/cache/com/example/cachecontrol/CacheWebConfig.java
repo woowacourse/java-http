309120c1d45ea4f46a56e45d1c6424cbec28019c
gyungchan Jo <namejgc@naver.com>
2024-09-13 03:19:08 +0900

    feat: 학습 테스트 구현

--- study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
+++ study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
@@ -1,13 +1,33 @@
 package cache.com.example.cachecontrol;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
+
+import java.time.Duration;
 
 @Configuration
 public class CacheWebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(noCacheInterceptor());
+        registry.addInterceptor(resourceCacheInterceptor());
+    }
+
+    private WebContentInterceptor noCacheInterceptor() {
+        WebContentInterceptor webContentInterceptor = new WebContentInterceptor();
+        webContentInterceptor.addCacheMapping(CacheControl.noCache().cachePrivate(), "/");
+
+        return webContentInterceptor;
+    }
+
+    private WebContentInterceptor resourceCacheInterceptor() {
+        WebContentInterceptor webContentInterceptor = new WebContentInterceptor();
+        webContentInterceptor.addCacheMapping(CacheControl.maxAge(Duration.ofDays(365)).cachePublic(), "/resources/**");
+
+        return webContentInterceptor;
     }
 }

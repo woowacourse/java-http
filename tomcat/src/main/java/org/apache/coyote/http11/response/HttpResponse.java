d20ae0398e3d7178090214c1189326c22d46ef44
BurningFalls <sherlocky31415@gmail.com>
2024-09-12 10:01:49 +0900

    feature: HttpResponse 클래스 생성

---
+++ tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponse.java
@@ -0,0 +1,53 @@
+package org.apache.coyote.http11.response;
+
+import java.util.Map;
+
+import org.apache.coyote.http11.response.component.ResponseBody;
+import org.apache.coyote.http11.response.component.ResponseHeader;
+import org.apache.coyote.http11.response.component.StatusLine;
+
+public class HttpResponse {
+
+    private final StatusLine statusLine;
+    private final ResponseHeader headers;
+    private final ResponseBody responseBody;
+
+    public HttpResponse() {
+        this.statusLine = new StatusLine();
+        this.headers = new ResponseHeader();
+        this.responseBody = new ResponseBody();
+    }
+
+    public void setStatusLine(String statusCode, String statusMessage) {
+        statusLine.setStatusCode(statusCode);
+        statusLine.setStatusMessage(statusMessage);
+    }
+
+    public void setFieldValue(String field, String value) {
+        headers.setFieldValue(field, value);
+    }
+
+    public void setBody(String body) {
+        this.responseBody.setBody(body);
+    }
+
+    public String getVersion() {
+        return statusLine.getVersion();
+    }
+
+    public String getStatusCode() {
+        return statusLine.getStatusCode();
+    }
+
+    public String getStatusMessage() {
+        return statusLine.getStatusMessage();
+    }
+
+    public Map<String, String> getResponseHeader() {
+        return headers.getResponseHeader();
+    }
+
+    public String getBody() {
+        return responseBody.getBody();
+    }
+}

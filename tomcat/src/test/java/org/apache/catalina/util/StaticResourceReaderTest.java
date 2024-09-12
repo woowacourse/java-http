688ab570c38e9dfab871715bd8a00d98a9c84ec0
HyungUk Ryu <hw0603@naver.com>
2024-09-12 17:03:57 +0900

    test(StaticRecourseReaderTest): 정적 리소스를 읽어오는 테스트 추가

---
+++ tomcat/src/test/java/org/apache/catalina/util/StaticResourceReaderTest.java
@@ -0,0 +1,43 @@
+package org.apache.catalina.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import org.apache.coyote.http11.response.ResponseFile;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class StaticResourceReaderTest {
+
+    @DisplayName("정적 리소스를 읽어온다.")
+    @Test
+    void read() {
+        // given
+        String path = "/500.html";
+
+        // when
+        ResponseFile file = StaticResourceReader.read(path);
+
+        // then
+        assertThat(file).isNotNull();
+    }
+
+    @DisplayName("정적 리소스가 존재하는지 확인한다.")
+    @ParameterizedTest
+    @MethodSource("provideTestCase")
+    void isExist(String filePath, boolean expected) {
+        assertThat(StaticResourceReader.isExist(filePath)).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> provideTestCase() {
+        return Stream.of(
+                Arguments.of("/css/styles.css", true),
+                Arguments.of("/404.html", true),
+                Arguments.of("/index.html", true),
+                Arguments.of("/notfound.html", false)
+        );
+    }
+}

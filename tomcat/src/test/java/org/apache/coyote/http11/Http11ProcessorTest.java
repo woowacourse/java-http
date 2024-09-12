2c4b38de79e95c14e6083b93881b3aa3be1b3f59
HoeSeong123 <ghltjd3@gmail.com>
2024-09-13 00:26:01 +0900

    test: 테스트 디스플레이명 변경

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -26,8 +26,8 @@ class Http11ProcessorTest {
         // then
         var expected = String.join("\r\n",
                 "HTTP/1.1 200 OK ",
-                "Content-Type: text/html;charset=utf-8 ",
                 "Content-Length: 12 ",
+                "Content-Type: text/html;charset=utf-8 ",
                 "",
                 "Hello world!");
 
@@ -53,8 +53,8 @@ class Http11ProcessorTest {
         // then
         final URL resource = getClass().getClassLoader().getResource("static/index.html");
         var expected = "HTTP/1.1 200 OK \r\n" +
-                "Content-Type: text/html;charset=utf-8 \r\n" +
                 "Content-Length: " + new File(resource.getPath()).length() + " \r\n" +
+                "Content-Type: text/html;charset=utf-8 \r\n" +
                 "\r\n"+
                 new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
 
@@ -82,8 +82,8 @@ class Http11ProcessorTest {
         // then
         final URL resource = getClass().getClassLoader().getResource("static/css/styles.css");
         var expected = "HTTP/1.1 200 OK \r\n" +
-                "Content-Type: text/css;charset=utf-8 \r\n" +
                 "Content-Length: " + new File(resource.getPath()).length() + " \r\n" +
+                "Content-Type: text/css;charset=utf-8 \r\n" +
                 "\r\n"+
                 new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
 
@@ -113,8 +113,8 @@ class Http11ProcessorTest {
             // then
             final URL resource = getClass().getClassLoader().getResource("static/login.html");
             var expected = "HTTP/1.1 200 OK \r\n" +
-                    "Content-Type: text/html;charset=utf-8 \r\n" +
                     "Content-Length: " + new File(resource.getPath()).length() + " \r\n" +
+                    "Content-Type: text/html;charset=utf-8 \r\n" +
                     "\r\n" +
                     new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
 
@@ -201,8 +201,8 @@ class Http11ProcessorTest {
             // then
             final URL resource = getClass().getClassLoader().getResource("static/register.html");
             var expected = "HTTP/1.1 200 OK \r\n" +
-                    "Content-Type: text/html;charset=utf-8 \r\n" +
                     "Content-Length: " + new File(resource.getPath()).length() + " \r\n" +
+                    "Content-Type: text/html;charset=utf-8 \r\n" +
                     "\r\n" +
                     new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
 
@@ -210,7 +210,7 @@ class Http11ProcessorTest {
         }
 
         @Test
-        @DisplayName("로그인 성공")
+        @DisplayName("회원가입 성공")
         void loginSuccess() throws IOException {
             // given
             final String httpRequest = String.join("\r\n",

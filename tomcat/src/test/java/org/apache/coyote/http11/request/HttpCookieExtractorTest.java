3d5c6e429a335e2ad79c62045611ee9328161fa6
masonkimseoul <masonkimseoul@gmail.com>
2024-09-13 00:26:22 +0900

    test: 헤더 쿠키 추출 테스트 작성

---
+++ tomcat/src/test/java/org/apache/coyote/http11/request/HttpCookieExtractorTest.java
@@ -0,0 +1,28 @@
+package org.apache.coyote.http11.request;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class HttpCookieExtractorTest {
+
+    private static final String cookies = "yummy_cookie=choco; tasty_cookie=strawberry; JSESSIONID=656cef62-e3c4-40bc-a8df-94732920ed46";
+
+    @Test
+    @DisplayName("헤더에서 쿠키를 추출한다.")
+    void should_separate_cookie_from_request_header() {
+        //when
+        HttpRequestHeaders httpRequestHeaders = new HttpRequestHeaders(Map.of("Cookie", cookies));
+
+        //given
+        HttpCookie httpCookie = HttpCookieExtractor.extractCookie(httpRequestHeaders);
+
+        //then
+        assertThat(httpCookie.getCookieValue("yummy_cookie")).isEqualTo("choco");
+        assertThat(httpCookie.getCookieValue("tasty_cookie")).isEqualTo("strawberry");
+        assertThat(httpCookie.getCookieValue("JSESSIONID")).isEqualTo("656cef62-e3c4-40bc-a8df-94732920ed46");
+    }
+}

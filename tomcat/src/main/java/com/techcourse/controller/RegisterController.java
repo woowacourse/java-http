63c259f5be631e8e9f2011d5864ea3951a3197a4
yoonjuho <dbswn990@naver.com>
2024-09-12 17:30:29 +0900

    feat: 회원가입 관련 요청 처리 컨트롤러 구현

---
+++ tomcat/src/main/java/com/techcourse/controller/RegisterController.java
@@ -0,0 +1,50 @@
+package com.techcourse.controller;
+
+import static org.apache.catalina.webresources.FileResource.NOT_FOUND_RESOURCE_URI;
+
+import java.util.Map;
+
+import org.apache.catalina.connector.HttpRequest;
+import org.apache.catalina.connector.HttpResponse;
+import org.apache.catalina.servlet.Controller;
+import org.apache.tomcat.util.http.ResourceURI;
+import org.apache.tomcat.util.http.parser.QueryStringParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.techcourse.db.InMemoryUserRepository;
+import com.techcourse.model.User;
+
+public class RegisterController implements Controller {
+    private static final Logger log = LoggerFactory.getLogger(RegisterController.class);
+    private static final ResourceURI REGISTER_RESOURCE_URI = new ResourceURI("/register.html");
+    private static final ResourceURI REDIRECT_RESOURCE_URI = new ResourceURI("/index.html");
+
+    @Override
+    public void service(HttpRequest request, HttpResponse response) throws Exception {
+        switch (request.getHttpMethod()) {
+            case GET -> doGet(request, response);
+            case POST -> doPost(request, response);
+        }
+    }
+
+    private void doGet(HttpRequest request, HttpResponse response) throws Exception {
+        response.writeStaticResource(REGISTER_RESOURCE_URI);
+    }
+
+    private void doPost(HttpRequest request, HttpResponse response) throws Exception {
+        Map<String, String> parsed = QueryStringParser.parse(request.httpBody().body());
+        String account = parsed.get("account");
+        String password = parsed.get("password");
+        String email = parsed.get("email");
+        User user = new User(account, password, email);
+        if (user.isValid()) {
+            InMemoryUserRepository.save(user);
+            log.info("save user: {}", user);
+            response.sendRedirect(REDIRECT_RESOURCE_URI);
+            return;
+        }
+        log.error("account={}, password={}, email={}, 회원가입에 실패하였습니다.", account, password, email);
+        response.sendRedirect(NOT_FOUND_RESOURCE_URI);
+    }
+}

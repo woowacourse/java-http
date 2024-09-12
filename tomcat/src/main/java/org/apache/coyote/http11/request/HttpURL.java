f50c1e139853899b3a816c6c537e3465a9e1d229
HyungUk Ryu <hw0603@naver.com>
2024-09-12 15:22:45 +0900

    test(NameValuePairsTest): 이름-값 쌍 파싱 테스트 추가

--- tomcat/src/main/java/org/apache/coyote/http11/request/HttpURL.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/HttpURL.java
@@ -8,6 +8,7 @@ public record HttpURL(String fullUrl, String path, NameValuePairs queryParameter
     private static final String PATH_QUERY_DELIMITER = "\\?";
     private static final int PATH_INDEX = 0;
     private static final int QUERY_INDEX = 1;
+    private static final int PATH_QUERY_LENGTH = 2;
 
     public static HttpURL from(String fullUrl) {
         return new HttpURL(fullUrl, parsePath(fullUrl), parseQueryParameters(fullUrl));
@@ -18,7 +19,7 @@ public record HttpURL(String fullUrl, String path, NameValuePairs queryParameter
     }
 
     private static NameValuePairs parseQueryParameters(String url) {
-        String[] pathAndQuery = url.split(PATH_QUERY_DELIMITER, 2);
+        String[] pathAndQuery = url.split(PATH_QUERY_DELIMITER, PATH_QUERY_LENGTH);
         if (isQueryPartExist(pathAndQuery)) {
             return NameValuePairs.empty();
         }
@@ -26,6 +27,6 @@ public record HttpURL(String fullUrl, String path, NameValuePairs queryParameter
     }
 
     private static boolean isQueryPartExist(String[] pathAndQuery) {
-        return pathAndQuery.length != 2 || pathAndQuery[QUERY_INDEX].isEmpty();
+        return pathAndQuery.length != PATH_QUERY_LENGTH || pathAndQuery[QUERY_INDEX].isEmpty();
     }
 }

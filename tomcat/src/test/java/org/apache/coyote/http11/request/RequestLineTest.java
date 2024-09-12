713c9245839ac34b0c10e6f7cc4aebec3983e874
devhoya97 <diajbh123@gmail.com>
2024-09-12 11:23:17 +0900

    test: requestLine 예외처리 메서드 테스트

--- tomcat/src/test/java/org/apache/coyote/http11/request/RequestLineTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/request/RequestLineTest.java
@@ -1,6 +1,7 @@
 package org.apache.coyote.http11.request;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -8,9 +9,9 @@ import org.junit.jupiter.api.Test;
 
 class RequestLineTest {
 
-    @DisplayName("String 한 줄로 RequestLine 객체를 생성할 수 있습니다.")
+    @DisplayName("String 한 줄로 RequestLine 객체를 생성할 수 있다.")
     @Test
-    void tdd() {
+    void createRequestLine() {
         // given
         String rawRequestLine = "GET /login HTTP/1.1";
 
@@ -23,5 +24,17 @@ class RequestLineTest {
                 () -> assertThat(requestLine.getPath()).isEqualTo("/login"),
                 () -> assertThat(requestLine.getVersion()).isEqualTo("HTTP/1.1")
         );
+    }
+
+    @DisplayName("request line을 공백 기준으로 쪼갰을 때, 3개의 요소로 이루어져있지 않으면 예외를 발생시킨다.")
+    @Test
+    void createRequestFail() {
+        // given
+        String rawRequestLine = "GET /loginHTTP/1.1";
+
+        // when & then
+        assertThatThrownBy(() -> new RequestLine(rawRequestLine)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("request line은 공백 기준으로 쪼갰을 때, 3개의 요소로 이루어져야 합니다. " +
+                        "현재 요소 갯수 = 2");
     }
 }

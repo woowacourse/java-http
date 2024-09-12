4701b0411ca0c6ae68bd34567d555a0b12ef93ca
hyeonjilee <skylar12200@gmail.com>
2024-09-12 22:33:24 +0900

    refactor: HttpResponse에 원시값 포장 적용

--- tomcat/src/main/java/org/apache/coyote/response/HttpResponse.java
+++ tomcat/src/main/java/org/apache/coyote/response/HttpResponse.java
@@ -1,77 +1,28 @@
 package org.apache.coyote.response;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
 import org.apache.coyote.http.HeaderName;
-import org.apache.coyote.coockie.HttpCookie;
-import org.apache.coyote.http.ResourceType;
 import org.apache.coyote.http.StatusCode;
-import org.apache.coyote.request.HttpRequest;
 
 public class HttpResponse {
 
-    public static final String STATIC_PATH = "/static";
-
     private final StatusLine statusLine;
-    private final Map<String, String> header;
-    private final HttpCookie cookie;
-    private String body;
+    private final ResponseHeader header;
+    private final ResponseBody body;
 
-    public HttpResponse(HttpRequest httpRequest, ResourceType resourceType) throws IOException {
+    public HttpResponse() {
         this.statusLine = new StatusLine();
-        if (resourceType == ResourceType.NON_STATIC) {
-            this.body = "";
-        }
-        if (resourceType == ResourceType.STATIC) {
-            this.body = mapBody(httpRequest.getPath());
-        }
-        this.header = mapHeader(httpRequest, resourceType);
-        this.cookie = httpRequest.getHttpCookie();
-    }
-
-    private String mapBody(String resource) throws IOException {
-        StringBuilder rawBody = new StringBuilder();
-        Path path = Path.of(getClass().getResource(STATIC_PATH + resource).getPath());
-
-        Files.readAllLines(path)
-                .forEach(line -> rawBody.append(line).append("\r\n"));
-        return String.valueOf(rawBody);
-    }
-
-    private Map<String, String> mapHeader(HttpRequest httpRequest, ResourceType resourceType) throws IOException {
-        Map<String, String> headerEntry = new HashMap<>();
-        headerEntry.put(HeaderName.SET_COOKIE.getValue(), httpRequest.getCookieResponse());
-        headerEntry.put(HeaderName.CONTENT_TYPE.getValue(), httpRequest.getContentType());
-        if (resourceType == ResourceType.STATIC) {
-            headerEntry.put(HeaderName.CONTENT_LENGTH.getValue(), String.valueOf(body.getBytes().length));
-        }
-        return headerEntry;
+        this.header = new ResponseHeader();
+        this.body = new ResponseBody();
     }
 
     public String getReponse() {
         StringBuilder response = new StringBuilder();
 
-        response.append(statusLine.getStatusLineResponse())
+        response.append(statusLine.getResponse())
                 .append("\r\n")
-                .append(getHeaderResponse())
+                .append(header.getResponse())
                 .append("\r\n")
-                .append(body);
-        return String.valueOf(response);
-    }
-
-    private String getHeaderResponse() {
-        StringBuilder response = new StringBuilder();
-
-        for (Entry<String, String> headerEntry : header.entrySet()) {
-            response.append(headerEntry.getKey())
-                    .append(": ")
-                    .append(headerEntry.getValue())
-                    .append("\r\n");
-        }
+                .append(body.getContent());
         return String.valueOf(response);
     }
 
@@ -79,21 +30,12 @@ public class HttpResponse {
         statusLine.setStatusCode(statusCode);
     }
 
-    public void setHeader(HeaderName headerName, String value) {
-        header.put(headerName.getValue(), value);
+    public void addHeader(HeaderName headerName, String value) {
+        header.addHeader(headerName, value);
     }
 
-    public void generateJSESSIONID() {
-        cookie.generateJSESSIONID();
-        header.put(HeaderName.SET_COOKIE.getValue(), cookie.getResponse());
-    }
-
-    public String getJESSIONID() {
-        return cookie.getJESSIONID();
-    }
-
-    public void setBody(String resource) throws IOException {
-        this.body = mapBody(resource);
-        header.put(HeaderName.CONTENT_LENGTH.getValue(), String.valueOf(body.getBytes().length));
+    public void setBody(String resource) {
+        body.setBody(resource);
+        header.addHeader(HeaderName.CONTENT_LENGTH, String.valueOf(body.getLength()));
     }
 }

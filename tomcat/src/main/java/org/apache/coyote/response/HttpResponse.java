1a7ca5bda9593856af97da97de8273f378c69bae
Eugene Jang <kyunellroll@gmail.com>
2024-09-13 00:02:43 +0900

    modify(HttpResponse): 요청을 보내는 send 메서드 생성
    - 메서드에 따라 반환값을 확인하는 테스트 작성

---
+++ tomcat/src/main/java/org/apache/coyote/response/HttpResponse.java
@@ -0,0 +1,85 @@
+package org.apache.coyote.response;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import org.apache.coyote.cookie.HttpCookie;
+import org.apache.coyote.protocolVersion.ProtocolVersion;
+import org.apache.coyote.request.HttpRequest;
+import org.apache.coyote.response.body.ResponseBody;
+import org.apache.coyote.response.header.ResponseHeader;
+import org.apache.coyote.response.responseLine.ResponseLine;
+import org.apache.coyote.util.ContentType;
+import org.apache.coyote.util.HttpStatus;
+import org.apache.coyote.util.ResourceFinder;
+
+public class HttpResponse {
+
+    private static final String DEFAULT_BODY = "Hello world!";
+    private static final String SPACE = " ";
+    private static final String BLANK = "";
+    private static final String CRLF = "\r\n";
+
+    private final ResponseLine responseLine;
+    private final ResponseHeader header;
+    private final ResponseBody responseBody;
+    private final OutputStream outputStream;
+
+    public HttpResponse(OutputStream outputStream) {
+        this.responseLine = new ResponseLine(ProtocolVersion.ofHTTP1());
+        this.header = new ResponseHeader();
+        this.responseBody = new ResponseBody();
+        this.outputStream = outputStream;
+    }
+
+    public void setCookies(String sessionId) {
+        header.addCookie(HttpCookie.ofJSessionId(sessionId).combineCookie());
+    }
+
+    public byte[] combineResponseToBytes() {
+        String response = String.join(CRLF,
+                responseLine.toCombinedResponse() + SPACE,
+                header.toCombinedHeader(),
+                BLANK,
+                responseBody.getBody());
+
+        return response.getBytes();
+    }
+
+    public void sendError(HttpStatus httpStatus) {
+        sendRedirect(httpStatus.getStatusCode() + ContentType.HTML.getExtension());
+    }
+
+    public void sendRedirect(String location) {
+        header.addLocation(location);
+        responseLine.setHttpStatus(HttpStatus.FOUND);
+
+        flushBuffer();
+    }
+
+    public void sendStaticResourceResponse(HttpRequest httpRequest, HttpStatus httpStatus) {
+        String resource = ResourceFinder.findBy(httpRequest.getResourcePath());
+
+        setBody(resource, httpStatus, httpRequest.findContentType(), resource.getBytes().length);
+        flushBuffer();
+    }
+
+    public void sendDefaultResponse() {
+        setBody(DEFAULT_BODY, HttpStatus.OK, ContentType.PLAIN, DEFAULT_BODY.getBytes().length);
+        flushBuffer();
+    }
+
+    private void flushBuffer() {
+        try {
+            outputStream.write(combineResponseToBytes());
+            outputStream.flush();
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private void setBody(String body, HttpStatus httpStatus, ContentType contentType, int length) {
+        responseBody.setBody(body);
+        responseLine.setHttpStatus(httpStatus);
+        header.addContentLengthAndType(contentType, length);
+    }
+}
---
+++ tomcat/src/test/java/org/apache/coyote/response/HttpResponseTest.java
@@ -0,0 +1,55 @@
+package org.apache.coyote.response;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.OutputStream;
+import org.apache.coyote.fixture.HttpRequestFixture;
+import org.apache.coyote.request.HttpRequest;
+import org.apache.coyote.util.HttpStatus;
+import org.apache.coyote.util.ResourceFinder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class HttpResponseTest {
+
+    private final HttpResponse httpResponse;
+
+    public HttpResponseTest() {
+        this.httpResponse = new HttpResponse(OutputStream.nullOutputStream());
+    }
+
+    @Test
+    @DisplayName("Error를 보내는 경우 해당하는 Status에 일치하는 페이지로 redirect 한다.")
+    void sendError() {
+        httpResponse.sendError(HttpStatus.NOT_FOUND);
+
+        assertTrue(new String(httpResponse.combineResponseToBytes()).contains("Location: 404.html"));
+    }
+
+    @Test
+    @DisplayName("요청한 Location으로 redirect 한다.")
+    void sendRedirect() {
+        httpResponse.sendRedirect("/index.html");
+
+        assertTrue(new String(httpResponse.combineResponseToBytes()).contains("Location: /index.html"));
+    }
+
+    @Test
+    @DisplayName("정적 파일을 반환한다.")
+    void sendStaticResourceResponse() {
+        HttpRequest httpRequest = HttpRequestFixture.GET_STATIC_MAIN_PATH_REQUEST;
+
+        httpResponse.sendStaticResourceResponse(httpRequest, HttpStatus.OK);
+        String resource = ResourceFinder.findBy("/index.html");
+
+        assertTrue(new String(httpResponse.combineResponseToBytes()).contains(resource));
+    }
+
+    @Test
+    @DisplayName("Default 페이지를 반환한다.")
+    void sendDefaultResponse() {
+        httpResponse.sendDefaultResponse();
+
+        assertTrue(new String(httpResponse.combineResponseToBytes()).contains("Hello world!"));
+    }
+}

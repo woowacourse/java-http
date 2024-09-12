4f1d96ad93afd7ed2a3907bf8c0ecf73193c6928
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-09-13 01:25:29 +0900

    refactor(ContentType): 이미지 파일 타입 설정 가능하도록 개선

--- tomcat/src/main/java/org/apache/catalina/http/ContentType.java
+++ tomcat/src/main/java/org/apache/catalina/http/ContentType.java
@@ -5,21 +5,24 @@ import java.util.List;
 
 public enum ContentType {
 
-    HTML("text/html", ".html"),
-    CSS("text/css", ".css"),
-    JS("text/javascript", ".js"),
-    PLAIN("text/plain", ".txt"),
+    HTML("text/html", "text/html", ".html"),
+    CSS("text/css", "text/css", ".css"),
+    JS("text/javascript", "text/javascript", ".js"),
+    PLAIN("text/plain", "text/plain", ".txt"),
+    SVG_IMAGE("image/avif", "image/svg+xml", "svg"),
     ;
 
     private static final String DEFAULT_CHARSET = "charset=utf-8";
     private static final String MEDIA_TYPE_CHARSET_DELIMITER = ";";
     public static final String CONTENT_TYPE_SEPARATOR = ",";
     public static final int CONTENT_TYPE_INDEX = 0;
-    private final String value;
+    private final String accept;
+    private final String type;
     private final String fileExtension;
 
-    ContentType(String value, String fileExtension) {
-        this.value = value;
+    ContentType(String accept, String type, String fileExtension) {
+        this.accept = accept;
+        this.type = type;
         this.fileExtension = fileExtension;
     }
 
@@ -30,7 +33,7 @@ public enum ContentType {
         List<String> types = List.of(acceptHeader.split(CONTENT_TYPE_SEPARATOR));
         String contentType = types.get(CONTENT_TYPE_INDEX).trim();
 
-        return Arrays.stream(values()).filter(value -> value.value.equals(contentType))
+        return Arrays.stream(values()).filter(value -> value.accept.equals(contentType))
                 .findAny()
                 .orElse(ContentType.HTML);
     }
@@ -42,6 +45,6 @@ public enum ContentType {
 
     @Override
     public String toString() {
-        return value + MEDIA_TYPE_CHARSET_DELIMITER + DEFAULT_CHARSET;
+        return type + MEDIA_TYPE_CHARSET_DELIMITER + DEFAULT_CHARSET;
     }
 }

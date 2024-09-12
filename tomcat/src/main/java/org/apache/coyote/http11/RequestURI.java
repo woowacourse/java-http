6c6163741f00e13a71d2a78da7ada0c2c63f44c9
HoeSeong123 <ghltjd3@gmail.com>
2024-09-12 16:39:40 +0900

    refactor: URI 클래스명 변경

---
+++ tomcat/src/main/java/org/apache/coyote/http11/RequestURI.java
@@ -0,0 +1,27 @@
+package org.apache.coyote.http11;
+
+public class RequestURI {
+
+    private final String path;
+    private final QueryParameters parameters;
+
+    public RequestURI(String path, QueryParameters parameters) {
+        this.path = path;
+        this.parameters = parameters;
+    }
+
+    public RequestURI(String requestURI) {
+        if(requestURI.contains("?")) {
+            int index = requestURI.indexOf("?");
+            path = requestURI.substring(0, index);
+            parameters = new QueryParameters(requestURI.substring(index + 1));
+            return;
+        }
+        path = requestURI;
+        parameters = new QueryParameters("");
+    }
+
+    public String getPath() {
+        return path;
+    }
+}

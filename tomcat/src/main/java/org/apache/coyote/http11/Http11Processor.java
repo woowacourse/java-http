fbbddb12c43d06d808b9c302a4166ccd15cd5ed6
nayonsoso <panda0329@naver.com>
2024-09-13 00:48:02 +0900

    feat: 에러 핸들링 구문 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,14 +1,15 @@
 package org.apache.coyote.http11;
 
-import com.techcourse.exception.UncheckedServletException;
 import java.io.IOException;
 import java.net.Socket;
-import org.apache.coyote.Processor;
 import org.apache.catalina.controller.AbstractController;
+import org.apache.catalina.exception.ApplicationException;
 import org.apache.catalina.handler.HandlerMapping;
+import org.apache.catalina.view.ViewResolver;
+import org.apache.coyote.Processor;
+import org.apache.coyote.http11.exception.HttpConnectorException;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.response.HttpResponse;
-import org.apache.catalina.view.ViewResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -42,12 +43,18 @@ public class Http11Processor implements Runnable, Processor {
 
             outputStream.write(response.getBytes());
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (IOException e) {
+            log.error(e.getMessage(), e, this);
+        } catch (HttpConnectorException e) {
+            // todo: 500 error page
+            log.error(e.getMessage(), e, this);
+        } catch (ApplicationException e) {
+            // todo: map to proper error page
             log.error(e.getMessage(), e, this);
         }
     }
 
-    private String getResponse(HttpRequest request) throws IOException {
+    private String getResponse(HttpRequest request) throws IOException, ApplicationException {
         AbstractController controller = handlerMapping.getController(request.header());
         if (controller != null) {
             HttpResponse response = new HttpResponse();

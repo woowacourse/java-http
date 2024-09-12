a078d1abace805b37898cabaaa41e9eb759f6464
linirini <2001yerin@naver.com>
2024-09-12 22:19:38 +0900

    feat: Executors로 Thread Pool 적용

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.apache.coyote.http11.Http11Processor;
 import org.slf4j.Logger;
@@ -14,16 +16,19 @@ public class Connector implements Runnable {
 
     private static final int DEFAULT_PORT = 8080;
     private static final int DEFAULT_ACCEPT_COUNT = 100;
+    private static final int MAX_THREAD_COUNT = 250;
 
     private final ServerSocket serverSocket;
+    private final ExecutorService threadPool;
     private boolean stopped;
 
     public Connector() {
-        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT);
+        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT, MAX_THREAD_COUNT);
     }
 
-    public Connector(final int port, final int acceptCount) {
+    public Connector(final int port, final int acceptCount, final int maxThreads) {
         this.serverSocket = createServerSocket(port, acceptCount);
+        this.threadPool = Executors.newFixedThreadPool(maxThreads);
         this.stopped = false;
     }
 
@@ -55,7 +60,8 @@ public class Connector implements Runnable {
 
     private void connect() {
         try {
-            process(serverSocket.accept());
+            Socket connection = serverSocket.accept();
+            threadPool.submit(() -> process(connection));
         } catch (IOException e) {
             log.error(e.getMessage(), e);
         }

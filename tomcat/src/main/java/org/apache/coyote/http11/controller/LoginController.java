da3030316711befd016352525ced58456db3f231
김민겸 <mingyum119@naver.com>
2024-09-12 14:25:41 +0900

    feat: 로그인을 담당하는 컨트롤러 구현

---
+++ tomcat/src/main/java/org/apache/coyote/http11/controller/LoginController.java
@@ -0,0 +1,59 @@
+package org.apache.coyote.http11.controller;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
+
+import org.apache.coyote.http11.common.Body;
+import org.apache.coyote.http11.common.Properties;
+import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.response.HttpResponse;
+import org.apache.coyote.http11.session.Session;
+import org.apache.coyote.http11.session.SessionManager;
+
+import com.techcourse.db.InMemoryUserRepository;
+import com.techcourse.model.User;
+
+public class LoginController extends AbstractController {
+
+	@Override
+	protected void doPost(HttpRequest request, HttpResponse response) {
+		Body body = request.getBody();
+		Properties properties = body.parseProperty();
+
+		String account = properties.get("account");
+		String password = properties.get("password");
+		Optional<User> user = InMemoryUserRepository.findByAccount(account);
+		if (user.isPresent() && user.get().checkPassword(password)) {
+			Session session = SessionManager.createSession(user.get());
+
+			response.setVersionOfProtocol("HTTP/1.1");
+			response.setStatusCode(302);
+			response.setStatusMessage("Found");
+			response.setHeaders(Map.of(
+				"Set-Cookie", "JSESSIONID=" + session.getId(),
+				"Location", "http://localhost:8080/401.html"
+			));
+		}
+	}
+
+	@Override
+	protected void doGet(HttpRequest request, HttpResponse response) throws IOException {
+		Session session = new Session(request);
+		Optional<User> userBySession = SessionManager.findUserBySession(session);
+
+		response.setVersionOfProtocol("HTTP/1.1");
+		if (userBySession.isPresent()) {
+			response.setStatusCode(302);
+			response.setStatusMessage("Found");
+			response.setHeaders(Map.of(
+				"Location", "http://localhost:8080" + "index.html"
+			));
+		} else {
+			response.setVersionOfProtocol("HTTP/1.1");
+			response.setStatusCode(200);
+			response.setStatusMessage("OK");
+			response.setBody("static" + request.getPath().value() + ".html");
+		}
+	}
+}

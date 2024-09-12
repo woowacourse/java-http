4686e6e3dece06e1aefd6de6de7a83db4c4bf245
jongmee <ra123221@gmail.com>
2024-09-12 10:46:11 +0900

    refactor: Http11Processor에서 handler 로직 제거

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,7 +1,6 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
-import com.techcourse.service.UserService;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,7 +16,6 @@ public class Http11Processor implements Runnable, Processor {
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
 
     private final Socket connection;
-    private final ResourceReader resourceReader = ResourceReader.getInstance();
 
     public Http11Processor(Socket connection) {
         this.connection = connection;
@@ -36,104 +34,19 @@ public class Http11Processor implements Runnable, Processor {
              OutputStream outputStream = connection.getOutputStream()) {
 
             HttpRequest httpRequest = HttpRequestParser.parse(bufferedReader);
-            String path = httpRequest.getPath();
+            Handler handler = RequestMapper.getHandler(httpRequest);
 
             HttpResponse httpResponse = new HttpResponse(HttpVersion.HTTP_1_1)
                     .addHttpStatusCode(HttpStatusCode.NOT_FOUND);
-
-            if (path.equals("/")) {
-                getRoot(httpResponse);
-            } else if (path.endsWith(".html")) {
-                getHtml(httpResponse, path);
-            } else if (path.endsWith("css") || path.endsWith("js")) {
-                getStaticResource(httpResponse, path);
-            } else if (path.startsWith("/login")) {
-                login(httpResponse, httpRequest);
-            } else if (path.equals("/register")) {
-                register(httpResponse, httpRequest);
+            if (handler != null) {
+                httpResponse = handler.doHandle(httpRequest, httpResponse); // null 처리
             }
 
             String response = HttpResponseParser.parse(httpResponse);
-
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
-    }
-
-    private void getRoot(HttpResponse httpResponse) {
-        httpResponse.addContentType(new ContentType(MediaType.HTML, "charset=utf-8"))
-                .addHttpStatusCode(HttpStatusCode.OK)
-                .addResponseBody("Hello world!");
-    }
-
-    private void getHtml(HttpResponse httpResponse, String path) throws IOException {
-        String responseBody = resourceReader.loadResourceAsString(path);
-        httpResponse.addContentType(new ContentType(MediaType.HTML, "charset=utf-8"))
-                .addHttpStatusCode(HttpStatusCode.OK)
-                .addResponseBody(responseBody);
-    }
-
-    private void getStaticResource(HttpResponse httpResponse, String path) throws IOException {
-        int lastIndexOfDot = path.lastIndexOf(".");
-        String postfix = path.substring(lastIndexOfDot + 1);
-        String responseBody = resourceReader.loadResourceAsString(path);
-        httpResponse.addContentType(ContentType.from(postfix))
-                .addHttpStatusCode(HttpStatusCode.OK)
-                .addResponseBody(responseBody);
-    }
-
-    private void login(HttpResponse httpResponse, HttpRequest httpRequest) throws IOException {
-        HttpMethod httpRequestMethod = httpRequest.getHttpMethod();
-        if (httpRequestMethod == HttpMethod.POST) {
-            String redirectUrl = "/index.html";
-            HttpRequestParameter requestParameter = httpRequest.getHttpRequestParameter();
-            try {
-                String sessionId = UserService.login(requestParameter);
-                httpResponse.addHttpStatusCode(HttpStatusCode.FOUND)
-                        .addCookie("JSESSIONID", sessionId)
-                        .addCookie("Max-Age", "600")
-                        .addRedirectUrl(redirectUrl);
-            } catch (IllegalArgumentException e) {
-                httpResponse.addHttpStatusCode(HttpStatusCode.FOUND)
-                        .addRedirectUrl("/401.html");
-            }
-        } else if (httpRequestMethod == HttpMethod.GET && !validateSession(httpRequest.getSessionId())) {
-            String responseBody = resourceReader.loadResourceAsString("login.html");
-            httpResponse.addContentType(new ContentType(MediaType.HTML, "charset=utf-8"))
-                    .addHttpStatusCode(HttpStatusCode.OK)
-                    .addResponseBody(responseBody);
-        } else if (httpRequestMethod == HttpMethod.GET && validateSession(httpRequest.getSessionId())) {
-            httpResponse.addHttpStatusCode(HttpStatusCode.FOUND)
-                    .addRedirectUrl("/index.html");
-        }
-    }
-
-    private void register(HttpResponse httpResponse, HttpRequest httpRequest) throws IOException {
-        HttpMethod httpRequestMethod = httpRequest.getHttpMethod();
-        if (httpRequestMethod == HttpMethod.POST) {
-            String redirectUrl = "/index.html";
-            HttpRequestParameter requestParameter = httpRequest.getHttpRequestParameter();
-            try {
-                UserService.createUser(requestParameter);
-            } catch (IllegalArgumentException e) {
-                redirectUrl = "/400.html";
-            }
-            httpResponse.addHttpStatusCode(HttpStatusCode.FOUND)
-                    .addRedirectUrl(redirectUrl);
-        } else if (httpRequestMethod == HttpMethod.GET) {
-            String responseBody = resourceReader.loadResourceAsString("register.html");
-            httpResponse.addContentType(new ContentType(MediaType.HTML, "charset=utf-8"))
-                    .addHttpStatusCode(HttpStatusCode.OK)
-                    .addResponseBody(responseBody);
-        }
-    }
-
-    private boolean validateSession(String sessionId) {
-        if (sessionId == null) {
-            return false;
-        }
-        return SessionManager.findSession(sessionId) != null;
     }
 }

a8ed6832d25be43a284833693f80d4c33381e703
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-09-12 19:19:27 +0900

    refactor(Session): HttpSession 상속 제거

--- tomcat/src/main/java/org/apache/coyote/session/SessionManager.java
+++ tomcat/src/main/java/org/apache/coyote/session/SessionManager.java
@@ -1,6 +1,5 @@
 package org.apache.coyote.session;
 
-import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,7 +7,7 @@ import org.apache.catalina.Manager;
 
 public class SessionManager implements Manager {
 
-    private static final Map<String, HttpSession> SESSIONS = new HashMap<>();
+    private static final Map<String, Session> SESSIONS = new HashMap<>();
     private static final SessionManager INSTANCE = new SessionManager();
 
     private SessionManager() {
@@ -19,17 +18,17 @@ public class SessionManager implements Manager {
     }
 
     @Override
-    public void add(HttpSession session) {
+    public void add(Session session) {
         SESSIONS.put(session.getId(), session);
     }
 
     @Override
-    public HttpSession findSession(String id) throws IOException {
+    public Session findSession(String id) throws IOException {
         return SESSIONS.get(id);
     }
 
     @Override
-    public void remove(HttpSession session) {
+    public void remove(Session session) {
         SESSIONS.remove(session.getId());
     }
 

b69588d19301d49058be738364f7da01b3eaf95d
Eugene Jang <kyunellroll@gmail.com>
2024-09-12 23:37:46 +0900

    modify(ResponseBody): 문자열만 담도록 변경

---
+++ tomcat/src/main/java/org/apache/coyote/response/body/ResponseBody.java
@@ -0,0 +1,14 @@
+package org.apache.coyote.response.body;
+
+public class ResponseBody {
+
+    private String body;
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}

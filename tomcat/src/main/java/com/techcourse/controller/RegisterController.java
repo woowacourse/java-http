95d215a44bda8e09961dd625e8f9d773100aed6d
juha <khabh@naver.com>
2024-09-12 11:04:35 +0900

    refactor: 회원가입 로직 리팩토링

--- tomcat/src/main/java/com/techcourse/controller/RegisterController.java
+++ tomcat/src/main/java/com/techcourse/controller/RegisterController.java
@@ -1,12 +1,12 @@
 package com.techcourse.controller;
 
 import com.techcourse.db.InMemoryUserRepository;
+import com.techcourse.exception.SafeExecutionWrapper;
 import com.techcourse.model.User;
 import java.util.List;
 import org.apache.catalina.controller.AbstractController;
 import org.apache.catalina.controller.Handler;
 import org.apache.coyote.http11.request.HttpRequest;
-import org.apache.coyote.http11.request.Queries;
 import org.apache.coyote.http11.response.HttpResponse;
 import org.apache.coyote.http11.response.HttpStatus;
 import org.apache.coyote.http11.response.ResponseFile;
@@ -18,11 +18,15 @@ public class RegisterController extends AbstractController {
     private static final Logger log = LoggerFactory.getLogger(RegisterController.class);
     private static final String REGISTER_PATH = "/register";
     private static final String REGISTER_PAGE = "/register.html";
+    private static final String REGISTER_SUCCESS_PAGE = "/index.html";
+    private static final String ACCOUNT = "account";
+    private static final String PASSWORD = "password";
+    private static final String EMAIL = "email";
 
     public RegisterController() {
         List<Handler> handlers = List.of(
-                Handler.ofPost(REGISTER_PATH, this::doRegisterPost),
-                Handler.ofGet(REGISTER_PATH, this::doRegisterGet)
+                Handler.ofPost(REGISTER_PATH, SafeExecutionWrapper.withExceptionHandling(this::doRegisterPost)),
+                Handler.ofGet(REGISTER_PATH, SafeExecutionWrapper.withExceptionHandling(this::doRegisterGet))
         );
         registerHandlers(handlers);
     }
@@ -34,14 +38,15 @@ public class RegisterController extends AbstractController {
     }
 
     private void doRegisterPost(HttpRequest request, HttpResponse response) {
-        Queries queries = Queries.of(request.getBody());
-        String account = queries.get("account");
-        User user = new User(account, queries.get("password"), queries.get("email"));
+        String account = request.getBodyParameter(ACCOUNT);
+        String password = request.getBodyParameter(PASSWORD);
+        String email = request.getBodyParameter(EMAIL);
+        User user = new User(account, password, email);
 
         validateRegisterAccount(account);
         InMemoryUserRepository.save(user);
 
-        response.redirectTo("/index.html");
+        response.redirectTo(REGISTER_SUCCESS_PAGE);
     }
 
     private void validateRegisterAccount(String account) {

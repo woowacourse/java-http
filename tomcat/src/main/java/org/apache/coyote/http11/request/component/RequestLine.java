e1d1b46b82a69aa386ea72ba3348e05467aa41b8
BurningFalls <sherlocky31415@gmail.com>
2024-09-12 10:00:13 +0900

    feature: RequestLine 객체화

---
+++ tomcat/src/main/java/org/apache/coyote/http11/request/component/RequestLine.java
@@ -0,0 +1,57 @@
+package org.apache.coyote.http11.request.component;
+
+import java.util.List;
+import java.util.Objects;
+
+public class RequestLine {
+    private static final int ELEMENTS_COUNT = 3;
+    private static final String ELEMENT_DELIMITER = " ";
+    private static final int METHOD_IDX = 0;
+    private static final int REQUEST_URI_IDX = 1;
+    private static final int VERSION_IDX = 2;
+
+    private final String method;
+    private final RequestUri requestUri;
+    private final String version;
+
+    public RequestLine(String inputRequestLine) {
+        validate(inputRequestLine);
+        List<String> elements = List.of(inputRequestLine.split(ELEMENT_DELIMITER));
+        this.method = elements.get(METHOD_IDX);
+        this.requestUri = new RequestUri(elements.get(REQUEST_URI_IDX));
+        this.version = elements.get(VERSION_IDX);
+    }
+
+    private void validate(String inputRequestLine) {
+        checkNotNullOrBlank(inputRequestLine);
+        checkElementsCount(inputRequestLine);
+    }
+
+    private void checkNotNullOrBlank(String inputRequestLine) {
+        if (Objects.isNull(inputRequestLine) || inputRequestLine.isBlank()) {
+            throw new IllegalArgumentException("Request Line은 비어있을 수 없습니다.");
+        }
+    }
+
+    private void checkElementsCount(String inputRequestLine) {
+        if (inputRequestLine.split(" ").length != ELEMENTS_COUNT) {
+            throw new IllegalArgumentException("Request Line 형식이 올바르지 않습니다. 형식은 'METHOD PATH PROTOCOL_VERSION'이어야 합니다.");
+        }
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getRequestUri() {
+        return requestUri.getRequestUri();
+    }
+
+    public String getRequestUrl() {
+        return requestUri.getRequestUrl();
+    }
+
+    public String getExtension() {
+        return requestUri.getExtension();
+    }
+}
---
+++ tomcat/src/main/java/org/apache/coyote/http11/request/component/RequestUri.java
@@ -0,0 +1,51 @@
+package org.apache.coyote.http11.request.component;
+
+import java.util.Arrays;
+
+public class RequestUri {
+    private static final String ROOT = "/";
+    private static final String DOT = ".";
+    private static final String DELIMITER = "\\.";
+    private static final String DEFAULT_EXTENSION = ".html";
+    private static final String DEFAULT_MIME_TYPE = "html";
+
+    private final String requestUri;
+
+    public RequestUri(String inputRequestUri) {
+        this.requestUri = makeToFileUri(inputRequestUri);
+    }
+
+    private String makeToFileUri(String uri) {
+        if (isRoot(uri) || containsDot(uri)) {
+            return uri;
+        }
+        return uri + DEFAULT_EXTENSION;
+    }
+
+    private boolean isRoot(String uri) {
+        return uri.equals(ROOT);
+    }
+
+    private boolean containsDot(String uri) {
+        return uri.contains(DOT);
+    }
+
+    public String getRequestUri() {
+        return requestUri;
+    }
+
+    public String getRequestUrl() {
+        return Arrays.asList(requestUri.split(DELIMITER)).getFirst();
+    }
+
+    private boolean isRoot() {
+        return requestUri.equals(ROOT);
+    }
+
+    public String getExtension() {
+        if (isRoot()) {
+            return DEFAULT_MIME_TYPE;
+        }
+        return Arrays.asList(requestUri.split("\\.")).getLast();
+    }
+}

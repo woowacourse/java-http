dbf31f5951a20a414e4165f57e01f608ad177e17
ay-eonii <naayen@naver.com>
2024-09-12 13:51:57 +0900

    refactor: 응답 생성 StringJoiner 사용하도록 변경

--- tomcat/src/main/java/org/apache/catalina/response/ResponseParser.java
+++ tomcat/src/main/java/org/apache/catalina/response/ResponseParser.java
@@ -1,31 +1,35 @@
 package org.apache.catalina.response;
 
 import java.util.Map;
+import java.util.StringJoiner;
 
 public class ResponseParser {
 
-    private static final String HEADER_FORMAT = "%s: %s \r\n";
+    private static final String STATUS_LINE_FORMAT = "%s %d %s";
+    private static final String HEADER_FORMAT = "%s: %s";
+    private static final String LINE_SEPARATOR = "\r\n";
 
     public String parse(HttpResponse response) {
-        StringBuilder stringBuilder = new StringBuilder();
+        StringJoiner responseJoiner = new StringJoiner(" " + LINE_SEPARATOR);
         StatusLine statusLine = response.getStatusLine();
-        stringBuilder.append(
-                String.format("%s %d %s \r\n",
-                        statusLine.getProtocolVersion(),
-                        statusLine.getStatusCode(),
-                        statusLine.getStatusMessage())
-        );
+        responseJoiner.add(String.format(STATUS_LINE_FORMAT,
+                statusLine.getProtocolVersion(),
+                statusLine.getStatusCode(),
+                statusLine.getStatusMessage()));
 
         Map<Header, Object> headers = response.getHeaders();
         for (Header header : headers.keySet()) {
-            stringBuilder.append(String.format(HEADER_FORMAT, header.value(), headers.get(header)));
+            responseJoiner.add(String.format(HEADER_FORMAT,
+                    header.value(),
+                    headers.get(header)));
         }
 
         String body = response.getBody();
-        stringBuilder.append(Header.CONTENT_LENGTH.value() + body.getBytes().length + " \r\n")
-                .append("\r\n")
-                .append(body);
+        responseJoiner.add(String.format(HEADER_FORMAT,
+                        Header.CONTENT_LENGTH.value(),
+                        body.getBytes().length))
+                .add(LINE_SEPARATOR + body);
 
-        return stringBuilder.toString();
+        return responseJoiner.toString();
     }
 }

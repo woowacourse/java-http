f6c61d90df88b0c9339c4c7e6418498b90f24739
Eugene Jang <kyunellroll@gmail.com>
2024-09-13 00:52:24 +0900

    fix(RequestProcessor): ifPresentOrElse문으로 변경

--- tomcat/src/main/java/org/apache/coyote/http11/RequestProcessor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/RequestProcessor.java
@@ -1,11 +1,12 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.controller.model.Controller;
-import java.util.Optional;
 import org.apache.coyote.request.HttpRequest;
 import org.apache.coyote.request.mapper.RequestMapping;
 import org.apache.coyote.response.HttpResponse;
 import org.apache.coyote.util.HttpStatus;
+
+import java.util.Optional;
 
 public class RequestProcessor {
 
@@ -18,12 +19,7 @@ public class RequestProcessor {
     public void process(HttpRequest httpRequest, HttpResponse httpResponse) {
         Optional<Controller> mappedController = requestMapping.findController(httpRequest);
 
-        if (mappedController.isEmpty()) {
-            httpResponse.sendError(HttpStatus.NOT_FOUND);
-        }
-        if (mappedController.isPresent()) {
-            Controller controller = mappedController.get();
-            controller.service(httpRequest, httpResponse);
-        }
+        mappedController.ifPresentOrElse(controller -> controller.service(httpRequest, httpResponse),
+                () -> httpResponse.sendError(HttpStatus.NOT_FOUND));
     }
 }

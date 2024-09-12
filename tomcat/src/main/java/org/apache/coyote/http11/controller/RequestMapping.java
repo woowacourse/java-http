0baefeb8361cbc816fe34744371b12c1dbf63d71
tackyu <tack970913@gmail.com>
2024-09-13 07:22:19 +0900

    feat: 컨트롤러 매퍼 생성

---
+++ tomcat/src/main/java/org/apache/coyote/http11/controller/RequestMapping.java
@@ -0,0 +1,42 @@
+package org.apache.coyote.http11.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.catalina.SessionManager;
+import org.apache.coyote.http11.HttpRequest;
+
+public class RequestMapping {
+
+    public static final String FILE_EXTENSION_DELIMITER = ".";
+    private static final Map<String, Controller> mapper = new HashMap<>();
+    private static final SessionManager sessionManager = SessionManager.getSessionManager();
+
+    static {
+        mapper.put("/", new HomeController());
+        mapper.put("/index", new IndexController());
+        mapper.put("/login", new LoginController(sessionManager));
+        mapper.put("/register", new RegisterController(sessionManager));
+    }
+
+    public Controller getController(HttpRequest request) {
+        String path = request.getPath();
+        int extensionIndex = path.indexOf(FILE_EXTENSION_DELIMITER);
+
+        if (extensionIndex == -1) {
+            validatePath(path);
+            return mapper.get(path);
+        }
+
+        String pathWithoutExtension = path.substring(0, extensionIndex);
+        if (mapper.containsKey(pathWithoutExtension)) {
+            return mapper.get(pathWithoutExtension);
+        }
+        return new ResourceController();
+    }
+
+    private void validatePath(String path) {
+        if (!mapper.containsKey(path)) {
+            throw new IllegalArgumentException("invalid path: " + path);
+        }
+    }
+}

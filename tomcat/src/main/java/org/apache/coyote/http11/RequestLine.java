4d00749e291c63a8f4a7883aa42c5cfc107b63b2
fromitive <fromitive@gmail.com>
2024-09-13 15:22:26 +0900

    style: 불필요한 import문 정리

--- tomcat/src/main/java/org/apache/coyote/http11/RequestLine.java
+++ tomcat/src/main/java/org/apache/coyote/http11/RequestLine.java
@@ -1,23 +1,29 @@
 package org.apache.coyote.http11;
 
+import java.util.List;
 import org.apache.coyote.http11.exception.RequestException;
 
 public class RequestLine {
+
+    private static final int REQUEST_METHOD_INDEX = 0;
+    private static final int REQUEST_URL_INDEX = 1;
+    private static final int REQUEST_VERSION_INDEX = 2;
+    private static final String REQUEST_LINE_DELIMITER = " ";
 
     private final String method;
     private final String url;
     private final String version;
 
     public RequestLine(String rawRequestLine) {
-        String[] requestPart = rawRequestLine.split(" ");
+        List<String> requestPart = List.of(rawRequestLine.split(REQUEST_LINE_DELIMITER));
         validateRequestPartLength(requestPart);
-        this.method = requestPart[0];
-        this.url = requestPart[1];
-        this.version = requestPart[2];
+        this.method = requestPart.get(REQUEST_METHOD_INDEX);
+        this.url = requestPart.get(REQUEST_URL_INDEX);
+        this.version = requestPart.get(REQUEST_VERSION_INDEX);
     }
 
-    private void validateRequestPartLength(String[] requestPart) {
-        if (requestPart.length != 3) {
+    private void validateRequestPartLength(List<String> requestPart) {
+        if (requestPart.size() != 3) {
             throw new RequestException(HttpStatusCode.BAD_REQUEST, "/500.html");
         }
     }

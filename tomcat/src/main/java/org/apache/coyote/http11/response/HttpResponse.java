0979e75a0f19bac81a83ff6b65f54077404805c9
hoyeonyy <highoption@naver.com>
2024-09-13 16:34:20 +0900

    refactor: 상수화

--- tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponse.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponse.java
@@ -8,6 +8,13 @@ import org.apache.coyote.http11.HttpHeader;
 
 public class HttpResponse {
 
+    private static final String comma = "\\.";
+    private static final String LOCATION = "Location";
+    private static final String CRLF = "\r\n";
+    private static final String FOUND = "Found";
+    private static final String CONTENT_TYPE = "Content-Type";
+    private static final String CONTENT_LENGTH = "Content-Length";
+    private static final String TEXT_HTML_CHARSET_UTF_8 = "text/html;charset=utf-8";
     private StatusLine statusLine;
     private HttpHeader httpHeader;
     private String body;
@@ -20,8 +27,8 @@ public class HttpResponse {
     public void setRoot(String protocol) {
         body = "Hello world!";
         statusLine = new StatusLine(protocol, "200", "OK");
-        httpHeader.putHeader("Content-Type", "text/html;charset=utf-8");
-        httpHeader.putHeader("Content-Length", String.valueOf(body.getBytes().length));
+        httpHeader.putHeader(CONTENT_TYPE, TEXT_HTML_CHARSET_UTF_8);
+        httpHeader.putHeader(CONTENT_LENGTH, String.valueOf(body.getBytes().length));
     }
 
     public void setResource(String protocol, String url) throws IOException {
@@ -30,7 +37,7 @@ public class HttpResponse {
         statusLine = new StatusLine(protocol, "200", "OK");
 
         httpHeader.putContentType(getExtension(url));
-        httpHeader.putHeader("Content-Length", String.valueOf(body.getBytes().length));
+        httpHeader.putHeader(CONTENT_LENGTH, String.valueOf(body.getBytes().length));
     }
 
     public void setNotFound(String protocol) throws IOException {
@@ -38,8 +45,8 @@ public class HttpResponse {
         body = new String(Files.readAllBytes(new File(path.getFile()).toPath()));
         statusLine = new StatusLine(protocol, "404", "Not Found");
 
-        httpHeader.putHeader("Content-Type", "text/html;charset=utf-8");
-        httpHeader.putHeader("Content-Length", String.valueOf(body.getBytes().length));
+        httpHeader.putHeader(CONTENT_TYPE, TEXT_HTML_CHARSET_UTF_8);
+        httpHeader.putHeader(CONTENT_LENGTH, String.valueOf(body.getBytes().length));
     }
 
     public void setLoginFail(String protocol) throws IOException {
@@ -47,26 +54,26 @@ public class HttpResponse {
         body = new String(Files.readAllBytes(new File(path.getFile()).toPath()));
         statusLine = new StatusLine(protocol, "500", "Internal Server Error");
 
-        httpHeader.putHeader("Content-Type", "text/html;charset=utf-8");
-        httpHeader.putHeader("Content-Length", String.valueOf(body.getBytes().length));
+        httpHeader.putHeader(CONTENT_TYPE, TEXT_HTML_CHARSET_UTF_8);
+        httpHeader.putHeader(CONTENT_LENGTH, String.valueOf(body.getBytes().length));
     }
 
     public void setLoginPage(String protocol, String url) throws IOException {
         URL path = getClass().getClassLoader().getResource("static" + url + ".html");
         body = new String(Files.readAllBytes(new File(path.getFile()).toPath()));
         statusLine = new StatusLine(protocol, "200", "OK");
-        httpHeader.putHeader("Content-Type", "text/html;charset=utf-8");
-        httpHeader.putHeader("Content-Length", String.valueOf(body.getBytes().length));
+        httpHeader.putHeader(CONTENT_TYPE, TEXT_HTML_CHARSET_UTF_8);
+        httpHeader.putHeader(CONTENT_LENGTH, String.valueOf(body.getBytes().length));
     }
 
     public void successLogin(String protocol) {
-        statusLine = new StatusLine(protocol, "302", "Found");
-        httpHeader.putHeader("Location", "/index.html");
+        statusLine = new StatusLine(protocol, "302", FOUND);
+        httpHeader.putHeader(LOCATION, "/index.html");
     }
 
     public void failLogin(String protocol) {
-        statusLine = new StatusLine(protocol, "302", "Found");
-        httpHeader.putHeader("Location", "/401.html");
+        statusLine = new StatusLine(protocol, "302", FOUND);
+        httpHeader.putHeader(LOCATION, "/401.html");
     }
 
     public void putHeader(String key, String value) {
@@ -74,13 +81,13 @@ public class HttpResponse {
     }
 
     public String getExtension(String url) {
-        String[] types = url.split("\\.");
+        String[] types = url.split(comma);
         return types[1];
     }
 
     @Override
     public String toString() {
-        return String.join("\r\n",
+        return String.join(CRLF,
                 statusLine.getStatusLine(),
                 httpHeader.getHttpHeader(),
                 body);

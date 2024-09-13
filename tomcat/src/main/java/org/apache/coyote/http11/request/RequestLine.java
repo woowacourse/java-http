6aa8b66c160db00fff409b41a2412da77b540d63
J-I-H-O <jeongjiho0731@gmail.com>
2024-09-13 11:32:38 +0900

    refactor: 매직넘버 상수 추출

--- tomcat/src/main/java/org/apache/coyote/http11/request/RequestLine.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/RequestLine.java
@@ -6,33 +6,45 @@ import java.util.Map;
 
 public class RequestLine {
 
+    private static final int METHOD_INDEX = 0;
+    private static final int PATH_INDEX = 1;
+    private static final int QUERY_PARAMETER_ENTRY_SIZE = 2;
+    private static final int QUERY_PARAMETER_KEY_INDEX = 0;
+    private static final int QUERY_PARAMETER_VALUE_INDEX = 1;
+
     private String method;
     private String path;
     private Map<String, String> queryParameters;
 
     public static RequestLine create(String requestLine) {
         String[] splitRequestLine = requestLine.split(" ");
-        String method = splitRequestLine[0];
-        String path = splitRequestLine[1];
+        String method = splitRequestLine[METHOD_INDEX];
+        String path = splitRequestLine[PATH_INDEX];
 
-        String inputQueryString = "";
+        String queryString = "";
         int queryStringIndex = requestLine.indexOf("?");
         if (queryStringIndex >= 0) {
             path = requestLine.substring(0, queryStringIndex);
-            inputQueryString = requestLine.substring(queryStringIndex)
+            queryString = requestLine.substring(queryStringIndex)
                     .replace("?", "");
         }
-
-        Map<String, String> queryParameters = new HashMap<>();
-        Arrays.stream(inputQueryString.split("&"))
-                .forEach(queryParameterEntry -> {
-                    String[] split = queryParameterEntry.split("=");
-                    if (split.length == 2 && !split[0].isBlank() && !split[1].isBlank()) {
-                        queryParameters.put(split[0], split[1]);
-                    }
-                });
+        Map<String, String> queryParameters = parseQueryParameters(queryString);
 
         return new RequestLine(method, path, queryParameters);
+    }
+
+    private static Map<String, String> parseQueryParameters(String queryString) {
+        Map<String, String> queryParameters = new HashMap<>();
+        Arrays.stream(queryString.split("&"))
+                .forEach(queryParameterEntry -> {
+                    String[] split = queryParameterEntry.split("=");
+                    if (split.length == QUERY_PARAMETER_ENTRY_SIZE
+                            && !split[QUERY_PARAMETER_KEY_INDEX].isBlank()
+                            && !split[QUERY_PARAMETER_VALUE_INDEX].isBlank()) {
+                        queryParameters.put(split[QUERY_PARAMETER_KEY_INDEX], split[QUERY_PARAMETER_VALUE_INDEX]);
+                    }
+                });
+        return queryParameters;
     }
 
     private RequestLine(String method, String path, Map<String, String> queryParameters) {

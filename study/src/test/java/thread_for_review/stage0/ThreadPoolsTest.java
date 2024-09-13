72f10cf8ee338b9445eb11c6889abfae407a69bb
reddevilmidzy <midzy0228@gmail.com>
2024-09-14 00:10:01 +0900

    학습 테스트

---
+++ study/src/test/java/thread_for_review/stage0/ThreadPoolsTest.java
@@ -0,0 +1,69 @@
+package thread_for_review.stage0;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 스레드 풀은 무엇이고 어떻게 동작할까? 테스트를 통과시키고 왜 해당 결과가 나왔는지 생각해보자.
+ * <p>
+ * Thread Pools https://docs.oracle.com/javase/tutorial/essential/concurrency/pools.html
+ * <p>
+ * Introduction to Thread Pools in Java https://www.baeldung.com/thread-pool-java-and-guava
+ */
+class ThreadPoolsTest {
+
+    private static final Logger log = LoggerFactory.getLogger(ThreadPoolsTest.class);
+
+    @Test
+    void testNewFixedThreadPool() {
+        final var executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
+        executor.submit(logWithSleep("hello fixed thread pools"));
+        executor.submit(logWithSleep("hello fixed thread pools"));
+        executor.submit(logWithSleep("hello fixed thread pools"));
+
+        // 올바른 값으로 바꿔서 테스트를 통과시키자.
+        final int expectedPoolSize = 2;
+        final int expectedQueueSize = 1;
+
+        assertAll(
+                () -> assertThat(executor.getPoolSize()).isEqualTo(expectedPoolSize),
+                () -> assertThat(executor.getQueue()).hasSize(expectedQueueSize)
+        );
+    }
+
+    @Test
+    void testNewCachedThreadPool() {
+        final var executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
+        executor.submit(logWithSleep("hello cached thread pools"));
+        executor.submit(logWithSleep("hello cached thread pools"));
+        executor.submit(logWithSleep("hello cached thread pools"));
+
+        // 올바른 값으로 바꿔서 테스트를 통과시키자.
+        final int expectedPoolSize = 3;
+        final int expectedQueueSize = 0;
+
+        assertAll(
+                () -> assertThat(executor.getPoolSize()).isEqualTo(expectedPoolSize),
+                () -> assertThat(executor.getQueue()).hasSize(expectedQueueSize)
+        );
+
+    }
+
+    private Runnable logWithSleep(final String message) {
+        return () -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            log.info(message);
+        };
+    }
+}

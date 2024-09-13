36ac33dead5d2c51cf2ea15e841103d9406b7b58
pricelees <priceelees@gmail.com>
2024-09-13 17:21:10 +0900

    feat: SessionManager의 Map 타입 변경 (-> ConcurrentHashMap)

--- tomcat/src/main/java/org/apache/coyote/http11/http/session/SessionManager.java
+++ tomcat/src/main/java/org/apache/coyote/http11/http/session/SessionManager.java
@@ -1,13 +1,13 @@
 package org.apache.coyote.http11.http.session;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import com.techcourse.model.User;
 
 public class SessionManager {
 
-	private static final Map<String, Session> sessions = new HashMap<>();
+	private static final Map<String, Session> sessions = new ConcurrentHashMap<>();
 
 	public static void createSession(String id, User user) {
 		sessions.put(id, new Session(id, user));

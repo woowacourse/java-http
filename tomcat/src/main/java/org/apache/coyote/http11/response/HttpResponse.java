2b6e43624c840425a68d5d8830a911a93496a0ae
J-I-H-O <jeongjiho0731@gmail.com>
2024-09-13 14:47:19 +0900

    refactor: 메서드 분리 및 상수 추출

--- tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponse.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponse.java
@@ -9,22 +9,20 @@ import java.util.Map;
 
 public class HttpResponse {
 
-    private String statusCode = "200 OK";
-    private Map<String, String> headers = new LinkedHashMap<>();
-    private String body = "";
+    private static final String DEFAULT_STATUS_CODE = "200 OK";
+    private static final String DEFAULT_RESPONSE_BODY = "";
+    public static final String FILE_EXTENSION_DELIMITER = ".";
 
-    public void setStatusCode(String statusCode) {
-        this.statusCode = statusCode;
-    }
+    private Map<String, String> headers = new LinkedHashMap<>();
+    private String statusCode = DEFAULT_STATUS_CODE;
+    private String body = DEFAULT_RESPONSE_BODY;
 
     public void addHeader(String key, String value) {
         headers.put(key, value);
     }
 
-    public void setBodyWithContentType(String body, String contentType) {
-        this.body = body;
-        this.headers.put("Content-Type", contentType);
-        this.headers.put("Content-Length", String.valueOf(body.getBytes().length));
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
     }
 
     public void setBodyWithStaticResource(String filePath) {
@@ -33,28 +31,35 @@ public class HttpResponse {
             if (resource == null) {
                 throw new IOException("존재하지 않는 파일입니다. : " + filePath);
             }
+
             byte[] fileContents = Files.readAllBytes(new File(resource.getFile()).toPath());
-
-            String fileExtension = "html";
-            int fileExtensionIndex = filePath.lastIndexOf(".");
-            if (fileExtensionIndex >= 0) {
-                fileExtension = filePath.substring(fileExtensionIndex)
-                        .replace(".", "");
-            }
-
-            setBodyWithContentType(
-                    new String(fileContents),
-                    String.format("%s%s%s", "text/", fileExtension, ";charset=utf-8")
-            );
+            String fileExtension = getFileExtension(filePath);
+            
+            setBodyWithContentType(new String(fileContents), "text/" + fileExtension + ";charset=utf-8");
         } catch (IOException e) {
             this.statusCode = "500 Internal Server Error";
         }
     }
 
+    public void setBodyWithContentType(String body, String contentType) {
+        this.body = body;
+        this.headers.put("Content-Type", contentType);
+        this.headers.put("Content-Length", String.valueOf(body.getBytes().length));
+    }
+
+    private String getFileExtension(String filePath) {
+        String fileExtension = "html";
+        int fileExtensionIndex = filePath.lastIndexOf(FILE_EXTENSION_DELIMITER);
+        if (fileExtensionIndex >= 0) {
+            fileExtension = filePath.substring(fileExtensionIndex)
+                    .replace(FILE_EXTENSION_DELIMITER, "");
+        }
+        return fileExtension;
+    }
+
     @Override
     public String toString() {
-        // TODO: HTTP 버전도 request와 통일시키기
-        String statusLine = String.format("%s %s ", "HTTP/1.1", this.statusCode);
+        String statusLine = "HTTP/1.1" + this.statusCode;
         StringBuilder headerBuilder = new StringBuilder();
         headers.entrySet()
                 .stream()

48e094303a3c0cb485648132735015d392853f62
donghoony <aru0504@naver.com>
2024-09-13 15:08:02 +0900

    fix: ProcessorTest 통과하도록 수정

--- tomcat/src/main/java/com/techcourse/config/ServletContextConfiguration.java
+++ tomcat/src/main/java/com/techcourse/config/ServletContextConfiguration.java
@@ -1,5 +1,6 @@
 package com.techcourse.config;
 
+import com.techcourse.servlet.IndexRedirectServlet;
 import com.techcourse.servlet.LoginServlet;
 import com.techcourse.servlet.RegisterServlet;
 import org.apache.catalina.servlet.ServletContext;
@@ -21,6 +22,7 @@ public class ServletContextConfiguration {
 
     private static void buildContext() {
         ServletContext context = new ServletContext();
+        context.addServlet("/", new IndexRedirectServlet());
         context.addServlet("/login", new LoginServlet());
         context.addServlet("/register", new RegisterServlet());
         servletContext = context;

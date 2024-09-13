04789f0549496a733bd15fc23224e74f75961304
임수빈 <robin980108@naver.com>
2024-09-13 13:20:26 +0900

    fix: 스레드 풀 최대 개수 250으로 설정

--- tomcat/src/main/java/org/apache/catalina/startup/Tomcat.java
+++ tomcat/src/main/java/org/apache/catalina/startup/Tomcat.java
@@ -11,7 +11,7 @@ public class Tomcat {
     private static final Logger log = LoggerFactory.getLogger(Tomcat.class);
 
     public void start(RequestMappings requestMappings) {
-        var connector = new Connector(requestMappings, 10);
+        var connector = new Connector(requestMappings, 250);
         connector.start();
 
         try {

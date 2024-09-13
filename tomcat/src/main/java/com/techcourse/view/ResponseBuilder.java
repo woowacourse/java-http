216bc8b7dd217a9c14401dfefbe546a19113a7a8
nayonsoso <panda0329@naver.com>
2024-09-13 10:50:47 +0900

    refactor: 응답 객체 수정

--- tomcat/src/main/java/com/techcourse/view/ResponseBuilder.java
+++ tomcat/src/main/java/com/techcourse/view/ResponseBuilder.java
@@ -4,37 +4,23 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import org.apache.catalina.exception.TomcatException;
+import org.apache.coyote.http11.response.HttpStatusCode;
 
-class ResponseBuilder {
+public class ResponseBuilder {
 
-    String buildSuccessfulResponse(String responseBody) {
-        return this.buildSuccessfulResponse("text/html", responseBody);
-    }
+    public String buildExceptionResponse(TomcatException e) throws IOException {
+        HttpStatusCode httpStatusCode = e.getHttpStatusCode();
+        int statusCode = httpStatusCode.getStatusCode();
 
-    String buildSuccessfulResponse(String contentType, String responseBody) {
-        return String.join("\r\n",
-                "HTTP/1.1 200 OK ",
-                "Content-Type: " + contentType + ";charset=utf-8 ",
-                "Content-Length: " + responseBody.getBytes().length + " ",
+        URL url = getClass().getClassLoader().getResource("static/" + statusCode + ".html");
+        String responseBody = new String(Files.readAllBytes(new File(url.getFile()).toPath()));
+
+        return String.join(System.lineSeparator(),
+                "HTTP/1.1 " + statusCode + " " + httpStatusCode.getStatusMessage(),
+                "Content-Type: text/html;charset=utf-8",
+                "Content-Length: " + responseBody.getBytes().length,
                 "",
                 responseBody);
-    }
-
-    String buildNotFoundResponse() throws IOException {
-        URL badRequestURL = getClass().getClassLoader().getResource("static/404.html");
-        String responseBody = new String(Files.readAllBytes(new File(badRequestURL.getFile()).toPath()));
-
-        return String.join("\r\n",
-                "HTTP/1.1 404 NOT_FOUND ",
-                "Content-Type: text/html;charset=utf-8 ",
-                "Content-Length: " + responseBody.getBytes().length + " ",
-                "",
-                responseBody);
-    }
-
-    String buildRedirectResponse(String location) {
-        return String.join("\r\n",
-                "HTTP/1.1 302 FOUND ",
-                "Location: " + location);
     }
 }

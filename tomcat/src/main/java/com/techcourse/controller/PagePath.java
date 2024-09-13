bdcc037bc22f4e95bdcc35b8d2c0aa2a1af9be93
KIMGYUTAE <kiimwf0819@gmail.com>
2024-09-13 11:12:30 +0900

    refactor: FrontController 응답 페이지가 정상 노출되도록 변경

--- tomcat/src/main/java/com/techcourse/controller/PagePath.java
+++ tomcat/src/main/java/com/techcourse/controller/PagePath.java
@@ -6,7 +6,8 @@ public enum PagePath {
     REGISTER_PAGE("/register.html"),
     LOGIN_PAGE("/login.html"),
     UNAUTHORIZED_PAGE("/401.html"),
-    NOT_FOUND_PAGE("/404.html")
+    NOT_FOUND_PAGE("/404.html"),
+    INTERNAL_SERVER_ERROR_PAGE("/500.html")
     ;
 
     private final String path;
--- tomcat/src/main/java/org/was/controller/FrontController.java
+++ tomcat/src/main/java/org/was/controller/FrontController.java
@@ -1,7 +1,12 @@
 package org.was.controller;
 
+import static com.techcourse.controller.PagePath.INTERNAL_SERVER_ERROR_PAGE;
+import static com.techcourse.controller.PagePath.NOT_FOUND_PAGE;
+import static org.apache.coyote.http11.HttpStatusCode.INTERNAL_SERVER_ERROR;
+import static org.apache.coyote.http11.HttpStatusCode.METHOD_NOT_ALLOWED;
+import static org.apache.coyote.http11.HttpStatusCode.NOT_FOUND;
+
 import java.io.IOException;
-import org.apache.coyote.http11.HttpStatusCode;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.response.HttpResponse;
 import org.was.controller.exception.ControllerNotFoundException;
@@ -24,20 +29,38 @@ public class FrontController {
         return INSTANCE;
     }
 
-    public void service(HttpRequest request, HttpResponse response) {
+    public void service(HttpRequest request, HttpResponse response) throws IOException {
         try {
             Controller controller = requestMapping.getController(request);
             ResponseResult responseResult = controller.service(request);
             responseConverter.convert(response, viewResolver, responseResult);
 
         } catch (ControllerNotFoundException e) {
-            responseConverter.convert(response, HttpStatusCode.NOT_FOUND, "/404.html");
+            responseNotFound(response);
 
         } catch (MethodNotAllowedException e) {
-            responseConverter.convert(response, HttpStatusCode.METHOD_NOT_ALLOWED, e.getMessage());
+            responseMethodNotAllowed(response, e);
 
         } catch (IOException e) {
-            responseConverter.convert(response, HttpStatusCode.INTERNAL_SERVER_ERROR, "/500.html");
+            responseInternalServerError(response);
         }
+    }
+
+    private void responseNotFound(HttpResponse response) throws IOException {
+        ResponseResult responseResult = ResponseResult
+                .status(NOT_FOUND)
+                .path(NOT_FOUND_PAGE.getPath());
+        responseConverter.convert(response, viewResolver, responseResult);
+    }
+
+    private void responseMethodNotAllowed(HttpResponse response, MethodNotAllowedException e) {
+        responseConverter.convert(response, METHOD_NOT_ALLOWED, e.getMessage());
+    }
+
+    private void responseInternalServerError(HttpResponse response) throws IOException {
+        ResponseResult responseResult = ResponseResult
+                .status(INTERNAL_SERVER_ERROR)
+                .path(INTERNAL_SERVER_ERROR_PAGE.getPath());
+        responseConverter.convert(response, viewResolver, responseResult);
     }
 }

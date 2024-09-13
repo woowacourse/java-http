546ce126e0b4334a280ae9494f60cce6a893397e
fram1998 <koust6u@pusan.ac.kr>
2024-09-13 15:29:26 +0900

    test: 테스트 케이스 추가

--- tomcat/src/test/java/org/apache/catalina/servlet/LoginControllerTest.java
+++ tomcat/src/test/java/org/apache/catalina/servlet/LoginControllerTest.java
@@ -1,10 +1,12 @@
 package org.apache.catalina.servlet;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.apache.catalina.servlet.controller.LoginController;
 import org.apache.support.Dummy;
 import org.apache.tomcat.http.common.body.FormUrlEncodeBody;
+import org.apache.tomcat.http.exception.AuthenticationException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -26,4 +28,35 @@ class LoginControllerTest {
         assertThat(response.getHeaderContent("Set-Cookie")).isNotBlank();
     }
 
+    @Test
+    @DisplayName("로그인이 된 상태로 GET 요청을 하면 index.html을 반환한다.")
+    void return_index_dot_html_when_called_get_request_with_valid_jsession_id() {
+        // given
+        final var controller = new LoginController();
+        final var request = Dummy.getHttpRequestBody(
+                new FormUrlEncodeBody("account=gugu&password=password"));
+        final var response = Dummy.getHttpResponse();
+        controller.doPost(request, response);
+        final var cookie = response.getHeaderContent("Set-Cookie");
+
+        // when
+        controller.doGet(Dummy.getHttpRequestCookie(cookie), response);
+
+        // then
+        assertThat(response.getHeaderContent("Location")).isEqualTo("http://localhost:8080/index.html");
+    }
+
+    @Test
+    @DisplayName("로그인 되지 않은 상태에서 GET요청을 할시 예외를 던진다.")
+    void throw_exception_when_called_get_request_without_valid_jsession_id() {
+        // given
+        final var controller = new LoginController();
+        final var request = Dummy.getHttpRequestBody(
+                new FormUrlEncodeBody("account=gugu&password=password1"));
+        final var response = Dummy.getHttpResponse();
+
+        // when & then
+        assertThatThrownBy(() -> controller.doPost(request, response))
+                .isInstanceOf(AuthenticationException.class);
+    }
 }
--- tomcat/src/test/java/org/apache/support/Dummy.java
+++ tomcat/src/test/java/org/apache/support/Dummy.java
@@ -33,6 +33,14 @@ public class Dummy {
         return new HttpRequest(requestLine, requestHeaders, textTypeBody, cookie);
     }
 
+    public static HttpRequest getHttpRequestCookie(final String cookieValue) {
+        final var requestLine = new RequestLine(Method.GET, URI.create("/"), new Version(1, 1));
+        final var requestHeaders = new RequestHeaders("");
+        final var textTypeBody = new TextTypeBody("");
+        final var cookie = new Cookie(cookieValue);
+        return new HttpRequest(requestLine, requestHeaders, textTypeBody, cookie);
+    }
+
 
     public static HttpRequest getHttpRequestBody(final Body body) {
         final var requestLine = new RequestLine(Method.GET, URI.create("/"), new Version(1, 1));

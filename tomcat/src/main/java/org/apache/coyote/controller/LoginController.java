848f767a3afbb8c96fe5eea320d3105ffa55b9ed
hyeonjilee <skylar12200@gmail.com>
2024-09-13 14:43:31 +0900

    refactor: 주석 제거

--- tomcat/src/main/java/org/apache/coyote/controller/LoginController.java
+++ tomcat/src/main/java/org/apache/coyote/controller/LoginController.java
@@ -47,33 +47,4 @@ public class LoginController extends AbstractController {
             response.setBody("/login.html");
         }
     }
-//
-//    private static void login(HttpRequest request, HttpResponse response, SessionManager sessionManager) {
-//        String account = request.getQueryParam("account");
-//        String password = request.getQueryParam("password");
-//        Optional<User> user = InMemoryUserRepository.findByAccount(account);
-//
-//        if (user.isEmpty() || !user.get().checkPassword(password)) {
-//            response.setStatusCode(StatusCode.UNAUTHORIZED);
-//            response.setBody("/401.html");
-//        }
-//        if (user.isPresent() && user.get().checkPassword(password)) {
-//            response.setStatusCode(StatusCode.FOUND);
-//            response.setBody("/index.html");
-//
-//            String sessionId = sessionManager.generateSession(user.get());
-//            response.addHeader(HeaderName.SET_COOKIE, sessionId);
-//        }
-//    }
-//
-//    private static void showLogin(HttpRequest request, HttpResponse response, SessionManager sessionManager) {
-//        if (request.hasSession() && sessionManager.isSessionExist(request.getSessionId())) {
-//            response.setStatusCode(StatusCode.FOUND); //
-//            response.addHeader(HeaderName.LOCATION, "/index.html");
-//        }
-//        if (!request.hasSession() || !sessionManager.isSessionExist(request.getSessionId())) {
-//            response.setStatusCode(StatusCode.OK);
-//            response.setBody("/login.html");
-//        }
-//    }
 }

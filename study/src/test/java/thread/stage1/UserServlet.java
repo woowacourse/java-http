edeb6a77965f705f85e25a6291e07add8ec1c0c4
slimsha2dy <chy2581@naver.com>
2024-09-13 12:41:44 +0900

    feat: Thread 학습 테스트 코드 추가

--- study/src/test/java/thread/stage1/UserServlet.java
+++ study/src/test/java/thread/stage1/UserServlet.java
@@ -1,17 +1,17 @@
 package thread.stage1;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class UserServlet {
 
-    private final List<User> users = new ArrayList<>();
+    private final List<User> users = new CopyOnWriteArrayList<>();
 
     public void service(final User user) {
         join(user);
     }
 
-    private void join(final User user) {
+    private synchronized void join(final User user) {
         if (!users.contains(user)) {
             users.add(user);
         }

52439a2dff844a1c757ef850fa87f16b0a546186
juha <khabh@naver.com>
2024-09-13 14:47:56 +0900

    refactor: HttpEndpoint에서 parameters 제거

--- tomcat/src/main/java/org/apache/catalina/controller/HttpEndpoint.java
+++ tomcat/src/main/java/org/apache/catalina/controller/HttpEndpoint.java
@@ -1,7 +1,6 @@
 package org.apache.catalina.controller;
 
 import java.util.Objects;
-import java.util.Set;
 import org.apache.coyote.http11.request.HttpMethod;
 import org.apache.coyote.http11.request.HttpRequest;
 
@@ -10,23 +9,16 @@ public class HttpEndpoint {
 
     private final String path;
     private final HttpMethod method;
-    private final Set<String> parameters;
 
     public HttpEndpoint(String path, HttpMethod method) {
-        this(path, method, Set.of());
-    }
-
-    public HttpEndpoint(String path, HttpMethod method, Set<String> parameters) {
         this.path = path;
         this.method = method;
-        this.parameters = parameters;
     }
 
     public static HttpEndpoint of(HttpRequest httpRequest) {
         return new HttpEndpoint(
                 httpRequest.getPath(),
-                httpRequest.getMethod(),
-                httpRequest.getQueryKeys()
+                httpRequest.getMethod()
         );
     }
 
@@ -44,13 +36,12 @@ public class HttpEndpoint {
             return false;
         }
         HttpEndpoint endpoint = (HttpEndpoint) o;
-        return Objects.equals(path, endpoint.path) && method == endpoint.method && Objects.equals(
-                parameters, endpoint.parameters);
+        return Objects.equals(path, endpoint.path) && method == endpoint.method;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(path, method, parameters);
+        return Objects.hash(path, method);
     }
 }
 

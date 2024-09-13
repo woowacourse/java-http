1a5b1c65bd47359329ac44401412838e80373636
nayonsoso <panda0329@naver.com>
2024-09-13 10:51:21 +0900

    refactor: 응답 헤더가 순서를 가지고 있도록 설정

--- tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponse.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponse.java
@@ -1,12 +1,12 @@
 package org.apache.coyote.http11.response;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class HttpResponse {
 
     private HttpStatusCode statusCode;
-    private final Map<String, String> headers = new HashMap<>();
+    private final Map<String, String> headers = new LinkedHashMap<>();
     private String body;
 
     public HttpResponse() {
@@ -29,7 +29,15 @@ public class HttpResponse {
     }
 
     public void setContentType(String contentType) {
+        if("text/html".equals(contentType)) {
+            this.headers.put("Content-Type", contentType + ";charset=utf-8");
+            return;
+        }
         this.headers.put("Content-Type", contentType);
+    }
+
+    public void setContentLength() {
+        this.headers.put("Content-Length", String.valueOf(body.length()));
     }
 
     public void setBody(String body) {

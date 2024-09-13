02d851d1cdd671a63a96e743784f7b13a2a5c766
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-09-13 13:54:58 +0900

    test: test 수정

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -24,7 +24,7 @@ class Http11ProcessorTest {
         var expected = String.join("\r\n",
                 "HTTP/1.1 200 OK ",
                 "Content-Type: text/html;charset=utf-8 ",
-                "Content-Length: 12 ",
+                "Content-Length: 12",
                 "",
                 "Hello world!");
 
@@ -37,7 +37,7 @@ class Http11ProcessorTest {
         final String httpRequest= String.join("\r\n",
                 "GET /index.html HTTP/1.1 ",
                 "Host: localhost:8080 ",
-                "Connection: keep-alive ",
+                "Connection: keep-alive",
                 "",
                 "");
 
@@ -50,7 +50,7 @@ class Http11ProcessorTest {
         // then
         final URL resource = getClass().getClassLoader().getResource("static/index.html");
         var expected = "HTTP/1.1 200 OK \r\n" +
-                "Content-Type: text/html;charset=utf-8 \r\n" +
+                "Content-Type: text/html;charset=utf-8\r\n" +
                 "Content-Length: 5564\r\n" +
                 "\r\n"+
                 new String(Files.readAllBytes(new File(resource.getFile()).toPath()));

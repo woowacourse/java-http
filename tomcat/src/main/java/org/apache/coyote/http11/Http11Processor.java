1bac966ad8fe272d4d0ed4441743ce4b4150d8ab
slimsha2dy <chy2581@naver.com>
2024-09-13 15:50:21 +0900

    refactor: 불필요한 코드 삭제

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -6,11 +6,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import org.apache.catalina.servlet.DispatcherServlet;
-import org.apache.catalina.session.SessionManager;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,13 +17,11 @@ public class Http11Processor implements Runnable, Processor {
 
     private final Socket connection;
     private final DispatcherServlet dispatcherServlet;
-    private final SessionManager sessionManager;
     private final HttpRequestParser requestParser;
 
     public Http11Processor(final Socket connection) {
         this.connection = connection;
         this.dispatcherServlet = new DispatcherServlet();
-        this.sessionManager = SessionManager.getInstance();
         this.requestParser = HttpRequestParser.getInstance();
     }
 
@@ -45,49 +39,10 @@ public class Http11Processor implements Runnable, Processor {
             HttpResponse response = new HttpResponse();
 
             dispatcherServlet.requestMapping(request, response);
-//            String resourceName = response.getResourceName();
-//            if (resourceName == null) {
-//                resourceName = request.getPath();
-//                response.setResourceName(resourceName);
-//            }
-//            String responseBody = getResource(resourceName);
-//            String resourceExtension = getExtension(resourceName);
-//
-//            if (!request.hasCookieFrom("JSESSIONID")) {
-//                String sessionId = UUID.randomUUID().toString();
-//                response.setHttpCookie(HttpCookie.ofJSessionId(sessionId));
-//            }
-//            response.setResponseBody(responseBody);
-//            response.setContentType(resourceExtension);
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
-        }
-    }
-
-    private String getExtension(String resourceName) {
-        if (resourceName == null) {
-            return "html";
-        }
-        int extensionIndex = resourceName.indexOf('.') + 1;
-        if (extensionIndex == 0) {
-            return "html";
-        }
-        return resourceName.substring(extensionIndex);
-    }
-
-    private String getResource(String resourceName) throws URISyntaxException, IOException {
-        URL resource = getClass().getClassLoader()
-                .getResource("static" + resourceName);
-
-        if (resource == null) {
-            return "Hello world!";
-        }
-        try {
-            return Files.readString(Paths.get(resource.toURI()));
-        } catch (IOException e) {
-            return "Hello world!";
         }
     }
 }

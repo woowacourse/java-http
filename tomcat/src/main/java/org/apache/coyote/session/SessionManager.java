f150a88bc9b7db14df6342fb732037ab3d04c47f
yoonseo choi <choiyoonseo02@gmail.com>
2024-09-13 16:55:46 +0900

    feat: 학습테스트 및 스레드풀, 동기화 설정

--- tomcat/src/main/java/org/apache/coyote/session/SessionManager.java
+++ tomcat/src/main/java/org/apache/coyote/session/SessionManager.java
@@ -1,12 +1,12 @@
 package org.apache.coyote.session;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import org.apache.catalina.Manager;
 
 public class SessionManager implements Manager {
 
-    private static final Map<String, Session> SESSIONS = new HashMap<>();
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
     private static final SessionManager INSTANCE = new SessionManager();
 
     private SessionManager() {

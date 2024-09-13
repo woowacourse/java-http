4d181ab9ca7b705cdca9994ce24222eb4cfe51c6
kunsanglee <gosmdochee2@gmail.com>
2024-09-13 16:53:38 +0900

    feat: Thread 0단계 학습 테스트 완료

--- study/src/test/java/thread/stage0/SynchronizationTest.java
+++ study/src/test/java/thread/stage0/SynchronizationTest.java
@@ -1,12 +1,11 @@
 package thread.stage0;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
 
 /**
  * 다중 스레드 환경에서 두 개 이상의 스레드가 변경 가능한(mutable) 공유 데이터를 동시에 업데이트하면 경쟁 조건(race condition)이 발생한다.
@@ -26,12 +25,36 @@ class SynchronizationTest {
      * https://www.baeldung.com/java-synchronized
      */
     @Test
-    void testSynchronized() throws InterruptedException {
+    void testWithoutSynchronized() throws InterruptedException {
         var executorService = Executors.newFixedThreadPool(3);
         var synchronizedMethods = new SynchronizedMethods();
 
         IntStream.range(0, 1000)
                 .forEach(count -> executorService.submit(synchronizedMethods::calculate));
+        executorService.awaitTermination(500, TimeUnit.MILLISECONDS);
+
+        assertThat(synchronizedMethods.getSum()).isNotEqualTo(1000);
+    }
+
+    @Test
+    void testWithSynchronized() throws InterruptedException {
+        var executorService = Executors.newFixedThreadPool(3);
+        var synchronizedMethods = new SynchronizedMethods();
+
+        IntStream.range(0, 1000)
+                .forEach(count -> executorService.submit(synchronizedMethods::calculateWithSynchronizedMethod));
+        executorService.awaitTermination(500, TimeUnit.MILLISECONDS);
+
+        assertThat(synchronizedMethods.getSum()).isEqualTo(1000);
+    }
+
+    @Test
+    void testSynchronizedBlockInMethod() throws InterruptedException {
+        var executorService = Executors.newFixedThreadPool(3);
+        var synchronizedMethods = new SynchronizedMethods();
+
+        IntStream.range(0, 1000)
+                .forEach(count -> executorService.submit(synchronizedMethods::calculateWithSynchronizedBlockInMethod));
         executorService.awaitTermination(500, TimeUnit.MILLISECONDS);
 
         assertThat(synchronizedMethods.getSum()).isEqualTo(1000);
@@ -43,6 +66,28 @@ class SynchronizationTest {
 
         public void calculate() {
             setSum(getSum() + 1);
+        }
+
+        /**
+         * 인스턴스 메서드는 메서드를 소유한 클래스의 인스턴스를 통해 동기화 됩니다.
+         * 즉, 클래스 인스턴스당 하나의 스레드만 이 메서드를 실행할 수 있습니다.
+         */
+        public synchronized void calculateWithSynchronizedMethod() {
+            setSum(getSum() + 1);
+        }
+
+        /**
+         * 때로는 전체 메서드를 동기화하지 않고, 그 안의 일부 명령어만 동기화하고 싶을 때가 있습니다.
+         * synchronized를 블록에 적용 하면 이를 달성할 수 있습니다.
+         * 동기화된 블록 에 매개변수 this를 전달한 것을 주목하세요. 이것은 모니터 객체입니다.
+         * 블록 내부의 코드는 모니터 객체에서 동기화됩니다.
+         * 간단히 말해서, 모니터 객체당 하나의 스레드만 해당 코드 블록 내부에서 실행할 수 있습니다.
+         * 메서드가 정적 이면 객체 참조 대신 클래스 이름을 전달하고 클래스는 블록 동기화를 위한 모니터가 됩니다.
+         */
+        public void calculateWithSynchronizedBlockInMethod() {
+            synchronized (this) {
+                setSum(getSum() + 1);
+            }
         }
 
         public int getSum() {

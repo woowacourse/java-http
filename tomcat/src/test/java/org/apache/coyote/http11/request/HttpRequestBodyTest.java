3a6896535e6e3924c49457349208a7974ce4ff21
HoeSeong123 <ghltjd3@gmail.com>
2024-09-13 17:39:00 +0900

    refactor: 코드 스타일 수정

--- tomcat/src/test/java/org/apache/coyote/http11/request/HttpRequestBodyTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/request/HttpRequestBodyTest.java
@@ -1,7 +1,6 @@
 package org.apache.coyote.http11.request;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;

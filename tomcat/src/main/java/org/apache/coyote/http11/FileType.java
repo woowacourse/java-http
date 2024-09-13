cda1b39dc3631ba72543529202b665c6efe30654
hangillee <skfcb10@naver.com>
2024-09-13 16:59:25 +0900

    refactor: 파일 타입 및 확장자 정보 반환 클래스 리팩토링

--- tomcat/src/main/java/org/apache/coyote/http11/FileType.java
+++ tomcat/src/main/java/org/apache/coyote/http11/FileType.java
@@ -1,17 +1,28 @@
 package org.apache.coyote.http11;
 
+import java.util.Arrays;
+
 public enum FileType {
-    HTML("text/html"),
-    JAVASCRIPT("text/javascript"),
-    CSS("text/css");
+    HTML("text/html", ".html"),
+    JAVASCRIPT("text/javascript", ".js"),
+    CSS("text/css", ".css");
 
     private final String value;
+    private final String extension;
 
-    FileType(String value) {
+    FileType(String value, String extension) {
         this.value = value;
+        this.extension = extension;
     }
 
     public String getValue() {
         return value;
+    }
+
+    public static FileType parseFilenameExtension(final String path) {
+        return Arrays.stream(FileType.values())
+                .filter(fileType -> path.endsWith(fileType.extension))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 파일 확장자입니다."));
     }
 }

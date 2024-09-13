888efa0d41d437a8c767062ae8b068792838f194
le2sky <leehaneul990623@gmail.com>
2024-09-13 11:15:14 +0900

    feat: abstract controller 미지원 메서드 처리

--- tomcat/src/main/java/org/apache/coyote/http11/AbstractController.java
+++ tomcat/src/main/java/org/apache/coyote/http11/AbstractController.java
@@ -7,11 +7,15 @@ public class AbstractController implements Controller {
         HttpMethod httpMethod = request.getHttpMethod();
         if (httpMethod.isGet()) {
             doGet(request, response);
+            return;
         }
 
         if (httpMethod.isPost()) {
             doPost(request, response);
+            return;
         }
+
+        throw new UnsupportedOperationException("지원하지 않는 HTTP 메서드입니다.");
     }
 
     protected void doGet(HttpRequest request, HttpResponse response) throws Exception {

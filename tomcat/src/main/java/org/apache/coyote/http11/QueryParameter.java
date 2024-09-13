7783891ee6023adca5c9fa6f894b54872dabfb24
le2sky <leehaneul990623@gmail.com>
2024-09-13 13:14:30 +0900

    refactor: QueryParameter 가독성 개선
    - depth 1로 변경
    - 상수 추출

--- tomcat/src/main/java/org/apache/coyote/http11/QueryParameter.java
+++ tomcat/src/main/java/org/apache/coyote/http11/QueryParameter.java
@@ -1,5 +1,6 @@
 package org.apache.coyote.http11;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -8,6 +9,9 @@ public class QueryParameter {
 
     private static final String PAIR_DELIMITER = "=";
     private static final String PARAMETER_DELIMITER = "&";
+    private static final int KEY_PAIR_LENGTH = 2;
+    private static final int KEY_POSITION = 0;
+    private static final int VALUE_POSITION = 1;
 
     private final Map<String, String> queryParameter = new HashMap<>();
 
@@ -22,20 +26,22 @@ public class QueryParameter {
     private void parseQueryParameter(String queryParameter) {
         String[] pairs = queryParameter.split(PARAMETER_DELIMITER);
 
-        for (String pair : pairs) {
-            if (pair.contains(PAIR_DELIMITER)) {
-                String[] keyValuePair = pair.split(PAIR_DELIMITER);
-                putIfValidPair(keyValuePair);
-            }
+        Arrays.stream(pairs).forEach(this::parsePair);
+    }
+
+    private void parsePair(String pair) {
+        if (pair.contains(PAIR_DELIMITER)) {
+            String[] keyValuePair = pair.split(PAIR_DELIMITER);
+            putIfValidPair(keyValuePair);
         }
     }
 
     private void putIfValidPair(String[] keyValuePair) {
-        if (keyValuePair.length != 2) {
+        if (keyValuePair.length != KEY_PAIR_LENGTH) {
             return;
         }
-        String key = keyValuePair[0];
-        String value = keyValuePair[1];
+        String key = keyValuePair[KEY_POSITION];
+        String value = keyValuePair[VALUE_POSITION];
 
         queryParameter.put(key, value);
     }

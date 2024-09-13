399106aac43a37a53ef3c192c9b975a1d3927061
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-09-13 17:32:51 +0900

    feat: SessionManager 동시성 컬렉션 인터페이스로 선언

--- tomcat/src/main/java/org/apache/coyote/session/SessionManager.java
+++ tomcat/src/main/java/org/apache/coyote/session/SessionManager.java
@@ -1,12 +1,13 @@
 package org.apache.coyote.session;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import org.apache.catalina.Manager;
 
 public class SessionManager implements Manager {
 
-    private static final ConcurrentHashMap<String, Session> SESSIONS = new ConcurrentHashMap<>();
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
     private static final SessionManager INSTANCE = new SessionManager();
 
     private SessionManager() {

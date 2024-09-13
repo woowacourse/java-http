7b26b8b980de6eebcf25d133803ae52bfeb25089
김민수 MINSOO KIM <alstn113@gmail.com>
2024-09-13 09:59:50 +0900

    feat: 고정 스레드 풀 적용

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import org.apache.catalina.Controller;
 import org.apache.coyote.http11.Http11Processor;
 import org.slf4j.Logger;
@@ -15,19 +18,24 @@ public class Connector implements Runnable {
 
     private static final int DEFAULT_PORT = 8080;
     private static final int DEFAULT_ACCEPT_COUNT = 100;
+    private static final int MAX_THREADS = 250;
 
     private final Controller controller;
     private final ServerSocket serverSocket;
     private boolean stopped;
+    private final ExecutorService executorService;
 
     public Connector(final Controller controller) {
-        this(controller, DEFAULT_PORT, DEFAULT_ACCEPT_COUNT);
+        // maxThreads: 스레드 풀에서 동시에 실행할 수 있는 최대 스레드 수입니다. 초과하면 대기 큐에 추가된다.
+        // acceptCount: 대기 큐가 가득 찼을 때 추가 요청이 대기할 수 있는 큐의 크기이다.
+        this(controller, DEFAULT_PORT, DEFAULT_ACCEPT_COUNT, MAX_THREADS);
     }
 
-    public Connector(final Controller controller, final int port, final int acceptCount) {
+    public Connector(final Controller controller, final int port, final int acceptCount, final int maxThreads) {
         this.controller = controller;
         this.serverSocket = createServerSocket(port, acceptCount);
         this.stopped = false;
+        this.executorService = Executors.newFixedThreadPool(maxThreads);
     }
 
     private ServerSocket createServerSocket(final int port, final int acceptCount) {
@@ -68,16 +76,31 @@ public class Connector implements Runnable {
         if (connection == null) {
             return;
         }
-        var processor = new Http11Processor(controller, connection);
-        new Thread(processor).start();
+        Http11Processor processor = new Http11Processor(controller, connection);
+        executorService.execute(processor);
     }
 
     public void stop() {
         stopped = true;
         try {
             serverSocket.close();
+            shutdown();
         } catch (IOException e) {
             log.error(e.getMessage(), e);
+        }
+    }
+
+    private void shutdown() {
+        try {
+            executorService.shutdown(); // 실행 중인 모든 Task가 수행되면 종료
+            // 800ms 이내에 종료되지 않으면 실행 중인 모든 Task를 즉시 종료
+            if (!executorService.awaitTermination(800, TimeUnit.MILLISECONDS)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            log.error(e.getMessage(), e);
+            executorService.shutdownNow(); // 인터럽트 발생 경우에도 종료
+            Thread.currentThread().interrupt(); // 인터럽트 상태를 다시 설정
         }
     }
 

abdde7b7114053bd9e0792c45d07bc9a6dc17af7
coli-geonwoo <coli-geonwoo@gmail.com>
2024-09-13 17:18:44 +0900

    test: CookieSerializerTest 작성

---
+++ tomcat/src/test/java/org/apache/coyote/http11/serdes/CookieSerializerTest.java
@@ -0,0 +1,14 @@
+package org.apache.coyote.http11.serdes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CookieSerializerTest {
+
+    @DisplayName("쿠키를 직렬화 할 수 있다")
+    @Test
+    void serialize() {
+    }
+}

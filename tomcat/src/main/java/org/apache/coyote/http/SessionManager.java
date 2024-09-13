76ed64150fa526eac187a7a3f36fe4f42f3137e3
hjk0761 <hjk0761@naver.com>
2024-09-13 15:08:51 +0900

    feat: 세션 매니저에 동시성 컬랙션 적용

--- tomcat/src/main/java/org/apache/coyote/http/SessionManager.java
+++ tomcat/src/main/java/org/apache/coyote/http/SessionManager.java
@@ -4,12 +4,13 @@ import org.apache.catalina.Manager;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class SessionManager implements Manager {
 
     private static SessionManager instance;
 
-    private static final Map<String, Session> SESSIONS = new HashMap<>();
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
 
     @Override
     public void add(final Session session) {

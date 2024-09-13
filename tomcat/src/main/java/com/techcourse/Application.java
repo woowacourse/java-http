d58afe569f297f73bc7464a2b16500edae561dd4
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-09-13 17:54:31 +0900

    feat: ExecutorService 추가

--- tomcat/src/main/java/com/techcourse/Application.java
+++ tomcat/src/main/java/com/techcourse/Application.java
@@ -12,6 +12,7 @@ import org.apache.coyote.http11.request.RequestMapper;
 import org.apache.coyote.http11.session.SessionManager;
 
 import java.util.Map;
+import java.util.concurrent.*;
 
 public class Application {
 
@@ -24,11 +25,25 @@ public class Application {
         final ResourceController resourceController = new ResourceController();
         final SessionManager sessionManager = new SessionManager();
 
-        final ConnectionListener connectionListener = new CatalinaConnectionListener(
-                new CoyoteAdapter(requestMapper, resourceController, sessionManager));
-        final Connector connector = new Connector(connectionListener);
+        final BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(100);
+        final ThreadPoolExecutor executorService = new ThreadPoolExecutor(
+                0, 250, 0L,
+                TimeUnit.MILLISECONDS, queue, new ThreadPoolExecutor.CallerRunsPolicy());
 
-        final var tomcat = new Tomcat(connector);
+        final var tomcat = createTomcat(requestMapper, resourceController, sessionManager,executorService);
         tomcat.start();
+    }
+
+    private static Tomcat createTomcat(final RequestMapper requestMapper,
+                                       final ResourceController resourceController,
+                                       final SessionManager sessionManager,
+                                       final ExecutorService executorService) {
+
+        final ConnectionListener connectionListener = new CatalinaConnectionListener(
+                new CoyoteAdapter(requestMapper, resourceController, sessionManager),
+                executorService
+        );
+        final Connector connector = new Connector(connectionListener);
+        return new Tomcat(connector);
     }
 }

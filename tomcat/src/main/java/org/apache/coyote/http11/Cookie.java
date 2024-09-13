ef5bb3879be86f3c43c69760714ae442e3d47855
le2sky <leehaneul990623@gmail.com>
2024-09-13 13:10:49 +0900

    refactor: cookie 가독성 개선
    - depth를 1로 변경했다.
    - 상수를 추출했다.

--- tomcat/src/main/java/org/apache/coyote/http11/Cookie.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Cookie.java
@@ -1,5 +1,6 @@
 package org.apache.coyote.http11;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -8,6 +9,9 @@ public class Cookie {
 
     private static final String PAIR_DELIMITER = "=";
     private static final String COOKIE_DELIMITER = ";";
+    private static final int KEY_PAIR_LENGTH = 2;
+    private static final int KET_POSITION = 0;
+    private static final int VALUE_POSITION = 1;
 
     private final Map<String, String> cookie = new HashMap<>();
 
@@ -22,20 +26,22 @@ public class Cookie {
     private void parseCookie(String cookie) {
         String[] pairs = cookie.split(COOKIE_DELIMITER);
 
-        for (String pair : pairs) {
-            if (pair.contains(PAIR_DELIMITER)) {
-                String[] keyValuePair = pair.trim().split(PAIR_DELIMITER);
-                putIfValidPair(keyValuePair);
-            }
+        Arrays.stream(pairs).forEach(this::parsePair);
+    }
+
+    private void parsePair(String pair) {
+        if (pair.contains(PAIR_DELIMITER)) {
+            String[] keyValuePair = pair.trim().split(PAIR_DELIMITER);
+            putIfValidPair(keyValuePair);
         }
     }
 
     private void putIfValidPair(String[] keyValuePair) {
-        if (keyValuePair.length != 2) {
+        if (keyValuePair.length != KEY_PAIR_LENGTH) {
             return;
         }
-        String key = keyValuePair[0].trim();
-        String value = keyValuePair[1].trim();
+        String key = keyValuePair[KET_POSITION].trim();
+        String value = keyValuePair[VALUE_POSITION].trim();
 
         cookie.put(key, value);
     }

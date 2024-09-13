ddd266cec2f967054c9015145afea7185aea446e
PgmJun <84304802+PgmJun@users.noreply.github.com>
2024-09-13 16:53:43 +0900

    refactor: 미사용 메서드 제거

--- tomcat/src/main/java/org/apache/coyote/http11/resource/ResourceParser.java
+++ tomcat/src/main/java/org/apache/coyote/http11/resource/ResourceParser.java
@@ -6,14 +6,6 @@ import org.apache.coyote.http11.HttpRequest;
 
 public class ResourceParser {
 
-    public static File getRequestFile(HttpRequest httpRequest) {
-        try {
-            return getFile(httpRequest.getPath());
-        } catch (NullPointerException exception) {
-            return getFile("/404.html");
-        }
-    }
-
     public static File getRequestFile(String filePath) {
         try {
             return getFile(filePath);

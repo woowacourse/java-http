4ef640f19bae5aeac99f5daba9be6bbdba6a84cd
jongmee <ra123221@gmail.com>
2024-09-13 14:30:58 +0900

    refactor: SessionManager 자료구조 ConcurrentHashMap으로 변경

--- tomcat/src/main/java/org/apache/coyote/http11/session/SessionManager.java
+++ tomcat/src/main/java/org/apache/coyote/http11/session/SessionManager.java
@@ -1,11 +1,11 @@
 package org.apache.coyote.http11.session;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class SessionManager {
 
-    private static final Map<String, Session> SESSIONS = new HashMap<>();
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
 
     public static void add(Session session) {
         SESSIONS.put(session.getId(), session);

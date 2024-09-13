62eb2f8d32f2ed5d74088142c979f89108451971
HyungHoKim00 <hkim1109@naver.com>
2024-09-13 15:26:26 +0900

    refactor: MAX_THREADS 변경

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -16,7 +16,7 @@ public class Connector implements Runnable {
 
     private static final int DEFAULT_PORT = 8080;
     private static final int DEFAULT_ACCEPT_COUNT = 100;
-    private static final int DEFAULT_MAX_THREADS = 10;
+    private static final int DEFAULT_MAX_THREADS = 50;
 
     private final ExecutorService executor;
     private final ServerSocket serverSocket;

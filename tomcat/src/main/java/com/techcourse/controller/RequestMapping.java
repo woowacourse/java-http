ad0b55d2cf1d7791f7f9ecf2c2bc577e7e40df31
재우 <Akaps6768@gmail.com>
2024-09-14 17:34:14 +0900

    refactor: 불필요한 코드 삭제

--- tomcat/src/main/java/com/techcourse/controller/RequestMapping.java
+++ tomcat/src/main/java/com/techcourse/controller/RequestMapping.java
@@ -5,14 +5,10 @@ import java.util.Map;
 import org.apache.coyote.http.HttpRequest;
 import org.apache.coyote.http.HttpResponse;
 import org.apache.coyote.http.MediaType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class RequestMapping implements Controller {
 
     private static final RequestMapping INSTANCE = new RequestMapping();
-
-    private static final Logger log = LoggerFactory.getLogger(RequestMapping.class);
 
     private final Map<String, Controller> controllers;
 
@@ -30,7 +26,6 @@ public class RequestMapping implements Controller {
 
     @Override
     public void service(final HttpRequest request, final HttpResponse response) throws Exception {
-        log.info("request: \n{}", request.asString());
         String path = request.getPath();
         if (MediaType.isSupportedExtension(path)) {
             new StaticResourceHandler().service(request, response);

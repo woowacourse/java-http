a04e37bcfa1a1c0d648ab913120f8641d95f717e
kelly6bf <kelly6bf@gmail.com>
2024-09-14 16:07:01 +0900

    Revert "test: Greeting Controller Test 문제 풀이"
    This reverts commit 6cd6a1a59f770983574a4a2b4e2689b2d9844412.

--- study/src/main/java/cache/com/example/etag/EtagFilterConfiguration.java
+++ study/src/main/java/cache/com/example/etag/EtagFilterConfiguration.java
@@ -1,21 +1,12 @@
 package cache.com.example.etag;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @Configuration
 public class EtagFilterConfiguration {
 
-    @Bean
-    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
-        ShallowEtagHeaderFilter filter = new ShallowEtagHeaderFilter();
-
-        FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean = new FilterRegistrationBean<>(filter);
-        filterRegistrationBean.addUrlPatterns("/etag");
-        filterRegistrationBean.addUrlPatterns("/resources/*");
-
-        return filterRegistrationBean;
-    }
+//    @Bean
+//    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+//        return null;
+//    }
 }
--- study/src/main/java/cache/com/example/version/CacheBustingWebConfig.java
+++ study/src/main/java/cache/com/example/version/CacheBustingWebConfig.java
@@ -1,10 +1,7 @@
 package cache.com.example.version;
 
-import java.time.Duration;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -23,7 +20,6 @@ public class CacheBustingWebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         registry.addResourceHandler(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/**")
-                .addResourceLocations("classpath:/static/")
-                .setCacheControl(CacheControl.maxAge(Duration.ofDays(365)).cachePublic());
+                .addResourceLocations("classpath:/static/");
     }
 }

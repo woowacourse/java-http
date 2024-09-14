b61638dab39e199ec04d4e72969bd1d12c964153
재우 <Akaps6768@gmail.com>
2024-09-14 16:55:31 +0900

    test: 메서드명 변경

--- tomcat/src/test/java/com/techcourse/controller/LoginControllerTest.java
+++ tomcat/src/test/java/com/techcourse/controller/LoginControllerTest.java
@@ -2,15 +2,11 @@ package com.techcourse.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-
-import org.apache.coyote.http.HttpRequest;
 import org.apache.coyote.http.HttpResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class LoginControllerTest {
+class LoginControllerTest extends ControllerTest {
 
     private final LoginController sut = new LoginController();
 
@@ -42,11 +38,11 @@ class LoginControllerTest {
     }
 
     @Test
-    @DisplayName("로그인 실패: 401로 리다이렉트")
-    void login401() throws Exception {
+    @DisplayName("로그인 실패: 아이디가 일치하지 않는 경우 401로 리다이렉트")
+    void login_failure_401_account_not_found() throws Exception {
         // given
         var account = "nobody";
-        var password = "wrong password";
+        var password = "password";
         var requestBody = "account=%s&password=%s".formatted(account, password);
         var request = httpRequest(String.join("\r\n",
                 "POST /login HTTP/1.1 ",
@@ -68,9 +64,30 @@ class LoginControllerTest {
                 .contains("Location: /401.html ");
     }
 
-    private HttpRequest httpRequest(final String input) throws IOException {
-        byte[] inputBytes = input.getBytes();
-        var inputStream = new ByteArrayInputStream(inputBytes);
-        return new HttpRequest(inputStream);
+    @Test
+    @DisplayName("로그인 실패: 비밀번호가 일치하지 않는 경우 401로 리다이렉트")
+    void login_failure_401_password_not_found() throws Exception {
+        // given
+        var account = "gugu";
+        var password = "wrong password";
+        var requestBody = "account=%s&password=%s".formatted(account, password);
+        var request = httpRequest(String.join("\r\n",
+                "POST /login HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "Content-Length: 80 ",
+                "Content-Type: application/x-www-form-urlencoded ",
+                "Accept: */* ",
+                "",
+                requestBody));
+        var response = new HttpResponse();
+
+        // when
+        sut.service(request, response);
+
+        // then
+        assertThat(response.asString())
+                .contains("HTTP/1.1 302 FOUND ")
+                .contains("Location: /401.html ");
     }
 }

01a0d1ab23258a617c9a3e15d54b41f345d3a1c2
김민수 MINSOO KIM <alstn113@gmail.com>
2024-09-14 14:04:04 +0900

    refactor: Session도 동일하게 동시성 자료구조로 변경

--- tomcat/src/main/java/org/apache/catalina/Session.java
+++ tomcat/src/main/java/org/apache/catalina/Session.java
@@ -3,11 +3,12 @@ package org.apache.catalina;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Session {
 
     private final String id;
-    private final Map<String, Object> values = new HashMap<>();
+    private final Map<String, Object> values = new ConcurrentHashMap<>();
 
     public Session() {
         this.id = UUID.randomUUID().toString();

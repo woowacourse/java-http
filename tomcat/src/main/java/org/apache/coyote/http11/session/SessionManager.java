fcc1b8996346877a81bbe96e034fa4494ad1d7cc
김민겸 <mingyum119@naver.com>
2024-09-16 01:13:41 +0900

    feat: 세션 저장소에 동시성 컬렉션 사용

--- tomcat/src/main/java/org/apache/coyote/http11/session/SessionManager.java
+++ tomcat/src/main/java/org/apache/coyote/http11/session/SessionManager.java
@@ -3,11 +3,12 @@ package org.apache.coyote.http11.session;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 import com.techcourse.model.User;
 
 public final class SessionManager {
-	private static final Map<Session, User> sessions = new HashMap<>();
+	private static final Map<Session, User> sessions = new ConcurrentHashMap<>();
 
 	public static Session createSession(User user) {
 		Session session = new Session();

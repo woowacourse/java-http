cdf7788f786928672b7506ccad7d95adb4d9839e
unifolio0 <boho82ha@gmail.com>
2024-09-15 15:50:13 +0900

    test: Connector 테스트 추가

---
+++ tomcat/src/test/java/org/apache/catalina/connector/ConnectorTest.java
@@ -0,0 +1,49 @@
+package org.apache.catalina.connector;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.net.Socket;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ConnectorTest {
+
+    @DisplayName("서버를 종료하면 더 이상 클라이언트를 받을 수 없다")
+    @Test
+    void stop() {
+        Connector connector = new Connector();
+        connector.start();
+        connector.stop();
+
+        assertThatThrownBy(() -> {
+            try (Socket clientSocket = new Socket("localhost", 8080)) {}
+        }).isInstanceOf(Exception.class);
+    }
+
+    @DisplayName("스레드 풀의 크기는 250이다")
+    @Test
+    void testServerStop() {
+        final var executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(250);
+        for (int i = 0; i < 350; i++) {
+            executor.submit(logWithSleep());
+        }
+        final int expectedPoolSize = 250;
+        final int expectedQueueSize = 100;
+
+        assertThat(expectedPoolSize).isEqualTo(executor.getPoolSize());
+        assertThat(expectedQueueSize).isEqualTo(executor.getQueue().size());
+    }
+
+    private Runnable logWithSleep() {
+        return () -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+}

b7f9f58fa4c1be3d825430a429eccd22f845e779
ikjo39 <chi4321@naver.com>
2024-09-16 13:50:49 +0900

    refactor(SessionManager): ConcurrentMap -> Map 수정

--- tomcat/src/main/java/org/apache/catalina/SessionManager.java
+++ tomcat/src/main/java/org/apache/catalina/SessionManager.java
@@ -1,12 +1,12 @@
 package org.apache.catalina;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 public class SessionManager implements Manager {
 
     private static final SessionManager INSTANCE = new SessionManager();
-    private static final ConcurrentMap<String, Session> SESSIONS = new ConcurrentHashMap<>();
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
 
     private SessionManager() {
     }

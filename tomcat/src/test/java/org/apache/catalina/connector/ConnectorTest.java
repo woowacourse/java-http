c5c86349b2b103233e88e910cfe9f177db7335c2
SANGHUN OH <121424793+unifolio0@users.noreply.github.com>
2024-09-16 16:43:35 +0900

    [4단계 - Tomcat 구현하기] 비토(오상훈) 미션 제출합니다 (#768)
    * fix: remove implementation logback-classic on gradle (#501)
    
    * fix: add threads min-spare configuration on properties (#502)
    
    * test: Connector 테스트 추가
    
    * test: 에러 종류 수정
    
    * test: 메소드명 수정
    
    * test: 변수 수정
    
    ---------
    
    Co-authored-by: Gyeongho Yang <chchch1213@naver.com>

---
+++ tomcat/src/test/java/org/apache/catalina/connector/ConnectorTest.java
@@ -0,0 +1,50 @@
+package org.apache.catalina.connector;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.net.ConnectException;
+import java.net.Socket;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ConnectorTest {
+
+    @DisplayName("서버를 종료하면 더 이상 클라이언트를 받을 수 없다")
+    @Test
+    void stop() {
+        Connector connector = new Connector();
+        connector.start();
+        connector.stop();
+
+        assertThatThrownBy(() -> {
+            try (Socket clientSocket = new Socket("localhost", 8080)) {}
+        }).isInstanceOf(ConnectException.class);
+    }
+
+    @DisplayName("스레드 풀의 크기는 250이다")
+    @Test
+    void testServerStop() {
+        final var executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(Connector.DEFAULT_MAX_THREADS);
+        for (int i = 0; i < 350; i++) {
+            executor.submit(threadSleep());
+        }
+        final int expectedPoolSize = Connector.DEFAULT_MAX_THREADS;
+        final int expectedQueueSize = 100;
+
+        assertThat(expectedPoolSize).isEqualTo(executor.getPoolSize());
+        assertThat(expectedQueueSize).isEqualTo(executor.getQueue().size());
+    }
+
+    private Runnable threadSleep() {
+        return () -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+}

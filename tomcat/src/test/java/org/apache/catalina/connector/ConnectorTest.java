90b07154476c416d32db11296c0e7610de806fe4
Gyeongho Yang <chchch1213@naver.com>
2024-09-16 18:02:16 +0900

    test: write exactly

--- tomcat/src/test/java/org/apache/catalina/connector/ConnectorTest.java
+++ tomcat/src/test/java/org/apache/catalina/connector/ConnectorTest.java
@@ -1,5 +1,8 @@
 package org.apache.catalina.connector;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -13,24 +16,36 @@ import support.TestHttpUtils;
 class ConnectorTest {
 
     @Test
-    @DisplayName("켜져있는 서버 메인 페이지를 600개 요청 해본다.")
+    @DisplayName("켜져있는 서버 메인 페이지를 600개 요청후 성공이 스레드 개수 250과 큐 100개 합친 350인지 확인한다.")
     void request600() throws InterruptedException {
         // given
         int threadCount = 600;
+        int expectedCount = 350;
         ExecutorService pool = Executors.newFixedThreadPool(threadCount);
-        List<Future<?>> futures = new ArrayList<>();
+        List<Future<Integer>> futures = new ArrayList<>();
 
         // when
         for (int i = 0; i < threadCount; i++) {
-            futures.add(pool.submit(() -> TestHttpUtils.send("/index.html")));
+            futures.add(pool.submit(() -> {
+                HttpResponse<String> response = TestHttpUtils.send("/index.html");
+                return response.statusCode();
+            }));
         }
-        pool.awaitTermination(10, TimeUnit.SECONDS);
 
-        long doneCount = futures.stream()
-                .filter(Future::isDone)
+        pool.shutdown();
+        pool.awaitTermination(5, TimeUnit.MINUTES);
+
+        long successCount = futures.stream()
+                .filter(future -> {
+                    try {
+                        return future.get() == 200;
+                    } catch (Exception e) {
+                        return false;
+                    }
+                })
                 .count();
 
         // then
-        System.out.println("doneCount = " + doneCount);
+        assertThat(successCount).isEqualTo(expectedCount);
     }
 }

7f8ceb41d7abbec5ef25e7ace20cd4bf10a9d3f6
KIMGYUTAE <kiimwf0819@gmail.com>
2024-09-17 00:27:43 +0900

    refactor: 사용하지 않는 import 제거

--- tomcat/src/test/java/org/apache/catalina/connector/ConnectorTest.java
+++ tomcat/src/test/java/org/apache/catalina/connector/ConnectorTest.java
@@ -1,10 +1,8 @@
 package org.apache.catalina.connector;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;

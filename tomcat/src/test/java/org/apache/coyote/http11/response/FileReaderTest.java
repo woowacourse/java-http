9690f33083a5c6ce759986e2b7b4c2940daac3c3
SCY <helenason@naver.com>
2024-09-16 20:14:31 +0900

    chore: 테스트 패키지 위치 설정

--- tomcat/src/test/java/org/apache/coyote/http11/response/FileReaderTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/response/FileReaderTest.java
@@ -1,4 +1,4 @@
-package org.apache.coyote.http11.helper;
+package org.apache.coyote.http11.response;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;

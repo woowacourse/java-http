4a114ec5e3934aa1bd03fcfd3a471fb9d7a0cc09
eun-byeol <joeunbyeol98@gmail.com>
2024-09-16 18:54:04 +0900

    test: HttpResponseTest 패키지 이동

--- tomcat/src/test/java/org/apache/coyote/http11/message/response/HttpResponseTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/message/response/HttpResponseTest.java
@@ -1,12 +1,10 @@
-package org.apache.coyote.http11;
+package org.apache.coyote.http11.message.response;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.apache.coyote.http11.message.common.ContentType;
 import org.apache.coyote.http11.message.common.HttpHeader;
-import org.apache.coyote.http11.message.response.HttpResponse;
-import org.apache.coyote.http11.message.response.StatusCode;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 

8789c5c558bbb8f1bcaeb68680a05fa4ca760cf0
재우 <Akaps6768@gmail.com>
2024-09-16 23:06:10 +0900

    refactor: http request method handling 추상화

--- tomcat/src/main/java/com/techcourse/controller/AbstractController.java
+++ tomcat/src/main/java/com/techcourse/controller/AbstractController.java
@@ -1,25 +1,42 @@
 package com.techcourse.controller;
 
+import java.util.Map;
+
 import org.apache.coyote.http.HttpMethod;
 import org.apache.coyote.http.HttpRequest;
 import org.apache.coyote.http.HttpResponse;
 
 public abstract class AbstractController implements Controller {
 
+    private final Map<HttpMethod, RequestHandling> requestHandling;
+
+    protected AbstractController() {
+        requestHandling = Map.of(
+                HttpMethod.GET, this::doGet,
+                HttpMethod.POST, this::doPost
+        );
+    }
+
     @Override
     public void service(HttpRequest request, HttpResponse response) throws Exception {
         HttpMethod method = request.getMethod();
-        if (method.isGet()) {
-            doGet(request, response);
+        RequestHandling handling = requestHandling.get(method);
+        if (handling != null) {
+            handling.handle(request, response);
+            return;
         }
-        if (method.isPost()) {
-            doPost(request, response);
-        }
+        sendNotAllowed(request, response);
     }
 
     protected void doPost(HttpRequest request, HttpResponse response) throws Exception {
+        sendNotAllowed(request, response);
     }
 
     protected void doGet(HttpRequest request, HttpResponse response) throws Exception {
+        sendNotAllowed(request, response);
+    }
+
+    private void sendNotAllowed(HttpRequest request, HttpResponse response) {
+        response.sendRedirect("/405.html");
     }
 }

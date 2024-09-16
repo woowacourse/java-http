fa688ac0949e73428cc2b6f8c867d35fb9e240a9
J-I-H-O <jeongjiho0731@gmail.com>
2024-09-16 18:21:56 +0900

    refactor: RegisterController 메서드 분리

--- tomcat/src/main/java/com/techcourse/controller/RegisterController.java
+++ tomcat/src/main/java/com/techcourse/controller/RegisterController.java
@@ -4,6 +4,7 @@ import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.model.User;
 import java.util.Optional;
 import org.apache.catalina.SessionManager;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.coyote.http11.HttpHeader;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.response.HttpResponse;
@@ -27,31 +28,44 @@ public class RegisterController extends Controller {
 
     @Override
     public void doPost(HttpRequest request, HttpResponse response) {
-        String bodyInputAccount = request.getBodyParameter("account");
-        String bodyInputPassword = request.getBodyParameter("password");
-        String bodyInputEmail = request.getBodyParameter("email");
+        try {
+            String account = request.getBodyParameter("account");
+            String password = request.getBodyParameter("password");
+            String email = request.getBodyParameter("email");
 
-        if (bodyInputAccount == null || bodyInputPassword == null || bodyInputEmail == null) {
+            String sessionId = register(account, password, email);
+
+            response.setStatus(HttpStatus.FOUND);
+            response.addHeader(HttpHeader.SET_COOKIE, "JSESSIONID=" + sessionId);
+            response.addHeader(HttpHeader.LOCATION, "/index.html");
+        } catch (IllegalArgumentException e) {
             response.setStatus(HttpStatus.BAD_REQUEST);
             response.setBodyWithStaticResource("/register.html");
-            return;
         }
+    }
 
-        Optional<User> optionalUser = InMemoryUserRepository.findByAccount(bodyInputAccount);
+    private String register(String account, String password, String email) {
+        validateBlank(account, password, email);
+        Optional<User> optionalUser = InMemoryUserRepository.findByAccount(account);
+        validateUserExist(optionalUser);
 
-        if (!optionalUser.isEmpty()) {
-            response.setStatus(HttpStatus.BAD_REQUEST);
-            response.setBodyWithStaticResource("/register.html");
-            return;
-        }
-
-        User user = new User(bodyInputAccount, bodyInputPassword, bodyInputEmail);
+        User user = new User(account, password, email);
         InMemoryUserRepository.save(user);
         log.info(user.toString());
 
         String sessionId = SessionManager.put(user);
-        response.setStatus(HttpStatus.FOUND);
-        response.addHeader(HttpHeader.SET_COOKIE, "JSESSIONID=" + sessionId);
-        response.addHeader(HttpHeader.LOCATION, "/index.html");
+        return sessionId;
+    }
+
+    private void validateBlank(String account, String password, String email) {
+        if (StringUtils.isBlank(account) || StringUtils.isBlank(password) || StringUtils.isBlank(email)) {
+            throw new IllegalArgumentException("parameter could not be blank.");
+        }
+    }
+
+    private void validateUserExist(Optional<User> optionalUser) {
+        if (optionalUser.isPresent()) {
+            throw new IllegalArgumentException("user already exists.");
+        }
     }
 }

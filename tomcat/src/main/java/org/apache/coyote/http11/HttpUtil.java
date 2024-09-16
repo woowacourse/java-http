0a7475d282d12cbf36fd22566f2760be16518209
SungKyum Kim <sungkyum1@naver.com>
2024-09-17 00:37:16 +0900

    refactor: 상수 분리

--- tomcat/src/main/java/org/apache/coyote/http11/HttpUtil.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpUtil.java
@@ -5,19 +5,25 @@ import java.util.Map;
 
 public class HttpUtil {
 
+	private static final String MULTI_ITEM_DELIMITER = "&";
+	private static final String KEY_VALUE_DELIMITER = "=";
+	private static final int KEY_VALUE_SPLIT_LIMIT = 2;
+	public static final int KEY_INDEX = 0;
+	public static final int VALUE_INDEX = 1;
+
 	private HttpUtil() {
 
 	}
 
 	public static Map<String, String> parseQueryString(String body) {
 		Map<String, String> parameters = new HashMap<>();
-		String[] pairs = body.split("&");
+		String[] pairs = body.split(MULTI_ITEM_DELIMITER);
 
 		for (String pair : pairs) {
-			String[] keyValue = pair.split("=", 2);
+			String[] keyValue = pair.split(KEY_VALUE_DELIMITER, KEY_VALUE_SPLIT_LIMIT);
 
-			String key = keyValue[0];
-			String value = keyValue.length > 1 ? keyValue[1] : "";
+			String key = keyValue[KEY_INDEX];
+			String value = keyValue[VALUE_INDEX];
 
 			parameters.put(key, value);
 		}

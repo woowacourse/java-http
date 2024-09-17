e029c7dbda72c74fcbb535a02c5609f4c0e1dee1
Choo <choo000407@naver.com>
2024-09-18 01:52:11 +0900

    refactor: 사용 안하는 필드 제거

--- tomcat/src/main/java/org/apache/catalina/controller/AbstractController.java
+++ tomcat/src/main/java/org/apache/catalina/controller/AbstractController.java
@@ -1,14 +1,11 @@
 package org.apache.catalina.controller;
 
-import org.apache.catalina.handler.ViewResolver;
 import org.apache.coyote.http.HttpMethod;
 import org.apache.coyote.http.HttpStatusCode;
 import org.apache.coyote.http.request.HttpRequest;
 import org.apache.coyote.http.response.HttpResponse;
 
 public abstract class AbstractController implements Controller {
-
-    protected final ViewResolver viewResolver = new ViewResolver(); // TODO: HttpResponse 내부로 옮기기
 
     @Override
     public void service(final HttpRequest request, final HttpResponse response) throws Exception {

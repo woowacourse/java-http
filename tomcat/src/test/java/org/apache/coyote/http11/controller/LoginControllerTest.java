183796d762371d3542389d2aa396692f93b8921f
tsulocalize <tsulocalizing@gmail.com>
2024-09-17 16:01:16 +0900

    test: 리다이렉트 테스트 추가

--- tomcat/src/test/java/org/apache/coyote/http11/controller/LoginControllerTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/controller/LoginControllerTest.java
@@ -74,4 +74,48 @@ public class LoginControllerTest {
         assertThat(new String(response.getBytes()))
                 .contains("200 OK");
     }
+
+    @Test
+    @DisplayName("이미 로그인되어 있는 경우 index.html로 리다이렉트 된다.")
+    void doGet_alreadyLogin() throws IOException {
+        String JSESSIONID = getSessionId();
+
+        String loginRequest =
+                "GET /login HTTP/1.1" + "\r\n" +
+                        "Host: localhost:8080" + "\r\n" +
+                        "Cookie: JSESSIONID=" + JSESSIONID + "\r\n" +
+                        "Connection: keep-alive" + "\r\n\r\n";
+
+        Http11Request request = Http11Request.from(new ByteArrayInputStream(loginRequest.getBytes()));
+        Http11Response response = Http11Response.ok();
+
+        LoginController loginController = new LoginController();
+        loginController.doGet(request, response);
+
+        String actual = new String(response.getBytes());
+        assertThat(actual)
+                .contains("302 Found")
+                .contains("index.html");
+    }
+
+    private String getSessionId() throws IOException {
+        String loginRequest =
+                "POST /login HTTP/1.1" + "\r\n" +
+                        "Host: localhost:8080" + "\r\n" +
+                        "Connection: keep-alive" + "\r\n" +
+                        "Content-Length: 30" + "\r\n" +
+                        "\r\n" +
+                        "account=gugu&password=password";
+
+        Http11Request request = Http11Request.from(new ByteArrayInputStream(loginRequest.getBytes()));
+        Http11Response response = Http11Response.ok();
+
+        LoginController loginController = new LoginController();
+        loginController.doPost(request, response);
+
+        String s = new String(response.getBytes());
+        int from = s.indexOf("JSESSIONID=") + "JSESSIONID=".length();
+        int to = s.indexOf("Location");
+        return s.substring(from, to).trim();
+    }
 }

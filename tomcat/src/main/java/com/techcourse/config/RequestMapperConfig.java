e03912c2c73abfc9ee58f2c91d3526821dc2e79a
takoyakimchi <c4nyou@gmail.com>
2024-09-18 19:04:58 +0900

    feat: 스레드의 동작을 테스트하기 위한 컨트롤러 작성

--- tomcat/src/main/java/com/techcourse/config/RequestMapperConfig.java
+++ tomcat/src/main/java/com/techcourse/config/RequestMapperConfig.java
@@ -2,6 +2,7 @@ package com.techcourse.config;
 
 import com.techcourse.controller.LoginController;
 import com.techcourse.controller.RegisterController;
+import com.techcourse.controller.ThreadTestController;
 import org.apache.catalina.controller.RequestMapper;
 
 public class RequestMapperConfig {
@@ -15,5 +16,6 @@ public class RequestMapperConfig {
     public void configure() {
         requestMapper.addMapping("/login", new LoginController());
         requestMapper.addMapping("/register", new RegisterController());
+        requestMapper.addMapping("/test", new ThreadTestController());
     }
 }

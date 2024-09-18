f6fc51676153a1cbf5f37557b95ccc81f2b481d3
J-I-H-O <jeongjiho0731@gmail.com>
2024-09-18 15:34:37 +0900

    test: stage2 학습 테스트

--- study/src/test/java/thread/stage2/TestHttpUtils.java
+++ study/src/test/java/thread/stage2/TestHttpUtils.java
@@ -5,9 +5,14 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.time.Duration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TestHttpUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(SampleController.class);
 
     private static final HttpClient httpClient = HttpClient.newBuilder()
             .version(HttpClient.Version.HTTP_1_1)
@@ -15,13 +20,16 @@ public class TestHttpUtils {
             .build();
 
     public static HttpResponse<String> send(final String path) {
+        log.info("connection requested");
         final var request = HttpRequest.newBuilder()
                 .uri(URI.create("http://localhost:8080" + path))
                 .timeout(Duration.ofSeconds(1))
                 .build();
 
         try {
-            return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
+            log.info("request sent");
+            return response;
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }

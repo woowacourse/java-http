fadc6e239c2916cc0a232864d098b3e46cec20c8
kelly6bf <kelly6bf@gmail.com>
2024-09-18 16:15:51 +0900

    refactor : Controller & AbstractController 개선

--- tomcat/src/main/java/org/apache/catalina/controller/AbstractController.java
+++ tomcat/src/main/java/org/apache/catalina/controller/AbstractController.java
@@ -3,19 +3,15 @@ package org.apache.catalina.controller;
 import org.apache.coyote.http11.message.HttpMethod;
 import org.apache.coyote.http11.message.request.HttpRequest;
 import org.apache.coyote.http11.message.request.HttpRequestLine;
+import org.apache.coyote.http11.message.request.HttpRequestUri;
 import org.apache.coyote.http11.message.response.HttpResponse;
 
 public abstract class AbstractController implements Controller {
 
-    private final String baseUri;
-
-    public AbstractController(final String baseUri) {
-        this.baseUri = baseUri;
-    }
-
-    @Override
-    public boolean canHandleRequest(final HttpRequest request) {
-        return request.getRequestLine().matchRequestUri(baseUri);
+    protected boolean matchRequestUriWithBaseUri(final HttpRequest httpRequest, final String baseUri) {
+        final HttpRequestLine requestLine = httpRequest.getRequestLine();
+        final HttpRequestUri requestUri = requestLine.getHttpRequestUri();
+        return requestUri.matchRequestUri(baseUri);
     }
 
     @Override
@@ -28,13 +24,10 @@ public abstract class AbstractController implements Controller {
 
         if (requestLine.matchHttpMethod(HttpMethod.POST)) {
             doPost(request, response);
-            return;
         }
-
-        // TODO : Method Not Supported 구현
     }
 
-    protected abstract void doGet(final HttpRequest request, final HttpResponse response);
+    protected abstract void doGet(HttpRequest request, HttpResponse response);
 
-    protected abstract void doPost(final HttpRequest request, final HttpResponse response);
+    protected abstract void doPost(HttpRequest request, HttpResponse response);
 }

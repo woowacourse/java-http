c6d515f3d507c0c13e2922c0f04d1a61d60cd15f
PgmJun <84304802+PgmJun@users.noreply.github.com>
2024-09-18 23:56:51 +0900

    feat: ID 설정 로직 추가

--- tomcat/src/main/java/com/techcourse/db/InMemoryUserRepository.java
+++ tomcat/src/main/java/com/techcourse/db/InMemoryUserRepository.java
@@ -2,26 +2,44 @@ package com.techcourse.db;
 
 import com.techcourse.model.User;
 
+import java.lang.reflect.Field;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class InMemoryUserRepository {
+
+    private static AtomicLong ID_GENERATOR = new AtomicLong(1);
 
     private static final Map<String, User> database = new ConcurrentHashMap<>();
 
     static {
-        final User user = new User(1L, "gugu", "password", "hkkang@woowahan.com");
-        database.put(user.getAccount(), user);
+        final User user = new User("gugu", "password", "hkkang@woowahan.com");
+        save(user);
     }
 
     public static void save(User user) {
+        setId(user);
         database.put(user.getAccount(), user);
+    }
+
+    private static void setId(User user) {
+        try {
+            Field field = user.getClass().getDeclaredField("id");
+            field.setAccessible(true);
+            field.set(user, ID_GENERATOR.getAndIncrement());
+        } catch (NoSuchFieldException e) {
+            throw new IllegalArgumentException(e);
+        } catch (IllegalAccessException e) {
+            throw new IllegalArgumentException(e);
+        }
     }
 
     public static Optional<User> findByAccount(String account) {
         return Optional.ofNullable(database.get(account));
     }
 
-    private InMemoryUserRepository() {}
+    private InMemoryUserRepository() {
+    }
 }

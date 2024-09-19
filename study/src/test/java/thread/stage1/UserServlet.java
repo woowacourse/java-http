296f6a42cbc2ef86227e084ab259fb3072e8afd6
kelly6bf <kelly6bf@gmail.com>
2024-09-19 17:37:45 +0900

    study : 불필요한 학습 테스트 로직 제거

--- study/src/test/java/thread/stage1/UserServlet.java
+++ study/src/test/java/thread/stage1/UserServlet.java
@@ -2,18 +2,14 @@ package thread.stage1;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 public class UserServlet {
 
-    /**
-     * CopyOnWriteArrayList를 사용하면 테스트가 통과될거라 기대했는데 통과되지 않는다. 원인을 찾아봐야 한다.
-     */
-//    private final List<User> users = new CopyOnWriteArrayList<>();
     private final List<User> users = new ArrayList<>();
 
     /**
      * synchronized 키워드를 사용해서 한 스레드가 해당 메서드를 사용하면 다른 스레드는 접근하지 못하게 락(Lock)을 거는 방식으로 동시성 문제를 해결할 수 있다.
+     *
      * @param user
      */
     public synchronized void service(final User user) {

cd6878b573468cede3bb5e77a97628e908158209
yechop <channetattoo@gmail.com>
2024-09-19 11:00:01 +0900

    refactor: 스트림이 나와있지 않고 한번에 리턴하도록 리팩토링

--- tomcat/src/main/java/org/apache/coyote/http11/util/StaticResourceReader.java
+++ tomcat/src/main/java/org/apache/coyote/http11/util/StaticResourceReader.java
@@ -1,13 +1,15 @@
 package org.apache.coyote.http11.util;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 public class StaticResourceReader {
 
     public static String read(String resourceUri) throws IOException {
-        final InputStream staticResource = StaticResourceReader.class.getClassLoader()
-                .getResourceAsStream("static" + resourceUri);
-        return new String(staticResource.readAllBytes());
+        return new String(
+                StaticResourceReader.class
+                        .getClassLoader()
+                        .getResourceAsStream("static" + resourceUri)
+                        .readAllBytes()
+        );
     }
 }

53b84494bf458426a2977f1fa7ae3e9e9de737f3
yechop <channetattoo@gmail.com>
2024-09-19 11:14:08 +0900

    chore: 테스트 DisplayName 추가

--- tomcat/src/test/java/org/apache/coyote/http11/util/ContentTypeResolverTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/util/ContentTypeResolverTest.java
@@ -2,10 +2,12 @@ package org.apache.coyote.http11.util;
 
 import org.apache.coyote.http11.constants.ContentType;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ContentTypeResolverTest {
 
+    @DisplayName("요청의 resourceUri에서 파일 확장자를 찾는다")
     @Test
     void resolve() {
         final ContentType contentType = ContentTypeResolver.resolve("/test.css", "text/css");

cd55746c6987ae87a99fcd498a12734dfc3f14e5
ashsty <qkralsdk991@naver.com>
2024-09-20 13:05:56 +0900

    refactor: 리다이렉트 페이지 관련 로직 제거

--- tomcat/src/main/java/org/apache/coyote/controller/AbstractController.java
+++ tomcat/src/main/java/org/apache/coyote/controller/AbstractController.java
@@ -6,10 +6,7 @@ import org.apache.coyote.http11.response.HttpResponse;
 import org.apache.coyote.http11.response.HttpStatus;
 
 public abstract class AbstractController implements Controller {
-
-    private static final String METHOD_NOT_ALLOWED_PAGE = "/405.html";
-    private static final String NOT_IMPLEMENTED_PAGE = "/501.html";
-
+    
     @Override
     public void service(HttpRequest request, HttpResponse response) throws Exception {
         HttpMethod httpMethod = request.getHttpMethod();
@@ -28,17 +25,14 @@ public abstract class AbstractController implements Controller {
     private static void validateMethod(HttpMethod httpMethod, HttpResponse response) {
         if (!httpMethod.isValidMethod()) {
             response.setStatus(HttpStatus.NOT_IMPLEMENTED);
-            response.redirectTo(NOT_IMPLEMENTED_PAGE);
         }
     }
 
     protected void doPost(HttpRequest request, HttpResponse response) {
         response.setStatus(HttpStatus.METHOD_NOT_ALLOWED);
-        response.redirectTo(METHOD_NOT_ALLOWED_PAGE);
     }
 
     protected void doGet(HttpRequest request, HttpResponse response) {
         response.setStatus(HttpStatus.METHOD_NOT_ALLOWED);
-        response.redirectTo(METHOD_NOT_ALLOWED_PAGE);
     }
 }

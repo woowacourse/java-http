08c01552237cae8269c765351bc445a2d9a99f22
yechop <channetattoo@gmail.com>
2024-09-20 14:40:22 +0900

    refactor: 변수명 변경

--- tomcat/src/main/java/org/apache/coyote/http11/util/ContentTypeResolver.java
+++ tomcat/src/main/java/org/apache/coyote/http11/util/ContentTypeResolver.java
@@ -5,8 +5,8 @@ import org.apache.coyote.http11.constants.ContentType;
 public class ContentTypeResolver {
 
     public static ContentType resolve(final String resourceUri, final String acceptLine) {
-        final int lastDotIndex = resourceUri.lastIndexOf(".");
-        final String fileExtension = resourceUri.substring(lastDotIndex + 1);
+        final int extensionStartIndex = resourceUri.lastIndexOf(".") + 1;
+        final String fileExtension = resourceUri.substring(extensionStartIndex);
 
         if (acceptLine != null && acceptLine.contains(fileExtension)) {
             return ContentType.toContentType(fileExtension);

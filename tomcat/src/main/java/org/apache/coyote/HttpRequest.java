49f023454df110083f94fd185695b0c130cd1a65
Dompoo <dlckdrms0517@gmail.com>
2025-09-02 21:14:15 +0900

    feat: HttpRequest 구현

--- tomcat/src/main/java/org/apache/coyote/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/HttpRequest.java
@@ -1,32 +1,39 @@
 package org.apache.coyote;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public record HttpRequest(
         HttpMethod method,
         String uri
 ) {
-    public static HttpRequest from(InputStream inputStream) {
-        // TODO : 구현
-        throw new UnsupportedOperationException();
+    public static HttpRequest from(InputStream inputStream) throws IOException {
+        List<String> strings = readInputStream(inputStream);
+        String[] headers = strings.getFirst().split(" ");
+        String method = headers[0];
+        String uri = headers[1];
+        return new HttpRequest(HttpMethod.parse(method), uri);
     }
 
-    /*
-    private byte[] readHttpRequest(HttpRequest request) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(request));
-        StringBuilder requestBuilder = new StringBuilder();
-        String line;
+    private static List<String> readInputStream(InputStream inputStream) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
+        List<String> result = new ArrayList<>();
+        String line;
         while ((line = reader.readLine()) != null) {
-            requestBuilder.append(line).append("\r\n");
+            result.add(line);
             if (line.isEmpty()) {
                 break;
             }
         }
 
-        return requestBuilder.toString().getBytes();
+        return result;
     }
-     */
 
     public enum HttpMethod {
         GET,
@@ -34,6 +41,15 @@ public record HttpRequest(
         PUT,
         PATCH,
         DELETE,
-        OPTIONS
+        OPTIONS,
+        ;
+
+        public static HttpMethod parse(String str) {
+            return Arrays.stream(HttpMethod.values())
+                    .filter(value -> value.name().equalsIgnoreCase(str))
+                    .findFirst()
+                    .orElseThrow();
+            // TODO : 명확한 예외 타입 사용
+        }
     }
 }

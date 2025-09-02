aa53ef939f0da1b2e99ebdef0271e6681377fa7d
Dompoo <dlckdrms0517@gmail.com>
2025-09-02 21:20:15 +0900

    feat: IndexPageServlet 구현

--- tomcat/src/main/java/com/techcourse/servlet/IndexPageServlet.java
+++ tomcat/src/main/java/com/techcourse/servlet/IndexPageServlet.java
@@ -2,6 +2,11 @@ package com.techcourse.servlet;
 
 import org.apache.coyote.HttpRequest;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
 import static org.apache.coyote.HttpRequest.HttpMethod.GET;
 
 public class IndexPageServlet implements Servlet {
@@ -13,6 +18,25 @@ public class IndexPageServlet implements Servlet {
 
     @Override
     public String handle(HttpRequest request) {
-        return "";
+        String responseBody = indexHtml();
+
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+    }
+
+    private String indexHtml() {
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("static/index.html")
+        ) {
+            if (inputStream == null) throw new IllegalStateException("파일을 찾을 수 없습니다: index.html");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            return reader.lines().collect(Collectors.joining("\n"));
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
     }
 }

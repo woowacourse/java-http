f2982f3016ff8719b0b4857f729aecf5d1ac5ee4
ChoiDongjun <adong0808@naver.com>
2025-09-03 17:51:06 +0900

    feat: /index.html에 응답 기능 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,15 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +18,8 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
+
+    public static final String STATIC_RESOURCE_PREFIX = "static";
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
 
@@ -27,20 +38,34 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final var br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String firstLine = br.readLine();
+            String[] splitFirstLine = firstLine.split(" ");
+            String resourceName = STATIC_RESOURCE_PREFIX + splitFirstLine[1];
+            System.out.println(resourceName);
+            URI resourceUri = getClass().getClassLoader().getResource(resourceName).toURI();
+            Path path = Path.of(resourceUri);
+            long contentsLength = Files.size(path);
 
-            final var responseBody = "Hello world!";
+//            String line;
+//            while(!(line = br.readLine()).isEmpty()) {
+//                String[] splitLine = line.split(" ");
+//
+//            }
+
+            final var responseBody = Files.readString(path, StandardCharsets.UTF_8);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "Content-Length: " + contentsLength + " ",
                     "",
                     responseBody);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
         }
     }

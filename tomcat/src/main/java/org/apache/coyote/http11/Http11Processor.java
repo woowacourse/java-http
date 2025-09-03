77132ab6161f9985a47041658f39b407ff2e1c81
kjyyjk <rlawnsdud920@gmail.com>
2025-09-04 00:00:18 +0900

    feat: GET /index.html 응답하기

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,12 +5,20 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.stream.Collectors;
 
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final String STATIC_RESOURCE_PATH = "static";
+    private static final String INDEX_PAGE_URI = "/index.html";
 
     private final Socket connection;
 
@@ -29,8 +37,19 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            String line;
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            String firstLineOfRequestHeader = br.readLine();
+            String requestUri = firstLineOfRequestHeader.split(" ")[1];
+            // 헤더
+            while ((line = br.readLine()) != null) {
+                if (line.equals("")) {
+                    break;
+                }
+            }
 
+            // 바디
+            var responseBody = getResponseBody(requestUri);
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",
@@ -43,5 +62,13 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String getResponseBody(String requestUri) throws IOException {
+        if (requestUri.equals("/")) {
+            return "Hello world!";
+        }
+        URL resource = getClass().getClassLoader().getResource(STATIC_RESOURCE_PATH + INDEX_PAGE_URI);
+        return new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
     }
 }

90291c7d73bd89a7b9037254395c6a3d29194845
seaniiio <cnc4750@naver.com>
2025-09-04 01:13:47 +0900

    feat: http request header를 읽어 정적 html를 반환하는 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,18 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -27,9 +33,21 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
 
-            final var responseBody = "Hello world!";
+            final List<String> requestHeader = new ArrayList<>();
+            while (reader.ready()) {
+                requestHeader.add(reader.readLine());
+            }
+            final String requestLine = requestHeader.getFirst();
+            final String requestUri = requestLine.split(" ")[1];
+
+            String responseBody = "Hello world!";
+            if (!requestUri.equals("/")) {
+                final URL resource = getClass().getClassLoader().getResource("static" + requestUri);
+                responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",

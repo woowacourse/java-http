6c78bb7376b85d45a1ad99cbf5b2e4ad741b5619
sung-june27 <kmymy27@gmail.com>
2025-09-03 19:35:32 +0900

    feat: index.html 반환 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,19 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,8 +35,19 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
-
-            final var responseBody = "Hello world!";
+            final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            final String startLine = bufferedReader.readLine();
+            final String[] split = startLine.split(" ");
+            final String requestTarget = split[1];
+            String responseBody;
+            if (requestTarget.equals("/")) {
+                responseBody = "Hello World!";
+            } else {
+                final URL resource = getClass().getClassLoader().getResource("static/" + requestTarget);
+                final Path path = Paths.get(resource.getPath());
+                final List<String> strings = Files.readAllLines(path);
+                responseBody = String.join("\r\n", strings);
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",

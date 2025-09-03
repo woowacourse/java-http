7cefef51c4a8756f35d725e625c120d74620ee79
jye326 <jye326@naver.com>
2025-09-03 22:21:44 +0900

    feat: step1 기능 추가
    1. GET /index.html 응답하기
    2. CSS 지원하기
    3. Query String 파싱

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,22 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,20 +38,49 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            String uri = br.readLine().split(" ")[1];
 
-            final var responseBody = "Hello world!";
+            if(uri.startsWith("/login")){
+                int index = uri.indexOf("?");
+                String queryString = uri.substring(index + 1);
+                uri = "/login.html";
 
+                Map<String, String> params = new HashMap<>();
+                String[] pairs = queryString.split("&");
+                for (String pair : pairs) {
+                    String[] parts = pair.split("=");
+                    if (parts.length == 2) {
+                        params.put(parts[0], parts[1]);
+                    }
+                }
+
+                String account = params.get("account");
+                String password = params.get("password");
+                User user = InMemoryUserRepository.findByAccount(account)
+                        .orElseThrow(() -> new IllegalArgumentException("그런 계정은 없다."));
+                if(user.checkPassword(password)){
+                    System.out.println(user);
+                }
+            }
+
+            final var responseBody = new String(Files.readAllBytes(getStaticPath(uri)));
+            String contentType = uri.split("\\.")[1];
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Type: text/" + contentType + ";charset=utf-8 ",
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
                     responseBody);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private Path getStaticPath(String uri) throws URISyntaxException {
+        return Paths.get(getClass().getClassLoader().getResource("static" + uri).toURI());
     }
 }

7f2055202155c34e475747e38d209ab3daec20d4
wogus <ind07152@naver.com>
2025-09-04 19:17:35 +0900

    feat: index.html 제공 기능 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,23 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.coyote.Processor;
+import org.apache.coyote.httpObject.HttpHeader;
+import org.apache.coyote.httpObject.HttpMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,19 +40,69 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final var bufferReader = new BufferedReader(new InputStreamReader(inputStream));
+            final HttpHeader httpHeader = readHttpHeader(bufferReader);
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            final HttpMethod httpMethod = httpHeader.getHttpMethod();
+            final String path = httpHeader.getPath();
 
-            outputStream.write(response.getBytes());
-            outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+            if (httpMethod == HttpMethod.GET && path.equals("/")) {
+                responseHome(outputStream);
+            }
+
+            if (httpMethod == HttpMethod.GET && path.equals("/index.html")) {
+                responseIndexHtml(outputStream);
+            }
+
+        } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private void responseIndexHtml(final OutputStream outputStream) throws URISyntaxException, IOException {
+        final URI uri = getClass().getClassLoader()
+                .getResource("static/index.html")
+                .toURI();
+        final Path htmlPath = Path.of(uri);
+        final byte[] read = Files.readAllBytes(htmlPath);
+        final String body = new String(read, StandardCharsets.UTF_8);
+
+        final var response = String.join(
+                "\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + body.getBytes().length + " ",
+                "",
+                body
+        );
+        outputStream.write(response.getBytes());
+        outputStream.flush();
+    }
+
+    private HttpHeader readHttpHeader(final BufferedReader bufferReader) throws IOException {
+        final String requestLine = bufferReader.readLine();
+        final List<String> headers = new ArrayList<>();
+        String headerLine;
+        while ((headerLine = bufferReader.readLine()) != null && !headerLine.isEmpty()) {
+            headers.add(headerLine);
+        }
+
+        return new HttpHeader(requestLine, headers);
+    }
+
+    private void responseHome(final OutputStream outputStream) throws IOException {
+        final var responseBody = "Hello world!";
+
+        final var response = String.join(
+                "\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody
+        );
+
+        outputStream.write(response.getBytes());
+        outputStream.flush();
     }
 }

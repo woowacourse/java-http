5a837c7067e954d327a7d3e486cbe4774f70aa25
Minuring <mw9505@gmail.com>
2025-09-04 13:11:21 +0900

    feat: index.html 응답

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,16 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,7 +33,8 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final var httpRequestMessage = readMessage(inputStream);
+            final var responseBody = createResponseBody(httpRequestMessage);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -43,5 +48,28 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String readMessage(final InputStream inputStream) throws IOException {
+        final var bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        final var stringBuilder = new StringBuilder();
+
+        while (true) {
+            final var line = bufferedReader.readLine();
+            if (line == null || line.isEmpty()) {
+                break;
+            }
+            stringBuilder.append(line).append("\r\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    private String createResponseBody(final String httpRequestMessage) throws IOException {
+        if (httpRequestMessage.startsWith("GET /index.html HTTP/1.1")) {
+            final var indexUrl = getClass().getClassLoader().getResource("static/index.html");
+            return Files.readString(Path.of(indexUrl.getPath()));
+        }
+
+        return "Hello world!";
     }
 }

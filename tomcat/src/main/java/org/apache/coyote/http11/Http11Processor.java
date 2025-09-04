7aedcfbcd794efb3fa34086cd3b3ce0889997428
bowook <mbosx316@naver.com>
2025-09-05 02:46:36 +0900

    feat: index.html 응답하기 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,14 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,9 +35,29 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final BufferedReader br = new BufferedReader(new InputStreamReader(inputStream)))
+        {
+            final String line = br.readLine(); //GET /index.html HTTP/1.1
+            final String[] requestValues = line.split(" "); //[GET, /index.html, HTTP/1.1]
 
-            final var responseBody = "Hello world!";
+            String filePath = requestValues[1]; // /index.html
+            final URL url = getClass().getClassLoader().getResource("static" + filePath); // static/index.html
+
+            String responseBody;
+            if (filePath.equals("/")) {
+                responseBody = "Hello world!";
+            }
+            else if (url != null ) {
+                Path path = Paths.get(url.toURI());
+                responseBody = Files.readString(path);
+            }
+            else {
+                //파일을 못찾은거니까 404 줘야징
+                URL notFoundUrl = getClass().getClassLoader().getResource("static/404.html");
+                Path path = Paths.get(notFoundUrl.toURI());
+                responseBody = Files.readString(path);
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -40,7 +68,7 @@ public class Http11Processor implements Runnable, Processor {
 
             outputStream.write(response.getBytes());
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
         }
     }

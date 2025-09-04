b63b0bac0f5a89b6b66b4edff52926b336d6bd7d
2jin2.1031 <2jin2.1031@gmail.com>
2025-09-04 17:07:57 +0900

    test: study.IOStreamTest.OutputStream_학습_테스트.OutputStream은_사용하고_나서_close_처리를_해준다 학습

--- study/src/test/java/study/IOStreamTest.java
+++ study/src/test/java/study/IOStreamTest.java
@@ -88,10 +88,12 @@ class IOStreamTest {
             final OutputStream outputStream = mock(OutputStream.class);
 
             /**
-             * todo
              * try-with-resources를 사용한다.
              * java 9 이상에서는 변수를 try-with-resources로 처리할 수 있다.
              */
+            try (OutputStream os = new DataOutputStream(outputStream)) {
+                os.write("hello".getBytes());
+            }
 
             verify(outputStream, atLeastOnce()).close();
         }

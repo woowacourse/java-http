eeebde3f0e5bb9016f8da583e91c1b83e8f0f5a9
jhan0121 <jhan0121@gmail.com>
2025-09-04 16:59:07 +0900

    feat: HttpResponseLine 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/http/response/HttpResponseLine.java
@@ -0,0 +1,22 @@
+package org.apache.coyote.http11.http.response;
+
+import org.apache.coyote.http11.http.common.startline.HttpVersion;
+
+public class HttpResponseLine {
+
+    HttpVersion version;
+    HttpStatus status;
+
+    public static HttpResponseLine of(final HttpVersion version, final HttpStatus status) {
+        return new HttpResponseLine(version, status);
+    }
+
+    private HttpResponseLine(final HttpVersion version, final HttpStatus status) {
+        this.version = version;
+        this.status = status;
+    }
+
+    public String getFormat() {
+        return version.getValue() + " " + status.getFormattedName();
+    }
+}

5c187985b0d7d20b61b0ec98192a7d3e9cc03254
yesjuhee <juhee0924@g.skku.edu>
2025-09-04 16:29:42 +0900

    refactor: HTML 응답 리팩터링
    - 메서드 분리
    - `.html` 확장자일 경우 모두 파일 응답 시도
    - 파일이 존재하지 않을 경우 `404.html` 반환

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -3,7 +3,9 @@ package org.apache.coyote.http11;
 import com.techcourse.exception.UncheckedServletException;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
@@ -15,6 +17,10 @@ import org.slf4j.LoggerFactory;
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+
+    public static final int REQUEST_URL_INDEX = 1;
+    public static final String DEFAULT_REQUEST_URL = "/";
+    public static final String DEFAULT_RESPONSE_BODY = "Hello world!";
 
     private final Socket connection;
 
@@ -34,39 +40,51 @@ public class Http11Processor implements Runnable, Processor {
                 final var inputStream = connection.getInputStream();
                 final var outputStream = connection.getOutputStream()
         ) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            String requestLine = reader.readLine();
-            String requestURL = requestLine.split(" ")[1]; // exception 처리, 상수화
-            if (requestURL.equals("/index.html")) {
-                URL resource = getClass().getClassLoader().getResource("static/index.html");
+            final String requestURL = parseRequestURL(inputStream);
 
+            if (requestURL.endsWith(".html")) {
+                final URL resource = getStaticResource(requestURL);
                 final String responseBody = new String(Files.readAllBytes(Paths.get(resource.getFile())));
-
-                final var response = String.join("\r\n",
-                        "HTTP/1.1 200 OK ",
-                        "Content-Type: text/html;charset=utf-8 ",
-                        "Content-Length: " + responseBody.getBytes().length + " ",
-                        "",
-                        responseBody);
-
-                outputStream.write(response.getBytes());
-                outputStream.flush();
+                sendResponse(responseBody, outputStream);
                 return;
             }
-
-            final var responseBody = "Hello world!";
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
-
-            outputStream.write(response.getBytes());
-            outputStream.flush();
+            sendResponse(DEFAULT_RESPONSE_BODY, outputStream);
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String parseRequestURL(final InputStream inputStream) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        try {
+            String requestLine = reader.readLine();
+            return requestLine.split(" ")[REQUEST_URL_INDEX];
+        } catch (final IOException e) {
+            log.error(e.getMessage(), e);
+        }
+        return DEFAULT_REQUEST_URL;
+    }
+
+    private URL getStaticResource(final String requestURL) {
+        URL resource = getClass().getClassLoader().getResource(String.format("static%s", requestURL));
+        if (resource == null) {
+            return getClass().getClassLoader().getResource("static/404.html");
+        }
+        return resource;
+    }
+
+    private void sendResponse(final String responseBody, final OutputStream outputStream) throws IOException {
+        final String response = createHttpResponse(responseBody);
+        outputStream.write(response.getBytes());
+        outputStream.flush();
+    }
+
+    private static String createHttpResponse(String responseBody) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
     }
 }

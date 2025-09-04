a28b2aeebbabf4e2e41b84cc5e6ec79329037fa5
jhpark1227 <junhyeok0246@naver.com>
2025-09-04 14:10:53 +0900

    feat: GET /index.html 응답하기 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,16 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,20 +32,39 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            String requestStartLine = br.readLine();
+            String url = requestStartLine.split(" ")[1];
 
-            final var responseBody = "Hello world!";
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            String response;
+            if ("/".equals(url)) {
+                response = createHtmlResponse("Hello world!");
+            } else {
+                Path staticResource = getStaticResource(url);
+                response = createHtmlResponse(Files.readString(staticResource));
+            }
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private Path getStaticResource(String url) {
+        URL resourceURL = getClass().getClassLoader().getResource("static" + url);
+        if (resourceURL == null) {
+            throw new RuntimeException("리소스가 존재하지 않습니다. 경로:" + url);
+        }
+        return Path.of(resourceURL.getFile());
+    }
+
+    private String createHtmlResponse(String body) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + body.getBytes().length + " ",
+                "",
+                body);
     }
 }

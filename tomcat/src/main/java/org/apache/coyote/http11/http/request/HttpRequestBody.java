ac64b9c161dc49851811532ea32ef9c656d82d6c
jhan0121 <jhan0121@gmail.com>
2025-09-04 16:58:42 +0900

    feat: HttpRequestBody 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/http/request/HttpRequestBody.java
@@ -0,0 +1,38 @@
+package org.apache.coyote.http11.http.request;
+
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.coyote.http11.http.common.header.HttpHeader;
+import org.apache.coyote.http11.http.common.header.HttpHeaderKey;
+
+public class HttpRequestBody {
+
+    private final String value;
+
+    public static HttpRequestBody of(final InputStream inputStream, final HttpHeader httpHeader) throws IOException {
+        validateNull(inputStream, httpHeader);
+        return new HttpRequestBody(inputStream, httpHeader);
+    }
+
+    private static void validateNull(final InputStream inputStream, final HttpHeader httpHeader) {
+        if (inputStream == null) {
+            throw new IllegalArgumentException("inputStraem은 null일 수 없습니다");
+        }
+        if (httpHeader == null) {
+            throw new IllegalArgumentException("httpHeader는 null일 수 없습니다");
+        }
+    }
+
+    private HttpRequestBody(final InputStream inputStream, final HttpHeader httpHeader) throws IOException {
+        if (!httpHeader.containsKey(HttpHeaderKey.CONTENT_LENGTH.getValue())) {
+            this.value = null;
+            return;
+        }
+        int contentLength = Integer.parseInt(httpHeader.getValue(HttpHeaderKey.CONTENT_LENGTH.getValue()));
+        this.value = new String(inputStream.readNBytes(contentLength));
+    }
+
+    public String getValue() {
+        return value;
+    }
+}

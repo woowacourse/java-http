68108b2bb810f8ad3bebcf44d2153bcf0394a003
Firedrago95 <yonghwa95@naver.com>
2025-09-04 22:14:24 +0900

    refactor: HttpRequest, HttpResponse 책임 분리 및 메서드 분리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -8,8 +8,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Optional;
 import org.apache.coyote.Processor;
@@ -35,100 +35,57 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var inputStreamReader = new InputStreamReader(inputStream,StandardCharsets.UTF_8);
-             final var bufferedReader = new BufferedReader(inputStreamReader);
+             final var bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
              final var outputStream = connection.getOutputStream()) {
 
-            final var firstLine = bufferedReader.readLine();
-            var responseBody = "Hello world!";
-            var contentType = "text/html;charset=utf-8";
+            HttpRequest request = new HttpRequest(bufferedReader);
+            HttpResponse response = new HttpResponse(outputStream);
+            final String path = request.getPath();
 
-            if (firstLine.contains("/index.html")) {
-                final var resource = getClass().getClassLoader().getResource("static/index.html");
-                if (resource != null) {
-                    final var path = Paths.get(resource.toURI());
-                    responseBody = Files.readString(path);
-                }
+            if ("/login".equals(path)) {
+                handleLogin(request, response);
+                response.send();
+                return;
             }
 
-            if (firstLine.contains("/css/styles.css")) {
-                final var resource = getClass().getClassLoader().getResource("static/css/styles.css");
-                if (resource != null) {
-                    final var path = Paths.get(resource.toURI());
-                    responseBody = Files.readString(path);
-                    contentType = "text/css;charset=utf-8";
-                }
+            if (path.endsWith(".css")) {
+                serveStaticFile(path, response, "text/css;charset=utf-8");
+                response.send();
+                return;
             }
 
-            if (firstLine.contains("/js/scripts.js")) {
-                final var resource = getClass().getClassLoader().getResource("static/js/scripts.js");
-                if (resource != null) {
-                    final var path = Paths.get(resource.toURI());
-                    responseBody = Files.readString(path);
-                    contentType = "text/javascript;charset=utf-8";
-                }
+            if (path.endsWith(".js")) {
+                serveStaticFile(path, response, "text/javascript;charset=utf-8");
+                response.send();
+                return;
             }
 
-            if (firstLine.contains("/assets/chart-area.js")) {
-                final var resource = getClass().getClassLoader().getResource("static/assets/chart-area.js");
-                if (resource != null) {
-                    final var path = Paths.get(resource.toURI());
-                    responseBody = Files.readString(path);
-                    contentType = "text/javascript;charset=utf-8";
-                }
-            }
-
-            if (firstLine.contains("/assets/chart-bar.js")) {
-                final var resource = getClass().getClassLoader().getResource("static/assets/chart-bar.js");
-                if (resource != null) {
-                    final var path = Paths.get(resource.toURI());
-                    responseBody = Files.readString(path);
-                    contentType = "text/javascript;charset=utf-8";
-                }
-            }
-
-            if (firstLine.contains("/assets/chart-pie.js")) {
-                final var resource = getClass().getClassLoader().getResource("static/assets/chart-pie.js");
-                if (resource != null) {
-                    final var path = Paths.get(resource.toURI());
-                    responseBody = Files.readString(path);
-                    contentType = "text/javascript;charset=utf-8";
-                }
-            }
-
-            if (firstLine.contains("/login")) {
-                final var resource = getClass().getClassLoader().getResource("static/login.html");
-                if (resource != null) {
-                    final var path = Paths.get(resource.toURI());
-                    responseBody = Files.readString(path);
-                    contentType = "text/html;charset=utf-8";
-                }
-
-                String[] httpHeaderFirstLine = firstLine.split(" ");
-                String uri = httpHeaderFirstLine[1];
-                String queryString = uri.split("\\?")[1];
-                String account = queryString.split("&")[0].split("=")[1];
-                String password = queryString.split("&")[1].split("=")[1];
-                Optional<User> byAccount = InMemoryUserRepository.findByAccount(account);
-                if (byAccount.isPresent()) {
-                    User user = byAccount.get();
-                    if (user.checkPassword(password)) {
-                        log.info("user: {}", user);
-                    }
-                }
-            }
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: " + contentType + " ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
-
-            outputStream.write(response.getBytes());
-            outputStream.flush();
+            serveStaticFile("/index.html", response, "text/html;charset=utf-8");
+            response.send();
         } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
+        }
+    }
+
+    private void handleLogin(HttpRequest request, HttpResponse response) throws IOException, URISyntaxException {
+        String account = request.getQueryParam("account");
+        String password = request.getQueryParam("password");
+
+        Optional<User> userOptional = InMemoryUserRepository.findByAccount(account);
+        if (userOptional.isPresent() && userOptional.get().checkPassword(password)) {
+            log.info("user: {}", userOptional.get());
+        }
+
+        serveStaticFile("/login.html", response, "text/html;charset=utf-8");
+    }
+
+    private void serveStaticFile(String path, HttpResponse response, String contentType) throws IOException, URISyntaxException {
+        final var resource = getClass().getClassLoader().getResource("static" + path);
+        if (resource != null) {
+            final Path resourcePath = Paths.get(resource.toURI());
+            byte[] body = Files.readAllBytes(resourcePath);
+            response.setContentType(contentType);
+            response.setBody(body);
         }
     }
 }
---
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpResponse.java
@@ -0,0 +1,37 @@
+package org.apache.coyote.http11;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class HttpResponse {
+
+    private final OutputStream outputStream;
+    private String contentType = "text/html;charset=utf-8";
+    private byte[] body = new byte[0];
+
+    public HttpResponse(OutputStream outputStream) {
+        this.outputStream = outputStream;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public void setBody(byte[] body) {
+        this.body = body;
+    }
+
+    public void send() throws IOException {
+        final String responseHeaders = String.join("\r\n",
+                "HTTP/1.1 200 OK",
+                "Content-Type: " + contentType,
+                "Content-Length: " + body.length,
+                "",
+                "");
+
+        outputStream.write(responseHeaders.getBytes(StandardCharsets.UTF_8));
+        outputStream.write(body);
+        outputStream.flush();
+    }
+}

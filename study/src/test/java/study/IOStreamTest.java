734173250ec468963c1a9e821f1dcd8d3233f831
2jin2.1031 <2jin2.1031@gmail.com>
2025-09-04 17:24:41 +0900

    test: study.IOStreamTest.InputStream_학습_테스트.InputStream은_사용하고_나서_close_처리를_해준다 학습

--- study/src/test/java/study/IOStreamTest.java
+++ study/src/test/java/study/IOStreamTest.java
@@ -143,10 +143,12 @@ class IOStreamTest {
             final InputStream inputStream = mock(InputStream.class);
 
             /**
-             * todo
              * try-with-resources를 사용한다.
              * java 9 이상에서는 변수를 try-with-resources로 처리할 수 있다.
              */
+            try (InputStream is = new DataInputStream(inputStream)) {
+                is.read();
+            }
 
             verify(inputStream, atLeastOnce()).close();
         }

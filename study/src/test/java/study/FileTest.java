3a72974a0f285fd9bbdd4955e9d6921b38165101
Chaeyoung714 <202102760@hufs.ac.kr>
2025-09-04 13:14:29 +0900

    test: FileTest 수행

--- study/src/test/java/study/FileTest.java
+++ study/src/test/java/study/FileTest.java
@@ -3,8 +3,12 @@ package study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.nio.file.Path;
-import java.util.Collections;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.*;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,7 +22,7 @@ class FileTest {
 
     /**
      * resource 디렉터리 경로 찾기
-     *
+     * <p>
      * File 객체를 생성하려면 파일의 경로를 알아야 한다.
      * 자바 애플리케이션은 resource 디렉터리에 HTML, CSS 같은 정적 파일을 저장한다.
      * resource 디렉터리의 경로는 어떻게 알아낼 수 있을까?
@@ -28,26 +32,49 @@ class FileTest {
         final String fileName = "nextstep.txt";
 
         // todo
-        final String actual = "";
+        final File file = new File("/Users/ichaeyeong/Desktop/woowacourse/level3/java-http/study/src/test/resources", fileName);
+        final String actual = file.getAbsolutePath();
+
+        // 파일을 읽을 수 있다.
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         assertThat(actual).endsWith(fileName);
     }
 
     /**
      * 파일 내용 읽기
-     *
+     * <p>
      * 읽어온 파일의 내용을 I/O Stream을 사용해서 사용자에게 전달 해야 한다.
      * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws URISyntaxException, IOException {
         final String fileName = "nextstep.txt";
 
         // todo
-        final Path path = null;
+        /*
+         file의 경로여야 한다. (디렉토리면 안됨)
+         상대경로로 두면 아예 경로를 찾지 못한다 - 절대경로로 인식하는 듯하다.
+         */
+        /*
+        Path.of나 Paths.get을 통해 경로를 조각조각 붙일 수 있다.
+         */
+        final Path path = Path.of("/Users/ichaeyeong/Desktop/woowacourse/level3/java-http/study/src/test/resources", fileName);
 
         // todo
-        final List<String> actual = Collections.emptyList();
+        final List<String> actual = Files.readAllLines(path);
+
+        // File을 대상으로 CRUD할 수 있다.
+        final Path newPath = Path.of("/Users/ichaeyeong/Desktop/woowacourse/level3/java-http/study/src/test/resources", "copy.txt");
+        Files.copy(path, newPath, StandardCopyOption.REPLACE_EXISTING);
+        Files.writeString(newPath, "copy of nextstep");
 
         assertThat(actual).containsOnly("nextstep");
     }

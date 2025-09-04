e62c2880d1848706db342d82c08158e9bad72b8d
jhan0121 <jhan0121@gmail.com>
2025-09-04 16:59:16 +0900

    feat: HttpResponseBody 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/http/response/HttpResponseBody.java
@@ -0,0 +1,57 @@
+package org.apache.coyote.http11.http.response;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+public class HttpResponseBody {
+
+    private final String value;
+
+    public static HttpResponseBody emptyBody() {
+        String emptyBody = null;
+        return new HttpResponseBody(emptyBody);
+    }
+
+    public static HttpResponseBody withString(String value) {
+        validateNull(value);
+        return new HttpResponseBody(value);
+    }
+
+    private HttpResponseBody(final String value) {
+        final URL url = getClass().getClassLoader().getResource("static/" + value);
+
+        if (url == null) {
+            this.value = value;
+            return;
+        }
+
+        try {
+            Path path = Paths.get(url.toURI());
+            this.value = new String(Files.readAllBytes(path));
+        } catch (IOException | URISyntaxException e) {
+            throw new IllegalArgumentException("response 구성 중 문제가 발생하였습니다", e);
+        }
+    }
+
+    private static void validateNull(final String value) {
+        if (value == null) {
+            throw new IllegalArgumentException("문자열은 null일 수 없습니다");
+        }
+    }
+
+    public Optional<String> getValue() {
+        return Optional.of(value);
+    }
+
+    public int getByteLength() {
+        if (value == null) {
+            throw new IllegalArgumentException("body 내용물이 존재하지 않습니다");
+        }
+        return value.getBytes().length;
+    }
+}

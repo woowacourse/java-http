a670ec4930ce15198405004811383766982ec9ab
kimdev <kim__dev@hufs.ac.kr>
2025-09-04 17:26:52 +0900

    feat: 정적 파일 GET 요청 처리 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,15 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,18 +38,31 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String[] methodAndUriAndProtocol = bufferedReader.readLine().split(" ");
+            String method = methodAndUriAndProtocol[0];
+            String uri = methodAndUriAndProtocol[1];
 
-            final var response = String.join("\r\n",
+            if (method.equals("GET")) {
+                String responseBody;
+                if (uri == null || uri.equals("/")) {
+                    responseBody = "Hello world!";
+                } else {
+                    Path path = Path.of(getClass().getClassLoader().getResource("static" + uri).toURI());
+                    responseBody = new String(Files.readAllBytes(path));
+                }
+
+                final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
                     responseBody);
 
-            outputStream.write(response.getBytes());
-            outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+            }
+        } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
         }
     }

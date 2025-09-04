41cc0ec0df43fe2a71c16720a2174a311be76d2a
changui <122252160+changuii@users.noreply.github.com>
2025-09-04 19:03:19 +0900

    feat: 3단계 완료

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,25 +1,26 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.Socket;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -47,13 +48,56 @@ public class Http11Processor implements Runnable, Processor {
                 return;
             }
 
+            String uriAndQueryParameters = requestValues.get("Uri");
+            String uri = uriAndQueryParameters;
+
+            Map<String, String> queryParameters = new HashMap<>();
+            int queryParameterIndex = uriAndQueryParameters.indexOf("?");
+            if (queryParameterIndex != -1) {
+                uri = uriAndQueryParameters.substring(0, queryParameterIndex);
+                String queryParameterTexts = uriAndQueryParameters.substring(queryParameterIndex + 1,
+                        uriAndQueryParameters.length());
+
+                for (String queryParameterText : queryParameterTexts.split("&")){
+                    String[] keyAndValue = queryParameterText.split("=");
+                    queryParameters.put(keyAndValue[0], keyAndValue[1]);
+                }
+            }
+
+            if("/login".equals(uri) && !queryParameters.isEmpty()){
+                String account = queryParameters.getOrDefault("account", "");
+                String password = queryParameters.getOrDefault("password", "");
+
+                Optional<User> optionalUser = InMemoryUserRepository.findByAccount(account);
+                if(optionalUser.isEmpty()){
+                    outputStream.write("HTTP/1.1 401 UNAUTHORIZED ".getBytes());
+                    outputStream.flush();
+                    return;
+                }
+
+                User user = optionalUser.get();
+                if(!user.checkPassword(password)){
+                    outputStream.write("HTTP/1.1 401 UNAUTHORIZED ".getBytes());
+                    outputStream.flush();
+                    return;
+                }
+
+                log.info("user: "+ user);
+                return;
+            }
+
             ClassLoader classLoader = getClass().getClassLoader();
-            URL url = classLoader.getResource(requestValues.get("Uri"));
+            URL url = classLoader.getResource("static" + uri);
+
+            if (url == null) {
+                url = classLoader.getResource("static" + uri + ".html");
+            }
 
             var responseBody = "Hello world!";
-            var contentType = "";
+            var contentType = "text/html";
             if(url != null){
                 Path resourcePath = Path.of(url.toURI());
+
                 if(Files.isRegularFile(resourcePath)){
                     List<String> resourceValues = Files.readAllLines(resourcePath);
                     responseBody = resourceValues.stream()
@@ -85,7 +129,7 @@ public class Http11Processor implements Runnable, Processor {
 
         String[] requestLine = firstLine.split(" ");
         values.put("Method", requestLine[0]);
-        values.put("Uri", "static" + requestLine[1]);
+        values.put("Uri", requestLine[1]);
         values.put("Version", requestLine[2]);
 
         String line = bufferedReader.readLine();

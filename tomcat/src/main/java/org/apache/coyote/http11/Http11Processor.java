8d44d64a2caa4ed420fe162e060450234757a6a6
LeeJaeHoon <dlwogns3413@naver.com>
2025-09-04 17:47:34 +0900

    feat: login query String 파싱 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,8 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,6 +10,8 @@ import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -53,6 +57,45 @@ public class Http11Processor implements Runnable, Processor {
             }
             if (requestMethod.equals("GET") && requestUrl.equals("/index.html")) {
                 URL resource = getClass().getClassLoader().getResource("static/index.html");
+                Path resourcePath = Path.of(resource.getPath());
+                byte[] bytes = Files.readAllBytes(resourcePath);
+
+                response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + bytes.length + " ",
+                        "",
+                        new String(bytes));
+            }
+            if (requestMethod.equals("GET") && requestUrl.equals("/css/styles.css")) {
+                URL resource = getClass().getClassLoader().getResource("static/css/styles.css");
+                Path resourcePath = Path.of(resource.getPath());
+                byte[] bytes = Files.readAllBytes(resourcePath);
+
+                response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/css;charset=utf-8 ",
+                        "Content-Length: " + bytes.length + " ",
+                        "",
+                        new String(bytes));
+            }
+            if (requestMethod.equals("GET") && requestUrl.equals("/login?account=gugu&password=password")) {
+                String uri = "/login?account=gugu&password=password";
+                int index = uri.indexOf("?");
+                String path = uri.substring(0, index);
+                String queryStrings = uri.substring(index + 1);
+                Map<String, String> queryStringMap = new HashMap<>();
+                for (String queryString : queryStrings.split("&")) {
+                    String[] strings = queryString.split("=");
+                    String key = strings[0];
+                    String value = strings[1];
+                    queryStringMap.put(key, value);
+                }
+                User user = InMemoryUserRepository.findByAccount(queryStringMap.get("account")).get();
+                if (user.checkPassword(queryStringMap.get("password"))) {
+                    log.info("user = {}", user);
+                }
+                URL resource = getClass().getClassLoader().getResource("static/login.html");
                 Path resourcePath = Path.of(resource.getPath());
                 byte[] bytes = Files.readAllBytes(resourcePath);
 

b76a1776f5e937a897f99deab73b317ceefdc544
seaniiio <cnc4750@naver.com>
2025-09-05 00:22:43 +0900

    refactor: process() 메서드 분리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -7,8 +7,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -47,53 +49,49 @@ public class Http11Processor implements Runnable, Processor {
             httpRequest.parseHttpRequest();
             final String requestPath = httpRequest.getRequestPath();
 
-            URL resource = null;
+            if (requestPath.equals("/")) {
+                final HttpResponse response = HttpResponse.createWelcomeHttpResponse();
+                sendHttpResponse(response, outputStream);
+                return;
+            }
 
             if (requestPath.equals("/login")) {
-                resource = getStaticResource("/login.html");
-
-                if (httpRequest.isQueryStringExists()) {
-                    final Map<String, String> parameters = httpRequest.getQueryParameters();
-                    final String account = parameters.get("account");
-                    final String password = parameters.get("password");
-                    InMemoryUserRepository.findByAccount(account)
-                            .ifPresent(user -> {
-                                if (user.checkPassword(password)) {
-                                    log.info("user: " + user);
-                                }
-                            });
-                }
+                final URL resource = getStaticResource("/login.html");
+                final HttpResponse response = getHttpResponse(resource);
+                sendHttpResponse(response, outputStream);
+                logUserInformationIfExists(httpRequest);
+                return;
             }
 
-            if (!requestPath.equals("/") && !requestPath.equals("/login")) {
-                resource = getStaticResource(httpRequest.getRequestPath());
-            }
+            final URL resource = getStaticResource(httpRequest.getRequestPath());
+            final HttpResponse response = getHttpResponse(resource);
+            sendHttpResponse(response, outputStream);
 
-            HttpResponse response = HttpResponse.createWelcomeHttpResponse();
-            if (resource != null) {
-                final String responseLine = "HTTP/1.1 200 OK";
-                final byte[] responseBody = readFile(resource);
-                final LinkedHashMap<String, String> responseHeaders = new LinkedHashMap<>();
-                responseHeaders.put("Content-Type", getMimeType(resource));
-                responseHeaders.put("Content-Length", String.valueOf(responseBody.length));
-
-                response = new HttpResponse(responseLine, responseHeaders, responseBody);
-            }
-
-            String parsedResponse = response.parseHttpResponse();
-            outputStream.write(parsedResponse.getBytes());
-            outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
     }
 
-    private URL getStaticResource(final String path) throws FileNotFoundException {
+    private URL getStaticResource(final String path) throws IOException {
         final URL resource = getClass().getClassLoader().getResource("static" + path);
         if (resource == null) {
             throw new FileNotFoundException();
         }
         return resource;
+    }
+
+    private void logUserInformationIfExists(HttpRequest httpRequest) {
+        if (httpRequest.isQueryStringExists()) {
+            final Map<String, String> parameters = httpRequest.getQueryParameters();
+            final String account = parameters.get("account");
+            final String password = parameters.get("password");
+            InMemoryUserRepository.findByAccount(account)
+                    .ifPresent(user -> {
+                        if (user.checkPassword(password)) {
+                            log.info("user: " + user);
+                        }
+                    });
+        }
     }
 
     private byte[] readFile(final URL resource) throws IOException {
@@ -103,5 +101,21 @@ public class Http11Processor implements Runnable, Processor {
     private String getMimeType(final URL resource) {
         final String responseResourceExtension = resource.getPath().split("\\.")[1];
         return MIME_TYPES.get(responseResourceExtension);
+    }
+
+    private HttpResponse getHttpResponse(final URL resource) throws IOException {
+        final String responseLine = "HTTP/1.1 200 OK";
+        final byte[] responseBody = readFile(resource);
+        final LinkedHashMap<String, String> responseHeaders = new LinkedHashMap<>();
+        responseHeaders.put("Content-Type", getMimeType(resource));
+        responseHeaders.put("Content-Length", String.valueOf(responseBody.length));
+
+        return new HttpResponse(responseLine, responseHeaders, responseBody);
+    }
+
+    private void sendHttpResponse(final HttpResponse response, final OutputStream outputStream) throws IOException {
+        final String parsedResponse = response.parseHttpResponse();
+        outputStream.write(parsedResponse.getBytes(StandardCharsets.UTF_8));
+        outputStream.flush();
     }
 }

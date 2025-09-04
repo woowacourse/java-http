25fbf3e3e3a3709b34078eb00059fdd1555f0c09
eunseongu <rndmstjs75@gmail.com>
2025-09-04 13:56:22 +0900

    feat: 1단계 요구사항 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,23 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,19 +40,77 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String startLine = bufferedReader.readLine();
+            String rawPath = startLine.split(" ")[1];
+            String responseBody;
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            if (rawPath.equals("/")) {
+                responseBody = "Hello world!";
+                writeResponse(responseBody, "html", outputStream);
+            }
 
-            outputStream.write(response.getBytes());
-            outputStream.flush();
+            if (rawPath.contains("?")) {
+                int index = rawPath.indexOf("?");
+                Map<String, String> queryParams = parseQueryParams(rawPath, index);
+                Optional<User> user = InMemoryUserRepository.findByAccount(queryParams.get("account"));
+                System.out.println("user = " + user);
+
+                rawPath = rawPath.substring(0, index) + ".html";
+            }
+
+            String contentType = getContentType(rawPath);
+            Path filePath = getFilePath(rawPath);
+            String header = getHeader(bufferedReader);
+
+            responseBody = new String(Files.readAllBytes(filePath));
+            writeResponse(responseBody, contentType, outputStream);
+
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private Map<String, String> parseQueryParams(String path, int index) {
+        String queryString = path.substring(index + 1);
+        String[] queries = queryString.split("&");
+
+        Map<String, String> queryParams = new HashMap<>();
+        for (String query : queries) {
+            String[] paramPair = query.split("=");
+            queryParams.put(paramPair[0], paramPair[1]);
+        }
+        return queryParams;
+    }
+
+    private String getContentType(String path) {
+        String[] split = path.split("\\.");
+        return split[split.length - 1];
+    }
+
+    private Path getFilePath(String path) {
+        URL resource = getClass().getClassLoader().getResource("static" + path);
+        return new File(resource.getFile()).toPath();
+    }
+
+    private String getHeader(BufferedReader bufferedReader) throws IOException {
+        String line;
+        StringBuilder sb = new StringBuilder();
+        while ((line = bufferedReader.readLine()) != null && !line.isEmpty()) {
+            sb.append(line).append("\r\n");
+        }
+        return sb.toString();
+    }
+
+    private void writeResponse(String responseBody, String contentType, OutputStream outputStream) throws IOException {
+        String response = String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: " + "text/" + contentType + ";charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+
+        outputStream.write(response.getBytes());
+        outputStream.flush();
     }
 }

a5a3a0814c393424ef73aca56794b86a45178748
2jin2.1031 <2jin2.1031@gmail.com>
2025-09-04 13:32:19 +0900

    test: study.FileTest.resource_디렉터리에_있는_파일의_경로를_찾는다 학습

--- study/src/test/java/study/FileTest.java
+++ study/src/test/java/study/FileTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import java.io.File;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -27,8 +28,7 @@ class FileTest {
     void resource_디렉터리에_있는_파일의_경로를_찾는다() {
         final String fileName = "nextstep.txt";
 
-        // todo
-        final String actual = "";
+        final String actual = new File(fileName).getName();
 
         assertThat(actual).endsWith(fileName);
     }

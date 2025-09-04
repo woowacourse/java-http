3c28ca61d6e32e8e2232c62e79e32922e1e9dcf3
songsunkook <songsunkook@gmail.com>
2025-09-04 15:26:54 +0900

    feat: HttpMethod 열거형 추가 및 파라미터 파싱 로직 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -8,9 +8,10 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
@@ -40,40 +41,54 @@ public class Http11Processor implements Runnable, Processor {
              final var outputStream = connection.getOutputStream()) {
             StringBuilder requestBuilder = new StringBuilder();
             String line;
-            java.io.BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-            
+            java.io.BufferedReader reader = new BufferedReader(
+                new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+
             while ((line = reader.readLine()) != null) {
                 if (line.isEmpty()) {
                     break;
                 }
                 requestBuilder.append(line).append("\r\n");
             }
-            
+
             String request = requestBuilder.toString();
             String header = request.split("\r\n")[0];
             String[] words = header.split(" ");
             log.info(header);
-            URL url = getClass().getClassLoader().getResource("static" + words[1]);
-            if (url == null) {
+
+            HttpMethod httpMethod = HttpMethod.from(words[0]);
+            String[] url = words[1].split("\\?");
+            String path = url[0];
+            String param = url.length > 1 ? url[1] : null;
+            if (param != null) {
+                Map<String, String> params = new HashMap<>();
+                for (var p : param.split("&")) {
+                    String key = p.split("=")[0];
+                    String value = p.split("=")[1];
+                    params.put(key, value);
+                }
+            }
+
+            URL resource = getClass().getClassLoader().getResource("static" + path);
+            if (resource == null) {
                 log.info("Resource not found!");
                 return;
             }
-            Path path = Paths.get(url.toURI());
-            List<String> strings = Files.readAllLines(path);
+            List<String> strings = Files.readAllLines(Paths.get(resource.toURI()));
 
             StringBuilder sb = new StringBuilder();
-            for(var str : strings) {
+            for (var str : strings) {
                 sb.append(str + "\n");
             }
 
             final var responseBody = sb.toString();
 
             final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
 
             outputStream.write(response.getBytes());
             outputStream.flush();

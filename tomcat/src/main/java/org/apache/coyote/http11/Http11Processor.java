8c12933816c3cc76142305a32d582789e986bc9a
kjyyjk <rlawnsdud920@gmail.com>
2025-09-04 23:29:21 +0900

    refactor: 전체 구조 리팩터링

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,5 +1,7 @@
 package org.apache.coyote.http11;
 
+import static java.util.stream.Collectors.toUnmodifiableMap;
+
 import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
 import com.techcourse.model.User;
@@ -11,21 +13,21 @@ import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Optional;
+
 
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
     private static final String STATIC_RESOURCE_PATH = "static";
-    private static final String REQUEST_URI_DELIMITER = "?";
-    private static final String QUERY_STRING_DELIMITER = "&";
-    private static final String END_OF_REQUEST_HEADER = "";
 
     private final Socket connection;
 
@@ -42,46 +44,27 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+        try (final InputStream inputStream = connection.getInputStream();
+             final OutputStream outputStream = connection.getOutputStream();
+             final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            // 요청 헤더
+            String requestLine = bufferedReader.readLine();
 
-            // 헤더
-            String firstLineOfRequestHeader = br.readLine();
-            String requestUri = firstLineOfRequestHeader.split(" ")[1];
-            String line;
-            while ((line = br.readLine()) != null) {
-                if (line.equals(END_OF_REQUEST_HEADER)) {
-                    break;
-                }
-            }
+            // 요청 헤더 파싱
+            String requestUri = requestLine.split(" ")[1];
+            String requestUriPath = getRequestUriPath(requestUri);
+            Map<String, String> queryParameters = getQueryParameters(requestUri);
 
-            int index = requestUri.lastIndexOf(REQUEST_URI_DELIMITER);
-
-            String requestUriPath = requestUri;
-            Map<String, String> keyValues = new HashMap<>();
-            if (index != -1) {
-                requestUriPath = requestUri.substring(0, index);
-                // keyValues 저장;
-                String queryString = requestUri.substring(index + 1);
-                for (String keyValue : queryString.split(QUERY_STRING_DELIMITER)) {
-                    String[] split = keyValue.split("=");
-                    keyValues.put(split[0], split[1]);
-                }
-            }
-
-            // 바디
-            var staticFileName = getStaticFileName(requestUriPath, keyValues);
-            var responseBody = getResponseBody(staticFileName);
-            var contentType = getContentType(staticFileName);
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: %s;charset=utf-8 ".formatted(contentType),
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            // 응답
+            String responseBody = getResponseBody(requestUriPath, queryParameters);
+            String contentType = getContentType(requestUriPath);
+            final String response = String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: %s;charset=utf-8 ".formatted(contentType),
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
@@ -89,46 +72,72 @@ public class Http11Processor implements Runnable, Processor {
         }
     }
 
-    private String getStaticFileName(String requestUriPath, Map<String, String> keyValues) throws IOException {
-        if (requestUriPath.equals("/")) {
-            return "";
+    private String getRequestUriPath(String requestUri) {
+        int index = requestUri.lastIndexOf("?");
+        if (index == -1) {
+            return requestUri;
         }
-        if (requestUriPath.equals("/login")) {
-            return login(keyValues);
-        }
-
-        return requestUriPath;
+        return requestUri.substring(0, requestUri.lastIndexOf("?"));
     }
 
-    private String getResponseBody(String staticFileName) throws IOException {
-        if (staticFileName.isBlank()) {
+    private Map<String, String> getQueryParameters(String requestUri) {
+        int index = requestUri.lastIndexOf("?");
+        if (index == -1) {
+            return Map.of();
+        }
+        String queryString = requestUri.substring(index + 1);
+        return Arrays.stream(queryString.split("&"))
+            .map(parameter -> parameter.split("="))
+            .collect(toUnmodifiableMap(keyValue -> keyValue[0], keyValue -> keyValue[1]));
+    }
+
+    private String getResponseBody(String requestUriPath, Map<String, String> keyValues) throws IOException {
+        if (requestUriPath.equals("/")) {
             return "Hello world!";
         }
-
-        String staticFilePath = STATIC_RESOURCE_PATH + "/" + staticFileName;
-        URL resource = getClass().getClassLoader().getResource(staticFilePath);
-        return new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+        if (requestUriPath.equals("/login")) {
+            login(keyValues);
+            return readStaticFile("/login.html");
+        }
+        return readStaticFile(requestUriPath);
     }
 
-    private String login(Map<String, String> keyValues) {
+    private void login(Map<String, String> keyValues) {
         String account = keyValues.get("account");
         String password = keyValues.get("password");
         if (account != null && password != null) {
             Optional<User> findUser = InMemoryUserRepository.findByAccount(account);
             boolean isValidAccount = findUser.isPresent();
             if (!isValidAccount) {
-                return null;
+                throw new IllegalArgumentException("Invalid account " + account);
             }
             User user = findUser.get();
             log.atInfo().log("user: {}", user);
         }
-        return "login.html";
     }
 
-    private String getContentType(String staticFileName) {
-        if (staticFileName.endsWith(".css")) {
+    private String readStaticFile(String filePath) throws IOException {
+        String staticFilePath = STATIC_RESOURCE_PATH + filePath;
+        URL resource = getClass().getClassLoader().getResource(staticFilePath);
+        return new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+    }
+
+    private String getContentType(String requestUriPath) throws IOException {
+        if (requestUriPath.equals("/")) {
+            return "text/html";
+        }
+        if (requestUriPath.equals("/login")) {
+            return "text/html";
+        }
+        if (requestUriPath.endsWith(".css")) {
             return "text/css";
         }
-        return "text/html";
+        if (requestUriPath.endsWith(".html")) {
+            return "text/html";
+        }
+        if (requestUriPath.endsWith(".js")) {
+            return "text/javascript";
+        }
+        throw new IllegalArgumentException("지원하지 않는 요청 uri 입니다. " + requestUriPath);
     }
 }

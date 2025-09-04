5941e75426112072438ccec6e07df1ddce034685
rladmstn <oz33177351@gmail.com>
2025-09-04 23:23:28 +0900

    feat: 로그인 페이지 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,8 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -8,6 +10,10 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,45 +41,88 @@ public class Http11Processor implements Runnable, Processor {
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
             String line = reader.readLine();
-            if (line == null) { return; }
+            if (line == null) {
+                return;
+            }
 
             String[] request = line.split(" ");
             String httpMethod = request[0];
             String uri = request[1];
             String protocol = request[2];
 
-            if(httpMethod.equals("GET")){
-                final var contentType = parseContentType(uri);
+            final var contentType = parseContentType(uri);
+
+            if (httpMethod.equals("GET") && uri.startsWith("/login")) {
+
+                int index = uri.indexOf("?");
+                String path = uri.substring(0, index);
+                Map<String, String> queryString = parseQueryString(uri.substring(index + 1));
+
+                final var body = getResponseBody(path);
+
+                Optional<User> user = InMemoryUserRepository.findByAccount(queryString.get("account"));
+                if (user.isPresent() && user.get().checkPassword(queryString.get("password"))) {
+                    log.info("user: {}", user.get());
+                }
+
+                final var response = createHttpResponse(body, contentType);
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+            } else if (httpMethod.equals("GET")) {
                 final var body = getResponseBody(uri);
 
                 final var response = createHttpResponse(body, contentType);
                 outputStream.write(response.getBytes());
                 outputStream.flush();
             }
+
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
     }
 
-    private String getResponseBody(String uri) throws IOException {
-        URL resource = getResource(uri);
-        if(resource == null){
+    private Map<String, String> parseQueryString(String queryString) {
+        String[] parameters = queryString.split("&");
+        Map<String, String> queryParameters = new HashMap<>();
+
+        Arrays.stream(parameters)
+                .forEach(parameter -> {
+                            String[] split = parameter.split("=");
+                            queryParameters.put(split[0], split[1]);
+                        }
+                );
+
+        return queryParameters;
+    }
+
+    private String getResponseBody(String path) throws IOException {
+        URL resource = getResource(path);
+        if (resource == null) {
             return "Hello world!";
         }
         return new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
     }
 
-    private URL getResource(final String uri) {
-        if(uri.equals("/")) {
-            return null;
+    private URL getResource(String path) {
+        String targetPath;
+
+        if (path.equals("/")) {
+            targetPath = "/index.html";
+        } else if (path.equals("/login")) {
+            targetPath = "/login.html";
+        } else {
+            targetPath = path;
         }
-        return getClass().getClassLoader().getResource("static" + uri);
+
+        return getClass().getClassLoader().getResource("static" + targetPath);
     }
 
     private String parseContentType(String uri) {
-        if(uri.endsWith(".css")){
+        if (uri.endsWith(".css")) {
             return "text/css;charset=utf-8";
-        } else{
+        } else if (uri.endsWith(".js")) {
+            return "application/javascript;charset=utf-8";
+        } else {
             return "text/html;charset=utf-8";
         }
     }

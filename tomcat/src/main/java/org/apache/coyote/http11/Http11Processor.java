065ba1e191be500dfcdc39fabc313f5eb26d6f4c
kjyyjk <rlawnsdud920@gmail.com>
2025-09-04 11:05:00 +0900

    feat: Query String 파싱

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,9 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
+
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,13 +15,18 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.Optional;
 
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
     private static final String STATIC_RESOURCE_PATH = "static";
+    private static final String REQUEST_URI_DELIMITER = "?";
+    private static final String QUERY_STRING_DELIMITER = "&";
+    private static final String END_OF_REQUEST_HEADER = "";
 
     private final Socket connection;
+
 
     public Http11Processor(final Socket connection) {
         this.connection = connection;
@@ -41,14 +49,49 @@ public class Http11Processor implements Runnable, Processor {
             String requestUri = firstLineOfRequestHeader.split(" ")[1];
             // 헤더
             while ((line = br.readLine()) != null) {
-                if (line.equals("")) {
+                if (line.equals(END_OF_REQUEST_HEADER)) {
                     break;
                 }
             }
 
+            String requestUriPath = requestUri;
+            int index = requestUri.lastIndexOf(REQUEST_URI_DELIMITER);
+            if (index != -1) {
+                requestUriPath = requestUri.substring(0, index);
+                String queryString = requestUri.substring(index + 1);
+                String[] keyValues = queryString.split(QUERY_STRING_DELIMITER);
+                String account = null;
+                String password = null;
+                for (String keyValue : keyValues) {
+                    String[] split = keyValue.split("=");
+                    String key = split[0];
+                    String value = split[1];
+                    if (key.equals("account")) {
+                        account = value;
+                    }
+                    if (key.equals("password")) {
+                        password = value;
+                    }
+                }
+
+                if (account != null && password != null) {
+                    Optional<User> findUser = InMemoryUserRepository.findByAccount(account);
+                    boolean isValidAccount = findUser.isPresent();
+                    if (!isValidAccount) {
+                        return;
+                    }
+                    User user = findUser.get();
+                    log.atInfo().log("user: {}", user);
+                }
+            }
+
+            if (requestUriPath.equals("/login")) {
+                requestUriPath+=".html";
+            }
+
             // 바디
-            var responseBody = getResponseBody(requestUri);
-            var contentType = getContentType(requestUri);
+            var responseBody = getResponseBody(requestUriPath);
+            var contentType = getContentType(requestUriPath);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -63,21 +106,21 @@ public class Http11Processor implements Runnable, Processor {
         }
     }
 
-    private String getResponseBody(String requestUri) throws IOException {
-        if (requestUri.equals("/")) {
+    private String getResponseBody(String requestUriPath) throws IOException {
+        if (requestUriPath.equals("/")) {
             return "Hello world!";
         }
-        URL resource = getClass().getClassLoader().getResource(STATIC_RESOURCE_PATH + requestUri);
+        URL resource = getClass().getClassLoader().getResource(STATIC_RESOURCE_PATH + requestUriPath);
         return new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
     }
 
-    private String getContentType(String requestUri) {
-        if (requestUri.endsWith(".css")) {
+    private String getContentType(String requestUriPath) {
+        if (requestUriPath.endsWith(".css")) {
             return "text/css";
         }
-        if (requestUri.endsWith(".html")) {
+        if (requestUriPath.endsWith(".html")) {
             return "text/html";
         }
-        return "text/plain";
+        return "text/html";
     }
 }

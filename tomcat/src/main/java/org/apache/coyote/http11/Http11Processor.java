d912add0eb0d52e0c0469b7752c0c8eaf02cd954
ljhee92 <weilainib7@gmail.com>
2025-09-04 20:56:35 +0900

    feat: GET /index.html 응답하기

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,17 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,7 +34,18 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+
+            final String firstLineOfHeader = br.readLine();
+            final String requestUri = firstLineOfHeader.split(" ")[1];
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.isEmpty()) {
+                    break;
+                }
+            }
+
+            final String responseBody = getResponseBody(requestUri);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -43,5 +59,17 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String getResponseBody(final String requestUri) throws IOException {
+        if (requestUri.equals("/")) {
+            return "Hello world!";
+        }
+
+        final String fileName = "static" + requestUri;
+        final URL resource  = getClass().getClassLoader().getResource(fileName);
+        final File file = new File(resource.getFile());
+        final Path path = file.toPath();
+        return new String(Files.readAllBytes(path));
     }
 }

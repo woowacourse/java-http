9fac299824fe96c9d6731c66991d8ba5bc1bdd26
황재웅 <wodnd0131@naver.com>
2025-09-04 18:49:10 +0900

    feat: html 띄우기

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,8 +1,15 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,19 +35,41 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            final var response = getResponse(inputStream);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    private String getResponse(InputStream inputStream) throws IOException, URISyntaxException {
+        final var bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        final var stringBuilder = new StringBuilder();
+
+        while (bufferedReader.ready()) {
+            final var readLine = bufferedReader.readLine();
+            stringBuilder.append(readLine).append("\r\n");
+        }
+
+        final var request = stringBuilder.toString();
+        log.info("request : {}", request);
+        final var requestSplit = request.split(" ");
+        final var resourcePath = requestSplit[1];
+        final var resource = getClass().getClassLoader().getResource("static" + resourcePath);
+        final var path = Paths.get(Objects.requireNonNull(resource).toURI());
+
+        final var responseBody = Files.readString(path);
+        log.info("responseBody : {}", responseBody);
+
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
     }
 }

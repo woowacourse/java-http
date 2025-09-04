e8fd58bad92b9b3ff727f2cad8b36ac5c4c4268a
wogus <ind07152@naver.com>
2025-09-04 19:15:47 +0900

    feat: HttpMethod enum 클래스 생성

---
+++ tomcat/src/main/java/org/apache/coyote/httpObject/HttpMethod.java
@@ -0,0 +1,27 @@
+package org.apache.coyote.httpObject;
+
+import java.util.Arrays;
+
+public enum HttpMethod {
+
+    GET("GET"),
+    POST("POST"),
+    PUT("PUT"),
+    PATCH("PATCH"),
+    DELETE("DELETE"),
+    HEAD("HEAD"),
+    OPTIONS("OPTIONS");
+
+    private final String method;
+
+    HttpMethod(final String method) {
+        this.method = method;
+    }
+
+    public static HttpMethod findHttpMethod(final String input) {
+        return Arrays.stream(HttpMethod.values())
+                .filter(method -> method.method.equals(input))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않은 HTTPMethod 입니다."));
+    }
+}

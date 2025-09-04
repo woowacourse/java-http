aaf99b395c17e011042ccb7c0dbaf628cbc78a7c
Gabin Choi <choegabin171@gmail.com>
2025-09-04 17:07:57 +0900

    test: fileTest 완성

--- study/src/test/java/study/FileTest.java
+++ study/src/test/java/study/FileTest.java
@@ -1,54 +1,61 @@
 package study;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.nio.file.Path;
-import java.util.Collections;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 /**
- * 웹서버는 사용자가 요청한 html 파일을 제공 할 수 있어야 한다.
- * File 클래스를 사용해서 파일을 읽어오고, 사용자에게 전달한다.
+ * 웹서버는 사용자가 요청한 html 파일을 제공 할 수 있어야 한다. File 클래스를 사용해서 파일을 읽어오고, 사용자에게 전달한다.
  */
 @DisplayName("File 클래스 학습 테스트")
 class FileTest {
 
     /**
      * resource 디렉터리 경로 찾기
-     *
-     * File 객체를 생성하려면 파일의 경로를 알아야 한다.
-     * 자바 애플리케이션은 resource 디렉터리에 HTML, CSS 같은 정적 파일을 저장한다.
-     * resource 디렉터리의 경로는 어떻게 알아낼 수 있을까?
+     * <p>
+     * File 객체를 생성하려면 파일의 경로를 알아야 한다. 자바 애플리케이션은 resource 디렉터리에 HTML, CSS 같은 정적 파일을 저장한다. resource 디렉터리의 경로는 어떻게 알아낼 수
+     * 있을까?
      */
     @Test
     void resource_디렉터리에_있는_파일의_경로를_찾는다() {
         final String fileName = "nextstep.txt";
 
-        // todo
-        final String actual = "";
+        // 현재 클래스를 가져온다. 여기서는 FileTest
+        Class<? extends FileTest> aClass = getClass();
 
-        assertThat(actual).endsWith(fileName);
+        // 현재 클래스를 JVM에 등록한 ClassLoader를 불러온다. (reflection 사용)
+        ClassLoader classLoader = aClass.getClassLoader();
+
+        // classLoader로 classpath를 읽을 수 있음
+        // 이 때 resources들의 파일은 가장 상위(루트)에 등록됨
+        // nextstep.txt는 resources 바로 아래에 등록된 파일이라서 fileName만으로 가져올 수 있다.
+        // 만약 txt/nextstep.txt면 지금 fileName만으로는 찾을 수 없음
+        URL resource = classLoader.getResource(fileName);
+        String path = resource.getPath();
+
+        assertThat(path).endsWith(fileName);
     }
 
     /**
      * 파일 내용 읽기
-     *
-     * 읽어온 파일의 내용을 I/O Stream을 사용해서 사용자에게 전달 해야 한다.
-     * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
+     * <p>
+     * 읽어온 파일의 내용을 I/O Stream을 사용해서 사용자에게 전달 해야 한다. File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws IOException {
         final String fileName = "nextstep.txt";
 
-        // todo
-        final Path path = null;
+        final String actual = getClass().getClassLoader().getResource(fileName).getPath();
+        final Path filePath = Path.of(actual);
 
-        // todo
-        final List<String> actual = Collections.emptyList();
+        final List<String> fileLines = Files.readAllLines(filePath);
 
-        assertThat(actual).containsOnly("nextstep");
+        assertThat(fileLines).containsOnly("nextstep");
     }
 }

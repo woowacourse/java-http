ba8dea90798bb65491dacd90d45765b9f9e921a3
byesol <bsj5967@naver.com>
2025-09-05 02:44:58 +0900

    feat: GET /index.html 응답하기 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,14 @@
 package org.apache.coyote.http11;
 
-import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,18 +31,62 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final var reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            final String requestLine = reader.readLine();
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            if (requestLine == null) {
+                return;
+            }
 
-            outputStream.write(response.getBytes());
-            outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+            String uri = requestLine.split(" ")[1];
+            if ("/".equals(uri)) {
+                final var responseBody = "Hello world!";
+
+                final var response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody);
+
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+                return;
+            }
+            String resourcePath = uri.substring(1);
+            try (InputStream fileInputStream = getClass().getClassLoader()
+                    .getResourceAsStream("static/" + resourcePath)) {
+
+                if (fileInputStream == null) {
+
+                    String responseBody = "404 Not Found";
+                    byte[] bodyBytes = responseBody.getBytes(StandardCharsets.UTF_8);
+
+                    final var response = String.join("\r\n",
+                            "HTTP/1.1 404 Not Found",
+                            "Content-Type: text/plain;charset=utf-8",
+                            "Content-Length: " + bodyBytes.length,
+                            "");
+
+                    outputStream.write(response.getBytes());
+                    outputStream.write(bodyBytes);
+                } else {
+
+                    byte[] bodyBytes = fileInputStream.readAllBytes();
+
+                    final var response = String.join("\r\n",
+                            "HTTP/1.1 200 OK ",
+                            "Content-Type: text/html;charset=utf-8 ",
+                            "Content-Length: " + bodyBytes.length + " ",
+                            "",
+                            "");
+                    outputStream.write(response.getBytes());
+                    outputStream.write(bodyBytes);
+                }
+                outputStream.flush();
+            }
+
+        } catch (IOException e) {
             log.error(e.getMessage(), e);
         }
     }

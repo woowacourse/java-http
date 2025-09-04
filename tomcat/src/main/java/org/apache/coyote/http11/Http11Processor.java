6c98848f526e0eb6200b11e939a4930653a09e10
LeeJaeHoon <dlwogns3413@naver.com>
2025-09-04 16:47:15 +0900

    feat: index.html 응답 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,16 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,17 +33,41 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String startLine = bufferedReader.readLine();
+            String[] startLines = startLine.split(" ");
+            String requestMethod = startLines[0];
+            String requestUrl = startLines[1];
+            String requestHttpVersion = startLines[2];
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            String response = null;
+            if (requestMethod.equals("GET") && requestUrl.equals("/")) {
+                final var responseBody = "Hello world!";
 
-            outputStream.write(response.getBytes());
-            outputStream.flush();
+                response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody);
+            }
+            if (requestMethod.equals("GET") && requestUrl.equals("/index.html")) {
+                URL resource = getClass().getClassLoader().getResource("static/index.html");
+                Path resourcePath = Path.of(resource.getPath());
+                byte[] bytes = Files.readAllBytes(resourcePath);
+
+                response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + bytes.length + " ",
+                        "",
+                        new String(bytes));
+            }
+
+            if (response != null) {
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+            }
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }

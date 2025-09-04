4ec76ab9e5049752702d50aa91f5af6809a77aa0
songsunkook <songsunkook@gmail.com>
2025-09-04 15:09:04 +0900

    feat: get, css 진행

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,22 @@
 package org.apache.coyote.http11;
 
-import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
+import com.techcourse.exception.UncheckedServletException;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,8 +38,35 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
+            StringBuilder requestBuilder = new StringBuilder();
+            String line;
+            java.io.BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            
+            while ((line = reader.readLine()) != null) {
+                if (line.isEmpty()) {
+                    break;
+                }
+                requestBuilder.append(line).append("\r\n");
+            }
+            
+            String request = requestBuilder.toString();
+            String header = request.split("\r\n")[0];
+            String[] words = header.split(" ");
+            log.info(header);
+            URL url = getClass().getClassLoader().getResource("static" + words[1]);
+            if (url == null) {
+                log.info("Resource not found!");
+                return;
+            }
+            Path path = Paths.get(url.toURI());
+            List<String> strings = Files.readAllLines(path);
 
-            final var responseBody = "Hello world!";
+            StringBuilder sb = new StringBuilder();
+            for(var str : strings) {
+                sb.append(str + "\n");
+            }
+
+            final var responseBody = sb.toString();
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -40,7 +77,7 @@ public class Http11Processor implements Runnable, Processor {
 
             outputStream.write(response.getBytes());
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
         }
     }

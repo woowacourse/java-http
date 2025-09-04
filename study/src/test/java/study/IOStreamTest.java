eb1c49c9302c61924ec6ba7d64742897e124bdfd
2jin2.1031 <2jin2.1031@gmail.com>
2025-09-04 17:36:54 +0900

    test: study.IOStreamTest.FilterStream_학습_테스트.필터인_BufferedInputStream를_사용해보자 학습

--- study/src/test/java/study/IOStreamTest.java
+++ study/src/test/java/study/IOStreamTest.java
@@ -170,15 +170,16 @@ class IOStreamTest {
          * 버퍼 크기를 지정하지 않으면 버퍼의 기본 사이즈는 얼마일까?
          */
         @Test
-        void 필터인_BufferedInputStream를_사용해보자() {
+        void 필터인_BufferedInputStream를_사용해보자() throws IOException {
             final String text = "필터에 연결해보자.";
             final InputStream inputStream = new ByteArrayInputStream(text.getBytes());
-            final InputStream bufferedInputStream = null;
 
-            final byte[] actual = new byte[0];
+            try (InputStream bufferedInputStream = new BufferedInputStream(inputStream)) {
+                byte[] actual = bufferedInputStream.readAllBytes();
 
-            assertThat(bufferedInputStream).isInstanceOf(FilterInputStream.class);
-            assertThat(actual).isEqualTo("필터에 연결해보자.".getBytes());
+                assertThat(bufferedInputStream).isInstanceOf(FilterInputStream.class);
+                assertThat(actual).isEqualTo("필터에 연결해보자.".getBytes());
+            }
         }
     }
 

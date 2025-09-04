b6ff1accc8a131e9f0ca5706cb8005050bf30e83
Jin409 <sualng123@gmail.com>
2025-09-04 16:41:50 +0900

    fix: 잘못된 경로 파악 로직 수정

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,7 +5,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
@@ -16,19 +15,10 @@ import org.slf4j.LoggerFactory;
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
-    private static final String[] STATIC_FILE_EXTENSIONS = {
-            "html", "htm",
-            "css",
-            "js",
-            "png", "jpg", "jpeg", "gif", "webp", "svg",
-            "woff", "woff2", "ttf", "otf", "eot",
-            "mp4", "mp3", "webm", "ogg", "wav"
-    };
-    private static final String STATIC_FILE_ROOT_PATH = "static/";
     private static final String OK_RESPONSE_LINE = "HTTP/1.1 200 OK ";
-
-    private static final String CONTENT_TYPE_RESPONSE_LINE_OF_HTML = "Content-Type: text/html;charset=utf-8 ";
+    private static final String CONTENT_TYPE_RESPONSE_HEADER_KEY = "Content-Type: ";
     private static final String CONTENT_LENGTH_RESPONSE_HEADER_KEY = "Content-Length: ";
+    private static final String CONTENT_TYPE_RESPONSE_LINE_CHARSET_UTF_8 = ";charset=utf-8 ";
     public static final String DEFAULT_RESPONSE_BODY = "Hello world!";
 
     private final Socket connection;
@@ -52,23 +42,23 @@ public class Http11Processor implements Runnable, Processor {
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
             String url = parseUrl(bufferedReader);
-            String response = null;
+            String responseBody = null;
 
-            for (String extension : STATIC_FILE_EXTENSIONS) {
-                if (url.contains(extension)) {
-                    response = handleForStaticResource(url, outputStream);
-                }
+            if (StaticResourceExtension.anyMatch(url)) {
+                responseBody = handleForStaticResource(url);
             }
 
-            if (response == null) {
-                final var responseBody = DEFAULT_RESPONSE_BODY;
-                response = String.join("\r\n",
-                        OK_RESPONSE_LINE,
-                        CONTENT_TYPE_RESPONSE_LINE_OF_HTML,
-                        CONTENT_LENGTH_RESPONSE_HEADER_KEY + responseBody.getBytes().length + " ",
-                        "",
-                        responseBody);
+            if (responseBody == null) {
+                responseBody = DEFAULT_RESPONSE_BODY;
             }
+
+            final var response = String.join("\r\n",
+                    OK_RESPONSE_LINE,
+                    CONTENT_TYPE_RESPONSE_HEADER_KEY + StaticResourceExtension.findMimeTypeByUrl(url)
+                            + CONTENT_TYPE_RESPONSE_LINE_CHARSET_UTF_8,
+                    CONTENT_LENGTH_RESPONSE_HEADER_KEY + responseBody.getBytes().length + " ",
+                    "",
+                    responseBody);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
@@ -86,20 +76,22 @@ public class Http11Processor implements Runnable, Processor {
         return firstLine.split(" ")[1];
     }
 
-    private String handleForStaticResource(String url, OutputStream outputStream) throws IOException {
-        String fileName = STATIC_FILE_ROOT_PATH + url;
-        final URL resource = getClass().getClassLoader().getResource(fileName);
-        if (resource == null) {
-            throw new IllegalArgumentException("해당 파일이 존재하지 않습니다.");
+    private String handleForStaticResource(String url) throws IOException {
+        URL resource = getPathOfResource(url);
+        return readFile(resource);
+    }
+
+    private URL getPathOfResource(String url) {
+        URL resource = getClass().getClassLoader().getResource("static/" + url);
+        if (resource != null) {
+            return resource;
         }
 
+        throw new IllegalArgumentException("해당 파일이 존재하지 않습니다.");
+    }
+
+    private static String readFile(URL resource) throws IOException {
         File file = new File(resource.getFile());
-        String responseBody = Files.readString(file.toPath());
-        return String.join("\r\n",
-                OK_RESPONSE_LINE,
-                CONTENT_TYPE_RESPONSE_LINE_OF_HTML,
-                CONTENT_LENGTH_RESPONSE_HEADER_KEY + responseBody.getBytes().length + " ",
-                "",
-                responseBody);
+        return Files.readString(file.toPath());
     }
 }
--- tomcat/src/main/java/org/apache/coyote/http11/StaticResourceExtension.java
+++ tomcat/src/main/java/org/apache/coyote/http11/StaticResourceExtension.java
@@ -1,24 +1,20 @@
 package org.apache.coyote.http11;
 
 import java.util.Arrays;
-import java.util.List;
 
 public enum StaticResourceExtension {
 
-    HTML("html", "text/html", List.of("static")),
-    CSS("css", "text/css", List.of("static/css")),
-    JS("js", "application/javascript", List.of("static/js", "static/assets")),
-    SVG("svg", "image/svg+xml", List.of("static/assets/img")),
-    ;
+    HTML("html", "text/html"),
+    CSS("css", "text/css"),
+    JS("js", "application/javascript"),
+    SVG("svg", "image/svg+xml");
 
     private final String extension;
     private final String mimeType;
-    private final List<String> paths;
 
-    StaticResourceExtension(String extension, String mimeType, List<String> paths) {
+    StaticResourceExtension(String extension, String mimeType) {
         this.extension = extension;
         this.mimeType = mimeType;
-        this.paths = paths;
     }
 
     public static boolean anyMatch(String comparedExtension) {
@@ -35,23 +31,11 @@ public enum StaticResourceExtension {
                 .orElseThrow(() -> new IllegalArgumentException("일치하는 MIME TYPE 이 존재하지 않는 확장명입니다."));
     }
 
-    public static List<String> findPathsByUrl(String url) {
-        return Arrays.stream(StaticResourceExtension.values())
-                .filter(staticResourceExtension -> url.contains(staticResourceExtension.getExtension()))
-                .findFirst()
-                .map(StaticResourceExtension::getPaths)
-                .orElseThrow(() -> new IllegalArgumentException("일치하는 경로가 존재하지 않는 확장명입니다."));
-    }
-
     public String getExtension() {
         return extension;
     }
 
     public String getMimeType() {
         return mimeType;
-    }
-
-    public List<String> getPaths() {
-        return paths;
     }
 }

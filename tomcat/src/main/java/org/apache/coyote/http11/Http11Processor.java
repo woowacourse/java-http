b2f730b2d1a8ae865b101deb72f5b52f08b6baf0
민경윤 <ruddbs3308@naver.com>
2025-09-04 22:42:14 +0900

    feat: HTTP 요청을 read하고 파싱하는 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,22 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.coyote.Processor;
+import org.apache.coyote.http11.reqeust.HttpHeaders;
+import org.apache.coyote.http11.reqeust.HttpRequest;
+import org.apache.coyote.http11.reqeust.util.HttpRequestBuilder;
+import org.apache.coyote.http11.reqeust.util.HttpRequestParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -26,8 +36,10 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+        try (final InputStream inputStream = connection.getInputStream();
+             final OutputStream outputStream = connection.getOutputStream()
+        ) {
+            final HttpRequest request = getHttpRequest(inputStream);
 
             final var responseBody = "Hello world!";
 
@@ -43,5 +55,48 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private HttpRequest getHttpRequest(final InputStream inputStream) throws IOException {
+        final HttpRequestParser parser = HttpRequestParser.get();
+
+        final List<String> requestLines = getRequestLines(inputStream);
+        final String startLine = requestLines.getFirst();
+        final List<String> headerLines = requestLines.subList(1, requestLines.size());
+
+        return new HttpRequestBuilder()
+                .method(parser.parseHttpMethod(startLine))
+                .uri(parser.parseRequestUri(startLine))
+                .protocolVersion(parser.parseHttpVersion(startLine))
+                .headers(parser.parseHeaders(headerLines))
+                .body(getBodyIfExists(inputStream, parser.parseHeaders(headerLines)))
+                .build();
+    }
+
+    private List<String> getRequestLines(final InputStream inputStream) throws IOException {
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        final List<String> requestLines = new ArrayList<>();
+        while (true) {
+            final String line = bufferedReader.readLine();
+            if (line != null && !line.isEmpty()) {
+                break;
+            }
+            requestLines.add(line);
+        }
+
+        return requestLines;
+    }
+
+    private String getBodyIfExists(
+            final InputStream inputStream,
+            final HttpHeaders headers
+    ) throws IOException {
+        if (headers.getHeader("Content-Length") == null) {
+            return null;
+        }
+        int contentLength = Integer.parseInt(headers.getHeader("Content-Length"));
+        byte[] bodyBytes = inputStream.readNBytes(contentLength);
+
+        return new String(bodyBytes, StandardCharsets.UTF_8);
     }
 }

6174f352a8b79a15046f463c10270e4a693fadc7
junhaa <wooha34567@gmail.com>
2025-09-04 10:28:13 +0900

    feat: /index.html 요청 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,19 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.StringTokenizer;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,14 +36,39 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            final StringTokenizer requestLine = new StringTokenizer(reader.readLine());
+            final String method = requestLine.nextToken();
+            final URI uri = URI.create(requestLine.nextToken());
+            final String protocol = requestLine.nextToken();
 
-            final var response = String.join("\r\n",
+            String responseBody;
+            String contentType = "text/html";
+
+            if (method.equals("GET") && uri.getPath().equals("/index.html")) {
+                final String filePath = getClass().getClassLoader().getResource("static/index.html").getPath();
+                final File indexFile = new File(filePath);
+                contentType = Files.probeContentType(Path.of(filePath));
+
+                try {
+                    responseBody = new String(Files.readAllBytes(indexFile.toPath()), StandardCharsets.UTF_8);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } else if (method.equals("GET") && uri.getPath().equals("/")) {
+                responseBody = "Hello world!";
+            } else {
+                throw new UncheckedServletException(new UnsupportedOperationException("지원하지 않는 요청입니다."));
+            }
+
+            final var response = String.join(
+                    "\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Type: " + contentType + ";charset=utf-8 ",
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
-                    responseBody);
+                    responseBody
+            );
 
             outputStream.write(response.getBytes());
             outputStream.flush();

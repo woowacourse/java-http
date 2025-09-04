1edb91fc5f60751a617b7c9f79dd1ac05114a3bc
songsunkook <songsunkook@gmail.com>
2025-09-04 16:32:17 +0900

    refactor: http processor 리팩토링

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -2,6 +2,7 @@ package org.apache.coyote.http11;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.URISyntaxException;
@@ -42,78 +43,96 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
-            StringBuilder requestBuilder = new StringBuilder();
-            String line;
-            java.io.BufferedReader reader = new BufferedReader(
-                new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-
-            while ((line = reader.readLine()) != null) {
-                if (line.isEmpty()) {
-                    break;
-                }
-                requestBuilder.append(line).append("\r\n");
-            }
-
-            String request = requestBuilder.toString();
+            String request = parseRequest(inputStream);
             String header = request.split("\r\n")[0];
             String[] words = header.split(" ");
-            log.info(header);
-
+            String requestPath = words[1].split("\\?")[0];
             HttpMethod httpMethod = HttpMethod.from(words[0]);
-            String[] url = words[1].split("\\?");
-            String path = url[0];
-            String param = url.length > 1 ? url[1] : null;
-            Map<String, String> params = new HashMap<>();
-            if (param != null) {
-                for (var p : param.split("&")) {
-                    String key = p.split("=")[0];
-                    String value = p.split("=")[1];
-                    params.put(key, value);
-                }
+            Map<String, String> params = parseParameters(words[1]);
+
+            if (requestPath.equals("/login")) {
+                requestPath = "/login.html";
+                login(params);
             }
 
-            if (path.equals("/login")) {
-                path = "/login.html";
-                String account = params.get("account");
-                String password = params.get("password");
-                if (account == null || password == null) {
-                    log.info("필수 정보가 누락되었습니다.");
-                }
-                Optional<User> user = InMemoryUserRepository.findByAccount(account);
-                if (user.isEmpty()) {
-                    log.info("존재하지 않는 사용자입니다.");
-                } else if (!user.get().checkPassword(password)) {
-                    log.info("비밀번호가 틀렸습니다.");
-                } else {
-                    log.info(user.toString());
-                }
-            }
-
-            URL resource = getClass().getClassLoader().getResource("static" + path);
-            if (resource == null) {
-                log.info("Resource not found!");
-                return;
-            }
-            List<String> strings = Files.readAllLines(Paths.get(resource.toURI()));
-
-            StringBuilder sb = new StringBuilder();
-            for (var str : strings) {
-                sb.append(str + "\n");
-            }
-
-            final var responseBody = sb.toString();
-
-            final var response = String.join("\r\n",
-                "HTTP/1.1 200 OK ",
-                "Content-Type: text/html;charset=utf-8 ",
-                "Content-Length: " + responseBody.getBytes().length + " ",
-                "",
-                responseBody);
-
+            final var responseBody = getStaticPage(requestPath);
+            final var response = buildResponse(responseBody);
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String buildResponse(String responseBody) {
+        int bodyLength = responseBody == null ? 0 : responseBody.getBytes().length;
+        final var response = String.join("\r\n",
+            "HTTP/1.1 200 OK ",
+            "Content-Type: text/html;charset=utf-8 ",
+            "Content-Length: " + bodyLength + " ",
+            "",
+            responseBody);
+        return response;
+    }
+
+    private String getStaticPage(String requestPath) throws IOException, URISyntaxException {
+        URL resource = getClass().getClassLoader().getResource("static" + requestPath);
+        if (resource == null) {
+            log.info("Resource not found!");
+            return null;
+        }
+        List<String> strings = Files.readAllLines(Paths.get(resource.toURI()));
+
+        StringBuilder sb = new StringBuilder();
+        for (var str : strings) {
+            sb.append(str).append("\n");
+        }
+        return sb.toString();
+    }
+
+    private void login(Map<String, String> params) {
+        Optional<User> user = Optional.empty();
+        String account = params.get("account");
+        String password = params.get("password");
+        if (account == null || password == null) {
+            log.info("필수 정보가 누락되었습니다.");
+        } else {
+            user = InMemoryUserRepository.findByAccount(account);
+        }
+        if (user.isEmpty()) {
+            log.info("존재하지 않는 사용자입니다.");
+        } else if (!user.get().checkPassword(password)) {
+            log.info("비밀번호가 틀렸습니다.");
+        } else {
+            log.info(user.toString());
+        }
+    }
+
+    private Map<String, String> parseParameters(String url) {
+        HashMap<String, String> parameters = new HashMap<>();
+        String[] words = url.split("\\?");
+        if (words.length > 1) {
+            for (var p : words[1].split("&")) {
+                String key = p.split("=")[0];
+                String value = p.split("=")[1];
+                parameters.put(key, value);
+            }
+        }
+        return parameters;
+    }
+
+    private String parseRequest(InputStream inputStream) throws IOException {
+        StringBuilder requestBuilder = new StringBuilder();
+        String line;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+
+        while ((line = reader.readLine()) != null) {
+            if (line.isEmpty()) {
+                break;
+            }
+            requestBuilder.append(line).append("\r\n");
+        }
+
+        return requestBuilder.toString();
     }
 }

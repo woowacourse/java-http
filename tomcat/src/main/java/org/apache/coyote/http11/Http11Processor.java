84b63f594ddd1effe82eb7af68dc1e14f23c0dd6
yesjuhee <juhee0924@g.skku.edu>
2025-09-04 16:00:09 +0900

    feat: GET /index.html 응답하기 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,16 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -26,8 +30,29 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+        try (
+                final var inputStream = connection.getInputStream();
+                final var outputStream = connection.getOutputStream()
+        ) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String requestLine = reader.readLine();
+            String requestURL = requestLine.split(" ")[1]; // exception 처리, 상수화
+            if (requestURL.equals("/index.html")) {
+                URL resource = getClass().getClassLoader().getResource("static/index.html");
+
+                final String responseBody = new String(Files.readAllBytes(Paths.get(resource.getFile())));
+
+                final var response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody);
+
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+                return;
+            }
 
             final var responseBody = "Hello world!";
 

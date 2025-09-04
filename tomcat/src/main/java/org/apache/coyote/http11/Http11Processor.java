23a3f01971bf93753854178563da0c7deba05e26
Yehyeok Bang <qkddpgur318@gmail.com>
2025-09-04 21:52:49 +0900

    feat: 인덱스 페이지 요청 처리 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,13 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -26,20 +27,55 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
-
-            final var responseBody = "Hello world!";
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
-
-            outputStream.write(response.getBytes());
-            outputStream.flush();
+        try (
+                final var inputStream = connection.getInputStream();
+                final var inputReader = new BufferedReader(new InputStreamReader(inputStream));
+                final var outputStream = connection.getOutputStream()
+        ) {
+            final String line = inputReader.readLine();
+            if (line == null || line.isBlank()) {
+                final String responseBody = "Hello world!";
+                final var response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody);
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+                return;
+            }
+            final String requestUri = line.split(" ")[1];
+            if (requestUri.equals("/")) {
+                final String responseBody = "Hello world!";
+                final var response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody);
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+                return;
+            }
+            final String resourcePath = "static" + requestUri;
+            try (
+                    final var resourceStream = getClass().getClassLoader().getResourceAsStream(resourcePath)
+            ) {
+                if (resourceStream == null) {
+                    return;
+                }
+                final byte[] responseBody = resourceStream.readAllBytes();
+                final var response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: " + "text/html;charset=utf-8 ",
+                        "Content-Length: " + responseBody.length + " ",
+                        "",
+                        "");
+                outputStream.write(response.getBytes());
+                outputStream.write(responseBody);
+                outputStream.flush();
+            }
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }

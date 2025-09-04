5b1b5318fbb8e9996b2c64eb14f4ccf7c9c8f4ba
wogus <ind07152@naver.com>
2025-09-04 19:16:08 +0900

    feat: HttpHeader 파싱 기능 추가

---
+++ tomcat/src/main/java/org/apache/coyote/httpObject/HttpHeaderParser.java
@@ -0,0 +1,51 @@
+package org.apache.coyote.httpObject;
+
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class HttpHeaderParser {
+
+    private static final Pattern REQUEST_LINE_PATTERN = Pattern.compile("^(\\S+)\\s+(\\S+)\\s+(\\S+)$");
+
+    protected static HttpMethod findHttpMethod(final String requestLine) {
+        validateRequestLine(requestLine);
+        final StringTokenizer stringTokenizer = new StringTokenizer(requestLine);
+        return HttpMethod.findHttpMethod(stringTokenizer.nextToken());
+    }
+
+    protected static String findPath(final String requestLine) {
+        validateRequestLine(requestLine);
+        final StringTokenizer stringTokenizer = new StringTokenizer(requestLine);
+        stringTokenizer.nextToken();
+        return stringTokenizer.nextToken();
+    }
+
+    protected static String findProtocol(final String requestLine) {
+        validateRequestLine(requestLine);
+        final StringTokenizer stringTokenizer = new StringTokenizer(requestLine);
+        stringTokenizer.nextToken();
+        stringTokenizer.nextToken();
+        return stringTokenizer.nextToken();
+    }
+
+    public static Map<String, String> getHeaders(final List<String> headers) {
+        return headers.stream()
+                .map(header -> header.split(": ", 2))
+                .filter(parts -> parts.length == 2)
+                .collect(Collectors.toMap(
+                        parts -> parts[0].trim(),
+                        parts -> parts[1].trim()
+                ));
+    }
+
+    private static void validateRequestLine(final String requestLine) {
+        Matcher matcher = REQUEST_LINE_PATTERN.matcher(requestLine);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("requestLine의 형식이 잘못되었습니다.");
+        }
+    }
+}

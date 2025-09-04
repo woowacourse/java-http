7c6fdadb2017ecd3065687f75ce0c5884467fb33
Gabin Choi <choegabin171@gmail.com>
2025-09-05 01:41:35 +0900

    feat(Http11Processor): http 요청을 처리하고 응답을 반환하는 기능 구현
    - Get /resource 요청 시 적절한 파일을 찾아서 반환한다.

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,21 +1,31 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.coyote.Processor;
+import org.apache.coyote.TomcatController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
 
     private final Socket connection;
+    private final TomcatController tomcatController;
 
-    public Http11Processor(final Socket connection) {
+    public Http11Processor(final Socket connection, TomcatController tomcatController) {
         this.connection = connection;
+        this.tomcatController = tomcatController;
     }
 
     @Override
@@ -29,19 +39,71 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            final var requestData = getRequestData(inputStream);
+            log.info("requestData : {}", requestData);
+            tomcatController.handleRequest(requestData);
+            final var response = getResponse(requestData);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
+
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private RequestData getRequestData(InputStream inputStream) throws IOException {
+        final var inputReader = new BufferedReader(new InputStreamReader(inputStream));
+        List<String> rawHttpRequest = new ArrayList<>();
+        String line;
+        while ((line = inputReader.readLine()) != null && !line.isBlank()) {
+            rawHttpRequest.add(line);
+        }
+        if (rawHttpRequest.isEmpty()) {
+            throw new IllegalArgumentException("잘못된 요청입니다.");
+        }
+        return RequestData.of(rawHttpRequest);
+    }
+
+    private String getResponse(RequestData requestData) {
+        try {
+            final String staticFilePath = getStaticFilePath(requestData);
+            final var responseBody = readFile(staticFilePath);
+            return String.join("\r\n",
+                    requestData.getHttpVersion().getResponseHeader() + " " + HttpStatus.OK.getResponseLabel(),
+                    HttpContentType.getByFileName(staticFilePath).getResponseHeader(),
+                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "",
+                    responseBody);
+        } catch (UncheckedServletException e) {
+            log.error(e.getMessage(), e);
+        }
+        throw new IllegalArgumentException("응답 생성 중에 오류가 발생했습니다.");
+    }
+
+    private String getStaticFilePath(RequestData requestData) {
+        String resource = requestData.getResource();
+        String staticFilePath = "static" + resource;
+        if (resource.isBlank() || resource.equals("/")) {
+            staticFilePath = "static/index.html";
+        }
+        if (requestData.getHttpContentType() == HttpContentType.HTML && !staticFilePath.endsWith(".html")) {
+            staticFilePath += ".html";
+        }
+        return staticFilePath;
+    }
+
+    private String readFile(String staticFilePath) {
+        try {
+            final URL resourceUrl = getClass().getClassLoader().getResource(staticFilePath);
+            if (resourceUrl == null) {
+                throw new IllegalArgumentException("존재하지 않는 리소스입니다.: " + staticFilePath);
+            }
+            String path = resourceUrl.getPath();
+            return Files.readString(Path.of(path));
+        } catch (IOException e) {
+            log.error("파일을 불러오는데 실패했습니다. : {}", e);
+        }
+        throw new IllegalArgumentException("파일을 불러오는데 실패했습니다.: " + staticFilePath);
     }
 }
--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -1,22 +1,23 @@
 package org.apache.coyote.http11;
 
-import org.junit.jupiter.api.Test;
-import support.StubSocket;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.apache.coyote.TomcatController;
+import org.junit.jupiter.api.Test;
+import support.StubSocket;
 
 class Http11ProcessorTest {
 
     @Test
     void process() {
         // given
+        final var tomcatController = new TomcatController();
         final var socket = new StubSocket();
-        final var processor = new Http11Processor(socket);
+        final var processor = new Http11Processor(socket, tomcatController);
 
         // when
         processor.process(socket);
@@ -35,7 +36,8 @@ class Http11ProcessorTest {
     @Test
     void index() throws IOException {
         // given
-        final String httpRequest= String.join("\r\n",
+        final var tomcatController = new TomcatController();
+        final String httpRequest = String.join("\r\n",
                 "GET /index.html HTTP/1.1 ",
                 "Host: localhost:8080 ",
                 "Connection: keep-alive ",
@@ -43,7 +45,7 @@ class Http11ProcessorTest {
                 "");
 
         final var socket = new StubSocket(httpRequest);
-        final Http11Processor processor = new Http11Processor(socket);
+        final Http11Processor processor = new Http11Processor(socket, tomcatController);
 
         // when
         processor.process(socket);
@@ -53,7 +55,7 @@ class Http11ProcessorTest {
         var expected = "HTTP/1.1 200 OK \r\n" +
                 "Content-Type: text/html;charset=utf-8 \r\n" +
                 "Content-Length: 5564 \r\n" +
-                "\r\n"+
+                "\r\n" +
                 new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
 
         assertThat(socket.output()).isEqualTo(expected);

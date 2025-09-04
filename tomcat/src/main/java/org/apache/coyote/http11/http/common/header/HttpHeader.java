7e8d911d70da671ae5e7696c17ef99c1dfe17a1a
jhan0121 <jhan0121@gmail.com>
2025-09-04 16:56:59 +0900

    feat: HttpHeader 객체 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/http/common/header/HttpHeader.java
@@ -0,0 +1,121 @@
+package org.apache.coyote.http11.http.common.header;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.apache.coyote.http11.http.common.HttpSplitFormat;
+
+public class HttpHeader {
+
+    private final Map<String, String> httpHeaderInfo;
+
+    public static HttpHeader from(final BufferedReader bufferedReader) throws IOException {
+        validateNull(bufferedReader);
+        return new HttpHeader(bufferedReader);
+    }
+
+    public static HttpHeader from(final Map<String, String> httpHeaderInfo) {
+        validateNull(httpHeaderInfo);
+        return new HttpHeader(httpHeaderInfo);
+    }
+
+    private static void validateNull(final BufferedReader bufferedReader) {
+        if (bufferedReader == null) {
+            throw new IllegalArgumentException("bufferedReader는 null일 수 없습니다.");
+        }
+    }
+
+    private static void validateNull(final Map<String, String> httpHeaderInfo) {
+        if (httpHeaderInfo == null) {
+            throw new IllegalArgumentException("httpHeaderInfo는 null일 수 없습니다.");
+        }
+    }
+
+    private HttpHeader(final BufferedReader bufferedReader) throws IOException {
+        this.httpHeaderInfo = createHeaderInfo(bufferedReader);
+    }
+
+    private HttpHeader(final Map<String, String> httpHeaderInfo) {
+        this.httpHeaderInfo = httpHeaderInfo;
+    }
+
+    private Map<String, String> createHeaderInfo(final BufferedReader bufferedReader) throws IOException {
+        List<String> headerLines = readHeaderLines(bufferedReader);
+        return parseHeaderLines(headerLines);
+    }
+
+    private List<String> readHeaderLines(final BufferedReader bufferedReader) throws IOException {
+        List<String> headerLines = new ArrayList<>();
+        String headerLine = null;
+        while ((headerLine = bufferedReader.readLine()) != null) {
+            if ("".equals(headerLine)) {
+                break;
+            }
+            headerLines.add(headerLine);
+        }
+        return headerLines;
+    }
+
+    private Map<String, String> parseHeaderLines(final List<String> httpHeaderLines) {
+        Map<String, String> httpHeaderInfo = new HashMap<>();
+        for (String requestPayload : httpHeaderLines) {
+            int headerSplitIndex = requestPayload.indexOf(HttpSplitFormat.HEADER.getValue());
+            validateSplitFormat(requestPayload, headerSplitIndex);
+
+            String headerKey = clean(requestPayload.substring(0, headerSplitIndex));
+            String headerValue = clean(requestPayload.substring(headerSplitIndex + 1));
+
+            validateHeaderFormat(headerKey, headerValue);
+            httpHeaderInfo.put(headerKey, headerValue);
+        }
+        return httpHeaderInfo;
+    }
+
+    private static void validateSplitFormat(final String requestPayload, final int headerSplitIndex) {
+        if (headerSplitIndex == -1 || headerSplitIndex == requestPayload.length() - 1) {
+            throw new IllegalArgumentException(
+                    "유효하지 읺은 header 형식입니다: requestPayload=%s, headerSplitIndex=%d".formatted(requestPayload,
+                            headerSplitIndex));
+        }
+    }
+
+    private void validateHeaderFormat(final String headerKey, final String headerValue) {
+        if (headerKey == null || headerValue == null) {
+            throw new IllegalArgumentException(
+                    "header key와 value는 null일 수 없습니다: headerKey=%s, headerValue=%s".formatted(headerKey, headerValue));
+        }
+        if (headerKey.isBlank()) {
+            throw new IllegalArgumentException("header의 key값은 빈 값일 수 없습니다");
+        }
+    }
+
+    private String clean(String target) {
+        return target.trim().toLowerCase();
+    }
+
+    public boolean containsKey(String target) {
+        return httpHeaderInfo.containsKey(target);
+    }
+
+    public String getValue(String target) {
+        if (!httpHeaderInfo.containsKey(target)) {
+            throw new IllegalArgumentException("존재하지 않는 header입니다: %s".formatted(target));
+        }
+        return httpHeaderInfo.get(target);
+    }
+
+    public List<String> getFormat() {
+        List<String> result = new ArrayList<>();
+
+        for (Entry<String, String> stringStringEntry : httpHeaderInfo.entrySet()) {
+            String headerLine = stringStringEntry.getKey() + ":" + stringStringEntry.getValue();
+            result.add(headerLine);
+        }
+
+        return result;
+    }
+}

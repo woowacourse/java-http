fbcc9359349f64934bed0f78ba46966425667b71
Jin409 <sualng123@gmail.com>
2025-09-04 15:20:05 +0900

    feat: 정적 파일을 반환하도록 코드 수정

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,16 +1,35 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
-
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final String[] STATIC_FILE_EXTENSIONS = {
+            "html", "htm",
+            "css",
+            "js",
+            "png", "jpg", "jpeg", "gif", "webp", "svg",
+            "woff", "woff2", "ttf", "otf", "eot",
+            "mp4", "mp3", "webm", "ogg", "wav"
+    };
+    private static final String STATIC_FILE_ROOT_PATH = "static/";
+    private static final String OK_RESPONSE_LINE = "HTTP/1.1 200 OK ";
+
+    private static final String CONTENT_TYPE_RESPONSE_LINE_OF_HTML = "Content-Type: text/html;charset=utf-8 ";
+    private static final String CONTENT_LENGTH_RESPONSE_HEADER_KEY = "Content-Length: ";
+    public static final String DEFAULT_RESPONSE_BODY = "Hello world!";
 
     private final Socket connection;
 
@@ -29,19 +48,58 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            String url = parseUrl(bufferedReader);
+            String response = null;
+
+            for (String extension : STATIC_FILE_EXTENSIONS) {
+                if (url.contains(extension)) {
+                    response = handleForStaticResource(url, outputStream);
+                }
+            }
+
+            if (response == null) {
+                final var responseBody = DEFAULT_RESPONSE_BODY;
+                response = String.join("\r\n",
+                        OK_RESPONSE_LINE,
+                        CONTENT_TYPE_RESPONSE_LINE_OF_HTML,
+                        CONTENT_LENGTH_RESPONSE_HEADER_KEY + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody);
+            }
 
             outputStream.write(response.getBytes());
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (IOException |
+                 UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String parseUrl(BufferedReader bufferedReader) throws IOException {
+        String firstLine = bufferedReader.readLine();
+        if (firstLine.isBlank()) {
+            throw new IllegalArgumentException("요청 형식이 잘못되었습니다.");
+        }
+        return firstLine.split(" ")[1];
+    }
+
+    private String handleForStaticResource(String url, OutputStream outputStream) throws IOException {
+        String fileName = STATIC_FILE_ROOT_PATH + url;
+        final URL resource = getClass().getClassLoader().getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("해당 파일이 존재하지 않습니다.");
+        }
+
+        File file = new File(resource.getFile());
+        String responseBody = Files.readString(file.toPath());
+        return String.join("\r\n",
+                OK_RESPONSE_LINE,
+                CONTENT_TYPE_RESPONSE_LINE_OF_HTML,
+                CONTENT_LENGTH_RESPONSE_HEADER_KEY + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
     }
 }

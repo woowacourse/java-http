c77fe959d927ef92bb9fa48dba70a486e48dea5b
MinJun Choi <mj043000@naver.com>
2025-09-05 17:02:26 +0900

    refactor: 파일 타입 기본값 상수화

--- tomcat/src/main/java/org/apache/coyote/http11/ContentTypeMapper.java
+++ tomcat/src/main/java/org/apache/coyote/http11/ContentTypeMapper.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 public class ContentTypeMapper {
 
+    private static final String APPLICATION_OCTET_STREAM = "application/octet-stream";
     private static final Map<String, String> TYPES = Map.of(
             "html", "text/html;charset=utf-8",
             "css", "text/css",
@@ -13,9 +14,9 @@ public class ContentTypeMapper {
     public static String get(String extension) {
         int dotIndex = extension.lastIndexOf('.');
         if (dotIndex == -1) {
-            return "application/octet-stream";
+            return APPLICATION_OCTET_STREAM;
         }
         String ext = extension.substring(dotIndex + 1);
-        return TYPES.getOrDefault(ext, "application/octet-stream");
+        return TYPES.getOrDefault(ext, APPLICATION_OCTET_STREAM);
     }
 }

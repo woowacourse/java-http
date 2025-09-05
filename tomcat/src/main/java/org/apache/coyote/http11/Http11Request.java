826b8ca7f83a732d33c2e938037d6869ee737df1
supernovaMK <minkikim2002@gmail.com>
2025-09-05 10:32:07 +0900

    feat: Http11Request 구현
    - Http 1.1 기준으로 요청의 정보를 담당하는 객체를 구현함.

---
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Request.java
@@ -0,0 +1,83 @@
+package org.apache.coyote.http11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Http11Request {
+
+    private static final Charset DEFAULD_CHARSET = StandardCharsets.UTF_8;
+    private static final String QUREYPARAMETER_DELIMETER = "?";
+
+    private InputStream inputStream;
+    private String header;
+    private String firstLine;
+    private String[] firstLineConditions;
+    private String path;
+    private Map<String, String> queryParams;
+
+    public Http11Request(InputStream inputStream) throws IOException {
+        this.inputStream = inputStream;
+        extractHeader();
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getQueryParamValue(String queryParam) {
+        return queryParams.get(queryParam);
+    }
+
+    public Map<String, String> getQueryParams() {
+        return queryParams;
+    }
+
+    public String getRequestMethod() {
+        return firstLineConditions[0];
+    }
+
+    private void extractHeader() throws IOException {
+        final StringBuilder requestHeader = new StringBuilder();
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(inputStream, DEFAULD_CHARSET));
+
+        String line;
+        while (((line = bufferedReader.readLine()) != null) && (!line.isEmpty())) {
+            requestHeader.append(line).append("\r\n");
+        }
+        header = requestHeader.toString();
+        String[] requestConditions = requestHeader.toString().split("\r\n");
+        firstLine = requestConditions[0];
+        firstLineConditions = firstLine.split(" ");
+
+        extractUri();
+    }
+
+    private void extractUri() {
+        String uri = firstLineConditions[1];
+
+        if (uri.contains(QUREYPARAMETER_DELIMETER)) {
+            int queryStartIndex = uri.indexOf(QUREYPARAMETER_DELIMETER);
+            path = uri.substring(0, queryStartIndex);
+            String queryString = uri.substring(queryStartIndex + 1);
+
+            queryParams = Arrays.stream(queryString.split("&"))
+                    .map(param -> param.split("=", 2))
+                    .collect(Collectors.toMap(
+                            arr -> arr[0],
+                            arr -> arr[1] //TODO: 쿼리 파라미터에서 "query" 처럼 "="을 아예 쓰지 않는 경우 추후에 고려
+                    ));
+
+        } else {
+            path = uri;
+            queryParams = Map.of();
+        }
+    }
+}

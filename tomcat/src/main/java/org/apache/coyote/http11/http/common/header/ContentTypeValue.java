d2097534b24cd05b73e02a74764b5b57eacfdea5
jhan0121 <jhan0121@gmail.com>
2025-09-05 22:01:22 +0900

    refactor: ContentTypeValue 쿼리 파라미터 대응 정규식 변경

--- tomcat/src/main/java/org/apache/coyote/http11/http/common/header/ContentTypeValue.java
+++ tomcat/src/main/java/org/apache/coyote/http11/http/common/header/ContentTypeValue.java
@@ -5,9 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public enum ContentTypeValue {
-    HTML(".*\\.html$", "text/html"),
-    CSS(".*\\.css$", "text/css"),
-    JAVASCRIPT(".*\\.js$", "text/javascript"),
+    HTML(".*\\.html(\\?.*)?$", "text/html"),
+    CSS(".*\\.css(\\?.*)?$", "text/css"),
+    JAVASCRIPT(".*\\.js(\\?.*)?$", "text/javascript"),
     ;
 
     private static final Logger log = LoggerFactory.getLogger(ContentTypeValue.class);

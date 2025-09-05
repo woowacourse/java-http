8c9127292801bd08b1c51f1478a40786c0aa157d
ryan-dia <cjfdnjs158@naver.com>
2025-09-05 20:46:34 +0900

    chore: 변수 네이밍 변경 및 에러 로깅 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,14 +1,11 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
-import java.util.List;
 import org.apache.coyote.Processor;
-import org.apache.coyote.http11.handler.StaticFileHandler;
 import org.apache.coyote.http11.dto.HttpRequest;
 import org.apache.coyote.http11.handler.Handler;
-import org.apache.coyote.http11.handler.HttpRequestHandler;
-import org.apache.coyote.http11.handler.HttpResponseDispatcher;
-import org.apache.coyote.http11.handler.Responses;
+import org.apache.coyote.http11.util.HttpRequestParser;
+import org.apache.coyote.http11.helper.Responses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,16 +17,11 @@ public class Http11Processor implements Runnable, Processor {
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
 
     private final Socket connection;
-    private final HttpRequestHandler httpRequestHandler;
-    private final Handler httpResponseDispatcher;
+    private final Handler dispatcher;
 
-    public Http11Processor(
-            final Socket connection,
-            final HttpRequestHandler httpRequestHandler
-    ) {
+    public Http11Processor(Socket connection, Handler dispatcher) {
         this.connection = connection;
-        this.httpRequestHandler = httpRequestHandler;
-        this.httpResponseDispatcher = new HttpResponseDispatcher(List.of(new StaticFileHandler("static", "index.html")));
+        this.dispatcher = dispatcher;
     }
 
     @Override
@@ -39,17 +31,18 @@ public class Http11Processor implements Runnable, Processor {
     }
 
     @Override
-    public void process(final Socket connection) {
+    public void process(Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
-
-            HttpRequest httpRequest = httpRequestHandler.parse(inputStream);
-            httpResponseDispatcher.handle(httpRequest, outputStream);
+            HttpRequest httpRequest = HttpRequestParser.parse(inputStream);
+            dispatcher.handle(httpRequest, outputStream);
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
             try {
                 Responses.serverError(connection.getOutputStream(), "HTTP/1.1");
-            } catch (IOException ignore) { /* 무시 */ }
+            } catch (IOException ioException) {
+                log.error("서버 에러 응답 전송 실패: {}", ioException.getMessage(), ioException);
+            }
         }
     }
 }

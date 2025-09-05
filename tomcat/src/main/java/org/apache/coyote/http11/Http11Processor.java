4a0ad3d774c4e168ee6578de229127df94dc142a
jeyong <joon6093@naver.com>
2025-09-05 17:24:50 +0900

    [1단계 - HTTP 서버 구현하기] 머피(송제용) 미션 제출합니다 (#789)
    * fix: 테스트 실패 의존성 문제 해결
    
    * test: File 학습 테스트
    
    * test: IOStream 학습 테스트
    
    * feat: GET /index.html 응답하기
    
    * feat: CSS 지원하기
    
    * feat: Query String 파싱
    
    * refactor: 추상화 수준 고려
    
    * chore: 불필요한 의존성 주석 처리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,23 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -27,21 +38,122 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final var reader = new BufferedReader(new InputStreamReader(inputStream))) {
 
-            final var responseBody = "Hello world!";
+            final var requestLine = reader.readLine();
+            if (requestLine == null || requestLine.isBlank()) {
+                return;
+            }
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            final var uri = parseRequestUri(requestLine);
+            final var pathAndQuery = extractPathAndQuery(uri);
+            final var path = pathAndQuery.get("path");
+            final var params = parseQueryString(pathAndQuery.get("query"));
 
-            outputStream.write(response.getBytes());
-            outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+            final byte[] responseBody;
+            final String contentType;
+
+            if ("/login".equals(path)) {
+                final var resourceUrl = Objects.requireNonNull(getClass().getClassLoader()
+                        .getResource("static/login.html"));
+                responseBody = Files.readAllBytes(Path.of(resourceUrl.toURI()));
+                contentType = "text/html;charset=utf-8 ";
+
+                final var account = params.get("account");
+                final var password = params.get("password");
+                InMemoryUserRepository.findByAccount(account)
+                        .ifPresent(user -> {
+                            if (user.checkPassword(password)) {
+                                log.info("user: {}", user);
+                            }
+                        });
+
+            } else {
+                final var resourcePath = "static" + path;
+                final var resourceUrl = getClass().getClassLoader()
+                        .getResource(resourcePath);
+
+                if (resourceUrl != null && !Files.isDirectory(Path.of(resourceUrl.toURI()))) {
+                    responseBody = Files.readAllBytes(Path.of(resourceUrl.toURI()));
+                    contentType = getContentType(path);
+                } else {
+                    responseBody = "Hello world!".getBytes(StandardCharsets.UTF_8);
+                    contentType = "text/html;charset=utf-8 ";
+                }
+            }
+
+            writeResponse(outputStream, responseBody, contentType);
+        } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String parseRequestUri(final String requestLine) {
+        final var parts = requestLine.split(" ");
+
+        return parts[1];
+    }
+
+    private Map<String, String> extractPathAndQuery(final String uri) {
+        final var result = new HashMap<String, String>();
+        final var index = uri.indexOf("?");
+
+        if (index != -1) {
+            result.put("path", uri.substring(0, index));
+            result.put("query", uri.substring(index + 1));
+        } else {
+            result.put("path", uri);
+            result.put("query", null);
+        }
+
+        return result;
+    }
+
+    private Map<String, String> parseQueryString(final String queryString) {
+        final var paramMap = new HashMap<String, String>();
+
+        if (queryString == null) {
+            return paramMap;
+        }
+
+        final var queries = queryString.split("&");
+        for (final var query : queries) {
+            final var keyValue = query.split("=", 2);
+            if (keyValue.length == 2) {
+                paramMap.put(keyValue[0], keyValue[1]);
+            }
+        }
+
+        return paramMap;
+    }
+
+    private String getContentType(final String path) {
+        if (path.endsWith(".html")) {
+            return "text/html;charset=utf-8 ";
+        }
+        if (path.endsWith(".css")) {
+            return "text/css;charset=utf-8 ";
+        }
+
+        return "text/plain;charset=utf-8 ";
+    }
+
+    private void writeResponse(
+            final OutputStream outputStream,
+            final byte[] body,
+            final String contentType
+    ) throws IOException {
+        final var headers = String.join(
+                "\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: " + contentType,
+                "Content-Length: " + body.length + " ",
+                ""
+        );
+
+        outputStream.write((headers + "\r\n").getBytes(StandardCharsets.UTF_8));
+        outputStream.write(body);
+        outputStream.flush();
     }
 }

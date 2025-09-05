0d50b021cf243febe67d4ef729a1cd0aa896624e
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-09-05 09:00:01 +0900

    feat: login 요청 처리 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,13 +1,17 @@
 package org.apache.coyote.http11;
 
 import ch.qos.logback.core.joran.spi.NoAutoStartUtil;
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -104,6 +108,44 @@ public class Http11Processor implements Runnable, Processor {
                         "",
                         body);
             }
+
+            if (requestResource.startsWith("/login")) {
+
+                int index = requestResource.indexOf("?");
+                String resource = requestResource.substring(0, index);
+                String queryString = requestResource.substring(index + 1);
+
+                Map<String, String> queryParameters = new HashMap<>();
+                String[] parameters = queryString.split("&");
+                for (String parameter : parameters) {
+                    String key = parameter.split("=")[0];
+                    String value = parameter.split("=")[1];
+                    queryParameters.put(key, value);
+                }
+
+
+                String resourcePath = "./static" + resource + ".html";
+                URL systemResource = ClassLoader.getSystemResource(resourcePath);
+                Path path = Path.of(systemResource.getPath());
+                final var responseBody = Files.readAllBytes(path);
+                body = new String(responseBody);
+
+                String extension = resourcePath.substring(resourcePath.lastIndexOf('.') + 1);
+                String contentType = String.join("/","text",extension);
+                response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: " + contentType + ";charset=utf-8 ",
+                        "Content-Length: " + body.getBytes().length + " ",
+                        "",
+                        body);
+
+                User user = InMemoryUserRepository.findByAccount(queryParameters.get("account")).orElse(null);
+                if (user != null) {
+                    System.out.println(user);
+                }
+            }
+
+
 
             if ("/".equals(requestResource)) {
                 body = "Hello world!";

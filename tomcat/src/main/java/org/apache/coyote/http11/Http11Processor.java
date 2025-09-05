cedef19677e0f662f159ee4816bd0a10f86b7204
junhaa <wooha34567@gmail.com>
2025-09-06 04:01:30 +0900

    refactor: 에러 HttpResponse 변경 메서드 명 수정

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -68,16 +68,16 @@ public class Http11Processor implements Runnable, Processor {
             final HttpRequest request = HttpRequestParser.parseRequest(inputStream);
             handleRequest(request, response);
         } catch (IllegalArgumentException e){
-            setErrorResponse(BAD_REQUEST, response, e);
+            updateResponseWithError(BAD_REQUEST, response, e);
             log.warn("잘못된 요청 형식: {}", e.getMessage());
         } catch (NoSuchElementException e) {
-            setErrorResponse(NOT_FOUND, response, e);
+            updateResponseWithError(NOT_FOUND, response, e);
             log.warn("존재하지 않는 리소스: {}", e.getMessage());
         } catch (UnsupportedOperationException e) {
-            setErrorResponse(METHOD_NOT_ALLOWED, response, e);
+            updateResponseWithError(METHOD_NOT_ALLOWED, response, e);
             log.warn("지원하지 않는 HTTP 메서드: {}", e.getMessage());
         } catch (UncheckedServletException e) {
-            setErrorResponse(INTERNAL_SERVER_ERROR, response, e);
+            updateResponseWithError(INTERNAL_SERVER_ERROR, response, e);
             log.error("예상치 못한 서버 오류 발생", e);
         }
         return response;
@@ -97,7 +97,7 @@ public class Http11Processor implements Runnable, Processor {
         }
     }
 
-    private void setErrorResponse(HttpStatus status, HttpResponse response, Exception e) {
+    private void updateResponseWithError(HttpStatus status, HttpResponse response, Exception e) {
         response.setStatus(status);
         response.setBody(e.getMessage());
         response.setContentType("text/plain;charset=utf-8");

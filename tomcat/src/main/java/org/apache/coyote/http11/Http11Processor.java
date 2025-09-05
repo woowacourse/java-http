af0cea15d5505ac6cf8d73fe475322463cb7e289
Wooyoung <quantumjump10@gmail.com>
2025-09-05 20:21:34 +0900

    feat: HTML 정적 리소스 응답 처리 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,16 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,12 +33,30 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            final String requestLine = reader.readLine();
+            String[] parsedLine = requestLine.split(" ");
+            String requestUrl = parsedLine[1];
+
+            URL resource;
+            if (requestUrl.endsWith(".html")) {
+                resource = getClass().getClassLoader().getResource("static" + requestUrl);
+            } else {
+                resource = getClass().getClassLoader().getResource(requestUrl);
+            }
+
+            final var responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+
+            String contentType = "text/plain;charset=utf-8";
+            if (requestUrl.endsWith(".html")) {
+                contentType = "text/html;charset=utf-8";
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "Content-Type: " + contentType + " ",
+                    "Content-Length: " + responseBody.getBytes().length,
                     "",
                     responseBody);
 

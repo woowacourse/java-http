035870916a460de4edb91084f67f254fdf3dc47b
geonwoo <kysub99@naver.com>
2025-09-05 14:55:03 +0900

    feat: css 지원 기능 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -2,8 +2,9 @@ package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Socket;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
@@ -12,9 +13,6 @@ import java.nio.file.Paths;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -48,10 +46,14 @@ public class Http11Processor implements Runnable, Processor {
             final Path filePath = Paths.get(resource.toURI());
             final byte[] bytes = Files.readAllBytes(filePath);
             final String responseBody = new String(bytes);
+            String contentType = "text/html";
+            if (path.endsWith(".css")) {
+                contentType = "text/css";
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Type: " + contentType + ";charset=utf-8 ",
                     "Content-Length: " + bytes.length + " ",
                     "",
                     responseBody);

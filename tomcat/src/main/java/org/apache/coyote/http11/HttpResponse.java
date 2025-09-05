2fc679eef521cbd546f7a938a93cc8dcccf8b34c
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-09-05 22:12:11 +0900

    feat: Http 응답 객체 구현

---
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpResponse.java
@@ -0,0 +1,34 @@
+package org.apache.coyote.http11;
+
+public class HttpResponse {
+    private final String statusLine;
+    private final String contentType;
+    private final String body;
+
+    public HttpResponse(String statusLine, String contentType, String body) {
+        this.statusLine = statusLine;
+        this.contentType = contentType;
+        this.body = body;
+    }
+
+    public static HttpResponse ok(String body) {
+        return new HttpResponse("HTTP/1.1 200 OK", "text/html;charset=utf-8", body);
+    }
+
+    public static HttpResponse notFound() {
+        return new HttpResponse("HTTP/1.1 404 Not Found", "text/html;charset=utf-8", "404 Not Found");
+    }
+
+    public String toHttpResponse() {
+        return String.join("\r\n",
+                statusLine + " ",
+                "Content-Type: " + contentType + " ",
+                "Content-Length: " + body.getBytes().length + " ",
+                "",
+                body);
+    }
+
+    public byte[] getBytes() {
+        return toHttpResponse().getBytes();
+    }
+}

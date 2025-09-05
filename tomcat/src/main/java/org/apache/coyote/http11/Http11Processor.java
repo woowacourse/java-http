3971108d472a80fc1369c6ec9d204bef62ded944
Lee Dong Yeop <s01054830455@gmail.com>
2025-09-05 14:12:53 +0900

    feat: 3. [Query String 파싱] 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,9 +1,12 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
@@ -31,8 +34,8 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+        try (final InputStream inputStream = connection.getInputStream();
+             final OutputStream outputStream = connection.getOutputStream()) {
 
             final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
             final BufferedReader br = new BufferedReader(inputStreamReader);
@@ -54,14 +57,40 @@ public class Http11Processor implements Runnable, Processor {
                 writeAndFlush(outputStream, response);
             }
 
+            if (httpMethod.equals("GET") && endPoint.startsWith("/login")) {
+                final int index = endPoint.indexOf("?");
+                final String path = endPoint.substring(0, index);
+
+                final String queryString = endPoint.substring(index + 1);
+                final String[] splitQueryString = queryString.split("&");
+                final String[] split = splitQueryString[0].split("=");
+                final User user = getUserByAccount(split);
+
+                log.info("user: {}", user.toString());
+                final URL resource = getClass().getClassLoader().getResource("static" + path + ".html");
+                final String responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+                final String response = createHtmlResponse(responseBody);
+                writeAndFlush(outputStream, response);
+                return;
+            }
+
             final URL resource = getClass().getClassLoader().getResource("static" + endPoint);
             final String responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
-            final var response = createHtmlResponse(responseBody);
+            final String response = createHtmlResponse(responseBody);
             writeAndFlush(outputStream, response);
 
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String createHtmlResponse(final String responseBody) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
     }
 
     private String createCssResponse(final String responseBody) {
@@ -73,13 +102,9 @@ public class Http11Processor implements Runnable, Processor {
                 responseBody);
     }
 
-    private String createHtmlResponse(final String responseBody) {
-        return String.join("\r\n",
-                "HTTP/1.1 200 OK ",
-                "Content-Type: text/html;charset=utf-8 ",
-                "Content-Length: " + responseBody.getBytes().length + " ",
-                "",
-                responseBody);
+    private User getUserByAccount(final String[] split) {
+        return InMemoryUserRepository.findByAccount(split[1])
+                .orElseThrow(() -> new IllegalArgumentException("user를 찾을 수 없습니다."));
     }
 
     private void writeAndFlush(final OutputStream outputStream, final String response) throws IOException {

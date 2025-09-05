c33e2b0e13e760f82172ea47f89800f54bc87e39
sonjh919 <kingjh1125@gmail.com>
2025-09-05 10:47:00 +0900

    refactor: delete ContentType from ResponseBody

--- tomcat/src/main/java/org/apache/coyote/response/HttpResponseGenerator.java
+++ tomcat/src/main/java/org/apache/coyote/response/HttpResponseGenerator.java
@@ -1,12 +1,17 @@
 package org.apache.coyote.response;
 
+import org.apache.coyote.response.responseHeader.ContentType;
+import org.apache.coyote.response.responseHeader.ResponseHeader;
+import org.apache.coyote.response.responseLine.HttpStatus;
+import org.apache.coyote.response.responseLine.ResponseLine;
+
 public class HttpResponseGenerator {
 
     public static HttpResponse generate(final String body, final ContentType contentType, final HttpStatus httpStatus) {
 
         ResponseLine responseLine = new ResponseLine(httpStatus);
         ResponseHeader responseHeader = new ResponseHeader(body.getBytes().length, contentType);
-        ResponseBody responseBody = new ResponseBody(body, contentType);
+        ResponseBody responseBody = new ResponseBody(body);
 
         return new HttpResponse(responseLine, responseHeader, responseBody);
     }
--- tomcat/src/main/java/org/apache/coyote/response/ResponseBody.java
+++ tomcat/src/main/java/org/apache/coyote/response/ResponseBody.java
@@ -3,11 +3,9 @@ package org.apache.coyote.response;
 public class ResponseBody {
 
     private final String body;
-    private final ContentType type;
 
-    public ResponseBody(final String body, final ContentType type) {
+    public ResponseBody(final String body) {
         this.body = body;
-        this.type = type;
     }
 
     public String getBody() {

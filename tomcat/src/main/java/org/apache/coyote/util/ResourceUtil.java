d6f63b043c5c4f4451752b53281d34f952c6b3e8
threepebbles <threepebbles@naver.com>
2025-09-05 10:49:13 +0900

    feat: resources 하위에서 정적 파일 읽는 기능 추가

---
+++ tomcat/src/main/java/org/apache/coyote/util/ResourceUtil.java
@@ -0,0 +1,48 @@
+package org.apache.coyote.util;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
+public class ResourceUtil {
+
+    public static boolean isStaticResourceExist(final String path, final Class<?> classLoader)
+            throws FileNotFoundException {
+        final String STATIC_PATH = "/static";
+
+        try {
+            URL url = Objects.requireNonNull(
+                    classLoader.getResource(STATIC_PATH + path)
+            );
+            URI uri = url.toURI();
+            Path filePath = Paths.get(uri);
+
+            return Files.isRegularFile(filePath);
+        } catch (URISyntaxException e) {
+            throw new FileNotFoundException(path + ": file doesn't exist");
+        }
+    }
+
+    public static String readStaticResource(final String path, final Class<?> classLoader)
+            throws FileNotFoundException {
+        final String STATIC_PATH = "/static";
+
+        try {
+            URL url = Objects.requireNonNull(
+                    classLoader.getResource(STATIC_PATH + path)
+            );
+            URI uri = url.toURI();
+            Path filePath = Paths.get(uri);
+
+            return Files.readString(filePath);
+        } catch (URISyntaxException | IOException e) {
+            throw new FileNotFoundException(path + ": file doesn't exist");
+        }
+    }
+}

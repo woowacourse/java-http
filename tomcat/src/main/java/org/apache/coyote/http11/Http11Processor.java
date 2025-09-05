808a6bf9d874c06bc752381e6203ba6732bfb402
GiSeok Kang <ime.rokz@gmail.com>
2025-09-05 23:44:41 +0900

    refactor: HttpResponse 외부 객체로 분리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -4,13 +4,9 @@ import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
 import com.techcourse.model.User;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Optional;
@@ -36,26 +32,6 @@ public class Http11Processor implements Runnable, Processor {
         process(connection);
     }
 
-    static class HttpResponse {
-
-        private final String contentType;
-        private final String responseBody;
-
-        public HttpResponse(String contentType, String responseBody) {
-            this.contentType = contentType;
-            this.responseBody = responseBody;
-        }
-
-        public String serveResponse() {
-            return String.join("\r\n",
-                "HTTP/1.1 200 OK ",
-                "Content-Type: text/" + contentType + ";charset=utf-8 ",
-                "Content-Length: " + responseBody.getBytes(StandardCharsets.UTF_8).length + " ",
-                "",
-                responseBody);
-        }
-    }
-
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
@@ -74,11 +50,11 @@ public class Http11Processor implements Runnable, Processor {
     }
 
     private HttpResponse handle(final HttpRequest httpRequest) throws IOException {
-        if (httpRequest.getUri().equals("/")) {
+        if (httpRequest.uri().equals("/")) {
             return new HttpResponse("html", "Hello world!");
         }
 
-        if (httpRequest.getUri().startsWith("/login")) {
+        if (httpRequest.uri().startsWith("/login")) {
             Map<String, String> queryStrings = httpRequest.getQueryStrings();
             String account = queryStrings.get("account");
 
@@ -87,14 +63,14 @@ public class Http11Processor implements Runnable, Processor {
 
             return new HttpResponse(
                 "html",
-                Arrays.toString(staticFileLoader.readAllFileWithUri(httpRequest.getUri())) + "html"
+                Arrays.toString(staticFileLoader.readAllFileWithUri(httpRequest.uri())) + "html"
             );
         }
 
-        int extensionIndex = httpRequest.getUri().lastIndexOf(".");
-        String extension = httpRequest.getUri().substring(extensionIndex + 1);
+        int extensionIndex = httpRequest.uri().lastIndexOf(".");
+        String extension = httpRequest.uri().substring(extensionIndex + 1);
 
-        String staticFile = new String(staticFileLoader.readAllFileWithUri(httpRequest.getUri()));
+        String staticFile = new String(staticFileLoader.readAllFileWithUri(httpRequest.uri()));
         return new HttpResponse(extension, staticFile);
     }
 }

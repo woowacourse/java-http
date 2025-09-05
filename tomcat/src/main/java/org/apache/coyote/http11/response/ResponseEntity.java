600929ab49f40d39357a16bd4528e6831fddb027
threepebbles <threepebbles@naver.com>
2025-09-05 10:52:28 +0900

    feat: HttpResponse를 편하게 작성하기 위해 ResponseEntity 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/response/ResponseEntity.java
@@ -0,0 +1,59 @@
+package org.apache.coyote.http11.response;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResponseEntity {
+
+    private ResponseEntity() {
+    }
+
+    public static HttpResponse ok(String body) {
+        return ok(body, "text/plain;charset=utf-8");
+    }
+
+    public static HttpResponse ok(String body, String contentType) {
+        byte[] bodyBytes = body.getBytes(StandardCharsets.UTF_8);
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", contentType);
+        headers.put("Content-Length", String.valueOf(bodyBytes.length));
+
+        return new HttpResponse(
+                new StatusLine(
+                        String.join(" ",
+                                "HTTP/1.1",
+                                String.valueOf(HttpStatus.OK.getHttpStatusCode()),
+                                HttpStatus.OK.getReasonPhrase()
+                        )
+                ),
+                headers,
+                body
+        );
+    }
+
+    public static HttpResponse notFound(String body) {
+        return notFound(body, "text/plain;charset=utf-8");
+    }
+
+    public static HttpResponse notFound(String body, String contentType) {
+        byte[] bodyBytes = body.getBytes(StandardCharsets.UTF_8);
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", contentType);
+        headers.put("Content-Length", String.valueOf(bodyBytes.length));
+
+        return new HttpResponse(
+                new StatusLine(
+                        String.join(" ",
+                                "HTTP/1.1",
+                                String.valueOf(HttpStatus.NOT_FOUND.getHttpStatusCode()),
+                                HttpStatus.NOT_FOUND.getReasonPhrase()
+                        )
+                ),
+                headers,
+                body
+        );
+    }
+}

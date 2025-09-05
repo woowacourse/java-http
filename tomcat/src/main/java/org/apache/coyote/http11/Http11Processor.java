bc04d500f66ce497c78eb98d335ba57a63c6b3f6
byesol <bsj5967@naver.com>
2025-09-06 02:06:27 +0900

    refactor: private method 분리를 통한 리팩토링

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
@@ -48,6 +49,7 @@ public class Http11Processor implements Runnable, Processor {
 
             String path = uri;
             String queryString = null;
+
             if (uri.contains("?")) {
                 int queryIndex = uri.indexOf("?");
                 path = uri.substring(0, queryIndex);
@@ -55,106 +57,112 @@ public class Http11Processor implements Runnable, Processor {
             }
 
             if ("/".equals(path)) {
-                final var responseBody = "Hello world!";
-                final var response = String.join("\r\n",
-                        "HTTP/1.1 200 OK ",
-                        "Content-Type: text/html;charset=utf-8 ",
-                        "Content-Length: " + responseBody.getBytes(StandardCharsets.UTF_8).length + " ",
-                        "",
-                        responseBody);
-
-                outputStream.write(response.getBytes(StandardCharsets.UTF_8));
-                outputStream.flush();
+                handleRoot(outputStream);
                 return;
             }
 
             if ("/login".equals(path)) {
-                if (queryString == null) {
-                    return;
-                }
-
-                Map<String, String> params = new HashMap<>();
-                for (String pair : queryString.split("&")) {
-                    String[] keyValue = pair.split("=");
-                    if (keyValue.length == 2) {
-                        params.put(keyValue[0], keyValue[1]);
-                    }
-                }
-                String account = params.get("account");
-                String password = params.get("password");
-
-                log.info("로그인 시도: account={}, password={}", account, password);
-                Optional<User> userOptional = InMemoryUserRepository.findByAccount(account);
-
-                if (userOptional.isPresent() && userOptional.get().checkPassword(password)) {
-                    log.info("user : {}", userOptional.get());
-                } else {
-                    log.info("로그인 실패: 아이디 또는 비밀번호가 일치하지 않습니다.");
-                }
-
-                try (InputStream fileInputStream = getClass().getClassLoader()
-                        .getResourceAsStream("static/login.html")) {
-                    if (fileInputStream == null) {
-                        return;
-                    }
-                    byte[] bodyBytes = fileInputStream.readAllBytes();
-
-                    final var response = String.join("\r\n",
-                            "HTTP/1.1 200 OK ",
-                            "Content-Type: text/html;charset=utf-8 ",
-                            "Content-Length: " + bodyBytes.length + " ",
-                            "",
-                            "");
-
-                    outputStream.write(response.getBytes(StandardCharsets.UTF_8));
-                    outputStream.write(bodyBytes);
-                    outputStream.flush();
-                    return;
-                }
+                handleLogin(outputStream, queryString);
+                return;
             }
 
-            String resourcePath = uri.substring(1);
-            try (InputStream fileInputStream = getClass().getClassLoader()
-                    .getResourceAsStream("static/" + resourcePath)) {
-
-                if (fileInputStream == null) {
-                    String responseBody = "404 Not Found";
-                    byte[] bodyBytes = responseBody.getBytes(StandardCharsets.UTF_8);
-
-                    final var response = String.join("\r\n",
-                            "HTTP/1.1 404 Not Found",
-                            "Content-Type: text/plain;charset=utf-8",
-                            "Content-Length: " + bodyBytes.length,
-                            "",
-                            "");
-
-                    outputStream.write(response.getBytes(StandardCharsets.UTF_8));
-                    outputStream.write(bodyBytes);
-                } else {
-                    String contentType = "text/html;charset=utf-8";
-                    if (uri.endsWith(".css")) {
-                        contentType = "text/css";
-                    } else if (uri.endsWith(".js")) {
-                        contentType = "application/javascript";
-                    }
-
-                    byte[] bodyBytes = fileInputStream.readAllBytes();
-
-                    final var response = String.join("\r\n",
-                            "HTTP/1.1 200 OK ",
-                            "Content-Type: " + contentType + " ",
-                            "Content-Length: " + bodyBytes.length + " ",
-                            "",
-                            "");
-
-                    outputStream.write(response.getBytes(StandardCharsets.UTF_8));
-                    outputStream.write(bodyBytes);
-                }
-                outputStream.flush();
-            }
+            handleStaticResource(outputStream, path);
 
         } catch (IOException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+
+    private void handleRoot(OutputStream outputStream) throws IOException {
+        final var responseBody = "Hello world!";
+        sendOkResponse(outputStream, "text/html;charset=utf-8", responseBody.getBytes(StandardCharsets.UTF_8));
+    }
+
+
+    private void handleLogin(OutputStream outputStream, String queryString) throws IOException {
+        if (queryString != null) {
+            Map<String, String> params = parseQueryString(queryString);
+            String account = params.get("account");
+            String password = params.get("password");
+
+            log.info("로그인 시도: account={}, password={}", account, password);
+            Optional<User> userOptional = InMemoryUserRepository.findByAccount(account);
+
+            if (userOptional.isPresent() && userOptional.get().checkPassword(password)) {
+                log.info("로그인 성공: {}", userOptional.get());
+            } else {
+                log.info("로그인 실패: 아이디 또는 비밀번호가 일치하지 않습니다.");
+            }
+        }
+
+        loadStaticResource(outputStream, "/login.html");
+    }
+
+    private void handleStaticResource(OutputStream outputStream, String path) throws IOException {
+        loadStaticResource(outputStream, path);
+    }
+
+    private Map<String, String> parseQueryString(String queryString) {
+        Map<String, String> params = new HashMap<>();
+        for (String pair : queryString.split("&")) {
+            String[] keyValue = pair.split("=");
+            if (keyValue.length == 2) {
+                params.put(keyValue[0], keyValue[1]);
+            }
+        }
+        return params;
+    }
+
+    private void loadStaticResource(OutputStream outputStream, String path) throws IOException {
+        String resourcePath = "static" + path;
+        try (InputStream fileInputStream = getClass().getClassLoader().getResourceAsStream(resourcePath)) {
+            if (fileInputStream == null) {
+                sendNotFoundResponse(outputStream);
+                return;
+            }
+
+            String contentType = determineContentType(path);
+            byte[] bodyBytes = fileInputStream.readAllBytes();
+            sendOkResponse(outputStream, contentType, bodyBytes);
+        }
+    }
+
+    private String determineContentType(String path) {
+        if (path.endsWith(".css")) {
+            return "text/css";
+        }
+        if (path.endsWith(".js")) {
+            return "application/javascript";
+        }
+        return "text/html;charset=utf-8";
+    }
+
+    private void sendOkResponse(OutputStream outputStream, String contentType, byte[] body) throws IOException {
+        final var response = String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: " + contentType,
+                "Content-Length: " + body.length + " ",
+                "",
+                "");
+
+        outputStream.write(response.getBytes(StandardCharsets.UTF_8));
+        outputStream.write(body);
+        outputStream.flush();
+    }
+
+    private void sendNotFoundResponse(OutputStream outputStream) throws IOException {
+        final var responseBody = "404 Not Found";
+        final var bodyBytes = responseBody.getBytes(StandardCharsets.UTF_8);
+
+        final var response = String.join("\r\n",
+                "HTTP/1.1 404 Not Found ",
+                "Content-Type: text/plain;charset=utf-8 ",
+                "Content-Length: " + bodyBytes.length,
+                "",
+                responseBody);
+
+        outputStream.write(response.getBytes(StandardCharsets.UTF_8));
+        outputStream.flush();
     }
 }

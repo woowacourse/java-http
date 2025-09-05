458c4964e639a935f8a92ae712d290ae7b927deb
ykmxxi <ykmxxi97@gmail.com>
2025-09-05 09:41:28 +0900

    feat: Query String 파싱

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -8,13 +8,17 @@ import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -89,7 +93,35 @@ public class Http11Processor implements Runnable, Processor {
         if ("/".equals(requestTarget)) {
             return "Hello world!".getBytes(StandardCharsets.UTF_8);
         }
+        if (requestTarget.startsWith("/login")) {
+            int index = requestTarget.indexOf("?");
+            if (index == -1) {
+                throw new IllegalArgumentException("ID와 PW를 입력해주세요.");
+            }
+            String path = requestTarget.substring(0, index);
+            String queryString = requestTarget.substring(index + 1);
+            Map<String, String> params = new HashMap<>();
+            for (String param : queryString.split("&")) {
+                String[] keyValue = param.split("=");
+                if (keyValue.length == 2) {
+                    params.put(keyValue[0], keyValue[1]);
+                }
+            }
 
+            User user = InMemoryUserRepository.findByAccount(params.get("account"))
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+            if (!user.checkPassword(params.get("password"))) {
+                throw new IllegalArgumentException("ID 또는 PW가 일치하지 않습니다.");
+            }
+            log.info("로그인 성공. user: {}", user);
+            log.info("path: {}", path);
+
+            final Path resourcePath = Path.of(Objects.requireNonNull(
+                getClass().getClassLoader()
+                    .getResource(String.join("", "static", path + ".html"))
+            ).toURI());
+            return Files.readAllBytes(resourcePath);
+        }
         final Path resourcePath = Path.of(Objects.requireNonNull(
             getClass().getClassLoader()
                 .getResource(String.join("", "static", requestTarget))

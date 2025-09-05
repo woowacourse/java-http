24a45990148c8001a967e2c4ceff2700d8c19480
RaZel713 <razel7130@gmail.com>
2025-09-05 19:12:41 +0900

    feat: IMAGE_X_ICON 추가 및 예외 메시지 개선

--- tomcat/src/main/java/com/techcourse/http/common/ContentType.java
+++ tomcat/src/main/java/com/techcourse/http/common/ContentType.java
@@ -9,6 +9,7 @@ public enum ContentType {
     TEXT_HTML("html", "text/html"),
     TEXT_CSS("css", "text/css"),
     APPLICATION_JAVASCRIPT("js", "application/javascript"),
+    IMAGE_X_ICON("ico", "image/x-icon"),
     IMAGE_SVG_XML("svg", "image/svg+xml"),
     ;
 
@@ -25,7 +26,7 @@ public enum ContentType {
         return Arrays.stream(values())
                 .filter(value -> value.extension.equals(extension))
                 .findFirst()
-                .orElseThrow(() -> new NotFoundException("존재하지 않는 Content type 확장자입니다."));
+                .orElseThrow(() -> new NotFoundException("존재하지 않는 Content type 확장자입니다: " + extension));
     }
 
     public String getMediaType() {

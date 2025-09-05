bb5fd962781deb6049b66a6d64ba0da37afc11b5
jhan0121 <jhan0121@gmail.com>
2025-09-05 23:05:07 +0900

    refactor: HttpRequestBody 생성자 인코딩 명시

--- tomcat/src/main/java/org/apache/coyote/http11/http/request/HttpRequestBody.java
+++ tomcat/src/main/java/org/apache/coyote/http11/http/request/HttpRequestBody.java
@@ -2,6 +2,7 @@ package org.apache.coyote.http11.http.request;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import org.apache.coyote.http11.http.common.header.HttpHeader;
 import org.apache.coyote.http11.http.common.header.HttpHeaderKey;
 
@@ -15,7 +16,7 @@ public class HttpRequestBody {
             return;
         }
         final int contentLength = Integer.parseInt(httpHeader.getValue(HttpHeaderKey.CONTENT_LENGTH.getValue()));
-        this.value = new String(inputStream.readNBytes(contentLength));
+        this.value = new String(inputStream.readNBytes(contentLength), StandardCharsets.UTF_8);
     }
 
     public static HttpRequestBody of(final InputStream inputStream, final HttpHeader httpHeader) throws IOException {

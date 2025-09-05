d42c54603831bc2b75b76650ed45d5f1716166ce
Chaeyoung714 <202102760@hufs.ac.kr>
2025-09-05 22:24:59 +0900

    refactor: 불필요한 코드 제거 및 상수화

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Request.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Request.java
@@ -5,6 +5,11 @@ import java.util.List;
 import java.util.Map;
 
 public class Http11Request {
+
+    private static final String HEADER_DELIMITER = ": ";
+    private static final String QUERY_PARAM_START_SYMBOL = "?";
+    private static final String QUERY_PARAM_DELIMITER = "[=?&]";
+
     private final String method;
     private final String target;
     private final Map<String, String> queryParams;
@@ -14,14 +19,22 @@ public class Http11Request {
 
     public static Http11Request create(final List<String> requestMessage) {
         int pointer = 0;
+
         final String[] firstLine = requestMessage.get(pointer++).split(" ");
         final String method = firstLine[0];
         final String target = firstLine[1];
         final String httpVersion = firstLine[2];
 
+        final Map<String, String> queryParams = getQueryParams(target);
+        final Map<String, String> headers = getHeaders(requestMessage, pointer);
+
+        return new Http11Request(method, target, queryParams, httpVersion, headers, null);
+    }
+
+    private static Map<String, String> getQueryParams(final String target) {
         final Map<String, String> queryParams = new HashMap<>();
-        if (target.contains("?")) {
-            final String[] targetWithQueryParams = target.split("[=?&]");
+        if (target.contains(QUERY_PARAM_START_SYMBOL)) {
+            final String[] targetWithQueryParams = target.split(QUERY_PARAM_DELIMITER);
             int queryParamPointer = 1;
             while (queryParamPointer < targetWithQueryParams.length) {
                 final String key = targetWithQueryParams[queryParamPointer++];
@@ -30,7 +43,13 @@ public class Http11Request {
                 queryParams.put(key, value);
             }
         }
+        return queryParams;
+    }
 
+    private static Map<String, String> getHeaders(
+            final List<String> requestMessage,
+            int pointer
+    ) {
         String line;
         final Map<String, String> headers = new HashMap<>();
         while (pointer < requestMessage.size()) {
@@ -39,28 +58,24 @@ public class Http11Request {
                 break;
             }
 
-            final String[] headerLine = line.split(": ");
-
+            final String[] headerLine = line.split(HEADER_DELIMITER);
             if (headerLine.length != 2) {
                 throw new IllegalArgumentException("Wrong Http Request Header");
             }
 
             headers.put(headerLine[0], headerLine[1]);
         }
-
-        return new Http11Request(method, target, queryParams, httpVersion, headers, null);
-
-//        final StringBuilder body = new StringBuilder();
-//        while ((line = requestMessage.get(pointer++)).isBlank()) {
-//            body.append(line);
-//        }
-//
-//        if (body.isEmpty()) {
-//        }
-//        return new Http11Request(method, target, queryParams, httpVersion, headers, body.toString());
+        return headers;
     }
 
-    private Http11Request(final String method, final String target, final Map<String, String> queryParams, final String httpVersion, final Map<String, String> headers, final String body) {
+    private Http11Request(
+            final String method,
+            final String target,
+            final Map<String, String> queryParams,
+            final String httpVersion,
+            final Map<String, String> headers,
+            final String body
+    ) {
         this.method = method;
         this.target = target;
         this.queryParams = queryParams;
@@ -69,14 +84,14 @@ public class Http11Request {
         this.body = body;
     }
 
-    public String getTarget() {
-        return target;
-    }
-
-    public String findQueryParam(String key) {
+    public String findQueryParam(final String key) {
         if (queryParams.containsKey(key)) {
             return queryParams.get(key);
         }
         throw new IllegalArgumentException("No Query Parameter : " + key);
+    }
+
+    public String getTarget() {
+        return target;
     }
 }

19a9c8e6d67db32a4ff427097c04416ba8205d13
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-09-05 18:23:54 +0900

    refactor: 유저, 인메모리 데이터베이스 코드 개선

--- tomcat/src/main/java/com/techcourse/model/User.java
+++ tomcat/src/main/java/com/techcourse/model/User.java
@@ -1,5 +1,13 @@
 package com.techcourse.model;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
 public class User {
 
     private final Long id;
@@ -7,32 +15,15 @@ public class User {
     private final String password;
     private final String email;
 
-    public User(Long id, String account, String password, String email) {
-        this.id = id;
-        this.account = account;
-        this.password = password;
-        this.email = email;
+    public static User withId(final Long id, final String account, final String password, final String email) {
+        return new User(id, account, password, email);
     }
 
-    public User(String account, String password, String email) {
-        this(null, account, password, email);
-    }
-
-    public boolean checkPassword(String password) {
+    public boolean checkPassword(final String password) {
         return this.password.equals(password);
     }
 
-    public String getAccount() {
-        return account;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", account='" + account + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
+    public boolean isPersisted() { // TODO move to superClass
+        return id != null;
     }
 }

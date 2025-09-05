8bf29832af4fb404c543a01c24d55ea664aa2bd1
RaZel713 <razel7130@gmail.com>
2025-09-05 11:03:28 +0900

    feat: 커스텀 예외 추가
    - NotFoundException 추가
    - UncheckedServletException 수정

---
+++ tomcat/src/main/java/com/techcourse/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.techcourse.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(final String message) {
+        super(message);
+    }
+}

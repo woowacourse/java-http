7e9bcdd0ec0947dce821f144ffae8df790d85062
CheChe903 <cheche903@naver.com>
2025-09-06 19:11:51 +0900

    feat: css 파일 받을 수 있도록 ContentType 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/ContentType.java
@@ -0,0 +1,17 @@
+package org.apache.coyote.http11;
+
+public enum ContentType {
+
+    HTML("text/html;charset=utf-8"),
+    CSS("text/css");
+
+    private String value;
+
+    ContentType(final String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
--- tomcat/src/main/java/org/apache/coyote/http11/HttpResponse.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpResponse.java
@@ -1,5 +1,7 @@
 package org.apache.coyote.http11;
 
+import static org.apache.coyote.http11.ContentType.CSS;
+import static org.apache.coyote.http11.ContentType.HTML;
 import static org.apache.coyote.http11.HttpStatus.NOT_FOUND;
 import static org.apache.coyote.http11.HttpStatus.OK;
 
@@ -17,29 +19,39 @@ public class HttpResponse {
     }
 
     public void ok(final byte[] body) throws IOException {
-        sendResponse(body, OK);
+        sendResponse(body, HTML, OK);
     }
 
     public void ok(final String filePath) throws IOException {
         try (InputStream fileInputStream = getClass().getClassLoader().getResourceAsStream(filePath)) {
             if (fileInputStream == null) {
-                sendResponse(new byte[0], NOT_FOUND);
+                sendResponse(new byte[0], HTML, NOT_FOUND);
                 return;
             }
             byte[] body = fileInputStream.readAllBytes();
-            sendResponse(body, OK);
+            sendResponse(body, getContentType(filePath), OK);
         }
     }
 
-    private void sendResponse(byte[] body, HttpStatus httpStatus) throws IOException {
+    private void sendResponse(byte[] body, ContentType contentType, HttpStatus httpStatus) throws IOException {
         final var response = String.join("\r\n",
                 "HTTP/1.1 " + httpStatus.getStatusCode() + " " + httpStatus.getReasonPhrase() + " ",
-                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Type: " + contentType.getValue() + " ",
                 "Content-Length: " + body.length + " ",
                 "",
                 "");
         outputStream.write(response.getBytes(StandardCharsets.UTF_8));
         outputStream.write(body);
         outputStream.flush();
+    }
+
+    private ContentType getContentType(String filePath) throws IOException {
+        if (filePath.endsWith(".html")) {
+            return HTML;
+        }
+        if (filePath.endsWith(".css")) {
+            return CSS;
+        }
+        throw new IOException("지원하지 않는 파일 형식입니다.");
     }
 }

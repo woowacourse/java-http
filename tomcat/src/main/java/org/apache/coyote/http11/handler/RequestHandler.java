18a8ff6b66b2468cba5abe7fe4a1e59212d51fcb
kimdev <kim__dev@hufs.ac.kr>
2025-09-06 23:03:06 +0900

    refactor: 정적 파일 처리 담당 클래스 분리

--- tomcat/src/main/java/org/apache/coyote/http11/handler/RequestHandler.java
+++ tomcat/src/main/java/org/apache/coyote/http11/handler/RequestHandler.java
@@ -1,18 +1,10 @@
 package org.apache.coyote.http11.handler;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import org.apache.coyote.http11.httpRequest.HttpRequest;
 import org.apache.coyote.http11.httpResponse.HttpResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class RequestHandler {
-
-    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private final ApiRouter apiRouter = new ApiRouter();
 
@@ -29,19 +21,6 @@ public class RequestHandler {
         if (resourceUrl == null) {
             return apiRouter.route(httpRequest);
         }
-        return handleStaticFile(httpRequest, resourceUrl);
-    }
-
-    private HttpResponse handleStaticFile(HttpRequest httpRequest, URL resourceUrl) {
-        try {
-            String responseBody = Files.readString(Path.of(resourceUrl.toURI()));
-            if (httpRequest.getPath().endsWith(".css")) {
-                return new HttpResponse("200 OK", "text/css;charset=utf-8", responseBody);
-            }
-            return new HttpResponse("200 OK", "text/html;charset=utf-8", responseBody);
-        } catch (IOException | URISyntaxException exception) {
-            logger.error(exception.getMessage(), exception);
-            return new HttpResponse("404 Not Found", "text/html;charset=utf-8", null);
-        }
+        return StaticFileHandler.handle(httpRequest, resourceUrl);
     }
 }
---
+++ tomcat/src/main/java/org/apache/coyote/http11/handler/StaticFileHandler.java
@@ -0,0 +1,29 @@
+package org.apache.coyote.http11.handler;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.apache.coyote.http11.httpRequest.HttpRequest;
+import org.apache.coyote.http11.httpResponse.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StaticFileHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(StaticFileHandler.class);
+
+    public static HttpResponse handle(HttpRequest httpRequest, URL resourceUrl) {
+        try {
+            String responseBody = Files.readString(Path.of(resourceUrl.toURI()));
+            if (httpRequest.getPath().endsWith(".css")) {
+                return new HttpResponse("200 OK", "text/css;charset=utf-8", responseBody);
+            }
+            return new HttpResponse("200 OK", "text/html;charset=utf-8", responseBody);
+        } catch (IOException | URISyntaxException exception) {
+            logger.error(exception.getMessage(), exception);
+            return new HttpResponse("404 Not Found", "text/html;charset=utf-8", null);
+        }
+    }
+}

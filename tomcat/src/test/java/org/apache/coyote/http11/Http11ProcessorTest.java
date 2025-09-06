5aa1ac57f92e4b1620a15dccc1177617468f7926
Wooyoung <quantumjump10@gmail.com>
2025-09-06 17:43:46 +0900

    test: css 및 js 테스트 추가

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -1,14 +1,15 @@
 package org.apache.coyote.http11;
 
-import org.junit.jupiter.api.Test;
-import support.StubSocket;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.junit.jupiter.api.Test;
+import support.StubSocket;
 
 class Http11ProcessorTest {
 
@@ -55,6 +56,65 @@ class Http11ProcessorTest {
                 "Content-Length: 5564 \r\n" +
                 "\r\n"+
                 new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+
+        assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @Test
+    void css() throws IOException {
+        // given
+        final String httpRequest= String.join("\r\n",
+                "GET /css/styles.css HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+        final var socket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        Path path = Paths.get(getClass().getClassLoader()
+                .getResource("static/css/styles.css")
+                .getPath());
+        int contentLength = Files.readAllBytes(path).length;
+        var expected = "HTTP/1.1 200 OK \r\n" +
+                "Content-Type: text/css;charset=utf-8 \r\n" +
+                "Content-Length: " + contentLength + " \r\n\r\n"+
+                new String(Files.readAllBytes(path));
+
+        assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @Test
+    void js() throws IOException {
+        // given
+        final String httpRequest= String.join("\r\n",
+                "GET /js/scripts.js HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+        final var socket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        Path path = Paths.get(getClass().getClassLoader()
+                .getResource("static/js/scripts.js")
+                .getPath());
+        int contentLength = Files.readAllBytes(path).length;
+        var expected = "HTTP/1.1 200 OK \r\n" +
+                "Content-Type: application/javascript;charset=utf-8 \r\n" +
+                "Content-Length: " + contentLength + " \r\n" +
+                "\r\n"+
+                new String(Files.readAllBytes(path));
 
         assertThat(socket.output()).isEqualTo(expected);
     }

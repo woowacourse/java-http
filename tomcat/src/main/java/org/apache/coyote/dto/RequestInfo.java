2d7710f066214920c3fc6ef9e453a3ecf66a8218
DEV_WOONG <ywcsuwon@gmail.com>
2025-09-07 20:23:18 +0900

    feat: request 정보를 담는 dto 클래스 구현

--- tomcat/src/main/java/org/apache/coyote/dto/RequestInfo.java
+++ tomcat/src/main/java/org/apache/coyote/dto/RequestInfo.java
@@ -1,4 +1,6 @@
 package org.apache.coyote.dto;
 
-public record RequestInfo(String method, String path) {
+import java.util.Map;
+
+public record RequestInfo(String method, String path, Map<String, String> queryParams) {
 }

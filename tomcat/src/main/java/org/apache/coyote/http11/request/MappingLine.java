ccc07d06d59f9422f7a81197caf2371573536d13
2jin2.1031 <2jin2.1031@gmail.com>
2025-09-07 21:21:06 +0900

    refactor(http11): mappingLine 생성 방식 개선

--- tomcat/src/main/java/org/apache/coyote/http11/request/MappingLine.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/MappingLine.java
@@ -1,27 +1,46 @@
 package org.apache.coyote.http11.request;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class MappingLine { // GET /endPoint HTTP/1.1
 
-    private final String requestMapping; // GET
+    private final String requestMapping; // GET //-
     private final String url; // /endPoint
     private final String protocol; // HTTP/1.1
 
-    public MappingLine(String requestMapping, String url, String protocol) {
+    private MappingLine(String requestMapping, String url, String protocol) {
         this.requestMapping = requestMapping;
         this.url = url;
         this.protocol = protocol;
     }
 
-    public MappingLine(String url, String requestMapping) {
-        this(url, requestMapping, "HTTP/1.1");
+    public MappingLine(String[] parts) {
+        this(parts[0], parts[1], parts[2]);
     }
 
-    @Override
-    public String toString() {
-        return String.join(" ",
-                requestMapping,
-                url,
-                protocol
-        );
+    public MappingLine(BufferedReader bufferedReader) throws IOException {
+        this(parseRequestLine(bufferedReader));
+    }
+
+    private static String[] parseRequestLine(BufferedReader bufferedReader) throws IOException {
+        String requestLine = bufferedReader.readLine();
+        if (requestLine == null || requestLine.isEmpty()) {
+            throw new IllegalArgumentException("request is Empty");
+        }
+
+        String[] parts = requestLine.split(" ", 3);
+        if (parts.length < 3) {
+            throw new IllegalArgumentException();
+        }
+        return parts;
+    }
+
+    public String getRequestMapping() {
+        return requestMapping;
+    }
+
+    public String getUrl() {
+        return url;
     }
 }

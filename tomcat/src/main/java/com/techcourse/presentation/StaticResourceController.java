f4b8116de8805a13816bc68e384fd401be966a1a
saera <yook.saera@gmail.com>
2025-09-08 18:10:38 +0900

    refactor: static 블럭 제거

--- tomcat/src/main/java/com/techcourse/presentation/StaticResourceController.java
+++ tomcat/src/main/java/com/techcourse/presentation/StaticResourceController.java
@@ -6,8 +6,8 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,12 +15,10 @@ import org.slf4j.LoggerFactory;
 public class StaticResourceController implements Controller {
 
     private static final Logger log = LoggerFactory.getLogger(StaticResourceController.class);
-    public static final String BASE_DIRECTORY_NAME = "static";
-    private static final Map<String, Path> RESOURCES = new HashMap<>();
+    private static final String BASE_DIRECTORY_NAME = "static";
+    private static final Map<String, Path> RESOURCE_PATHS = new ConcurrentHashMap<>();
 
-    static {
-        RESOURCES.put("/", null);
-
+    public StaticResourceController() {
         final URL baseUrl = Thread.currentThread()
                 .getContextClassLoader()
                 .getResource(BASE_DIRECTORY_NAME);
@@ -31,33 +29,36 @@ public class StaticResourceController implements Controller {
         }
     }
 
-    private static Path getBasePath(final URL baseUrl) {
-        final Path dirPath;
+    private Path getBasePath(final URL baseUrl) {
+        final Path basePath;
         try {
-            dirPath = Paths.get(baseUrl.toURI());
+            basePath = Paths.get(baseUrl.toURI());
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
-        log.debug("정적 자원 디렉토리 경로: {}", dirPath);
-        return dirPath;
+        log.debug("정적 자원 디렉토리 경로: {}", basePath);
+        return basePath;
     }
 
-    private static void registerResourcePaths(final Path basePath) {
+    private void registerResourcePaths(final Path basePath) {
         try (final Stream<Path> files = Files.walk(basePath)) {
             files.filter(Files::isRegularFile)
                     .forEach(path -> {
                         final String pathName = path.toString().split(BASE_DIRECTORY_NAME)[1];
-                        RESOURCES.put(pathName, path);
+                        RESOURCE_PATHS.computeIfAbsent(pathName, key -> path);
                     });
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        log.debug("정적 자원 경로 등록 완료: {}", RESOURCES.keySet());
+        log.debug("정적 자원 경로 등록 완료: {}", RESOURCE_PATHS.keySet());
     }
 
     @Override
     public boolean isResponsible(final String path) {
-        return RESOURCES.containsKey(path);
+        if ("/".equals(path)) {
+            return true;
+        }
+        return RESOURCE_PATHS.containsKey(path);
     }
 
     @Override
@@ -66,7 +67,7 @@ public class StaticResourceController implements Controller {
             return new ResponseWithType("text/html", "Hello world!");
         }
 
-        final Path filePath = RESOURCES.get(request.path());
+        final Path filePath = RESOURCE_PATHS.get(request.path());
         try {
             final String contentType = Files.probeContentType(filePath);
             final String response = new String(Files.readAllBytes(filePath));

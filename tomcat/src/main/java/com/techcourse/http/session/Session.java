07255076427ad26fdc1b84fa52f7a1ab515549e4
RaZel713 <razel7130@gmail.com>
2025-09-09 03:25:47 +0900

    feat: 세션 관리 기능을 위한 Session 및 SessionRepository 클래스 추가

---
+++ tomcat/src/main/java/com/techcourse/http/session/Session.java
@@ -0,0 +1,38 @@
+package com.techcourse.http.session;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class Session {
+    private final String id;
+    private final Map<String, Object> values = new HashMap<>();
+
+    public Session(final String id) {
+        this.id = id;
+    }
+
+    public static Session newSession() {
+        return new Session(String.valueOf(UUID.randomUUID()));
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Object getAttribute(final String name) {
+        return values.get(name);
+    }
+
+    public void setAttribute(final String name, final Object value) {
+        values.put(name, value);
+    }
+
+    public void removeAttribute(final String name) {
+        values.remove(name);
+    }
+
+    public void invalidate() {
+        SessionRepository.removeById(id);
+    }
+}
---
+++ tomcat/src/main/java/com/techcourse/http/session/SessionRepository.java
@@ -0,0 +1,24 @@
+package com.techcourse.http.session;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class SessionRepository {
+    private static final Map<String, Session> SESSIONS = new HashMap<>();
+
+    public static void save(final Session session) {
+        SESSIONS.put(session.getId(), session);
+    }
+
+    public static Optional<Session> findById(final String id) {
+        if (SESSIONS.containsKey(id)) {
+            return Optional.ofNullable(SESSIONS.get(id));
+        }
+        return Optional.empty();
+    }
+
+    public static void removeById(final String id) {
+        SESSIONS.remove(id);
+    }
+}

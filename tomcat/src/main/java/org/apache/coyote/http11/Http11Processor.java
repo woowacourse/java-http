528dd00a9ec54078e65fd70cc7497171a26c29ae
kjyyjk <rlawnsdud920@gmail.com>
2025-09-08 13:54:33 +0900

    refactor: 미사용 import 제거

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -13,7 +13,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;

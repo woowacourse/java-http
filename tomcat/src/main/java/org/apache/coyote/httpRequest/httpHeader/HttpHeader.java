81cf95d8913c491531fc22a8db8038ad2a13ffef
wogus <ind07152@naver.com>
2025-09-08 17:49:08 +0900

    feat: 쿠키 조회 기능 추가

--- tomcat/src/main/java/org/apache/coyote/httpRequest/httpHeader/HttpHeader.java
+++ tomcat/src/main/java/org/apache/coyote/httpRequest/httpHeader/HttpHeader.java
@@ -1,7 +1,9 @@
 package org.apache.coyote.httpRequest.httpHeader;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import org.apache.coyote.Cookie;
 
 public class HttpHeader {
 
@@ -23,6 +25,33 @@ public class HttpHeader {
 
     public Map<String, String> getQueries() {
         return requestLine.getQueryValues();
+    }
+
+    public boolean hasCookie(final String key) {
+        List<Cookie> cookies = getCookies();
+        if (cookies.isEmpty()) {
+            return false;
+        }
+        for (final Cookie cookie : cookies) {
+            if (cookie.getKey().equals(key)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private List<Cookie> getCookies() {
+        final List<Cookie> findCookies = new ArrayList<>();
+        final String cookieValues = headers.get("Cookie");
+        if (cookieValues == null) {
+            return findCookies;
+        }
+        final String[] cookies = cookieValues.split(";");
+        for (String cookie : cookies) {
+            String[] cookieValue = cookie.split("=");
+            findCookies.add(new Cookie(cookieValue[0], cookieValue[1]));
+        }
+        return findCookies;
     }
 
     public String getHeader(final String key) {

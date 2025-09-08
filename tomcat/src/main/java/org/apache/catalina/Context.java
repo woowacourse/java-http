4c48b27a6e65578d37393e5913bc1d35130dfa3a
MinJun Choi <mj043000@naver.com>
2025-09-08 17:35:56 +0900

    refactor: 서블릿 초기화 로직 Context에서 Tomcat으로 이동
    - Context에서 ServletContainer 초기화 로직 제거 및 Tomcat으로 이동
    - Tomcat 생성자에서 ServletContainer 직접 생성 및 설정
    - Context 간소화: ServletContainer 관련 코드 제거
    - Tomcat 클래스에 서블릿 등록 로직 추가
      - `/login`, `/register`, `/*` 경로에 각각 `LoginServlet`, `RegisterServlet`, `StaticResourceServlet` 등록
    - 코드 가독성 및 역할 분리 개선

--- tomcat/src/main/java/org/apache/catalina/Context.java
+++ tomcat/src/main/java/org/apache/catalina/Context.java
@@ -1,18 +1,8 @@
 package org.apache.catalina;
 
-import com.techcourse.servlet.LoginServlet;
-import com.techcourse.servlet.RegisterServlet;
-import com.techcourse.servlet.StaticResourceServlet;
-
 public class Context {
 
     public ServletContainer createServletContainer() {
-        final ServletContainer container = new ServletContainer();
-
-        container.addServlet("/login", new LoginServlet());
-        container.addServlet("/register", new RegisterServlet());
-        container.addServlet("/*", new StaticResourceServlet());
-
-        return container;
+        return new ServletContainer();
     }
 }

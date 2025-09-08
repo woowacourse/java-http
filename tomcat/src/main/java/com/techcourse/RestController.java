2d8f18ad6b97b29eadf635cb1684bd1650f92bd7
2jin2.1031 <2jin2.1031@gmail.com>
2025-09-08 20:37:45 +0900

    feat: 로깅 추가

--- tomcat/src/main/java/com/techcourse/RestController.java
+++ tomcat/src/main/java/com/techcourse/RestController.java
@@ -2,11 +2,15 @@ package com.techcourse;
 
 import com.techcourse.model.User;
 import java.util.Map;
+import org.apache.coyote.http11.Http11Processor;
 import org.apache.coyote.http11.response.HttpResponse;
 import org.apache.coyote.http11.response.ResponseEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RestController {
 
+    private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
     private final Service service;
 
     public RestController(Service service) {
@@ -19,6 +23,7 @@ public class RestController {
 
     public HttpResponse signIn(Map<String, String> loginRequest) {
         User user = service.getUser(loginRequest);
+        log.info("{}", user.toString());
         return ResponseEntity.ok(user.toString());
     }
 }

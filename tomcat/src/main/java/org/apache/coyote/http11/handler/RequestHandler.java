306dfd500e2dd83bace81ecef3d39f286e2c37d6
threepebbles <threepebbles@naver.com>
2025-09-09 16:50:19 +0900

    chore: 주석 추가

--- tomcat/src/main/java/org/apache/coyote/http11/handler/RequestHandler.java
+++ tomcat/src/main/java/org/apache/coyote/http11/handler/RequestHandler.java
@@ -3,6 +3,7 @@ package org.apache.coyote.http11.handler;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.response.HttpResponse;
 
+// StaticResourceHandler(정적 리소스 처리 전용 핸들러)와 ControllerHandler(그 외의 엔드포인트)로 분리
 public interface RequestHandler {
 
     boolean canHandle(HttpRequest request);

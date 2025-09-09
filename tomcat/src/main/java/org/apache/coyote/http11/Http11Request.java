1f9128e3c063bce0a09c103c0a4da955c337d221
AppleMint98 <applemint9821@gmail.com>
2025-09-09 10:45:44 +0900

    fix: charset 고정

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Request.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Request.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -17,7 +18,7 @@ public class Http11Request {
     private Http11Cookie cookie;
 
     public Http11Request(final InputStream inputStream) throws IOException {
-        final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
         String[] requestLine = reader.readLine().split(" ");
         this.method = requestLine[0];
         this.uri = requestLine[1];

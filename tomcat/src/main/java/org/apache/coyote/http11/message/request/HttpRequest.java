41e60d330668e60e8f2c8ab968e6a99f66a11250
Wooyoung <quantumjump10@gmail.com>
2025-09-10 02:06:21 +0900

    refactor: 파싱 로직을 HttpRequest 내부로 이동

--- tomcat/src/main/java/org/apache/coyote/http11/message/request/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/http11/message/request/HttpRequest.java
@@ -1,18 +1,32 @@
 package org.apache.coyote.http11.message.request;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import org.apache.coyote.http11.message.HttpHeaders;
 
 public class HttpRequest {
     private final String method;
     private final String path;
     private final Map<String, String> queryParams;
+    private final HttpHeaders headers;
     private final String version;
+    private final String body;
 
-    public HttpRequest(String method, String path, Map<String, String> queryParams, String version) {
+    private HttpRequest(String method, String path, Map<String, String> queryParams, HttpHeaders headers, String version,
+                       String body) {
         this.method = method;
         this.path = path;
         this.queryParams = queryParams;
+        this.headers = headers;
         this.version = version;
+        this.body = body;
+    }
+
+    public static HttpRequest from(final BufferedReader reader) throws IOException {
+        return parse(reader);
     }
 
     public String getMethod() {
@@ -27,7 +41,94 @@ public class HttpRequest {
         return queryParams;
     }
 
+    public HttpHeaders getHeaders() {
+        return headers;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
     public String getVersion() {
         return version;
+    }
+
+    public boolean equalMethod(String method) {
+        return this.method.equals(method);
+    }
+
+    public boolean equalContentType(final String mimeType) {
+        final String contentType = headers.getFirst("Content-Type");
+        return contentType.equals(mimeType);
+    }
+
+    public Map<String, String> getFormParams() {
+        if (equalContentType("application/x-www-form-urlencoded")) {
+            return parseUrlEncoded(body);
+        }
+        return Collections.emptyMap();
+    }
+
+    private static HttpRequest parse(final BufferedReader reader) throws IOException {
+        final String requestLine = reader.readLine();
+
+        if (requestLine == null || requestLine.isBlank()) {
+            throw new IllegalArgumentException("Empty request line");
+        }
+
+        final String[] parts = requestLine.split(" ");
+
+        validateRequestLine(parts, requestLine);
+
+        final String method = parts[0];
+        final String requestUri = parts[1];
+        final String version = parts[2];
+
+        final HttpHeaders headers = new HttpHeaders();
+        String line;
+        while ((line = reader.readLine()) != null && !line.isBlank()) {
+            String[] headerParts = line.split(": ", 2);
+            headers.addHeader(headerParts[0], headerParts[1]);
+        }
+
+        String path = requestUri;
+        if (requestUri.contains("?")) {
+            final int index = requestUri.indexOf("?");
+            path = requestUri.substring(0, index);
+            final String queryString = requestUri.substring(index + 1);
+            return new HttpRequest(method, path, parseUrlEncoded(queryString), headers, version, null);
+        }
+
+        String body = null;
+        if ("POST".equals(method)) {
+            int contentLength = Integer.parseInt(headers.getContentLength());
+            if (contentLength > 0) {
+                char[] bodyChars = new char[contentLength];
+                reader.read(bodyChars, 0, contentLength);
+                body = new String(bodyChars);
+            }
+            return new HttpRequest(method, path, Collections.emptyMap(), headers, version, body);
+        }
+
+        return new HttpRequest(method, path, Collections.emptyMap(), headers, version, null);
+    }
+
+    private static void validateRequestLine(String[] parts, String requestLine) {
+        if (parts.length != 3) {
+            throw new IllegalArgumentException("Invalid HTTP request line: " + requestLine);
+        }
+    }
+
+    private static Map<String, String> parseUrlEncoded(final String input) {
+        final Map<String, String> params = new HashMap<>();
+        final String[] pairs = input.split("&");
+
+        for (String pair : pairs) {
+            final String[] keyValue = pair.split("=", 2);
+            if (keyValue.length == 2) {
+                params.put(keyValue[0], keyValue[1]);
+            }
+        }
+        return params;
     }
 }

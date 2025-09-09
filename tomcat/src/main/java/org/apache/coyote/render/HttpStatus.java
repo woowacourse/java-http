5dd456437c1b0faa9c533f7f5795c5130d32587e
DEV_WOONG <ywcsuwon@gmail.com>
2025-09-09 10:10:19 +0900

    feat: HTTP 상태코드 클래스 구현

---
+++ tomcat/src/main/java/org/apache/coyote/render/HttpStatus.java
@@ -0,0 +1,41 @@
+package org.apache.coyote.render;
+
+public enum HttpStatus {
+    OK(200, "OK"),
+    CREATED(201, "Created"),
+    NO_CONTENT(204, "No Content"),
+
+    MOVED_PERMANENTLY(301, "Moved Permanently"),
+    FOUND(302, "Found"),
+    NOT_MODIFIED(304, "Not Modified"),
+
+    BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    CONFLICT(409, "Conflict"),
+    UNPROCESSABLE_ENTITY(422, "Unprocessable Entity"),
+    TOO_MANY_REQUESTS(429, "Too Many Requests"),
+
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    BAD_GATEWAY(502, "Bad Gateway"),
+    SERVICE_UNAVAILABLE(503, "Service Unavailable"),
+    GATEWAY_TIMEOUT(504, "Gateway Timeout");
+
+    private final int statusCode;
+    private final String message;
+
+    HttpStatus(final int statusCode, final String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

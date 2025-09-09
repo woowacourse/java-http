e3843da83b3a551f25b8a40c6ce52f0e17e1043c
DEV_WOONG <ywcsuwon@gmail.com>
2025-09-09 11:01:29 +0900

    feat: 의존성 관리를 위한 Config 클래스  생성

---
+++ tomcat/src/main/java/org/apache/coyote/config/AppConfig.java
@@ -0,0 +1,31 @@
+package org.apache.coyote.config;
+
+import org.apache.coyote.render.PageRenderer;
+import org.apache.coyote.render.UserLoginHandler;
+import org.apache.coyote.render.UserRegisterHandler;
+import org.apache.coyote.router.RequestRouter;
+
+public class AppConfig {
+
+    private static AppConfig instance;
+    private final RequestRouter requestRouter;
+
+    private AppConfig() {
+        this.requestRouter = createRequestRouter();
+    }
+
+    public static AppConfig getInstance() {
+        if (instance == null) {
+            instance = new AppConfig();
+        }
+        return instance;
+    }
+
+    public RequestRouter getRequestRouter() {
+        return requestRouter;
+    }
+
+    private RequestRouter createRequestRouter() {
+        return new RequestRouter(new UserLoginHandler(), new UserRegisterHandler(), new PageRenderer());
+    }
+}

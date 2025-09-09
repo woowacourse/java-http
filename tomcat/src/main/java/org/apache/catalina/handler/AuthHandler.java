d50ce1a275057a5c33e91ef91f548e996750f0c0
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-09-10 01:07:39 +0900

    [step2] 회원가입 구현

--- tomcat/src/main/java/org/apache/catalina/handler/AuthHandler.java
+++ tomcat/src/main/java/org/apache/catalina/handler/AuthHandler.java
@@ -13,18 +13,25 @@ import java.util.function.BiConsumer;
 import org.apache.catalina.StaticResourceUtils;
 import org.apache.coyote.util.HttpRequest;
 import org.apache.coyote.util.HttpResponse;
+import org.apache.coyote.util.RequestBody;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LoginHandler implements HttpHandler {
+public class AuthHandler implements HttpHandler {
 
-    private static final Logger log = LoggerFactory.getLogger(LoginHandler.class);
+    private static final Logger log = LoggerFactory.getLogger(AuthHandler.class);
 
     private final Map<String, BiConsumer<HttpRequest, HttpResponse>> PATH_HANDLER = Map.of(
             "POST/login", this::login,
-            "GET/login", this::getLoginPage
+            "GET/login", this::getLoginPage,
+            "POST/register", this::register,
+            "GET/register", this::getRegisterPage
     );
-    private final List<String> PATHS = List.of("/login");
+
+    private final List<String> PATHS = List.of(
+            "/login",
+            "/register"
+    );
 
     @Override
     public void handle(final HttpRequest request, final HttpResponse response) {
@@ -37,6 +44,33 @@ public class LoginHandler implements HttpHandler {
     @Override
     public List<String> getAllPath() {
         return new ArrayList<>(PATHS);
+    }
+
+    private void register(HttpRequest httpRequest, HttpResponse httpResponse) {
+        RequestBody requestBody = httpRequest.requestBody();
+        String account = requestBody.getValue("account");
+        String email = requestBody.getValue("email");
+        String password = requestBody.getValue("password");
+        InMemoryUserRepository.save(new User(account, email, password));
+
+        String httpVersion = httpRequest.getHttpVersion();
+        int statusCode = 302;
+        String statusMessage = "Found";
+        String responseBody = StaticResourceUtils.readResourceContent("/index.html");
+
+        httpResponse.putHeader("Content-Type", getContentType(httpRequest.getRequestPath()) + ";charset=utf-8");
+        httpResponse.putHeader("Content-Length", String.valueOf(responseBody.getBytes(StandardCharsets.UTF_8).length));
+        httpResponse.update(httpVersion, statusCode, statusMessage, responseBody);
+    }
+
+    private void getRegisterPage(HttpRequest httpRequest, HttpResponse httpResponse) {
+        String httpVersion = httpRequest.getHttpVersion();
+        String responseBody = StaticResourceUtils.readResourceContent(httpRequest.getRequestPath());
+        int statusCode = 200;
+        String statusMessage = "OK";
+        httpResponse.putHeader("Content-Type", StaticResourceUtils.getContentType(httpRequest.getRequestPath()) + ";charset=utf-8");
+        httpResponse.putHeader("Content-Length", String.valueOf(responseBody.getBytes(StandardCharsets.UTF_8).length));
+        httpResponse.update(httpVersion, statusCode, statusMessage, responseBody);
     }
 
     private void getLoginPage(HttpRequest httpRequest, HttpResponse httpResponse) {
--- tomcat/src/main/java/org/apache/catalina/handler/HttpHandlerMapper.java
+++ tomcat/src/main/java/org/apache/catalina/handler/HttpHandlerMapper.java
@@ -3,19 +3,23 @@ package org.apache.catalina.handler;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HttpHandlerMapper {
 
+    private static final Logger log = LoggerFactory.getLogger(AuthHandler.class);
     private static final Map<List<String>, HttpHandler> MAPPER = new HashMap<>();
 
     static {
         HttpHandler staticHandler = new StaticHandler();
-        HttpHandler loginHandler = new LoginHandler();
+        HttpHandler loginHandler = new AuthHandler();
         MAPPER.put(staticHandler.getAllPath(), staticHandler);
         MAPPER.put(loginHandler.getAllPath(), loginHandler);
     }
 
     public HttpHandler mappingHandler(String path) {
+        log.info("찾으려는 경로 : {}", path);
         return MAPPER.keySet().stream()
                 .filter(paths -> paths.contains(path))
                 .map(MAPPER::get)

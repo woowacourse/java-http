7886091747b9111f883a3e018f387b65dca2964f
threepebbles <threepebbles@naver.com>
2025-09-09 20:31:13 +0900

    feat: Session 구현하기

--- tomcat/src/main/java/com/techcourse/controller/LoginController.java
+++ tomcat/src/main/java/com/techcourse/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.techcourse.service.LoginService;
 import java.util.Map;
 import java.util.UUID;
 import org.apache.catalina.controller.AbstractController;
+import org.apache.catalina.session.Cookie;
 import org.apache.catalina.session.Session;
 import org.apache.catalina.session.SessionManager;
 import org.apache.coyote.http11.request.HttpRequest;
@@ -23,6 +24,13 @@ public class LoginController extends AbstractController {
 
     @Override
     protected void doGet(HttpRequest httpRequest, HttpResponse httpResponse) {
+        if (isLoginUser(httpRequest)) {
+            String body = ResourceUtil.readStaticResource("/index.html", this.getClass());
+
+            httpResponse.setHttpResponse(ResponseEntity.found(body, "text/html;charset=utf-8"));
+            return;
+        }
+
         Map<String, String> queryParams = httpRequest.getQueryParams();
 
         // account, password 쿼리 파라미터가 둘 다 없는 경우 login.html 반환
@@ -52,9 +60,24 @@ public class LoginController extends AbstractController {
         httpResponse.setHttpResponse(ResponseEntity.found(body, "text/html;charset=utf-8"));
 
         Session session = new Session(UUID.randomUUID().toString());
-        session.setAttribute("account", account);
+        session.setAttribute(session.getId(), account);
         sessionManager.add(session);
         httpResponse.addCookie("JSESSIONID=" + session.getId());
+    }
+
+    private boolean isLoginUser(HttpRequest httpRequest) {
+        Cookie cookie = httpRequest.getCookie();
+        String sessionIdInCookie = cookie.getAttribute("JSESSIONID");
+        if (sessionIdInCookie == null) {
+            return false;
+        }
+
+        String sessionId = sessionManager.findSession(sessionIdInCookie).getId();
+        if (sessionId == null) {
+            return false;
+        }
+
+        return sessionIdInCookie.equals(sessionId);
     }
 
     private boolean isValidQueryParams(Map<String, String> queryParams) {
--- tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequest.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.catalina.session.Cookie;
 
 /**
  * @see <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/Messages#http_requests"></a>
@@ -72,6 +73,10 @@ public class HttpRequest {
 
     public Map<String, String> getHeaders() {
         return headers;
+    }
+
+    public Cookie getCookie() {
+        return new Cookie(headers.get("Cookie"));
     }
 
     public Map<String, String> getQueryParams() {

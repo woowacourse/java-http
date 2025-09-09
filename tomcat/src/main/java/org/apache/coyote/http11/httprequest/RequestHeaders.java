2d0395d88b12fc2c0db48d6cbcb16d07e6ebd6ca
seaniiio <cnc4750@naver.com>
2025-09-10 02:03:56 +0900

    refactor: 헤더 형식이 올바르지 않은 경우 400 에러를 응답하도록 수정

--- tomcat/src/main/java/org/apache/coyote/http11/httprequest/RequestHeaders.java
+++ tomcat/src/main/java/org/apache/coyote/http11/httprequest/RequestHeaders.java
@@ -1,8 +1,10 @@
 package org.apache.coyote.http11.httprequest;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import org.apache.coyote.http11.exception.HttpStatusException;
+import org.apache.coyote.http11.httpresponse.HttpStatusCode;
 
 public class RequestHeaders {
 
@@ -11,14 +13,16 @@ public class RequestHeaders {
     private final Map<String, String> headers;
 
     public static RequestHeaders from(final List<String> rawRequestHeaders) {
-        final Map<String, String> bodyParameters = rawRequestHeaders.stream()
-                .map(param -> param.split(HEADER_SEPARATOR))
-                .collect(Collectors.toMap(
-                        param -> param[0],
-                        param -> param[1],
-                        (oldValue, newValue) -> newValue
-                ));
-
+        final Map<String, String> bodyParameters = new HashMap<>();
+        for (String header : rawRequestHeaders) {
+            final String[] keyAndValue = header.split(HEADER_SEPARATOR);
+            final String key = keyAndValue[0];
+            if (keyAndValue.length != 2) {
+                throw new HttpStatusException(HttpStatusCode.NOT_FOUND);
+            }
+            final String value = keyAndValue[1];
+            bodyParameters.put(key, value);
+        }
         return new RequestHeaders(bodyParameters);
     }
 

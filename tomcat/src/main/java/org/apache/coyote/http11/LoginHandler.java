25c244d792cdeac37492fbdecf596ed3eecf6453
Wooyoung <quantumjump10@gmail.com>
2025-09-09 19:24:14 +0900

    feat: 로그인 페이지 리다이렉트 로직 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/LoginHandler.java
@@ -0,0 +1,85 @@
+package org.apache.coyote.http11;
+
+import com.techcourse.db.InMemoryUserRepository;
+import com.techcourse.model.User;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Optional;
+import org.apache.coyote.http11.message.HttpHeaders;
+import org.apache.coyote.http11.message.StatusLine;
+import org.apache.coyote.http11.message.request.HttpRequest;
+import org.apache.coyote.http11.message.response.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoginHandler implements Handler {
+
+    private static final Logger log = LoggerFactory.getLogger(LoginHandler.class);
+
+    @Override
+    public boolean canHandle(HttpRequest request) {
+        return "/login".equals(request.getPath());
+    }
+
+    @Override
+    public HttpResponse handle(HttpRequest request) throws IOException {
+        final String httpVersion = request.getVersion();
+
+        final String account = request.getQueryParams().get("account");
+        final String password = request.getQueryParams().get("password");
+
+        if (account == null || password == null) {
+            return loginPageResponse(httpVersion,"/login.html");
+        }
+
+        Optional<User> authenticatedUser = authenticate(account, password);
+
+        if (authenticatedUser.isPresent()) {
+            User user = authenticatedUser.get();
+            log.info("user : {}", user);
+            return redirectResponse(httpVersion,"/index.html");
+        }
+
+        return redirectResponse(httpVersion,"/401.html");
+    }
+
+    private Optional<User> authenticate(final String account, final String password) {
+        return InMemoryUserRepository.findByAccount(account)
+                .filter(user -> user.checkPassword(password));
+    }
+
+    private HttpResponse loginPageResponse(String httpVersion, String location) throws IOException {
+        String resourcePath = "static" + location;
+        URL resource = getClass().getClassLoader().getResource(resourcePath);
+
+        String body = "";
+        if (resource != null) {
+            body = Files.readString(new File(resource.getFile()).toPath(), StandardCharsets.UTF_8);
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.addHeader("Content-Type", ContentType.HTML.getMimeType());
+        headers.addHeader("Content-Length", String.valueOf(body.getBytes(StandardCharsets.UTF_8).length));
+
+        return new HttpResponse(
+                new StatusLine(httpVersion, StatusCode.OK),
+                headers,
+                body.getBytes(StandardCharsets.UTF_8)
+        );
+    }
+
+    private HttpResponse redirectResponse(final String httpVersion, final String location) throws IOException {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.addHeader("location", location);
+        headers.addHeader("Content-Length", "0");
+
+        return new HttpResponse(
+                new StatusLine(httpVersion, StatusCode.FOUND),
+                headers,
+                new byte[0]
+        );
+    }
+}

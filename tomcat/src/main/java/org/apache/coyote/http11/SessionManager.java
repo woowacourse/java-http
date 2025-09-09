a8f76ceda7ca66e6209d2ac970f8a5913c1ba2b9
jjunh0 <94045552+jjunh0@users.noreply.github.com>
2025-09-09 21:01:12 +0900

    [2단계 - 로그인 구현하기] 두리(김준호) 미션 제출합니다. (#885)
    * feat: 1단계 미션 구현
    
    * feat: 회원 정보 일치 여부 검사 추가
    
    * fix: 파싱 버그 수정
    
    * feat: 2단계 요구사항 구현
    
    * feat: 세션 매니저 싱글턴 구현, 로그인시 GET /login 요청에 리다이렉션 하도록 수정

---
+++ tomcat/src/main/java/org/apache/coyote/http11/SessionManager.java
@@ -0,0 +1,32 @@
+package org.apache.coyote.http11;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.catalina.Manager;
+
+public class SessionManager implements Manager {
+    // static!
+    private static final Map<String, Session> SESSIONS = new HashMap<>();
+    private static final SessionManager INSTANCE = new SessionManager();
+
+    public static SessionManager getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public void add(final Session session) {
+        SESSIONS.put(session.getId(), session);
+    }
+
+    @Override
+    public Session findSession(final String id) {
+        return SESSIONS.get(id);
+    }
+
+    @Override
+    public void remove(final String id) {
+        SESSIONS.remove(id);
+    }
+
+    private SessionManager() {}
+}

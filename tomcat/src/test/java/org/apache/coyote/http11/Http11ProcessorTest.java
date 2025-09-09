65480d6ccbd9876c36d044c26d239274cbd17ee0
songsunkook <songsunkook@gmail.com>
2025-09-09 10:15:20 +0900

    test: 테스트 수정

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -59,9 +59,10 @@ class Http11ProcessorTest {
         // Paths.get()에서 내부적으로 URL 디코딩이 수행되어 올바른 경로 생성.
         final URL resource = getClass().getClassLoader().getResource("static/index.html");
         Path path = Paths.get(resource.toURI());
+        int length = Files.readAllBytes(path).length;
         var expected = "HTTP/1.1 200 OK\r\n" +
                 "Content-Type: text/html;charset=utf-8\r\n" +
-                "Content-Length: 5564\r\n" +
+                "Content-Length: " + length + "\r\n" +
                 "\r\n"+
                 new String(Files.readAllBytes(path));
 

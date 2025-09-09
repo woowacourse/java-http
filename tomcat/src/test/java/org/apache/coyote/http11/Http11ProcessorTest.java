eb9f4b2f31e39e6b3e04d89a0f01984eb17354e4
minSsan <msp1125@hanyang.ac.kr>
2025-09-09 16:05:19 +0900

    test(Http11Processor): 로그인 성공, 실패 테스트 케이스 추가

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -1,5 +1,6 @@
 package org.apache.coyote.http11;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import support.StubSocket;
 
@@ -57,5 +58,47 @@ class Http11ProcessorTest {
                 new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
 
         assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @DisplayName("올바른 id, password 입력 시 리다이렉션")
+    @Test
+    void success_login() {
+        // given
+        final String request = String.join("\r\n",
+                "GET /login?account=gugu&password=password HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+        final var socket = new StubSocket(request);
+        final var processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        assertThat(socket.output()).contains("HTTP/1.1 302 Found");
+    }
+
+    @DisplayName("유효하지 않은 id, password 입력 시 401")
+    @Test
+    void error_login() {
+        // given
+        final String request = String.join("\r\n",
+                "GET /login?account=&password= HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+
+        final var socket = new StubSocket(request);
+        final var processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        assertThat(socket.output()).contains("HTTP/1.1 401 Unauthorized");
     }
 }

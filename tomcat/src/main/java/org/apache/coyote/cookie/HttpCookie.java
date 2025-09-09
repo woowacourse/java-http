50cc8b73dc5f797ca359a6ebf3c4d1f00db37bd5
DEV_WOONG <ywcsuwon@gmail.com>
2025-09-09 21:01:45 +0900

    feat: HttpCookie 클래스 생성

---
+++ tomcat/src/main/java/org/apache/coyote/cookie/HttpCookie.java
@@ -0,0 +1,47 @@
+package org.apache.coyote.cookie;
+
+import com.techcourse.model.User;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class HttpCookie {
+    private static final String JSESSIONID = "JSESSIONID";
+
+    private final Map<String, User> sessionStorage = new HashMap<>();
+    private Map<String, String> cookies = new HashMap<>();
+
+    public HttpCookie(String cookieHeader) {
+        if (cookieHeader != null && !cookieHeader.isEmpty()) {
+            parseCookies(cookieHeader);
+        }
+    }
+
+    public boolean hasJSESSIONID() {
+        return cookies.containsKey(JSESSIONID);
+    }
+
+    public String getSessionId() {
+        return cookies.get(JSESSIONID);
+    }
+
+    public String generateJSESSIONID(User user) {
+        UUID uuid = UUID.randomUUID();
+        String sessionId = uuid.toString();
+        sessionStorage.put(sessionId, user);
+        return sessionId;
+    }
+
+    private void parseCookies(String cookieHeader) {
+        String[] pairs = cookieHeader.split(";");
+        for (String pair : pairs) {
+            String[] keyValue = pair.trim().split("=", 2);
+            if (keyValue.length == 2) {
+                String key = keyValue[0].trim();
+                String value = keyValue[1].trim();
+                cookies.put(key, value);
+            }
+        }
+    }
+}
+

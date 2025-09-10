4e4a54e84e7bdcb859f0b17441763b11b47e8381
2jin2.1031 <2jin2.1031@gmail.com>
2025-09-10 20:19:41 +0900

    fix(RestController): 로거 초기화 시 잘못된 클래스 참조 수정

--- tomcat/src/main/java/com/techcourse/controller/RestController.java
+++ tomcat/src/main/java/com/techcourse/controller/RestController.java
@@ -6,7 +6,6 @@ import com.techcourse.model.User;
 import com.techcourse.service.Service;
 import java.util.Map;
 import java.util.UUID;
-import org.apache.coyote.http11.Http11Processor;
 import org.apache.coyote.http11.response.HttpResponse;
 import org.apache.coyote.http11.response.ResponseEntity;
 import org.slf4j.Logger;
@@ -14,7 +13,7 @@ import org.slf4j.LoggerFactory;
 
 public class RestController {
 
-    private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final Logger log = LoggerFactory.getLogger(RestController.class);
     private final Service service;
     private final SessionManager sessionManager;
 

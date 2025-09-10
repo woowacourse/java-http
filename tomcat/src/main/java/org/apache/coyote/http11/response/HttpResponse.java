133628e86a35c0289379d4205e8bf18287e5b4cb
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-09-10 20:59:48 +0900

    refactor: HttpResponse 리팩토링

--- tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponse.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponse.java
@@ -1,40 +1,53 @@
 package org.apache.coyote.http11.response;
 
-public record HttpResponse(
-        HttpStatus status,
-        HttpResponseHeader header,
-        String body) {
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpResponse {
+    
+    private final HttpStatus status;
+    private final String body;
+    private final Map<String, String> headers = new HashMap<>();
+
+    public HttpResponse(HttpStatus status, String body) {
+        this.status = status;
+        this.body = body;
+    }
+
+    public void addHeader(String name, String value) {
+        headers.put(name, value);
+    }
 
     public static HttpResponse ok(String body, MimeType mimeType) {
-        return new HttpResponse(
-            HttpStatus.OK,
-            HttpResponseHeader.of(mimeType, body),
-            body
-        );
+        HttpResponse response = new HttpResponse(HttpStatus.OK, body);
+        response.addHeader("Content-Type", mimeType.getValue());
+        response.addHeader("Content-Length", String.valueOf(body.getBytes().length));
+        return response;
     }
 
     public static HttpResponse notFound() {
-        return new HttpResponse(
-            HttpStatus.NOT_FOUND,
-            HttpResponseHeader.of(MimeType.TEXT_HTML, "404 Not Found"),
-            "404 Not Found"
-        );
+        HttpResponse response = new HttpResponse(HttpStatus.NOT_FOUND, "404 Not Found");
+        response.addHeader("Content-Type", "text/html");
+        response.addHeader("Content-Length", String.valueOf("404 Not Found".getBytes().length));
+        return response;
     }
 
     public static HttpResponse redirect(String location) {
-        return new HttpResponse(
-            HttpStatus.FOUND,
-            HttpResponseHeader.redirect(location),
-            ""
-        );
+        HttpResponse response = new HttpResponse(HttpStatus.FOUND, "");
+        response.addHeader("Location", location);
+        return response;
     }
 
     public String toHttpResponse() {
-        return String.join("\r\n",
-                status.getStatusLine() + " ",
-                header.toHeaderString(),
-                "",
-                body);
+        StringBuilder response = new StringBuilder();
+        response.append(status.getStatusLine()).append("\r\n");
+        
+        for (Map.Entry<String, String> header : headers.entrySet()) {
+            response.append(header.getKey()).append(": ").append(header.getValue()).append("\r\n");
+        }
+        
+        response.append("\r\n").append(body);
+        return response.toString();
     }
 
     public byte[] getBytes() {

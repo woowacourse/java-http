e92a6ea9aa3144dac201a70f27cef660c39172dd
wogus <ind07152@naver.com>
2025-09-10 15:43:49 +0900

    feat: 에러 코드 및 커스텀 예외 생성

---
+++ tomcat/src/main/java/org/apache/coyote/error/ErrorCode.java
@@ -0,0 +1,27 @@
+package org.apache.coyote.error;
+
+import org.apache.coyote.httpResponse.StatusCode;
+
+public enum ErrorCode {
+
+    NOT_ALLOW_PATH("지원하지 않은 경로입니다.", StatusCode.NOT_FOUND),
+    NOT_ALLOW_METHOD("지원하지 않은 HTTP 메서드입니다.", StatusCode.NOT_ALLOW_METHOD),
+    NOT_EXISTS_STATIC_RESOURCE("존재하지 않은 정적 파일입니다.", StatusCode.NOT_FOUND),
+    NOT_EXISTS_MEMBER("존재하지 않은 회원입니다.", StatusCode.UNAUTHORIZED);
+
+    private final String message;
+    private final StatusCode statusCode;
+
+    ErrorCode(final String message, final StatusCode statusCode) {
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public StatusCode getStatusCode() {
+        return statusCode;
+    }
+}

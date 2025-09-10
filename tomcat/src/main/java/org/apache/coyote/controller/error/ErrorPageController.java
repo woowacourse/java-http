2b4b3b4bff3d2e8506fd5004d2a769816206e5c0
wogus <ind07152@naver.com>
2025-09-10 15:50:41 +0900

    feat: 에러 처리 기능 추가

---
+++ tomcat/src/main/java/org/apache/coyote/controller/error/ErrorPageController.java
@@ -0,0 +1,48 @@
+package org.apache.coyote.controller.error;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.apache.coyote.controller.Controller;
+import org.apache.coyote.error.ErrorPage;
+import org.apache.coyote.httpRequest.HttpRequest;
+import org.apache.coyote.httpRequest.httpHeader.HttpHeader;
+import org.apache.coyote.httpResponse.HttpResponse;
+
+public class ErrorPageController implements Controller {
+
+    @Override
+    public void service(final HttpRequest request, final HttpResponse response) throws IOException {
+        final HttpHeader httpHeader = request.getHttpHeader();
+        String path = httpHeader.getPurePath();
+        responseErrorPage(path, response);
+    }
+
+    private void responseErrorPage(
+            final String errorPagePath,
+            final HttpResponse httpResponse
+    ) throws IOException {
+        final String body = getStaticResponseBody("static" + errorPagePath);
+        httpResponse.updateStatusLine("HTTP/1.1", ErrorPage.findStatusCode(errorPagePath));
+        httpResponse.updateBody(body);
+        httpResponse.addHeader("Content-Type", "text/html;charset=utf-8");
+        httpResponse.addHeader("Content-Length", String.valueOf(body.getBytes(StandardCharsets.UTF_8).length));
+    }
+
+    private String getStaticResponseBody(final String fileUrl) throws IOException {
+        try {
+            final URI uri = getClass().getClassLoader()
+                    .getResource(fileUrl)
+                    .toURI();
+            final Path htmlPath = Path.of(uri);
+            final byte[] read = Files.readAllBytes(htmlPath);
+            final String body = new String(read, StandardCharsets.UTF_8);
+            return body;
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException("존재하지 않은 정적 파일입니다.");
+        }
+    }
+}
---
+++ tomcat/src/main/java/org/apache/coyote/error/ErrorPage.java
@@ -0,0 +1,41 @@
+package org.apache.coyote.error;
+
+import java.util.Arrays;
+import org.apache.coyote.httpResponse.StatusCode;
+
+public enum ErrorPage {
+
+    PAGE_401("/401.html", StatusCode.UNAUTHORIZED),
+    PAGE_404("/404.html", StatusCode.NOT_FOUND),
+    PAGE_405("/405.html", StatusCode.NOT_ALLOW_METHOD),
+    PAGE_500("/500.html",StatusCode.INTERNAL_SERVER_ERROR),
+    ;
+
+    private final String page;
+    private final StatusCode statusCode;
+
+    ErrorPage(final String page, final StatusCode statusCode) {
+        this.page = page;
+        this.statusCode = statusCode;
+    }
+
+    public static String findErrorPage(final StatusCode statusCode) {
+        return Arrays.stream(ErrorPage.values())
+                .filter(errorPage -> errorPage.statusCode.equals(statusCode))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당 에러 코드에 대한 에러 페이지가 없습니다."))
+                .page;
+    }
+
+    public static StatusCode findStatusCode(final String path) {
+        return Arrays.stream(ErrorPage.values())
+                .filter(errorPage -> errorPage.page.equals(path))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당 페이지에 대한 에러 코드가 없습니다."))
+                .statusCode;
+    }
+
+    public String getPage() {
+        return page;
+    }
+}

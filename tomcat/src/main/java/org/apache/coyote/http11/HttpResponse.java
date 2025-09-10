657f8e5e327ce03be8a152132116f977511dcbc3
taek2222 <oot309@daum.net>
2025-09-10 23:56:02 +0900

    study: 에러 바디 내용 static 반환

--- tomcat/src/main/java/org/apache/coyote/http11/HttpResponse.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpResponse.java
@@ -2,10 +2,14 @@ package org.apache.coyote.http11;
 
 import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HttpResponse implements AutoCloseable {
 
+    private static final Logger log = LoggerFactory.getLogger(HttpResponse.class);
     private final BufferedOutputStream outputStream;
 
     public HttpResponse(BufferedOutputStream outputStream) {
@@ -30,7 +34,7 @@ public class HttpResponse implements AutoCloseable {
     }
 
     public void sendError(HttpStatus status) throws IOException {
-        String body = "<h1>" + status.getCode() + " " + status.getMessage() + "</h1>";
+        byte[] body = getErrorBody(status);
         String header = String.format("""
                         HTTP/1.1 %d %s\r
                         Content-Type: text/html; charset=UTF-8\r
@@ -40,11 +44,26 @@ public class HttpResponse implements AutoCloseable {
                         """,
                 status.getCode(),
                 status.getMessage(),
-                body.getBytes().length
+                body.length
         );
         outputStream.write(header.getBytes(StandardCharsets.UTF_8));
-        outputStream.write(body.getBytes());
+        outputStream.write(body);
         outputStream.flush();
+    }
+
+    private byte[] getErrorBody(HttpStatus status) {
+        System.out.println(status.getCode());
+        String errorPage = String.format("static/%d.html", status.getCode());
+        try (InputStream errorPageStream = getClass().getClassLoader().getResourceAsStream(errorPage)) {
+            if (errorPageStream != null) {
+                return errorPageStream.readAllBytes();
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+
+        String defaultBody = "<h1>" + status.getCode() + " " + status.getMessage() + "</h1>";
+        return defaultBody.getBytes(StandardCharsets.UTF_8);
     }
 
     @Override

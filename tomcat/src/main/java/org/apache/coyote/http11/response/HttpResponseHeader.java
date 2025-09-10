6845d9e1cba72498d93d8cc028ead3dc8896a5b1
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-09-10 15:03:18 +0900

    refactor: 헤더에 리다이렉트를 위한 정팩메 추가 및 반환 메세지 수정

--- tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponseHeader.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponseHeader.java
@@ -1,14 +1,30 @@
 package org.apache.coyote.http11.response;
 
-record HttpResponseHeader(MimeType mimeType, int contentLength) {
+record HttpResponseHeader(MimeType mimeType, int contentLength, String location) {
     
     public static HttpResponseHeader of(MimeType mimeType, String body) {
-        return new HttpResponseHeader(mimeType, body.getBytes().length);
+        return new HttpResponseHeader(mimeType, body.getBytes().length, null);
+    }
+
+    public static HttpResponseHeader redirect(String location) {
+        return new HttpResponseHeader(null, 0, location);
     }
 
     public String toHeaderString() {
-        return String.join("\r\n",
-                "Content-Type: " + mimeType.getValue() + " ",
-                "Content-Length: " + contentLength + " ");
+        StringBuilder headers = new StringBuilder();
+        
+        if (mimeType != null) {
+            headers.append("Content-Type: ").append(mimeType.getValue()).append(" \r\n");
+        }
+        
+        if (contentLength > 0) {
+            headers.append("Content-Length: ").append(contentLength).append(" \r\n");
+        }
+        
+        if (location != null) {
+            headers.append("Location: ").append(location).append(" \r\n");
+        }
+        
+        return headers.toString().trim();
     }
 }

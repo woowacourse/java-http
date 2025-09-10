dbf8c9d35b6e9475d87f0a780cf2f8578f12af84
seaniiio <cnc4750@naver.com>
2025-09-10 16:47:39 +0900

    refactor: 요청 uri의 파싱 로직을 Parser 클래스로 분리

--- tomcat/src/main/java/org/apache/coyote/http11/request/startline/RequestUri.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/startline/RequestUri.java
@@ -5,34 +5,14 @@ import java.util.Map;
 
 public class RequestUri {
 
-    private static final String QUERY_STRING_SEPARATOR = "?";
-    private static final String QUERY_STRING_REGEX = "\\?";
-    private static final String QUERY_PARAMETER_SEPARATOR = "&";
-    private static final String QUERY_PARAMETER_KEY_VALUE_SEPARATOR = "=";
-
     private final String requestPath;
     private final Map<String, String> queryParameters;
 
     public static RequestUri from(final String rawRequestUri) {
-        final String[] requestUri = rawRequestUri.split(QUERY_STRING_REGEX);
-        final String requestPath = requestUri[0];
+        final RequestUriParser parser = RequestUriParser.getInstance();
 
-        if (!rawRequestUri.contains(QUERY_STRING_SEPARATOR)) {
-            return new RequestUri(requestPath, new HashMap<>());
-        }
-
-        final String queryString = requestUri[1];
-        final String[] queryParameters = queryString.split(QUERY_PARAMETER_SEPARATOR);
-        final Map<String, String> parameters = new HashMap<>();
-        for (String parameter : queryParameters) {
-            final String[] keyAndValue = parameter.split(QUERY_PARAMETER_KEY_VALUE_SEPARATOR);
-            final String key = keyAndValue[0];
-            String value = "";
-            if (keyAndValue.length == 2) {
-                value = keyAndValue[1];
-            }
-            parameters.put(key, value);
-        }
+        final String requestPath = parser.parsePath(rawRequestUri);
+        final Map<String, String> parameters = parser.parseQueryParameters(rawRequestUri);
 
         return new RequestUri(requestPath, parameters);
     }
---
+++ tomcat/src/main/java/org/apache/coyote/http11/request/startline/RequestUriParser.java
@@ -0,0 +1,46 @@
+package org.apache.coyote.http11.request.startline;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestUriParser {
+
+    private static final String QUERY_STRING_SEPARATOR = "?";
+    private static final String QUERY_STRING_REGEX = "\\?";
+    private static final String QUERY_PARAMETER_SEPARATOR = "&";
+    private static final String QUERY_PARAMETER_KEY_VALUE_SEPARATOR = "=";
+
+    private static final RequestUriParser INSTANCE = new RequestUriParser();
+
+    public static RequestUriParser getInstance() {
+        return INSTANCE;
+    }
+
+    public String parsePath(final String rawRequestUri) {
+        return rawRequestUri.split(QUERY_STRING_REGEX)[0];
+    }
+
+    public Map<String, String> parseQueryParameters(final String rawRequestUri) {
+        if (rawRequestUri.contains(QUERY_STRING_SEPARATOR)) {
+            return new HashMap<>();
+        }
+
+        final String queryString = rawRequestUri.split(QUERY_STRING_REGEX)[1];
+        final String[] queryParameters = queryString.split(QUERY_PARAMETER_SEPARATOR);
+        final Map<String, String> parameters = new HashMap<>();
+        for (String parameter : queryParameters) {
+            final String[] keyAndValue = parameter.split(QUERY_PARAMETER_KEY_VALUE_SEPARATOR);
+            final String key = keyAndValue[0];
+            String value = "";
+            if (keyAndValue.length == 2) {
+                value = keyAndValue[1];
+            }
+            parameters.put(key, value);
+        }
+
+        return parameters;
+    }
+
+    private RequestUriParser() {
+    }
+}

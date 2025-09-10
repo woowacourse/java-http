700d084439eacc98e7c5dd3e64add57a45a78dbb
Jin409 <sualng123@gmail.com>
2025-09-10 23:57:06 +0900

    refactor: parseToMap 사용하도록 수정

--- tomcat/src/main/java/org/apache/catalina/controller/RegisterController.java
+++ tomcat/src/main/java/org/apache/catalina/controller/RegisterController.java
@@ -3,14 +3,13 @@ package org.apache.catalina.controller;
 import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.model.User;
 import jakarta.servlet.http.HttpSession;
-import java.util.HashMap;
 import java.util.Map;
 import org.apache.catalina.cookie.ResponseCookie;
 import org.apache.catalina.session.SessionManager;
+import org.apache.coyote.http11.message.HttpStatus;
 import org.apache.coyote.http11.message.request.HttpRequest;
 import org.apache.coyote.http11.message.response.HttpResponse;
 import org.apache.coyote.http11.message.response.HttpResponseHeader;
-import org.apache.coyote.http11.message.HttpStatus;
 import org.apache.coyote.http11.message.response.StatusLine;
 
 public class RegisterController implements Controller {
@@ -25,9 +24,8 @@ public class RegisterController implements Controller {
 
     @Override
     public void service(HttpRequest request, HttpResponse response) throws Exception {
-        String requestBody = request.getRequestBody();
+        Map<String, String> parsedRequestBody = request.parseToMap();
 
-        Map<String, String> parsedRequestBody = parseRequestBody(requestBody);
         User user = new User(parsedRequestBody.get("account"), parsedRequestBody.get("password"),
                 parsedRequestBody.get("email"));
         InMemoryUserRepository.save(user);
@@ -41,20 +39,6 @@ public class RegisterController implements Controller {
 
         response.setStatusLine(statusLine);
         response.setHttpResponseHeader(httpResponseHeader);
-    }
-
-    private Map<String, String> parseRequestBody(String requestBody) {
-        Map<String, String> parsedRequestBody = new HashMap<>();
-
-        String[] pairs = requestBody.split("&");
-        for (String pair : pairs) {
-            String[] keyValue = pair.split("=");
-            String key = keyValue[0];
-            String value = keyValue[1];
-            parsedRequestBody.put(key, value);
-        }
-
-        return parsedRequestBody;
     }
 
     private ResponseCookie getCookie(HttpRequest httpRequest, User user) {

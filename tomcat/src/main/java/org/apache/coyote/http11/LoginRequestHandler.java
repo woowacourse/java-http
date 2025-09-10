42d4a431ccc394bdb2d42ba98d33415c5ca6c8d5
LeeJaeHoon <dlwogns3413@naver.com>
2025-09-10 12:17:43 +0900

    feat: 로그인 후 login 페이지 이동시 index.html로 리다이렉트 로직 구현

--- tomcat/src/main/java/org/apache/coyote/http11/LoginRequestHandler.java
+++ tomcat/src/main/java/org/apache/coyote/http11/LoginRequestHandler.java
@@ -1,5 +1,7 @@
 package org.apache.coyote.http11;
 
+import org.apache.catalina.Session;
+
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
@@ -10,16 +12,33 @@ public class LoginRequestHandler implements HttpRequestHandler {
     @Override
     public boolean support(final HttpRequest httpRequest) {
         return httpRequest.getRequestMethod() == RequestMethod.GET &&
-                httpRequest.getRequestUrl().startsWith("/login");
+                httpRequest.getRequestUrl()
+                        .startsWith("/login");
     }
 
     @Override
     public String response(final HttpRequest httpRequest) {
-        URL resource = getClass().getClassLoader().getResource("static/login.html");
+        Session session = httpRequest.getSession(false);
+        if (session != null && session.getAttribute("loginUser") != null) {
+            return createRedirectResponse("http://localhost:8080/index.html");
+        }
+
+        URL resource = getClass().getClassLoader()
+                .getResource("static/login.html");
         Path resourcePath = Path.of(resource.getPath());
         byte[] bytes = readAllBytes(resourcePath);
 
         return createSuccessResponse(bytes);
+    }
+
+    private String createRedirectResponse(final String redirectUrl) {
+        return String.join(
+                "\r\n",
+                "HTTP/1.1 302 Found ",
+                "Content-Length: " + 0 + " ",
+                "Location: " + redirectUrl + " ",
+                ""
+        );
     }
 
     private byte[] readAllBytes(final Path resourcePath) {
@@ -31,11 +50,13 @@ public class LoginRequestHandler implements HttpRequestHandler {
     }
 
     private String createSuccessResponse(final byte[] bytes) {
-        return String.join("\r\n",
+        return String.join(
+                "\r\n",
                 "HTTP/1.1 200 OK ",
                 "Content-Type: text/html;charset=utf-8 ",
                 "Content-Length: " + bytes.length + " ",
                 "",
-                new String(bytes));
+                new String(bytes)
+        );
     }
 }

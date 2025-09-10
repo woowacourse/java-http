6e234cfef24684de6fa11630eaa7c3034a69aa35
Chaeyoung714 <202102760@hufs.ac.kr>
2025-09-10 09:39:26 +0900

    refactor: 세션 저장소를 ConcurrentMap으로 관리

--- tomcat/src/main/java/org/apache/catalina/SessionManager.java
+++ tomcat/src/main/java/org/apache/catalina/SessionManager.java
@@ -5,10 +5,11 @@ import org.apache.coyote.http11.Session;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class SessionManager implements Manager{
 
-    private static final Map<String, Session> SESSIONS = new HashMap<>();
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
 
     @Override
     public void add(final Session session) {

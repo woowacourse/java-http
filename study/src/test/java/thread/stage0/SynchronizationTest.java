5c44153551bc080e9018e2ea2db70d2e2e051b28
taek2222 <oot309@daum.net>
2025-09-11 01:56:27 +0900

    study: 학습 테스트

--- study/src/test/java/thread/stage0/SynchronizationTest.java
+++ study/src/test/java/thread/stage0/SynchronizationTest.java
@@ -1,35 +1,32 @@
 package thread.stage0;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.IntStream;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
 
 /**
- * 다중 스레드 환경에서 두 개 이상의 스레드가 변경 가능한(mutable) 공유 데이터를 동시에 업데이트하면 경쟁 조건(race condition)이 발생한다.
- * 자바는 공유 데이터에 대한 스레드 접근을 동기화(synchronization)하여 경쟁 조건을 방지한다.
- * 동기화된 블록은 하나의 스레드만 접근하여 실행할 수 있다.
- *
- * Synchronization
- * https://docs.oracle.com/javase/tutorial/essential/concurrency/sync.html
+ * 다중 스레드 환경에서 두 개 이상의 스레드가 변경 가능한(mutable) 공유 데이터를 동시에 업데이트하면 경쟁 조건(race condition)이 발생한다. 자바는 공유 데이터에 대한 스레드 접근을
+ * 동기화(synchronization)하여 경쟁 조건을 방지한다. 동기화된 블록은 하나의 스레드만 접근하여 실행할 수 있다.
+ * <p>
+ * Synchronization https://docs.oracle.com/javase/tutorial/essential/concurrency/sync.html
  */
 class SynchronizationTest {
 
     /**
-     * 테스트가 성공하도록 SynchronizedMethods 클래스에 동기화를 적용해보자.
-     * synchronized 키워드에 대하여 찾아보고 적용하면 된다.
-     *
-     * Guide to the Synchronized Keyword in Java
-     * https://www.baeldung.com/java-synchronized
+     * 테스트가 성공하도록 SynchronizedMethods 클래스에 동기화를 적용해보자. synchronized 키워드에 대하여 찾아보고 적용하면 된다.
+     * <p>
+     * Guide to the Synchronized Keyword in Java https://www.baeldung.com/java-synchronized
      */
     @Test
     void testSynchronized() throws InterruptedException {
         var executorService = Executors.newFixedThreadPool(3);
         var synchronizedMethods = new SynchronizedMethods();
 
+        // 스레드 풀 안에 있는 스레드 중 하나가 calculate() 메서드를 호출하면서 sum이 1씩 증가한다.
         IntStream.range(0, 1000)
                 .forEach(count -> executorService.submit(synchronizedMethods::calculate));
         executorService.awaitTermination(500, TimeUnit.MILLISECONDS);
@@ -39,10 +36,22 @@ class SynchronizationTest {
 
     private static final class SynchronizedMethods {
 
+        private final ReentrantLock lock = new ReentrantLock();
         private int sum = 0;
 
+        // synchronized 키워드가 붙은 메서드는 동기화된 블록으로 작동한다.
+//        synchronized public void calculate() {
+//            setSum(getSum() + 1);
+//        }
+
+        // lock 사용한 동기화 블록으로 작동한다.
         public void calculate() {
-            setSum(getSum() + 1);
+            lock.lock();
+            try {
+                setSum(getSum() + 1);
+            } finally {
+                lock.unlock();
+            }
         }
 
         public int getSum() {

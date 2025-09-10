9f7aab6cc6836bd7049c2917f97699798609b377
ppparkta <pppppark2@gmail.com>
2025-09-11 02:24:51 +0900

    refactor: HttpStatus enum 분리

---
+++ tomcat/src/main/java/org/apache/coyote/util/response/HttpStatus.java
@@ -0,0 +1,26 @@
+package org.apache.coyote.util.response;
+
+public enum HttpStatus {
+    OK(200, "OK"),
+    FOUND(302, "Found"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+
+    private final int code;
+    private final String message;
+
+    HttpStatus(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

2bb7f67d5dfdedb57afd583d72624e1905379038
YuyoungRhee <bbclara@naver.com>
2025-09-10 19:03:00 +0900

    refactor(SessionManager): ConcurrentHashMap 활용으로 세션 관리 동시성 문제 해결

--- tomcat/src/main/java/org/apache/coyote/http11/SessionManager.java
+++ tomcat/src/main/java/org/apache/coyote/http11/SessionManager.java
@@ -1,13 +1,13 @@
 package org.apache.coyote.http11;
 
 import jakarta.servlet.http.HttpSession;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import org.apache.catalina.Manager;
 
 public class SessionManager implements Manager {
 
-    private static final Map<String, HttpSession> SESSIONS = new HashMap<>();
+    private static final Map<String, HttpSession> SESSIONS = new ConcurrentHashMap<>();
 
     @Override
     public void add(HttpSession session) {

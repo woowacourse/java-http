c6003bc0b85d7ecbc1bd914eb14ffb20f2b840a7
geonwoo kim <115832836+kysub99@users.noreply.github.com>
2025-09-10 20:18:55 +0900

    [2단계 - 로그인 구현하기] 조로(김건우) 미션 제출합니다 (#919)
    * feat: 로그인 시 리다이렉트
    
    * refactor: 구조 리팩터링
    
    * feat: 회원 가입 구현
    
    * feat: JSESSIONID 구현
    
    * refactor: HttpSession 클래스 분리
    
    * feat: Session, SessionManager 구현
    
    * refactor: final 사용
    
    * fix: 불필요한 세션 생성 제거
    
    * fix: ok resposne에 대해서 세션ID 전달 제거
    
    * fix: 디코딩 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -9,12 +9,14 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +26,7 @@ public class Http11Processor implements Runnable, Processor {
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
 
     private final Socket connection;
+    private final SessionManager sessionManager = SessionManager.getInstance();
 
     public Http11Processor(final Socket connection) {
         this.connection = connection;
@@ -51,7 +54,26 @@ public class Http11Processor implements Runnable, Processor {
                 return;
             }
 
-            final String response = handleRequest(requestInfo);
+            final Map<String, String> requestHeaders = new HashMap<>();
+            String line;
+            while ((line = reader.readLine()) != null && !line.isEmpty()) {
+                final String[] headerParts = line.split(": ", 2);
+                if (headerParts.length == 2) {
+                    String key = URLDecoder.decode(headerParts[0], StandardCharsets.UTF_8);
+                    String value = URLDecoder.decode(headerParts[1], StandardCharsets.UTF_8);
+                    requestHeaders.put(key, value);
+                }
+            }
+
+            String requestBody = "";
+            if ("POST".equals(requestInfo[0]) && requestHeaders.containsKey("Content-Length")) {
+                int contentLength = Integer.parseInt(requestHeaders.get("Content-Length"));
+                char[] buffer = new char[contentLength];
+                reader.read(buffer, 0, contentLength);
+                requestBody = new String(buffer);
+            }
+
+            final String response = handleRequest(requestInfo, requestHeaders, requestBody);
 
             outputStream.write(response.getBytes(StandardCharsets.UTF_8));
             outputStream.flush();
@@ -66,49 +88,79 @@ public class Http11Processor implements Runnable, Processor {
             return null;
         }
 
+        final String method = requestLineArray[0];
         final String uri = requestLineArray[1];
-        final int index = uri.indexOf("?");
-        final String path = (index != -1) ? uri.substring(0, index) : uri;
-        final String queryString = (index != -1) ? uri.substring(index + 1) : "";
 
-        return new String[]{path, queryString};
+        return new String[]{method, uri};
     }
 
-    private String handleRequest(final String[] requestInfo) throws IOException, URISyntaxException {
-        String path = requestInfo[0];
-        final String queryString = requestInfo[1];
+    private String handleRequest(final String[] requestInfo, final Map<String, String> requestHeaders,
+                                 final String requestBody) throws IOException, URISyntaxException {
+        final String method = requestInfo[0];
+        final String path = requestInfo[1];
+
+        final HttpCookie cookie = new HttpCookie(requestHeaders.get("Cookie"));
+        final String existingJSessionId = cookie.getJSessionId();
+
+        final Session session = (existingJSessionId != null) ? sessionManager.findSession(existingJSessionId) : null;
+        final User user = (session != null) ? (User) session.getAttribute("user") : null;
 
         if ("/login".equals(path)) {
-            handleLogin(queryString);
-            path += ".html";
-        }
-
-        final byte[] bytes = readFile(path);
-        return generateResponse(path, bytes);
-    }
-
-    private void handleLogin(final String queryString) {
-        final Map<String, String> parameters = parseQueryString(queryString);
-        if (login(parameters)) {
-            log.info("login successful");
-            return;
-        }
-        log.info("login failed");
-    }
-
-    private Map<String, String> parseQueryString(final String queryString) {
-        final Map<String, String> parameters = new HashMap<>();
-        if (queryString != null && !queryString.isEmpty()) {
-            final String[] pairs = queryString.split("&");
-            for (final String pair : pairs) {
-                final String[] keyValue = pair.split("=");
-                parameters.put(keyValue[0], keyValue[1]);
+            if (user != null && "GET".equals(method)) {
+                return generateRedirectResponse("/index.html", existingJSessionId);
             }
+            return handleLogin(method, requestBody);
         }
-        return parameters;
+        if ("/register".equals(path)) {
+            return handleRegister(method, requestBody);
+        }
+
+        return serveStaticFile(path, existingJSessionId);
     }
 
-    private boolean login(final Map<String, String> parameters) {
+    private String handleLogin(final String method, final String requestBody) throws IOException, URISyntaxException {
+        if ("POST".equals(method)) {
+            return processLogin(requestBody);
+        }
+        return serveStaticFile("/login.html", null);
+    }
+
+    private String handleRegister(final String method, final String requestBody)
+            throws IOException, URISyntaxException {
+        if ("POST".equals(method)) {
+            return processRegister(requestBody);
+        }
+        return serveStaticFile("/register.html", null);
+    }
+
+    private String processLogin(final String requestBody) {
+        final Map<String, String> parameters = parseFormData(requestBody);
+        final boolean loginSuccess = authenticateUser(parameters);
+
+        if (loginSuccess) {
+            final String jSessionId = UUID.randomUUID().toString();
+            final User user = InMemoryUserRepository.findByAccount(parameters.get("account")).get();
+
+            Session newSession = new Session(jSessionId);
+            newSession.setAttribute("user", user);
+            sessionManager.add(newSession);
+
+            return generateRedirectResponse("/index.html", jSessionId);
+        }
+
+        return generateRedirectResponse("/401.html", null);
+    }
+
+    private String processRegister(final String requestBody) {
+        final Map<String, String> parameters = parseFormData(requestBody);
+        final boolean registerSuccess = registerUser(parameters);
+
+        final String redirectLocation = registerSuccess ? "/index.html" : "/register.html";
+
+        return generateRedirectResponse(redirectLocation, null);
+    }
+
+    private boolean authenticateUser(final Map<String, String> parameters) {
         final String account = parameters.get("account");
         final String password = parameters.get("password");
 
@@ -118,7 +170,6 @@ public class Http11Processor implements Runnable, Processor {
 
         final User user = InMemoryUserRepository.findByAccount(account)
                 .orElse(null);
-
         if (user == null) {
             return false;
         }
@@ -130,27 +181,92 @@ public class Http11Processor implements Runnable, Processor {
         return false;
     }
 
+    private boolean registerUser(final Map<String, String> parameters) {
+        final String account = parameters.get("account");
+        final String password = parameters.get("password");
+        final String email = parameters.get("email");
+
+        if (account == null || password == null || email == null) {
+            return false;
+        }
+
+        if (InMemoryUserRepository.findByAccount(account).isPresent()) {
+            return false;
+        }
+        try {
+            final User newUser = new User(account, password, email);
+            InMemoryUserRepository.save(newUser);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private String serveStaticFile(final String path, final String existingJSessionId)
+            throws IOException, URISyntaxException {
+        final byte[] fileBytes = readFile(path);
+
+        return generateOkResponse(path, fileBytes, existingJSessionId);
+    }
+
+    private Map<String, String> parseFormData(final String formData) {
+        final Map<String, String> parameters = new HashMap<>();
+        if (formData != null && !formData.isEmpty()) {
+            final String[] pairs = formData.split("&");
+            for (final String pair : pairs) {
+                final String[] keyValue = pair.split("=");
+                if (keyValue.length >= 2) {
+                    String key = URLDecoder.decode(keyValue[0], StandardCharsets.UTF_8);
+                    String value = URLDecoder.decode(keyValue[1], StandardCharsets.UTF_8);
+                    parameters.put(key, value);
+                }
+            }
+        }
+        return parameters;
+    }
+
+    private String generateRedirectResponse(final String location, final String jSessionId) {
+        StringBuilder response = new StringBuilder();
+        response.append("HTTP/1.1 302 Found\r\n");
+        response.append("Location: ").append(location).append("\r\n");
+        response.append("Content-Type: text/html; charset=UTF-8\r\n");
+        response.append("Content-Length: 0\r\n");
+
+        if (jSessionId != null) {
+            response.append("Set-Cookie: JSESSIONID=").append(jSessionId).append("\r\n");
+        }
+
+        response.append("\r\n");
+        return response.toString();
+    }
+
     private byte[] readFile(final String path) throws IOException, URISyntaxException {
         final URL resource = getClass().getClassLoader().getResource("static" + path);
         final Path filePath = Paths.get(resource.toURI());
         return Files.readAllBytes(filePath);
     }
 
-    private String generateResponse(final String path, final byte[] bytes) {
+    private String generateOkResponse(final String path, final byte[] bytes, final String jSessionId) {
         final String responseBody = new String(bytes);
         final String contentType = getContentType(path);
 
-        return String.join("\r\n",
-                "HTTP/1.1 200 OK ",
-                "Content-Type: " + contentType + ";charset=utf-8 ",
-                "Content-Length: " + bytes.length + " ",
-                "",
-                responseBody);
+        StringBuilder response = new StringBuilder();
+        response.append("HTTP/1.1 200 OK\r\n");
+        response.append("Content-Type: ").append(contentType).append(";charset=utf-8\r\n");
+        response.append("Content-Length: ").append(bytes.length).append("\r\n");
+
+        response.append("\r\n");
+        response.append(responseBody);
+
+        return response.toString();
     }
 
     private String getContentType(final String path) {
         if (path.endsWith(".css")) {
             return "text/css";
+        }
+        if (path.endsWith(".js")) {
+            return "application/javascript";
         }
         return "text/html";
     }
---
+++ tomcat/src/main/java/org/apache/coyote/http11/SessionManager.java
@@ -0,0 +1,33 @@
+package org.apache.coyote.http11;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.apache.catalina.Manager;
+
+public class SessionManager implements Manager {
+
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
+    private static final SessionManager instance = new SessionManager();
+
+    private SessionManager() {
+    }
+
+    @Override
+    public void add(final Session session) {
+        SESSIONS.put(session.getId(), session);
+    }
+
+    @Override
+    public Session findSession(final String id) {
+        return SESSIONS.get(id);
+    }
+
+    @Override
+    public void remove(final String id) {
+        SESSIONS.remove(id);
+    }
+
+    public static SessionManager getInstance() {
+        return instance;
+    }
+}

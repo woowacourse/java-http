19981dbaff2c84f7d962ee21eaf273ed3f59a994
byesol <bsj5967@naver.com>
2025-09-11 01:52:35 +0900

    refactor: 정적 컨트롤러 분리

---
+++ tomcat/src/main/java/com/techcourse/controller/StaticResourceController.java
@@ -0,0 +1,41 @@
+package com.techcourse.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.coyote.http11.ContentType;
+import org.apache.coyote.http11.HttpRequest;
+import org.apache.coyote.http11.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StaticResourceController implements Controller {
+    private static final Logger log = LoggerFactory.getLogger(StaticResourceController.class);
+
+    @Override
+    public void service(HttpRequest request, HttpResponse response) {
+        String path = request.getPath();
+        try {
+            loadStaticResource(response, path);
+        } catch (IOException e) {
+            log.error("Error while loading static resource", e);
+            response.sendInternalServerError();
+        }
+    }
+
+    private void loadStaticResource(HttpResponse response, String path) throws IOException {
+        String resourcePath = "static" + path;
+        try (InputStream fileInputStream = getClass().getClassLoader().getResourceAsStream(resourcePath)) {
+            if (fileInputStream == null) {
+                response.sendNotFound();
+                return;
+            }
+            String contentType = determineContentType(path);
+            byte[] bodyBytes = fileInputStream.readAllBytes();
+            response.sendOk(contentType, bodyBytes);
+        }
+    }
+
+    private String determineContentType(String path) {
+        return ContentType.fromPath(path);
+    }
+}

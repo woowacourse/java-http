3cb36764e2ba7ce245037e96da6f60c09f56aa6a
wogus <ind07152@naver.com>
2025-09-10 15:50:10 +0900

    feat: 요청 처리 객체 생성

---
+++ tomcat/src/main/java/org/apache/coyote/controller/RequestHandler.java
@@ -0,0 +1,34 @@
+package org.apache.coyote.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.coyote.controller.dynamic.HomeController;
+import org.apache.coyote.controller.dynamic.LoginController;
+import org.apache.coyote.controller.dynamic.RegisterController;
+import org.apache.coyote.controller.resource.ResourceType;
+import org.apache.coyote.controller.resource.StaticController;
+
+public class RequestHandler {
+
+    private static Controller staticController;
+    private static Map<String, Controller> dynamicControllers;
+
+    static {
+        staticController = new StaticController();
+        dynamicControllers = new HashMap<>();
+        dynamicControllers.put("/", new HomeController());
+        dynamicControllers.put("/login", new LoginController());
+        dynamicControllers.put("/register", new RegisterController());
+    }
+
+    public static Controller getController(final String path) {
+        Controller findController = dynamicControllers.getOrDefault(path, null);
+        if (ResourceType.isStaticRequest(path)) {
+            findController = staticController;
+        }
+        if (findController != null) {
+            return findController;
+        }
+        throw new IllegalArgumentException("지원하지 않은 경로입니다.");
+    }
+}

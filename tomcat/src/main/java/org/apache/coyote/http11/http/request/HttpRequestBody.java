2b73f33615058896fe6d23fb794984bff5aa491a
jhan0121 <jhan0121@gmail.com>
2025-09-10 20:15:08 +0900

    refactor: HttpRequest 생성 로직 수정
    - HttpRequest에 bufferedReader 라인 읽기 책임 위임
    - HttpRequestBody에 bufferedReader 라인 읽기 책임 제거

--- tomcat/src/main/java/org/apache/coyote/http11/http/request/HttpRequestBody.java
+++ tomcat/src/main/java/org/apache/coyote/http11/http/request/HttpRequestBody.java
@@ -1,14 +1,9 @@
 package org.apache.coyote.http11.http.request;
 
-import http.HttpHeaderKey;
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
-import org.apache.coyote.http11.http.common.header.HttpHeader;
 
 public class HttpRequestBody {
 
@@ -18,37 +13,8 @@ public class HttpRequestBody {
         this.value = value;
     }
 
-    public static HttpRequestBody of(final BufferedReader bufferedReader, final HttpHeader httpHeader)
-            throws IOException {
-        validateNotNull(bufferedReader, httpHeader);
-
-        Optional<String> contentLengthOpt = httpHeader.getFirstValue(
-                HttpHeaderKey.CONTENT_LENGTH.getValue().toLowerCase());
-
-        byte[] body = new byte[0];
-
-        if (contentLengthOpt.isPresent()) {
-            final int contentLength = Integer.parseInt(contentLengthOpt.get());
-            if (contentLength > 0) {
-                char[] bodyReads = new char[contentLength];
-
-                int readLength = bufferedReader.read(bodyReads, 0, contentLength);
-
-                if (readLength > 0) {
-                    body = new String(bodyReads, 0, readLength).getBytes(StandardCharsets.UTF_8);
-                }
-            }
-        }
-        return new HttpRequestBody(body);
-    }
-
-    private static void validateNotNull(final BufferedReader bufferedReader, final HttpHeader httpHeader) {
-        if (bufferedReader == null) {
-            throw new IllegalArgumentException("BufferedReader는 null일 수 없습니다");
-        }
-        if (httpHeader == null) {
-            throw new IllegalArgumentException("HttpHeader는 null일 수 없습니다");
-        }
+    public static HttpRequestBody from(final byte[] bytes) {
+        return new HttpRequestBody(bytes);
     }
 
     public Map<String, String> getBodyElement() {

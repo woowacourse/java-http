152f2558244a8ce9aece3b57cdbe16c874b61e1d
ryan-dia <cjfdnjs158@naver.com>
2025-09-10 11:14:56 +0900

    refactor: 메서드 시그니처 변경

--- tomcat/src/main/java/org/apache/coyote/http11/handler/Handler.java
+++ tomcat/src/main/java/org/apache/coyote/http11/handler/Handler.java
@@ -1,12 +1,12 @@
 package org.apache.coyote.http11.handler;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import org.apache.coyote.http11.dto.HttpRequest;
+import org.apache.coyote.http11.request.dto.HttpRequest;
+import org.apache.coyote.http11.response.HttpResponse;
 
 public interface Handler {
 
     boolean canHandle(HttpRequest request);
 
-    void handle(HttpRequest request, OutputStream outputStream) throws IOException;
+    void handle(HttpRequest request, HttpResponse response) throws IOException;
 }

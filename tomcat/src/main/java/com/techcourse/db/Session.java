3f5f833c22f9314061b15d4979426954cadb634f
2jin2.1031 <2jin2.1031@gmail.com>
2025-09-10 20:18:25 +0900

    fix(Session): 잘못된 키 참조 수정

--- tomcat/src/main/java/com/techcourse/db/Session.java
+++ tomcat/src/main/java/com/techcourse/db/Session.java
@@ -17,11 +17,11 @@ public class Session {
     }
 
     public Object getAttribute(final String name) {
-        return values.get("name");
+        return values.get(name);
     }
 
     public void setAttribute(final String name, final Object value) {
-        values.put("name", value);
+        values.put(name, value);
     }
 
     public void removeAttribute(final String name) {

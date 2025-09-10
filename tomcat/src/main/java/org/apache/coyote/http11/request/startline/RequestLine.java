f091e41dfdc23b07c6f19b7808a834f116f0d962
seaniiio <cnc4750@naver.com>
2025-09-10 17:02:25 +0900

    refactor: 요청 startLine에 대한 파싱 로직을 Parser 클래스로 분리

--- tomcat/src/main/java/org/apache/coyote/http11/request/startline/RequestLine.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/startline/RequestLine.java
@@ -2,17 +2,16 @@ package org.apache.coyote.http11.request.startline;
 
 public class RequestLine {
 
-    private static final String REQUEST_HEADER_SEPARATOR = " ";
-
     private final HttpMethod httpMethod;
     private final RequestUri requestUri;
     private final String httpVersion;
 
     public static RequestLine from(final String rawRequestLine) {
-        final String[] requestLineElements = rawRequestLine.split(REQUEST_HEADER_SEPARATOR);
-        final HttpMethod httpMethod = HttpMethod.from(requestLineElements[0]);
-        final RequestUri requestUri = RequestUri.from(requestLineElements[1]);
-        final String httpVersion = requestLineElements[2];
+        final RequestLineParser parser = RequestLineParser.getInstance();
+
+        final HttpMethod httpMethod = parser.parseMethod(rawRequestLine);
+        final RequestUri requestUri = parser.parseUri(rawRequestLine);
+        final String httpVersion = parser.parseVersion(rawRequestLine);
 
         return new RequestLine(httpMethod, requestUri, httpVersion);
     }
@@ -29,13 +28,13 @@ public class RequestLine {
         return this.requestUri.getRequestPath();
     }
 
+    public boolean isMethodEqualsTo(final HttpMethod httpMethod) {
+        return this.httpMethod == httpMethod;
+    }
+
     private RequestLine(final HttpMethod httpMethod, final RequestUri requestUri, final String httpVersion) {
         this.httpMethod = httpMethod;
         this.requestUri = requestUri;
         this.httpVersion = httpVersion;
-    }
-
-    public boolean isMethodEqualsTo(final HttpMethod httpMethod) {
-        return this.httpMethod == httpMethod;
     }
 }

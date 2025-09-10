cc9e92768f4cdfc9274b012bd2af3900598942da
ppparkta <pppppark2@gmail.com>
2025-09-11 02:28:02 +0900

    refactor: 변경되지 않은 컨트롤러 부분 수정

--- tomcat/src/main/java/org/apache/catalina/mapping/RequestMapping.java
+++ tomcat/src/main/java/org/apache/catalina/mapping/RequestMapping.java
@@ -2,16 +2,16 @@ package org.apache.catalina.mapping;
 
 import java.util.HashMap;
 import java.util.Map;
-import org.apache.catalina.core.HomeController;
 import org.apache.catalina.core.LoginController;
 import org.apache.catalina.core.RegisterController;
+import org.apache.catalina.core.RootController;
 
 public class RequestMapping {
 
     private static final Map<String, Controller> controllers = new HashMap<>();
 
     static {
-        controllers.put("/", new HomeController());
+        controllers.put("/", new RootController());
         controllers.put("/login", new LoginController());
         controllers.put("/register", new RegisterController());
     }

94794fc9a58d0197e13069382e8af45fb7dfd8e5
byesol <bsj5967@naver.com>
2025-09-11 02:19:39 +0900

    refactor: 프론트 컨트롤러 분리

---
+++ tomcat/src/main/java/com/techcourse/controller/FrontController.java
@@ -0,0 +1,33 @@
+package com.techcourse.controller;
+
+import org.apache.coyote.http11.HttpRequest;
+import org.apache.coyote.http11.HttpResponse;
+
+public class FrontController {
+
+    private final RequestMapping requestMapping;
+    private final AbstractController staticResourceController;
+
+    public FrontController() {
+        this.requestMapping = new RequestMapping();
+        this.staticResourceController = new StaticResourceController();
+        initializeControllers();
+    }
+
+    private void initializeControllers() {
+        requestMapping.addController("/", new HelloController());
+        requestMapping.addController("/login", new LoginController());
+        requestMapping.addController("/register", new RegisterController());
+    }
+
+    public void service(HttpRequest request, HttpResponse response) throws Exception {
+        String path = request.getPath();
+        Controller controller = requestMapping.getController(path);
+        if (controller == null) {
+            staticResourceController.service(request, response);
+            return;
+        }
+        controller.service(request, response);
+    }
+}
+

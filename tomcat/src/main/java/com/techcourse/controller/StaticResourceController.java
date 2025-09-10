170a7a0a992e5624d2e74c6c8acdd05f663f6eba
jhan0121 <jhan0121@gmail.com>
2025-09-10 17:22:26 +0900

    feat: StaticResourceController 추가

---
+++ tomcat/src/main/java/com/techcourse/controller/StaticResourceController.java
@@ -0,0 +1,49 @@
+package com.techcourse.controller;
+
+import java.util.Set;
+import org.apache.coyote.http11.controller.AbstractController;
+import org.apache.coyote.http11.http.request.HttpRequest;
+import org.apache.coyote.http11.http.response.HttpResponse;
+
+public class StaticResourceController extends AbstractController {
+
+    private static final Set<String> SUPPORTED_EXTENSIONS = Set.of(
+            ".html", ".css", ".js", ".ico"
+    );
+
+    @Override
+    public boolean isProvide(final String path) {
+        if ("/".equals(path)) {
+            return true;
+        }
+
+        int lastDotIndex = path.lastIndexOf('.');
+        if (lastDotIndex == -1) {
+            return false;
+        }
+        String extension = path.substring(lastDotIndex);
+        return SUPPORTED_EXTENSIONS.contains(extension);
+    }
+
+    @Override
+    protected void doGet(final HttpRequest request, final HttpResponse response) {
+        final String path = request.getPath();
+
+        if (path.contains("..")) {
+            response.setNotFound();
+            return;
+        }
+        if ("/".equals(path)) {
+            response.setOk("Hello world!");
+            return;
+        }
+
+        final String resourcePath = path.substring(1);
+        response.setOk(resourcePath);
+    }
+
+    @Override
+    protected void doPost(final HttpRequest request, final HttpResponse response) {
+        response.setInternalServerError();
+    }
+}

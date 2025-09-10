2f4fdb0f601c473a7734d79884dd7ed588479792
MinJun Choi <mj043000@naver.com>
2025-09-10 13:58:16 +0900

    refactor: SessionManager의 HashMap을 ConcurrentHashMap으로 변경
    - 동시성 안전성을 위해 SESSIONS 필드 자료 구조를 HashMap에서 ConcurrentHashMap으로 변경

--- tomcat/src/main/java/org/apache/catalina/SessionManager.java
+++ tomcat/src/main/java/org/apache/catalina/SessionManager.java
@@ -1,12 +1,12 @@
 package org.apache.catalina;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public final class SessionManager implements Manager {
 
-    private static final Map<String, Session> SESSIONS = new HashMap<>();
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
     private static final SessionManager INSTANCE = new SessionManager();
 
     private SessionManager() {

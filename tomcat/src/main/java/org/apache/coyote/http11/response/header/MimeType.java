450819cf09f9c34154bbbf7dfe64e204ac399d52
seaniiio <cnc4750@naver.com>
2025-09-10 20:48:56 +0900

    feat: MimeType을 표현하는 enum 생성

---
+++ tomcat/src/main/java/org/apache/coyote/http11/response/header/MimeType.java
@@ -0,0 +1,31 @@
+package org.apache.coyote.http11.response.header;
+
+import java.util.Arrays;
+import org.apache.coyote.http11.exception.HttpStatusException;
+import org.apache.coyote.http11.response.startline.HttpStatusCode;
+
+public enum MimeType {
+
+    HTML("html", "text/html;charset=utf-8"),
+    CSS("css", "text/css;charset=utf-8"),
+    JS("js", "text/javascript;charset=utf-8");
+
+    private final String extension;
+    private final String mimeType;
+
+    MimeType(final String extension, final String mimeType) {
+        this.extension = extension;
+        this.mimeType = mimeType;
+    }
+
+    public static MimeType of(final String givenExtension) {
+        return Arrays.stream(MimeType.values())
+                .filter(type -> type.extension.equals(givenExtension))
+                .findFirst()
+                .orElseThrow(() -> new HttpStatusException(HttpStatusCode.INTERNAL_SERVER_ERROR));
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+}

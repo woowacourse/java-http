8024994922f1ffe472c290755ad5b88360ff0360
byesol <bsj5967@naver.com>
2025-09-11 02:20:05 +0900

    refactor: 프로세서 리팩토링

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,13 +1,9 @@
 package org.apache.coyote.http11;
 
-import com.techcourse.db.InMemoryUserRepository;
+import com.techcourse.controller.FrontController;
 import com.techcourse.exception.BadRequestException;
-import com.techcourse.model.User;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.util.Optional;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,10 +13,12 @@ public class Http11Processor implements Runnable, Processor {
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
     private final Socket connection;
     private final SessionHandler sessionHandler;
+    private final FrontController frontController;
 
     public Http11Processor(final Socket connection) {
         this.connection = connection;
         this.sessionHandler = new SessionHandler();
+        this.frontController = new FrontController();
     }
 
     @Override
@@ -36,7 +34,9 @@ public class Http11Processor implements Runnable, Processor {
             HttpResponse response = new HttpResponse(outputStream);
             try {
                 HttpRequest request = new HttpRequest(inputStream);
-                dispatchRequest(request, response);
+                Session session = sessionHandler.getSession(request, response);
+                request.setSession(session);
+                frontController.service(request, response);
             } catch (BadRequestException e) {
                 response.sendBadRequest();
             } catch (Exception e) {
@@ -45,85 +45,5 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException e) {
             log.error(e.getMessage(), e);
         }
-    }
-
-    private void dispatchRequest(HttpRequest request, HttpResponse response) throws IOException {
-        String path = request.getPath();
-        Session session = sessionHandler.getSession(request, response);
-        if ("/".equals(path)) {
-            handleRoot(response);
-        } else if ("/login".equals(path)) {
-            handleLogin(request, response, session);
-        } else if ("/register".equals(path)) {
-            handleRegister(request, response);
-        } else {
-            handleStaticResource(response, path);
-        }
-    }
-
-    private void handleRegister(HttpRequest request, HttpResponse response) throws IOException {
-        if ("POST".equalsIgnoreCase(request.getMethod())) {
-            String account = request.getQueryParam("account");
-            String password = request.getQueryParam("password");
-            String email = request.getQueryParam("email");
-
-            if (account != null && password != null && email != null) {
-                User user = new User(account, password, email);
-                InMemoryUserRepository.save(user);
-                log.info("회원가입 완료: {}", user);
-                response.sendRedirect("/index.html");
-                return;
-            }
-        }
-        loadStaticResource(response, "/register.html");
-    }
-
-    private void handleRoot(HttpResponse response) throws IOException {
-        final var responseBody = "Hello world!";
-        response.sendOk("text/html;charset=utf-8", responseBody.getBytes(StandardCharsets.UTF_8));
-    }
-
-    private void handleLogin(HttpRequest request, HttpResponse response, Session session) throws IOException {
-        if ("GET".equals(request.getMethod()) && session.getAttribute("user") != null) {
-            response.sendRedirect("/index.html");
-            return;
-        }
-        if ("POST".equals(request.getMethod())) {
-            String account = request.getQueryParam("account");
-            String password = request.getQueryParam("password");
-            Optional<User> userOptional = InMemoryUserRepository.findByAccount(account);
-            if (userOptional.isEmpty() || !userOptional.get().checkPassword(password)) {
-                log.info("로그인 실패: 아이디 또는 비밀번호 불일치");
-                response.sendRedirect("/401.html");
-                return;
-            }
-            User user = userOptional.get();
-            log.info("로그인 성공: {}", user);
-            session.setAttribute("user", user);
-            response.sendRedirect("/index.html");
-            return;
-        }
-        loadStaticResource(response, "/login.html");
-    }
-
-    private void handleStaticResource(HttpResponse response, String path) throws IOException {
-        loadStaticResource(response, path);
-    }
-
-    private void loadStaticResource(HttpResponse response, String path) throws IOException {
-        String resourcePath = "static" + path;
-        try (InputStream fileInputStream = getClass().getClassLoader().getResourceAsStream(resourcePath)) {
-            if (fileInputStream == null) {
-                response.sendNotFound();
-                return;
-            }
-            String contentType = determineContentType(path);
-            byte[] bodyBytes = fileInputStream.readAllBytes();
-            response.sendOk(contentType, bodyBytes);
-        }
-    }
-
-    private String determineContentType(String path) {
-        return ContentType.fromPath(path);
     }
 }

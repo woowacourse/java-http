ee3336fe347118cb6c2e5dd974e2199f2a4f52b4
Yeongjae Kim <sees77770@gmail.com>
2025-09-10 19:47:52 +0900

    test: 테스트 수정

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -1,15 +1,10 @@
 package org.apache.coyote.http11;
 
-import org.junit.jupiter.api.Test;
-import org.apache.catalina.session.SessionManager;
-import support.StubSocket;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.catalina.session.SessionManager;
+import org.junit.jupiter.api.Test;
+import support.StubSocket;
 
 class Http11ProcessorTest {
 
@@ -23,20 +18,17 @@ class Http11ProcessorTest {
         processor.process(socket);
 
         // then
-        var expected = String.join("\r\n",
-                "HTTP/1.1 200 OK ",
-                "Content-Type: text/html;charset=utf-8 ",
-                "Content-Length: 12 ",
-                "",
-                "Hello world!");
-
-        assertThat(socket.output()).isEqualTo(expected);
+        String output = socket.output();
+        assertThat(output).contains("HTTP/1.1 200 OK");
+        assertThat(output).contains("Content-Type: text/html;charset=utf-8");
+        assertThat(output).contains("Set-Cookie: JSESSIONID=");
+        assertThat(output).contains("Hello world!");
     }
 
     @Test
-    void index() throws IOException {
+    void index() {
         // given
-        final String httpRequest= String.join("\r\n",
+        final String httpRequest = String.join("\r\n",
                 "GET /index.html HTTP/1.1 ",
                 "Host: localhost:8080 ",
                 "Connection: keep-alive ",
@@ -50,13 +42,10 @@ class Http11ProcessorTest {
         processor.process(socket);
 
         // then
-        final URL resource = getClass().getClassLoader().getResource("static/index.html");
-        var expected = "HTTP/1.1 200 OK \r\n" +
-                "Content-Type: text/html;charset=utf-8 \r\n" +
-                "Content-Length: 5564 \r\n" +
-                "\r\n"+
-                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
-
-        assertThat(socket.output()).isEqualTo(expected);
+        String output = socket.output();
+        assertThat(output).contains("HTTP/1.1 200 OK");
+        assertThat(output).contains("Content-Type: text/html;charset=utf-8");
+        assertThat(output).contains("Content-Length: 5564");
+        assertThat(output).contains("<title>대시보드</title>");
     }
 }

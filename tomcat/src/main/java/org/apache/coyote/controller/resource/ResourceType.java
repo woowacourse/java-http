2451f01177873139bc8e49fb0b14a1d68068a1a1
wogus <ind07152@naver.com>
2025-09-10 15:44:56 +0900

    feat: 응답 타입 enum 객체 생성

---
+++ tomcat/src/main/java/org/apache/coyote/controller/resource/ResourceType.java
@@ -0,0 +1,35 @@
+package org.apache.coyote.controller.resource;
+
+import java.util.Arrays;
+
+public enum ResourceType {
+
+    HTML(".html", "text/html;charset=utf-8"),
+    CSS(".css", "text/css;charset=utf-8"),
+    JS(".js", "application/javascript;charset=utf-8"),
+    SVG(".svg","image/svg+xml");
+
+    private final String extension;
+    private final String type;
+
+    ResourceType(final String extension, final String type) {
+        this.extension = extension;
+        this.type = type;
+    }
+
+    public static ResourceType findResourceType(final String path) {
+        return Arrays.stream(ResourceType.values())
+                .filter(resourceType -> path.endsWith(resourceType.extension))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("지원하지 않은 형식입니다."));
+    }
+
+    public static boolean isStaticRequest(final String path) {
+        return Arrays.stream(ResourceType.values())
+                .anyMatch(resourceType -> path.endsWith(resourceType.extension));
+    }
+
+    public String getType() {
+        return type;
+    }
+}

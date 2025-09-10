b82af0565717e38e1d83dc53b761d0c266b4c419
CheChe903 <cheche903@naver.com>
2025-09-10 23:01:14 +0900

    feat: 응답 쿠키 저장 방식 변경

--- tomcat/src/main/java/org/apache/coyote/http11/HttpResponse.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpResponse.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import org.apache.coyote.http11.session.HttpCookie;
 
 public class HttpResponse {
 

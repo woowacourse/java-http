39518cc4159e840f611b6ed46b50d49ba003b6c2
jeyong <joon6093@naver.com>
2025-09-11 15:24:57 +0900

    test: Resource not found가 아닌 페이지를 반환을 검증

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -22,7 +22,7 @@ class Http11ProcessorTest {
         processor.process(socket);
 
         // then
-        assertThat(socket.output()).contains("Resource not found");
+        assertThat(socket.output()).contains("Location: /404.html");
     }
 
     @Test

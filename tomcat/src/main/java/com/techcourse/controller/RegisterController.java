4c05f8cef9323f1fe43f4ebc13c11b6414ded599
2jin2.1031 <2jin2.1031@gmail.com>
2025-09-12 05:28:55 +0900

    feat(registerController): 회원가입 전용 RegisterController 분리

---
+++ tomcat/src/main/java/com/techcourse/controller/RegisterController.java
@@ -0,0 +1,29 @@
+package com.techcourse.controller;
+
+import com.techcourse.ResponseWriters;
+import com.techcourse.service.Service;
+import java.io.IOException;
+import java.util.Map;
+import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.response.HttpResponse;
+
+public class RegisterController extends AbstractController {
+
+    private final Service service;
+
+    public RegisterController(Service service) {
+        this.service = service;
+    }
+
+    @Override
+    public void doGet(HttpRequest request, HttpResponse response) throws IOException {
+        ResponseWriters.ok(response, "register.html");
+    }
+
+    @Override
+    public void doPost(HttpRequest request, HttpResponse response) {
+        Map<String, String> signInRequest = request.getBodyMap();
+        service.create(signInRequest);
+        ResponseWriters.found(response, "/index.html");
+    }
+}
--- tomcat/src/main/java/org/apache/coyote/http11/dispatcher/HandlerMapping.java
+++ tomcat/src/main/java/org/apache/coyote/http11/dispatcher/HandlerMapping.java
@@ -2,6 +2,7 @@ package org.apache.coyote.http11.dispatcher;
 
 import com.techcourse.controller.HelloController;
 import com.techcourse.controller.LoginController;
+import com.techcourse.controller.RegisterController;
 import com.techcourse.db.SessionManager;
 import com.techcourse.service.Service;
 import java.util.Map;
@@ -17,8 +18,10 @@ public class HandlerMapping {
     }
 
     public static void init() {
+        Service service = new Service();
         mappings.put("/", new HelloController());
-        mappings.put("/login", new LoginController(new Service(), SessionManager.getInstance()));
+        mappings.put("/login", new LoginController(service, SessionManager.getInstance()));
+        mappings.put("/register", new RegisterController(service));
     }
 
     public Object getHandler(HttpRequest httpRequest) {

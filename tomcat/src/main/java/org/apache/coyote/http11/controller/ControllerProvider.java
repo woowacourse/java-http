c0075dbff8da9f67cbf73e065727901f82f56307
jhan0121 <jhan0121@gmail.com>
2025-09-11 16:02:32 +0900

    refactor: 사용자 controller 등록 로직 추가

--- tomcat/src/main/java/org/apache/coyote/http11/controller/ControllerProvider.java
+++ tomcat/src/main/java/org/apache/coyote/http11/controller/ControllerProvider.java
@@ -1,24 +1,24 @@
 package org.apache.coyote.http11.controller;
 
-import com.techcourse.controller.LoginController;
-import com.techcourse.controller.RegisterController;
-import com.techcourse.controller.StaticResourceController;
-import com.techcourse.service.LoginService;
-import com.techcourse.service.RegisterService;
+import java.util.ArrayList;
 import java.util.List;
 import org.apache.coyote.http11.exception.NotFoundException;
 
 public enum ControllerProvider {
     INSTANCE;
 
-    private static final List<Controller> CONTROLLERS = List.of(
-            new RegisterController(new RegisterService()),
-            new LoginController(new LoginService()),
-            new StaticResourceController()
-    );
+    private final List<Controller> controllers;
+
+    ControllerProvider() {
+        this.controllers = new ArrayList<>();
+    }
+
+    public void register(final List<Controller> controllers) {
+        this.controllers.addAll(controllers);
+    }
 
     public Controller findByPath(final String path) {
-        return CONTROLLERS.stream()
+        return controllers.stream()
                 .filter(controller -> controller.isProvidableUrl(path))
                 .findFirst()
                 .orElseThrow(() -> new NotFoundException("존재하지 않는 path입니다: %s".formatted(path)));

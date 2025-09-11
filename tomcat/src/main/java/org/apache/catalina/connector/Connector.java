4085dab0f6925ede4fa2ee38dd539d6805c912b1
ljhee92 <weilainib7@gmail.com>
2025-09-11 18:02:06 +0900

    feat: Thread Pool 적용

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -1,13 +1,16 @@
 package org.apache.catalina.connector;
 
-import org.apache.coyote.http11.Http11Processor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import org.apache.coyote.http11.Http11Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Connector implements Runnable {
 
@@ -15,17 +18,27 @@ public class Connector implements Runnable {
 
     private static final int DEFAULT_PORT = 8080;
     private static final int DEFAULT_ACCEPT_COUNT = 100;
+    private static final int DEFAULT_CORE_POLL_SIZE = 2;
+    private static final int DEFAULT_MAX_THREADS = 100; // 기본은 200, t4g.small 기준 설정
 
     private final ServerSocket serverSocket;
     private boolean stopped;
+    private final ExecutorService executorService;
 
     public Connector() {
-        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT);
+        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT, DEFAULT_MAX_THREADS);
     }
 
-    public Connector(final int port, final int acceptCount) {
+    public Connector(final int port, final int acceptCount, final int maxThreads) {
         this.serverSocket = createServerSocket(port, acceptCount);
         this.stopped = false;
+        this.executorService = new ThreadPoolExecutor(
+                DEFAULT_CORE_POLL_SIZE, // corePoolSize(항상 유지할 스레드 수)
+                maxThreads, // maximumPoolSize(최대 스레드 수)
+                0L, TimeUnit.MILLISECONDS, // keepAliveTime(core 초과 스레드가 대기할 시간)
+                new ArrayBlockingQueue<>(acceptCount), // 대기 큐 크기 제한
+                new ThreadPoolExecutor.AbortPolicy() // 거부 정책(풀과 큐가 모두 찼을 때 새 작업 처리 정책)
+        );
     }
 
     private ServerSocket createServerSocket(final int port, final int acceptCount) {
@@ -66,8 +79,16 @@ public class Connector implements Runnable {
         if (connection == null) {
             return;
         }
-        var processor = new Http11Processor(connection);
-        new Thread(processor).start();
+
+        executorService.execute(() -> {
+            try {
+                var processor = new Http11Processor(connection);
+                processor.run();
+            } catch (Exception e) {
+                log.error("HTTP 요청 중 예외 발생, client={}, port={}",
+                        connection.getInetAddress(), connection.getPort(), e);
+            }
+        });
     }
 
     public void stop() {
@@ -76,6 +97,8 @@ public class Connector implements Runnable {
             serverSocket.close();
         } catch (IOException e) {
             log.error(e.getMessage(), e);
+        } finally {
+            executorService.shutdown(); // 서버 종료 시 스레드풀 정리
         }
     }
 

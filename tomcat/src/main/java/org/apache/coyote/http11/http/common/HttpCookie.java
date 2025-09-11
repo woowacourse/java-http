ead97df1d82e48c0487402f0010704ce7d94632f
jhan0121 <jhan0121@gmail.com>
2025-09-11 17:18:10 +0900

    fix: 쿠키 파싱 로직 오류 수정

--- tomcat/src/main/java/org/apache/coyote/http11/http/common/HttpCookie.java
+++ tomcat/src/main/java/org/apache/coyote/http11/http/common/HttpCookie.java
@@ -24,13 +24,14 @@ public class HttpCookie {
 
     private static Map<String, String> parseCookies(final String rawCookie) {
         return Arrays.stream(rawCookie.trim().split(HttpSplitFormat.COOKIE.getValue()))
+                .map(String::trim)
                 .filter(rawCookieElement -> !rawCookieElement.isBlank())
                 .map(rawCookieElement -> rawCookieElement.split(HttpSplitFormat.COOKIE_ELEMENT.getValue(), 2))
-                .filter(cookieElement -> cookieElement.length != 2)
-                .filter(cookieElement -> cookieElement[0].isBlank())
+                .filter(cookieElement -> cookieElement.length == 2)
+                .filter(cookieElement -> !cookieElement[0].trim().isEmpty())
                 .collect(Collectors.toMap(
-                        cookieElement -> cookieElement[0],
-                        cookieElement -> cookieElement[1]
+                        cookieElement -> cookieElement[0].trim(),
+                        cookieElement -> cookieElement[1].trim()
                 ));
     }
 

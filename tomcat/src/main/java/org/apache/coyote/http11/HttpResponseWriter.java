7b17935cd467bc6c6e8d68f645b16af4d35f5a78
Mint <audwl03071@gmail.com>
2025-09-11 10:18:03 +0900

    feat: 항상 헤더 추가하지 않도록 수정

--- tomcat/src/main/java/org/apache/coyote/http11/HttpResponseWriter.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpResponseWriter.java
@@ -2,16 +2,10 @@ package org.apache.coyote.http11;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Map;
 
 public class HttpResponseWriter {
 
     public void write(final OutputStream outputStream, final HttpResponse response) throws IOException {
-        Map<String, String> headers = response.headers();
-        byte[] body = response.body();
-        headers.putIfAbsent("Content-Type", "text/html;charset=utf-8");
-        headers.putIfAbsent("Content-Length", String.valueOf(body.length));
-
         outputStream.write(response.asHeaderString().getBytes());
         outputStream.write(response.body());
         outputStream.flush();

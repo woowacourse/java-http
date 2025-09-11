fe59f8d28fb8ae3149cc28f33e34dd2b0f342fb0
AppleMint98 <applemint9821@gmail.com>
2025-09-11 15:16:15 +0900

    feat: 출력 역할 Response 분리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -2,18 +2,15 @@ package org.apache.coyote.http11;
 
 import java.io.IOException;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 import org.apache.coyote.Processor;
 import org.apache.coyote.http11.controller.Controller;
 import org.apache.coyote.http11.controller.util.ControllerMapper;
-import org.apache.coyote.http11.exception.Http11ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
-    private static final String CRLF = "\r\n";
 
     private final Socket connection;
 
@@ -36,13 +33,9 @@ public class Http11Processor implements Runnable, Processor {
 
             final Controller controller = ControllerMapper.getController(request.getPath());
             controller.service(request, response);
-
-            outputStream.write(response.buildResponse(StandardCharsets.UTF_8));
-            outputStream.flush();
+            response.send();
         } catch (IOException e) {
             log.error(e.getMessage(), e);
-        } catch (Http11ParseException ex) {
-            log.error(ex.getMessage(), ex); // TODO: response.sendError()로 변경
         } catch (Exception e) {
             throw new RuntimeException(e);
         }

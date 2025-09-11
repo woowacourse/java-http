2d31793d6b548dd88c28f3c5a148295f97962aa6
supernovaMK <minkikim2002@gmail.com>
2025-09-11 14:31:23 +0900

    refactor: SessionManager에서 concurrentHashMap 의 null 방지 로직 추가

--- tomcat/src/main/java/org/apache/coyote/session/SessionManager.java
+++ tomcat/src/main/java/org/apache/coyote/session/SessionManager.java
@@ -13,6 +13,9 @@ public class SessionManager {
     }
 
     public Session findSession(final String id) throws IOException {
+        if (id == null) {
+            return null;
+        }
         return SESSIONS.getOrDefault(id, null);
     }
 

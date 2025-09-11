85c67fb21744ad3e75212f2d0d08d36a31378c6d
nourzoo <tmddus4671@gmail.com>
2025-09-11 17:32:16 +0900

    feat: ThreadPoolExecutor 사용해 Thread Pool 적용

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -1,13 +1,19 @@
 package org.apache.catalina.connector;
 
-import org.apache.coyote.http11.Http11Processor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static ch.qos.logback.core.util.CloseUtil.closeQuietly;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import org.apache.coyote.http11.Http11Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Connector implements Runnable {
 
@@ -15,17 +21,29 @@ public class Connector implements Runnable {
 
     private static final int DEFAULT_PORT = 8080;
     private static final int DEFAULT_ACCEPT_COUNT = 100;
+    private static final int DEFAULT_CORE_THREADS = 3;
+    private static final int DEFAULT_MAX_THREADS = 200;
+    private static final int KEEP_ALIVE_SECONDS = 60;
+    private static final int DEFAULT_TIMEOUT = 10;
 
     private final ServerSocket serverSocket;
     private boolean stopped;
+    private final ExecutorService executor;
 
     public Connector() {
-        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT);
+        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT, DEFAULT_CORE_THREADS, DEFAULT_MAX_THREADS);
     }
 
-    public Connector(final int port, final int acceptCount) {
+    public Connector(final int port, final int acceptCount, final int coreThreads, final int maxThreads) {
         this.serverSocket = createServerSocket(port, acceptCount);
         this.stopped = false;
+        this.executor = new ThreadPoolExecutor(
+                coreThreads,
+                maxThreads,
+                KEEP_ALIVE_SECONDS,
+                TimeUnit.SECONDS,
+                new LinkedBlockingDeque<>()
+        );
     }
 
     private ServerSocket createServerSocket(final int port, final int acceptCount) {
@@ -66,8 +84,12 @@ public class Connector implements Runnable {
         if (connection == null) {
             return;
         }
-        var processor = new Http11Processor(connection);
-        new Thread(processor).start();
+        var task = new Http11Processor(connection);
+        try {
+            executor.execute(task);
+        } catch (RejectedExecutionException ex) {
+            closeQuietly(connection);
+        }
     }
 
     public void stop() {
@@ -76,6 +98,17 @@ public class Connector implements Runnable {
             serverSocket.close();
         } catch (IOException e) {
             log.error(e.getMessage(), e);
+        }
+        if (executor != null) {
+            executor.shutdown();
+            try {
+                if (!executor.awaitTermination(DEFAULT_TIMEOUT, TimeUnit.SECONDS)) {
+                    executor.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                executor.shutdownNow();
+                Thread.currentThread().interrupt();
+            }
         }
     }
 

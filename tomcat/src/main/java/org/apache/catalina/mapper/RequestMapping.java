c86317a95018ac486956dbe6fdbcf31d7a13d820
junhaa <wooha34567@gmail.com>
2025-09-11 09:43:54 +0900

    refactor: 매핑 경로 Map.of()를 이용해서 저장

--- tomcat/src/main/java/org/apache/catalina/mapper/RequestMapping.java
+++ tomcat/src/main/java/org/apache/catalina/mapper/RequestMapping.java
@@ -4,8 +4,6 @@ import com.techcourse.controller.DefaultController;
 import com.techcourse.controller.LoginController;
 import com.techcourse.controller.RegisterController;
 import com.techcourse.controller.RootController;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import org.apache.catalina.controller.Controller;
 import org.apache.catalina.request.ServletRequest;
@@ -16,12 +14,11 @@ public class RequestMapping {
     private static final DefaultController DEFAULT_CONTROLLER;
 
     static {
-        final Map<String, Controller> mappings = new HashMap<>();
-        mappings.put("/", new RootController());
-        mappings.put("/login", new LoginController());
-        mappings.put("/register", new RegisterController());
-
-        MAPPINGS = Collections.unmodifiableMap(mappings);
+        MAPPINGS = Map.of(
+                "/", new RootController(),
+                "/login", new LoginController(),
+                "/register", new RegisterController()
+        );
         DEFAULT_CONTROLLER = new DefaultController();
     }
 

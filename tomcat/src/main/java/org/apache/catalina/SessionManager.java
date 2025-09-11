994fbd7e48f1631aee5fdbedc0efc7f25d9cb5a9
민경윤 <ruddbs3308@naver.com>
2025-09-11 23:36:36 +0900

    refactor: 불필요한 import문 제거

--- tomcat/src/main/java/org/apache/catalina/SessionManager.java
+++ tomcat/src/main/java/org/apache/catalina/SessionManager.java
@@ -1,7 +1,5 @@
 package org.apache.catalina;
 
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import org.apache.coyote.http11.HttpSession;

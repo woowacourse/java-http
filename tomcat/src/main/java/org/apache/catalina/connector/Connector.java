3a4dedc2dba5a9bac06ad51c73cf3d17aad95ed1
Firedrago95 <yonghwa95@naver.com>
2025-09-11 19:42:39 +0900

    refactor: 최대 쓰레드풀 250개로 설정

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -16,7 +16,7 @@ public class Connector implements Runnable {
 
     private static final int DEFAULT_PORT = 8080;
     private static final int DEFAULT_ACCEPT_COUNT = 100;
-    private static final int DEFAULT_MAX_THREAD = 10;
+    private static final int DEFAULT_MAX_THREAD = 250;
 
     private final ServerSocket serverSocket;
     private final ExecutorService executorService;

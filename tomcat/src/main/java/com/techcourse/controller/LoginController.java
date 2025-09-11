abc6b26b2b28710478a69ff1146cab66d2a6d007
2jin2.1031 <2jin2.1031@gmail.com>
2025-09-12 06:00:12 +0900

    feat: 쿠키 파싱 책임을 HttpCookie에 위임

--- tomcat/src/main/java/com/techcourse/controller/LoginController.java
+++ tomcat/src/main/java/com/techcourse/controller/LoginController.java
@@ -8,6 +8,7 @@ import com.techcourse.service.Service;
 import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
+import org.apache.coyote.http11.request.HttpCookie;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.response.HttpResponse;
 import org.slf4j.Logger;
@@ -50,27 +51,18 @@ public class LoginController extends AbstractController {
         }
     }
 
-    private boolean existsSession(Map<String, String> headers) { // TODO 2025. 9. 12. 04:39: parsing의 책임은 cookie의 것이 아닌가
+    private boolean existsSession(Map<String, String> headers) {
         String cookie = headers.get("Cookie");
-
         if (cookie == null) {
             return false;
         }
 
-        String[] parsedCookie = cookie.split("; "); // Idea-1f980704=d1410481-d266-4764-a4dd-47a3d9d19f64; Pycharm-edf2faa0=91c6849a-33b8-4d86-a27b-bd16d51f090a; Webstorm-b369078d=8a15b985-71c2-42b0-96b4-eb3e64f0dfe5; JSESSIONID=d4d9915e-323d-43af-beb0-a60ac9e7c6b7
-        for (String parsedValue : parsedCookie) {
-            String[] splits = parsedValue.split("=", 2);
-            String name = splits[0];
-            String value = splits[1];
-
-            if (name.equals("JSESSIONID")) {
-                Session session = sessionManager.findSession(value);
-                if (session == null) {
-                    return false;
-                }
-                return true;
-            }
+        String sessionId = HttpCookie.extractCookieValue(cookie, "JSESSIONID");
+        if (sessionId == null) {
+            return false;
         }
-        return false;
+
+        Session session = sessionManager.findSession(sessionId);
+        return session != null;
     }
 }

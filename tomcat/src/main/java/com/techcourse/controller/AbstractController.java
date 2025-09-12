189a230f427a447bfd97d2837ba8f573b7ec7af9
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-09-12 10:22:30 +0900

    feat: 추상 컨트롤러 객체 구현

---
+++ tomcat/src/main/java/com/techcourse/controller/AbstractController.java
@@ -0,0 +1,41 @@
+package com.techcourse.controller;
+
+import org.apache.coyote.http11.request.HttpMethodType;
+import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.response.HttpResponse;
+
+public abstract class AbstractController implements Controller {
+
+    @Override
+    public HttpResponse service(HttpRequest request) {
+        HttpMethodType method = request.getMethodType();
+        
+        return switch (method) {
+            case GET -> doGet(request);
+            case POST -> doPost(request);
+            case PUT -> doPut(request);
+            case DELETE -> doDelete(request);
+            case PATCH -> doPatch(request);
+        };
+    }
+
+    protected HttpResponse doGet(HttpRequest request) {
+        return HttpResponse.notFound();
+    }
+
+    protected HttpResponse doPost(HttpRequest request) {
+        return HttpResponse.notFound();
+    }
+
+    protected HttpResponse doPut(HttpRequest request) {
+        return HttpResponse.notFound();
+    }
+
+    protected HttpResponse doDelete(HttpRequest request) {
+        return HttpResponse.notFound();
+    }
+
+    protected HttpResponse doPatch(HttpRequest request) {
+        return HttpResponse.notFound();
+    }
+}

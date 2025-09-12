ed90ab210d80de2f6083da99d2ec10a61dc6fd77
LeeJaeHoon <dlwogns3413@naver.com>
2025-09-12 21:07:16 +0900

    feat: 스레드 풀 사용하도록 변경

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Connector implements Runnable {
 
@@ -15,17 +17,22 @@ public class Connector implements Runnable {
 
     private static final int DEFAULT_PORT = 8080;
     private static final int DEFAULT_ACCEPT_COUNT = 100;
+    private static final int DEFAULT_MAX_THREADS = 200;
 
     private final ServerSocket serverSocket;
+    private final ExecutorService threadPool;
+
     private boolean stopped;
 
+
     public Connector() {
-        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT);
+        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT, DEFAULT_MAX_THREADS);
     }
 
-    public Connector(final int port, final int acceptCount) {
+    public Connector(final int port, final int acceptCount, final int maxThreads) {
         this.serverSocket = createServerSocket(port, acceptCount);
         this.stopped = false;
+        this.threadPool = Executors.newFixedThreadPool(Math.min(DEFAULT_MAX_THREADS, maxThreads));
     }
 
     private ServerSocket createServerSocket(final int port, final int acceptCount) {
@@ -66,14 +73,14 @@ public class Connector implements Runnable {
         if (connection == null) {
             return;
         }
-        var processor = new Http11Processor(connection);
-        new Thread(processor).start();
+        threadPool.execute(new Http11Processor(connection));
     }
 
     public void stop() {
         stopped = true;
         try {
             serverSocket.close();
+            threadPool.shutdown();
         } catch (IOException e) {
             log.error(e.getMessage(), e);
         }

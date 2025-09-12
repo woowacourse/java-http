238406fd4a977028dd2d36453564f44ecee0c24b
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-09-12 22:00:38 +0900

    feat: 동시정 컬렉션 적용

--- tomcat/src/main/java/org/apache/coyote/http11/SessionManager.java
+++ tomcat/src/main/java/org/apache/coyote/http11/SessionManager.java
@@ -1,19 +1,19 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.model.User;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class SessionManager  {
 
-    private static final Map<String, Session> SESSIONS = new HashMap<>();
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
 
     public static void add(final Session session) {
         SESSIONS.put(session.getId(), session);
     }
 
     public static Session findSession(final String id) {
-        return SESSIONS.getOrDefault(id, null);
+        return SESSIONS.get(id);
     }
 
     public static void remove(final String id) {

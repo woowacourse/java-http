746dac9cc9f3513020c778ad868e48228526f212
ryan-dia <cjfdnjs158@naver.com>
2025-09-12 12:14:52 +0900

    feat: 전역에서 예외처리해주는 핸들러 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package org.apache.coyote.http11.exception;
+
+import java.rmi.ServerException;
+import javassist.NotFoundException;
+import org.apache.coyote.http11.http.HttpStatus;
+
+public class GlobalExceptionHandler {
+
+    public ErrorPage resolve(Exception e) {
+        if (e instanceof IllegalArgumentException) {
+            return new ErrorPage("static/404.html", HttpStatus.BAD_REQUEST);
+        }
+        if (e instanceof SecurityException) {
+            return new ErrorPage("static/401.html", HttpStatus.FORBIDDEN);
+        }
+        if (e instanceof UnsupportedOperationException) {
+            return new ErrorPage("static/404.html", HttpStatus.METHOD_NOT_ALLOWED);
+        }
+        if (e instanceof ServerException) {
+            return new ErrorPage("static/500.html", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        if (e instanceof NotFoundException) {
+            return new ErrorPage("static/404.html", HttpStatus.BAD_REQUEST);
+        }
+
+        return new ErrorPage("errors/500.html", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    public record ErrorPage(String path, HttpStatus status) {}
+}

0df55dcb5cdf3b56ce5e3cccfc0539ab7f9601f2
Wooyoung <quantumjump10@gmail.com>
2025-09-13 00:39:11 +0900

    feat: LoginController 추가

---
+++ tomcat/src/main/java/com/techcourse/controller/LoginController.java
@@ -0,0 +1,72 @@
+package com.techcourse.controller;
+
+import com.techcourse.db.InMemoryUserRepository;
+import com.techcourse.model.User;
+import java.io.IOException;
+import java.util.Map;
+import org.apache.catalina.controller.AbstractController;
+import org.apache.coyote.http11.StatusCode;
+import org.apache.coyote.http11.message.HttpCookie;
+import org.apache.coyote.http11.message.HttpHeaders;
+import org.apache.coyote.http11.message.StatusLine;
+import org.apache.coyote.http11.message.request.HttpRequest;
+import org.apache.coyote.http11.message.response.HttpResponse;
+import org.apache.coyote.http11.session.Session;
+import org.apache.coyote.http11.session.SessionManager;
+
+public class LoginController extends AbstractController {
+    private final SessionManager sessionManager = new SessionManager();
+
+    @Override
+    protected void doGet(HttpRequest request, HttpResponse response) throws IOException {
+        if (request.hasSessionCookie()) {
+            final Session session = sessionManager.findSession(request.getJsessionId());
+            if (session != null && session.getAttribute("user") != null) {
+                redirect(request, response, "/index.html");
+            }
+        }
+
+        renderPage(request, response, "/login.html");
+    }
+
+    @Override
+    protected void doPost(HttpRequest request, HttpResponse response) throws IOException {
+        final Map<String, String> params = request.getFormParams();
+
+        final String account = params.get("account");
+        final String password = params.get("password");
+
+        if (account == null || password == null) {
+            renderPage(request, response, "/login.html");
+            return;
+        }
+
+        InMemoryUserRepository.findByAccount(account)
+                .filter(user -> user.checkPassword(password))
+                .ifPresentOrElse(
+                        user -> loginUser(request, response, user),
+                        () -> redirect(request, response, "/401.html")
+                );
+    }
+
+    private void loginUser(
+            final HttpRequest request,
+            final HttpResponse response,
+            final User user
+    ) {
+        final Session session = new Session();
+        session.setAttribute("user", user);
+        sessionManager.add(session);
+
+        final HttpHeaders headers = new HttpHeaders();
+        headers.addHeader("Location", "/index.html");
+
+        final HttpCookie cookie = HttpCookie.of("JSESSIONID", session.getId());
+        headers.addHeader("Set-Cookie", cookie.toHeaderCookie());
+        headers.addHeader("Content-Length", "0");
+
+        response.setStatusLine(new StatusLine(request.getVersion(), StatusCode.FOUND));
+        response.setHeaders(headers);
+        response.setBody(new byte[0]);
+    }
+}

f5cd16f488196a5dc7075cb8caab92f17c8d4a0e
ryan-dia <cjfdnjs158@naver.com>
2025-09-12 12:18:32 +0900

    test: 이전 미션 테스트 제거

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -2,9 +2,6 @@ package org.apache.coyote.http11;
 
 import java.util.List;
 import org.apache.coyote.http11.handler.DispatcherHandler;
-import org.apache.coyote.http11.handler.Handler;
-import org.apache.coyote.http11.handler.dynamic.LoginHandler;
-import org.apache.coyote.http11.handler.statics.StaticResourceHandler;
 import org.junit.jupiter.api.Test;
 import support.StubSocket;
 
@@ -37,34 +34,34 @@ class Http11ProcessorTest {
 //        assertThat(socket.output()).isEqualTo(expected);
 //    }
 
-    @Test
-    void index() throws IOException {
-        // given
-        final String httpRequest= String.join("\r\n",
-                "GET /index.html HTTP/1.1 ",
-                "Host: localhost:8080 ",
-                "Connection: keep-alive ",
-                "",
-                "");
-
-        final var socket = new StubSocket(httpRequest);
-        List<Handler> handlers = List.of(
-                new StaticResourceHandler("static", "index.html"),
-                new LoginHandler()
-        );
-        final Http11Processor processor = new Http11Processor(socket, new DispatcherHandler(handlers));
-
-        // when
-        processor.process(socket);
-
-        // then
-        final URL resource = getClass().getClassLoader().getResource("static/index.html");
-        var expected = "HTTP/1.1 200 OK\r\n" +
-                "Content-Type: text/html;charset=utf-8\r\n" +
-                "Content-Length: 5564\r\n" +
-                "\r\n"+
-                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
-
-        assertThat(socket.output()).isEqualTo(expected);
-    }
+//    @Test
+//    void index() throws IOException {
+//        // given
+//        final String httpRequest= String.join("\r\n",
+//                "GET /index.html HTTP/1.1 ",
+//                "Host: localhost:8080 ",
+//                "Connection: keep-alive ",
+//                "",
+//                "");
+//
+//        final var socket = new StubSocket(httpRequest);
+//        List<Handler> handlers = List.of(
+//                new StaticResourceHandler("static", "index.html"),
+//                new LoginHandler()
+//        );
+//        final Http11Processor processor = new Http11Processor(socket, new DispatcherHandler(handlers));
+//
+//        // when
+//        processor.process(socket);
+//
+//        // then
+//        final URL resource = getClass().getClassLoader().getResource("static/index.html");
+//        var expected = "HTTP/1.1 200 OK\r\n" +
+//                "Content-Type: text/html;charset=utf-8\r\n" +
+//                "Content-Length: 5564\r\n" +
+//                "\r\n"+
+//                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+//
+//        assertThat(socket.output()).isEqualTo(expected);
+//    }
 }

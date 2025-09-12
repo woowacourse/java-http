d6b1bbebb92672d7bf91410bf51e814b588f8cc2
Wooyoung <quantumjump10@gmail.com>
2025-09-13 00:35:50 +0900

    feat: AbstractController 추가

---
+++ tomcat/src/main/java/org/apache/catalina/controller/AbstractController.java
@@ -0,0 +1,67 @@
+package org.apache.catalina.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import org.apache.coyote.http11.ContentType;
+import org.apache.coyote.http11.StatusCode;
+import org.apache.coyote.http11.message.HttpHeaders;
+import org.apache.coyote.http11.message.StatusLine;
+import org.apache.coyote.http11.message.request.HttpRequest;
+import org.apache.coyote.http11.message.response.HttpResponse;
+
+public abstract class AbstractController implements Controller {
+
+    @Override
+    public void service(HttpRequest request, HttpResponse response) throws IOException {
+        if (request.equalMethod("GET")) {
+            doGet(request, response);
+        }
+
+        if (request.equalMethod("POST")) {
+            doPost(request, response);
+        }
+    }
+
+    protected void doGet(HttpRequest request, HttpResponse response) throws IOException {}
+
+    protected void doPost(HttpRequest request, HttpResponse response) throws IOException {}
+
+    protected String readResource(final String resourcePath) throws IOException {
+        final URL resource = getClass().getClassLoader().getResource("static" + resourcePath);
+        if (resource == null) return "";
+        return Files.readString(new File(resource.getFile()).toPath(), StandardCharsets.UTF_8);
+    }
+
+    protected void redirect(
+            final HttpRequest request,
+            final HttpResponse response,
+            final String location
+    ) {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.addHeader("Location", location);
+        headers.addHeader("Content-Length", "0");
+
+        response.setStatusLine(new StatusLine(request.getVersion(), StatusCode.FOUND));
+        response.setHeaders(headers);
+        response.setBody(new byte[0]);
+    }
+
+    protected void renderPage(
+            final HttpRequest request,
+            final HttpResponse response,
+            final String resourcePath
+    ) throws IOException {
+        final String body = readResource(resourcePath);
+
+        final HttpHeaders headers = new HttpHeaders();
+        headers.addHeader("Content-Type", ContentType.HTML.getMimeType());
+        headers.addHeader("Content-Length", String.valueOf(body.getBytes(StandardCharsets.UTF_8).length));
+
+        response.setStatusLine(new StatusLine(request.getVersion(), StatusCode.OK));
+        response.setHeaders(headers);
+        response.setBody(body.getBytes(StandardCharsets.UTF_8));
+    }
+}

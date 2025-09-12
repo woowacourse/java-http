858b5966ec0300ebb505102552775a7e249d5d67
ryan-dia <cjfdnjs158@naver.com>
2025-09-12 12:12:51 +0900

    feat: httpStatus 상태 코드 추가

--- tomcat/src/main/java/org/apache/coyote/http11/http/HttpStatus.java
+++ tomcat/src/main/java/org/apache/coyote/http11/http/HttpStatus.java
@@ -4,24 +4,26 @@ public enum HttpStatus {
     OK(200, "OK"),
     CREATED(201, "Created"),
     FOUND(302, "Found"),
+    BAD_REQUEST(400, "Bad Request"),
     UNAUTHORIZED(401, "Unauthorized"),
+    FORBIDDEN(403, "Forbidden"),
     NOT_FOUND(404, "Not Found"),
     METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
     INTERNAL_SERVER_ERROR(500, "Internal Server Error");
 
     private final int code;
-    private final String reason;
+    private final String message;
 
-    HttpStatus(int code, String reason) {
+    HttpStatus(int code, String message) {
         this.code = code;
-        this.reason = reason;
+        this.message = message;
     }
 
     public int getCode() {
         return code;
     }
 
-    public String getReason() {
-        return reason;
+    public String getMessage() {
+        return message;
     }
 }

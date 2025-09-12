f2af264db8aeb203231b3d1ba2c6972197306f4e
Wooyoung <quantumjump10@gmail.com>
2025-09-13 00:39:31 +0900

    feat: RequestMapping 추가

---
+++ tomcat/src/main/java/org/apache/catalina/controller/RequestMapping.java
@@ -0,0 +1,17 @@
+package org.apache.catalina.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestMapping {
+
+    private final Map<String, Controller> mappings = new HashMap<>();
+
+    public void registerMapping(final String path, final Controller controller) {
+        mappings.put(path, controller);
+    }
+
+    public Controller getController(final String path) {
+        return mappings.get(path);
+    }
+}

ebd16ea80fc77419e77d0754400be1ffe6a29cbb
jhpark1227 <junhyeok0246@naver.com>
2025-09-13 15:00:31 +0900

    feat: 동시성 컬렉션 적용

--- tomcat/src/main/java/org/apache/catalina/SessionManager.java
+++ tomcat/src/main/java/org/apache/catalina/SessionManager.java
@@ -1,12 +1,12 @@
 package org.apache.catalina;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class SessionManager implements Manager {
 
     private static final SessionManager INSTANCE = new SessionManager();
-    private final Map<String, Session> SESSIONS = new HashMap<>();
+    private final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
 
     private SessionManager() {
     }

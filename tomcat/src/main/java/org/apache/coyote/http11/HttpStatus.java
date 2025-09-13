318a8653517a88ab817c54c18ab71544a7c09efd
taek2222 <oot309@daum.net>
2025-09-13 22:49:21 +0900

    feat: 405 에러 코드 추가 정의

--- tomcat/src/main/java/org/apache/coyote/http11/HttpStatus.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpStatus.java
@@ -6,6 +6,7 @@ public enum HttpStatus {
     BAD_REQUEST(400, "Bad Request"),
     UNAUTHORIZED(401, "Unauthorized"),
     NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
     INTERNAL_SERVER_ERROR(500, "Internal Server Error");
 
     private final int code;

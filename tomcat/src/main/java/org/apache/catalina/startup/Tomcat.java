e154a8c10a5eaa453a8d414760804482ccd49eaa
AppleMint98 <applemint9821@gmail.com>
2025-09-13 22:16:59 +0900

    feat: 쓰레드 풀 적용

--- tomcat/src/main/java/org/apache/catalina/startup/Tomcat.java
+++ tomcat/src/main/java/org/apache/catalina/startup/Tomcat.java
@@ -12,7 +12,7 @@ public class Tomcat {
 
     public void start() {
         var connector = new Connector();
-        connector.start();
+        connector.run();
 
         try {
             // make the application wait until we press any key.

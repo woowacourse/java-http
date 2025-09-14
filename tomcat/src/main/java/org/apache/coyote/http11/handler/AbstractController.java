72d6ed3b01388cf8eeec50ad3404bd9d6a08848e
kimdev <kim__dev@hufs.ac.kr>
2025-09-14 16:30:44 +0900

    refactor: 지원하지 않는 메서드로 요청 시 405 반환하도록 수정

--- tomcat/src/main/java/org/apache/coyote/http11/handler/AbstractController.java
+++ tomcat/src/main/java/org/apache/coyote/http11/handler/AbstractController.java
@@ -1,8 +1,11 @@
 package org.apache.coyote.http11.handler;
 
+import org.apache.coyote.http11.general.HttpHeaders;
 import org.apache.coyote.http11.handler.applicationRequest.ApplicationRequest;
 import org.apache.coyote.http11.handler.applicationResponse.ApplicationResponse;
+import org.apache.coyote.http11.handler.applicationResponse.JsonResponse;
 import org.apache.coyote.http11.httpRequest.HttpMethod;
+import org.apache.coyote.http11.httpResponse.HttpStatus;
 
 public abstract class AbstractController implements Controller {
 
@@ -16,8 +19,10 @@ public abstract class AbstractController implements Controller {
             case HttpMethod.GET -> {
                 return doGet(applicationRequest);
             }
+            default -> {
+                return new JsonResponse(HttpStatus.METHOD_NOT_ALLOWED, HttpHeaders.empty(), "지원하지 않는 요청 방식입니다.");
+            }
         }
-        throw new IllegalArgumentException("지원하지 않는 요청 방식입니다.");
     }
 
     protected abstract ApplicationResponse doGet(ApplicationRequest applicationRequest);

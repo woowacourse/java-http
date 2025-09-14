c8ee3f8c27c68b5d7b61f30ae222f5db72c06f1d
Park Minseon <63039855+minSsan@users.noreply.github.com>
2025-09-14 20:01:59 +0900

    [4단계 - 동시성 확장하기] 밍곰(박민선) 제출합니다. (#1079)
    * feat: 1. Executors로 Thread Pool 적용
    
    * refactor: 태스크 실행 트리거 메서드 변경 - Future 불필요 하므로 submit -> execute
    
    * refactor: ExecutorService 생성 조건 변경

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -8,6 +8,10 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Connector implements Runnable {
 
@@ -15,17 +19,24 @@ public class Connector implements Runnable {
 
     private static final int DEFAULT_PORT = 8080;
     private static final int DEFAULT_ACCEPT_COUNT = 100;
+    private static final int DEFAULT_MAX_THREADS_COUNT = 250;
 
     private final ServerSocket serverSocket;
+    private final ExecutorService executorService;
     private boolean stopped;
 
     public Connector() {
-        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT);
+        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT, DEFAULT_MAX_THREADS_COUNT);
     }
 
-    public Connector(final int port, final int acceptCount) {
+    public Connector(final int port, final int acceptCount, final int maxThreads) {
         this.serverSocket = createServerSocket(port, acceptCount);
         this.stopped = false;
+        this.executorService = new ThreadPoolExecutor(maxThreads, maxThreads,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(acceptCount),
+                new ThreadPoolExecutor.CallerRunsPolicy() // 스레드 제한 수 초과시 refuse 하지 않고 caller 에서 처리
+        );
     }
 
     private ServerSocket createServerSocket(final int port, final int acceptCount) {
@@ -67,7 +78,7 @@ public class Connector implements Runnable {
             return;
         }
         var processor = new Http11Processor(connection);
-        new Thread(processor).start();
+        executorService.execute(processor);
     }
 
     public void stop() {

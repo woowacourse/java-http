00e95dcc0db2cb312239d6bd21b83202fe32066f
2jin2.1031 <2jin2.1031@gmail.com>
2025-09-14 17:35:08 +0900

    feat: 동시성 컬렉션 사용

--- tomcat/src/main/java/com/techcourse/db/SessionManager.java
+++ tomcat/src/main/java/com/techcourse/db/SessionManager.java
@@ -1,13 +1,12 @@
 package com.techcourse.db;
 
-import jakarta.servlet.http.HttpSession;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class SessionManager {
 
     private static final SessionManager INSTANCE = new SessionManager();
-    private static final Map<String, Session> SESSIONS = new HashMap<>();
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
 
     private SessionManager() {
     }
@@ -20,11 +19,23 @@ public class SessionManager {
         SESSIONS.put(session.getId(), session);
     }
 
+    public boolean addIfAbsent(Session session) {
+        return SESSIONS.putIfAbsent(session.getId(), session) == null;
+    }
+
     public Session findSession(String id) {
         return SESSIONS.get(id);
     }
 
-    public void remove(HttpSession session) {
+    public void remove(Session session) {
         SESSIONS.remove(session.getId());
+    }
+
+    public boolean removeIfSame(Session session) {
+        return SESSIONS.remove(session.getId(), session);
+    }
+
+    public void clear() {
+        SESSIONS.clear();
     }
 }

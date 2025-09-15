75386e195b9920b6f131a00608fc7454ef15aaa0
wook <mbosx316@naver.com>
2025-09-15 21:06:22 +0900

    [4단계 - 동시성 확장하기] 우가(민보욱) 미션 제출합니다. (#1086)
    * feat: thread pool 적용
    
    * feat: thread pool 적용
    
    * feat: thread pool 적용
    
    - disk i/o는 있음.
    
    * feat: RejectedExecutionException 처리

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import org.apache.catalina.FrontController;
 import org.apache.coyote.http11.processor.Http11Processor;
 import org.slf4j.Logger;
@@ -15,17 +18,32 @@ public class Connector implements Runnable {
 
     private static final int DEFAULT_PORT = 8080;
     private static final int DEFAULT_ACCEPT_COUNT = 100;
+    private static final long KEEP_ALIVE = 30L;
+    private static final int DEFAULT_CORE_POOL_SIZE = 8;
+    private static final int DEFAULT_MAX_THREADS = 16;
 
+    private final ThreadPoolExecutor threadPool;
     private final ServerSocket serverSocket;
-    private boolean stopped;
+    private volatile boolean stopped;
 
     public Connector() {
-        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT);
+        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT, DEFAULT_CORE_POOL_SIZE, DEFAULT_MAX_THREADS);
     }
 
-    public Connector(final int port, final int acceptCount) {
+    public Connector(final int port, final int acceptCount, final int corePoolSize, final int maxThreads) {
         this.serverSocket = createServerSocket(port, acceptCount);
         this.stopped = false;
+        this.threadPool = new ThreadPoolExecutor(
+                corePoolSize,
+                maxThreads,
+                KEEP_ALIVE,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(acceptCount)
+        );
+
+        this.threadPool.setRejectedExecutionHandler((r, executor) -> {
+            log.error("큐가 곽차서 더이상 작업을 받을 수 없습니다. 작업 클래스={}", r.getClass().getName());
+        });
     }
 
     private ServerSocket createServerSocket(final int port, final int acceptCount) {
@@ -67,13 +85,12 @@ public class Connector implements Runnable {
         if (connection == null) {
             return;
         }
-
-        var processor = new Http11Processor(connection, frontController);
-        new Thread(processor).start();
+        threadPool.submit(new Http11Processor(connection, frontController));
     }
 
     public void stop() {
         stopped = true;
+        threadPool.shutdown(); //그러면, 새로운 작업은 막고, 이미 진행중인 작업은 실행
         try {
             serverSocket.close();
         } catch (IOException e) {

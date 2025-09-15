9c0426b59b36094224d6d4abde371fa7346d6714
Skyrockets <71587293+Astro-Yu@users.noreply.github.com>
2025-09-15 21:58:41 +0900

    [4단계 - 동시성 확장하기] 아마(유석현) 미션 제출합니다. (#1096)
    * feat: Executors로 Thread Pool 적용
    
    * feat: 동시성 컬렉션 사용하기

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -1,13 +1,14 @@
 package org.apache.catalina.connector;
 
-import org.apache.coyote.http11.Http11Processor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.apache.coyote.http11.Http11Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Connector implements Runnable {
 
@@ -15,16 +16,20 @@ public class Connector implements Runnable {
 
     private static final int DEFAULT_PORT = 8080;
     private static final int DEFAULT_ACCEPT_COUNT = 100;
+    private static final int DEFAULT_MAX_THREAD_COUNT = 10;
 
     private final ServerSocket serverSocket;
+    private final ExecutorService executorService;
     private boolean stopped;
 
     public Connector() {
-        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT);
+        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT, DEFAULT_MAX_THREAD_COUNT);
     }
 
-    public Connector(final int port, final int acceptCount) {
+    public Connector(final int port, final int acceptCount, final int maxThreads) {
         this.serverSocket = createServerSocket(port, acceptCount);
+        // 여기서 고정된 크기의 스레드 풀 생성
+        this.executorService = Executors.newFixedThreadPool(maxThreads);
         this.stopped = false;
     }
 
@@ -62,17 +67,25 @@ public class Connector implements Runnable {
         }
     }
 
+    // 메서드 요청마다 스레드 새로 생성 중.
+    // 요청마다 새 스레드를 생성하지 말고, 미리 스레드를 생성 후 스레드 풀에 넣어둠.
+    // 요청이 오면 해당 스레드 풀에서 꺼내 쓴다.
     private void process(final Socket connection) {
         if (connection == null) {
             return;
         }
         var processor = new Http11Processor(connection);
-        new Thread(processor).start();
+
+        // 스레드를 생성하지 말고, 스레드 풀에 작업을 submit 함.
+        executorService.submit(processor);
     }
 
+    // 서버가 멈추면 스레드풀도 없어져야 함.
     public void stop() {
         stopped = true;
         try {
+            // 스레드풀 종료
+            executorService.shutdown();
             serverSocket.close();
         } catch (IOException e) {
             log.error(e.getMessage(), e);
--- tomcat/src/main/java/org/apache/catalina/session/SessionManager.java
+++ tomcat/src/main/java/org/apache/catalina/session/SessionManager.java
@@ -1,14 +1,14 @@
 package org.apache.catalina.session;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import org.apache.catalina.Manager;
 
 public class SessionManager implements Manager {
 
     // static!
-    private static final Map<String, Session> SESSIONS = new HashMap<>();
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
     private static final SessionManager sessionManager = new SessionManager();
 
     private SessionManager() {

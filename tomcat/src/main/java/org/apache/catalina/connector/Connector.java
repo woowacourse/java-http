1b07537ac1513d890a5af7b8d8132349267c09e8
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-09-16 00:03:51 +0900

    feat: newFixedThreadPool을 활용해서 스레드풀 생성

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.http.Http11Processor;
 
@@ -12,16 +14,19 @@ public class Connector implements Runnable, AutoCloseable {
 
     private static final int DEFAULT_PORT = 8080;
     private static final int DEFAULT_ACCEPT_COUNT = 100;
+    private static final int DEFAULT_MAX_THREADS = 100;
 
     private final ServerSocket serverSocket;
+    private final ExecutorService threadPool;
     private boolean stopped;
 
     public Connector() {
-        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT);
+        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT, DEFAULT_MAX_THREADS);
     }
 
-    public Connector(final int port, final int acceptCount) {
+    public Connector(final int port, final int acceptCount, final int maxThreads) {
         this.serverSocket = createServerSocket(port, acceptCount);
+        this.threadPool = Executors.newFixedThreadPool(maxThreads);
         this.stopped = false;
     }
 
@@ -63,8 +68,11 @@ public class Connector implements Runnable, AutoCloseable {
         if (connection == null) {
             return;
         }
-        final var processor = new Http11Processor(connection);
-        new Thread(processor).start();
+
+        threadPool.execute(() -> {
+            final var processor = new Http11Processor(connection);
+            processor.run();
+        });
     }
 
     public void stop() {

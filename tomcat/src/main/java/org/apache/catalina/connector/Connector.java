a70ff727b1d8601247ca28c64e66d1b0a4153de7
Wooyoung <quantumjump10@gmail.com>
2025-09-15 09:41:01 +0900

    refactor: Connector stop 호출 시, 스레드풀 종료하도록 수정

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -82,6 +82,8 @@ public class Connector implements Runnable {
         } catch (IOException e) {
             log.error(e.getMessage(), e);
         }
+
+        executorService.shutdown();
     }
 
     private int checkPort(final int port) {

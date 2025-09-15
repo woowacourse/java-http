ccf09055719004701fb3eaa4ab35111673154afb
Chaeyoung714 <202102760@hufs.ac.kr>
2025-09-15 11:55:38 +0900

    refactor: 스레드풀 설정 추가

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -8,8 +8,7 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class Connector implements Runnable {
 
@@ -17,17 +16,25 @@ public class Connector implements Runnable {
 
     private static final int DEFAULT_PORT = 8080;
     private static final int DEFAULT_ACCEPT_COUNT = 100;
+    private static final int DEFAULT_CORE_POOL_SIZE = 10;
+    private static final int DEFAULT_MAX_POOL_SIZE = 250;
+    private static final int DEFAULT_IDLE_THREAD_KEEP_ALIVE_TIME = 60;
 
     private final ServerSocket serverSocket;
     private final ExecutorService executorService;
     private boolean stopped;
 
     public Connector() {
-        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT, 10);
+        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT, DEFAULT_CORE_POOL_SIZE, DEFAULT_MAX_POOL_SIZE);
     }
 
-    public Connector(final int port, final int acceptCount, final int maxThreads) {
-        this.executorService = Executors.newFixedThreadPool(maxThreads);
+    public Connector(final int port, final int acceptCount, final int coreThreads, final int maxThreads) {
+        this.executorService = new ThreadPoolExecutor(
+                coreThreads,
+                maxThreads,
+                DEFAULT_IDLE_THREAD_KEEP_ALIVE_TIME, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(DEFAULT_ACCEPT_COUNT)
+        );
         this.serverSocket = createServerSocket(port, acceptCount);
         this.stopped = false;
     }

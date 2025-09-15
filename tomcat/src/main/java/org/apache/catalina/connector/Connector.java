3bf549185752bf4e94bfe13c83efa469e0dbb04f
RaZel713 <razel7130@gmail.com>
2025-09-15 20:49:01 +0900

    feat: Executors로 Thread Pool 적용

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -1,13 +1,15 @@
 package org.apache.catalina.connector;
 
-import org.apache.coyote.http11.Http11Processor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import org.apache.coyote.http11.Http11Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Connector implements Runnable {
 
@@ -15,17 +17,20 @@ public class Connector implements Runnable {
 
     private static final int DEFAULT_PORT = 8080;
     private static final int DEFAULT_ACCEPT_COUNT = 100;
+    private static final int DEFAULT_MAX_THREADS = 200;
 
     private final ServerSocket serverSocket;
+    private final ExecutorService executorService;
     private boolean stopped;
 
     public Connector() {
-        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT);
+        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT, DEFAULT_MAX_THREADS);
     }
 
-    public Connector(final int port, final int acceptCount) {
+    public Connector(final int port, final int acceptCount, final int maxThreads) {
         this.serverSocket = createServerSocket(port, acceptCount);
         this.stopped = false;
+        this.executorService = Executors.newFixedThreadPool(maxThreads);
     }
 
     private ServerSocket createServerSocket(final int port, final int acceptCount) {
@@ -41,7 +46,7 @@ public class Connector implements Runnable {
     public void start() {
         var thread = new Thread(this);
         thread.setDaemon(true);
-        thread.start();
+        thread.start(); // Connector 실행용 스레드
         stopped = false;
         log.info("Web Application Server started {} port.", serverSocket.getLocalPort());
     }
@@ -67,15 +72,24 @@ public class Connector implements Runnable {
             return;
         }
         var processor = new Http11Processor(connection);
-        new Thread(processor).start();
+        executorService.execute(processor); // ThreadPool 개수 안에서 실행
     }
 
     public void stop() {
         stopped = true;
         try {
+            executorService.shutdown(); // 새로운 작업 제출을 막고 기존 작업 종료 대기 시작
+
+            if (executorService.awaitTermination(60, TimeUnit.SECONDS)) { // 작업 종료 대기
+                executorService.shutdownNow(); // 대기해도 종료 안되면 강제 셧다운
+            }
+
             serverSocket.close();
         } catch (IOException e) {
             log.error(e.getMessage(), e);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); // 상태 복원
+            log.warn("인터럽트 발생! (정상 종료 신호 가능성 있음)");
         }
     }
 

dc8d3e79c6eca3f982513634744b7a618186d56d
eoehd1ek <eoehd2ek@gmail.com>
2025-09-16 23:21:49 +0900

    refactor: ExecutorService 종료 로직 보강

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -6,6 +6,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import org.apache.coyote.http11.Http11Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -70,7 +71,7 @@ public class Connector implements Runnable {
         if (connection == null) {
             return;
         }
-        
+
         executorService.submit(() -> {
             var processor = new Http11Processor(connection);
             processor.run();
@@ -82,6 +83,17 @@ public class Connector implements Runnable {
         try {
             serverSocket.close();
         } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(5, TimeUnit.SECONDS)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
+            Thread.currentThread().interrupt();
             log.error(e.getMessage(), e);
         }
     }

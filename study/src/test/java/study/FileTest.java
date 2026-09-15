beb3642c76ca76bebe5e6ec79fb08429976ff07b
2jaeheon <lchc.dev@gmail.com>
2026-09-15 17:09:27 +0900

    test(study): 파일의 내용 읽어오기
    Signed-off-by: 2jaeheon <lchc.dev@gmail.com>

--- study/src/test/java/study/FileTest.java
+++ study/src/test/java/study/FileTest.java
@@ -2,9 +2,11 @@ package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -37,15 +39,14 @@ class FileTest {
      * 읽어온 파일의 내용을 I/O Stream을 사용해서 사용자에게 전달 해야 한다. File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws IOException {
         final String fileName = "nextstep.txt";
+        URL resource = getClass().getClassLoader().getResource(fileName);
 
-        // todo
-        final Path path = null;
+        final Path path = new File(resource.getPath()).toPath();
+        final List<String> actual = Files.readAllLines(path);
 
-        // todo
-        final List<String> actual = Collections.emptyList();
-
+        assertThat(resource).isNotNull();
         assertThat(actual).containsOnly("nextstep");
     }
 }

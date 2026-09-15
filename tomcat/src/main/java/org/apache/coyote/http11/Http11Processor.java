909d2cab329e91fe19f83d2e517629bf39c05385
bee9827 <112379970+bee9827@users.noreply.github.com>
2026-09-15 18:33:46 +0900

    feat: CSS 지원하기

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -9,6 +9,7 @@ import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
 import java.util.Objects;
+import javax.annotation.Nonnull;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +18,8 @@ public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
     private static final String INDEX_URI = "/index.html";
+    private static final String STYLES_CSS = "/styles.css";
+    private static final String RESOURCES_PREFIX = "static";
 
     private final Socket connection;
 
@@ -37,13 +40,15 @@ public class Http11Processor implements Runnable, Processor {
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
             String line = reader.readLine();
+            String method = line.split(" ")[0];
             String uri = line.split(" ")[1];
 
-            final var responseBody = getResponseBody(uri);
+
+            final var responseBody = getResponseBody(method,uri);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Type: text/" + getExtension(uri) + ";charset=utf-8 ",
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
                     responseBody);
@@ -55,18 +60,37 @@ public class Http11Processor implements Runnable, Processor {
         }
     }
 
-    private String getResponseBody(String uri) {
-        if (uri.equals("/")) {
+    private String getResponseBody(String method, String uri) {
+        if (uri.equals("/") && method.equals("GET")) {
             return "Hello world!";
-        }
-        else if (uri.equals(INDEX_URI)) {
-            final URL resource = getClass().getClassLoader().getResource("static/index.html");
-            try {
-                return new String(Files.readAllBytes(new File(Objects.requireNonNull(resource).getFile()).toPath()));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+        } else if (uri.equals(INDEX_URI) && method.equals("GET")) {
+            return modelToView(INDEX_URI);
+        } else if (uri.equals(STYLES_CSS) && method.equals("GET")) {
+            return modelToView(STYLES_CSS);
         }
         throw new IllegalArgumentException("잘못된 주소입니다.");
+    }
+
+    private String getExtension(String uri) {
+        if (uri.equals("/")) {
+            return "html";
+        }
+        if (uri.endsWith(".html")) {
+            return "html";
+        }
+        if (uri.endsWith(".css")) {
+            return "css";
+        }
+        throw new IllegalArgumentException("확인이 되지 않는 확장자 입니다.");
+    }
+
+    @Nonnull
+    private String modelToView(String uri) {
+        final URL resource = getClass().getClassLoader().getResource(RESOURCES_PREFIX + uri);
+        try {
+            return new String(Files.readAllBytes(new File(Objects.requireNonNull(resource).getFile()).toPath()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

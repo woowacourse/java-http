59fa4c8b9c3fbbf01360f17400f33b291f80caf8
kangrae <kangrae0501@gmail.com>
2026-09-15 22:29:18 +0900

    feat: request line과 header를 읽고 적절한 resource를 반환하는 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,16 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -26,10 +30,38 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+        try (
+                final var inputStream = connection.getInputStream();
+                final var outputStream = connection.getOutputStream();
+                final var bufferedReader = new BufferedReader(new InputStreamReader(inputStream))
+        ) {
+            final String requestLine = bufferedReader.readLine();
+            if (requestLine == null) {
+                return;
+            }
 
-            final var responseBody = "Hello world!";
+            String line = bufferedReader.readLine();
+            while (!"".equals(line)) {
+                if (line == null) {
+                    return;
+                }
+                line = bufferedReader.readLine();
+            }
+
+            final String[] requestComponents = requestLine.split(" ");
+            final String requestUri = requestComponents[1];
+
+            final String responseBody;
+            if ("/".equals(requestUri)) {
+                responseBody = "Hello world!";
+            } else {
+                final String resourceName = "static" + requestUri;
+                final String fileName = Objects.requireNonNull(
+                        getClass().getClassLoader().getResource(resourceName)
+                ).getPath();
+
+                responseBody = Files.readString(Path.of(fileName));
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",

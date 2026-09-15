5993302bc5ee15e3b59dccb6ef731275fcd3e38e
ghkdtjfxkd <syt1128@gmail.com>
2026-09-15 23:33:02 +0900

    feat: 인덱스 페이지 접근 기능 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,17 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,19 +34,41 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            String filePath = parseRequestPathFrom(inputStream);
+            var responseBody = resolveContentOf(filePath);
+            final var response = buildResponseWith(responseBody);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String parseRequestPathFrom(InputStream inputStream) throws IOException {
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        return bufferedReader.readLine().split(" ")[1];
+    }
+
+    private String resolveContentOf(String filePath) throws IOException {
+        URL resource = getResource(filePath);
+        if (!filePath.equals("/") && resource != null) {
+            return Files.readString(new File(resource.getFile()).toPath());
+        }
+        return "Hello world!";
+    }
+
+    private URL getResource(String filename) {
+        String path = "static" + filename;
+        return getClass().getClassLoader().getResource(path);
+    }
+
+    private String buildResponseWith(String responseBody) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
     }
 }

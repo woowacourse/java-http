6495017b45e07db69cd8bbe2a7635ee950b5a4b8
koreaioi <koreaioi@gachon.ac.kr>
2026-09-15 23:03:23 +0900

    test: FileTest 학습 테스트 코드 작성

--- study/src/test/java/study/FileTest.java
+++ study/src/test/java/study/FileTest.java
@@ -3,8 +3,12 @@ package study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,37 +22,55 @@ class FileTest {
 
     /**
      * resource 디렉터리 경로 찾기
-     *
+     * <p>
      * File 객체를 생성하려면 파일의 경로를 알아야 한다.
      * 자바 애플리케이션은 resource 디렉터리에 HTML, CSS 같은 정적 파일을 저장한다.
      * resource 디렉터리의 경로는 어떻게 알아낼 수 있을까?
      */
     @Test
     void resource_디렉터리에_있는_파일의_경로를_찾는다() {
+        // given
         final String fileName = "nextstep.txt";
 
-        // todo
-        final String actual = "";
+        // when
+        final String actual = getClass().getClassLoader().getResource(fileName).getPath();
 
+        // then
         assertThat(actual).endsWith(fileName);
     }
 
     /**
      * 파일 내용 읽기
-     *
+     * <p>
      * 읽어온 파일의 내용을 I/O Stream을 사용해서 사용자에게 전달 해야 한다.
      * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void Files를_사용해_파일의_내용을_읽는다() throws Exception {
+        // given
         final String fileName = "nextstep.txt";
+        Path file = Path.of(getClass().getClassLoader().getResource(fileName).toURI());
 
-        // todo
-        final Path path = null;
+        // when
+        final List<String> actual = Files.readAllLines(file, StandardCharsets.UTF_8);
 
-        // todo
-        final List<String> actual = Collections.emptyList();
+        // then
+        assertThat(actual).containsOnly("nextstep");
+    }
+
+    @Test
+    void File과_Stream를_사용해_파일의_내용을_읽는다() throws Exception {
+        // given
+        final String fileName = "nextstep.txt";
+        final File file = new File(getClass().getClassLoader().getResource(fileName).toURI());
+
+        // File을 기반으로 BufferedReader 스트림 생성
+        final List<String> actual;
+        try (BufferedReader reader = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8))) {
+            actual = reader.lines().toList();
+        }
 
         assertThat(actual).containsOnly("nextstep");
     }
+
 }

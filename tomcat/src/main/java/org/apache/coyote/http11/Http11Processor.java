cf8fafaa52acb526380601d2e93b67a1a2085950
bee9827 <112379970+bee9827@users.noreply.github.com>
2026-09-15 17:48:27 +0900

    feat: GET /index.html 응답하기

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,16 +1,22 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.Objects;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
-
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final String INDEX_URI = "/index.html";
 
     private final Socket connection;
 
@@ -29,7 +35,11 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = reader.readLine();
+            String uri = line.split(" ")[1];
+
+            final var responseBody = getResponseBody(uri);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -43,5 +53,20 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String getResponseBody(String uri) {
+        if (uri.equals("/")) {
+            return "Hello world!";
+        }
+        else if (uri.equals(INDEX_URI)) {
+            final URL resource = getClass().getClassLoader().getResource("static/index.html");
+            try {
+                return new String(Files.readAllBytes(new File(Objects.requireNonNull(resource).getFile()).toPath()));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        throw new IllegalArgumentException("잘못된 주소입니다.");
     }
 }

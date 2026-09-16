cf1a8d9cac1ac3106bc78b665390e3f3dba3c6a5
yItWorks <yitworksdev@gmail.com>
2026-09-16 21:59:48 +0900

    feat: add access to index.html

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,19 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,7 +36,9 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            String path = extractRequestPath(inputStream);
+            URL url = findStaticResource(path);
+            final String responseBody = resolveContentOf(url);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -42,6 +51,35 @@ public class Http11Processor implements Runnable, Processor {
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        }
+    }
+
+    private String extractRequestPath(InputStream inputStream) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        try {
+            String line = br.readLine();
+            Objects.requireNonNull(line);
+
+            return line.split(" ")[1];
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private URL findStaticResource(String path) {
+        return getClass().getClassLoader().getResource("static" + path);
+    }
+
+    private String resolveContentOf(URL fileUrl) {
+        if (fileUrl == null || fileUrl.getPath().endsWith("/")) {
+            return "Hello world!";
+        }
+
+        try {
+            Path path = new File(fileUrl.getFile()).toPath();
+            return Files.readString(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }

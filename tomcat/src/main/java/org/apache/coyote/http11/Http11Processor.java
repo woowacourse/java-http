b62d498ff6a2aa866be4af9a7691e6742fcade0f
nn98 <jkllhgb@gmail.com>
2026-09-16 18:46:20 +0900

    feat: /login 응답 및 콘솔 출력

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,5 +1,7 @@
 package org.apache.coyote.http11;
 
+import static com.techcourse.db.InMemoryUserRepository.findByAccount;
+
 import com.techcourse.exception.UncheckedServletException;
 import java.io.BufferedReader;
 import java.io.File;
@@ -37,15 +39,37 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
+            String html = "html";
+            String css = "css";
+
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
             String firstLine = reader.readLine();
             StringTokenizer streamTokenizer = new StringTokenizer(firstLine);
             String method = streamTokenizer.nextToken();
             log.info("method: {}", method);
-            String path = streamTokenizer.nextToken();
+            String query = streamTokenizer.nextToken();
+            String[] uri = query.split("\\?");
+            String path = uri[0];
             log.info("path: {}", path);
 
-            String html = "html";
+            if (uri.length > 1 && path.equals("/login")) {
+                String[] queryParams = uri[1].split("&");
+                for (String param : queryParams) {
+                    log.info("param: {}", param);
+                    String name =  param.split("=")[0];
+                    String value =  param.split("=")[1];
+                    if (name.equals("account")) {
+                        log.info(findByAccount(value).toString());
+                        final URL resource = getClass().getClassLoader().getResource("static" + path + "." + html);
+                        final var responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+                        response(responseBody, outputStream, html);
+                        return;
+                    }
+                }
+            }
+
+            log.info("path: {}", path);
+
             if (path.equals("/")) {
                 log.info("path is empty");
                 final var responseBody = "Hello world!";
@@ -54,7 +78,6 @@ public class Http11Processor implements Runnable, Processor {
             }
 
             if (path.startsWith("/css")) {
-                String css = "css";
                 log.info(css);
                 final URL resource = getClass().getClassLoader().getResource("static" + path);
                 final var responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));

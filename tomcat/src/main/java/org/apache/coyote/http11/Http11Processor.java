1febe6213e860b5282b85e4d63c0af55faf9f739
Wonel <jwjjwj1960@naver.com>
2026-09-16 17:33:34 +0900

    [1단계 - HTTP 서버 구현하기] 워넬(전원준) 미션 제출합니다. (#1114)
    * test: 파일 학습 테스트 작성
    
    * test: 입출력 스트림 학습 테스트 진행
    
    * feat: GET /index.html 응답하기 추가
    
    - 루트일 때 Hello World!를 출력하는 respond와 다른 값이 들어왔을 때 resource에서 static 경로를 읽어와서 반환할 수 있도록 분리
    
    * feat: css 불러오기 기능 추가
    
    - respondCssHeader로 .css로 끝날 때 정상 반환할 수 있도록 기능 추가
    
    * feat: queryString 파싱 기능 추가
    
    - queryString 파싱으로 회원 계정 및 비밀번호 구하기
    - 파싱으로 얻은 account, password로 비밀번호 일치 불일치 로그 확인
    
    * refactor: 중복 respond 제거 및 body 초기화
    
    - respondCss와 respondStaticResource 내용이 비슷해서 중복사용된 부분 통합
    - resourceStream이 null일 때 발생하는 문제를 responseBody 초기화 후 null이 아닐 때 입력하는 방식으로 변경
    - 쿼리 분리 중 null 값에 대한 접근 문제 해결

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,16 +1,24 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
-
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final int ELEMENT_SIZE = 2;
 
     private final Socket connection;
 
@@ -26,9 +34,52 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+        try (InputStream inputStream = connection.getInputStream();
+            OutputStream outputStream = connection.getOutputStream();
+             final var reader = new BufferedReader(
+                     new InputStreamReader(inputStream, StandardCharsets.UTF_8));) {
 
+            final var requestLine = reader.readLine();
+            if (requestLine == null) {
+                return;
+            }
+
+            final String[] tokens = requestLine.split(" ");
+            final String requestTarget = tokens[1];
+
+            int index = requestTarget.indexOf("?");
+            String path = requestTarget;
+            String queryString = "";
+
+            if (index != -1) {
+                path = requestTarget.substring(0, index);
+                queryString = requestTarget.substring(index + 1);
+            }
+
+            log.debug("request line: {}", requestLine);
+
+            String line;
+            while ((line = reader.readLine()) != null && !line.equals("")) {
+                log.debug("header : {}", line);
+            }
+
+            if (path.equals("/")) {
+                respondHelloWorld(outputStream);
+                return;
+            }
+
+            if (path.equals("/login")) {
+                handleLogin(queryString, outputStream);
+                return;
+            }
+            respondStaticResource(path, outputStream);
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    private void respondHelloWorld(OutputStream outputStream) {
+        try {
             final var responseBody = "Hello world!";
 
             final var response = String.join("\r\n",
@@ -43,5 +94,78 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private void respondStaticResource(String requestTarget, OutputStream outputStream) {
+        try (final var resourceStream = getClass()
+                .getClassLoader()
+                .getResourceAsStream("static" + requestTarget)) {
+
+            byte[] responseBody = new byte[0];
+            if (resourceStream != null) {
+                responseBody = resourceStream.readAllBytes();
+            }
+
+            final var response = String.join("\r\n",
+                    "HTTP/1.1 200 OK ",
+                    "Content-Type: " + contentType(requestTarget),
+                    "Content-Length: " + responseBody.length + " ",
+                    "",
+                    new String(responseBody));
+
+            outputStream.write(response.getBytes());
+            outputStream.flush();
+        } catch (IOException | UncheckedServletException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    private void handleLogin(String queryString, OutputStream outputStream) {
+        Map<String, String> params = parseQueryString(queryString);
+        String account = params.get("account");
+        String password = params.get("password");
+
+        if (account == null || password == null) {
+            log.debug("로그인 파라미터가 부족합니다.");
+            return;
+        }
+
+        final boolean authenticated = InMemoryUserRepository
+                .findByAccount(account)
+                .filter(user -> user.checkPassword(password))
+                .isPresent();
+
+        if (authenticated) {
+            log.debug("로그인 성공: {}", account);
+        } else {
+            log.debug("비밀번호 불일치: {}", account);
+        }
+    }
+
+    private Map<String, String> parseQueryString(String queryString) {
+        String[] queryList = queryString.split("&");
+        Map<String, String> params = new HashMap<>();
+        for (String str : queryList) {
+            String[] query = str.split("=");
+            if (query.length < ELEMENT_SIZE) {
+                continue;
+            }
+            params.put(query[0], query[1]);
+        }
+
+        return params;
+    }
+
+    private String contentType(final String requestTarget) {
+        if (requestTarget.endsWith(".css")) {
+            return "text/css;charset=utf-8 ";
+        }
+        if (requestTarget.endsWith(".js")) {
+            return "application/javascript;charset=utf-8 ";
+        }
+        if (requestTarget.endsWith(".svg")) {
+            return "image/svg+xml ";
+        }
+        return "text/html;charset=utf-8 ";
     }
 }

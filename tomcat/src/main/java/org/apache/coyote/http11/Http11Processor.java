36aa73e7979bfb32ff14ae58463785f54661f679
전원준 <jwjjwj1960@naver.com>
2026-09-16 09:30:45 +0900

    feat: GET /index.html 응답하기 추가
    - 루트일 때 Hello World!를 출력하는 respond와 다른 값이 들어왔을 때 resource에서 static 경로를 읽어와서 반환할 수 있도록 분리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,19 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Objects;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -26,9 +33,38 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+        try (InputStream inputStream = connection.getInputStream();
+            OutputStream outputStream = connection.getOutputStream();
+             final var reader = new BufferedReader(
+                     new InputStreamReader(inputStream, StandardCharsets.UTF_8));) {
 
+            final var requestLine = reader.readLine();
+            if (requestLine == null) {
+                return;
+            }
+            final String[] tokens = requestLine.split(" ");
+            final String requestTarget = tokens[1];
+
+            log.debug("request line: {}", requestLine);
+
+            String line;
+            while ((line = reader.readLine()) != null && !line.equals("")) {
+                line = reader.readLine();
+                log.debug("header : {}", line);
+            }
+
+            if (requestTarget.equals("/")) {
+                respondHelloWorld(outputStream);
+                return ;
+            }
+            respondStaticResource(requestTarget, outputStream);
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    public void respondHelloWorld(OutputStream outputStream) {
+        try {
             final var responseBody = "Hello world!";
 
             final var response = String.join("\r\n",
@@ -37,6 +73,28 @@ public class Http11Processor implements Runnable, Processor {
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
                     responseBody);
+
+            outputStream.write(response.getBytes());
+            outputStream.flush();
+        } catch (IOException | UncheckedServletException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    public void respondStaticResource(String requestTarget, OutputStream outputStream) {
+        try {
+            final var resourceStream = getClass()
+                    .getClassLoader()
+                    .getResourceAsStream("static" + requestTarget);
+
+            final var responseBody = Objects.requireNonNull(resourceStream).readAllBytes();
+
+            final var response = String.join("\r\n",
+                    "HTTP/1.1 200 OK ",
+                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Length: " + responseBody.length + " ",
+                    "",
+                    new String(responseBody));
 
             outputStream.write(response.getBytes());
             outputStream.flush();

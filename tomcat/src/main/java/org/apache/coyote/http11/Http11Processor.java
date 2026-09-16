e8606043c3710bea8d21a5d3a124702e32992332
Jihyun3478 <leejihyun3478@gmail.com>
2026-09-17 00:12:32 +0900

    feat: 쿼리 스트링 파싱 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,8 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,7 +11,9 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,8 +24,10 @@ public class Http11Processor implements Runnable, Processor {
     private static final Map<String, String> CONTENT_TYPE = Map.of("html", "text/html", "css", "text/css", "js", "application/javascript");
     private static final String STATIC_ROOT = "static";
     private static final String DEFAULT_REQUEST = "/";
-    private static final String INDEX_REQUEST = "/index.html";
-    private static final String NOT_FOUND_REQUEST = "/404.html";
+    private static final String INDEX_PAGE = "/index.html";
+    private static final String LOGIN_REQUEST = "/login";
+    private static final String LOGIN_PAGE = "/login.html";
+    private static final String NOT_FOUND_PAGE = "/404.html";
 
     private final Socket connection;
 
@@ -44,16 +50,29 @@ public class Http11Processor implements Runnable, Processor {
 
             String requestUri = reader.readLine().split(" ")[1];
 
+            String[] uriParts = requestUri.split("\\?");
+            requestUri = uriParts[0];
+
+            String queryString = "";
+            if (uriParts.length > 1) {
+                queryString = uriParts[1];
+            }
+            Map<String, String> queryParams = parseQueryString(queryString);
+
             String response = "";
             if (requestUri.equals(DEFAULT_REQUEST)) {
-                requestUri = INDEX_REQUEST;
+                requestUri = INDEX_PAGE;
+            }
+            if (requestUri.equals(LOGIN_REQUEST)) {
+                requestUri = LOGIN_PAGE;
+                login(queryParams);
             }
 
             URL url = getClass().getClassLoader().getResource(STATIC_ROOT + requestUri);
             if (url == null) {
-                url = getClass().getClassLoader().getResource(STATIC_ROOT + NOT_FOUND_REQUEST);
+                url = getClass().getClassLoader().getResource(STATIC_ROOT + NOT_FOUND_PAGE);
                 String responseBody = new String(Files.readAllBytes(Paths.get(url.toURI())));
-                String extension = getExtension(NOT_FOUND_REQUEST);
+                String extension = getExtension(NOT_FOUND_PAGE);
 
                 response = "HTTP/1.1 404 Not Found \r\n" +
                         "Content-Type: " + CONTENT_TYPE.get(extension) + ";charset=utf-8 \r\n" +
@@ -73,6 +92,35 @@ public class Http11Processor implements Runnable, Processor {
             log.error(e.getMessage(), e);
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private Map<String, String> parseQueryString(String queryString) {
+        Map<String, String> queryParams = new HashMap<>();
+        if (queryString.isEmpty()) {
+            return queryParams;
+        }
+
+        for (String param : queryString.split("&")) {
+            String[] keyAndValue = param.split("=", 2);
+            if (keyAndValue.length == 2) {
+                queryParams.put(keyAndValue[0], keyAndValue[1]);
+            }
+        }
+
+        return queryParams;
+    }
+
+    private void login(Map<String, String> queryParams) {
+        String account = queryParams.get("account");
+        String password = queryParams.get("password");
+        if (account == null || password == null) {
+            return;
+        }
+
+        Optional<User> user = InMemoryUserRepository.findByAccount(account);
+        if (user.isPresent() && user.get().checkPassword(password)) {
+            log.info("회원 조회 결과: user={}", user.get());
         }
     }
 

d334cc9148ede7cb13a5ff5d3851c4fe7d90cb71
alstj2384 <ass91160@naver.com>
2026-09-16 20:51:43 +0900

    refactor: 공통 메서드 추출

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -3,22 +3,19 @@ package org.apache.coyote.http11;
 import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
 import com.techcourse.model.User;
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -39,168 +36,131 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             // step 1-1 connection으로 받은 inputStream은 byteStream.  GET /index.html...이 바이트 형태로 저장된 상태임.
-             // step 1-1 URL이 /index.html 인지 확인해야 하므로, 바이트를 문자로 변환해야 한다.
              final var inputStreamReader = new InputStreamReader(inputStream);
-
-             // step 1-1 버퍼링해서 받기.. (근데 아직 왜 써야하는지 와닿지가 않음)
              final var bufferedReader = new BufferedReader(inputStreamReader);
              final var outputStream = connection.getOutputStream()) {
 
-            // step 1-1 모든 형태를 파싱하는 건 아직 어려우니, 첫 라인을 공백 단위로 split한다.
-            String firstHeaderLine = bufferedReader.readLine();
-            String[] tokens = firstHeaderLine.split(" ");
-            log.info("요청 헤더: {}, 요청 URL: {}", firstHeaderLine, tokens[1]);
+            String[] startLineTokens = bufferedReader.readLine().split(" ");
+            String httpUrl = startLineTokens[1];
 
-            // step 1-1 1번 인덱스 요소가 index.html과 같으면, resources/static/index.html을 읽어서 HTTP Response 포맷에 맞게 바꿔서 내려준다.
-            if(tokens[1].startsWith("/index.html")) {
-                // 빌드 기준 산출물 위치가 달라지므로, 상대 경로를 사용해야 한다.
-                URL url = getClass().getClassLoader().getResource("static/index.html");
-                byte[] contents = Files.readAllBytes(new File(url.getFile()).toPath());
+            Map<String, String> headers = parseHeaders(bufferedReader);
 
-                final var responseBody = new String(contents);
-
-                final var response = String.join("\r\n",
-                        "HTTP/1.1 200 OK ",
-                        "Content-Type: text/html;charset=utf-8 ",
-                        "Content-Length: " + responseBody.getBytes().length + " ",
-                        "",
-                        responseBody);
+            if (httpUrl.startsWith("/index.html")) {
+                final String body = readFile("static/index.html");
+                final var response = createResponse(body, headers);
 
                 outputStream.write(response.getBytes());
                 outputStream.flush();
                 return;
             }
 
-            if(tokens[1].startsWith("/login")) {
-                // 빌드 기준 산출물 위치가 달라지므로, 상대 경로를 사용해야 한다.
-                URL url = getClass().getClassLoader().getResource("static/login.html");
-                byte[] contents = Files.readAllBytes(new File(url.getFile()).toPath());
-
-                final var responseBody = new String(contents);
-
-                final var response = String.join("\r\n",
-                        "HTTP/1.1 200 OK ",
-                        "Content-Type: text/html;charset=utf-8 ",
-                        "Content-Length: " + responseBody.getBytes().length + " ",
-                        "",
-                        responseBody);
+            if (httpUrl.startsWith("/login")) {
+                final String body = readFile("static/login.html");
+                final var response = createResponse(body, headers);
 
                 outputStream.write(response.getBytes());
                 outputStream.flush();
 
+                Map<String, String> queryParams = parseQueryParams(startLineTokens);
 
-                // 1-3 계정 찾아서 출력하기
-                // 쿼리 파라미터 확인하기
-                int questionMarkIndex = tokens[1].lastIndexOf("?");
-                String queries = tokens[1].substring(questionMarkIndex+1);
-                String[] query = queries.split("&");
-                Map<String, String> queryMap = new HashMap<>();
-                for (String s : query) {
-                    String[] keyToken = s.split("=");
-                    queryMap.put(keyToken[0], keyToken[1]);
-                }
-
-                String account = queryMap.get("account");
-                String password = queryMap.get("password");
+                String account = queryParams.get("account");
+                String password = queryParams.get("password");
 
                 Optional<User> user = InMemoryUserRepository.findByAccount(account);
-                if(user.isPresent()){
+                if (user.isPresent()) {
                     User user1 = user.get();
-                    if(user1.checkPassword(password)){
+                    if (user1.checkPassword(password)) {
                         log.info("{}", user1);
                     }
                 }
                 return;
             }
 
-            // step 1-2 경로가 /css/style.css인 경우, css 파일도 내려준다.
-            if(tokens[1].startsWith("/css/styles.css")){
-                URL url = getClass().getClassLoader().getResource("static/css/styles.css");
-                byte[] contents = Files.readAllBytes(new File(url.getFile()).toPath());
-
-                final var responseBody = new String(contents);
-
-                final var response = String.join("\r\n",
-                        "HTTP/1.1 200 OK ",
-                        "Content-Type: text/css;charset=utf-8 ",
-                        "Content-Length: " + responseBody.getBytes().length + " ",
-                        "",
-                        responseBody);
+            if (httpUrl.startsWith("/css/styles.css")) {
+                final String body = readFile("static/css/styles.css");
+                final var response = createResponse(body, headers);
 
                 outputStream.write(response.getBytes());
                 outputStream.flush();
                 return;
             }
 
-            // step 1-2 경로가 /css/style.css인 경우, css 파일도 내려준다.
-            if(tokens[1].startsWith("/assets/chart-bar.js")){
-                URL url = getClass().getClassLoader().getResource("static/assets/chart-bar.js");
-                byte[] contents = Files.readAllBytes(new File(url.getFile()).toPath());
-
-                final var responseBody = new String(contents);
-
-                final var response = String.join("\r\n",
-                        "HTTP/1.1 200 OK ",
-                        "Content-Type: text/js;charset=utf-8 ",
-                        "Content-Length: " + responseBody.getBytes().length + " ",
-                        "",
-                        responseBody);
-
-                outputStream.write(response.getBytes());
-                outputStream.flush();
-                return;
-            }
-            // step 1-2 경로가 /css/style.css인 경우, css 파일도 내려준다.
-            if(tokens[1].startsWith("/js/scripts.js")){
-                URL url = getClass().getClassLoader().getResource("static/js/scripts.js");
-                byte[] contents = Files.readAllBytes(new File(url.getFile()).toPath());
-
-                final var responseBody = new String(contents);
-
-                final var response = String.join("\r\n",
-                        "HTTP/1.1 200 OK ",
-                        "Content-Type: text/js;charset=utf-8 ",
-                        "Content-Length: " + responseBody.getBytes().length + " ",
-                        "",
-                        responseBody);
-
-                outputStream.write(response.getBytes());
-                outputStream.flush();
-                return;
-            }
-            // step 1-2 경로가 /css/style.css인 경우, css 파일도 내려준다.
-            if(tokens[1].startsWith("/assets/chart-pie.js")){
-                URL url = getClass().getClassLoader().getResource("static/assets/chart-pie.js");
-                byte[] contents = Files.readAllBytes(new File(url.getFile()).toPath());
-
-                final var responseBody = new String(contents);
-
-                final var response = String.join("\r\n",
-                        "HTTP/1.1 200 OK ",
-                        "Content-Type: text/js;charset=utf-8 ",
-                        "Content-Length: " + responseBody.getBytes().length + " ",
-                        "",
-                        responseBody);
+            if (httpUrl.startsWith("/assets/chart-bar.js")) {
+                final String body = readFile("static/assets/chart-bar.js");
+                final var response = createResponse(body, headers);
 
                 outputStream.write(response.getBytes());
                 outputStream.flush();
                 return;
             }
 
-            final var responseBody = "Hello world!";
+            if (httpUrl.startsWith("/js/scripts.js")) {
+                final String body = readFile("static/js/scripts.js");
+                final var response = createResponse(body, headers);
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+                return;
+            }
 
+            if (httpUrl.startsWith("/assets/chart-pie.js")) {
+                final String body = readFile("static/assets/chart-pie.js");
+                final var response = createResponse(body, headers);
+
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+                return;
+            }
+
+            final var response = createResponse("Hello world!", headers);
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private Map<String, String> parseHeaders(BufferedReader bufferedReader) throws IOException {
+        // 각 요청을 처리하는 스레드는 개별적인 http11Processor 인스턴스를 가지므로, 동시성을 보장하는 자료구조를 사용하지 않았습니다.
+        Map<String, String> headers = new HashMap<>();
+        String nextLine = null;
+        while ((nextLine = bufferedReader.readLine()) != null) {
+            // Host에 콜론(:)이 포함될 수 있으므로 가장 먼저 만나는 콜론을 기준으로 나눕니다.
+            int colonIndex = nextLine.indexOf(':');
+            String key = nextLine.substring(0, colonIndex);
+            String value = nextLine.substring(colonIndex + 1);
+
+            headers.put(key, value);
+        }
+        return headers;
+    }
+
+    private String createResponse(String responseBody, Map<String, String> headers) throws IOException {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: " + headers.get("Content-Type") + " ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+    }
+
+    private String readFile(String path) throws IOException {
+        URL url = getClass().getClassLoader().getResource(path);
+        return new String(Files.readAllBytes(new File(url.getFile()).toPath()));
+    }
+
+    private Map<String, String> parseQueryParams(String[] queryParamLine) {
+        int questionMarkIndex = queryParamLine[1].lastIndexOf("?");
+
+        String queries = queryParamLine[1].substring(questionMarkIndex + 1);
+        String[] query = queries.split("&");
+
+        Map<String, String> queryMap = new HashMap<>();
+        for (String s : query) {
+            String[] keyToken = s.split("=");
+            queryMap.put(keyToken[0], keyToken[1]);
+        }
+
+        return queryMap;
     }
 }

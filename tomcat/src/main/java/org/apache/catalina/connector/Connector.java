6325707f26f70df3871cf84e7bfc4db7e0254753
woohyeon <woohyeon6885@gmail.com>
2026-09-16 21:29:19 +0900

    refactor: 서버 종료 방식을 shutdown hook으로 변경

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -1,13 +1,12 @@
 package org.apache.catalina.connector;
 
-import org.apache.coyote.http11.Http11Processor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import org.apache.coyote.http11.Http11Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Connector implements Runnable {
 
@@ -17,7 +16,7 @@ public class Connector implements Runnable {
     private static final int DEFAULT_ACCEPT_COUNT = 100;
 
     private final ServerSocket serverSocket;
-    private boolean stopped;
+    private volatile boolean stopped;
 
     public Connector() {
         this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT);
@@ -32,56 +31,16 @@ public class Connector implements Runnable {
         try {
             final int checkedPort = checkPort(port);
             final int checkedAcceptCount = checkAcceptCount(acceptCount);
+
             return new ServerSocket(checkedPort, checkedAcceptCount);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
     }
 
-    public void start() {
-        var thread = new Thread(this);
-        thread.setDaemon(true);
-        thread.start();
-        stopped = false;
-        log.info("Web Application Server started {} port.", serverSocket.getLocalPort());
-    }
-
-    @Override
-    public void run() {
-        // 클라이언트가 연결될때까지 대기한다.
-        while (!stopped) {
-            connect();
-        }
-    }
-
-    private void connect() {
-        try {
-            process(serverSocket.accept());
-        } catch (IOException e) {
-            log.error(e.getMessage(), e);
-        }
-    }
-
-    private void process(final Socket connection) {
-        if (connection == null) {
-            return;
-        }
-        var processor = new Http11Processor(connection);
-        new Thread(processor).start();
-    }
-
-    public void stop() {
-        stopped = true;
-        try {
-            serverSocket.close();
-        } catch (IOException e) {
-            log.error(e.getMessage(), e);
-        }
-    }
-
     private int checkPort(final int port) {
-        final var MIN_PORT = 1;
-        final var MAX_PORT = 65535;
+        final int MIN_PORT = 1;
+        final int MAX_PORT = 65535;
 
         if (port < MIN_PORT || MAX_PORT < port) {
             return DEFAULT_PORT;
@@ -91,5 +50,51 @@ public class Connector implements Runnable {
 
     private int checkAcceptCount(final int acceptCount) {
         return Math.max(acceptCount, DEFAULT_ACCEPT_COUNT);
+    }
+
+    public void startListening() {
+        Thread thread = new Thread(this);
+        thread.start();
+
+        stopped = false;
+
+        log.info("Web Application Server started {} port.", serverSocket.getLocalPort());
+    }
+
+    @Override
+    public void run() {
+        while (!stopped) {
+            acceptConnection();
+        }
+    }
+
+    private void acceptConnection() {
+        try {
+            Socket connection = serverSocket.accept();
+            dispatch(connection);
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    private void dispatch(final Socket connection) {
+        if (connection == null) {
+            return;
+        }
+
+        Http11Processor processor = new Http11Processor(connection);
+
+        Thread thread = new Thread(processor);
+        thread.start();
+    }
+
+    public void stopListening() {
+        stopped = true;
+
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
     }
 }
--- tomcat/src/main/java/org/apache/catalina/startup/Tomcat.java
+++ tomcat/src/main/java/org/apache/catalina/startup/Tomcat.java
@@ -4,24 +4,22 @@ import org.apache.catalina.connector.Connector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 
 public class Tomcat {
 
     private static final Logger log = LoggerFactory.getLogger(Tomcat.class);
 
     public void start() {
-        var connector = new Connector();
-        connector.start();
+        Connector connector = new Connector();
+        connector.startListening();
 
-        try {
-            // make the application wait until we press any key.
-            System.in.read();
-        } catch (IOException e) {
-            log.error(e.getMessage(), e);
-        } finally {
+        registerShutdownHook(connector);
+    }
+
+    private void registerShutdownHook(Connector connector) {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             log.info("web server stop.");
-            connector.stop();
-        }
+            connector.stopListening();
+        }));
     }
 }

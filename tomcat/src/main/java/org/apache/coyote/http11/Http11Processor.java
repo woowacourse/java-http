1f1beace8c3b1aea98c9e7ad6a66a0478fdaa07f
JunHyung1206 <pjh120610@naver.com>
2026-09-16 11:59:02 +0900

    feat: index.html 응답하기

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,8 +5,16 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,7 +37,18 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final var reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            String line = reader.readLine();
+            if (line == null) {
+                return;
+            }
+            var responseBody = "Hello world!";
+
+            String[] tokens = line.split(" ");
+            if(!tokens[1].equals("/")) {
+                final var resourceFile = getResourceFile("static" +tokens[1]);
+                responseBody = Files.readString(resourceFile.toPath(), StandardCharsets.UTF_8);
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -42,6 +61,16 @@ public class Http11Processor implements Runnable, Processor {
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        }
+    }
+
+    private File getResourceFile(String fileName) {
+        final URL resource = getClass().getClassLoader().getResource(fileName);
+
+        try {
+            return Path.of(resource.toURI()).toFile();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
         }
     }
 }

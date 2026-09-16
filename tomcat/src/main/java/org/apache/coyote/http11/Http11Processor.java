bad4884d07c7ca554b58afa1fe2c9926466348ad
snowshower <wonsb25@gmail.com>
2026-09-16 22:47:56 +0900

    feat: GET /index.html 응답하기 구현
    Signed-off-by: snowshower <wonsb25@gmail.com>

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,21 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,18 +38,40 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            final BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            final String line = bufferedReader.readLine();
+            if (line == null) {
+                return;
+            }
+            final String[] lines = line.split(" ");
+            final String uri = lines[1];
+
+            var responseBody = "Hello world!";
+            int contentLength = responseBody.getBytes().length;
+
+            if (uri.equals("/index.html")) {
+                URL resourceUrl = getClass().getClassLoader().getResource("static/index.html");
+                if (resourceUrl == null) {
+                    return;
+                }
+                URI resourceUri = resourceUrl.toURI();
+                Path path = Paths.get(resourceUri);
+                byte[] fileBytes = Files.readAllBytes(path);
+                responseBody = new String(fileBytes, StandardCharsets.UTF_8);
+                contentLength = fileBytes.length;
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "Content-Length: " + contentLength + " ",
                     "",
                     responseBody);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
         }
     }

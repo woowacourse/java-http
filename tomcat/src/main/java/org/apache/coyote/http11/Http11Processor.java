7c2714f582559748abf503a91ec79aef9c101943
yItWorks <yitworksdev@gmail.com>
2026-09-16 22:34:05 +0900

    feat: add access to another content type

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -17,6 +17,7 @@ import org.slf4j.LoggerFactory;
 
 public class Http11Processor implements Runnable, Processor {
 
+    private static final String STATIC_ROOT = "static";
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
 
     private final Socket connection;
@@ -38,11 +39,12 @@ public class Http11Processor implements Runnable, Processor {
 
             String path = extractRequestPath(inputStream);
             URL url = findStaticResource(path);
+            String contentType = findContentType(path);
             final String responseBody = resolveContentOf(url);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Type: " + contentType + ";charset=utf-8 ",
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
                     responseBody);
@@ -67,7 +69,23 @@ public class Http11Processor implements Runnable, Processor {
     }
 
     private URL findStaticResource(String path) {
-        return getClass().getClassLoader().getResource("static" + path);
+        return getClass().getClassLoader().getResource(STATIC_ROOT + path);
+    }
+
+    private String findContentType(String path) {
+        if (path.endsWith(".html") || path.endsWith("/")) {
+            return "text/html";
+        }
+
+        if (path.endsWith(".css")) {
+            return "text/css";
+        }
+
+        if (path.endsWith(".js")) {
+            return "text/javascript";
+        }
+
+        throw new RuntimeException("알 수 없는 Content-Type입니다.");
     }
 
     private String resolveContentOf(URL fileUrl) {

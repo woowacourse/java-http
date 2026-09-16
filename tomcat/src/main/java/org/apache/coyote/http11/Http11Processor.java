36ca05584ad66fdde4fd8484b9c789cdf219abc7
kdongsu5509 <kod66170@gmail.com>
2026-09-16 16:54:29 +0900

    feat : GET /index.html 응답하기

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,19 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,20 +35,44 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
-
-            final var responseBody = "Hello world!";
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            String requestEndPoint = parseRequestEndPointFromInputStream(inputStream);
+            var responseBody = consistProperBodyContents(requestEndPoint);
+            final var response = consistResponseWithBodyAndHeader(responseBody);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String consistResponseWithBodyAndHeader(String responseBody) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+    }
+
+    private String consistProperBodyContents(String requestEndPoint) throws URISyntaxException, IOException {
+        StringBuilder sb = new StringBuilder();
+
+        if (!requestEndPoint.equals("/")) {
+            String fileName = requestEndPoint.replaceFirst("/", "");
+
+            URL resource = getClass().getClassLoader().getResource("static/" + fileName);
+            Path path = Path.of(resource.toURI());
+
+            return sb.append(Files.readString(path)).toString();
+        }
+
+        return "Hello world!";
+    }
+
+    private String parseRequestEndPointFromInputStream(InputStream inputStream) {
+        Stream<String> requestInfo = new BufferedReader(new InputStreamReader(inputStream)).lines();
+        String firstLineOfHttpRequest = requestInfo.filter(x -> x.contains("HTTP")).findAny().get();
+        return firstLineOfHttpRequest.split(" ")[1];
     }
 }

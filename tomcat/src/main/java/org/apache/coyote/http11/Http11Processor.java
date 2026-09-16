ffc55d260cc591d778231d04cca98575e75c238c
Jihyun3478 <leejihyun3478@gmail.com>
2026-09-16 23:37:44 +0900

    feat: css 지원 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -2,24 +2,26 @@ package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Socket;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Map;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
-
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
-    private static final String defaultRequest = "/";
-    private static final String indexRequest = "/index.html";
+    private static final Map<String, String> CONTENT_TYPE = Map.of("html", "text/html", "css", "text/css", "js", "application/javascript");
+    private static final String STATIC_ROOT = "static";
+    private static final String DEFAULT_REQUEST = "/";
+    private static final String INDEX_REQUEST = "/index.html";
+    private static final String NOT_FOUND_REQUEST = "/404.html";
 
     private final Socket connection;
 
@@ -43,24 +45,26 @@ public class Http11Processor implements Runnable, Processor {
             String requestUri = reader.readLine().split(" ")[1];
 
             String response = "";
-            if (requestUri.equals(defaultRequest)) {
-                final var responseBody = "Hello world!";
+            if (requestUri.equals(DEFAULT_REQUEST)) {
+                requestUri = INDEX_REQUEST;
+            }
 
-                response = String.join("\r\n",
-                        "HTTP/1.1 200 OK ",
-                        "Content-Type: text/html;charset=utf-8 ",
-                        "Content-Length: " + responseBody.getBytes().length + " ",
-                        "",
-                        responseBody);
-                
-            } else if (requestUri.equals(indexRequest)) {
-                URL url = getClass().getClassLoader().getResource("static/index.html");
+            URL url = getClass().getClassLoader().getResource(STATIC_ROOT + requestUri);
+            if (url == null) {
+                url = getClass().getClassLoader().getResource(STATIC_ROOT + NOT_FOUND_REQUEST);
+                String responseBody = new String(Files.readAllBytes(Paths.get(url.toURI())));
+                String extension = getExtension(NOT_FOUND_REQUEST);
+
+                response = "HTTP/1.1 404 Not Found \r\n" +
+                        "Content-Type: " + CONTENT_TYPE.get(extension) + ";charset=utf-8 \r\n" +
+                        "Content-Length: " + getContentLength(responseBody) + " \r\n\r\n" + responseBody;
+            } else {
+                String responseBody = new String(Files.readAllBytes(Paths.get(url.toURI())));
+                String extension = getExtension(requestUri);
 
                 response = "HTTP/1.1 200 OK \r\n" +
-                        "Content-Type: text/html;charset=utf-8 \r\n" +
-                        "Content-Length: 5564 \r\n" +
-                        "\r\n"+
-                        new String(Files.readAllBytes(Paths.get(url.toURI())));
+                        "Content-Type: " + CONTENT_TYPE.get(extension) + ";charset=utf-8 \r\n" +
+                        "Content-Length: " + getContentLength(responseBody) + " \r\n\r\n" + responseBody;
             }
 
             outputStream.write(response.getBytes());
@@ -70,5 +74,14 @@ public class Http11Processor implements Runnable, Processor {
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private String getExtension(String requestUri) {
+        int lastIndex = requestUri.lastIndexOf(".");
+        return requestUri.substring(lastIndex + 1);
+    }
+
+    private int getContentLength(String responseBody) {
+        return responseBody.getBytes().length;
     }
 }

d2df6ecab56753d4d89dac2752cbaa8f06b12f14
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2026-09-16 17:52:20 +0900

    refactor: HTTP 요청 처리 메서드 분리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -8,10 +8,13 @@ import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -34,82 +37,117 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var requestLine = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8)).readLine();
-            var requestUri = "/";
-            var responseBody = "Hello world!";
-            var contentType = "text/html;charset=utf-8";
+            final var requestUri = getRequestUri(inputStream);
+            final var requestPath = getRequestPath(requestUri);
+            final var parameters = parseQueryString(requestUri);
+            authenticate(requestPath, parameters);
 
-            if (requestLine != null) {
-                requestUri = requestLine.split(" ")[1];
-            }
-
-            var requestPath = requestUri;
-            var queryString = "";
-            final var queryStringIndex = requestUri.indexOf("?");
-
-            if (queryStringIndex >= 0) {
-                requestPath = requestUri.substring(0, queryStringIndex);
-                queryString = requestUri.substring(queryStringIndex + 1);
-            }
-
-            var resourcePath = "";
-            if ("/index.html".equals(requestPath)
-                    || "/css/styles.css".equals(requestPath)
-                    || requestPath.startsWith("/js/")
-                    || requestPath.startsWith("/assets/")) {
-                resourcePath = "static" + requestPath;
-            }
-            if ("/login".equals(requestPath)) {
-                resourcePath = "static/login.html";
-            }
-
-            if (!resourcePath.isEmpty()) {
-                try (final var resource = getClass().getClassLoader().getResourceAsStream(resourcePath)) {
-                    responseBody = new String(resource.readAllBytes(), StandardCharsets.UTF_8);
-                }
-            }
-
-            if (requestPath.endsWith(".css")) {
-                contentType = "text/css;charset=utf-8";
-            }
-            if (requestPath.endsWith(".js")) {
-                contentType = "application/javascript;charset=utf-8";
-            }
-
-            final var parameters = new HashMap<String, String>();
-            if (!queryString.isEmpty()) {
-                final var queryParameters = queryString.split("&");
-                for (final var queryParameter : queryParameters) {
-                    final var keyValue = queryParameter.split("=", 2);
-                    if (keyValue.length == 2) {
-                        parameters.put(keyValue[0], keyValue[1]);
-                    }
-                }
-            }
-
-            final var account = parameters.get("account");
-            final var password = parameters.get("password");
-            if ("/login".equals(requestPath) && account != null && password != null) {
-                final var foundUser = InMemoryUserRepository.findByAccount(account);
-                if (foundUser.isPresent()) {
-                    final var user = foundUser.get();
-                    if (user.checkPassword(password)) {
-                        log.info("login user: {}", user);
-                    }
-                }
-            }
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: " + contentType + " ",
-                    "Content-Length: " + responseBody.getBytes(StandardCharsets.UTF_8).length + " ",
-                    "",
-                    responseBody);
-
-            outputStream.write(response.getBytes(StandardCharsets.UTF_8));
-            outputStream.flush();
+            final var responseBody = getResponseBody(requestPath);
+            final var contentType = getContentType(requestPath);
+            writeResponse(outputStream, contentType, responseBody);
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String getRequestUri(final InputStream inputStream) throws IOException {
+        final var requestLine = new BufferedReader(
+                new InputStreamReader(inputStream, StandardCharsets.UTF_8)
+        ).readLine();
+        if (requestLine == null) {
+            return "/";
+        }
+        return requestLine.split(" ")[1];
+    }
+
+    private String getRequestPath(final String requestUri) {
+        final var queryStringIndex = requestUri.indexOf("?");
+        if (queryStringIndex >= 0) {
+            return requestUri.substring(0, queryStringIndex);
+        }
+        return requestUri;
+    }
+
+    private Map<String, String> parseQueryString(final String requestUri) {
+        final var parameters = new HashMap<String, String>();
+        final var queryStringIndex = requestUri.indexOf("?");
+        if (queryStringIndex < 0) {
+            return parameters;
+        }
+
+        final var queryString = requestUri.substring(queryStringIndex + 1);
+        if (!queryString.isEmpty()) {
+            final var queryParameters = queryString.split("&");
+            for (final var queryParameter : queryParameters) {
+                final var keyValue = queryParameter.split("=", 2);
+                if (keyValue.length == 2) {
+                    parameters.put(keyValue[0], keyValue[1]);
+                }
+            }
+        }
+
+        return parameters;
+    }
+
+    private void authenticate(final String requestPath, final Map<String, String> parameters) {
+        final var account = parameters.get("account");
+        final var password = parameters.get("password");
+        if (!"/login".equals(requestPath) || account == null || password == null) {
+            return;
+        }
+
+        final var foundUser = InMemoryUserRepository.findByAccount(account);
+        if (foundUser.isPresent()) {
+            final var user = foundUser.get();
+            if (user.checkPassword(password)) {
+                log.info("login user: {}", user);
+            }
+        }
+    }
+
+    private byte[] getResponseBody(final String requestPath) throws IOException {
+        var responseBody = "Hello world!".getBytes(StandardCharsets.UTF_8);
+        var resourcePath = "";
+
+        if ("/index.html".equals(requestPath)
+                || "/css/styles.css".equals(requestPath)
+                || requestPath.startsWith("/js/")
+                || requestPath.startsWith("/assets/")) {
+            resourcePath = "static" + requestPath;
+        }
+        if ("/login".equals(requestPath)) {
+            resourcePath = "static/login.html";
+        }
+
+        if (!resourcePath.isEmpty()) {
+            try (final var resource = getClass().getClassLoader().getResourceAsStream(resourcePath)) {
+                responseBody = resource.readAllBytes();
+            }
+        }
+        return responseBody;
+    }
+
+    private String getContentType(final String requestPath) {
+        if (requestPath.endsWith(".css")) {
+            return "text/css;charset=utf-8";
+        }
+        if (requestPath.endsWith(".js")) {
+            return "application/javascript;charset=utf-8";
+        }
+        return "text/html;charset=utf-8";
+    }
+
+    private void writeResponse(final OutputStream outputStream, final String contentType, final byte[] responseBody)
+            throws IOException {
+        final var responseHeaders = String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: " + contentType + " ",
+                "Content-Length: " + responseBody.length + " ",
+                "",
+                "");
+
+        outputStream.write(responseHeaders.getBytes(StandardCharsets.UTF_8));
+        outputStream.write(responseBody);
+        outputStream.flush();
     }
 }

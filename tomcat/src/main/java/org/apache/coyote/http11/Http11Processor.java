6f713e55708f70c3314e1c2c35b110dbdc20895a
Kim, Joon <kjoon418@naver.com>
2026-09-16 22:03:51 +0900

    feat: CSS 지원하기 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -37,7 +37,7 @@ public class Http11Processor implements Runnable, Processor {
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Type: " + getContentType(path) + " ",
                     "Content-Length: " + responseBody.getBytes(StandardCharsets.UTF_8).length + " ",
                     "",
                     responseBody);
@@ -47,6 +47,16 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String getContentType(String path) {
+        if (path.endsWith(".html") || "/".equals(path)) {
+            return "text/html;charset=utf-8";
+        }
+        if (path.endsWith(".css")) {
+            return "text/css;charset=utf-8";
+        }
+        return "application/octet-stream";
     }
 
     private String getResponseBody(String path) {

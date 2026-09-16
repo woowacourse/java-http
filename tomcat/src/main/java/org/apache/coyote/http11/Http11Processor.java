de8d394cf8a500234f80c64fa3037c8e9cb72654
nn98 <jkllhgb@gmail.com>
2026-09-16 18:21:34 +0900

    feat: /index.html 응답

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,14 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.StreamTokenizer;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.StringTokenizer;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,19 +37,38 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String firstLine = reader.readLine();
+            StringTokenizer streamTokenizer = new StringTokenizer(firstLine);
+            String method = streamTokenizer.nextToken();
+            log.info("method: {}", method);
+            String path = streamTokenizer.nextToken();
+            log.info("path: {}", path);
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            if (path.equals("/")) {
+                log.info("path is empty");
+                final var responseBody = "Hello world!";
+                response(responseBody, outputStream);
+                return;
+            }
 
-            outputStream.write(response.getBytes());
-            outputStream.flush();
+            final URL resource = getClass().getClassLoader().getResource("static" + path);
+            final var responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+            response(responseBody, outputStream);
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private static void response(String responseBody, OutputStream outputStream) throws IOException {
+        final var response = String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+
+        outputStream.write(response.getBytes());
+        outputStream.flush();
     }
 }

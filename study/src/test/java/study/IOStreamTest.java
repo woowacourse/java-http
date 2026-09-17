9d6fdfb35db1d062303fdf656b42db746cbd151d
smiinii <sm020320@naver.com>
2026-09-17 20:53:49 +0900

    [1단계 - HTTP 서버 구현하기] 루크(이성민) 미션 제출합니다. (#1118)

--- study/src/test/java/study/IOStreamTest.java
+++ study/src/test/java/study/IOStreamTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import java.nio.charset.StandardCharsets;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -49,10 +50,7 @@ class IOStreamTest {
             final byte[] bytes = {110, 101, 120, 116, 115, 116, 101, 112};
             final OutputStream outputStream = new ByteArrayOutputStream(bytes.length);
 
-            /**
-             * todo
-             * OutputStream 객체의 write 메서드를 사용해서 테스트를 통과시킨다
-             */
+            outputStream.write(bytes);
 
             final String actual = outputStream.toString();
 
@@ -73,11 +71,7 @@ class IOStreamTest {
         void BufferedOutputStream을_사용하면_버퍼링이_가능하다() throws IOException {
             final OutputStream outputStream = mock(BufferedOutputStream.class);
 
-            /**
-             * todo
-             * flush를 사용해서 테스트를 통과시킨다.
-             * ByteArrayOutputStream과 어떤 차이가 있을까?
-             */
+            outputStream.flush();
 
             verify(outputStream, atLeastOnce()).flush();
             outputStream.close();
@@ -91,11 +85,8 @@ class IOStreamTest {
         void OutputStream은_사용하고_나서_close_처리를_해준다() throws IOException {
             final OutputStream outputStream = mock(OutputStream.class);
 
-            /**
-             * todo
-             * try-with-resources를 사용한다.
-             * java 9 이상에서는 변수를 try-with-resources로 처리할 수 있다.
-             */
+            try (outputStream) {
+            }
 
             verify(outputStream, atLeastOnce()).close();
         }
@@ -128,7 +119,10 @@ class IOStreamTest {
              * todo
              * inputStream에서 바이트로 반환한 값을 문자열로 어떻게 바꿀까?
              */
-            final String actual = "";
+            final String actual = new String(
+                    inputStream.readAllBytes(),
+                    StandardCharsets.UTF_8
+            );
 
             assertThat(actual).isEqualTo("🤩");
             assertThat(inputStream.read()).isEqualTo(-1);
@@ -143,11 +137,8 @@ class IOStreamTest {
         void InputStream은_사용하고_나서_close_처리를_해준다() throws IOException {
             final InputStream inputStream = mock(InputStream.class);
 
-            /**
-             * todo
-             * try-with-resources를 사용한다.
-             * java 9 이상에서는 변수를 try-with-resources로 처리할 수 있다.
-             */
+            try (inputStream) {
+            }
 
             verify(inputStream, atLeastOnce()).close();
         }
@@ -169,12 +160,12 @@ class IOStreamTest {
          * 버퍼 크기를 지정하지 않으면 버퍼의 기본 사이즈는 얼마일까?
          */
         @Test
-        void 필터인_BufferedInputStream를_사용해보자() {
+        void 필터인_BufferedInputStream를_사용해보자() throws IOException {
             final String text = "필터에 연결해보자.";
             final InputStream inputStream = new ByteArrayInputStream(text.getBytes());
-            final InputStream bufferedInputStream = null;
+            final InputStream bufferedInputStream = new BufferedInputStream(inputStream);
 
-            final byte[] actual = new byte[0];
+            final byte[] actual = bufferedInputStream.readAllBytes();
 
             assertThat(bufferedInputStream).isInstanceOf(FilterInputStream.class);
             assertThat(actual).isEqualTo("필터에 연결해보자.".getBytes());
@@ -203,9 +194,21 @@ class IOStreamTest {
                     "😇🙂🙃😉😌😍🥰😘😗😙😚",
                     "😋😛😝😜🤪🤨🧐🤓😎🥸🤩",
                     "");
-            final InputStream inputStream = new ByteArrayInputStream(emoji.getBytes());
+            final InputStream inputStream =
+                    new ByteArrayInputStream(emoji.getBytes(StandardCharsets.UTF_16));
 
             final StringBuilder actual = new StringBuilder();
+
+            try (BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(inputStream, StandardCharsets.UTF_16))) {
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    actual.append(line).append("\r\n");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
 
             assertThat(actual).hasToString(emoji);
         }
--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,13 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,16 +36,100 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            String requestLine = bufferedReader.readLine();
+
+            if (requestLine == null) {
+                return;
+            }
+
+            String uri = requestLine.split(" ")[1];
+
+            String path = uri;
+            String queryString = "";
+
+            int index = uri.indexOf("?");
+
+            if (index != -1) {
+                path = uri.substring(0, index);
+                queryString = uri.substring(index + 1);
+            }
+
+            String line = bufferedReader.readLine();
+
+            while (!"".equals(line)) {
+                if (line == null) {
+                    return;
+                }
+
+                line = bufferedReader.readLine();
+            }
+
+
+            var responseBody = "Hello world!";
+            var contentType = "text/html;charset=utf-8";
+
+            if (path.equals("/index.html")) {
+                URL resource = getClass().getClassLoader()
+                        .getResource("static/index.html");
+
+                responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+            }
+
+            if (path.equals("/css/styles.css")) {
+                URL resource = getClass().getClassLoader()
+                        .getResource("static/css/styles.css");
+
+                responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+                contentType = "text/css;charset=utf-8";
+            }
+
+            if (path.equals("/login")) {
+                URL resource = getClass().getClassLoader()
+                        .getResource("static/login.html");
+
+                responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+
+                if (!queryString.isEmpty()) {
+                    String[] parameters = queryString.split("&");
+
+                    String account = "";
+                    String password = "";
+
+                    for (String parameter : parameters) {
+                        String[] keyValue = parameter.split("=", 2);
+
+                        if (keyValue.length != 2) {
+                            continue;
+                        }
+
+                        if (keyValue[0].equals("account")) {
+                            account = keyValue[1];
+                        } else if (keyValue[0].equals("password")) {
+                            password = keyValue[1];
+                        }
+                    }
+
+                    var user = InMemoryUserRepository.findByAccount(account);
+
+                    if (user.isPresent()) {
+                        if (user.get().checkPassword(password)) {
+                            log.info("로그인 성공 : account={}", user.get().getAccount());
+                        }
+                    }
+                }
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "Content-Type: " + contentType + " ",
+                    "Content-Length: " + responseBody.getBytes(StandardCharsets.UTF_8).length + " ",
                     "",
                     responseBody);
 
-            outputStream.write(response.getBytes());
+            outputStream.write(response.getBytes(StandardCharsets.UTF_8));
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);

32a2cfa3781fd0d74a0756cfb1557e267c57aa58
Jiihyun <cwlgusc@gmail.com>
2026-09-17 20:56:00 +0900

    feat: 로그인 요청의 Query String 파싱 및 회원 조회

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,13 +1,17 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +19,7 @@ import org.slf4j.LoggerFactory;
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+
     private static final Map<String, String> CONTENT_TYPES = Map.of(
             ".html", "text/html;charset=utf-8",
             ".css", "text/css;charset=utf-8",
@@ -38,7 +43,9 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(inputStream)
+            );
 
             String requestLine = reader.readLine();
             if (requestLine == null) {
@@ -48,12 +55,18 @@ public class Http11Processor implements Runnable, Processor {
             readHeaders(reader);
 
             String requestUri = extractRequestUri(requestLine);
-            InputStream resourceStream = getResourceStream(requestUri);
 
-            String responseBody = getResponseBody(requestUri, resourceStream);
-            String response = createResponse(requestUri, responseBody);
+            String path = extractPath(requestUri);
+            Map<String, String> queryParams = extractQueryParams(requestUri);
+            handleLogin(path, queryParams);
 
-            outputStream.write(response.getBytes());
+            String resourcePath = resolveResourcePath(path);
+            InputStream resourceStream = getResourceStream(resourcePath);
+
+            String responseBody = getResponseBody(path, resourceStream);
+            String response = createResponse(resourcePath, responseBody);
+
+            outputStream.write(response.getBytes(StandardCharsets.UTF_8));
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
@@ -72,34 +85,92 @@ public class Http11Processor implements Runnable, Processor {
         return request[1];
     }
 
-    private InputStream getResourceStream(final String requestUri) {
-        String resourcePath = "static" + requestUri;
-
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        return classLoader.getResourceAsStream(resourcePath);
+    private String extractPath(final String requestUri) {
+        int queryStringIndex = requestUri.indexOf("?");
+        if (queryStringIndex == -1) {
+            return requestUri;
+        }
+        return requestUri.substring(0, queryStringIndex);
     }
 
-    private static String getResponseBody(String requestUri, InputStream resourceStream) throws IOException {
-        if (requestUri.equals("/")) {
+    private Map<String, String> extractQueryParams(final String requestUri) {
+        int queryStringIndex = requestUri.indexOf("?");
+        if (queryStringIndex == -1) {
+            return Map.of();
+        }
+        String queryString = requestUri.substring(queryStringIndex + 1);
+        return parseQueryString(queryString);
+    }
+
+    private Map<String, String> parseQueryString(final String queryString) {
+        Map<String, String> params = new HashMap<>();
+        String[] pairs = queryString.split("&");
+        for (String pair : pairs) {
+            String[] keyValue = pair.split("=", 2);
+            params.put(keyValue[0], keyValue[1]);
+        }
+        return params;
+    }
+
+    private void handleLogin(final String path, final Map<String, String> queryParams) {
+        if (!path.equals("/login")) {
+            return;
+        }
+        String account = queryParams.get("account");
+        String password = queryParams.get("password");
+        if (account == null || password == null) {
+            return;
+        }
+        Optional<User> user = InMemoryUserRepository.findByAccount(account);
+        if (user.isEmpty()) {
+            return;
+        }
+        User foundUser = user.get();
+        if (foundUser.checkPassword(password)) {
+            log.info("login user: {}", foundUser);
+        }
+    }
+
+    private String resolveResourcePath(final String path) {
+        if (path.equals("/")) {
+            return path;
+        }
+        String fileName = path.substring(path.lastIndexOf("/") + 1);
+        if (!fileName.contains(".")) {
+            return path + ".html";
+        }
+        return path;
+    }
+
+    private InputStream getResourceStream(final String resourcePath) {
+        String path = "static" + resourcePath;
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        return classLoader.getResourceAsStream(path);
+    }
+
+    private String getResponseBody(final String path, final InputStream resourceStream) throws IOException {
+        if (path.equals("/")) {
             return "Hello world!";
         }
         return new String(resourceStream.readAllBytes(), StandardCharsets.UTF_8);
     }
 
-    private String createResponse(final String requestUri, final String responseBody) {
-        String contentType = CONTENT_TYPES.entrySet().stream()
-                .filter(entry -> requestUri.endsWith(entry.getKey()))
+    private String createResponse(final String resourcePath, final String responseBody) {
+        String contentType = CONTENT_TYPES.entrySet()
+                .stream()
+                .filter(entry -> resourcePath.endsWith(entry.getKey()))
                 .map(Map.Entry::getValue)
                 .findFirst()
-                .orElse("text/plain;charset=utf-8");
-
+                .orElse("text/html;charset=utf-8");
         byte[] responseBodyBytes = responseBody.getBytes(StandardCharsets.UTF_8);
-
-        return String.join("\r\n",
-                "HTTP/1.1 200 OK",
-                "Content-Type: " + contentType,
-                "Content-Length: " + responseBodyBytes.length,
-                "",
+        return String.format(
+                "HTTP/1.1 200 OK \r\n"
+                        + "Content-Type: %s \r\n"
+                        + "Content-Length: %d \r\n"
+                        + "\r\n"
+                        + "%s",
+                contentType,
+                responseBodyBytes.length,
                 responseBody
         );
     }

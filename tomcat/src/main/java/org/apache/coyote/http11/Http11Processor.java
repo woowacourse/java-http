02b16543b282c9c3260bd493d3f780612cc86674
poketopa <lhs5427ll@gmail.com>
2026-09-18 08:27:50 +0900

    refactor: 경로, 파라미터 명, 콘텐츠 타입 상수 분리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -25,6 +25,22 @@ public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
 
+    private static final String ROOT_PATH = "/";
+    private static final String LOGIN_PATH = "/login";
+    private static final String STATIC_RESOURCE_DIRECTORY = "static";
+    private static final String HTML_EXTENSION = ".html";
+    private static final String DEFAULT_RESPONSE_BODY = "Hello world!";
+    private static final String ACCOUNT_PARAMETER = "account";
+    private static final String PASSWORD_PARAMETER = "password";
+    private static final String HTML_CONTENT_TYPE = "text/html";
+    private static final String CSS_CONTENT_TYPE = "text/css";
+    private static final String ACCEPT_HEADER_PREFIX = "Accept:";
+    private static final String CRLF = "\r\n";
+    private static final String OK_STATUS_LINE = "HTTP/1.1 200 OK ";
+    private static final String CONTENT_TYPE_HEADER_PREFIX = "Content-Type: ";
+    private static final String UTF_8_CHARSET_PARAMETER = ";charset=utf-8 ";
+    private static final String CONTENT_LENGTH_HEADER_PREFIX = "Content-Length: ";
+
     private final Socket connection;
 
     public Http11Processor(final Socket connection) {
@@ -77,39 +93,39 @@ public class Http11Processor implements Runnable, Processor {
             // 경로 없음 -> Hello world!
             // 경로 존재하면 파일 읽기
             String responseBody;
-            if(url.equals("/")) {
-                responseBody = "Hello world!";
-            } else if (urlPath.equals("/login")) {
-                URL resource = getClass().getClassLoader().getResource("static" + urlPath + ".html");
+            if(url.equals(ROOT_PATH)) {
+                responseBody = DEFAULT_RESPONSE_BODY;
+            } else if (urlPath.equals(LOGIN_PATH)) {
+                URL resource = getClass().getClassLoader().getResource(STATIC_RESOURCE_DIRECTORY + urlPath + HTML_EXTENSION);
                 responseBody = Files.readString(Paths.get(resource.toURI()), StandardCharsets.UTF_8);
             } else {
-                URL resource = getClass().getClassLoader().getResource("static" + urlPath);
+                URL resource = getClass().getClassLoader().getResource(STATIC_RESOURCE_DIRECTORY + urlPath);
                 responseBody = Files.readString(Paths.get(resource.toURI()), StandardCharsets.UTF_8);
             }
 
             // 로그
-            if(urlPath.equals("/login") && isQuery && queryMap.containsKey("account") && queryMap.containsKey("password")) {
-                InMemoryUserRepository.findByAccount(queryMap.get("account"))
-                        .filter(user -> user.checkPassword(queryMap.get("password")))
+            if(urlPath.equals(LOGIN_PATH) && isQuery && queryMap.containsKey(ACCOUNT_PARAMETER) && queryMap.containsKey(PASSWORD_PARAMETER)) {
+                InMemoryUserRepository.findByAccount(queryMap.get(ACCOUNT_PARAMETER))
+                        .filter(user -> user.checkPassword(queryMap.get(PASSWORD_PARAMETER)))
                         .ifPresent(user -> log.info("회원 조회 성공: {}", user));
             }
 
             // 헤더를 하나씩 읽으면서 Accept 존재하면 확장자 설정
             // Accept 존재하지 않으면 기본값 text/html로 설정
-            String contentType = "text/html";
+            String contentType = HTML_CONTENT_TYPE;
             for(String line : header) {
-                if(line.startsWith("Accept:")) {
-                    if(line.contains("text/css")) {
-                        contentType = "text/css";
+                if(line.startsWith(ACCEPT_HEADER_PREFIX)) {
+                    if(line.contains(CSS_CONTENT_TYPE)) {
+                        contentType = CSS_CONTENT_TYPE;
                     }
                     break;
                 }
             }
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: " + contentType + ";charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+            final var response = String.join(CRLF,
+                    OK_STATUS_LINE,
+                    CONTENT_TYPE_HEADER_PREFIX + contentType + UTF_8_CHARSET_PARAMETER,
+                    CONTENT_LENGTH_HEADER_PREFIX + responseBody.getBytes().length + " ",
                     "",
                     responseBody);
 

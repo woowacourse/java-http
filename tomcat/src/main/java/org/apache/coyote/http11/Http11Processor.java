a5361ad76487b0b8dba3ff07a6eb70ee4b722f0a
woohyeon <woohyeon6885@gmail.com>
2026-09-17 14:35:45 +0900

    feat: 요청 URI에 해당하는 정적 파일 응답 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +16,7 @@ public class Http11Processor implements Runnable, Processor {
 
     private final Socket connection;
 
-    public Http11Processor(final Socket connection) {
+    public Http11Processor(Socket connection) {
         this.connection = connection;
     }
 
@@ -29,21 +30,44 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final InputStream inputStream = connection.getInputStream();
              final OutputStream outputStream = connection.getOutputStream()) {
-            final String responseBody = "Hello world!";
+            HttpRequestLine requestLine = HttpRequestLine.from(inputStream);
+            final String uri = requestLine.getUri();
 
+            final String responseBody = readContentsFrom(uri);
             final String response = String.join(
                     "\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "Content-Length: " + responseBody.getBytes(StandardCharsets.UTF_8).length + " ",
                     "",
                     responseBody
             );
 
-            outputStream.write(response.getBytes());
+            outputStream.write(response.getBytes(StandardCharsets.UTF_8));
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String readContentsFrom(String uri) throws IOException {
+        if (uri.equals("/")) {
+            return "Hello world!";
+        }
+
+        try (InputStream resource = findResource(uri)) {
+            return new String(resource.readAllBytes(), StandardCharsets.UTF_8);
+        }
+    }
+
+    private InputStream findResource(String uri) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream resource = classLoader.getResourceAsStream("static" + uri);
+
+        if (resource == null) {
+            resource = classLoader.getResourceAsStream("static/404.html");
+        }
+
+        return resource;
     }
 }
---
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpRequestLine.java
@@ -0,0 +1,47 @@
+package org.apache.coyote.http11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class HttpRequestLine {
+
+    private static final String SP = " ";
+    private static final Charset DEFAULT_CHARSETS = StandardCharsets.US_ASCII;
+
+    private final String method;
+    private final String uri;
+
+    private HttpRequestLine(String method, String uri) {
+        this.method = method;
+        this.uri = uri;
+    }
+
+    public static HttpRequestLine from(InputStream inputStream) throws IOException {
+        final String requestLine  = new BufferedReader(
+                new InputStreamReader(inputStream, DEFAULT_CHARSETS)).readLine();
+
+        if (requestLine == null) {
+            throw new IOException("클라이언트가 요청 없이 연결을 닫았습니다.");
+        }
+
+        final String[] requestLineParts = requestLine.split(SP);
+
+        if (requestLineParts.length != 3) {
+            throw new IOException("잘못된 HTTP Request Line 형식입니다: " + requestLine);
+        }
+
+        return new HttpRequestLine(requestLineParts[0], requestLineParts[1]);
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+}

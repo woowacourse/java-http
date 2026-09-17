be173a00da75a260bd8cfd788f12f4666e494897
Jiihyun <cwlgusc@gmail.com>
2026-09-17 12:55:31 +0900

    feat:  GET /index.html 응답 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,15 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,19 +32,61 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            String requestLine = reader.readLine();
+            if (requestLine == null) {
+                return;
+            }
+
+            readHeaders(reader);
+
+            String requestUri = extractRequestUri(requestLine);
+            InputStream resourceStream = getResourceStream(requestUri);
+
+            String responseBody = getResponseBody(requestUri, resourceStream);
+            String response = createResponse(responseBody);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private void readHeaders(final BufferedReader reader) throws IOException {
+        String line = reader.readLine();
+        while (line != null && !line.isEmpty()) {
+            line = reader.readLine();
+        }
+    }
+
+    private String extractRequestUri(final String requestLine) {
+        String[] request = requestLine.split(" ");
+        return request[1];
+    }
+
+    private InputStream getResourceStream(final String requestUri) {
+        String resourcePath = "static" + requestUri;
+
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        return classLoader.getResourceAsStream(resourcePath);
+    }
+
+    private static String getResponseBody(String requestUri, InputStream resourceStream) throws IOException {
+        if (requestUri.equals("/")) {
+            return "Hello world!";
+        }
+        return new String(resourceStream.readAllBytes(), StandardCharsets.UTF_8);
+    }
+
+    private String createResponse(final String responseBody) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody
+        );
     }
 }

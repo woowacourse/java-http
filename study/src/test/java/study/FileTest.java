24b272a3d5247a37405133695b8eb95c147ee90f
jyt6640 <jyt6640@gmail.com>
2026-09-17 12:23:06 +0900

    test: FileTest 테스트 구현

--- study/src/test/java/study/FileTest.java
+++ study/src/test/java/study/FileTest.java
@@ -1,5 +1,11 @@
 package study;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -26,9 +32,10 @@ class FileTest {
     @Test
     void resource_디렉터리에_있는_파일의_경로를_찾는다() {
         final String fileName = "nextstep.txt";
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource(fileName);
 
-        // todo
-        final String actual = "";
+        final String actual = resource.getPath();
 
         assertThat(actual).endsWith(fileName);
     }
@@ -40,14 +47,17 @@ class FileTest {
      * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws URISyntaxException, IOException {
         final String fileName = "nextstep.txt";
+        ClassLoader classLoader = getClass().getClassLoader();
+        URI uri = classLoader.getResource(fileName).toURI();
 
         // todo
-        final Path path = null;
+        final Path path = Paths.get(uri);
+
 
         // todo
-        final List<String> actual = Collections.emptyList();
+        final List<String> actual = Files.readAllLines(path);
 
         assertThat(actual).containsOnly("nextstep");
     }

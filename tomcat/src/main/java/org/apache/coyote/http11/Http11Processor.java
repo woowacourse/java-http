86c597d4bbfd0f4f27e3a7067edb5e4d44861354
haeseon lee <haeseon4816@gmail.com>
2026-09-17 17:09:54 +0900

    feat: GET /index.html 응답하기 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,8 +5,14 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,20 +34,61 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
+            final var requestPath = parseRequestPath(inputStream);
 
-            final var responseBody = "Hello world!";
+            final byte[] responseBody;
+            if (requestPath.equals("/")) {
+                responseBody = "Hello world!".getBytes(StandardCharsets.UTF_8);
+            } else {
+                responseBody = readStaticResource(requestPath);
+            }
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
-
-            outputStream.write(response.getBytes());
-            outputStream.flush();
+            writeResponse(outputStream, responseBody);
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String parseRequestPath(final InputStream inputStream) throws IOException {
+        final var reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.US_ASCII));
+
+        final var line = reader.readLine();
+
+        if (line == null || line.isEmpty()) {
+            throw new IllegalArgumentException("HTTP 요청 라인이 비어있습니다.");
+        }
+
+        String[] token = line.trim().split("\\s+");
+
+        if (token.length != 3) {
+            throw new IllegalArgumentException("올바르지 않은 HTTP 요청 라인입니다.");
+        }
+
+        return token[1];
+    }
+
+    private byte[] readStaticResource(final String path) throws IOException {
+        final var resourcePath = "static" + path;
+
+        final var resource = getClass().getClassLoader().getResourceAsStream(resourcePath);
+
+        if (resource == null) {
+            throw new FileNotFoundException("파일을 찾을 수 없습니다." + resourcePath);
+        }
+
+        try (resource) {
+            return resource.readAllBytes();
+        }
+    }
+
+    private void writeResponse(final OutputStream outputStream, final byte[] bytes) throws IOException {
+        final var response = String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + bytes.length + " ",
+                "\r\n");
+        outputStream.write(response.getBytes());
+        outputStream.write(bytes);
+        outputStream.flush();
     }
 }

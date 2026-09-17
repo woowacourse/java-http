bb9d876ec3d8aa28f99a9e684b0dfdd4a06c4b2c
박병훈 <leafloveu@naver.com>
2026-09-17 20:13:13 +0900

    feat: index.html 및 css 적용

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -4,9 +4,12 @@ import com.techcourse.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,6 +31,59 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String requestLine = reader.readLine();
+            String[] tokens = requestLine.split(" ");
+
+            String method = tokens[0];
+            String path = tokens[1];           
+
+            Map<String, String> headerMap = new HashMap<>();
+
+            String header;
+            while(!(header=reader.readLine()).isEmpty()){
+                int index=header.indexOf(":");
+                String name=header.substring(0, index);
+                String value=header.substring(index+1).trim();
+                headerMap.put(name, value);
+            }
+            
+            if (method.equals("GET") && path.equals("/index.html")) {
+                final var fileStream=getClass()
+                        .getClassLoader()
+                        .getResourceAsStream("static/index.html");
+                final var fileBytes=fileStream.readAllBytes();
+                final var response = String.join("\r\n",
+                    "HTTP/1.1 200 OK ",
+                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Length: " + fileBytes.length + " ",
+                    "",
+                    new String(fileBytes));
+
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+                return;
+            }
+            
+            if (method.equals("GET") && path.equals("/css/styles.css")) {
+                final var fileStream = getClass()
+                        .getClassLoader()
+                        .getResourceAsStream("static/css/styles.css");
+
+                final var fileBytes = fileStream.readAllBytes();
+
+                final var response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/css;charset=utf-8 ",
+                        "Content-Length: " + fileBytes.length + " ",
+                        "",
+                        new String(fileBytes));
+
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+                return;
+            }
 
             final var responseBody = "Hello world!";
 

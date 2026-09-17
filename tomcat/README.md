b772391fdc9fef73fd5457a107c8c2ace011bef6
milan <kangrae0501@gmail.com>
2026-09-17 13:43:26 +0900

    [1단계 - HTTP 서버 구현하기] 밀란 미션 제출합니다. (#1110)
    * test: 파일 이름으로 리소스 경로 가져오기
    
    - 클래스 로더는 getResource로 기능을 제공한다.
    
    * test: 파일 내용 읽기
    
    - Files 클래스의 readAllLines에 path를 넘겨서 읽는다.
    
    * test: OutputStream 데이터 처리
    
    * test: BufferedOutputStream 버퍼 처리
    
    - 버퍼 클래스와 flush 학습
    
    * test: 자원 닫기
    
    * test: 바이트로 읽은 데이터 디코딩
    
    * test: 자원 회수
    
    * test: 버퍼를 사용하는 InputStream 생성
    
    - 버퍼의 기본 사이즈는 8192
    
    * test: InputStream 으로부터 BufferedReader 생성 및 사용
    
    * docs: 요구 사항 정리
    
    * feat: request line과 header를 읽고 적절한 resource를 반환하는 기능 구현
    
    * docs: 요구 사항 정리
    
    - css 적용 관련 요구 사항을 정리한다.
    
    * refactor: 메서드 분리
    
    - 동작을 분리하고 흐름이 드러나도록 한다.
    - 이후 클래스 분리를 고려한다.
    
    * feat: content type을 구분하여 반환하는 기능 구현
    
    - 지금은 css와 html만 지원하며, 추후 switch case등으로 다른 다양한 type도 지원하게 할 수 있다.
    
    * docs: 요구 사항 정리
    
    - 쿼리를 파싱하여 로그인 기능이 동작하게하는 요구 사항을 정리한다.
    
    * feat: 로그인 uri로의 접속을 올바르게 파싱하는 기능 구현
    
    - 1단계에서는 로그으로 아이디 패스워드 일치 여부를 보여준다.
    
    * refactor: 표현 축약 및 로깅 강화
    
    - 필요없는 지역변수 삭제
    - null 체크 삭제
    - resourceName으로부터 리소스를 찾을 수 없을 때를 대비하여 오류 메시지를 남긴다.

---
+++ tomcat/README.md
@@ -0,0 +1,43 @@
+# Tomcat 구현하기
+
+## 1단계 - HTTP 서버 구현하기
+
+> 간단한 HTTP 서버를 만들어보자.
+> 저장소에서 소스코드를 받아와서 메인 클래스를 실행하면 HTTP 서버가 실행된다.
+> 웹브라우저로 로컬 서버(http://localhost:8080) 에 접속하면 Hello world!가 보인다.
+> 정상 동작을 확인했으면 새로운 기능을 추가해보자.
+
+### 기능 요구 사항
+
+#### 1. GET /index.html 응답하기
+
+- [x] 인덱스 페이지(http://localhost:8080/index.html)에 접근할 수 있다.
+- [x] Http11ProcessorTest 테스트 클래스의 모든 테스트를 통과한다.
+
+브라우저에서 요청한 HTTP Request Header는 다음과 같다.
+
+```http request
+GET /index.html HTTP/1.1
+Host: localhost:8080
+Connection: keep-alive
+Accept: */*
+```
+
+#### 2. CSS 지원하기
+
+- [x] 사용자가 페이지를 열었을 때 CSS 파일도 호출한다.
+
+브라우저에서 요청한 HTTP Request Header는 다음과 같다.
+
+```http request
+GET /css/styles.css HTTP/1.1
+Host: localhost:8080
+Accept: text/css,*/*;q=0.1
+Connection: keep-alive
+``` 
+
+#### 3. Query String 파싱
+
+- [ ] http://localhost:8080/login?account=gugu&password=password으로 접속하면 로그인 페이지(login.html)를 보여준다.
+- [ ] 로그인 페이지 접속 시 Query String을 파싱해서 아이디, 비밀번호 일치를 검사한다.
+- [ ] 일치 한다면 콘솔창에 로그로 회원을 조회한 결과가 나오게 한다.
--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,19 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -26,14 +33,48 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+        try (
+                final var inputStream = connection.getInputStream();
+                final var outputStream = connection.getOutputStream();
+                final var bufferedReader = new BufferedReader(new InputStreamReader(inputStream))
+        ) {
+            final String requestLine = bufferedReader.readLine();
+            if (requestLine == null) {
+                return;
+            }
 
-            final var responseBody = "Hello world!";
+            if (!validateHeaders(bufferedReader)) {
+                return;
+            }
+
+            final String[] requestComponents = requestLine.split(" ");
+            final String requestUri = requestComponents[1];
+
+            final int queryIndex = requestUri.indexOf('?');
+            String path;
+            final String queryString;
+            if (queryIndex >= 0) {
+                path = requestUri.substring(0, queryIndex);
+                queryString = requestUri.substring(queryIndex + 1);
+            } else {
+                path = requestUri;
+                queryString = "";
+            }
+
+            if ("/login".equals(path)) {
+                if (!queryString.isBlank()) {
+                    final Map<String, String> parameters = parseQueryString(queryString);
+                    login(parameters);
+                }
+                path += ".html";
+            }
+
+            final String responseBody = getResponseBody(path);
+            final String contentType = resolveContentType(requestUri);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Type: " + contentType + ";charset=utf-8 ",
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
                     responseBody);
@@ -43,5 +84,61 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String getResponseBody(String path) throws IOException {
+        if ("/".equals(path)) {
+            return "Hello world!";
+        }
+        final String resourceName = "static" + path;
+        final String fileName = Objects.requireNonNull(
+                getClass().getClassLoader().getResource(resourceName),
+                "리소스를 찾을 수 없음: " + resourceName
+        ).getPath();
+
+        return Files.readString(Path.of(fileName));
+    }
+
+    private String resolveContentType(final String requestUri) {
+        if (requestUri.endsWith(".css")) {
+            return "text/css";
+        }
+        return "text/html";
+    }
+
+    private Map<String, String> parseQueryString(final String queryString) {
+        final Map<String, String> parameters = new HashMap<>();
+        if (queryString.isBlank()) {
+            return parameters;
+        }
+
+        for (String pair : queryString.split("&")) {
+            final String[] nameAndValue = pair.split("=", 2);
+            if (nameAndValue.length != 2) {
+                continue;
+            }
+            parameters.put(nameAndValue[0], nameAndValue[1]);
+        }
+        return parameters;
+    }
+
+    private void login(final Map<String, String> parameters) {
+        final String account = parameters.get("account");
+        final String password = parameters.get("password");
+
+        InMemoryUserRepository.findByAccount(account)
+                .filter(user -> user.checkPassword(password))
+                .ifPresent(user -> log.info("로그인 성공: {}", user));
+    }
+
+    private static boolean validateHeaders(BufferedReader bufferedReader) throws IOException {
+        String line = bufferedReader.readLine();
+        while (!"".equals(line)) {
+            if (line == null) {
+                return false;
+            }
+            line = bufferedReader.readLine();
+        }
+        return true;
     }
 }

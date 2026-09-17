bd13f35f122bc7e03ecdb410a64c64a1affb8730
MODUGGAGI <kiroro0814@naver.com>
2026-09-17 18:22:57 +0900

    feat: 1. GET /index.html 응답하기 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,15 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,22 +30,46 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void run() {
         log.info("connect host: {}, port: {}", connection.getInetAddress(), connection.getPort());
-        process(connection);
+        try {
+            process(connection);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public void process(final Socket connection) {
+    public void process(final Socket connection) throws URISyntaxException {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))
+        ) {
+            String[] split = bufferedReader.readLine().split(" ");
+            if (split[1].equals("/favicon.ico")) {
+                return;
+            }
+            final String fileName = "static" + split[1];
 
-            final var responseBody = "Hello world!";
+            URL resource = getClass().getClassLoader().getResource(fileName);
+            final Path path = Paths.get(Objects.requireNonNull(resource).toURI());
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            String response;
+            if (Files.isDirectory(path)) {
+                final var responseBody = "Hello world!";
+                response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody);
+            } else  {
+                byte[] responseBody = Files.readAllBytes(path);
+                response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + responseBody.length + " ",
+                        "",
+                        new String(responseBody, StandardCharsets.UTF_8));
+            }
 
             outputStream.write(response.getBytes());
             outputStream.flush();

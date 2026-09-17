901bb81c6de2ac1ca65554c2776742468a5ac2fe
gleaming9 <gleaming9@khu.ac.kr>
2026-09-18 01:24:10 +0900

    feat: 쿼리 파라미터 파싱 구현

--- tomcat/src/main/java/org/apache/coyote/http11/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpRequest.java
@@ -4,33 +4,55 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HttpRequest {
     private final String method;
     private final String requestTarget;
+    private final Map<String, String> queryParameters;
 
-    public HttpRequest(String method, String requestTarget) {
+    public HttpRequest(String method, String requestTarget, Map<String, String> queryParameters) {
         this.method = method;
         this.requestTarget = requestTarget;
+        this.queryParameters = queryParameters;
     }
 
     public static HttpRequest parse(InputStream inputStream) throws IOException {
         final var bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 
         final var requestLine = bufferedReader.readLine();
-        if(requestLine.isBlank()) {
+        if (requestLine.isBlank()) {
             throw new IllegalArgumentException("Request line is blank");
         }
-        final var parts = requestLine.trim().split(" ");
+        final var requestLineParts = requestLine.trim().split(" ");
+        final String method = requestLineParts[0];
 
-        return new HttpRequest(parts[0], parts[1]);
+        final var requestTargetParts = requestLineParts[1].split("\\?", 2);
+        final String requestTarget = requestTargetParts[0];
+
+        return new HttpRequest(method, requestTarget, initQueryParameters(requestTargetParts));
+    }
+
+    private static Map<String, String> initQueryParameters(String[] requestTargetParts) {
+        final var queryParameters = new HashMap<String, String>();
+        if (requestTargetParts.length < 2) {
+            return queryParameters;
+        }
+
+        final var queryPairs = requestTargetParts[1].split("&");
+        for (String queryPair : queryPairs) {
+            var nameAndValue = queryPair.split("=", 2);
+            queryParameters.put(nameAndValue[0], nameAndValue[1]);
+        }
+        return queryParameters;
     }
 
     public boolean isGetMethod() {
         return method.equals("GET");
     }
 
-    public boolean isPath(String path){
+    public boolean isPath(String path) {
         return requestTarget.equals(path);
     }
 
@@ -40,5 +62,9 @@ public class HttpRequest {
 
     public String getRequestTarget() {
         return requestTarget;
+    }
+
+    public Map<String, String> getQueryParameters() {
+        return Map.copyOf(queryParameters);
     }
 }

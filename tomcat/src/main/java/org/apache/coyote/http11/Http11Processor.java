2179365134a71e9d27eb2f2bd22c501ea10d5b66
2jaeheon <lchc.dev@gmail.com>
2026-09-17 14:14:29 +0900

    feat: queryString 파싱
    Signed-off-by: 2jaeheon <lchc.dev@gmail.com>

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,18 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import javax.annotation.Nonnull;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -57,11 +63,53 @@ public class Http11Processor implements Runnable, Processor {
                     responseBody
             );
 
+            logUser(path);
+
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private void logUser(String path) {
+        String queryString = extractQueryString(path);
+        Map<String, String> queryParams = parseQueryParams(queryString);
+
+        String account = queryParams.get("account");
+        if (account == null || account.isBlank()) {
+            return;
+        }
+
+        String password = queryParams.get("password");
+        if (password == null || password.isBlank()) {
+            return;
+        }
+
+        User user = InMemoryUserRepository.findByAccount(account)
+                .orElseThrow(() -> new NoSuchElementException("해당 계정을 찾을 수 없습니다"));
+
+        if (!user.checkPassword(password)) {
+            throw new IllegalArgumentException("아이디 또는 패스워드가 일치하지 않습니다");
+        }
+
+        log.info(String.valueOf(user));
+    }
+
+    @Nonnull
+    private static Map<String, String> parseQueryParams(String queryString) {
+        Map<String, String> queryParams = new HashMap<>();
+
+        String[] params = queryString.split("&");
+        for (String param : params) {
+            String[] values = param.split("=");
+
+            if (values.length == 2) {
+                queryParams.put(values[0], values[1]);
+            }
+        }
+
+        return queryParams;
     }
 
     private String resolveContentType(String resourcePath) {
@@ -86,12 +134,20 @@ public class Http11Processor implements Runnable, Processor {
 
     private String extractResourcePath(String path) {
         int index = path.indexOf("?");
-
         if (index == -1) {
             return path;
         }
 
         return path.substring(0, index);
+    }
+
+    private String extractQueryString(String path) {
+        int index = path.indexOf("?");
+        if (index == -1) {
+            return "";
+        }
+
+        return path.substring(index + 1);
     }
 
     private byte[] readResource(String resourcePath) throws IOException {

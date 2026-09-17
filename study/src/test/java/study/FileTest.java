8c5d56b38beb0dbd67b3414c6f32c3b461dc8157
Huiyeongkim <huiyeong9619@naver.com>
2026-09-17 15:39:26 +0900

    test: File 클래스 학습 테스트 TODO 작성

--- study/src/test/java/study/FileTest.java
+++ study/src/test/java/study/FileTest.java
@@ -3,8 +3,9 @@ package study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -28,8 +29,8 @@ class FileTest {
         final String fileName = "nextstep.txt";
 
         // todo
-        final String actual = "";
-
+        final String actual = getClass().getClassLoader().getResource(fileName).getPath();
+        System.out.println(actual);
         assertThat(actual).endsWith(fileName);
     }
 
@@ -40,14 +41,14 @@ class FileTest {
      * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws IOException {
         final String fileName = "nextstep.txt";
 
         // todo
-        final Path path = null;
+        final Path path = Path.of(getClass().getClassLoader().getResource(fileName).getPath());
 
         // todo
-        final List<String> actual = Collections.emptyList();
+        final List<String> actual = Files.readAllLines(path);
 
         assertThat(actual).containsOnly("nextstep");
     }

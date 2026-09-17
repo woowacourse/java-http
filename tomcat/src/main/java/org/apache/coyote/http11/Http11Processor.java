b4574b889708bec59230cab1be857bb8354f6d58
Huiyeongkim <huiyeong9619@naver.com>
2026-09-17 20:27:05 +0900

    feat: GET /index.html 응답 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,8 +5,12 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -27,9 +31,24 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
+             final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final String requestLine = bufferedReader.readLine();
+            if (requestLine == null) {
+                return;
+            }
+
+            final String requestUri = requestLine.split(" ")[1];
+            log.info("request uri: {}", requestUri);
+
+            String responseBody;
+            if (requestUri.equals("/index.html")) {
+                final Path path = Path.of(getClass().getClassLoader().getResource("static/index.html").getPath());
+                responseBody = Files.readString(path);
+            }
+            else
+                responseBody = "Hello world!";
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",

a55e1e3d584fee81212f9807808b7974176999dc
구민서 <minseo12379@gmail.com>
2026-09-17 21:40:14 +0900

    feat(http): 로그인 쿼리 파싱 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,9 +1,15 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,30 +40,55 @@ public class Http11Processor implements Runnable, Processor {
              final var reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
 
             final String requestLine = reader.readLine();
-            final String requestPath = requestLine.split(" ")[1];
+            final String requestUri = requestLine.split(" ")[1];
+
+            final int queryStringIndex = requestUri.indexOf("?");
+
+            final String path;
+            final String queryString;
+
+            if (queryStringIndex == -1) {
+                path = requestUri;
+                queryString = "";
+            } else {
+                path = requestUri.substring(0, queryStringIndex);
+                queryString = requestUri.substring(queryStringIndex + 1);
+            }
 
             while (!reader.readLine().isEmpty()) {
 
             }
 
-            if ("/index.html".equals(requestPath)) {
+            if ("/index.html".equals(path)) {
                 final byte[] responseBody = getClass()
                         .getClassLoader()
                         .getResourceAsStream("static/index.html")
                         .readAllBytes();
 
                 outputStream.write(response(responseBody, "text/html;charset=utf-8").getBytes(StandardCharsets.UTF_8));
-            } else if ("/css/styles.css".equals(requestPath)) {
+            } else if ("/login".equals(path)) {
+                if (!queryString.isEmpty()) {
+                    login(queryString);
+                }
+
+                final byte[] responseBody = getClass()
+                        .getClassLoader()
+                        .getResourceAsStream("static/login.html")
+                        .readAllBytes();
+
+                outputStream.write(response(responseBody, "text/html;charset=utf-8").getBytes(StandardCharsets.UTF_8));
+
+            } else if ("/css/styles.css".equals(path)) {
               final byte[] responseBody = getClass()
                       .getClassLoader()
                       .getResourceAsStream("static/css/styles.css")
                       .readAllBytes();
 
               outputStream.write(response(responseBody, "text/css;charset=utf-8").getBytes(StandardCharsets.UTF_8));
-            } else if (requestPath.endsWith(".js")) {
+            } else if (path.endsWith(".js")) {
                 final byte[] responseBody = getClass()
                         .getClassLoader()
-                        .getResourceAsStream("static" + requestPath)
+                        .getResourceAsStream("static" + path)
                         .readAllBytes();
 
                 outputStream.write(response(responseBody, "application/javascript;charset=utf-8").getBytes(StandardCharsets.UTF_8));
@@ -81,5 +112,31 @@ public class Http11Processor implements Runnable, Processor {
                 "Content-Length: " + responseBody.length + " ",
                 "",
                 new String(responseBody, StandardCharsets.UTF_8));
+    }
+
+    private void login(final String queryString) {
+        if (queryString.isEmpty()) {
+            return;
+        }
+
+        final Map<String, String> parameters = Arrays.stream(queryString.split("&"))
+                .map(parameter -> parameter.split("=", 2))
+                .collect(Collectors.toMap(
+                        parameter -> parameter[0],
+                        parameter -> parameter.length > 1 ? parameter[1] : ""
+                ));
+
+        final String account = parameters.get("account");
+        final String password = parameters.get("password");
+
+        if (account == null || password == null) {
+            return;
+        }
+
+        final Optional<User> user = InMemoryUserRepository.findByAccount(account);
+
+        if (user.isPresent() && user.get().checkPassword(password)) {
+            log.info("로그인한 회원: {}", user.get());
+        }
     }
 }

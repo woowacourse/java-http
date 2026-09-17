cabb9584b6d5b641ae1a4578ceabf7762781dc86
JohnPrk <uo3641493@gmail.com>
2026-09-17 19:54:37 +0900

    test: 클래스패스 리소스 경로 찾기 및 파일 읽기 학습 테스트 구현
    - ClassLoader.getResource로 resource 디렉터리의 nextstep.txt URL 조회
    - 조회한 URL을 URI로 바꿔 Path를 만들고 Files.readAllLines로 파일 내용을 줄 단위로 조회

--- study/src/test/java/study/FileTest.java
+++ study/src/test/java/study/FileTest.java
@@ -3,8 +3,11 @@ package study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -28,7 +31,9 @@ class FileTest {
         final String fileName = "nextstep.txt";
 
         // todo
-        final String actual = "";
+        final URL resource = getClass().getClassLoader().getResource(fileName);
+
+        final String actual = resource.getFile();
 
         assertThat(actual).endsWith(fileName);
     }
@@ -40,14 +45,14 @@ class FileTest {
      * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws URISyntaxException, IOException {
         final String fileName = "nextstep.txt";
 
         // todo
-        final Path path = null;
+        final URL resource = getClass().getClassLoader().getResource(fileName);
+        final Path path = Path.of(resource.toURI());
 
-        // todo
-        final List<String> actual = Collections.emptyList();
+        final List<String> actual = Files.readAllLines(path);
 
         assertThat(actual).containsOnly("nextstep");
     }

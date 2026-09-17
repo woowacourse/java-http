59b7a94387a869e5633a7c963a69d7175de14a17
haeseon lee <haeseon4816@gmail.com>
2026-09-17 17:07:42 +0900

    test: 학습 테스트 구현

--- study/src/test/java/study/IOStreamTest.java
+++ study/src/test/java/study/IOStreamTest.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 import static org.mockito.Mockito.*;
 
 /**
@@ -54,6 +56,8 @@ class IOStreamTest {
              * OutputStream 객체의 write 메서드를 사용해서 테스트를 통과시킨다
              */
 
+            outputStream.write(bytes);
+
             final String actual = outputStream.toString();
 
             assertThat(actual).isEqualTo("nextstep");
@@ -79,6 +83,12 @@ class IOStreamTest {
              * ByteArrayOutputStream과 어떤 차이가 있을까?
              */
 
+            final BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+
+            bufferedOutputStream.write("nextstep".getBytes());
+
+            bufferedOutputStream.flush();
+
             verify(outputStream, atLeastOnce()).flush();
             outputStream.close();
         }
@@ -96,6 +106,14 @@ class IOStreamTest {
              * try-with-resources를 사용한다.
              * java 9 이상에서는 변수를 try-with-resources로 처리할 수 있다.
              */
+            final BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+            try {
+                bufferedOutputStream.write("nextstep".getBytes());
+            } finally {
+                bufferedOutputStream.flush();
+            }
+
+            bufferedOutputStream.close();
 
             verify(outputStream, atLeastOnce()).close();
         }
@@ -128,7 +146,18 @@ class IOStreamTest {
              * todo
              * inputStream에서 바이트로 반환한 값을 문자열로 어떻게 바꿀까?
              */
-            final String actual = "";
+            byte[] result = new byte[bytes.length];
+
+            int data = inputStream.read();
+            int index = 0;
+
+            while (data != -1) {
+                byte b = (byte) data;
+                result[index++] = b;
+                data = inputStream.read();
+            }
+
+            final String actual = new String(result, StandardCharsets.UTF_8);
 
             assertThat(actual).isEqualTo("🤩");
             assertThat(inputStream.read()).isEqualTo(-1);
@@ -148,6 +177,9 @@ class IOStreamTest {
              * try-with-resources를 사용한다.
              * java 9 이상에서는 변수를 try-with-resources로 처리할 수 있다.
              */
+            try (BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream)) {
+                bufferedInputStream.read();
+            }
 
             verify(inputStream, atLeastOnce()).close();
         }
@@ -169,12 +201,14 @@ class IOStreamTest {
          * 버퍼 크기를 지정하지 않으면 버퍼의 기본 사이즈는 얼마일까?
          */
         @Test
-        void 필터인_BufferedInputStream를_사용해보자() {
+        void 필터인_BufferedInputStream를_사용해보자() throws IOException {
             final String text = "필터에 연결해보자.";
             final InputStream inputStream = new ByteArrayInputStream(text.getBytes());
-            final InputStream bufferedInputStream = null;
+            final InputStream bufferedInputStream = new BufferedInputStream(inputStream);
 
-            final byte[] actual = new byte[0];
+            final byte[] actual = new byte[text.getBytes().length];
+
+            bufferedInputStream.read(actual);
 
             assertThat(bufferedInputStream).isInstanceOf(FilterInputStream.class);
             assertThat(actual).isEqualTo("필터에 연결해보자.".getBytes());
@@ -197,7 +231,7 @@ class IOStreamTest {
          * 필터인 BufferedReader를 사용하면 readLine 메서드를 사용해서 문자열(String)을 한 줄 씩 읽어올 수 있다.
          */
         @Test
-        void BufferedReader를_사용하여_문자열을_읽어온다() {
+        void BufferedReader를_사용하여_문자열을_읽어온다() throws IOException {
             final String emoji = String.join("\r\n",
                     "😀😃😄😁😆😅😂🤣🥲☺️😊",
                     "😇🙂🙃😉😌😍🥰😘😗😙😚",
@@ -205,8 +239,16 @@ class IOStreamTest {
                     "");
             final InputStream inputStream = new ByteArrayInputStream(emoji.getBytes());
 
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            String line = bufferedReader.readLine();
             final StringBuilder actual = new StringBuilder();
 
+            while (line != null) {
+                actual.append(line).append("\r\n");
+                line = bufferedReader.readLine();
+            }
             assertThat(actual).hasToString(emoji);
         }
     }

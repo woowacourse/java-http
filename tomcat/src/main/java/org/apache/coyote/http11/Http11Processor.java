890128e2d9972034b0257bb85c233e1bf60b1860
alstj2384 <ass91160@naver.com>
2026-09-18 00:15:00 +0900

    feat: 회원가입 성공/실패 기능 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -29,6 +29,7 @@ public class Http11Processor implements Runnable, Processor {
     private static final String QUERY_STRING_DELIMITER = "?";
     private static final String PARAM_DELIMITER = "&";
     private static final String KEY_VALUE_DELIMITER = "=";
+    private static final String CONTENT_LENGTH = "Content-Length";
 
     private final Socket connection;
 
@@ -50,7 +51,16 @@ public class Http11Processor implements Runnable, Processor {
              final var outputStream = connection.getOutputStream()) {
 
             String[] startLineTokens = bufferedReader.readLine().split(START_LINE_DELIMITER);
+            HttpMethod httpMethod = HttpMethod.valueOf(startLineTokens[0]); // HttpMethod 잘못 입력 시 예외가 발생함.(인지중)
             String httpUrl = startLineTokens[1];
+            HttpVersion httpVersion = HttpVersion.getByString(startLineTokens[2]); // ENUM과 맞지 않는 값 입력 시 예외 발생 (인지중)
+
+            log.info("{}, {}, {}", httpMethod.name(), httpUrl, httpVersion.getValue());
+            // 헤더 가져오기
+            Map<String, String> headers = parseHeaders(bufferedReader);
+
+            // body 가져오기
+            String httpBody = parseBody(bufferedReader, headers.get(CONTENT_LENGTH));
 
             if (httpUrl.startsWith("/index.html")) {
                 final String body = readFile("static/index.html");
@@ -61,8 +71,18 @@ public class Http11Processor implements Runnable, Processor {
                 return;
             }
 
-            if (httpUrl.startsWith("/login")) {
-                Map<String, String> queryParams = parseQueryParams(startLineTokens);
+            if (httpUrl.startsWith("/login") && httpMethod == HttpMethod.GET) {
+                final String body = readFile("static/login.html");
+                final var response = createResponse(body, CONTENT_TYPE_TEXT_HTML);
+
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+
+                return;
+            }
+
+            if (httpUrl.startsWith("/login") && httpMethod == HttpMethod.POST) {
+                Map<String, String> queryParams = parseQueryParam(httpBody);
 
                 String account = queryParams.get("account");
                 String password = queryParams.get("password");
@@ -72,7 +92,7 @@ public class Http11Processor implements Runnable, Processor {
                             .filter(user -> user.checkPassword(password));
                     if (loginUser.isPresent()) {
                         User user = loginUser.get();
-                        log.info("{}", user);
+                        log.info("로그인 성공! 아이디 : {}", user.getAccount());
 
                         String response = createRedirectResponse("/index.html");
                         outputStream.write(response.getBytes());
@@ -87,13 +107,31 @@ public class Http11Processor implements Runnable, Processor {
 
                     return;
                 }
+            }
 
-                final String body = readFile("static/login.html");
+            if (httpUrl.startsWith("/register") && httpMethod == HttpMethod.GET) {
+                final String body = readFile("static/register.html");
                 final var response = createResponse(body, CONTENT_TYPE_TEXT_HTML);
 
                 outputStream.write(response.getBytes());
                 outputStream.flush();
+                return;
+            }
 
+            if (httpUrl.startsWith("/register") && httpMethod == HttpMethod.POST) {
+                final var response = createRedirectResponse("/index.html");
+                Map<String, String> queryParams = parseQueryParam(httpBody);
+
+                String account = queryParams.get("account");
+                String email = queryParams.get("email");
+                String password = queryParams.get("password");
+
+                if (account != null && email != null && password != null) {
+                    InMemoryUserRepository.save(new User(account, password, email)); // 중복 검사 하지 않음! (인지중)
+                }
+
+                outputStream.write(response.getBytes());
+                outputStream.flush();
                 return;
             }
 
@@ -159,6 +197,46 @@ public class Http11Processor implements Runnable, Processor {
         }
     }
 
+    private String parseBody(BufferedReader bufferedReader, String contentLengthString) throws IOException {
+        if (contentLengthString == null) {
+            return "";
+        }
+        int contentLength = Integer.parseInt(contentLengthString);
+
+        if (contentLength > 0) {
+            char[] buffer = new char[contentLength];
+            int totalRead = 0;
+
+            while (totalRead < contentLength) {
+                int read = bufferedReader.read(buffer, totalRead, contentLength - totalRead);
+                if (read == -1) {
+                    throw new IOException("Content-Length와 Body 길이가 일치하지 않습니다.");
+                }
+                totalRead += read;
+            }
+
+            return new String(buffer);
+        }
+        return "";
+    }
+
+    private Map<String, String> parseHeaders(BufferedReader bufferedReader) throws IOException {
+        HashMap<String, String> headers = new HashMap<>();
+        String line;
+        while (!(line = bufferedReader.readLine()).isEmpty()) {
+            // 가장 왼쪽의 콜론을 기준으로 파싱한다.
+            int firstColonIndex = line.indexOf(':');
+            if (firstColonIndex == -1) {
+                throw new IllegalArgumentException("헤더 양식이 잘못되었습니다.");
+            }
+
+            String key = line.substring(0, firstColonIndex).strip();
+            String value = line.substring(firstColonIndex + 1).strip();
+            headers.put(key, value);
+        }
+        return headers;
+    }
+
     private String createRedirectResponse(String redirectUrl) {
         return String.join("\r\n",
                 "HTTP/1.1 302 Found ",
@@ -188,6 +266,10 @@ public class Http11Processor implements Runnable, Processor {
         }
 
         String queryLine = queryParamLine[1].substring(queryStringDelimiterIndex + 1);
+        return parseQueryParam(queryLine);
+    }
+
+    private Map<String, String> parseQueryParam(String queryLine) {
         String[] params = queryLine.split(PARAM_DELIMITER);
 
         Map<String, String> queries = new HashMap<>();
@@ -195,7 +277,6 @@ public class Http11Processor implements Runnable, Processor {
             String[] keyToken = param.split(KEY_VALUE_DELIMITER);
             queries.put(keyToken[0], keyToken[1]);
         }
-
         return queries;
     }
 }

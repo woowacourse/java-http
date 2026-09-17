1a5068cb23c352ea28212c31e7b486ff5d04547e
wontop02 <wjy2002kr@gmail.com>
2026-09-18 02:58:15 +0900

    feat: 동작하는 코드 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,23 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,11 +40,71 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            // 요청 헤더 읽기
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            final StringBuilder builder = new StringBuilder();
 
+            String line;
+            while (!(line = reader.readLine()).isBlank()) {
+                builder.append(line);
+                builder.append("\r\n");
+            }
+
+            String request = builder.toString();
+            if (request.isBlank()) {
+                return;
+            }
+
+            // 경로, 타입, 바디 가져옴
+            String uri = request.split(" ")[1];
+            log.info("uri: {}", uri);
+
+            String type = "html";
+            if (uri.contains(".")) {
+                type = List.of(uri.split("\\.")).getLast();
+            }
+            log.info("type: {}", type);
+
+            String queryString = "";
+            if (uri.contains("?")) {
+                queryString = List.of(uri.split("\\?")).getLast();
+            }
+
+            log.info("queryString: {}", queryString);
+
+            Map<String, String> pairs = new LinkedHashMap<>();
+            if (!queryString.isBlank()) {
+                uri = List.of(uri.split("\\?")).getFirst() + ".html";
+                List<String> queries = List.of(queryString.split("&"));
+
+                for (String query : queries) {
+                    String[] pair = query.split("=");
+                    log.info("pair[0]: {}", pair[0]);
+                    log.info("pair[1]: {}", pair[1]);
+                    pairs.put(pair[0], pair[1]);
+                }
+                Optional<User> user = InMemoryUserRepository.findByAccount(pairs.get("account"));
+                if (user.isPresent() && user.get().checkPassword(pairs.get("password"))) {
+                    log.info("user : {}", user.get());
+                }
+            }
+
+            var responseBody = "Hello world!";
+            if (!uri.equals("/") && !uri.isBlank()) {
+                URL resource = getClass().getClassLoader().getResource("static" + uri);
+//                log.info("resource = {}", resource);
+
+                final String filePath = resource.getFile();
+                final Path path = Paths.get(filePath);
+//                log.info("path = {}", path);
+
+                responseBody = Files.readString(path);
+            }
+
+            // 응답 생성
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Type: text/" + type + ";charset=utf-8 ",
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
                     responseBody);

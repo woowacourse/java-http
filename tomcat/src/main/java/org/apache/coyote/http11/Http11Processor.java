4e69d03e9f20594e38ad951eae32b969495e778e
박병훈 <leafloveu@naver.com>
2026-09-18 04:28:52 +0900

    feat: query string 파싱 로그인 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,15 +1,17 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
-import org.apache.coyote.Processor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.techcourse.model.User;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.coyote.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -37,43 +39,44 @@ public class Http11Processor implements Runnable, Processor {
             String[] tokens = requestLine.split(" ");
 
             String method = tokens[0];
-            String path = tokens[1];           
+            String path = tokens[1];
 
-            Map<String, String> headerMap = new HashMap<>();
+            Map<String, String> params = new HashMap<>();
+            if(path.contains("?")) {
+                String[] paramTokens = path.split("\\?");
+                path = paramTokens[0];
 
-            String header;
-            while(!(header=reader.readLine()).isEmpty()){
-                int index=header.indexOf(":");
-                String name=header.substring(0, index);
-                String value=header.substring(index+1).trim();
-                headerMap.put(name, value);
+                for(String param : paramTokens[1].split("&")) {
+                    String[] pair = param.split("=");
+                    if(pair.length == 2) {
+                        params.put(pair[0], pair[1]);
+                    }
+                }
             }
-            
+
             if (method.equals("GET") && path.equals("/index.html")) {
-                final var fileStream=getClass()
-                        .getClassLoader()
-                        .getResourceAsStream("static/index.html");
-                final var fileBytes=fileStream.readAllBytes();
-                final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + fileBytes.length + " ",
-                    "",
-                    new String(fileBytes));
+                final var fileStream = getClass().getClassLoader().getResourceAsStream("static/index.html");
+                final var fileBytes = fileStream.readAllBytes();
+                final var response = String.join(
+                        "\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + fileBytes.length + " ",
+                        "",
+                        new String(fileBytes));
 
                 outputStream.write(response.getBytes());
                 outputStream.flush();
                 return;
             }
-            
+
             if (method.equals("GET") && path.equals("/css/styles.css")) {
-                final var fileStream = getClass()
-                        .getClassLoader()
-                        .getResourceAsStream("static/css/styles.css");
+                final var fileStream = getClass().getClassLoader().getResourceAsStream("static/css/styles.css");
 
                 final var fileBytes = fileStream.readAllBytes();
 
-                final var response = String.join("\r\n",
+                final var response = String.join(
+                        "\r\n",
                         "HTTP/1.1 200 OK ",
                         "Content-Type: text/css;charset=utf-8 ",
                         "Content-Length: " + fileBytes.length + " ",
@@ -85,9 +88,40 @@ public class Http11Processor implements Runnable, Processor {
                 return;
             }
 
+            if(method.equals("GET") && path.equals("/login")) {
+                User user = InMemoryUserRepository.findByAccount(params.get("account"))
+                        .orElseThrow(() -> new RuntimeException("아이디 또는 비밀번호가 틀렸습니다."));
+
+                if(!user.checkPassword(params.get("password"))) {
+                    throw new RuntimeException("아이디 또는 비밀번호가 틀렸습니다.");
+                }
+
+                log.info(user.toString());
+
+                final var fileStream = getClass()
+                        .getClassLoader()
+                        .getResourceAsStream("static/login.html");
+
+                final var fileBytes = fileStream.readAllBytes();
+
+                final var response = String.join(
+                        "\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + fileBytes.length + " ",
+                        "",
+                        new String(fileBytes));
+
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+                return;
+            }
+
+
             final var responseBody = "Hello world!";
 
-            final var response = String.join("\r\n",
+            final var response = String.join(
+                    "\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",
                     "Content-Length: " + responseBody.getBytes().length + " ",

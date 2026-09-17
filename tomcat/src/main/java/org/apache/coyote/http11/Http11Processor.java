6cde8152fec60c822a5db08b77c6c04ef18a2378
Jinriro <riro12.hanyang.ac.kr>
2026-09-17 15:20:40 +0900

    feat: GET /index.html 정적 페이지 응답 지원

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,8 +5,15 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,16 +36,50 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = reader.readLine();
+
+            if (line == null) {
+                return;
+            }
+
+            String[] requestLine = line.split(" ");
+
+            String method = requestLine[0];
+            String path = requestLine[1];
+            String version = requestLine[2];
+
+            String header;
+
+            while ((header = reader.readLine()) != null && !header.isEmpty()) {
+                System.out.println(header);
+            }
+
+            // 추후 requestLine(method/path) 검증 추가 예정
+
+            URL resource = getClass()
+                    .getClassLoader()
+                    .getResource("static" + path);
+
+            if (resource == null) {
+                return;
+            }
+
+            byte[] body;
+            try (InputStream resourceStream = resource.openStream()) {
+                body = resourceStream.readAllBytes();
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "Content-Length: " + body.length + " ",
                     "",
-                    responseBody);
+                    ""
+            );
 
-            outputStream.write(response.getBytes());
+            outputStream.write(response.getBytes(StandardCharsets.UTF_8));
+            outputStream.write(body);
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);

69422877f656ffd27a48fd01fb9396670f5db254
gahyeonnni <hyeonnni@naver.com>
2026-09-18 15:07:51 +0900

    refactor: process의 책임을 메서드로 분리
    요청 파싱, 로그인 처리, 정적 리소스 응답, 응답 조립을 모두 담당하고 있어
    createResponse, parsePath, login, createResourceResponse, buildResponse로
    분리한다.
    
    statusLine, responseBody, contentType을 분기마다 덮어쓰는 대신 각 메서드가
    완성된 응답을 반환하도록 바꿔 가변 변수를 제거하고 중첩된 조건문을 없앤다.

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -20,7 +20,13 @@ public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
 
+    private static final String OK = "HTTP/1.1 200 OK ";
+    private static final String NOT_FOUND = "HTTP/1.1 404 Not Found ";
+    private static final String DEFAULT_CONTENT_TYPE = "text/html";
+    private static final String DEFAULT_BODY = "Hello world!";
     private static final String NOT_FOUND_PAGE = "static/404.html";
+    private static final String LOGIN_PATH = "/login";
+    private static final String ROOT_PATH = "/";
 
     private final Socket connection;
 
@@ -38,53 +44,10 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream();
-            final var bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+             final var bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
 
-            String requestLine = bufferedReader.readLine();
-
-            String statusLine = "HTTP/1.1 200 OK ";
-            String responseBody = "Hello world!";
-            String contentType = "text/html";
-
-            if (requestLine != null) {
-                String uri = requestLine.split(" ")[1];
-
-                String path = uri;
-                String queryString = "";
-                int index = uri.indexOf("?");
-
-                if (index != -1) {
-                    path = uri.substring(0, index);
-                    queryString = uri.substring(index + 1);
-                }
-
-                if (path.equals("/login") && !queryString.isEmpty()) {
-                    final Map<String, String> params = parseQueryString(queryString);
-                    InMemoryUserRepository.findByAccount(params.get("account"))
-                            .filter(user -> user.checkPassword(params.get("password")))
-                            .ifPresent(user -> log.info("{}", user));
-                }
-
-                if (!path.equals("/")) {
-                    final String resourcePath = toResourcePath(path);
-                    final URL resource = getClass().getClassLoader().getResource(resourcePath);
-                    if (resource != null) {
-                        responseBody = readResource(resource);
-                        contentType = getContentType(resourcePath);
-                    } else {
-                        statusLine = "HTTP/1.1 404 Not Found ";
-                        final URL notFound = getClass().getClassLoader().getResource(NOT_FOUND_PAGE);
-                        responseBody = readResource(notFound);
-                    }
-                }
-            }
-
-            final var response = String.join("\r\n",
-                    statusLine,
-                    "Content-Type: " + contentType + ";charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            final String requestLine = bufferedReader.readLine();
+            final String response = createResponse(requestLine);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
@@ -93,12 +56,79 @@ public class Http11Processor implements Runnable, Processor {
         }
     }
 
-    private String getContentType(String string) {
-        if (string.endsWith(".css"))
+    private String createResponse(final String requestLine) throws IOException {
+        if (requestLine == null) {
+            return buildResponse(OK, DEFAULT_CONTENT_TYPE, DEFAULT_BODY);
+        }
+
+        final String uri = requestLine.split(" ")[1];
+        final String path = parsePath(uri);
+        final String queryString = parseQueryString(uri);
+
+        login(path, queryString);
+
+        return createResourceResponse(path);
+    }
+
+    private String parsePath(final String uri) {
+        final int index = uri.indexOf("?");
+        if (index == -1) {
+            return uri;
+        }
+        return uri.substring(0, index);
+    }
+
+    private String parseQueryString(final String uri) {
+        final int index = uri.indexOf("?");
+        if (index == -1) {
+            return "";
+        }
+        return uri.substring(index + 1);
+    }
+
+    private void login(final String path, final String queryString) {
+        if (!path.equals(LOGIN_PATH) || queryString.isEmpty()) {
+            return;
+        }
+
+        final Map<String, String> params = parseParams(queryString);
+        InMemoryUserRepository.findByAccount(params.get("account"))
+                .filter(user -> user.checkPassword(params.get("password")))
+                .ifPresent(user -> log.info("{}", user));
+    }
+
+    private String createResourceResponse(final String path) throws IOException {
+        if (path.equals(ROOT_PATH)) {
+            return buildResponse(OK, DEFAULT_CONTENT_TYPE, DEFAULT_BODY);
+        }
+
+        final String resourcePath = toResourcePath(path);
+        final URL resource = getClass().getClassLoader().getResource(resourcePath);
+        if (resource == null) {
+            final URL notFound = getClass().getClassLoader().getResource(NOT_FOUND_PAGE);
+            return buildResponse(NOT_FOUND, DEFAULT_CONTENT_TYPE, readResource(notFound));
+        }
+
+        return buildResponse(OK, getContentType(resourcePath), readResource(resource));
+    }
+
+    private String buildResponse(final String statusLine, final String contentType, final String body) {
+        return String.join("\r\n",
+                statusLine,
+                "Content-Type: " + contentType + ";charset=utf-8 ",
+                "Content-Length: " + body.getBytes().length + " ",
+                "",
+                body);
+    }
+
+    private String getContentType(final String resourcePath) {
+        if (resourcePath.endsWith(".css")) {
             return "text/css";
-        if (string.endsWith(".js"))
+        }
+        if (resourcePath.endsWith(".js")) {
             return "application/javascript";
-        return "text/html";
+        }
+        return DEFAULT_CONTENT_TYPE;
     }
 
     private String readResource(final URL resource) throws IOException {
@@ -112,7 +142,7 @@ public class Http11Processor implements Runnable, Processor {
         return "static" + path + ".html";
     }
 
-    private Map<String, String> parseQueryString(final String queryString) {
+    private Map<String, String> parseParams(final String queryString) {
         final Map<String, String> params = new HashMap<>();
         for (final String parameter : queryString.split("&")) {
             final String[] keyAndValue = parameter.split("=", 2);

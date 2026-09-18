e69b04664e4d8a0deb1f63d81ee111e239869b2d
Yeji-Kim-Erica <yejikim.erica@gmail.com>
2026-09-18 21:58:12 +0900

    feat: GET /index.html 응답하기

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,8 +5,12 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -27,9 +31,15 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+             final BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
 
-            final var responseBody = "Hello world!";
+            final String requestLine = bufferedReader.readLine();
+            final String[] requestLineComponents = requestLine.split(" ");
+            final String requestURI = requestLineComponents[1];
+
+            final var responseBody = readResource(requestURI);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -43,5 +53,17 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String readResource(String requestURI) throws IOException {
+        if (requestURI.matches("/")) {
+            return "Hello world!";
+        }
+
+        // 자바 프로젝트를 빌드하면 src/main/resources 폴더 안에 있는 파일들이 빌드 결과물(클래스패스)의 최상위 루트로 복사된다. 따라서 앞에 /static을 붙여줘야 함
+        final String resourceURL = getClass().getClassLoader().getResource("static" + requestURI).getPath();
+        final Path path = Path.of(resourceURL);
+
+        return Files.readString(path);
     }
 }

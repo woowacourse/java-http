0210e4496b051572f8b0e5df1f84009eb7eb3edc
Suyeon Lee <choubung@sookmyung.ac.kr>
2026-09-18 21:48:57 +0900

    feat: 확장자에 따른 contentType 지정과 로그인 log 출력 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,9 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
+import javassist.NotFoundException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,12 +15,14 @@ import java.net.Socket;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
     private static final String ROOT = "static";
+    private static final String CONTENT_TYPE_HTML = "text/html";
+    private static final String CONTENT_TYPE_CSS = "text/css";
+    private static final String CONTENT_TYPE_JS = "application/javascript";
 
     private final Socket connection;
 
@@ -40,21 +45,43 @@ public class Http11Processor implements Runnable, Processor {
             String requestLine = br.readLine();
 
             var responseBody = "Hello world!";
+            String contentType = CONTENT_TYPE_HTML;
 
             if (requestLine != null) {
                 String[] words = requestLine.split(" ");
 
                 if (!"/".equals(words[1])) {
-                    var resource = ClassLoader.getSystemResource(ROOT + words[1]);
-                    Path path = Path.of(resource.toURI());
+                    String uri = words[1];
+                    String uriForPath = uri;
+                    int index = uri.indexOf("?");
 
+                    if (uri.startsWith("/login")) {
+                        uriForPath = "/login.html";
+                    }
+
+                    if (index != -1) {
+                        String pathFromUri = uri.substring(0, index);
+                        String queryString = uri.substring(index + 1);
+
+                        if(pathFromUri.equals("/login") && !"".equals(queryString)) {
+                            login(queryString);
+                        }
+                    }
+
+                    var resource = ClassLoader.getSystemResource(ROOT + uriForPath);
+                    Path path = Path.of(resource.toURI());
                     responseBody = Files.readString(path);
+
+                    String[] splitUrl = uriForPath.split("\\.");
+                    if (splitUrl.length > 1) {
+                        contentType = findContentType(splitUrl[1]);
+                    }
                 }
             }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Type: " + contentType + ";charset=utf-8 ",
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
                     responseBody);
@@ -64,5 +91,41 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private void login(String query) {
+        String account = parseLoginQuery(query, "account");
+        String password = parseLoginQuery(query, "password");
+
+        User user = InMemoryUserRepository.findByAccount(account)
+                .orElseThrow();
+
+        if (user.checkPassword(password)) {
+            log.info("user : {}", user.toString());
+        }
+    }
+
+    private String parseLoginQuery(String query, String target) {
+        String[] splitQuery = query.split("\\&");
+        for (String q : splitQuery) {
+            String[] keyAndValue = q.split("=");
+            if (keyAndValue[0].startsWith(target)) {
+                return keyAndValue[1];
+            }
+        }
+
+        throw new IllegalArgumentException();
+    }
+
+    private String findContentType(String extension) {
+        if (extension.equals("css")) {
+            return CONTENT_TYPE_CSS;
+        }
+
+        if (extension.equals("js")) {
+            return CONTENT_TYPE_JS;
+        }
+
+        return CONTENT_TYPE_HTML;
     }
 }

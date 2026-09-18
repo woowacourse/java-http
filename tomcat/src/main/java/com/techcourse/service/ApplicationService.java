d0962cf1f6139baf4ce6eb6b35fce15fcddd2430
Jeshin Kim <jjason0904@cau.ac.kr>
2026-09-18 09:47:07 +0900

    refactor(ApplicationService): 미사용 import 제거

--- tomcat/src/main/java/com/techcourse/service/ApplicationService.java
+++ tomcat/src/main/java/com/techcourse/service/ApplicationService.java
@@ -3,7 +3,6 @@ package com.techcourse.service;
 import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.model.User;
 import java.util.Optional;
-import org.apache.catalina.Session;
 
 public class ApplicationService {
 

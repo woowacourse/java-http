90897b804352aceffb857f48318ae69437f8a84b
sauter001 <lsj457@naver.com>
2026-09-18 18:20:05 +0900

    feat: SVG 파일 MIME 타입 추가

--- tomcat/src/main/java/org/apache/coyote/MimeType.java
+++ tomcat/src/main/java/org/apache/coyote/MimeType.java
@@ -7,6 +7,7 @@ public enum MimeType {
     TEXT_HTML("text/html;charset=utf-8", "html"),
     TEXT_CSS("text/css;charset=utf-8", "css"),
     TEXT_JAVASCRIPT("text/javascript;charset=utf-8", "js"),
+    IMAGE_SVG("image/svg+xml", "svg"),
     ;
 
     private final String typeName;

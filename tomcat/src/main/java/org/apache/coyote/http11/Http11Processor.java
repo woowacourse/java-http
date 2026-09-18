9189feb97cfc04f29f243091e226f52cf378cf8e
symflee <symflee@gmail.com>
2026-09-18 20:37:25 +0900

    feat: 요구사항 'GET /index.html 응답하기' 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,16 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +21,7 @@ import java.net.Socket;
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final String STATIC_RESOURCE_PATH = "static";
 
     private final Socket connection;
 
@@ -29,14 +40,33 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String header = reader.readLine();
+
+            String requestTarget = header.split(" ")[1];
+
+            String responseBody = "Hello world!";
+            Path path;
+            if (!requestTarget.equals("/")){
+                String resourceName = STATIC_RESOURCE_PATH + requestTarget;
+                URL resource = getClass().getClassLoader().getResource(resourceName);
+                try {
+                    path = Path.of(resource.toURI());
+                    responseBody = Files.readString(path);
+                } catch (URISyntaxException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
-                    responseBody);
+                    responseBody
+            );
+
 
             outputStream.write(response.getBytes());
             outputStream.flush();

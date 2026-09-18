62db453df593ee24addb96188c09a2455121c2e1
Suyeon Lee <choubung@sookmyung.ac.kr>
2026-09-18 20:09:40 +0900

    feat: request 주소 매핑 응답 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,12 +5,19 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final String ROOT = "static";
 
     private final Socket connection;
 
@@ -29,7 +36,21 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            String requestLine = br.readLine();
+
+            var responseBody = "Hello world!";
+
+            if (requestLine != null) {
+                String[] words = requestLine.split(" ");
+
+                if (!"/".equals(words[1])) {
+                    var resource = ClassLoader.getSystemResource(ROOT + words[1]);
+                    Path path = Path.of(resource.toURI());
+
+                    responseBody = Files.readString(path);
+                }
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -40,7 +61,7 @@ public class Http11Processor implements Runnable, Processor {
 
             outputStream.write(response.getBytes());
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
         }
     }

3599660cce97b21b0a2ed607ba5964a564036543
이프, If <jihwan1535@nate.com>
2026-09-18 15:16:17 +0900

    [1단계 - HTTP 서버 구현하기] 이프(김지환) 미션 제출합니다. (#1156)
    * test: 학습 테스트 끝
    
    * feat: index 페이지 조회 기능 추가
    
    * feat: MIME contentType 처리
    
    * feat: login query param 파싱 추가
    
    * fix: 테스트 계약으로 인해 기존 root 경로 리다이렉션 제거

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,16 +1,26 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
-
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final String STATIC_RESOURCE_PATH = "static";
+    private static final String NOT_FOUND_RESOURCE_PATH = "static/404.html";
 
     private final Socket connection;
 
@@ -29,19 +39,101 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final var reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            final var resourcePath = extractResourcePath(reader.readLine());
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
-
-            outputStream.write(response.getBytes());
-            outputStream.flush();
+            if ("/".equals(resourcePath)) {
+                writeResponse(outputStream, "text/html", "200 OK", "Hello world!".getBytes(StandardCharsets.UTF_8));
+                return;
+            }
+            writeResource(outputStream, STATIC_RESOURCE_PATH + appendHtmlExtension(resourcePath));
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private static String extractResourcePath(final String requestLine) {
+        String uri = requestLine.split(" ")[1];
+        int index = uri.indexOf("?");
+        if (index == -1) {
+            return uri;
+        }
+
+        String path = uri.substring(0, index);
+        Map<String, String> queryParams = parseQueries(uri.substring(index + 1));
+        InMemoryUserRepository.findByAccount(queryParams.get("account"))
+                .ifPresent(user -> {
+                    if (user.checkPassword(queryParams.get("password"))) {
+                        log.info("user : {}", user);
+                    }
+                });
+        return path;
+    }
+
+    private static Map<String, String> parseQueries(String queryString) {
+        String[] queries = queryString.split("&");
+        Map<String, String> params = new HashMap<>();
+        for (String query : queries) {
+            String[] pair = query.split("=");
+            params.put(pair[0], pair[1]);
+        }
+        return params;
+    }
+
+    private void writeResource(
+            final OutputStream outputStream,
+            final String resourcePath
+    ) throws IOException {
+        try (InputStream resource = getClass().getClassLoader().getResourceAsStream(resourcePath)) {
+            if (resource == null) {
+                writeNotFoundResponse(outputStream);
+                return;
+            }
+
+            final var contentType = MimeTypeResolver.resolve(resourcePath);
+            writeResponse(outputStream, contentType, "200 OK", resource.readAllBytes());
+        }
+    }
+
+    private void writeNotFoundResponse(final OutputStream outputStream) throws IOException {
+        try (InputStream resource = Objects.requireNonNull(
+                getClass().getClassLoader().getResourceAsStream(NOT_FOUND_RESOURCE_PATH),
+                "404 페이지를 찾을 수 없습니다."
+        )) {
+            writeResponse(outputStream, "text/html", "404 Not Found", resource.readAllBytes());
+        }
+    }
+
+    private void writeResponse(
+            final OutputStream outputStream,
+            final String contentType,
+            final String status,
+            final byte[] responseBody
+    ) throws IOException {
+        final var responseHeader = String.join("\r\n",
+                "HTTP/1.1 " + status + " ",
+                "Content-Type: " + toContentTypeHeader(contentType) + " ",
+                "Content-Length: " + responseBody.length + " ",
+                "",
+                "");
+
+        outputStream.write(responseHeader.getBytes(StandardCharsets.UTF_8));
+        outputStream.write(responseBody);
+        outputStream.flush();
+    }
+
+    private String toContentTypeHeader(final String contentType) {
+        if (contentType.startsWith("text/")) {
+            return contentType + ";charset=utf-8";
+        }
+        return contentType;
+    }
+
+    private static String appendHtmlExtension(final String resourcePath) {
+        final var fileName = resourcePath.substring(resourcePath.lastIndexOf('/') + 1);
+        if (fileName.contains(".")) {
+            return resourcePath;
+        }
+        return resourcePath + ".html";
     }
 }

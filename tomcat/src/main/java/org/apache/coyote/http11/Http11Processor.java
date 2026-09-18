62a0733f2ee44e00a6f266258cafcf3233a19009
Yeji-Kim-Erica <yejikim.erica@gmail.com>
2026-09-18 23:03:17 +0900

    feat: Query String 파싱

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,8 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +13,7 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Optional;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -56,9 +59,20 @@ public class Http11Processor implements Runnable, Processor {
         }
     }
 
-    private String readResource(String requestURI) throws IOException {
+    private String readResource(final String requestURI) throws IOException {
         if (requestURI.matches("/")) {
             return "Hello world!";
+        }
+
+        if (requestURI.startsWith("/login")) {
+            if (requestURI.contains("?")) {
+                loginAndRetrieveUserInfo(requestURI);
+            }
+
+            final String resourceURL = getClass().getClassLoader().getResource("static/login.html").getPath();
+            final Path path = Path.of(resourceURL);
+
+            return Files.readString(path);
         }
 
         // 자바 프로젝트를 빌드하면 src/main/resources 폴더 안에 있는 파일들이 빌드 결과물(클래스패스)의 최상위 루트로 복사된다. 따라서 앞에 /static을 붙여줘야 함
@@ -66,5 +80,35 @@ public class Http11Processor implements Runnable, Processor {
         final Path path = Path.of(resourceURL);
 
         return Files.readString(path);
+    }
+
+    private void loginAndRetrieveUserInfo(final String requestURI) {
+        final int index = requestURI.indexOf("?");
+        final String[] loginInfos = requestURI.substring(index + 1).split("&");
+        String account = "";
+        String password = "";
+        for (String loginInfo : loginInfos) {
+            final String[] keyAndValue = loginInfo.split("=");
+            final String key = keyAndValue[0];
+            final String value = keyAndValue[1];
+            if (key.matches("account")) {
+                account = value;
+                continue;
+            }
+            if (key.matches("password")) {
+                password = value;
+            }
+        }
+
+        if (!account.isBlank() && !password.isBlank()) {
+            Optional<User> retrieveResult = InMemoryUserRepository.findByAccount(account);
+            if (retrieveResult.isEmpty()) {
+                return;
+            }
+            final User retrievedUser = retrieveResult.get();
+            if (retrievedUser.checkPassword(password)) {
+                System.out.println("user : " + retrievedUser);
+            }
+        }
     }
 }

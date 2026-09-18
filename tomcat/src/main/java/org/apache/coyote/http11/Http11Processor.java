f87ad25f31a62e1b42023af5dcf2f8ebabd991da
frombunny <rucy011121@gmail.com>
2026-09-18 15:07:05 +0900

    feat: CSS 지원하기

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -21,8 +21,11 @@ public class Http11Processor implements Runnable, Processor {
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
     public static final int REQUEST_TARGET_INDEX = 1;
     public static final String STATIC = "static";
+    private static final String CSS_EXTENSION = ".css";
     public static final String HTTP_1_1_200_OK = "HTTP/1.1 200 OK ";
     public static final String CONTENT_TYPE_TEXT_HTML_CHARSET_UTF_8 = "Content-Type: text/html;charset=utf-8 ";
+    public static final String CONTENT_TYPE_CSS = "Content-Type: text/css;charset=utf-8 ";
+
     public static final String CONTENT_LENGTH = "Content-Length: ";
     public static final String HELLO_WORLD = "Hello world!";
     public static final String HOME_PATH = "/";
@@ -57,11 +60,14 @@ public class Http11Processor implements Runnable, Processor {
                 line = bufferedReader.readLine();
             }
 
-            final String responseBody = getResponseBody(stringBuilder.toString().split(" ")[REQUEST_TARGET_INDEX]);
+            String requestTarget = stringBuilder.toString().split(" ")[REQUEST_TARGET_INDEX];
+            final String responseBody = getResponseBody(requestTarget);
+
+            String contentType = getContentType(requestTarget);
 
             final var response = String.join(CRLF,
                     HTTP_1_1_200_OK,
-                    CONTENT_TYPE_TEXT_HTML_CHARSET_UTF_8,
+                    contentType,
                     CONTENT_LENGTH + responseBody.getBytes().length + " ",
                     "",
                     responseBody);
@@ -72,6 +78,13 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException | URISyntaxException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String getContentType(String requestTarget) {
+        if (requestTarget.endsWith(CSS_EXTENSION)) {
+            return CONTENT_TYPE_CSS;
+        }
+        return CONTENT_TYPE_TEXT_HTML_CHARSET_UTF_8;
     }
 
     private String getResponseBody(String requestTarget) throws URISyntaxException, IOException {

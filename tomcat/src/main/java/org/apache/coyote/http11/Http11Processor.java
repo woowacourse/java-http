f1a18abad121a0cb26998edfd87bf46b4dfd3d7b
xxbeann <yjb4945@gmail.com>
2026-09-18 15:27:28 +0900

    feat: Accept 헤더에 따라 CSS 요청에 text/css Content-Type 응답

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -10,6 +10,8 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,7 +19,18 @@ import org.slf4j.LoggerFactory;
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+
+    private static final String CRLF = "\r\n";
     private static final String STATIC_RESOURCE_ROOT = "static";
+    private static final String NOT_FOUND_PAGE = "/404.html";
+
+    private static final String ACCEPT_HEADER = "Accept";
+    private static final String ACCEPT_ANY = "*/*";
+    private static final String TEXT_HTML = "text/html;charset=utf-8";
+    private static final String TEXT_CSS = "text/css";
+
+    private static final String STATUS_OK = "HTTP/1.1 200 OK ";
+    private static final String STATUS_NOT_FOUND = "HTTP/1.1 404 Not Found ";
 
     private final Socket connection;
 
@@ -43,23 +56,24 @@ public class Http11Processor implements Runnable, Processor {
                 return;
             }
             final var requestHeaders = readHeaders(bufferedReader);
+            final var contentType = decideContentType(requestHeaders);
             final var path = parsePath(requestLine);
 
-            var statusLine = "HTTP/1.1 200 OK ";
+            var statusLine = STATUS_OK;
             final byte[] responseBody;
             if (path.equals("/")) {
                 responseBody = "Hello world!".getBytes();
             } else {
                 var resourceUrl = findResource(path);
                 if (resourceUrl == null) {
-                    statusLine = "HTTP/1.1 404 Not Found ";
-                    resourceUrl = findResource("/404.html");
+                    statusLine = STATUS_NOT_FOUND;
+                    resourceUrl = findResource(NOT_FOUND_PAGE);
                 }
                 log.info("{} -> {}", statusLine, resourceUrl);
                 responseBody = Files.readAllBytes(Path.of(resourceUrl.toURI()));
             }
 
-            final var response = buildResponse(statusLine, "text/html;charset=utf-8", responseBody);
+            final var response = buildResponse(statusLine, contentType, responseBody);
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
@@ -75,13 +89,23 @@ public class Http11Processor implements Runnable, Processor {
         return requestLine;
     }
 
-    private String readHeaders(final BufferedReader reader) throws IOException {
-        final var headers = new StringBuilder();
+    private Map<String, String> readHeaders(final BufferedReader reader) throws IOException {
+        final Map<String, String> headers = new LinkedHashMap<>();
         String headerLine;
         while ((headerLine = reader.readLine()) != null && !headerLine.isEmpty()) {
-            headers.append(headerLine).append("\r\n");
+            final String[] nameAndValue = headerLine.split(":", 2);
+            headers.put(nameAndValue[0], nameAndValue[1].trim());
         }
-        return headers.toString();
+        log.info("{}", headers);
+        return headers;
+    }
+
+    private String decideContentType(final Map<String, String> requestHeaders) {
+        final var accept = requestHeaders.getOrDefault(ACCEPT_HEADER, ACCEPT_ANY);
+        if (accept.contains(TEXT_CSS)) {
+            return TEXT_CSS;
+        }
+        return TEXT_HTML;
     }
 
     private String parsePath(final String requestLine) {
@@ -94,7 +118,7 @@ public class Http11Processor implements Runnable, Processor {
     }
 
     private String buildResponse(final String statusLine, final String contentType, final byte[] body) {
-        return String.join("\r\n",
+        return String.join(CRLF,
                 statusLine,
                 "Content-Type: " + contentType + " ",
                 "Content-Length: " + body.length + " ",

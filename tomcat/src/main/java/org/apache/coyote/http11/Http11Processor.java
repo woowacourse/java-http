15325be26a6ca3ffca6e3858159555c9346a2749
LEEJaeHyeok97 <hazardous10@naver.com>
2026-09-18 13:21:54 +0900

    feat: 1번 미션(GET /index.html 응답하기) 수행 코드 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,16 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +20,9 @@ import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
+    private static final String STATIC = "static/";
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    public static final String WHITESPACE_REGEX = " ";
 
     private final Socket connection;
 
@@ -29,19 +41,55 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+
+            String requestLine = bufferedReader.readLine();
+            if (requestLine == null) {
+                return;
+            }
+
+            String[] parts = requestLine.split(WHITESPACE_REGEX);
+            String requestUri = parts[1].substring(1);
+
+            final String fileName = requestUri;
+
+            URL resource = getClass().getClassLoader().getResource(STATIC + fileName);
+            if (validateURLIsNull(resource, outputStream)) {
+                return;
+            }
+
+            Path path = new File(resource.getPath()).toPath();
+
+            byte[] body = Files.readAllBytes(Path.of(resource.toURI()));
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "Content-Length: " + body.length + " ",
                     "",
-                    responseBody);
+                    new String(Files.readAllBytes(path)));
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    private static boolean validateURLIsNull(URL resource, OutputStream outputStream) throws IOException {
+        if (resource == null) {
+            String notFound = String.join("\r\n",
+                "HTTP/1.1 404 Not Found ",
+                "Content-Length: 0 ",
+                "",
+                "");
+
+            outputStream.write(notFound.getBytes(StandardCharsets.UTF_8));
+            outputStream.flush();
+            return true;
+        }
+        return false;
     }
 }

95c9e75a7752036e3584bed246b71fc6facbbd6b
haeseon lee <haeseon4816@gmail.com>
2026-09-19 23:55:19 +0900

    test: 로그인 성공 및 실패 응답 검증

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import support.StubSocket;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -57,5 +59,89 @@ class Http11ProcessorTest {
                 new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
 
         assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @Test
+    void 로그인에_성공하면_인덱스_페이지를_응답한다() throws IOException {
+        // given
+        final var request = String.join("\r\n",
+                "GET /login?account=gugu&password=password HTTP/1.1",
+                "Host: localhost:8080",
+                "",
+                "");
+
+        final var socket = new StubSocket(request);
+        final var processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        final var responseBody = readResource("static/index.html");
+        final var expected = createResponse(
+                "HTTP/1.1 200 OK ",
+                responseBody
+        );
+
+        assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @Test
+    void 로그인에_실패하면_인증_실패_페이지를_응답한다() throws IOException {
+        // given
+        final var request = String.join("\r\n",
+                "GET /login?account=gugu&password=wrong HTTP/1.1",
+                "Host: localhost:8080",
+                "",
+                "");
+
+        final var socket = new StubSocket(request);
+        final var processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        final var responseBody = readResource("static/401.html");
+        final var expected = createResponse(
+                "HTTP/1.1 200 OK ",
+                responseBody
+        );
+
+        assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    private String readResource(final String path) throws IOException {
+        final var resource = getClass()
+                .getClassLoader()
+                .getResourceAsStream(path);
+
+        if (resource == null) {
+            throw new FileNotFoundException(path);
+        }
+
+        try (resource) {
+            return new String(
+                    resource.readAllBytes(),
+                    StandardCharsets.UTF_8
+            );
+        }
+    }
+
+    private String createResponse(
+            final String statusLine,
+            final String responseBody
+    ) {
+        final var contentLength = responseBody
+                .getBytes(StandardCharsets.UTF_8)
+                .length;
+
+        return String.join("\r\n",
+                statusLine,
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + contentLength + " ",
+                "",
+                responseBody
+        );
     }
 }

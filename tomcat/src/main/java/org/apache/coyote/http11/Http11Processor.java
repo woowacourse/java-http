c93f06ab493ec65cfd7c9353bafb33715d98fdb8
kcnsmoothie <202412434@pukyong.ac.kr>
2026-09-19 17:17:55 +0900

    [1단계 - HTTP 서버 구현하기] 아나키(이수지) 미션 제출합니다. (#1186)
    * feat: File, I/O Stream 학습 테스트 작성
    
    * feat: 요청을 한 줄씩 읽어들이는 기능 구현
    
    * chore: test 관련 설정 추가
    
    * feat: GET /index.html 응답하기 구현
    
    * feat: CSS 지원하기 구현
    
    * feat: Query String 파싱 구현
    
    * refactor: Content-Type에 charset을 응답 헤더 생성 시 일괄 추가
    
    * refactor: 빈 value 값도 파싱 후 검증하도록 수정

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,15 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,16 +38,73 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+
+            String request = reader.readLine();
+            if (request == null) {
+                return;
+            }
+            String requestUri = request.split(" ")[1];
+
+            String line = reader.readLine();
+
+            while (!"".equals(line)) {
+                if (line == null) {
+                    return;
+                }
+
+                line = reader.readLine();
+            }
+
+            String queryString = "";
+            int index = requestUri.indexOf("?");
+            if (index != -1) {
+                queryString = requestUri.substring(index + 1);
+                requestUri = requestUri.substring(0, index);
+            }
+
+            if (requestUri.equals("/login")) {
+                Map<String, String> params = new HashMap<>();
+                for (String param : queryString.split("&")) {
+                    String[] keyValue = param.split("=", 2);
+                    if (keyValue.length != 2 || keyValue[1].isBlank()) {
+                        continue;
+                    }
+                    params.put(keyValue[0], keyValue[1]);
+                }
+
+                InMemoryUserRepository.findByAccount(params.getOrDefault("account", ""))
+                        .filter(user -> user.checkPassword(params.get("password")))
+                        .ifPresent(user -> log.info("user : {}", user));
+
+                requestUri = "/login.html";
+            }
+
+            var responseBody = "Hello world!";
+
+            var contentType = "text/html";
+
+            if (!requestUri.equals("/")) {
+                //클래스는 클래스로더에 대한 정보를 가짐
+                //클래스로더는 파일의 위치에 대한 정보를 가짐
+                //getResource는 파일을 찾지 못하면 null을 반환함
+                URL resource = getClass().getClassLoader().getResource("static" + requestUri);
+
+                if (requestUri.endsWith(".css")) {
+                    contentType = "text/css";
+                }
+
+                responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()), StandardCharsets.UTF_8);
+            }
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "Content-Type: "+ contentType + ";charset=utf-8 ",
+                    "Content-Length: " + responseBody.getBytes(StandardCharsets.UTF_8).length + " ",
                     "",
                     responseBody);
 
-            outputStream.write(response.getBytes());
+            outputStream.write(response.getBytes(StandardCharsets.UTF_8));
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);

085061c933a7b3843ef157db136e802d995a2a7f
나의찬 <cab07036@naver.com>
2026-09-20 02:35:34 +0900

    refactor: HTTP 응답 생성을 HttpResponse로 분리
    Http11Processor가 응답 상태와 헤더, 본문만 결정하도록 변경한다.
    응답 직렬화와 리다이렉트 처리를 HttpResponse에 위임한다.

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -11,7 +11,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
@@ -41,17 +40,18 @@ public class Http11Processor implements Runnable, Processor {
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
             HttpRequest request = HttpRequest.from(reader);
+            HttpResponse response = new HttpResponse(outputStream);
 
             if (request.getMethod().equals("POST") && request.getPath().equals("/register")) {
                 register(request.getBodyParams());
-                sendRedirect(outputStream, "/index.html");
+                response.sendRedirect("/index.html");
                 return;
             }
 
             if (request.getMethod().equals("POST") && request.getPath().equals("/login")) {
                 final boolean loginSucceed = login(request.getBodyParams());
                 final String location = loginSucceed ? "/index.html" : "/401.html";
-                sendRedirect(outputStream, location);
+                response.sendRedirect(location);
                 return;
             }
 
@@ -63,15 +63,10 @@ public class Http11Processor implements Runnable, Processor {
             }
             String contentType = determineContentType(request.getPath());
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 " + statusLine + " ",
-                    "Content-Type: " + contentType + " ",
-                    "Content-Length: " + responseBody.length + " ",
-                    "",
-                    new String(responseBody));
-
-            outputStream.write(response.getBytes());
-            outputStream.flush();
+            response.setStatus(statusLine);
+            response.addHeader("Content-Type", contentType);
+            response.setBody(responseBody);
+            response.send();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
@@ -101,18 +96,6 @@ public class Http11Processor implements Runnable, Processor {
 
         log.info("로그인 성공! 아이디 : {}", foundUser.getAccount());
         return true;
-    }
-
-    private void sendRedirect(final OutputStream outputStream, final String location) throws IOException {
-        final String response = String.join("\r\n",
-                "HTTP/1.1 302 Found ",
-                "Location: " + location + " ",
-                "Content-Length: 0 ",
-                "",
-                "");
-
-        outputStream.write(response.getBytes());
-        outputStream.flush();
     }
 
     private byte[] createResponseBody(final String path) throws IOException {

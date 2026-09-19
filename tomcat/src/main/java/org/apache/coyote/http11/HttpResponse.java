d5b90c9aade20a03875a5ed75052dc0aceb17ba5
나의찬 <cab07036@naver.com>
2026-09-20 02:33:09 +0900

    feat: HTTP 응답 객체 구현
    상태 코드와 헤더, 본문을 조합해 HTTP 응답을 전송한다.
    일반 응답과 리다이렉트 응답 생성을 지원한다.

---
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpResponse.java
@@ -0,0 +1,52 @@
+package org.apache.coyote.http11;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HttpResponse {
+
+    private final OutputStream outputStream;
+    private final List<String> headers = new ArrayList<>();
+    private String statusLine;
+    private byte[] body = new byte[0];
+
+    public HttpResponse(final OutputStream outputStream) {
+        this.outputStream = outputStream;
+    }
+
+    public void setStatus(final String statusLine) {
+        this.statusLine = statusLine;
+    }
+
+    public void addHeader(final String name, final String value) {
+        headers.add(name + ": " + value);
+    }
+
+    public void setBody(final byte[] body) {
+        this.body = body;
+    }
+
+    public void sendRedirect(final String location) throws IOException {
+        setStatus("302 Found");
+        addHeader("Location", location);
+        setBody(new byte[0]);
+        send();
+    }
+
+    public void send() throws IOException {
+        final StringBuilder head = new StringBuilder();
+        head.append("HTTP/1.1 ").append(statusLine).append(" \r\n");
+        for (String header : headers) {
+            head.append(header).append(" \r\n");
+        }
+        head.append("Content-Length: ").append(body.length).append(" \r\n");
+        head.append("\r\n");
+
+        outputStream.write(head.toString().getBytes(StandardCharsets.UTF_8));
+        outputStream.write(body);
+        outputStream.flush();
+    }
+}

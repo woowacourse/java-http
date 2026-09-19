bec81c732c6724dcfb19892a5bffb5b04df65078
snow___shower <wonsb25@gmail.com>
2026-09-19 21:10:09 +0900

    [1단계 - HTTP 서버 구현하기] 소낙눈(원형빈) 미션 제출합니다. (#1158)
    * test: 파일 학습 테스트 진행
    
    Signed-off-by: snowshower <wonsb25@gmail.com>
    
    * test: 입출력 스트림 학습 테스트 진행
    
    Signed-off-by: snowshower <wonsb25@gmail.com>
    
    * chore: gradle 버전 설정
    
    Signed-off-by: snowshower <wonsb25@gmail.com>
    
    * feat: GET /index.html 응답하기 구현
    
    Signed-off-by: snowshower <wonsb25@gmail.com>
    
    * feat: CSS 지원하기 구현
    
    Signed-off-by: snowshower <wonsb25@gmail.com>
    
    * feat: Query String 파싱 구현
    
    Signed-off-by: snowshower <wonsb25@gmail.com>
    
    * fix: 로그인 파라미터 검증
    
    Signed-off-by: snowshower <wonsb25@gmail.com>
    
    * refactor: 예상하지 못한 예외 로깅 처리
    
    Signed-off-by: snowshower <wonsb25@gmail.com>
    
    * fix: 404.html 활용한 리소스 누락 처리
    
    Signed-off-by: snowshower <wonsb25@gmail.com>
    
    ---------
    
    Signed-off-by: snowshower <wonsb25@gmail.com>

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,22 @@
 package org.apache.coyote.http11;
 
-import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.db.InMemoryUserRepository;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,19 +39,134 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            final BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            final String requestLine = bufferedReader.readLine();
+            if (requestLine == null) {
+                return;
+            }
+            final String[] lines = requestLine.split(" ");
+            final String uri = lines[1];
+
+            int queryIndex = uri.indexOf("?");
+
+            String path = uri;
+            String queryString = "";
+
+            if (queryIndex != -1) {
+                path = uri.substring(0, queryIndex);
+                queryString = uri.substring(queryIndex + 1);
+            }
+
+            readHeaders(bufferedReader);
+
+            var responseBody = "Hello world!";
+            int contentLength = responseBody.getBytes().length;
+            String contentType = getContentType(path);
+            String statusLine = "HTTP/1.1 200 OK ";
+
+            if (!path.equals("/")) {
+                String resourcePath = getResourcePath(path);
+
+                byte[] fileBytes = readResource(resourcePath);
+
+                if (fileBytes == null) {
+                    fileBytes = readResource("static/404.html");
+                    if (fileBytes == null) {
+                        throw new IllegalArgumentException("404.html 리소스를 찾을 수 없습니다.");
+                    }
+                    statusLine = "HTTP/1.1 404 Not Found ";
+                } else if (path.equals("/login") && !queryString.isEmpty()) {
+                    login(queryString);
+                }
+
+                responseBody = new String(fileBytes, StandardCharsets.UTF_8);
+                contentLength = fileBytes.length;
+            }
 
             final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    statusLine,
+                    "Content-Type: " + contentType + " ",
+                    "Content-Length: " + contentLength + " ",
                     "",
                     responseBody);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
-            log.error(e.getMessage(), e);
+        } catch (IOException | URISyntaxException e) {
+            log.error("HTTP 요청 처리 중 입출력 오류가 발생했습니다.", e);
+        } catch (RuntimeException e) {
+            log.error("HTTP 요청 처리 중 예상하지 못한 오류가 발생했습니다.", e);
         }
+    }
+
+    private byte[] readResource(String resourcePath) throws IOException, URISyntaxException {
+        URL resourceUrl = getClass().getClassLoader().getResource(resourcePath);
+        if (resourceUrl == null) {
+            return null;
+        }
+        URI resourceUri = resourceUrl.toURI();
+        Path path = Paths.get(resourceUri);
+
+        return Files.readAllBytes(path);
+    }
+
+    private void readHeaders(BufferedReader bufferedReader) throws IOException {
+        String headerLine = bufferedReader.readLine();
+        while (headerLine != null && !headerLine.isEmpty()) {
+            headerLine = bufferedReader.readLine();
+            if (headerLine == null) {
+                return;
+            }
+        }
+    }
+
+    private String getResourcePath(String path) {
+        if (path.equals("/login")) {
+            return "static/login.html";
+        }
+        return "static" + path;
+    }
+
+    private void login(String queryString) {
+        Map<String, String> loginInfo = parseQueryString(queryString);
+
+        String account = loginInfo.get("account");
+        String password = loginInfo.get("password");
+
+        if (account == null || account.isBlank() || password == null || password.isBlank()) {
+            return;
+        }
+
+        var optionalUser = InMemoryUserRepository.findByAccount(account);
+
+        if (optionalUser.isPresent()) {
+            var user = optionalUser.get();
+
+            if (user.checkPassword(password)) {
+                log.info("login user: {}", user);
+            }
+        }
+    }
+
+    private Map<String, String> parseQueryString(String queryString) {
+        Map<String, String> parameters = new HashMap<>();
+
+        for (String parameter : queryString.split("&")) {
+            String[] keyValue = parameter.split("=", 2);
+
+            if (keyValue.length == 2) {
+                parameters.put(keyValue[0], keyValue[1]);
+            }
+        }
+
+        return parameters;
+    }
+
+    private String getContentType(String path) {
+        if (path.endsWith(".css")) {
+            return "text/css";
+        }
+        return "text/html;charset=utf-8";
     }
 }

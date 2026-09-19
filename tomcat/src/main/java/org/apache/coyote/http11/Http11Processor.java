53a889b9dacbc7c74cdb91e384feb58e0d86f147
Green <121741455+2Jaeheon@users.noreply.github.com>
2026-09-19 11:34:59 +0900

    [1단계 - HTTP 서버 구현하기] 초록(이재헌) 미션 제출합니다. (#1136)
    * test(study): 리소스 디렉토리의 파일 경로 찾기
    
    Signed-off-by: 2jaeheon <lchc.dev@gmail.com>
    
    * test(study): 파일의 내용 읽어오기
    
    Signed-off-by: 2jaeheon <lchc.dev@gmail.com>
    
    * test(study): outputStream의 write 메서드 알아보기
    
    Signed-off-by: 2jaeheon <lchc.dev@gmail.com>
    
    * test(study): 스트림을 닫기
    
    Signed-off-by: 2jaeheon <lchc.dev@gmail.com>
    
    * test(study): InputStream의 read로 읽어오기
    
    Signed-off-by: 2jaeheon <lchc.dev@gmail.com>
    
    * test(study): 스트림 사용 후 close 호출
    
    Signed-off-by: 2jaeheon <lchc.dev@gmail.com>
    
    * test(study): 인풋 스트림 리더 사용하기
    
    Signed-off-by: 2jaeheon <lchc.dev@gmail.com>
    
    * chore: gradle 의존성추가
    
    Signed-off-by: 2jaeheon <lchc.dev@gmail.com>
    
    * feat(index): index 페이지로 리다이렉트
    
    Signed-off-by: 2jaeheon <lchc.dev@gmail.com>
    
    * feat(index): 요청 첫 줄 가져와서 파싱
    
    Signed-off-by: 2jaeheon <lchc.dev@gmail.com>
    
    * feat(css): CSS 및 JS 가지고 오기
    
    Signed-off-by: 2jaeheon <lchc.dev@gmail.com>
    
    * feat(login): loign 페이지 나오도록 수정
    
    Signed-off-by: 2jaeheon <lchc.dev@gmail.com>
    
    * feat: queryString 파싱
    
    Signed-off-by: 2jaeheon <lchc.dev@gmail.com>
    
    * fix: js ContentType 오류 수정
    
    Signed-off-by: 2jaeheon <lchc.dev@gmail.com>
    
    ---------
    
    Signed-off-by: 2jaeheon <lchc.dev@gmail.com>

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,21 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import javax.annotation.Nonnull;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -26,22 +35,132 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
+
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-            final var responseBody = "Hello world!";
+            String readLine = bufferedReader.readLine();
+            if (readLine == null) {
+                return;
+            }
+
+            String[] requestHeader = readLine.split(" ");
+            String path = requestHeader[1];
+            String resourcePath = extractResourcePath(path);
+
+            byte[] bytes = resolveResponseBody(resourcePath);
+            String responseBody = new String(bytes, StandardCharsets.UTF_8);
+
+            String contentType = resolveContentType(resourcePath);
 
             final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "HTTP/1.1 200 OK",
+                    "Content-Type: " + contentType,
+                    "Content-Length: " + bytes.length,
                     "",
-                    responseBody);
+                    responseBody
+            );
+
+            logUser(path);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        }
+    }
+
+    private void logUser(String path) {
+        String queryString = extractQueryString(path);
+        Map<String, String> queryParams = parseQueryParams(queryString);
+
+        String account = queryParams.get("account");
+        if (account == null || account.isBlank()) {
+            return;
+        }
+
+        String password = queryParams.get("password");
+        if (password == null || password.isBlank()) {
+            return;
+        }
+
+        User user = InMemoryUserRepository.findByAccount(account)
+                .orElseThrow(() -> new NoSuchElementException("해당 계정을 찾을 수 없습니다"));
+
+        if (!user.checkPassword(password)) {
+            throw new IllegalArgumentException("아이디 또는 패스워드가 일치하지 않습니다");
+        }
+
+        log.info(String.valueOf(user));
+    }
+
+    @Nonnull
+    private static Map<String, String> parseQueryParams(String queryString) {
+        Map<String, String> queryParams = new HashMap<>();
+
+        String[] params = queryString.split("&");
+        for (String param : params) {
+            String[] values = param.split("=");
+
+            if (values.length == 2) {
+                queryParams.put(values[0], values[1]);
+            }
+        }
+
+        return queryParams;
+    }
+
+    private String resolveContentType(String resourcePath) {
+        if (resourcePath.endsWith(".css")) {
+            return "text/css;charset=utf-8";
+        }
+
+        if (resourcePath.endsWith(".js")) {
+            return "text/javascript;charset=utf-8";
+        }
+
+        return "text/html;charset=utf-8";
+    }
+
+    private byte[] resolveResponseBody(String resourcePath) throws IOException {
+        if (resourcePath.equals("/")) {
+            return "Hello world!".getBytes(StandardCharsets.UTF_8);
+        }
+
+        if (resourcePath.equals("/login")) {
+            return readResource("static" + resourcePath + ".html");
+        }
+
+        return readResource("static" + resourcePath);
+    }
+
+    private String extractResourcePath(String path) {
+        int index = path.indexOf("?");
+        if (index == -1) {
+            return path;
+        }
+
+        return path.substring(0, index);
+    }
+
+    private String extractQueryString(String path) {
+        int index = path.indexOf("?");
+        if (index == -1) {
+            return "";
+        }
+
+        return path.substring(index + 1);
+    }
+
+    private byte[] readResource(String resourcePath) throws IOException {
+        try (InputStream resourceStream = getClass().getClassLoader().getResourceAsStream(resourcePath)) {
+            if (resourceStream == null) {
+                throw new IOException(resourcePath + " 파일을 찾을 수 없습니다.");
+            }
+
+            return resourceStream.readAllBytes();
         }
     }
 }

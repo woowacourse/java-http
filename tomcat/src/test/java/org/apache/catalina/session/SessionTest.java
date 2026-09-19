25cda8632ff08b4d207ec8e3d14aa04fc3a3368c
나의찬 <cab07036@naver.com>
2026-09-20 02:56:26 +0900

    feat: 클라이언트별 Session 구현
    세션 ID와 속성 값을 저장한다.
    세션 속성의 조회와 삭제, 전체 무효화를 지원한다.

---
+++ tomcat/src/test/java/org/apache/catalina/session/SessionTest.java
@@ -0,0 +1,35 @@
+package org.apache.catalina.session;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SessionTest {
+
+    @Test
+    void 세션_ID를_반환한다() {
+        final Session session = new Session("session-id");
+
+        assertThat(session.getId()).isEqualTo("session-id");
+    }
+
+    @Test
+    void 속성을_저장하고_조회한다() {
+        final Session session = new Session("session-id");
+        final Object user = new Object();
+
+        session.setAttribute("user", user);
+
+        assertThat(session.getAttribute("user")).isSameAs(user);
+    }
+
+    @Test
+    void 속성을_삭제한다() {
+        final Session session = new Session("session-id");
+        session.setAttribute("user", new Object());
+
+        session.removeAttribute("user");
+
+        assertThat(session.getAttribute("user")).isNull();
+    }
+}

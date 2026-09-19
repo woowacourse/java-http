fcc726b661bfadf696d7bd688602a4139cce92db
나의찬 <cab07036@naver.com>
2026-09-19 16:42:35 +0900

    refactor: 로그인 성공 여부를 반환하도록 변경
    사용자 존재 여부와 비밀번호 일치 여부에 따라
    로그인 결과를 boolean 값으로 반환한다.
    
    # Conflicts:
    #	tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,21 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.Map;
+import java.util.Optional;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,19 +38,75 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String requestLine = reader.readLine();
+
+            if (requestLine == null) {
+                return;
+            }
+
+            HttpRequest request = HttpRequest.from(requestLine);
+
+            if (request.getPath().equals("/login") && !request.getQueryParams().isEmpty()) {
+                login(request.getQueryParams());
+            }
+
+            String statusLine = "200 OK";
+            byte[] responseBody = createResponseBody(request.getPath());
+            if (responseBody == null) {
+                statusLine = "404 Not Found";
+                responseBody = createResponseBody("/404.html");
+            }
+            String contentType = determineContentType(request.getPath());
 
             final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "HTTP/1.1 " + statusLine + " ",
+                    "Content-Type: " + contentType + " ",
+                    "Content-Length: " + responseBody.length + " ",
                     "",
-                    responseBody);
+                    new String(responseBody));
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private boolean login(final Map<String, String> params) {
+        final Optional<User> user = InMemoryUserRepository.findByAccount(params.get("account"));
+        if (user.isEmpty()) {
+            log.info("존재하지 않는 계정입니다.");
+            return false;
+        }
+
+        final User foundUser = user.get();
+        if (!foundUser.checkPassword(params.get("password"))) {
+            log.info("비밀번호가 일치하지 않습니다.");
+            return false;
+        }
+
+        log.info("로그인 성공! 아이디 : {}", foundUser.getAccount());
+        return true;
+    }
+
+    private byte[] createResponseBody(final String path) throws IOException {
+        if (path.equals("/")) {
+            return "Hello world!".getBytes();
+        }
+        final String resourcePath = path.equals("/login") ? "/login.html" : path;
+        final URL resource = getClass().getClassLoader().getResource("static" + resourcePath);
+        if (resource == null) {
+            return null;
+        }
+        final File file = new File(resource.getFile());
+        return Files.readAllBytes(file.toPath());
+    }
+
+    private String determineContentType(final String path) {
+        if (path.endsWith(".css")) {
+            return "text/css;charset=utf-8";
+        }
+        return "text/html;charset=utf-8";
     }
 }

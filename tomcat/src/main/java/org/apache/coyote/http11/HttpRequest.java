750f168dfcbef571d69c553ec45a7eab1a4ad9d2
JYL35 <154980427+JYL35@users.noreply.github.com>
2026-09-19 19:40:22 +0900

    refactor: HttpRequestUri를 HttpRequest로 통합

--- tomcat/src/main/java/org/apache/coyote/http11/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpRequest.java
@@ -1,45 +1,72 @@
 package org.apache.coyote.http11;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class HttpRequest {
 
     private final String ALLOWED_METHOD = "GET";
     private final String ALLOWED_VERSION = "HTTP/1.1";
+    private static final String ROOT_PATH = "/";
+    private static final String STATIC_TARGET_PATH = "static";
 
     private final String method;
-    private final HttpRequestUri httpRequestUri;
+    private final String path;
+    private final Map<String, String> queryParameters;
     private final String version;
 
-    private HttpRequest(String method, HttpRequestUri httpRequestUri, String version) {
+    private HttpRequest(String method, String path, Map<String, String> queryParameters, String version) {
         validate(method, version);
         this.method = method;
-        this.httpRequestUri = httpRequestUri;
+        this.path = path;
+        this.queryParameters = queryParameters;
         this.version = version;
     }
 
     public static HttpRequest from(String startLine) {
         StringTokenizer stringTokenizer = new StringTokenizer(startLine, " ");
         String method = stringTokenizer.nextToken();
-        HttpRequestUri httpRequestUri = HttpRequestUri.from(stringTokenizer.nextToken());
+        String requestUri = stringTokenizer.nextToken();
         String version = stringTokenizer.nextToken();
-        return new HttpRequest(method, httpRequestUri, version);
+
+        int index = requestUri.indexOf("?");
+        if (index == -1) {
+            return new HttpRequest(method, requestUri, Map.of(), version);
+        }
+        String path = requestUri.substring(0, index) + ".html";
+        Map<String, String> queryParameters = parseQueryParameters(requestUri.substring(index + 1));
+        return new HttpRequest(method, path, queryParameters, version);
     }
 
     public boolean isRoot() {
-        return httpRequestUri.isRoot();
+        return ROOT_PATH.equals(path);
     }
 
     public boolean isParameterEmpty() {
-        return httpRequestUri.isParameterEmpty();
+        return queryParameters.isEmpty();
     }
 
     public String getParameter(String key) {
-        return httpRequestUri.getParameter(key);
+        return queryParameters.get(key);
     }
 
     public String getResourcePath() {
-        return httpRequestUri.getResourcePath();
+        return STATIC_TARGET_PATH + path;
+    }
+
+    private static Map<String, String> parseQueryParameters(String queryString) {
+        Map<String, String> queryParameters = new HashMap<>();
+        StringTokenizer stringTokenizer = new StringTokenizer(queryString, "&");
+
+        while (stringTokenizer.hasMoreTokens()) {
+            String parameter = stringTokenizer.nextToken();
+            int index = parameter.indexOf("=");
+            String key = parameter.substring(0, index);
+            String value = parameter.substring(index + 1);
+            queryParameters.put(key, value);
+        }
+        return queryParameters;
     }
 
     private void validate(String method, String version) {

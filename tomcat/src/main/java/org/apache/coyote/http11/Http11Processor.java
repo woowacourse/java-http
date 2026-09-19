dbb14d70a6a54c4d2126efa6a6f6381aafde8987
Sunwook Lee <85978148+sun007021@users.noreply.github.com>
2026-09-19 16:52:36 +0900

    [1단계 - HTTP 서버 구현하기] 아론(이선욱) 미션 제출합니다. (#1198)
    * feat: tomcat 정적 파일 반환 구현
    
    * feat: css 지원 추가
    
    * fix: Query String 파싱 login에서 없는 경우 분기 처리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,16 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,19 +39,61 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            final BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            final String uri = bufferedReader.readLine().split(" ")[1].substring(1);
+            int index = uri.indexOf("?");
+            String path = uri;
+            String queryString = "";
+            if (index != -1) {
+                path = uri.substring(0, index);
+                queryString = uri.substring(index + 1);
+            }
+            HashMap<String, String> params = parseQueryString(queryString);
 
+            if (path.equals("login")) {
+                path = "login.html";
+                if (params.containsKey("account")) {
+                    InMemoryUserRepository.findByAccount(params.get("account"))
+                            .filter(user -> user.checkPassword(params.get("password")))
+                            .ifPresent(user -> log.info("user : {}", user));
+                }
+            }
+
+            final URL url = getClass().getClassLoader().getResource("static/" + path);
+
+            final var responseBody = Files.readAllBytes(Path.of(url.toURI()));
+
+            final String contentType = path.substring(path.lastIndexOf(".") + 1);
+            String responseContentType = "text/html;charset=utf-8";
+            if (contentType.equals("css")) {
+                responseContentType = "text/css;charset=utf-8";
+            }
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "Content-Type: " + responseContentType + " ",
+                    "Content-Length: " + responseBody.length + " ",
                     "",
-                    responseBody);
+                    new String(responseBody));
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    private HashMap<String, String> parseQueryString(String queryString) {
+        HashMap<String, String> params = new HashMap<>();
+        String[] pairs = queryString.split("&");
+        for (String pair : pairs) {
+            String[] keyValue = pair.split("=");
+            if (keyValue.length == 2) {
+                params.put(keyValue[0], keyValue[1]);
+            }
+        }
+        return params;
     }
 }

7e6fc50f98edb5fddd4999c8ca9b72cc24140295
이재혁 <hazardous10@naver.com>
2026-09-19 19:28:33 +0900

    [1단계 - HTTP 서버 구현하기] 누누(이재혁) 미션 제출합니다. (#1190)
    * feat: IOStreamTest 학습 테스트 학습 테스트 통과 완료
    
    * feat: FileTest 테스트 통과를 위한 todo 코드 추가
    
    * feat: 1번 미션(GET /index.html 응답하기) 수행 코드 추가
    
    * feat: Root request 응답 분기 코드 추가
    
    * feat: 2.CSS 지원하기 구현 내용 추가
    
    * feat: 3단계 쿼리 파싱. 로그인 창 렌더링 구현
    
    * feat: 3단계 쿼리 스트링. 로그인 패스워드 검증과 로깅 추가
    
    * refactor: createResponse의 http 헤더 값 중복 제거
    
    * feat: /login 엔드포인트 접속 시 login.html 렌더링 하도록 수정

--- study/src/test/java/study/IOStreamTest.java
+++ study/src/test/java/study/IOStreamTest.java
@@ -53,6 +53,7 @@ class IOStreamTest {
              * todo
              * OutputStream 객체의 write 메서드를 사용해서 테스트를 통과시킨다
              */
+            outputStream.write(bytes);
 
             final String actual = outputStream.toString();
 
@@ -79,6 +80,7 @@ class IOStreamTest {
              * ByteArrayOutputStream과 어떤 차이가 있을까?
              */
 
+            outputStream.flush(); // 버퍼가 아직 가득 차지 않은 상황에서 버퍼의 내용을 강제로 전송
             verify(outputStream, atLeastOnce()).flush();
             outputStream.close();
         }
@@ -96,6 +98,12 @@ class IOStreamTest {
              * try-with-resources를 사용한다.
              * java 9 이상에서는 변수를 try-with-resources로 처리할 수 있다.
              */
+
+            try (outputStream) {
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
             verify(outputStream, atLeastOnce()).close();
         }
@@ -128,7 +136,7 @@ class IOStreamTest {
              * todo
              * inputStream에서 바이트로 반환한 값을 문자열로 어떻게 바꿀까?
              */
-            final String actual = "";
+            final String actual = new String(inputStream.readAllBytes());
 
             assertThat(actual).isEqualTo("🤩");
             assertThat(inputStream.read()).isEqualTo(-1);
@@ -148,6 +156,9 @@ class IOStreamTest {
              * try-with-resources를 사용한다.
              * java 9 이상에서는 변수를 try-with-resources로 처리할 수 있다.
              */
+            try(inputStream) {
+
+            }
 
             verify(inputStream, atLeastOnce()).close();
         }
@@ -169,12 +180,12 @@ class IOStreamTest {
          * 버퍼 크기를 지정하지 않으면 버퍼의 기본 사이즈는 얼마일까?
          */
         @Test
-        void 필터인_BufferedInputStream를_사용해보자() {
+        void 필터인_BufferedInputStream를_사용해보자() throws IOException {
             final String text = "필터에 연결해보자.";
             final InputStream inputStream = new ByteArrayInputStream(text.getBytes());
-            final InputStream bufferedInputStream = null;
+            final InputStream bufferedInputStream = new BufferedInputStream(inputStream);
 
-            final byte[] actual = new byte[0];
+            final byte[] actual = bufferedInputStream.readAllBytes();
 
             assertThat(bufferedInputStream).isInstanceOf(FilterInputStream.class);
             assertThat(actual).isEqualTo("필터에 연결해보자.".getBytes());
@@ -197,17 +208,24 @@ class IOStreamTest {
          * 필터인 BufferedReader를 사용하면 readLine 메서드를 사용해서 문자열(String)을 한 줄 씩 읽어올 수 있다.
          */
         @Test
-        void BufferedReader를_사용하여_문자열을_읽어온다() {
+        void BufferedReader를_사용하여_문자열을_읽어온다() throws IOException {
             final String emoji = String.join("\r\n",
                     "😀😃😄😁😆😅😂🤣🥲☺️😊",
                     "😇🙂🙃😉😌😍🥰😘😗😙😚",
                     "😋😛😝😜🤪🤨🧐🤓😎🥸🤩",
                     "");
             final InputStream inputStream = new ByteArrayInputStream(emoji.getBytes());
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
             final StringBuilder actual = new StringBuilder();
 
-            assertThat(actual).hasToString(emoji);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                actual.append(line + "\r\n");
+            }
+
+            assertThat(actual.toString()).hasToString(emoji);
         }
     }
 }
--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,17 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +21,15 @@ import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
 
+    private static final String RESOURCE_FILE_PREFIX = "static/";
+    private static final String WHITESPACE_REGEX = " ";
+
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+
+    private static final String QUESTION_MARK = "?";
+    private static final String HTML_EXTENSION = ".html";
+    private static final String CSS_EXTENSION = ".css";
+    private static final String JS_EXTENSION = ".js";
 
     private final Socket connection;
 
@@ -27,21 +46,127 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+            final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(inputStream, StandardCharsets.UTF_8));
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            String requestLine = bufferedReader.readLine();
+
+            String[] parts = requestLine.split(WHITESPACE_REGEX);
+            String part = parts[1];
+            if (isRootRequest(part, outputStream)) {
+                return;
+            }
+
+            String requestUri = part.substring(1);
+
+            URL resource = getClass().getClassLoader()
+                .getResource(RESOURCE_FILE_PREFIX + requestUri);
+
+            if (requestUri.equals("login")) {
+                resource = getClass().getClassLoader()
+                    .getResource(RESOURCE_FILE_PREFIX + requestUri + HTML_EXTENSION);
+            }
+
+            if (requestUri.contains(QUESTION_MARK)) {
+                int index = requestUri.indexOf(QUESTION_MARK);
+                part = requestUri.substring(0, index);
+                String queryString = requestUri.substring(index + 1);
+
+                resource = getClass().getClassLoader()
+                    .getResource(RESOURCE_FILE_PREFIX + part + HTML_EXTENSION);
+
+                String[] splitQuery = queryString.split("&");
+                String account = splitQuery[0].substring(8);
+                String password = splitQuery[1].substring(9);
+
+                User user = InMemoryUserRepository.findByAccount(account)
+                    .orElseThrow(() -> new IllegalStateException("등록되지 않은 계정입니다."));
+
+                user.isMatchPassword(password);
+
+                log.info("user : {}", user);
+            }
+
+            if (validateURLIsNull(resource, outputStream)) {
+                return;
+            }
+
+            Path path = new File(resource.getPath()).toPath();
+            byte[] body = Files.readAllBytes(Path.of(resource.toURI()));
+
+            String response = createResponse(part, body, path);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    private static String createResponse(String part, byte[] body, Path path) throws IOException {
+        String contentType = resolveContentType(part);
+        if (contentType == null) {
+            return "";
+        }
+
+        return String.join("\r\n",
+            "HTTP/1.1 200 OK ",
+            "Content-Type: " + contentType + ";charset=utf-8 ",
+            "Content-Length: " + body.length + " ",
+            "",
+            new String(Files.readAllBytes(path)));
+    }
+
+    private static String resolveContentType(String part) {
+        if (part.endsWith(CSS_EXTENSION)) {
+            return "text/css";
+        }
+        if (part.endsWith(JS_EXTENSION)) {
+            return "text/javascript";
+        }
+        if (part.endsWith(HTML_EXTENSION) || part.equals("login") || part.equals("/login")) {
+            return "text/html";
+        }
+
+        return null;
+    }
+
+    private static boolean isRootRequest(String part, OutputStream outputStream)
+        throws IOException {
+        if (part.equals("/")) {
+            final var responseBody = "Hello world!";
+
+            final var response = String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+
+            outputStream.write(response.getBytes());
+            outputStream.flush();
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean validateURLIsNull(URL resource, OutputStream outputStream)
+        throws IOException {
+        if (resource == null) {
+            String notFound = String.join("\r\n",
+                "HTTP/1.1 404 Not Found ",
+                "Content-Length: 0 ",
+                "",
+                "");
+
+            outputStream.write(notFound.getBytes(StandardCharsets.UTF_8));
+            outputStream.flush();
+            return true;
+        }
+        return false;
     }
 }

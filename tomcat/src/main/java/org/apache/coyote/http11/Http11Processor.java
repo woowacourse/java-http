361fc9646dce7a83e54748b0e7b712b5b5d3a9f0
Jaecheol Park <symflee@gmail.com>
2026-09-20 17:32:02 +0900

    [1단계 - HTTP 서버 구현하기] 나무 미션 제출합니다. (#1207)
    * test: FileTest 수행
    
    * test: IOStreamTest 수행
    
    * feat: 요구사항 'GET /index.html 응답하기' 구현
    
    * feat: 요구사항 'CSS 지원하기' 구현
    
    * feat: 요구사항 'Query String 파싱' 구현
    
    * refactor(Http11Processor): 메서드 분리
    
    * fix: 리소스 확장자 판별 및 예외 처리 보완

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,16 +1,36 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.Socket;
-
 public class Http11Processor implements Runnable, Processor {
-
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final String ROOT_RESOURCE_PATH = "/";
+    private static final String DEFAULT_MESSAGE = "Hello world!";
+    private static final String STATIC_RESOURCE_PATH = "static";
+    private static final String REQUEST_LINE_ELEMENT_SEPARATOR = " ";
+    private static final int REQUEST_TARGET_INDEX = 1;
+    private static final String PATH_QUERY_SEPARATOR = "?";
+    private static final String QUERY_PARAMETER_SEPARATOR = "&";
+    private static final String QUERY_PARAMETER_NAME_VALUE_SEPARATOR = "=";
+    private static final String FILE_EXTENSION_SEPARATOR = ".";
 
     private final Socket connection;
 
@@ -27,21 +47,161 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            final List<String> requestHead = readRequestHead(reader);
+            final String requestLine = extractRequestLine(requestHead);
 
-            final var responseBody = "Hello world!";
+            final String requestTarget = extractRequestTarget(requestLine);
+
+            String responseBody = resolveResponseBody(requestTarget);
+            String contentType = resolveContentType(extractTargetPath(requestTarget));
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Type: " + contentType,
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
-                    responseBody);
+                    responseBody
+            );
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private List<String> readRequestHead(BufferedReader reader) throws IOException {
+        final List<String> requestHeader = new ArrayList<>();
+
+        while (true) {
+            String currentLine = reader.readLine();
+            if (currentLine == null || currentLine.isEmpty()) {
+                break;
+            }
+            requestHeader.add(currentLine);
+        }
+        return requestHeader;
+    }
+
+    private String extractRequestLine(List<String> requestHead) {
+        if (requestHead.isEmpty()) {
+            return "";
+        }
+
+        return requestHead.getFirst();
+    }
+
+    private String extractRequestTarget(String requestLine) {
+        return requestLine.split(REQUEST_LINE_ELEMENT_SEPARATOR)[REQUEST_TARGET_INDEX];
+    }
+
+    private String resolveResponseBody(String requestTarget) {
+        if (requestTarget.equals(ROOT_RESOURCE_PATH)) {
+            return DEFAULT_MESSAGE;
+        }
+        String targetPath = extractTargetPath(requestTarget);
+
+        if (targetPath.equals("/login")) {
+            handleLogin(requestTarget);
+
+            return readResourceAsString("/login.html");
+        }
+
+        return readResourceAsString(targetPath);
+    }
+
+    private String extractTargetPath(String requestTarget) {
+        if (requestTarget.contains(PATH_QUERY_SEPARATOR)) {
+            int delimiterIndex = requestTarget.indexOf(PATH_QUERY_SEPARATOR);
+            return requestTarget.substring(0, delimiterIndex);
+        }
+        return requestTarget;
+    }
+
+    private void handleLogin(String requestTarget) {
+        String targetQueryString = extractTargetQueryString(requestTarget);
+        Map<String, String> queryParameters = parseQueryParameters(targetQueryString);
+
+        String account = queryParameters.get("account");
+        String password = queryParameters.get("password");
+
+        InMemoryUserRepository.findByAccount(account)
+                .filter(user -> user.checkPassword(password))
+                .ifPresent(user -> log.info(String.valueOf(user)));
+    }
+
+    private String extractTargetQueryString(String requestTarget) {
+        if (requestTarget.contains(PATH_QUERY_SEPARATOR)) {
+            int delimiterIndex = requestTarget.indexOf(PATH_QUERY_SEPARATOR);
+            return requestTarget.substring(delimiterIndex + PATH_QUERY_SEPARATOR.length());
+        }
+
+        return "";
+    }
+
+    private Map<String, String> parseQueryParameters(String targetQueryString) {
+        Map<String, String> queryParameters = new HashMap<>();
+
+        for (String queryParameter : targetQueryString.split(QUERY_PARAMETER_SEPARATOR)) {
+            int separatorIndex = queryParameter.indexOf(QUERY_PARAMETER_NAME_VALUE_SEPARATOR);
+
+            if (separatorIndex < 0) {
+                continue;
+            }
+
+            String name = queryParameter.substring(0, separatorIndex);
+            String value = queryParameter.substring(
+                    separatorIndex + QUERY_PARAMETER_NAME_VALUE_SEPARATOR.length()
+            );
+
+            queryParameters.put(name, value);
+        }
+        return queryParameters;
+    }
+
+    private String readResourceAsString(String resourceName) {
+        URL resource = getClass().getClassLoader().getResource(STATIC_RESOURCE_PATH + resourceName);
+        if (resource == null) {
+            return "";
+        }
+
+        try {
+            URI uri = resource.toURI();
+
+            return Files.readString(Path.of(uri));
+        } catch (IOException | URISyntaxException e) {
+            throw new UncheckedServletException(e);
+        }
+    }
+
+    private String readResourceAsStringd(String resourceName) {
+        URL resource = getClass().getClassLoader()
+                .getResource(STATIC_RESOURCE_PATH + resourceName);
+
+        if (resource == null) {
+            try {
+                throw new FileNotFoundException(resourceName);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        try {
+            return Files.readString(Path.of(resource.toURI()));
+        } catch (IOException | URISyntaxException e) {
+            throw new UncheckedServletException(e);
+        }
+    }
+
+    private String resolveContentType(String content) {
+        int index = content.lastIndexOf(FILE_EXTENSION_SEPARATOR);
+        if (index < 0) {
+            return "text/html;charset=utf-8 ";
+        }
+
+        String extension = content.substring(index + FILE_EXTENSION_SEPARATOR.length());
+        return "text/" + extension + ";charset=utf-8 ";
     }
 }

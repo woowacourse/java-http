d688e447b4aca62b3cefa28546b24b38511c88aa
koreaioi <koreaioi@gachon.ac.kr>
2026-09-20 19:37:09 +0900

    feature: RequestBody 추가
    - RequestBodyTest 테스트 코드 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/request/RequestBody.java
@@ -0,0 +1,30 @@
+package org.apache.coyote.http11.request;
+
+public class RequestBody {
+
+    private static final String FORM_URLENCODED = "application/x-www-form-urlencoded";
+
+    private final String value;
+    private final String contentType;
+
+    public RequestBody(String value, String contentType) {
+        this.value = value;
+        this.contentType = contentType;
+    }
+
+    public String getValue(String key) {
+        return parse().getValue(key);
+    }
+
+    /**
+     * NOTE
+     * Tomcat 미션에서는 application/json으로 오는 json형식 RequestBody를 지원하지 않는다.
+     * */
+    private QueryParams parse() {
+        if (contentType == null || contentType.equals(FORM_URLENCODED)) {
+            return new QueryParams(value);
+        }
+        throw new IllegalArgumentException("지원하지 않는 Content-Type 입니다: " + contentType);
+    }
+
+}

27eed0b6c966fdc94b70b6e1d4b2fcbebc8bcee8
sauter001 <lsj457@naver.com>
2026-09-20 23:20:17 +0900

    test: 테스트에 @DisplayName 추가

--- tomcat/src/test/java/org/apache/coyote/http11/FormContentsTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/FormContentsTest.java
@@ -1,16 +1,19 @@
 package org.apache.coyote.http11;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("폼 본문 파싱")
 class FormContentsTest {
 
     private static final String FORM_CONTENT_TYPE = "application/x-www-form-urlencoded";
 
     @Test
+    @DisplayName("폼 본문을 키와 값으로 파싱한다")
     void parseFormBody() {
         // when
         final FormContents contents = FormContents.of(FORM_CONTENT_TYPE, body("account=gugu&password=secret"));
@@ -21,6 +24,7 @@ class FormContentsTest {
     }
 
     @Test
+    @DisplayName("퍼센트 인코딩된 값을 디코딩한다")
     void decodePercentEncodedValue() {
         // when
         final FormContents contents = FormContents.of(FORM_CONTENT_TYPE, body("email=hkkang%40woowahan.com"));
@@ -30,6 +34,7 @@ class FormContentsTest {
     }
 
     @Test
+    @DisplayName("더하기 기호를 공백으로 디코딩한다")
     void decodePlusAsSpace() {
         // when
         final FormContents contents = FormContents.of(FORM_CONTENT_TYPE, body("name=hello+world"));
@@ -39,6 +44,7 @@ class FormContentsTest {
     }
 
     @Test
+    @DisplayName("Content-Type에 charset이 붙어 있어도 파싱한다")
     void parseWhenContentTypeHasCharset() {
         // when
         final FormContents contents = FormContents.of(
@@ -49,6 +55,7 @@ class FormContentsTest {
     }
 
     @Test
+    @DisplayName("본문이 없으면 빈 값을 반환한다")
     void emptyWhenBodyIsNull() {
         // when
         final FormContents contents = FormContents.of(FORM_CONTENT_TYPE, null);
@@ -58,6 +65,7 @@ class FormContentsTest {
     }
 
     @Test
+    @DisplayName("폼이 아닌 Content-Type이면 파싱하지 않는다")
     void emptyWhenContentTypeIsNotForm() {
         // when
         final FormContents contents = FormContents.of("application/json", body("account=gugu"));
@@ -67,6 +75,7 @@ class FormContentsTest {
     }
 
     @Test
+    @DisplayName("Content-Type이 없으면 파싱하지 않는다")
     void emptyWhenContentTypeIsMissing() {
         // when
         final FormContents contents = FormContents.of(null, body("account=gugu"));
@@ -76,6 +85,7 @@ class FormContentsTest {
     }
 
     @Test
+    @DisplayName("형식이 어긋난 구간은 건너뛴다")
     void skipMalformedSegment() {
         // when
         final FormContents contents = FormContents.of(FORM_CONTENT_TYPE, body("account=gugu&broken&password=secret"));
--- tomcat/src/test/java/org/apache/coyote/http11/RequestUriTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/RequestUriTest.java
@@ -1,14 +1,17 @@
 package org.apache.coyote.http11;
 
 import org.apache.coyote.exception.HttpParseException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@DisplayName("요청 URI 해석")
 class RequestUriTest {
 
     @Test
+    @DisplayName("쿼리스트링이 없으면 경로만 가진다")
     void pathWithoutQueryString() {
         // when
         final RequestUri uri = new RequestUri("/login");
@@ -19,6 +22,7 @@ class RequestUriTest {
     }
 
     @Test
+    @DisplayName("경로와 쿼리스트링을 분리한다")
     void separatePathAndQueryString() {
         // when
         final RequestUri uri = new RequestUri("/login?account=gugu&password=secret");
@@ -30,6 +34,7 @@ class RequestUriTest {
     }
 
     @Test
+    @DisplayName("퍼센트 인코딩된 값을 디코딩한다")
     void decodePercentEncodedValue() {
         // when
         final RequestUri uri = new RequestUri("/register?email=hkkang%40woowahan.com");
@@ -39,6 +44,7 @@ class RequestUriTest {
     }
 
     @Test
+    @DisplayName("쿼리스트링 안의 물음표는 값으로 유지한다")
     void keepQuestionMarkInsideQueryString() {
         // when
         final RequestUri uri = new RequestUri("/search?query=what?");
@@ -49,6 +55,7 @@ class RequestUriTest {
     }
 
     @Test
+    @DisplayName("슬래시로 시작하지 않는 URI는 예외를 던진다")
     void rejectUriNotStartingWithSlash() {
         // expect
         assertThatThrownBy(() -> new RequestUri("login"))

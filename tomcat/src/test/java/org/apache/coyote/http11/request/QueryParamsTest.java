29710abf5a73f4bbd5cc01376ffc18f19232e66c
koreaioi <koreaioi@gachon.ac.kr>
2026-09-20 12:46:33 +0900

    feature: QueryParams 객체 추가

---
+++ tomcat/src/test/java/org/apache/coyote/http11/request/QueryParamsTest.java
@@ -0,0 +1,40 @@
+package org.apache.coyote.http11.request;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class QueryParamsTest {
+
+    @Test
+    @DisplayName("유효한 key로 queryParam 조회 시 적절한 값을 반환한다.")
+    void validKeyReturnValidValue() {
+        // given
+        String query = "account=gugu&password=password";
+        QueryParams queryParams = new QueryParams(query);
+
+        // when
+        String account = queryParams.getValue("account");
+        String password = queryParams.getValue("password");
+
+        // then
+        assertThat(account).isEqualTo("gugu");
+        assertThat(password).isEqualTo("password");
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 key로 queryParam 조회 시 Null을 반환한다.")
+    void invalidKeyReturnNull() {
+        // given
+        String query = "account=gugu&password=password";
+        QueryParams queryParams = new QueryParams(query);
+
+        // when
+        String actual = queryParams.getValue("InvalidKey");
+
+        // then
+        assertThat(actual).isNull();
+    }
+
+}

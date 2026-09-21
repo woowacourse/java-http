c0398536b20aed9ded76edcf0ed14ce89c081391
jyt6640 <jyt6640@gmail.com>
2026-09-21 21:47:57 +0900

    feat: SessionManager 기능 구현
    - 추가
    - 찾기
    - 제거

---
+++ tomcat/src/main/java/org/apache/catalina/SessionManager.java
@@ -0,0 +1,32 @@
+package org.apache.catalina;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SessionManager implements Manager {
+
+    private static final Map<String, Session> SESSIONS = new HashMap<>();
+    private static final SessionManager INSTANCE = new SessionManager();
+
+    @Override
+    public void add(final Session session) {
+        SESSIONS.put(session.getId(), session);
+    }
+
+    @Override
+    public Session findSession(final String id) {
+        return SESSIONS.get(id);
+    }
+
+    @Override
+    public void remove(Session session) {
+        SESSIONS.remove(session.getId());
+    }
+
+    public static SessionManager getInstance() {
+        return INSTANCE;
+    }
+
+    private SessionManager() {
+    }
+}

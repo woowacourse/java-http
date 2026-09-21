bffe9a94b8d717d89404c9af9314fe9f2dca5cd4
JunHyung1206 <pjh120610@naver.com>
2026-09-21 13:47:08 +0900

    refactor: HttpSession 구현 제거

--- tomcat/src/main/java/org/apache/catalina/Session.java
+++ tomcat/src/main/java/org/apache/catalina/Session.java
@@ -1,14 +1,9 @@
 package org.apache.catalina;
 
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.http.HttpSession;
-import jakarta.servlet.http.HttpSessionContext;
-
-import java.util.Enumeration;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class Session implements HttpSession {
+public class Session {
 
     private final String id;
     private final Map<String, Object> values = new ConcurrentHashMap<>();
@@ -17,22 +12,14 @@ public class Session implements HttpSession {
         this.id = id;
     }
 
-    @Override
-    public long getCreationTime() {
-        return 0;
-    }
-
-    @Override
     public String getId() {
         return id;
     }
 
-    @Override
     public Object getAttribute(final String name) {
         return values.get(name);
     }
 
-    @Override
     public void setAttribute(final String name, final Object value) {
         if (value == null) {
             removeAttribute(name);
@@ -41,69 +28,14 @@ public class Session implements HttpSession {
         values.put(name, value);
     }
 
-    @Override
     public void removeAttribute(final String name) {
         values.remove(name);
     }
 
-    @Override
+
     public void invalidate() {
         values.clear();
         SessionManager.getInstance().remove(id);
     }
 
-    @Override
-    public long getLastAccessedTime() {
-        return 0;
-    }
-
-    @Override
-    public ServletContext getServletContext() {
-        return null;
-    }
-
-    @Override
-    public void setMaxInactiveInterval(int interval) {
-
-    }
-
-    @Override
-    public int getMaxInactiveInterval() {
-        return 0;
-    }
-
-    @Override
-    public HttpSessionContext getSessionContext() {
-        return null;
-    }
-
-    @Override
-    public Object getValue(String name) {
-        return null;
-    }
-
-    @Override
-    public Enumeration<String> getAttributeNames() {
-        return null;
-    }
-
-    @Override
-    public String[] getValueNames() {
-        return new String[0];
-    }
-
-    @Override
-    public void putValue(String name, Object value) {
-
-    }
-
-    @Override
-    public void removeValue(String name) {
-
-    }
-
-    @Override
-    public boolean isNew() {
-        return false;
-    }
 }

2db7b75f7c6c5952c64bb507284ed825a64f7e17
koreaioi <koreaioi@gachon.ac.kr>
2026-09-22 00:59:57 +0900

    chore: Http11Processor에 Dispatcher 적용을 위해 주석처리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -3,6 +3,7 @@ package org.apache.coyote.http11;
 import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
 import com.techcourse.model.User;
+import org.apache.catalina.dispatcher.Dispatcher;
 import org.apache.coyote.Processor;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.request.line.HttpMethod;
@@ -26,11 +27,11 @@ public class Http11Processor implements Runnable, Processor {
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
 
     private final Socket connection;
-    private final SessionManager sessionManager;
+    private final Dispatcher dispatcher;
 
-    public Http11Processor(final Socket connection, final SessionManager sessionManager) {
+    public Http11Processor(final Socket connection, final Dispatcher dispatcher) {
         this.connection = connection;
-        this.sessionManager = sessionManager;
+        this.dispatcher = dispatcher;
     }
 
     @Override
@@ -44,104 +45,104 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            HttpRequest request = HttpRequest.from(inputStream);
-            Session session = sessionManager.findSession(request.getCookie("JSESSIONID"));
-
-            String path = request.getPath();
-            if (request.getHttpMethod().equals(HttpMethod.GET) && request.getPath().equals("/register")) {
-                // FIXME HttpResponse.ok 형식 중복
-                path = "/register.html";
-                URL resourcePath = getClass().getClassLoader().getResource("static" + path);
-                final var contentType = ContentType.from(path);
-                final var content = Files.readString(Path.of(resourcePath.toURI()), StandardCharsets.UTF_8);
-                HttpResponse httpResponse = HttpResponse.ok(contentType, content);
-                httpResponse.write(outputStream);
-                return;
-            }
-
-            if (request.getHttpMethod().equals(HttpMethod.POST) && request.getPath().equals("/register")) {
-                String account = request.getBodyValue("account");
-                String password = request.getBodyValue("password");
-                String email = request.getBodyValue("email");
-
-                User user = new User(account, password, email);
-                InMemoryUserRepository.save(user);
-
-                // FIXME HttpResponse.redirect 형식 중복
-                HttpResponse httpResponse = HttpResponse.redirect("/index.html");
-                httpResponse.write(outputStream);
-                return;
-            }
-
-            // THINK 반복되는 엔드포인트 매핑 리팩터링 - P1
-            if (request.getHttpMethod().equals(HttpMethod.GET)
-                    && (request.getPath().equals("/login") || request.getPath().equals("/login.html"))) {
-                if (session != null && session.getAttribute("user") != null) {
-                    // FIXME HttpResponse.redirect 형식 중복
-                    HttpResponse httpResponse = HttpResponse.redirect("/index.html");
-                    httpResponse.write(outputStream);
-                    return;
-                }
-
-                // FIXME HttpResponse.ok 형식 중복
-                path = "/login.html";
-                URL resourcePath = getClass().getClassLoader().getResource("static" + path);
-                final var contentType = ContentType.from(path);
-                final var content = Files.readString(Path.of(resourcePath.toURI()), StandardCharsets.UTF_8);
-                HttpResponse httpResponse = HttpResponse.ok(contentType, content);
-                httpResponse.write(outputStream);
-                return;
-            }
-
-            if (request.getHttpMethod().equals(HttpMethod.POST) && request.getPath().equals("/login")) {
-                String account = request.getBodyValue("account");
-                String password = request.getBodyValue("password");
-                Optional<User> loginedUser = InMemoryUserRepository.findByAccount(account)
-                        .filter(user -> user.checkPassword(password));
-
-                if (loginedUser.isPresent()) {
-                    UUID sessionId = UUID.randomUUID();
-                    HttpCookie cookie = new HttpCookie("JSESSIONID=" + sessionId);
-                    Session newSession = new Session(sessionId.toString());
-                    newSession.setAttribute("user", loginedUser.get());
-                    sessionManager.add(newSession);
-
-                    // FIXME HttpResponse.redirect 형식 중복
-                    HttpResponse httpResponse = HttpResponse.redirect("/index.html", cookie);
-                    httpResponse.write(outputStream);
-                    return;
-                } else {
-                    // FIXME HttpResponse.redirect 형식 중복
-                    HttpResponse httpResponse = HttpResponse.redirect("/401.html");
-                    httpResponse.write(outputStream);
-                    return;
-                }
-            }
-
-            if (request.getPath().equals("/")) {
-                // FIXME HttpResponse.ok 형식 중복
-                path = "/index.html";
-                URL resourcePath = getClass().getClassLoader().getResource("static" + path);
-                final var contentType = ContentType.from(path);
-                final var content = Files.readString(Path.of(resourcePath.toURI()), StandardCharsets.UTF_8);
-                HttpResponse httpResponse = HttpResponse.ok(contentType, content);
-                httpResponse.write(outputStream);
-                return;
-            }
-
-            // FIXME HttpResponse.ok 형식 중복
-            URL resourcePath = getClass().getClassLoader().getResource("static" + path); // 여기까지 온 path는 잘못된 경로일 수 있으므로 검증.
-            if (resourcePath == null) {
-                final var notFoundResponse = getResponseBody("404 Not Found", "text/html", "<h1>404 Not Found</h1>");
-                outputStream.write(notFoundResponse.getBytes(StandardCharsets.UTF_8));
-                outputStream.flush();
-                return;
-            }
-
-            final var contentType = ContentType.from(path);
-            final var content = Files.readString(Path.of(resourcePath.toURI()), StandardCharsets.UTF_8);
-            HttpResponse httpResponse = HttpResponse.ok(contentType, content);
-            httpResponse.write(outputStream);
+//            HttpRequest request = HttpRequest.from(inputStream);
+//            Session session = sessionManager.findSession(request.getCookie("JSESSIONID"));
+//
+//            String path = request.getPath();
+//            if (request.getHttpMethod().equals(HttpMethod.GET) && request.getPath().equals("/register")) {
+//                // FIXME HttpResponse.ok 형식 중복
+//                path = "/register.html";
+//                URL resourcePath = getClass().getClassLoader().getResource("static" + path);
+//                final var contentType = ContentType.from(path);
+//                final var content = Files.readString(Path.of(resourcePath.toURI()), StandardCharsets.UTF_8);
+//                HttpResponse httpResponse = HttpResponse.ok(contentType, content);
+//                httpResponse.write(outputStream);
+//                return;
+//            }
+//
+//            if (request.getHttpMethod().equals(HttpMethod.POST) && request.getPath().equals("/register")) {
+//                String account = request.getBodyValue("account");
+//                String password = request.getBodyValue("password");
+//                String email = request.getBodyValue("email");
+//
+//                User user = new User(account, password, email);
+//                InMemoryUserRepository.save(user);
+//
+//                // FIXME HttpResponse.redirect 형식 중복
+//                HttpResponse httpResponse = HttpResponse.redirect("/index.html");
+//                httpResponse.write(outputStream);
+//                return;
+//            }
+//
+//            // THINK 반복되는 엔드포인트 매핑 리팩터링 - P1
+//            if (request.getHttpMethod().equals(HttpMethod.GET)
+//                    && (request.getPath().equals("/login") || request.getPath().equals("/login.html"))) {
+//                if (session != null && session.getAttribute("user") != null) {
+//                    // FIXME HttpResponse.redirect 형식 중복
+//                    HttpResponse httpResponse = HttpResponse.redirect("/index.html");
+//                    httpResponse.write(outputStream);
+//                    return;
+//                }
+//
+//                // FIXME HttpResponse.ok 형식 중복
+//                path = "/login.html";
+//                URL resourcePath = getClass().getClassLoader().getResource("static" + path);
+//                final var contentType = ContentType.from(path);
+//                final var content = Files.readString(Path.of(resourcePath.toURI()), StandardCharsets.UTF_8);
+//                HttpResponse httpResponse = HttpResponse.ok(contentType, content);
+//                httpResponse.write(outputStream);
+//                return;
+//            }
+//
+//            if (request.getHttpMethod().equals(HttpMethod.POST) && request.getPath().equals("/login")) {
+//                String account = request.getBodyValue("account");
+//                String password = request.getBodyValue("password");
+//                Optional<User> loginedUser = InMemoryUserRepository.findByAccount(account)
+//                        .filter(user -> user.checkPassword(password));
+//
+//                if (loginedUser.isPresent()) {
+//                    UUID sessionId = UUID.randomUUID();
+//                    HttpCookie cookie = new HttpCookie("JSESSIONID=" + sessionId);
+//                    Session newSession = new Session(sessionId.toString());
+//                    newSession.setAttribute("user", loginedUser.get());
+//                    sessionManager.add(newSession);
+//
+//                    // FIXME HttpResponse.redirect 형식 중복
+//                    HttpResponse httpResponse = HttpResponse.redirect("/index.html", cookie);
+//                    httpResponse.write(outputStream);
+//                    return;
+//                } else {
+//                    // FIXME HttpResponse.redirect 형식 중복
+//                    HttpResponse httpResponse = HttpResponse.redirect("/401.html");
+//                    httpResponse.write(outputStream);
+//                    return;
+//                }
+//            }
+//
+//            if (request.getPath().equals("/")) {
+//                // FIXME HttpResponse.ok 형식 중복
+//                path = "/index.html";
+//                URL resourcePath = getClass().getClassLoader().getResource("static" + path);
+//                final var contentType = ContentType.from(path);
+//                final var content = Files.readString(Path.of(resourcePath.toURI()), StandardCharsets.UTF_8);
+//                HttpResponse httpResponse = HttpResponse.ok(contentType, content);
+//                httpResponse.write(outputStream);
+//                return;
+//            }
+//
+//            // FIXME HttpResponse.ok 형식 중복
+//            URL resourcePath = getClass().getClassLoader().getResource("static" + path); // 여기까지 온 path는 잘못된 경로일 수 있으므로 검증.
+//            if (resourcePath == null) {
+//                final var notFoundResponse = getResponseBody("404 Not Found", "text/html", "<h1>404 Not Found</h1>");
+//                outputStream.write(notFoundResponse.getBytes(StandardCharsets.UTF_8));
+//                outputStream.flush();
+//                return;
+//            }
+//
+//            final var contentType = ContentType.from(path);
+//            final var content = Files.readString(Path.of(resourcePath.toURI()), StandardCharsets.UTF_8);
+//            HttpResponse httpResponse = HttpResponse.ok(contentType, content);
+//            httpResponse.write(outputStream);
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         } catch (URISyntaxException e) {
@@ -150,52 +151,14 @@ public class Http11Processor implements Runnable, Processor {
     }
 
     // THINK: 추후 응답 관련 내용을 응집화한 HttpResponse으로 포장 - P0
-    public String getResponseBody(String status, String contentType, String content) {
-        byte[] bytes = content.getBytes(StandardCharsets.UTF_8);
-        return String.join("\r\n",
-                "HTTP/1.1 " + status,
-                "Content-Type: " + contentType + ";charset=utf-8",
-                "Content-Length: " + bytes.length,
-                "",
-                content);
-    }
-
-    public String redirect(String status, String location) {
-        return String.join("\r\n",
-                "HTTP/1.1 " + status,
-                "Location: " + location,
-                "Content-Length: 0",
-                "");
-    }
-
-    public String redirect(String status, String location, HttpCookie cookie) {
-        log.info("Cookie : {}", cookie.serialize());
-        return String.join("\r\n",
-                "HTTP/1.1 " + status,
-                "Location: " + location,
-                "Set-Cookie: " + cookie.serialize(),
-                "Content-Length: 0",
-                "");
-    }
-
-    private String contentTypeOf(String path) {
-        int dotIndex = path.lastIndexOf('.');
-        String extension = (dotIndex == -1) ? "" : path.substring(dotIndex);
-
-        return switch (extension) {
-            case ".css" -> "text/css";
-            case ".js" -> "text/javascript";
-            case ".ico" -> "image/x-icon";
-            default -> "text/html";
-        };
-    }
-
-    private String readStaticResource(String path) throws IOException, URISyntaxException {
-        URL resource = getClass().getClassLoader().getResource("static" + path);
-        if (resource == null) {
-            return null;
-        }
-        return Files.readString(Path.of(resource.toURI()), StandardCharsets.UTF_8);
-    }
+//    public String getResponseBody(String status, String contentType, String content) {
+//        byte[] bytes = content.getBytes(StandardCharsets.UTF_8);
+//        return String.join("\r\n",
+//                "HTTP/1.1 " + status,
+//                "Content-Type: " + contentType + ";charset=utf-8",
+//                "Content-Length: " + bytes.length,
+//                "",
+//                content);
+//    }
 
 }

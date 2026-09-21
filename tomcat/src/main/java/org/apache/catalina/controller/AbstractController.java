0d5397f2a0e35f1e7838c546451cebe229dd2bfa
koreaioi <koreaioi@gachon.ac.kr>
2026-09-22 00:24:17 +0900

    refactor: IOException을 외부로 던지도록 개선

--- tomcat/src/main/java/org/apache/catalina/controller/AbstractController.java
+++ tomcat/src/main/java/org/apache/catalina/controller/AbstractController.java
@@ -3,10 +3,12 @@ package org.apache.catalina.controller;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.response.HttpResponse;
 
+import java.io.IOException;
+
 public abstract class AbstractController implements Controller {
 
     @Override
-    public String doService(HttpRequest request, HttpResponse response) {
+    public String doService(HttpRequest request, HttpResponse response) throws IOException {
         return switch (request.getHttpMethod()) {
             case GET -> doGet(request, response);
             case POST -> doPost(request, response);
@@ -14,13 +16,12 @@ public abstract class AbstractController implements Controller {
         };
     }
 
-    protected String doGet(HttpRequest request, HttpResponse response) {
+    protected String doGet(HttpRequest request, HttpResponse response) throws IOException {
         throw new UnsupportedOperationException();
     }
 
-    protected String doPost(HttpRequest request, HttpResponse response) {
+    protected String doPost(HttpRequest request, HttpResponse response) throws IOException {
         throw new UnsupportedOperationException();
     }
-
 
 }
--- tomcat/src/main/java/org/apache/catalina/controller/Controller.java
+++ tomcat/src/main/java/org/apache/catalina/controller/Controller.java
@@ -3,8 +3,10 @@ package org.apache.catalina.controller;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.response.HttpResponse;
 
+import java.io.IOException;
+
 public interface Controller {
 
-    String doService(HttpRequest request, HttpResponse response);
+    String doService(HttpRequest request, HttpResponse response) throws IOException;
 
 }

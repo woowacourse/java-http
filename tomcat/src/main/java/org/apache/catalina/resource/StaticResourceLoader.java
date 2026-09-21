d6ec00c9f979ea37bf4d49703b99c71bdc5d37c6
JunHyung1206 <pjh120610@naver.com>
2026-09-21 22:14:35 +0900

    fix: 클래스패스 스트림으로 정적 리소스 로딩

--- tomcat/src/main/java/org/apache/catalina/resource/StaticResourceLoader.java
+++ tomcat/src/main/java/org/apache/catalina/resource/StaticResourceLoader.java
@@ -1,38 +1,20 @@
 package org.apache.catalina.resource;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class StaticResourceLoader {
 
-    private File getResourceFile(String fileName) throws FileNotFoundException {
-        URL resource = getClass()
-                .getClassLoader()
-                .getResource(fileName);
-
-        if (resource == null) {
-            throw new FileNotFoundException(fileName);
-        }
-
-        try {
-            return Path.of(resource.toURI()).toFile();
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public String load(String path) throws IOException {
-        File resourceFile = getResourceFile("static" + path);
+        String resourceName = "static" + path;
 
-        return Files.readString(
-                resourceFile.toPath(),
-                StandardCharsets.UTF_8
-        );
+        try (InputStream resource = getClass().getClassLoader().getResourceAsStream(resourceName)) {
+            if (resource == null) {
+                throw new FileNotFoundException(resourceName);
+            }
+            return new String(resource.readAllBytes(), StandardCharsets.UTF_8);
+        }
     }
 }

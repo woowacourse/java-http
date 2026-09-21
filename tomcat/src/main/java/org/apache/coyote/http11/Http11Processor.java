f3d02c8b17ef242854a5f3a6a834fd9a96f37a15
Pino Moon <timmymoon@naver.com>
2026-09-21 16:51:23 +0900

    [1단계 - HTTP 서버 구현하기] 피노(문해찬) 미션 제출합니다. (#1213)
    * chore: 톰캣 테스트 런처 버전 정렬
    
    * feat: index 페이지 응답 지원
    
    * feat: CSS 응답 지원
    
    * feat: 로그인 쿼리 스트링 파싱
    
    * test: 파일과 입출력 스트림 학습 테스트 완료

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,21 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.db.InMemoryUserRepository;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,19 +38,90 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final var bufferedReader = new BufferedReader(
+                    new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            final String requestLine = bufferedReader.readLine();
+            if (requestLine == null) {
+                return;
+            }
+
+            final String requestUri = requestLine.split(" ")[1];
+            final String requestPath = requestPath(requestUri);
+            logIn(requestPath, requestUri);
+
+            final String responseBody = responseBody(requestPath);
+            final String contentType = contentType(requestPath);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "Content-Type: " + contentType + " ",
+                    "Content-Length: " + responseBody.getBytes(StandardCharsets.UTF_8).length + " ",
                     "",
                     responseBody);
 
-            outputStream.write(response.getBytes());
+            outputStream.write(response.getBytes(StandardCharsets.UTF_8));
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String requestPath(final String requestUri) {
+        final int queryStringIndex = requestUri.indexOf("?");
+        if (queryStringIndex < 0) {
+            return requestUri;
+        }
+        return requestUri.substring(0, queryStringIndex);
+    }
+
+    private void logIn(final String requestPath, final String requestUri) {
+        final int queryStringIndex = requestUri.indexOf("?");
+        if (!"/login".equals(requestPath) || queryStringIndex < 0) {
+            return;
+        }
+
+        final String queryString = requestUri.substring(queryStringIndex + 1);
+        final Map<String, String> queryParameters = queryParameters(queryString);
+        final String account = queryParameters.get("account");
+        final String password = queryParameters.get("password");
+        if (account == null || password == null) {
+            return;
+        }
+
+        InMemoryUserRepository.findByAccount(account)
+                .filter(user -> user.checkPassword(password))
+                .ifPresent(user -> log.info("login user: {}", user.getAccount()));
+    }
+
+    private Map<String, String> queryParameters(final String queryString) {
+        final Map<String, String> queryParameters = new HashMap<>();
+        for (String parameter : queryString.split("&")) {
+            final String[] nameAndValue = parameter.split("=", 2);
+            if (nameAndValue.length == 2) {
+                queryParameters.put(nameAndValue[0], nameAndValue[1]);
+            }
+        }
+        return queryParameters;
+    }
+
+    private String responseBody(final String requestPath) throws IOException {
+        if ("/".equals(requestPath)) {
+            return "Hello world!";
+        }
+
+        final String resourcePath = "/login".equals(requestPath) ? "/login.html" : requestPath;
+        final URL resource = getClass().getClassLoader().getResource("static" + resourcePath);
+        if (resource == null) {
+            return "";
+        }
+
+        return Files.readString(Path.of(resource.getPath()), StandardCharsets.UTF_8);
+    }
+
+    private String contentType(final String requestPath) {
+        if (requestPath.endsWith(".css")) {
+            return "text/css;charset=utf-8";
+        }
+        return "text/html;charset=utf-8";
     }
 }

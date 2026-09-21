15c821715cc112fd8930da25d8fa7921f4282c75
koreaioi <koreaioi@gachon.ac.kr>
2026-09-22 00:17:18 +0900

    feature: Controller 인터페이스 설계

---
+++ tomcat/src/main/java/org/apache/catalina/controller/Controller.java
@@ -0,0 +1,10 @@
+package org.apache.catalina.controller;
+
+import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.response.HttpResponse;
+
+public interface Controller {
+
+    String service(HttpRequest request, HttpResponse response);
+
+}

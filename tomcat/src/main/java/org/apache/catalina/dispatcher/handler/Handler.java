dc8ed3b99afe18714fcc1602fe6a7e1a09448581
koreaioi <koreaioi@gachon.ac.kr>
2026-09-22 00:56:03 +0900

    feature: HandlerMapping 추가

--- tomcat/src/main/java/org/apache/catalina/dispatcher/handler/Handler.java
+++ tomcat/src/main/java/org/apache/catalina/dispatcher/handler/Handler.java
@@ -3,10 +3,12 @@ package org.apache.catalina.dispatcher.handler;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.response.HttpResponse;
 
+import java.io.IOException;
+
 public interface Handler {
 
     boolean supports(HttpRequest request);
 
-    String handle(HttpRequest request, HttpResponse response) throws Exception;
+    String handle(HttpRequest request, HttpResponse response) throws IOException;
 
 }

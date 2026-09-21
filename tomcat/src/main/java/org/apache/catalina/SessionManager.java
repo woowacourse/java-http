a32a1df3e4252236750b5b589a4b53655d6122b1
sun <sun007021@gmail.com>
2026-09-21 21:27:47 +0900

    feat: Session 구현

---
+++ tomcat/src/main/java/org/apache/catalina/SessionManager.java
@@ -0,0 +1,32 @@
+package org.apache.catalina;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SessionManager implements Manager {
+
+    private static final SessionManager INSTANCE = new SessionManager();
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
+
+    private SessionManager() {
+    }
+
+    public static SessionManager getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public void add(final Session session) {
+        SESSIONS.put(session.getId(), session);
+    }
+
+    @Override
+    public Session findSession(final String id) {
+        return SESSIONS.get(id);
+    }
+
+    @Override
+    public void remove(final String id) {
+        SESSIONS.remove(id);
+    }
+}

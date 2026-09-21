213d281143ad73badd15fa1d23441f66308cee4b
jyt6640 <jyt6640@gmail.com>
2026-09-21 18:58:46 +0900

    test: J_SESSION_ID 발행 테스트 추가

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -134,4 +134,25 @@ class Http11ProcessorTest {
         assertThat(InMemoryUserRepository.findByAccount("pobi"))
                 .isPresent();
     }
+
+    @Test
+    void jSessionIdIssue_success() {
+        // given
+        final String httpRequest = String.join("\r\n",
+                "GET /index.html HTTP/1.1",
+                "Host: localhost:8080",
+                "",
+                ""
+        );
+
+        final var socket = new StubSocket(httpRequest);
+        final var processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        assertThat(socket.output())
+                .contains("Set-Cookie: JSESSIONID=");
+    }
 }

97c1ca9b288d33ec6e1e1bb1ed484b0b2bd90918
koreaioi <koreaioi@gachon.ac.kr>
2026-09-22 00:17:46 +0900

    feature: 여러 컨트롤러가 상속받을 추상 클래스 AbstractController 추가
    - 행동 규약은 Controller로 묶음

---
+++ tomcat/src/main/java/org/apache/catalina/controller/AbstractController.java
@@ -0,0 +1,26 @@
+package org.apache.catalina.controller;
+
+import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.response.HttpResponse;
+
+public abstract class AbstractController implements Controller {
+
+    @Override
+    public String service(HttpRequest request, HttpResponse response) {
+        return switch (request.getHttpMethod()) {
+            case GET -> doGet(request, response);
+            case POST -> doPost(request, response);
+            default -> throw new UnsupportedOperationException();
+        };
+    }
+
+    protected String doGet(HttpRequest request, HttpResponse response) {
+        throw new UnsupportedOperationException();
+    }
+
+    protected String doPost(HttpRequest request, HttpResponse response) {
+        throw new UnsupportedOperationException();
+    }
+
+
+}

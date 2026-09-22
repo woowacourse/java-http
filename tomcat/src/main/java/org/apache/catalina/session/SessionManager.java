a65d494bfb40ec20b4cc06f9df70aeb1d052ffc8
sinequanonn <dleck288@gmail.com>
2026-09-22 20:06:26 +0900

    feat: Session 및 SessionManager 구현

---
+++ tomcat/src/main/java/org/apache/catalina/session/SessionManager.java
@@ -0,0 +1,24 @@
+package org.apache.catalina.session;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SessionManager {
+
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
+
+    private SessionManager() {}
+
+    public static void add(final Session session) {
+        SESSIONS.put(session.getId(), session);
+    }
+
+    public static Session findSession(final String id) throws IOException {
+        return SESSIONS.get(id);
+    }
+
+    public static void remove(final Session session) {
+        SESSIONS.remove(session.getId());
+    }
+}

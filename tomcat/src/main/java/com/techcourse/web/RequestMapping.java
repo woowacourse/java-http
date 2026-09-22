9091f7b5ecb419ccba338aeb42dd9920c129b4f3
Jeshin Kim <jjason0904@cau.ac.kr>
2026-09-22 10:27:57 +0900

    refactor(RequestMapping): Controller 분기 Map을 사용한 방식으로 수정

--- tomcat/src/main/java/com/techcourse/web/RequestMapping.java
+++ tomcat/src/main/java/com/techcourse/web/RequestMapping.java
@@ -6,33 +6,25 @@ import com.techcourse.controller.LoginController;
 import com.techcourse.controller.RegisterController;
 import com.techcourse.controller.StaticResourceController;
 import com.techcourse.service.ApplicationService;
+import java.util.Map;
 import org.apache.coyote.HttpRequest;
 
 public class RequestMapping {
 
-    private final Controller loginController;
-    private final Controller registerController;
-    private final Controller homeController;
+    private final Map<String, Controller> controllers;
     private final Controller staticResourceController;
 
     public RequestMapping(ApplicationService applicationService, StaticResourceHandler staticResourceHandler) {
-        loginController = new LoginController(applicationService, staticResourceHandler);
-        registerController = new RegisterController(applicationService, staticResourceHandler);
-        homeController = new HomeController();
+        controllers = Map.of(
+                "/login", new LoginController(applicationService, staticResourceHandler),
+                "/register", new RegisterController(applicationService, staticResourceHandler),
+                "/", new HomeController()
+        );
         staticResourceController = new StaticResourceController(staticResourceHandler);
     }
 
     public Controller getController(HttpRequest request) {
-        if ("/login".equals(request.path())) {
-            return loginController;
-        }
-        if ("/register".equals(request.path())) {
-            return registerController;
-        }
-        if ("/".equals(request.path())) {
-            return homeController;
-        }
-        return staticResourceController;
+        return controllers.getOrDefault(request.path(), staticResourceController);
     }
 
 }

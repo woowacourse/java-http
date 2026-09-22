e1d0a6d045ce1526c8e8f65c0957666413810c18
gleaming9 <gleaming9@khu.ac.kr>
2026-09-23 01:15:44 +0900

    feat: 세션과 세션 관리자 구현

---
+++ tomcat/src/main/java/org/apache/catalina/SessionManager.java
@@ -0,0 +1,39 @@
+package org.apache.catalina;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SessionManager implements Manager {
+
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
+    private static final SessionManager INSTANCE = new SessionManager();
+
+    public static SessionManager getInstance() {
+        return INSTANCE;
+    }
+
+    public Session createSession() {
+        Session session = new Session(UUID.randomUUID().toString());
+        add(session);
+        return session;
+    }
+
+    @Override
+    public void add(Session session) {
+        SESSIONS.put(session.getId(), session);
+    }
+
+    @Override
+    public Session findSession(String id) {
+        return SESSIONS.get(id);
+    }
+
+    @Override
+    public void remove(Session session) {
+        SESSIONS.remove(session.getId());
+    }
+
+    private SessionManager() {
+    }
+}

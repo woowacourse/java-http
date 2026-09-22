cc4d1f167bc8b406530fe0cabc140853b424b72c
YEJIN KIM <glow.yejin@gmail.com>
2026-09-22 19:19:15 +0900

    refactor: HttpRequest, HttpResponse를 사용하도록 변경하고 죽은 코드를 제거

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,14 +1,11 @@
 package org.apache.coyote.http11;
 
-import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,9 +16,6 @@ import java.net.Socket;
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
-    private static final String QUERY_SEPARATOR = "?";
-    private static final String PARAMETER_SEPARATOR = "&";
-    private static final String KEY_VALUE_SEPARATOR = "=";
 
     private final Socket connection;
 
@@ -42,21 +36,12 @@ public class Http11Processor implements Runnable, Processor {
                      new InputStreamReader(connection.getInputStream()))
              ) {
 
-            String requestLine = reader.readLine();
-            if (requestLine == null) { return; }
+            HttpRequest request = HttpRequest.from(reader);
 
-            String[] requestLineTokens = requestLine.split(" ");
-            String uri = requestLineTokens[1];
+            String contentType = resolveContentType(request.getPath());
+            String responseBody = resolveResponseBody(request.getPath());
 
-            String responseBody = resolveResponseBody(uri);
-            String contentType = resolveContentType(uri);
-
-            String response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: " + contentType + ";charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            HttpResponse response = HttpResponse.ok(contentType, responseBody);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
@@ -65,44 +50,31 @@ public class Http11Processor implements Runnable, Processor {
         }
     }
 
-    private String resolveResponseBody(String uri) throws IOException {
-        if (uri.equals("/")) {
+    private String resolveContentType(String path) {
+        if (path.endsWith(".css")) {
+            return "text/css";
+        }
+        if (path.endsWith(".js")) {
+            return "text/javascript";
+        }
+        return "text/html";
+    }
+
+    private String resolveResponseBody(String path) throws IOException {
+        if (path.equals("/")) {
             return "Hello world!";
         }
 
-        String path = "static" + uri;
-
-        if (uri.contains(QUERY_SEPARATOR)) {
-            int index = uri.indexOf(QUERY_SEPARATOR);
-            path = "static" + uri.substring(0, index);
-            Map<String, String> params = new HashMap<>();
-            for (String pair : uri.substring(index + 1).split(PARAMETER_SEPARATOR)) {
-                String[] keyValue = pair.split(KEY_VALUE_SEPARATOR);
-                params.put(keyValue[0], keyValue[1]);
-            }
-            InMemoryUserRepository.findByAccount(params.get("account"))
-                    .ifPresent(user -> log.info("user: {}", user));
+        String resourcePath = "static" + path;
+        if (!resourcePath.contains(".")) {
+            resourcePath += ".html";
         }
 
-        if (!path.contains(".")) {
-            path += ".html";
-        }
-
-        URL resource = getClass().getClassLoader().getResource(path);
+        URL resource = getClass().getClassLoader().getResource(resourcePath);
         if (resource == null) {
             return "404 Not Found";
         }
 
         return new String(Files.readAllBytes(Path.of(resource.getPath())));
-    }
-
-    private String resolveContentType (String uri) {
-        if (uri.endsWith(".css")) {
-            return "text/css";
-        }
-        if (uri.endsWith(".js")) {
-            return "text/javascript";
-        }
-        return "text/html";
     }
 }

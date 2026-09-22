4a0001e4e7a36f05588432c9051685eb03e33ab7
K-iise <kimyungi1004@naver.com>
2026-09-22 21:53:02 +0900

    feat: HttpRequest, HttpRequestParser 클래스 구현

---
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpRequest.java
@@ -0,0 +1,40 @@
+package org.apache.coyote.http11;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpRequest {
+    private final String method;
+    private final String path;
+    private final String version;
+    private final Map<String, String> headers = new HashMap<String, String>();
+    private final String responseBody;
+
+    public HttpRequest(String method, String path, String version, Map<String, String> headers, String responseBody) {
+        this.method = method;
+        this.path = path;
+        this.version = version;
+        this.headers.putAll(headers);
+        this.responseBody = responseBody;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public String getResponseBody() {
+        return responseBody;
+    }
+}
---
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpRequestParser.java
@@ -0,0 +1,57 @@
+package org.apache.coyote.http11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpRequestParser {
+
+    public static HttpRequest parse(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String[] requestLineParts = readRequestLineParts(bufferedReader);
+        String method = requestLineParts[0];
+        String path = requestLineParts[1];
+        String version = requestLineParts[2];
+
+        Map<String, String> requestHeaders = readRequestHeaders(bufferedReader);
+        int contentLength = Integer.parseInt(requestHeaders.getOrDefault("Content-Length", "0"));
+        String requestBody = readRequestBody(bufferedReader, contentLength);
+        return new HttpRequest(method, path, version, requestHeaders, requestBody);
+    }
+
+    private static String[] readRequestLineParts(BufferedReader reader) throws IOException {
+        String requestLine = reader.readLine();
+        return requestLine.split(" ");
+    }
+
+    private static Map<String, String> readRequestHeaders(BufferedReader bufferedReader)
+            throws IOException {
+        Map<String, String> requestHeaders = new HashMap<>();
+        String requestHeaderLine;
+        while ((requestHeaderLine = bufferedReader.readLine()) != null) {
+            if (requestHeaderLine.isEmpty()) {
+                break;
+            }
+            String[] header = requestHeaderLine.split(":", 2);
+            requestHeaders.put(header[0].trim(), header[1].trim());
+        }
+        return requestHeaders;
+    }
+
+    private static String readRequestBody(BufferedReader bufferedReader, int contentLength) throws IOException {
+        char[] buffer = new char[contentLength];
+        int readLength = 0;
+
+        while (readLength < contentLength) {
+            int currentLength = bufferedReader.read(buffer, readLength, contentLength - readLength);
+            if (currentLength == -1) {
+                break;
+            }
+            readLength += currentLength;
+        }
+        return new String(buffer, 0, readLength);
+    }
+}

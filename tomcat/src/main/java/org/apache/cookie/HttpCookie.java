c123f6394fd2e026c8506d2c4697fc902d142faa
5jonyuk <131836965+5jonyuk@users.noreply.github.com>
2026-09-22 10:04:01 +0900

    [2단계 - 로그인 구현하기] 이삭(오종혁) 미션 제출합니다. (#1229)
    * [1단계 - HTTP 서버 구현하기] 이삭(오종혁) 미션 제출합니다. (#1148)
    
    * feat: 파일, 입출력 스트림 학습테스트 추가
    
    * chore: 테스트 의존성 추가
    
    * feat: 1. GET /index.html 응답하기
    
    * feat: 2. CSS 지원하기
    
    * 3. Query String 파싱 및 리팩터링
    
    * feat: 첫번째 리뷰 반영
    
    * refactor: 변수명 변경
    
    * feat: 1. HTTP Status Code 302
    
    * refactor: 1. HTTP Status Code 302
    
    * feat: 2. POST 방식으로 회원가입
    
    * feat: 3. Cookie에 JSESSIONID 값 저장하기
    
    * feat: 4. Session 구현하기
    
    * fix: 피드백 반영
    
    * fix: status 뒤에 공백 추가하여 실패하던 테스트 수정

---
+++ tomcat/src/main/java/org/apache/cookie/HttpCookie.java
@@ -0,0 +1,32 @@
+package org.apache.cookie;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class HttpCookie {
+    private final Map<String, String> cookieMap;
+
+    public HttpCookie() {
+        this.cookieMap = new HashMap<>();
+    }
+
+    public void parseCookie(String cookie) {
+        if (cookie == null || cookie.isBlank()) {
+            return;
+        }
+        for (String c : cookie.split(";")) {
+            cookieMap.put(c.trim().split("=")[0], c.trim().split("=")[1]);
+        }
+    }
+
+    public String getCookieValue(String key) {
+        return cookieMap.get(key);
+    }
+
+    public String generateCookie() {
+        String value = UUID.randomUUID().toString();
+        cookieMap.put("JSESSIONID", value);
+        return value;
+    }
+}

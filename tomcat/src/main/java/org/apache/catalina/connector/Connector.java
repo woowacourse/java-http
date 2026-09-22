28f6582d2b18ec101710f42efa46a6c7376cec82
Jeshin Kim <jjason0904@cau.ac.kr>
2026-09-22 16:44:15 +0900

    refactor(Connector): 미사용 import문 제거

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -1,6 +1,5 @@
 package org.apache.catalina.connector;
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import org.apache.coyote.Adapter;

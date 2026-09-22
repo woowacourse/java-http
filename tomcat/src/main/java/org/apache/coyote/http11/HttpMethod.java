82bdbf5dbbb28f7fcee31ae892f7707f4b8709af
kangrae <kangrae0501@gmail.com>
2026-09-22 21:56:49 +0900

    refactor: http method 클래스 구현

---
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpMethod.java
@@ -0,0 +1,26 @@
+package org.apache.coyote.http11;
+
+public enum HttpMethod {
+
+    OPTIONS,
+    GET,
+    HEAD,
+    POST,
+    PUT,
+    DELETE,
+    TRACE,
+    CONNECT;
+
+    public static HttpMethod from(final String httpMethod) {
+        if (httpMethod == null) {
+            throw new IllegalArgumentException("HTTP 메서드가 없습니다.");
+        }
+
+        try {
+            return HttpMethod.valueOf(httpMethod);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("지원하지 않는 HTTP 메서드: " + httpMethod, e);
+        }
+    }
+
+}
--- tomcat/src/main/java/org/apache/coyote/http11/RequestLine.java
+++ tomcat/src/main/java/org/apache/coyote/http11/RequestLine.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 public class RequestLine {
 
-    private final String method;
+    private final HttpMethod method;
     private final String path;
     private final String queryString;
     private final String protocolVersion;
@@ -12,12 +12,12 @@ public class RequestLine {
     public RequestLine(final String requestLine) {
         Objects.requireNonNull(requestLine, "요청의 첫 줄은 비어있을 수 없습니다.");
 
-        final String[] components = requestLine.trim().split(" ");
+        final String[] components = requestLine.strip().split(" ");
         if (components.length != 3) {
             throw new IllegalArgumentException("잘못된 요청 첫 줄: " + requestLine);
         }
 
-        this.method = components[0];
+        this.method = HttpMethod.from(components[0]);
         this.protocolVersion = components[2];
 
         final String requestUri = components[1];
@@ -32,22 +32,15 @@ public class RequestLine {
     }
 
     public boolean isPost() {
-        return "POST".equals(method);
+        return HttpMethod.POST.equals(method);
     }
 
     public boolean isGet() {
-        return "GET".equals(method);
-    }
-
-    public String getMethod() {
-        return method;
+        return HttpMethod.GET.equals(method);
     }
 
     public String getPath() {
         return path;
     }
 
-    public String getQueryString() {
-        return queryString;
-    }
 }

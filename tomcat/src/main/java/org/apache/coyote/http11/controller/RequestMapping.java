45949bcad8eabd3e8e5b02bff5064ece1bcae200
K-iise <kimyungi1004@naver.com>
2026-09-23 21:40:50 +0900

    refactor: Map을 활용한 URI별 Controller 매핑 구조 개선

--- tomcat/src/main/java/org/apache/coyote/http11/controller/RequestMapping.java
+++ tomcat/src/main/java/org/apache/coyote/http11/controller/RequestMapping.java
@@ -1,16 +1,24 @@
 package org.apache.coyote.http11.controller;
 
+import java.util.Map;
 import org.apache.coyote.http11.HttpRequest;
 
 public class RequestMapping {
 
+    private final Map<String, Controller> controllers;
+
+    public RequestMapping() {
+        this.controllers = Map.of("/login", new LoginController(),
+                "/register", new RegisterController(),
+                "/", new StaticResourceController());
+    }
+
     public Controller getController(HttpRequest request) {
-        if (request.getPath().startsWith("/login")) {
-            return new LoginController();
-        }
-        if (request.getPath().startsWith("/register")) {
-            return new RegisterController();
-        }
-        return new StaticResourceController();
+        String path = normalizePath(request.getPath());
+        return controllers.getOrDefault(path, controllers.get("/"));
+    }
+
+    private String normalizePath(final String path) {
+        return path.replaceFirst("(\\.html)?(\\?.*)?$", "");
     }
 }

a963ded6b918406d0dd59c76c162a00aee138373
K-iise <kimyungi1004@naver.com>
2026-09-23 21:25:45 +0900

    feat: Controller 인터페이스 및 추상 클래스 도입

---
+++ tomcat/src/main/java/org/apache/coyote/http11/controller/AbstractController.java
@@ -0,0 +1,22 @@
+package org.apache.coyote.http11.controller;
+
+import org.apache.coyote.http11.HttpRequest;
+import org.apache.coyote.http11.HttpResponse;
+
+public abstract class AbstractController implements Controller {
+    @Override
+    public void service(HttpRequest request, HttpResponse response) throws Exception {
+        if (request.getMethod().equals("GET")) {
+            doGet(request, response);
+        }
+        if (request.getMethod().equals("POST")) {
+            doPost(request, response);
+        }
+    }
+
+    protected void doPost(HttpRequest request, HttpResponse response) throws Exception {
+    }
+
+    protected void doGet(HttpRequest request, HttpResponse response) throws Exception {
+    }
+}

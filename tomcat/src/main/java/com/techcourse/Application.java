c3ec0c681c42ef34ca7bae9e3b04c785b1936e2f
K-iise <kimyungi1004@naver.com>
2026-09-23 21:26:55 +0900

    refactor: RequestMapping 생성 책임 분리 및 의존성 주입

--- tomcat/src/main/java/com/techcourse/Application.java
+++ tomcat/src/main/java/com/techcourse/Application.java
@@ -1,11 +1,13 @@
 package com.techcourse;
 
 import org.apache.catalina.startup.Tomcat;
+import org.apache.coyote.http11.controller.RequestMapping;
 
 public class Application {
 
     public static void main(String[] args) {
-        final var tomcat = new Tomcat();
+        final var requestMapping = new RequestMapping();
+        final var tomcat = new Tomcat(requestMapping);
         tomcat.start();
     }
 }

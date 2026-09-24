1fbe55cab9dc071bea4c92778a712fbe51fb607b
Yeji-Kim-Erica <yejikim.erica@gmail.com>
2026-09-24 15:53:05 +0900

    [1단계 - HTTP 서버 구현하기] 테리(김예지) 미션 제출합니다. (#1188)
    * File 학습 테스트
    
    * IOStream 학습 테스트
    
    * build: JUnit Platform 런타임 의존성 해결
    
    * feat: GET /index.html 응답하기
    
    * feat: CSS 지원하기
    
    * feat: Query String 파싱
    
    * refactor: 문자열 일치 여부를 matches 대신 equals로 확인하도록 수정
    
    * refactor: 조회된 로그인 기록을 log를 사용해 출력
    
    * refactor: 자원 읽기 과정을 별도 메서드로 분리
    
    - fix: /로 요청 시, index.html 화면을 보여주도록 수정
    
    * fix: 운영체제에 구애받지 않고 파일 경로를 찾을 수 있게 수정

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,22 @@
 package org.apache.coyote.http11;
 
+import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.Optional;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -27,13 +37,23 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+             final BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
 
-            final var responseBody = "Hello world!";
+            final String requestLine = bufferedReader.readLine();
+            if (requestLine == null) return;
+
+            final String requestTarget = requestLine.split("\\s+")[1];
+
+            final String resourcePath = handleRequest(requestTarget);
+
+            final var responseBody = readResource(resourcePath);
+            final String contentType = requestTarget.endsWith(".css") ? "text/css" : "text/html;charset=utf-8";
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Type: " + contentType + " ",
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
                     responseBody);
@@ -42,6 +62,64 @@ public class Http11Processor implements Runnable, Processor {
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        }
+    }
+
+    private String handleRequest(final String requestTarget) {
+        if (requestTarget.equals("/")) {
+            return "static/index.html";
+        }
+
+        if (requestTarget.startsWith("/login")) {
+            if (requestTarget.contains("?")) {
+                loginAndRetrieveUserInfo(requestTarget);
+            }
+            return "static/login.html";
+        }
+
+        return "static" + requestTarget;
+    }
+
+    private String readResource(final String resourcePath) throws IOException {
+        try {
+            final URI resourceURI = Objects.requireNonNull(ClassLoader.getSystemClassLoader().getResource(resourcePath)).toURI();
+            final Path path = Path.of(resourceURI);
+            return Files.readString(path);
+        } catch (NullPointerException e) {
+            log.error("{} 자료가 존재하지 않습니다.", resourcePath);
+        } catch (URISyntaxException e) {
+            log.error(e.getMessage(), e);
+        }
+        return "";
+    }
+
+    private void loginAndRetrieveUserInfo(final String requestURI) {
+        final int index = requestURI.indexOf("?");
+        final String[] loginInfos = requestURI.substring(index + 1).split("&");
+        String account = "";
+        String password = "";
+        for (String loginInfo : loginInfos) {
+            final String[] keyAndValue = loginInfo.split("=");
+            final String key = keyAndValue[0];
+            final String value = keyAndValue[1];
+            if (key.equals("account")) {
+                account = value;
+                continue;
+            }
+            if (key.equals("password")) {
+                password = value;
+            }
+        }
+
+        if (!account.isBlank() && !password.isBlank()) {
+            Optional<User> retrieveResult = InMemoryUserRepository.findByAccount(account);
+            if (retrieveResult.isEmpty()) {
+                return;
+            }
+            final User retrievedUser = retrieveResult.get();
+            if (retrievedUser.checkPassword(password)) {
+                log.info("user : {}", retrievedUser);
+            }
         }
     }
 }

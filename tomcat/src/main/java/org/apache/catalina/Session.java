411da3ddf7b361730878bbb502eed038fa8f0cec
kangrae <kangrae0501@gmail.com>
2026-09-24 20:03:55 +0900

    refactor: 사용하지 않는 Session 메서드 제거

--- tomcat/src/main/java/org/apache/catalina/Session.java
+++ tomcat/src/main/java/org/apache/catalina/Session.java
@@ -8,7 +8,6 @@ public class Session {
     private final String id;
     private final Map<String, Object> values = new HashMap<>();
 
-
     public Session(final String id) {
         this.id = id;
     }
@@ -23,12 +22,5 @@ public class Session {
 
     public void setAttribute(final String name, final Object value) {
         values.put(name, value);
-    }
-
-    public void removeAttribute(final String name) {
-        values.remove(name);
-    }
-
-    public void invalidate() {
     }
 }

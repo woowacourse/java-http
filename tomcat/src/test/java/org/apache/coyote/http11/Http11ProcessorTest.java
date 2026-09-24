71472bf7b838e6d3c2c039817db87618006b6b58
jyt6640 <jyt6640@gmail.com>
2026-09-24 18:02:19 +0900

    test: 정정 fallback 테스트 추가 및 결과값 수정

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -1,28 +1,28 @@
 package org.apache.coyote.http11;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.techcourse.controller.LoginController;
+import com.techcourse.controller.RegisterController;
 import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.model.User;
-import java.util.HashMap;
-import java.util.Map;
-import org.apache.catalina.Session;
-import org.apache.catalina.SessionManager;
-import org.junit.jupiter.api.Test;
-import support.StubSocket;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.apache.catalina.Session;
+import org.apache.catalina.SessionManager;
+import org.apache.coyote.http11.controller.RequestMapping;
+import org.junit.jupiter.api.Test;
+import support.StubSocket;
 
 class Http11ProcessorTest {
 
     @Test
     void process() {
         // given
-        final var socket = new StubSocket();
-        final var processor = new Http11Processor(socket);
+        final StubSocket socket = new StubSocket();
+        final Http11Processor processor = createProcessor(socket);
 
         // when
         processor.process(socket);
@@ -30,14 +30,15 @@ class Http11ProcessorTest {
         // then
         assertThat(socket.output())
                 .contains("HTTP/1.1 200 OK")
-                .contains("Content-Type: text/html;charset=utf-8")
+                .contains("content-type: text/html;charset=utf-8")
                 .contains("Hello world!");
     }
 
     @Test
     void index() throws IOException {
         // given
-        final String httpRequest = String.join("\r\n",
+        final String httpRequest = String.join(
+                "\r\n",
                 "GET /index.html HTTP/1.1",
                 "Host: localhost:8080",
                 "Connection: keep-alive",
@@ -45,8 +46,8 @@ class Http11ProcessorTest {
                 ""
         );
 
-        final var socket = new StubSocket(httpRequest);
-        final var processor = new Http11Processor(socket);
+        final StubSocket socket = new StubSocket(httpRequest);
+        final Http11Processor processor = createProcessor(socket);
 
         // when
         processor.process(socket);
@@ -57,22 +58,27 @@ class Http11ProcessorTest {
                 .getResource("static/index.html");
 
         final String responseBody = new String(
-                Files.readAllBytes(new File(resource.getFile()).toPath())
+                Files.readAllBytes(
+                        new File(resource.getFile()).toPath()
+                )
         );
 
         assertThat(socket.output())
                 .contains("HTTP/1.1 200 OK")
-                .contains("Set-Cookie: JSESSIONID=")
-                .contains("Content-Type: text/html;charset=utf-8")
-                .contains("Content-Length: 5564")
+                .contains("set-cookie: JSESSIONID=")
+                .contains("content-type: text/html;charset=utf-8")
+                .contains("content-length: 5564")
                 .contains(responseBody);
     }
 
     @Test
     void login_success() {
         // given
-        final String body = "account=gugu&password=password";
-        final String httpRequest = String.join("\r\n",
+        final String body =
+                "account=gugu&password=password";
+
+        final String httpRequest = String.join(
+                "\r\n",
                 "POST /login HTTP/1.1",
                 "Content-Length: " + body.length(),
                 "Content-Type: application/x-www-form-urlencoded",
@@ -81,23 +87,26 @@ class Http11ProcessorTest {
                 body
         );
 
-        final var socket = new StubSocket(httpRequest);
-        final var processor = new Http11Processor(socket);
+        final StubSocket socket = new StubSocket(httpRequest);
+        final Http11Processor processor = createProcessor(socket);
 
         // when
         processor.process(socket);
 
         // then
         assertThat(socket.output())
-                .contains("302")
-                .contains("Location: /index.html");
+                .contains("HTTP/1.1 302 Found")
+                .contains("location: /index.html");
     }
 
     @Test
     void login_failure() {
         // given
-        final String body = "account=gugu&password=gugu";
-        final String httpRequest = String.join("\r\n",
+        final String body =
+                "account=gugu&password=gugu";
+
+        final String httpRequest = String.join(
+                "\r\n",
                 "POST /login HTTP/1.1",
                 "Content-Length: " + body.length(),
                 "Content-Type: application/x-www-form-urlencoded",
@@ -106,97 +115,113 @@ class Http11ProcessorTest {
                 body
         );
 
-        final var socket = new StubSocket(httpRequest);
-        final var processor = new Http11Processor(socket);
+        final StubSocket socket = new StubSocket(httpRequest);
+        final Http11Processor processor = createProcessor(socket);
 
         // when
         processor.process(socket);
 
         // then
         assertThat(socket.output())
-                .contains("302")
-                .contains("Location: /401.html");
+                .contains("HTTP/1.1 302 Found")
+                .contains("location: /401.html");
     }
 
     @Test
     void register_success() {
         // given
-        final String body = "account=pobi&password=1234&email=pobi@test.com";
-        final String httpRequest = String.join("\r\n",
+        final String body =
+                "account=pobi&password=1234&email=pobi@test.com";
+
+        final String httpRequest = String.join(
+                "\r\n",
                 "POST /register HTTP/1.1",
                 "Content-Length: " + body.length(),
                 "Content-Type: application/x-www-form-urlencoded",
                 "",
                 body
         );
-        final var socket = new StubSocket(httpRequest);
-        final var processor = new Http11Processor(socket);
+
+        final StubSocket socket = new StubSocket(httpRequest);
+        final Http11Processor processor = createProcessor(socket);
 
         // when
         processor.process(socket);
 
         // then
         assertThat(socket.output())
-                .contains("302")
-                .contains("Location: /index.html");
+                .contains("HTTP/1.1 302 Found")
+                .contains("location: /index.html");
 
-        assertThat(InMemoryUserRepository.findByAccount("pobi"))
-                .isPresent();
+        assertThat(
+                InMemoryUserRepository.findByAccount("pobi")
+        ).isPresent();
     }
 
     @Test
     void jSessionIdIssue_success() {
         // given
-        final String httpRequest = String.join("\r\n",
+        final String httpRequest = String.join(
+                "\r\n",
                 "GET /index.html HTTP/1.1",
                 "Host: localhost:8080",
                 "",
                 ""
         );
 
-        final var socket = new StubSocket(httpRequest);
-        final var processor = new Http11Processor(socket);
+        final StubSocket socket = new StubSocket(httpRequest);
+        final Http11Processor processor = createProcessor(socket);
 
         // when
         processor.process(socket);
 
         // then
         assertThat(socket.output())
-                .contains("Set-Cookie: JSESSIONID=");
+                .contains("set-cookie: JSESSIONID=");
     }
 
     @Test
     void jSessionIdIssue_success_already_have_no_issue() {
         // given
-        final String httpRequest = String.join("\r\n",
+        final String sessionId = "existing-id";
+
+        final Session session = new Session(sessionId);
+        SessionManager.getInstance().add(session);
+
+        final String httpRequest = String.join(
+                "\r\n",
                 "GET /index.html HTTP/1.1",
-                "Cookie: JSESSIONID=existing-id",
+                "Cookie: JSESSIONID=" + sessionId,
                 "",
                 ""
         );
 
-        final var socket = new StubSocket(httpRequest);
-        final var processor = new Http11Processor(socket);
+        final StubSocket socket = new StubSocket(httpRequest);
+        final Http11Processor processor = createProcessor(socket);
 
         // when
         processor.process(socket);
 
         // then
         assertThat(socket.output())
-                .doesNotContain("Set-Cookie: JSESSIONID=");
+                .doesNotContain("set-cookie: JSESSIONID=");
     }
 
     @Test
     void login_success_save_user_in_session() {
         // given
         final String sessionId = "login-session-id";
-        final String body = "account=gugu&password=password";
+        final String body =
+                "account=gugu&password=password";
 
-        final SessionManager sessionManager = SessionManager.getInstance();
+        final SessionManager sessionManager =
+                SessionManager.getInstance();
+
         final Session session = new Session(sessionId);
         sessionManager.add(session);
 
-        final String httpRequest = String.join("\r\n",
+        final String httpRequest = String.join(
+                "\r\n",
                 "POST /login HTTP/1.1",
                 "Cookie: JSESSIONID=" + sessionId,
                 "Content-Length: " + body.length(),
@@ -205,22 +230,25 @@ class Http11ProcessorTest {
                 body
         );
 
-        final var socket = new StubSocket(httpRequest);
-        final var processor = new Http11Processor(socket);
+        final StubSocket socket = new StubSocket(httpRequest);
+        final Http11Processor processor = createProcessor(socket);
 
         // when
         processor.process(socket);
 
         // then
-        final Session savedSession = sessionManager.findSession(sessionId);
-        final User user = (User) savedSession.getAttribute("user");
+        final Session savedSession =
+                sessionManager.findSession(sessionId);
+
+        final User user = (User) savedSession
+                .getAttribute("user");
 
         assertThat(user).isNotNull();
         assertThat(user.getAccount()).isEqualTo("gugu");
 
         assertThat(socket.output())
-                .contains("302")
-                .contains("Location: /index.html");
+                .contains("HTTP/1.1 302 Found")
+                .contains("location: /index.html");
     }
 
     @Test
@@ -229,6 +257,7 @@ class Http11ProcessorTest {
         final String sessionId = "login-session";
 
         final Session session = new Session(sessionId);
+
         final User user = new User(
                 "gugu",
                 "password",
@@ -236,24 +265,82 @@ class Http11ProcessorTest {
         );
 
         session.setAttribute("user", user);
-        SessionManager.getInstance().add(session);
 
-        final String httpRequest = String.join("\r\n",
+        SessionManager.getInstance()
+                .add(session);
+
+        final String httpRequest = String.join(
+                "\r\n",
                 "GET /login HTTP/1.1",
                 "Cookie: JSESSIONID=" + sessionId,
                 "",
                 ""
         );
 
-        final var socket = new StubSocket(httpRequest);
-        final var processor = new Http11Processor(socket);
+        final StubSocket socket = new StubSocket(httpRequest);
+        final Http11Processor processor = createProcessor(socket);
 
         // when
         processor.process(socket);
 
         // then
         assertThat(socket.output())
-                .contains("302")
-                .contains("Location: /index.html");
+                .contains("HTTP/1.1 302 Found")
+                .contains("location: /index.html");
+    }
+
+    @Test
+    void not_logged_in_user_access_login_response_login_page()
+            throws IOException {
+        // given
+        final String httpRequest = String.join(
+                "\r\n",
+                "GET /login HTTP/1.1",
+                "",
+                ""
+        );
+
+        final StubSocket socket = new StubSocket(httpRequest);
+        final Http11Processor processor = createProcessor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        final URL resource = getClass()
+                .getClassLoader()
+                .getResource("static/login.html");
+
+        final String responseBody = new String(
+                Files.readAllBytes(
+                        new File(resource.getFile()).toPath()
+                )
+        );
+
+        assertThat(socket.output())
+                .contains("HTTP/1.1 200 OK")
+                .contains(responseBody);
+    }
+
+    private Http11Processor createProcessor(
+            final StubSocket socket
+    ) {
+        final RequestMapping requestMapping =
+                new RequestMapping();
+
+        requestMapping.addController(
+                "/login",
+                new LoginController()
+        );
+
+        requestMapping.addController(
+                "/register",
+                new RegisterController()
+        );
+
+        return new Http11Processor(
+                socket,
+                requestMapping
+        );
     }
 }

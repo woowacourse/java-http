dce5a54208ac995e88cefd055b71aa74d0b7fffb
koreaioi <koreaioi@gachon.ac.kr>
2026-09-24 23:31:56 +0900

    test: ConcurrencyTest 학습 테스트 추가
    - CopyOnWriteArrayList 사용
    - addIfAbsent사용

--- study/src/test/java/thread/stage1/UserServlet.java
+++ study/src/test/java/thread/stage1/UserServlet.java
@@ -2,19 +2,18 @@ package thread.stage1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class UserServlet {
 
-    private final List<User> users = new ArrayList<>();
+    private final CopyOnWriteArrayList<User> users = new CopyOnWriteArrayList<>();
 
     public void service(final User user) {
         join(user);
     }
 
     private void join(final User user) {
-        if (!users.contains(user)) {
-            users.add(user);
-        }
+        users.addIfAbsent(user);
     }
 
     public int size() {

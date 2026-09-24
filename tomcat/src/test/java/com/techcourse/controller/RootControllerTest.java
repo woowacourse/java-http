df85541de062511c3cfb2c4718a69c640ebc3ce2
나의찬 <cab07036@naver.com>
2026-09-25 01:13:27 +0900

    feat: 루트 경로 요청 컨트롤러 구현
    - 루트 GET 요청에 Hello world 응답
    - 응답 상태와 콘텐츠 타입 및 본문 설정

---
+++ tomcat/src/test/java/com/techcourse/controller/RootControllerTest.java
@@ -0,0 +1,31 @@
+package com.techcourse.controller;
+
+import org.apache.coyote.http11.HttpRequest;
+import org.apache.coyote.http11.HttpResponse;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RootControllerTest {
+
+    @Test
+    void 루트_GET_요청에_Hello_world를_응답한다() throws Exception {
+        final HttpRequest request = HttpRequest.from("GET / HTTP/1.1");
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        final HttpResponse response = new HttpResponse(outputStream);
+        final RootController controller = new RootController();
+
+        controller.service(request, response);
+
+        assertThat(outputStream.toString(StandardCharsets.UTF_8))
+                .isEqualTo(String.join("\r\n",
+                        "HTTP/1.1 200 OK",
+                        "Content-Type: text/html;charset=utf-8",
+                        "Content-Length: 12",
+                        "",
+                        "Hello world!"));
+    }
+}

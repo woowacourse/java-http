5a71209d249f8e48bfd94ca678e908b7eea76ad4
jyt6640 <jyt6640@gmail.com>
2026-09-24 16:37:14 +0900

    feat: ResponseHeaders 구현
    - addHeader 기능
    - createHeaderLines 기능

---
+++ tomcat/src/main/java/org/apache/coyote/http11/response/ResponseHeaders.java
@@ -0,0 +1,47 @@
+package org.apache.coyote.http11.response;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+public class ResponseHeaders {
+
+    private final Map<String, List<String>> headers = new HashMap<>();
+
+    public void addHeader(final String name, final String value) {
+        final String normalizedName = name.toLowerCase(Locale.ROOT);
+
+        headers.computeIfAbsent(
+                normalizedName,
+                key -> new ArrayList<>()
+        ).add(value);
+    }
+
+    public String getHeader(final String name) {
+        final String normalizedName = name.toLowerCase(Locale.ROOT);
+        final List<String> values = headers.get(normalizedName);
+
+        if (values == null || values.isEmpty()) {
+            return null;
+        }
+
+        return values.getFirst();
+    }
+
+    public String toResponseHeaders() {
+        final StringBuilder result = new StringBuilder();
+
+        for (Map.Entry<String, List<String>> entry : headers.entrySet()) {
+            for (String value : entry.getValue()) {
+                result.append(entry.getKey())
+                        .append(": ")
+                        .append(value)
+                        .append("\r\n");
+            }
+        }
+
+        return result.toString();
+    }
+}

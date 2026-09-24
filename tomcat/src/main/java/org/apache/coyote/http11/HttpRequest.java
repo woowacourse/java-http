658bf8739a4b13f962d0ddb465243940520b2dbb
jyt6640 <jyt6640@gmail.com>
2026-09-24 16:12:34 +0900

    feat: RequestHeaders 구현
    - getHeader 기능
    - 대소문자를 소문자로 정렬 기능
    - getContentLength 기능

--- tomcat/src/main/java/org/apache/coyote/http11/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpRequest.java
@@ -3,24 +3,41 @@ package org.apache.coyote.http11;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 
 public class HttpRequest {
 
-    private static final String CONTENT_LENGTH = "content-length";
-
     private final RequestLine requestLine;
-    private final Map<String, List<String>> headers;
+    private final RequestHeaders headers;
     private final RequestBody body;
 
     public HttpRequest(final BufferedReader reader) {
         requestLine = resolveRequestLine(reader);
-        headers = resolveHeaders(reader);
+        headers = new RequestHeaders(reader);
         body = resolveBody(reader);
+    }
+
+    public String getMethod() {
+        return requestLine.getMethod();
+    }
+
+    public String getUri() {
+        return requestLine.getUri();
+    }
+
+    public String getPath() {
+        return requestLine.getPath();
+    }
+
+    public String getVersion() {
+        return requestLine.getVersion();
+    }
+
+    public String getHeader(final String name) {
+        return headers.getHeader(name);
+    }
+
+    public RequestBody getBody() {
+        return body;
     }
 
     private RequestLine resolveRequestLine(final BufferedReader reader) {
@@ -37,39 +54,8 @@ public class HttpRequest {
         }
     }
 
-    private Map<String, List<String>> resolveHeaders(final BufferedReader reader) {
-        final Map<String, List<String>> headers = new HashMap<>();
-
-        try {
-            String line = reader.readLine();
-
-            while (line != null && !line.isBlank()) {
-                final String[] header = line.split(":", 2);
-
-                if (header.length != 2) {
-                    throw new IllegalArgumentException("혀용되지 않는 header line : " + line);
-                }
-
-                final String name = header[0]
-                        .trim()
-                        .toLowerCase(Locale.ROOT);
-
-                final String value = header[1].trim();
-
-                headers.computeIfAbsent(name, key -> new ArrayList<>())
-                        .add(value);
-
-                line = reader.readLine();
-            }
-
-            return headers;
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
-    }
-
     private RequestBody resolveBody(final BufferedReader reader) {
-        final int contentLength = resolveContentLength();
+        final int contentLength = headers.getContentLength();
 
         if (contentLength == 0) {
             return new RequestBody("");
@@ -99,77 +85,5 @@ public class HttpRequest {
             throw new UncheckedIOException(e);
         }
     }
-
-    private int resolveContentLength() {
-        final List<String> values = headers.get(CONTENT_LENGTH);
-
-        if (values == null || values.isEmpty()) {
-            return 0;
-        }
-
-        String contentLength = null;
-
-        for (String headerValue : values) {
-            for (String value : headerValue.split(",")) {
-                final String trimmedValue = value.trim();
-
-                if (contentLength == null) {
-                    contentLength = trimmedValue;
-                    continue;
-                }
-
-                if (!contentLength.equals(trimmedValue)) {
-                    throw new IllegalArgumentException("conflicting Content-Length: " + values);
-                }
-            }
-        }
-
-        if (contentLength == null) {
-            return 0;
-        }
-
-        try {
-            final int length = Integer.parseInt(contentLength);
-
-            if (length < 0) {
-                throw new IllegalArgumentException("Invalid Content-Length: " + contentLength);
-            }
-
-            return length;
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Invalid Content-Length: " + contentLength, e);
-        }
-    }
-
-    public String getMethod() {
-        return requestLine.getMethod();
-    }
-
-    public String getUri() {
-        return requestLine.getUri();
-    }
-
-    public String getPath() {
-        return requestLine.getPath();
-    }
-
-    public String getVersion() {
-        return requestLine.getVersion();
-    }
-
-    public String getHeader(final String name) {
-        final List<String> values = headers.get(
-                name.toLowerCase(Locale.ROOT)
-        );
-
-        if (values == null || values.isEmpty()) {
-            return null;
-        }
-
-        return values.getFirst();
-    }
-
-    public RequestBody getBody() {
-        return body;
-    }
 }
+

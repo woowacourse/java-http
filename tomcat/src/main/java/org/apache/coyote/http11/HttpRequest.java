1db1fa64defdc78bd260bec161189106b6aca6b0
snowshower <wonsb25@gmail.com>
2026-09-25 18:54:00 +0900

    feat: HttpRequest 클래스 구현
    Signed-off-by: snowshower <wonsb25@gmail.com>

---
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpRequest.java
@@ -0,0 +1,59 @@
+package org.apache.coyote.http11;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class HttpRequest {
+
+    private final RequestLine requestLine;
+    private final Map<String, String> headers;
+    private final String body;
+
+    public HttpRequest(RequestLine requestLine, Map<String, String> headers, String body) {
+        validateRequestLine(requestLine);
+        validateHeaders(headers);
+        validateBody(body);
+        this.requestLine = requestLine;
+        this.headers = Map.copyOf(headers);
+        this.body = body;
+    }
+
+    private void validateRequestLine(RequestLine requestLine) {
+        if (requestLine == null) {
+            throw new IllegalArgumentException("[ERROR] RequestLine은 null일 수 없습니다.");
+        }
+    }
+
+    private void validateHeaders(Map<String, String> headers) {
+        if (headers == null) {
+            throw new IllegalArgumentException("[ERROR] HTTP 요청 헤더는 null일 수 없습니다.");
+        }
+
+        for (Entry<String, String> entry : headers.entrySet()) {
+            if (entry.getKey() == null) {
+                throw new IllegalArgumentException("[ERROR] HTTP 요청 헤더 이름은 null일 수 없습니다.");
+            }
+            if (entry.getValue() == null) {
+                throw new IllegalArgumentException("[ERROR] HTTP 요청 헤더 값은 null일 수 없습니다.");
+            }
+        }
+    }
+
+    private void validateBody(String body) {
+        if (body == null) {
+            throw new IllegalArgumentException("[ERROR] HTTP 요청 body는 null일 수 없습니다.");
+        }
+    }
+
+    public RequestLine getRequestLine() {
+        return requestLine;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}

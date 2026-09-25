d7601b30e91d1a2e6fbf8e530c5f6530ead1954d
sinequanonn <dleck288@gmail.com>
2026-09-25 16:55:13 +0900

    feat: HttpStatus 구현

---
+++ tomcat/src/main/java/org/apache/coyote/http11/response/HttpStatus.java
@@ -0,0 +1,27 @@
+package org.apache.coyote.http11.response;
+
+public enum HttpStatus {
+    OK(200, "OK"),
+    FOUND(302, "Found"),
+    BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    NOT_FOUND(404, "Not Found"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    ;
+
+    private final int code;
+    private final String response;
+
+    HttpStatus(final int code, final String response) {
+        this.code = code;
+        this.response = response;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+}

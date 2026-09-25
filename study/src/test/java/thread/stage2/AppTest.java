c8b475d98f1fafa34abb86175708c3ae80fcc778
koreaioi <koreaioi@gachon.ac.kr>
2026-09-25 13:26:53 +0900

    test: AppTest 학습 테스트 추가

--- study/src/test/java/thread/stage2/AppTest.java
+++ study/src/test/java/thread/stage2/AppTest.java
@@ -21,6 +21,35 @@ class AppTest {
      * - http call count
      * - 테스트 결과값
      */
+
+    /**
+     * 기존 application.yml 상황
+     * server:
+     *   tomcat:
+     *     accept-count: 1
+     *     max-connections: 1
+     *     threads:
+     *       min-spare: 2
+     *       max: 2
+     *
+     * 발생 상황
+     * 1. 테스트 thread3은 응답을 받음.
+     * 2. 테스트 thread6은 request time out
+     * 서버에서는 둘 다 응답함.
+     *
+     * 기존에는 accept-count=1 그리고 max-connections=1이라 Tomcat이 관리하는 커넥션이 1개 OS 대기큐에 1개
+     * 커넥션을 끊는 설정이 별도로 없어, default로 keep-alive-timeout, max-keep-alive-requests가 설정된다.
+     * 따라서 별도 요청이 없어도 keep-alive-timeout default동안 Tomcat이 해당 커넥션을 가지고 있는다.
+     *
+     * 이때, TestHttpUtils에서 send()에 timeout 1초를 걸어두었다.
+     * 따라서 client쪽에서 1초 뒤 응답이 없으면 연결을 끊는다.
+     * 이때, Client쪽에서 연결을 끊는 요청을 보내게 된다. thread3에 대한 연결을 끊겠다는 요청은 바로 도달하여 연결이 끊긴다.
+     * 그러나 thread 6에대한 연결을 끊는 요청은, 그 전에 아직 send() 요청이 안읽혀서 연결이 바로 끊기지 않는다.
+     * 그래서 send() 요청이 수행 되고나서 연결이 끊긴다.
+     *
+     * 해결방안
+     * max-connections를 2로 올린다. max-threads 값도 2이기 때문에 가능하다.
+     * */
     @Test
     void test() throws Exception {
         final var NUMBER_OF_THREAD = 10;

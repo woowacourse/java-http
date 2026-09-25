03e33734c45cfe32faa970dc7b0bd8ed354d5221
sinequanonn <dleck288@gmail.com>
2026-09-25 23:35:42 +0900

    feat: 정적 리소스 처리용 StaticResources, StaticResourceController 구현

---
+++ tomcat/src/main/java/org/apache/catalina/controller/StaticResourceController.java
@@ -0,0 +1,31 @@
+package org.apache.catalina.controller;
+
+import org.apache.catalina.util.StaticResources;
+import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.response.ContentType;
+import org.apache.coyote.http11.response.HttpResponse;
+import org.apache.coyote.http11.response.HttpStatus;
+
+import java.util.Optional;
+
+public class StaticResourceController implements Controller {
+
+    private static final String NOT_FOUND_PAGE = "/404.html";
+
+    @Override
+    public void service(HttpRequest request, HttpResponse response) throws Exception {
+        final String path = request.getPath();
+        final Optional<String> body = StaticResources.read(path);
+
+        if (body.isEmpty()) {
+            response.setStatus(HttpStatus.NOT_FOUND);
+            response.setContentType(ContentType.HTML);
+            response.setBody(StaticResources.read(NOT_FOUND_PAGE).orElse(""));
+            return;
+        }
+
+        response.setStatus(HttpStatus.OK);
+        response.setContentType(ContentType.from(path));
+        response.setBody(body.get());
+    }
+}

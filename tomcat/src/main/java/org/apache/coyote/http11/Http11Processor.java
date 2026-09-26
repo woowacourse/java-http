81370a5ceed14ef807f51fecedbeb98eeb1e50b9
Bean <yjb4945@gmail.com>
2026-09-26 16:42:11 +0900

    [2단계 - 로그인 구현하기] 정콩이(유정빈) 미션 제출합니다.  (#1254)
    * feat: 로그인 결과에 따라 302로 리다이렉트
    
    - account와 password로 회원을 조회하고 비밀번호가 맞으면 로그인 성공으로 판단한다
    - 성공하면 /index.html, 실패하면 /401.html을 Location으로 302 응답한다
    - buildResponse가 Location 헤더를 값이 있을 때만 포함하도록 줄 목록으로 조립한다
    
    * feat: POST 방식으로 회원가입과 로그인 처리
    
    * feat: JSESSIONID 쿠키가 없는 요청에 Set-Cookie 응답
    
    - Cookie 헤더를 파싱하는 HttpCookie 클래스를 추가한다
    - JSESSIONID가 없으면 UUID로 만들어 Set-Cookie 헤더로 내려준다
    - Http11ProcessorTest의 요청에 JSESSIONID 쿠키를 넣어 Set-Cookie가 붙지 않는 경우를 검증한다
    
    * feat: 세션으로 로그인 상태를 저장하고 로그인 상태에서 /login 접근 시 index.html로 리다이렉트
    
    - Session과 SessionManager를 추가하고 Manager 인터페이스의 타입을 Session으로 바꾼다
    - 로그인 성공 시 새 세션에 User를 담고 세션 id를 JSESSIONID로 내려준다
    - 쿠키의 JSESSIONID로 세션을 찾아 로그인 상태면 GET /login을 index.html로 리다이렉트한다
    
    * refactor: var에서 Session으로 타입명 변경
    
    * fix: 폼 파라미터가 없거나 형식이 잘못된 요청을 예외 없이 처리
    
    - 이름=값 형식이 아닌 쿼리 조각은 건너뛰어 ArrayIndexOutOfBoundsException을 막는다
    - account나 password가 null이면 조회하지 않고 로그인 실패로 처리한다
    - 회원가입도 필수 값이 없으면 저장하지 않고 /register로 되돌린다
    
    * refactor: SessionManager를 Manager 타입으로 주입받도록 변경
    
    - Connector가 Manager를 하나 들고 Http11Processor 생성자로 넘긴다
    - Http11Processor가 구체 클래스 대신 인터페이스만 알게 되어 구현 교체가 가능해진다
    - 세션 생성 책임을 Manager.createSession으로 옮긴다

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -2,6 +2,7 @@ package org.apache.coyote.http11;
 
 import com.techcourse.db.InMemoryUserRepository;
 import com.techcourse.exception.UncheckedServletException;
+import com.techcourse.model.User;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,8 +13,13 @@ import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import org.apache.catalina.Manager;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,21 +31,36 @@ public class Http11Processor implements Runnable, Processor {
     private static final String CRLF = "\r\n";
     private static final String STATIC_RESOURCE_ROOT = "static";
     private static final String NOT_FOUND_PAGE = "/404.html";
+    private static final String UNAUTHORIZED_PAGE = "/401.html";
+    private static final String INDEX_PAGE = "/index.html";
     private static final String LOGIN_PATH = "/login";
+    private static final String REGISTER_PATH = "/register";
     private static final String ACCOUNT_PARAMETER = "account";
+    private static final String PASSWORD_PARAMETER = "password";
+    private static final String EMAIL_PARAMETER = "email";
 
     private static final String ACCEPT_HEADER = "Accept";
+    private static final String CONTENT_LENGTH_HEADER = "Content-Length";
+    private static final String COOKIE_HEADER = "Cookie";
     private static final String ACCEPT_ANY = "*/*";
     private static final String TEXT_HTML = "text/html;charset=utf-8";
     private static final String TEXT_CSS = "text/css";
 
     private static final String STATUS_OK = "HTTP/1.1 200 OK ";
+    private static final String STATUS_FOUND = "HTTP/1.1 302 Found ";
     private static final String STATUS_NOT_FOUND = "HTTP/1.1 404 Not Found ";
 
-    private final Socket connection;
+    private static final String POST = "POST";
+    private static final String GET = "GET";
 
-    public Http11Processor(final Socket connection) {
+    private static final String USER_ATTRIBUTE = "user";
+
+    private final Socket connection;
+    private final Manager sessionManager;
+
+    public Http11Processor(final Socket connection, final Manager sessionManager) {
         this.connection = connection;
+        this.sessionManager = sessionManager;
     }
 
     @Override
@@ -60,17 +81,54 @@ public class Http11Processor implements Runnable, Processor {
                 return;
             }
             final var requestHeaders = readHeaders(bufferedReader);
+            final var cookie = new HttpCookie(requestHeaders.get(COOKIE_HEADER));
+            var setCookie = "";
+            if (!cookie.hasJSessionId()) {
+                setCookie = HttpCookie.JSESSIONID + "=" + UUID.randomUUID();
+            }
             final var contentType = decideContentType(requestHeaders);
             final var uri = parseUri(requestLine);
+            final var method = parseMethod(requestLine);
             final var pathAndQueryString = splitPathAndQueryString(uri);
             final var path = pathAndQueryString[0];
-            final var queryParameters = parseQueryParameters(pathAndQueryString);
-            logLoginUser(path, queryParameters);
 
             var statusLine = STATUS_OK;
+            var location = "";
             final byte[] responseBody;
-            if (path.equals("/")) {
+            if (path.equals("/")) { // Hello World
                 responseBody = "Hello world!".getBytes();
+            } else if (method.equals(POST) && path.equals(REGISTER_PATH)) { // 회원가입
+                final var requestBody = readBody(bufferedReader, requestHeaders);
+                final var formParameters = parseQueryParameters(requestBody);
+
+                statusLine = STATUS_FOUND;
+
+                if (register(formParameters)) {
+                    location = INDEX_PAGE;
+                } else {
+                    location = REGISTER_PATH;
+                }
+                responseBody = new byte[0];
+            } else if (method.equals(POST) && path.equals(LOGIN_PATH)) {  // 로그인
+                final var requestBody = readBody(bufferedReader, requestHeaders);
+                final var formParameters = parseQueryParameters(requestBody);
+
+                statusLine = STATUS_FOUND;
+                final var loginUser = login(formParameters);
+                if (loginUser.isPresent()) {
+                    final var session = sessionManager.createSession();
+                    session.setAttribute(USER_ATTRIBUTE, loginUser.get());
+                    setCookie = HttpCookie.JSESSIONID + "=" + session.getId();
+                    location = INDEX_PAGE;
+                    log.info("Session id: {} -> {}", session.getId(), session.getAttribute(USER_ATTRIBUTE));
+                } else {
+                    location = UNAUTHORIZED_PAGE;
+                }
+                responseBody = new byte[0];
+            } else if (method.equals(GET) && path.equals(LOGIN_PATH) && isLoggedIn(cookie)) { // GET 로그인
+                statusLine = STATUS_FOUND;
+                location = INDEX_PAGE;
+                responseBody = new byte[0];
             } else {
                 var resourceUrl = findResource(path);
                 if (resourceUrl == null) {
@@ -81,7 +139,7 @@ public class Http11Processor implements Runnable, Processor {
                 responseBody = Files.readAllBytes(Path.of(resourceUrl.toURI()));
             }
 
-            final var response = buildResponse(statusLine, contentType, responseBody);
+            final var response = buildResponse(statusLine, location, setCookie, contentType, responseBody);
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
@@ -89,6 +147,18 @@ public class Http11Processor implements Runnable, Processor {
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private boolean isLoggedIn(HttpCookie cookie) throws IOException {
+        if (!cookie.hasJSessionId()) {
+            return false;
+        }
+        final var jSessionId = cookie.getJSessionId();
+        final var session = sessionManager.findSession(jSessionId);
+        if (session == null) {
+            return false;
+        }
+        return session.getAttribute(USER_ATTRIBUTE) != null;
     }
 
     private String readRequestLine(final BufferedReader reader) throws IOException {
@@ -104,8 +174,16 @@ public class Http11Processor implements Runnable, Processor {
             final String[] nameAndValue = headerLine.split(":", 2);
             headers.put(nameAndValue[0], nameAndValue[1].trim());
         }
-        log.info("headers: {}", headers);
+        log.info("requestHeaders: {}", headers);
         return headers;
+    }
+
+    private String readBody(final BufferedReader reader, final Map<String, String> requestHeaders)
+            throws IOException {
+        final int contentLength = Integer.parseInt(requestHeaders.get(CONTENT_LENGTH_HEADER));
+        final char[] buffer = new char[contentLength];
+        reader.read(buffer, 0, contentLength);
+        return new String(buffer);
     }
 
     private String decideContentType(final Map<String, String> requestHeaders) {
@@ -121,29 +199,54 @@ public class Http11Processor implements Runnable, Processor {
         return tokens[1];
     }
 
+    private String parseMethod(final String requestLine) {
+        final String[] tokens = requestLine.split(" ");
+        return tokens[0];
+    }
+
     private String[] splitPathAndQueryString(final String uri) {
         return uri.split("\\?", 2);
     }
 
-    private Map<String, String> parseQueryParameters(final String[] pathAndQueryString) {
-        final Map<String, String> queryParameters = new LinkedHashMap<>();
-        if (pathAndQueryString.length < 2) {
-            return queryParameters;
+    private Map<String, String> parseQueryParameters(final String queryString) {
+        final Map<String, String> parameters = new LinkedHashMap<>();
+        if (queryString.isEmpty()) {
+            return parameters;
         }
-        final String[] pairs = pathAndQueryString[1].split("&");
+        final String[] pairs = queryString.split("&");
         for (int i = 0; i < pairs.length; i++) {
             final String[] nameAndValue = pairs[i].split("=", 2);
-            queryParameters.put(URLDecoder.decode(nameAndValue[0], StandardCharsets.UTF_8),
-                    URLDecoder.decode(nameAndValue[1], StandardCharsets.UTF_8));
+            if (nameAndValue.length == 2) {
+                parameters.put(URLDecoder.decode(nameAndValue[0], StandardCharsets.UTF_8),
+                        URLDecoder.decode(nameAndValue[1], StandardCharsets.UTF_8));
+            }
         }
-        return queryParameters;
+        return parameters;
     }
 
-    private void logLoginUser(final String path, final Map<String, String> queryParameters) {
-        if (path.equals(LOGIN_PATH) && queryParameters.containsKey(ACCOUNT_PARAMETER)) {
-            InMemoryUserRepository.findByAccount(queryParameters.get(ACCOUNT_PARAMETER))
-                    .ifPresent(user -> log.info("user: {}", user));
+    private boolean register(final Map<String, String> formParameters) {
+        final var account = formParameters.get(ACCOUNT_PARAMETER);
+        final var password = formParameters.get(PASSWORD_PARAMETER);
+        final var email = formParameters.get(EMAIL_PARAMETER);
+        if (account == null || password == null || email == null) {
+            return false;
         }
+        final var user = new User(account, password, email);
+        InMemoryUserRepository.save(user);
+        log.info("register success: {}", user);
+        return true;
+    }
+
+    private Optional<User> login(final Map<String, String> formParameters) {
+        final var account = formParameters.get(ACCOUNT_PARAMETER);
+        final var password = formParameters.get(PASSWORD_PARAMETER);
+        if (account == null || password == null) {
+            return Optional.empty();
+        }
+        final var user = InMemoryUserRepository.findByAccount(account)
+                .filter(found -> found.checkPassword(password));
+        user.ifPresent(found -> log.info("login success: {}", found));
+        return user;
     }
 
     private URL findResource(final String path) {
@@ -153,12 +256,20 @@ public class Http11Processor implements Runnable, Processor {
         return getClass().getClassLoader().getResource(STATIC_RESOURCE_ROOT + path + ".html");
     }
 
-    private String buildResponse(final String statusLine, final String contentType, final byte[] body) {
-        return String.join(CRLF,
-                statusLine,
-                "Content-Type: " + contentType + " ",
-                "Content-Length: " + body.length + " ",
-                "",
-                new String(body, StandardCharsets.UTF_8));
+    private String buildResponse(final String statusLine, final String location, final String setCookie,
+                                 final String contentType, final byte[] body) {
+        final List<String> lines = new ArrayList<>();
+        lines.add(statusLine);
+        if (!location.isEmpty()) {
+            lines.add("Location: " + location + " ");
+        }
+        if (!setCookie.isEmpty()) {
+            lines.add("Set-Cookie: " + setCookie + " ");
+        }
+        lines.add("Content-Type: " + contentType + " ");
+        lines.add("Content-Length: " + body.length + " ");
+        lines.add("");
+        lines.add(new String(body, StandardCharsets.UTF_8));
+        return String.join(CRLF, lines);
     }
 }
--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -1,22 +1,29 @@
 package org.apache.coyote.http11;
 
-import org.junit.jupiter.api.Test;
-import support.StubSocket;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.apache.catalina.SessionManager;
+import org.junit.jupiter.api.Test;
+import support.StubSocket;
 
 class Http11ProcessorTest {
 
     @Test
     void process() {
         // given
-        final var socket = new StubSocket();
-        final var processor = new Http11Processor(socket);
+        final String httpRequest = String.join("\r\n",
+                "GET / HTTP/1.1",
+                "Host: localhost:8080",
+                "Cookie: JSESSIONID=656cef62-e3c4-40bc-a8df-94732920ed46",
+                "",
+                "");
+
+        final var socket = new StubSocket(httpRequest);
+        final var processor = new Http11Processor(socket, SessionManager.getInstance());
 
         // when
         processor.process(socket);
@@ -35,15 +42,16 @@ class Http11ProcessorTest {
     @Test
     void index() throws IOException {
         // given
-        final String httpRequest= String.join("\r\n",
+        final String httpRequest = String.join("\r\n",
                 "GET /index.html HTTP/1.1 ",
                 "Host: localhost:8080 ",
                 "Connection: keep-alive ",
+                "Cookie: JSESSIONID=656cef62-e3c4-40bc-a8df-94732920ed46 ",
                 "",
                 "");
 
         final var socket = new StubSocket(httpRequest);
-        final Http11Processor processor = new Http11Processor(socket);
+        final Http11Processor processor = new Http11Processor(socket, SessionManager.getInstance());
 
         // when
         processor.process(socket);
@@ -53,7 +61,7 @@ class Http11ProcessorTest {
         var expected = "HTTP/1.1 200 OK \r\n" +
                 "Content-Type: text/html;charset=utf-8 \r\n" +
                 "Content-Length: 5564 \r\n" +
-                "\r\n"+
+                "\r\n" +
                 new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
 
         assertThat(socket.output()).isEqualTo(expected);

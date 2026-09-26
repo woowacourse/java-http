3e71106c37a7e735ea3874c709fe7065b93c3f27
kkb <gimgyubin1225@gmail.com>
2026-09-27 00:13:27 +0900

    feat: Thread Pool 적용

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Connector implements Runnable {
 
@@ -16,19 +18,22 @@ public class Connector implements Runnable {
 
     private static final int DEFAULT_PORT = 8080;
     private static final int DEFAULT_ACCEPT_COUNT = 100;
+    private static final int DEFAULT_MAX_THREAD = 20;
 
     private final ServerSocket serverSocket;
     private final Adapter adapter;
     private boolean stopped;
+    private final ExecutorService executorService;
 
     public Connector(final Adapter adapter) {
-        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT, adapter);
+        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT, adapter, DEFAULT_MAX_THREAD);
     }
 
-    public Connector(final int port, final int acceptCount, final Adapter adapter) {
+    public Connector(final int port, final int acceptCount, final Adapter adapter, final int maxThreads) {
         this.serverSocket = createServerSocket(port, acceptCount);
         this.adapter = adapter;
         this.stopped = false;
+        this.executorService = Executors.newFixedThreadPool(maxThreads);
     }
 
     private ServerSocket createServerSocket(final int port, final int acceptCount) {
@@ -70,11 +75,12 @@ public class Connector implements Runnable {
             return;
         }
         var processor = new Http11Processor(connection, adapter);
-        new Thread(processor).start();
+        executorService.execute(processor);
     }
 
     public void stop() {
         stopped = true;
+        executorService.shutdown();
         try {
             serverSocket.close();
         } catch (IOException e) {

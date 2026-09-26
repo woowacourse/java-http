ebd6b613605aaa1e48bf24c5dbc78b732de410d1
poketopa <lhs5427ll@gmail.com>
2026-09-26 15:43:29 +0900

    fix: SVG 정적 리소스의 Content-Type 지정

--- tomcat/src/main/java/org/apache/coyote/http11/ContentType.java
+++ tomcat/src/main/java/org/apache/coyote/http11/ContentType.java
@@ -4,6 +4,7 @@ public enum ContentType {
     HTML("text/html;charset=UTF-8"),
     CSS("text/css;charset=UTF-8"),
     JAVASCRIPT("text/javascript;charset=UTF-8"),
+    SVG("image/svg+xml"),
     TEXT("text/plain;charset=UTF-8");
 
     private final String value;
@@ -25,6 +26,9 @@ public enum ContentType {
         }
         if (path.endsWith(".js")) {
             return JAVASCRIPT;
+        }
+        if (path.endsWith(".svg")) {
+            return SVG;
         }
         return TEXT;
     }

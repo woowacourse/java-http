a95f5d2177318e08d2264f3c0c3f716be86b2e66
ghkdtjfxkd <syt1128@gmail.com>
2026-09-26 16:38:10 +0900

    test: 테스트 실행 환경 및 기존 테스트 수정

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -22,14 +22,12 @@ class Http11ProcessorTest {
         processor.process(socket);
 
         // then
-        var expected = String.join("\r\n",
-                "HTTP/1.1 200 OK ",
-                "Content-Type: text/html;charset=utf-8 ",
-                "Content-Length: 12 ",
-                "",
-                "Hello world!");
-
-        assertThat(socket.output()).isEqualTo(expected);
+        assertThat(socket.output())
+                .startsWith("HTTP/1.1 200 OK \r\n")
+                .contains("Set-Cookie: JSESSIONID=")
+                .contains("Content-Type: text/html;charset=utf-8 \r\n")
+                .contains("Content-Length: 12 \r\n")
+                .endsWith("\r\n\r\nHello world!");
     }
 
     @Test
@@ -50,12 +48,12 @@ class Http11ProcessorTest {
 
         // then
         final URL resource = getClass().getClassLoader().getResource("static/index.html");
-        var expected = "HTTP/1.1 200 OK \r\n" +
-                "Content-Type: text/html;charset=utf-8 \r\n" +
-                "Content-Length: 5564 \r\n" +
-                "\r\n"+
-                new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+        final String body = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
 
-        assertThat(socket.output()).isEqualTo(expected);
+        assertThat(socket.output())
+                .startsWith("HTTP/1.1 200 OK \r\n")
+                .contains("Content-Type: text/html;charset=utf-8 \r\n")
+                .contains("Content-Length: 5564 \r\n")
+                .endsWith("\r\n\r\n" + body);
     }
 }

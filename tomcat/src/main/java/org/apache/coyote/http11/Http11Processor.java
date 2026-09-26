ab56864e6fb7642345b3eb8c5a74f6a9c4f314a0
wontop02 <wjy2002kr@gmail.com>
2026-09-26 22:44:07 +0900

    refactor: HttpRequest, HttpResponse 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -7,13 +7,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
@@ -42,164 +35,93 @@ public class Http11Processor implements Runnable, Processor {
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
-            final String request = readRequest(reader);
-            log.info(request);
+            final HttpRequest request = new HttpRequest(reader);
+            final HttpResponse response = new HttpResponse(request.getProtocolVersion());
 
-            String method = request.split(" ")[0];
-            String uri = request.split(" ")[1];
-            String body = readRequestBody(method, request, reader);
-            log.info("body: {}", body);
-            final String type = findType(uri);
-            String status = "200 OK";
-            HttpCookie httpCookie = new HttpCookie(findCookies(request));
-            boolean hasJSessionId = hasJSessionId(httpCookie);
-            String jSessionId = httpCookie.get("JSESSIONID");
-            String setCookie = "";
+            String path = request.getPath();
+            response.setPath(request.getPath());
+            String jSessionId = "";
+
+            if (request.hasHeader("Cookie")) {
+                HttpCookie httpCookie = new HttpCookie(request.getHeaderValue("Cookie"));
+                if (hasJSessionId(httpCookie)) {
+                    jSessionId = httpCookie.get("JSESSIONID");
+                }
+            }
+
+            boolean hasJSessionId = !jSessionId.isEmpty();
 
             final SessionManager manager = SessionManager.getInstance();
 
-            if (uri.equals("/login") && method.equals("GET")) {
+            if (path.equals("/login") && request.getMethod().equals("GET")) {
                 if (hasJSessionId) {
                     log.info("쿠키 존재!");
-                    String id = httpCookie.get("JSESSIONID");
-                    log.info("session: {}", manager.findSession(id));
-                    if (manager.findSession(id) != null) {
+                    log.info("session: {}", manager.findSession(jSessionId));
+                    if (manager.findSession(jSessionId) != null) {
                         log.info("세션 존재!");
-                        uri = "/index.html";
+                        response.setStatus("302 FOUND");
+                        response.addHeader("Location", "/index.html");
                     }
                 }
             }
 
-            if (!body.isEmpty()) {
-                Map<String, String> pairs = findQueries(body);
-                if (uri.equals("/login")) {
-                    if (userMatching(pairs.get("account"), pairs.get("password"))) {
-                        log.info("로그인 성공! id: {}", pairs.get("account"));
-                        if (manager.findSession(jSessionId) == null) {
-                            jSessionId = UUID.randomUUID().toString();
-                            String account = pairs.get("account");
-                            Session session = new Session(jSessionId);
-                            boolean isPresent = InMemoryUserRepository.findByAccount(account).isPresent();
-                            if (isPresent) {
-                                User user = InMemoryUserRepository.findByAccount(account).get();
-                                session.setAttribute("user", user);
-                                manager.add(session);
-                                setCookie = "Set-Cookie: JSESSIONID=" + jSessionId + " ";
-                                log.info("set cookie: {}", setCookie);
-                            }
-                        }
-                        status = "302 FOUND";
-                        uri = "/index.html";
-                    } else {
-                        uri = "/401.html";
-                    }
+            if (request.hasBody()) {
+                if (path.equals("/login")) {
+                    login(request, response, manager, jSessionId);
                 } else {
-                    User user = new User(pairs.get("account"), pairs.get("password"), pairs.get("email"));
+                    User user = new User(request.getBodyValue("account"), request.getBodyValue("password"),
+                            request.getBodyValue("email"));
                     log.info("user: {}", user);
                     InMemoryUserRepository.save(user);
-                    uri = "/index.html";
+                    response.setStatus("302 FOUND");
+                    response.addHeader("Location", "/index.html");
                 }
             }
 
-            log.info("uri: {}", uri);
+            log.info("uri: {}", path);
 
-            final String responseBody = makeResponseBody(uri);
-            final String response = makeResponse(status, type, responseBody, setCookie);
+            String responseValue = response.response();
 
-            outputStream.write(response.getBytes());
+            outputStream.write(responseValue.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
     }
 
-    private String readRequest(BufferedReader reader) throws IOException {
-        final StringBuilder builder = new StringBuilder();
-
-        String line;
-        while (!(line = reader.readLine()).isBlank()) {
-            builder.append(line);
-            builder.append("\r\n");
+    private void login(HttpRequest request, HttpResponse response, SessionManager manager, String jSessionId) {
+        String account = request.getBodyValue("account");
+        String password = request.getBodyValue("password");
+        if (userMatching(account, password)) {
+            if (manager.findSession(jSessionId) == null) {
+                addSession(account, response, manager);
+            }
+            response.addHeader("Location", "/index.html");
+        } else {
+            response.addHeader("Location", "/401.html");
         }
-        return builder.toString();
+        response.setStatus("302 FOUND");
     }
 
-    private String readRequestBody(String method, String request, BufferedReader reader) throws IOException {
-        if (!method.equals("POST")) {
-            return "";
+    private void addSession(String account, HttpResponse response, SessionManager manager) {
+        String jSessionId = UUID.randomUUID().toString();
+        Session session = new Session(jSessionId);
+        boolean isPresent = InMemoryUserRepository.findByAccount(account).isPresent();
+        if (isPresent) {
+            User user = InMemoryUserRepository.findByAccount(account).get();
+            session.setAttribute("user", user);
+            manager.add(session);
+            response.addHeader("Set-Cookie", "JSESSIONID=" + jSessionId + " ");
         }
-        String bodyLength = request.split("Content-Length:", 2)[1].trim();
-        bodyLength = bodyLength.split("\r\n")[0].trim();
-        int length = Integer.parseInt(bodyLength);
-        char[] body = new char[length];
-        reader.read(body, 0, length);
-        return new String(body);
-    }
-
-    private String findType(String uri) {
-        if (uri.contains(".")) {
-            return List.of(uri.split("\\.")).getLast();
-        }
-        return "html";
     }
 
     private boolean hasJSessionId(HttpCookie httpCookie) {
         return httpCookie.containsKey("JSESSIONID");
     }
 
-    private String findCookies(String request) {
-        if (!request.contains("Cookie")) {
-            return "";
-        }
-        String allCookies = request.split("Cookie: ", 2)[1];
-        log.info("allCookies: {}", allCookies);
-        return allCookies.trim();
-    }
-
-    private Map<String, String> findQueries(String queryString) {
-        final List<String> queries = List.of(queryString.split("&"));
-        final Map<String, String> pairs = new HashMap<>();
-
-        for (String query : queries) {
-            String[] pair = query.split("=", 2);
-            pairs.put(pair[0], pair[1]);
-        }
-        return pairs;
-    }
-
     private boolean userMatching(String account, String password) {
         return InMemoryUserRepository.findByAccount(account)
                 .filter(user -> user.checkPassword(password))
                 .isPresent();
-    }
-
-    private String makeResponseBody(String uri) throws IOException {
-        if (uri.equals("/") || uri.isBlank()) {
-            uri = "/index.html";
-        }
-        if (uri.equals("/login") || uri.equals("/register")) {
-            uri = uri + ".html";
-        }
-        final URL resource = getClass().getClassLoader().getResource("static" + uri);
-
-        final String filePath = resource.getFile();
-        final Path path = Paths.get(filePath);
-
-        return Files.readString(path);
-    }
-
-    private String makeResponse(String status, String type, String responseBody, String setCookie) {
-        String response = String.join("\r\n",
-                "HTTP/1.1 " + status + " ",
-                "Content-Type: text/" + type + ";charset=utf-8 ",
-                "Content-Length: " + responseBody.getBytes().length + " ");
-
-        if (!setCookie.isBlank()) {
-            response = String.join("\r\n", response, setCookie);
-        }
-
-        response = String.join("\r\n", response, "", responseBody);
-
-        return response;
     }
 }
---
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpResponse.java
@@ -0,0 +1,84 @@
+package org.apache.coyote.http11;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HttpResponse {
+
+    private final String protocolVersion;
+    private String status;
+    private final Map<String, String> headers = new LinkedHashMap<>();
+    private String path;
+
+    public HttpResponse(String protocolVersion) {
+        this.protocolVersion = protocolVersion;
+        this.status = "200 OK";
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void addHeader(String key, String value) {
+        this.headers.put(key, value);
+    }
+
+    public String response() throws IOException {
+        String responseBody = makeResponseBody();
+        addContentType();
+
+        headers.put("Content-Length", responseBody.getBytes().length + "");
+
+        String response = String.join("\r\n",
+                protocolVersion.trim() + " " + status.trim() + " ",
+                responseHeaders()
+        );
+
+        response = String.join("\r\n", response, responseBody);
+
+        return response;
+    }
+
+    private void addContentType() {
+        String type = "html";
+        if (path.contains(".")) {
+            type = List.of(path.split("\\.")).getLast();
+        }
+        headers.put("Content-Type", "text/" + type + ";charset=utf-8");
+    }
+
+    private String responseHeaders() {
+        StringBuilder builder = new StringBuilder();
+        for (Map.Entry<String, String> header : headers.entrySet()) {
+            builder.append(header.getKey()).append(": ");
+            builder.append(header.getValue()).append(" ");
+            builder.append("\r\n");
+        }
+        return builder.toString();
+    }
+
+    private String makeResponseBody() throws IOException {
+        if (path.equals("/")) {
+            return "Hello world!";
+        }
+        if (path.equals("/login") || path.equals("/register")) {
+            path = path + ".html";
+        }
+        final URL resource = getClass().getClassLoader().getResource("static" + path);
+
+        final String filePath = resource.getFile();
+        final Path path = Paths.get(filePath);
+
+        return Files.readString(path);
+    }
+}

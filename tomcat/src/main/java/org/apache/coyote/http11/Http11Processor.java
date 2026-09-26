e6ec2afac3513fe7711b461c01c5ae7363fbea09
xxbeann <yjb4945@gmail.com>
2026-09-26 20:57:43 +0900

    refactor: 요청 파싱을 HttpRequest로 분리
    - 요청 줄, 헤더, 바디 읽기와 파싱을 HttpRequest.from으로 옮긴다
    - 질의 문자열과 폼 바디 파라미터를 한 맵으로 합쳐 getParameters로 제공한다
    - Http11Processor에서 파싱 메서드 일곱 개를 제거한다

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -9,12 +9,10 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -40,7 +38,6 @@ public class Http11Processor implements Runnable, Processor {
     private static final String EMAIL_PARAMETER = "email";
 
     private static final String ACCEPT_HEADER = "Accept";
-    private static final String CONTENT_LENGTH_HEADER = "Content-Length";
     private static final String COOKIE_HEADER = "Cookie";
     private static final String ACCEPT_ANY = "*/*";
     private static final String TEXT_HTML = "text/html;charset=utf-8";
@@ -76,21 +73,20 @@ public class Http11Processor implements Runnable, Processor {
              final var bufferedReader = new BufferedReader(inputStreamReader);
              final var outputStream = connection.getOutputStream()) {
 
-            final var requestLine = readRequestLine(bufferedReader);
-            if (requestLine == null) {
+            final var request = HttpRequest.from(bufferedReader);
+            log.info("HttpRequest: {}", request);
+            if (request == null) {
                 return;
             }
-            final var requestHeaders = readHeaders(bufferedReader);
-            final var cookie = new HttpCookie(requestHeaders.get(COOKIE_HEADER));
+
+            final var cookie = new HttpCookie(request.getHeader(COOKIE_HEADER));
             var setCookie = "";
             if (!cookie.hasJSessionId()) {
                 setCookie = HttpCookie.JSESSIONID + "=" + UUID.randomUUID();
             }
-            final var contentType = decideContentType(requestHeaders);
-            final var uri = parseUri(requestLine);
-            final var method = parseMethod(requestLine);
-            final var pathAndQueryString = splitPathAndQueryString(uri);
-            final var path = pathAndQueryString[0];
+            final var contentType = decideContentType(request.getHeader(ACCEPT_HEADER));
+            final var method = request.getMethod();
+            final var path = request.getPath();
 
             var statusLine = STATUS_OK;
             var location = "";
@@ -98,8 +94,7 @@ public class Http11Processor implements Runnable, Processor {
             if (path.equals("/")) { // Hello World
                 responseBody = "Hello world!".getBytes();
             } else if (method.equals(POST) && path.equals(REGISTER_PATH)) { // 회원가입
-                final var requestBody = readBody(bufferedReader, requestHeaders);
-                final var formParameters = parseQueryParameters(requestBody);
+                final var formParameters = request.getParameters();
 
                 statusLine = STATUS_FOUND;
 
@@ -110,8 +105,7 @@ public class Http11Processor implements Runnable, Processor {
                 }
                 responseBody = new byte[0];
             } else if (method.equals(POST) && path.equals(LOGIN_PATH)) {  // 로그인
-                final var requestBody = readBody(bufferedReader, requestHeaders);
-                final var formParameters = parseQueryParameters(requestBody);
+                final var formParameters = request.getParameters();
 
                 statusLine = STATUS_FOUND;
                 final var loginUser = login(formParameters);
@@ -161,67 +155,11 @@ public class Http11Processor implements Runnable, Processor {
         return session.getAttribute(USER_ATTRIBUTE) != null;
     }
 
-    private String readRequestLine(final BufferedReader reader) throws IOException {
-        final var requestLine = reader.readLine();
-        log.info("requestLine: {}", requestLine);
-        return requestLine;
-    }
-
-    private Map<String, String> readHeaders(final BufferedReader reader) throws IOException {
-        final Map<String, String> headers = new LinkedHashMap<>();
-        String headerLine;
-        while ((headerLine = reader.readLine()) != null && !headerLine.isEmpty()) {
-            final String[] nameAndValue = headerLine.split(":", 2);
-            headers.put(nameAndValue[0], nameAndValue[1].trim());
-        }
-        log.info("requestHeaders: {}", headers);
-        return headers;
-    }
-
-    private String readBody(final BufferedReader reader, final Map<String, String> requestHeaders)
-            throws IOException {
-        final int contentLength = Integer.parseInt(requestHeaders.get(CONTENT_LENGTH_HEADER));
-        final char[] buffer = new char[contentLength];
-        reader.read(buffer, 0, contentLength);
-        return new String(buffer);
-    }
-
-    private String decideContentType(final Map<String, String> requestHeaders) {
-        final var accept = requestHeaders.getOrDefault(ACCEPT_HEADER, ACCEPT_ANY);
-        if (accept.contains(TEXT_CSS)) {
+    private String decideContentType(final String accept) {
+        if (accept != null && accept.contains(TEXT_CSS)) {
             return TEXT_CSS;
         }
         return TEXT_HTML;
-    }
-
-    private String parseUri(final String requestLine) {
-        final String[] tokens = requestLine.split(" ");
-        return tokens[1];
-    }
-
-    private String parseMethod(final String requestLine) {
-        final String[] tokens = requestLine.split(" ");
-        return tokens[0];
-    }
-
-    private String[] splitPathAndQueryString(final String uri) {
-        return uri.split("\\?", 2);
-    }
-
-    private Map<String, String> parseQueryParameters(final String queryString) {
-        final Map<String, String> parameters = new LinkedHashMap<>();
-        if (queryString.isEmpty()) {
-            return parameters;
-        }
-        final String[] pairs = queryString.split("&");
-        for (int i = 0; i < pairs.length; i++) {
-            final String[] nameAndValue = pairs[i].split("=", 2);
-            if (nameAndValue.length == 2) {
-                parameters.put(URLDecoder.decode(nameAndValue[0], StandardCharsets.UTF_8),
-                        URLDecoder.decode(nameAndValue[1], StandardCharsets.UTF_8));
-            }
-        }
-        return parameters;
     }
 
     private boolean register(final Map<String, String> formParameters) {

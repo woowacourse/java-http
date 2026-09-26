3f2baf157e497e144b6c5eb06e856cf16476472f
2jaeheon <lchc.dev@gmail.com>
2026-09-26 09:55:43 +0900

    feat: RequestLine에서 HTTP version과 path 파싱
    Signed-off-by: 2jaeheon <lchc.dev@gmail.com>

--- tomcat/src/main/java/org/apache/coyote/http11/RequestLine.java
+++ tomcat/src/main/java/org/apache/coyote/http11/RequestLine.java
@@ -1,10 +1,23 @@
 package org.apache.coyote.http11;
 
-public record RequestLine(String method, String target) {
-
+public record RequestLine(
+        String method,
+        String target,
+        String version
+) {
     public static RequestLine parse(String text) {
         String[] parts = text.split(" ", 3);
 
-        return new RequestLine(parts[0], parts[1]);
+        return new RequestLine(parts[0], parts[1], parts[2]);
+    }
+
+    public String path() {
+        int queryIndex = target.indexOf("?");
+
+        if (queryIndex == -1) {
+            return target;
+        }
+
+        return target.substring(0, queryIndex);
     }
 }

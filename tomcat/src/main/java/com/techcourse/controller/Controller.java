fe9080f78e3118f870d50024a41b5bc8f9525f8c
2jaeheon <lchc.dev@gmail.com>
2026-09-26 12:36:21 +0900

    feat: 요청 처리를 위한 Controller 인터페이스 추가
    Signed-off-by: 2jaeheon <lchc.dev@gmail.com>

---
+++ tomcat/src/main/java/com/techcourse/controller/Controller.java
@@ -0,0 +1,9 @@
+package com.techcourse.controller;
+
+import java.io.IOException;
+import org.apache.coyote.http11.HttpRequest;
+import org.apache.coyote.http11.HttpResponse;
+
+public interface Controller {
+    HttpResponse handle(HttpRequest request) throws IOException;
+}
